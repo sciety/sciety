2457609c4634138b83413ca6f653fd8da026d4c8
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-04-17 10:26:08 +0100

    Gracefully shut the server down

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,9 +1,22 @@
+import { createTerminus, TerminusOptions } from '@godaddy/terminus';
 import http, { IncomingMessage, ServerResponse } from 'http';
 import handler from 'serve-handler';
 
 const server = http.createServer(async (request: IncomingMessage, response: ServerResponse): Promise<void> => {
   await handler(request, response, { public: 'static' });
 })
+
+const terminusOptions: TerminusOptions = {
+  onShutdown: async (): Promise<void> => {
+    console.log('Server shutting down')
+  },
+  onSignal: async (): Promise<void> => {
+    console.log('Signal received')
+  },
+  signals: ['SIGINT', 'SIGTERM'],
+};
+
+createTerminus(server, terminusOptions);
 
 server.listen(80, (): void => {
   console.log('Server running');
