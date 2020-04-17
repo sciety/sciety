61904a89e473844ff517ca379d415526e5a118be
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-04-17 16:10:29 +0100

    Extract ping handler

---
+++ frontend/src/handlers/ping.ts
@@ -0,0 +1,12 @@
+import { Handler, HTTPVersion } from 'find-my-way';
+import { IncomingMessage, ServerResponse } from 'http';
+import { OK } from 'http-status-codes';
+
+export default (): Handler<HTTPVersion.V1> => (
+  (request: IncomingMessage, response: ServerResponse): void => {
+    response.setHeader('Cache-Control', 'no-store, must-revalidate');
+    response.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+    response.writeHead(OK);
+    response.end('pong');
+  }
+);
