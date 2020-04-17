3b9ded7c983ce4af69a6b44f6e87ef00048ee976
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-04-17 10:31:35 +0100

    Add better logging

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,17 +1,28 @@
 import { createTerminus, TerminusOptions } from '@godaddy/terminus';
+import debug from 'debug';
 import http, { IncomingMessage, ServerResponse } from 'http';
 import handler from 'serve-handler';
 
+const log = debug('http:server');
+const requestLog = log.extend('request');
+const responseLog = log.extend('response');
+
+log('Starting server');
+
 const server = http.createServer(async (request: IncomingMessage, response: ServerResponse): Promise<void> => {
+  requestLog(`${request.method} ${request.url} HTTP/${request.httpVersion}`);
+
   await handler(request, response, { public: 'static' });
+
+  responseLog(`HTTP/${request.httpVersion} ${response.statusCode} ${response.statusMessage}`);
 })
 
 const terminusOptions: TerminusOptions = {
   onShutdown: async (): Promise<void> => {
-    console.log('Server shutting down')
+    log('Server shutting down')
   },
   onSignal: async (): Promise<void> => {
-    console.log('Signal received')
+    log('Signal received')
   },
   signals: ['SIGINT', 'SIGTERM'],
 };
@@ -19,5 +30,5 @@ const terminusOptions: TerminusOptions = {
 createTerminus(server, terminusOptions);
 
 server.listen(80, (): void => {
-  console.log('Server running');
+  log('Server running');
 });
