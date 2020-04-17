bb9dc6ba4d008def4c7783d5adff6443ba45eaff
Giorgio Sironi <g.sironi@elifesciences.org>
2020-04-17 14:08:49 +0100

    Extend tests to 3 HTML pages

--- frontend/test/index.test.ts
+++ frontend/test/index.test.ts
@@ -2,8 +2,12 @@ import request from 'supertest';
 import createServer from '../src/server';
 
 describe('the application', (): void => {
-  it.each(['/'])('should respond with 200 OK on %s', async (path: string): Promise<void> => {
-    const response = await request(createServer()).get(path).set('Accept', '*/*');
+  it.each([
+    '/',
+    '/article1.html',
+    '/article2.html',
+  ])('should respond with 200 OK on %s', async (path: string): Promise<void> => {
+    const response = await request(createServer()).get(path).redirects(1);
 
     expect(response.status).toBe(200);
   });
