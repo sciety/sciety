84209bf9dff65e697c1c48c955a337df04c32161
Giorgio Sironi <g.sironi@elifesciences.org>
2020-04-21 10:09:04 +0100

    Add link to full rewview in review-summary template

--- frontend/src/templates/review-summary.ts
+++ frontend/src/templates/review-summary.ts
@@ -1,7 +1,12 @@
 interface Review {
   summary: string,
+  url: string,
 };
 
 export default (review: Review): string => (
-  `<p>${review.summary}</p>`
+  `<p>${review.summary}</p>
+  <a href="${review.url}">
+    Read the full review
+  </a>
+`
 );
--- frontend/test/templates/review-summary.test.ts
+++ frontend/test/templates/review-summary.test.ts
@@ -1,12 +1,20 @@
 import templateReviewSummary from '../../src/templates/review-summary';
 
 describe('review-summary template', (): void => {
+  const review = {
+    summary: 'Pretty good.',
+    url: 'https://example.com/some-review',
+  };
+
   it('renders the summary of a review', () => {
-    const review = {
-      summary: 'Pretty good.',
-    };
     const actual = templateReviewSummary(review);
 
     expect(actual).toEqual(expect.stringContaining(review.summary));
+  });
+
+  it('renders the link to a full review', () => {
+    const actual = templateReviewSummary(review);
+
+    expect(actual).toEqual(expect.stringContaining(`<a href="${review.url}">`));
   });
 });
