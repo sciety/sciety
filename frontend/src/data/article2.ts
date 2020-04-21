0c17c58e236939f1def87eb1f2405c6c47fac6f0
Kevin Rutherford <kevin@rutherford-software.com>
2020-04-21 11:21:51 +0100

    Add the reviews the article2 data

--- frontend/src/data/article2.ts
+++ frontend/src/data/article2.ts
@@ -26,4 +26,63 @@ export default {
   a unique mechanism mediated by KAT2A/B-dependent acetylation of a non-histone protein, which fine-tunes the DNA damage
   response and hence promotes genome stability.
 </p>`,
+  reviews: [
+    {
+      author: 'EMBOpress',
+      url: 'https://hypothes.is/a/JFxMDnpkEeqyx09LkDotfQ',
+      publicationDate: new Date('2020-04-09'),
+      summary: `We thank all the Reviewers for taking the time to evaluate our manuscript and providing us with
+constructive feedback. We are pleased to hear that all Reviewers appreciate the importance and
+significance of our study, commenting that our conclusions are ‘convincing, are supported by the presented
+experimental results’ and that our study ‘will yield novel insights into the regulation and function of
+PALB2 in DNA repair’.`,
+    },
+    {
+      author: 'EMBOpress',
+      url: 'https://hypothes.is/a/I-FC6HpkEeqdRav-80EtSA',
+      publicationDate: new Date('2020-04-09'),
+      summary: `<p>
+  The authors describe a series of experiments examining the consequence of acetylation, within a defined
+  motif (Chromatin Association Motif; ChAM), on the cellular roles of the protein PALB2 (Partner and
+  Localizer of BRCA2).
+</p>
+
+<p>
+  The key conclusions drawn by the authors are generally convincing and are supported by the presented
+  experimental results, which indicate that acetylation of PALB2 by KAT2A/KAT2B modulates its cellular
+  behaviour and response to DNA damage.
+</p>`,
+    },
+    {
+      author: 'EMBOpress',
+      url: 'https://hypothes.is/a/I36NiHpkEeqdZutVpxr6uQ',
+      publicationDate: new Date('2020-04-09'),
+      summary: `<p>
+  This manuscript reports the control of PALB2 - chromatin interaction by the acetylation of a particular
+  lysine-rich domain of the protein called ChAM. This acetylation is shown to be mediated by the
+  acetyltransferases KAT2A/B. Following these investigations, the authors made an effort to place their
+  findings in the context of DNA replication and DNA repair.
+</p>`,
+    },
+    {
+      author: 'EMBOpress',
+      url: 'https://hypothes.is/a/I0iGrHpkEeqtkxu-1NyIbQ',
+      publicationDate: new Date('2020-04-09'),
+      summary: `<p>
+  Fournier et al. detect acetylation within the chromatin association motif (ChAM) of PALB2 and demonstrate
+  that KAT2 can acetylate these 7 lysine residues within this region. They then generate K to R mutations
+  (7R) or K to Q mutations (7Q) at these sites and perform assays of fluorescence recovery after
+  photobleaching (FRAP) to measure mobility as a measure of chromatin association, RAD51 foci, PALB2
+  recruitment at sites of laser-induced DNA damage, and sensitivity to olaparib. They find increased
+  mobility of the 7Q mutant of PALB2 but not 7R in the absence of exogenous DNA damage, as well as defects
+  in DNA damage-induced RAD51 foci and resistance to olaparib. On this basis, the authors conclude that
+  acetylation is required for the association of PALB2 with undamaged chromatin and that deacetylation
+  permits mobilization and association with BRCA1 to enable proper DNA repair. While the manuscript is
+  generally well-written, many of the systems are rather elegant, and this study may yield novel insights
+  into the regulation and function of PALB2 in DNA repair, there are some missing experiments to be added
+  and important contradictions that should be resolved in order to fully establish the new model the authors
+  propose.
+</p>`,
+    },
+  ],
 };
