ee26a3428fab2641dc588ebb2050c636e9bfbad8
Kevin Rutherford <kevin@rutherford-software.com>
2020-04-22 09:03:26 +0100

    Add a test for the new article handler

--- frontend/test/handlers/article.test.ts
+++ frontend/test/handlers/article.test.ts
@@ -1,28 +1,26 @@
 import { OK } from 'http-status-codes';
 import request, { Response } from 'supertest';
-import article2 from '../../src/handlers/article2';
+import createServer from '../../src/server';
+import article1 from '../../src/data/article1';
 
-const makeRequest = async (): Promise<Response> => (
-  request(article2()).get('/')
-);
+describe('article handler', (): void => {
+  let response: Response;
 
-describe('article2 handler', (): void => {
+  beforeEach(async () => {
+    const doiParam = encodeURIComponent(article1.doi);
+    response = await request(createServer()).get(`/articles/${doiParam}`);
+  });
+
   it('returns a successful response', async (): Promise<void> => {
-    const response = await makeRequest();
-
     expect(response.status).toBe(OK);
   });
 
   it('is HTML', async (): Promise<void> => {
-    const response = await makeRequest();
-
     expect(response.type).toBe('text/html');
     expect(response.charset).toBe('UTF-8');
   });
 
   it('has an HTML5 body', async (): Promise<void> => {
-    const response = await makeRequest();
-
     expect(response.text).toEqual(expect.stringMatching(/^<!doctype html>/i));
   });
 });
