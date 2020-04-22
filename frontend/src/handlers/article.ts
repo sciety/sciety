f6c2688889778a854b06efeb394ba7abe31415f5
Kevin Rutherford <kevin@rutherford-software.com>
2020-04-22 10:54:23 +0100

    Separate article fetching from the article router

--- frontend/src/handlers/article.ts
+++ frontend/src/handlers/article.ts
@@ -1,36 +1,31 @@
 import { Handler, HTTPVersion } from 'find-my-way';
 import { IncomingMessage, ServerResponse } from 'http';
 import { OK, NOT_FOUND, INTERNAL_SERVER_ERROR } from 'http-status-codes';
-import article1 from '../data/article1';
-import article2 from '../data/article2';
 import templateArticlePage from '../templates/article-page';
 import templatePage from '../templates/page';
+import fetchArticle from './fetch-article';
 
 type ArticleParams = {
   [k: string]: string | undefined;
 };
 
-export default (): Handler<HTTPVersion.V1> => {
-  const allArticles = [
-    article1,
-    article2,
-  ];
-  return (request: IncomingMessage, response: ServerResponse, params: ArticleParams): void => {
+export default (): Handler<HTTPVersion.V1> => (
+  (request: IncomingMessage, response: ServerResponse, params: ArticleParams): void => {
     if (typeof params.id === 'undefined') {
       response.writeHead(INTERNAL_SERVER_ERROR);
       response.end('DOI `id` parameter not present');
       return;
     }
     const doi = decodeURIComponent(params.id);
-    const matches = allArticles.filter((article) => article.doi === doi);
+    const article = fetchArticle(doi);
     response.setHeader('Content-Type', 'text/html; charset=UTF-8');
-    if (matches.length !== 1) {
-      response.writeHead(NOT_FOUND);
-      response.end(`${doi} not found`);
-    } else {
-      const page = templatePage(templateArticlePage(matches[0]));
+    if (article) {
+      const page = templatePage(templateArticlePage(article));
       response.writeHead(OK);
       response.end(page);
+    } else {
+      response.writeHead(NOT_FOUND);
+      response.end(`${doi} not found`);
     }
-  };
-};
+  }
+);
---
+++ frontend/src/handlers/fetch-article.ts
@@ -0,0 +1,15 @@
+import article1 from '../data/article1';
+import article2 from '../data/article2';
+import { Article } from '../templates/types/article';
+
+export default (doi: string): Article | undefined => {
+  const allArticles = [
+    article1,
+    article2,
+  ];
+  const matches = allArticles.filter((article) => article.doi === doi);
+  if (matches.length !== 1) {
+    return undefined;
+  }
+  return matches[0];
+};
