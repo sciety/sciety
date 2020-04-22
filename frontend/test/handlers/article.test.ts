750ec7803360b3168b0e58ea3ff8c4c828aeb241
Kevin Rutherford <kevin@rutherford-software.com>
2020-04-22 09:10:18 +0100

    Test the case when a DOI is not found

--- frontend/test/handlers/article.test.ts
+++ frontend/test/handlers/article.test.ts
@@ -1,4 +1,4 @@
-import { OK } from 'http-status-codes';
+import { OK, NOT_FOUND } from 'http-status-codes';
 import request, { Response } from 'supertest';
 import createServer from '../../src/server';
 import article1 from '../../src/data/article1';
@@ -6,21 +6,33 @@ import article1 from '../../src/data/article1';
 describe('article handler', (): void => {
   let response: Response;
 
-  beforeEach(async () => {
-    const doiParam = encodeURIComponent(article1.doi);
-    response = await request(createServer()).get(`/articles/${doiParam}`);
+  describe('when the article exists', (): void => {
+    beforeEach(async () => {
+      const doiParam = encodeURIComponent(article1.doi);
+      response = await request(createServer()).get(`/articles/${doiParam}`);
+    });
+
+    it('returns a successful response', async (): Promise<void> => {
+      expect(response.status).toBe(OK);
+    });
+
+    it('is HTML', async (): Promise<void> => {
+      expect(response.type).toBe('text/html');
+      expect(response.charset).toBe('UTF-8');
+    });
+
+    it('has an HTML5 body', async (): Promise<void> => {
+      expect(response.text).toEqual(expect.stringMatching(/^<!doctype html>/i));
+    });
   });
 
-  it('returns a successful response', async (): Promise<void> => {
-    expect(response.status).toBe(OK);
-  });
+  describe('when the article does not exist', (): void => {
+    beforeEach(async () => {
+      response = await request(createServer()).get('/articles/rubbish');
+    });
 
-  it('is HTML', async (): Promise<void> => {
-    expect(response.type).toBe('text/html');
-    expect(response.charset).toBe('UTF-8');
-  });
-
-  it('has an HTML5 body', async (): Promise<void> => {
-    expect(response.text).toEqual(expect.stringMatching(/^<!doctype html>/i));
+    it('returns a 404 response', async (): Promise<void> => {
+      expect(response.status).toBe(NOT_FOUND);
+    });
   });
 });
