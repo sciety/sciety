1ce30e361c0d4596825745913fa64bc0672dc1d6
Giorgio Sironi <g.sironi@elifesciences.org>
2020-04-22 09:50:09 +0100

    Introduce ArticleParams type in article handler

--- frontend/src/handlers/article.ts
+++ frontend/src/handlers/article.ts
@@ -1,17 +1,26 @@
 import { Handler, HTTPVersion } from 'find-my-way';
 import { IncomingMessage, ServerResponse } from 'http';
-import { OK, NOT_FOUND } from 'http-status-codes';
+import { OK, NOT_FOUND, INTERNAL_SERVER_ERROR } from 'http-status-codes';
 import article1 from '../data/article1';
 import article2 from '../data/article2';
 import templateArticlePage from '../templates/article-page';
 import templatePage from '../templates/page';
+
+type ArticleParams = {
+  [k: string]: string | undefined;
+};
 
 export default (): Handler<HTTPVersion.V1> => {
   const allArticles = [
     article1,
     article2,
   ];
-  return (request: IncomingMessage, response: ServerResponse, params: any): void => {
+  return (request: IncomingMessage, response: ServerResponse, params: ArticleParams): void => {
+    if (typeof params.id === 'undefined') {
+      response.writeHead(INTERNAL_SERVER_ERROR);
+      response.end('DOI `id` parameter not present');
+      return;
+    }
     const doi = decodeURIComponent(params.id);
     const matches = allArticles.filter((article) => article.doi === doi);
     response.setHeader('Content-Type', 'text/html; charset=UTF-8');
