16557ce6c1a682b9bf5273fd274b6db8e75e34f6
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-04-23 16:01:06 +0100

    Add test giving an example of fetching metadata for a DOI

--- frontend/test/api/fetch-review.test.ts
+++ frontend/test/api/fetch-review.test.ts
@@ -1,7 +1,16 @@
+import { namedNode } from '@rdfjs/data-model';
+import fetch from '@rdfjs/fetch';
+import formats from '@rdfjs/formats-common';
+import JsonLdParser from '@rdfjs/parser-jsonld';
+import datasetFactory from 'rdf-dataset-indexed';
+import { DatasetCore } from 'rdf-js';
 import createFetchReview from '../../src/api/fetch-review';
 import article3 from '../../src/data/article3';
 
 describe('fetch-review', (): void => {
+  formats.parsers.set('application/vnd.schemaorg.ld+json', new JsonLdParser());
+  const fetchOptions = { factory: { dataset: datasetFactory }, formats };
+
   describe('review found', (): void => {
     it('returns the review', () => {
       const fetchReview = createFetchReview();
@@ -15,5 +24,12 @@ describe('fetch-review', (): void => {
       const fetchReview = createFetchReview();
       expect(() => fetchReview('10.1234/5678')).toThrow(new Error('Review DOI 10.1234/5678 not found'));
     });
+  });
+
+  it('we can fetch metadata for a DOI', async () => {
+    const response = await fetch<DatasetCore>(`https://doi.org/${article3.reviews[0].doi}`, fetchOptions);
+    const dataset = await response.dataset();
+
+    expect(dataset.match(namedNode(`https://doi.org/${article3.reviews[0].doi}`), namedNode('http://schema.org/name')).size).toBeGreaterThanOrEqual(1);
   });
 });
