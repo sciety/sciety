65f0ac01c6880c9da79940b0d8b32070891fe827
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-04-23 11:49:28 +0100

    Expand fetch-reviewed-article test to include reviews

--- frontend/test/api/fetch-reviewed-article.test.ts
+++ frontend/test/api/fetch-reviewed-article.test.ts
@@ -1,15 +1,26 @@
 import createFetchReviewedArticle from '../../src/api/fetch-reviewed-article';
+import article3 from '../../src/data/article3';
 
 describe('fetch-reviewed-article "api"', (): void => {
-  it('article found', () => {
-    const fetchReviewedArticle = createFetchReviewedArticle();
-    const articleDoi = '10.1101/833392';
-    const reviewedArticle = fetchReviewedArticle(articleDoi);
-    expect(reviewedArticle.article.doi).toBe(articleDoi);
+  describe('article found', (): void => {
+    it('includes the article', () => {
+      const fetchReviewedArticle = createFetchReviewedArticle();
+      const reviewedArticle = fetchReviewedArticle(article3.article.doi);
+      expect(reviewedArticle.article.doi).toBe(article3.article.doi);
+    });
+
+    it('includes the reviews', () => {
+      const fetchReviewedArticle = createFetchReviewedArticle();
+      const reviewedArticle = fetchReviewedArticle(article3.article.doi);
+      expect(reviewedArticle.reviews).toHaveLength(1);
+      expect(reviewedArticle.reviews[0].doi).toBe(article3.reviews[0].doi);
+    });
   });
 
-  it('article not found', () => {
-    const fetchReviewedArticle = createFetchReviewedArticle();
-    expect(() => fetchReviewedArticle('10.1234/5678')).toThrow(new Error('Article DOI 10.1234/5678 not found'));
+  describe('article not found', (): void => {
+    it('throws an error', () => {
+      const fetchReviewedArticle = createFetchReviewedArticle();
+      expect(() => fetchReviewedArticle('10.1234/5678')).toThrow(new Error('Article DOI 10.1234/5678 not found'));
+    });
   });
 });
