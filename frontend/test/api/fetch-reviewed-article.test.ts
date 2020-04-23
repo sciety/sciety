3cd54fb40291caa4925448933913dc37e0a45422
Giorgio Sironi <g.sironi@elifesciences.org>
2020-04-23 10:41:14 +0100

    Introduce characterization test for fetch-reviewed-article found case

--- frontend/test/api/fetch-reviewed-article.test.ts
+++ frontend/test/api/fetch-reviewed-article.test.ts
@@ -1,6 +1,13 @@
 import createFetchReviewedArticle from '../../src/api/fetch-reviewed-article';
 
 describe('fetch-reviewed-article "api"', (): void => {
+  it('article found', () => {
+    const fetchReviewedArticle = createFetchReviewedArticle();
+    const articleDoi = '10.1101/2020.03.16.993469';
+    const reviewedArticle = fetchReviewedArticle(articleDoi);
+    expect(reviewedArticle.article.doi).toBe(articleDoi);
+  });
+
   it('article not found', () => {
     const fetchReviewedArticle = createFetchReviewedArticle();
     expect(() => fetchReviewedArticle('10.1234/5678')).toThrow(new Error('Article DOI 10.1234/5678 not found'));
