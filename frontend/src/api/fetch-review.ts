4d2b838a5764642a2e3169708d456bd83028e9e1
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-04-24 16:10:54 +0100

    Remove check that a review is known before fetching to avoid mismatch with hard-coded data

--- frontend/src/api/fetch-review.ts
+++ frontend/src/api/fetch-review.ts
@@ -1,7 +1,5 @@
 import { namedNode } from '@rdfjs/data-model';
 import { schema } from '@tpluscode/rdf-ns-builders';
-import article3 from '../data/article3';
-import article4 from '../data/article4';
 import { FetchDataset } from './fetch-dataset';
 import { Review } from '../types/review';
 
@@ -9,15 +7,6 @@ export type FetchReview = (doi: string) => Promise<Review>;
 
 export default (fetchDataset: FetchDataset): FetchReview => (
   async (doi: string): Promise<Review> => {
-    const allReviews = [
-      ...article3.reviews,
-      ...article4.reviews,
-    ];
-
-    if (!allReviews.find((review) => review.doi === doi)) {
-      throw new Error(`Review DOI ${doi} not found`);
-    }
-
     const reviewIri = namedNode(`https://doi.org/${doi}`);
     const dataset = await fetchDataset(reviewIri);
 
--- frontend/test/api/fetch-review.test.ts
+++ frontend/test/api/fetch-review.test.ts
@@ -4,31 +4,21 @@ import { schema } from '@tpluscode/rdf-ns-builders';
 import { FetchDataset } from '../../src/api/fetch-dataset';
 import createFetchReview from '../../src/api/fetch-review';
 import article3 from '../../src/data/article3';
-import shouldNotBeCalled from '../should-not-be-called';
 
 describe('fetch-review', (): void => {
-  describe('review found', (): void => {
-    it('returns the review', async () => {
-      const fetchDataset: FetchDataset = async (iri) => {
-        const authorIri = blankNode();
+  it('returns the review', async () => {
+    const fetchDataset: FetchDataset = async (iri) => {
+      const authorIri = blankNode();
 
-        return datasetFactory([
-          quad(iri, schema.datePublished, literal('2020-02-20')),
-          quad(iri, schema.description, literal('A summary')),
-          quad(iri, schema.author, authorIri),
-          quad(authorIri, schema.name, literal('Author name')),
-        ]);
-      };
-      const fetchReview = createFetchReview(fetchDataset);
-      const review = await fetchReview(article3.reviews[0].doi);
-      expect(review.publicationDate).toStrictEqual(new Date('2020-02-20'));
-    });
-  });
-
-  describe('review not found', (): void => {
-    it('throws an error', () => {
-      const fetchReview = createFetchReview(shouldNotBeCalled);
-      expect(fetchReview('10.1234/5678')).rejects.toStrictEqual(new Error('Review DOI 10.1234/5678 not found'));
-    });
+      return datasetFactory([
+        quad(iri, schema.datePublished, literal('2020-02-20')),
+        quad(iri, schema.description, literal('A summary')),
+        quad(iri, schema.author, authorIri),
+        quad(authorIri, schema.name, literal('Author name')),
+      ]);
+    };
+    const fetchReview = createFetchReview(fetchDataset);
+    const review = await fetchReview(article3.reviews[0].doi);
+    expect(review.publicationDate).toStrictEqual(new Date('2020-02-20'));
   });
 });
