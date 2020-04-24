d53ce2e2127c4ed37a92f521c4fcc60228fb3423
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-04-24 15:44:05 +0100

    Prefer using the review reference repository to check for the existance of a reviewed article

--- frontend/src/api/fetch-reviewed-article.ts
+++ frontend/src/api/fetch-reviewed-article.ts
@@ -9,31 +9,22 @@ export type FetchReviewedArticle = (doi: string) => Promise<ReviewedArticle>;
 export default (reviewReferenceRepository: ReviewReferenceRepository, fetchReview: FetchReview):
 FetchReviewedArticle => (
   async (doi: string): Promise<ReviewedArticle> => {
+    const articleReviews = reviewReferenceRepository.findReviewDoisForArticleDoi(doi);
+
+    if (articleReviews.length === 0) {
+      throw new Error(`Article DOI ${doi} not found`);
+    }
+
     const allArticles = [
       article3,
       article4,
     ];
 
-
-    const allReviewedArticles = {
-      [article3.article.doi]: reviewReferenceRepository.findReviewDoisForArticleDoi(article3.article.doi),
-      [article4.article.doi]: reviewReferenceRepository.findReviewDoisForArticleDoi(article4.article.doi),
-    };
-
-    if (!(doi in allReviewedArticles)) {
-      throw new Error(`Article DOI ${doi} not found`);
-    }
-
-    const matches = allArticles.filter((reviewedArticle) => reviewedArticle.article.doi === doi);
-    if (matches.length !== 1) {
-      throw new Error(`Article DOI ${doi} not found`);
-    }
-
-    const reviewedArticle = matches[0];
+    const [matched] = allArticles.filter((reviewedArticle) => reviewedArticle.article.doi === doi);
 
     return {
-      article: reviewedArticle.article,
-      reviews: await Promise.all(allReviewedArticles[doi].map(fetchReview)),
+      article: matched.article,
+      reviews: await Promise.all(articleReviews.map(fetchReview)),
     };
   }
 );
