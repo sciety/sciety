d649cbe189d55112a372105623c9a89a5f05d773
Giorgio Sironi <g.sironi@elifesciences.org>
2020-04-27 11:58:28 +0100

    Create everything lazily in test server

--- frontend/test/handlers/server.ts
+++ frontend/test/handlers/server.ts
@@ -10,18 +10,17 @@ import createRouter, { RouterServices } from '../../src/router';
 import createServer from '../../src/server';
 import staticRoute from '../../src/static';
 
-const fetchDataset: FetchDataset = async (iri) => {
-  const authorIri = blankNode();
-
-  return datasetFactory([
-    quad(iri, schema.datePublished, literal('2020-02-20')),
-    quad(iri, schema.description, literal('A summary')),
-    quad(iri, schema.author, authorIri),
-    quad(authorIri, schema.name, literal('Author name')),
-  ]);
-};
-
 export default (): Server => {
+  const fetchDataset: FetchDataset = async (iri) => {
+    const authorIri = blankNode();
+
+    return datasetFactory([
+      quad(iri, schema.datePublished, literal('2020-02-20')),
+      quad(iri, schema.description, literal('A summary')),
+      quad(iri, schema.author, authorIri),
+      quad(authorIri, schema.name, literal('Author name')),
+    ]);
+  };
   const fetchReview = createFetchReview(fetchDataset);
   const fetchReviewedArticle = createFetchReviewedArticle(reviewReferenceRepository, fetchReview);
   const services: RouterServices = { fetchReviewedArticle, reviewReferenceRepository };
