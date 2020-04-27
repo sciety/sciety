20ed0d87da8d963e2e94264fa370f81a8516623b
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-04-27 11:29:29 +0100

    Introduce basic DOI validation

--- frontend/src/handlers/reviews.ts
+++ frontend/src/handlers/reviews.ts
@@ -1,14 +1,35 @@
 import { Handler, HTTPVersion } from 'find-my-way';
 import { IncomingMessage, ServerResponse } from 'http';
-import { SEE_OTHER } from 'http-status-codes';
+import { BAD_REQUEST, SEE_OTHER } from 'http-status-codes';
 import parseBody from 'co-body';
 import ReviewReferenceRepository from '../types/review-reference-repository';
+
+const doiRegex = /^(?:doi:|(?:(?:https?:\/\/)?(?:dx\.)?doi\.org\/))?(10\.[0-9]{4,}(?:\.[1-9][0-9]*)*\/(?:[^%"#?\s])+)$/;
+const zenodoPrefix = '10.5281';
 
 export default (reviewReferenceRepository: ReviewReferenceRepository): Handler<HTTPVersion.V1> => (
   async (request: IncomingMessage, response: ServerResponse): Promise<void> => {
     const body: { articledoi: string; reviewdoi: string } = await parseBody.form(request);
 
-    reviewReferenceRepository.add({ articleDoi: body.articledoi, reviewDoi: body.reviewdoi });
+    const [, reviewDoi] = doiRegex.exec(body.reviewdoi) || [];
+
+    if (!reviewDoi) {
+      response.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+      response.writeHead(BAD_REQUEST);
+      response.end('Not a possible DOI.');
+
+      return;
+    }
+
+    if (!(reviewDoi.startsWith(`${zenodoPrefix}/`))) {
+      response.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+      response.writeHead(BAD_REQUEST);
+      response.end('Not a Zenodo DOI.');
+
+      return;
+    }
+
+    reviewReferenceRepository.add({ articleDoi: body.articledoi, reviewDoi });
 
     response.setHeader('Location', `/articles/${encodeURIComponent(body.articledoi)}`);
     response.writeHead(SEE_OTHER);
--- frontend/test/handlers/reviews.test.ts
+++ frontend/test/handlers/reviews.test.ts
@@ -1,28 +1,73 @@
-import { SEE_OTHER } from 'http-status-codes';
+import { BAD_REQUEST, SEE_OTHER } from 'http-status-codes';
 import request from 'supertest';
 import createServer from './server';
 import reviewReferenceRepository from '../../src/data/review-references';
 
 describe('reviews handler', (): void => {
-  it('returns a created response', async (): Promise<void> => {
+  it.each([
+    '10.5281/zenodo.3678326',
+    'doi:10.5281/zenodo.3678326',
+    'http://dx.doi.org/10.5281/zenodo.3678326',
+    'https://dx.doi.org/10.5281/zenodo.3678326',
+    'http://doi.org/10.5281/zenodo.3678326',
+    'https://doi.org/10.5281/zenodo.3678326',
+  ])('returns a created response (%s)', async (reviewDoi: string): Promise<void> => {
     const articleDoi = '10.1101/2000.1234';
     const articleDoiParam = encodeURIComponent(articleDoi);
+    const reviewDoiParam = encodeURIComponent(reviewDoi);
     const response = await request(createServer())
       .post('/reviews')
-      .send(`articledoi=${articleDoiParam}&reviewdoi=10.5281%2Fzenodo.3678326`);
+      .send(`articledoi=${articleDoiParam}&reviewdoi=${reviewDoiParam}`);
+
     expect(response.status).toBe(SEE_OTHER);
     expect(response.header.location).toBe(`/articles/${articleDoiParam}`);
   });
 
-  it('adds the review reference to the repository', async (): Promise<void> => {
+  it.each([
+    ['10.5281/zenodo.3678326', '10.5281/zenodo.3678326'],
+    ['doi:10.5281/zenodo.3678326', '10.5281/zenodo.3678326'],
+    ['http://dx.doi.org/10.5281/zenodo.3678326', '10.5281/zenodo.3678326'],
+    ['https://dx.doi.org/10.5281/zenodo.3678326', '10.5281/zenodo.3678326'],
+    ['http://doi.org/10.5281/zenodo.3678326', '10.5281/zenodo.3678326'],
+    ['https://doi.org/10.5281/zenodo.3678326', '10.5281/zenodo.3678326'],
+  ])('adds the review reference to the repository (%s)', async (reviewDoi: string, expected: string): Promise<void> => {
     const articleDoi = '10.1101/2000.1234';
     const articleDoiParam = encodeURIComponent(articleDoi);
-    const reviewDoi = '10.5281/zenodo.3678326';
-    const reviewDoiParam = encodeURIComponent(articleDoi);
+    const reviewDoiParam = encodeURIComponent(reviewDoi);
     await request(createServer())
       .post('/reviews')
       .send(`articledoi=${articleDoiParam}&reviewdoi=${reviewDoiParam}`);
 
-    expect(reviewReferenceRepository.findReviewDoisForArticleDoi(articleDoi)).toContain(reviewDoi);
+    expect(reviewReferenceRepository.findReviewDoisForArticleDoi(articleDoi)).toContain(expected);
+  });
+
+  it.each([
+    '10..1000/journal.pone.0011111',
+    '1.1/1.1',
+    '10/134980',
+    '10.001/001#00',
+  ])('requires a possible review DOI (%s)', async (reviewDoi: string): Promise<void> => {
+    const response = await request(createServer())
+      .post('/reviews')
+      .send(`articledoi=10.1101%2F2000.1234&reviewdoi=${encodeURIComponent(reviewDoi)}`);
+
+    expect(response.status).toBe(BAD_REQUEST);
+    expect(response.text).toBe('Not a possible DOI.');
+  });
+
+  it.each([
+    '10.1000/journal.pone.0011111',
+    'doi:10.1000/journal.pone.0011111',
+    'http://dx.doi.org/10.1016/j.neuron.2014.09.004',
+    'https://dx.doi.org/10.1016/j.neuron.2014.09.004',
+    'http://doi.org/10.1016/j.neuron.2014.09.004',
+    'https://doi.org/10.1016/j.neuron.2014.09.004',
+  ])('requires a Zenodo review DOI (%s)', async (reviewDoi: string): Promise<void> => {
+    const response = await request(createServer())
+      .post('/reviews')
+      .send(`articledoi=10.1101%2F2000.1234&reviewdoi=${encodeURIComponent(reviewDoi)}`);
+
+    expect(response.status).toBe(BAD_REQUEST);
+    expect(response.text).toBe('Not a Zenodo DOI.');
   });
 });
