209e7ecbea6f03d72191f1b59de09f3df5ab0b47
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-04-28 11:27:53 +0100

    Use dedicated fetch implementation

--- frontend/src/api/fetch-dataset.ts
+++ frontend/src/api/fetch-dataset.ts
@@ -1,7 +1,8 @@
 import { EventEmitter } from 'events';
-import fetch from '@rdfjs/fetch-lite';
+import rdfFetch from '@rdfjs/fetch-lite';
 import JsonLdParser from '@rdfjs/parser-jsonld';
 import SinkMap from '@rdfjs/sink-map';
+import fetch from 'isomorphic-fetch';
 import datasetFactory from 'rdf-dataset-indexed';
 import { DatasetCore, NamedNode, Stream } from 'rdf-js';
 import createLogger from '../logger';
@@ -19,11 +20,11 @@ export default (): FetchDataset => {
   const factory = { dataset: datasetFactory };
   const parsers = new SinkMap<EventEmitter, Stream>();
   parsers.set('application/vnd.schemaorg.ld+json', new JsonLdParser());
-  const fetchOptions = { factory, formats: { parsers } };
+  const fetchOptions = { factory, fetch, formats: { parsers } };
 
   return async (iri: NamedNode): Promise<DatasetCore> => {
     log(`Fetching dataset for ${iri.value}`);
-    const response = await fetch<DatasetCore>(iri.value, fetchOptions);
+    const response = await rdfFetch<DatasetCore>(iri.value, fetchOptions);
 
     if (!response.ok) {
       throw new FetchDatasetError(`Received a ${response.status} ${response.statusText} for ${response.url}`);
