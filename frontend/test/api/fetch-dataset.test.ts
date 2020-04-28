8904d7ae3cba097a080e1d2b8ba81f462e29f486
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-04-28 14:31:05 +0100

    Extract createStubFetch factory

--- frontend/test/api/fetch-dataset.test.ts
+++ frontend/test/api/fetch-dataset.test.ts
@@ -1,17 +1,21 @@
 import { namedNode } from '@rdfjs/data-model';
-import rdfFetch from '@rdfjs/fetch-lite';
+import rdfFetch, { DatasetResponse } from '@rdfjs/fetch-lite';
 import datasetFactory from 'rdf-dataset-indexed';
 import { DatasetCore } from 'rdf-js';
 import createFetchDataset, { FetchDatasetError } from '../../src/api/fetch-dataset';
 import article3 from '../../src/data/article3';
+
+const createStubFetch = (response: Partial<DatasetResponse<DatasetCore>>): typeof rdfFetch => (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async (): Promise<any> => response
+);
 
 describe('fetch-dataset', (): void => {
   describe('dataset found', (): void => {
     it('fetches a dataset for an IRI', async () => {
       const iri = namedNode(`https://doi.org/${article3.reviews[0].doi}`);
       const cannedDataset = datasetFactory([]);
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const stubFetch: typeof rdfFetch = async (): Promise<any> => ({
+      const stubFetch = createStubFetch({
         ok: true,
         dataset: async (): Promise<DatasetCore> => cannedDataset,
       });
@@ -26,8 +30,7 @@ describe('fetch-dataset', (): void => {
   describe('dataset not found', (): void => {
     it('throws an errors', async (): Promise<void> => {
       const iri = namedNode('https://doi.org/not-a-doi');
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const stubFetch: typeof rdfFetch = async (): Promise<any> => ({
+      const stubFetch = createStubFetch({
         ok: false,
         status: 404,
         statusText: 'Not Found',
