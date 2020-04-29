3ce60f3a9c389c72646e4addd1b751dbf767dca2
Giorgio Sironi <g.sironi@elifesciences.org>
2020-04-29 15:18:57 +0100

    Introduce DOI type

--- frontend/src/data/doi.ts
+++ frontend/src/data/doi.ts
@@ -1,6 +1,8 @@
+import { DOI } from '../types/doi';
+
 const doiRegex = /^(?:doi:|(?:(?:https?:\/\/)?(?:dx\.)?doi\.org\/))?(10\.[0-9]{4,}(?:\.[1-9][0-9]*)*\/(?:[^%"#?\s])+)$/;
 
-export default (something: string): object => {
+export default (something: string): DOI => {
   const [, doi] = doiRegex.exec(something) || [];
 
   if (!doi) {
