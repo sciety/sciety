4bb02e60297260a587b12d699088e83e56311803
Giorgio Sironi <g.sironi@elifesciences.org>
2020-04-29 15:13:51 +0100

    Add doi module in Domain Model

---
+++ frontend/src/data/doi.ts
@@ -0,0 +1,12 @@
+const doiRegex = /^(?:doi:|(?:(?:https?:\/\/)?(?:dx\.)?doi\.org\/))?(10\.[0-9]{4,}(?:\.[1-9][0-9]*)*\/(?:[^%"#?\s])+)$/;
+
+export default (something: string): object => {
+  const [, doi] = doiRegex.exec(something) || [];
+
+  if (!doi) {
+    throw new Error('Not a possible DOI.');
+  }
+  return {
+    toString(): string { return doi; },
+  };
+};
---
+++ frontend/test/data/doi.test.ts
@@ -0,0 +1,23 @@
+import createDoi from '../../src/data/doi';
+
+describe('doi', () => {
+  it.each([
+    '10.5281/zenodo.3678326',
+    'doi:10.5281/zenodo.3678326',
+    'http://dx.doi.org/10.5281/zenodo.3678326',
+    'https://dx.doi.org/10.5281/zenodo.3678326',
+    'http://doi.org/10.5281/zenodo.3678326',
+    'https://doi.org/10.5281/zenodo.3678326',
+  ])('accepts valid DOI syntax', (doiSyntaxExample) => {
+    expect(createDoi(doiSyntaxExample).toString()).toEqual('10.5281/zenodo.3678326');
+  });
+
+  it.each([
+    '10..1000/journal.pone.0011111',
+    '1.1/1.1',
+    '10/134980',
+    '10.001/001#00',
+  ])('rejects invalid DOI syntax', (badDoiSyntaxExample) => {
+    expect(() => createDoi(badDoiSyntaxExample)).toThrow();
+  });
+});
