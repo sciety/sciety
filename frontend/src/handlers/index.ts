6cedc25bf73feb2bc6ef84f27d626f6901514145
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-05-01 14:57:21 +0100

    Add find-reviews-for-an-article form to the homepage

--- frontend/src/handlers/index.ts
+++ frontend/src/handlers/index.ts
@@ -1,12 +1,31 @@
 import { Middleware, RouterContext } from '@koa/router';
+import { BadRequest } from 'http-errors';
 import { Next } from 'koa';
 import { FetchAllArticleTeasers } from '../api/fetch-all-article-teasers';
+import { article3 } from '../data/article-dois';
+import Doi from '../data/doi';
 import templateArticleTeaser from '../templates/article-teaser';
 import templateListItems from '../templates/list-items';
 import templatePage from '../templates/page';
 
 export default (fetchAllArticleTeasers: FetchAllArticleTeasers): Middleware => (
-  async ({ response }: RouterContext, next: Next): Promise<void> => {
+  async ({ request, response }: RouterContext, next: Next): Promise<void> => {
+    if (request.query.articledoi) {
+      let doi: Doi;
+
+      try {
+        doi = new Doi(request.query.articledoi);
+      } catch (error) {
+        throw new BadRequest('Not a valid DOI.');
+      }
+
+      response.redirect(`/articles/${doi}`);
+
+      await next();
+
+      return;
+    }
+
     const teasers = (await fetchAllArticleTeasers()).map(templateArticleTeaser);
 
     response.body = templatePage(`<main>
@@ -19,6 +38,31 @@ export default (fetchAllArticleTeasers: FetchAllArticleTeasers): Middleware => (
 
   </header>
 
+  <form method="get" action="/" class="find-reviews">
+
+    <fieldset>
+
+      <legend class="find-reviews__title">
+        Find reviews for an article
+      </legend>
+
+      <div class="find-reviews__row">
+
+        <label>
+          <span class="visually-hidden">DOI of an article</span>
+          <input type="text" name="articledoi" placeholder="${article3}" class="find-reviews__article-doi" required>
+        </label>
+    
+        <button type="submit" class="find-reviews__submit">
+          <span class="visually-hidden">Find reviews</span>
+        </button>
+
+      </div>
+
+    </fieldset>
+
+  </form>
+  
   <section class="teaser-list">
 
     <h2 class="teaser-list__title">
