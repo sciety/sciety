4e541799c805eb570015dd8d4949bc8b7fceca4e
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-05-05 15:22:07 +0100

    Extract community data

---
+++ src/data/communities.ts
@@ -0,0 +1,13 @@
+import { Community } from '../types/community';
+
+const communities: Array<Community> = [
+  {
+    id: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
+    name: 'eLife',
+    description: `eLife is a non-profit organisation created by funders and led by researchers. Our mission is to
+accelerate discovery by operating a platform for research communication that encourages and recognises the most
+responsible behaviours in science.`,
+  },
+];
+
+export default communities;
--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,12 @@ import bodyParser from 'koa-bodyparser';
 import { FetchAllArticleTeasers } from './api/fetch-all-article-teasers';
 import { FetchCommunityArticles } from './api/fetch-community-articles';
 import { FetchReviewedArticle } from './api/fetch-reviewed-article';
+import communities from './data/communities';
 import article from './handlers/article';
 import community from './handlers/community';
 import index from './handlers/index';
 import ping from './handlers/ping';
 import reviews from './handlers/reviews';
-import { Community } from './types/community';
 import ReviewReferenceRepository from './types/review-reference-repository';
 
 export type RouterServices = {
@@ -21,18 +21,10 @@ export type RouterServices = {
 export default (services: RouterServices): Router => {
   const router = new Router();
 
-  const eLifeCommunity: Community = {
-    id: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
-    name: 'eLife',
-    description: `eLife is a non-profit organisation created by funders and led by researchers.
-Our mission is to accelerate discovery by operating a platform for research communication that
-encourages and recognises the most responsible behaviours in science.`,
-  };
-
   router.get('/ping', ping());
   router.get('/', index(services.fetchAllArticleTeasers));
   router.get('/articles/:doi(.+)', article(services.fetchReviewedArticle));
-  router.get('/communities/:id', community([eLifeCommunity], services.fetchCommunityArticles));
+  router.get('/communities/:id', community(communities, services.fetchCommunityArticles));
   router.post('/reviews', bodyParser({ enableTypes: ['form'] }), reviews(services.reviewReferenceRepository));
 
   return router;
