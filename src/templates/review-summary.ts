31c7fd25974debdd1dbacae8d42c63d8e07ff90e
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-05-05 11:23:27 +0100

    Add placeholder link to the editorial community page

--- src/templates/review-summary.ts
+++ src/templates/review-summary.ts
@@ -4,7 +4,7 @@ import { Review } from '../types/review';
 export default (review: Review, idNamespace: string): string => (
   `<article class="review-summary">
     <h3 class="review-summary__title">
-    Reviewed by <span id="${idNamespace}-author">eLife</span>
+    Reviewed by <a href="/communities/b560187e-f2fb-4ff9-a861-a204f3fc0fb0" id="${idNamespace}-author">eLife</a>
     on ${templateDate(review.publicationDate)}
   </h3>
   ${review.summary}
