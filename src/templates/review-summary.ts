588787c8fa70ed10275c7585bf5ab556e1c79861
Giorgio Sironi <g.sironi@elifesciences.org>
2020-05-06 10:36:45 +0100

    Remove hardcoding of communities[0].name

--- src/templates/review-summary.ts
+++ src/templates/review-summary.ts
@@ -5,7 +5,7 @@ import { Review } from '../types/review';
 export default (review: Review, idNamespace: string): string => (
   `<article class="review-summary">
     <h3 class="review-summary__title">
-    Reviewed by <a href="/communities/${communities[0].id}" id="${idNamespace}-author">eLife</a>
+    Reviewed by <a href="/communities/${communities[0].id}" id="${idNamespace}-author">${communities[0].name}</a>
     on ${templateDate(review.publicationDate)}
   </h3>
   ${review.summary}
