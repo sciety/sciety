131aafd4b425a008a7903c943b1e8e1bae69796a
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-05-07 14:21:03 +0100

    Convert ReviewedArticle to ArticlePage

--- src/middleware/convert-article-and-reviews-to-article-page.ts
+++ src/middleware/convert-article-and-reviews-to-article-page.ts
@@ -3,6 +3,8 @@ import { Next } from 'koa';
 
 export default (): Middleware => (
   async (ctx: RouterContext, next: Next): Promise<void> => {
+    ctx.prc.articlePage = ctx.prc.reviewedArticle;
+
     await next();
   }
 );
