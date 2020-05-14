24aff66c4500e797dd63d356e8406b92b78625a7
Kevin Rutherford <kevin@rutherford-software.com>
2020-05-14 15:44:16 +0100

    Add unit tests of the render-editorial-community-page middleware #55

---
+++ test/editorial-community/render-editorial-community-page.test.ts
@@ -0,0 +1,54 @@
+import { Context, Middleware, Response } from 'koa';
+import Doi from '../../src/data/doi';
+import renderEditorialCommunityPage from '../../src/editorial-community-page/render-editorial-community-page';
+import createContext from '../context';
+import runMiddleware from '../middleware';
+
+const invokeMiddleware = async (ctx: Context, next?: Middleware): Promise<Response> => {
+  const { response } = await runMiddleware(renderEditorialCommunityPage(), ctx, next);
+  return response;
+};
+
+describe('render-editorial-community-page middleware', (): void => {
+  let ctx: Context;
+
+  beforeEach(() => {
+    ctx = createContext();
+    ctx.state = {
+      viewModel: {
+        name: 'community-name',
+        description: 'community-description',
+        teasers: [{
+          doi: new Doi('10.1111/1111'),
+          title: 'title-1',
+        },
+        {
+          doi: new Doi('10.2222/2222'),
+          title: 'title-2',
+        }],
+      },
+    };
+  });
+
+  it('renders the editorial community name', async (): Promise<void> => {
+    await invokeMiddleware(ctx);
+
+    expect(ctx.response.body).toStrictEqual(expect.stringContaining('community-name'));
+  });
+
+  it('renders the editorial community description', async (): Promise<void> => {
+    const next = jest.fn();
+    await invokeMiddleware(ctx, next);
+
+    expect(ctx.response.body).toStrictEqual(expect.stringContaining('community-description'));
+  });
+
+  it('renders the article links', async (): Promise<void> => {
+    await invokeMiddleware(ctx);
+
+    expect(ctx.response.body).toStrictEqual(expect.stringContaining('10.1111/1111'));
+    expect(ctx.response.body).toStrictEqual(expect.stringContaining('title-1'));
+    expect(ctx.response.body).toStrictEqual(expect.stringContaining('10.2222/2222'));
+    expect(ctx.response.body).toStrictEqual(expect.stringContaining('title-2'));
+  });
+});
