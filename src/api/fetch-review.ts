0a0113982d225d601867a0737dd4d59999bdd092
Kevin Rutherford <kevin@rutherford-software.com>
2020-05-18 11:27:48 +0100

    Add more logging when fetching a review #63

--- src/api/fetch-review.ts
+++ src/api/fetch-review.ts
@@ -2,22 +2,27 @@ import { namedNode } from '@rdfjs/data-model';
 import { schema } from '@tpluscode/rdf-ns-builders';
 import { FetchDataset } from './fetch-dataset';
 import Doi from '../data/doi';
+import createLogger from '../logger';
 import { Review } from '../types/review';
 
 export type FetchReview = (doi: Doi) => Promise<Review>;
 
-export default (fetchDataset: FetchDataset): FetchReview => (
-  async (doi: Doi): Promise<Review> => {
+export default (fetchDataset: FetchDataset): FetchReview => {
+  const log = createLogger('api:fetch-review');
+  return async (doi: Doi): Promise<Review> => {
     const reviewIri = namedNode(`https://doi.org/${doi}`);
+    log(`Fetching review ${reviewIri.value}`);
     const graph = await fetchDataset(reviewIri);
 
     const publicationDate = new Date(graph.out(schema.datePublished).value || 0);
     const summary = graph.out(schema.description).value || 'Unknown summary';
 
-    return {
+    const response: Review = {
       doi,
       publicationDate,
       summary,
     };
-  }
-);
+    log(`Retrieved review: ${JSON.stringify({ doi, publicationDate })}`);
+    return response;
+  };
+};
