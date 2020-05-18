3fd635e07ccd171e246da439baec5031eeef6570
Giorgio Sironi <g.sironi@elifesciences.org>
2020-05-18 14:13:25 +0100

    Extract bootstrap-reviews module

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { createTerminus, TerminusOptions } from '@godaddy/terminus';
 import createFetchArticle from './api/fetch-article';
 import createFetchDataset from './api/fetch-dataset';
 import createFetchReview from './api/fetch-review';
+import bootstrapReviews from './bootstrap-reviews';
 import Doi from './data/doi';
 import createEditorialCommunityRepository from './data/in-memory-editorial-communities';
 import createReviewReferenceRepository from './data/in-memory-review-references';
@@ -17,19 +18,7 @@ log('Starting server');
 const editorialCommunities = createEditorialCommunityRepository();
 
 const reviewReferenceRepository = createReviewReferenceRepository();
-const bootstrapArticlesAndReviews = {
-  '10.1101/642017': '10.5281/zenodo.3820276',
-  '10.1101/615682': '10.5281/zenodo.3820283',
-  '10.1101/629618': '10.5281/zenodo.3820289',
-  '10.1101/600445': '10.5281/zenodo.3820295',
-  '10.1101/252593': '10.5281/zenodo.3820302',
-  '10.1101/497495': '10.5281/zenodo.3820314',
-  '10.1101/856153': '10.5281/zenodo.3821160',
-  '10.1101/871517': '10.5281/zenodo.3821167',
-  '10.1101/610014': '10.5281/zenodo.3821171',
-  '10.1101/822718': '10.5281/zenodo.3821367',
-};
-Object.entries(bootstrapArticlesAndReviews).forEach(([article, review]) => {
+Object.entries(bootstrapReviews).forEach(([article, review]) => {
   reviewReferenceRepository.add(
     new Doi(article),
     new Doi(review),
