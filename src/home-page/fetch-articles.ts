b5459e9398626448ce1d0a4a311fac57959f6690
Giorgio Sironi <g.sironi@elifesciences.org>
2020-05-19 10:57:59 +0100

    Add hardcoded fetch-articles middleware for homepage #74

---
+++ src/home-page/fetch-articles.ts
@@ -0,0 +1,25 @@
+import { Context, Middleware, Next } from 'koa';
+import { FetchArticle } from '../api/fetch-article';
+import Doi from '../data/doi';
+
+export default (
+  fetchArticle: FetchArticle,
+): Middleware => (
+  async (ctx: Context, next: Next): Promise<void> => {
+    const articleVersionDois = [
+      new Doi('10.1101/833392'),
+      new Doi('10.1101/642017'),
+      new Doi('10.1101/615682'),
+      new Doi('10.1101/629618'),
+      new Doi('10.1101/600445'),
+    ];
+
+    ctx.state.fetchedArticles = Promise.all(articleVersionDois.map(fetchArticle)).then((fetchedArticles) => (
+      fetchedArticles.reduce((fetchedArticlesMap, fetchedArticle) => ({
+        ...fetchedArticlesMap, [fetchedArticle.doi.toString()]: fetchArticle,
+      }), {})
+    ));
+
+    await next();
+  }
+);
---
+++ test/home-page/fetch-articles.test.ts
@@ -0,0 +1,45 @@
+import { Context, Middleware, Response } from 'koa';
+import { FetchArticle } from '../../src/api/fetch-article';
+import Doi from '../../src/data/doi';
+import fetchArticles from '../../src/home-page/fetch-articles';
+import { FetchedArticle } from '../../src/types/fetched-article';
+import createContext from '../context';
+import runMiddleware from '../middleware';
+
+const invokeMiddleware = async (
+  fetchArticle: FetchArticle,
+  ctx: Context,
+  next?: Middleware,
+): Promise<Response> => {
+  const { response } = await runMiddleware(fetchArticles(fetchArticle), ctx, next);
+  return response;
+};
+
+const fetchArticle: FetchArticle = async (doi: Doi) => (
+  {
+    doi,
+  } as FetchedArticle
+);
+
+describe('fetch-articles middleware', (): void => {
+  let ctx: Context;
+
+  beforeEach(() => {
+    ctx = createContext();
+    ctx.state = {};
+  });
+
+  it('adds fetched articles to the context', async (): Promise<void> => {
+    await invokeMiddleware(fetchArticle, ctx);
+
+    expect(ctx.state).toHaveProperty('fetchedArticles');
+    // expect(await ctx.state.fetchedArticles).toHaveProperty('10.1101/833392');
+  });
+
+  it('calls the next middleware', async (): Promise<void> => {
+    const next = jest.fn();
+    await invokeMiddleware(fetchArticle, ctx, next);
+
+    expect(next).toHaveBeenCalledTimes(1);
+  });
+});
