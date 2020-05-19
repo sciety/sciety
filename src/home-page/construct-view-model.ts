f1e15e900f35eb02b05b9f5eb6459e85241391f2
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-05-19 10:33:13 +0100

    Use populated editorial communities on the home page #74

--- src/home-page/construct-view-model.ts
+++ src/home-page/construct-view-model.ts
@@ -3,32 +3,34 @@ import Doi from '../data/doi';
 
 export default (): Middleware => (
   async (ctx: Context, next: Next): Promise<void> => {
+    const { editorialCommunities } = ctx.state;
+
     ctx.state.viewModel = {
       mostRecentReviews: [
         {
           articleDoi: new Doi('10.1101/833392'),
           articleTitle: 'Uncovering the hidden antibiotic potential of Cannabis',
-          editorialCommunityName: 'eLife',
+          editorialCommunityName: editorialCommunities['b560187e-f2fb-4ff9-a861-a204f3fc0fb0'].name,
         },
         {
           articleDoi: new Doi('10.1101/642017'),
           articleTitle: 'Toxoplasma gondii Infection Drives Conversion of NK Cells into ILC1s',
-          editorialCommunityName: 'eLife',
+          editorialCommunityName: editorialCommunities['b560187e-f2fb-4ff9-a861-a204f3fc0fb0'].name,
         },
         {
           articleDoi: new Doi('10.1101/615682'),
           articleTitle: 'A genetic selection reveals functional metastable structures embedded in a toxin-encoding mRNA',
-          editorialCommunityName: 'eLife',
+          editorialCommunityName: editorialCommunities['b560187e-f2fb-4ff9-a861-a204f3fc0fb0'].name,
         },
         {
           articleDoi: new Doi('10.1101/629618'),
           articleTitle: 'Androgen-regulated transcription of ESRP2 drives alternative splicing patterns in prostate cancer',
-          editorialCommunityName: 'eLife',
+          editorialCommunityName: editorialCommunities['b560187e-f2fb-4ff9-a861-a204f3fc0fb0'].name,
         },
         {
           articleDoi: new Doi('10.1101/600445'),
           articleTitle: 'Extensive Ribosome and RF2 Rearrangements during Translation Termination',
-          editorialCommunityName: 'eLife',
+          editorialCommunityName: editorialCommunities['b560187e-f2fb-4ff9-a861-a204f3fc0fb0'].name,
         },
       ],
     };
--- test/home-page/construct-view-model.test.ts
+++ test/home-page/construct-view-model.test.ts
@@ -13,7 +13,13 @@ describe('construct-view-model middleware', (): void => {
 
   beforeEach(() => {
     ctx = createContext();
-    ctx.state = {};
+    ctx.state = {
+      editorialCommunities: {
+        'b560187e-f2fb-4ff9-a861-a204f3fc0fb0': {
+          name: 'eLife',
+        },
+      },
+    };
   });
 
   it('adds most recent reviews to the context', async (): Promise<void> => {
