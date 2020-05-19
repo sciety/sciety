76e95e0505b37bf6eb43b687e479b38d075afcd6
Kevin Rutherford <kevin@rutherford-software.com>
2020-05-19 11:11:31 +0100

    Add hard-coded genuine abstracts for all sample articles. Closes #84

--- src/data/abstracts.ts
+++ src/data/abstracts.ts
@@ -9,4 +9,73 @@ export default {
    cells that circulate widely and persist independent of ongoing infection. Moreover, these changes appear permanent,
    as supported by epigenetic analyses. Thus, these studies markedly expand current concepts of NK cells, ILCs, and
    their potential conversion.`,
+  '10.1101/615682': `Post-transcriptional regulation plays important roles to finely tune gene expression in bacteria.
+  In particular, regulation of type I toxin-antitoxin (TA) systems is achieved through sophisticated mechanisms
+  involving toxin mRNA folding. Here, we set up a genetic approach to decipher the molecular underpinnings behind
+  the regulation of a type I TA in <em>Helicobacter pylori</em>.
+  We used the lethality induced by chromosomal inactivation of the antitoxin to select mutations that suppress toxicity.
+  We found that single point mutations are sufficient to allow cell survival.
+  Mutations located either in the 5’ untranslated region or within the open reading frame of the toxin hamper its
+  translation by stabilizing stem-loop structures that sequester the Shine-Dalgarno sequence.
+  We propose that these short hairpins correspond to metastable structures that are transiently formed during
+  transcription to avoid premature toxin expression. This work uncovers the co-transcriptional inhibition of translation
+  as an additional layer of TA regulation in bacteria.`,
+  '10.1101/629618': `<p id="p-3">Prostate is the most frequent cancer in men. Prostate cancer progression is driven
+  by androgen steroid hormones, and delayed by androgen deprivation therapy (ADT). Androgens control transcription
+  by stimulating androgen receptor (AR) activity, yet also control pre-mRNA splicing through less clear mechanisms.
+  Here we find androgens regulate splicing through AR-mediated transcriptional control of the epithelial-specific
+  splicing regulator <em>ESRP2</em>. Both <em>ESRP2</em> and its close paralog <em>ESRP1</em> are highly expressed
+  in primary prostate cancer. Androgen stimulation induces splicing switches in many endogenous ESRP2-controlled
+  mRNA isoforms, including a key splicing switch in the metastatic regulator <em>FLNB</em> which is associated with
+  disease relapse. <em>ESRP2</em> expression in clinical prostate cancer is repressed by ADT, which may thus
+  inadvertently dampen epithelial splice programmes. Supporting this, <em>FLNB</em> splicing was reciprocally
+  switched by the AR antagonist bicalutamide (Casodex<sup>®</sup>). Our data reveal a new mechanism of splicing
+  control in prostate cancer with important implications for metastatic disease progression.</p><
+  div id="sec-1" class="subsection"><p><strong>Key points</strong></p>
+  <ul class="list-unord " id="list-1"><li id="list-item-1"><p id="p-4">Transcriptional regulation of ESRP2 by the androgen receptor controls splice isoform patterns in prostate cancer cells.</p></li><li id="list-item-2"><p id="p-5">Splicing switches regulated by the androgen-ESRP2 axis include a splice isoform in the <em>FLNB</em> gene that is a known metastatic driver.</p></li><li id="list-item-3"><p id="p-6">Both ESRP1 and ESRP2 are highly expressed in prostate cancer tissue.</p></li><li id="list-item-4"><p id="p-7">Ectopic expression of ESRP1 and 2 inhibits prostate cancer cell growth.</p></li><li id="list-item-5"><p id="p-8">By repressing ESRP2 expression androgen deprivation therapy (ADT) may dampen epithelial splicing programmes to inadvertently prime disease progression towards metastasis.</p></li></ul></div>`,
+  '10.1101/600445': `Protein synthesis ends when a ribosome reaches an mRNA stop codon. Release factors (RFs) decode
+  the stop codon, hydrolyze peptidyl-tRNA to release the nascent protein, and then dissociate to allow ribosome recycling.
+  To visualize termination by RF2, we resolved a cryo-EM ensemble of <em>E. coli</em> 70S•RF2 structures at up to 3.3
+  Å in a single sample. Five structures suggest a highly dynamic termination pathway. Upon peptidyl-tRNA hydrolysis,
+  the CCA end of deacyl-tRNA departs from the peptidyl transferase center. The catalytic GGQ loop of RF2 is rearranged
+  into a long β-hairpin that plugs the peptide tunnel, biasing a nascent protein toward the ribosome exit. Ribosomal
+  intersubunit rotation destabilizes the catalytic RF2 domain on the 50S subunit and disassembles the central
+  intersubunit bridge B2a, resulting in RF2 departure. Our structures visualize how local rearrangements and spontaneous
+  inter-subunit rotation poise the newly-made protein and RF2 to dissociate in preparation for ribosome recycling.`,
+  '10.1101/252593': `Understanding heterogeneity in neural phenotypes is an important goal on the path to precision
+  medicine for autism spectrum disorders (ASD). Age is a critically important variable in normal structural brain
+  development and examining structural features with respect to age-related norms could help to explain ASD
+  heterogeneity in neural phenotypes. Here we examined how cortical thickness (CT) in ASD can be parameterized as an
+  individualized metric of deviance relative to typically-developing (TD) age-related norms. Across a large sample
+  (n=870 per group) and wide age range (5-40 years), we applied a normative modelling approach that provides
+  individualized whole-brain maps of age-related CT deviance in ASD. This approach isolates a subgroup of ASD
+  individuals with highly age-deviant CT. The median prevalence of this ASD subgroup across all brain regions is 7.6%,
+  and can reach as high as 10% for some brain regions. This work showcases an individualized approach for understanding
+  ASD heterogeneity that could potentially further prioritize work on a subset of individuals with significant cortical
+  pathophysiology represented in age-related CT deviance. Rather than cortical thickness pathology being a widespread
+  characteristic of most ASD patients, only a small subset of ASD individuals are actually highly deviant relative to
+  age-norms. These individuals drive small on-average effects from case-control comparisons. Rather than sticking to the
+  diagnostic label of autism, future research should pivot to focus on isolating subsets of autism patients with highly
+  deviant phenotypes and better understand the underlying mechanisms that drive those phenotypes.`,
+  '10.1101/497495': `Evolution under asexuality is predicted to impact genomes in numerous ways, but empirical evidence
+  remains unclear. Case studies of individual asexual animals have reported peculiar genomic features which were suggested
+  to be caused by asexuality, including high heterozygosity, a high abundance of horizontally acquired genes, a low
+  transposable element load, or the presence of palindromes. We systematically characterized these genomic features in
+  published genomes of 26 asexual animals representing at least 18 independent transitions to asexuality. Surprisingly,
+  not a single feature is systematically replicated across a majority of these transitions, suggesting that no genomic
+  feature is characteristic of asexuality and that previously reported patterns were lineage specific rather than caused
+  by asexuality. We found that only asexuals of hybrid origin were characterized by high heterozygosity levels. Asexuals
+  that were not of hybrid origin appeared to be largely homozygous, independently of the cellular mechanism underlying
+  asexuality. Overall, despite the importance of recombination rate variation for the evolution of sexual animal genomes,
+  the genome-wide absence of recombination does not appear to have had the dramatic effects which are expected from
+  classical theoretical models. The reasons for this are probably a combination of lineage-specific patterns, impact of
+  the origin of asexuality, and a survivorship bias of asexual lineages.`,
+  '10.1101/856153': `Insect navigation arises from the coordinated action of concurrent guidance systems but the neural
+  mechanisms through which each functions, and are then coordinated, remains unknown. We propose that insects require distinct strategies to retrace familiar routes (route-following) and directly return from novel to familiar terrain (homing) using different aspects of frequency encoded views that are processed in different neural pathways. We also demonstrate how the Central Complex and Mushroom Bodies regions of the insect brain may work in tandem to coordinate the directional output of different guidance cues through a contextually switched ring-attractor inspired by neural recordings. The resultant unified model of insect navigation reproduces behavioural data from a series of cue conflict experiments in realistic animal environments and offers testable hypotheses of where and how insects process visual cues, utilise the different information that they provide and coordinate their outputs to achieve the adaptive behaviours observed in the wild.`,
+  '10.1101/871517': `<p id="p-3">Loss of functional RAB18 causes the autosomal recessive condition Warburg Micro syndrome.
+  To better understand this disease, we used proximity biotinylation in HEK293 and HeLa cells to generate an inventory of potential RAB18 effectors. A restricted set of 25 RAB18-interactions were regulated by the binary RAB3GAP1-RAB3GAP2 RAB18-guanine nucleotide exchange factor (GEF) complex. These included three groups of functionally interrelated proteins: a group of microtubule-interacting/membrane shaping proteins; a group of proteins involved in membrane tethering and docking; and a group of lipid-modifying/lipid transport proteins. We provide confirmatory evidence for several of the interactors (SPG20/SPART, SEC22A, TMCO4). Further we provide functional evidence that RAB18 links the Δ8-Δ7 sterol isomerase emopamil binding protein (EBP) to a molecular machinery mobilizing the products of EBP-catalysis. The cholesterol precursor lathosterol accumulates in RAB18-null cells, and <em>de novo</em> cholesterol biosynthesis is reduced. Our data demonstrate that GEF-dependent Rab-interactions are highly amenable to interrogation by proximity biotinylation and suggest that Micro syndrome is a cholesterol biosynthesis disorder.</p><div id="sec-1" class="subsection"><p id="p-4"><strong>SUMMARY STATEMENT</strong> We used proximity biotinylation together with guanine nucleotide exchange factor (GEF)-null cell lines to discriminate functional RAB18-interactions. This approach revealed that RAB18 mediates lathosterol mobilization and cholesterol biosynthesis.</p></div>`,
+  '10.1101/610014': `<p id="p-2">Disrupted sleep is a major feature of Alzheimer’s Disease (AD), often arising years before symptoms of cognitive decline.
+  Prolonged wakefulness exacerbates the production of amyloid-beta (Aβ) species, a major driver of AD progression, suggesting that sleep loss further accelerates AD through a vicious cycle. However, the mechanisms by which Aβ affects sleep are unknown. We demonstrate in zebrafish that Aβ acutely and reversibly enhances or suppresses sleep as a function of oligomer length. Genetic disruptions revealed that short Aβ oligomers induce acute wakefulness through Adrenergic receptor b2 (Adrb2) and Progesterone membrane receptor component 1 (Pgrmc1), while longer Aβ forms induce sleep through a pharmacologically tractable Prion Protein (PrP) signalling cascade. Our data indicate that Aβ can trigger a bi-directional sleep/wake switch. Alterations to the brain’s Aβ oligomeric milieu, such as during the progression of AD, may therefore disrupt sleep via changes in acute signalling events.</p><div id="sec-1"><p><strong>HIGHLIGHTS</strong></p><ul id="list-1"><li id="list-item-1"><p id="p-3">Amyloid beta oligomers can drive either sleep or wakefulness, depending on their size</p></li><li id="list-item-2"><p id="p-4">Wakefulness driven by short amyloid beta oligomers requires binding partners Adrenergic Beta Receptor 2 and Pgrmc1</p></li><li id="list-item-3"><p id="p-5">Long amyloid beta oligomers drive sleep through interaction with Prion Protein</p></li><li id="list-item-4"><p id="p-6">The in vivo sleep effects of amyloid beta can be pharmacologically blocked by targeting several steps of the Amyloid beta-Prion Protein signalling cascade.</p></li></ul></div></div>`,
+  '10.1101/822718': `Cortisol, corticosterone and aldosterone activate full-length glucocorticoid receptor (GR) from elephant shark, a cartilaginous fish belonging to the oldest group of jawed vertebrates.
+  Activation by aldosterone a mineralocorticoid, indicates partial divergence of elephant shark GR from the MR. Progesterone activates elephant shark MR, but not elephant shark GR. Progesterone inhibits steroid binding to elephant shark GR, but not to human GR. Deletion of the N-terminal domain (NTD) from elephant shark GR (Truncated GR) reduced the response to corticosteroids, while truncated and full-length elephant shark MR had similar responses to corticosteroids. Chimeras of elephant shark GR NTD fused to MR DBD+LBD had increased activation by corticosteroids and progesterone compared to full-length elephant shark MR. Elephant shark MR NTD fused to GR DBD+LBD had similar activation as full-length elephant shark MR, indicating that activation of human GR by the NTD evolved early in GR divergence from the MR.`,
 } as Record<string, string|undefined>;
