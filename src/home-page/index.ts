94275fc9d9d170a1b974773c9c4fb0dfdef40f59
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-05-19 11:31:02 +0100

    Use the fetch-articles middleware on the home page #74

--- src/home-page/index.ts
+++ src/home-page/index.ts
@@ -1,12 +1,14 @@
 import { Middleware } from '@koa/router';
 import compose from 'koa-compose';
 import constructViewModel from './construct-view-model';
+import fetchArticles from './fetch-articles';
 import populateEditorialCommunities from './populate-editorial-communities';
 import renderHomePage from './render-home-page';
 import { Adapters } from '../types/adapters';
 
 export default (adapters: Adapters): Middleware => (
   compose([
+    fetchArticles(adapters.fetchArticle),
     populateEditorialCommunities(adapters.editorialCommunities),
     constructViewModel(),
     renderHomePage(adapters.editorialCommunities),
