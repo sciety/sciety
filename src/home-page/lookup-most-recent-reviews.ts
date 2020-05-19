a825fd65af73020ed383268512101e48b94895b6
Kevin Rutherford <kevin@rutherford-software.com>
2020-05-19 13:31:54 +0100

    Update hardcoded review data to refer to newer versions of the reviews #87

--- src/home-page/lookup-most-recent-reviews.ts
+++ src/home-page/lookup-most-recent-reviews.ts
@@ -7,27 +7,27 @@ export default (): Middleware => (
     const mostRecentReviews: Array<ReviewReference> = [
       {
         articleVersionDoi: new Doi('10.1101/642017'),
-        reviewDoi: new Doi('10.5281/zenodo.3820276'),
+        reviewDoi: new Doi('10.5281/zenodo.3833746'),
         editorialCommunityId: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
       },
       {
         articleVersionDoi: new Doi('10.1101/615682'),
-        reviewDoi: new Doi('10.5281/zenodo.3820283'),
+        reviewDoi: new Doi('10.5281/zenodo.3833918'),
         editorialCommunityId: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
       },
       {
         articleVersionDoi: new Doi('10.1101/629618'),
-        reviewDoi: new Doi('10.5281/zenodo.3820289'),
+        reviewDoi: new Doi('10.5281/zenodo.3833917'),
         editorialCommunityId: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
       },
       {
         articleVersionDoi: new Doi('10.1101/600445'),
-        reviewDoi: new Doi('10.5281/zenodo.3820295'),
+        reviewDoi: new Doi('10.5281/zenodo.3833915'),
         editorialCommunityId: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
       },
       {
         articleVersionDoi: new Doi('10.1101/252593'),
-        reviewDoi: new Doi('10.5281/zenodo.3820302'),
+        reviewDoi: new Doi('10.5281/zenodo.3833893'),
         editorialCommunityId: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
       },
     ];
