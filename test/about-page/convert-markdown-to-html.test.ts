014feee91fe0c9e120d27973edc0c2c7c838b8aa
Kevin Rutherford <kevin@rutherford-software.com>
2020-05-19 14:54:01 +0100

    Add tests for the markdown rendering middleware #85

---
+++ test/about-page/convert-markdown-to-html.test.ts
@@ -0,0 +1,33 @@
+import { Context, Middleware, Response } from 'koa';
+import convertMarkdownToHtml from '../../src/about-page/convert-markdown-to-html';
+import createContext from '../context';
+import runMiddleware from '../middleware';
+
+const invokeMiddleware = async (ctx: Context, next?: Middleware): Promise<Response> => {
+  const { response } = await runMiddleware(convertMarkdownToHtml(), ctx, next);
+  return response;
+};
+
+describe('convert-markdown-to-html middleware', (): void => {
+  let ctx: Context;
+
+  beforeEach(() => {
+    ctx = createContext();
+    ctx.state = {
+      markdown: '# Title',
+    };
+  });
+
+  it('converts the markdown to HTML', async (): Promise<void> => {
+    await invokeMiddleware(ctx);
+
+    expect(ctx.state.html).toStrictEqual('<h1 id="title">Title</h1>');
+  });
+
+  it('calls the next middleware', async (): Promise<void> => {
+    const next = jest.fn();
+    await invokeMiddleware(ctx, next);
+
+    expect(next).toHaveBeenCalledTimes(1);
+  });
+});
