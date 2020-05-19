3b2784aaf7af1790eb96ee02c0f7aca296c1b16e
Giorgio Sironi <g.sironi@elifesciences.org>
2020-05-19 15:33:32 +0100

    Remove hardcoding from orderByAddedDescending in in-memory-review-references

--- src/data/in-memory-review-references.ts
+++ src/data/in-memory-review-references.ts
@@ -5,7 +5,7 @@ import ReviewReferenceRepository from '../types/review-reference-repository';
 
 export default (): ReviewReferenceRepository => {
   const log = createLogger('repository:in-memory-review-references');
-  const reviewReferences: Array<ReviewReference> = [];
+  const reviewReferences: Array<ReviewReference & { added: Date }> = [];
 
   const reviewReferenceRepository: ReviewReferenceRepository = {
     add: (articleVersionDoi: Doi, reviewDoi: Doi, editorialCommunityId: string, added: Date) => {
@@ -29,36 +29,9 @@ export default (): ReviewReferenceRepository => {
         .filter((reference) => reference.editorialCommunityId === editorialCommunityId)
     ),
 
-    orderByAddedDescending: () => {
-      const mostRecentReviewReferences: Array<ReviewReference> = [
-        {
-          articleVersionDoi: new Doi('10.1101/642017'),
-          reviewDoi: new Doi('10.5281/zenodo.3833746'),
-          editorialCommunityId: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
-        },
-        {
-          articleVersionDoi: new Doi('10.1101/615682'),
-          reviewDoi: new Doi('10.5281/zenodo.3833918'),
-          editorialCommunityId: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
-        },
-        {
-          articleVersionDoi: new Doi('10.1101/629618'),
-          reviewDoi: new Doi('10.5281/zenodo.3833917'),
-          editorialCommunityId: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
-        },
-        {
-          articleVersionDoi: new Doi('10.1101/252593'),
-          reviewDoi: new Doi('10.5281/zenodo.3833893'),
-          editorialCommunityId: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
-        },
-        {
-          articleVersionDoi: new Doi('10.1101/600445'),
-          reviewDoi: new Doi('10.5281/zenodo.3833915'),
-          editorialCommunityId: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
-        },
-      ];
-      return mostRecentReviewReferences;
-    },
+    orderByAddedDescending: () => reviewReferences.sort((a, b) => (
+      b.added.getTime() - a.added.getTime()
+    )),
   };
   return reviewReferenceRepository;
 };
