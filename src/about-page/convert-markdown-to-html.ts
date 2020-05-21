732c5cfd0c1a8f56a439f34bea84bd91ba1b81aa
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-05-21 16:26:14 +0100

    Disable Showdown's default header identifiers

--- src/about-page/convert-markdown-to-html.ts
+++ src/about-page/convert-markdown-to-html.ts
@@ -2,7 +2,7 @@ import { Context, Middleware, Next } from 'koa';
 import showdown from 'showdown';
 
 export default (): Middleware => {
-  const converter = new showdown.Converter();
+  const converter = new showdown.Converter({ noHeaderId: true });
   return async (ctx: Context, next: Next): Promise<void> => {
     const text: string = await ctx.state.markdown;
     ctx.state.html = converter.makeHtml(text);
