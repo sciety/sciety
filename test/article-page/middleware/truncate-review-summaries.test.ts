487c4a8ccd89642c74c7987104a5cb630494c4d8
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-05-21 10:24:32 +0100

    Test that review summaries are left unchanged #99

--- test/article-page/middleware/truncate-review-summaries.test.ts
+++ test/article-page/middleware/truncate-review-summaries.test.ts
@@ -1,5 +1,6 @@
 import { Context, Middleware, Response } from 'koa';
 import truncateReviewSummaries from '../../../src/article-page/middleware/truncate-review-summaries';
+import { ReviewViewModel } from '../../../src/article-page/types/article-page-view-model';
 import createContext from '../../context';
 import runMiddleware from '../../middleware';
 
@@ -9,6 +10,28 @@ const makeRequest = async (ctx: Context, next?: Middleware): Promise<Response> =
 };
 
 describe('truncate-review-summaries middleware', (): void => {
+  it('does not change review summaries', async (): Promise<void> => {
+    const context = createContext();
+    context.state = {
+      articlePage: {
+        reviews: [
+          {
+            summary: 'Summary 1',
+          },
+          {
+            summary: 'Summary 2',
+          },
+        ],
+      },
+    };
+
+    await makeRequest(createContext());
+
+    const actual = context.state.articlePage.reviews.map((review: ReviewViewModel) => review.summary);
+
+    expect(actual).toStrictEqual(['Summary 1', 'Summary 2']);
+  });
+
   it('calls the next middleware', async (): Promise<void> => {
     const next = jest.fn();
     await makeRequest(createContext(), next);
