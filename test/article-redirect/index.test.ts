feb070a33435a163a253881c1e20667a15e1eb05
Kevin Rutherford <kevin@rutherford-software.com>
2020-05-22 13:05:06 +0100

    Fix some duplication in the redirect test #128

--- test/article-redirect/index.test.ts
+++ test/article-redirect/index.test.ts
@@ -2,12 +2,14 @@ import { PERMANENT_REDIRECT } from 'http-status-codes';
 import request, { Response } from 'supertest';
 import createServer from '../handlers/server';
 
+const arbitraryDoi: string = '10.1101/2000.1234';
+
 describe('article redirect route', (): void => {
   let response: Response;
 
   beforeEach(async () => {
     const { server } = createServer();
-    response = await request(server).get('/articles?doi=10.1101/2000.1234');
+    response = await request(server).get(`/articles?doi=${arbitraryDoi}`);
   });
 
   it('returns a redirect response', async (): Promise<void> => {
@@ -15,6 +17,6 @@ describe('article redirect route', (): void => {
   });
 
   it('redirects to the article page', async (): Promise<void> => {
-    expect(response.header.location).toBe('/articles/10.1101/2000.1234');
+    expect(response.header.location).toBe(`/articles/${arbitraryDoi}`);
   });
 });
