cbc53ebaac15f4d51c7a3a89c5c299f726b7f386
David Moulton <d.moulton@elifesciences.org>
2020-05-22 11:36:44 +0100

    Redirect to the requested DOI #128

--- src/article-redirect/index.ts
+++ src/article-redirect/index.ts
@@ -1,10 +1,11 @@
 import { PERMANENT_REDIRECT } from 'http-status-codes';
-import { Context, Middleware, Next } from 'koa';
+import { Middleware, RouterContext } from '@koa/router';
+import { Next } from 'koa';
 
 export default (): Middleware => (
-  async (ctx: Context, next: Next): Promise<void> => {
+  async (ctx: RouterContext, next: Next): Promise<void> => {
     ctx.response.status = PERMANENT_REDIRECT;
-    ctx.response.redirect('/articles/10.1101/2000.1234');
+    ctx.response.redirect(`/articles/${ctx.params.doi}`);
 
     await next();
   }
