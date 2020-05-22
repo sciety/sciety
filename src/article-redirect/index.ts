6613c9abebe88fb011444605ae0017b9323402b2
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-05-22 11:42:15 +0100

    Fix query string usage #128

--- src/article-redirect/index.ts
+++ src/article-redirect/index.ts
@@ -1,11 +1,10 @@
 import { PERMANENT_REDIRECT } from 'http-status-codes';
-import { Middleware, RouterContext } from '@koa/router';
-import { Next } from 'koa';
+import { Context, Middleware, Next } from 'koa';
 
 export default (): Middleware => (
-  async (ctx: RouterContext, next: Next): Promise<void> => {
+  async (ctx: Context, next: Next): Promise<void> => {
     ctx.response.status = PERMANENT_REDIRECT;
-    ctx.response.redirect(`/articles/${ctx.params.doi}`);
+    ctx.response.redirect(`/articles/${ctx.request.query.doi}`);
 
     await next();
   }
