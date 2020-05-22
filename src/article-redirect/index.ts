2796de151f75ba0eb13bf409b59d522168c922fa
Giorgio Sironi <g.sironi@elifesciences.org>
2020-05-22 11:19:11 +0100

    Fix linting of imports

--- src/article-redirect/index.ts
+++ src/article-redirect/index.ts
@@ -1,5 +1,5 @@
-import { Middleware, Next, Context } from 'koa';
 import { PERMANENT_REDIRECT } from 'http-status-codes';
+import { Context, Middleware, Next } from 'koa';
 
 export default (): Middleware => (
   async (ctx: Context, next: Next): Promise<void> => {
