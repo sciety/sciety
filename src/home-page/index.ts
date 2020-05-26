122166e918ae9ad124561a39c4eef0b17d4100dd
Kevin Rutherford <kevin@rutherford-software.com>
2020-05-26 11:55:14 +0100

    Remove unused home page middleware #128

--- src/home-page/index.ts
+++ src/home-page/index.ts
@@ -1,18 +1,7 @@
 import { Middleware } from '@koa/router';
-import compose from 'koa-compose';
-import constructViewModel from './construct-view-model';
-import fetchArticles from './fetch-articles';
-import lookupMostRecentReviewReferences from './lookup-most-recent-review-references';
-import populateEditorialCommunities from './populate-editorial-communities';
 import renderHomePage from './render-home-page';
 import { Adapters } from '../types/adapters';
 
 export default (adapters: Adapters): Middleware => (
-  compose([
-    lookupMostRecentReviewReferences(adapters.reviewReferenceRepository),
-    fetchArticles(adapters.fetchArticle),
-    populateEditorialCommunities(adapters.editorialCommunities),
-    constructViewModel(),
-    renderHomePage(adapters.editorialCommunities, adapters.reviewReferenceRepository, adapters.fetchArticle),
-  ])
+  renderHomePage(adapters.editorialCommunities, adapters.reviewReferenceRepository, adapters.fetchArticle)
 );
