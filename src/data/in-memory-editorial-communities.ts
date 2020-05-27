8955ff0c498d00d1fd8a26cb4f740e08674c59bd
Giorgio Sironi <g.sironi@elifesciences.org>
2020-05-27 14:02:09 +0100

    Add HTML for PREreview page #134

--- src/data/in-memory-editorial-communities.ts
+++ src/data/in-memory-editorial-communities.ts
@@ -46,9 +46,29 @@ export default (): EditorialCommunityRepository => {
     {
       id: '10360d97-bf52-4aef-b2fa-2f60d319edd7',
       name: 'PREreview Community',
-      description: `<p>PREreview's mission is to bring more diversity to scholarly peer review by supporting and
-      empowering community of researchers, particularly those at early stages of their career (ECRs) to review
-      preprints.</p>`,
+      description: `<h2 id="summary">Summary</h2>
+      <p>PREreview's mission is to bring more diversity to scholarly peer review by supporting and empowering 
+      community of researchers, particularly those at early stages of their career (ECRs) to review preprints.</p>
+      <p>We envision a system in which any researcher, regardless of their prior engagement with journal-organized
+       peer review, career level, geographical location, institutional affiliation, research field, gender, sexual 
+       orientation, and ability is invited to share openly constructive feedback on early version of research 
+       manuscripts shared as preprints on any web server.</p>
+      </p>
+      <h2 id="peer-review-model">Peer review model</h2>
+      <p>At PREreview we believe all researchers should be allowed to help others by reviewing the work of their 
+       peers, as long as it is done constructively.</p>
+      <p>In the interest of fostering an open and welcoming environment we, as contributors and maintainers, 
+       pledge to making participation in our project and our community a harassment-free experience for everyone,
+       regardless of age, body size, disability, ethnicity, gender identity and expression, level of experience,
+       nationality, personal appearance, race, religion, or sexual identity and orientation.</p>
+      <p>We believe it is our duty as scientists at any level of our career to contribute to scientific evaluation 
+       in the form of peer review. PREreview provides a space for any researcher, independently of their career level,
+       to provide feedback to emerging scientific output.</p>
+      <p>At PREreview we ask all contributors to disclose any competing interest (CI) that may exist between a rapid
+       review author (or affiliated  organization) and the author(s) (or affiliated organization) of the reviewed 
+       preprint.</p>
+      <h2 id="people">People</h2>
+      <p><a href="https://content.prereview.org/about/#toc-anchor_4">https://content.prereview.org/about/#toc-anchor_4</a></p>`,
     },
     {
       id: '53ed5364-a016-11ea-bb37-0242ac130002',
