231999c7de525acb2813c611373c3b2be78a3656
David Moulton <d.moulton@elifesciences.org>
2020-05-27 13:45:17 +0100

    Lint fix

--- src/data/in-memory-editorial-communities.ts
+++ src/data/in-memory-editorial-communities.ts
@@ -13,14 +13,14 @@ export default (): EditorialCommunityRepository => {
     {
       id: '10360d97-bf52-4aef-b2fa-2f60d319edd7',
       name: 'PREreview Community',
-      description: `PREreview's mission is to bring more diversity to scholarly peer review by supporting and 
-      empowering community of researchers, particularly those at early stages of their career (ECRs) to review 
+      description: `PREreview's mission is to bring more diversity to scholarly peer review by supporting and
+      empowering community of researchers, particularly those at early stages of their career (ECRs) to review
       preprints.`,
     },
     {
       id: '53ed5364-a016-11ea-bb37-0242ac130002',
       name: 'PeerJ',
-      description: `This is a placeholder text for the PeerJ editorial community.`,
+      description: 'This is a placeholder text for the PeerJ editorial community.',
     },
   ];
 
