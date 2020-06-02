37c2404de7983e92d02a89a14ed2aa200673abee
Giorgio Sironi <g.sironi@elifesciences.org>
2020-06-02 15:34:20 +0100

    [bug] Remove hardcoded abstract for 10.1101/642017 #82
    The Crossref abstract contains a badly formatted <sup> tag that makes it render separately from the previous word, making it different from Biorxiv

--- src/data/abstracts.ts
+++ src/data/abstracts.ts
@@ -1,14 +1,4 @@
 export default {
-  '10.1101/642017': `Innate lymphoid cells (ILCs) were originally classified based on their cytokine profiles, placing
-   natural killer (NK) cells and ILC1s together, but recent studies support their separation into different lineages
-   at steady-state. However, tumors may induce NK cell conversion into ILC1-like cells that are limited to the tumor
-   microenvironment and whether this conversion occurs beyond this environment remains unknown. Here we describe
-   <em>Toxoplasma gondii</em> infection converts NK cells into cells resembling steady-state ILC1s that are
-   heterogeneous and distinct from both steady-state NK cells and ILC1s in uninfected mice. Most toxoplasma-induced
-   ILC1s were Eomes-dependent, indicating that NK cells can give rise to Eomes<sup>−</sup> Tbet-dependent ILC1-like
-   cells that circulate widely and persist independent of ongoing infection. Moreover, these changes appear permanent,
-   as supported by epigenetic analyses. Thus, these studies markedly expand current concepts of NK cells, ILCs, and
-   their potential conversion.`,
   '10.1101/615682': `Post-transcriptional regulation plays important roles to finely tune gene expression in bacteria.
   In particular, regulation of type I toxin-antitoxin (TA) systems is achieved through sophisticated mechanisms
   involving toxin mRNA folding. Here, we set up a genetic approach to decipher the molecular underpinnings behind
