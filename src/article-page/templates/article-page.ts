e4048f3d54fdb6a795ed29f617742a5a56540734
Kevin Rutherford <kevin@rutherford-software.com>
2020-06-02 09:12:52 +0100

    Extract a factory function for a renderPageHeader component in the article-page #128

--- src/article-page/templates/article-page.ts
+++ src/article-page/templates/article-page.ts
@@ -1,18 +1,32 @@
 import addReviewForm from './add-review-form';
 import templateArticlePageHeader from './article-page-header';
 import templateReviewSummary from './review-summary';
+import Doi from '../../data/doi';
 import templateListItems from '../../templates/list-items';
 import EditorialCommunityRepository from '../../types/editorial-community-repository';
 import { ArticlePageViewModel } from '../types/article-page-view-model';
+
+interface ArticleHeader {
+  title: string;
+  authors: Array<string>;
+  doi: Doi;
+  publicationDate: Date;
+}
+
+const createRenderPageHeader = (header: () => ArticleHeader) => (
+  (): string => templateArticlePageHeader(header())
+);
 
 export default (
   { article, reviews }: ArticlePageViewModel,
   editorialCommunities: EditorialCommunityRepository,
 ): string => {
   const reviewSummaries = reviews.map((review, index) => templateReviewSummary(review, `review-${index}`));
+  const articleHeaderAdapter = (): ArticleHeader => article;
+  const renderPageheader = createRenderPageHeader(articleHeaderAdapter);
   return `<article>
 
-    ${templateArticlePageHeader(article)}
+    ${renderPageheader()}
 
     <div class="content">
 
