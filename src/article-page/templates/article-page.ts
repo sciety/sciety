da3aa3fe487019bf4a93e75ba442d931152e4cc2
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-06-03 15:09:16 +0100

    Use extracted render-page-header component on the article page #128

--- src/article-page/templates/article-page.ts
+++ src/article-page/templates/article-page.ts
@@ -1,33 +1,17 @@
 import addReviewForm from './add-review-form';
-import templateArticlePageHeader from './article-page-header';
 import templateReviewSummary from './review-summary';
-import Doi from '../../data/doi';
 import templateListItems from '../../templates/list-items';
 import EditorialCommunityRepository from '../../types/editorial-community-repository';
 import createRenderArticleAbstract, { GetArticleAbstract } from '../render-article-abstract';
+import createRenderPageHeader, { GetArticleDetails } from '../render-page-header';
 import { ArticlePageViewModel } from '../types/article-page-view-model';
-
-interface ArticleDetails {
-  title: string;
-  authors: Array<string>;
-  doi: Doi;
-  publicationDate: Date;
-}
-
-type RenderPageHeader = (doi: Doi) => Promise<string>;
-
-type GetArticleDetails = (doi: Doi) => Promise<ArticleDetails>;
-
-const createRenderPageHeader = (getArticleDetails: GetArticleDetails): RenderPageHeader => (
-  async (doi) => templateArticlePageHeader(await getArticleDetails(doi))
-);
 
 export default async (
   { article, reviews }: ArticlePageViewModel,
   editorialCommunities: EditorialCommunityRepository,
 ): Promise<string> => {
   const reviewSummaries = reviews.map((review, index) => templateReviewSummary(review, `review-${index}`));
-  const getArticleDetailsAdapter = async (): Promise<ArticleDetails> => article;
+  const getArticleDetailsAdapter: GetArticleDetails = async () => article;
   const abstractAdapter: GetArticleAbstract = async () => ({ content: article.abstract });
   const renderPageHeader = createRenderPageHeader(getArticleDetailsAdapter);
   const renderArticleAbstract = createRenderArticleAbstract(abstractAdapter);
--- test/article-page/render-page-header.test.ts
+++ test/article-page/render-page-header.test.ts
@@ -2,17 +2,38 @@ import createRenderPageHeader, { GetArticleDetails } from '../../src/article-pag
 import Doi from '../../src/data/doi';
 
 describe('render-page-header component', (): void => {
-  it('renders the page header for an article', async (): Promise<void> => {
+  let rendered: string;
+
+  beforeEach(async () => {
     const getArticleDetails: GetArticleDetails = async (doi) => ({
       title: `Lorem ipsum ${doi}`,
-      authors: [],
+      authors: ['Gary', 'Uncle Wiggly'],
       publicationDate: new Date('2020-06-03'),
     });
 
     const renderPageHeader = createRenderPageHeader(getArticleDetails);
 
-    const rendered = await renderPageHeader(new Doi('10.1101/815689'));
+    rendered = await renderPageHeader(new Doi('10.1101/815689'));
+  });
 
+  it('renders inside an header tag', () => {
+    expect(rendered).toStrictEqual(expect.stringMatching(/^\s*<header\s/));
+  });
+
+  it('renders the title for an article', async (): Promise<void> => {
     expect(rendered).toStrictEqual(expect.stringContaining('Lorem ipsum 10.1101/815689'));
+  });
+
+  it('renders the article DOI', () => {
+    expect(rendered).toStrictEqual(expect.stringContaining('10.1101/815689'));
+  });
+
+  it('renders the article publication date', () => {
+    expect(rendered).toStrictEqual(expect.stringContaining('2020-06-03'));
+  });
+
+  it('renders the article authors', () => {
+    expect(rendered).toStrictEqual(expect.stringContaining('Gary'));
+    expect(rendered).toStrictEqual(expect.stringContaining('Uncle Wiggly'));
   });
 });
