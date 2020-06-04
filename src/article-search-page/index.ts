d9e41451dabcecf3de88917c9e6e723eb64572b7
Kevin Rutherford <kevin@rutherford-software.com>
2020-06-04 12:14:08 +0100

    Display all returned search results #138

--- src/article-search-page/index.ts
+++ src/article-search-page/index.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 import { Context, Middleware, Next } from 'koa';
 import createLogger from '../logger';
+import templateListItems from '../templates/list-items';
 
+interface SearchResult {
+  doi: string;
+  title: string;
+};
 
 export default (): Middleware => {
   const log = createLogger('article-search-page:index');
@@ -9,13 +14,13 @@ export default (): Middleware => {
     const uri = `https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=${ctx.request.query.doi}%20PUBLISHER%3A%22bioRxiv%22&format=json&pageSize=10`;
     const response = await axios.get(uri);
     log(response.data);
-    const article = response.data.resultList.result[0];
+    const articles = response.data.resultList.result.map((result: SearchResult) => (
+      `<a href="/articles/${result.doi}">${result.title}</a>`
+    ));
     ctx.response.body = `
       <h1>Search results</h1>
       <ul>
-        <li>
-          <a href="/articles/${article.doi}">${article.title}</a>
-        </li>
+        ${templateListItems(articles)}
       </ul>
     `;
 
