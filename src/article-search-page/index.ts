030ee6c86764a65152c38ee6e347b766976224e6
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-06-04 13:48:35 +0100

    Wrap renderSearchResults in a factory function #138

--- src/article-search-page/index.ts
+++ src/article-search-page/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Context, Middleware, Next } from 'koa';
+import { Middleware } from 'koa';
 import createLogger from '../logger';
 import templateListItems from '../templates/list-items';
 
@@ -8,28 +8,34 @@ interface SearchResult {
   title: string;
 }
 
-export const renderSearchResults = async (query: string): Promise<string> => {
-  const uri = `https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=${query}%20PUBLISHER%3A%22bioRxiv%22&format=json&pageSize=10`;
-  const response = await axios.get(uri);
-  const log = createLogger('article-search-page:index');
-  log(response.data);
-  const articles = response.data.resultList.result.map((result: SearchResult) => (
-    `<a href="/articles/${result.doi}">${result.title}</a>`
-  ));
-  return `
-    <ul>
-      ${templateListItems(articles)}
-    </ul>
-  `;
-};
+type RenderSearchResults = (query: string) => Promise<string>;
 
-export default (): Middleware => (
-  async (ctx: Context, next: Next): Promise<void> => {
+export const createRenderSearchResults = (): RenderSearchResults => (
+  async (query) => {
+    const uri = `https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=${query}%20PUBLISHER%3A%22bioRxiv%22&format=json&pageSize=10`;
+    const response = await axios.get(uri);
+    const log = createLogger('article-search-page:index');
+    log(response.data);
+    const articles = response.data.resultList.result.map((result: SearchResult) => (
+      `<a href="/articles/${result.doi}">${result.title}</a>`
+    ));
+    return `
+      <ul>
+        ${templateListItems(articles)}
+      </ul>
+    `;
+  }
+);
+
+export default (): Middleware => {
+  const renderSearchResults = createRenderSearchResults();
+
+  return async (ctx, next) => {
     ctx.response.body = `
       <h1>Search results</h1>
       ${await renderSearchResults(ctx.request.query.doi)}
     `;
 
     await next();
-  }
-);
+  };
+};
--- test/article-search-page/render-search-results.test.ts
+++ test/article-search-page/render-search-results.test.ts
@@ -1,8 +1,8 @@
-import { renderSearchResults } from '../../src/article-search-page';
+import { createRenderSearchResults } from '../../src/article-search-page';
 
 describe('render-search-results component', (): void => {
   it('queries by a DOI', async (): Promise<void> => {
-    const rendered = await renderSearchResults('10.1101/833392');
+    const rendered = await createRenderSearchResults()('10.1101/833392');
 
     expect(rendered).toStrictEqual(expect.stringContaining('10.1101/833392'));
   });
