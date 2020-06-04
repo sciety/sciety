5d9e81bf536a2bd81312be5cf1fb555f68279b20
David Moulton <d.moulton@elifesciences.org>
2020-06-04 13:35:06 +0100

    Extract component renderSearchResults in article-search-page #138

--- src/article-search-page/index.ts
+++ src/article-search-page/index.ts
@@ -8,22 +8,28 @@ interface SearchResult {
   title: string;
 }
 
-export default (): Middleware => {
+const renderSearchResults = async (query: string): Promise<string> => {
+  const uri = `https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=${query}%20PUBLISHER%3A%22bioRxiv%22&format=json&pageSize=10`;
+  const response = await axios.get(uri);
   const log = createLogger('article-search-page:index');
-  return async (ctx: Context, next: Next): Promise<void> => {
-    const uri = `https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=${ctx.request.query.doi}%20PUBLISHER%3A%22bioRxiv%22&format=json&pageSize=10`;
-    const response = await axios.get(uri);
-    log(response.data);
-    const articles = response.data.resultList.result.map((result: SearchResult) => (
-      `<a href="/articles/${result.doi}">${result.title}</a>`
-    ));
+  log(response.data);
+  const articles = response.data.resultList.result.map((result: SearchResult) => (
+    `<a href="/articles/${result.doi}">${result.title}</a>`
+  ));
+  return `
+    <ul>
+      ${templateListItems(articles)}
+    </ul>
+  `;
+};
+
+export default (): Middleware => (
+  async (ctx: Context, next: Next): Promise<void> => {
     ctx.response.body = `
       <h1>Search results</h1>
-      <ul>
-        ${templateListItems(articles)}
-      </ul>
+      ${await renderSearchResults(ctx.request.query.doi)}
     `;
 
     await next();
-  };
-};
+  }
+);
