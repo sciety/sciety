23f8e5707e12fa3a84cc829b7739201ff1d60a6f
David Moulton <d.moulton@elifesciences.org>
2020-06-04 11:46:36 +0100

    Get bioRxiv search results from EuropePMC #138

--- src/article-redirect/index.ts
+++ src/article-redirect/index.ts
@@ -1,7 +1,14 @@
+import axios from 'axios';
 import { Context, Middleware, Next } from 'koa';
+import createLogger from '../logger';
 
-export default (): Middleware => (
-  async (ctx: Context, next: Next): Promise<void> => {
+
+export default (): Middleware => {
+  const log = createLogger('article-redirect:index');
+  return async (ctx: Context, next: Next): Promise<void> => {
+    const uri = `https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=${ctx.request.query.doi}%20PUBLISHER%3A%22bioRxiv%22&format=json&pageSize=10`;
+    const response = await axios.get(uri);
+    log(response.data);
     ctx.response.body = `
       <h1>Search results</h1>
       <ul>
@@ -12,5 +19,5 @@ export default (): Middleware => (
     `;
 
     await next();
-  }
-);
+  };
+};
