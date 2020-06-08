4a38594c9c24eecafcb9e0e53f06e9620535a379
Kevin Rutherford <kevin@rutherford-software.com>
2020-06-08 09:21:43 +0100

    Extract functions to render two more components in the article-page #128

--- src/article-page/templates/article-page.ts
+++ src/article-page/templates/article-page.ts
@@ -10,36 +10,37 @@ export default async (
   { article, reviews }: ArticlePageViewModel,
   editorialCommunities: EditorialCommunityRepository,
 ): Promise<string> => {
-  const reviewSummaries = reviews.map((review, index) => templateReviewSummary(review, `review-${index}`));
   const getArticleDetailsAdapter: GetArticleDetails = async () => article;
   const abstractAdapter: GetArticleAbstract = async () => ({ content: article.abstract });
   const renderPageHeader = createRenderPageHeader(getArticleDetailsAdapter);
   const renderArticleAbstract = createRenderArticleAbstract(abstractAdapter);
-  return `<article>
-
-    ${await renderPageHeader(article.doi)}
-
-    <div class="content">
-
-      ${await renderArticleAbstract(article.doi)}
-
-      <section class="review-summary-list">
-        <h2 class="ui header">
-          Review summaries
-        </h2>
-        <ol class="review-summary-list__list">
-          ${templateListItems(reviewSummaries)}
-        </ol>
-      </section>
-
+  const renderReviewSummaries = (): string => {
+    const reviewSummaries = reviews.map((review, index) => templateReviewSummary(review, `review-${index}`));
+    return `
+      <h2 class="ui header">
+        Review summaries
+      </h2>
+      <ol class="review-summary-list__list">
+        ${templateListItems(reviewSummaries)}
+      </ol>
+    `;
+  };
+  const renderAddReviewForm = (): string => `
+    <div class="add-review__form">
+      <h2 class="ui header"> Add a review<br/>to this article </h2>
+      ${addReviewForm(article, editorialCommunities)}
     </div>
-
+  `;
+  return `<article>
+    ${await renderPageHeader(article.doi)}
+    <div class="content">
+      ${await renderArticleAbstract(article.doi)}
+      <section class="review-summary-list">
+        ${renderReviewSummaries()}
+      </section>
+    </div>
     <aside>
-      <div class="add-review__form">
-        <h2 class="ui header"> Add a review<br/>to this article </h2>
-        ${addReviewForm(article, editorialCommunities)}
-      </div>
+      ${renderAddReviewForm()}
     </aside>
-
   </article>`;
 };
