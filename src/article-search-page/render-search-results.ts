2777772058aee457c1b94b33132d24e56d8ac11b
Giorgio Sironi <g.sironi@elifesciences.org>
2020-06-09 15:22:56 +0100

    Isolate Europe PMC Bounded Context behind anti-corruption layer #161

--- src/article-search-page/render-search-results.ts
+++ src/article-search-page/render-search-results.ts
@@ -19,20 +19,20 @@ type RenderSearchResults = (query: string) => Promise<string>;
 
 const log = createLogger('article-search-page:render-search-results');
 
-type GetSearchResults = (europePmcQuery: string) => Promise<{
-  result: Array<SearchResult>;
-  hitCount: number;
+type GetSearchResults = (query: string) => Promise<{
+  items: Array<SearchResult>;
+  total: number;
 }>;
 
 const createFetchEuropePmcSearchResults = (getJson: GetJson): GetSearchResults => (
-  async (europePmcQuery) => {
+  async (query) => {
     const uri = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search'
-      + `?query=${europePmcQuery}%20PUBLISHER%3A%22bioRxiv%22&format=json&pageSize=10`;
+      + `?query=${query}%20PUBLISHER%3A%22bioRxiv%22&format=json&pageSize=10`;
     const data = await getJson(uri) as EuropePmcQueryResponse;
     log(data);
     return {
-      result: data.resultList.result,
-      hitCount: data.hitCount,
+      items: data.resultList.result,
+      total: data.hitCount,
     };
   }
 );
@@ -44,7 +44,7 @@ export default (
   async (query) => {
     const fetchEuropePmcSearchResults = createFetchEuropePmcSearchResults(getJson);
     const europePmcSearchResults = await fetchEuropePmcSearchResults(query);
-    const articles = await Promise.all(europePmcSearchResults.result.map(renderSearchResult));
+    const articles = await Promise.all(europePmcSearchResults.items.map(renderSearchResult));
     let searchResultsList = '';
     if (articles.length) {
       searchResultsList = `
@@ -55,7 +55,7 @@ export default (
     }
 
     return `
-      <p>${europePmcSearchResults.hitCount} search results.</p>
+      <p>${europePmcSearchResults.total} search results.</p>
       ${searchResultsList}
     `;
   }
