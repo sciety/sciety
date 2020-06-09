35dd1fd6831319d7ac3d9e7e429231b4123dafda
Kevin Rutherford <kevin@rutherford-software.com>
2020-06-09 10:35:12 +0100

    Separate middleware from rendering concerns #161

--- src/article-search-page/index.ts
+++ src/article-search-page/index.ts
@@ -1,18 +1,28 @@
 import { Middleware } from 'koa';
 import createRenderSearchResults from './render-search-results';
+import Doi from '../data/doi';
 import { Adapters } from '../types/adapters';
 
+export type GetJson = (uri: string) => Promise<object>;
+
+const createRenderArticleSearchPage = (
+  getJson: GetJson,
+  fetchReviewReferences: (articleVersionDoi: Doi) => Array<unknown>,
+) => async (query: string): Promise<string> => {
+  const renderSearchResults = createRenderSearchResults(getJson, fetchReviewReferences);
+  return `
+    <h1 class="header">Search results</h1>
+    ${await renderSearchResults(query)}
+  `;
+};
+
 export default (adapters: Adapters): Middleware => {
-  const renderSearchResults = createRenderSearchResults(
+  const renderArticleSearchPage = createRenderArticleSearchPage(
     adapters.getJson,
     adapters.reviewReferenceRepository.findReviewsForArticleVersionDoi,
   );
   return async (ctx, next) => {
-    ctx.response.body = `
-      <h1 class="header">Search results</h1>
-      ${await renderSearchResults(ctx.request.query.query)}
-    `;
-
+    ctx.response.body = await renderArticleSearchPage(ctx.request.query.query);
     await next();
   };
 };
