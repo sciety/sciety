90fc8a3d5b916debad63bf1f3189918640d94ee3
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-06-09 14:28:24 +0100

    Change add-review form styling to UI #164

--- src/article-page/templates/add-review-form.ts
+++ src/article-page/templates/add-review-form.ts
@@ -3,24 +3,27 @@ import EditorialCommunityRepository from '../../types/editorial-community-reposi
 
 export default (doi: Doi, editorialCommunities: EditorialCommunityRepository): string => {
   const options = editorialCommunities.all().map((ec) => `<option value="${ec.id}">${ec.name}</option>`);
-  return `<form method="post" action="/reviews" class="add-review-form">
+  return `<form method="post" action="/reviews" class="ui form">
     <input type="hidden" name="articleversiondoi" value="${doi}">
-    <p class="help-text">
+    <p>
       Please select the Editorial Community you represent and enter a Zenodo DOI.
     </p>
-    <select
-      name="editorialcommunityid"
-      id="editorialcommunityid">
-      ${options}
-    </select>
-    <input
-      type="text"
-      name="reviewdoi"
-      id="reviewdoi"
-      placeholder="Zenodo review DOI"
-      class="add-review-form__review-doi"
-      required>
-    <button type="submit" class="add-review-form__submit">
+    <div class="field">
+      <select
+        name="editorialcommunityid"
+        id="editorialcommunityid">
+        ${options}
+      </select>
+    </div>
+    <div class="field">
+      <input
+        type="text"
+        name="reviewdoi"
+        id="reviewdoi"
+        placeholder="Zenodo review DOI"
+        required>
+    </div>
+    <button type="submit" class="ui primary button">
       Add review
     </button>
   </form>`;
