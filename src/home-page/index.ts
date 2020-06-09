758443a9de72f236981cdd58894c6966c7768e74
Kevin Rutherford <kevin@rutherford-software.com>
2020-06-09 11:27:06 +0100

    Rename render-home-page to render-page for consistency #161

--- src/home-page/index.ts
+++ src/home-page/index.ts
@@ -1,8 +1,8 @@
 import { Middleware } from '@koa/router';
 import createRenderEditorialCommunities from './render-editorial-communities';
 import createRenderFindArticle from './render-find-article';
-import createRenderPage from './render-home-page';
 import createMostRecentReviews, { ReviewReference } from './render-most-recent-reviews';
+import createRenderPage from './render-page';
 import createRenderPageHeader from './render-page-header';
 import { Adapters } from '../types/adapters';
 
