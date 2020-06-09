edb5655773f5c72dec3c0183564ae8ea7020d713
David Moulton <d.moulton@elifesciences.org>
2020-06-09 10:38:46 +0100

    Use semantic-ui to style editorial community images #164

--- src/editorial-community-page/templates/header.ts
+++ src/editorial-community-page/templates/header.ts
@@ -7,7 +7,7 @@ interface Header {
 export default (header: Header): string => {
   let h1: string;
   if (header.logo !== undefined) {
-    h1 = `<img src="${header.logo}" alt="${header.name}">`;
+    h1 = `<img src="${header.logo}" alt="${header.name}" class="ui image">`;
   } else {
     h1 = header.name;
   }
