e2f377ad5371a02c704f6b2769f55d7ec460f000
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-06-11 14:26:52 +0100

    Extract hard-coded endorsements into a separate module #146

--- src/article-search-page/hard-coded-endorsing-editorial-communities.ts
+++ src/article-search-page/hard-coded-endorsing-editorial-communities.ts
@@ -1,4 +1,5 @@
 import { GetEndorsingEditorialCommunities } from './render-search-result';
+import endorsements from '../bootstrap-endorsements';
 
 export type GetNameForEditorialCommunity = (id: string) => string;
 
@@ -6,10 +7,6 @@ export default (
   getNameForEditorialCommunity: GetNameForEditorialCommunity,
 ): GetEndorsingEditorialCommunities => (
   async (doi) => {
-    const endorsements: Record<string, Array<string>|undefined> = {
-      '10.1101/209320': ['53ed5364-a016-11ea-bb37-0242ac130002'],
-      '10.1101/312330': ['53ed5364-a016-11ea-bb37-0242ac130002'],
-    };
     const editorialCommunityIds = endorsements[doi.value] ?? [];
     return editorialCommunityIds.map(getNameForEditorialCommunity);
   }
---
+++ src/bootstrap-endorsements.ts
@@ -0,0 +1,4 @@
+export default {
+  '10.1101/209320': ['53ed5364-a016-11ea-bb37-0242ac130002'],
+  '10.1101/312330': ['53ed5364-a016-11ea-bb37-0242ac130002'],
+} as Record<string, Array<string> | undefined>;
