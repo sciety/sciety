b2e1fa2f40e2a3c3626356c8b3f94c3ad1a6d184
Giorgio Sironi <g.sironi@elifesciences.org>
2020-06-11 10:37:39 +0100

    Split test for title and authors #146

--- test/article-search-page/render-search-result.test.ts
+++ test/article-search-page/render-search-result.test.ts
@@ -7,6 +7,24 @@ import Doi from '../../src/data/doi';
 const getReviewCount = (): number => 2;
 
 describe('render-search-result component', (): void => {
+  it('displays title and authors', async (): Promise<void> => {
+    const getCommentCount: GetCommentCount = async () => 0;
+    const getEndorsingEditorialCommunities: GetEndorsingEditorialCommunities = async () => [];
+    const rendered = await createRenderSearchResult(
+      getCommentCount,
+      getReviewCount,
+      getEndorsingEditorialCommunities,
+    )({
+      doi: new Doi('10.1101/833392'),
+      title: 'the title',
+      authors: '1, 2, 3',
+    });
+
+    expect(rendered).toStrictEqual(expect.stringContaining('10.1101/833392'));
+    expect(rendered).toStrictEqual(expect.stringContaining('the title'));
+    expect(rendered).toStrictEqual(expect.stringContaining('1, 2, 3'));
+  });
+
   describe('a comment count is available', (): void => {
     it('displays the number of comments', async (): Promise<void> => {
       const getCommentCount: GetCommentCount = async () => 37;
@@ -21,9 +39,7 @@ describe('render-search-result component', (): void => {
         authors: '1, 2, 3',
       });
 
-      expect(rendered).toStrictEqual(expect.stringContaining('10.1101/833392'));
       expect(rendered).toStrictEqual(expect.stringContaining('37'));
-      expect(rendered).toStrictEqual(expect.stringContaining('1, 2, 3'));
     });
   });
 
