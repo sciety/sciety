58cff76d2d452f5030ffc3e3f47164c57a5881cd
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-06-11 10:47:20 +0100

    Test that the list of endorsing editorial communities is displayed in render-search-result #146

--- test/article-search-page/render-search-result.test.ts
+++ test/article-search-page/render-search-result.test.ts
@@ -62,4 +62,23 @@ describe('render-search-result component', (): void => {
       expect(rendered).toStrictEqual(expect.stringContaining('n/a'));
     });
   });
+
+  describe('a list of endorsing editorial communities is available', (): void => {
+    it('displays the endorsing editorial communities', async (): Promise<void> => {
+      const getCommentCount: GetCommentCount = async () => 0;
+      const getEndorsingEditorialCommunities: GetEndorsingEditorialCommunities = async () => ['PeerJ', 'eLife'];
+      const rendered = await createRenderSearchResult(
+        getCommentCount,
+        getReviewCount,
+        getEndorsingEditorialCommunities,
+      )({
+        doi: new Doi('10.1101/833392'),
+        title: 'the title',
+        authors: '1, 2, 3',
+      });
+
+      expect(rendered).toStrictEqual(expect.stringMatching(/Endorsed by[\s\S]*?PeerJ/));
+      expect(rendered).toStrictEqual(expect.stringMatching(/Endorsed by[\s\S]*?eLife/));
+    });
+  });
 });
