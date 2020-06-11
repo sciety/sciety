9e636770905e6be6bd211ab654454569208af456
Giorgio Sironi <g.sironi@elifesciences.org>
2020-06-11 14:14:00 +0100

    Introduce map in hard-coded-endorsing-editorial-communities #146

--- src/article-search-page/hard-coded-endorsing-editorial-communities.ts
+++ src/article-search-page/hard-coded-endorsing-editorial-communities.ts
@@ -5,7 +5,12 @@ export type GetNameForEditorialCommunity = (id: string) => string;
 export default (
   getNameForEditorialCommunity: GetNameForEditorialCommunity,
 ): GetEndorsingEditorialCommunities => (
-  async (doi) => (
-    doi.value === '10.1101/209320' ? [await getNameForEditorialCommunity('53ed5364-a016-11ea-bb37-0242ac130002')] : []
-  )
+  async (doi) => {
+    const endorsements: Record<string, Array<string>|undefined> = {
+      '10.1101/209320': ['53ed5364-a016-11ea-bb37-0242ac130002'],
+      '10.1101/312330': ['53ed5364-a016-11ea-bb37-0242ac130002'],
+    };
+    const editorialCommunityIds = endorsements[doi.value] ?? [];
+    return editorialCommunityIds.map(getNameForEditorialCommunity);
+  }
 );
