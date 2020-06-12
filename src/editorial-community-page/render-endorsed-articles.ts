76f032eafbea78830a5b9f58adb9cfc73fe9b9da
Giorgio Sironi <g.sironi@elifesciences.org>
2020-06-12 15:29:19 +0100

    Extract read model for endorsedArticles #150

--- src/editorial-community-page/render-endorsed-articles.ts
+++ src/editorial-community-page/render-endorsed-articles.ts
@@ -1,3 +1,5 @@
+import Doi from '../data/doi';
+
 type RenderEndorsedArticles = (editorialCommunityId: string) => Promise<string>;
 
 export default (): RenderEndorsedArticles => (
@@ -5,6 +7,14 @@ export default (): RenderEndorsedArticles => (
     if (editorialCommunityId !== '53ed5364-a016-11ea-bb37-0242ac130002') {
       return '';
     }
+
+    const endorsedArticles = [
+      {
+        doi: new Doi('10.1101/209320'),
+        title: 'Marine cyanolichens from different littoral zones are associated with distinct bacterial communities',
+      },
+    ];
+
     return `
       <section class="ui basic vertical segment">
 
@@ -15,7 +25,7 @@ export default (): RenderEndorsedArticles => (
         <ol class="ui relaxed divided items">
           <li class="item">
             <div class="content">
-              <a href="/articles/10.1101/209320" class="header">Marine cyanolichens from different littoral zones are associated with distinct bacterial communities</a>
+              <a href="/articles/${endorsedArticles[0].doi.value}" class="header">${endorsedArticles[0].title}</a>
             </div>
           </li>
         </ol>
