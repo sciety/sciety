91325f299fcc7306bc7b989fc9d6f02d85c9e0a8
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-06-12 13:49:14 +0100

    Use JSDOM to test HTML #150

--- test/editorial-community/render-endorsed-articles.test.ts
+++ test/editorial-community/render-endorsed-articles.test.ts
@@ -1,12 +1,13 @@
+import { JSDOM } from 'jsdom';
 import createRenderEndorsedArticles from '../../src/editorial-community-page/render-endorsed-articles';
 
 describe('render-endorsed-articles component', (): void => {
   describe('when there are no endorsed articles', (): void => {
-    it('does not display the component heading', (): void => {
+    it('does not display anything', async (): Promise<void> => {
       const renderEndorsedArticles = createRenderEndorsedArticles();
-      const rendered = renderEndorsedArticles('any-old-id');
+      const rendered = JSDOM.fragment(await renderEndorsedArticles('any-old-id'));
 
-      expect(rendered).toStrictEqual(expect.not.stringContaining('<h2'));
+      expect(rendered.childNodes).toHaveLength(0);
     });
   });
 });
