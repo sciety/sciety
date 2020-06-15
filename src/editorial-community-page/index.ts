71391980a69e36a329d0aa04123b8bdfa55bb4ce
David Moulton <d.moulton@elifesciences.org>
2020-06-15 15:44:29 +0100

    Inline middleware in editorial-community-page #150

--- src/editorial-community-page/index.ts
+++ src/editorial-community-page/index.ts
@@ -1,11 +1,18 @@
-import { Middleware } from '@koa/router';
-import renderEditorialCommunityPage from './middleware/render-editorial-community-page';
+import { Middleware, RouterContext } from '@koa/router';
+import { Next } from 'koa';
+import createRenderPage from './render-page';
 import { Adapters } from '../types/adapters';
 
-export default (adapters: Adapters): Middleware => (
-  renderEditorialCommunityPage(
-    adapters.editorialCommunities,
+export default (adapters: Adapters): Middleware => {
+  const renderPage = createRenderPage(
     adapters.fetchArticle,
     adapters.reviewReferenceRepository,
-  )
-);
+    adapters.editorialCommunities,
+  );
+
+  return async (ctx: RouterContext, next: Next): Promise<void> => {
+    ctx.response.body = await renderPage(ctx.params.id);
+
+    await next();
+  };
+};
