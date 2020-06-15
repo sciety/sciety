7d8b7f5b205d268fd3c529e4e7200468c5ba54fb
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-06-15 10:13:55 +0100

    Don't display endorsed articles if the list is empty, rather than it isn't for PeerJ #150

--- src/editorial-community-page/render-endorsed-articles.ts
+++ src/editorial-community-page/render-endorsed-articles.ts
@@ -13,20 +13,24 @@ type RenderEndorsedArticles = (editorialCommunityId: string) => Promise<string>;
 
 export default (): RenderEndorsedArticles => (
   async (editorialCommunityId) => {
-    if (editorialCommunityId !== '53ed5364-a016-11ea-bb37-0242ac130002') {
-      return '';
+    const endorsedArticleData = [];
+
+    if (editorialCommunityId === '53ed5364-a016-11ea-bb37-0242ac130002') {
+      endorsedArticleData.push(
+        {
+          doi: new Doi('10.1101/209320'),
+          title: 'Marine cyanolichens from different littoral zones are associated with distinct bacterial communities',
+        },
+        {
+          doi: new Doi('10.1101/312330'),
+          title: 'A Real Time PCR Assay for Quantification of Parasite Burden in Murine Models of Leishmaniasis',
+        },
+      );
     }
 
-    const endorsedArticleData = [
-      {
-        doi: new Doi('10.1101/209320'),
-        title: 'Marine cyanolichens from different littoral zones are associated with distinct bacterial communities',
-      },
-      {
-        doi: new Doi('10.1101/312330'),
-        title: 'A Real Time PCR Assay for Quantification of Parasite Burden in Murine Models of Leishmaniasis',
-      },
-    ];
+    if (endorsedArticleData.length === 0) {
+      return '';
+    }
 
     const endorsedArticles = await Promise.all(endorsedArticleData.map(renderEndorsedArticle));
 
