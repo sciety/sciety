8379ba86034e5ac85f073d8b74809f906fd48c2c
Kevin Rutherford <kevin@rutherford-software.com>
2020-06-15 09:27:25 +0100

    Remove the truncate-review-summaries middleware #128

--- src/article-page/index.ts
+++ src/article-page/index.ts
@@ -4,7 +4,6 @@ import convertArticleAndReviewsToArticlePage from './middleware/convert-article-
 import fetchArticleForArticlePage from './middleware/fetch-article-for-article-page';
 import fetchReviewsForArticlePage from './middleware/fetch-reviews-for-article-page';
 import renderArticlePage from './middleware/render-article-page';
-import truncateReviewSummaries from './middleware/truncate-review-summaries';
 import validateBiorxivDoi from './middleware/validate-biorxiv-doi';
 import validateDoiParam from './middleware/validate-doi-param';
 import { Adapters } from '../types/adapters';
@@ -16,7 +15,6 @@ export default (adapters: Adapters): Middleware => (
     fetchArticleForArticlePage(adapters.fetchArticle),
     fetchReviewsForArticlePage(adapters.reviewReferenceRepository, adapters.fetchReview),
     convertArticleAndReviewsToArticlePage(adapters.editorialCommunities),
-    truncateReviewSummaries(1500),
     renderArticlePage(adapters.editorialCommunities),
   ])
 );
