bd2e2426b4be0367ba9678a8cbab73e505d7f450
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-06-15 11:07:25 +0100

    Separate hard-coded list of endorsed articles from their titles #150

--- src/editorial-community-page/render-endorsed-articles.ts
+++ src/editorial-community-page/render-endorsed-articles.ts
@@ -13,22 +13,27 @@ type RenderEndorsedArticles = (editorialCommunityId: string) => Promise<string>;
 
 type GetEndorsedArticles = (editorialCommunityId: string) => Promise<Array<{doi: Doi; title: string}>>;
 
-export const getHardCodedEndorsedArticles: GetEndorsedArticles = async (editorialCommunityId) => {
-  if (editorialCommunityId !== '53ed5364-a016-11ea-bb37-0242ac130002') {
-    return [];
-  }
+export type GetArticleTitle = (doi: Doi) => Promise<string>;
 
-  return [
-    {
-      doi: new Doi('10.1101/209320'),
-      title: 'Marine cyanolichens from different littoral zones are associated with distinct bacterial communities',
-    },
-    {
-      doi: new Doi('10.1101/312330'),
-      title: 'A Real Time PCR Assay for Quantification of Parasite Burden in Murine Models of Leishmaniasis',
-    },
-  ];
-};
+export const createGetHardCodedEndorsedArticles = (getArticleTitle: GetArticleTitle): GetEndorsedArticles => (
+  async (editorialCommunityId) => {
+    if (editorialCommunityId !== '53ed5364-a016-11ea-bb37-0242ac130002') {
+      return [];
+    }
+
+    const articleDois = [
+      new Doi('10.1101/209320'),
+      new Doi('10.1101/312330'),
+    ];
+
+    return Promise.all(articleDois.map(async (articleDoi) => (
+      {
+        doi: articleDoi,
+        title: await getArticleTitle(articleDoi),
+      }
+    )));
+  }
+);
 
 export default (getEndorsedArticles: GetEndorsedArticles): RenderEndorsedArticles => (
   async (editorialCommunityId) => {
--- src/editorial-community-page/render-page.ts
+++ src/editorial-community-page/render-page.ts
@@ -1,4 +1,7 @@
-import createRenderEndorsedArticles, { getHardCodedEndorsedArticles } from './render-endorsed-articles';
+import createRenderEndorsedArticles, {
+  createGetHardCodedEndorsedArticles,
+  GetArticleTitle,
+} from './render-endorsed-articles';
 import templateHeader from './templates/header';
 import templateReviewedArticles from './templates/reviewed-articles';
 import Doi from '../data/doi';
@@ -22,12 +25,20 @@ const createRenderPageHeader = (): RenderPageHeader => (
   async (editorialCommunity) => Promise.resolve(templateHeader(editorialCommunity))
 );
 
+const articleTitles: Record<string, string> = {
+  '10.1101/209320': 'Marine cyanolichens from different littoral zones are associated with distinct bacterial communities',
+  '10.1101/312330': 'A Real Time PCR Assay for Quantification of Parasite Burden in Murine Models of Leishmaniasis',
+};
+
 export default async (
   editorialCommunityId: string,
   viewModel: EditorialCommunity & ReviewedArticles,
 ): Promise<string> => {
   const renderPageHeader = createRenderPageHeader();
-  const renderEndorsedArticles = createRenderEndorsedArticles(getHardCodedEndorsedArticles);
+  const getArticleTitle: GetArticleTitle = async (articleDoi) => (
+    articleTitles[articleDoi.value] ?? 'Unknown article title'
+  );
+  const renderEndorsedArticles = createRenderEndorsedArticles(createGetHardCodedEndorsedArticles(getArticleTitle));
 
   return `
     ${await renderPageHeader(viewModel)}
