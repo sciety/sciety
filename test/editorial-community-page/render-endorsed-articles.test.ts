c73dd438fd6120b819dc0e5edee2252c2c2c0d45
Kevin Rutherford <kevin@rutherford-software.com>
2020-06-15 10:42:32 +0100

    Convert the render-endorsed-articles test into a unit test #150

--- test/editorial-community-page/render-endorsed-articles.test.ts
+++ test/editorial-community-page/render-endorsed-articles.test.ts
@@ -1,10 +1,11 @@
 import { JSDOM } from 'jsdom';
-import createRenderEndorsedArticles, { getHardCodedEndorsedArticles } from '../../src/editorial-community-page/render-endorsed-articles';
+import Doi from '../../src/data/doi';
+import createRenderEndorsedArticles from '../../src/editorial-community-page/render-endorsed-articles';
 
 describe('render-endorsed-articles component', (): void => {
   describe('when there are no endorsed articles', (): void => {
     it('does not display anything', async (): Promise<void> => {
-      const renderEndorsedArticles = createRenderEndorsedArticles(getHardCodedEndorsedArticles);
+      const renderEndorsedArticles = createRenderEndorsedArticles(async () => []);
       const rendered = JSDOM.fragment(await renderEndorsedArticles('any-old-id'));
 
       expect(rendered.childNodes).toHaveLength(0);
@@ -13,7 +14,12 @@ describe('render-endorsed-articles component', (): void => {
 
   describe('when there is an endorsed article', () => {
     it('has the expected heading and link', async () => {
-      const renderEndorsedArticles = createRenderEndorsedArticles(getHardCodedEndorsedArticles);
+      const renderEndorsedArticles = createRenderEndorsedArticles(async () => [
+        {
+          doi: new Doi('10.1101/209320'),
+          title: 'Some article title',
+        },
+      ]);
       const rendered = JSDOM.fragment(await renderEndorsedArticles('53ed5364-a016-11ea-bb37-0242ac130002'));
 
       expect(rendered.querySelector('h2')?.nodeName).toBe('H2');
