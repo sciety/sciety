004303472a7ae602ee1ec189559dc9f43c1b396c
Kevin Rutherford <kevin@rutherford-software.com>
2020-06-23 09:00:06 +0100

    Inline DOI validation middlewares #128

--- src/article-page/middleware/render-article-page.ts
+++ src/article-page/middleware/render-article-page.ts
@@ -1,10 +1,15 @@
 import { Middleware, RouterContext } from '@koa/router';
+import { NotFound } from 'http-errors';
 import { Next } from 'koa';
 import { FetchCrossrefArticle } from '../../api/fetch-crossref-article';
+import Doi from '../../data/doi';
+import createLogger from '../../logger';
 import EditorialCommunityRepository from '../../types/editorial-community-repository';
 import ReviewReferenceRepository from '../../types/review-reference-repository';
 import renderPage, { FetchReviews } from '../render-page';
 import { GetCommentCount } from '../render-page-header';
+
+const biorxivPrefix = '10.1101';
 
 export default (
   editorialCommunities: EditorialCommunityRepository,
@@ -12,10 +17,22 @@ export default (
   fetchArticle: FetchCrossrefArticle,
   fetchReviews: FetchReviews,
   reviewReferenceRepository: ReviewReferenceRepository,
-): Middleware => (
-  async (ctx: RouterContext, next: Next): Promise<void> => {
+): Middleware => {
+  const log = createLogger('middleware:render-article-page');
+  let doi: Doi;
+  return async (ctx: RouterContext, next: Next): Promise<void> => {
+    try {
+      doi = new Doi(ctx.params.doi || '');
+    } catch (error) {
+      log(`Article ${ctx.params.doi} not found: (${error})`);
+      throw new NotFound(`${ctx.params.doi} not found`);
+    }
+    if (!(doi.hasPrefix(biorxivPrefix))) {
+      log(`Article ${doi} is not from bioRxiv`);
+      throw new NotFound('Not a bioRxiv DOI.');
+    }
     ctx.response.body = await renderPage(
-      ctx.state.articleDoi,
+      doi,
       fetchReviews,
       editorialCommunities,
       getCommentCount,
@@ -25,5 +42,5 @@ export default (
     );
 
     await next();
-  }
-);
+  };
+};
