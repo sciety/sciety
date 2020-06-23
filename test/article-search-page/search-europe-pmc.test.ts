098ceb06d8acb7766b1962e9558138aaef30ff54
Giorgio Sironi <g.sironi@elifesciences.org>
2020-06-23 16:12:24 +0100

    Add happy path test for adapter #177

---
+++ test/article-search-page/search-europe-pmc.test.ts
@@ -0,0 +1,31 @@
+import createSearchEuropePmc from '../../src/article-search-page/search-europe-pmc';
+import Doi from '../../src/data/doi';
+
+describe('search-europe-pmc adapter', () => {
+  it('converts Europe PMC search result into our Domain Model', async () => {
+    const adapter = createSearchEuropePmc(async () => ({
+      hitCount: 1,
+      resultList: {
+        result: [
+          {
+            doi: '10.1111/1234',
+            title: 'Article title',
+            authorString: 'Author 1, Author 2',
+            firstPublicationDate: '2019-11-07',
+          },
+        ],
+      },
+    }));
+
+    const results = await adapter('some query');
+
+    expect(results.total).toStrictEqual(1);
+    expect(results.items).toHaveLength(1);
+    expect(results.items[0]).toMatchObject({
+      doi: new Doi('10.1111/1234'),
+      title: 'Article title',
+      authors: 'Author 1, Author 2',
+      postedDate: new Date('2019-11-07'),
+    });
+  });
+});
