c9939c2f297bbeeccacf622ae180040747b5019f
Kevin Rutherford <kevin@rutherford-software.com>
2020-06-25 12:02:41 +0100

    A review id can be a DOI or a Hypothes.is annotation id #179

--- src/types/review-id.ts
+++ src/types/review-id.ts
@@ -1,3 +1,4 @@
 import Doi from '../data/doi';
+import HypothesisAnnotationId from '../data/hypothesis-annotation-id';
 
-export type ReviewId = Doi;
+export type ReviewId = Doi | HypothesisAnnotationId;
