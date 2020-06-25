c2db94037993a1b78558843e5bdc8e6b1871efe4
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-06-25 16:02:34 +0100

    Add first Review Commons review #178

--- src/bootstrap-reviews.ts
+++ src/bootstrap-reviews.ts
@@ -59,4 +59,5 @@ export default [
   { article: '10.1101/751099', review: '10.7287/peerj.8938v0.1/reviews/2', editorialCommunityIndex: 2, added: new Date('2020-06-07') },
   { article: '10.1101/751099', review: '10.7287/peerj.8938v0.2/reviews/2', editorialCommunityIndex: 2, added: new Date('2020-06-06') },
   { article: '10.1101/751099', review: '10.7287/peerj.8938v0.2/reviews/1', editorialCommunityIndex: 2, added: new Date('2020-06-07') },
+  { article: '10.1101/826180', review: '5Znarq_TEeqgVdfomCVLHQ', editorialCommunityIndex: 4, added: new Date('2020-06-25') },
 ];
