47dae7541d83adeb10d79b3352fd9f9ddc48f1ae
Kevin Rutherford <kevin@rutherford-software.com>
2020-06-26 08:39:41 +0100

    Inline the final article-page middleware #128

--- src/article-page/index.ts
+++ src/article-page/index.ts
@@ -1,6 +1,8 @@
-import { Middleware } from '@koa/router';
+import { Middleware, RouterContext } from '@koa/router';
+import { Next } from 'koa';
 import createFetchReviews from './fetch-reviews';
-import renderArticlePage from './middleware/render-article-page';
+import renderPage from './render-page';
+import validateBiorxivDoi from './validate-biorxiv-doi';
 import { Adapters } from '../types/adapters';
 
 export default (adapters: Adapters): Middleware => {
@@ -9,11 +11,17 @@ export default (adapters: Adapters): Middleware => {
     adapters.fetchReview,
     adapters.editorialCommunities,
   );
-  return renderArticlePage(
-    adapters.editorialCommunities,
-    adapters.getBiorxivCommentCount,
-    adapters.fetchArticle,
-    fetchReviews,
-    adapters.reviewReferenceRepository,
-  );
+  return async (ctx: RouterContext, next: Next): Promise<void> => {
+    const doi = validateBiorxivDoi(ctx.params.doi);
+    ctx.response.body = await renderPage(
+      doi,
+      fetchReviews,
+      adapters.editorialCommunities,
+      adapters.getBiorxivCommentCount,
+      adapters.fetchArticle,
+      adapters.fetchArticle,
+      adapters.reviewReferenceRepository,
+    );
+    await next();
+  };
 };
