2f47b6ed2c09b7cdbba81d3ae8bb51ba54366084
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-06-26 09:57:40 +0100

    Test fetch-hypothesis-annotation happy path #179

---
+++ test/api/fetch-hypothesis-annotation.test.ts
@@ -0,0 +1,28 @@
+import createFetchHypothesisAnnotation, { GetJson } from '../../src/api/fetch-hypothesis-annotation'
+import HypothesisAnnotationId from '../../src/data/hypothesis-annotation-id';
+import { Review } from '../../src/types/review';
+
+const date = '2019-09-12T09:55:46.146050+00:00';
+const hypothesisAnnotationId = new HypothesisAnnotationId('fhAtGNVDEemkyCM-sRPpVQ');
+
+describe('fetch-hypothesis-annotation', (): void => {
+  it('returns the review', async () => {
+    const getJson: GetJson = async () => ({
+      created: date,
+      text: 'Very good',
+      links: {
+        incontext: 'https://www.example.com',
+      },
+    });
+    const fetchHypothesisAnnotation = createFetchHypothesisAnnotation(getJson);
+    const review = await fetchHypothesisAnnotation(hypothesisAnnotationId);
+
+    const expected: Review = {
+      publicationDate: new Date(date),
+      summary: 'Very good',
+      url: new URL('https://www.example.com'),
+    };
+
+    expect(review).toStrictEqual(expected);
+  });
+});
