620cbe662639a41523891f616009af5b7f83737b
Giorgio Sironi <g.sironi@elifesciences.org>
2020-07-02 11:43:17 +0100

    Shellcheck and optimize smoke test script #184

--- .github/smoke-test.sh
+++ .github/smoke-test.sh
@@ -3,14 +3,14 @@ set -e
 
 function finish() {
   echo "Stopping $container"
-  docker stop $container
+  docker stop "$container"
 }
 
 trap finish EXIT
 
-container=`docker run -d ${IMAGE:-liberoadmin/prc-frontend}:${IMAGE_TAG:-local}`
+container=$(docker run -d "liberoadmin/prc-frontend:${IMAGE_TAG:-local}")
 
-timeout --foreground 25 bash << EOT
+timeout --foreground 10 bash << EOT
   while true; do
     current=\$(docker inspect "${container}" | jq -r '.[0].State.Health.Status')
     echo "${container} is in state: \${current}"
