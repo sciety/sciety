079f704ff4293a378b1655918c49dedef696770e
Kevin Rutherford <kevin@rutherford-software.com>
2020-07-02 11:25:24 +0100

    Ping the container until it is healthy #184

--- .github/smoke-test.sh
+++ .github/smoke-test.sh
@@ -1,4 +1,22 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
 
-true
+function finish() {
+  echo "Stopping $container"
+  docker stop $container
+}
+
+trap finish EXIT
+
+container=`docker run -d ${IMAGE:-liberoadmin/prc-frontend}:${IMAGE_TAG:-local}`
+
+timeout --foreground 25 bash << EOT
+  while true; do
+    current=\$(docker inspect "${container}" | jq -r '.[0].State.Health.Status')
+    echo "${container} is in state: \${current}"
+    if [ "\$current" == "healthy" ]; then
+        break
+    fi
+    sleep 1
+  done
+EOT
