88407c9cf84d4cb9989926de7d156f0db808e240
Kevin Rutherford <kevin@rutherford-software.com>
2020-07-03 13:59:33 +0100

    Inline a template so that all components render in a more consistent way #128

--- src/editorial-community-page/render-page-header.ts
+++ src/editorial-community-page/render-page-header.ts
@@ -1,17 +1,31 @@
-import templateHeader from './templates/header';
+export type RenderPageHeader = (editorialCommunityId: string) => Promise<string>;
 
-interface EditorialCommunity {
+export type GetEditorialCommunity = (editorialCommunityId: string) => Promise<{
   name: string;
   description: string;
   logo?: string;
-}
-
-export type RenderPageHeader = (editorialCommunityId: string) => Promise<string>;
-
-export type GetEditorialCommunity = (editorialCommunityId: string) => Promise<EditorialCommunity>;
+}>;
 
 export default (
   getEditorialCommunity: GetEditorialCommunity,
 ): RenderPageHeader => (
-  async (editorialCommunityId) => Promise.resolve(templateHeader(await getEditorialCommunity(editorialCommunityId)))
+  async (editorialCommunityId) => {
+    const editorialCommunity = await getEditorialCommunity(editorialCommunityId);
+    let h1: string;
+    if (editorialCommunity.logo !== undefined) {
+      h1 = `<img src="${editorialCommunity.logo}" alt="${editorialCommunity.name}" class="ui image">`;
+    } else {
+      h1 = editorialCommunity.name;
+    }
+    return `
+      <header class="ui basic padded vertical segment">
+        <h1 class="ui header">
+          ${h1}
+        </h1>
+      </header>
+      <section class="ui basic vertical segment">
+        ${editorialCommunity.description}
+      </section>
+    `;
+  }
 );
