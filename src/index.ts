9b0641c438b7f9de5a3d02c05f1d63c5f468dc41
Kevin Rutherford <kevin@rutherford-software.com>
2020-07-07 12:34:38 +0100

    Move instantiation of all infrastructure into one place #182

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,9 @@
 import { AsyncLocalStorage } from 'async_hooks';
 import { createTerminus, TerminusOptions } from '@godaddy/terminus';
-import axios from 'axios';
-import bootstrapReviews from './bootstrap-reviews';
-import createFetchCrossrefArticle, { MakeHttpRequest } from './infrastructure/fetch-crossref-article';
-import createFetchDataciteReview from './infrastructure/fetch-datacite-review';
-import createFetchDataset from './infrastructure/fetch-dataset';
-import createFetchHypothesisAnnotation from './infrastructure/fetch-hypothesis-annotation';
-import createFetchReview from './infrastructure/fetch-review';
-import createFetchStaticFile from './infrastructure/fetch-static-file';
-import createGetBiorxivCommentCount from './infrastructure/get-biorxiv-comment-count';
-import createGetDisqusPostCount from './infrastructure/get-disqus-post-count';
-import createEditorialCommunityRepository from './infrastructure/in-memory-editorial-communities';
-import createReviewReferenceRepository from './infrastructure/in-memory-review-references';
+import createInfrastructure from './infrastructure';
 import { createDebugLogger } from './logger';
 import createRouter from './router';
 import createServer from './server';
-import { Adapters } from './types/adapters';
-import Doi from './types/doi';
-import HypothesisAnnotationId from './types/hypothesis-annotation-id';
-import { ReviewId } from './types/review-id';
 
 const asyncLocalStorage = new AsyncLocalStorage<string>();
 
@@ -26,50 +11,7 @@ const logger = createDebugLogger(asyncLocalStorage);
 
 logger('debug', 'Starting server');
 
-const editorialCommunities = createEditorialCommunityRepository();
-
-const reviewReferenceRepository = createReviewReferenceRepository(logger);
-for (const {
-  article, review, editorialCommunityIndex, added,
-} of bootstrapReviews) {
-  let reviewId: ReviewId;
-  try {
-    reviewId = new Doi(review);
-  } catch {
-    reviewId = new HypothesisAnnotationId(review);
-  }
-  void reviewReferenceRepository.add(
-    new Doi(article),
-    reviewId,
-    editorialCommunities.all()[editorialCommunityIndex].id,
-    added,
-  );
-}
-
-const makeHttpRequest: MakeHttpRequest = async (uri, acceptHeader) => {
-  const response = await axios.get(uri, { headers: { Accept: acceptHeader } });
-
-  return response.data;
-};
-
-const getJson: Adapters['getJson'] = async (uri) => {
-  const response = await axios.get(uri);
-
-  return response.data;
-};
-
-const fetchDataset = createFetchDataset();
-const fetchDataciteReview = createFetchDataciteReview(fetchDataset);
-const fetchHypothesisAnnotation = createFetchHypothesisAnnotation(getJson);
-const adapters: Adapters = {
-  fetchArticle: createFetchCrossrefArticle(makeHttpRequest, logger),
-  getBiorxivCommentCount: createGetBiorxivCommentCount(createGetDisqusPostCount(getJson)),
-  fetchReview: createFetchReview(fetchDataciteReview, fetchHypothesisAnnotation),
-  fetchStaticFile: createFetchStaticFile(),
-  editorialCommunities,
-  reviewReferenceRepository,
-  getJson,
-};
+const adapters = createInfrastructure(logger);
 
 const router = createRouter(adapters);
 
---
+++ src/infrastructure/index.ts
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import createFetchCrossrefArticle, { MakeHttpRequest } from './fetch-crossref-article';
+import createFetchDataciteReview from './fetch-datacite-review';
+import createFetchDataset from './fetch-dataset';
+import createFetchHypothesisAnnotation from './fetch-hypothesis-annotation';
+import createFetchReview from './fetch-review';
+import createFetchStaticFile from './fetch-static-file';
+import createGetBiorxivCommentCount from './get-biorxiv-comment-count';
+import createGetDisqusPostCount from './get-disqus-post-count';
+import createEditorialCommunityRepository from './in-memory-editorial-communities';
+import createReviewReferenceRepository from './in-memory-review-references';
+import bootstrapReviews from '../bootstrap-reviews';
+import { Logger } from '../logger';
+import { Adapters } from '../types/adapters';
+import Doi from '../types/doi';
+import HypothesisAnnotationId from '../types/hypothesis-annotation-id';
+import { ReviewId } from '../types/review-id';
+import ReviewReferenceRepository from '../types/review-reference-repository';
+
+const editorialCommunities = createEditorialCommunityRepository();
+
+const populateReviewReferenceRepository = (logger: Logger): ReviewReferenceRepository => {
+  const repository = createReviewReferenceRepository(logger);
+
+  for (const {
+    article, review, editorialCommunityIndex, added,
+  } of bootstrapReviews) {
+    let reviewId: ReviewId;
+    try {
+      reviewId = new Doi(review);
+    } catch {
+      reviewId = new HypothesisAnnotationId(review);
+    }
+    void repository.add(
+      new Doi(article),
+      reviewId,
+      editorialCommunities.all()[editorialCommunityIndex].id,
+      added,
+    );
+  }
+  return repository;
+};
+
+const makeHttpRequest: MakeHttpRequest = async (uri, acceptHeader) => {
+  const response = await axios.get(uri, { headers: { Accept: acceptHeader } });
+  return response.data;
+};
+
+const getJson: Adapters['getJson'] = async (uri) => {
+  const response = await axios.get(uri);
+  return response.data;
+};
+
+const createInfrastructure = (logger: Logger): Adapters => {
+  const fetchDataset = createFetchDataset();
+  const fetchDataciteReview = createFetchDataciteReview(fetchDataset);
+  const fetchHypothesisAnnotation = createFetchHypothesisAnnotation(getJson);
+
+  return {
+    fetchArticle: createFetchCrossrefArticle(makeHttpRequest, logger),
+    getBiorxivCommentCount: createGetBiorxivCommentCount(createGetDisqusPostCount(getJson)),
+    fetchReview: createFetchReview(fetchDataciteReview, fetchHypothesisAnnotation),
+    fetchStaticFile: createFetchStaticFile(),
+    editorialCommunities,
+    reviewReferenceRepository: populateReviewReferenceRepository(logger),
+    getJson,
+  };
+};
+
+export default createInfrastructure;
