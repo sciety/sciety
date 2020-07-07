6025a6d98375ec46a439ac00447c3b70acb354dc
Kevin Rutherford <kevin@rutherford-software.com>
2020-07-07 14:45:22 +0100

    Delete some now-obsolete functions and types #182

--- src/infrastructure/in-memory-endorsements-repository.ts
+++ src/infrastructure/in-memory-endorsements-repository.ts
@@ -2,20 +2,6 @@ import endorsements from '../data/bootstrap-endorsements';
 import Doi from '../types/doi';
 import EndorsementsRepository from '../types/endorsements-repository';
 
-type GetEndorsingEditorialCommunityIds = (doi: Doi) => Promise<Array<string>>;
-
-export const endorsingEditorialCommunityIds: GetEndorsingEditorialCommunityIds = async (doi) => (
-  endorsements[doi.value] ?? []
-);
-
-export type GetEndorsedArticleDois = (editorialCommunityId: string) => Promise<Array<Doi>>;
-
-export const endorsedBy: GetEndorsedArticleDois = async (editorialCommunityId) => (
-  Object.entries(endorsements)
-    .filter((entry) => entry[1]?.includes(editorialCommunityId))
-    .map((entry) => new Doi(entry[0]))
-);
-
 export default (): EndorsementsRepository => {
   const data: Record<string, Array<string> | undefined> = endorsements;
 
