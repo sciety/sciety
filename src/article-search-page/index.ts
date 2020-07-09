45bd406502f3cd73bd436c54eb23b697bbfa3097
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-07-09 14:28:13 +0100

    Separate creation of the search-europe-pmc adapter from the component #71

--- src/article-search-page/index.ts
+++ src/article-search-page/index.ts
@@ -6,14 +6,10 @@ import createRenderSearchResult, {
   GetReviewCount,
   RenderSearchResult,
 } from './render-search-result';
-import createRenderSearchResults, { RenderSearchResults } from './render-search-results';
+import createRenderSearchResults from './render-search-results';
 import createSearchEuropePmc from './search-europe-pmc';
-import { Logger } from '../logger';
 import { Adapters } from '../types/adapters';
 import EndorsementsRepository from '../types/endorsements-repository';
-import { Json } from '../types/json';
-
-type GetJson = (uri: string) => Promise<Json>;
 
 const buildRenderSearchResult = (
   getCommentCount: GetCommentCount,
@@ -32,15 +28,6 @@ const buildRenderSearchResult = (
   );
 };
 
-const buildRenderSearchResults = (
-  getJson: GetJson,
-  logger: Logger,
-  renderSearchResult: RenderSearchResult,
-): RenderSearchResults => {
-  const findArticles = createSearchEuropePmc(getJson, logger);
-  return createRenderSearchResults(findArticles, renderSearchResult);
-};
-
 export default (adapters: Adapters): Middleware => {
   const getReviewCount: GetReviewCount = async (doi) => (
     (await adapters.reviewReferenceRepository.findReviewsForArticleVersionDoi(doi)).length
@@ -51,7 +38,8 @@ export default (adapters: Adapters): Middleware => {
     async (editorialCommunityId) => (await adapters.editorialCommunities.lookup(editorialCommunityId)).unsafelyUnwrap(),
     adapters.endorsements,
   );
-  const renderSearchResults = buildRenderSearchResults(adapters.getJson, adapters.logger, renderSearchResult);
+  const searchEuropePmc = createSearchEuropePmc(adapters.getJson, adapters.logger);
+  const renderSearchResults = createRenderSearchResults(searchEuropePmc, renderSearchResult);
 
   const renderPage = createRenderPage(renderSearchResults);
   return async (ctx, next) => {
