434173c4c5c530bd5242491d77de66be57344f80
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-07-09 14:16:21 +0100

    Stop logging when checking the DOI for an article page is a bioRxiv DOI #71

--- src/article-page/validate-biorxiv-doi.ts
+++ src/article-page/validate-biorxiv-doi.ts
@@ -1,20 +1,16 @@
 import { NotFound } from 'http-errors';
-import createLogger from '../logger';
 import Doi from '../types/doi';
 
 const biorxivPrefix = '10.1101';
 
 export default (value: string): Doi => {
-  const log = createLogger('middleware:render-article-page');
   let doi: Doi;
   try {
     doi = new Doi(value || '');
   } catch (error) {
-    log(`Invalid DOI '${value}': (${error})`);
     throw new NotFound(`${value} not found`);
   }
   if (!(doi.hasPrefix(biorxivPrefix))) {
-    log(`DOI ${doi} is not from bioRxiv`);
     throw new NotFound('Not a bioRxiv DOI.');
   }
   return doi;
