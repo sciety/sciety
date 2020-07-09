d50e96412969b6936344bc6020b7cd98c2ef5e59
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-07-09 08:53:23 +0100

    Use type imports for rdf-js

--- test/infrastructure/fetch-dataset.test.ts
+++ test/infrastructure/fetch-dataset.test.ts
@@ -5,7 +5,7 @@
 import { namedNode } from '@rdfjs/data-model';
 import rdfFetch, { DatasetResponse } from '@rdfjs/fetch-lite';
 import datasetFactory from 'rdf-dataset-indexed';
-import { DatasetCore } from 'rdf-js'; // eslint-disable-line import/no-extraneous-dependencies -- https://github.com/benmosher/eslint-plugin-import/issues/1618
+import type { DatasetCore } from 'rdf-js';
 import createFetchDataset, { FetchDatasetError } from '../../src/infrastructure/fetch-dataset';
 import Doi from '../../src/types/doi';
 
