f9791685d4e1490ee74a7af7c194cad4b6e2bf56
Kevin Rutherford <kevin@rutherford-software.com>
2020-07-14 11:54:08 +0100

    Address a lint warning #186

--- src/infrastructure/get-disqus-post-count.ts
+++ src/infrastructure/get-disqus-post-count.ts
@@ -14,9 +14,11 @@ type GetDisqusPostCount = (uri: string) => Promise<Maybe<number>>;
 
 export default (getJson: GetJson, logger: Logger): GetDisqusPostCount => (
   async (uri) => {
+    const disqusApiKey = process.env.DISQUS_API_KEY ?? '';
+
     logger('debug', `Fetching Disqus threads list for ${uri}`);
     try {
-      const disqusData = await getJson(`https://disqus.com/api/3.0/threads/list.json?api_key=${process.env.DISQUS_API_KEY}&forum=biorxivstage&thread=link:${uri}`) as DisqusData;
+      const disqusData = await getJson(`https://disqus.com/api/3.0/threads/list.json?api_key=${disqusApiKey}&forum=biorxivstage&thread=link:${uri}`) as DisqusData;
       logger('debug', `Disqus response: ${JSON.stringify(disqusData)}`);
       return Maybe.just(disqusData.response[0].posts);
     } catch (e) {
