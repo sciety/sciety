1901e425b1f0c8d0d590a2c2521d0f31c4d8d47b
David Moulton <d.moulton@elifesciences.org>
2020-07-17 10:17:55 +0100

    Allow fetching of all eLife-endorsed bioRxiv articles #171

--- scripts/find-elife-endorsements.ts
+++ scripts/find-elife-endorsements.ts
@@ -4,6 +4,11 @@ import { JsonCompatible } from '../src/types/json';
 const elifeId = 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0';
 
 type BiorxivResponse = JsonCompatible<{
+  messages: Array<{
+    cursor: number | string,
+    count: number,
+    total: number,
+  }>,
   collection: Array<{
     biorxiv_doi: string,
   }>
@@ -12,12 +17,24 @@ type BiorxivResponse = JsonCompatible<{
 void (async (): Promise<void> => {
   const today = new Date().toISOString().split('T')[0];
 
-  const { data } = await axios.get<BiorxivResponse>(`https://api.biorxiv.org/publisher/10.7554/2000-01-01/${today}/0`);
-
-  const endorsements = data.collection.map((item) => ({
+  const { data: firstPage } = await axios.get<BiorxivResponse>(`https://api.biorxiv.org/publisher/10.7554/2000-01-01/${today}/0`);
+  let endorsements = firstPage.collection.map((item) => ({
     article: item.biorxiv_doi,
     editorialCommunity: elifeId,
   }));
+
+  const { count } = firstPage.messages[0];
+  const numRequestsNeeded = Math.ceil(firstPage.messages[0].total / count);
+  for (let i = 1; i < numRequestsNeeded; i += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    const { data } = await axios.get<BiorxivResponse>(`https://api.biorxiv.org/publisher/10.7554/2000-01-01/${today}/${count * i}`);
+    endorsements = endorsements.concat(
+      data.collection.map((item) => ({
+        article: item.biorxiv_doi,
+        editorialCommunity: elifeId,
+      })),
+    );
+  }
 
   process.stdout.write(JSON.stringify(endorsements, undefined, 2));
 })();
