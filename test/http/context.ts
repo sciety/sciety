85735a9aa3e84cbfb174bbacd3372027e4ea3873
Kevin Rutherford <kevin@rutherford-software.com>
2020-07-17 16:31:52 +0100

    Remove unused middleware test harness #128

--- test/http/context.ts
+++
@@ -1,7 +0,0 @@
-import Koa, { DefaultState, ExtendableContext, ParameterizedContext } from 'koa';
-import { Request, Response } from 'mock-http';
-
-export default <Context extends ExtendableContext = ExtendableContext>
-({ request = new Request(), response = new Response() } = {}): ParameterizedContext<DefaultState, Context> => (
-  new Koa<DefaultState, Context>().createContext(request, response) as ParameterizedContext<DefaultState, Context>
-);
