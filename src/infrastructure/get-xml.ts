e7bb2cfcd297cadc405311308675da5181f369af
David Moulton <d.moulton@elifesciences.org>
2020-07-27 11:35:51 +0100

    Retry requests for Crossref XML #203

--- src/infrastructure/get-xml.ts
+++ src/infrastructure/get-xml.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 
 type GetXml = (uri: string, acceptHeader: string) => Promise<string>;
 
 const getXml: GetXml = async (uri, acceptHeader) => {
-  const response = await axios.get<string>(uri, { headers: { Accept: acceptHeader } });
+  const client = axios.create();
+  axiosRetry(client, { retries: 3 });
+  const response = await client.get<string>(uri, { headers: { Accept: acceptHeader } });
   return response.data;
 };
 
