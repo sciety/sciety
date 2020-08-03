d4ee16dbe838d8f7cedeb3fc8535e089b76452af
David Moulton <d.moulton@elifesciences.org>
2020-08-03 12:14:16 +0100

    Template all ArticleReviewedEvents on home page #205

--- src/home-page/render-feed.ts
+++ src/home-page/render-feed.ts
@@ -34,38 +34,31 @@ const events: Array<Event> = [
     articleTitle: 'Functional assessment of cell entry and receptor usage for lineage B β-coronaviruses, including 2019-nCoV',
   },
   {
+    type: 'ArticleReviewed',
     imageUrl: 'https://pbs.twimg.com/profile_images/1095287970939265026/xgyGFDJk_200x200.jpg',
     date: new Date('2020-07-09'),
-    summary: `
-      <a href="/editorial-communities/53ed5364-a016-11ea-bb37-0242ac130002">PeerJ</a> reviewed <a href="/articles/10.1101/751099">Diffusion tubes: a method for the mass culture of ctenophores and other pelagic marine invertebrates</a>
-    `,
+    editorialCommunityId: '53ed5364-a016-11ea-bb37-0242ac130002',
+    editorialCommunityName: 'PeerJ',
+    articleId: '10.1101/751099',
+    articleTitle: 'Diffusion tubes: a method for the mass culture of ctenophores and other pelagic marine invertebrates',
   },
   {
+    type: 'ArticleReviewed',
     imageUrl: 'https://pbs.twimg.com/profile_images/1204012644660854784/E8JhkG7__200x200.jpg',
     date: new Date('2020-07-09'),
-    summary: `
-      <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">
-        Review Commons
-      </a>
-      reviewed
-      <a href="/articles/10.1101/2019.12.13.875419">
-        Unconventional kinetochore kinases KKT2 and KKT3 have a unique zinc finger that promotes their
-        kinetochore localization
-      </a>
-    `,
+    editorialCommunityId: '316db7d9-88cc-4c26-b386-f067e0f56334',
+    editorialCommunityName: 'Review Commons',
+    articleId: '10.1101/2019.12.13.875419',
+    articleTitle: 'Unconventional kinetochore kinases KKT2 and KKT3 have a unique zinc finger that promotes their kinetochore localization',
   },
   {
+    type: 'ArticleReviewed',
     imageUrl: 'https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png',
     date: new Date('2020-07-08'),
-    summary: `
-      <a href="/editorial-communities/b560187e-f2fb-4ff9-a861-a204f3fc0fb0">
-        eLife
-      </a>
-      reviewed
-      <a href="/articles/10.1101/2020.05.14.095547">
-        GATA-1-dependent histone H3K27ac mediates erythroid cell-specific interaction between CTCF sites
-      </a>
-    `,
+    editorialCommunityId: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
+    editorialCommunityName: 'eLife',
+    articleId: '10.1101/2020.05.14.095547',
+    articleTitle: 'GATA-1-dependent histone H3K27ac mediates erythroid cell-specific interaction between CTCF sites',
   },
   {
     imageUrl: 'https://pbs.twimg.com/profile_images/1095287970939265026/xgyGFDJk_200x200.jpg',
@@ -81,17 +74,13 @@ const events: Array<Event> = [
     `,
   },
   {
+    type: 'ArticleReviewed',
     imageUrl: 'https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png',
     date: new Date('2020-07-08'),
-    summary: `
-      <a href="/editorial-communities/b560187e-f2fb-4ff9-a861-a204f3fc0fb0">
-        eLife
-      </a>
-      reviewed
-      <a href="/articles/10.1101/2020.05.14.095547">
-        GATA-1-dependent histone H3K27ac mediates erythroid cell-specific interaction between CTCF sites
-      </a>
-    `,
+    editorialCommunityId: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
+    editorialCommunityName: 'eLife',
+    articleId: '10.1101/2020.05.14.095547',
+    articleTitle: 'GATA-1-dependent histone H3K27ac mediates erythroid cell-specific interaction between CTCF sites',
   },
   {
     imageUrl: 'https://pbs.twimg.com/profile_images/1204012644660854784/E8JhkG7__200x200.jpg',
