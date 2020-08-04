e4182d205a8f143fa5bf6b4438bb2b3116de206a
Giorgio Sironi <g.sironi@elifesciences.org>
2020-08-04 16:36:10 +0200

    Write test list for adapter #206

--- test/home-page/get-events-from-bootstrap-data.test.ts
+++ test/home-page/get-events-from-bootstrap-data.test.ts
@@ -1,10 +1,9 @@
 describe('get-events-from-bootstrap-data', () => {
-  it('sorts by date descending', async () => {
-  });
+  it.todo('sorts by date descending');
 
   describe('when there\'s a small number of items', () => {
     it.todo('returns exactly those');
-  })
+  });
 
   describe('when there are too many items to display', () => {
     it.todo('returns just N items');
