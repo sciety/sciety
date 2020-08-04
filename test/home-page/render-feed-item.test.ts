194caea36bd45b9d3b9a4b53fae5557e69c96217
David Moulton <d.moulton@hive.review>
2020-08-04 11:16:01 +0100

    Add skeleton test file for render-feed-item #205

---
+++ test/home-page/render-feed-item.test.ts
@@ -0,0 +1,29 @@
+describe('render-feed-item', (): void => {
+  describe('when given an ArticleEndorsedEvent', () => {
+    it.todo('displays the article title');
+
+    it.todo('displays the word "endorsed"');
+
+    it.todo('displays the actor name');
+
+    it.todo('displays the event date');
+  });
+
+  describe('when given an ArticleReviewedEvent', () => {
+    it.todo('displays the article title');
+
+    it.todo('displays the word "reviewed"');
+
+    it.todo('displays the actor name');
+
+    it.todo('displays the event date');
+  });
+
+  describe('when given an EditorialCommunityJoinedEvent', () => {
+    it.todo('displays the actor name');
+
+    it.todo('displays the word "joined"');
+
+    it.todo('displays the event date');
+  });
+});
