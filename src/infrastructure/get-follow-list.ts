9913237dc336df739585a205ffc34e5351de4178
Giorgio Sironi <g.sironi@elifesciences.org>
2020-08-05 17:10:14 +0200

    Populate GetFollowList in infrastructure index #211

--- src/infrastructure/get-follow-list.ts
+++ src/infrastructure/get-follow-list.ts
@@ -1,15 +1,7 @@
-import EditorialCommunityId from '../types/editorial-community-id';
 import { FollowList } from '../types/follow-list';
 
 export type GetFollowList = () => Promise<FollowList>;
 
-export default (): GetFollowList => (
-  async () => [
-    new EditorialCommunityId('316db7d9-88cc-4c26-b386-f067e0f56334'), // Review Commons
-    new EditorialCommunityId('10360d97-bf52-4aef-b2fa-2f60d319edd8'), // A PREreview Journal Club
-    new EditorialCommunityId('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'), // eLife
-    new EditorialCommunityId('10360d97-bf52-4aef-b2fa-2f60d319edd7'), // PREReview
-    new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'), // PeerJ
-    new EditorialCommunityId('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de'), // PCI Zoology
-  ]
+export default (followList: FollowList): GetFollowList => (
+  async () => followList
 );
--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -8,7 +8,7 @@ import createFetchReview from './fetch-review';
 import createFetchStaticFile from './fetch-static-file';
 import createGetBiorxivCommentCount from './get-biorxiv-comment-count';
 import createGetDisqusPostCount from './get-disqus-post-count';
-import createGetFollowList from './get-follow-list';
+import createGetFollowList, { GetFollowList } from './get-follow-list';
 import createGetXml from './get-xml';
 import createEditorialCommunityRepository from './in-memory-editorial-communities';
 import createEndorsementsRepository from './in-memory-endorsements-repository';
@@ -35,6 +35,18 @@ const populateEditorialCommunities = (logger: Logger): EditorialCommunityReposit
     void repository.add(editorialCommunity);
   }
   return repository;
+};
+
+const populateGetFollowList = (): GetFollowList => {
+  const getFollowList = createGetFollowList([
+    new EditorialCommunityId('316db7d9-88cc-4c26-b386-f067e0f56334'), // Review Commons
+    new EditorialCommunityId('10360d97-bf52-4aef-b2fa-2f60d319edd8'), // A PREreview Journal Club
+    new EditorialCommunityId('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'), // eLife
+    new EditorialCommunityId('10360d97-bf52-4aef-b2fa-2f60d319edd7'), // PREReview
+    new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'), // PeerJ
+    new EditorialCommunityId('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de'), // PCI Zoology
+  ]);
+  return getFollowList;
 };
 
 const populateEndorsementsRepository = (logger: Logger): EndorsementsRepository => {
@@ -89,7 +101,7 @@ const createInfrastructure = (): Adapters => {
   const fetchHypothesisAnnotation = createFetchHypothesisAnnotation(getJson, logger);
   const searchEuropePmc = createSearchEuropePmc(getJson, logger);
   const editorialCommunities = populateEditorialCommunities(logger);
-  const getFollowList = createGetFollowList();
+  const getFollowList = populateGetFollowList();
 
   return {
     fetchArticle: createFetchCrossrefArticle(getXml, logger),
