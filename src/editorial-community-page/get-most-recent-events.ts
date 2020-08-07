23b2ef701eaef9641b429754edf5e3e1f0845fb8
Kevin Rutherford <kevin@rutherford-software.com>
2020-08-07 15:21:31 +0100

    No need to use a FollowList when filtering editorial community events #212

--- src/editorial-community-page/get-most-recent-events.ts
+++ src/editorial-community-page/get-most-recent-events.ts
@@ -1,13 +1,11 @@
 import { GetEvents } from './render-feed';
 import { Event } from '../types/events';
-import FollowList from '../types/follow-list';
 
 type FilterFunction = (event: Event) => boolean;
 export type FilterEvents = (filterFunction: FilterFunction, maxCount: number) => Promise<Array<Event>>;
 
 export default (filterEvents: FilterEvents, maxCount: number): GetEvents => (
-  async (editorialCommunityId) => {
-    const followList = new FollowList([editorialCommunityId]);
-    return filterEvents((event) => followList.follows(event.actorId), maxCount);
-  }
+  async (editorialCommunityId) => (
+    filterEvents((event) => event.actorId.value === editorialCommunityId.value, maxCount)
+  )
 );
