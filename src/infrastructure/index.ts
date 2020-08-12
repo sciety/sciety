7a66031090359eec661df5df743c23dd0bbecd2c
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-08-12 14:05:27 +0100

    Create stub CSV files for the remaining communities #217

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -84,6 +84,10 @@ const getJson = async (uri: string): Promise<Json> => {
 const getEventsFromDataFiles = (): ReadonlyArray<DomainEvent> => {
   const editorialCommunities = [
     '53ed5364-a016-11ea-bb37-0242ac130002',
+    '74fd66e9-3b90-4b5a-a4ab-5be83db4c5de',
+    '316db7d9-88cc-4c26-b386-f067e0f56334',
+    '10360d97-bf52-4aef-b2fa-2f60d319edd7',
+    '10360d97-bf52-4aef-b2fa-2f60d319edd8',
     'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
   ];
 
