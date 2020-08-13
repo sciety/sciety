3ff9f83caf64427999689a8071a2f0ded375a9a5
Giorgio Sironi <g.sironi@elifesciences.org>
2020-08-13 15:41:20 +0100

    Fill in review events from all pages containing PeerJ articles #217

--- scripts/find-reviews-from-crossref-via-biorxiv.ts
+++ scripts/find-reviews-from-crossref-via-biorxiv.ts
@@ -30,27 +30,36 @@ type CrossrefResponse ={
 void (async (): Promise<void> => {
   process.stdout.write('Date,Article DOI,Review ID\n');
 
-  const { data: biorxivData } = await axios.get<BiorxivResponse>(
-    'https://api.biorxiv.org/publisher/10.7717/2000-01-01/2020-08-13/0',
-  );
-
-  for (const biorxivItem of biorxivData.collection) {
-    const publishedDoi = biorxivItem.published_doi;
-    const biorxivDoi = biorxivItem.biorxiv_doi;
-
-    // specify a User-Agent: https://github.com/CrossRef/rest-api-doc/issues/491
+  let offset = 0;
+  let total: number;
+  do {
     // eslint-disable-next-line no-await-in-loop
-    const { data } = await axios.get<CrossrefResponse>(
-      `https://api.crossref.org/prefixes/10.7287/works?rows=1000&filter=type:peer-review,relation.object:${publishedDoi}`,
-      { headers: { 'User-Agent': 'TheHive (http://hive.review; mailto:team@hive.review)' } },
+    const { data: biorxivData } = await axios.get<BiorxivResponse>(
+      `https://api.biorxiv.org/publisher/10.7717/2000-01-01/2020-08-13/${offset}`,
     );
+    const { count } = biorxivData.messages[0];
+    total = biorxivData.messages[0].total;
 
-    data.message.items.forEach((item) => {
-      const [year, month, day] = item['published-print']['date-parts'][0];
-      const date = new Date(year, month - 1, day);
-      const reviewDoi = item.DOI;
+    for (const biorxivItem of biorxivData.collection) {
+      const publishedDoi = biorxivItem.published_doi;
+      const biorxivDoi = biorxivItem.biorxiv_doi;
 
-      process.stdout.write(`${date.toISOString()},${biorxivDoi},doi:${reviewDoi}\n`);
-    });
-  }
+      // specify a User-Agent: https://github.com/CrossRef/rest-api-doc/issues/491
+      // eslint-disable-next-line no-await-in-loop
+      const { data } = await axios.get<CrossrefResponse>(
+        `https://api.crossref.org/prefixes/10.7287/works?rows=1000&filter=type:peer-review,relation.object:${publishedDoi}`,
+        { headers: { 'User-Agent': 'TheHive (http://hive.review; mailto:team@hive.review)' } },
+      );
+
+      data.message.items.forEach((item) => {
+        const [year, month, day] = item['published-print']['date-parts'][0];
+        const date = new Date(year, month - 1, day);
+        const reviewDoi = item.DOI;
+
+        process.stdout.write(`${date.toISOString()},${biorxivDoi},doi:${reviewDoi}\n`);
+      });
+    }
+
+    offset += count;
+  } while (offset < total);
 })();
