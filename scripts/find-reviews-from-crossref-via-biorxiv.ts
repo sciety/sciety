1250b9a86562044696727e4afe1d5f29e3c88581
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-08-13 14:48:48 +0100

    Find all reviews for the first PeerJ-reviewed article #217

--- scripts/find-reviews-from-crossref-via-biorxiv.ts
+++ scripts/find-reviews-from-crossref-via-biorxiv.ts
@@ -22,11 +22,15 @@ void (async (): Promise<void> => {
     published_doi: '10.7717/peerj.434',
   };
   // specify a User-Agent: https://github.com/CrossRef/rest-api-doc/issues/491
-  const { data } = await axios.get<CrossrefResponse>('https://api.crossref.org/prefixes/10.7287/works?filter=type:peer-review,relation.object:10.7717/peerj.434', { headers: { 'User-Agent': 'TheHive (http://hive.review; mailto:team@hive.review)' } });
+  const { data } = await axios.get<CrossrefResponse>('https://api.crossref.org/prefixes/10.7287/works?rows=1000&filter=type:peer-review,relation.object:10.7717/peerj.434', { headers: { 'User-Agent': 'TheHive (http://hive.review; mailto:team@hive.review)' } });
   process.stdout.write('Date,Article DOI,Review ID\n');
-  const [year, month, day] = data.message.items[0]['published-print']['date-parts'][0];
-  const date = new Date(year, month - 1, day);
   const articleDoi = biorxivResponseFirstCollectionItem.biorxiv_doi;
-  const reviewDoi = data.message.items[0].DOI;
-  process.stdout.write(`${date.toISOString()},${articleDoi},doi:${reviewDoi}\n`);
+
+  data.message.items.forEach((item) => {
+    const [year, month, day] = item['published-print']['date-parts'][0];
+    const date = new Date(year, month - 1, day);
+    const reviewDoi = item.DOI;
+
+    process.stdout.write(`${date.toISOString()},${articleDoi},doi:${reviewDoi}\n`);
+  });
 })();
