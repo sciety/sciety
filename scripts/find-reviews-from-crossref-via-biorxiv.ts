0e8bf2ecde24848d4ae469c3314f87733d70dc50
Giorgio Sironi <g.sironi@elifesciences.org>
2020-08-13 15:14:10 +0100

    Source published DOI for first PeerJ article from Biorxiv #217

--- scripts/find-reviews-from-crossref-via-biorxiv.ts
+++ scripts/find-reviews-from-crossref-via-biorxiv.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
+type BiorxivResponse = {
+  messages: Array<{
+    cursor: number | string,
+    count: number,
+    total: number,
+  }>,
+  collection: Array<{
+    biorxiv_doi: string,
+    published_doi: string,
+  }>
+};
+
 type CrossrefResponse ={
   message: {
     items: [
@@ -16,21 +28,25 @@ type CrossrefResponse ={
 };
 
 void (async (): Promise<void> => {
-  // from https://api.biorxiv.org/publisher/10.7717/2000-01-01/2020-08-13/0
-  const biorxivResponseFirstCollectionItem = {
-    biorxiv_doi: '10.1101/002717',
-    published_doi: '10.7717/peerj.434',
-  };
+  const { data: biorxivData } = await axios.get<BiorxivResponse>(
+    'https://api.biorxiv.org/publisher/10.7717/2000-01-01/2020-08-13/0',
+  );
+  const biorxivResponseFirstCollectionItem = biorxivData.collection[0];
+  const publishedDoi = biorxivResponseFirstCollectionItem.published_doi;
+  const biorxivDoi = biorxivResponseFirstCollectionItem.biorxiv_doi;
+
   // specify a User-Agent: https://github.com/CrossRef/rest-api-doc/issues/491
-  const { data } = await axios.get<CrossrefResponse>('https://api.crossref.org/prefixes/10.7287/works?rows=1000&filter=type:peer-review,relation.object:10.7717/peerj.434', { headers: { 'User-Agent': 'TheHive (http://hive.review; mailto:team@hive.review)' } });
+  const { data } = await axios.get<CrossrefResponse>(
+    `https://api.crossref.org/prefixes/10.7287/works?rows=1000&filter=type:peer-review,relation.object:${publishedDoi}`,
+    { headers: { 'User-Agent': 'TheHive (http://hive.review; mailto:team@hive.review)' } },
+  );
   process.stdout.write('Date,Article DOI,Review ID\n');
-  const articleDoi = biorxivResponseFirstCollectionItem.biorxiv_doi;
 
   data.message.items.forEach((item) => {
     const [year, month, day] = item['published-print']['date-parts'][0];
     const date = new Date(year, month - 1, day);
     const reviewDoi = item.DOI;
 
-    process.stdout.write(`${date.toISOString()},${articleDoi},doi:${reviewDoi}\n`);
+    process.stdout.write(`${date.toISOString()},${biorxivDoi},doi:${reviewDoi}\n`);
   });
 })();
