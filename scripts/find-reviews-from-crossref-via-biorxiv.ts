3d356ac7e4bd17f3028b66a6bff484c5995d3609
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-08-13 15:20:51 +0100

    Fetch reviews from the first page of bioRxiv results #217

--- scripts/find-reviews-from-crossref-via-biorxiv.ts
+++ scripts/find-reviews-from-crossref-via-biorxiv.ts
@@ -28,25 +28,29 @@ type CrossrefResponse ={
 };
 
 void (async (): Promise<void> => {
+  process.stdout.write('Date,Article DOI,Review ID\n');
+
   const { data: biorxivData } = await axios.get<BiorxivResponse>(
     'https://api.biorxiv.org/publisher/10.7717/2000-01-01/2020-08-13/0',
   );
-  const biorxivResponseFirstCollectionItem = biorxivData.collection[0];
-  const publishedDoi = biorxivResponseFirstCollectionItem.published_doi;
-  const biorxivDoi = biorxivResponseFirstCollectionItem.biorxiv_doi;
 
-  // specify a User-Agent: https://github.com/CrossRef/rest-api-doc/issues/491
-  const { data } = await axios.get<CrossrefResponse>(
-    `https://api.crossref.org/prefixes/10.7287/works?rows=1000&filter=type:peer-review,relation.object:${publishedDoi}`,
-    { headers: { 'User-Agent': 'TheHive (http://hive.review; mailto:team@hive.review)' } },
-  );
-  process.stdout.write('Date,Article DOI,Review ID\n');
+  for (const biorxivItem of biorxivData.collection) {
+    const publishedDoi = biorxivItem.published_doi;
+    const biorxivDoi = biorxivItem.biorxiv_doi;
 
-  data.message.items.forEach((item) => {
-    const [year, month, day] = item['published-print']['date-parts'][0];
-    const date = new Date(year, month - 1, day);
-    const reviewDoi = item.DOI;
+    // specify a User-Agent: https://github.com/CrossRef/rest-api-doc/issues/491
+    // eslint-disable-next-line no-await-in-loop
+    const { data } = await axios.get<CrossrefResponse>(
+      `https://api.crossref.org/prefixes/10.7287/works?rows=1000&filter=type:peer-review,relation.object:${publishedDoi}`,
+      { headers: { 'User-Agent': 'TheHive (http://hive.review; mailto:team@hive.review)' } },
+    );
 
-    process.stdout.write(`${date.toISOString()},${biorxivDoi},doi:${reviewDoi}\n`);
-  });
+    data.message.items.forEach((item) => {
+      const [year, month, day] = item['published-print']['date-parts'][0];
+      const date = new Date(year, month - 1, day);
+      const reviewDoi = item.DOI;
+
+      process.stdout.write(`${date.toISOString()},${biorxivDoi},doi:${reviewDoi}\n`);
+    });
+  }
 })();
