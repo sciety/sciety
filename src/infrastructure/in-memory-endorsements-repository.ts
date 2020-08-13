e28e92878f5de395355ad3a75c5e38d4f15d573f
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-08-13 13:18:02 +0100

    Remove intermediate type #219

--- src/infrastructure/in-memory-endorsements-repository.ts
+++ src/infrastructure/in-memory-endorsements-repository.ts
@@ -1,32 +1,18 @@
-import Doi from '../types/doi';
 import { ArticleEndorsedEvent } from '../types/domain-events';
-import EditorialCommunityId from '../types/editorial-community-id';
 import EndorsementsRepository from '../types/endorsements-repository';
-
-interface Endorsement {
-  article: Doi;
-  editorialCommunity: EditorialCommunityId;
-}
 
 export default (
   events: ReadonlyArray<ArticleEndorsedEvent>,
-): EndorsementsRepository => {
-  const data: Array<Endorsement> = events.map((event) => ({
-    article: event.articleId,
-    editorialCommunity: event.actorId,
-  }));
+): EndorsementsRepository => ({
+  endorsingEditorialCommunityIds: async (doi) => (
+    events
+      .filter((event) => event.articleId.value === doi.value)
+      .map((event) => event.actorId)
+  ),
 
-  return {
-    endorsingEditorialCommunityIds: async (doi) => (
-      data
-        .filter((e) => e.article.value === doi.value)
-        .map((e) => e.editorialCommunity)
-    ),
-
-    endorsedBy: async (editorialCommunityId) => (
-      data
-        .filter((e) => e.editorialCommunity.value === editorialCommunityId.value)
-        .map((e) => e.article)
-    ),
-  };
-};
+  endorsedBy: async (editorialCommunityId) => (
+    events
+      .filter((event) => event.actorId.value === editorialCommunityId.value)
+      .map((event) => event.articleId)
+  ),
+});
