6545b7d39d2844a0527a97b27acc225d452a94be
Kevin Rutherford <kevin@rutherford-software.com>
2020-08-14 13:01:44 +0100

    Extract a constant defining one year in milliseconds #215

--- src/http/identify-user.ts
+++ src/http/identify-user.ts
@@ -2,12 +2,14 @@ import { Middleware } from '@koa/router';
 import { v4 as uuidv4 } from 'uuid';
 import { Logger } from '../infrastructure/logger';
 
+const OneYear = 1000 * 60 * 60 * 24 * 365;
+
 export default (logger: Logger): Middleware => (
   async (context, next) => {
     const userIdentity = context.cookies.get('hiveSession') ?? uuidv4();
     logger('debug', 'User identity', { userIdentity });
     context.cookies.set('hiveSession', userIdentity, {
-      maxAge: 1000 * 60 * 60 * 24 * 365,
+      maxAge: OneYear,
       sameSite: 'strict',
     });
 
