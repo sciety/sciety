78ce40957d248cd1e24f109679b79e4b8a08b570
Kevin Rutherford <kevin@rutherford-software.com>
2020-08-17 11:58:15 +0100

    Parse the PCI RSS to find reviews #204

--- scripts/find-reviews-from-pci.ts
+++ scripts/find-reviews-from-pci.ts
@@ -1,19 +1,26 @@
 import axios from 'axios';
+import { DOMParser } from 'xmldom';
+
+const pciCommunityprefix = process.argv[2];
+
+const parser = new DOMParser({
+  errorHandler: (_, msg) => {
+    throw msg;
+  },
+});
 
 void (async (): Promise<void> => {
   process.stdout.write('Date,Article DOI,Review ID\n');
 
-  const links = [
-    'https://zool.peercommunityin.org/public/rec?id=4',
-    'https://zool.peercommunityin.org/public/rec?id=1',
-    'https://zool.peercommunityin.org/public/rec?id=3',
-  ];
+  const { data: feed } = await axios.get(`https://${pciCommunityprefix}.peercommunityin.org/public/rss4bioRxiv`);
+  const doc = parser.parseFromString(feed, 'text/xml');
 
-  links.forEach(async (link) => {
-    const { data } = await axios.get<string>(link);
+  for (const link of Array.from(doc.getElementsByTagName('link'))) {
+    const url = link.getElementsByTagName('url')[0];
+    const { data } = await axios.get<string>(url?.textContent ?? '');
     const [,date] = /<meta name="citation_publication_date" content="(.*?)" \/>/.exec(data) ?? [];
     const articleDoi = '';
     const [,reviewDoi] = /<meta name="citation_doi" content="(.*?)" \/>/.exec(data) ?? [];
     process.stdout.write(`${new Date(date).toISOString()},${articleDoi},${reviewDoi}\n`);
-  });
+  }
 })();
