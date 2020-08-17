091836a6ef951af7874e609c0790ad8517e8f853
Kevin Rutherford <kevin@rutherford-software.com>
2020-08-17 11:37:26 +0100

    Roughly parse PCI review pages #204

--- scripts/find-reviews-from-pci.ts
+++ scripts/find-reviews-from-pci.ts
@@ -1,3 +1,19 @@
+import axios from 'axios';
+
 void (async (): Promise<void> => {
   process.stdout.write('Date,Article DOI,Review ID\n');
+
+  const links = [
+    'https://zool.peercommunityin.org/public/rec?id=4',
+    'https://zool.peercommunityin.org/public/rec?id=1',
+    'https://zool.peercommunityin.org/public/rec?id=3',
+  ];
+
+  links.forEach(async (link) => {
+    const { data } = await axios.get<string>(link);
+    const date = new Date();
+    const articleDoi = '';
+    const [,reviewDoi] = /<meta name="citation_doi" content="(.*?)" \/>/.exec(data) ?? [];
+    process.stdout.write(`${date.toISOString()},${articleDoi},${reviewDoi}\n`);
+  });
 })();
