f702f192673c4a131520fb5e57eb1f42457225b0
Kevin Rutherford <kevin@rutherford-software.com>
2020-08-20 10:36:13 +0100

    Remove unused fields form the hardcoded data #223

--- src/user-page/get-hardcoded-followed-editorial-communities.ts
+++ src/user-page/get-hardcoded-followed-editorial-communities.ts
@@ -7,26 +7,14 @@ export type GetEditorialCommunity = (editorialCommunityId: EditorialCommunityId)
 }>;
 
 const list = [
-  {
-    id: new EditorialCommunityId('316db7d9-88cc-4c26-b386-f067e0f56334'),
-    name: 'Review Commons',
-    avatarUrl: 'https://pbs.twimg.com/profile_images/1204012644660854784/E8JhkG7__200x200.jpg',
-  },
-  {
-    id: new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
-    name: 'PeerJ',
-    avatarUrl: 'https://pbs.twimg.com/profile_images/1095287970939265026/xgyGFDJk_200x200.jpg',
-  },
-  {
-    id: new EditorialCommunityId('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de'),
-    name: 'Peer Community In Zoology',
-    avatarUrl: 'https://pbs.twimg.com/profile_images/1278236903549145089/qqgLuJu__400x400.jpg',
-  },
+  new EditorialCommunityId('316db7d9-88cc-4c26-b386-f067e0f56334'),
+  new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
+  new EditorialCommunityId('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de'),
 ];
 
 export default (getEditorialCommunity: GetEditorialCommunity): GetFollowedEditorialCommunities => (
-  async () => Promise.all(list.map(async ({ id }) => ({
-    id,
-    ...await getEditorialCommunity(id),
+  async () => Promise.all(list.map(async (editorialCommunityId) => ({
+    id: editorialCommunityId,
+    ...await getEditorialCommunity(editorialCommunityId),
   })))
 );
