d45ad9052f678adf2ed92687b4054e255f4be7bc
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-08-20 11:09:21 +0100

    Remove hard-coding of editorial community IDs #223

--- src/user-page/index.ts
+++ src/user-page/index.ts
@@ -2,6 +2,7 @@ import createGetHardcodedFollowedEditorialCommunities, { GetEditorialCommunity, 
 import createRenderFollowToggle from './render-follow-toggle';
 import createRenderFollowedEditorialCommunity from './render-followed-editorial-community';
 import createRenderPage from './render-page';
+import { UserFollowedEditorialCommunityEvent } from '../types/domain-events';
 import EditorialCommunityId from '../types/editorial-community-id';
 import EditorialCommunityRepository from '../types/editorial-community-repository';
 import FollowList from '../types/follow-list';
@@ -18,12 +19,31 @@ interface Params {
 
 type RenderPage = (params: Params) => Promise<string>;
 
+const hardcodedEvents: Array<UserFollowedEditorialCommunityEvent> = [
+  {
+    type: 'UserFollowedEditorialCommunity',
+    date: new Date(),
+    userId: userId('someone'),
+    editorialCommunityId: new EditorialCommunityId('316db7d9-88cc-4c26-b386-f067e0f56334'),
+  },
+  {
+    type: 'UserFollowedEditorialCommunity',
+    date: new Date(),
+    userId: userId('someone'),
+    editorialCommunityId: new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
+  },
+  {
+    type: 'UserFollowedEditorialCommunity',
+    date: new Date(),
+    userId: userId('someone'),
+    editorialCommunityId: new EditorialCommunityId('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de'),
+  },
+];
+
 export default (ports: Ports): RenderPage => {
-  const getFollowedEditorialCommunityIds: GetFollowedEditorialCommunityIds = async () => [
-    new EditorialCommunityId('316db7d9-88cc-4c26-b386-f067e0f56334'),
-    new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
-    new EditorialCommunityId('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de'),
-  ];
+  const getFollowedEditorialCommunityIds: GetFollowedEditorialCommunityIds = async () => (
+    hardcodedEvents.map((event) => event.editorialCommunityId)
+  );
 
   const getEditorialCommunity: GetEditorialCommunity = async (editorialCommunityId) => (
     (await ports.editorialCommunities.lookup(editorialCommunityId)).unsafelyUnwrap()
