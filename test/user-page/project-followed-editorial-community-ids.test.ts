dcff7bb9e7ae1ab108f595e350725e8aedb442ac
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-08-20 11:55:07 +0100

    Test the contents of the list #223

--- test/user-page/project-followed-editorial-community-ids.test.ts
+++ test/user-page/project-followed-editorial-community-ids.test.ts
@@ -1,3 +1,4 @@
+import EditorialCommunityId from '../../src/types/editorial-community-id';
 import userId from '../../src/types/user-id';
 import createProjectFollowedEditorialCommunityIds from '../../src/user-page/project-followed-editorial-community-ids';
 
@@ -5,8 +6,13 @@ describe('project-followed-editorial-community-ids', () => {
   it('returns a list', async () => {
     const projectFollowedEditorialCommunityIds = createProjectFollowedEditorialCommunityIds();
 
-    const list = await projectFollowedEditorialCommunityIds(userId('someone'));
+    const actual = await projectFollowedEditorialCommunityIds(userId('someone'));
+    const expected = [
+      new EditorialCommunityId('316db7d9-88cc-4c26-b386-f067e0f56334'),
+      new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
+      new EditorialCommunityId('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de'),
+    ];
 
-    expect(list).toHaveLength(3);
+    expect(actual).toStrictEqual(expected);
   });
 });
