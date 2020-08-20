b0fd45c26398e6af7452ec399d2829df69bd5bac
Giorgio Sironi <g.sironi@elifesciences.org>
2020-08-20 13:32:01 +0100

    Introduce GetAllEvents port #223

--- src/user-page/index.ts
+++ src/user-page/index.ts
@@ -3,6 +3,8 @@ import createProjectFollowedEditorialCommunityIds from './project-followed-edito
 import createRenderFollowToggle from './render-follow-toggle';
 import createRenderFollowedEditorialCommunity from './render-followed-editorial-community';
 import createRenderPage from './render-page';
+import { DomainEvent } from '../types/domain-events';
+import EditorialCommunityId from '../types/editorial-community-id';
 import EditorialCommunityRepository from '../types/editorial-community-repository';
 import FollowList from '../types/follow-list';
 import userId from '../types/user-id';
@@ -18,6 +20,56 @@ interface Params {
 
 type RenderPage = (params: Params) => Promise<string>;
 
+const hardcodedEvents: Array<DomainEvent> = [
+  {
+    type: 'UserFollowedEditorialCommunity',
+    date: new Date(),
+    userId: userId('someone'),
+    editorialCommunityId: new EditorialCommunityId('316db7d9-88cc-4c26-b386-f067e0f56334'),
+  },
+  {
+    type: 'UserFollowedEditorialCommunity',
+    date: new Date(),
+    userId: userId('someone'),
+    editorialCommunityId: new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
+  },
+  {
+    type: 'UserUnfollowedEditorialCommunity',
+    date: new Date(),
+    userId: userId('someone'),
+    editorialCommunityId: new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
+  },
+  {
+    type: 'UserFollowedEditorialCommunity',
+    date: new Date(),
+    userId: userId('someone'),
+    editorialCommunityId: new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
+  },
+  {
+    type: 'UserFollowedEditorialCommunity',
+    date: new Date(),
+    userId: userId('someone'),
+    editorialCommunityId: new EditorialCommunityId('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de'),
+  },
+  {
+    type: 'EditorialCommunityJoined',
+    date: new Date(),
+    actorId: new EditorialCommunityId('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de'),
+  },
+  {
+    type: 'UserFollowedEditorialCommunity',
+    date: new Date(),
+    userId: userId('someoneelse'),
+    editorialCommunityId: new EditorialCommunityId('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'),
+  },
+  {
+    type: 'UserUnfollowedEditorialCommunity',
+    date: new Date(),
+    userId: userId('someoneelse'),
+    editorialCommunityId: new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
+  },
+];
+
 export default (ports: Ports): RenderPage => {
   const getEditorialCommunity: GetEditorialCommunity = async (editorialCommunityId) => (
     (await ports.editorialCommunities.lookup(editorialCommunityId)).unsafelyUnwrap()
@@ -26,7 +78,7 @@ export default (ports: Ports): RenderPage => {
   const renderFollowToggle = createRenderFollowToggle();
   const renderFollowedEditorialCommunity = createRenderFollowedEditorialCommunity(renderFollowToggle);
   const getHardcodedFollowedEditorialCommunities = createGetHardcodedFollowedEditorialCommunities(
-    createProjectFollowedEditorialCommunityIds(),
+    createProjectFollowedEditorialCommunityIds(async () => hardcodedEvents),
     getEditorialCommunity,
   );
   const renderPage = createRenderPage(getHardcodedFollowedEditorialCommunities, renderFollowedEditorialCommunity);
--- test/user-page/project-followed-editorial-community-ids.test.ts
+++ test/user-page/project-followed-editorial-community-ids.test.ts
@@ -1,10 +1,60 @@
 import EditorialCommunityId from '../../src/types/editorial-community-id';
 import userId from '../../src/types/user-id';
-import createProjectFollowedEditorialCommunityIds from '../../src/user-page/project-followed-editorial-community-ids';
+import createProjectFollowedEditorialCommunityIds, { GetAllEvents } from '../../src/user-page/project-followed-editorial-community-ids';
 
 describe('project-followed-editorial-community-ids', () => {
+  const getAllEvents: GetAllEvents = async () => [
+    {
+      type: 'UserFollowedEditorialCommunity',
+      date: new Date(),
+      userId: userId('someone'),
+      editorialCommunityId: new EditorialCommunityId('316db7d9-88cc-4c26-b386-f067e0f56334'),
+    },
+    {
+      type: 'UserFollowedEditorialCommunity',
+      date: new Date(),
+      userId: userId('someone'),
+      editorialCommunityId: new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
+    },
+    {
+      type: 'UserUnfollowedEditorialCommunity',
+      date: new Date(),
+      userId: userId('someone'),
+      editorialCommunityId: new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
+    },
+    {
+      type: 'UserFollowedEditorialCommunity',
+      date: new Date(),
+      userId: userId('someone'),
+      editorialCommunityId: new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
+    },
+    {
+      type: 'UserFollowedEditorialCommunity',
+      date: new Date(),
+      userId: userId('someone'),
+      editorialCommunityId: new EditorialCommunityId('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de'),
+    },
+    {
+      type: 'EditorialCommunityJoined',
+      date: new Date(),
+      actorId: new EditorialCommunityId('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de'),
+    },
+    {
+      type: 'UserFollowedEditorialCommunity',
+      date: new Date(),
+      userId: userId('someoneelse'),
+      editorialCommunityId: new EditorialCommunityId('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'),
+    },
+    {
+      type: 'UserUnfollowedEditorialCommunity',
+      date: new Date(),
+      userId: userId('someoneelse'),
+      editorialCommunityId: new EditorialCommunityId('53ed5364-a016-11ea-bb37-0242ac130002'),
+    },
+  ];
+
   it('returns a list', async () => {
-    const projectFollowedEditorialCommunityIds = createProjectFollowedEditorialCommunityIds();
+    const projectFollowedEditorialCommunityIds = createProjectFollowedEditorialCommunityIds(getAllEvents);
 
     const actual = await projectFollowedEditorialCommunityIds(userId('someone'));
     const expected = [
