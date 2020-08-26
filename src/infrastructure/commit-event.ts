2e83c657d4a5ad98fef9fac70aa332065a556520
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-08-26 13:35:15 +0100

    Add remaining event fields to the payload #227

--- src/infrastructure/commit-event.ts
+++ src/infrastructure/commit-event.ts
@@ -1,8 +1,21 @@
 import { Pool } from 'pg';
 import { Logger } from './logger';
 import { DomainEvent } from '../types/domain-events';
+import EditorialCommunityId from '../types/editorial-community-id';
 
 type CommitEvent = (event: DomainEvent) => Promise<void>;
+
+const replacer = (key: string, value: unknown): unknown => {
+  if (['date', 'type'].includes(key)) {
+    return undefined;
+  }
+
+  if (value instanceof EditorialCommunityId) {
+    return value.value;
+  }
+
+  return value;
+};
 
 export default (
   events: Array<DomainEvent>,
@@ -14,7 +27,7 @@ export default (
       await pool.query('CREATE TABLE IF NOT EXISTS events (type varchar, date timestamp, payload jsonb);');
       const insertionResult = await pool.query(
         'INSERT INTO events (type, date, payload) VALUES ($1, $2, $3) RETURNING *;',
-        [event.type, event.date.toISOString(), {}],
+        [event.type, event.date.toISOString(), JSON.stringify(event, replacer)],
       );
       logger('debug', 'Insertion result', { result: insertionResult.rows });
     } catch (error) {
