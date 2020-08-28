42d35c147120c883254f7b84dc25792e61f3bc71
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-08-28 09:59:05 +0100

    Make all event types readonly

--- src/types/domain-events.ts
+++ src/types/domain-events.ts
@@ -4,56 +4,56 @@ import { EventId } from './event-id';
 import { ReviewId } from './review-id';
 import { UserId } from './user-id';
 
-export type EditorialCommunityEndorsedArticleEvent = {
+export type EditorialCommunityEndorsedArticleEvent = Readonly<{
   type: 'EditorialCommunityEndorsedArticle';
   date: Date;
   editorialCommunityId: EditorialCommunityId;
   articleId: Doi;
-};
+}>;
 
 export const isEditorialCommunityEndorsedArticleEvent = (event: DomainEvent):
 event is EditorialCommunityEndorsedArticleEvent => (
   event.type === 'EditorialCommunityEndorsedArticle'
 );
 
-export type EditorialCommunityReviewedArticleEvent = {
+export type EditorialCommunityReviewedArticleEvent = Readonly<{
   type: 'EditorialCommunityReviewedArticle';
   date: Date;
   editorialCommunityId: EditorialCommunityId;
   articleId: Doi;
   reviewId: ReviewId;
-};
+}>;
 
 export const isEditorialCommunityReviewedArticleEvent = (event: DomainEvent):
 event is EditorialCommunityReviewedArticleEvent => (
   event.type === 'EditorialCommunityReviewedArticle'
 );
 
-export type EditorialCommunityJoinedEvent = {
+export type EditorialCommunityJoinedEvent = Readonly<{
   type: 'EditorialCommunityJoined';
   date: Date;
   editorialCommunityId: EditorialCommunityId;
-};
+}>;
 
 export const isEditorialCommunityJoinedEvent = (event: DomainEvent): event is EditorialCommunityJoinedEvent => (
   event.type === 'EditorialCommunityJoined'
 );
 
-export type UserFollowedEditorialCommunityEvent = {
+export type UserFollowedEditorialCommunityEvent = Readonly<{
   id: EventId,
   type: 'UserFollowedEditorialCommunity';
   date: Date;
   userId: UserId;
   editorialCommunityId: EditorialCommunityId;
-};
+}>;
 
-export type UserUnfollowedEditorialCommunityEvent = {
+export type UserUnfollowedEditorialCommunityEvent = Readonly<{
   id: EventId,
   type: 'UserUnfollowedEditorialCommunity';
   date: Date;
   userId: UserId;
   editorialCommunityId: EditorialCommunityId;
-};
+}>;
 
 export type DomainEvent =
   EditorialCommunityEndorsedArticleEvent |
