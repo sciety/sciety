f5565671a5798f8d44b3cf12dbaf55f00e7b8928
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-09-01 15:02:13 +0100

    Add stub tests for get-twitter-user-details #229

---
+++ test/infrastructure/get-twitter-user-details.test.ts
@@ -0,0 +1,7 @@
+describe('get-twitter-user-details', () => {
+  it.todo('returns the avatar URL for the user');
+
+  it.todo('uses a default avatar URL if the Twitter user does not exist');
+
+  it.todo('uses a default avatar URL if the Twitter API is unavailable');
+});
