39235ab277110ac129cf7df675825ba3f60a933a
Giorgio Sironi <g.sironi@elifesciences.org>
2020-09-03 15:24:34 +0100

    Hardcode a single follower in the eLife editorial community page #228

--- src/editorial-community-page/render-followers.ts
+++ src/editorial-community-page/render-followers.ts
@@ -3,12 +3,28 @@ import EditorialCommunityId from '../types/editorial-community-id';
 type RenderFollowers = (editorialCommunityId: EditorialCommunityId) => Promise<string>;
 
 export default (): RenderFollowers => (
-  async () => `
-    <section class="ui very padded vertical segment">
-      <h2 class="ui header">
-        Followers
-      </h2>
-      <p>No followers yet.</p>
-    </section>
-  `
-);
+  async (editorialCommunityId) => {
+    let contents = '<p>No followers yet.</p>';
+    if (editorialCommunityId.value === 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0') {
+      contents = `
+        <ul class="ui list">
+          <li class="item">
+            <img class="ui avatar image" src="https://pbs.twimg.com/profile_images/622704117635543040/DQRaHUah_normal.jpg">
+            <div class="content">
+              <a class="header" href="/users/47998559">Giorgio Sironi 🇮🇹🇬🇧🇪🇺</a>
+              @giorgiosironi
+            </div>
+          </li>
+        </ul>
+      `;
+    }
+
+    return `
+      <section class="ui very padded vertical segment">
+        <h2 class="ui header">
+          Followers
+        </h2>
+        ${contents}
+      </section>
+    `;
+  });
--- test/editorial-community-page/render-followers.test.ts
+++ test/editorial-community-page/render-followers.test.ts
@@ -2,11 +2,23 @@ import createRenderFollowers from '../../src/editorial-community-page/render-fol
 import EditorialCommunityId from '../../src/types/editorial-community-id';
 
 describe('render-followers', () => {
-  it('renders a message saying there are no followers', async () => {
-    const renderFollowers = createRenderFollowers();
+  describe('for eLife', () => {
+    it('hardcode a single follower linked to their user page', async () => {
+      const renderFollowers = createRenderFollowers();
 
-    const rendered = await renderFollowers(new EditorialCommunityId('some-id'));
+      const rendered = await renderFollowers(new EditorialCommunityId('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'));
 
-    expect(rendered).toContain('No followers yet');
+      expect(rendered).toContain('/users/47998559');
+    });
+  });
+
+  describe('for other communities', () => {
+    it('renders a message saying there are no followers', async () => {
+      const renderFollowers = createRenderFollowers();
+
+      const rendered = await renderFollowers(new EditorialCommunityId('some-id'));
+
+      expect(rendered).toContain('No followers yet');
+    });
   });
 });
