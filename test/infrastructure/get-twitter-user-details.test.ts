a12c58140059a3cf6ee6970edc25553a5d0a2e72
David Moulton <d.moulton@hive.review>
2020-09-03 10:26:38 +0100

    Implement test for Twitter avatar URL #229

--- test/infrastructure/get-twitter-user-details.test.ts
+++ test/infrastructure/get-twitter-user-details.test.ts
@@ -1,5 +1,22 @@
+import { GetTwitterResponse } from '../../src/infrastructure/get-twitter-response';
+import createGetTwitterUserDetails from '../../src/infrastructure/get-twitter-user-details';
+import userId from '../../src/types/user-id';
+import dummyLogger from '../dummy-logger';
+
 describe('get-twitter-user-details', () => {
-  it.todo('returns the avatar URL for the user');
+  it('returns the avatar URL for the user', async () => {
+    const avatarUrl = 'http://example.com';
+    const getTwitterResponse: GetTwitterResponse = async () => ({
+      data: {
+        profile_image_url: avatarUrl,
+      },
+    });
+    const getTwitterUserDetails = createGetTwitterUserDetails(getTwitterResponse, dummyLogger);
+    const result = await getTwitterUserDetails(userId('12345'));
+    const userDetails = result.unsafelyUnwrap();
+
+    expect(userDetails.avatarUrl).toBe(avatarUrl);
+  });
 
   it.todo('returns an error if the Twitter user does not exist');
 
