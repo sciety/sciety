89a0086bf469a6bd14aa913c1fa99790054fbba3
Kevin Rutherford <kevin@rutherford-software.com>
2020-09-07 09:57:02 +0100

    Add a test for filtering by editorial community #228

--- test/editorial-community-page/project-follower-ids.test.ts
+++ test/editorial-community-page/project-follower-ids.test.ts
@@ -5,7 +5,7 @@ import { generate } from '../../src/types/event-id';
 import toUserId from '../../src/types/user-id';
 
 describe('project-follower-ids', () => {
-  it('projects a list of follower ids based on follow events for a given community', async () => {
+  it('projects a list of follower ids based on follow events', async () => {
     const events: ReadonlyArray<UserFollowedEditorialCommunityEvent> = [
       {
         id: generate(),
@@ -22,13 +22,27 @@ describe('project-follower-ids', () => {
         editorialCommunityId: new EditorialCommunityId('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'),
       },
     ];
-
     const getAllEvents: GetAllEvents = async () => events;
-
     const projectFollowerIds = createProjectFollowerIds(getAllEvents);
-
     const followerIds = await projectFollowerIds(new EditorialCommunityId('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'));
 
     expect(followerIds).toHaveLength(2);
+  });
+
+  it('ignores events from other communities', async () => {
+    const events: ReadonlyArray<UserFollowedEditorialCommunityEvent> = [
+      {
+        id: generate(),
+        type: 'UserFollowedEditorialCommunity',
+        date: new Date(),
+        userId: toUserId('23776533'),
+        editorialCommunityId: new EditorialCommunityId('something'),
+      },
+    ];
+    const getAllEvents: GetAllEvents = async () => events;
+    const projectFollowerIds = createProjectFollowerIds(getAllEvents);
+    const followerIds = await projectFollowerIds(new EditorialCommunityId('other'));
+
+    expect(followerIds).toHaveLength(0);
   });
 });
