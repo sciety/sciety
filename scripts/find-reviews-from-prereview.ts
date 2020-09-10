c8e443567ee70e318aaf9ac84c611bb4eb4eef73
Kevin Rutherford <kevin@rutherford-software.com>
2020-09-10 11:07:43 +0100

    Introduce a seam ready to fetch PREreview article details #235

--- scripts/find-reviews-from-prereview.ts
+++ scripts/find-reviews-from-prereview.ts
@@ -1,24 +1,36 @@
 import axios from 'axios';
-import { Maybe } from 'true-myth';
 
-type PrereviewRow = {
+type ArticleSummary = {
   id: string,
   n_prereviews: number,
 };
 
-type PrereviewResponse = {
-  results: ReadonlyArray<PrereviewRow>,
+type Review = {
+  date_created: string;
+  doi: string;
 };
 
-const biorxivPrefix = /^10\.1101\//;
+type Article = {
+  prereviews: Array<Review>;
+};
 
-const formatRow = (row: PrereviewRow): Maybe<string> => {
-  const articleDoi = row.id.replace(/^doi\//, '');
-  if (!articleDoi.match(biorxivPrefix)) {
-    return Maybe.nothing();
+type PrereviewResponse = {
+  results: ReadonlyArray<ArticleSummary>,
+};
+
+const biorxivPrefix = /^doi\/10\.1101\//;
+
+const formatRow = (articleId: string, row: Review): string => {
+  const articleDoi = articleId.replace(/^doi\//, '');
+
+  return `${new Date(row.date_created).toISOString()},${articleDoi},doi:${row.doi}`;
+};
+
+const fetchReviews = (article: ArticleSummary): Array<Review> => {
+  if (!article.id.match(biorxivPrefix)) {
+    return [];
   }
-
-  return Maybe.just(`${new Date().toISOString()},${articleDoi},doi:10.5281/zenodo.3678326`);
+  return [];
 };
 
 void (async (): Promise<void> => {
@@ -29,9 +41,13 @@ void (async (): Promise<void> => {
     'https://www.prereview.org/data/preprints/search',
     { headers: { Accept: 'application/json' } },
   );
-  data.results.forEach((row) => {
-    if (row.n_prereviews > 0) {
-      formatRow(row).map((formatted) => process.stdout.write(`${formatted}\n`));
+  data.results.forEach((articleSummary) => {
+    if (articleSummary.n_prereviews > 0) {
+      const reviews = fetchReviews(articleSummary);
+      reviews.forEach((review) => {
+        const formatted = formatRow(articleSummary.id, review);
+        process.stdout.write(`${formatted}\n`);
+      });
     }
   });
 })();
