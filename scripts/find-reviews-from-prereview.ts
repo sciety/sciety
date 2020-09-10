b208f17e683c13331c7f78b7320c6e7dd978ddfd
Giorgio Sironi <g.sironi@elifesciences.org>
2020-09-10 10:02:20 +0100

    Read first article and review from Prereview API #235

--- scripts/find-reviews-from-prereview.ts
+++ scripts/find-reviews-from-prereview.ts
@@ -1,5 +1,20 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 void (async (): Promise<void> => {
   process.stdout.write('Date,Article DOI,Review ID\n');
+
+  type PrereviewResponse = {
+    results: [
+      {
+        id: string,
+      }
+    ]
+  };
+  // -d '{"query":{"string":null,"sortBy":"reviews","page":1}}'
+  const { data } = await axios.post<PrereviewResponse>(
+    'https://www.prereview.org/data/preprints/search',
+    { headers: { Accept: 'application/json' } },
+  );
+  const row = data.results[0];
+  process.stdout.write(`${new Date().toISOString()},${row.id.replace(/^doi\//, '')},doi:10.5281/zenodo.3678326\n`);
 })();
