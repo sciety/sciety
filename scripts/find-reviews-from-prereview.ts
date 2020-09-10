7e3c6a5a973e0879a2d38a8da087df0dbc27dc0b
Kevin Rutherford <kevin@rutherford-software.com>
2020-09-10 10:17:03 +0100

    Fetch the first page of PREreviews #235

--- scripts/find-reviews-from-prereview.ts
+++ scripts/find-reviews-from-prereview.ts
@@ -7,6 +7,7 @@ void (async (): Promise<void> => {
     results: [
       {
         id: string,
+        n_prereviews: number,
       }
     ]
   };
@@ -15,6 +16,9 @@ void (async (): Promise<void> => {
     'https://www.prereview.org/data/preprints/search',
     { headers: { Accept: 'application/json' } },
   );
-  const row = data.results[0];
-  process.stdout.write(`${new Date().toISOString()},${row.id.replace(/^doi\//, '')},doi:10.5281/zenodo.3678326\n`);
+  data.results.forEach((row) => {
+    if (row.n_prereviews > 0) {
+      process.stdout.write(`${new Date().toISOString()},${row.id.replace(/^doi\//, '')},doi:10.5281/zenodo.3678326\n`);
+    }
+  });
 })();
