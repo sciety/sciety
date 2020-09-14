d493517788fb5657520b59ae7f6e20bcf41c8853
Giorgio Sironi <g.sironi@elifesciences.org>
2020-09-14 14:45:20 +0100

    Add empty feed message to editorial community page #231

--- src/editorial-community-page/render-feed.ts
+++ src/editorial-community-page/render-feed.ts
@@ -18,15 +18,19 @@ export default (
 ): RenderFeed => async (editorialCommunityId, userId) => {
   const events = await getEvents(editorialCommunityId);
   const feedItems = await Promise.all(events.map(renderFeedItem));
+  let content = '<p>It looks like this community hasn’t evaluated any articles yet. Try coming back later!</p>';
+  if (feedItems.length > 0) {
+    content = `<ol class="ui large feed" role="list">
+      ${templateListItems(feedItems, 'event')}
+    </ol>`;
+  }
   return `
       <section class="ui very padded vertical segment">
         <h2 class="ui header">
           Feed
         </h2>
         ${await renderFollowToggle(userId, editorialCommunityId)}
-        <ol class="ui large feed" role="list">
-          ${templateListItems(feedItems, 'event')}
-        </ol>
+        ${content}
       </section>
     `;
 };
