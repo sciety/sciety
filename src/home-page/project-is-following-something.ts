fed91fd01d70d60926c27f758dd33b385553f685
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-09-14 15:28:58 +0100

    Simplify implementation of project-is-following-something to store a count #231

--- src/home-page/project-is-following-something.ts
+++ src/home-page/project-is-following-something.ts
@@ -1,19 +1,18 @@
 import { IsFollowingSomething } from './render-feed';
 import { DomainEvent } from '../types/domain-events';
-import EditorialCommunityId from '../types/editorial-community-id';
 
 export type GetAllEvents = () => Promise<ReadonlyArray<DomainEvent>>;
 
 export default (getAllEvents: GetAllEvents): IsFollowingSomething => (
   async (userId) => {
-    const result = new Set<string>();
+    let count = 0;
     (await getAllEvents()).forEach((event) => {
       if (event.type === 'UserFollowedEditorialCommunity' && event.userId === userId) {
-        result.add(event.editorialCommunityId.value);
+        count += 1;
       } else if (event.type === 'UserUnfollowedEditorialCommunity' && event.userId === userId) {
-        result.delete(event.editorialCommunityId.value);
+        count -= 1;
       }
     });
-    return Array.from(result).map((id: string) => new EditorialCommunityId(id)).length > 0;
+    return count > 0;
   }
 );
