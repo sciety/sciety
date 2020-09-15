2c04d33bbba74f7a6180c719dd0bb1a50171ba08
Giorgio Sironi <g.sironi@elifesciences.org>
2020-09-15 10:30:13 +0100

    Turn Disqus 500 into warning logs rather than errors

--- src/infrastructure/get-disqus-post-count.ts
+++ src/infrastructure/get-disqus-post-count.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from 'axios';
 import { Maybe } from 'true-myth';
 import { Logger } from './logger';
 import { Json, JsonCompatible } from '../types/json';
@@ -12,6 +13,9 @@ type DisqusData = JsonCompatible<{
 
 type GetDisqusPostCount = (uri: string) => Promise<Maybe<number>>;
 
+const isAxiosError = (error: unknown): error is AxiosError => typeof error === 'object'
+    && error?.hasOwnProperty('isAxiosError') === true;
+
 export default (getJson: GetJson, logger: Logger): GetDisqusPostCount => (
   async (uri) => {
     const disqusApiKey = process.env.DISQUS_API_KEY ?? '';
@@ -22,14 +26,21 @@ export default (getJson: GetJson, logger: Logger): GetDisqusPostCount => (
       logger('debug', 'Received response from Disqus', { response: disqusData });
       return Maybe.just(disqusData.response[0].posts);
     } catch (error) {
-      const payload = { error };
+      const payload: { error: unknown, status?: number } = { error };
+      if (isAxiosError(error)) {
+        payload.status = error.response?.status;
+      }
       if (error instanceof Error) {
         payload.error = {
           message: error.message,
           stack: error.stack,
         };
       }
-      logger('error', 'Disqus API error', payload);
+      if (payload.status && payload.status >= 500) {
+        logger('warn', 'Disqus API server error', payload);
+      } else {
+        logger('error', 'Disqus generic error', payload);
+      }
       return Maybe.nothing();
     }
   }
