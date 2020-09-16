dc90b6412017be188807943e25110ef55f18af99
Giorgio Sironi <g.sironi@elifesciences.org>
2020-09-16 15:40:24 +0100

    Extract get-hardcoded-recommendations adapter #242

--- src/article-page/index.ts
+++ src/article-page/index.ts
@@ -1,5 +1,6 @@
 import { Maybe, Result } from 'true-myth';
 import ensureBiorxivDoi from './ensure-biorxiv-doi';
+import createGetHardcodedRecommendations from './get-hardcoded-recommendations';
 import createRenderArticleAbstract, { GetArticleAbstract, RenderArticleAbstract } from './render-article-abstract';
 import createRenderPage, { RenderPageError } from './render-page';
 import createRenderPageHeader, {
@@ -9,7 +10,7 @@ import createRenderPageHeader, {
   GetReviewCount,
   RenderPageHeader,
 } from './render-page-header';
-import createRenderRecommendations, { GetRecommendations } from './render-recommendations';
+import createRenderRecommendations from './render-recommendations';
 import createRenderReview, {
   GetEditorialCommunityName as GetEditorialCommunityNameForRenderReview,
   GetReview,
@@ -87,30 +88,7 @@ type RenderPage = (params: Params) => Promise<Result<string, RenderPageError>>;
 export default (ports: Ports): RenderPage => {
   const renderPageHeader = buildRenderPageHeader(ports);
   const renderAbstract = buildRenderAbstract(ports.fetchArticle);
-  const getRecommendations: GetRecommendations = async (doi) => {
-    if (doi.value === '10.1101/2020.06.03.20119925') {
-      return [{
-        content: `
-          Sequencing and analyzing SARS-Cov-2 genomes in nearly real time has the potential
-          to quickly confirm (and inform) our knowledge of, and response to, the current pandemic 
-          [1,2]. In this manuscript [3], Danesh and colleagues use the earliest set of
-          available SARS-Cov-2 genome sequences available from France to make inferences
-          about the timing of the major epidemic wave, the duration of infections,
-           and the efficacy of lockdown measures. Their phylodynamic estimates -- based on
-          fitting genomic data to molecular clock and transmission models -- are reassuringly 
-          close to estimates based on 'traditional' epidemiological methods: the French 
-          epidemic likely began in mid-January or early February 2020, and spread relatively 
-          rapidly (doubling every 3-5 days), with people remaining infectious for a median 
-          of 5 days [4,5]. These transmission parameters are broadly in line with estimates 
-          from China [6,7], but are currently unknown in France (in the absence of contact 
-          tracing data). By estimating the temporal reproductive number (Rt), the authors 
-          detected a slowing down of the epidemic in the most recent period of the study, 
-          after mid-March, supporting the efficacy of lockdown measures. 
-        `,
-      }];
-    }
-    return [];
-  };
+  const getRecommendations = createGetHardcodedRecommendations();
   const renderRecommendations = createRenderRecommendations(getRecommendations);
   const renderReviews = buildRenderReviews(ports);
   const renderPage = createRenderPage(
