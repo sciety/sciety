b2b396657e7499445f6fae3b0b019c9b2d22f0fa
Kevin Rutherford <kevin@rutherford-software.com>
2020-09-16 15:49:28 +0100

    Create get-recommendation-content inline #242

--- src/article-page/get-hardcoded-recommendations.ts
+++ src/article-page/get-hardcoded-recommendations.ts
@@ -1,27 +1,15 @@
+import { URL } from 'url';
 import { GetRecommendations } from './render-recommendations';
 
-export default (): GetRecommendations => (
+export type GetRecommendationContent = (url: URL) => Promise<string>;
+
+export default (
+  getRecommendationContent: GetRecommendationContent,
+): GetRecommendations => (
   async (doi) => {
     if (doi.value === '10.1101/2020.06.03.20119925') {
-      // getRecommendationContent('https://doi.org/10.24072/pci.evolbiol.100107')
       return [{
-        content: `
-          Sequencing and analyzing SARS-Cov-2 genomes in nearly real time has the potential
-          to quickly confirm (and inform) our knowledge of, and response to, the current pandemic 
-          [1,2]. In this manuscript [3], Danesh and colleagues use the earliest set of
-          available SARS-Cov-2 genome sequences available from France to make inferences
-          about the timing of the major epidemic wave, the duration of infections,
-           and the efficacy of lockdown measures. Their phylodynamic estimates -- based on
-          fitting genomic data to molecular clock and transmission models -- are reassuringly 
-          close to estimates based on 'traditional' epidemiological methods: the French 
-          epidemic likely began in mid-January or early February 2020, and spread relatively 
-          rapidly (doubling every 3-5 days), with people remaining infectious for a median 
-          of 5 days [4,5]. These transmission parameters are broadly in line with estimates 
-          from China [6,7], but are currently unknown in France (in the absence of contact 
-          tracing data). By estimating the temporal reproductive number (Rt), the authors 
-          detected a slowing down of the epidemic in the most recent period of the study, 
-          after mid-March, supporting the efficacy of lockdown measures. 
-        `,
+        content: await getRecommendationContent(new URL('https://doi.org/10.24072/pci.evolbiol.100107')),
       }];
     }
     return [];
--- src/article-page/index.ts
+++ src/article-page/index.ts
@@ -1,6 +1,6 @@
 import { Maybe, Result } from 'true-myth';
 import ensureBiorxivDoi from './ensure-biorxiv-doi';
-import createGetHardcodedRecommendations from './get-hardcoded-recommendations';
+import createGetHardcodedRecommendations, { GetRecommendationContent } from './get-hardcoded-recommendations';
 import createRenderArticleAbstract, { GetArticleAbstract, RenderArticleAbstract } from './render-article-abstract';
 import createRenderPage, { RenderPageError } from './render-page';
 import createRenderPageHeader, {
@@ -88,7 +88,24 @@ type RenderPage = (params: Params) => Promise<Result<string, RenderPageError>>;
 export default (ports: Ports): RenderPage => {
   const renderPageHeader = buildRenderPageHeader(ports);
   const renderAbstract = buildRenderAbstract(ports.fetchArticle);
-  const getRecommendations = createGetHardcodedRecommendations();
+  const getRecommendationContent: GetRecommendationContent = async () => `
+    Sequencing and analyzing SARS-Cov-2 genomes in nearly real time has the potential
+    to quickly confirm (and inform) our knowledge of, and response to, the current pandemic 
+    [1,2]. In this manuscript [3], Danesh and colleagues use the earliest set of
+    available SARS-Cov-2 genome sequences available from France to make inferences
+    about the timing of the major epidemic wave, the duration of infections,
+     and the efficacy of lockdown measures. Their phylodynamic estimates -- based on
+    fitting genomic data to molecular clock and transmission models -- are reassuringly 
+    close to estimates based on 'traditional' epidemiological methods: the French 
+    epidemic likely began in mid-January or early February 2020, and spread relatively 
+    rapidly (doubling every 3-5 days), with people remaining infectious for a median 
+    of 5 days [4,5]. These transmission parameters are broadly in line with estimates 
+    from China [6,7], but are currently unknown in France (in the absence of contact 
+    tracing data). By estimating the temporal reproductive number (Rt), the authors 
+    detected a slowing down of the epidemic in the most recent period of the study, 
+    after mid-March, supporting the efficacy of lockdown measures. 
+  `;
+  const getRecommendations = createGetHardcodedRecommendations(getRecommendationContent);
   const renderRecommendations = createRenderRecommendations(getRecommendations);
   const renderReviews = buildRenderReviews(ports);
   const renderPage = createRenderPage(
