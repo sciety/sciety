23852832b8f2fd703fb21a8385aa1ee3f8b81140
Giorgio Sironi <g.sironi@elifesciences.org>
2020-09-21 15:19:08 +0100

    Fix markup and style feed on article page #245

--- src/article-page/render-flavoured-page.ts
+++ src/article-page/render-flavoured-page.ts
@@ -6,143 +6,90 @@ const feed = `
   <section>
     <h2>Feed</h2>
 
-    <ol class="ui large feed" role="list">
-      <li class="event">
-    <div class="label">
-      <img src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
-    </div>
+    <ol role="list">
+      <li class="article-feed-item">
+      <img class="article-feed-item__avatar" src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
     <div class="content">
-      <div class="date">
-        <time datetime="2020-06-30">Jun 30, 2020</time>
-      </div>
-      <div class="summary">
-        <a href="https://elifesciences.org/articles/58925">Online version updated</a> by <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">eLife</a>
-    </div>
+        <time class="article-feed-item__date" datetime="2020-06-30">Jun 30, 2020</time>
+        <p class="article-feed-item__title"><a href="https://elifesciences.org/articles/58925">Online version updated</a> by <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">eLife</a></p>
     </div>
   </li>
-      <li class="event">
-    <div class="label">
-      <img src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
-    </div>
-    <div class="content">
-      <div class="date">
-        <time datetime="2020-06-11">Jun 11, 2020</time>
-      </div>
-      <div class="summary">
-        <a href="https://elifesciences.org/articles/58925v2">Full online version published</a> by <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">eLife</a>
-    </div>
+
+      <li class="article-feed-item">
+      <img class="article-feed-item__avatar" src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
+    <div>
+        <time class="article-feed-item__date" datetime="2020-06-11">Jun 11, 2020</time>
+        <p class="article-feed-item__title"><a href="https://elifesciences.org/articles/58925v2">Full online version published</a> by <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">eLife</a></p>
     </div>
   </li>
-      <li class="event">
-    <div class="label">
-      <img src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
-    </div>
-    <div class="content">
-      <div class="date">
-        <time datetime="2020-05-28">May 28, 2020</time>
+
+      <li class="article-feed-item">
+      <img class="article-feed-item__avatar" src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
+      <div>
+        <time class="article-feed-item__date" datetime="2020-05-28">May 28, 2020</time>
+        <p class="article-feed-item__title"><a href="https://elifesciences.org/articles/58925v1">Accepted manuscript published as PDF</a> by <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">eLife</a></p>
       </div>
-      <div class="summary">
-        <a href="https://elifesciences.org/articles/58925v1">Accepted manuscript published as PDF</a> by <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">eLife</a>
-    </div>
-    </div>
-  </li>
+    </li>
     
-    <li class="event">
-    <div class="label">
-      <img src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
-    </div>
+    <li class="article-feed-item">
+      <img class="article-feed-item__avatar" src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
     <div class="content">
-      <div class="date">
-        <time datetime="2020-05-18">May 18, 2020</time>
-      </div>
-      <div class="summary">
-        <a href="https://elifesciences.org/articles/58925#sa1">Accepted</a> by <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">eLife</a>
+        <time class="article-feed-item__date" datetime="2020-05-18">May 18, 2020</time>
+        <p class="article-feed-item__title"><a href="https://elifesciences.org/articles/58925#sa1">Accepted</a> by <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">eLife</a></p>
         <p>
         In this manuscript the authors develop a computational approach designed to identify robust genetic interactions that can be used to predict tumor cell genetic vulnerabilities. The authors find that oncogene addiction, as opposed to synthetic lethality, tends to be a more robust predictor of genetic dependencies in tumor cells. They also find that robust genetic interactions in cancer are enriched for gene pairs whose protein products physically interact. Therefore, the latter could be considered a surrogate in target selection for tumors with currently undruggable driver oncogenes.
         </p>
     </div>
-    </div>
   </li>
     
-    <li class="event">
-    <div class="label">
-      <img src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
-    </div>
-    <div class="content">
-      <div class="date">
-        <time datetime="2020-05-15">May 15, 2020</time>
-      </div>
-      <div class="summary">
-        Submitted to <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">eLife</a>
-    </div>
+    <li class="article-feed-item">
+      <img class="article-feed-item__avatar" src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
+    <div>
+        <time class="article-feed-item__date" datetime="2020-05-15">May 15, 2020</time>
+        <p class="article-feed-item__title">Submitted to <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">eLife</a></p>
     </div>
   </li>
   
-  <li class="event">
-    <div class="label">
-      <img src="https://pbs.twimg.com/profile_images/1204012644660854784/E8JhkG7__200x200.jpg" alt="">
-    </div>
-    <div class="content">
-      <div class="date">
-        <time datetime="2020-05-14">May 14, 2020</time>
-      </div>
-      <div class="summary">
-    Author responded to <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">Review Commons</a>
+  <li class="article-feed-item">
+    <img class="article-feed-item__avatar" src="https://pbs.twimg.com/profile_images/1204012644660854784/E8JhkG7__200x200.jpg" alt="">
+    <div>
+        <time class="article-feed-item__date" datetime="2020-05-14">May 14, 2020</time>
+    <p class="article-feed-item__title">Author responded to <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">Review Commons</a></p>
     <h3>Reply to the reviewers</h3>
     <h4>Reviewer #1</h4>
     <p>
     Our response: We thank the reviewer for the positive assessment of our manuscript and have addressed the issue of study bias in response to the specific queries below. ...
     </p>
-    <div class="extra">
-          <a href="https://hyp.is/GFEW8JXMEeqJQcuc-6NFhQ/www.biorxiv.org/content/10.1101/646810v2" class="ui basic secondary button" id="review-0-read-more" aria-labelledby="review-0-read-more review-0-editorial-community">
+          <a href="https://hyp.is/GFEW8JXMEeqJQcuc-6NFhQ/www.biorxiv.org/content/10.1101/646810v2">
             Read the full response
-            <i class="right chevron icon"></i>
           </a>
-    </div>
-    </>
-    </div>
     </div>
   </li>
 
-  <li class="event">
-    <div class="label">
-      <img src="https://pbs.twimg.com/profile_images/1204012644660854784/E8JhkG7__200x200.jpg" alt="">
-    </div>
-    <div class="content">
-      <div class="date">
-        <time datetime="2020-05-14">May 14, 2020</time>
-      </div>
-      <div class="summary">
-    Reviewed by <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">Review Commons</a>
-    <h3>Summary</h3>
-    <p>
-      Reproducibility of genetic interactions across studies is low. The authors identify reproducible genetic interactions and ask the question of what are properties of robust genetic interactions. They find that 1. oncogene addiction tends to be more robust than synthetic lethality and 2. genetic interactions among physically interacting proteins tend to be more robust. They then use protein-protein interactions (PPIs) to guide the detection of genetic interactions involving passenger gene alterations.
-    </p>
-    <h3>Major comments</h3>
-    <p>
-      The claims of the manuscript are clear and well supported by computational analyses. My only concern is the influence of (study) bias on the observed enrichment of physical protein interactions among genetic interactions. 1. Due to higher statistical power the here described approach favors genetic interactions involving frequently altered cancer genes (as acknowledged by the authors). 2. Also some of the libraries in the genetic screens might be biased towards better characterized screens. 3. PPI networks are highly biased towards well studied proteins (in which well studied proteins - in …
-    </p>
-    <div class="extra">
-          <a href="https://hyp.is/F4-xmpXMEeqf3_-2H0r-9Q/www.biorxiv.org/content/10.1101/646810v2" class="ui basic secondary button" id="review-0-read-more" aria-labelledby="review-0-read-more review-0-editorial-community">
-            Read the full review
-            <i class="right chevron icon"></i>
-          </a>
-    </div>
-    </>
-    </div>
+  <li class="article-feed-item">
+    <img class="article-feed-item__avatar" src="https://pbs.twimg.com/profile_images/1204012644660854784/E8JhkG7__200x200.jpg" alt="">
+    <div>
+      <time class="article-feed-item__date" datetime="2020-05-14">May 14, 2020</time>
+      <p class="article-feed-item__title">Reviewed by <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">Review Commons</a></p>
+      <h3>Summary</h3>
+      <p>
+        Reproducibility of genetic interactions across studies is low. The authors identify reproducible genetic interactions and ask the question of what are properties of robust genetic interactions. They find that 1. oncogene addiction tends to be more robust than synthetic lethality and 2. genetic interactions among physically interacting proteins tend to be more robust. They then use protein-protein interactions (PPIs) to guide the detection of genetic interactions involving passenger gene alterations.
+      </p>
+      <h3>Major comments</h3>
+      <p>
+        The claims of the manuscript are clear and well supported by computational analyses. My only concern is the influence of (study) bias on the observed enrichment of physical protein interactions among genetic interactions. 1. Due to higher statistical power the here described approach favors genetic interactions involving frequently altered cancer genes (as acknowledged by the authors). 2. Also some of the libraries in the genetic screens might be biased towards better characterized screens. 3. PPI networks are highly biased towards well studied proteins (in which well studied proteins - in …
+      </p>
+      <a href="https://hyp.is/F4-xmpXMEeqf3_-2H0r-9Q/www.biorxiv.org/content/10.1101/646810v2">
+        Read the full review
+      </a>
     </div>
   </li>
 
-  <li class="event">
-    <div class="label">
-      <img src="https://pbs.twimg.com/profile_images/1204012644660854784/E8JhkG7__200x200.jpg" alt="">
-    </div>
+  <li class="article-feed-item">
+    <img class="article-feed-item__avatar" src="https://pbs.twimg.com/profile_images/1204012644660854784/E8JhkG7__200x200.jpg" alt="">
     <div class="content">
-      <div class="date">
-        <time datetime="2020-05-14">May 14, 2020</time>
-      </div>
-      <div class="summary">
-    Reviewed by <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">Review Commons</a>
+        <time class="article-feed-item__date" datetime="2020-05-14">May 14, 2020</time>
+    <p class="article-feed-item__title">Reviewed by <a href="/editorial-communities/316db7d9-88cc-4c26-b386-f067e0f56334">Review Commons</a><p>
     <h3>Summary</h3>
     <p>
     In this manuscript, Lord et al. describe the analysis of loss-of-function (LOF) screens in cancer cell lines to identify robust (i.e., technically reproducible and shared across cell lines) genetic dependencies. The authors integrate data from 4 large-scale LOF studies (DRIVE, AVANA, DEPMAP and SCORE) to estimate the reproducibility of their individual findings and examine their agreement with other types of functional information, such as physical binding. The main conclusions from the analyses are that: a) oncogene-driven cancer cell lines are more sensitive to the inhibition of the oncogene itself than any other gene in the genome; b) robust genetic interactions (i.e., those observed in multiple datasets and cell lines driven by the same oncogene/tumour suppressor) are enriched for gene pairs encoding physically interacting proteins.
@@ -153,41 +100,25 @@ const feed = `
 
     -Could the authors …
     </p>
-    <div class="extra">
-          <a href="https://hyp.is/F7e5QpXMEeqnbCM3UE6XLQ/www.biorxiv.org/content/10.1101/646810v2" class="ui basic secondary button" id="review-0-read-more" aria-labelledby="review-0-read-more review-0-editorial-community">
+          <a href="https://hyp.is/F7e5QpXMEeqnbCM3UE6XLQ/www.biorxiv.org/content/10.1101/646810v2">
             Read the full review
-            <i class="right chevron icon"></i>
           </a>
-    </div>
-    </div>
     </div>
   </li>
 
-  <li class="event">
-    <div class="label">
-      <img src="https://pbs.twimg.com/profile_images/956882186996662272/lwyH1HFe_200x200.jpg" alt="">
-    </div>
+  <li class="article-feed-item">
+      <img class="article-feed-item__avatar" src="https://pbs.twimg.com/profile_images/956882186996662272/lwyH1HFe_200x200.jpg" alt="">
     <div class="content">
-      <div class="date">
-        <time datetime="2019-05-24">Jan 11, 2020</time>
-      </div>
-      <div class="summary">
-        <a href="https://www.biorxiv.org/content/10.1101/646810v2?versioned=true">Version 2 published on Biorxiv</a>
-    </div>
+        <time class="article-feed-item__date" datetime="2019-05-24">Jan 11, 2020</time>
+        <p class="article-feed-item__title"><a href="https://www.biorxiv.org/content/10.1101/646810v2?versioned=true">Version 2 published on Biorxiv</a></p>
     </div>
   </li>
   
-  <li class="event">
-    <div class="label">
-      <img src="https://pbs.twimg.com/profile_images/956882186996662272/lwyH1HFe_200x200.jpg" alt="">
-    </div>
+  <li class="article-feed-item">
+      <img class="article-feed-item__avatar" src="https://pbs.twimg.com/profile_images/956882186996662272/lwyH1HFe_200x200.jpg" alt="">
     <div class="content">
-      <div class="date">
-        <time datetime="2019-05-24">May 24, 2019</time>
-      </div>
-      <div class="summary">
-        <a href="https://www.biorxiv.org/content/10.1101/646810v1?versioned=true">Version 1 published on Biorxiv</a>
-    </div>
+        <time class="article-feed-item__date" datetime="2019-05-24">May 24, 2019</time>
+        <p class="article-feed-item__title"><a href="https://www.biorxiv.org/content/10.1101/646810v1?versioned=true">Version 1 published on Biorxiv</a></p>
     </div>
   </li>
 
