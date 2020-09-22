66531d44bfdc3198ee17e9f6434129530abf559b
Giorgio Sironi <g.sironi@elifesciences.org>
2020-09-22 10:03:19 +0100

    Remove semantic UI from reviews section #246

--- src/article-page/render-flavour-b.ts
+++ src/article-page/render-flavour-b.ts
@@ -50,13 +50,10 @@ export default (): string => `
           
           <p>Macrophages in the tumor microenvironment are causally linked with prostate cancer development and progression, yet little is known about their composition in neoplastic human tissue. By performing single cell transcriptomic analysis of human prostate cancer resident macrophages, three distinct populations were identified in the diseased prostate. Unexpectedly, macrophages isolated from the tumor-adjacent site of the prostatectomy specimen were identical to those from the tumorous site. Markers associated with canonical M1 and M2 macrophage phenotypes were identifiable, however these were not the main factors defining unique subtypes. The genes selectively associated with each macrophage cluster were used to develop a gene signature which was highly associated with both recurrence-free and metastasis-free survival. These results highlight the relevance of tissue-specific macrophage subtypes in the tumour microenvironment for prostate cancer progression and demonstrates the utility of profiling single-cell transcriptomics in human tumor samples as a strategy to design gene classifiers for patient prognostication.</p>
         
-          <a href="https://doi.org/10.1101/2020.06.19.160770" class="ui basic secondary button">
+          <a href="https://doi.org/10.1101/2020.06.19.160770">
             Read the full article
-            <i class="right chevron icon"></i>
           </a>
       </section>
-      <div class="ui hidden clearing section divider"></div>
-    
     
       <section id="endorsements">
         
@@ -64,8 +61,8 @@ export default (): string => `
     
     
         <section id="review-summaries">
-          <ol role="list" class="ui very relaxed divided items list"> 
-            <li class="item">  
+          <ol role="list"> 
+            <li>  
               <article class="content">          
                 <img class="article-feed__item__avatar" src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
                 <h2>
@@ -74,7 +71,7 @@ export default (): string => `
                     eLife
                   </a>
                 </h2>
-                <div class="meta" data-test-id="reviewPublicationDate"><time datetime="2020-09-15">Sep 15, 2020</time></div>
+                <div><time datetime="2020-09-15">Sep 15, 2020</time></div>
                 <p>
                   While we all considered the value of the dataset as a useful resource for the community, providing a
                   transcriptional landscape of prostatic monocytic cells, we all agreed that the study remains too
@@ -92,9 +89,9 @@ export default (): string => `
         <h2>
           Reviews
         </h2>
-        <ol role="list" class="ui very relaxed divided items list">
-<li class="item">
-      <article class="content">
+        <ol role="list">
+<li>
+      <article>
         <img class="article-feed__item__avatar" src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
 
         <h3>
@@ -104,24 +101,22 @@ export default (): string => `
           </a>
         </h3>
 
-        <div class="meta" data-test-id="reviewPublicationDate"><time datetime="2020-09-15">Sep 15, 2020</time></div>
+        <div><time datetime="2020-09-15">Sep 15, 2020</time></div>
 
-        <div class="description" data-test-id="reviewSummary"><p>###Reviewer #1:</p>
+        <div><p>###Reviewer #1:</p>
 <p>In this manuscript Siefert et al., profile human prostate cancer-associated macrophage subtypes by single-cell RNA-seq. This analysis identified three major sub-population of macrophage (cluster-0, 1, and 2) in human prostate cancer and adjacent normal tissue. Next, the authors investigate the association of macrophage subtypes with recurrence and metastasis in independent prostate cancer cohorts. This leads to the identification of CSF1R+ (cluster-0) macrophage as a cell type associated with early recurrence and metastasis in prostate cancer. Overall this is an interesting study, however, in the absence of specific presence and/or enrichment of cluster-0 in tumor tissue it is not clear why these macrophages lead to early relapse or metastasis in prostate cancer. Moreover, the absence of any validation and/or functional analysis further diminish the broader implication of this observation.</p>
 <p>1) Overall, the authors have employed very good QC parameters to filter superior quality cells. However, they detected batch effects in data (patient-specific clustering) and therefore employed batch correction methods. Unfortunately, after batch correction, they fail to detect tumor-specific macrophage populations in prostate cancer. The authors' reason that this could be due to the broader effect of 'tumor' on the adjacent normal ecosystem. However, in the absence of a comparison between macrophages from normal prostate and prostate tumor, it's difficult …</div>
 
-        <div class="extra">
-          <a href="https://hyp.is/MGYjtPdTEeqFAGMfJZKfBQ/www.biorxiv.org/content/10.1101/2020.06.19.160770v1" class="ui basic secondary button" id="review-1-read-more"
-            aria-labelledby="review-1-read-more review-1-editorial-community">
+        <div>
+          <a href="https://hyp.is/MGYjtPdTEeqFAGMfJZKfBQ/www.biorxiv.org/content/10.1101/2020.06.19.160770v1">
             Read the full review
-            <i class="right chevron icon"></i>
           </a>
         </div>
 
       </article>
     </li>
-<li class="item">
-      <article class="content">
+<li>
+      <article>
         <img class="article-feed__item__avatar" src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
 
         <h3>
@@ -131,26 +126,24 @@ export default (): string => `
           </a>
         </h3>
 
-        <div class="meta" data-test-id="reviewPublicationDate"><time datetime="2020-09-15">Sep 15, 2020</time></div>
+        <div><time datetime="2020-09-15">Sep 15, 2020</time></div>
 
-        <div class="description" data-test-id="reviewSummary"><p>###Reviewer #2:</p>
+        <div><p>###Reviewer #2:</p>
 <p>The manuscript is a single-cell RNA-seq approach to macrophages (CD3- and CD14/CD11b+) from prostatic adenocarcinoma tissue as well as adjacent non-tumorous prostate tissue. The authors find that three RNA-seq-defined macrophage subset clusters were found in both tumour and adjacent prostate in varying proportions in their patient series. These clusters show only weak associations of expression of genes related to the 'M1' and 'M2' macrophage activation status. They also show no differential association of expression of genes involved in T cell response regulation. One cluster appears to show evidence of NF-kappaB and WNT signalling but little interferon signalling, while another shows strong interferon signalling but poor WNT signalling, and the third cluster ('cluster 1') appears likely to consist of cells in cycle. These are intriguing populations for further work.</p>
 <p>The authors then derive a differentially expressed gene signature, and show that it correlates with clinically relevant parameters in publicly available data sets. These correlations are very interesting from a translational perspective.</p>
 <p>The data are substantive, and provide a valuable resource database for the transcriptional landscape of prostatic monocytic cells. However, the findings remain primarily empirical correlations at this stage, with very limited mechanistic implications.</p>
 <p>1) The patient numbers analysed are very small. There are only four clinical …</div>
 
-        <div class="extra">
-          <a href="https://hyp.is/Sxjt1vdTEeqrk589L9I9zg/www.biorxiv.org/content/10.1101/2020.06.19.160770v1" class="ui basic secondary button" id="review-2-read-more"
-            aria-labelledby="review-2-read-more review-2-editorial-community">
+        <div>
+          <a href="https://hyp.is/Sxjt1vdTEeqrk589L9I9zg/www.biorxiv.org/content/10.1101/2020.06.19.160770v1">
             Read the full review
-            <i class="right chevron icon"></i>
           </a>
         </div>
 
       </article>
     </li>
-<li class="item">
-      <article class="content">
+<li>
+      <article>
         <img class="article-feed__item__avatar" src="https://pbs.twimg.com/profile_images/1239550325188710402/7_lY-IyL_200x200.png" alt="">
 
         <h3>
@@ -160,19 +153,17 @@ export default (): string => `
           </a>
         </h3>
 
-        <div class="meta" data-test-id="reviewPublicationDate"><time datetime="2020-09-15">Sep 15, 2020</time></div>
+        <div><time datetime="2020-09-15">Sep 15, 2020</time></div>
 
-        <div class="description" data-test-id="reviewSummary"><p>###Reviewer #3:</p>
+        <div><p>###Reviewer #3:</p>
 <p>The work by the group of Andries Bergman investigates the heterogeneity of macrophages in prostate cancer. They identified three macrophage subsets in tumorigenic tissue, which were also present in adjacent areas. All three subpopulations were clearly distinct on the molecular level, however, none of these subsets had a clear M1 or M2 phenotype. Accordingly a gene signature could be extracted that correlates with metastasis-free survival of patients and might have prognostic value.</p>
 <p>Even though the manuscript is interesting, well written and the finding that no clear difference in macrophage composition is evident between adjacent and tumorigenic areas is surprising and new, the paper is not sufficient in its current form to fully support the presented messages.</p>
 <p>Main points:</p>
 <p>1) The authors state that they identified three distinct populations of tissue resident macrophages in prostate tissue, independent of the localisation. This finding is surprising, since an accumulation of monocyte-derived tumor(-associated) macrophages can be observed in almost all tumors. According to the material and methods section, the authors did not digest their tissue. What is the impact of digestion vs. non-digestion on macrophage recovery from human prostate tissue? Is it possible that especially tissue-resident macrophage subsets embedded in the parenchyma were missed? A detailed flow cytometry experiment needs to be performed in order to identify …</div>
 
-        <div class="extra">
-          <a href="https://hyp.is/e23E8vdTEeqrE0uwZQ5ZmQ/www.biorxiv.org/content/10.1101/2020.06.19.160770v1" class="ui basic secondary button" id="review-3-read-more"
-            aria-labelledby="review-3-read-more review-3-editorial-community">
+        <div>
+          <a href="https://hyp.is/e23E8vdTEeqrE0uwZQ5ZmQ/www.biorxiv.org/content/10.1101/2020.06.19.160770v1">
             Read the full review
-            <i class="right chevron icon"></i>
           </a>
         </div>
 
