d948712217b8ac7135253ca7da4486872963a369
Giorgio Sironi <g.sironi@elifesciences.org>
2020-09-22 10:08:46 +0100

    Remove unused markup #246

--- src/article-page/render-flavour-b.ts
+++ src/article-page/render-flavour-b.ts
@@ -30,10 +30,7 @@ export default (): string => `
 
         
       <a href="#reviews">
-        <div>
-          Reviews
-          <span>4</span>
-        </div>
+        4 Reviews
       </a>
     
         
@@ -55,11 +52,6 @@ export default (): string => `
           </a>
       </section>
     
-      <section id="endorsements">
-        
-      </section>
-    
-    
         <section id="review-summaries">
           <ol role="list"> 
             <li>  
@@ -71,7 +63,7 @@ export default (): string => `
                     eLife
                   </a>
                 </h2>
-                <div><time datetime="2020-09-15">Sep 15, 2020</time></div>
+                <time datetime="2020-09-15">Sep 15, 2020</time>
                 <p>
                   While we all considered the value of the dataset as a useful resource for the community, providing a
                   transcriptional landscape of prostatic monocytic cells, we all agreed that the study remains too
@@ -101,17 +93,16 @@ export default (): string => `
           </a>
         </h3>
 
-        <div><time datetime="2020-09-15">Sep 15, 2020</time></div>
+        <time datetime="2020-09-15">Sep 15, 2020</time>
 
         <div><p>###Reviewer #1:</p>
 <p>In this manuscript Siefert et al., profile human prostate cancer-associated macrophage subtypes by single-cell RNA-seq. This analysis identified three major sub-population of macrophage (cluster-0, 1, and 2) in human prostate cancer and adjacent normal tissue. Next, the authors investigate the association of macrophage subtypes with recurrence and metastasis in independent prostate cancer cohorts. This leads to the identification of CSF1R+ (cluster-0) macrophage as a cell type associated with early recurrence and metastasis in prostate cancer. Overall this is an interesting study, however, in the absence of specific presence and/or enrichment of cluster-0 in tumor tissue it is not clear why these macrophages lead to early relapse or metastasis in prostate cancer. Moreover, the absence of any validation and/or functional analysis further diminish the broader implication of this observation.</p>
-<p>1) Overall, the authors have employed very good QC parameters to filter superior quality cells. However, they detected batch effects in data (patient-specific clustering) and therefore employed batch correction methods. Unfortunately, after batch correction, they fail to detect tumor-specific macrophage populations in prostate cancer. The authors' reason that this could be due to the broader effect of 'tumor' on the adjacent normal ecosystem. However, in the absence of a comparison between macrophages from normal prostate and prostate tumor, it's difficult …</div>
+<p>1) Overall, the authors have employed very good QC parameters to filter superior quality cells. However, they detected batch effects in data (patient-specific clustering) and therefore employed batch correction methods. Unfortunately, after batch correction, they fail to detect tumor-specific macrophage populations in prostate cancer. The authors' reason that this could be due to the broader effect of 'tumor' on the adjacent normal ecosystem. However, in the absence of a comparison between macrophages from normal prostate and prostate tumor, it's difficult …
+</div>
 
-        <div>
           <a href="https://hyp.is/MGYjtPdTEeqFAGMfJZKfBQ/www.biorxiv.org/content/10.1101/2020.06.19.160770v1">
             Read the full review
           </a>
-        </div>
 
       </article>
     </li>
@@ -126,19 +117,18 @@ export default (): string => `
           </a>
         </h3>
 
-        <div><time datetime="2020-09-15">Sep 15, 2020</time></div>
+        <time datetime="2020-09-15">Sep 15, 2020</time>
 
         <div><p>###Reviewer #2:</p>
 <p>The manuscript is a single-cell RNA-seq approach to macrophages (CD3- and CD14/CD11b+) from prostatic adenocarcinoma tissue as well as adjacent non-tumorous prostate tissue. The authors find that three RNA-seq-defined macrophage subset clusters were found in both tumour and adjacent prostate in varying proportions in their patient series. These clusters show only weak associations of expression of genes related to the 'M1' and 'M2' macrophage activation status. They also show no differential association of expression of genes involved in T cell response regulation. One cluster appears to show evidence of NF-kappaB and WNT signalling but little interferon signalling, while another shows strong interferon signalling but poor WNT signalling, and the third cluster ('cluster 1') appears likely to consist of cells in cycle. These are intriguing populations for further work.</p>
 <p>The authors then derive a differentially expressed gene signature, and show that it correlates with clinically relevant parameters in publicly available data sets. These correlations are very interesting from a translational perspective.</p>
 <p>The data are substantive, and provide a valuable resource database for the transcriptional landscape of prostatic monocytic cells. However, the findings remain primarily empirical correlations at this stage, with very limited mechanistic implications.</p>
-<p>1) The patient numbers analysed are very small. There are only four clinical …</div>
+<p>1) The patient numbers analysed are very small. There are only four clinical …
+</div>
 
-        <div>
           <a href="https://hyp.is/Sxjt1vdTEeqrk589L9I9zg/www.biorxiv.org/content/10.1101/2020.06.19.160770v1">
             Read the full review
           </a>
-        </div>
 
       </article>
     </li>
@@ -153,19 +143,18 @@ export default (): string => `
           </a>
         </h3>
 
-        <div><time datetime="2020-09-15">Sep 15, 2020</time></div>
+        <time datetime="2020-09-15">Sep 15, 2020</time>
 
         <div><p>###Reviewer #3:</p>
 <p>The work by the group of Andries Bergman investigates the heterogeneity of macrophages in prostate cancer. They identified three macrophage subsets in tumorigenic tissue, which were also present in adjacent areas. All three subpopulations were clearly distinct on the molecular level, however, none of these subsets had a clear M1 or M2 phenotype. Accordingly a gene signature could be extracted that correlates with metastasis-free survival of patients and might have prognostic value.</p>
 <p>Even though the manuscript is interesting, well written and the finding that no clear difference in macrophage composition is evident between adjacent and tumorigenic areas is surprising and new, the paper is not sufficient in its current form to fully support the presented messages.</p>
 <p>Main points:</p>
-<p>1) The authors state that they identified three distinct populations of tissue resident macrophages in prostate tissue, independent of the localisation. This finding is surprising, since an accumulation of monocyte-derived tumor(-associated) macrophages can be observed in almost all tumors. According to the material and methods section, the authors did not digest their tissue. What is the impact of digestion vs. non-digestion on macrophage recovery from human prostate tissue? Is it possible that especially tissue-resident macrophage subsets embedded in the parenchyma were missed? A detailed flow cytometry experiment needs to be performed in order to identify …</div>
+<p>1) The authors state that they identified three distinct populations of tissue resident macrophages in prostate tissue, independent of the localisation. This finding is surprising, since an accumulation of monocyte-derived tumor(-associated) macrophages can be observed in almost all tumors. According to the material and methods section, the authors did not digest their tissue. What is the impact of digestion vs. non-digestion on macrophage recovery from human prostate tissue? Is it possible that especially tissue-resident macrophage subsets embedded in the parenchyma were missed? A detailed flow cytometry experiment needs to be performed in order to identify …
+</div>
 
-        <div>
           <a href="https://hyp.is/e23E8vdTEeqrE0uwZQ5ZmQ/www.biorxiv.org/content/10.1101/2020.06.19.160770v1">
             Read the full review
           </a>
-        </div>
 
       </article>
     </li>
