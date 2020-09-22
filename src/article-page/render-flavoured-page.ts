da173ca7d739a966ae440e768e70365edc1e7e2a
David Moulton <d.moulton@hive.review>
2020-09-22 09:48:59 +0100

    Remove Semantic UI from flavour A author list & style #245

--- src/article-page/render-flavoured-page.ts
+++ src/article-page/render-flavoured-page.ts
@@ -133,21 +133,20 @@ const renderFlavourA = (): string => `
 <article class="hive-grid hive-grid--article">
   <div class="main-content">
     
-      <header class="ui basic padded vertical segment">
+      <header>
         <h1>Integrative analysis of large-scale loss-of-function screens identifies robust cancer-associated genetic interactions</h1>
 
-        <ol aria-label="Authors of this article" class="ui comma separated horizontal list" role="list">
-          <li class="item">Christopher J. Lord</li>
-<li class="item">Niall Quinn</li>
-<li class="item">Colm J. Ryan</li>
-
+        <ol aria-label="Authors of this article" role="list" class="article-author-list">
+          <li>Christopher J. Lord</li>
+          <li >Niall Quinn</li>
+          <li>Colm J. Ryan</li>
         </ol>
 
-        <ul aria-label="Publication details" class="ui list">
-          <li class="item">
+        <ul aria-label="Publication details">
+          <li>
             DOI <a href="https://doi.org/10.1101/646810">10.1101/646810</a>
           </li>
-          <li class="item">
+          <li>
             Posted <time datetime="2019-05-24">May 24, 2019</time>
           </li>
         </ul>
@@ -164,12 +163,11 @@ const renderFlavourA = (): string => `
           
           <p>Genetic interactions, such as synthetic lethal effects, can now be systematically identified in cancer cell lines using high-throughput genetic perturbation screens. Despite this advance, few genetic interactions have been reproduced across multiple studies and many appear highly context-specific. Understanding which genetic interactions are robust in the face of the molecular heterogeneity observed in tumours and what factors influence this robustness could streamline the identification of therapeutic targets. Here, we develop a computational approach to identify robust genetic interactions that can be reproduced across independent experiments and across non-overlapping cell line panels. We used this approach to evaluate >140,000 potential genetic interactions involving cancer driver genes and identified 1,520 that are significant in at least one study but only 220 that reproduce across multiple studies. Analysis of these interactions demonstrated that: (i) oncogene addiction effects are more robust than oncogene-related synthetic lethal effects; and (ii) robust genetic interactions in cancer are enriched for gene pairs whose protein products physically interact. This suggests that protein-protein interactions can be used not only to understand the mechanistic basis of genetic interaction effects, but also to prioritise robust targets for further development. To explore the utility of this approach, we used a protein-protein interaction network to guide the search for robust synthetic lethal interactions associated with passenger gene alterations and validated two novel robust synthetic lethalities.</p>
         
-          <a href="https://doi.org/10.1101/646810" class="ui basic secondary button">
+          <a href="https://doi.org/10.1101/646810">
             Read the full article
-            <i class="right chevron icon"></i>
           </a>
       </section>
-      <div class="ui hidden clearing section divider"></div>
+
     
       ${feed}
       
