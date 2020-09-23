5b61c9386a48c853df7fcee46705e6af98b13941
Giorgio Sironi <g.sironi@elifesciences.org>
2020-09-23 09:37:02 +0100

    Add and style <header> elements for reviews #246

--- src/article-page/render-flavour-b.ts
+++ src/article-page/render-flavour-b.ts
@@ -80,9 +80,10 @@ export default (): string => `
                 <ol role="list" class="evaluation-section__review_list">
                   <li>
                     <article>
-                      <time datetime="2020-09-15">Sep 15, 2020</time>
-
-                      <p>###Reviewer #1:</p>
+                      <header class="evaluation-section__review_list__header">
+                        <time datetime="2020-09-15">Sep 15, 2020</time>
+                        <div>Reviewer #1:</div>
+                      </header>
                       <p>In this manuscript Siefert et al., profile human prostate cancer-associated macrophage subtypes by single-cell RNA-seq. This analysis identified three major sub-population of macrophage (cluster-0, 1, and 2) in human prostate cancer and adjacent normal tissue. Next, the authors investigate the association of macrophage subtypes with recurrence and metastasis in independent prostate cancer cohorts. This leads to the identification of CSF1R+ (cluster-0) macrophage as a cell type associated with early recurrence and metastasis in prostate cancer. Overall this is an interesting study, however, in the absence of specific presence and/or enrichment of cluster-0 in tumor tissue it is not clear why these macrophages lead to early relapse or metastasis in prostate cancer. Moreover, the absence of any validation and/or functional analysis further diminish the broader implication of this observation.</p>
                       <p>1) Overall, the authors have employed very good QC parameters to filter superior quality cells. However, they detected batch effects in data (patient-specific clustering) and therefore employed batch correction methods. Unfortunately, after batch correction, they fail to detect tumor-specific macrophage populations in prostate cancer. The authors' reason that this could be due to the broader effect of 'tumor' on the adjacent normal ecosystem. However, in the absence of a comparison between macrophages from normal prostate and prostate tumor, it's difficult to conclude that tumors influence the macrophage in adjacent normal tissue. Given the well established phenomenon of tumor-associated macrophage this observation is surprising and an alternative explanation could be possible artifacts induced during the batch correction (i.e. integration) leading to the removal of subtle differences between tumor vs adjacent normal macrophages.</p>
                       <p>2) This study identifies three major sub-population of macrophages in prostate cancer. Authors discuss the limitation of M1/M2 nomenclature to define macrophage spectrum, which is evident from their analysis as well. However, they also don't provide a marker-based nomenclature of these macrophage clusters. It will be beneficial for the community to know the specific markers of these macrophage sub-populations which will be important for flow-cytometer or imaging-based validation of these populations. It is really important to validate the identity of single-cell RNA-seq clusters by flow or imaging analysis. However, the lack of validation remains one of the major limitations of this study. Not sure given the COVID situation it is possible but it will be very beneficial for the community.</p>
@@ -92,9 +93,11 @@ export default (): string => `
                   </li>
                   <li>
                     <article>
-                      <time datetime="2020-09-15">Sep 15, 2020</time>
+                      <header class="evaluation-section__review_list__header">
+                        <time datetime="2020-09-15">Sep 15, 2020</time>
+                        <div>Reviewer #2:</div>
+                      </header>
 
-                      <p>###Reviewer #2:</p>
                       <p>The manuscript is a single-cell RNA-seq approach to macrophages (CD3- and CD14/CD11b+) from prostatic adenocarcinoma tissue as well as adjacent non-tumorous prostate tissue. The authors find that three RNA-seq-defined macrophage subset clusters were found in both tumour and adjacent prostate in varying proportions in their patient series. These clusters show only weak associations of expression of genes related to the 'M1' and 'M2' macrophage activation status. They also show no differential association of expression of genes involved in T cell response regulation. One cluster appears to show evidence of NF-kappaB and WNT signalling but little interferon signalling, while another shows strong interferon signalling but poor WNT signalling, and the third cluster ('cluster 1') appears likely to consist of cells in cycle. These are intriguing populations for further work.</p>
                       <p>The authors then derive a differentially expressed gene signature, and show that it correlates with clinically relevant parameters in publicly available data sets. These correlations are very interesting from a translational perspective.</p>
                       <p>The data are substantive, and provide a valuable resource database for the transcriptional landscape of prostatic monocytic cells. However, the findings remain primarily empirical correlations at this stage, with very limited mechanistic implications.</p>
@@ -107,9 +110,11 @@ export default (): string => `
                   </li>
                   <li>
                     <article>
-                      <time datetime="2020-09-15">Sep 15, 2020</time>
+                      <header class="evaluation-section__review_list__header">
+                        <time datetime="2020-09-15">Sep 15, 2020</time>
+                        <div>Reviewer #3:</div>
+                      </header>
 
-                      <p>###Reviewer #3:</p>
                       <p>The work by the group of Andries Bergman investigates the heterogeneity of macrophages in prostate cancer. They identified three macrophage subsets in tumorigenic tissue, which were also present in adjacent areas. All three subpopulations were clearly distinct on the molecular level, however, none of these subsets had a clear M1 or M2 phenotype. Accordingly a gene signature could be extracted that correlates with metastasis-free survival of patients and might have prognostic value.</p>
                       <p>Even though the manuscript is interesting, well written and the finding that no clear difference in macrophage composition is evident between adjacent and tumorigenic areas is surprising and new, the paper is not sufficient in its current form to fully support the presented messages.</p>
                       <p>Main points:</p>
