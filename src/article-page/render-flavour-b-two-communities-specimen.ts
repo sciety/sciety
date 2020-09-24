822c1bb94148f7bb4c4edacaec4e7ed25552fe31
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-09-24 15:15:45 +0100

    Add header content #249

--- src/article-page/render-flavour-b-two-communities-specimen.ts
+++ src/article-page/render-flavour-b-two-communities-specimen.ts
@@ -1,3 +1,24 @@
 export default (): string => `
-Flavour B Two communities specimen
+<article class="hive-grid hive-grid--article">
+
+      <header class="article-header">
+        <h1>Integrative analysis of large-scale loss-of-function screens identifies robust cancer-associated genetic interactions</h1>
+
+        <ol aria-label="Authors of this article" role="list" class="article-author-list">
+          <li>Christopher J. Lord</li>
+          <li >Niall Quinn</li>
+          <li>Colm J. Ryan</li>
+        </ol>
+
+        <ul aria-label="Publication details" class="article-meta-data-list" role="list">
+          <li>
+            DOI <a href="https://doi.org/10.1101/646810">10.1101/646810</a>
+          </li>
+          <li>
+            Posted <time datetime="2019-05-24">May 24, 2019</time>
+          </li>
+        </ul>
+
+      </header>
+</article>
 `;
