37ef865f9c978e08097944f811608737f6b535f3
Kevin Rutherford <kevin@rutherford-software.com>
2020-10-02 15:22:48 +0100

    Render the event even when there is no review text #281

--- src/article-page/render-reviewed-event.ts
+++ src/article-page/render-reviewed-event.ts
@@ -21,28 +21,53 @@ const renderAvatar = (url: URL): string => `
 
 export default (
   teaserChars: number,
-): RenderReviewedEvent => (review: Review): string => `
-  <li class="article-feed__item">
-    ${renderAvatar(review.editorialCommunityAvatar)}
-    <div class="article-feed__item_body" data-behaviour="collapse_to_teaser">
-      ${templateDate(review.occurredAt, 'article-feed__item__date')}
-      <div class="article-feed__item__title">
-        Reviewed by
-        <a href="/editorial-communities/${review.editorialCommunityId.value}">
-          ${review.editorialCommunityName}
-        </a>
-      </div>
+): RenderReviewedEvent => (review: Review): string => {
+  if (review.fullText.isNothing()) {
+    return `
+      <li class="article-feed__item">
+        ${renderAvatar(review.editorialCommunityAvatar)}
+        <div class="article-feed__item_body">
+          ${templateDate(review.occurredAt, 'article-feed__item__date')}
+          <div class="article-feed__item__title">
+            Reviewed by
+            <a href="/editorial-communities/${review.editorialCommunityId.value}">
+              ${review.editorialCommunityName}
+            </a>
+          </div>
 
-      <div class="hidden" data-teaser>
-        ${clip(review.fullText.or(Maybe.just('')).unsafelyUnwrap(), teaserChars)}
-      </div>
-      <div data-full-text>
-        ${review.fullText.or(Maybe.just('')).unsafelyUnwrap()}
-        <a href="${review.source.toString()}" class="article-feed__item__read_more article-call-to-action-link">
-          Read the original source
-        </a>
-      </div>
+          <div>
+            <a href="${review.source.toString()}" class="article-feed__item__read_more article-call-to-action-link">
+              Read the original source
+            </a>
+          </div>
 
-    </div>
-  </li>
-`;
+        </div>
+      </li>
+    `;
+  }
+  return `
+    <li class="article-feed__item">
+      ${renderAvatar(review.editorialCommunityAvatar)}
+      <div class="article-feed__item_body" data-behaviour="collapse_to_teaser">
+        ${templateDate(review.occurredAt, 'article-feed__item__date')}
+        <div class="article-feed__item__title">
+          Reviewed by
+          <a href="/editorial-communities/${review.editorialCommunityId.value}">
+            ${review.editorialCommunityName}
+          </a>
+        </div>
+
+        <div class="hidden" data-teaser>
+          ${clip(review.fullText.or(Maybe.just('')).unsafelyUnwrap(), teaserChars)}
+        </div>
+        <div data-full-text>
+          ${review.fullText.or(Maybe.just('')).unsafelyUnwrap()}
+          <a href="${review.source.toString()}" class="article-feed__item__read_more article-call-to-action-link">
+            Read the original source
+          </a>
+        </div>
+
+      </div>
+    </li>
+  `;
+};
--- test/article-page/render-reviewed-event.test.ts
+++ test/article-page/render-reviewed-event.test.ts
@@ -5,7 +5,7 @@ import createRenderReviewedEvent from '../../src/article-page/render-reviewed-ev
 import EditorialCommunityId from '../../src/types/editorial-community-id';
 
 describe('render-reviewed-event', () => {
-  describe('when the review has full text', () => {
+  describe('when the review has long full text', () => {
     it('renders the full text', () => {
       const fullText = 'A very long review';
       const renderReviewedEvent = createRenderReviewedEvent(6);
@@ -20,17 +20,40 @@ describe('render-reviewed-event', () => {
           fullText: Maybe.just(fullText),
         }),
       );
-      const itemBody = rendered.querySelector('[data-behaviour="collapse_to_teaser"]');
+      const toggleableContent = rendered.querySelector('[data-behaviour="collapse_to_teaser"]');
       const fullTextWrapper = rendered.querySelector('[data-full-text]');
       const teaserWrapper = rendered.querySelector('[data-teaser]');
 
-      expect(itemBody).not.toBeNull();
+      expect(toggleableContent).not.toBeNull();
       expect(fullTextWrapper?.textContent).toStrictEqual(expect.stringContaining(fullText));
       expect(teaserWrapper?.textContent).toStrictEqual(expect.stringContaining('A …'));
     });
   });
 
-  describe('when the review has no full text', () => {
+  describe('when the review has short full text', () => {
     it.todo('renders without a teaser');
+  });
+
+  describe('when the review has no full text', () => {
+    it('renders without a teaser', () => {
+      const source = 'http://example.com/source';
+      const renderReviewedEvent = createRenderReviewedEvent(6);
+
+      const rendered = JSDOM.fragment(
+        renderReviewedEvent({
+          source: new URL(source),
+          occurredAt: new Date(),
+          editorialCommunityId: new EditorialCommunityId('community-1'),
+          editorialCommunityName: 'Community 1',
+          editorialCommunityAvatar: new URL('http://example.com/avatar'),
+          fullText: Maybe.nothing(),
+        }),
+      );
+      const toggleableContent = rendered.querySelector('[data-behaviour="collapse_to_teaser"]');
+      const sourceLinkUrl = rendered.querySelector('.article-feed__item__read_more')?.getAttribute('href');
+
+      expect(toggleableContent).toBeNull();
+      expect(sourceLinkUrl).toStrictEqual(source);
+    });
   });
 });
