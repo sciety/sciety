3db06a8f719d6e5fcdc9bf1e3fec079a0d903fe0
Kevin Rutherford <kevin@rutherford-software.com>
2020-10-02 14:42:31 +0100

    Add a happy-path test for render-review-event #261

---
+++ test/article-page/render-reviewed-event.test.ts
@@ -0,0 +1,27 @@
+import { URL } from 'url';
+import { Maybe } from 'true-myth';
+import createRenderReviewedEvent from '../../src/article-page/render-reviewed-event';
+import EditorialCommunityId from '../../src/types/editorial-community-id';
+
+describe('render-reviewed-event', () => {
+  describe('when the review has full text', () => {
+    it('renders the full text', () => {
+      const fullText = 'A very long review';
+      const renderReviewedEvent = createRenderReviewedEvent(6);
+      const rendered = renderReviewedEvent({
+        source: new URL('http://example.com'),
+        occurredAt: new Date(),
+        editorialCommunityId: new EditorialCommunityId('community-1'),
+        editorialCommunityName: 'Community 1',
+        editorialCommunityAvatar: new URL('http://example.com/avatar'),
+        fullText: Maybe.just(fullText),
+      });
+
+      expect(rendered).toStrictEqual(expect.stringContaining(fullText));
+    });
+  });
+
+  describe('when the review has no full text', () => {
+    it.todo('renders without a teaser');
+  });
+});
