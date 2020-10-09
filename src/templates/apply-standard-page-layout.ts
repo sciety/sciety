399829863d74c3558b6dc321969387c4cc420b19
Will Byrne <w.byrne@elifesciences.org>
2020-10-09 15:52:02 +0100

    Add google tag manager #286

--- src/templates/apply-standard-page-layout.ts
+++ src/templates/apply-standard-page-layout.ts
@@ -18,6 +18,25 @@ if (process.env.GOOGLE_ANALYTICS_TRACKING_ID) {
   `;
 }
 
+let googleTagManagerHead = '';
+let googleTagManagerBody = '';
+if (process.env.GOOGLE_TAG_MANAGER) {
+  googleTagManagerHead = `
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NX7CQB4');</script>
+<!-- End Google Tag Manager -->`;
+
+  googleTagManagerBody = `
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NX7CQB4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->`;
+}
+
 const loggedInMenuItems = (user: User): string => `
   <li class="item">
     <a href="/users/${user.id}">My profile</a>
@@ -36,14 +55,18 @@ const loggedOutMenuItems = (): string => `
 
 export default (page: string, user: Maybe<User>): string => `<!doctype html>
 <html lang="en">
-  <meta charset="utf-8">
+<head>
+  <meta charset="utf-8">  
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>
     The Hive
   </title>
+  ${googleTagManagerHead}
   <link rel="stylesheet" href="/static/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent/3.1.1/cookieconsent.min.css">
-
+</head>
+<body>
+  ${googleTagManagerBody}
   <header class="u-full-width">
 
     <nav class="site-header">
@@ -97,5 +120,6 @@ export default (page: string, user: Maybe<User>): string => `<!doctype html>
       }
     });
   </script>
+</body>
 </html>
 `;
