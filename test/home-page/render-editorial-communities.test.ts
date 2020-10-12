af1ed32e9848c35261db47813a6864f6ed4e4ae0
Giorgio Sironi <g.sironi@elifesciences.org>
2020-10-12 13:38:58 +0100

    Update unit test with new type #277

--- test/home-page/render-editorial-communities.test.ts
+++ test/home-page/render-editorial-communities.test.ts
@@ -1,21 +1,24 @@
+import { URL } from 'url';
 import { Maybe } from 'true-myth';
-import createRenderEditorialCommunities from '../../src/home-page/render-editorial-communities';
+import createRenderEditorialCommunities, { GetAllEditorialCommunities } from '../../src/home-page/render-editorial-communities';
 import EditorialCommunityId from '../../src/types/editorial-community-id';
 
 describe('render-editorial-communities', (): void => {
   it('lists all of the hard-coded editorial communities', async (): Promise<void> => {
-    const dummyCommunities = [
+    const dummyGetAllEditorialCommunities: GetAllEditorialCommunities = async () => [
       {
         id: new EditorialCommunityId('A'),
         name: 'Editorial Community A',
+        avatar: new URL('http://example.com'),
       },
       {
         id: new EditorialCommunityId('B'),
         name: 'Editorial Community B',
+        avatar: new URL('http://example.com'),
       },
     ];
     const renderEditorialCommunities = createRenderEditorialCommunities(
-      async () => dummyCommunities,
+      dummyGetAllEditorialCommunities,
       async () => '',
     );
     const rendered = await renderEditorialCommunities(Maybe.nothing());
