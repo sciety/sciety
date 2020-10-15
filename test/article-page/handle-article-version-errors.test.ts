f5fccb07a6a4781ce4e0040aca8b6e5eda78baee
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-10-15 12:10:40 +0100

    Test for null op handle-article-version-error wrapper #279

--- test/article-page/handle-article-version-errors.test.ts
+++ test/article-page/handle-article-version-errors.test.ts
@@ -1,6 +1,25 @@
+import { URL } from 'url';
+import createHandleArticleVersionErrors from '../../src/article-page/handle-article-version-errors';
+import { FeedItem, GetFeedItems } from '../../src/article-page/render-feed';
+import Doi from '../../src/types/doi';
+
 describe('handle-article-version-errors', () => {
   describe('there are article version events', () => {
-    it.todo('remains unchanged');
+    it('remains unchanged', async () => {
+      const inputItems: ReadonlyArray<FeedItem> = [
+        {
+          type: 'article-version',
+          version: 1,
+          occurredAt: new Date(),
+          source: new URL('https://example.com'),
+        },
+      ];
+      const originalGetFeedItems: GetFeedItems = async () => inputItems;
+      const handleArticleVersionErrors = createHandleArticleVersionErrors(originalGetFeedItems);
+      const feedItems = await handleArticleVersionErrors(new Doi('10.1111/123456'));
+
+      expect(feedItems).toStrictEqual(inputItems);
+    });
   });
 
   describe('there are no article version events', () => {
