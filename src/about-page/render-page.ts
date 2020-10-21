66e304feed8e11c7e8a7d5295f925f32cc7e6001
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-10-21 10:51:22 +0100

    Remove Semantic UI segments from the about page #290

--- src/about-page/render-page.ts
+++ src/about-page/render-page.ts
@@ -5,12 +5,10 @@ export type GetHtml = (filename: string) => Promise<string>;
 export default (
   getHtml: GetHtml,
 ): RenderPage => async (filename) => `
-  <header class="ui basic padded vertical segment">
+  <header class="page-header">
     <h1>
       About The Hive
     </h1>
   </header>
-  <section class="ui basic vertical segment">
-    ${await getHtml(filename)}
-  </section>
+  ${await getHtml(filename)}
 `;
