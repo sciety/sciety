f8f704378caf3c6bcd7a983ff5f3bd65cbb03ec8
Kevin Rutherford <kevin@rutherford-software.com>
2020-10-26 09:44:43 +0000

    Add exceptional text to the terms-page #290

--- src/terms-page/index.ts
+++ src/terms-page/index.ts
@@ -8,4 +8,19 @@ export default (): RenderPage => async () => `
       This website is operated by eLife Sciences Publications, Ltd under their terms
       and conditions as set out at <a href="https://elifesciences.org/terms">elifesciences.org/terms</a>.
     </p>
+    <p>
+      This website is operated by eLife Sciences Publications, Ltd under their terms and conditions as
+      set out at elifesciences.org/terms unless otherwise stated through amendments below.
+    </p>
+    <h2>
+      Exceptions to Ownership
+    </h2>
+    <p>
+      Unless otherwise indicated the Editorial Community names, logos and trademarks are owned by
+      their respective owners and not by eLife or its licensors.
+      The article content is created by the authors stated on the article page and is not owned by
+      eLife or its licensors, and is subject to the licence terms shown against it.
+      Evaluation content is created by the Editorial Community named alongside the evaluation content
+      and is not owned by eLife or its licensors, and is subject to the licence terms shown on the Editorial Community's page.
+    </p>
   `;
