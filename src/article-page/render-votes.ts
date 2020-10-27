2a14ac81ccfe4b7c3d1d4e70e14f8a74527dfdce
David Moulton <d.moulton@hive.review>
2020-10-27 11:03:18 +0000

    Add up vote & down vote buttons to renderVotes #303

--- src/article-page/render-votes.ts
+++ src/article-page/render-votes.ts
@@ -9,8 +9,16 @@ export default (getVotes: GetVotes): RenderVotes => (
     const { upVotes, downVotes } = await getVotes(reviewId);
     return `
     <div>
-      ${upVotes} people found this helpful,<br/>
+      ${upVotes} people found this helpful
+      <form method="post" action="/upvote">
+        <input type="hidden" name="reviewid" value="${reviewId.toString()}">
+        <button type="submit" aria-label="Indicate that this review is helpful"><img src="/static/images/thumb-up-outline.svg" alt=""></button>
+      </form>
       ${downVotes} people found this unhelpful
+      <form method="post" action="/downvote">
+        <input type="hidden" name="reviewid" value="${reviewId.toString()}">
+        <button type="submit" aria-label="Indicate that this review is not helpful"><img src="/static/images/thumb-down-outline.svg" alt=""></button>
+      </form>
     </div>
   `;
   }
