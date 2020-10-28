2ac6e328a1b7a81113f8e148a768a9a0687bd690
Giorgio Sironi <g.sironi@elifesciences.org>
2020-10-28 12:01:38 +0000

    Produce UserFoundReviewHelpful event #303

--- src/vote/vote-command.ts
+++ src/vote/vote-command.ts
@@ -1,6 +1,19 @@
+import Doi from '../types/doi';
+import { UserFoundReviewHelpfulEvent } from '../types/domain-events';
+import { generate } from '../types/event-id';
+import toUserId from '../types/user-id';
+
 type VoteCommand = () => Promise<void>;
-export type CommitEvents = (events: []) => void;
+export type CommitEvents = (events: ReadonlyArray<UserFoundReviewHelpfulEvent>) => void;
 
 export default (commitEvents: CommitEvents): VoteCommand => async () => {
-  commitEvents([]);
+  commitEvents([
+    {
+      id: generate(),
+      type: 'UserFoundReviewHelpful',
+      date: new Date(),
+      userId: toUserId('someuser'),
+      reviewId: new Doi('10.1111/12345678'),
+    },
+  ]);
 };
--- test/vote/vote-command.test.ts
+++ test/vote/vote-command.test.ts
@@ -4,12 +4,17 @@ describe('vote-command', () => {
   describe('when no vote has been cast', () => {
     describe('and input contains an upvote', () => {
       // TODO: Test is incomplete
-      it('produces an event containing an upvote', async () => {
+      it('produces a UserFoundReviewHelpfulEvent', async () => {
         const commitEvents = jest.fn();
         const voteCommand = createVoteCommand(commitEvents);
         await voteCommand();
 
         expect(commitEvents).toHaveBeenCalledTimes(1);
+        expect(commitEvents).toHaveBeenCalledWith([
+          expect.objectContaining({
+            type: 'UserFoundReviewHelpful',
+          }),
+        ]);
       });
 
       it.todo('produces an event containing review ID');
