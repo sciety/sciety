b850bd9cc453742de8f47ac3282509bd6a780bb8
Giorgio Sironi <g.sironi@elifesciences.org>
2020-10-29 11:05:42 +0000

    Add test list for project-user-vote #303

---
+++ test/article-page/project-user-vote.test.ts
@@ -0,0 +1,17 @@
+describe('project-user-vote', () => {
+  describe('no vote events', () => {
+    it.todo('returns `not`');
+  });
+
+  describe('one up vote event', () => {
+    it.todo('returns `up`');
+  });
+
+  describe('one up vote event from another user', () => {
+    it.todo('returns `not`');
+  });
+  
+  describe('one up vote event for another review', () => {
+    it.todo('returns `not`');
+  });
+});
