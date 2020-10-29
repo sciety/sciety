f4cb7e89b532e6162d5b25cf614e94c76be19ce1
Giorgio Sironi <g.sironi@elifesciences.org>
2020-10-29 15:40:32 +0000

    Remove log superseded by event #303

--- src/vote/index.ts
+++ src/vote/index.ts
@@ -1,11 +1,9 @@
 import { Middleware } from '@koa/router';
 import createVoteCommand, { CommitEvents, GetAllEvents } from './vote-command';
-import { Logger } from '../infrastructure/logger';
 import toReviewId from '../types/review-id';
 import { User } from '../types/user';
 
 type Ports = {
-  logger: Logger;
   commitEvents: CommitEvents;
   getAllEvents: GetAllEvents;
 };
@@ -15,9 +13,6 @@ export default (ports: Ports): Middleware<{ user: User }> => async (context, nex
   const { user } = context.state;
   const reviewId = toReviewId(context.request.body.reviewid);
   await voteCommand(user, reviewId);
-
-  // TODO: code is in the wrong place
-  ports.logger('info', 'User voted', { user });
 
   context.redirect('back');
 
