a4e88b5192fbce3479d786beac7e51bda14cc4d9
Kevin Rutherford <kevin@rutherford-software.com>
2020-11-02 11:42:33 +0000

    Remove 'vote' from some tests #303

--- test/respond/handle-response-to-review.test.ts
+++ test/respond/handle-response-to-review.test.ts
@@ -3,9 +3,9 @@ import Doi from '../../src/types/doi';
 import toUserId from '../../src/types/user-id';
 
 describe('handle-response-to-review', () => {
-  describe('when no vote has been cast', () => {
-    describe('and input contains an upvote', () => {
-      it('produces a UserFoundReviewHelpfulEvent', async () => {
+  describe('when no response has been given', () => {
+    describe('when the user says it is helpful', () => {
+      it('commits the event', async () => {
         const commitEvents = jest.fn();
         const handleResponseToReview = createHandleResponseToReview(async () => [], commitEvents);
         await handleResponseToReview(
@@ -21,51 +21,12 @@ describe('handle-response-to-review', () => {
           }),
         ]);
       });
-
-      it('produces a UserFoundReviewHelpfulEvent containing a review ID', async () => {
-        const commitEvents = jest.fn();
-        const handleResponseToReview = createHandleResponseToReview(async () => [], commitEvents);
-        const reviewId = new Doi('10.1111/123456');
-        await handleResponseToReview(
-          { id: toUserId('anyuser') },
-          reviewId,
-          'respond-helpful',
-        );
-
-        expect(commitEvents).toHaveBeenCalledWith([
-          expect.objectContaining({
-            reviewId,
-          }),
-        ]);
-      });
-
-      it('produces an event containing user ID', async () => {
-        const commitEvents = jest.fn();
-        const handleResponseToReview = createHandleResponseToReview(async () => [], commitEvents);
-        const userId = toUserId('currentuser');
-        await handleResponseToReview(
-          // TODO: pass in just a UserId?
-          { id: userId },
-          new Doi('10.1111/123456'),
-          'respond-helpful',
-        );
-
-        expect(commitEvents).toHaveBeenCalledWith([
-          expect.objectContaining({
-            userId,
-          }),
-        ]);
-      });
     });
   });
 
-  describe('when an upvote has been cast', () => {
-    describe('and input contains a revoke upvote', () => {
-      it.todo('should fire a `vote revoked` event');
+  describe('when a helpful response has been given', () => {
+    describe('and input contains a revoke response', () => {
+      it.todo('commits the event');
     });
-  });
-
-  describe('given no previous response when the user responds yes', () => {
-    it.todo('trigger user responded yes event');
   });
 });
