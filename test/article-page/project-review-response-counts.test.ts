73f0166164650a6f0e7c0b96d1bbe67732b89135
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-11-03 11:32:53 +0000

    Test n `helpful` case for count projection #303

--- test/article-page/project-review-response-counts.test.ts
+++ test/article-page/project-review-response-counts.test.ts
@@ -1,5 +1,7 @@
 import createProjectReviewResponseCounts from '../../src/article-page/project-review-response-counts';
 import Doi from '../../src/types/doi';
+import { generate } from '../../src/types/event-id';
+import toUserId from '../../src/types/user-id';
 
 describe('project-review-response-counts', () => {
   describe('given no events', () => {
@@ -14,7 +16,39 @@ describe('project-review-response-counts', () => {
   });
 
   describe('given N different users responded helpful events', () => {
-    it.todo('returns N `helpful` and 0 `not helpful`');
+    it('returns N `helpful` and 0 `not helpful`', async () => {
+      const reviewId = new Doi('10.1234/5678');
+      const userA = toUserId('A');
+      const userB = toUserId('B');
+      const userC = toUserId('C');
+
+      const projectReviewResponseCounts = createProjectReviewResponseCounts(async () => [
+        {
+          type: 'UserFoundReviewHelpful',
+          id: generate(),
+          date: new Date(),
+          reviewId,
+          userId: userA,
+        },
+        {
+          type: 'UserFoundReviewHelpful',
+          id: generate(),
+          date: new Date(),
+          reviewId,
+          userId: userB,
+        },
+        {
+          type: 'UserFoundReviewHelpful',
+          id: generate(),
+          date: new Date(),
+          reviewId,
+          userId: userC,
+        },
+      ]);
+      const projected = await projectReviewResponseCounts(reviewId);
+
+      expect(projected).toStrictEqual({ helpfulCount: 3, notHelpfulCount: 0 });
+    });
   });
 
   describe('given a single user responded helpful and revoked the helpful response', () => {
