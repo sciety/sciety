fe0999b6f1146a0e8062ea2ad86bc7e40f4a8b75
Giorgio Sironi <g.sironi@elifesciences.org>
2020-11-03 09:28:23 +0000

    Implement test for respond-helpful-command happy path #303

--- test/respond/respond-helpful-command.test.ts
+++ test/respond/respond-helpful-command.test.ts
@@ -1,6 +1,23 @@
+import { GetAllEvents, respondHelpful } from '../../src/respond/respond-helpful-command';
+import Doi from '../../src/types/doi';
+import toUserId from '../../src/types/user-id';
+
 describe('respond-helpful-command', () => {
   describe('no-response-state for this review and user', () => {
-    it.todo('return UserFoundReviewHelpful event');
+    it('return UserFoundReviewHelpful event', async () => {
+      const userId = toUserId('someone');
+      const reviewId = new Doi('10.1234/5678');
+      const getAllEvents: GetAllEvents = async () => [];
+
+      const events = await respondHelpful(getAllEvents)(userId, reviewId);
+
+      expect(events).toHaveLength(1);
+      expect(events[0]).toMatchObject({
+        type: 'UserFoundReviewHelpful',
+        userId,
+        reviewId,
+      });
+    });
   });
 
   describe('helpful-state for this review and user', () => {
