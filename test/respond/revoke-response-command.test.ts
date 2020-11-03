918e06f6e46941cbdda1df7cf487b8f4776ed2cf
Kevin Rutherford <kevin@rutherford-software.com>
2020-11-03 13:22:31 +0000

    Add a characterisation test for revoke-response-command #303

--- test/respond/revoke-response-command.test.ts
+++ test/respond/revoke-response-command.test.ts
@@ -1,13 +1,28 @@
+import { revokeResponse } from '../../src/respond/revoke-response-command';
+import Doi from '../../src/types/doi';
+import toUserId from '../../src/types/user-id';
+
 describe('revoke-response-command', () => {
-  describe('no-response-state for this review and user', () => {
+  describe('given no-response state for this review and user', () => {
     it.todo('error');
   });
 
-  describe('helpful-state for this review and user', () => {
-    it.todo('return UserRevokedFindingReviewHelpful event');
+  describe('given helpful state for this review and user', () => {
+    it('return UserRevokedFindingReviewHelpful event', async () => {
+      const reviewId = new Doi('10.1111/333333');
+      const userId = toUserId('someone');
+      const events = await revokeResponse()(userId, reviewId);
+
+      expect(events).toHaveLength(1);
+      expect(events[0]).toMatchObject({
+        type: 'UserRevokedFindingReviewHelpful',
+        reviewId,
+        userId,
+      });
+    });
   });
 
-  describe('not-helpful-state for this review and user', () => {
+  describe('given not-helpful state for this review and user', () => {
     it.todo('return UserRevokedFindingReviewNotHelpful event');
   });
 });
