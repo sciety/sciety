5a2749df93608e71a358ddcf95ddd6fa3d0f5359
Kevin Rutherford <kevin@rutherford-software.com>
2020-11-03 10:02:51 +0000

    Add another test case for the no-response state #303

--- test/respond/respond-helpful-command.test.ts
+++ test/respond/respond-helpful-command.test.ts
@@ -1,14 +1,32 @@
 import { GetAllEvents, respondHelpful } from '../../src/respond/respond-helpful-command';
 import Doi from '../../src/types/doi';
+import { UserFoundReviewHelpfulEvent } from '../../src/types/domain-events';
 import { generate } from '../../src/types/event-id';
-import toUserId from '../../src/types/user-id';
+import { ReviewId } from '../../src/types/review-id';
+import toUserId, { UserId } from '../../src/types/user-id';
+
+type EventType = UserFoundReviewHelpfulEvent;
+
+const makeEvent = (type: EventType['type'], userId: UserId, reviewId: ReviewId): EventType => ({
+  id: generate(),
+  date: new Date(),
+  type,
+  userId,
+  reviewId,
+});
+
+const userId = toUserId('someone');
+const reviewId = new Doi('10.1234/5678');
 
 describe('respond-helpful-command', () => {
   describe('no-response-state for this review and user', () => {
-    it('return UserFoundReviewHelpful event', async () => {
-      const userId = toUserId('someone');
-      const reviewId = new Doi('10.1234/5678');
-      const getAllEvents: GetAllEvents = async () => [];
+    it.each([
+      ['no events', []],
+      ['different review helpful', [
+        makeEvent('UserFoundReviewHelpful', userId, new Doi('10.1101/444444')),
+      ]],
+    ])('given %s, return UserFoundReviewHelpful event', async (_, history) => {
+      const getAllEvents: GetAllEvents = async () => history;
 
       const events = await respondHelpful(getAllEvents)(userId, reviewId);
 
@@ -22,18 +40,10 @@ describe('respond-helpful-command', () => {
   });
 
   describe('helpful-state for this review and user', () => {
-    it('return no events', async () => {
-      const userId = toUserId('someone');
-      const reviewId = new Doi('10.1234/5678');
-      const getAllEvents: GetAllEvents = async () => [
-        {
-          id: generate(),
-          date: new Date(),
-          type: 'UserFoundReviewHelpful',
-          userId,
-          reviewId,
-        },
-      ];
+    it.each([
+      ['single helpful event', [makeEvent('UserFoundReviewHelpful', userId, reviewId)]],
+    ])('given %s, return no events', async (_, history) => {
+      const getAllEvents: GetAllEvents = async () => history;
 
       const events = await respondHelpful(getAllEvents)(userId, reviewId);
 
