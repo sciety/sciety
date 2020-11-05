2382a341d2a646bafc9ff5594e86fea75608b8e1
Kevin Rutherford <kevin@rutherford-software.com>
2020-11-05 15:38:57 +0000

    Add an integration test checking the article page og: meta tags #298

---
+++ test/http/page-handler.test.ts
@@ -0,0 +1,19 @@
+import { JSDOM } from 'jsdom';
+import request, { Response } from 'supertest';
+import createServer from './server';
+
+describe('page-handler', (): void => {
+  describe('article page', () => {
+    it('sets the og meta tags to the article details', async () => {
+      const { server } = await createServer();
+      const response: Response = await request(server).get('/articles/10.1101/646810');
+      const html = response.text;
+      const rendered = JSDOM.fragment(html);
+      const ogTitle = rendered.querySelector('meta[property="og:title"]')?.getAttribute('content');
+      const ogDescription = rendered.querySelector('meta[property="og:description"]')?.getAttribute('content');
+
+      expect(ogTitle).toBe('Article title');
+      expect(ogDescription).toBe('Article abstract.');
+    });
+  });
+});
