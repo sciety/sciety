de327c205a529e9fc6e8aceba904d0a749aff400
David Moulton <d.moulton@elifesciences.org>
2020-11-11 14:44:31 +0000

    Swap showdown for remarkable on about page #317

--- src/about-page/index.ts
+++ src/about-page/index.ts
@@ -1,4 +1,4 @@
-import showdown from 'showdown';
+import { Remarkable } from 'remarkable';
 import createRenderPage, { GetHtml, RenderPage } from './render-page';
 
 export type FetchStaticFile = (filename: string) => Promise<string>;
@@ -10,10 +10,10 @@ interface Ports {
 type AboutPage = () => ReturnType<RenderPage>;
 
 export default (ports: Ports): AboutPage => {
-  const converter = new showdown.Converter({ noHeaderId: true });
+  const remarkable = new Remarkable({ html: true });
   const getHtml: GetHtml = async (filename) => {
     const text = await ports.fetchStaticFile(filename);
-    return converter.makeHtml(text);
+    return remarkable.render(text);
   };
   const renderPage = createRenderPage(getHtml);
   return async () => (
