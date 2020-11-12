d41adec677f9c83c79aaa3a800ef457b364deadb
Giorgio Sironi <g.sironi@elifesciences.org>
2020-11-12 10:19:58 +0000

    Exploratory tests from language handbook
    https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes-func.html

---
+++ test/type-theory.test.ts
@@ -0,0 +1,87 @@
+describe('type theory', () => {
+  describe('built-in types', () => {
+    it('has all the javascript one', () => {
+      const a: number = 42;
+      const b: string = 'forty-two';
+      const c: bigint = BigInt('424242424242424242424242424242');
+      const d: boolean = true;
+      const e: symbol = Symbol('my-iterator');
+      const f: null = null;
+      const g: undefined = undefined;
+      const h: object = {};
+      expect([a, b, c, d, e, f, g, h]).toHaveLength(8);
+    });
+
+    it('prefers Record instead of object', () => {
+      const record: Record<string, number> = {};
+      record['answer'] = 42;
+      expect(record.answer).toStrictEqual(42);
+    });
+
+    it('uses unknown as the top type', () => {
+      const acceptsEverything = (_input: unknown) => 42;
+      acceptsEverything('some-input');
+    });
+
+    it('uses never as the bottom type', () => {
+      const infiniteLoop = (): never => { while (true) { console.log('one more iteration'); } };
+    });
+
+    describe('provides unit types', () => {
+      const canOnlyHaveOneValue = 'foobar';
+      it('allows operations', () => {
+        expect(canOnlyHaveOneValue.substr(0, 3)).toStrictEqual('foo');
+      });
+    });
+  });
+
+  describe('data structures', () => {
+    it('provides Arrays', () => {
+      const array: Array<string> = ['a', 'b'];
+      expect(array).toHaveLength(2);
+    });
+
+    it('provides (im)mutable tuples', () => {
+      const tuple: readonly [string, string] = ['a', 'b'];
+      expect(tuple).toHaveLength(2);
+      // doesn't compile:
+      //tuple.push('c'); 
+      //tuple.pop();
+    });
+  });
+
+  describe('higher-order types', () => {
+    it('provides non-discriminated unions', () => {
+      const numberOrString: number|string = 42;
+      switch (true) {
+        // switch with typeof, or existing properties or tags you put in yourself (e.g. `.type` === ...)
+        case typeof numberOrString === 'number':
+          expect(numberOrString).toStrictEqual(42);
+          break;
+        case typeof numberOrString === 'string':
+          expect(numberOrString).toStrictEqual('forty-two');
+          break;
+      }
+    })
+
+    it('provides intersection types', () => {
+      type Combined = { a: number } & { b: string };
+      const combined: Combined = {'a': 42, b: 'forty-two'};
+      expect(combined.a).toStrictEqual(42);
+    });
+  });
+
+  describe('provides type aliases', () => {
+    it('for domain-specific names', () => {
+      type Size = [number, number];
+      let x: Size = [101, 999];
+      expect(x[0]).toStrictEqual(101);
+    });
+
+    describe('can use tagged intersections to restrict a new type without runtime overhead', () => {
+      type UserId = string & { __compileTimeOnly: any };
+      const userId: UserId = 'my-user-id' as UserId;
+      expect(userId.length).toStrictEqual(10);
+    });
+  });
+});
