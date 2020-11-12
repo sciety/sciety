ed1e1a93e10494edbb858fea5fe2b6fd0ea11083
Kevin Rutherford <kevin@rutherford-software.com>
2020-11-12 13:18:38 +0000

    Sanitise HTML #317

--- src/types/sanitised-html-fragment.ts
+++ src/types/sanitised-html-fragment.ts
@@ -1,5 +1,8 @@
+import sanitiseHtml from 'sanitize-html';
 import { HtmlFragment } from './html-fragment';
 
 export type SanitisedHtmlFragment = HtmlFragment & { readonly SanitisedHtmlFragment: unique symbol };
 
-export const sanitise = (value: HtmlFragment): SanitisedHtmlFragment => value as SanitisedHtmlFragment;
+export const sanitise = (value: HtmlFragment): SanitisedHtmlFragment => (
+  sanitiseHtml(value) as SanitisedHtmlFragment
+);
---
+++ test/types/sanitised-html-fragment.test.ts
@@ -0,0 +1,18 @@
+import { toHtmlFragment } from '../../src/types/html-fragment';
+import { sanitise } from '../../src/types/sanitised-html-fragment';
+
+describe('sanitise', () => {
+  it('cleans up mismatched HTML tags', () => {
+    const dirtyInput = toHtmlFragment('<strong><em>bold italic</strong> italic</em>');
+    const sanitised = sanitise(dirtyInput);
+
+    expect(sanitised).toStrictEqual('<strong><em>bold italic</em></strong> italic');
+  });
+
+  it('removes <script> tags', () => {
+    const dirtyInput = toHtmlFragment('cleaned <script>var bad = true;</script> html');
+    const sanitised = sanitise(dirtyInput);
+
+    expect(sanitised).toStrictEqual('cleaned  html');
+  });
+});
