62b7c55e21f4a3b94c2c1a8fc9146573ede09b77
Kevin Rutherford <kevin@rutherford-software.com>
2020-11-13 10:58:04 +0000

    Convert user-page to return an HtmlFragment #317

--- src/user-page/render-page.ts
+++ src/user-page/render-page.ts
@@ -1,28 +1,29 @@
 import { Maybe, Result } from 'true-myth';
+import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { UserId } from '../types/user-id';
 
 type RenderPageError = {
   type: 'not-found' | 'unavailable',
-  content: string
+  content: HtmlFragment
 };
 
 export type RenderPage = (
   userId: UserId,
   viewingUserId: Maybe<UserId>,
-) => Promise<Result<{content: string}, RenderPageError>>;
+) => Promise<Result<{content: HtmlFragment}, RenderPageError>>;
 
 type Component = (userId: UserId, viewingUserId: Maybe<UserId>) => Promise<Result<string, 'not-found' | 'unavailable'>>;
 
 const template = (header: string) => (followList: string) => (
   {
-    content: `
+    content: toHtmlFragment(`
       <div class="sciety-grid sciety-grid--user">
         ${header}
         <div class="user-page-contents">
           ${followList}
         </div>
       </div>
-    `,
+    `),
   }
 );
 
@@ -40,12 +41,12 @@ export default (
       if (e === 'not-found') {
         return {
           type: 'not-found',
-          content: 'User not found',
+          content: toHtmlFragment('User not found'),
         };
       }
       return {
         type: 'unavailable',
-        content: 'User information unavailable',
+        content: toHtmlFragment('User information unavailable'),
       };
     });
 };
