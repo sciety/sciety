ab8f64e45eb456f9eec772e0e0bcccea23d86578
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-11-13 11:05:37 +0000

    Convert editorial-community-page to return HtmlFragment #317

--- src/editorial-community-page/render-page.ts
+++ src/editorial-community-page/render-page.ts
@@ -1,18 +1,19 @@
 import { Maybe, Result } from 'true-myth';
 import EditorialCommunityId from '../types/editorial-community-id';
+import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { UserId } from '../types/user-id';
 
 type Component = (editorialCommunityId: EditorialCommunityId, userId: Maybe<UserId>) => Promise<string>;
 
 type RenderPageError = {
   type: 'not-found',
-  content: string,
+  content: HtmlFragment,
 };
 
 export type RenderPage = (
   editorialCommunityId: EditorialCommunityId,
   userId: Maybe<UserId>
-) => Promise<Result<{content: string}, RenderPageError>>;
+) => Promise<Result<{content: HtmlFragment}, RenderPageError>>;
 
 export default (
   renderPageHeader: Component,
@@ -23,7 +24,7 @@ export default (
   async (editorialCommunityId, userId) => {
     try {
       return Result.ok({
-        content: `
+        content: toHtmlFragment(`
           <div class="sciety-grid sciety-grid--editorial-community">
             ${await renderPageHeader(editorialCommunityId, userId)}
     
@@ -35,13 +36,13 @@ export default (
               ${await renderFeed(editorialCommunityId, userId)}
             </div>
           </div>
-        `,
+        `),
       });
     // TODO: push Results further down
     } catch (error: unknown) {
       return Result.err({
         type: 'not-found',
-        content: `Editorial community id '${editorialCommunityId.value}' not found`,
+        content: toHtmlFragment(`Editorial community id '${editorialCommunityId.value}' not found`),
       });
     }
   }
