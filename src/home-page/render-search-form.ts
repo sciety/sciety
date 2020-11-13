6caf96106ababe7e8007f6f84e1a96b2752c8c69
David Moulton <d.moulton@elifesciences.org>
2020-11-13 13:51:44 +0000

    Inline search icon on home page #312

--- src/home-page/render-search-form.ts
+++ src/home-page/render-search-form.ts
@@ -1,5 +1,7 @@
 type RenderFindArticle = () => Promise<string>;
 
+const searchIconSvg = '<svg class="search-form__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><style>.st1{fill:none}</style><path class="st0" d="M15.5 14h-.8l-.3-.3c1-1.1 1.6-2.6 1.6-4.2C16 5.9 13.1 3 9.5 3S3 5.9 3 9.5 5.9 16 9.5 16c1.6 0 3.1-.6 4.2-1.6l.3.3v.8l5 5 1.5-1.5-5-5zm-6 0C7 14 5 12 5 9.5S7 5 9.5 5 14 7 14 9.5 12 14 9.5 14z"/><path class="st1" d="M0 0h24v24H0V0z"/></svg>';
+
 export default (): RenderFindArticle => (
   async () => (`
     <section class="search-form">
@@ -10,7 +12,7 @@ export default (): RenderFindArticle => (
         </h3>
 
         <div class="search-form__inputs">
-          <input type="text" size="19" name="query" placeholder="Keywords, author name, DOI ..." class="search-form__input" required><button type="submit" class="search-form__button"><img src="/static/images/search-icon.svg" alt=""></button>
+          <input type="text" size="19" name="query" placeholder="Keywords, author name, DOI ..." class="search-form__input" required><button type="submit" class="search-form__button">${searchIconSvg}</button>
         </div>
 
       </form>
