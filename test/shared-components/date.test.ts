7bf97c533521570fb09fc76c8104619edbf2f5ae
Kevin Rutherford <kevin@rutherford-software.com>
2020-11-16 09:53:30 +0000

    Update some shared-components tests to use HtmlFragment #317

--- test/shared-components/date.test.ts
+++ test/shared-components/date.test.ts
@@ -1,4 +1,5 @@
 import templateDate from '../../src/shared-components/date';
+import { toHtmlFragment } from '../../src/types/html-fragment';
 
 describe('date template', (): void => {
   it.each([
@@ -7,12 +8,12 @@ describe('date template', (): void => {
   ])('returns a time element on %s', async (string: string, display: string): Promise<void> => {
     const actual = templateDate(new Date(string));
 
-    expect(actual).toBe(`<time datetime="${string}">${display}</time>`);
+    expect(actual).toBe(toHtmlFragment(`<time datetime="${string}">${display}</time>`));
   });
 
   it('adds a class', () => {
     const actual = templateDate(new Date('2002-05-04'), 'a-class-name');
 
-    expect(actual).toBe('<time datetime="2002-05-04" class="a-class-name">May 4, 2002</time>');
+    expect(actual).toBe(toHtmlFragment('<time datetime="2002-05-04" class="a-class-name">May 4, 2002</time>'));
   });
 });
--- test/shared-components/list-items.test.ts
+++ test/shared-components/list-items.test.ts
@@ -1,11 +1,12 @@
 import templateListItems from '../../src/shared-components/list-items';
+import { HtmlFragment, toHtmlFragment } from '../../src/types/html-fragment';
 
 describe('list-items template', (): void => {
   it.each([
-    [[], ''],
-    [['Alice'], '<li class="item">Alice</li>\n'],
-    [['Alice', 'Bob'], '<li class="item">Alice</li>\n<li class="item">Bob</li>\n'],
-  ])('returns <li> elements on %s', async (items: Array<string>, expected: string): Promise<void> => {
+    [[], toHtmlFragment('')],
+    [['Alice'], toHtmlFragment('<li class="item">Alice</li>\n')],
+    [['Alice', 'Bob'], toHtmlFragment('<li class="item">Alice</li>\n<li class="item">Bob</li>\n')],
+  ])('returns <li> elements on %s', async (items: Array<string>, expected: HtmlFragment): Promise<void> => {
     const actual = templateListItems(items);
 
     expect(actual).toBe(expected);
