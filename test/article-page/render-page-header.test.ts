4f2bff61df3382e82f2a4b3bcbb5467916d7285c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-11-19 15:36:26 +0000

    Remove duplicate unseafelyUnwrap in render-page-header.test #310

--- test/article-page/render-page-header.test.ts
+++ test/article-page/render-page-header.test.ts
@@ -5,6 +5,7 @@ import createRenderPageHeader, {
   RenderPageHeader,
 } from '../../src/article-page/render-page-header';
 import Doi from '../../src/types/doi';
+import { HtmlFragment } from '../../src/types/html-fragment';
 
 const getArticleDetails: GetArticleDetails<never> = async (doi) => (Result.ok({
   title: `Lorem ipsum ${doi.value}`,
@@ -13,30 +14,30 @@ const getArticleDetails: GetArticleDetails<never> = async (doi) => (Result.ok({
 
 describe('render-page-header component', (): void => {
   let renderPageHeader: RenderPageHeader<never>;
-  let rendered: Result<string, never>;
+  let rendered: HtmlFragment;
 
   beforeEach(async () => {
     renderPageHeader = createRenderPageHeader(getArticleDetails);
-    rendered = await renderPageHeader(new Doi('10.1101/815689'));
+    rendered = (await renderPageHeader(new Doi('10.1101/815689'))).unsafelyUnwrap();
   });
 
   it('renders inside an header tag', () => {
-    expect(rendered.unsafelyUnwrap()).toStrictEqual(expect.stringMatching(/^\s*<header\s|>/));
+    expect(rendered).toStrictEqual(expect.stringMatching(/^\s*<header\s|>/));
   });
 
   it('renders the title for an article', async (): Promise<void> => {
-    expect(rendered.unsafelyUnwrap()).toStrictEqual(expect.stringContaining('Lorem ipsum 10.1101/815689'));
+    expect(rendered).toStrictEqual(expect.stringContaining('Lorem ipsum 10.1101/815689'));
   });
 
   it('renders the article DOI according to CrossRef display guidelines', () => {
-    const links = JSDOM.fragment(rendered.unsafelyUnwrap()).querySelectorAll('a');
+    const links = JSDOM.fragment(rendered).querySelectorAll('a');
 
     expect(links).toHaveLength(1);
     expect(links[0].textContent).toStrictEqual('https://doi.org/10.1101/815689');
   });
 
   it('renders the article authors', () => {
-    expect(rendered.unsafelyUnwrap()).toStrictEqual(expect.stringContaining('Gary'));
-    expect(rendered.unsafelyUnwrap()).toStrictEqual(expect.stringContaining('Uncle Wiggly'));
+    expect(rendered).toStrictEqual(expect.stringContaining('Gary'));
+    expect(rendered).toStrictEqual(expect.stringContaining('Uncle Wiggly'));
   });
 });
