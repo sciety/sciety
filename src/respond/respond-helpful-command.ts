bb596f84d204f2e8b8f331ca442ce64eb914749b
David Moulton <d.moulton@elifesciences.org>
2020-11-27 11:28:49 +0000

    Use ReviewResponse module to reduce duplication #303

--- src/respond/respond-helpful-command.ts
+++ src/respond/respond-helpful-command.ts
@@ -1,9 +1,7 @@
+import { reviewResponse } from './review-response';
 import {
   DomainEvent,
   UserFoundReviewHelpfulEvent,
-  UserFoundReviewNotHelpfulEvent,
-  UserRevokedFindingReviewHelpfulEvent,
-  UserRevokedFindingReviewNotHelpfulEvent,
 } from '../types/domain-events';
 import { generate } from '../types/event-id';
 import { ReviewId } from '../types/review-id';
@@ -15,50 +13,9 @@ type RespondHelpful = (userId: UserId, reviewId: ReviewId) => Promise<
 ReadonlyArray<UserFoundReviewHelpfulEvent>
 >;
 
-type InterestingEvent =
-  | UserFoundReviewHelpfulEvent
-  | UserRevokedFindingReviewHelpfulEvent
-  | UserFoundReviewNotHelpfulEvent
-  | UserRevokedFindingReviewNotHelpfulEvent;
-
-type Response = 'none' | 'helpful' | 'not-helpful';
-
 export const respondHelpful = (getAllEvents: GetAllEvents): RespondHelpful => async (userId, reviewId) => {
-  const ofInterest = (await getAllEvents())
-    // TODO: deduplicate filtering with other command(s) and factor out tests that duplicate this logic
-    .filter(
-      (event): event is InterestingEvent => (
-        event.type === 'UserFoundReviewHelpful'
-        || event.type === 'UserRevokedFindingReviewHelpful'
-        || event.type === 'UserFoundReviewNotHelpful'
-        || event.type === 'UserRevokedFindingReviewNotHelpful'
-      ),
-    )
-    .filter((event) => event.userId === userId && event.reviewId.toString() === reviewId.toString());
-
-  let response: Response;
-
-  // TODO: fold if else into switch
-  if (ofInterest.length === 0) {
-    response = 'none';
-  } else {
-    const typeOfMostRecentEvent = ofInterest[ofInterest.length - 1].type;
-
-    switch (typeOfMostRecentEvent) {
-      case 'UserRevokedFindingReviewHelpful':
-        response = 'none';
-        break;
-      case 'UserRevokedFindingReviewNotHelpful':
-        response = 'none';
-        break;
-      case 'UserFoundReviewHelpful':
-        response = 'helpful';
-        break;
-      case 'UserFoundReviewNotHelpful':
-        response = 'not-helpful';
-        break;
-    }
-  }
+  const ofInterest = (await getAllEvents());
+  const response = reviewResponse(userId, reviewId)(ofInterest);
 
   if (response === 'helpful') {
     return [];
