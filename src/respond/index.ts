e86bfc73114b733a51f26ee637835afaa19fc7b3
Giorgio Sironi <g.sironi@elifesciences.org>
2020-11-27 16:23:50 +0000

    Remove handle-response-to-review #303

--- src/respond/index.ts
+++ src/respond/index.ts
@@ -1,13 +1,15 @@
 import { Middleware } from '@koa/router';
 import * as T from 'fp-ts/lib/Task';
 import { pipe } from 'fp-ts/lib/function';
-import { CommitEvents } from './handle-response-to-review';
 import { GetAllEvents, respondHelpful } from './respond-helpful-command';
 import { respondNotHelpful } from './respond-not-helpful-command';
 import { reviewResponse } from './review-response';
 import { revokeResponse } from './revoke-response-command';
+import { RuntimeGeneratedEvent } from '../types/domain-events';
 import toReviewId from '../types/review-id';
 import { User } from '../types/user';
+
+type CommitEvents = (events: ReadonlyArray<RuntimeGeneratedEvent>) => void;
 
 type Ports = {
   commitEvents: CommitEvents;
