6dd5554817d05584d2f2a5979fc3bbcb71b0f501
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-12-03 10:29:02 +0000

    Add test shapes for the user page title #340

---
+++ test/user-page/render-page.test.ts
@@ -0,0 +1,13 @@
+describe('render-page', () => {
+  describe('when the user display name is found', () => {
+    it.todo('is used as the page title');
+  });
+
+  describe('when the user display name is not found', () => {
+    it.todo('returns a not-found error page');
+  });
+
+  describe('when the user display name is unavailable', () => {
+    it.todo('returns an unavailable error page');
+  });
+});
