1e8477afffed258f608ae417966e64ad6bb83b4f
Giorgio Sironi <g.sironi@elifesciences.org>
2020-12-03 10:46:44 +0000

    Implement happy path test for user-page display name rendering #340

--- test/user-page/render-page.test.ts
+++ test/user-page/render-page.test.ts
@@ -1,6 +1,20 @@
+import { Maybe, Result } from 'true-myth';
+import toUserId from '../../src/types/user-id';
+import createRenderPage from '../../src/user-page/render-page';
+
 describe('render-page', () => {
   describe('when the user display name is found', () => {
-    it.todo('is used as the page title');
+    it('is used as the page title', async () => {
+      const renderPage = createRenderPage(
+        async () => Result.ok(''),
+        async () => Result.ok(''),
+        async () => 'someone',
+      );
+
+      const result = await renderPage(toUserId('1234'), Maybe.nothing());
+
+      expect(result.unsafelyUnwrap().title).toStrictEqual('someone');
+    });
   });
 
   describe('when the user display name is not found', () => {
