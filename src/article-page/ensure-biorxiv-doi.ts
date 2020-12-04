e67ec3d6dc052c83394403f16abbba2f64fe5b44
Will Byrne <w.byrne@elifesciences.org>
2020-12-04 16:27:45 +0000

    use a pipe and filter in ensure-biorxiv-doi #128

--- src/article-page/ensure-biorxiv-doi.ts
+++ src/article-page/ensure-biorxiv-doi.ts
@@ -1,18 +1,10 @@
 import * as O from 'fp-ts/lib/Option';
+import { pipe } from 'fp-ts/lib/function';
 import Doi from '../types/doi';
 
 const biorxivPrefix = '10.1101';
 
-export default (value: string): O.Option<Doi> => {
-  let doi: Doi;
-  try {
-    doi = new Doi(value);
-  } catch (error: unknown) {
-    return O.none;
-  }
-
-  if (!(doi.hasPrefix(biorxivPrefix))) {
-    return O.none;
-  }
-  return O.some(doi);
-};
+export default (value: string): O.Option<Doi> => pipe(
+  O.tryCatch(() => new Doi(value)),
+  O.filter((doi) => doi.hasPrefix(biorxivPrefix)),
+);
