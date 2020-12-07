611df5051cf469df2a445a252208aa98505b3239
Kevin Rutherford <kevin@rutherford-software.com>
2020-12-07 14:47:23 +0000

    Allow template-list-items to operate on a readonly array #128

--- src/shared-components/list-items.ts
+++ src/shared-components/list-items.ts
@@ -1,6 +1,6 @@
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
-export default (items: Array<HtmlFragment>, itemClass = 'item'): HtmlFragment => (
+export default (items: ReadonlyArray<HtmlFragment>, itemClass = 'item'): HtmlFragment => (
   toHtmlFragment(
     items.map((item: HtmlFragment): string => `<li class="${itemClass}">${item}</li>\n`)
       .join(''),
