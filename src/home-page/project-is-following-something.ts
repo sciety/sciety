b38758fac8bc16859c3f18288a1795ea6db5fddc
Kevin Rutherford <kevin@rutherford-software.com>
2020-12-09 13:12:08 +0000

    Use a pipe to map the Task without awaiting #128

--- src/home-page/project-is-following-something.ts
+++ src/home-page/project-is-following-something.ts
@@ -1,19 +1,24 @@
+import * as A from 'fp-ts/lib/ReadonlyArray';
 import * as T from 'fp-ts/lib/Task';
+import { pipe } from 'fp-ts/lib/function';
 import { IsFollowingSomething } from './render-feed';
 import { DomainEvent } from '../types/domain-events';
+import { UserId } from '../types/user-id';
 
 export type GetAllEvents = T.Task<ReadonlyArray<DomainEvent>>;
 
-export default (getAllEvents: GetAllEvents): IsFollowingSomething => (
-  (userId) => async () => {
-    let count = 0;
-    (await getAllEvents()).forEach((event) => {
-      if (event.type === 'UserFollowedEditorialCommunity' && event.userId === userId) {
-        count += 1;
-      } else if (event.type === 'UserUnfollowedEditorialCommunity' && event.userId === userId) {
-        count -= 1;
-      }
-    });
-    return count > 0;
+const countFollowedCommunities = (userId: UserId) => (count: number, event: DomainEvent): number => {
+  if (event.type === 'UserFollowedEditorialCommunity' && event.userId === userId) {
+    return count + 1;
   }
+  if (event.type === 'UserUnfollowedEditorialCommunity' && event.userId === userId) {
+    return count - 1;
+  }
+  return count;
+};
+
+export default (getAllEvents: GetAllEvents): IsFollowingSomething => (userId) => pipe(
+  getAllEvents,
+  T.map(A.reduce(0, countFollowedCommunities(userId))),
+  T.map((count) => count > 0),
 );
