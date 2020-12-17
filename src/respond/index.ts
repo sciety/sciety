eeb5d770a39dd0ccde7bc1bcc5e093176ff4a513
Kevin Rutherford <kevin@rutherford-software.com>
2020-12-17 12:03:30 +0000

    Extract a validator for the response command #303

--- src/respond/index.ts
+++ src/respond/index.ts
@@ -1,4 +1,6 @@
 import { Middleware } from '@koa/router';
+import * as O from 'fp-ts/lib/Option';
+import { pipe } from 'fp-ts/lib/function';
 import { BadRequest } from 'http-errors';
 import { commandHandler, CommitEvents } from './command-handler';
 import { GetAllEvents } from './respond-helpful-command';
@@ -10,19 +12,26 @@ type Ports = {
   getAllEvents: GetAllEvents;
 };
 
+type ValidCommand = 'respond-helpful' | 'respond-not-helpful' | 'revoke-response';
+
+const validateCommand = O.fromPredicate((command): command is ValidCommand => (
+  command === 'respond-helpful' || command === 'revoke-response' || command === 'respond-not-helpful'
+));
+
 export const createRespondHandler = (ports: Ports): Middleware<{ user: User }> => async (context, next) => {
   const { user } = context.state;
   const reviewId = toReviewId(context.request.body.reviewid);
 
   const command = context.request.body.command as string;
-  if (command !== 'respond-helpful' && command !== 'revoke-response' && command !== 'respond-not-helpful') {
-    throw new BadRequest();
-  }
+  const validatedCommand = validateCommand(command);
 
   await commandHandler(
     ports.commitEvents,
     ports.getAllEvents,
-    command,
+    pipe(
+      validatedCommand,
+      O.getOrElse<ValidCommand>(() => { throw new BadRequest(); }),
+    ),
     user.id,
     reviewId,
   )();
