e4be3f22183d545fd5a60ff7ad68fe9e6c5b1121
Giorgio Sironi <g.sironi@elifesciences.org>
2020-12-18 11:40:08 +0000

    Pass test Twitter user credentials to Taiko test #303

--- feature-test/login.js
+++ feature-test/login.js
@@ -1,13 +1,16 @@
+const dotenv = require("dotenv")
 const { openBrowser, goto, click, closeBrowser, into, textBox, write } = require('taiko');
+
+dotenv.config();
+
 (async () => {
     try {
         await openBrowser();
         await goto('localhost:8080');
         await click('Log in');
-        await write('admin', into(textBox('Username')));
-        await write('password', into(textBox('Password')));
+        await write(process.env.TAIKO_TWITTER_USERNAME, into(textBox('Username')));
+        await write(process.env.TAIKO_TWITTER_PASSWORD, into(textBox('Password')));
         await click('Sign in');
-        await click('Return');
     } catch (error) {
         console.error(error);
     } finally {
