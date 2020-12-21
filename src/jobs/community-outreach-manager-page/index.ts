af8319f4b6eec323e508c17917139bd498cd1df1
Kevin Rutherford <kevin@rutherford-software.com>
2020-12-21 12:54:56 +0000

    Convert an adapter to a flow #128

--- src/jobs/community-outreach-manager-page/index.ts
+++ src/jobs/community-outreach-manager-page/index.ts
@@ -1,4 +1,5 @@
 import * as T from 'fp-ts/lib/Task';
+import { flow } from 'fp-ts/lib/function';
 import { Remarkable } from 'remarkable';
 import createRenderPage, { GetHtml, RenderPage } from './render-page';
 
@@ -12,10 +13,10 @@ type CommunityOutreachManagerPage = () => ReturnType<RenderPage>;
 
 export default (ports: Ports): CommunityOutreachManagerPage => {
   const remarkable = new Remarkable({ html: true });
-  const getHtml: GetHtml = async (filename) => {
-    const text = await ports.fetchStaticFile(filename)();
-    return remarkable.render(text);
-  };
+  const getHtml: GetHtml = flow(
+    ports.fetchStaticFile,
+    T.map((md: string) => remarkable.render(md)),
+  );
   const renderPage = createRenderPage(getHtml);
   return async () => (
     renderPage('jobs/community-outreach-manager.md')
