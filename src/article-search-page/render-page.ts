aa4213cf4cef8bde9652160c0a56782b3c783993
Giorgio Sironi <g.sironi@elifesciences.org>
2020-12-22 10:52:52 +0000

    Return error page for search #358

--- src/article-search-page/render-page.ts
+++ src/article-search-page/render-page.ts
@@ -1,30 +1,40 @@
 import { Result } from 'true-myth';
 import { RenderSearchResults } from './render-search-results';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
+import { RenderPageError } from '../types/render-page-error';
 
 export type RenderPage = (query: string) => Promise<Result<{
   title: string,
   content: HtmlFragment,
-}, never>>;
+}, RenderPageError>>;
 
 export default (
   renderSearchResults: RenderSearchResults,
 ): RenderPage => async (query) => {
-  const searchResults = await renderSearchResults(query)();
-  return Result.ok({
-    title: 'Search results',
-    content: toHtmlFragment(`
-    <div class="sciety-grid sciety-grid--simple">
+  try {
+    const searchResults = await renderSearchResults(query)();
+    return Result.ok({
+      title: 'Search results',
+      content: toHtmlFragment(`
+      <div class="sciety-grid sciety-grid--simple">
 
-      <header class="page-header">
-        <h1>Search results</h1>
-      </header>
+        <header class="page-header">
+          <h1>Search results</h1>
+        </header>
 
-      <section class="ui basic vertical segment">
-        ${searchResults}
-      </section>
+        <section class="ui basic vertical segment">
+          ${searchResults}
+        </section>
 
-    </div>
-  `),
-  });
+      </div>
+    `),
+    });
+  } catch (error: unknown) {
+    return Result.err({
+      type: 'unavailable',
+      message: toHtmlFragment(`
+        We’re having trouble searching for you, please come back later.
+      `),
+    });
+  }
 };
