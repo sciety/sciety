c73381d2d2a9354e91b88df1fe59531dbe1cfe22
Kevin Rutherford <kevin@rutherford-software.com>
2020-12-22 11:23:33 +0000

    Remove a Promise #128

--- src/user-page/render-followed-editorial-community.ts
+++ src/user-page/render-followed-editorial-community.ts
@@ -1,12 +1,19 @@
 import { URL } from 'url';
 import * as O from 'fp-ts/lib/Option';
 import * as T from 'fp-ts/lib/Task';
+import { pipe } from 'fp-ts/lib/function';
 import EditorialCommunityId from '../types/editorial-community-id';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { UserId } from '../types/user-id';
 
+interface Community {
+  id: EditorialCommunityId;
+  name: string;
+  avatar: URL;
+}
+
 export type RenderFollowedEditorialCommunity = (
-  editorialCommunity: EditorialCommunity,
+  community: Community,
   userId: O.Option<UserId>,
 ) => T.Task<HtmlFragment>;
 
@@ -15,26 +22,24 @@ type RenderFollowToggle = (
   editorialcommunityid: EditorialCommunityId,
 ) => T.Task<HtmlFragment>;
 
-interface EditorialCommunity {
-  id: EditorialCommunityId;
-  name: string;
-  avatar: URL;
-}
+const render = (community: Community) => (toggle: string): string => `
+  <div class="label">
+    <img src="${community.avatar.toString()}" alt="">
+  </div>
+  <div class="content">
+    <div class="summary">
+      <a href="/editorial-communities/${community.id.value}">${community.name}</a>
+    </div>
+  </div>
+  <div class="right floated">
+    ${toggle}
+  </div>
+`;
 
 export default (
   renderFollowToggle: RenderFollowToggle,
-): RenderFollowedEditorialCommunity => (
-  (editorialCommunity, userId) => async () => toHtmlFragment(`
-    <div class="label">
-      <img src="${editorialCommunity.avatar.toString()}" alt="">
-    </div>
-    <div class="content">
-      <div class="summary">
-        <a href="/editorial-communities/${editorialCommunity.id.value}">${editorialCommunity.name}</a>
-      </div>
-    </div>
-    <div class="right floated">
-      ${await renderFollowToggle(userId, editorialCommunity.id)()}
-    </div>
-  `)
+): RenderFollowedEditorialCommunity => (community, userId) => pipe(
+  renderFollowToggle(userId, community.id),
+  T.map(render(community)),
+  T.map(toHtmlFragment),
 );
