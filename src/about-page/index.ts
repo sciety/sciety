37c4d240bf48c6aa48c9107e60f81b03aa06197c
Kevin Rutherford <kevin@rutherford-software.com>
2020-12-23 10:30:37 +0000

    Remove the last Promise form the about-page #128

--- src/about-page/index.ts
+++ src/about-page/index.ts
@@ -1,7 +1,7 @@
 import * as T from 'fp-ts/lib/Task';
-import { flow } from 'fp-ts/lib/function';
+import { flow, pipe } from 'fp-ts/lib/function';
 import { Remarkable } from 'remarkable';
-import createRenderPage, { GetHtml, RenderPage } from './render-page';
+import renderPage, { RenderPage } from './render-page';
 
 export type FetchStaticFile = (filename: string) => T.Task<string>;
 
@@ -9,16 +9,14 @@ interface Ports {
   fetchStaticFile: FetchStaticFile;
 }
 
-type AboutPage = () => ReturnType<RenderPage>;
+type AboutPage = ReturnType<RenderPage>;
 
-export default (ports: Ports): AboutPage => {
-  const remarkable = new Remarkable({ html: true });
-  const getHtml: GetHtml = flow(
-    ports.fetchStaticFile,
-    T.map((md: string) => remarkable.render(md)),
-  );
-  const renderPage = createRenderPage(getHtml);
-  return async () => (
-    renderPage('about.md')
-  );
-};
+export default (ports: Ports): AboutPage => pipe(
+  'about.md',
+  renderPage(
+    flow(
+      ports.fetchStaticFile,
+      T.map((md: string) => new Remarkable({ html: true }).render(md)),
+    ),
+  ),
+);
--- test/about-page/render-page.test.ts
+++ test/about-page/render-page.test.ts
@@ -5,7 +5,7 @@ describe('render-about-page middleware', (): void => {
   it('returns a page with the given HTML embedded', async (): Promise<void> => {
     const html = '<h1>About stuff</h1>';
     const renderPage: RenderPage = createRenderPage(() => T.of(html));
-    const rendered = await renderPage('anyfile.md');
+    const rendered = await renderPage('anyfile.md')();
 
     expect(rendered.isOk()).toBe(true);
     expect(rendered.unsafelyUnwrap().content).toStrictEqual(expect.stringContaining(html));
