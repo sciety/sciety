7a4738c3281f854734b84824387ee5737f16173f
Giorgio Sironi <g.sironi@elifesciences.org>
2021-01-04 10:18:17 +0000

    Redirect to review anchor after authentication #303

--- src/http/require-authentication.ts
+++ src/http/require-authentication.ts
@@ -5,6 +5,10 @@ export const createRequireAuthentication = (): Middleware<{ user?: User }> => (
   async (context, next) => {
     if (!(context.state.user)) {
       context.session.successRedirect = context.request.headers.referer ?? '/';
+      if (context.session.targetFragmentId) {
+        context.session.successRedirect = `${context.session.successRedirect as string}#${context.session.targetFragmentId as string}`;
+        delete context.session.targetFragmentId;
+      }
       context.redirect('/log-in');
       return;
     }
--- src/respond/save-respond-command.ts
+++ src/respond/save-respond-command.ts
@@ -3,5 +3,7 @@ import { Middleware, ParameterizedContext } from 'koa';
 export const saveRespondCommand: Middleware = async (context: ParameterizedContext, next) => {
   context.session.command = context.request.body.command;
   context.session.reviewId = context.request.body.reviewid;
+  // TODO: don't put in the session, just in the context
+  context.session.targetFragmentId = context.request.body.reviewid;
   await next();
 };
