97832c91778daf850bc9e1599fbf45a0804cd956
Kevin Rutherford <kevin@rutherford-software.com>
2021-01-04 10:41:28 +0000

    Don't store the target fragment id in the session #303

--- src/http/require-authentication.ts
+++ src/http/require-authentication.ts
@@ -1,13 +1,17 @@
 import { Middleware, ParameterizedContext } from 'koa';
 import { User } from '../types/user';
 
-export const createRequireAuthentication = (): Middleware<{ user?: User }> => (
+type State = {
+  user?: User,
+  targetFragmentId?: string,
+};
+
+export const createRequireAuthentication = (): Middleware<State> => (
   async (context, next) => {
     if (!(context.state.user)) {
       context.session.successRedirect = context.request.headers.referer ?? '/';
-      if (context.session.targetFragmentId) {
-        context.session.successRedirect = `${context.session.successRedirect as string}#${context.session.targetFragmentId as string}`;
-        delete context.session.targetFragmentId;
+      if (context.state.targetFragmentId) {
+        context.session.successRedirect = `${context.session.successRedirect as string}#${context.state.targetFragmentId}`;
       }
       context.redirect('/log-in');
       return;
--- src/respond/save-respond-command.ts
+++ src/respond/save-respond-command.ts
@@ -3,7 +3,6 @@ import { Middleware, ParameterizedContext } from 'koa';
 export const saveRespondCommand: Middleware = async (context: ParameterizedContext, next) => {
   context.session.command = context.request.body.command;
   context.session.reviewId = context.request.body.reviewid;
-  // TODO: don't put in the session, just in the context
-  context.session.targetFragmentId = context.request.body.reviewid;
+  context.state.targetFragmentId = context.request.body.reviewid;
   await next();
 };
