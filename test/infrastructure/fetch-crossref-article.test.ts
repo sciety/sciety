dbe07bfd24b3f31bf0087aee7bf0076f6a51b092
Giorgio Sironi <g.sironi@elifesciences.org>
2021-01-06 09:25:27 +0000

    Convert abstract tests to parse-crossref-article #365

--- test/infrastructure/fetch-crossref-article.test.ts
+++ test/infrastructure/fetch-crossref-article.test.ts
@@ -19,44 +19,50 @@ const crossrefResponseWith = (content: string): string => `
 
 describe('fetch-crossref-article', (): void => {
   describe('fetching the abstract', (): void => {
+    const doi = new Doi('10.1101/339747');
+    const parser = new DOMParser({
+      errorHandler: (_, msg) => {
+        throw msg;
+      },
+    });
+
     it('extracts the abstract text from the XML response', async () => {
-      const doi = new Doi('10.1101/339747');
-      const getXml: GetXml = async () => crossrefResponseWith(`
+      const response = crossrefResponseWith(`
         <abstract>
           Some random nonsense.
         </abstract>`);
-      const abstract = (await createFetchCrossrefArticle(getXml, dummyLogger)(doi)()).unsafelyUnwrap();
+      const doc = parser.parseFromString(response, 'text/xml');
+      const abstract = getAbstract(doc, doi, dummyLogger);
 
-      expect(abstract.abstract).toStrictEqual(expect.stringContaining('Some random nonsense.'));
+      expect(abstract).toStrictEqual(expect.stringContaining('Some random nonsense.'));
     });
 
     it('removes the <abstract> element', async () => {
-      const doi = new Doi('10.1101/339747');
-      const getXml: GetXml = async () => crossrefResponseWith(`
+      const response = crossrefResponseWith(`
         <abstract class="something">
           Some random nonsense.
         </abstract>`);
-      const abstract = (await createFetchCrossrefArticle(getXml, dummyLogger)(doi)()).unsafelyUnwrap();
+      const doc = parser.parseFromString(response, 'text/xml');
+      const abstract = getAbstract(doc, doi, dummyLogger);
 
-      expect(abstract.abstract).toStrictEqual(expect.not.stringContaining('<abstract>'));
-      expect(abstract.abstract).toStrictEqual(expect.not.stringContaining('</abstract>'));
+      expect(abstract).toStrictEqual(expect.not.stringContaining('<abstract>'));
+      expect(abstract).toStrictEqual(expect.not.stringContaining('</abstract>'));
     });
 
     it('removes the first <title> if present', async () => {
-      const doi = new Doi('10.1101/339747');
-      const getXml: GetXml = async () => crossrefResponseWith(`
+      const response = crossrefResponseWith(`
         <abstract>
           <title class="something">Abstract</title>
           Some random nonsense.
         </abstract>`);
-      const abstract = (await createFetchCrossrefArticle(getXml, dummyLogger)(doi)()).unsafelyUnwrap();
+      const doc = parser.parseFromString(response, 'text/xml');
+      const abstract = getAbstract(doc, doi, dummyLogger);
 
-      expect(abstract.abstract).toStrictEqual(expect.not.stringContaining('Abstract'));
+      expect(abstract).toStrictEqual(expect.not.stringContaining('Abstract'));
     });
 
     it('replaces remaining <title>s with HTML <h3>s', async () => {
-      const doi = new Doi('10.1101/339747');
-      const getXml: GetXml = async () => crossrefResponseWith(`
+      const response = crossrefResponseWith(`
         <abstract>
           <title class="something">expected to be removed</title>
           <p>Lorem ipsum</p>
@@ -65,17 +71,17 @@ describe('fetch-crossref-article', (): void => {
           <title class="something">should also be an h3</title>
           </sec>
         </abstract>`);
-      const abstract = (await createFetchCrossrefArticle(getXml, dummyLogger)(doi)()).unsafelyUnwrap();
+      const doc = parser.parseFromString(response, 'text/xml');
+      const abstract = getAbstract(doc, doi, dummyLogger);
 
-      expect(abstract.abstract).toStrictEqual(expect.stringContaining('<h3>should be an h3</h3>'));
-      expect(abstract.abstract).toStrictEqual(expect.stringContaining('<h3>should also be an h3</h3>'));
-      expect(abstract.abstract).toStrictEqual(expect.not.stringContaining('<title>'));
-      expect(abstract.abstract).toStrictEqual(expect.not.stringContaining('</title>'));
+      expect(abstract).toStrictEqual(expect.stringContaining('<h3>should be an h3</h3>'));
+      expect(abstract).toStrictEqual(expect.stringContaining('<h3>should also be an h3</h3>'));
+      expect(abstract).toStrictEqual(expect.not.stringContaining('<title>'));
+      expect(abstract).toStrictEqual(expect.not.stringContaining('</title>'));
     });
 
     it('renders italic if present', async () => {
-      const doi = new Doi('10.1101/339747');
-      const getXml: GetXml = async () => crossrefResponseWith(`
+      const response = crossrefResponseWith(`
         <abstract>
           <title>Abstract</title>
           <p>
@@ -86,15 +92,15 @@ describe('fetch-crossref-article', (): void => {
             efficacy of cannabigerol in a murine systemic infection model caused by MRSA. We also show that cannabinoids are effective against Gram-negative organisms whose outer membrane is permeabilized, where cannabigerol acts on the inner membrane. Finally, we demonstrate that cannabinoids work in combination with polymyxin B against multi-drug resistant Gram-negative pathogens, revealing the broad-spectrum therapeutic potential for cannabinoids.
           </p>
         </abstract>`);
-      const abstract = (await createFetchCrossrefArticle(getXml, dummyLogger)(doi)()).unsafelyUnwrap();
+      const doc = parser.parseFromString(response, 'text/xml');
+      const abstract = getAbstract(doc, doi, dummyLogger);
 
-      expect(abstract.abstract).toStrictEqual(expect.stringContaining('<i>Cannabis sativa</i>'));
-      expect(abstract.abstract).toStrictEqual(expect.stringContaining('<i>in vivo</i>'));
+      expect(abstract).toStrictEqual(expect.stringContaining('<i>Cannabis sativa</i>'));
+      expect(abstract).toStrictEqual(expect.stringContaining('<i>in vivo</i>'));
     });
 
     it('replaces <list> unordered list with HTML <ul>', async () => {
-      const doi = new Doi('10.1101/339747');
-      const getXml: GetXml = async () => crossrefResponseWith(`
+      const response = crossrefResponseWith(`
         <abstract>
           <list class="something" list-type="bullet" id="id-1">
             <list-item class="something">
@@ -105,33 +111,33 @@ describe('fetch-crossref-article', (): void => {
             </list-item>
           </list>
         </abstract>`);
-      const abstract = (await createFetchCrossrefArticle(getXml, dummyLogger)(doi)()).unsafelyUnwrap();
+      const doc = parser.parseFromString(response, 'text/xml');
+      const abstract = getAbstract(doc, doi, dummyLogger);
 
-      expect(abstract.abstract).toStrictEqual(expect.stringContaining('<ul>'));
-      expect(abstract.abstract).toStrictEqual(expect.stringContaining('</ul>'));
-      expect(abstract.abstract).toStrictEqual(expect.stringContaining('<li>'));
-      expect(abstract.abstract).toStrictEqual(expect.stringContaining('</li>'));
+      expect(abstract).toStrictEqual(expect.stringContaining('<ul>'));
+      expect(abstract).toStrictEqual(expect.stringContaining('</ul>'));
+      expect(abstract).toStrictEqual(expect.stringContaining('<li>'));
+      expect(abstract).toStrictEqual(expect.stringContaining('</li>'));
     });
 
-    it('replaces <sec> with HTML <section>', async () => {
-      const doi = new Doi('10.1101/339747');
-      const getXml: GetXml = async () => crossrefResponseWith(`
+    it('replaces <sec> with HTML <section>', () => {
+      const response = crossrefResponseWith(`
         <abstract>
           <sec class="something">
             <p>Lorem ipsum</p>
           </sec>
         </abstract>`);
-      const abstract = (await createFetchCrossrefArticle(getXml, dummyLogger)(doi)()).unsafelyUnwrap();
+      const doc = parser.parseFromString(response, 'text/xml');
+      const abstract = getAbstract(doc, doi, dummyLogger);
 
-      expect(abstract.abstract).toStrictEqual(expect.stringContaining('<section>'));
-      expect(abstract.abstract).toStrictEqual(expect.stringContaining('</section>'));
-      expect(abstract.abstract).toStrictEqual(expect.not.stringContaining('<sec>'));
-      expect(abstract.abstract).toStrictEqual(expect.not.stringContaining('</sec>'));
+      expect(abstract).toStrictEqual(expect.stringContaining('<section>'));
+      expect(abstract).toStrictEqual(expect.stringContaining('</section>'));
+      expect(abstract).toStrictEqual(expect.not.stringContaining('<sec>'));
+      expect(abstract).toStrictEqual(expect.not.stringContaining('</sec>'));
     });
 
-    it('strips <title> named Graphical abstract', async () => {
-      const doi = new Doi('10.1101/339747');
-      const getXml: GetXml = async () => crossrefResponseWith(`
+    it('strips <title> named Graphical abstract', () => {
+      const response = crossrefResponseWith(`
         <abstract>
           <title>First title</title>
           <sec>
@@ -141,14 +147,14 @@ describe('fetch-crossref-article', (): void => {
             </fig>
           </sec>
         </abstract>`);
-      const abstract = (await createFetchCrossrefArticle(getXml, dummyLogger)(doi)()).unsafelyUnwrap();
+      const doc = parser.parseFromString(response, 'text/xml');
+      const abstract = getAbstract(doc, doi, dummyLogger);
 
-      expect(abstract.abstract).toStrictEqual(expect.not.stringContaining('Graphical abstract'));
+      expect(abstract).toStrictEqual(expect.not.stringContaining('Graphical abstract'));
     });
 
-    it('strips <section> elements that are empty or only contain whitespace', async () => {
-      const doi = new Doi('10.1101/339747');
-      const getXml: GetXml = async () => crossrefResponseWith(`
+    it('strips <section> elements that are empty or only contain whitespace', () => {
+      const response = crossrefResponseWith(`
         <abstract>
           <sec>
             <fig id="ufig1" position="float" fig-type="figure" orientation="portrait">
@@ -157,14 +163,14 @@ describe('fetch-crossref-article', (): void => {
           </sec>
         </abstract>`);
 
-      const abstract = (await createFetchCrossrefArticle(getXml, dummyLogger)(doi)()).unsafelyUnwrap();
+      const doc = parser.parseFromString(response, 'text/xml');
+      const abstract = getAbstract(doc, doi, dummyLogger);
 
-      expect(abstract.abstract).toStrictEqual(expect.not.stringContaining('<section>'));
-      expect(abstract.abstract).toStrictEqual(expect.not.stringContaining('</section>'));
+      expect(abstract).toStrictEqual(expect.not.stringContaining('<section>'));
+      expect(abstract).toStrictEqual(expect.not.stringContaining('</section>'));
     });
 
-    it('doesn\'t strip <section> elements that are not empty', async () => {
-      const doi = new Doi('10.1101/339747');
+    it('doesn\'t strip <section> elements that are not empty', () => {
       const response = crossrefResponseWith(`
         <abstract>
           <sec>
@@ -172,7 +178,6 @@ describe('fetch-crossref-article', (): void => {
           </sec>
         </abstract>`);
 
-      const parser = new DOMParser();
       const doc = parser.parseFromString(response, 'text/xml');
       const abstract = getAbstract(doc, doi, dummyLogger);
 
