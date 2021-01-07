0798580aa13479abbcfc846d088a0916ed94e18c
Kevin Rutherford <kevin@rutherford-software.com>
2021-01-07 10:34:59 +0000

    Use the official Twitter logo for the button #366

--- src/article-page/render-tweet-this.ts
+++ src/article-page/render-tweet-this.ts
@@ -7,7 +7,7 @@ export const renderTweetThis = (doi: Doi): HtmlFragment => {
     const tweetText = `Check out this great article I found on @ScietyHQ, where the community can evaluate and curate the latest research: https://sciety.org/articles/${doi.value}?utm_source=twitter&utm_medium=social&utm_campaign=tweet_button`;
     tweetThis = `
     <a class="tweet-button" target="_blank" href="https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}">
-      <img src="/static/images/twitter-logo.svg" alt=""> Tweet this
+      <img class="tweet-button__icon" src="/static/images/twitter-logo.svg" alt=""> Tweet this
     </a>
   `;
   }
