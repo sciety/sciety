19a04e4b3fc6db7f1f563ef88fc67ce5d8087671
David Moulton <d.moulton@elifesciences.org>
2021-01-07 10:21:25 +0000

    Style Tweet this CTA as a button #366

--- src/article-page/render-tweet-this.ts
+++ src/article-page/render-tweet-this.ts
@@ -6,7 +6,7 @@ export const renderTweetThis = (doi: Doi): HtmlFragment => {
   if (process.env.EXPERIMENT_ENABLED === 'true') {
     const tweetText = `Check out this great article I found on @ScietyHQ, where the community can evaluate and curate the latest research: https://sciety.org/articles/${doi.value}?utm_source=twitter&utm_medium=social&utm_campaign=tweet_button`;
     tweetThis = `
-    <a target="_blank" href="https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}">
+    <a class="tweet-button" target="_blank" href="https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}">
       <img src="/static/images/twitter-logo.svg" alt=""> Tweet this
     </a>
   `;
