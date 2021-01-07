4fdc45c1774f77ba164c14944db3598802009ad3
Kevin Rutherford <kevin@rutherford-software.com>
2021-01-07 07:59:21 +0000

    Prepare page-handler to receive TaskEithers from the pages themselves #128

--- src/http/page-handler.ts
+++ src/http/page-handler.ts
@@ -1,11 +1,14 @@
 import { Middleware } from '@koa/router';
+import * as E from 'fp-ts/lib/Either';
 import * as O from 'fp-ts/lib/Option';
 import * as T from 'fp-ts/lib/Task';
+import * as TE from 'fp-ts/lib/TaskEither';
 import { pipe } from 'fp-ts/lib/function';
 import { NOT_FOUND, OK, SERVICE_UNAVAILABLE } from 'http-status-codes';
 import { Result } from 'true-myth';
 import { renderErrorPage } from './render-error-page';
 import { applyStandardPageLayout, Page } from '../shared-components/apply-standard-page-layout';
+import { HtmlFragment } from '../types/html-fragment';
 import { RenderPageError } from '../types/render-page-error';
 import { User } from '../types/user';
 
@@ -20,38 +23,28 @@ type RenderPage = (params: {
   user: O.Option<User>;
 }) => T.Task<RenderedResult>;
 
-const successToStatusCode = (): number => OK;
+type InterimResult = {
+  title: string;
+  content: HtmlFragment;
+  openGraph?: {
+    title: string;
+    description: string;
+  };
+  status: number;
+};
 
-const errorTypeToStatusCode = ({ type }: RenderPageError): number => (
-  type === 'not-found' ? NOT_FOUND : SERVICE_UNAVAILABLE
-);
-
-type FoldToPage = (
-  pageResult: RenderedResult,
-) => Page;
-
-const foldToPage: FoldToPage = (pageResult) => (
-  pageResult.unwrapOrElse((error) => ({
-    title: 'Error',
-    content: renderErrorPage(error.message),
-  }))
-);
-
-const addScietySuffixIfNotHomepage = (requestPath: string) => (page: Page): Page => ({
+const addScietySuffixIfNotHomepage = (requestPath: string) => (page: InterimResult): InterimResult => ({
   ...page,
   title: requestPath === '/' ? page.title : `${page.title} | Sciety`,
 });
 
-const createResponse = (user: O.Option<User>, requestPath: string) => (rendered: RenderedResult) => ({
-  type: 'html',
-  body: pipe(
-    rendered,
-    foldToPage,
-    addScietySuffixIfNotHomepage(requestPath),
-    applyStandardPageLayout(user),
-  ),
-  status: rendered.map(successToStatusCode).unwrapOrElse(errorTypeToStatusCode),
-});
+const toTaskEither = (rendered: T.Task<RenderedResult>): TE.TaskEither<RenderPageError, Page> => pipe(
+  rendered,
+  T.map((result) => result
+    .map((page) => E.right<RenderPageError, Page>(page))
+    .unwrapOrElse((error) => E.left<RenderPageError, Page>(error))),
+  T.chain(TE.fromEither),
+);
 
 export default (
   renderPage: RenderPage,
@@ -68,9 +61,26 @@ export default (
     const response = await pipe(
       params,
       renderPage,
-      T.map(createResponse(user, context.request.path)),
+      toTaskEither,
+      TE.fold(
+        (error) => T.of({
+          title: 'Error',
+          content: renderErrorPage(error.message),
+          status: error.type === 'not-found' ? NOT_FOUND : SERVICE_UNAVAILABLE,
+        }),
+        (page) => T.of({
+          ...page,
+          status: OK,
+        }),
+      ),
+      T.map(addScietySuffixIfNotHomepage(context.request.path)),
+      T.map((obj) => ({
+        body: applyStandardPageLayout(user)(obj),
+        status: obj.status,
+      })),
     )();
 
+    context.response.type = 'html';
     Object.assign(context.response, response);
 
     await next();
