0ffe332cf13175534df9cad7d9339225a315893c
David Moulton <d.moulton@elifesciences.org>
2021-01-07 11:54:11 +0000

    Refactor to make render-tweet-this more functional #366

--- src/article-page/render-tweet-this.ts
+++ src/article-page/render-tweet-this.ts
@@ -1,11 +1,27 @@
+import { flow } from 'fp-ts/lib/function';
 import Doi from '../types/doi';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
-export const renderTweetThis = (doi: Doi): HtmlFragment => {
-  const tweetText = `Check out this great article I found on @ScietyHQ, where the community can evaluate and curate the latest research: https://sciety.org/articles/${doi.value}?utm_source=twitter&utm_medium=social&utm_campaign=tweet_button`;
-  return toHtmlFragment(`
-    <a class="tweet-button" target="_blank" href="https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}">
-      <img class="tweet-button__icon" src="/static/images/twitter-logo.svg" alt=""> Tweet this
-    </a>
-  `);
-};
+const constructLink = (doi: Doi): string => `
+  https://sciety.org/articles/${doi.value}?utm_source=twitter&utm_medium=social&utm_campaign=tweet_button
+`;
+
+const constructTweetText = (link: string): string => `
+  Check out this great article I found on @ScietyHQ, where the community can evaluate and curate the latest research: ${link}
+`;
+
+const constructAnchor = (encodedTweetText: string): string => `
+  <a class="tweet-button" target="_blank" href="https://twitter.com/intent/tweet?text=${encodedTweetText}">
+    <img class="tweet-button__icon" src="/static/images/twitter-logo.svg" alt=""> Tweet this
+  </a>
+`;
+
+type RenderTweetThis = (doi: Doi) => HtmlFragment;
+
+export const renderTweetThis: RenderTweetThis = flow(
+  constructLink,
+  constructTweetText,
+  encodeURIComponent,
+  constructAnchor,
+  toHtmlFragment,
+);
