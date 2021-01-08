a728e7ecdcb33fa58f5c1b6a97759e1b6c653faa
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-08 10:54:26 +0000

    prepare search-europe-pmc to return a TaskEither instead of throwing #359

--- src/infrastructure/search-europe-pmc.ts
+++ src/infrastructure/search-europe-pmc.ts
@@ -1,5 +1,7 @@
 import { URLSearchParams } from 'url';
 import * as T from 'fp-ts/lib/Task';
+import * as TE from 'fp-ts/lib/TaskEither';
+import { pipe } from 'fp-ts/lib/pipeable';
 import { Logger } from './logger';
 import Doi from '../types/doi';
 import { Json, JsonCompatible } from '../types/json';
@@ -13,10 +15,12 @@ interface SearchResult {
   postedDate: Date;
 }
 
-export type SearchEuropePmc = (query: string) => T.Task<{
+type SearchResults = {
   items: Array<SearchResult>;
   total: number;
-}>;
+};
+
+export type SearchEuropePmc = (query: string) => T.Task<SearchResults>;
 
 type EuropePmcQueryResponse = JsonCompatible<{
   hitCount: number;
@@ -30,27 +34,32 @@ type EuropePmcQueryResponse = JsonCompatible<{
   };
 }>;
 
-export default (getJson: GetJson, logger: Logger): SearchEuropePmc => (
-  (query) => async () => {
-    const queryString = new URLSearchParams({
-      query: `${query} PUBLISHER:"bioRxiv" sort_date:y`,
-      format: 'json',
-      pageSize: '10',
-    });
+const constructQueryParams = (query: string): URLSearchParams => (
+  new URLSearchParams({
+    query: `${query} PUBLISHER:"bioRxiv" sort_date:y`,
+    format: 'json',
+    pageSize: '10',
+  }));
 
-    const uri = `https://www.ebi.ac.uk/europepmc/webservices/rest/search?${queryString.toString()}`;
-    let data: EuropePmcQueryResponse;
+const constructSearchUrl = (queryParams: URLSearchParams): string => `https://www.ebi.ac.uk/europepmc/webservices/rest/search?${queryParams.toString()}`;
 
-    try {
-      data = await getJson(uri) as EuropePmcQueryResponse;
-    } catch (error: unknown) {
-      logger('error', 'Failed to search Europe PMC', { query, uri, error });
+const logError = (logger: Logger) => (error: unknown): 'unavailable' => {
+  logger('error', 'Failed to search Europe PMC', { error });
+  return 'unavailable';
+};
 
-      throw error;
-    }
+const search = async (getJson: GetJson, query: string): Promise<Json> => (
+  pipe(
+    query,
+    constructQueryParams,
+    constructSearchUrl,
+    getJson,
+  )
+);
 
-    logger('debug', 'Received Europe PMC search results', { data });
-
+const constructSearchResults = (json: Json): TE.TaskEither<'unavailable', SearchResults> => {
+  try {
+    const data = json as EuropePmcQueryResponse;
     const items = data.resultList.result.map((item): SearchResult => ({
       doi: new Doi(item.doi),
       title: item.title,
@@ -58,9 +67,24 @@ export default (getJson: GetJson, logger: Logger): SearchEuropePmc => (
       postedDate: new Date(item.firstPublicationDate),
     }));
 
-    return {
+    return TE.right({
       items,
       total: data.hitCount,
-    };
+    });
+  } catch (error: unknown) {
+    return TE.left('unavailable');
   }
+};
+
+export default (getJson: GetJson, logger: Logger): SearchEuropePmc => (
+  (query) => (
+    pipe(
+      TE.tryCatch(async () => search(getJson, query), logError(logger)),
+      TE.chain(constructSearchResults),
+      TE.fold(
+        (error) => { throw new Error(error); },
+        (searchResults) => T.of(searchResults),
+      ),
+    )
+  )
 );
