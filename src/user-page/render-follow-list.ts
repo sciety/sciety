4e688e1eff04f6bb6fc4c619be71b3c93d9a193a
Kevin Rutherford <kevin@rutherford-software.com>
2021-01-11 13:10:04 +0000

    Use a pipe to render the user follow list #128

--- src/user-page/render-follow-list.ts
+++ src/user-page/render-follow-list.ts
@@ -1,6 +1,7 @@
 import { URL } from 'url';
 import * as O from 'fp-ts/lib/Option';
 import * as T from 'fp-ts/lib/Task';
+import { pipe } from 'fp-ts/lib/function';
 import { Result } from 'true-myth';
 import { RenderFollowedEditorialCommunity } from './render-followed-editorial-community';
 import templateListItems from '../shared-components/list-items';
@@ -16,6 +17,30 @@ export type GetFollowedEditorialCommunities = (userId: UserId) => T.Task<Readonl
   avatar: URL,
 }>>;
 
+const followingNothing = `
+  <div class="ui info message">
+    <div class="header">
+      They’re not following anything
+    </div>
+    <p>When they do, they’ll be listed here.</p>
+  </div>
+`;
+
+const followListSection = (list: string): string => `
+  <section>
+    <h2>
+      Following
+    </h2>
+    ${list}
+  </section>
+`;
+
+const renderList = (list: Array<HtmlFragment>): string => ((list.length === 0) ? followingNothing : `
+  <ol class="ui large feed" role="list">
+    ${templateListItems(list, 'event')}
+  </ol>
+`);
+
 export default (
   getFollowedEditorialCommunities: GetFollowedEditorialCommunities,
   renderFollowedEditorialCommunity: RenderFollowedEditorialCommunity,
@@ -24,30 +49,13 @@ export default (
     const list = await Promise.all((await getFollowedEditorialCommunities(userId)())
       .map(async (editorialCommunity) => renderFollowedEditorialCommunity(editorialCommunity, viewingUserId)()));
 
-    let renderedFollowList: HtmlFragment;
-    if (list.length > 0) {
-      renderedFollowList = toHtmlFragment(`
-        <ol class="ui large feed" role="list">
-          ${templateListItems(list, 'event')}
-        </ol>
-      `);
-    } else {
-      renderedFollowList = toHtmlFragment(`
-        <div class="ui info message">
-          <div class="header">
-            They’re not following anything
-          </div>
-          <p>When they do, they’ll be listed here.</p>
-        </div>
-      `);
-    }
-    return Result.ok(toHtmlFragment(`
-      <section>
-        <h2>
-          Following
-        </h2>
-        ${renderedFollowList}
-      </section>
-    `));
+    return Result.ok(
+      pipe(
+        list,
+        renderList,
+        followListSection,
+        toHtmlFragment,
+      ),
+    );
   }
 );
