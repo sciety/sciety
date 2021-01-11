f32a0fb6710f8a4285cfb5c54bec3b767f73c243
Kevin Rutherford <kevin@rutherford-software.com>
2021-01-11 15:48:47 +0000

    Separate rendering from hard-coded data in render-saved-articles #369

--- src/user-page/render-saved-articles.ts
+++ src/user-page/render-saved-articles.ts
@@ -1,21 +1,43 @@
 import * as TE from 'fp-ts/lib/TaskEither';
+import templateListItems from '../shared-components/list-items';
+import Doi from '../types/doi';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { UserId } from '../types/user-id';
+
+type SavedArticle = {
+  doi: Doi,
+  title: HtmlFragment,
+};
+
+const savedArticles = (userId: UserId): ReadonlyArray<SavedArticle> => {
+  if (userId === '1295307136415735808') {
+    return [
+      {
+        doi: new Doi('10.1101/2020.07.04.187583'),
+        title: toHtmlFragment('Gender, race and parenthood impact academic productivity during the COVID-19 pandemic: from survey to action'),
+      },
+      {
+        doi: new Doi('10.1101/2020.09.09.289785'),
+        title: toHtmlFragment('The Costs and Benefits of a Modified Biomedical Science Workforce'),
+      },
+    ];
+  }
+  return [];
+};
 
 export const renderSavedArticles = (userId: UserId): TE.TaskEither<never, HtmlFragment> => {
   let savedArticlesList = toHtmlFragment('');
 
-  if (userId === '1295307136415735808') {
+  const items = savedArticles(userId).map((item) => toHtmlFragment(`
+    <a href="/articles/${item.doi.value}" class="saved-articles__link">${item.title}</a>
+  `));
+  if (items.length > 0) {
+    const list = templateListItems(items, 'saved-articles__item');
     savedArticlesList = toHtmlFragment(`
       <section>
         <h2>Saved articles</h2>
         <ol class="saved-articles">
-          <li class="saved-articles__item">
-            <a href="/articles/10.1101/2020.07.04.187583" class="saved-articles__link">Gender, race and parenthood impact academic productivity during the COVID-19 pandemic: from survey to action</a>
-          </li>
-          <li class="saved-articles__item">
-            <a href="/articles/10.1101/2020.09.09.289785" class="saved-articles__link">The Costs and Benefits of a Modified Biomedical Science Workforce</a>
-          </li>
+          ${list}
         </ol>
       </section>
     `);
