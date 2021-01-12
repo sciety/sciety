3f2ccf2e2a8e46ccdc28a72c9c06834f96ff1ea2
Giorgio Sironi <g.sironi@elifesciences.org>
2021-01-12 10:04:08 +0000

    Add characterization test for hardcoded-get-saved-articles #369

---
+++ test/user-page/hardcoded-get-saved-articles.test.ts
@@ -0,0 +1,25 @@
+import Doi from '../../src/types/doi';
+import { toHtmlFragment } from '../../src/types/html-fragment';
+import toUserId from '../../src/types/user-id';
+import { getSavedArticles } from '../../src/user-page/hardcoded-get-saved-articles';
+
+describe('hardcoded-get-saved-articles', () => {
+  describe('when the user has saved articles', () => {
+    it('returns doi and title for those articles', async () => {
+      const savedArticles = getSavedArticles(toUserId('1295307136415735808'));
+
+      expect(savedArticles[0]).toMatchObject({
+        doi: new Doi('10.1101/2020.07.04.187583'),
+        title: toHtmlFragment('Gender, race and parenthood impact academic productivity during the COVID-19 pandemic: from survey to action'),
+      });
+    });
+  });
+
+  describe('when the user has no saved articles', () => {
+    it('returns an empty array', async () => {
+      const savedArticles = getSavedArticles(toUserId('anything-else'));
+
+      expect(savedArticles).toHaveLength(0);
+    });
+  });
+});
