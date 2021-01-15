109f97a685bb1c7b965bf729f32b4cb180b52a41
Will Byrne <w.byrne@elifesciences.org>
2021-01-15 14:23:54 +0000

    Move harcoding out of render function #370

--- src/article-page/render-saved-link.ts
+++ src/article-page/render-saved-link.ts
@@ -12,17 +12,18 @@ const templateSavedLink = (userId: UserId): string => `
   </a>
 `;
 
-export const renderSavedLink: RenderSavedLink = (doi, userId) => {
+type HasUserSavedArticle = (doi: Doi, userId: UserId) => boolean;
+const hasUserSavedArticle: HasUserSavedArticle = (doi, userId) => {
   const savedDois = ['10.1101/2020.07.04.187583', '10.1101/2020.09.09.289785'];
-
-  return pipe(
-    userId,
-    O.filter((u) => u === '1295307136415735808'),
-    O.filter(() => savedDois.includes(doi.value)),
-    O.map(templateSavedLink),
-    O.fold(
-      constant(''),
-      identity,
-    ),
-  );
+  return userId === '1295307136415735808' && savedDois.includes(doi.value);
 };
+
+export const renderSavedLink: RenderSavedLink = (doi, userId) => pipe(
+  userId,
+  O.filter((u) => hasUserSavedArticle(doi, u)),
+  O.map(templateSavedLink),
+  O.fold(
+    constant(''),
+    identity,
+  ),
+);
