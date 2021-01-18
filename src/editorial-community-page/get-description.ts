eb5c3fc8d4c2e5a3df64095ab1740f927f0ff06c
Kevin Rutherford <kevin@rutherford-software.com>
2021-01-18 08:16:52 +0000

    Remove a Promise #128

--- src/editorial-community-page/get-description.ts
+++ src/editorial-community-page/get-description.ts
@@ -1,13 +1,16 @@
 import * as T from 'fp-ts/lib/Task';
+import { pipe } from 'fp-ts/lib/function';
 import { Remarkable } from 'remarkable';
 import { EditorialCommunity } from '../types/editorial-community';
 
 type FetchStaticFile = (filename: string) => T.Task<string>;
 
+const convertMarkdownToHtml = (md: string): string => new Remarkable({ html: true }).render(md);
+
 type GetDescription = (fetchStaticFile: FetchStaticFile) => (community: EditorialCommunity) => T.Task<string>;
 
-export const getDescription: GetDescription = (fetchStaticFile) => (editorialCommunity) => async () => {
-  const converter = new Remarkable({ html: true });
-  const markdown = await fetchStaticFile(`editorial-communities/${editorialCommunity.descriptionPath}`)();
-  return converter.render(markdown);
-};
+export const getDescription: GetDescription = (fetchStaticFile) => (editorialCommunity) => pipe(
+  `editorial-communities/${editorialCommunity.descriptionPath}`,
+  fetchStaticFile,
+  T.map(convertMarkdownToHtml),
+);
