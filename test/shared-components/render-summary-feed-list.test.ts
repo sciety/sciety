48dc518cd5ec63d4ff0d81f19589068abe0d8c68
Kevin Rutherford <kevin@rutherford-software.com>
2021-01-18 12:56:04 +0000

    Use pipes to clarify a test #128

--- test/shared-components/render-summary-feed-list.test.ts
+++ test/shared-components/render-summary-feed-list.test.ts
@@ -1,5 +1,6 @@
 import * as O from 'fp-ts/lib/Option';
 import * as T from 'fp-ts/lib/Task';
+import { pipe } from 'fp-ts/lib/function';
 import renderSummaryFeedList from '../../src/shared-components/render-summary-feed-list';
 import { toHtmlFragment } from '../../src/types/html-fragment';
 import shouldNotBeCalled from '../should-not-be-called';
@@ -7,10 +8,10 @@ import shouldNotBeCalled from '../should-not-be-called';
 describe('render-summary-feed-list', () => {
   describe('when there are events', () => {
     it('returns a list', async () => {
-      const events = [
-        {}, {}, {},
-      ];
-      const rendered = await renderSummaryFeedList(() => T.of(toHtmlFragment('')))(events)();
+      const rendered = await pipe(
+        [ {}, {}, {}, ],
+        renderSummaryFeedList(() => T.of(toHtmlFragment(''))),
+      )();
 
       expect(rendered).toStrictEqual(O.some(expect.stringContaining('<ol class="summary-feed-list"')));
     });
@@ -18,7 +19,10 @@ describe('render-summary-feed-list', () => {
 
   describe('when there are no events', () => {
     it('returns nothing', async () => {
-      const rendered = await renderSummaryFeedList(shouldNotBeCalled)([])();
+      const rendered = await pipe(
+        [],
+        renderSummaryFeedList(shouldNotBeCalled),
+      )();
 
       expect(rendered).toBe(O.none);
     });
