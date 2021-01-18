e995d048da39cae8614e35f0e7433cb934bcdb6d
David Moulton <d.moulton@elifesciences.org>
2021-01-18 09:26:49 +0000

    Add todo tests for project-has-user-saved-article #370

---
+++ test/article-page/project-has-user-saved-article.test.ts
@@ -0,0 +1,17 @@
+describe('project-has-user-saved-article', () => {
+  describe('when the user has saved the article', () => {
+    it.todo('returns true');
+  });
+
+  describe('when the user has not saved the article', () => {
+    it.todo('returns false');
+  });
+
+  describe('when the user has saved another article', () => {
+    it.todo('returns false');
+  });
+
+  describe('when another user has saved this article', () => {
+    it.todo('returns false');
+  });
+});
