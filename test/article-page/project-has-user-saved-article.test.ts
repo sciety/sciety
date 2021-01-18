e416ef375a525fb229823a0b2155505ff30a8bd5
Giorgio Sironi <g.sironi@elifesciences.org>
2021-01-18 09:34:06 +0000

    Implement tests for current project-has-user-saved-article #370

--- test/article-page/project-has-user-saved-article.test.ts
+++ test/article-page/project-has-user-saved-article.test.ts
@@ -1,17 +1,49 @@
+import { projectHasUserSavedArticle } from '../../src/article-page/project-has-user-saved-article';
+import Doi from '../../src/types/doi';
+import toUserId from '../../src/types/user-id';
+
 describe('project-has-user-saved-article', () => {
   describe('when the user has saved the article', () => {
-    it.todo('returns true');
+    it('returns true', async () => {
+      const result = await projectHasUserSavedArticle(
+        new Doi('10.1101/2020.07.04.187583'),
+        toUserId('1295307136415735808'),
+      )();
+
+      expect(result).toBe(true);
+    });
   });
 
   describe('when the user has not saved the article', () => {
-    it.todo('returns false');
+    it('returns false', async () => {
+      const result = await projectHasUserSavedArticle(
+        new Doi('10.1101/some-doi'),
+        toUserId('the-user'),
+      )();
+
+      expect(result).toBe(false);
+    });
   });
 
   describe('when the user has saved another article', () => {
-    it.todo('returns false');
+    it('returns false', async () => {
+      const result = await projectHasUserSavedArticle(
+        new Doi('10.1101/some-doi'),
+        toUserId('1295307136415735808'),
+      )();
+
+      expect(result).toBe(false);
+    });
   });
 
   describe('when another user has saved this article', () => {
-    it.todo('returns false');
+    it('returns false', async () => {
+      const result = await projectHasUserSavedArticle(
+        new Doi('10.1101/2020.07.04.187583'),
+        toUserId('the-user'),
+      )();
+
+      expect(result).toBe(false);
+    });
   });
 });
