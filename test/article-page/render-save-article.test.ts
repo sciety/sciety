e5fdd283cd3b32085cd3b2e9f12be4f388019b8c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-19 14:51:49 +0000

    test not logged case for render-save-article #371

--- test/article-page/render-save-article.test.ts
+++ test/article-page/render-save-article.test.ts
@@ -1,6 +1,19 @@
+import * as O from 'fp-ts/lib/Option';
+import * as T from 'fp-ts/lib/Task';
+import { constant } from 'fp-ts/lib/function';
+import { renderSaveArticle } from '../../src/article-page/render-save-article';
+import Doi from '../../src/types/doi';
+
 describe('render-save-article', () => {
   describe('not logged in', () => {
-    it.todo('renders nothing');
+    it('renders nothing', async () => {
+      const projection = jest.fn().mockImplementation(constant(T.of(false)));
+      const render = renderSaveArticle(projection);
+      const rendered = await render(new Doi('10.1111/foobar'), O.none)();
+
+      expect(rendered).toStrictEqual('');
+      expect(projection).not.toHaveBeenCalled();
+    });
   });
 
   describe('logged in and article is saved', () => {
