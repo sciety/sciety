0e256f6552a7393d6dcd74e5fa2c20dd14504da3
Will Byrne <w.byrne@elifesciences.org>
2021-01-19 15:15:53 +0000

    Test logged in and logged in but unsaved cases #371

--- test/article-page/render-save-article.test.ts
+++ test/article-page/render-save-article.test.ts
@@ -3,6 +3,7 @@ import * as T from 'fp-ts/lib/Task';
 import { constant } from 'fp-ts/lib/function';
 import { renderSaveArticle } from '../../src/article-page/render-save-article';
 import Doi from '../../src/types/doi';
+import toUserId from '../../src/types/user-id';
 
 describe('render-save-article', () => {
   describe('not logged in', () => {
@@ -17,10 +18,22 @@ describe('render-save-article', () => {
   });
 
   describe('logged in and article is saved', () => {
-    it.todo('renders is-saved-link');
+    it('renders is-saved-link', async () => {
+      const projection = constant(T.of(true));
+      const render = renderSaveArticle(projection);
+      const rendered = await render(new Doi('10.1111/foobar'), O.some(toUserId('user')))();
+
+      expect(rendered).toContain('Saved to my list');
+    });
   });
 
   describe('logged in and article is not saved', () => {
-    it.todo('renders save-to-your-list-form');
+    it('renders save-to-your-list-form', async () => {
+      const projection = constant(T.of(false));
+      const render = renderSaveArticle(projection);
+      const rendered = await render(new Doi('10.1111/foobar'), O.some(toUserId('user')))();
+
+      expect(rendered).toContain('Save to my list');
+    });
   });
 });
