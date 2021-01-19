f29e9e2b4b2ca38024a56a1bc202013394203f90
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-19 14:42:12 +0000

    test list for render-save-article #371

---
+++ test/article-page/render-save-article.test.ts
@@ -0,0 +1,13 @@
+describe('render-save-article', () => {
+  describe('not logged in', () => {
+    it.todo('renders nothing');
+  });
+
+  describe('logged in and article is saved', () => {
+    it.todo('renders is-saved-link');
+  });
+
+  describe('logged in and article is not saved', () => {
+    it.todo('renders save-to-your-list-form');
+  });
+});
