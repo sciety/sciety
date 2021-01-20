7c9da639f2e6fce5f206201437cd9574db1b9905
Giorgio Sironi <g.sironi@elifesciences.org>
2021-01-20 12:12:56 +0000

    Do not save the article if already saved #371

--- src/save-article/index.ts
+++ src/save-article/index.ts
@@ -1,18 +1,34 @@
 import { Middleware } from '@koa/router';
+import * as RA from 'fp-ts/lib/ReadonlyArray';
+import * as T from 'fp-ts/lib/Task';
+import * as B from 'fp-ts/lib/boolean';
+import { constant, pipe } from 'fp-ts/lib/function';
 import Doi from '../types/doi';
-import { userSavedArticle, UserSavedArticleEvent } from '../types/domain-events';
+import {
+  DomainEvent, isUserSavedArticleEvent, userSavedArticle, UserSavedArticleEvent,
+} from '../types/domain-events';
+
 import { User } from '../types/user';
 
 type Ports = {
+  getAllEvents: T.Task<ReadonlyArray<DomainEvent>>;
   commitEvents: (events: ReadonlyArray<UserSavedArticleEvent>) => Promise<void>,
 };
 
 export const saveArticleHandler = (ports: Ports): Middleware<{ user: User }> => async (context, next) => {
   const user = context.state.user as User;
   const articleId = new Doi(context.request.body.articleid);
-  await ports.commitEvents([
-    userSavedArticle(user.id, articleId),
-  ]);
+  await pipe(
+    ports.getAllEvents,
+    T.map(RA.some((event) => (
+      isUserSavedArticleEvent(event) && event.userId === user.id && event.articleId.value === articleId.value))),
+    T.map(B.fold(
+      () => [userSavedArticle(user.id, articleId)],
+      constant([]),
+    )),
+    T.chain((events) => async () => ports.commitEvents(events)),
+  )();
+
   context.redirect('back');
 
   await next();
--- test/save-article/index.test.ts
+++ test/save-article/index.test.ts
@@ -1,6 +1,8 @@
 import { RouterContext } from '@koa/router';
+import * as T from 'fp-ts/lib/Task';
 import { saveArticleHandler } from '../../src/save-article';
 import Doi from '../../src/types/doi';
+import { userSavedArticle } from '../../src/types/domain-events';
 import { User } from '../../src/types/user';
 import toUserId from '../../src/types/user-id';
 
@@ -23,9 +25,10 @@ describe('save-article', () => {
         },
       } as unknown) as RouterContext<{ user: User }>;
 
+      const getAllEvents = T.of([]);
       const commitEvents = jest.fn();
 
-      await saveArticleHandler({ commitEvents })(context, jest.fn());
+      await saveArticleHandler({ getAllEvents, commitEvents })(context, jest.fn());
 
       expect(commitEvents).toHaveBeenCalledWith([expect.objectContaining({
         type: 'UserSavedArticle',
@@ -36,7 +39,32 @@ describe('save-article', () => {
   });
 
   describe('when the user has already saved the article', () => {
-    it.todo('does nothing');
+    it('does nothing', async () => {
+      const userId = toUserId('user-id');
+      const articleId = new Doi('10.1234/5678');
+      const context = ({
+        request: {
+          body: {
+            articleid: articleId.value,
+          },
+        },
+        redirect: jest.fn(),
+        state: {
+          user: {
+            id: userId,
+          },
+        },
+      } as unknown) as RouterContext<{ user: User }>;
+
+      const getAllEvents = T.of([
+        userSavedArticle(userId, articleId),
+      ]);
+      const commitEvents = jest.fn();
+
+      await saveArticleHandler({ getAllEvents, commitEvents })(context, jest.fn());
+
+      expect(commitEvents).toHaveBeenCalledWith([]);
+    });
   });
 
   it('redirects back', async () => {
@@ -54,7 +82,8 @@ describe('save-article', () => {
       },
     } as unknown) as RouterContext<{ user: User }>;
 
-    await saveArticleHandler({ commitEvents: jest.fn() })(context, jest.fn());
+    const getAllEvents = T.of([]);
+    await saveArticleHandler({ getAllEvents, commitEvents: jest.fn() })(context, jest.fn());
 
     expect(context.redirect).toHaveBeenCalledWith('back');
   });
