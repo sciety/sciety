f3515fc9c6e56db3280ee562998527605421368e
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-01-20 11:27:26 +0000

    Sketch out tests for the save-article handler #371

---
+++ test/save-article/index.test.ts
@@ -0,0 +1,11 @@
+describe('save-article', () => {
+  describe('when the user has not already saved the article', () => {
+    it.todo('commits a UserSavedArticle event');
+  });
+
+  describe('when the user has already saved the article', () => {
+    it.todo('does nothing');
+  });
+
+  it.todo('redirects back');
+});
