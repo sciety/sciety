cb11dbb0dfee32749c2d9a279ea4af3d3e93d3b3
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-01-21 14:43:01 +0000

    Port tests to finishSaveArticleCommand #372

--- test/save-article/finish-save-article-command.test.ts
+++ test/save-article/finish-save-article-command.test.ts
@@ -1,10 +1,69 @@
+import { RouterContext } from '@koa/router';
 import * as T from 'fp-ts/lib/Task';
 import { ParameterizedContext } from 'koa';
 import { finishSaveArticleCommand } from '../../src/save-article/finish-save-article-command';
 import { Doi } from '../../src/types/doi';
+import { userSavedArticle } from '../../src/types/domain-events';
+import { User } from '../../src/types/user';
 import { toUserId } from '../../src/types/user-id';
 
 describe('finish-save-article-command', () => {
+  describe('when the user has not already saved the article', () => {
+    it('commits a UserSavedArticle event', async () => {
+      const userId = toUserId('user-id');
+      const articleId = new Doi('10.1234/5678');
+      const context = ({
+        session: {
+          command: 'save-article',
+          articleId: articleId.value,
+        },
+        state: {
+          user: {
+            id: userId,
+          },
+        },
+      } as unknown) as RouterContext<{ user: User }>;
+
+      const getAllEvents = T.of([]);
+      const commitEvents = jest.fn().mockImplementation(() => T.of(undefined));
+
+      await finishSaveArticleCommand({ getAllEvents, commitEvents })(context, jest.fn());
+
+      expect(commitEvents).toHaveBeenCalledWith([expect.objectContaining({
+        type: 'UserSavedArticle',
+        userId,
+        articleId,
+      })]);
+    });
+  });
+
+  describe('when the user has already saved the article', () => {
+    it('does not commit any events', async () => {
+      const userId = toUserId('user-id');
+      const articleId = new Doi('10.1234/5678');
+      const context = ({
+        session: {
+          command: 'save-article',
+          articleId: articleId.value,
+        },
+        state: {
+          user: {
+            id: userId,
+          },
+        },
+      } as unknown) as RouterContext<{ user: User }>;
+
+      const getAllEvents = T.of([
+        userSavedArticle(userId, articleId),
+      ]);
+      const commitEvents = jest.fn().mockImplementation(() => T.of(undefined));
+
+      await finishSaveArticleCommand({ getAllEvents, commitEvents })(context, jest.fn());
+
+      expect(commitEvents).toHaveBeenCalledWith([]);
+    });
+  });
+
   describe('after saving', () => {
     it('deletes session parameters', async () => {
       const userId = toUserId('user-id');
