6652bf5a82daa3484ee413c0182426c5327fcc7e
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-22 11:15:45 +0000

    fix describes and it for respond feature-test #372

--- feature-test/respond.test.ts
+++ feature-test/respond.test.ts
@@ -5,20 +5,22 @@ import {
 
 jest.setTimeout(15000);
 
-describe('login', () => {
+describe('respond', () => {
   afterAll(closeBrowser);
 
-  it('authenticates via Twitter', async () => {
-    dotenv.config();
-    await openBrowser();
-    await goto('localhost:8080/articles/10.1101/2020.07.13.199174');
-    await click('Got it!');
-    await click($('.article-feed__item:first-child button[value="respond-helpful"]'));
-    await write(process.env.TAIKO_TWITTER_USERNAME ?? '', into(textBox('Username')));
-    await write(process.env.TAIKO_TWITTER_PASSWORD ?? '', into(textBox('Password')));
-    await click('Sign in');
-    const result = await text('1', toRightOf($('.article-feed__item:first-child button[value="revoke-response"]'))).exists();
+  describe('when not logged in', () => {
+    it('displays increased response count', async () => {
+      dotenv.config();
+      await openBrowser();
+      await goto('localhost:8080/articles/10.1101/2020.07.13.199174');
+      await click('Got it!');
+      await click($('.article-feed__item:first-child button[value="respond-helpful"]'));
+      await write(process.env.TAIKO_TWITTER_USERNAME ?? '', into(textBox('Username')));
+      await write(process.env.TAIKO_TWITTER_PASSWORD ?? '', into(textBox('Password')));
+      await click('Sign in');
+      const result = await text('1', toRightOf($('.article-feed__item:first-child button[value="revoke-response"]'))).exists();
 
-    expect(result).toBe(true);
+      expect(result).toBe(true);
+    });
   });
 });
