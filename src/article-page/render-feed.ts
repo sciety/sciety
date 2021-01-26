5ea89a589b4ab2a9e9949ed5449461f1dfcb6e7e
Kevin Rutherford <kevin@rutherford-software.com>
2021-01-26 10:52:30 +0000

    SHow a medrxiv error message for one article #384

--- src/article-page/render-feed.ts
+++ src/article-page/render-feed.ts
@@ -21,11 +21,26 @@ export const createRenderFeed = (
 ): RenderFeed => {
   const renderArticleVersionErrorFeedItem = createRenderArticleVersionErrorFeedItem();
 
-  const renderFeedItem = async (feedItem: FeedItem, userId: O.Option<UserId>): Promise<HtmlFragment> => {
+  const renderFeedItem = async (feedItem: FeedItem, userId: O.Option<UserId>, doi: Doi): Promise<HtmlFragment> => {
     switch (feedItem.type) {
       case 'article-version':
         return renderArticleVersionFeedItem(feedItem);
       case 'article-version-error':
+        if (doi.value === '10.1101/2020.09.03.20184051') {
+          return toHtmlFragment(`
+            <div class="article-feed__item_contents">
+              <img class="article-feed__item__avatar" src="https://pbs.twimg.com/profile_images/956565401588002816/0rESoCS0_200x200.jpg" alt="">
+              <div>
+                <p class="article-feed__item__title">
+                  Published on medRxiv
+                </p>
+                <p>
+                  We couldn't get version information from medRxiv. Please try refreshing this page.
+                </p>
+              </div>
+            </div>
+          `);
+        }
         return renderArticleVersionErrorFeedItem();
       case 'review':
         return renderReviewFeedItem(feedItem, userId)();
@@ -40,7 +55,7 @@ export const createRenderFeed = (
     }
 
     const items = await Promise.all(feedItems.map(
-      async (feedItem) => renderFeedItem(feedItem, userId),
+      async (feedItem) => renderFeedItem(feedItem, userId, doi),
     ));
 
     return Result.ok(toHtmlFragment(`
