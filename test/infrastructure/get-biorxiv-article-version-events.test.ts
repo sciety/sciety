0023dff4deb24ba069318695d7b96db715b4280f
Giorgio Sironi <g.sironi@elifesciences.org>
2021-01-27 10:30:02 +0000

    Add unit test for medrxiv article version events fetching #384

--- test/infrastructure/get-biorxiv-article-version-events.test.ts
+++ test/infrastructure/get-biorxiv-article-version-events.test.ts
@@ -5,35 +5,73 @@ import dummyLogger from '../dummy-logger';
 
 describe('get-biorxiv-article-version-events', () => {
   describe('when biorxiv is available', () => {
-    it('returns an article-version event for each article version', async () => {
-      const doi = new Doi('10.1101/2020.09.02.278911');
-      const getJson: GetJson = async () => ({
-        collection: [
-          {
-            date: '2020-01-02',
-            version: '2',
-          },
-          {
-            date: '2019-12-31',
-            version: '1',
-          },
-        ],
+    describe('when the server is biorxiv', () => {
+      it('returns an article-version event for each article version', async () => {
+        const doi = new Doi('10.1101/2020.09.02.278911');
+        const getJson: GetJson = jest.fn(async () => ({
+          collection: [
+            {
+              date: '2020-01-02',
+              version: '2',
+            },
+            {
+              date: '2019-12-31',
+              version: '1',
+            },
+          ],
+        }));
+
+        const getBiorxivArticleVersionEvents = createGetBiorxivArticleVersionEvents(getJson, dummyLogger);
+
+        const events = await getBiorxivArticleVersionEvents(doi, 'biorxiv');
+
+        expect(getJson).toHaveBeenCalledWith('https://api.biorxiv.org/details/biorxiv/10.1101/2020.09.02.278911');
+        expect(events).toHaveLength(2);
+        expect(events[0]).toStrictEqual({
+          source: new URL('https://www.biorxiv.org/content/10.1101/2020.09.02.278911v2'),
+          occurredAt: new Date('2020-01-02'),
+          version: 2,
+        });
+        expect(events[1]).toStrictEqual({
+          source: new URL('https://www.biorxiv.org/content/10.1101/2020.09.02.278911v1'),
+          occurredAt: new Date('2019-12-31'),
+          version: 1,
+        });
       });
+    });
 
-      const getBiorxivArticleVersionEvents = createGetBiorxivArticleVersionEvents(getJson, dummyLogger);
+    describe('when the server is medrxiv', () => {
+      it('returns an article-version event for each article version', async () => {
+        const doi = new Doi('10.1101/2020.09.02.278911');
+        const getJson: GetJson = jest.fn(async () => ({
+          collection: [
+            {
+              date: '2020-01-02',
+              version: '2',
+            },
+            {
+              date: '2019-12-31',
+              version: '1',
+            },
+          ],
+        }));
 
-      const events = await getBiorxivArticleVersionEvents(doi, 'biorxiv');
+        const getBiorxivArticleVersionEvents = createGetBiorxivArticleVersionEvents(getJson, dummyLogger);
 
-      expect(events).toHaveLength(2);
-      expect(events[0]).toStrictEqual({
-        source: new URL('https://www.biorxiv.org/content/10.1101/2020.09.02.278911v2'),
-        occurredAt: new Date('2020-01-02'),
-        version: 2,
-      });
-      expect(events[1]).toStrictEqual({
-        source: new URL('https://www.biorxiv.org/content/10.1101/2020.09.02.278911v1'),
-        occurredAt: new Date('2019-12-31'),
-        version: 1,
+        const events = await getBiorxivArticleVersionEvents(doi, 'medrxiv');
+
+        expect(getJson).toHaveBeenCalledWith('https://api.biorxiv.org/details/medrxiv/10.1101/2020.09.02.278911');
+        expect(events).toHaveLength(2);
+        expect(events[0]).toStrictEqual({
+          source: new URL('https://www.medrxiv.org/content/10.1101/2020.09.02.278911v2'),
+          occurredAt: new Date('2020-01-02'),
+          version: 2,
+        });
+        expect(events[1]).toStrictEqual({
+          source: new URL('https://www.medrxiv.org/content/10.1101/2020.09.02.278911v1'),
+          occurredAt: new Date('2019-12-31'),
+          version: 1,
+        });
       });
     });
   });
