77fef89cd2ca2e75c072967d94f52de8db2ae280
Giorgio Sironi <g.sironi@elifesciences.org>
2021-01-28 10:10:46 +0000

    Implement helpfulCount via reduce projection #376

--- src/article-page/project-review-response-counts.ts
+++ src/article-page/project-review-response-counts.ts
@@ -1,4 +1,6 @@
+import { respondNotHelpful } from './../respond/respond-not-helpful-command';
 import * as T from 'fp-ts/lib/Task';
+import * as RA from 'fp-ts/lib/ReadonlyArray';
 import { pipe } from 'fp-ts/lib/function';
 import { CountReviewResponses } from './render-review-responses';
 import {
@@ -33,7 +35,32 @@ const projection = (reviewId: ReviewId) => (events: ReadonlyArray<DomainEvent>) 
   return { helpfulCount, notHelpfulCount };
 };
 
-export const createProjectReviewResponseCounts = (getEvents: GetEvents): CountReviewResponses => (reviewId) => pipe(
-  getEvents,
-  T.map(projection(reviewId)),
-);
+export const createProjectReviewResponseCounts = (getEvents: GetEvents): CountReviewResponses => (reviewId) => {
+  // TODO: rename acc
+  const onNewEvent = (acc: {[key:string]: number}, newEvent: DomainEvent) => {
+    if (newEvent.type === 'UserFoundReviewHelpful' || newEvent.type === 'UserRevokedFindingReviewHelpful') {
+      const key = newEvent.reviewId.toString();
+      if (!(key in acc)) {
+        acc[key] = 0;
+      }
+      if (newEvent.type === 'UserFoundReviewHelpful') {
+        acc[key] = acc[key] + 1;
+      }
+      if (newEvent.type === 'UserRevokedFindingReviewHelpful') {
+        acc[key] = acc[key] - 1;
+      }
+    }
+    return acc;
+  };
+  const projectionState = pipe(
+    getEvents,
+    // TODO: understand what RA.foldMap does
+    T.map(RA.reduce({}, onNewEvent))
+  );
+  projectionState().then((state) => console.log(state));
+  // TODO: how to mutably-update the projection state on a new event?
+  return pipe(
+    projectionState,
+    T.map((acc) => ({ helpfulCount: acc[reviewId.toString()] ?? 0, notHelpfulCount: 0})),
+  );
+};
