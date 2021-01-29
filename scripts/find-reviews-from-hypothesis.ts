b3f68694685ce37b2aa772e6a9193ef3961a34e4
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-01-29 14:58:57 +0000

    Extract row processor #388

--- scripts/find-reviews-from-hypothesis.ts
+++ scripts/find-reviews-from-hypothesis.ts
@@ -3,13 +3,15 @@ import { JsonCompatible } from '../src/types/json';
 
 const publisherGroupId = process.argv[2];
 
+type Row = {
+  id: string,
+  created: string,
+  uri: string,
+};
+
 type HypothesisResponse = JsonCompatible<{
   total: number,
-  rows: Array<{
-    id: string,
-    created: string,
-    uri: string,
-  }>,
+  rows: Array<Row>,
 }>;
 
 void (async (): Promise<void> => {
@@ -17,7 +19,7 @@ void (async (): Promise<void> => {
   const { data: firstPage } = await axios.get<HypothesisResponse>(`https://api.hypothes.is/api/search?group=${publisherGroupId}&uri.parts=biorxiv&limit=${perPage}`);
   process.stdout.write('Date,Article DOI,Review ID\n');
 
-  firstPage.rows.forEach((row) => {
+  const processRow = (row: Row): void => {
     const doiRegex = '(10\\.[0-9]{4,}(?:\\.[1-9][0-9]*)*/(?:[^%"#?\\s])+)';
     const matches = new RegExp(`https://www.biorxiv.org/content/${doiRegex}v[0-9]+$`).exec(row.uri);
     if (matches === null) {
@@ -25,20 +27,14 @@ void (async (): Promise<void> => {
     }
     const doi = matches[1];
     process.stdout.write(`${row.created},${doi},hypothesis:${row.id}\n`);
-  });
+  };
+
+  firstPage.rows.forEach(processRow);
 
   const numRequestsNeeded = Math.ceil(firstPage.total / perPage);
 
   for (let i = 1; i < numRequestsNeeded; i += 1) {
     const { data } = await axios.get<HypothesisResponse>(`https://api.hypothes.is/api/search?group=${publisherGroupId}&uri.parts=biorxiv&limit=${perPage}&offset=${perPage * i}`);
-    data.rows.forEach((row) => {
-      const doiRegex = '(10\\.[0-9]{4,}(?:\\.[1-9][0-9]*)*/(?:[^%"#?\\s])+)';
-      const matches = new RegExp(`https://www.biorxiv.org/content/${doiRegex}v[0-9]+$`).exec(row.uri);
-      if (matches === null) {
-        throw new Error(`Cannot parse a DOI out of '${row.uri}'`);
-      }
-      const doi = matches[1];
-      process.stdout.write(`${row.created},${doi},hypothesis:${row.id}\n`);
-    });
+    data.rows.forEach(processRow);
   }
 })();
