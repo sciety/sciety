0c803134309478cdfa815a1abdfd20f065253c5b
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-29 15:09:01 +0000

    pass server as an argument #388

--- scripts/find-reviews-from-hypothesis.ts
+++ scripts/find-reviews-from-hypothesis.ts
@@ -14,27 +14,31 @@ type HypothesisResponse = JsonCompatible<{
   rows: Array<Row>,
 }>;
 
-void (async (): Promise<void> => {
+const processRow = (server: string) => (row: Row): void => {
+  const doiRegex = '(10\\.[0-9]{4,}(?:\\.[1-9][0-9]*)*/(?:[^%"#?\\s])+)';
+  const matches = new RegExp(`https://www.${server}.org/content/${doiRegex}v[0-9]+$`).exec(row.uri);
+  if (matches === null) {
+    throw new Error(`Cannot parse a DOI out of '${row.uri}'`);
+  }
+  const doi = matches[1];
+  process.stdout.write(`${row.created},${doi},hypothesis:${row.id}\n`);
+};
+
+const processServer = async (server: string): Promise<void> => {
   const perPage = 200;
-  const { data: firstPage } = await axios.get<HypothesisResponse>(`https://api.hypothes.is/api/search?group=${publisherGroupId}&uri.parts=biorxiv&limit=${perPage}`);
-  process.stdout.write('Date,Article DOI,Review ID\n');
+  const { data: firstPage } = await axios.get<HypothesisResponse>(`https://api.hypothes.is/api/search?group=${publisherGroupId}&uri.parts=${server}&limit=${perPage}`);
 
-  const processRow = (row: Row): void => {
-    const doiRegex = '(10\\.[0-9]{4,}(?:\\.[1-9][0-9]*)*/(?:[^%"#?\\s])+)';
-    const matches = new RegExp(`https://www.biorxiv.org/content/${doiRegex}v[0-9]+$`).exec(row.uri);
-    if (matches === null) {
-      throw new Error(`Cannot parse a DOI out of '${row.uri}'`);
-    }
-    const doi = matches[1];
-    process.stdout.write(`${row.created},${doi},hypothesis:${row.id}\n`);
-  };
-
-  firstPage.rows.forEach(processRow);
+  firstPage.rows.forEach(processRow(server));
 
   const numRequestsNeeded = Math.ceil(firstPage.total / perPage);
 
   for (let i = 1; i < numRequestsNeeded; i += 1) {
-    const { data } = await axios.get<HypothesisResponse>(`https://api.hypothes.is/api/search?group=${publisherGroupId}&uri.parts=biorxiv&limit=${perPage}&offset=${perPage * i}`);
-    data.rows.forEach(processRow);
+    const { data } = await axios.get<HypothesisResponse>(`https://api.hypothes.is/api/search?group=${publisherGroupId}&uri.parts=${server}&limit=${perPage}&offset=${perPage * i}`);
+    data.rows.forEach(processRow(server));
   }
+};
+
+void (async (): Promise<void> => {
+  process.stdout.write('Date,Article DOI,Review ID\n');
+  await processServer('biorxiv');
 })();
