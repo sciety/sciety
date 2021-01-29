61968dceacac8d579f83a03fae50592151415d95
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-29 09:26:42 +0000

    namespace Ord usage #384

--- src/article-page/compose-feed-events.ts
+++ src/article-page/compose-feed-events.ts
@@ -1,11 +1,11 @@
-import { contramap, getDualOrd, ordDate } from 'fp-ts/lib/Ord';
+import * as Ord from 'fp-ts/lib/Ord';
 import * as RA from 'fp-ts/lib/ReadonlyArray';
 import * as T from 'fp-ts/lib/Task';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { FeedEvent, GetFeedEvents } from './get-feed-events-content';
 
-const byDate = contramap<Date, FeedEvent>((event) => event.occurredAt)(ordDate);
-const byDateDescending = getDualOrd(byDate);
+const byDate = Ord.contramap<Date, FeedEvent>((event) => event.occurredAt)(Ord.ordDate);
+const byDateDescending = Ord.getDualOrd(byDate);
 
 export const composeFeedEvents = (
   ...composedGetFeedEvents: Array<GetFeedEvents>
