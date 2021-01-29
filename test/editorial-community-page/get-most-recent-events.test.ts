4a2762b2527c95846c7b13281df81ab71fad4363
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-29 08:44:36 +0000

    use reviewedArticle events in test #385

--- test/editorial-community-page/get-most-recent-events.test.ts
+++ test/editorial-community-page/get-most-recent-events.test.ts
@@ -1,24 +1,19 @@
 import * as T from 'fp-ts/lib/Task';
 import createGetMostRecentEvents, { GetAllEvents } from '../../src/editorial-community-page/get-most-recent-events';
 import { Doi } from '../../src/types/doi';
-import { DomainEvent } from '../../src/types/domain-events';
+import { DomainEvent, editorialCommunityReviewedArticle } from '../../src/types/domain-events';
 import { EditorialCommunityId } from '../../src/types/editorial-community-id';
+import { toReviewId } from '../../src/types/review-id';
 
 describe('get-most-recent-events', () => {
   const editorialCommunity1 = new EditorialCommunityId('1');
   const editorialCommunity2 = new EditorialCommunityId('2');
-  const endorsedBy = (editorialCommunityId: EditorialCommunityId): DomainEvent => ({
-    type: 'EditorialCommunityEndorsedArticle',
-    date: new Date('2020-07-08'),
-    editorialCommunityId,
-    articleId: new Doi('10.1101/751099'),
-  });
 
   it('only returns events for the given editorial community', async () => {
     const allEvents: ReadonlyArray<DomainEvent> = [
-      endorsedBy(editorialCommunity2),
-      endorsedBy(editorialCommunity1),
-      endorsedBy(editorialCommunity2),
+      editorialCommunityReviewedArticle(editorialCommunity2, new Doi('10.1101/123456'), toReviewId('reviewA')),
+      editorialCommunityReviewedArticle(editorialCommunity1, new Doi('10.1101/123456'), toReviewId('reviewB')),
+      editorialCommunityReviewedArticle(editorialCommunity2, new Doi('10.1101/123456'), toReviewId('reviewC')),
     ];
     const getAllEvents: GetAllEvents = T.of(allEvents);
     const getMostRecentEvents = createGetMostRecentEvents(getAllEvents, 20);
