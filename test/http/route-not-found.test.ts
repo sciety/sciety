370454c25732865b27152799eaf26d1d975eff09
Giorgio Sironi <g.sironi@elifesciences.org>
2021-02-01 11:05:27 +0000

    Add test for custom 404 page #392

---
+++ test/http/route-not-found.test.ts
@@ -0,0 +1,13 @@
+import { NOT_FOUND } from 'http-status-codes';
+import request from 'supertest';
+import { createTestServer } from './server';
+
+describe('route-not-found', () => {
+  it('returns a custom 404 page', async () => {
+    const { server } = await createTestServer();
+    const response = await request(server).get('/route-that-definitely-doesnt-exist-ever');
+
+    expect(response.status).toBe(NOT_FOUND);
+    expect(response.text).toContain('Oops!');
+  });
+});
