9b6f1da0d9717437e51decdd44fb73dd1cb2024b
Giorgio Sironi <g.sironi@elifesciences.org>
2021-02-02 12:02:14 +0000

    Add test list #381

---
+++ feature-test/authentication-and-redirect.test.ts
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv';
+import {
+  click, closeBrowser, goto, into, link, openBrowser, textBox, write,
+} from 'taiko';
+
+describe('authentication-and-redirect', () => {
+  beforeEach(async () => {
+    dotenv.config();
+    await openBrowser();
+  });
+
+  afterAll(closeBrowser);
+
+  it('log in works', async () => {
+    await goto('localhost:8080');
+    await click('Log in');
+    await write(process.env.TAIKO_TWITTER_USERNAME ?? '', into(textBox('Username')));
+    await write(process.env.TAIKO_TWITTER_PASSWORD ?? '', into(textBox('Password')));
+    await click('Sign in');
+    const result = await link('Log out').exists();
+
+    expect(result).toBe(true);
+  });
+
+  describe('not logged in', () => {
+    it.todo('log in from the article page returns to the article page');
+    it.todo('respond command returns to review fragment on the article page');
+    it.todo('follow command from the editorial community page returns to the editorial community page');
+  });
+
+  describe('logged in', () => {
+    it.todo('log out from the article page returns to the article page');
+    it.todo('respond command returns to review fragment on the article page');
+    it.todo('follow command from the editorial community page returns to the editorial community page');
+  });
+});
