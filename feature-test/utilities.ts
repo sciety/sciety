419d8d50998eef473562bdd515c81b617017affa
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-02-04 14:15:05 +0000

    throw if env vars for taiko are missing #381

--- feature-test/utilities.ts
+++ feature-test/utilities.ts
@@ -3,7 +3,11 @@ import {
 } from 'taiko';
 
 export const authenticateViaTwitter = async ():Promise<void> => {
-  await write(process.env.TAIKO_TWITTER_USERNAME ?? '', into(textBox('Username')));
-  await write(process.env.TAIKO_TWITTER_PASSWORD ?? '', into(textBox('Password')));
-  await click('Sign in');
+  if (process.env.TAIKO_TWITTER_USERNAME && process.env.TAIKO_TWITTER_PASSWORD) {
+    await write(process.env.TAIKO_TWITTER_USERNAME ?? '', into(textBox('Username')));
+    await write(process.env.TAIKO_TWITTER_PASSWORD ?? '', into(textBox('Password')));
+    await click('Sign in');
+  } else {
+    throw new Error('Missing TAIKO_TWITTER_USERNAME and/or TAIKO_TWITTER_PASSWORD env vars');
+  }
 };
