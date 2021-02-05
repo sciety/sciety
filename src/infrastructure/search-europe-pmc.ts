fba65a5aba33b5232f72c21fec43fe1e7eefe5e2
Kevin Rutherford <kevin@rutherford-software.com>
2021-02-05 14:40:20 +0000

    Use a codec to decode Europe PMC responses #380

--- src/infrastructure/search-europe-pmc.ts
+++ src/infrastructure/search-europe-pmc.ts
@@ -1,9 +1,13 @@
 import { URLSearchParams } from 'url';
+import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
+import { DateFromISOString } from 'io-ts-types/DateFromISOString';
+import * as PR from 'io-ts/PathReporter';
 import { Logger } from './logger';
 import { Doi } from '../types/doi';
-import { Json, JsonCompatible } from '../types/json';
+import { Json } from '../types/json';
 
 export type GetJson = (uri: string) => Promise<Json>;
 
@@ -21,17 +25,21 @@ type SearchResults = {
 
 export type SearchEuropePmc = (query: string) => TE.TaskEither<'unavailable', SearchResults>;
 
-type EuropePmcQueryResponse = JsonCompatible<{
-  hitCount: number,
-  resultList: {
-    result: Array<{
-      doi: string,
-      title: string,
-      authorString: string,
-      firstPublicationDate: string,
-    }>,
-  },
-}>;
+const resultDetails = t.type({
+  doi: t.string,
+  title: t.string,
+  authorString: t.string,
+  firstPublicationDate: DateFromISOString,
+});
+
+const europePmcResponse = t.type({
+  hitCount: t.number,
+  resultList: t.type({
+    result: t.array(resultDetails),
+  }),
+});
+
+type EuropePmcResponse = t.TypeOf<typeof europePmcResponse>;
 
 const constructQueryParams = (query: string): URLSearchParams => (
   new URLSearchParams({
@@ -42,11 +50,6 @@ const constructQueryParams = (query: string): URLSearchParams => (
 
 const constructSearchUrl = (queryParams: URLSearchParams): string => `https://www.ebi.ac.uk/europepmc/webservices/rest/search?${queryParams.toString()}`;
 
-const logError = (logger: Logger) => (error: unknown): 'unavailable' => {
-  logger('error', 'Failed to search Europe PMC', { error });
-  return 'unavailable';
-};
-
 const search = async (getJson: GetJson, query: string): Promise<Json> => (
   pipe(
     query,
@@ -56,30 +59,31 @@ const search = async (getJson: GetJson, query: string): Promise<Json> => (
   )
 );
 
-const constructSearchResults = (json: Json): TE.TaskEither<'unavailable', SearchResults> => {
-  try {
-    const data = json as EuropePmcQueryResponse;
-    const items = data.resultList.result.map((item): SearchResult => ({
-      doi: new Doi(item.doi),
-      title: item.title,
-      authors: item.authorString,
-      postedDate: new Date(item.firstPublicationDate),
-    }));
-
-    return TE.right({
-      items,
-      total: data.hitCount,
-    });
-  } catch (error: unknown) {
-    return TE.left('unavailable');
-  }
+const constructSearchResults = (data: EuropePmcResponse): SearchResults => {
+  const items = data.resultList.result.map((item): SearchResult => ({
+    doi: new Doi(item.doi),
+    title: item.title,
+    authors: item.authorString,
+    postedDate: new Date(item.firstPublicationDate),
+  }));
+  return {
+    items,
+    total: data.hitCount,
+  };
 };
 
-export const createSearchEuropePmc = (getJson: GetJson, logger: Logger): SearchEuropePmc => (
-  (query) => (
-    pipe(
-      TE.tryCatch(async () => search(getJson, query), logError(logger)),
-      TE.chain(constructSearchResults),
-    )
-  )
+export const createSearchEuropePmc = (getJson: GetJson, logger: Logger): SearchEuropePmc => (query) => pipe(
+  TE.tryCatch(async () => search(getJson, query), E.toError),
+  TE.chain(flow(
+    europePmcResponse.decode,
+    TE.fromEither,
+    TE.mapLeft((e) => new Error(PR.failure(e).join('\n'))),
+  )),
+  TE.bimap(
+    (error): 'unavailable' => {
+      logger('error', 'Could not parse EuropePMC response', { error });
+      return 'unavailable';
+    },
+    constructSearchResults,
+  ),
 );
