75d9012c86924729daa26e05ac944dc999a6fe59
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-02-05 11:58:24 +0000

    Move the rest of the logic into a pipe #380

--- src/user-page/project-followed-editorial-community-ids.ts
+++ src/user-page/project-followed-editorial-community-ids.ts
@@ -1,28 +1,28 @@
+import * as Eq from 'fp-ts/Eq';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
 import { GetFollowedEditorialCommunityIds } from './get-followed-editorial-communities-from-ids';
-import { DomainEvent } from '../types/domain-events';
-import { EditorialCommunityId } from '../types/editorial-community-id';
+import {
+  DomainEvent,
+  isUserFollowedEditorialCommunityEvent,
+  isUserUnfollowedEditorialCommunityEvent,
+  UserFollowedEditorialCommunityEvent,
+  UserUnfollowedEditorialCommunityEvent,
+} from '../types/domain-events';
 import { UserId } from '../types/user-id';
 
 export type GetAllEvents = T.Task<ReadonlyArray<DomainEvent>>;
 
-const projectFollowedCommunities = (userId: UserId) => (events: ReadonlyArray<DomainEvent>) => {
-  const result = new Set<string>();
-  events.forEach((event) => {
-    if (event.type === 'UserFollowedEditorialCommunity' && event.userId === userId) {
-      result.add(event.editorialCommunityId.value);
-    } else if (event.type === 'UserUnfollowedEditorialCommunity' && event.userId === userId) {
-      result.delete(event.editorialCommunityId.value);
-    }
-  });
-
-  return pipe(
-    [...result],
-    RA.map((id) => new EditorialCommunityId(id)),
-  );
-};
+const projectFollowedCommunities = (userId: UserId) => (events: ReadonlyArray<DomainEvent>) => pipe(
+  events,
+  RA.filter((event): event is UserFollowedEditorialCommunityEvent | UserUnfollowedEditorialCommunityEvent => (
+    isUserFollowedEditorialCommunityEvent(event) || isUserUnfollowedEditorialCommunityEvent(event)
+  )),
+  RA.filter((event) => event.userId === userId),
+  RA.map((event) => event.editorialCommunityId),
+  RA.uniq(Eq.fromEquals((a, b) => a.value === b.value)),
+);
 
 export const createProjectFollowedEditorialCommunityIds = (
   getAllEvents: GetAllEvents,
