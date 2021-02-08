c83b148237f1ddfa080b0e6e740397e7f3b56371
David Moulton <d.moulton@elifesciences.org>
2021-02-08 10:34:35 +0000

    Add feature flagged placeholder for specific NCRC review #405

--- src/article-page/render-feed.ts
+++ src/article-page/render-feed.ts
@@ -1,5 +1,6 @@
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { constant, pipe } from 'fp-ts/function';
@@ -43,6 +44,12 @@ export const createRenderFeed = (
     getFeedItems(doi, server),
     T.map(E.fromPredicate((items) => items.length > 0, constant('no-content' as const))),
     TE.chainW(TE.traverseArray((feedItem) => pipe(renderFeedItem(feedItem, userId), TE.rightTask))),
+    TE.map((items) => {
+      if (process.env.EXPERIMENT_ENABLED === 'true' && doi.value === '10.1101/2021.01.29.21250653') {
+        return RA.cons(toHtmlFragment('NCRC reviewed this article.'))(items);
+      }
+      return items;
+    }),
     TE.map((items) => `
       <section class="article-feed">
         <h2>Feed</h2>
