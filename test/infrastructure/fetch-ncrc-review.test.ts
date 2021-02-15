bfc3ee495547c87ecd38440903cb72dbcc9ac3dd
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-02-15 11:37:24 +0000

    Add simple fetch-ncrc-review test #410

---
+++ test/infrastructure/fetch-ncrc-review.test.ts
@@ -0,0 +1,19 @@
+import * as E from 'fp-ts/Either';
+import { fetchNcrcReview } from '../../src/infrastructure/fetch-ncrc-review';
+import * as NcrcId from '../../src/types/ncrc-id';
+
+describe('fetch-ncrc-review', () => {
+  describe('when the review is found', () => {
+    it('returns the review', async () => {
+      const id = NcrcId.fromString('0c88338d-a401-40f9-8bf8-ef0a43be4548');
+
+      const result = await fetchNcrcReview(id)();
+
+      expect(E.isRight(result)).toBe(true);
+    });
+  });
+
+  describe('when the review is not found', () => {
+    it.todo('returns not-found');
+  });
+});
