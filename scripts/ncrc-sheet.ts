10731fa6584c26ae5454c0941da9ee40fda48d24
Giorgio Sironi <g.sironi@elifesciences.org>
2021-02-16 09:46:46 +0000

    Dump data of a single NCRC article #410

---
+++ scripts/ncrc-sheet.ts
@@ -0,0 +1,21 @@
+import { google } from 'googleapis';
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: '/home/giorgio/sciety-0b0fcd073bf7.json',
+  scopes: ['https://www.googleapis.com/auth/cloud-platform', 'https://www.googleapis.com/auth/spreadsheets.readonly'],
+});
+
+google.options({
+  auth,
+});
+
+const sheets = google.sheets('v4');
+sheets.spreadsheets.values.get({
+  spreadsheetId: '1RJ_Neh1wwG6X0SkYZHjD-AEC9ykgAcya_8UCVNoE3SA',
+  range: 'Sheet1!A370:AF370',
+}, (err, res) => {
+  /* eslint-disable no-console */
+  if (err) return console.log(`The API returned an error: ${err.toString()}`);
+  const rows = res?.data?.values;
+  console.log(rows);
+});
