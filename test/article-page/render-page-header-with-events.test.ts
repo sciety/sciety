892bac175d167f4641586ef592c2555fb8b475b2
Giorgio Sironi <g.sironi@elifesciences.org>
2021-02-17 11:05:08 +0000

    Duplicate test list from real component #476

---
+++ test/article-page/render-page-header-with-events.test.ts
@@ -0,0 +1,6 @@
+describe('render-page-header-with-events', () => {
+  it.todo('renders inside an header tag');
+  it.todo('renders the title for an article');
+  it.todo('renders the article DOI according to CrossRef display guidelines');
+  it.todo('renders the article authors');
+});
