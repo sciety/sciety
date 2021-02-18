91b6db235327f4d1f4ba7ae9a41a684fec3a1b4d
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-02-18 16:16:25 +0000

    Remove duplication #410

--- src/infrastructure/fetch-ncrc-review.ts
+++ src/infrastructure/fetch-ncrc-review.ts
@@ -3,6 +3,7 @@ import { sequenceS } from 'fp-ts/Apply';
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { constant, flow, pipe } from 'fp-ts/function';
@@ -10,7 +11,7 @@ import { google, sheets_v4 } from 'googleapis';
 import { Logger } from './logger';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import * as NcrcId from '../types/ncrc-id';
-import Sheets = sheets_v4.Sheets;
+import Params$Resource$Spreadsheets$Values$Get = sheets_v4.Params$Resource$Spreadsheets$Values$Get;
 
 export type NcrcReview = {
   title: string,
@@ -34,7 +35,9 @@ type GetRowNumber = (id: NcrcId.NcrcId) => TE.TaskEither<'unavailable' | 'not-fo
 
 type GetNcrcReview = (row: number) => TE.TaskEither<'unavailable' | 'not-found', NcrcReview>;
 
-const getSheets = (): Sheets => {
+type QuerySheet = (params: Params$Resource$Spreadsheets$Values$Get) => TE.TaskEither<'unavailable', RNEA.ReadonlyNonEmptyArray<ReadonlyArray<string>>>;
+
+const querySheet = (logger: Logger): QuerySheet => (params) => {
   const auth = new google.auth.GoogleAuth({
     keyFile: '/var/run/secrets/app/.gcp-ncrc-key.json',
     scopes: ['https://www.googleapis.com/auth/cloud-platform', 'https://www.googleapis.com/auth/spreadsheets.readonly'],
@@ -44,34 +47,35 @@ const getSheets = (): Sheets => {
     auth,
   });
 
-  return google.sheets('v4');
+  return pipe(
+    TE.tryCatch(
+      async () => google.sheets('v4').spreadsheets.values.get(params),
+      (error) => {
+        logger('error', 'Error fetching Google sheet', { error });
+        return 'unavailable' as const;
+      },
+    ),
+    T.map(E.chain((res) => pipe(
+      res?.data?.values,
+      O.fromNullable,
+      O.chain(RNEA.fromArray),
+      O.altW(() => {
+        logger('error', 'Empty response from Google sheet api', { res });
+        return O.none;
+      }),
+      E.fromOption(constant('unavailable' as const)),
+    ))),
+  );
 };
 
 const getRowNumber = (logger: Logger): GetRowNumber => (id) => pipe(
-  getSheets(),
-  TE.right,
-  TE.chain((sheets) => TE.tryCatch(
-    async () => sheets.spreadsheets.values.get({
-      spreadsheetId: '1RJ_Neh1wwG6X0SkYZHjD-AEC9ykgAcya_8UCVNoE3SA',
-      range: 'Sheet1!A:A',
-      majorDimension: 'COLUMNS',
-    }),
-    (error) => {
-      logger('error', 'Error fetching Google sheet', { error });
-      return 'unavailable' as const;
-    },
-  )),
-  T.map(E.chain((res) => pipe(
-    res?.data?.values,
-    O.fromNullable,
-    O.chain(RA.head),
-    O.altW(() => {
-      logger('error', 'Empty response from Google sheet api', { res });
-      return O.none;
-    }),
-    E.fromOption(constant('unavailable' as const)),
-  ))),
+  querySheet(logger)({
+    spreadsheetId: '1RJ_Neh1wwG6X0SkYZHjD-AEC9ykgAcya_8UCVNoE3SA',
+    range: 'Sheet1!A:A',
+    majorDimension: 'COLUMNS',
+  }),
   T.map(E.chainW(flow(
+    RNEA.head,
     RA.findIndex((uuid) => NcrcId.eqNcrcId.equals(NcrcId.fromString(uuid), id)),
     O.map((n) => n + 1),
     O.altW(() => {
@@ -85,40 +89,23 @@ const getRowNumber = (logger: Logger): GetRowNumber => (id) => pipe(
 );
 
 const getNcrcReview = (logger: Logger): GetNcrcReview => (rowNumber) => pipe(
-  getSheets(),
-  TE.right,
-  TE.chain((sheets) => TE.tryCatch(
-    async () => sheets.spreadsheets.values.get({
-      spreadsheetId: '1RJ_Neh1wwG6X0SkYZHjD-AEC9ykgAcya_8UCVNoE3SA',
-      range: `Sheet1!A${rowNumber}:AF${rowNumber}`,
+  querySheet(logger)({
+    spreadsheetId: '1RJ_Neh1wwG6X0SkYZHjD-AEC9ykgAcya_8UCVNoE3SA',
+    range: `Sheet1!A${rowNumber}:AF${rowNumber}`,
+  }),
+  T.map(E.chain(flow(
+    RNEA.head,
+    (row) => ({
+      title: RA.lookup(2)(row),
+      ourTake: RA.lookup(7)(row),
+      studyDesign: RA.lookup(21)(row),
+      studyPopulationSetting: RA.lookup(9)(row),
+      mainFindings: RA.lookup(10)(row),
+      studyStrength: RA.lookup(11)(row),
+      limitations: RA.lookup(12)(row),
+      valueAdded: RA.lookup(8)(row),
     }),
-    (error) => {
-      logger('error', 'Error fetching Google sheet', { error });
-      return 'unavailable' as const;
-    },
-  )),
-  T.map(E.chain((res) => pipe(
-    res?.data?.values,
-    O.fromNullable,
-    O.chain(RA.head),
-    O.altW(() => {
-      logger('error', 'Empty response from Google sheet api', { res });
-      return O.none;
-    }),
-    // TODO: ensure that these are strings (codec?)
-    O.chain(flow(
-      (row) => ({
-        title: RA.lookup(2)(row),
-        ourTake: RA.lookup(7)(row),
-        studyDesign: RA.lookup(21)(row),
-        studyPopulationSetting: RA.lookup(9)(row),
-        mainFindings: RA.lookup(10)(row),
-        studyStrength: RA.lookup(11)(row),
-        limitations: RA.lookup(12)(row),
-        valueAdded: RA.lookup(8)(row),
-      }),
-      sequenceS(O.option),
-    )),
+    sequenceS(O.option),
     E.fromOption(constant('unavailable' as const)),
   ))),
 );
