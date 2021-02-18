cefb19dc8e8bb9ca493c57869a32b7df08deb1f3
Giorgio Sironi <g.sironi@elifesciences.org>
2021-02-18 10:56:02 +0000

    Extract ncrcReviews data structure #410

--- scripts/find-reviews-from-ncrc.ts
+++ scripts/find-reviews-from-ncrc.ts
@@ -1,4 +1,11 @@
 void (async (): Promise<void> => {
   process.stdout.write('Date,Article DOI,Review ID\n');
-  process.stdout.write('2021-02-04,10.1101/2021.01.29.21250653,ncrc:0c88338d-a401-40f9-8bf8-ef0a43be4548\n');
+  const ncrcReviews = [{
+    date: '2021-02-04',
+    doi: '10.1101/2021.01.29.21250653',
+    id: '0c88338d-a401-40f9-8bf8-ef0a43be4548',
+  }];
+  ncrcReviews.forEach((ncrcReview) => {
+    process.stdout.write(`${ncrcReview.date},${ncrcReview.doi},ncrc:${ncrcReview.id}\n`);
+  });
 })();
