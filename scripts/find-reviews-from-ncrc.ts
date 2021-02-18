3649453ccdad313ffc6ea2c70e76b73c1a247e74
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-02-18 11:10:07 +0000

    extract doi from link #410

--- scripts/find-reviews-from-ncrc.ts
+++ scripts/find-reviews-from-ncrc.ts
@@ -2,10 +2,11 @@ void (async (): Promise<void> => {
   process.stdout.write('Date,Article DOI,Review ID\n');
   const ncrcReviews = [{
     date: '2021-02-04',
-    doi: '10.1101/2021.01.29.21250653',
+    link: 'https://medrxiv.org/cgi/content/short/2021.01.29.21250653',
     id: '0c88338d-a401-40f9-8bf8-ef0a43be4548',
   }];
   ncrcReviews.forEach((ncrcReview) => {
-    process.stdout.write(`${ncrcReview.date},${ncrcReview.doi},ncrc:${ncrcReview.id}\n`);
+    const [,doiSuffix] = new RegExp('.*/([^/]*)$').exec(ncrcReview.link) ?? [];
+    process.stdout.write(`${ncrcReview.date},10.1101/${doiSuffix},ncrc:${ncrcReview.id}\n`);
   });
 })();
