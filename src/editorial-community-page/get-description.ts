47cb7f29630e08eec71b3b3147693a76c36c6eb4
Kevin Rutherford <kevin@rutherford-software.com>
2021-02-19 14:30:38 +0000

    Push a TE shim closer to infrastructure #380

--- src/editorial-community-page/get-description.ts
+++ src/editorial-community-page/get-description.ts
@@ -1,4 +1,5 @@
 import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { Remarkable } from 'remarkable';
 import { EditorialCommunity } from '../types/editorial-community';
@@ -7,10 +8,11 @@ type FetchStaticFile = (filename: string) => T.Task<string>;
 
 const convertMarkdownToHtml = (md: string): string => new Remarkable({ html: true }).render(md);
 
-type GetDescription = (fetchStaticFile: FetchStaticFile) => (community: EditorialCommunity) => T.Task<string>;
+type GetDescription = (fetchStaticFile: FetchStaticFile) => (group: EditorialCommunity) => TE.TaskEither<never, string>;
 
-export const getDescription: GetDescription = (fetchStaticFile) => (editorialCommunity) => pipe(
-  `editorial-communities/${editorialCommunity.descriptionPath}`,
+export const getDescription: GetDescription = (fetchStaticFile) => (group) => pipe(
+  `editorial-communities/${group.descriptionPath}`,
   fetchStaticFile,
   T.map(convertMarkdownToHtml),
+  TE.rightTask,
 );
--- src/editorial-community-page/render-description.ts
+++ src/editorial-community-page/render-description.ts
@@ -1,4 +1,3 @@
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { flow } from 'fp-ts/function';
 import { EditorialCommunity } from '../types/editorial-community';
@@ -6,17 +5,18 @@ import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
 type RenderDescription = (editorialCommunity: EditorialCommunity) => TE.TaskEither<never, HtmlFragment>;
 
-export type GetEditorialCommunityDescription = (editorialCommunity: EditorialCommunity) => T.Task<string>;
+export type GetEditorialCommunityDescription = (editorialCommunity: EditorialCommunity) => TE.TaskEither<never, string>;
 
 export const renderDescription = (
   getEditorialCommunityDescription: GetEditorialCommunityDescription,
 ): RenderDescription => flow(
   getEditorialCommunityDescription,
-  T.map((desc) => `
-    <section>
-      ${desc}
-    </section>
-  `),
-  T.map(toHtmlFragment),
-  TE.rightTask,
+  TE.map(flow(
+    (desc) => `
+      <section>
+        ${desc}
+      </section>
+    `,
+    toHtmlFragment,
+  )),
 );
