162f107a7a9d9c369b651015974f354aa679f6f9
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-02-22 12:01:12 +0000

    Rewrite as a pipe #380

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,35 @@
 import { createTerminus, TerminusOptions } from '@godaddy/terminus';
+import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import { identity, pipe } from 'fp-ts/function';
 import { createRouter } from './http/router';
 import { createApplicationServer } from './http/server';
 import { createInfrastructure } from './infrastructure';
+import { Logger } from './infrastructure/logger';
 
-void (async (): Promise<void> => {
-  const adapters = await createInfrastructure()();
+const terminusOptions = (logger: Logger): TerminusOptions => ({
+  onShutdown: async (): Promise<void> => {
+    logger('debug', 'Shutting server down');
+  },
+  onSignal: async (): Promise<void> => {
+    logger('debug', 'Signal received');
+  },
+  signals: ['SIGINT', 'SIGTERM'],
+});
 
-  const router = createRouter(adapters);
-
-  const server = createApplicationServer(
-    router,
-    adapters.logger,
-  );
-
-  const terminusOptions: TerminusOptions = {
-    onShutdown: async (): Promise<void> => {
-      adapters.logger('debug', 'Shutting server down');
-    },
-    onSignal: async (): Promise<void> => {
-      adapters.logger('debug', 'Signal received');
-    },
-    signals: ['SIGINT', 'SIGTERM'],
-  };
-
-  createTerminus(server, terminusOptions);
-
-  server.on('listening', (): void => adapters.logger('debug', 'Server running'));
-
-  server.listen(80);
-})();
+void pipe(
+  TE.Do,
+  TE.bind('adapters', () => TE.tryCatch(async () => createInfrastructure()(), identity)),
+  TE.bindW('router', ({ adapters }) => pipe(adapters, createRouter, TE.right)),
+  TE.chainW(({ adapters, router }) => pipe(
+    E.tryCatch(() => createApplicationServer(router, adapters.logger), identity),
+    E.map((server) => createTerminus(server, terminusOptions(adapters.logger))),
+    E.map((server) => server.on('listening', () => adapters.logger('debug', 'Server running'))),
+    TE.fromEither,
+  )),
+  T.map(E.fold(
+    (error) => pipe(process.stderr.write(`Unable to start:\n${String(error)}\n`), process.exit(1)),
+    (server) => server.listen(80),
+  )),
+)();
