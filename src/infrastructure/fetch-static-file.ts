165cdc22d53cb6879713374d8d9017e904c05760
Kevin Rutherford <kevin@rutherford-software.com>
2021-02-22 13:02:46 +0000

    Correctly handle filesystem errors #380

--- src/infrastructure/fetch-static-file.ts
+++ src/infrastructure/fetch-static-file.ts
@@ -1,11 +1,10 @@
-import { promises as fs } from 'fs';
+import * as fs from 'fs';
 import path from 'path';
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { Logger } from './logger';
 
-export type FetchStaticFile = (filename: string) => TE.TaskEither<never, string>;
+export type FetchStaticFile = (filename: string) => TE.TaskEither<'not-found' | 'unavailable', string>;
 
 export const fetchStaticFile = (logger: Logger): FetchStaticFile => (
   (filename) => {
@@ -13,10 +12,14 @@ export const fetchStaticFile = (logger: Logger): FetchStaticFile => (
     logger('debug', 'Fetch static file', { filename, fullPath });
     return pipe(
       fullPath,
-      fs.readFile,
-      T.of,
-      T.map((text) => text.toString()),
-      TE.rightTask,
+      TE.taskify(fs.readFile),
+      TE.bimap(
+        (error: NodeJS.ErrnoException) => {
+          logger('error', 'Failed to fetch static file', { error });
+          return error.code === 'ENOENT' ? 'not-found' : 'unavailable' as const;
+        },
+        (text) => text.toString(),
+      ),
     );
   }
 );
