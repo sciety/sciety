5a0f1858b0bb2a89f8544c9422475d815aee379a
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-02-23 11:49:04 +0000

    Extract articleActivityPage to its own module #486

--- src/article-page/activity-page.ts
+++ src/article-page/activity-page.ts
@@ -1,19 +1,16 @@
 import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
-import { flow, pipe } from 'fp-ts/function';
+import { pipe } from 'fp-ts/function';
 import { ensureBiorxivDoi } from './ensure-biorxiv-doi';
 import { FindVersionsForArticleDoi, getArticleFeedEvents } from './get-article-feed-events';
 import { GetReview } from './get-feed-events-content';
 import { projectHasUserSavedArticle } from './project-has-user-saved-article';
 import { createProjectReviewResponseCounts } from './project-review-response-counts';
 import { createProjectUserReviewResponse } from './project-user-review-response';
-import { createRenderArticleAbstract } from './render-article-abstract';
+import { RenderActivityPage, renderActivityPage } from './render-activity-page';
 import { renderArticleVersionFeedItem } from './render-article-version-feed-item';
 import { createRenderFeed } from './render-feed';
-import { renderMetaPage } from './render-meta-page';
-import { createRenderPage, RenderPage } from './render-page';
-import { createRenderPageHeader } from './render-page-header';
 import { createRenderReviewFeedItem } from './render-review-feed-item';
 import { createRenderReviewResponses } from './render-review-responses';
 import { renderSaveArticle } from './render-save-article';
@@ -27,6 +24,13 @@ import { ReviewId } from '../types/review-id';
 import { SanitisedHtmlFragment } from '../types/sanitised-html-fragment';
 import { User } from '../types/user';
 import { UserId } from '../types/user-id';
+
+type ActivityPage = (params: Params) => ReturnType<RenderActivityPage>;
+
+type Params = {
+  doi?: string,
+  user: O.Option<User>,
+};
 
 type FindReviewsForArticleDoi = (articleVersionDoi: Doi) => T.Task<ReadonlyArray<{
   reviewId: ReviewId,
@@ -43,7 +47,6 @@ type ArticleDetails = {
 
 type GetArticleDetails = (doi: Doi) => TE.TaskEither<'not-found'|'unavailable', ArticleDetails>;
 
-type GetEvents = T.Task<ReadonlyArray<DomainEvent>>;
 type Ports = {
   fetchArticle: GetArticleDetails,
   fetchReview: GetReview,
@@ -53,13 +56,7 @@ type Ports = {
   }>>,
   findReviewsForArticleDoi: FindReviewsForArticleDoi,
   findVersionsForArticleDoi: FindVersionsForArticleDoi,
-  getAllEvents: GetEvents,
-};
-
-export type Params = {
-  doi?: string,
-  flavour?: string,
-  user: O.Option<User>,
+  getAllEvents: T.Task<ReadonlyArray<DomainEvent>>,
 };
 
 const getUserId = (user: O.Option<User>): O.Option<UserId> => pipe(
@@ -67,21 +64,7 @@ const getUserId = (user: O.Option<User>): O.Option<UserId> => pipe(
   O.map((u) => u.id),
 );
 
-// TODO: Should these page types be unified?
-type ArticlePage = (params: Params) => ReturnType<RenderPage>;
-
-export const articlePage = (ports: Ports): ArticlePage => {
-  const renderPageHeader = createRenderPageHeader(
-    ports.fetchArticle,
-    renderTweetThis,
-    renderSaveArticle(projectHasUserSavedArticle(ports.getAllEvents)),
-  );
-  const renderAbstract = createRenderArticleAbstract(
-    flow(
-      ports.fetchArticle,
-      TE.map((article) => article.abstract),
-    ),
-  );
+export const articleActivityPage = (ports: Ports): ActivityPage => {
   const countReviewResponses = createProjectReviewResponseCounts(ports.getAllEvents);
   const renderFeed = createRenderFeed(
     getArticleFeedEvents(
@@ -99,40 +82,13 @@ export const articlePage = (ports: Ports): ArticlePage => {
     ),
     renderArticleVersionFeedItem,
   );
-  const renderPage = createRenderPage(
-    renderPageHeader,
-    renderAbstract,
+  const renderPage = renderActivityPage(
     renderFeed,
-    ports.fetchArticle,
-  );
-  return (params) => pipe(
-    params.doi ?? '',
-    ensureBiorxivDoi,
-    O.fold(
-      () => TE.left({
-        type: 'not-found',
-        message: toHtmlFragment(`${params.doi ?? 'Article'} not found`),
-      }),
-      (doi: Doi) => renderPage(doi, getUserId(params.user)),
-    ),
-  );
-};
-
-export { articleActivityPage } from './activity-page';
-
-export const articleMetaPage = (ports: Ports): ArticlePage => {
-  const renderAbstract = createRenderArticleAbstract(
-    flow(
-      ports.fetchArticle,
-      TE.map((article) => article.abstract),
-    ),
-  );
-  const renderPage = renderMetaPage(
-    renderAbstract,
     ports.fetchArticle,
     renderSaveArticle(projectHasUserSavedArticle(ports.getAllEvents)),
     renderTweetThis,
   );
+
   return (params) => pipe(
     params.doi ?? '',
     ensureBiorxivDoi,
