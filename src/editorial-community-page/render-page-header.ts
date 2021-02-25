ac7681f716774809501551f5d81447aa229d24e5
Giorgio Sironi <g.sironi@elifesciences.org>
2021-02-25 10:11:36 +0000

    Remove Semantic UI from group page header #486

--- src/editorial-community-page/render-page-header.ts
+++ src/editorial-community-page/render-page-header.ts
@@ -10,8 +10,8 @@ type Community = {
 
 const render = (editorialCommunity: Community): string => `
   <header class="page-header page-header--editorial-community">
+    <img src="${editorialCommunity.avatarPath}" alt="" class="page-header__avatar">
     <h1>
-      <img src="${editorialCommunity.avatarPath}" alt="" class="ui avatar image">
       ${editorialCommunity.name}
     </h1>
   </header>
