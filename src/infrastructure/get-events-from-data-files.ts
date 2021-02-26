e3934354ed27bb44a451b2a69f2ad73a95b31bc2
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-02-26 12:08:44 +0000

    Read events from files using a codec #380

--- src/infrastructure/get-events-from-data-files.ts
+++ src/infrastructure/get-events-from-data-files.ts
@@ -1,6 +1,11 @@
 import fs from 'fs';
 import csvParseSync from 'csv-parse/lib/sync';
+import * as A from 'fp-ts/Array';
+import * as E from 'fp-ts/Either';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
+import { flow, pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
 import { Doi } from '../types/doi';
 import { DomainEvent, editorialCommunityReviewedArticle } from '../types/domain-events';
 import { EditorialCommunityId } from '../types/editorial-community-id';
@@ -8,10 +13,16 @@ import { toReviewId } from '../types/review-id';
 
 /* eslint-disable no-continue */
 
+const review = t.tuple([
+  t.string, // TODO Data
+  t.string, // TODO Doi
+  t.string, // TODO ReviewId
+]);
+
 export const getEventsFromDataFiles = (
   editorialCommunityIds: ReadonlyArray<string>,
 ): TE.TaskEither<unknown, Array<DomainEvent>> => {
-  const parsedEvents: Array<DomainEvent> = [];
+  const parsedEvents = [];
 
   for (const csvFile of fs.readdirSync('./data/reviews')) {
     const editorialCommunityId = csvFile.replace('.csv', '');
@@ -19,14 +30,26 @@ export const getEventsFromDataFiles = (
       continue;
     }
     const fileContents = fs.readFileSync(`./data/reviews/${csvFile}`);
-    parsedEvents.push(...csvParseSync(fileContents, { fromLine: 2 })
-      .map(([date, articleDoi, reviewId]: [string, string, string]) => editorialCommunityReviewedArticle(
-        new EditorialCommunityId(editorialCommunityId),
-        new Doi(articleDoi),
-        toReviewId(reviewId),
-        new Date(date),
-      )));
+
+    parsedEvents.push(pipe(
+      csvParseSync(fileContents, { fromLine: 2 }),
+      A.map(flow(
+        review.decode,
+        E.map(([date, articleDoi, reviewId]) => editorialCommunityReviewedArticle(
+          new EditorialCommunityId(editorialCommunityId),
+          new Doi(articleDoi),
+          toReviewId(reviewId),
+          new Date(date),
+        )),
+      )),
+    ));
   }
 
-  return TE.right(parsedEvents);
+  return pipe(
+    parsedEvents,
+    A.flatten,
+    E.sequenceArray,
+    E.map(RA.toArray),
+    TE.fromEither,
+  );
 };
