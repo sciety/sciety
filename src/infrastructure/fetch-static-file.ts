06f749a70eb47d60b291368d5a87f100e76cca9b
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-03-01 09:59:16 +0000

    Complete a pipe #380

--- src/infrastructure/fetch-static-file.ts
+++ src/infrastructure/fetch-static-file.ts
@@ -1,25 +1,24 @@
 import * as fs from 'fs';
 import path from 'path';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
+import { flow } from 'fp-ts/function';
 import { Logger } from './logger';
 
 export type FetchStaticFile = (filename: string) => TE.TaskEither<'not-found' | 'unavailable', string>;
 
-export const fetchStaticFile = (logger: Logger): FetchStaticFile => (
+export const fetchStaticFile = (logger: Logger): FetchStaticFile => flow(
   (filename) => {
     const fullPath = path.resolve(__dirname, '..', '..', 'static', filename);
     logger('debug', 'Fetch static file', { filename, fullPath });
-    return pipe(
-      fullPath,
-      TE.taskify(fs.readFile),
-      TE.bimap(
-        (error) => {
-          logger('error', 'Failed to fetch static file', { error });
-          return error.code === 'ENOENT' ? 'not-found' : 'unavailable';
-        },
-        (text) => text.toString(),
-      ),
-    );
-  }
+
+    return fullPath;
+  },
+  TE.taskify(fs.readFile),
+  TE.bimap(
+    (error) => {
+      logger('error', 'Failed to fetch static file', { error });
+      return error.code === 'ENOENT' ? 'not-found' : 'unavailable';
+    },
+    (text) => text.toString(),
+  ),
 );
