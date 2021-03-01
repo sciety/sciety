f2698b46561f4e46fafbb09ac89b78382c379505
Will Byrne <w.byrne@elifesciences.org>
2021-03-01 11:34:10 +0000

    expand background color for whole activity page #488

--- src/article-page/render-activity-page.ts
+++ src/article-page/render-activity-page.ts
@@ -12,20 +12,22 @@ export const renderActivityPage = (components: {
   saveArticle: string,
   tweetThis: string,
 }): HtmlFragment => toHtmlFragment(`
-  <article class="sciety-grid sciety-grid--activity">
-    <header class="page-header page-header--article">
-      <h1>${components.articleDetails.title}</h1>
-      <div class="article-actions">
-        ${components.tweetThis}
-        ${components.saveArticle}
+  <div class="activity-page__background--filler">
+    <article class="sciety-grid sciety-grid--activity">
+      <header class="page-header page-header--article">
+        <h1>${components.articleDetails.title}</h1>
+        <div class="article-actions">
+          ${components.tweetThis}
+          ${components.saveArticle}
+        </div>
+      </header>
+      <div class="article-tabs">
+        <a class="article-tabs__tab article-tabs__link" href="/articles/meta/${components.doi.value}" aria-label="Discover article information and abstract">Article</a>
+        <h2 class="article-tabs__tab article-tabs__heading">Activity</h2>
       </div>
-    </header>
-    <div class="article-tabs">
-      <a class="article-tabs__tab article-tabs__link" href="/articles/meta/${components.doi.value}" aria-label="Discover article information and abstract">Article</a>
-      <h2 class="article-tabs__tab article-tabs__heading">Activity</h2>
-    </div>
-    <div class="main-content main-content--article">
-      ${components.feed}
-    </div>
-  </article>
+      <div class="main-content main-content--article">
+        ${components.feed}
+      </div>
+    </article>
+  </div>
 `);
