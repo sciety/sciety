71a4b6b9919c5a811d3130203a6b617f810ee019
Kevin Rutherford <kevin@rutherford-software.com>
2021-03-02 08:27:51 +0000

    Remove unnecessary awaits #380

--- test/respond/respond-helpful-command.test.ts
+++ test/respond/respond-helpful-command.test.ts
@@ -8,7 +8,7 @@ const reviewId = new Doi('10.1234/5678');
 describe('respond-helpful-command', () => {
   describe('none state for this review and user', () => {
     it('returns UserFoundReviewHelpful event', async () => {
-      const events = await respondHelpful('none', userId, reviewId);
+      const events = respondHelpful('none', userId, reviewId);
 
       expect(events).toHaveLength(1);
       expect(events[0]).toMatchObject({
@@ -21,7 +21,7 @@ describe('respond-helpful-command', () => {
 
   describe('helpful state for this review and user', () => {
     it('returns no events', async () => {
-      const events = await respondHelpful('helpful', userId, reviewId);
+      const events = respondHelpful('helpful', userId, reviewId);
 
       expect(events).toHaveLength(0);
     });
@@ -29,7 +29,7 @@ describe('respond-helpful-command', () => {
 
   describe('not-helpful state for this review and user', () => {
     it('returns UserRevokedFindingReviewNotHelpful and UserFoundReviewHelpful events', async () => {
-      const events = await respondHelpful('not-helpful', userId, reviewId);
+      const events = respondHelpful('not-helpful', userId, reviewId);
 
       expect(events).toHaveLength(2);
       expect(events[0]).toMatchObject({
