27c4e7249b32f76c69cea190697e87966122691a
Will Byrne <w.byrne@elifesciences.org>
2021-03-03 11:57:20 +0000

    Rename sciety-grid--article to sciety-grid--meta #488

--- src/article-page/render-meta-page.ts
+++ src/article-page/render-meta-page.ts
@@ -17,7 +17,7 @@ export const renderMetaPage = (components: {
   saveArticle: string,
   tweetThis: string,
 }): HtmlFragment => toHtmlFragment(`
-<article class="sciety-grid sciety-grid--article">
+<article class="sciety-grid sciety-grid--meta">
   <header class="page-header page-header--article">
     <h1>${components.articleDetails.title}</h1>
     <div class="article-actions">
