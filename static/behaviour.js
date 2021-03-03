0f78ea8306001bee0bf7f4552562451f88efd633
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-03-03 10:30:55 +0000

    Make the see more/less button appear as an inline link if possible #488

--- static/behaviour.js
+++ static/behaviour.js
@@ -1,7 +1,7 @@
 (function(doc) {
   function buildToggle() {
     const button = doc.createElement('button');
-    button.classList.add('article-feed__item-toggle', 'button', 'button--secondary');
+    button.classList.add('article-feed__item-toggle');
     return button;
   }
   const itemBodies = doc.querySelectorAll('[data-behaviour="collapse_to_teaser"]');
@@ -11,11 +11,21 @@
 
     const more = buildToggle();
     more.innerHTML = 'See more <span aria-hidden="true">+</span>';
-    teaser.appendChild(more);
+    if(teaser.lastElementChild?.nodeName === 'P') {
+      teaser.lastElementChild.appendChild(more);
+    } else {
+      teaser.appendChild(more);
+    }
 
     const less = buildToggle();
     less.innerHTML = 'See less <span aria-hidden="true">\u2212</span>';
-    fullText.insertBefore(less, fullText.querySelector('.article-feed__item__read_more'));
+    const elementBeforeReadSource = fullText.querySelector('.article-feed__item__read_more')?.previousElementSibling;
+    if(elementBeforeReadSource?.nodeName === 'P') {
+      elementBeforeReadSource.appendChild(doc.createTextNode(' '));
+      elementBeforeReadSource.appendChild(less);
+    } else {
+      fullText.insertBefore(less, fullText.querySelector('.article-feed__item__read_more'));
+    }
 
     teaser.classList.remove('hidden');
     fullText.classList.add('hidden');
