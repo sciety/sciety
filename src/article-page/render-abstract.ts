b1a3239d4fbf4fdef782e25c7015b3407c0ad314
Kevin Rutherford <kevin@rutherford-software.com>
2021-03-04 11:52:54 +0000

    Remove the margin below the cta #490

--- src/article-page/render-abstract.ts
+++ src/article-page/render-abstract.ts
@@ -4,8 +4,8 @@ import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 export const renderAbstract = (doi: Doi, abstract: HtmlFragment): HtmlFragment => toHtmlFragment(`
   <section class="article-abstract" role="doc-abstract">
     ${abstract}
-    <a href="https://doi.org/${doi.value}" class="full-article-button">
-      Read the full article
-    </a>
   </section>
+  <a href="https://doi.org/${doi.value}" class="full-article-button">
+    Read the full article
+  </a>
 `);
