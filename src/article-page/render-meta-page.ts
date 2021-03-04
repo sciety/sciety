e948a7d0421bc985fa4ef6acc259b63c754d5e58
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-03-04 11:05:47 +0000

    Add background #490

--- src/article-page/render-meta-page.ts
+++ src/article-page/render-meta-page.ts
@@ -17,35 +17,37 @@ export const renderMetaPage = (components: {
   saveArticle: string,
   tweetThis: string,
 }): HtmlFragment => toHtmlFragment(`
-<article class="sciety-grid sciety-grid--meta">
-  <header class="page-header page-header--article">
-    <h1>${components.articleDetails.title}</h1>
-    <div class="article-actions">
-      ${components.tweetThis}
-      ${components.saveArticle}
+<div class="meta-page__background--filler">
+  <article class="sciety-grid sciety-grid--meta">
+    <header class="page-header page-header--article">
+      <h1>${components.articleDetails.title}</h1>
+      <div class="article-actions">
+        ${components.tweetThis}
+        ${components.saveArticle}
+      </div>
+    </header>
+
+    <div class="article-tabs">
+      <h2 class="article-tabs__tab article-tabs__heading">Article</h2>
+      <a class="article-tabs__tab article-tabs__link" href="/articles/activity/${components.doi.value}" aria-label="Discover article activity">Activity</a>
     </div>
-  </header>
 
-  <div class="article-tabs">
-    <h2 class="article-tabs__tab article-tabs__heading">Article</h2>
-    <a class="article-tabs__tab article-tabs__link" href="/articles/activity/${components.doi.value}" aria-label="Discover article activity">Activity</a>
-  </div>
+    <div class="main-content main-content--article">
+      <section class="article-meta">
+        <h3>Authors</h3>
+        <ol aria-label="Authors of this article" class="article-author-list" role="list">
+          ${components.articleDetails.authors.map((author) => `<li>${author}</li>`).join('')}
+        </ol>
+        <ul aria-label="Publication details" class="article-meta-data-list" role="list">
+          <li>
+            <a href="https://doi.org/${components.doi.value}">https://doi.org/${components.doi.value}</a>
+          </li>
+        </ul>
+      </section>
 
-  <div class="main-content main-content--article">
-    <section class="article-meta">
-      <h3>Authors</h3>
-      <ol aria-label="Authors of this article" class="article-author-list" role="list">
-        ${components.articleDetails.authors.map((author) => `<li>${author}</li>`).join('')}
-      </ol>
-      <ul aria-label="Publication details" class="article-meta-data-list" role="list">
-        <li>
-          <a href="https://doi.org/${components.doi.value}">https://doi.org/${components.doi.value}</a>
-        </li>
-      </ul>
-    </section>
+      ${components.abstract}
+    </div>
 
-    ${components.abstract}
-  </div>
-
-</article>
+  </article>
+</div>
     `);
