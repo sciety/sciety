8bff6d6d2eeb5664822a79ae3ca7a6cf542dbd10
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-03-05 08:38:14 +0000

    string replace editorial-communities with groups in static file requests #484

--- src/http/load-static-file.ts
+++ src/http/load-static-file.ts
@@ -3,6 +3,7 @@ import { Middleware } from '@koa/router';
 import send from 'koa-send';
 
 export const loadStaticFile: Middleware = async (context, next) => {
-  await send(context, context.params.file, { root: path.resolve(__dirname, '../../static') });
+  const file = context.params.file.replace('editorial-communities', 'groups');
+  await send(context, file, { root: path.resolve(__dirname, '../../static') });
   await next();
 };
