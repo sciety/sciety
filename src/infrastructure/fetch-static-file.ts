1b8e0ca613deb81bad019c04ce878ff418b68c19
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-03-05 09:18:58 +0000

    Use an IO logger #380

--- src/infrastructure/fetch-static-file.ts
+++ src/infrastructure/fetch-static-file.ts
@@ -1,20 +1,26 @@
 import * as fs from 'fs';
 import path from 'path';
+import * as IO from 'fp-ts/IO';
 import * as RTE from 'fp-ts/ReaderTaskEither';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
-import { Logger } from './logger';
+import { flow, pipe } from 'fp-ts/function';
+import * as L from './logger';
 
-type FetchStaticFile = (filename: string) => RTE.ReaderTaskEither<Logger, 'not-found' | 'unavailable', string>;
+type FetchStaticFile = (filename: string) => RTE.ReaderTaskEither<L.LoggerIO, 'not-found' | 'unavailable', string>;
 
 export const fetchStaticFile: FetchStaticFile = (filename) => (logger) => pipe(
   path.resolve(__dirname, '..', '..', 'static', filename),
   TE.taskify(fs.readFile),
+  TE.swap,
+  TE.chainFirst(flow(
+    (error) => ({ error }),
+    L.error('Failed to read file'),
+    IO.chain(logger),
+    TE.fromIO,
+  )),
+  TE.swap,
   TE.bimap(
-    (error) => {
-      logger('error', 'Failed to fetch static file', { error });
-      return error.code === 'ENOENT' ? 'not-found' : 'unavailable';
-    },
-    (text) => text.toString(),
+    (error) => (error.code === 'ENOENT' ? 'not-found' : 'unavailable'),
+    String,
   ),
 );
--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -102,7 +102,7 @@ export const createInfrastructure = (): TE.TaskEither<unknown, Adapters> => pipe
           fetchHypothesisAnnotation(getJson, logger),
           fetchNcrcReview(logger),
         ),
-        fetchStaticFile: (...args) => fetchStaticFile(...args)(logger),
+        fetchStaticFile: (...args) => fetchStaticFile(...args)(loggerIO(logger)),
         searchEuropePmc: searchEuropePmc(getJsonWithRetries, logger),
         editorialCommunities,
         getEditorialCommunity: editorialCommunities.lookup,
