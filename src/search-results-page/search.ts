d09766426e832ce308d9adcbe4178e63587284a4
Will Byrne <w.byrne@elifesciences.org>
2021-03-08 15:11:52 +0000

    Add intenal GetGroup #487

--- src/search-results-page/search.ts
+++ src/search-results-page/search.ts
@@ -1,3 +1,4 @@
+import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
@@ -5,8 +6,8 @@ import * as TE from 'fp-ts/TaskEither';
 import { constant, flow, pipe } from 'fp-ts/function';
 import { ArticleSearchResult } from './render-search-result';
 import { SearchResults } from './render-search-results';
-import { bootstrapEditorialCommunities } from '../data/bootstrap-editorial-communities';
 import { Doi } from '../types/doi';
+import { Group } from '../types/group';
 import { GroupId } from '../types/group-id';
 import { toHtmlFragment } from '../types/html-fragment';
 import { ReviewId } from '../types/review-id';
@@ -17,6 +18,7 @@ type OriginalSearchResults = {
   total: number,
 };
 
+type GetGroup = (editorialCommunityId: GroupId) => T.Task<O.Option<Group>>;
 type FindArticles = (query: string) => TE.TaskEither<'unavailable', OriginalSearchResults>;
 
 export type FindReviewsForArticleDoi = (articleDoi: Doi) => T.Task<ReadonlyArray<{
@@ -27,24 +29,27 @@ export type FindReviewsForArticleDoi = (articleDoi: Doi) => T.Task<ReadonlyArray
 type Search = (query: string) => TE.TaskEither<'unavailable', SearchResults>;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const getDescription = (groupId: GroupId) => pipe(
-  bootstrapEditorialCommunities,
-  RA.lookup(2),
-  O.chain(flow((group) => group.shortDescription, O.fromNullable)),
-  O.getOrElse(constant('')),
-  toHtmlFragment,
-  sanitise,
-);
-
-const constructGroupResult = () => ({
-  _tag: 'Group' as const,
-  link: '/groups/53ed5364-a016-11ea-bb37-0242ac130002',
+const getGroup: GetGroup = (groupId: GroupId) => T.of(O.some({
+  id: new GroupId('53ed5364-a016-11ea-bb37-0242ac130002'),
   name: 'PeerJ',
-  description: getDescription(new GroupId('53ed5364-a016-11ea-bb37-0242ac130002')),
   avatarPath: '/static/groups/peerj--53ed5364-a016-11ea-bb37-0242ac130002.jpg',
-  followerCount: 47,
-  reviewCount: 835,
-});
+  descriptionPath: 'peerj--53ed5364-a016-11ea-bb37-0242ac130002.md',
+  shortDescription: 'PeerJ is an open access publisher of 7 peer-reviewed journals, and an editorial community of over 2000 Academic Editors and Advisors, and tens of thousands of authors and reviewers.',
+}));
+
+const constructGroupResult = (groupId: GroupId) => pipe(
+  groupId,
+  getGroup,
+  T.map(E.fromOption(() => 'not-found')),
+  TE.map((group) => ({
+    ...group,
+    description: sanitise(toHtmlFragment(group.shortDescription ?? '')),
+    _tag: 'Group' as const,
+    link: '/groups/53ed5364-a016-11ea-bb37-0242ac130002',
+    followerCount: 47,
+    reviewCount: 835,
+  })),
+);
 
 const findGroups = (query: string): ReadonlyArray<GroupId> => pipe(
   query,
@@ -59,14 +64,17 @@ const addGroupResults = (
   query: string,
 ) => (
   searchResults: SearchResults,
-) => pipe(
+): TE.TaskEither<never, SearchResults> => pipe(
   query,
   findGroups,
-  RA.map(constructGroupResult),
-  (hardcodedSearchResults) => ({
+  T.traverseArray(constructGroupResult),
+  T.map(RA.separate),
+  T.map(({ right }) => right),
+  T.map((hardcodedSearchResults) => ({
     total: searchResults.total + hardcodedSearchResults.length,
     items: [...hardcodedSearchResults, ...searchResults.items],
-  }),
+  })),
+  T.map(E.right),
 );
 
 export const search = (
@@ -99,5 +107,5 @@ export const search = (
     ),
     TE.rightTask,
   )),
-  TE.map(addGroupResults(query)),
+  TE.chainW(addGroupResults(query)),
 );
