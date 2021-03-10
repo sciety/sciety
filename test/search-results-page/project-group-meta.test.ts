35bd2f971164fa064a0d4bb94b18ee26f51f4957
David Moulton <d.moulton@elifesciences.org>
2021-03-10 14:32:47 +0000

    Add todo tests for projection #487

---
+++ test/search-results-page/project-group-meta.test.ts
@@ -0,0 +1,19 @@
+describe('project-group-meta', () => {
+  describe('the follower count', () => {
+    it.todo('No events');
+
+    it.todo('Five events');
+
+    it.todo('Count for my group distinguished from counts for other groups');
+
+    it.todo('Take account of unfollow events');
+  });
+
+  describe('the review count', () => {
+    it.todo('No events');
+
+    it.todo('Five events');
+
+    it.todo('Count for my article distinguished from counts for other articles');
+  });
+});
