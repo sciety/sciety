7504fa824870314d15c846ae8b4000410a10d531
Will Byrne <w.byrne@elifesciences.org>
2021-03-11 14:09:16 +0000

    Add no result search test #487

--- test/search-results-page/find-groups.test.ts
+++ test/search-results-page/find-groups.test.ts
@@ -2,19 +2,42 @@ import * as TE from 'fp-ts/TaskEither';
 import { findGroups } from '../../src/search-results-page/find-groups';
 import { GroupId } from '../../src/types/group-id';
 
+type Group = {
+  id: GroupId,
+  avatarPath?: string,
+  descriptionPath?: string,
+  name?: string,
+  shortDescription?: string,
+};
+
+const constructGroup = ({
+  id,
+  avatarPath = '',
+  descriptionPath = '',
+  name = '',
+  shortDescription = '',
+}: Group) => ({
+  id,
+  avatarPath,
+  descriptionPath,
+  name,
+  shortDescription,
+});
+
 describe('find-groups', () => {
   describe('provided a valid group name', () => {
     it('returns an array containing the groupId', async () => {
-      const group = {
-        id: new GroupId('12345'),
-        avatarPath: '',
-        descriptionPath: '',
-        name: 'My Group',
-        shortDescription: '',
-      };
-      const result = await findGroups(() => TE.right(''), [group])('My Group')();
+      const result = await findGroups(() => TE.right(''), [constructGroup({ id: new GroupId('12345'), name: 'My Group' })])('My Group')();
 
       expect(result).toStrictEqual([new GroupId('12345')]);
+    });
+  });
+
+  describe('provided an invalid group name', () => {
+    it('returns an empty array', async () => {
+      const result = await findGroups(() => TE.right(''), [constructGroup({ id: new GroupId('12345'), name: 'My Group' })])('Other Group')();
+
+      expect(result).toStrictEqual([]);
     });
   });
 });
