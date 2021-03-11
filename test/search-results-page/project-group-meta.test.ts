489ff4148470c25248d97672d95a249a50967dff
Will Byrne <w.byrne@elifesciences.org>
2021-03-11 11:51:34 +0000

    Implement project-group-meta tests #487

--- test/search-results-page/project-group-meta.test.ts
+++ test/search-results-page/project-group-meta.test.ts
@@ -1,19 +1,113 @@
+import { projectGroupMeta } from '../../src/search-results-page/project-group-meta';
+import { Doi } from '../../src/types/doi';
+import {
+  editorialCommunityReviewedArticle,
+  userFollowedEditorialCommunity,
+  userUnfollowedEditorialCommunity,
+} from '../../src/types/domain-events';
+import { GroupId } from '../../src/types/group-id';
+import { toUserId } from '../../src/types/user-id';
+
 describe('project-group-meta', () => {
   describe('the follower count', () => {
-    it.todo('No events');
+    describe('with no events', () => {
+      it('returns a zero count', () => {
+        const { followerCount } = projectGroupMeta([])(new GroupId('123'));
 
-    it.todo('Five events');
+        expect(followerCount).toBe(0);
+      });
+    });
 
-    it.todo('Count for my group distinguished from counts for other groups');
+    describe('with five events', () => {
+      it('returns a count of 5', () => {
+        const groupId = new GroupId('123');
+        const events = [
+          userFollowedEditorialCommunity(toUserId('1'), groupId),
+          userFollowedEditorialCommunity(toUserId('2'), groupId),
+          userFollowedEditorialCommunity(toUserId('3'), groupId),
+          userFollowedEditorialCommunity(toUserId('4'), groupId),
+          userFollowedEditorialCommunity(toUserId('5'), groupId),
+        ];
+        const { followerCount } = projectGroupMeta(events)(groupId);
 
-    it.todo('Take account of unfollow events');
+        expect(followerCount).toBe(5);
+      });
+    });
+
+    describe('with other groups follow events', () => {
+      it('returns a count of 2', () => {
+        const groupId = new GroupId('123');
+        const otherGroupId = new GroupId('321');
+        const events = [
+          userFollowedEditorialCommunity(toUserId('1'), groupId),
+          userFollowedEditorialCommunity(toUserId('2'), groupId),
+          userFollowedEditorialCommunity(toUserId('3'), otherGroupId),
+          userFollowedEditorialCommunity(toUserId('4'), otherGroupId),
+          userFollowedEditorialCommunity(toUserId('5'), otherGroupId),
+        ];
+        const { followerCount } = projectGroupMeta(events)(groupId);
+
+        expect(followerCount).toBe(2);
+      });
+    });
+
+    describe('with follow and unfollow events', () => {
+      it('returns the correct count', () => {
+        const groupId = new GroupId('123');
+        const events = [
+          userFollowedEditorialCommunity(toUserId('1'), groupId),
+          userFollowedEditorialCommunity(toUserId('2'), groupId),
+          userFollowedEditorialCommunity(toUserId('3'), groupId),
+          userUnfollowedEditorialCommunity(toUserId('1'), groupId),
+          userUnfollowedEditorialCommunity(toUserId('3'), groupId),
+        ];
+        const { followerCount } = projectGroupMeta(events)(groupId);
+
+        expect(followerCount).toBe(1);
+      });
+    });
   });
 
   describe('the review count', () => {
-    it.todo('No events');
+    describe('with no events', () => {
+      it('returns a zero count', () => {
+        const { reviewCount } = projectGroupMeta([])(new GroupId('123'));
 
-    it.todo('Five events');
+        expect(reviewCount).toBe(0);
+      });
+    });
 
-    it.todo('Count for my article distinguished from counts for other articles');
+    describe('with five events', () => {
+      it('returns a count of 5', () => {
+        const groupId = new GroupId('123');
+        const events = [
+          editorialCommunityReviewedArticle(groupId, new Doi('10.1111/12345'), new Doi('10.1111/11111')),
+          editorialCommunityReviewedArticle(groupId, new Doi('10.1111/12345'), new Doi('10.1111/22222')),
+          editorialCommunityReviewedArticle(groupId, new Doi('10.1111/12345'), new Doi('10.1111/33333')),
+          editorialCommunityReviewedArticle(groupId, new Doi('10.1111/12345'), new Doi('10.1111/44444')),
+          editorialCommunityReviewedArticle(groupId, new Doi('10.1111/12345'), new Doi('10.1111/55555')),
+        ];
+        const { reviewCount } = projectGroupMeta(events)(groupId);
+
+        expect(reviewCount).toBe(5);
+      });
+    });
+
+    describe('with reviewd from different groups', () => {
+      it('returns a count of the passed in group', () => {
+        const groupId = new GroupId('123');
+        const otherGroupId = new GroupId('321');
+        const events = [
+          editorialCommunityReviewedArticle(groupId, new Doi('10.1111/12345'), new Doi('10.1111/11111')),
+          editorialCommunityReviewedArticle(groupId, new Doi('10.1111/12345'), new Doi('10.1111/22222')),
+          editorialCommunityReviewedArticle(otherGroupId, new Doi('10.1111/12345'), new Doi('10.1111/33333')),
+          editorialCommunityReviewedArticle(otherGroupId, new Doi('10.1111/12345'), new Doi('10.1111/44444')),
+          editorialCommunityReviewedArticle(otherGroupId, new Doi('10.1111/12345'), new Doi('10.1111/55555')),
+        ];
+        const { reviewCount } = projectGroupMeta(events)(groupId);
+
+        expect(reviewCount).toBe(2);
+      });
+    });
   });
 });
