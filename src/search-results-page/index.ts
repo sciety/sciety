eadfcb60518e53fe27a25d8832e44fd78140a0d6
Kevin Rutherford <kevin@rutherford-software.com>
2021-03-16 13:05:30 +0000

    Extract testable functions #487

--- src/search-results-page/index.ts
+++ src/search-results-page/index.ts
@@ -1,110 +1,13 @@
 import { sequenceS } from 'fp-ts/Apply';
-import * as E from 'fp-ts/Either';
-import * as O from 'fp-ts/Option';
-import * as RA from 'fp-ts/ReadonlyArray';
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import { ArticleItem, GroupItem } from './data-types';
+import {
+  fetchExtraDetails, FindReviewsForArticleDoi, GetAllEvents, GetGroup,
+} from './fetch-extra-details';
 import { FindArticles, findMatchingArticles } from './find-matching-articles';
 import { FindGroups, findMatchingGroups } from './find-matching-groups';
 import { renderErrorPage, RenderPage, renderPage } from './render-page';
-import { ArticleViewModel, GroupViewModel } from './render-search-result';
-import { SearchResults } from './render-search-results';
-import { updateGroupMeta } from './update-group-meta';
-import { Doi } from '../types/doi';
-import { DomainEvent } from '../types/domain-events';
-import { Group } from '../types/group';
-import { GroupId } from '../types/group-id';
-import { toHtmlFragment } from '../types/html-fragment';
-import { ReviewId } from '../types/review-id';
-import { sanitise } from '../types/sanitised-html-fragment';
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-type Matches = {
-  query: string,
-  groups: ReadonlyArray<GroupItem>,
-  articles: {
-    items: ReadonlyArray<ArticleItem>,
-    total: number,
-  },
-};
-
-const selectSubsetToDisplay = (limit: number) => (state: Matches): LimitedSet => ({
-  ...state,
-  availableMatches: state.groups.length + state.articles.total,
-  itemsToDisplay: pipe(
-    [...state.groups, ...state.articles.items],
-    RA.takeLeft(limit),
-  ),
-});
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-type GetGroup = (editorialCommunityId: GroupId) => T.Task<O.Option<Group>>;
-
-type GetAllEvents = T.Task<ReadonlyArray<DomainEvent>>;
-
-type FindReviewsForArticleDoi = (articleDoi: Doi) => T.Task<ReadonlyArray<{
-  reviewId: ReviewId,
-  editorialCommunityId: GroupId,
-}>>;
-
-const populateArticleViewModel = (findReviewsForArticleDoi: FindReviewsForArticleDoi) => (item: ArticleItem): TE.TaskEither<'not-found', ArticleViewModel> => pipe(
-  item.doi,
-  findReviewsForArticleDoi, // TODO: Find reviewsForArticleDoi should return a TaskEither
-  T.map((reviews) => ({
-    ...item,
-    reviewCount: reviews.length,
-  })),
-  (f) => TE.rightTask<'not-found', ArticleViewModel>(f),
-
-);
-
-const populateGroupViewModel = (getGroup: GetGroup, getAllEvents: GetAllEvents) => (item:GroupItem) => pipe(
-  item.id,
-  getGroup,
-  T.map(E.fromOption(() => 'not-found' as const)),
-  TE.chainW((group) => pipe(
-    getAllEvents,
-    T.map(RA.reduce({ reviewCount: 0, followerCount: 0 }, updateGroupMeta(group.id))),
-    T.map((meta) => ({
-      _tag: 'Group' as const,
-      ...group,
-      ...meta,
-      description: sanitise(toHtmlFragment(group.shortDescription)),
-    })),
-    TE.rightTask,
-  )),
-);
-
-const fetchItemDetails = (ports: Ports) => (item: GroupItem | ArticleItem): TE.TaskEither<'not-found', GroupViewModel | ArticleViewModel> => {
-  if (item._tag === 'Article') {
-    return populateArticleViewModel(ports.findReviewsForArticleDoi)(item);
-  }
-
-  return populateGroupViewModel(ports.getGroup, ports.getAllEvents)(item);
-};
-
-type LimitedSet = {
-  query: string,
-  availableMatches: number,
-  itemsToDisplay: ReadonlyArray<GroupItem | ArticleItem>,
-};
-
-const fetchExtraDetails = (ports: Ports) => (state: LimitedSet): TE.TaskEither<never, SearchResults> => pipe(
-  state.itemsToDisplay,
-  T.traverseArray(fetchItemDetails(ports)),
-  T.map(RA.rights),
-  T.map((itemsToDisplay) => ({
-    ...state,
-    itemsToDisplay,
-  })),
-  TE.rightTask,
-);
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import { selectSubsetToDisplay } from './select-subset-to-display';
 
 type Ports = {
   findGroups: FindGroups,
