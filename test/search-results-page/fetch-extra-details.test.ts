bc2c9704c9e02eda7b41268f42daafff7fd5de83
Kevin Rutherford <kevin@rutherford-software.com>
2021-03-18 08:32:23 +0000

    Outline some tests #487

---
+++ test/search-results-page/fetch-extra-details.test.ts
@@ -0,0 +1,15 @@
+describe('fetch-extra-details', () => {
+  describe('given a found article', () => {
+    it.todo('returns a correct view model');
+  });
+
+  describe('given a found group', () => {
+    describe('when the details can be fetched', () => {
+      it.todo('returns a correct view model');
+    });
+
+    describe('when the details cannot be fetched', () => {
+      it.todo('removes the group from the list');
+    });
+  });
+});
