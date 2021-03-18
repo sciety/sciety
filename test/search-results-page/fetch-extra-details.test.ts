f1a14e555fc47c2f16cc7243810e34368fca8620
Kevin Rutherford <kevin@rutherford-software.com>
2021-03-18 08:52:57 +0000

    Implement a todo test #487

--- test/search-results-page/fetch-extra-details.test.ts
+++ test/search-results-page/fetch-extra-details.test.ts
@@ -1,6 +1,48 @@
+import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
+import { fetchExtraDetails } from '../../src/search-results-page/fetch-extra-details';
+import { Doi } from '../../src/types/doi';
+import { GroupId } from '../../src/types/group-id';
+import { shouldNotBeCalled } from '../should-not-be-called';
+
 describe('fetch-extra-details', () => {
   describe('given a found article', () => {
-    it.todo('returns a correct view model');
+    it('returns a correct view model', async () => {
+      const ports = {
+        findReviewsForArticleDoi: () => T.of([
+          {
+            reviewId: new Doi('10.1101/111111'),
+            editorialCommunityId: new GroupId('my-group'),
+          },
+        ]),
+        getAllEvents: shouldNotBeCalled,
+        getGroup: shouldNotBeCalled,
+      };
+      const matches = {
+        query: '',
+        availableMatches: 5,
+        itemsToDisplay: [
+          {
+            _tag: 'Article' as const,
+            doi: new Doi('10.1101/222222'),
+            title: '',
+            authors: '',
+            postedDate: new Date(),
+          },
+        ],
+      };
+      const viewModel = await fetchExtraDetails(ports)(matches)();
+
+      expect(viewModel).toStrictEqual(E.right({
+        query: '',
+        availableMatches: 5,
+        itemsToDisplay: [
+          expect.objectContaining({
+            reviewCount: 1,
+          }),
+        ],
+      }));
+    });
   });
 
   describe('given a found group', () => {
