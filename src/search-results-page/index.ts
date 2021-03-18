b0bb491617e7324b5ec1e6b57edede6762833c6b
Kevin Rutherford <kevin@rutherford-software.com>
2021-03-18 08:19:24 +0000

    Inline a couple of simple pipes #487

--- src/search-results-page/index.ts
+++ src/search-results-page/index.ts
@@ -1,13 +1,22 @@
 import { sequenceS } from 'fp-ts/Apply';
+import * as RA from 'fp-ts/ReadonlyArray';
+import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { MatchedArticle } from './data-types';
 import {
   fetchExtraDetails, FindReviewsForArticleDoi, GetAllEvents, GetGroup,
 } from './fetch-extra-details';
-import { FindArticles, findMatchingArticles } from './find-matching-articles';
-import { FindGroups, findMatchingGroups } from './find-matching-groups';
 import { renderErrorPage, RenderPage, renderPage } from './render-page';
 import { selectSubsetToDisplay } from './select-subset-to-display';
+import { GroupId } from '../types/group-id';
+
+type FindArticles = (query: string) => TE.TaskEither<'unavailable', {
+  items: ReadonlyArray<MatchedArticle>,
+  total: number,
+}>;
+
+type FindGroups = (q: string) => T.Task<ReadonlyArray<GroupId>>;
 
 type Ports = {
   findGroups: FindGroups,
@@ -26,8 +35,26 @@ type SearchResultsPage = (params: Params) => ReturnType<RenderPage>;
 export const searchResultsPage = (ports: Ports): SearchResultsPage => (params) => pipe(
   {
     query: TE.right(params.query),
-    articles: findMatchingArticles(ports.searchEuropePmc)(params.query),
-    groups: findMatchingGroups(ports.findGroups)(params.query),
+    articles: pipe(
+      params.query,
+      ports.searchEuropePmc,
+      TE.map((results) => ({
+        ...results,
+        items: results.items.map((article) => ({
+          _tag: 'Article' as const,
+          ...article,
+        })),
+      })),
+    ),
+    groups: pipe(
+      params.query,
+      ports.findGroups, // TODO: should only ask for 10 of n; should return a TE
+      T.map(RA.map((groupId) => ({
+        _tag: 'Group' as const,
+        id: groupId,
+      }))),
+      TE.rightTask,
+    ),
   },
   sequenceS(TE.taskEither),
   TE.map(selectSubsetToDisplay(10)),
