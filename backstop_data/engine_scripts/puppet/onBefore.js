6bbd62a19f200baecc6a079ae9ed3d4aacd1be46
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-03-18 11:16:37 +0000

    fix cookie consent #572

--- backstop_data/engine_scripts/puppet/onBefore.js
+++ backstop_data/engine_scripts/puppet/onBefore.js
@@ -2,7 +2,7 @@ module.exports = async (page, scenario, vp) => {
 
   if (!scenario.showCookieConsent) {
     const cookieConsentCookie = {
-      url: "https://.sciety.org",
+      url: "http://sciety_app",
       name: "cookieconsent_status",
       value: "deny",
     }
