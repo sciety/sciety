a514e9553dded119fd0aad5127de2a16723ab596
Will Byrne <w.byrne@elifesciences.org>
2021-03-22 11:06:32 +0000

    Style the feedback button #580

--- src/menu-page/menu-page.ts
+++ src/menu-page/menu-page.ts
@@ -5,12 +5,12 @@ export const menuPage = {
   content: toHtmlFragment(`
   <nav class="navigation-menu">
     <h1 class="navigation-menu__title">Menu</h1>
-    <ul role="list">
+    <ul role="list" class="navigation-menu__links">
       <li><a href="/">Home</a></li>
       <li><a href="/about">About</a></li>
     </ul>
     <footer>
-      <a href="#feedback" class="button button--feedback">Feedback</a>
+      <a href="#feedback" class="navigation-menu__feedback_button">Feedback</a>
       <small class="navigation-menu__small_print">
         &copy; 2021 eLife Sciences Publications Ltd.
         <a href="/legal">Legal information</a>
