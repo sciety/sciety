fe08518d3102fb7c98e80a75db25a0d58dbc45db
David Moulton <d.moulton@elifesciences.org>
2021-03-22 10:06:14 +0000

    Remove unused classes #580

--- src/menu-page/menu-page.ts
+++ src/menu-page/menu-page.ts
@@ -3,17 +3,17 @@ import { toHtmlFragment } from '../types/html-fragment';
 export const menuPage = {
   title: 'Menu',
   content: toHtmlFragment(`
-  <nav id="fly-out-menu" class="navigation-menu">
-    <h1 class="menu-title">Menu</h1>
-    <ul role="list" class="fly-out-menu-list">
+  <nav class="navigation-menu">
+    <h1>Menu</h1>
+    <ul role="list">
       <li><a href="/">Home</a></li>
       <li><a href="/about">About</a></li>
     </ul>
-    <footer class="fly-out-menu__footer">
+    <footer>
       <a href="#feedback" class="button button--feedback">Feedback</a>
       <small>
         &copy; 2021 eLife Sciences Publications Ltd.
-        <a href="/legal" class="fly-out-menu__footer__link">Legal information</a>
+        <a href="/legal">Legal information</a>
       </small>
     </footer>
   </nav>
