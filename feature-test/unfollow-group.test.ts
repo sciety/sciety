49222c4ab748e04ffbdc61ac86902b8d2de7130b
Will Byrne <w.byrne@elifesciences.org>
2021-03-26 14:47:10 +0000

    Add unfollow taiko test #595

---
+++ feature-test/unfollow-group.test.ts
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+import {
+  $, button, click, closeBrowser, goto, openBrowser, text, within,
+} from 'taiko';
+import { authenticateViaTwitter } from './utilities';
+
+describe('unfollow a group', () => {
+  beforeEach(async () => {
+    dotenv.config();
+    await openBrowser();
+  });
+
+  afterEach(closeBrowser);
+
+  describe('when logged in', () => {
+    beforeEach(async () => {
+      await goto('localhost:8080/');
+      await click('Got it!');
+      await click('Log in');
+      await authenticateViaTwitter();
+    });
+
+    describe('from the home page', () => {
+      it('removes the group to my profile page', async () => {
+        await goto('localhost:8080');
+        await click(button({ 'aria-label': 'Follow PeerJ' }));
+        await click(button({ 'aria-label': 'Unfollow PeerJ' }));
+        await click('My profile');
+        const groupExists = await text('PeerJ', within($('.followed-groups'))).exists();
+
+        expect(groupExists).toBe(false);
+      });
+    });
+  });
+});
