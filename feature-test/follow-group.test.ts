4296c66b598a15125ac6b3f5333066e6e10cbe05
Kevin Rutherford <kevin@rutherford-software.com>
2021-03-26 09:49:43 +0000

    Add a Taiko test that would have caught the defect #595

---
+++ feature-test/follow-group.test.ts
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv';
+import {
+  $, button, click, closeBrowser, goto, openBrowser, text, within,
+} from 'taiko';
+import { authenticateViaTwitter } from './utilities';
+
+describe('follow a group', () => {
+  beforeEach(async () => {
+    dotenv.config();
+    await openBrowser();
+  });
+
+  afterEach(closeBrowser);
+
+  describe('when logged in', () => {
+    beforeEach(async () => {
+      await goto('localhost:8080/');
+      await click('Log in');
+      await authenticateViaTwitter();
+    });
+
+    describe('from the home page', () => {
+      it('adds the group to my profile page', async () => {
+        await goto('localhost:8080');
+        await click(button({ 'aria-label': 'Follow NCRC' }));
+        await click('My profile');
+        const groupExists = await text('NCRC', within($('.followed-groups'))).exists();
+
+        expect(groupExists).toBe(true);
+      });
+    });
+
+    describe('from the group page', () => {
+      it.todo('adds the group to my profile page');
+    });
+
+    describe('from the profile of another user', () => {
+      it.todo('adds the group to my profile page');
+    });
+  });
+});
