0e372cd110b767794ea51b0d69d0360b6a71682c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-03-26 15:45:43 +0000

    extract menuFooter #593

--- src/menu-page/menu-page.ts
+++ src/menu-page/menu-page.ts
@@ -1,5 +1,5 @@
 import * as O from 'fp-ts/Option';
-import { menuItems } from '../shared-components/menu-items';
+import { menuFooter, menuItems } from '../shared-components/menu-items';
 import { toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 import { User } from '../types/user';
@@ -10,13 +10,7 @@ export const menuPage = (user: O.Option<User>): Page => ({
   <nav class="navigation-menu">
     <h1 class="navigation-menu__title">Menu</h1>
     ${menuItems(user)}
-    <footer class="navigation-menu__footer">
-      <a href="https://eepurl.com/g7qqcv" class="navigation-menu__feedback_button">Feedback</a>
-      <small class="navigation-menu__small_print">
-        &copy; 2021 eLife Sciences Publications Ltd.
-        <a href="/legal">Legal information</a>
-      </small>
-    </footer>
+    ${menuFooter}
   </nav>
   `),
 });
