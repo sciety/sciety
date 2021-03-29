e97509d4ac194f33378f365171e91757e3157838
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-03-29 11:52:44 +0100

    extract utility-bar sass #593

--- src/shared-components/utility-bar.ts
+++ src/shared-components/utility-bar.ts
@@ -3,24 +3,24 @@ import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { User } from '../types/user';
 
 const logInMenuItem = () => toHtmlFragment(`
-  <li class="menu-page__nav_list_item">
-    <a href="/log-in" class="menu-page__nav_list_link_button">Log in</a>
+  <li class="utility-bar__list_item">
+    <a href="/log-in" class="utility-bar__list_link_button">Log in</a>
   </li>
 `);
 
 const logOutMenuItem = () => toHtmlFragment(`
-  <li class="menu-page__nav_list_item">
-    <a href="/log-out" class="menu-page__nav_list_link_button">Log out</a>
+  <li class="utility-bar__list_item">
+    <a href="/log-out" class="utility-bar__list_link_button">Log out</a>
   </li>
 `);
 
 export const utilityBar = (user: O.Option<User>): HtmlFragment => toHtmlFragment(`
-  <nav class="menu-page__nav" aria-describedby="application-utilities">
+  <nav class="menu-page__nav utility-bar" aria-describedby="application-utilities">
       <div id="application-utilities" hidden>Sciety application utilities</div>
-      <ul class="menu-page__nav_list" role="list">
-        <li class="menu-page__nav_list_item menu-page__nav_list_item--search">
+      <ul class="utility-bar__list" role="list">
+        <li class="utility-bar__list_item utility-bar__list_item--search">
           <a href="/search">
-            <img src="/static/images/search-icon.svg" alt="Search" class="menu-page__nav_list__search_icon">
+            <img src="/static/images/search-icon.svg" alt="Search" class="utility-bar__list__search_icon">
           </a>
         </li>
         ${O.fold(logInMenuItem, logOutMenuItem)(user)}
