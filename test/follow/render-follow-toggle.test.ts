7a13209222995e6670f3dbbbe6a19834d24056b5
Giorgio Sironi <g.sironi@elifesciences.org>
2021-03-29 14:25:30 +0100

    Specify aria-label with tests #595

--- test/follow/render-follow-toggle.test.ts
+++ test/follow/render-follow-toggle.test.ts
@@ -4,26 +4,40 @@ import { GroupId } from '../../src/types/group-id';
 
 describe('render-follow-toggle', () => {
   describe('when the community is currently followed', () => {
+    const groupId = new GroupId('1234');
+    const rendered = JSDOM.fragment(
+      renderFollowToggle(groupId, 'My Group')(true),
+    );
+    const button = rendered.querySelector('button');
+
     it('shows an unfollow button', async () => {
-      const groupId = new GroupId('1234');
-      const rendered = JSDOM.fragment(
-        renderFollowToggle(groupId, 'My Group')(true),
-      );
-      const button = rendered.querySelector('button');
       const buttonText = button?.textContent;
 
       expect(buttonText).toContain('Unfollow');
     });
+
+    it('announces the action', async () => {
+      const ariaLabel = button?.getAttribute('aria-label');
+
+      expect(ariaLabel).toStrictEqual('Unfollow My Group');
+    });
   });
 
   describe('when the community is not currently followed', () => {
+    const groupId = new GroupId('1234');
+    const rendered = JSDOM.fragment(renderFollowToggle(groupId, 'My Group')(false));
+    const button = rendered.querySelector('button');
+
     it('shows a follow button', async () => {
-      const groupId = new GroupId('1234');
-      const rendered = JSDOM.fragment(renderFollowToggle(groupId, 'My Group')(false));
-      const button = rendered.querySelector('button');
       const buttonText = button?.textContent;
 
       expect(buttonText).toContain('Follow');
+    });
+
+    it('announces the action', async () => {
+      const ariaLabel = button?.getAttribute('aria-label');
+
+      expect(ariaLabel).toStrictEqual('Follow My Group');
     });
   });
 });
