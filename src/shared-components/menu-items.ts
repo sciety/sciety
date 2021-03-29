eaeb67c9042e54873e2a5d3460b390cd8c5fc71f
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-03-29 09:48:08 +0100

    Add profile icon #593

--- src/shared-components/menu-items.ts
+++ src/shared-components/menu-items.ts
@@ -5,7 +5,7 @@ import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { User } from '../types/user';
 
 const myProfileMenuItem = (user: User) => toHtmlFragment(`
-  <li><a href="/users/${user.id}" class="site-menu__link"><span>My profile</span></a></li>
+  <li><a href="/users/${user.id}" class="site-menu__link site-menu__link--profile"><span>My profile</span></a></li>
 `);
 
 export const menuItems = (user: O.Option<User>): HtmlFragment => toHtmlFragment(`
