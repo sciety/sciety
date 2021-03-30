d441c52be2c0f47cae55296f9ffba4cd8a6fa61a
Giorgio Sironi <g.sironi@elifesciences.org>
2021-03-30 14:47:23 +0100

    Only save constructed GroupId to session #595

--- src/follow/save-follow-command.ts
+++ src/follow/save-follow-command.ts
@@ -1,11 +1,24 @@
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
 import { Middleware } from 'koa';
 import { sessionGroupProperty } from './finish-follow-command';
 import { groupProperty } from './follow-handler';
+import * as GroupId from '../types/group-id';
 
 export const saveFollowCommand = (): Middleware => (
   async (context, next) => {
-    context.session.command = 'follow';
-    context.session[sessionGroupProperty] = context.request.body[groupProperty];
+    pipe(
+      context.request.body[groupProperty],
+      O.fromNullable,
+      O.chain(GroupId.fromString),
+      O.chainFirst((groupId) => {
+        // TODO: this side-effect could be captured differently
+        context.session.command = 'follow';
+        context.session[sessionGroupProperty] = groupId.toString();
+        return O.some('ok');
+      }),
+    );
+
     await next();
   }
 );
