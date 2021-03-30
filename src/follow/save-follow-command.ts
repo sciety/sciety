6847a8ea81739330c1c5d09adc700234f13a52e1
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-03-30 15:34:45 +0100

    extract saving to session to function #595

--- src/follow/save-follow-command.ts
+++ src/follow/save-follow-command.ts
@@ -1,10 +1,20 @@
 import * as O from 'fp-ts/Option';
-import { identity, pipe } from 'fp-ts/function';
+import { pipe } from 'fp-ts/function';
 import { StatusCodes } from 'http-status-codes';
-import { Middleware } from 'koa';
+import {
+  DefaultContext, DefaultState, Middleware, ParameterizedContext,
+} from 'koa';
 import { sessionGroupProperty } from './finish-follow-command';
 import { groupProperty } from './follow-handler';
 import * as GroupId from '../types/group-id';
+
+type Context = ParameterizedContext<DefaultState, DefaultContext>;
+
+// TODO: this side-effect could be captured differently
+const saveCommandAndGroupIdToSession = (context: Context) => (groupId: GroupId.GroupId): void => {
+  context.session.command = 'follow';
+  context.session[sessionGroupProperty] = groupId.toString();
+};
 
 export const saveFollowCommand = (): Middleware => (
   async (context, next) => {
@@ -12,15 +22,9 @@ export const saveFollowCommand = (): Middleware => (
       context.request.body[groupProperty],
       O.fromNullable,
       O.chain(GroupId.fromString),
-      O.chainFirst((groupId) => {
-        // TODO: this side-effect could be captured differently
-        context.session.command = 'follow';
-        context.session[sessionGroupProperty] = groupId.toString();
-        return O.some('ok');
-      }),
       O.fold(
         () => context.throw(StatusCodes.BAD_REQUEST),
-        identity,
+        saveCommandAndGroupIdToSession(context),
       ),
     );
 
