bc8ede74511ed80f3ff773f1afcf3449adaa9693
Will Byrne <w.byrne@elifesciences.org>
2021-03-30 11:26:12 +0100

    fpify script and handle errors #514

--- scripts/find-reviews-from-prereview.ts
+++ scripts/find-reviews-from-prereview.ts
@@ -1,11 +1,22 @@
 import axios from 'axios';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import { Json } from 'io-ts-types';
 
-void (async (): Promise<void> => {
-  const { data } = await axios.get<Json>(
-    'https://www.prereview.org/api/v2/preprints?limit=10',
-    { headers: { Accept: 'application/json' } },
-  );
-
-  process.stdout.write(JSON.stringify(data, undefined, 2));
-})();
+void pipe(
+  TE.tryCatch(
+    async () => axios.get<Json>(
+      'https://www.prereview.org/api/v2/preprints?limit=10',
+      { headers: { Accept: 'application/json' } },
+    ),
+    String,
+  ),
+  TE.bimap(
+    (error) => process.stderr.write(error),
+    ({ data }) => process.stdout.write(JSON.stringify(data, undefined, 2)),
+  ),
+  TE.fold(
+    () => process.exit(1),
+    () => process.exit(0),
+  ),
+)();
