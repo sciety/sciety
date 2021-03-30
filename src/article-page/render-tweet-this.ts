ae1b0c6864517f997017af7a16c01addfbe5f4d7
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-03-30 10:07:13 +0100

    Remove community from tweet text and remove spaces #609

--- src/article-page/render-tweet-this.ts
+++ src/article-page/render-tweet-this.ts
@@ -4,11 +4,11 @@ import { toHtmlFragment } from '../types/html-fragment';
 
 const constructLink = (doi: Doi) => `
   https://sciety.org/articles/${doi.value}?utm_source=twitter&utm_medium=social&utm_campaign=tweet_button
-`;
+`.trim();
 
 const constructTweetText = (link: string) => `
-  Check out this great article I found on @ScietyHQ, where the community can evaluate and curate the latest research: ${link}
-`;
+  Check out this great article I found on @ScietyHQ, where groups of trusted experts evaluate and curate the latest research: ${link}
+`.trim();
 
 const constructAnchor = (encodedTweetText: string) => `
   <a class="tweet-button" target="_blank" href="https://twitter.com/intent/tweet?text=${encodedTweetText}">
