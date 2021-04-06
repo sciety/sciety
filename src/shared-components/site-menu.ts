fbe66418168c9cba32fd47c2b2dc32d817b9fed4
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-04-06 10:52:02 +0100

    Align drawer text in IE 11 #617

--- src/shared-components/site-menu.ts
+++ src/shared-components/site-menu.ts
@@ -5,13 +5,13 @@ import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { User } from '../types/user';
 
 const myProfileMenuItem = (user: User) => toHtmlFragment(`
-  <li><a href="/users/${user.id}" class="site-menu__link site-menu__link--profile"><span>My profile</span></a></li>
+  <li><a href="/users/${user.id}" class="site-menu__link site-menu__link--profile"><span class="site-menu__link_text">My profile</span></a></li>
 `);
 
 export const siteMenuItems = (user: O.Option<User>): HtmlFragment => toHtmlFragment(`
   <ul role="list" class="site-menu__links">
-    <li><a href="/" class="site-menu__link site-menu__link--home"><span>Home</span></a></li>
-    <li><a href="/about" class="site-menu__link site-menu__link--about"><span>About</span></a></li>
+    <li><a href="/" class="site-menu__link site-menu__link--home"><span class="site-menu__link_text">Home</span></a></li>
+    <li><a href="/about" class="site-menu__link site-menu__link--about"><span class="site-menu__link_text">About</span></a></li>
     ${O.fold(constant(''), myProfileMenuItem)(user)}
   </ul>
 `);
