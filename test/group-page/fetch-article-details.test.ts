9b3ffa5299bc78c4ccb26cf55dfd597ecd202adb
Will Byrne <w.byrne@elifesciences.org>
2021-04-14 12:22:40 +0100

    Fix lint error #637

--- test/group-page/fetch-article-details.test.ts
+++ test/group-page/fetch-article-details.test.ts
@@ -2,8 +2,8 @@ import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { fetchArticleDetails } from '../../src/group-page/fetch-article-details';
 import { Doi } from '../../src/types/doi';
-import { toHtmlFragment } from "../../src/types/html-fragment";
-import { sanitise } from "../../src/types/sanitised-html-fragment";
+import { toHtmlFragment } from '../../src/types/html-fragment';
+import { sanitise } from '../../src/types/sanitised-html-fragment';
 
 describe('fetch-article-details', () => {
   describe('latest version date', () => {
