bf0b525e3c33bb0447bb7f0b22d2fac4e8c94881
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-04-15 15:29:52 +0100

    add test for evaluationCount #637

--- test/group-page/group-activities.test.ts
+++ test/group-page/group-activities.test.ts
@@ -5,25 +5,37 @@ import { editorialCommunityReviewedArticle } from '../../src/types/domain-events
 import { GroupId } from '../../src/types/group-id';
 
 describe('group-activities', () => {
-  describe('when a group has evaluated an article', () => {
-    it('includes the article', async () => {
-      const articleId = new Doi('10.1101/2020.09.15.286153');
-      const groupId = new GroupId('4eebcec9-a4bb-44e1-bde3-2ae11e65daaa');
-      const events = [
-        editorialCommunityReviewedArticle(
-          groupId,
-          articleId,
-          new Doi('10.24072/pci.animsci.100005'),
-          new Date('2020-12-15T00:00:00.000Z'),
-        ),
-      ];
+  describe('when only a single group has evaluated an article once', () => {
+    const articleId = new Doi('10.1101/2020.09.15.286153');
+    const groupId = new GroupId('4eebcec9-a4bb-44e1-bde3-2ae11e65daaa');
+    const events = [
+      editorialCommunityReviewedArticle(
+        groupId,
+        articleId,
+        new Doi('10.24072/pci.animsci.100005'),
+        new Date('2020-12-15T00:00:00.000Z'),
+      ),
+    ];
 
+    it('includes the article', () => {
       const activities = groupActivities(events)(groupId);
 
       expect(activities).toStrictEqual(
         O.some(expect.arrayContaining([
           expect.objectContaining({
             doi: articleId,
+          }),
+        ])),
+      );
+    });
+
+    it('has an evaluation count of 1', () => {
+      const activities = groupActivities(events)(groupId);
+
+      expect(activities).toStrictEqual(
+        O.some(expect.arrayContaining([
+          expect.objectContaining({
+            evaluationCount: 1,
           }),
         ])),
       );
