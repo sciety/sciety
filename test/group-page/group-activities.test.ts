84980b83d1e1bdc4ed6516a9f3335e7c5075424e
Giorgio Sironi <g.sironi@elifesciences.org>
2021-04-15 14:28:53 +0100

    Implement a characterization test #637

--- test/group-page/group-activities.test.ts
+++ test/group-page/group-activities.test.ts
@@ -1,5 +1,20 @@
+import * as O from 'fp-ts/Option';
+import { groupActivities } from '../../src/group-page/group-activities';
+import { Doi } from '../../src/types/doi';
+import { GroupId } from '../../src/types/group-id';
+
 describe('group-activities', () => {
   describe('when a group has evaluated an article', () => {
-    it.todo('includes the article');
+    it('includes the article', async () => {
+      const activities = groupActivities(new GroupId('1234'));
+
+      expect(activities).toStrictEqual(
+        O.some(expect.arrayContaining([
+          expect.objectContaining({
+            doi: new Doi('10.1101/2020.09.15.286153'),
+          }),
+        ])),
+      );
+    });
   });
 });
