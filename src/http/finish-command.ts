557a659e0d723b0da595e5dfca13d8e878a5dd08
Kevin Rutherford <kevin@rutherford-software.com>
2021-04-19 15:38:17 +0100

    Don't throw if the finish-follow-command fails #595

--- src/http/finish-command.ts
+++ src/http/finish-command.ts
@@ -1,18 +1,29 @@
 import * as O from 'fp-ts/Option';
 import { StatusCodes } from 'http-status-codes';
 import { Middleware } from 'koa';
+import { renderErrorPage } from './render-error-page';
 import { finishFollowCommand } from '../follow';
 import { sessionGroupProperty } from '../follow/finish-follow-command';
 import { Adapters } from '../infrastructure/adapters';
+import { applyStandardPageLayout } from '../shared-components';
+import { toHtmlFragment } from '../types/html-fragment';
 
 export const finishCommand = (adapters: Adapters): Middleware => async (context, next) => {
   if (context.session.command === 'follow') {
     const result = await finishFollowCommand(adapters)(context.session[sessionGroupProperty], context.state.user)();
-    if (O.isNone(result)) {
-      context.throw(StatusCodes.BAD_REQUEST);
-    }
     delete context.session.command;
     delete context.session[sessionGroupProperty];
+    if (O.isNone(result)) {
+      adapters.logger('error', 'Could not finish command', { session: context.session });
+      context.response.status = StatusCodes.INTERNAL_SERVER_ERROR;
+      context.response.body = applyStandardPageLayout(O.none)({
+        title: 'Error',
+        content: renderErrorPage(toHtmlFragment('Something went wrong, please try again.')),
+      });
+    } else {
+      await next();
+    }
+  } else {
+    await next();
   }
-  await next();
 };
