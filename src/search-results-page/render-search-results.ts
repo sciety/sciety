82d9b8f56fc0af4f1b90a940b5c9c0ae202629e6
Giorgio Sironi <g.sironi@elifesciences.org>
2021-04-23 13:31:42 +0100

    Delete render-search-result file #637

--- src/search-results-page/render-search-results.ts
+++ src/search-results-page/render-search-results.ts
@@ -3,9 +3,11 @@ import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import { constant, pipe } from 'fp-ts/function';
-import { ItemViewModel, renderSearchResult } from './render-search-result';
-import { templateListItems } from '../shared-components';
+import { GroupViewModel, renderGroupSearchResult } from './render-group-search-result';
+import { ArticleViewModel, renderArticleActivity, templateListItems } from '../shared-components';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
+
+export type ItemViewModel = ArticleViewModel & { _tag: 'Article' } | GroupViewModel & { _tag: 'Group' };
 
 export type SearchResults = {
   query: string,
@@ -28,6 +30,18 @@ const renderListIfNecessary = (articles: ReadonlyArray<HtmlFragment>) => pipe(
 );
 
 type RenderSearchResults = (rs: SearchResults) => HtmlFragment;
+
+// cannot inline this due to:
+// Array.prototype.map() expects a value to be returned at the end of arrow function
+//   array-callback-return
+const renderSearchResult = (result: ItemViewModel) => {
+  switch (result._tag) {
+    case 'Article':
+      return renderArticleActivity(result);
+    case 'Group':
+      return renderGroupSearchResult(result);
+  }
+};
 
 export const renderSearchResults: RenderSearchResults = (searchResults) => pipe(
   searchResults.itemsToDisplay,
