7480209d113a13014eafc322efdc9ddf576fc8b8
Kevin Rutherford <kevin@rutherford-software.com>
2021-04-27 11:13:28 +0100

    Extract another axios fetching function #675

--- src/infrastructure/fetchers.ts
+++ src/infrastructure/fetchers.ts
@@ -34,3 +34,14 @@ export const getStringWithHeadersRetriesAndDurationLogging: Gswhradl = (logger, 
     logger('debug', 'Response time to fetch article from Crossref', { uri, durationInMs });
   }
 };
+
+type Gjwhadl = (l: Logger) => (uri: string, h: Record<string, string>) => Promise<AxiosResponse<Json>>;
+
+export const getJsonWithHeadersAndDurationLogging: Gjwhadl = (logger) => async (uri, headers) => {
+  const startTime = new Date();
+  return axios.get<Json>(uri, { headers })
+    .finally(() => {
+      const durationInMs = new Date().getTime() - startTime.getTime();
+      logger('debug', 'Response time to fetch users from Twitter', { uri, durationInMs });
+    });
+};
--- src/infrastructure/get-twitter-response.ts
+++ src/infrastructure/get-twitter-response.ts
@@ -1,20 +1,13 @@
-import axios from 'axios';
 import { Json } from 'fp-ts/Json';
+import { getJsonWithHeadersAndDurationLogging } from './fetchers';
 import { Logger } from './logger';
 
 export type GetTwitterResponse = (url: string) => Promise<Json>;
 
-export const getTwitterResponse = (twitterApiBearerToken: string, logger: Logger): GetTwitterResponse => (
-  async (url) => {
-    const startTime = new Date();
-    return axios.get<Json>(
-      url,
-      { headers: { Authorization: `Bearer ${twitterApiBearerToken}` } },
-    )
-      .then((response) => response.data)
-      .finally(() => {
-        const durationInMs = new Date().getTime() - startTime.getTime();
-        logger('debug', 'Response time to fetch users from Twitter', { url, durationInMs });
-      });
-  }
+export const getTwitterResponse = (
+  twitterApiBearerToken: string,
+  logger: Logger,
+): GetTwitterResponse => async (url) => (
+  getJsonWithHeadersAndDurationLogging(logger)(url, { Authorization: `Bearer ${twitterApiBearerToken}` })
+    .then((response) => response.data)
 );
