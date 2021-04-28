8ad10271f6d57287a760d35ae8843f2f3c8c3ca7
Giorgio Sironi <g.sironi@elifesciences.org>
2021-04-28 14:42:11 +0100

    Add test list for followed-groups #643

---
+++ test/home-page/followed-groups.test.ts
@@ -0,0 +1,29 @@
+describe('followed-groups', () => {
+  describe('there are no follow events', () => {
+    it.todo('returns an empty array');
+  });
+
+  describe('there is a single follow event for the user', () => {
+    it.todo('returns the group id');
+  });
+
+  describe('there is a single follow event and a single unfollow event for the user', () => {
+    it.todo('returns an empty array');
+  });
+
+  describe('there are 4 follow events for different groups for the user', () => {
+    it.todo('returns the group ids');
+  })
+
+  describe('there are 4 follow events and 2 unfollow events for the user', () => {
+    it.todo('returns the group ids of the still followed groups');
+  })
+
+  describe('there is only a follow event for another user', () => {
+    it.todo('returns an empty array');
+  });
+
+  describe('there is a single follow event for the user, and a follow and unfollow event for another user', () => {
+    it.todo('returns an empty array');
+  });
+});
