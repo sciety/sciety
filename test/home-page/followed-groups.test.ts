d8bedac34754bbb41641baf4d01cbe1fa9b41cbc
Will Byrne <w.byrne@elifesciences.org>
2021-04-28 15:32:00 +0100

    Add followed-group stub and implement todo tests #643

--- test/home-page/followed-groups.test.ts
+++ test/home-page/followed-groups.test.ts
@@ -1,29 +1,95 @@
+import { followedGroups } from '../../src/home-page/followed-groups';
+import { userFollowedEditorialCommunity, userUnfollowedEditorialCommunity } from '../../src/types/domain-events';
+import { GroupId } from '../../src/types/group-id';
+import { toUserId } from '../../src/types/user-id';
+
 describe('followed-groups', () => {
+  const userId = toUserId('user');
+
   describe('there are no follow events', () => {
-    it.todo('returns an empty array');
+    it.skip('returns an empty array', () => {
+      const groupIds = followedGroups([])(userId);
+
+      expect(groupIds).toStrictEqual([]);
+    });
   });
 
   describe('there is a single follow event for the user', () => {
-    it.todo('returns the group id');
+    it.skip('returns the group id', () => {
+      const groupId = new GroupId('group');
+      const events = [userFollowedEditorialCommunity(userId, groupId)];
+      const groupIds = followedGroups(events)(userId);
+
+      expect(groupIds).toStrictEqual([groupId]);
+    });
   });
 
   describe('there is a single follow event and a single unfollow event for the user', () => {
-    it.todo('returns an empty array');
+    it.skip('returns an empty array', () => {
+      const groupId = new GroupId('group');
+      const events = [
+        userFollowedEditorialCommunity(userId, groupId),
+        userUnfollowedEditorialCommunity(userId, groupId),
+      ];
+      const groupIds = followedGroups(events)(userId);
+
+      expect(groupIds).toStrictEqual([]);
+    });
   });
 
-  describe('there are 4 follow events for different groups for the user', () => {
-    it.todo('returns the group ids');
+  describe('there are 2 follow events for different groups for the user', () => {
+    it.skip('returns the group ids', () => {
+      const groupId1 = new GroupId('group1');
+      const groupId2 = new GroupId('group2');
+      const events = [
+        userFollowedEditorialCommunity(userId, groupId1),
+        userFollowedEditorialCommunity(userId, groupId2),
+      ];
+      const groupIds = followedGroups(events)(userId);
+
+      expect(groupIds).toStrictEqual([groupId1, groupId2]);
+    });
   });
 
-  describe('there are 4 follow events and 2 unfollow events for the user', () => {
-    it.todo('returns the group ids of the still followed groups');
+  describe('there are 2 follow events and 1 unfollow events for the user', () => {
+    it.skip('returns the group ids of the still followed group', () => {
+      const groupId1 = new GroupId('group1');
+      const groupId2 = new GroupId('group1');
+      const events = [
+        userFollowedEditorialCommunity(userId, groupId1),
+        userFollowedEditorialCommunity(userId, groupId2),
+        userUnfollowedEditorialCommunity(userId, groupId1),
+      ];
+      const groupIds = followedGroups(events)(userId);
+
+      expect(groupIds).toStrictEqual([groupId2]);
+    });
   });
 
   describe('there is only a follow event for another user', () => {
-    it.todo('returns an empty array');
+    it.skip('returns an empty array', () => {
+      const groupId = new GroupId('group');
+      const events = [
+        userFollowedEditorialCommunity(toUserId('other-user'), groupId),
+      ];
+      const groupIds = followedGroups(events)(userId);
+
+      expect(groupIds).toStrictEqual([]);
+    });
   });
 
   describe('there is a single follow event for the user, and a follow and unfollow event for another user', () => {
-    it.todo('returns an empty array');
+    it.skip('returns an empty array', () => {
+      const groupId = new GroupId('group');
+      const otherUserId = toUserId('other-user');
+      const events = [
+        userFollowedEditorialCommunity(userId, groupId),
+        userFollowedEditorialCommunity(otherUserId, groupId),
+        userUnfollowedEditorialCommunity(otherUserId, groupId),
+      ];
+      const groupIds = followedGroups(events)(userId);
+
+      expect(groupIds).toStrictEqual([]);
+    });
   });
 });
