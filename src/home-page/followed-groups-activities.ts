69b04214e495c84fb878227707a4249c2460356a
Will Byrne <w.byrne@elifesciences.org>
2021-04-28 11:37:39 +0100

    Make map filter more idiomatic by seperating filtering and merging #643

--- src/home-page/followed-groups-activities.ts
+++ src/home-page/followed-groups-activities.ts
@@ -5,7 +5,7 @@ import * as RA from 'fp-ts/ReadonlyArray';
 import * as RM from 'fp-ts/ReadonlyMap';
 import * as S from 'fp-ts/Semigroup';
 import * as B from 'fp-ts/boolean';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
 import * as N from 'fp-ts/number';
 import { Doi, eqDoi } from '../types/doi';
 import {
@@ -79,9 +79,10 @@ export const followedGroupsActivities: FollowedGroupsActivities = (events) => (g
   events,
   RA.filter(isEditorialCommunityReviewedArticleEvent),
   RA.reduce(RM.empty, addEventToActivities(groupIds)),
-  RM.filterMapWithIndex((doi, activityDetails) => (activityDetails.evaluatedByFollowedGroup
-    ? O.some({ doi, ...activityDetails })
-    : O.none)),
+  RM.filterMapWithIndex(flow(
+    (doi, activityDetails) => O.some({ doi, ...activityDetails }),
+    O.filter((activityDetails) => activityDetails.evaluatedByFollowedGroup),
+  )),
   RM.values(byLatestActivityDateByGroupDesc),
   RA.takeLeft(20),
 );
