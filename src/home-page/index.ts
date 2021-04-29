a541e89fdc2164dafb5fc0cc7dba07b8e9558a1b
Kevin Rutherford <kevin@rutherford-software.com>
2021-04-29 10:17:09 +0100

    Remove data fetching from render-page #643

--- src/home-page/index.ts
+++ src/home-page/index.ts
@@ -1,3 +1,4 @@
+import { sequenceS } from 'fp-ts/Apply';
 import * as O from 'fp-ts/Option';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import * as T from 'fp-ts/Task';
@@ -10,12 +11,13 @@ import { projectIsFollowingSomething } from './project-is-following-something';
 import { renderFeed } from './render-feed';
 import { renderGroup } from './render-group';
 import { GetAllGroups, renderGroups } from './render-groups';
-import { renderPage, RenderPage } from './render-page';
+import { renderPage } from './render-page';
 import { renderPageHeader } from './render-page-header';
 import { renderFollowToggle } from '../follow/render-follow-toggle';
 import { renderSummaryFeedList } from '../shared-components';
 import { GroupId } from '../types/group-id';
 import { toHtmlFragment } from '../types/html-fragment';
+import { Page } from '../types/page';
 import { User } from '../types/user';
 import { UserId } from '../types/user-id';
 
@@ -31,18 +33,14 @@ type Params = {
   user: O.Option<User>,
 };
 
-type HomePage = (params: Params) => ReturnType<RenderPage>;
+type HomePage = (params: Params) => T.Task<Page>;
 
 export const homePage = (ports: Ports): HomePage => flow(
   (params) => params.user,
   O.map((user) => user.id),
-  renderPage(
-    renderPageHeader,
-    renderGroups(
-      ports.getAllGroups,
-      renderGroup(renderFollowToggle, ports.follows),
-    ),
-    renderFeed(
+  (userId) => ({
+    header: renderPageHeader(),
+    feed: renderFeed(
       projectIsFollowingSomething(ports.getAllEvents),
       getMostRecentEvents(ports.getAllEvents, ports.follows, 20),
       flow(
@@ -50,6 +48,12 @@ export const homePage = (ports: Ports): HomePage => flow(
         T.map(RNEA.fromReadonlyArray), // TODO shouldn't be needed, fp-ts types needs fixing
         TO.match(constant(pipe('', toHtmlFragment)), renderSummaryFeedList),
       ),
-    ),
-  ),
+    )(userId, []),
+    editorialCommunities: renderGroups(
+      ports.getAllGroups,
+      renderGroup(renderFollowToggle, ports.follows),
+    )(userId),
+  }),
+  sequenceS(T.ApplyPar),
+  T.map(renderPage),
 );
--- src/home-page/render-page.ts
+++ src/home-page/render-page.ts
@@ -1,15 +1,6 @@
-import { sequenceS } from 'fp-ts/Apply';
-import * as O from 'fp-ts/Option';
-import * as T from 'fp-ts/Task';
-import { flow, pipe } from 'fp-ts/function';
-import { RenderFeed } from './render-feed';
+import { pipe } from 'fp-ts/function';
 import { toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
-import { UserId } from '../types/user-id';
-
-export type RenderPage = (userId: O.Option<UserId>) => T.Task<Page>;
-
-type Component = (userId: O.Option<UserId>) => T.Task<string>;
 
 // TODO: should all be HtmlFragment
 type Components = {
@@ -30,21 +21,7 @@ const render = (components: Components) => `
   </div>
 `;
 
-const asPage = (components: Components): Page => ({
+export const renderPage = (components: Components): Page => ({
   title: 'Sciety: where research is evaluated and curated by the communities you trust',
   content: pipe(components, render, toHtmlFragment),
 });
-
-export const renderPage = (
-  renderPageHeader: Component,
-  renderEditorialCommunities: Component,
-  renderFeed: RenderFeed,
-): RenderPage => flow(
-  (userId) => ({
-    header: renderPageHeader(userId),
-    feed: renderFeed(userId, []),
-    editorialCommunities: renderEditorialCommunities(userId),
-  }),
-  sequenceS(T.ApplyPar),
-  T.map(asPage),
-);
