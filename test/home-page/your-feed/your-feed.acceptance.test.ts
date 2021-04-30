64fe62d2631f93f9906a508c75755ab74b2423ef
David Moulton <d.moulton@elifesciences.org>
2021-04-30 11:39:54 +0100

    Add skipped test #643

--- test/home-page/your-feed/your-feed.acceptance.test.ts
+++ test/home-page/your-feed/your-feed.acceptance.test.ts
@@ -2,11 +2,12 @@ import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import * as TO from 'fp-ts/TaskOption';
+import { JSDOM } from 'jsdom';
 import {
   followSomething, noEvaluationsYet, troubleFetchingTryAgain, welcomeMessage,
 } from '../../../src/home-page/your-feed/render-feed';
 import { yourFeed } from '../../../src/home-page/your-feed/your-feed';
-import { Doi } from '../../../src/types/doi';
+import { Doi, eqDoi } from '../../../src/types/doi';
 import { editorialCommunityReviewedArticle, userFollowedEditorialCommunity } from '../../../src/types/domain-events';
 import { GroupId } from '../../../src/types/group-id';
 import { toHtmlFragment } from '../../../src/types/html-fragment';
@@ -121,7 +122,37 @@ describe('your-feed acceptance', () => {
       it.todo('each article is only displayed once');
 
       describe('when details of an article cannot be fetched', () => {
-        it.todo('display the other 19 articles only');
+        it.skip('only displays the successfully fetched articles', async () => {
+          const groupId = new GroupId('NCRC');
+          const failingDoi = new Doi('10.1101/failing');
+          const adapters = {
+            fetchArticle: (doi: Doi) => (
+              eqDoi.equals(doi, failingDoi)
+                ? TE.left('unavailable' as const)
+                : TE.right({
+                  title: sanitise(toHtmlFragment('My article title')),
+                })),
+            getGroup: () => TO.some({
+              id: groupId,
+              name: 'NCRC',
+              avatarPath: '',
+              descriptionPath: '',
+              shortDescription: '',
+            }),
+            getAllEvents: T.of([
+              userFollowedEditorialCommunity(userId, groupId),
+              editorialCommunityReviewedArticle(groupId, failingDoi, new Doi('10.1101/111111')),
+              editorialCommunityReviewedArticle(groupId, new Doi('10.1101/success'), new Doi('10.1101/222222')),
+            ]),
+            follows: () => T.of(true),
+          };
+
+          const html = await yourFeed(adapters)(O.some(userId))();
+          const fragment = JSDOM.fragment(html);
+          const cards = Array.from(fragment.querySelectorAll('.article-activity-card'));
+
+          expect(cards).toHaveLength(1);
+        });
       });
 
       describe('when details of all articles cannot be fetched', () => {
