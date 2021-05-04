af6e6c805df717701cf314fcdd2725927d10ef03
Kevin Rutherford <kevin@rutherford-software.com>
2021-05-04 16:06:14 +0100

    Ensure arbitrary DOIs are all different #686

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,8 +1,22 @@
+import * as A from 'fp-ts/Array';
+import { pipe } from 'fp-ts/function';
 import { Doi } from '../src/types/doi';
 
-export const arbitraryDoi = (): Doi => new Doi('10.1101/arbitrary.doi.1');
+const arbitraryNumber = (min: number, max: number): number => (
+  Math.floor(Math.random() * (max - min + 1) + min)
+);
 
-export const arbitraryString = (): string => 'Lorem ipsum';
+export const arbitraryWord = (length: number): string => (
+  [...Array(length)].map(() => Math.random().toString(36)[2]).join('')
+);
+
+export const arbitraryDoi = (): Doi => new Doi(`10.1101/${arbitraryWord(8)}`);
+
+export const arbitraryString = (): string => pipe(
+  [...Array(arbitraryNumber(3, 20))],
+  A.map(() => arbitraryNumber(2, 10)),
+  A.map((n) => arbitraryWord(n)),
+).join(' ');
 
 export const arbitraryUri = (): string => 'http://something.com/example';
 
