31a4ad4bcee0b6a726889e679dd50fa29e09f461
Kevin Rutherford <kevin@rutherford-software.com>
2021-05-04 12:42:57 +0100

    Extract builders for ReviewFeedItem #686

---
+++ test/article-page/review-feed-item-helpers.ts
@@ -0,0 +1,39 @@
+import { URL } from 'url';
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { ReviewFeedItem } from '../../src/article-page/render-review-feed-item';
+import { GroupId } from '../../src/types/group-id';
+import { toHtmlFragment } from '../../src/types/html-fragment';
+import { sanitise } from '../../src/types/sanitised-html-fragment';
+import * as t from '../helpers';
+
+export const arbitrary = (): ReviewFeedItem => ({
+  type: 'review',
+  id: t.arbitraryDoi(),
+  source: O.some(new URL(t.arbitraryUri())),
+  occurredAt: new Date(),
+  groupId: new GroupId('group-1'),
+  groupName: 'group 1',
+  groupAvatar: '/avatar',
+  fullText: pipe(t.arbitraryString(), toHtmlFragment, sanitise, O.some),
+  counts: {
+    helpfulCount: 0,
+    notHelpfulCount: 0,
+  },
+  current: O.none,
+});
+
+export const withFullText = (fullText: string) => (rfi: ReviewFeedItem): ReviewFeedItem => ({
+  ...rfi,
+  fullText: pipe(fullText, toHtmlFragment, sanitise, O.some),
+});
+
+export const withNoFullText = (rfi: ReviewFeedItem): ReviewFeedItem => ({
+  ...rfi,
+  fullText: O.none,
+});
+
+export const withSource = (uri: string) => (rfi: ReviewFeedItem): ReviewFeedItem => ({
+  ...rfi,
+  source: O.some(new URL(uri)),
+});
