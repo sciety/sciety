6dd53c74f6c2ede4587ebec8983ee53f52fc60eb
Will Byrne <w.byrne@elifesciences.org>
2021-05-06 17:17:55 +0100

    replace call to new GroupId with a call to fromString #697

--- test/types/group-id.helper.ts
+++ test/types/group-id.helper.ts
@@ -1,6 +1,18 @@
-import { GroupId } from '../../src/types/group-id';
+import * as O from 'fp-ts/Option';
+import { flow, identity } from 'fp-ts/function';
+import { fromString, GroupId } from '../../src/types/group-id';
 import { arbitraryWord } from '../helpers';
 
-export const arbitraryGroupId = (): GroupId => new GroupId(`group-id-${arbitraryWord(6)}`);
+const fromStringNoOption: (id: string) => GroupId = flow(
+  fromString,
+  O.fold(
+    // eslint-disable-next-line jest/no-jasmine-globals
+    () => { throw new Error('not a valid groupId'); },
+    identity,
+  ),
+);
 
-export const groupIdFromString = (id: string): GroupId => new GroupId(`group-id-${id}`);
+export const arbitraryGroupId = (): GroupId => fromStringNoOption(`group-id-${arbitraryWord(6)}`);
+
+// The prepended group-id here prevents the fromString from returning O.none
+export const groupIdFromString = (id: string): GroupId => fromStringNoOption(`group-id-${id}`);
