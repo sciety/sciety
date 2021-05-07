47b109f618591937b69eb4c6b0f5b6db29b0e29f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-05-07 14:20:04 +0100

    pipefy groupsPage #698

--- src/groups-page/index.ts
+++ src/groups-page/index.ts
@@ -1,14 +1,18 @@
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import { toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 
-export const groupsPage = (): TE.TaskEither<never, Page> => TE.right(
-  {
+export const groupsPage = (): TE.TaskEither<never, Page> => pipe(
+  'Groups',
+  toHtmlFragment,
+  (content) => ({
     title: 'Groups',
-    content: toHtmlFragment('Groups'),
+    content,
     openGraph: {
       title: 'Groups',
       description: 'Groups',
     },
-  },
+  }),
+  TE.right,
 );
