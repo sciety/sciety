7a8882e9adf4a917ba23343c2600414570b91cd4
David Moulton <d.moulton@elifesciences.org>
2021-05-10 12:00:08 +0100

    Introduce image to landing page #699

--- src/landing-page/index.ts
+++ src/landing-page/index.ts
@@ -1,12 +1,21 @@
 import { toHtmlFragment } from '../types/html-fragment';
 
 const content = toHtmlFragment(`
-<div class="landing-page-content">
-  <h1>
-    The home of public preprint evaluation
-  </h1>    
-  <p>Open evaluation and curation together in one place. Let Sciety help you navigate the preprint landscape.</p>
-  <p>Follow the journey through <a href="/blog">our blog</a>.</p>
+<div class="landing-page">
+
+  <div class="landing-page__content">
+    <h1>
+      The home of public preprint evaluation
+    </h1>
+    <p>Open evaluation and curation together in one place. Let Sciety help you navigate the preprint landscape.</p>
+    <p>Follow the journey through <a href="/blog">our blog</a>.</p>
+  </div>
+
+  <picture>
+    <source srcset="/static/images/landing-page-illustration.svg" media="(min-width: 46.25em)">
+    <img src="data:" alt="" class="landing-page__image">
+  </picture>
+
 </div>
 `);
 
