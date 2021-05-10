6efd0b8b880c44daf49b778e120cf7e7c47f4c11
Kevin Rutherford <kevin@rutherford-software.com>
2021-05-10 14:46:09 +0100

    Use arbitrary test values #708

--- test/user-page/follow-list/project-followed-group-ids.test.ts
+++ test/user-page/follow-list/project-followed-group-ids.test.ts
@@ -1,24 +1,26 @@
 import * as T from 'fp-ts/Task';
 import { userFollowedEditorialCommunity, userSavedArticle, userUnfollowedEditorialCommunity } from '../../../src/types/domain-events';
-import { toUserId } from '../../../src/types/user-id';
-import { GetAllEvents, projectFollowedGroupIds } from '../../../src/user-page/follow-list/project-followed-group-ids';
+import { projectFollowedGroupIds } from '../../../src/user-page/follow-list/project-followed-group-ids';
 import { arbitraryDoi } from '../../types/doi.helper';
 import { arbitraryGroupId, groupIdFromString } from '../../types/group-id.helper';
+import { arbitraryUserId } from '../../types/user-id.helper';
+
+const importantUser = arbitraryUserId();
 
 describe('project-followed-group-ids', () => {
-  const getAllEvents: GetAllEvents = T.of([
-    userFollowedEditorialCommunity(toUserId('someone'), groupIdFromString('316db7d9-88cc-4c26-b386-f067e0f56334')),
-    userFollowedEditorialCommunity(toUserId('someone'), groupIdFromString('53ed5364-a016-11ea-bb37-0242ac130002')),
-    userUnfollowedEditorialCommunity(toUserId('someone'), groupIdFromString('53ed5364-a016-11ea-bb37-0242ac130002')),
-    userFollowedEditorialCommunity(toUserId('someone'), groupIdFromString('53ed5364-a016-11ea-bb37-0242ac130002')),
-    userFollowedEditorialCommunity(toUserId('someone'), groupIdFromString('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de')),
-    userSavedArticle(toUserId('someone'), arbitraryDoi()),
-    userFollowedEditorialCommunity(toUserId('someoneelse'), groupIdFromString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0')),
-    userUnfollowedEditorialCommunity(toUserId('someoneelse'), groupIdFromString('53ed5364-a016-11ea-bb37-0242ac130002')),
+  const getAllEvents = T.of([
+    userFollowedEditorialCommunity(importantUser, groupIdFromString('316db7d9-88cc-4c26-b386-f067e0f56334')),
+    userFollowedEditorialCommunity(importantUser, groupIdFromString('53ed5364-a016-11ea-bb37-0242ac130002')),
+    userUnfollowedEditorialCommunity(importantUser, groupIdFromString('53ed5364-a016-11ea-bb37-0242ac130002')),
+    userFollowedEditorialCommunity(importantUser, groupIdFromString('53ed5364-a016-11ea-bb37-0242ac130002')),
+    userFollowedEditorialCommunity(importantUser, groupIdFromString('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de')),
+    userSavedArticle(importantUser, arbitraryDoi()),
+    userFollowedEditorialCommunity(arbitraryUserId(), groupIdFromString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0')),
+    userUnfollowedEditorialCommunity(arbitraryUserId(), groupIdFromString('53ed5364-a016-11ea-bb37-0242ac130002')),
   ]);
 
   it('returns a list', async () => {
-    const actual = await projectFollowedGroupIds(getAllEvents)(toUserId('someone'))();
+    const actual = await projectFollowedGroupIds(getAllEvents)(importantUser)();
     const expected = [
       groupIdFromString('316db7d9-88cc-4c26-b386-f067e0f56334'),
       groupIdFromString('53ed5364-a016-11ea-bb37-0242ac130002'),
@@ -33,8 +35,8 @@ describe('project-followed-group-ids', () => {
 
     it('lists that group', async () => {
       const followed = await projectFollowedGroupIds(T.of([
-        userFollowedEditorialCommunity(toUserId('someone'), group1),
-      ]))(toUserId('someone'))();
+        userFollowedEditorialCommunity(importantUser, group1),
+      ]))(importantUser)();
 
       expect(followed).toStrictEqual([group1]);
     });
@@ -45,9 +47,9 @@ describe('project-followed-group-ids', () => {
 
     it('does not list that group', async () => {
       const followed = await projectFollowedGroupIds(T.of([
-        userFollowedEditorialCommunity(toUserId('someone'), group1),
-        userUnfollowedEditorialCommunity(toUserId('someone'), group1),
-      ]))(toUserId('someone'))();
+        userFollowedEditorialCommunity(importantUser, group1),
+        userUnfollowedEditorialCommunity(importantUser, group1),
+      ]))(importantUser)();
 
       expect(followed).toStrictEqual([]);
     });
