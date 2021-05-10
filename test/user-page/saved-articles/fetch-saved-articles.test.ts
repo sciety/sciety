65c2ebb42cc092fc378017947ecc668fb5d86f29
Kevin Rutherford <kevin@rutherford-software.com>
2021-05-10 13:56:23 +0100

    Use arbitrary values to aid test comprehension #707

--- test/user-page/saved-articles/fetch-saved-articles.test.ts
+++ test/user-page/saved-articles/fetch-saved-articles.test.ts
@@ -1,25 +1,27 @@
 import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
-import { Doi } from '../../../src/types/doi';
 import { toHtmlFragment } from '../../../src/types/html-fragment';
 import { fetchSavedArticles } from '../../../src/user-page/saved-articles/fetch-saved-articles';
+import { arbitraryString } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
+import { arbitraryDoi } from '../../types/doi.helper';
 
 describe('fetch-get-saved-articles', () => {
   describe('when the user has saved articles', () => {
     it('returns doi and title for those articles', async () => {
-      const getArticle = () => pipe(
-        'Gender, race and parenthood',
+      const articleId = arbitraryDoi();
+      const title = pipe(
+        arbitraryString(),
         toHtmlFragment,
         O.some,
-        T.of,
       );
-      const savedArticles = await fetchSavedArticles(getArticle)([new Doi('10.1101/2020.07.04.187583')])();
+      const getArticle = () => T.of(title);
+      const savedArticles = await fetchSavedArticles(getArticle)([articleId])();
 
       expect(savedArticles[0]).toMatchObject({
-        doi: new Doi('10.1101/2020.07.04.187583'),
-        title: O.some(toHtmlFragment('Gender, race and parenthood')),
+        doi: articleId,
+        title,
       });
     });
   });
