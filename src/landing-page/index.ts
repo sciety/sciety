84b8fd12f8567e71de48e3e0af855582581c16a6
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-05-10 14:29:57 +0100

    bring image display to acceptance #699

--- src/landing-page/index.ts
+++ src/landing-page/index.ts
@@ -7,12 +7,16 @@ const content = toHtmlFragment(`
     <h1>
       The home of public preprint evaluation
     </h1>
-    <p>Open evaluation and curation together in one place. Let Sciety help you navigate the preprint landscape.</p>
+    <p>
+      Open evaluation and curation together in one place.
+      <br>
+      Let Sciety help you navigate the preprint landscape.
+    </p>
     <p>Follow the journey through <a href="/blog">our blog</a>.</p>
   </div>
 
   <picture>
-    <source srcset="/static/images/landing-page-illustration.svg" media="(min-width: 46.25em)">
+    <source srcset="/static/images/landing-page-illustration.svg" media="(min-width: 60.25em)">
     <img src="data:" alt="" class="landing-page__image">
   </picture>
 
