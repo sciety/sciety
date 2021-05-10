96031a05c0b1be36df812c8fb6bc69024504f56f
Kevin Rutherford <kevin@rutherford-software.com>
2021-05-10 13:14:28 +0100

    Decouple a function from the index scope #707

--- src/user-page/index.ts
+++ src/user-page/index.ts
@@ -16,12 +16,22 @@ import { RenderPageError } from '../types/render-page-error';
 import { User } from '../types/user';
 import { UserId } from '../types/user-id';
 
+type FetchArticle = (doi: Doi) => TE.TaskEither<unknown, { title: HtmlFragment }>;
+
+const getTitle = (fetchArticle: FetchArticle) => flow(
+  fetchArticle,
+  T.map(flow(
+    O.fromEither,
+    O.map((article) => article.title),
+  )),
+);
+
 type GetUserDetails = (userId: UserId) => TE.TaskEither<'not-found' | 'unavailable', UserDetails>;
 
 type Ports = FollowListPorts & {
   getAllEvents: GetAllEvents,
   getUserDetails: GetUserDetails,
-  fetchArticle: (doi: Doi) => TE.TaskEither<unknown, { title: HtmlFragment }>,
+  fetchArticle: FetchArticle,
 };
 
 type Params = {
@@ -31,46 +41,36 @@ type Params = {
 
 type UserPage = (params: Params) => TE.TaskEither<RenderPageError, Page>;
 
-export const userPage = (ports: Ports): UserPage => {
-  const getTitle = flow(
-    ports.fetchArticle,
-    T.map(flow(
-      O.fromEither,
-      O.map((article) => article.title),
-    )),
+export const userPage = (ports: Ports): UserPage => (params) => {
+  const viewingUserId = pipe(
+    params.user,
+    O.map((user) => user.id),
   );
+  const userDetails = ports.getUserDetails(params.id);
 
-  return (params) => {
-    const viewingUserId = pipe(
-      params.user,
-      O.map((user) => user.id),
-    );
-    const userDetails = ports.getUserDetails(params.id);
-
-    return pipe(
-      {
-        header: pipe(
-          userDetails,
-          TE.map(renderHeader),
-        ),
-        followList: followList(ports)(params.id, viewingUserId),
-        savedArticlesList: pipe(
-          params.id,
-          projectSavedArticleDois(ports.getAllEvents),
-          T.chain(fetchSavedArticles(getTitle)),
-          T.map(renderSavedArticles),
-          TE.rightTask,
-        ),
-        userDisplayName: pipe(
-          userDetails,
-          TE.map(flow(
-            ({ displayName }) => displayName,
-            toHtmlFragment,
-          )),
-        ),
-      },
-      sequenceS(TE.ApplyPar),
-      TE.bimap(renderErrorPage, renderPage),
-    );
-  };
+  return pipe(
+    {
+      header: pipe(
+        userDetails,
+        TE.map(renderHeader),
+      ),
+      followList: followList(ports)(params.id, viewingUserId),
+      savedArticlesList: pipe(
+        params.id,
+        projectSavedArticleDois(ports.getAllEvents),
+        T.chain(fetchSavedArticles(getTitle(ports.fetchArticle))),
+        T.map(renderSavedArticles),
+        TE.rightTask,
+      ),
+      userDisplayName: pipe(
+        userDetails,
+        TE.map(flow(
+          ({ displayName }) => displayName,
+          toHtmlFragment,
+        )),
+      ),
+    },
+    sequenceS(TE.ApplyPar),
+    TE.bimap(renderErrorPage, renderPage),
+  );
 };
