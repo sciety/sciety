1ff869787499c55f09798fa81311048c4274a96a
Kevin Rutherford <kevin@rutherford-software.com>
2021-05-10 10:08:59 +0100

    Extract a recent-activity module #707

--- src/group-page/index.ts
+++ src/group-page/index.ts
@@ -6,54 +6,23 @@ import * as TE from 'fp-ts/TaskEither';
 import * as TO from 'fp-ts/TaskOption';
 import { flow, pipe } from 'fp-ts/function';
 import { countFollowersOf } from './count-followers';
-import { constructRecentGroupActivity } from './recent-activity/construct-recent-group-activity';
+import { recentActivity, RecentActivityPorts } from './recent-activity/recent-activity';
 import { FetchStaticFile, renderDescription } from './render-description';
 import { renderFollowers } from './render-followers';
 import { renderErrorPage, renderPage } from './render-page';
 import { renderPageHeader } from './render-page-header';
 import { renderFollowToggle } from '../follow/render-follow-toggle';
-import { fetchArticleDetails } from '../shared-components/article-card/fetch-article-details';
-import { FindVersionsForArticleDoi, getLatestArticleVersionDate } from '../shared-components/article-card/get-latest-article-version-date';
-import { ArticleServer } from '../types/article-server';
-import { Doi } from '../types/doi';
 import { DomainEvent } from '../types/domain-events';
 import { Group } from '../types/group';
 import { GroupId } from '../types/group-id';
-import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
+import { toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 import { RenderPageError } from '../types/render-page-error';
-import { SanitisedHtmlFragment } from '../types/sanitised-html-fragment';
 import { User } from '../types/user';
 import { UserId } from '../types/user-id';
 
-type RecentActivityPorts = {
-  fetchArticle: GetArticle,
-  findVersionsForArticleDoi: FindVersionsForArticleDoi,
-  getAllEvents: GetAllEvents,
-};
-
-type RecentActivity = (ports: RecentActivityPorts) => (group: Group) => TE.TaskEither<never, string | HtmlFragment>;
-
-const recentActivity: RecentActivity = (ports) => (group) => pipe(
-  group.id,
-  constructRecentGroupActivity(
-    fetchArticleDetails(
-      getLatestArticleVersionDate(ports.findVersionsForArticleDoi),
-      flow(ports.fetchArticle, T.map(O.fromEither)),
-    ),
-    ports.getAllEvents,
-  ),
-  TE.rightTask,
-);
-
 type FetchGroup = (groupId: GroupId) => TO.TaskOption<Group>;
 
-type Article = {
-  title: SanitisedHtmlFragment,
-  server: ArticleServer,
-  authors: ReadonlyArray<string>,
-};
-type GetArticle = (id: Doi) => TE.TaskEither<unknown, Article>;
 type GetAllEvents = T.Task<ReadonlyArray<DomainEvent>>;
 
 type Ports = RecentActivityPorts & {
