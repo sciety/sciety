17075425a9bcefae8f6ffee69d79e976c24c57c1
David Moulton <d.moulton@elifesciences.org>
2021-05-11 10:11:44 +0100

    Introduce extra level of container in landing page #701

--- src/landing-page/index.ts
+++ src/landing-page/index.ts
@@ -3,22 +3,24 @@ import { toHtmlFragment } from '../types/html-fragment';
 const content = toHtmlFragment(`
 <div class="landing-page">
 
-  <div class="landing-page__content">
-    <h1 class="landing-page__content__title">
-      The home of public preprint evaluation
-    </h1>
-    <p class="landing-page__content__byline">
-      Open evaluation and curation together in one place.
-      <br>
-      Let Sciety help you navigate the preprint landscape.
-    </p>
-    <p class="landing-page__content__byline">Follow the journey through <a href="/blog">our blog</a>.</p>
-  </div>
+  <div class="landing-page-hero">
+    <div class="landing-page-hero__content">
+      <h1 class="landing-page-hero__content__title">
+        The home of public preprint evaluation
+      </h1>
+      <p class="landing-page-hero__content__byline">
+        Open evaluation and curation together in one place.
+        <br>
+        Let Sciety help you navigate the preprint landscape.
+      </p>
+      <p class="landing-page-hero__content__byline">Follow the journey through <a href="/blog">our blog</a>.</p>
+    </div>
 
-  <picture>
-    <source srcset="/static/images/landing-page-illustration.svg" media="(min-width: 60.25em)">
-    <img src="data:" alt="" class="landing-page__image">
-  </picture>
+    <picture>
+      <source srcset="/static/images/landing-page-illustration.svg" media="(min-width: 60.25em)">
+      <img src="data:" alt="" class="landing-page-hero__image">
+    </picture>
+  </div>
 
 </div>
 `);
