a23ef0cdbd71aecd6d9662aed50d95ca3ff80d41
Giorgio Sironi <g.sironi@elifesciences.org>
2021-05-12 11:27:46 +0100

    Add spacing in the persona container #702

--- src/landing-page/index.ts
+++ src/landing-page/index.ts
@@ -25,11 +25,11 @@ const content = toHtmlFragment(`
   <div class="landing-page-personas">
     <h2 class="landing-page-personas__title">Where effort meets impact</h2>
     <div class="landing-page-personas__content">
-      <div>
+      <div class="landing-page-personas__persona">
         <img src="/static/images/landing-persona-readers.svg" alt="" class="landing-page-personas__image">
         <p>Quickly find relevant content and spend more time reading what you care about.</p>
       </div>
-      <div>
+      <div class="landing-page-personas__persona">
         <img src="/static/images/landing-persona-groups.svg" alt="" class="landing-page-personas__image">
         <p>Share your group's insights to grow your readership and keep the community informed.</p>
       </div>
