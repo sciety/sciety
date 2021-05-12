7cfa0168ebb1f7cba4a3b9e454285ddcabadcbb5
Will Byrne <w.byrne@elifesciences.org>
2021-05-12 11:04:38 +0100

    unstack images when not narrow #702

--- src/landing-page/index.ts
+++ src/landing-page/index.ts
@@ -24,10 +24,16 @@ const content = toHtmlFragment(`
 
   <div class="landing-page-personas">
     <h2 class="landing-page-personas__title">Where effort meets impact</h2>
-    <img src="/static/images/landing-persona-readers.svg" alt="" class="landing-page-personas__image">
-    <p>Quickly find relevant content and spend more time reading what you care about.</p>
-    <img src="/static/images/landing-persona-groups.svg" alt="" class="landing-page-personas__image">
-    <p>Share your group's insights to grow your readership and keep the community informed.</p>
+    <div class="landing-page-personas__content">
+      <div>
+        <img src="/static/images/landing-persona-readers.svg" alt="" class="landing-page-personas__image">
+        <p>Quickly find relevant content and spend more time reading what you care about.</p>
+      </div>
+      <div>
+        <img src="/static/images/landing-persona-groups.svg" alt="" class="landing-page-personas__image">
+        <p>Share your group's insights to grow your readership and keep the community informed.</p>
+      </div>
+    </div>
   </div>
 
   <div class="landing-page-calls-to-action">
