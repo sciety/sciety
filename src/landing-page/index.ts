33c39852ad17505fabc3be336b1fecddd5efe8f3
Kevin Rutherford <kevin@rutherford-software.com>
2021-05-13 15:41:07 +0100

    Add the group avatar to recently evaluated #718

--- src/landing-page/index.ts
+++ src/landing-page/index.ts
@@ -36,9 +36,9 @@ const content = toHtmlFragment(`
           <h3 class="article-card__title">
             <a class="article-card__link" href="/articles/activity/10.1101/2021.01.10.426076?utm_source=landingpage&utm_medium=banner&utm_campaign=recently-evaluated-1">Single-cell RNA-seq analysis reveals penaeid shrimp hemocyte subpopulations and cell differentiation process</a>
           </h3>
-          <p>
+          <p class="landing-page-card__group">
             <img class="group-card__avatar landing-page-card__avatar" src="/static/groups/elife--b560187e-f2fb-4ff9-a861-a204f3fc0fb0.png" alt="" />
-            Evaluated by <a href="/groups/b560187e-f2fb-4ff9-a861-a204f3fc0fb0">eLife</a>.
+            <span>Evaluated by <a href="/groups/b560187e-f2fb-4ff9-a861-a204f3fc0fb0">eLife</a>.</span>
           </p>
           <div class="article-card__meta">
             <time datetime="2021-05-12">May 12, 2021</time>
@@ -50,9 +50,9 @@ const content = toHtmlFragment(`
           <h3 class="article-card__title">
             <a class="article-card__link" href="/articles/activity/10.1101/2021.02.20.20248421?utm_source=landingpage&utm_medium=banner&utm_campaign=recently-evaluated-2">Nosocomial outbreak of SARS-CoV-2 in a “non-COVID-19” hospital ward: virus genome sequencing as a key tool to understand cryptic transmission</a>
           </h3>
-          <p>
+          <p class="landing-page-card__group">
             <img class="group-card__avatar landing-page-card__avatar" src="/static/groups/ncrc--62f9b0d0-8d43-4766-a52a-ce02af61bc6a.jpg" alt="" />
-            Evaluated by <a href="/groups/62f9b0d0-8d43-4766-a52a-ce02af61bc6a">NCRC</a>.
+            <span>Evaluated by <a href="/groups/62f9b0d0-8d43-4766-a52a-ce02af61bc6a">NCRC</a>.</span>
           </p>
           <div class="article-card__meta">
             <time datetime="2021-04-22">Apr 22, 2021</time>
@@ -64,9 +64,9 @@ const content = toHtmlFragment(`
           <h3 class="article-card__title">
             <a class="article-card__link" href="/articles/activity/10.1101/2021.04.12.439490?utm_source=landingpage&utm_medium=banner&utm_campaign=recently-evaluated-3">Design, Synthesis and Evaluation of WD-repeat containing protein 5 (WDR5) degraders</a>
           </h3>
-          <p>
+          <p class="landing-page-card__group">
             <img class="group-card__avatar landing-page-card__avatar" src="/static/groups/prereview-community--10360d97-bf52-4aef-b2fa-2f60d319edd7.jpg" alt="" />
-            Evaluated by <a href="/groups/10360d97-bf52-4aef-b2fa-2f60d319edd7">PREreview</a>.
+            <span>Evaluated by <a href="/groups/10360d97-bf52-4aef-b2fa-2f60d319edd7">PREreview</a>.</span>
           </p>
           <div class="article-card__meta">
             <time datetime="2021-05-10">May 10, 2021</time>
