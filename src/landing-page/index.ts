6a6875d53ca792543322bc3e57e9c492f4aaab9c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-05-13 13:19:09 +0100

    remove unwanted card content #718

--- src/landing-page/index.ts
+++ src/landing-page/index.ts
@@ -37,13 +37,8 @@ const content = toHtmlFragment(`
             <a class="article-card__link" href="/articles/activity/10.1101/2021.01.10.426076">Single-cell RNA-seq analysis reveals penaeid shrimp hemocyte subpopulations and cell differentiation process</a>
           </h3>
           
-            <div class="hidden" id="article-card-author-list-10.1101/2021.01.10.426076">This article's authors</div>
-            <ol class="article-card__authors" role="list" aria-describedby="article-card-author-list-10.1101/2021.01.10.426076">
-              <li class="article-card__author">Keiichiro Koiwai</li><li class="article-card__author">Takashi Koyama</li><li class="article-card__author">Soichiro Tsuda</li><li class="article-card__author">Atsushi Toyoda</li><li class="article-card__author">Kiyoshi Kikuchi</li><li class="article-card__author">Hiroaki Suzuki</li><li class="article-card__author">Ryuji Kawano</li>
-            </ol>
-          
           <div class="article-card__meta">
-            <span class="visually-hidden">This article has </span><span>5 evaluations</span><span>Latest version <time datetime="2021-01-27">Jan 27, 2021</time></span><span>Latest activity <time datetime="2021-05-12">May 12, 2021</time></span>
+            <time datetime="2021-05-12">May 12, 2021</time>
           </div>
         </article>
       </li>
@@ -53,13 +48,8 @@ const content = toHtmlFragment(`
             <a class="article-card__link" href="/articles/activity/10.1101/2021.02.20.20248421">Nosocomial outbreak of SARS-CoV-2 in a “non-COVID-19” hospital ward: virus genome sequencing as a key tool to understand cryptic transmission</a>
           </h3>
           
-            <div class="hidden" id="article-card-author-list-10.1101/2021.02.20.20248421">This article's authors</div>
-            <ol class="article-card__authors" role="list" aria-describedby="article-card-author-list-10.1101/2021.02.20.20248421">
-              <li class="article-card__author">Vítor Borges</li><li class="article-card__author">Joana Isidro</li><li class="article-card__author">Filipe Macedo</li><li class="article-card__author">José Neves</li><li class="article-card__author">Luís Silva</li><li class="article-card__author">Mário Paiva</li><li class="article-card__author">José Barata</li><li class="article-card__author">Judite Catarino</li><li class="article-card__author">Liliana Ciobanu</li><li class="article-card__author">Sílvia Duarte</li><li class="article-card__author">Luís Vieira</li><li class="article-card__author">Raquel Guiomar</li><li class="article-card__author">João Paulo Gomes</li>
-            </ol>
-          
           <div class="article-card__meta">
-            <span class="visually-hidden">This article has </span><span>1 evaluation</span><span>Latest version <time datetime="2021-02-23">Feb 23, 2021</time></span><span>Latest activity <time datetime="2021-04-22">Apr 22, 2021</time></span>
+            <time datetime="2021-04-22">Apr 22, 2021</time>
           </div>
         </article>
       </li>
@@ -69,13 +59,8 @@ const content = toHtmlFragment(`
             <a class="article-card__link" href="/articles/activity/10.1101/2021.04.12.439490">Design, Synthesis and Evaluation of WD-repeat containing protein 5 (WDR5) degraders</a>
           </h3>
           
-            <div class="hidden" id="article-card-author-list-10.1101/2021.04.12.439490">This article's authors</div>
-            <ol class="article-card__authors" role="list" aria-describedby="article-card-author-list-10.1101/2021.04.12.439490">
-              <li class="article-card__author">Anja Dölle</li><li class="article-card__author">Bikash Adhikari</li><li class="article-card__author">Andreas Krämer</li><li class="article-card__author">Janik Weckesser</li><li class="article-card__author">Nicola Berner</li><li class="article-card__author">Lena-Marie Berger</li><li class="article-card__author">Mathias Diebold</li><li class="article-card__author">Magdalena M. Szewczyk</li><li class="article-card__author">Dalia Barsyte-Lovejoy</li><li class="article-card__author">Cheryl H. Arrowsmith</li><li class="article-card__author">Jakob Gebel</li><li class="article-card__author">Frank Löhr</li><li class="article-card__author">Volker Dötsch</li><li class="article-card__author">Martin Eilers</li><li class="article-card__author">Stephanie Heinzlmeir</li><li class="article-card__author">Bernhard Küster</li><li class="article-card__author">Christoph Sotriffer</li><li class="article-card__author">Elmar Wolf</li><li class="article-card__author">Stefan Knapp</li>
-            </ol>
-          
           <div class="article-card__meta">
-            <span class="visually-hidden">This article has </span><span>1 evaluation</span><span>Latest version <time datetime="2021-04-12">Apr 12, 2021</time></span><span>Latest activity <time datetime="2021-05-10">May 10, 2021</time></span>
+            <time datetime="2021-05-10">May 10, 2021</time>
           </div>
         </article>
       </li>
