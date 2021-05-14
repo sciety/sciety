bacdfe72004d7babe9be44f7f3f41b9e744d9267
Kevin Rutherford <kevin@rutherford-software.com>
2021-05-14 16:03:30 +0100

    Test equality in classes

--- test/types/id-options/id-options-class.test.ts
+++ test/types/id-options/id-options-class.test.ts
@@ -1,24 +1,55 @@
 /* eslint-disable max-classes-per-file */
+import * as Eq from 'fp-ts/Eq';
+import { pipe } from 'fp-ts/function';
+import * as S from 'fp-ts/string';
 
 describe('id-options', () => {
   describe('class', () => {
     class A {
+      private readonly value: string;
+
+      public static eqA: Eq.Eq<A> = pipe(S.Eq, Eq.contramap((a) => a.toString()));
+
+      constructor(x: string) {
+        this.value = x;
+      }
+
+      toString(): string {
+        return this.value;
+      }
     }
 
     const isA = (x: unknown) => x instanceof A;
 
     class B {
+      private readonly value: string;
+
+      constructor(x: string) {
+        this.value = x;
+      }
+
+      toString(): string {
+        return this.value;
+      }
     }
 
-    const a: A = new A();
-    const b: B = new B();
+    const a: A = new A('a');
+    const b: B = new B('b');
 
     it('runtime discrimination', () => {
       expect(isA(a)).toBe(true);
       expect(isA(b)).toBe(false);
     });
 
-    it.todo('equality');
+    it('equality', () => {
+      expect(a === b).toBe(false); //                               compiler error
+      expect(a.toString() === b.toString()).toBe(false);
+      expect(a === new A('a')).toBe(false);
+      expect(A.eqA.equals(a, a)).toBe(true);
+      expect(A.eqA.equals(a, new A('a'))).toBe(true);
+      expect(A.eqA.equals(a, new A('b'))).toBe(false);
+      expect(A.eqA.equals(a, b)).toBe(false); //                    compiler error
+    });
 
     it.todo('use in Map keys');
 
