97b5358f8870fe325c591b6fa7179ac44a8fe3cf
David Moulton <d.moulton@elifesciences.org>
2021-05-17 11:20:43 +0100

    Remove unused import #727

--- src/landing-page/landing-page-layout.ts
+++ src/landing-page/landing-page-layout.ts
@@ -3,7 +3,7 @@ import {
   cookieConsent, googleTagManager, googleTagManagerNoScript,
 } from '../shared-components/analytics';
 import { head } from '../shared-components/head';
-import { siteMenuFooter, siteMenuItems } from '../shared-components/site-menu';
+import { siteMenuItems } from '../shared-components/site-menu';
 import { utilityBar } from '../shared-components/utility-bar';
 import { Page } from '../types/page';
 import { User } from '../types/user';
