9cd621d293b98cd2731a7e5639b93f996540192b
Kevin Rutherford <kevin@rutherford-software.com>
2021-05-18 08:17:18 +0100

    Note when test values are arbitrary #407

--- test/types/review-id.test.ts
+++ test/types/review-id.test.ts
@@ -1,40 +1,44 @@
 import * as O from 'fp-ts/Option';
-import { Doi } from '../../src/types/doi';
-import { HypothesisAnnotationId } from '../../src/types/hypothesis-annotation-id';
-import * as NcrcId from '../../src/types/ncrc-id';
-import { fromString, ReviewId, toString } from '../../src/types/review-id';
+import { pipe } from 'fp-ts/function';
+import { arbitraryDoi } from './doi.helper';
+import { arbitraryHypothesisAnnotationId } from './hypothesis-annotation-id.helper';
+import { arbitraryNcrcId } from './ncrc-id.helper';
+import * as RI from '../../src/types/review-id';
 
 describe('review-id', () => {
   describe('when is a DOI', () => {
     it('can be serialized and deserialized', () => {
-      const doiValue = '10.1111/12345678';
-      const reviewId: ReviewId = new Doi(doiValue);
-      const serialization = toString(reviewId);
-      const deserialized = fromString(serialization);
+      const reviewId = arbitraryDoi();
 
-      expect(deserialized).toStrictEqual(O.some(reviewId));
+      expect(pipe(
+        reviewId,
+        RI.toString,
+        RI.fromString,
+      )).toStrictEqual(O.some(reviewId));
     });
   });
 
   describe('when is a Hypothesis annotation id', () => {
     it('can be serialized and deserialized', () => {
-      const hypothesisValue = 'GFEW8JXMEeqJQcuc-6NFhQ';
-      const reviewId: ReviewId = new HypothesisAnnotationId(hypothesisValue);
-      const serialization = toString(reviewId);
-      const deserialized = fromString(serialization);
+      const reviewId = arbitraryHypothesisAnnotationId();
 
-      expect(deserialized).toStrictEqual(O.some(reviewId));
+      expect(pipe(
+        reviewId,
+        RI.toString,
+        RI.fromString,
+      )).toStrictEqual(O.some(reviewId));
     });
   });
 
   describe('when is an NCRC id', () => {
     it('can be serialized and deserialized', () => {
-      const ncrcValue = 'fc00c499-879f-4db5-9e4e-2b38438776d9';
-      const reviewId = NcrcId.fromString(ncrcValue);
-      const serialization = toString(reviewId);
-      const deserialized = fromString(serialization);
+      const reviewId = arbitraryNcrcId();
 
-      expect(deserialized).toStrictEqual(O.some(reviewId));
+      expect(pipe(
+        reviewId,
+        RI.toString,
+        RI.fromString,
+      )).toStrictEqual(O.some(reviewId));
     });
   });
 
@@ -42,7 +46,7 @@ describe('review-id', () => {
     it('returns a none', () => {
       const unrecognisedFormat = 'foo';
 
-      expect(fromString(unrecognisedFormat)).toStrictEqual(O.none);
+      expect(RI.fromString(unrecognisedFormat)).toStrictEqual(O.none);
     });
   });
 });
