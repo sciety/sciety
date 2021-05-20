870f78a001aecb6f5df3bcbb13af1563461c2db5
Kevin Rutherford <kevin@rutherford-software.com>
2021-05-20 14:13:09 +0100

    Remove duplicate calls to gtag #744

--- src/shared-components/analytics.ts
+++ src/shared-components/analytics.ts
@@ -29,16 +29,7 @@ const renderTagManagerScript = (userId: O.Option<UserId>) => (tagManagerId: stri
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','${tagManagerId}');
-  </script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', '${tagManagerId}');
-
     ${renderPageLoadedByLoggedInUserEvent(userId)}
-
   </script>
 `;
 
