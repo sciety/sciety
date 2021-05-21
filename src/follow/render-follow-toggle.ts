f5a7a536d5c4c75f0bdaaa2548f94ae85fbb3cc1
Giorgio Sironi <g.sironi@elifesciences.org>
2021-05-21 13:20:51 +0100

    Introduce follow-button style #735

--- src/follow/render-follow-toggle.ts
+++ src/follow/render-follow-toggle.ts
@@ -7,7 +7,7 @@ import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 const renderFollowButton = (groupId: GroupId, groupName: string) => `
   <form method="post" action="/follow">
     <input type="hidden" name="${groupProperty}" value="${groupId.value}" />
-    <button type="submit" class="button button--primary button--small" aria-label="Follow ${groupName}">
+    <button type="submit" class="follow-button" aria-label="Follow ${groupName}">
       Follow
     </button>
   </form>
