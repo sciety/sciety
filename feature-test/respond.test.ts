ab024b13d4535e1f4c60cbc1af473326da22b591
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-05-24 16:04:09 +0100

    skip taiko test involving 'respond' #757

--- feature-test/respond.test.ts
+++ feature-test/respond.test.ts
@@ -13,7 +13,7 @@ describe('respond', () => {
   afterEach(screenshotTeardown);
 
   describe('when not logged in', () => {
-    it('authenticates via twitter, returns and displays increased response count', async () => {
+    it.skip('authenticates via twitter, returns and displays increased response count', async () => {
       await goto('localhost:8080/articles/10.1101/2020.07.13.199174');
       await click($('.activity-feed__item:first-child button[value="respond-helpful"]'));
       await authenticateViaTwitter();
