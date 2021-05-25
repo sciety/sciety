df53b1b53022fb8aed76f54e71bd1e2f20fec4b4
David Moulton <d.moulton@elifesciences.org>
2021-05-25 09:51:14 +0100

    Ensure NCRC & DOI review ids can be serialized & deserialized #757

--- test/types/codecs/ReviewIdFromString.test.ts
+++ test/types/codecs/ReviewIdFromString.test.ts
@@ -1,18 +1,21 @@
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import { ReviewIdFromString } from '../../../src/types/codecs/ReviewIdFromString';
+import { ReviewId } from '../../../src/types/review-id';
+import { arbitraryDoi } from '../doi.helper';
 import { arbitraryHypothesisAnnotationId } from '../hypothesis-annotation-id.helper';
+import { arbitraryNcrcId } from '../ncrc-id.helper';
 
 describe('codec ReviewIdFromString', () => {
-  describe('given a hypothesis id', () => {
-    it('encodes and decodes back to the same value', () => {
-      const id = arbitraryHypothesisAnnotationId();
-
-      expect(pipe(
-        id,
-        ReviewIdFromString.encode,
-        ReviewIdFromString.decode,
-      )).toStrictEqual(E.right(id));
-    });
+  it.each([
+    [arbitraryHypothesisAnnotationId()],
+    [arbitraryNcrcId()],
+    [arbitraryDoi()],
+  ])('encodes and decodes back to the same value %s', (id: ReviewId) => {
+    expect(pipe(
+      id,
+      ReviewIdFromString.encode,
+      ReviewIdFromString.decode,
+    )).toStrictEqual(E.right(id));
   });
 });
