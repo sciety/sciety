2d9a51a8fe53b62215cefac985ae841a4b49c33c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-05-25 14:14:54 +0100

    test list #750

--- test/search-results-page/select-subset-to-display.test.ts
+++ test/search-results-page/select-subset-to-display.test.ts
@@ -6,47 +6,51 @@ import { arbitraryDoi } from '../types/doi.helper';
 import { arbitraryGroupId } from '../types/group-id.helper';
 
 describe('select-subset-to-display', () => {
-  describe('prioritizing groups over articles', () => {
-    it('returns 2 groups and 0 articles when given a limit of 2, and 2 of each', () => {
-      const group1 = {
-        _tag: 'Group' as const,
-        id: arbitraryGroupId(),
-      };
-      const group2 = {
-        _tag: 'Group' as const,
-        id: arbitraryGroupId(),
-      };
-      const article = {
-        _tag: 'Article' as const,
-        doi: arbitraryDoi(),
-        server: 'biorxiv' as const,
-        title: pipe('', toHtmlFragment, sanitise),
-        authors: [''],
-        postedDate: new Date(),
-      };
-      const state = {
-        query: '',
-        groups: [group1, group2],
-        articles: {
-          items: [article, article],
-          total: 2,
-        },
-      };
-      const result = selectSubsetToDisplay(2)(state);
+  describe('given no category', () => {
+    describe('prioritizes groups over articles', () => {
+      it('returns 2 groups and 0 articles when given a limit of 2, and 2 of each', () => {
+        const group1 = {
+          _tag: 'Group' as const,
+          id: arbitraryGroupId(),
+        };
+        const group2 = {
+          _tag: 'Group' as const,
+          id: arbitraryGroupId(),
+        };
+        const article = {
+          _tag: 'Article' as const,
+          doi: arbitraryDoi(),
+          server: 'biorxiv' as const,
+          title: pipe('', toHtmlFragment, sanitise),
+          authors: [''],
+          postedDate: new Date(),
+        };
+        const state = {
+          query: '',
+          groups: [group1, group2],
+          articles: {
+            items: [article, article],
+            total: 2,
+          },
+        };
+        const result = selectSubsetToDisplay(2)(state);
 
-      expect(result.itemsToDisplay).toStrictEqual([group1, group2]);
+        expect(result.itemsToDisplay).toStrictEqual([group1, group2]);
+      });
     });
+
+    describe('length limit', () => { });
+
+    describe('total calculation', () => { });
+
+    describe('maintaining order of results', () => { });
   });
 
-  describe('length limit', () => {
-
+  describe('given the category of "articles"', () => {
+    it.todo('returns only articles when given articles and groups');
   });
 
-  describe('total calculation', () => {
-
-  });
-
-  describe('maintaining order of results', () => {
-
+  describe('given the category of "groups"', () => {
+    it.todo('returns only groups when given articles and groups');
   });
 });
