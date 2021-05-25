20995ff0fc3d35fc91ae352e2622592b9bb85c26
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-05-25 15:15:18 +0100

    make test more expressive #750

--- test/search-results-page/select-subset-to-display.test.ts
+++ test/search-results-page/select-subset-to-display.test.ts
@@ -3,41 +3,42 @@ import { pipe } from 'fp-ts/function';
 import { selectSubsetToDisplay } from '../../src/search-results-page/select-subset-to-display';
 import { toHtmlFragment } from '../../src/types/html-fragment';
 import { sanitise } from '../../src/types/sanitised-html-fragment';
+import { arbitraryDate, arbitraryString } from '../helpers';
 import { arbitraryDoi } from '../types/doi.helper';
 import { arbitraryGroupId } from '../types/group-id.helper';
+
+const arbitraryArticleItem = () => ({
+  _tag: 'Article' as const,
+  doi: arbitraryDoi(),
+  server: 'biorxiv' as const,
+  title: pipe(arbitraryString(), toHtmlFragment, sanitise),
+  authors: [arbitraryString()],
+  postedDate: arbitraryDate(),
+});
+
+const arbitraryGroupItem = () => ({
+  _tag: 'Group' as const,
+  id: arbitraryGroupId(),
+});
 
 describe('select-subset-to-display', () => {
   describe('given no category', () => {
     describe('prioritizes groups over articles', () => {
       it('returns 2 groups and 0 articles when given a limit of 2, and 2 of each', () => {
-        const group1 = {
-          _tag: 'Group' as const,
-          id: arbitraryGroupId(),
-        };
-        const group2 = {
-          _tag: 'Group' as const,
-          id: arbitraryGroupId(),
-        };
-        const article = {
-          _tag: 'Article' as const,
-          doi: arbitraryDoi(),
-          server: 'biorxiv' as const,
-          title: pipe('', toHtmlFragment, sanitise),
-          authors: [''],
-          postedDate: new Date(),
-        };
+        const groupItem1 = arbitraryGroupItem();
+        const groupItem2 = arbitraryGroupItem();
         const state = {
           query: '',
           category: O.none,
-          groups: [group1, group2],
+          groups: [groupItem1, groupItem2],
           articles: {
-            items: [article, article],
+            items: [arbitraryArticleItem(), arbitraryArticleItem()],
             total: 2,
           },
         };
         const result = selectSubsetToDisplay(2)(state);
 
-        expect(result.itemsToDisplay).toStrictEqual([group1, group2]);
+        expect(result.itemsToDisplay).toStrictEqual([groupItem1, groupItem2]);
       });
     });
 
@@ -50,30 +51,19 @@ describe('select-subset-to-display', () => {
 
   describe('given the category of "articles"', () => {
     it('returns only articles when given articles and groups', () => {
-      const group = {
-        _tag: 'Group' as const,
-        id: arbitraryGroupId(),
-      };
-      const article = {
-        _tag: 'Article' as const,
-        doi: arbitraryDoi(),
-        server: 'biorxiv' as const,
-        title: pipe('', toHtmlFragment, sanitise),
-        authors: [''],
-        postedDate: new Date(),
-      };
+      const articleItem = arbitraryArticleItem();
       const state = {
         query: '',
         category: O.some('articles'),
-        groups: [group],
+        groups: [arbitraryGroupItem()],
         articles: {
-          items: [article],
+          items: [articleItem],
           total: 1,
         },
       };
       const result = selectSubsetToDisplay(2)(state);
 
-      expect(result.itemsToDisplay).toStrictEqual([article]);
+      expect(result.itemsToDisplay).toStrictEqual([articleItem]);
     });
 
     it.todo('shows the correct count of matches');
@@ -81,30 +71,19 @@ describe('select-subset-to-display', () => {
 
   describe('given the category of "groups"', () => {
     it('returns only groups when given articles and groups', () => {
-      const group = {
-        _tag: 'Group' as const,
-        id: arbitraryGroupId(),
-      };
-      const article = {
-        _tag: 'Article' as const,
-        doi: arbitraryDoi(),
-        server: 'biorxiv' as const,
-        title: pipe('', toHtmlFragment, sanitise),
-        authors: [''],
-        postedDate: new Date(),
-      };
+      const groupItem = arbitraryGroupItem();
       const state = {
         query: '',
         category: O.some('groups'),
-        groups: [group],
+        groups: [groupItem],
         articles: {
-          items: [article],
+          items: [arbitraryArticleItem()],
           total: 1,
         },
       };
       const result = selectSubsetToDisplay(2)(state);
 
-      expect(result.itemsToDisplay).toStrictEqual([group]);
+      expect(result.itemsToDisplay).toStrictEqual([groupItem]);
     });
 
     it.todo('shows the correct count of matches');
