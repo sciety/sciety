820c6bada4983305e7169416a7d78123118a2a5d
BlueReZZ <p.shannon@elifesciences.org>
2021-05-28 15:47:54 +0100

    Update personas headline on landing page

--- src/landing-page/personas.ts
+++ src/landing-page/personas.ts
@@ -2,7 +2,7 @@ import { toHtmlFragment } from '../types/html-fragment';
 
 export const personas = toHtmlFragment(`
   <section class="landing-page-personas">
-    <h2 class="landing-page-personas__title">Where effort meets impact</h2>
+    <h2 class="landing-page-personas__title">Where evaluation meets impact</h2>
     <div class="landing-page-personas__content">
       <div class="landing-page-persona">
         <img src="/static/images/landing-persona-readers.svg" alt="" class="landing-page-persona__image">
