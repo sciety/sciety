6631bc2a2d50cbd477252c15fa44691d2dc96ec7
Kevin Rutherford <kevin@rutherford-software.com>
2021-06-02 13:34:10 +0100

    Remove knowledge of specific evaluation locator types from fetch-review #781

--- src/infrastructure/fetch-review.ts
+++ src/infrastructure/fetch-review.ts
@@ -1,10 +1,8 @@
 import * as TE from 'fp-ts/TaskEither';
 import { Evaluation } from './evaluation';
-import { Doi } from '../types/doi';
-import { HypothesisAnnotationId } from '../types/hypothesis-annotation-id';
-import { ReviewId } from '../types/review-id';
+import * as RI from '../types/review-id';
 
-export type FetchReview = (id: ReviewId) => TE.TaskEither<'unavailable' | 'not-found', Evaluation>;
+export type FetchReview = (id: RI.ReviewId) => TE.TaskEither<'unavailable' | 'not-found', Evaluation>;
 
 export type EvaluationFetcher = (key: string) => ReturnType<FetchReview>;
 
@@ -12,16 +10,15 @@ export const fetchReview = (
   fetchDataciteReview: EvaluationFetcher,
   fetchHypothesisAnnotation: EvaluationFetcher,
   fetchNcrcReview: EvaluationFetcher,
-): FetchReview => (
-  (id) => {
-    if (id instanceof Doi) {
-      return fetchDataciteReview(id.value);
-    }
+): FetchReview => (id) => {
+  const fetchers = new Map<string, EvaluationFetcher>();
+  fetchers.set('doi', fetchDataciteReview);
+  fetchers.set('hypothesis', fetchHypothesisAnnotation);
+  fetchers.set('ncrc', fetchNcrcReview);
 
-    if (id instanceof HypothesisAnnotationId) {
-      return fetchHypothesisAnnotation(id.value);
-    }
-
-    return fetchNcrcReview(id.value);
+  const f = fetchers.get(RI.service(id));
+  if (f) {
+    return f(RI.key(id));
   }
-);
+  return TE.left('not-found');
+};
