360f5e557774ff1a7d877718ece61e9150c5ccb3
Kevin Rutherford <kevin@rutherford-software.com>
2021-06-04 14:00:44 +0100

    Extract a builder for HTML documents #614

--- test/infrastructure/fetch-prelight-highlight.test.ts
+++ test/infrastructure/fetch-prelight-highlight.test.ts
@@ -5,28 +5,25 @@ import { pipe } from 'fp-ts/function';
 import { fetchPrelightsHighlight } from '../../src/infrastructure/fetch-prelights-highlight';
 import { arbitraryString, arbitraryUri } from '../helpers';
 
-describe('fetch-prelight-highlight', () => {
-  const ogDescription = arbitraryString();
-  const getHtml = () => TE.right(`
-    <!doctype html>
-    <html class="no-js" lang="en">
-    <head> 
-        <meta property="og:url" content="" />
-        <meta property="og:title" content="" />
-        <meta property="og:description" content="" />
-        <meta property="og:description" content="" />
-        <meta property="og:locale" content="en_GB" />
-        <meta property="og:type" content="article" />
-        <meta property="og:description" content="${ogDescription}" />
-    </head>
-    <body>
-    </body>
-    </html>
-  `);
+const makeDoc = (descriptions: Array<string>) => `
+  <!doctype html>
+  <html class="no-js" lang="en">
+  <head>
+      <meta property="og:url" content="" />
+      <meta property="og:title" content="" />
+      <meta property="og:locale" content="en_GB" />
+      <meta property="og:type" content="article" />
+      ${descriptions.map((d) => `<meta property="og:description" content="${d}" />`).join('\n')}
+  </head>
+  <body>
+  </body>
+  </html>
+`;
 
+describe('fetch-prelight-highlight', () => {
   it('given an arbitrary URL the result contains the same URL', async () => {
     const guid = new URL(arbitraryUri());
-
+    const getHtml = () => TE.right(makeDoc([arbitraryString()]));
     const evaluationUrl = await pipe(
       fetchPrelightsHighlight(getHtml)(guid.toString()),
       TE.map((evaluation) => evaluation.url.toString()),
@@ -37,7 +34,8 @@ describe('fetch-prelight-highlight', () => {
 
   it('returns the summary of the prelight', async () => {
     const guid = new URL(arbitraryUri());
-
+    const ogDescription = arbitraryString();
+    const getHtml = () => TE.right(makeDoc(['', '', ogDescription]));
     const fullText = await pipe(
       fetchPrelightsHighlight(getHtml)(guid.toString()),
       TE.map((evaluation) => evaluation.fullText.toString()),
