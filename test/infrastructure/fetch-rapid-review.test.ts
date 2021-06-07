65584603050c727caf172aa8ae8d99b2bb383cf4
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-06-07 15:49:06 +0100

    test hardcoded full-text #407

--- test/infrastructure/fetch-rapid-review.test.ts
+++ test/infrastructure/fetch-rapid-review.test.ts
@@ -15,7 +15,15 @@ describe('fetch-rapid-review', () => {
     expect(evaluationUrl).toStrictEqual(E.right(doiUrl));
   });
 
-  it.todo('returns the summary as the fullText');
+  it('returns the summary as the fullText', async () => {
+    const doiUrl = arbitraryUri();
+    const fullText = await pipe(
+      fetchRapidReview(doiUrl),
+      TE.map((evaluation) => evaluation.fullText),
+    )();
+
+    expect(fullText).toStrictEqual(E.right(expect.stringContaining('The authors make a convincing argument for re-envisioning US public health, employment and anti-discrimination laws around social solidarity, and a compelling case for further scholarship that considers the public health implications of employment law.')));
+  });
 
   describe('cant find fullText', () => {
     it.todo('return unavailable');
