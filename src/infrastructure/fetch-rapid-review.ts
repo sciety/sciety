c11f258622e45362998e5d702ef17036ebdb987c
Will Byrne <w.byrne@elifesciences.org>
2021-06-07 15:43:42 +0100

    use key for url property in fetcher return #407

--- src/infrastructure/fetch-rapid-review.ts
+++ src/infrastructure/fetch-rapid-review.ts
@@ -3,7 +3,7 @@ import * as TE from 'fp-ts/TaskEither';
 import { EvaluationFetcher } from './fetch-review';
 import { toHtmlFragment } from '../types/html-fragment';
 
-export const fetchRapidReview: EvaluationFetcher = () => TE.right({
+export const fetchRapidReview: EvaluationFetcher = (key) => TE.right({
   fullText: toHtmlFragment('The authors make a convincing argument for re-envisioning US public health, employment and anti-discrimination laws around social solidarity, and a compelling case for further scholarship that considers the public health implications of employment law.'),
-  url: new URL('https://doi.org/10.1162/2e3983f5.9328aef6'),
+  url: new URL(key),
 });
--- test/infrastructure/fetch-rapid-review.test.ts
+++ test/infrastructure/fetch-rapid-review.test.ts
@@ -1,25 +1,11 @@
-import { URL } from 'url';
 import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { fetchRapidReview } from '../../src/infrastructure/fetch-rapid-review';
-import { toHtmlFragment } from '../../src/types/html-fragment';
-import * as RapidReviewDoi from '../../src/types/rapid-review-doi';
 import { arbitraryUri } from '../helpers';
 
 describe('fetch-rapid-review', () => {
-  it('returns the evaluation', async () => {
-    const reviewId = RapidReviewDoi.fromValidatedString('10.1162/2e3983f5.602c0e93');
-    const evaluation = await fetchRapidReview(reviewId)();
-    const expected = {
-      fullText: pipe('The authors make a convincing argument for re-envisioning US public health, employment and anti-discrimination laws around social solidarity, and a compelling case for further scholarship that considers the public health implications of employment law.', toHtmlFragment),
-      url: new URL('https://doi.org/10.1162/2e3983f5.9328aef6'),
-    };
-
-    expect(evaluation).toStrictEqual(E.right(expected));
-  });
-
-  it.skip('given an arbitrary URL the result contains the same URL', async () => {
+  it('given an arbitrary URL the result contains the same URL', async () => {
     const doiUrl = arbitraryUri();
     const evaluationUrl = await pipe(
       fetchRapidReview(doiUrl),
