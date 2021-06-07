fa0d1ef9421723d0ecf562d01e66708e7001518c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-06-07 15:39:18 +0100

    test list for fetch-rapid-review #407

--- test/infrastructure/fetch-rapid-review.test.ts
+++ test/infrastructure/fetch-rapid-review.test.ts
@@ -1,9 +1,11 @@
 import { URL } from 'url';
 import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { fetchRapidReview } from '../../src/infrastructure/fetch-rapid-review';
 import { toHtmlFragment } from '../../src/types/html-fragment';
 import * as RapidReviewDoi from '../../src/types/rapid-review-doi';
+import { arbitraryUri } from '../helpers';
 
 describe('fetch-rapid-review', () => {
   it('returns the evaluation', async () => {
@@ -15,5 +17,21 @@ describe('fetch-rapid-review', () => {
     };
 
     expect(evaluation).toStrictEqual(E.right(expected));
+  });
+
+  it.skip('given an arbitrary URL the result contains the same URL', async () => {
+    const doiUrl = arbitraryUri();
+    const evaluationUrl = await pipe(
+      fetchRapidReview(doiUrl),
+      TE.map((evaluation) => evaluation.url.toString()),
+    )();
+
+    expect(evaluationUrl).toStrictEqual(E.right(doiUrl));
+  });
+
+  it.todo('returns the summary as the fullText');
+
+  describe('cant find fullText', () => {
+    it.todo('return unavailable');
   });
 });
