ee5aeadf09ef7c211b27bd24d8825976401ff795
Giorgio Sironi <g.sironi@elifesciences.org>
2021-06-09 15:06:59 +0100

    Make description one of multiple meta tags to include #407

--- test/infrastructure/fetch-rapid-review.test.ts
+++ test/infrastructure/fetch-rapid-review.test.ts
@@ -9,6 +9,8 @@ const html = `
 <!DOCTYPE html>
 <html lang="en" data-reactroot="">
 <head>
+    <meta name="dc.title" content="Review 1: &quot;Differential effects of antiseptic mouth rinses on SARS-CoV-2 infectivity in vitro&quot;">
+    <meta name="dc.creator" content="Florence Carrouel">
     <meta name="description"
           content="This potentially informative in-vitro study finds that some commercially available mouth-rinses have different anti-viral activity/cytotoxicity. Additional animal models and clinical trials are needed to generalize the study’s findings."/>
     <meta property="og:description"
@@ -44,16 +46,18 @@ describe('fetch-rapid-review', () => {
     expect(evaluationUrl).toStrictEqual(E.right(doiUrl));
   });
 
-  it('returns the summary as the fullText', async () => {
-    const doiUrl = arbitraryUri();
-    const getHtml = () => TE.right(html);
-    const fullText = await pipe(
-      doiUrl,
-      fetchRapidReview(getHtml),
-      TE.map((evaluation) => evaluation.fullText),
-    )();
+  describe('fullText', () => {
+    it('returns the description as part of the fullText', async () => {
+      const doiUrl = arbitraryUri();
+      const getHtml = () => TE.right(html);
+      const fullText = await pipe(
+        doiUrl,
+        fetchRapidReview(getHtml),
+        TE.map((evaluation) => evaluation.fullText),
+      )();
 
-    expect(fullText).toStrictEqual(E.right(expect.stringContaining('This potentially informative in-vitro study finds that some commercially available mouth-rinses have different anti-viral activity/cytotoxicity. Additional animal models and clinical trials are needed to generalize the study’s findings.')));
+      expect(fullText).toStrictEqual(E.right(expect.stringContaining('This potentially informative in-vitro study finds that some commercially available mouth-rinses have different anti-viral activity/cytotoxicity. Additional animal models and clinical trials are needed to generalize the study’s findings.')));
+    });
   });
 
   describe('cant find fullText', () => {
