7e3df9eeb31e66798edeec2010b4ebb747fd1012
Giorgio Sironi <g.sironi@elifesciences.org>
2021-06-09 15:48:49 +0100

    Display Rapid Reviews summaries #407

--- src/infrastructure/fetch-rapid-review.ts
+++ src/infrastructure/fetch-rapid-review.ts
@@ -9,22 +9,38 @@ import { toHtmlFragment } from '../types/html-fragment';
 
 type GetHtml = (url: string) => TE.TaskEither<'unavailable', string>;
 
+const summary = (doc: Document) => pipe(
+  doc.querySelector('meta[name=description]')?.getAttribute('content'),
+  O.fromNullable,
+  E.fromOption(constant('not-found' as const)),
+  E.map((description) => `
+    <h3>Strength of evidence</h3>
+    <p>${description}</p>
+  `),
+);
+
+const review = (doc: Document) => pipe(
+  {
+    creator: O.fromNullable(doc.querySelector('meta[name="dc.creator"]')?.getAttribute('content')),
+    title: O.fromNullable(doc.querySelector('meta[name="dc.title"]')?.getAttribute('content')),
+    description: O.fromNullable(doc.querySelector('meta[name=description]')?.getAttribute('content')),
+  },
+  ({ description, creator, title }) => `
+    ${pipe(creator, O.fold(constant(''), (txt) => `<h3>${txt}</h3>`))}
+    ${pipe(title, O.fold(constant(''), (txt) => `<p>${txt}</p>`))}
+    ${pipe(description, O.fold(constant(''), (txt) => `<p>${txt}</p>`))}
+  `,
+  E.right,
+);
+
 export const fetchRapidReview = (getHtml: GetHtml): EvaluationFetcher => (key) => pipe(
   key,
   getHtml,
   TE.chainEitherKW(flow(
-    (doc) => new JSDOM(doc),
-    (dom) => ({
-      creator: O.fromNullable(dom.window.document.querySelector('meta[name="dc.creator"]')?.getAttribute('content')),
-      title: O.fromNullable(dom.window.document.querySelector('meta[name="dc.title"]')?.getAttribute('content')),
-      description: O.fromNullable(dom.window.document.querySelector('meta[name=description]')?.getAttribute('content')),
-    }),
-    ({ description, creator, title }) => `
-      ${pipe(creator, O.fold(constant(''), (txt) => `<h3>${txt}</h3>`))}
-      ${pipe(title, O.fold(constant(''), (txt) => `<p>${txt}</p>`))}
-      ${pipe(description, O.fold(constant(''), (txt) => `<p>${txt}</p>`))}
-    `,
-    E.right,
+    (html) => new JSDOM(html).window.document,
+    (doc) => (doc.querySelectorAll('meta[name="dc.creator"]').length > 1
+      ? summary(doc)
+      : review(doc)),
     E.map(toHtmlFragment),
   )),
   TE.map((fullText) => ({
