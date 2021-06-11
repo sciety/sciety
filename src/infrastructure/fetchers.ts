92cce3079b291f05e9d47e78c11e396997889eac
Kevin Rutherford <kevin@rutherford-software.com>
2021-06-11 08:37:50 +0100

    Make the headers optional #814

--- src/infrastructure/fetchers.ts
+++ src/infrastructure/fetchers.ts
@@ -8,23 +8,23 @@ export const getJsonResponse = async (uri: string): Promise<AxiosResponse<Json>>
 
 type Gswhradl = (l: Logger) => (uri: string, h: Record<string, string>) => Promise<AxiosResponse<string>>;
 
-export const getStringWithHeadersRetriesAndDurationLogging: Gswhradl = (logger) => async (uri, headers) => {
+export const getStringWithHeadersRetriesAndDurationLogging: Gswhradl = (logger) => async (uri, headers = {}) => {
   const startTime = new Date();
   try {
     return await axios.get<string>(uri.toString(), { headers });
   } finally {
     const durationInMs = new Date().getTime() - startTime.getTime();
-    logger('debug', 'Response time to fetch article from Crossref', { uri, durationInMs });
+    logger('debug', 'Response time', { uri, durationInMs });
   }
 };
 
 type Gjwhadl = (l: Logger) => (uri: string, h: Record<string, string>) => Promise<AxiosResponse<Json>>;
 
-export const getJsonWithHeadersAndDurationLogging: Gjwhadl = (logger) => async (uri, headers) => {
+export const getJsonWithHeadersAndDurationLogging: Gjwhadl = (logger) => async (uri, headers = {}) => {
   const startTime = new Date();
   return axios.get<Json>(uri, { headers })
     .finally(() => {
       const durationInMs = new Date().getTime() - startTime.getTime();
-      logger('debug', 'Response time to fetch users from Twitter', { uri, durationInMs });
+      logger('debug', 'Response time', { uri, durationInMs });
     });
 };
