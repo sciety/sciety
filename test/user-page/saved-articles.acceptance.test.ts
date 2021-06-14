a43f190193161d623658456d3746972cc98d5879
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-06-14 15:43:24 +0100

    implement test and skip #817

--- test/user-page/saved-articles.acceptance.test.ts
+++ test/user-page/saved-articles.acceptance.test.ts
@@ -1,6 +1,28 @@
+import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import { userSavedArticle } from '../../src/types/domain-events';
+import { savedArticles } from '../../src/user-page/saved-articles/saved-articles';
+import { arbitraryDoi } from '../types/doi.helper';
+import { arbitraryUserId } from '../types/user-id.helper';
+
 describe('saved-articles acceptance', () => {
   describe('article details unavailable for any article', () => {
-    it.todo('displays a single error message');
+    it.skip('displays a single error message', async () => {
+      const userId = arbitraryUserId();
+      const adapters = {
+        getAllEvents: T.of([
+          userSavedArticle(
+            userId,
+            arbitraryDoi(),
+          ),
+        ]),
+        fetchArticle: () => TE.left('unavailabe'),
+      };
+      const component = await savedArticles(adapters)(userId)();
+
+      expect(component).toStrictEqual(E.right(expect.stringContaining('<p>We couldn\'t find this information; please try again later.</p>')));
+    });
   });
 
   describe('user has no saved articles', () => {
