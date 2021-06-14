187d1407deedcd4619195d4a7bdd796ccb780da0
Giorgio Sironi <g.sironi@elifesciences.org>
2021-06-14 15:28:00 +0100

    Add starting test list #817

---
+++ test/user-page/saved-articles.acceptance.test.ts
@@ -0,0 +1,9 @@
+describe('saved-articles acceptance', () => {
+  describe('article details unavailable for any article', () => {
+    it.todo('displays a single error message');
+  });
+
+  describe('user has no saved articles', () => {
+    it.todo('does not render the component');
+  });
+});
