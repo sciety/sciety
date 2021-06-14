92ee88992c40f8b1ee6081c0ade1528532abd64a
Giorgio Sironi <g.sironi@elifesciences.org>
2021-06-14 13:21:04 +0100

    Implement todo test #741

--- test/infrastructure/fetch-review.test.ts
+++ test/infrastructure/fetch-review.test.ts
@@ -1,6 +1,26 @@
+import { URL } from 'url';
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import { EvaluationFetcher, fetchReview } from '../../src/infrastructure/fetch-review';
+import { ReviewId } from '../../src/types/review-id';
+import { arbitraryHtmlFragment, arbitraryUri, arbitraryWord } from '../helpers';
+
 describe('fetch-review', () => {
   describe('when a service is supported', () => {
-    it.todo('returns the fetched evaluation');
+    it('returns the fetched evaluation', async () => {
+      const serviceName = arbitraryWord();
+      const evaluation = {
+        fullText: arbitraryHtmlFragment(),
+        url: new URL(arbitraryUri()),
+      };
+      const fetchers = new Map<string, EvaluationFetcher>();
+      fetchers.set(serviceName, () => TE.right(evaluation));
+
+      const id = `${serviceName}:${arbitraryWord()}` as ReviewId;
+      const result = await fetchReview(fetchers)(id)();
+
+      expect(result).toStrictEqual(E.right(evaluation));
+    });
   });
 
   describe('when a service is not supported', () => {
