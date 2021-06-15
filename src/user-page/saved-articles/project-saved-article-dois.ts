b9721f8a4b5d9eb93b931c6fbe53a18bacf04ac7
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-06-15 10:51:10 +0100

    remove fetch-saved-articles through inlining #817

--- src/user-page/saved-articles/project-saved-article-dois.ts
+++ src/user-page/saved-articles/project-saved-article-dois.ts
@@ -1,10 +1,13 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import { flow, pipe } from 'fp-ts/function';
-import { GetSavedArticleDois } from './fetch-saved-articles';
+import { Doi } from '../../types/doi';
 import { DomainEvent, isUserSavedArticleEvent } from '../../types/domain-events';
+import { UserId } from '../../types/user-id';
 
 export type GetAllEvents = T.Task<ReadonlyArray<DomainEvent>>;
+
+type GetSavedArticleDois = (userId: UserId) => T.Task<ReadonlyArray<Doi>>;
 
 export const projectSavedArticleDois = (getAllEvents: GetAllEvents): GetSavedArticleDois => (userId) => pipe(
   getAllEvents,
--- src/user-page/saved-articles/saved-articles.ts
+++ src/user-page/saved-articles/saved-articles.ts
@@ -2,11 +2,13 @@ import * as E from 'fp-ts/Either';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { flow } from 'fp-ts/function';
-import { FetchArticle, fetchSavedArticles } from './fetch-saved-articles';
 import { GetAllEvents, projectSavedArticleDois } from './project-saved-article-dois';
 import { renderSavedArticles } from './render-saved-articles';
+import { Doi } from '../../types/doi';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 import { UserId } from '../../types/user-id';
+
+type FetchArticle = (doi: Doi) => TE.TaskEither<unknown, { doi: Doi, title: HtmlFragment }>;
 
 export type Ports = {
   getAllEvents: GetAllEvents,
@@ -19,7 +21,7 @@ type SavedArticles = (ports: Ports) => (u: UserId) => TE.TaskEither<never, HtmlF
 
 export const savedArticles: SavedArticles = (ports) => flow(
   projectSavedArticleDois(ports.getAllEvents),
-  T.chain(fetchSavedArticles(ports.fetchArticle)),
+  T.chain(TE.traverseArray(ports.fetchArticle)),
   T.map(E.fold(renderUnavailable, renderSavedArticles)),
   TE.rightTask,
 );
