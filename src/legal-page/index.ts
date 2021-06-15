8ae6adba06d8ab7bef8f660d4323e11d656be636
BlueReZZ <p.shannon@elifesciences.org>
2021-06-15 15:47:57 +0100

    Update privacy policy to show more detail on Intercom and Clarity #763

--- src/legal-page/index.ts
+++ src/legal-page/index.ts
@@ -48,17 +48,40 @@ export const legalPage: Page = {
         This site adds the following to the information specified in the privacy notice linked above.
       </p>
 
+      <h4>Twitter</h4>
       <p>
         When you log in to your Twitter account through our site (for example to store your feed preferences) that interaction is directly with Twitter Inc. We only receive from Twitter a user name and user identifier. Your Twitter user identifier is associated with items that you follow on the site (groups, people, papers and evaluation events) and used to publicly display events that you follow on your user page.
       </p>
 
+      <h4>Intercom</h4>
       <p>
-        Sciety uses an analytics service called Intercom to help improve our application. In particular, we provide a limited amount of your information (such as email address if you enter this into the chat field when asked) to Intercom, Inc. (“Intercom”) and utilise Intercom to collect data for analytics purposes when you visit our website or use our product.
-        As a data processor acting on our behalf, Intercom analyzes your use of our application and tracks our relationship by way of cookies and similar technologies so that we can improve our service to you. For more information on Intercom's use of cookies, please visit <a href="https://www.intercom.com/terms-and-policies#cookie-policy">https://www.intercom.com/terms-and-policies#cookie-policy</a>. We may also use Intercom as a medium for communications, either through email, or through messages within our application. As part of our service agreements, Intercom collects publicly available contact and social information related to you, such as your email address, gender, company, job title, photos, website URLs, social network handles and physical addresses, to enhance your user experience. For more information on the privacy practices of Intercom, please visit <a href="https://www.intercom.com/terms-and-policies#privacy">https://www.intercom.com/terms-and-policies#privacy</a>. Intercom’s services are governed by Intercom’s terms of use which can be found at <a href="https://www.intercom.com/terms-and-policies#terms">https://www.intercom.com/terms-and-policies#terms</a>. If you would like to opt out of having this information collected by or submitted to Intercom, please contact us.
+        Sciety uses an analytics service called Intercom to help improve our application. This is the service that powers the small icon in the bottom right of the page that allows you to chat directly with our team.  It also lets you find information by interacting with an automated response bot and leave us a message for feedback when we’re offline.
+      </p>
+      <p>
+        We do not send any of your data from Sciety directly to Intercom but when you interact with the chat widget Intercom will collect information about your visit on our behalf. This information includes web sessions, last seen time, first seen time, recent page views, sign up day, city and country (calculated from your IP address), last contacted time, last heard from time, tag (a group we might have assigned you to in order to organise our chats with you), operating system, browser language, browser version, language override and any messages, comments or conversations you have with us or the bot via the Intercom widget. 
+      </p>
+      <p>
+        You also have the option to give us your email address so that we can respond later but we only send this to Intercom if you enter it in the box when prompted by the Intercom widget - when supplying your email address we then store that in the Intercom platform along with information about the last email from us that you opened, the last link in an email from us that you clicked on and any emails from us that you have unsubscribed from.
+      </p>
+      <p>
+        Intercom is a very broad application but we use it in a limited capacity at Sciety, taking our users’ privacy into account. We only track and store the data mentioned above via Intercom, and only when you interact with the widget and consent to Statistics cookies. You can <a href="https://www.intercom.com/help/en/articles/1722980-how-intercom-tracks-and-stores-data">find out more about the way Intercom tracks and stores data in their post on the subject.</a>
+      </p>
+      <p>
+        For more information on Intercom's use of cookies, please visit <a href="https://www.intercom.com/terms-and-policies#cookie-policy">https://www.intercom.com/terms-and-policies#cookie-policy</a>. For more information on the privacy practices of Intercom, please visit <a href="https://www.intercom.com/terms-and-policies#privacy">https://www.intercom.com/terms-and-policies#privacy</a>. Intercom’s services are governed by Intercom’s terms of use which can be found at <a href="https://www.intercom.com/terms-and-policies#terms">https://www.intercom.com/terms-and-policies#terms</a>. If you would like to opt out of having this information collected by or submitted to Intercom, please contact us via email at <a href="mailto:connect@sciety.org">connect@sciety.org</a>.
       </p>
 
+      <h4>Microsoft Clarity</h4>
       <p>
-        Sciety makes use of a user behaviour analytics tool called Clarity from Microsoft that generates heatmaps and simulations of user scrolling and clicks by recording anonymous cursor movement data. As a data processor acting on our behalf, Microsoft analyzes your use of our application and tracks your visit between pages using cookies and similar technologies so that we can improve our service to you. For more information on Clarity please visit their Frequently Asked Questions <a href="https://docs.microsoft.com/en-us/clarity/faq">https://docs.microsoft.com/en-us/clarity/faq</a>, for more information on how Microsoft uses cookies please visit their privacy statement <a href="https://privacy.microsoft.com/privacystatement">https://privacy.microsoft.com/privacystatement</a> and for information on Microsoft's terms of use for Clarity please visit their Terms of use <a href="https://clarity.microsoft.com/terms">https://clarity.microsoft.com/terms</a>. If you would like to opt out of having this information collected by or submitted to Microsoft, please contact us.
+        Sciety makes use of a user behaviour analytics tool called Clarity from Microsoft that generates heatmaps and simulations of user scrolling and clicks by recording anonymous cursor movement data.
+      </p>
+      <p>
+        Microsoft may use the collected data to improve their own services although Microsoft does not sell data from Clarity. If you consent to Marketing cookies Microsoft will use a unique advertising ID on Sciety that is then used to provide targeted advertising on websites that use the same advertising network. Sciety does not use advertising but allows the use of this type of cookie as part of our adherence to the terms of use for Microsoft Clarity.
+      </p>
+      <p>
+         For more information on Clarity, particularly with regard to privacy, please visit their Frequently Asked Questions <a href="https://docs.microsoft.com/en-us/clarity/faq">https://docs.microsoft.com/en-us/clarity/faq</a>
+      </p>
+      <p>
+        For more information on how Microsoft uses cookies please visit their privacy statement <a href="https://privacy.microsoft.com/privacystatement">https://privacy.microsoft.com/privacystatement</a> and for information on Microsoft's terms of use for Clarity please visit their Terms of use <a href="https://clarity.microsoft.com/terms">https://clarity.microsoft.com/terms</a>. If you would like to opt out of having this information collected by or submitted to Microsoft, please contact us via email at <a href="mailto:connect@sciety.org">connect@sciety.org</a>.
       </p>
 
       <h3>
@@ -77,6 +100,10 @@ export const legalPage: Page = {
 
       <p>
         2nd June 2021: added Microsoft Clarity information to Sciety privacy policy.
+      </p>
+      
+      <p>
+        15th June 2021: clarified the exact use of data by Intercom and Microsoft Clarity as their own documentation was too broad
       </p>
 
       <h2>Cookie declaration</h2>
