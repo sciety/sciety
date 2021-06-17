8047a90d11840c646c1548a986bc7afd889859d2
Kevin Rutherford <kevin@rutherford-software.com>
2021-06-17 09:57:22 +0100

    Add a first test list for the saved-articles-page #819

---
+++ test/user-page/saved-articles-page/saved-articles-page.acceptance.test.ts
@@ -0,0 +1,17 @@
+describe('saved-articles-page', () => {
+  it.todo('shows the user details');
+
+  describe('when the user has saved articles', () => {
+    it.todo('shows the count in the tab');
+
+    it.todo('shows the articles as a list of cards');
+  });
+
+  describe('when the user has no saved articles', () => {
+    it.todo('shows a count of 0 in the tab');
+
+    it.todo('shows no list of article cards');
+
+    it.todo('shows a message saying that the user has no saved articles');
+  });
+});
