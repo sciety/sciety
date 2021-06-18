236c4ba80fa9f04576a4157beb8a423b1b4ce8ac
Kevin Rutherford <kevin@rutherford-software.com>
2021-06-18 13:10:53 +0100

    Remove the old user page #819

--- src/user-page/render-page.ts
+++ src/user-page/render-page.ts
@@ -1,32 +1,5 @@
-import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
-import { Page } from '../types/page';
+import { toHtmlFragment } from '../types/html-fragment';
 import { RenderPageError } from '../types/render-page-error';
-
-type Components = {
-  header: HtmlFragment,
-  followList: HtmlFragment,
-  userDisplayName: string,
-  savedArticles: HtmlFragment,
-};
-
-export const renderPage = ({
-  header, followList, savedArticles, userDisplayName,
-}: Components): Page => (
-  {
-    title: userDisplayName,
-    content: toHtmlFragment(`
-      <div class="page-content__background">
-        <div class="sciety-grid sciety-grid--user">
-          ${header}
-          <div class="user-page-contents">
-            ${savedArticles}
-            ${followList}
-          </div>
-        </div>
-      </div>
-    `),
-  }
-);
 
 export const renderErrorPage = (e: 'not-found' | 'unavailable'): RenderPageError => {
   if (e === 'not-found') {
