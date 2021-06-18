b6ead37e922c73111e95d4f314e2757f0230e7c2
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-06-18 13:17:53 +0100

    update link from article to saved-articles #819

--- src/article-page/render-saved-link.ts
+++ src/article-page/render-saved-link.ts
@@ -3,7 +3,7 @@ import { toHtmlFragment } from '../types/html-fragment';
 import { UserId } from '../types/user-id';
 
 const templateSavedLink = (userId: UserId) => `
-  <a class="saved-to-list" href="/users/${userId}#saved-articles">
+  <a class="saved-to-list" href="/users/${userId}/saved-articles">
     <img src="/static/images/playlist_add_check-24px.svg" alt="" class="saved-to-list__icon">
     Saved to my list
   </a>
