965bc6a56f2bf8476f22a972c1904561937abc1b
Giorgio Sironi <g.sironi@elifesciences.org>
2021-06-21 16:02:55 +0100

    Update CSS selector for saved-articles-page #819

--- feature-test/redirects.test.ts
+++ feature-test/redirects.test.ts
@@ -24,7 +24,7 @@ describe('legacy redirects', () => {
   describe('user page', () => {
     it('redirects to the saved-articles tab', async () => {
       await goto('localhost:8080/users/1295307136415735808');
-      const isSavedArticlesTab = await text('Saved articles', within($('h3.user-page-tab--heading'))).exists();
+      const isSavedArticlesTab = await text('Saved articles', within($('.tab--active'))).exists();
 
       expect(isSavedArticlesTab).toBe(true);
     });
