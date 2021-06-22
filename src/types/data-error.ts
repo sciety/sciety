fb22d8ca5c04977cdc93b28ee3a1b1a90b24e5ed
Kevin Rutherford <kevin@rutherford-software.com>
2021-06-22 11:55:27 +0100

    Encapsulate an error type with a fold #830

--- src/types/data-error.ts
+++ src/types/data-error.ts
@@ -1,6 +1,12 @@
+import { Lazy } from 'fp-ts/function';
+
 export const notFound = 'not-found';
 
-type DataError = typeof notFound | 'unavailable';
+export type DataError = typeof notFound | 'unavailable';
 
 // ts-unused-exports:disable-next-line
 export const isNotFound = (de: DataError): boolean => de === notFound;
+
+export const fold = <B>(opts: Record<'notFound' | 'unavailable', Lazy<B>>) => (e: DataError): B => (
+  isNotFound(e) ? opts.notFound() : opts.unavailable()
+);
--- src/user-page/render-error-page.ts
+++ src/user-page/render-error-page.ts
@@ -1,15 +1,17 @@
+import { pipe } from 'fp-ts/function';
+import * as DE from '../types/data-error';
 import { toHtmlFragment } from '../types/html-fragment';
 import { RenderPageError } from '../types/render-page-error';
 
-export const renderErrorPage = (e: 'not-found' | 'unavailable'): RenderPageError => {
-  if (e === 'not-found') {
-    return {
-      type: 'not-found',
-      message: toHtmlFragment('User not found'),
-    };
-  }
-  return {
-    type: 'unavailable',
-    message: toHtmlFragment('User information unavailable'),
-  };
-};
+export const renderErrorPage = (e: DE.DataError): RenderPageError => pipe(
+  e,
+  DE.fold({
+    notFound: () => 'User not found',
+    unavailable: () => 'User information unavailable',
+  }),
+  toHtmlFragment,
+  (message) => ({
+    type: e,
+    message,
+  }),
+);
