fee6ec6828942744af990614b91b73c1073d7dce
Will Byrne <w.byrne@elifesciences.org>
2021-06-23 13:47:26 +0100

    remove rendered whitespace in tablist #819

--- src/shared-components/tabs.ts
+++ src/shared-components/tabs.ts
@@ -12,22 +12,17 @@ type Tabs = (tabProps: {
   activeTabPanelContents: HtmlFragment,
 ) => HtmlFragment;
 
-const activeTab = (tab: Tab) => `
-  <li class="tab tab--active" role="presentation">
+const activeTab = (tab: Tab) => `<li class="tab tab--active" role="presentation">
     <span role="tab" id="active-tab" aria-selected="true">${tab.label}</span>
-  </li>
-`;
+  </li>`;
 
-const inactiveTab = (tab: Tab) => `
-  <li class="tab" role="presentation">
+const inactiveTab = (tab: Tab) => `<li class="tab" role="presentation">
     <a role="tab" href="${tab.url}">${tab.label}</a>
-  </li>
-`;
+  </li>`;
 
 export const tabs: Tabs = ({ tabList, activeTabIndex }) => (activeTabPanelContents) => toHtmlFragment(`
   <ul class="tab-list" role="tablist">
-    ${activeTabIndex === 0 ? activeTab(tabList[0]) : inactiveTab(tabList[0])}
-    ${activeTabIndex === 0 ? inactiveTab(tabList[1]) : activeTab(tabList[1])}
+    ${activeTabIndex === 0 ? activeTab(tabList[0]) : inactiveTab(tabList[0])}${activeTabIndex === 0 ? inactiveTab(tabList[1]) : activeTab(tabList[1])}
   </ul>
   <section class="tab-panel" role="tabpanel" aria-labelledby="active-tab">
     ${activeTabPanelContents}
