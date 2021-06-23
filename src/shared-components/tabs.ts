1334f4cb30d2a70e060706f1757ffb43593fe584
Giorgio Sironi <g.sironi@elifesciences.org>
2021-06-23 14:00:49 +0100

    Extract bothTabs to reduce line length #819

--- src/shared-components/tabs.ts
+++ src/shared-components/tabs.ts
@@ -5,12 +5,12 @@ export type Tab = {
   url: string,
 };
 
-type Tabs = (tabProps: {
+type TabProps = {
   tabList: [Tab, Tab],
   activeTabIndex: 0 | 1,
-}) => (
-  activeTabPanelContents: HtmlFragment,
-) => HtmlFragment;
+};
+
+type Tabs = (tabProps: TabProps) => (activeTabPanelContents: HtmlFragment) => HtmlFragment;
 
 const activeTab = (tab: Tab) => `<li class="tab tab--active" role="presentation">
     <span role="tab" id="active-tab" aria-selected="true">${tab.label}</span>
@@ -20,9 +20,13 @@ const inactiveTab = (tab: Tab) => `<li class="tab" role="presentation">
     <a role="tab" href="${tab.url}">${tab.label}</a>
   </li>`;
 
-export const tabs: Tabs = ({ tabList, activeTabIndex }) => (activeTabPanelContents) => toHtmlFragment(`
+const bothTabs = ({ tabList, activeTabIndex }: TabProps) => (activeTabIndex === 0
+  ? `${activeTab(tabList[0])}${inactiveTab(tabList[1])}`
+  : `${inactiveTab(tabList[0])}${activeTab(tabList[1])}`);
+
+export const tabs: Tabs = (tabProps) => (activeTabPanelContents) => toHtmlFragment(`
   <ul class="tab-list" role="tablist">
-    ${activeTabIndex === 0 ? activeTab(tabList[0]) : inactiveTab(tabList[0])}${activeTabIndex === 0 ? inactiveTab(tabList[1]) : activeTab(tabList[1])}
+    ${bothTabs(tabProps)}
   </ul>
   <section class="tab-panel" role="tabpanel" aria-labelledby="active-tab">
     ${activeTabPanelContents}
