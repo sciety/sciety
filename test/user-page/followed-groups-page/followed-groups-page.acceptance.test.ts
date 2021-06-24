a3795ecea2f772189b57081cd5695ed880f8dae4
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-06-24 11:11:58 +0100

    skipped test for group cards #818

--- test/user-page/followed-groups-page/followed-groups-page.acceptance.test.ts
+++ test/user-page/followed-groups-page/followed-groups-page.acceptance.test.ts
@@ -2,13 +2,16 @@ import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
+import * as TO from 'fp-ts/TaskOption';
 import { pipe } from 'fp-ts/function';
 import { JSDOM } from 'jsdom';
+import { userFollowedEditorialCommunity } from '../../../src/types/domain-events';
 import { Page } from '../../../src/types/page';
 import { RenderPageError } from '../../../src/types/render-page-error';
 import { followedGroupsPage } from '../../../src/user-page/followed-groups-page/followed-groups-page';
 import { arbitraryString, arbitraryUri, arbitraryWord } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
+import { arbitraryGroupId } from '../../types/group-id.helper';
 import { arbitraryUserId } from '../../types/user-id.helper';
 
 const contentOf = (page: TE.TaskEither<RenderPageError, Page>) => pipe(
@@ -64,5 +67,38 @@ describe('followed-groups-page', () => {
     )();
 
     expect(page).toStrictEqual(E.right(expect.objectContaining({ title: userDisplayName })));
+  });
+
+  describe('user is following groups', () => {
+    it.skip('displays followed groups as group cards', async () => {
+      const userId = arbitraryUserId();
+      const ports = {
+        follows: shouldNotBeCalled,
+        getGroup: () => TO.some({
+          id: arbitraryGroupId(),
+          name: arbitraryString(),
+          avatarPath: arbitraryString(),
+        }),
+        getUserDetails: () => TE.right({
+          avatarUrl: arbitraryUri(),
+          displayName: arbitraryString(),
+          handle: arbitraryWord(),
+        }),
+        getAllEvents: T.of([
+          userFollowedEditorialCommunity(userId, arbitraryGroupId()),
+          userFollowedEditorialCommunity(userId, arbitraryGroupId()),
+        ]),
+      };
+      const params = { id: userId, user: O.none };
+      const page = await pipe(
+        params,
+        followedGroupsPage(ports),
+        contentOf,
+        T.map(JSDOM.fragment),
+      )();
+      const groupCards = page.querySelectorAll('.group-card');
+
+      expect(groupCards).toHaveLength(2);
+    });
   });
 });
