2733be361b5845e3b22d234f41fb96337041378f
Giorgio Sironi <g.sironi@elifesciences.org>
2021-06-25 10:17:27 +0100

    Avoid filling in form in Taiko #848

--- feature-test/utilities.ts
+++ feature-test/utilities.ts
@@ -3,13 +3,13 @@ import {
 } from 'taiko';
 
 export const authenticateViaTwitter = async (): Promise<void> => {
-  if (process.env.TAIKO_TWITTER_USERNAME && process.env.TAIKO_TWITTER_PASSWORD) {
-    await write(process.env.TAIKO_TWITTER_USERNAME ?? '', into(textBox('Username')));
-    await write(process.env.TAIKO_TWITTER_PASSWORD ?? '', into(textBox('Password')));
-    await click('Sign in');
-  } else {
-    throw new Error('Missing TAIKO_TWITTER_USERNAME and/or TAIKO_TWITTER_PASSWORD env vars');
-  }
+  //if (process.env.TAIKO_TWITTER_USERNAME && process.env.TAIKO_TWITTER_PASSWORD) {
+  //  await write(process.env.TAIKO_TWITTER_USERNAME ?? '', into(textBox('Username')));
+  //  await write(process.env.TAIKO_TWITTER_PASSWORD ?? '', into(textBox('Password')));
+  //  await click('Sign in');
+  //} else {
+  //  throw new Error('Missing TAIKO_TWITTER_USERNAME and/or TAIKO_TWITTER_PASSWORD env vars');
+  //}
 };
 
 export const screenshotTeardown = async (): Promise<void> => {
