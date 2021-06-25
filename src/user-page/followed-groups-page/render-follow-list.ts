44d24022cab973a9ba82eac2b9e33937e8fd7e59
David Moulton <d.moulton@elifesciences.org>
2021-06-25 11:51:38 +0100

    Remove unreachable code #818

--- src/user-page/followed-groups-page/render-follow-list.ts
+++ src/user-page/followed-groups-page/render-follow-list.ts
@@ -1,23 +1,19 @@
-import * as O from 'fp-ts/Option';
-import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
-import { constant, flow } from 'fp-ts/function';
-import { templateListItems } from '../../shared-components/list-items';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 
 export const followingNothing = `
   <p class="followed-groups__no_groups">This user is currently not following any groups.</p>
 `;
 
-const renderList = (list: RNEA.ReadonlyNonEmptyArray<HtmlFragment>) => (`
+// TODO: should take a RNEA, but not possible due to traverse array in followList until fp-ts 2.11 / 3.0
+const templateListItems = (items: ReadonlyArray<HtmlFragment>, itemClass = 'item'): HtmlFragment => (
+  toHtmlFragment(
+    items.map((item: HtmlFragment) => `<li class="${itemClass}">${item}</li>\n`)
+      .join(''),
+  )
+);
+
+export const renderFollowList = (list: ReadonlyArray<HtmlFragment>): HtmlFragment => toHtmlFragment(`
   <ol class="followed-groups__list" role="list">
     ${templateListItems(list, 'followed-groups__item')}
   </ol>
 `);
-
-type RenderFollowList = (communities: ReadonlyArray<HtmlFragment>) => HtmlFragment;
-
-export const renderFollowList: RenderFollowList = flow(
-  RNEA.fromReadonlyArray,
-  O.fold(constant(followingNothing), renderList),
-  toHtmlFragment,
-);
