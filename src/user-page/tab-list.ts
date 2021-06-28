82337f7bc92388a8fb168be94b2d4895c358d3d9
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-06-28 11:01:52 +0100

    prepare tabList for followedGroupCount #831

--- src/user-page/tab-list.ts
+++ src/user-page/tab-list.ts
@@ -2,7 +2,13 @@ import { Tab } from '../shared-components/tabs';
 import { toHtmlFragment } from '../types/html-fragment';
 import { UserId } from '../types/user-id';
 
-export const tabList = (userId: UserId, count?: number): [Tab, Tab] => [
-  { label: toHtmlFragment(`Saved articles${count !== undefined ? ` (${count})` : ''}`), url: `/users/${userId}/saved-articles` },
-  { label: toHtmlFragment('Followed groups'), url: `/users/${userId}/followed-groups` },
+export const tabList = (userId: UserId, savedArticleCount?: number, followedGroupsCount?: number): [Tab, Tab] => [
+  {
+    label: toHtmlFragment(`Saved articles${savedArticleCount !== undefined ? ` (${savedArticleCount})` : ''}`),
+    url: `/users/${userId}/saved-articles`,
+  },
+  {
+    label: toHtmlFragment(`Followed groups${followedGroupsCount !== undefined ? ` (${followedGroupsCount})` : ''}`),
+    url: `/users/${userId}/followed-groups`,
+  },
 ];
