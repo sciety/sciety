6328c504761121f882087cfb07836187ecfd5097
David Moulton <d.moulton@elifesciences.org>
2021-06-29 15:42:14 +0100

    Simplify pipe #831

--- src/user-page/followed-groups-page/followed-groups-page.ts
+++ src/user-page/followed-groups-page/followed-groups-page.ts
@@ -1,7 +1,7 @@
 import { sequenceS } from 'fp-ts/Apply';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
-import { flow, pipe } from 'fp-ts/function';
+import { pipe } from 'fp-ts/function';
 import { followList, Ports as FollowListPorts } from './follow-list';
 import { followedGroupIds } from './project-followed-group-ids';
 import { tabs } from '../../shared-components/tabs';
@@ -30,24 +30,19 @@ type Params = {
 type FollowedGroupsPage = (params: Params) => TE.TaskEither<RenderPageError, Page>;
 
 export const followedGroupsPage = (ports: Ports): FollowedGroupsPage => ({ id }) => pipe(
-  ports.getUserDetails(id),
-  (details) => ({
-    activeTabIndex: TE.right(1 as const),
+  {
+    userDetails: ports.getUserDetails(id),
     dois: TE.rightTask(projectSavedArticleDois(ports.getAllEvents)(id)),
     groupIds: TE.rightTask(followedGroupIds(ports.getAllEvents)(id)),
-    header: pipe(
-      ports.getUserDetails(id),
-      TE.map(renderHeader),
-    ),
-    userDisplayName: pipe(
-      details,
-      TE.map(flow(
-        ({ displayName }) => displayName,
-        toHtmlFragment,
-      )),
-    ),
-  }),
+  },
   sequenceS(TE.ApplyPar),
+  TE.map(({ userDetails, dois, groupIds }) => ({
+    activeTabIndex: 1 as const,
+    header: renderHeader(userDetails),
+    userDisplayName: toHtmlFragment(userDetails.displayName),
+    dois,
+    groupIds,
+  })),
   TE.chainTaskK(({ groupIds, ...rest }) => pipe(
     followList(ports)(groupIds),
     T.map((content) => ({ ...rest, groupIds, content })),
