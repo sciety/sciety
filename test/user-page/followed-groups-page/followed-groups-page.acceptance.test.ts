2a0ecc221ba36780d07003fa0575e45e567882c5
Kevin Rutherford <kevin@rutherford-software.com>
2021-07-01 10:36:33 +0100

    Fix linting error #850

--- test/user-page/followed-groups-page/followed-groups-page.acceptance.test.ts
+++ test/user-page/followed-groups-page/followed-groups-page.acceptance.test.ts
@@ -1,5 +1,4 @@
 import * as E from 'fp-ts/Either';
-import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import * as TO from 'fp-ts/TaskOption';
