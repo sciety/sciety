baa73d473c64520f4a2db82c5c6b9fcf7e8fb5bc
Giorgio Sironi <g.sironi@elifesciences.org>
2021-07-05 13:20:16 +0100

    Rename to followingCount #847

--- src/user-page/tab-list.ts
+++ src/user-page/tab-list.ts
@@ -2,13 +2,13 @@ import { Tab } from '../shared-components/tabs';
 import { toHtmlFragment } from '../types/html-fragment';
 import { UserId } from '../types/user-id';
 
-export const tabList = (userId: UserId, savedArticleCount: number, followedGroupsCount: number): [Tab, Tab] => [
+export const tabList = (userId: UserId, savedArticleCount: number, followingCount: number): [Tab, Tab] => [
   {
     label: toHtmlFragment(`Saved articles (${savedArticleCount})`),
     url: `/users/${userId}/saved-articles`,
   },
   {
-    label: toHtmlFragment(`Following (${followedGroupsCount})`),
+    label: toHtmlFragment(`Following (${followingCount})`),
     url: `/users/${userId}/followed-groups`,
   },
 ];
