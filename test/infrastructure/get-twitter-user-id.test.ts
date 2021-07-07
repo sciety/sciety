c8c75096ba46e314397bd02e771724c9171ebd07
Will Byrne <w.byrne@elifesciences.org>
2021-07-07 14:18:23 +0100

    Test for when the request to twitter fails #855

--- test/infrastructure/get-twitter-user-id.test.ts
+++ test/infrastructure/get-twitter-user-id.test.ts
@@ -43,22 +43,12 @@ describe('get-twitter-user-id', () => {
     });
   });
 
-  describe('when the user handle is not valid', () => {
-    // {
-    //   "errors": [
-    //     {
-    //       "parameters": {
-    //         "username": [
-    //           "giorgdcsadsacsacdsani"
-    //         ]
-    //       },
-    //       "message": "The `username` query parameter value [...] does not match ^[A-Za-z0-9_]{1,15}$"
-    //     }
-    //   ],
-    //   "title": "Invalid Request",
-    //   "detail": "One or more parameters to your request was invalid.",
-    //   "type": "https://api.twitter.com/2/problems/invalid-request"
-    // }
-    it.todo('returns unavailable');
+  describe('when the request fails', () => {
+    it('returns unavailable', async () => {
+      const getTwitterResponse: GetTwitterResponse = async () => { throw new Error('test'); };
+      const result = await getTwitterUserId(getTwitterResponse)(arbitraryWord())();
+
+      expect(result).toStrictEqual(E.left(DE.unavailable));
+    });
   });
 });
