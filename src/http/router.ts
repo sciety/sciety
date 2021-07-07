1f9767daae5886991e7a3af78b24169a63112fbb
Will Byrne <w.byrne@elifesciences.org>
2021-07-07 09:59:49 +0100

    remove unused user parameter from codec #855

--- src/http/router.ts
+++ src/http/router.ts
@@ -81,9 +81,6 @@ const homePageParams = t.type({
 
 const userPageParams = t.type({
   descriptor: UserIdFromString,
-  user: tt.optionFromNullable(t.type({
-    id: UserIdFromString,
-  })),
 });
 
 export const createRouter = (adapters: Adapters): Router => {
