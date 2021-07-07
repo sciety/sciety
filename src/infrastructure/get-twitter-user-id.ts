9de322e225eee4216c863eeeb480ec3115e0eee0
Giorgio Sironi <g.sironi@elifesciences.org>
2021-07-07 13:58:39 +0100

    Introduce codec #855

--- src/infrastructure/get-twitter-user-id.ts
+++ src/infrastructure/get-twitter-user-id.ts
@@ -1,25 +1,44 @@
+import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
 import { GetTwitterResponse } from './get-twitter-response';
 import * as DE from '../types/data-error';
 import { toUserId, UserId } from '../types/user-id';
 
 export type GetTwitterUserId = (handle: string) => TE.TaskEither<DE.DataError, UserId>;
 
-type TwitterResponse = {
-  data?: {
-    id: string,
-    name: string,
-    username: string,
-  },
-  errors?: unknown,
-};
+const dataTwitterResponse = t.type({
+  data: t.type({
+    id: t.string,
+    name: t.string,
+    username: t.string,
+  }),
+});
+
+const errorsTwitterResponse = t.type({
+  errors: t.UnknownArray,
+});
+
+const twitterResponse = t.union([
+  dataTwitterResponse,
+  errorsTwitterResponse,
+]);
 
 export const getTwitterUserId = (getTwitterResponse: GetTwitterResponse): GetTwitterUserId => (handle) => pipe(
   TE.tryCatch(
     async () => getTwitterResponse(`https://api.twitter.com/2/users/by/username/${handle}`),
     () => DE.unavailable,
   ),
-  TE.map((json) => (json as TwitterResponse).data?.id ?? ''),
+  T.map(E.chain(flow(
+    twitterResponse.decode,
+    E.mapLeft(() => DE.unavailable),
+  ))),
+  TE.filterOrElseW(
+    dataTwitterResponse.is,
+    () => DE.notFound,
+  ),
+  TE.map((json) => json.data.id),
   TE.map(toUserId),
 );
