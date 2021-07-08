d94da60e3aa50a033db836b8678f6bf078ca0910
Giorgio Sironi <g.sironi@elifesciences.org>
2021-07-08 14:45:34 +0100

    Implement rudimental Redis cache #889

--- src/infrastructure/redis-cache.ts
+++ src/infrastructure/redis-cache.ts
@@ -1,7 +1,19 @@
+import Redis from 'ioredis';
 import { Doi } from '../types/doi';
 
 type DownstreamFetcher = (doi: Doi, acceptHeader: string) => Promise<string>;
 
 export const redisCache = (
   downstreamFetcher: DownstreamFetcher,
-): DownstreamFetcher => async (doi, acceptHeader) => downstreamFetcher(doi, acceptHeader);
+): DownstreamFetcher => async (doi, acceptHeader) => {
+  const redis = new Redis({
+    host: 'sciety_cache',
+  });
+  const cached = await redis.get(doi.toString());
+  if (cached !== null) {
+    return cached;
+  }
+  const fetched = await downstreamFetcher(doi, acceptHeader);
+  await redis.set(doi.toString(), fetched);
+  return fetched;
+};
