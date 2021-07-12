356cbffd3be775a2dd87d9f458aaae541615d041
Kevin Rutherford <kevin@rutherford-software.com>
2021-07-12 11:17:37 +0100

    Return a hardcoded single ingested article #909

--- src/http/router.ts
+++ src/http/router.ts
@@ -374,7 +374,14 @@ export const createRouter = (adapters: Adapters): Router => {
 
   // DOCMAPS
   router.get('/docmaps/v1/index', async (context, next) => {
-    context.response.body = {};
+    context.response.body = {
+      articles: [
+        {
+          doi: '10.1101/2021.04.25.441302',
+          docmap: '/docmaps/v1/articles/10.1101/2021.04.25.441302.docmap.json',
+        },
+      ],
+    };
 
     await next();
   });
