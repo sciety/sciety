df7cb28fb89a2b7d81eededd101f5af31c3a78bf
Kevin Rutherford <kevin@rutherford-software.com>
2021-07-12 15:44:51 +0100

    Add another test #910

--- test/article-page/activity-page/article-meta-tag-content.test.ts
+++ test/article-page/activity-page/article-meta-tag-content.test.ts
@@ -1,29 +1,31 @@
-import * as O from 'fp-ts/Option';
+import { URL } from 'url';
+import * as RFI from './review-feed-item.helper';
 import { articleMetaTagContent } from '../../../src/article-page/activity-page/article-meta-tag-content';
-import { arbitraryDate, arbitraryString } from '../../helpers';
-import { arbitraryGroupId } from '../../types/group-id.helper';
-import { arbitraryReviewId } from '../../types/review-id.helper';
+import { arbitraryDate, arbitraryNumber, arbitraryUri } from '../../helpers';
 
-const arbitraryFeedItem = (type: 'review') => ({
-  id: arbitraryReviewId(),
-  type,
-  source: O.none,
+const arbitraryArticleVersionFeedItem = () => ({
+  type: 'article-version' as const,
+  source: new URL(arbitraryUri()),
   occurredAt: arbitraryDate(),
-  groupId: arbitraryGroupId(),
-  groupName: arbitraryString(),
-  groupAvatar: arbitraryString(),
-  fullText: O.none,
-  counts: { helpfulCount: 0, notHelpfulCount: 0 },
-  current: O.none,
+  version: arbitraryNumber(1, 10),
+  server: 'biorxiv' as const,
 });
 
 describe('article-meta-tag-content', () => {
   it('returns a count of the evaluations', () => {
     const result = articleMetaTagContent([
-      arbitraryFeedItem('review'),
-      arbitraryFeedItem('review'),
+      RFI.arbitrary(),
+      RFI.arbitrary(),
     ]);
 
     expect(result.evaluationCount).toStrictEqual(2);
+  });
+
+  it('ignores non-evaluation feed items', () => {
+    const result = articleMetaTagContent([
+      arbitraryArticleVersionFeedItem(),
+    ]);
+
+    expect(result.evaluationCount).toStrictEqual(0);
   });
 });
