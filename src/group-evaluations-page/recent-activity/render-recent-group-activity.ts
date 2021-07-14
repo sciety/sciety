b0d82c66a1b043dbb9ae47dd3b5164e4eb9c2b6c
Giorgio Sironi <g.sironi@elifesciences.org>
2021-07-14 14:38:02 +0100

    Extract renderNextLink #923

--- src/group-evaluations-page/recent-activity/render-recent-group-activity.ts
+++ src/group-evaluations-page/recent-activity/render-recent-group-activity.ts
@@ -1,6 +1,6 @@
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
-import { flow, pipe } from 'fp-ts/function';
+import { flow } from 'fp-ts/function';
 import { ArticleViewModel, renderArticleCard } from '../../shared-components/article-card';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 
@@ -10,23 +10,22 @@ type RenderRecentGroupActivity = (
   articleViewModels: ReadonlyArray<ArticleViewModel>,
 ) => HtmlFragment;
 
+const renderNextLink = O.fold(
+  () => '',
+  (href: string) => `
+    <div class="search-results__link_container">
+      <a href="${href}" class="search-results__next_link">Next</a>
+    </div>
+  `,
+);
+
 export const renderRecentGroupActivity: RenderRecentGroupActivity = (nextPageHref) => flow(
   RA.map(renderArticleCard),
   RA.map((activity) => `<li class="group-activity-list__item">${activity}</li>`),
   (renderedActivities) => `
     <div>
       <ul class="group-activity-list" role="list">${renderedActivities.join('')}</ul>
-      ${pipe(
-    nextPageHref,
-    O.fold(
-      () => '',
-      (href) => `
-            <div class="search-results__link_container">
-              <a href="${href}" class="search-results__next_link">Next</a>
-            </div>
-          `,
-    ),
-  )}
+      ${renderNextLink(nextPageHref)}
     </div>
   `,
   toHtmlFragment,
