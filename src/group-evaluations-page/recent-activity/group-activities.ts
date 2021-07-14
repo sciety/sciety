552d89044e32ca53cb0d4763edd0364a2778395b
Kevin Rutherford <kevin@rutherford-software.com>
2021-07-14 11:32:50 +0100

    Show the most recent 20 articles #921

---
+++ src/group-evaluations-page/recent-activity/group-activities.ts
@@ -0,0 +1,94 @@
+import * as D from 'fp-ts/Date';
+import * as O from 'fp-ts/Option';
+import * as Ord from 'fp-ts/Ord';
+import * as RA from 'fp-ts/ReadonlyArray';
+import * as RM from 'fp-ts/ReadonlyMap';
+import * as S from 'fp-ts/Semigroup';
+import { flow, pipe } from 'fp-ts/function';
+import * as N from 'fp-ts/number';
+import { ArticleActivity } from '../../types/article-activity';
+import { Doi } from '../../types/doi';
+import {
+  DomainEvent, EditorialCommunityReviewedArticleEvent,
+  isEditorialCommunityReviewedArticleEvent,
+} from '../../types/domain-events';
+import { GroupId } from '../../types/group-id';
+
+type GroupActivities = (events: ReadonlyArray<DomainEvent>) => (groupId: GroupId) => ReadonlyArray<ArticleActivity>;
+
+type ActivityDetails = {
+  latestActivityDate: Date,
+  latestActivityByGroup: O.Option<Date>,
+  evaluationCount: number,
+};
+
+const semigroupActivityDetails: S.Semigroup<ActivityDetails> = S.struct({
+  latestActivityDate: S.max(D.Ord),
+  latestActivityByGroup: O.getMonoid(S.max(D.Ord)),
+  evaluationCount: N.SemigroupSum,
+});
+
+const eventToActivityDetails = (
+  event: EditorialCommunityReviewedArticleEvent,
+  groupId: GroupId,
+): ActivityDetails => ({
+  latestActivityDate: event.date,
+  latestActivityByGroup: pipe(
+    event.date,
+    O.fromPredicate(() => event.editorialCommunityId === groupId),
+  ),
+  evaluationCount: 1,
+});
+
+const combineActivityDetails = (a: ActivityDetails) => O.fold(
+  () => a,
+  (b: ActivityDetails) => semigroupActivityDetails.concat(a, b),
+);
+
+const updateActivity = (
+  event: EditorialCommunityReviewedArticleEvent,
+  groupId: GroupId,
+) => pipe(
+  eventToActivityDetails(event, groupId),
+  combineActivityDetails,
+);
+
+const addEventToActivities = (
+  groupId: GroupId,
+) => (
+  activities: Map<string, ActivityDetails>,
+  event: EditorialCommunityReviewedArticleEvent,
+) => pipe(
+  activities.get(event.articleId.value),
+  O.fromNullable,
+  updateActivity(event, groupId),
+  (activity) => activities.set(event.articleId.value, activity),
+);
+
+const byLatestActivityDateByGroupDesc: Ord.Ord<ArticleActivity & { latestActivityByGroup: Date }> = pipe(
+  D.Ord,
+  Ord.reverse,
+  Ord.contramap(
+    (activityDetails) => (activityDetails.latestActivityByGroup),
+  ),
+);
+
+const groupHasEvaluatedArticle = <T extends { latestActivityByGroup: O.Option<Date> }>(articleActivities: T) => pipe(
+  articleActivities.latestActivityByGroup,
+  O.map((latestActivityByGroup) => ({
+    ...articleActivities,
+    latestActivityByGroup,
+  })),
+);
+
+export const groupActivities: GroupActivities = (events) => (groupId) => pipe(
+  events,
+  RA.filter(isEditorialCommunityReviewedArticleEvent),
+  RA.reduce(new Map(), addEventToActivities(groupId)),
+  RM.filterMapWithIndex(flow(
+    (key, activityDetails) => ({ ...activityDetails, doi: new Doi(key) }),
+    groupHasEvaluatedArticle,
+  )),
+  RM.values(byLatestActivityDateByGroupDesc),
+  RA.takeLeft(20),
+);
---
+++ src/group-evaluations-page/recent-activity/recent-activity.ts
@@ -0,0 +1,43 @@
+import * as O from 'fp-ts/Option';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import { flow, pipe } from 'fp-ts/function';
+import { constructRecentGroupActivity } from './construct-recent-group-activity';
+import { fetchArticleDetails } from '../../shared-components/article-card/fetch-article-details';
+import { FindVersionsForArticleDoi, getLatestArticleVersionDate } from '../../shared-components/article-card/get-latest-article-version-date';
+import { ArticleServer } from '../../types/article-server';
+import { Doi } from '../../types/doi';
+import { DomainEvent } from '../../types/domain-events';
+import { Group } from '../../types/group';
+import { HtmlFragment } from '../../types/html-fragment';
+import { SanitisedHtmlFragment } from '../../types/sanitised-html-fragment';
+
+type Article = {
+  title: SanitisedHtmlFragment,
+  server: ArticleServer,
+  authors: ReadonlyArray<string>,
+};
+
+type GetArticle = (id: Doi) => TE.TaskEither<unknown, Article>;
+
+type GetAllEvents = T.Task<ReadonlyArray<DomainEvent>>;
+
+export type Ports = {
+  fetchArticle: GetArticle,
+  findVersionsForArticleDoi: FindVersionsForArticleDoi,
+  getAllEvents: GetAllEvents,
+};
+
+type RecentActivity = (ports: Ports) => (group: Group) => TE.TaskEither<never, HtmlFragment>;
+
+export const recentActivity: RecentActivity = (ports) => (group) => pipe(
+  group.id,
+  constructRecentGroupActivity(
+    fetchArticleDetails(
+      getLatestArticleVersionDate(ports.findVersionsForArticleDoi),
+      flow(ports.fetchArticle, T.map(O.fromEither)),
+    ),
+    ports.getAllEvents,
+  ),
+  TE.rightTask,
+);
