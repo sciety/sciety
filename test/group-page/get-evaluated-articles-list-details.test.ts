956c457de61f84d4ddcecc283132e28b8515902c
Kevin Rutherford <kevin@rutherford-software.com>
2021-07-16 09:43:56 +0100

    Sketch a test list for the projection #924

--- test/group-page/get-evaluated-articles-list-details.test.ts
+++ test/group-page/get-evaluated-articles-list-details.test.ts
@@ -2,10 +2,22 @@ import { getEvaluatedArticlesListDetails } from '../../src/group-page/get-evalua
 
 describe('get-evaluated-articles-list-details', () => {
   describe('when the group has evaluated no articles', () => {
-    it('returns an article count of 0', () => {
+    it('returns a count of 0', () => {
       const result = getEvaluatedArticlesListDetails([]);
 
       expect(result.articleCount).toStrictEqual(0);
     });
+  });
+
+  describe('when the group has evaluated some articles', () => {
+    it.todo('returns a count of the articles');
+  });
+
+  describe('when the group has evaluated one article more than once', () => {
+    it.todo('returns a count of 1');
+  });
+
+  describe('when a different group has evaluated some articles', () => {
+    it.todo('returns a count of 0');
   });
 });
