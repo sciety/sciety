8ed65a5719250096acc5bc903d46b63050d90061
Giorgio Sironi <g.sironi@elifesciences.org>
2021-07-16 10:50:29 +0100

    Use a Set for better performance #924
    On ScreenIt, 0.030s rather than >2s

--- src/group-page/get-evaluated-articles-list-details.ts
+++ src/group-page/get-evaluated-articles-list-details.ts
@@ -1,6 +1,6 @@
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as RS from 'fp-ts/ReadonlySet';
 import { pipe } from 'fp-ts/function';
-import * as S from 'fp-ts/string';
 import { DomainEvent, EditorialCommunityReviewedArticleEvent } from '../types/domain-events';
 
 import { GroupId } from '../types/group-id';
@@ -19,7 +19,7 @@ export const getEvaluatedArticlesListDetails = (
     RA.filter((event): event is EditorialCommunityReviewedArticleEvent => event.type === 'EditorialCommunityReviewedArticle'),
     RA.filter((event) => event.editorialCommunityId === groupId),
     RA.map((event) => event.articleId.value),
-    RA.uniq(S.Eq),
-    (es) => es.length,
+    (articleIds) => (new Set(articleIds)),
+    RS.size,
   ),
 });
