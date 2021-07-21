476652598c8bb8739190fe9800f6c504c6249179
David Moulton <d.moulton@elifesciences.org>
2021-07-21 13:59:09 +0100

    Implement saved articles list on user's list page #932

--- src/user-list-page/user-list-page.ts
+++ src/user-list-page/user-list-page.ts
@@ -1,21 +1,37 @@
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import * as DE from '../types/data-error';
+import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 import { RenderPageError } from '../types/render-page-error';
-import { Ports, savedArticles } from '../user-page/saved-articles/saved-articles';
+import { UserId } from '../types/user-id';
+import { GetAllEvents, projectSavedArticleDois } from '../user-page/saved-articles/project-saved-article-dois';
+import { Ports as SavedArticlePorts, savedArticles } from '../user-page/saved-articles/saved-articles';
 
 type Params = {
   handle: string,
 };
 
+type Ports = SavedArticlePorts & {
+  getAllEvents: GetAllEvents,
+  getUserId: (handle: string) => TE.TaskEither<DE.DataError, UserId>,
+};
+
 type UserListPage = (params: Params) => TE.TaskEither<RenderPageError, Page>;
 
-export const userListPage = (ports: Ports): UserListPage => () => pipe(
-  savedArticles(ports)([]),
-  T.map((content) => ({
-    title: 'Saved articles',
-    content,
-  })),
-  TE.rightTask,
+export const userListPage = (ports: Ports): UserListPage => ({ handle }) => pipe(
+  handle,
+  ports.getUserId,
+  TE.chainTaskK(projectSavedArticleDois(ports.getAllEvents)),
+  TE.chainTaskK(savedArticles(ports)),
+  TE.bimap(
+    (dataError) => ({
+      type: dataError,
+      message: toHtmlFragment('User not found.'),
+    }),
+    (content: HtmlFragment) => ({
+      title: 'Saved articles',
+      content,
+    }),
+  ),
 );
