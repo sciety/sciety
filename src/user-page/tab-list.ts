f2d41555ef905d3b2f60f108db36ee25239c8926
Giorgio Sironi <g.sironi@elifesciences.org>
2021-07-22 14:48:59 +0100

    Change links to user page lists tab #932

--- src/user-page/tab-list.ts
+++ src/user-page/tab-list.ts
@@ -4,7 +4,7 @@ import { toHtmlFragment } from '../types/html-fragment';
 export const tabList = (userHandle: string, followingCount: number): [Tab, Tab] => [
   {
     label: toHtmlFragment('Lists (1)'),
-    url: `/users/${userHandle}/saved-articles`,
+    url: `/users/${userHandle}/lists`,
   },
   {
     label: toHtmlFragment(`Following (${followingCount})`),
