90e881b5a50dbb5aa999b4962b1cddad39c277ff
David Moulton <d.moulton@elifesciences.org>
2021-07-23 09:42:04 +0100

    Implement test for user list card title link #933

--- test/user-page/user-list-card/user-list-card.test.ts
+++ test/user-page/user-list-card/user-list-card.test.ts
@@ -9,7 +9,11 @@ describe('user-list-card', () => {
     expect(rendered?.textContent).toContain('Saved articles');
   });
 
-  it.todo('links to the user list page');
+  it('the title holds a link', async () => {
+    const rendered = JSDOM.fragment(await userListCard(arbitraryWord())());
+
+    expect(rendered.querySelector('h3 a')).not.toBeNull();
+  });
 
   it.todo('displays the list owner\'s handle in the description');
 
