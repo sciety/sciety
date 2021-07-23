2defb783b99d317fc2f64eef30364899753641ad
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-07-23 10:01:50 +0100

    test list for user list details #933

---
+++ test/user-page/user-list-card/get-user-list-details.test.ts
@@ -0,0 +1,19 @@
+describe('get-user-list-details', () => {
+  describe('when the list contains no articles', () => {
+    it.todo('returns a count of 0');
+
+    it.todo('returns no last updated date');
+  });
+
+  describe('when the list contains some articles', () => {
+    it.todo('returns a count of the articles');
+
+    it.todo('returns the last updated date');
+  });
+
+  describe('when only a different user has saved articles', () => {
+    it.todo('returns a count of 0');
+
+    it.todo('returns no last updated date');
+  });
+});
