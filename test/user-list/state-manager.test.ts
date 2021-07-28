87454d24eb3df3113ea891f3aafb80b3d552d991
David Moulton <d.moulton@elifesciences.org>
2021-07-28 10:43:33 +0100

    Add todo tests for user list state manager #943

---
+++ test/user-list/state-manager.test.ts
@@ -0,0 +1,15 @@
+/* eslint-disable jest/lowercase-name */
+
+describe('state-manager', () => {
+  it.todo('Bob has not saved any articles: false');
+
+  it.todo('UserSavedArticle Bob, 1; ArticleRemovedFromUserList Bob, 1: false');
+
+  it.todo('UserSavedArticle Bob, 1: true');
+
+  it.todo('UserSavedArticle Bob, 1; ArticleRemovedFromUserList Bob, 1; UserSavedArticle Bob, 1: true');
+
+  it.todo('Alice saved article 1 but Bob has not: false');
+
+  it.todo('Bob has saved article 2: false');
+});
