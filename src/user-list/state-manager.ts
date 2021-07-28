544d2700cef7d163e5dcae989e7fe7668ad97269
Giorgio Sironi <g.sironi@elifesciences.org>
2021-07-28 13:54:44 +0100

    Implement user filter #943

--- src/user-list/state-manager.ts
+++ src/user-list/state-manager.ts
@@ -1,23 +1,27 @@
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
-import { flow } from 'fp-ts/function';
+import { pipe } from 'fp-ts/function';
 import {
   ArticleRemovedFromUserListEvent,
   DomainEvent, isArticleRemovedFromUserListEvent,
   isUserSavedArticleEvent,
   UserSavedArticleEvent,
 } from '../types/domain-events';
+import { UserId } from '../types/user-id';
 
 type RelevantEvent = UserSavedArticleEvent | ArticleRemovedFromUserListEvent;
-const isRelevantEvent = (event: DomainEvent): event is RelevantEvent => (
-  isUserSavedArticleEvent(event) || isArticleRemovedFromUserListEvent(event)
+
+const isRelevantEvent = (userId: UserId) => (event: DomainEvent): event is RelevantEvent => (
+  (isUserSavedArticleEvent(event) || isArticleRemovedFromUserListEvent(event)) 
+  && event.userId === userId
 );
 
-type StateManager = (events: ReadonlyArray<DomainEvent>) => boolean;
+type StateManager = (events: ReadonlyArray<DomainEvent>, userId: UserId) => boolean;
 
 // ts-unused-exports:disable-next-line
-export const stateManager: StateManager = flow(
-  RA.filter(isRelevantEvent),
+export const stateManager: StateManager = (events, userId) => pipe(
+  events,
+  RA.filter(isRelevantEvent(userId)),
   RA.last,
   O.map(isUserSavedArticleEvent),
   O.getOrElseW(() => false),
--- test/user-list/state-manager.test.ts
+++ test/user-list/state-manager.test.ts
@@ -7,7 +7,7 @@ import { arbitraryUserId } from '../types/user-id.helper';
 describe('state-manager', () => {
   it('Bob has not saved any articles: false', () => {
     const events: ReadonlyArray<DomainEvent> = [];
-    const articleInList = stateManager(events);
+    const articleInList = stateManager(events, arbitraryUserId());
 
     expect(articleInList).toBe(false);
   });
@@ -18,7 +18,7 @@ describe('state-manager', () => {
     const events: ReadonlyArray<DomainEvent> = [
       userSavedArticle(bob, article1),
     ];
-    const articleInList = stateManager(events);
+    const articleInList = stateManager(events, bob);
 
     expect(articleInList).toBe(true);
   });
@@ -30,12 +30,12 @@ describe('state-manager', () => {
       userSavedArticle(bob, article1),
       articleRemovedFromUserList(bob, article1),
     ];
-    const articleInList = stateManager(events);
+    const articleInList = stateManager(events, bob);
 
     expect(articleInList).toBe(false);
   });
 
-  it.skip('UserSavedArticle Bob, 1; ArticleRemovedFromUserList Bob, 1; UserSavedArticle Bob, 1: true', () => {
+  it('UserSavedArticle Bob, 1; ArticleRemovedFromUserList Bob, 1; UserSavedArticle Bob, 1: true', () => {
     const bob = arbitraryUserId();
     const article1 = arbitraryDoi();
     const events: ReadonlyArray<DomainEvent> = [
@@ -43,20 +43,19 @@ describe('state-manager', () => {
       articleRemovedFromUserList(bob, article1),
       userSavedArticle(bob, article1),
     ];
-    const articleInList = stateManager(events);
+    const articleInList = stateManager(events, bob);
 
     expect(articleInList).toBe(true);
   });
 
-  it.skip('Alice saved article 1 but Bob has not: false', () => {
+  it('Alice saved article 1 but Bob has not: false', () => {
     const alice = arbitraryUserId();
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const bob = arbitraryUserId();
     const article1 = arbitraryDoi();
     const events: ReadonlyArray<DomainEvent> = [
       userSavedArticle(alice, article1),
     ];
-    const articleInList = stateManager(events);
+    const articleInList = stateManager(events, bob);
 
     expect(articleInList).toBe(false);
   });
@@ -69,7 +68,7 @@ describe('state-manager', () => {
     const events: ReadonlyArray<DomainEvent> = [
       userSavedArticle(bob, article2),
     ];
-    const articleInList = stateManager(events);
+    const articleInList = stateManager(events, bob);
 
     expect(articleInList).toBe(false);
   });
