b51e2d6124095029ae3db1133e0450b87f22f05c
Kevin Rutherford <kevin@rutherford-software.com>
2021-07-30 16:08:25 +0100

    Separate review validation from result outputting #859

--- src/ingest/find-reviews-from-hypothesis-user.ts
+++ src/ingest/find-reviews-from-hypothesis-user.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
+import * as E from 'fp-ts/Either';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
+import {SkippedItem} from './update-all';
+import {Evaluation} from './evaluations';
 
 const userId = process.argv[2];
 
@@ -15,18 +19,21 @@ type HypothesisResponse = {
   rows: Array<Row>,
 };
 
-const processRow = (server: string) => (row: Row): void => {
-  // const doiRegex = '(10\\.[0-9]{4,}(?:\\.[1-9][0-9]*)*/(?:[^%"#?\\s])+)';
+// TODO bioRxiv/medRxiv content is available at multiple URL patterns:
+// curl "https://api.hypothes.is/api/search?uri.parts=biorxiv&limit=100" | jq --raw-output ".rows[].target[].source"
+
+const toEvaluation = (server: string) => (row: Row): E.Either<SkippedItem, Evaluation> => {
   const shortRegex = '((?:[^%"#?\\s])+)';
-  // TODO bioRxiv/medRxiv content is available at multiple URL patterns:
-  // curl "https://api.hypothes.is/api/search?uri.parts=biorxiv&limit=100" | jq --raw-output ".rows[].target[].source"
   const matches = new RegExp(`https?://(?:www.)?${server}.org/cgi/content/(?:10.1101|short)/${shortRegex}$`).exec(row.uri);
   if (matches === null) {
-    process.stderr.write(`Cannot parse a DOI out of '${row.uri}'\n`);
-  } else {
-    const doi = matches[1];
-    process.stdout.write(`${new Date(row.created).toISOString()},10.1101/${doi},hypothesis:${row.id}\n`);
+    return E.left({ item: row.uri, reason: 'Cannot parse into the biorxiv DOI' });
   }
+  const doi = matches[1];
+  return E.right({
+    date: new Date(row.created),
+    articleDoi: `10.1101/${doi}`,
+    evaluationLocator: `hypothesis:${row.id}`,
+  });
 };
 
 const processServer = (server: string) => async (): Promise<void> => {
@@ -36,7 +43,15 @@ const processServer = (server: string) => async (): Promise<void> => {
 
   // eslint-disable-next-line no-loops/no-loops
   while (data.rows.length > 0) {
-    data.rows.forEach(processRow(server));
+    pipe(
+      data.rows,
+      RA.map(toEvaluation(server)),
+      RA.rights,
+      RA.map((evaluation) => {
+        process.stdout.write(`${evaluation.date.toISOString()},${evaluation.articleDoi},${evaluation.evaluationLocator}\n`);
+        return evaluation;
+      }),
+    );
     latestDate = encodeURIComponent(data.rows[data.rows.length - 1].created);
     data = (await axios.get<HypothesisResponse>(`https://api.hypothes.is/api/search?user=${userId}&uri.parts=${server}&limit=${perPage}&sort=created&order=asc&search_after=${latestDate}`)).data;
   }
