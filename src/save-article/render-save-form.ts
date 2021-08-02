d5447d8bf1a04f60760677a1ccc7d69f1c99152c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-08-02 10:41:53 +0100

    Explicitely couple form and saving to session of save command #943

--- src/save-article/render-save-form.ts
+++ src/save-article/render-save-form.ts
@@ -1,9 +1,10 @@
+import { articleIdFieldName } from './save-save-article-command';
 import { Doi } from '../types/doi';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
 export const renderSaveForm = (doi: Doi): HtmlFragment => toHtmlFragment(`
   <form class="save-article-form" method="post" action="/save-article">
-    <input type="hidden" name="articleid" value="${doi.value}">
+    <input type="hidden" name="${articleIdFieldName}" value="${doi.value}">
     <button type="submit" class="save-article-button">
       <img class="save-article-button__icon" src="/static/images/playlist_add-24px.svg" alt=""> Save to my list
     </button>
--- src/save-article/save-save-article-command.ts
+++ src/save-article/save-save-article-command.ts
@@ -1,7 +1,9 @@
 import { Middleware } from 'koa';
 
+export const articleIdFieldName = 'articleid';
+
 export const saveSaveArticleCommand: Middleware = async (context, next) => {
   context.session.command = 'save-article';
-  context.session.articleId = context.request.body.articleid;
+  context.session.articleId = context.request.body[articleIdFieldName];
   await next();
 };
