ef40c0c813f5b6db7273e4b85172f0a39074a11c
Kevin Rutherford <kevin@rutherford-software.com>
2021-08-09 08:42:13 +0100

    Re-focus prelights fast tests #859

---
+++ test/ingest/prelights/extract-prelights.test.ts
@@ -0,0 +1,35 @@
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { arbitraryDate, arbitraryWord } from '../../helpers';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+import { extractPrelights } from '../../../src/ingest/prelights/extract-prelights';
+
+describe('extract-prelights', () => {
+  describe('given an item that is not a highlight', () => {
+    const guid = arbitraryWord();
+    const fetchData = shouldNotBeCalled;
+    const result = pipe(
+      [{
+        guid,
+        category: '<a name = "something">something</a>',
+        pubDate: arbitraryDate(),
+        preprintUrl: arbitraryWord(),
+      }],
+      extractPrelights(fetchData),
+    );
+
+    it('records no evaluations', async () => {
+      expect(await result()).toStrictEqual(expect.objectContaining({
+        evaluations: [],
+      }));
+    });
+
+    it('records the item as skipped', async () => {
+      expect(await result()).toStrictEqual(expect.objectContaining({
+        skippedItems: O.some([expect.objectContaining({
+          item: guid,
+        })]),
+      }));
+    });
+  });
+});
--- test/ingest/prelights/fetch-prelights-evaluations.test.ts
+++ test/ingest/prelights/fetch-prelights-evaluations.test.ts
@@ -1,11 +1,3 @@
-import * as E from 'fp-ts/Either';
-import * as O from 'fp-ts/Option';
-import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
-import { fetchPrelightsEvaluations } from '../../../src/ingest/prelights/fetch-prelights-evaluations';
-import { arbitraryWord } from '../../helpers';
-import { shouldNotBeCalled } from '../../should-not-be-called';
-
 describe('fetch-prelights-evaluations', () => {
   describe('when the feed contains a highlight ...', () => {
     describe('referring to a single preprint', () => {
@@ -24,48 +16,6 @@ describe('fetch-prelights-evaluations', () => {
       it.todo('records no evaluations');
 
       it.todo('records the item as skipped');
-    });
-  });
-
-  describe('when the feed contains an item that is not a highlight', () => {
-    const guid = arbitraryWord();
-    const xml = `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <rss version="2.0" >
-        <channel>
-          <item>
-            <category><![CDATA[<a name = "something">something</a>]]></category>
-            <guid isPermaLink="false">${guid}</guid>
-            <pubDate>Thu, 05 Aug 2021 23:05:29 +0000</pubDate>
-            <preprints>
-              <preprint>
-                <preprinturl>https://www.biorxiv.org/content/10.1101/2021.07.13.452203v1</preprinturl>
-              </preprint>
-            </preprints>
-          </item>
-        </channel>
-      </rss>
-    `;
-    const result = pipe(
-      {
-        fetchGoogleSheet: shouldNotBeCalled,
-        fetchData: <D>() => TE.right(xml) as unknown as D,
-      },
-      fetchPrelightsEvaluations(),
-    );
-
-    it('records no evaluations', async () => {
-      expect(await result()).toStrictEqual(E.right(expect.objectContaining({
-        evaluations: [],
-      })));
-    });
-
-    it('records the item as skipped', async () => {
-      expect(await result()).toStrictEqual(E.right(expect.objectContaining({
-        skippedItems: O.some([expect.objectContaining({
-          item: guid,
-        })]),
-      })));
     });
   });
 
