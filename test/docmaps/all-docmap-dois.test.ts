f13937a71d914435e757104bafead40616776460
Will Byrne <w.byrne@elifesciences.org>
2021-08-10 13:23:11 +0100

    add test list for all-docmap-dois #983

---
+++ test/docmaps/all-docmap-dois.test.ts
@@ -0,0 +1,9 @@
+describe('all-docmap-dois', () => {
+  describe('when there are evaluated events by the specified group', () => {
+    it.todo('returns a list of all the evaluated article ids');
+  });
+
+  describe('when there are evaluated events by the specified group and others', () => {
+    it.todo('returns a list of article ids evaluated by the specified group');
+  });
+});
