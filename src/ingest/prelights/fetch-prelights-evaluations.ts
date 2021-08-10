ed506332187ebb19b490d5e4e39364e77680277b
Kevin Rutherford <kevin@rutherford-software.com>
2021-08-10 08:22:09 +0100

    Extract prelights feed parsing to improve testability #859

--- src/ingest/prelights/fetch-prelights-evaluations.ts
+++ src/ingest/prelights/fetch-prelights-evaluations.ts
@@ -1,58 +1,11 @@
-import parser from 'fast-xml-parser';
-import * as E from 'fp-ts/Either';
-import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
-import { flow, pipe } from 'fp-ts/function';
-import * as t from 'io-ts';
-import * as tt from 'io-ts-types';
-import * as PR from 'io-ts/PathReporter';
+import { pipe } from 'fp-ts/function';
 import { extractPrelights } from './extract-prelights';
+import { identifyCandidates } from './identify-candidates';
 import { FetchData } from '../fetch-data';
 import { FetchEvaluations } from '../update-all';
 
 const key = process.env.PRELIGHTS_FEED_KEY ?? '';
-
-const itemCodec = t.type({
-  pubDate: tt.DateFromISOString,
-  category: t.string,
-  guid: t.string,
-  preprints: t.type({
-    preprint: t.union([
-      t.type({ preprinturl: t.string }),
-      t.array(t.type({ preprinturl: t.string })),
-    ]),
-  }),
-});
-
-const prelightsFeedCodec = t.type({
-  rss: t.type({
-    channel: t.type({
-      item: t.array(itemCodec),
-    }),
-  }),
-});
-
-type FeedItem = t.TypeOf<typeof itemCodec>;
-
-type Prelight = {
-  guid: string,
-  category: string,
-  pubDate: Date,
-  preprintUrl: string,
-};
-
-const toIndividualPrelights = (item: FeedItem): Array<Prelight> => {
-  if (item.preprints.preprint instanceof Array) {
-    return item.preprints.preprint.map((preprintItem) => ({
-      ...item,
-      preprintUrl: preprintItem.preprinturl,
-    }));
-  }
-  return [{
-    ...item,
-    preprintUrl: item.preprints.preprint.preprinturl,
-  }];
-};
 
 type Ports = {
   fetchData: FetchData,
@@ -60,14 +13,6 @@ type Ports = {
 
 export const fetchPrelightsEvaluations = (): FetchEvaluations => (ports: Ports) => pipe(
   ports.fetchData<string>(`https://prelights.biologists.com/feed/sciety/?key=${key}&hours=120`),
-  TE.map((responseBody) => parser.parse(responseBody, { arrayMode: /item/ }) as JSON),
-  TE.chainEitherK(flow(
-    prelightsFeedCodec.decode,
-    E.mapLeft((errors) => PR.failure(errors).join('\n')),
-  )),
-  TE.map(flow(
-    (feed) => feed.rss.channel.item,
-    RA.chain(toIndividualPrelights),
-  )),
+  TE.chainEitherK(identifyCandidates),
   TE.chainTaskK(extractPrelights(ports.fetchData)),
 );
---
+++ src/ingest/prelights/identify-candidates.ts
@@ -0,0 +1,61 @@
+import parser from 'fast-xml-parser';
+import * as E from 'fp-ts/Either';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { flow, pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
+import * as tt from 'io-ts-types';
+import * as PR from 'io-ts/PathReporter';
+
+const itemCodec = t.type({
+  pubDate: tt.DateFromISOString,
+  category: t.string,
+  guid: t.string,
+  preprints: t.type({
+    preprint: t.union([
+      t.type({ preprinturl: t.string }),
+      t.array(t.type({ preprinturl: t.string })),
+    ]),
+  }),
+});
+
+const prelightsFeedCodec = t.type({
+  rss: t.type({
+    channel: t.type({
+      item: t.array(itemCodec),
+    }),
+  }),
+});
+
+type FeedItem = t.TypeOf<typeof itemCodec>;
+
+type Prelight = {
+  guid: string,
+  category: string,
+  pubDate: Date,
+  preprintUrl: string,
+};
+
+const toIndividualPrelights = (item: FeedItem): Array<Prelight> => {
+  if (item.preprints.preprint instanceof Array) {
+    return item.preprints.preprint.map((preprintItem) => ({
+      ...item,
+      preprintUrl: preprintItem.preprinturl,
+    }));
+  }
+  return [{
+    ...item,
+    preprintUrl: item.preprints.preprint.preprinturl,
+  }];
+};
+
+export const identifyCandidates = (responseBody: string): E.Either<string, ReadonlyArray<Prelight>> => pipe(
+  parser.parse(responseBody, { arrayMode: /item/ }) as JSON,
+  prelightsFeedCodec.decode,
+  E.bimap(
+    (errors) => PR.failure(errors).join('\n'),
+    flow(
+      (feed) => feed.rss.channel.item,
+      RA.chain(toIndividualPrelights),
+    ),
+  ),
+);
