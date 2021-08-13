38879b77ddf36c6e414eb0b7f362c81a87b5f407
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-08-13 10:06:05 +0100

    separate hardcoded data from rendering concerns #994

---
+++ src/landing-page/cards/hardcoded-evaluation-cards.ts
@@ -0,0 +1,26 @@
+export const card1 = {
+  articleId: '10.1101/2021.08.02.21261504',
+  articleTitle: 'SARS-CoV-2 antibody binding and neutralization in dried blood spot eluates and paired plasma',
+  date: new Date('2021-08-06'),
+  groupId: '8ccea9c2-e6c8-4dd7-bf1d-37c3fa86ff65',
+  groupName: 'ScreenIT',
+  avatarPath: '/static/groups/screenit--8ccea9c2-e6c8-4dd7-bf1d-37c3fa86ff65.jpg',
+};
+
+export const card2 = {
+  articleId: '10.1101/2021.04.12.439487',
+  articleTitle: 'VHL ligand binding increases intracellular level of VHL',
+  date: new Date('2021-05-27'),
+  groupId: '10360d97-bf52-4aef-b2fa-2f60d319edd7',
+  groupName: 'PREreview',
+  avatarPath: '/static/groups/prereview-community--10360d97-bf52-4aef-b2fa-2f60d319edd7.jpg',
+};
+
+export const card3 = {
+  articleId: '10.1101/2021.03.13.435259',
+  articleTitle: 'Host phenology can drive the evolution of intermediate virulence strategies in some obligate-killer parasites',
+  date: new Date('2021-07-19'),
+  groupId: '19b7464a-edbe-42e8-b7cc-04d1eb1f7332',
+  groupName: 'Peer Community in Evolutionary Biology',
+  avatarPath: '/static/groups/pci-evolutionary-biology--19b7464a-edbe-42e8-b7cc-04d1eb1f7332.jpg',
+};
--- src/landing-page/cards/render-cards-section.ts
+++ src/landing-page/cards/render-cards-section.ts
@@ -1,5 +1,6 @@
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
+import { card1, card2, card3 } from './hardcoded-evaluation-cards';
 import { templateDate } from '../../shared-components/date';
 import * as DE from '../../types/data-error';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
@@ -13,34 +14,7 @@ type Card = {
   avatarPath: string,
 };
 
-const card1: Card = {
-  articleId: '10.1101/2021.08.02.21261504',
-  articleTitle: 'SARS-CoV-2 antibody binding and neutralization in dried blood spot eluates and paired plasma',
-  date: new Date('2021-08-06'),
-  groupId: '8ccea9c2-e6c8-4dd7-bf1d-37c3fa86ff65',
-  groupName: 'ScreenIT',
-  avatarPath: '/static/groups/screenit--8ccea9c2-e6c8-4dd7-bf1d-37c3fa86ff65.jpg',
-};
-
-const card2: Card = {
-  articleId: '10.1101/2021.04.12.439487',
-  articleTitle: 'VHL ligand binding increases intracellular level of VHL',
-  date: new Date('2021-05-27'),
-  groupId: '10360d97-bf52-4aef-b2fa-2f60d319edd7',
-  groupName: 'PREreview',
-  avatarPath: '/static/groups/prereview-community--10360d97-bf52-4aef-b2fa-2f60d319edd7.jpg',
-};
-
-const card3: Card = {
-  articleId: '10.1101/2021.03.13.435259',
-  articleTitle: 'Host phenology can drive the evolution of intermediate virulence strategies in some obligate-killer parasites',
-  date: new Date('2021-07-19'),
-  groupId: '19b7464a-edbe-42e8-b7cc-04d1eb1f7332',
-  groupName: 'Peer Community in Evolutionary Biology',
-  avatarPath: '/static/groups/pci-evolutionary-biology--19b7464a-edbe-42e8-b7cc-04d1eb1f7332.jpg',
-};
-
-const renderCard = (card: Card) => `
+const renderEvaluationCard = (card: Card) => `
   <article class="landing-page-card">
     <h3 class="landing-page-card__title">
       <a class="landing-page-card__link" href="/articles/activity/${card.articleId}">${card.articleTitle}</a>
@@ -81,25 +55,29 @@ const userListCards = (userLists: E.Either<DE.DataError, Record<string, HtmlFrag
   toHtmlFragment,
 );
 
+const evaluationCards = (c1: Card, c2: Card, c3: Card) => toHtmlFragment(`
+  <h2 class="landing-page-recently-evaluated__title">Recent group evaluations</h2>
+  <p class="landing-page-recently-evaluated__explanatory_text">
+    Highlighted evaluations by selected groups of discipline experts.
+    <a href="/groups">View all groups</a>.
+  </p>
+  <ul class="landing-page-recently-evaluated__articles">
+    <li>
+      ${renderEvaluationCard(c1)}
+    </li>
+    <li>
+      ${renderEvaluationCard(c2)}
+    </li>
+    <li>
+      ${renderEvaluationCard(c3)}
+    </li>
+  </ul>
+`);
+
 export const renderCardsSection = (userLists: E.Either<DE.DataError, Record<string, HtmlFragment>>): HtmlFragment => toHtmlFragment(`
   <section class="landing-page-recently-evaluated">
     ${userListCards(userLists)}
-    <h2 class="landing-page-recently-evaluated__title">Recent group evaluations</h2>
-    <p class="landing-page-recently-evaluated__explanatory_text">
-      Highlighted evaluations by selected groups of discipline experts.
-      <a href="/groups">View all groups</a>.
-    </p>
-    <ul class="landing-page-recently-evaluated__articles">
-      <li>
-        ${renderCard(card1)}
-      </li>
-      <li>
-        ${renderCard(card2)}
-      </li>
-      <li>
-        ${renderCard(card3)}
-      </li>
-    </ul>
+    ${evaluationCards(card1, card2, card3)}
     <div class="landing-page-recently-evaluated__call_to_action">
       <a href="/groups" class="landing-page-recently-evaluated__button">Discover more groups</a>
     </div>
