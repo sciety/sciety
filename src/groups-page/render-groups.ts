a65c199ef761b50698a419e25e6979d39b6a4e2f
David Moulton <d.moulton@elifesciences.org>
2021-08-16 14:29:54 +0100

    Add more context on the groups directory page #1000

--- src/groups-page/render-groups.ts
+++ src/groups-page/render-groups.ts
@@ -20,6 +20,9 @@ const render = (links: RNEA.ReadonlyNonEmptyArray<HtmlFragment>) => `
       <p>
         A Sciety group is a team of scientists who evaluate, curate and screen research articles. <a href="https://blog.sciety.org/sciety-groups/">Read more about Sciety groups</a>.
       </p>
+      <p>
+        Built for researchers to learn about the latest results, Sciety adds to its growing network by showcasing open preprint evaluations from groups of experts. <a href="https://blog.sciety.org/covid-groups/">Read more about how groups are selected for Sciety</a>.
+        </p>
     </header>
     <ol class="group-list" role="list">
       ${templateListItems(links, 'group-list__item')}
