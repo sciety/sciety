8c2e78236e0565ef7f403b0a72c267521d4fc923
Kevin Rutherford <kevin@rutherford-software.com>
2021-08-16 14:50:17 +0100

    Use the standard page layout for /groups #1000

--- src/groups-page/render-groups.ts
+++ src/groups-page/render-groups.ts
@@ -12,22 +12,24 @@ type RenderGroups = (userId: O.Option<UserId>) => T.Task<HtmlFragment>;
 export type GetAllGroups = T.Task<RNEA.ReadonlyNonEmptyArray<Group>>;
 
 const render = (links: RNEA.ReadonlyNonEmptyArray<HtmlFragment>) => `
-  <section class="groups-page">
-    <header class="page-header">
-      <h1>
-        Groups
-      </h1>
-      <p>
-        A Sciety group is a team of scientists who evaluate, curate and screen research articles. <a href="https://blog.sciety.org/sciety-groups/">Read more about Sciety groups</a>.
-      </p>
-      <p>
-        Built for researchers to learn about the latest results, Sciety adds to its growing network by showcasing open preprint evaluations from groups of experts. <a href="https://blog.sciety.org/covid-groups/">Read more about how groups are selected for Sciety</a>.
+  <div class="page-content__background">
+    <div class="sciety-grid sciety-grid--one-column">
+      <header class="page-header">
+        <h1>
+          Groups
+        </h1>
+        <p>
+          A Sciety group is a team of scientists who evaluate, curate and screen research articles. <a href="https://blog.sciety.org/sciety-groups/">Read more about Sciety groups</a>.
         </p>
-    </header>
-    <ol class="group-list" role="list">
-      ${templateListItems(links, 'group-list__item')}
-    </ol>
-  </section>
+        <p>
+          Built for researchers to learn about the latest results, Sciety adds to its growing network by showcasing open preprint evaluations from groups of experts. <a href="https://blog.sciety.org/covid-groups/">Read more about how groups are selected for Sciety</a>.
+          </p>
+      </header>
+      <ol class="group-list" role="list">
+        ${templateListItems(links, 'group-list__item')}
+      </ol>
+    </div>
+  </div>
 `;
 
 export const renderGroups = (
