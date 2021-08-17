461168a8b210e59163e5b9d43bfbd39603034157
Giorgio Sironi <g.sironi@elifesciences.org>
2021-08-17 14:32:45 +0200

    Extract renderStatistics #1004

--- src/landing-page/hero.ts
+++ src/landing-page/hero.ts
@@ -7,6 +7,20 @@ import { DomainEvent, isUserSavedArticleEvent } from '../domain-events';
 import { renderExampleSearches } from '../shared-components/render-example-searches';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
+const renderStatistics = (listCount: number) => toHtmlFragment(`
+  <div class="landing-page-hero__statistics">
+    <a href="https://blog.sciety.org/lists-on-sciety/" class="landing-page-hero__statistics_link">
+      <span class="landing-page-hero__statistic_number">${listCount}</span><span class="landing-page-hero__statistic_title"> user curated lists</span>
+    </a>
+    <a href="/groups" class="landing-page-hero__statistics_link">
+      <span class="landing-page-hero__statistic_number">20k</span><span class="landing-page-hero__statistic_title"> evaluations</span>
+    </a>
+    <a href="/groups" class="landing-page-hero__statistics_link">
+      <span class="landing-page-hero__statistic_number">15k</span><span class="landing-page-hero__statistic_title"> evaluated articles</span>
+    </a>
+  </div>
+`);
+
 type GetAllEvents = T.Task<ReadonlyArray<DomainEvent>>;
 
 export type Ports = {
@@ -36,17 +50,7 @@ export const hero: Hero = (ports) => pipe(
           Let Sciety help you navigate the preprint landscape.
         </p>
         <p class="landing-page-hero__content_byline">Follow the journey through <a href="/blog">our blog</a>.</p>
-        <div class="landing-page-hero__statistics">
-          <a href="https://blog.sciety.org/lists-on-sciety/" class="landing-page-hero__statistics_link">
-            <span class="landing-page-hero__statistic_number">${listCount}</span><span class="landing-page-hero__statistic_title"> user curated lists</span>
-          </a>
-          <a href="/groups" class="landing-page-hero__statistics_link">
-            <span class="landing-page-hero__statistic_number">20k</span><span class="landing-page-hero__statistic_title"> evaluations</span>
-          </a>
-          <a href="/groups" class="landing-page-hero__statistics_link">
-            <span class="landing-page-hero__statistic_number">15k</span><span class="landing-page-hero__statistic_title"> evaluated articles</span>
-          </a>
-        </div>
+        ${renderStatistics(listCount)}
         <form class="landing-page-hero__search_form" action="/search" method="get">
           <input type="hidden" name="category" value="articles">
           <label for="searchText" class="visually-hidden">Search term</label>
