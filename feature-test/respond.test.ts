3f65d74a688789682351af7e51c1a1e19930442a
David Moulton <d.moulton@elifesciences.org>
2021-08-18 09:46:04 +0100

    Use unique DOI in taiko test to fix intermittent CI failures

--- feature-test/respond.test.ts
+++ feature-test/respond.test.ts
@@ -14,7 +14,7 @@ describe('respond', () => {
 
   describe('when not logged in', () => {
     it('authenticates via twitter, returns and displays increased response count', async () => {
-      await goto('localhost:8080/articles/10.1101/2020.07.13.199174');
+      await goto('localhost:8080/articles/10.1101/722579');
       await click($('.activity-feed__item:first-child button[value="respond-helpful"]'));
       await authenticateViaTwitter();
       const result = await text('1', within($('.activity-feed__item:first-child button[value="revoke-response"]'))).exists();
