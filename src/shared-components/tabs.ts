cb2bbbcb1c48a71f62a5ccdbe3f2f7106e87473d
Giorgio Sironi <g.sironi@elifesciences.org>
2021-08-18 15:57:58 +0200

    Rename internals of tabs component #1006

--- src/shared-components/tabs.ts
+++ src/shared-components/tabs.ts
@@ -22,16 +22,20 @@ const inactiveTab = (tab: Tab) => `<li class="tab" role="presentation">
     <a role="tab" href="${tab.url}">${tab.label}</a>
   </li>`;
 
-const bothTabs = ({ tabList, activeTabIndex }: TabProps) => pipe(
+const renderTabList = ({ tabList, activeTabIndex }: TabProps) => pipe(
   tabList,
   RNEA.mapWithIndex((i, tab) => (i === activeTabIndex ? activeTab(tab) : inactiveTab(tab))),
   (tabs) => tabs.join(''),
+  (tabs) => `
+    <ul class="tab-list" role="tablist">
+      ${tabs}
+    </ul>
+  `,
+  toHtmlFragment,
 );
 
 export const tabs: Tabs = (tabProps) => (activeTabPanelContents) => toHtmlFragment(`
-  <ul class="tab-list" role="tablist">
-    ${bothTabs(tabProps)}
-  </ul>
+  ${renderTabList(tabProps)}
   <section class="tab-panel" role="tabpanel" aria-labelledby="active-tab">
     ${activeTabPanelContents}
   </section>
