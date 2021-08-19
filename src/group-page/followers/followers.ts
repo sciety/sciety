180bdb38d1576ac70c509f105fac528e8d2792b0
Kevin Rutherford <kevin@rutherford-software.com>
2021-08-19 15:48:21 +0100

    Separate projection data from Twitter data #1010

--- src/group-page/followers/followers.ts
+++ src/group-page/followers/followers.ts
@@ -1,5 +1,6 @@
 import { sequenceS } from 'fp-ts/Apply';
 import * as E from 'fp-ts/Either';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { flow, pipe } from 'fp-ts/function';
@@ -8,6 +9,7 @@ import { renderFollowers } from './render-followers';
 import { DomainEvent } from '../../domain-events';
 import { Group } from '../../types/group';
 import { HtmlFragment } from '../../types/html-fragment';
+import { toUserId } from '../../types/user-id';
 
 export type Ports = {
   getAllEvents: T.Task<ReadonlyArray<DomainEvent>>,
@@ -28,7 +30,15 @@ export const followers = (ports: Ports) => (group: Group): TE.TaskEither<never, 
       ports.getAllEvents,
       T.map(countFollowersOf(group.id)),
     ),
-    followers: T.of(process.env.EXPERIMENT_ENABLED === 'true' ? [userCardViewModel] : []),
+    followers: pipe(
+      process.env.EXPERIMENT_ENABLED === 'true' ? [{
+        userId: toUserId('1295307136415735808'),
+        listCount: 1,
+        followedGroupCount: 13,
+      }] : [],
+      RA.map(() => userCardViewModel),
+      T.of,
+    ),
   },
   sequenceS(T.ApplyPar),
   T.map(flow(
