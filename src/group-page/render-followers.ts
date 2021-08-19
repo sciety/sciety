f766e04aba5bafd68258f6b1c703741844edeca3
David Moulton <d.moulton@elifesciences.org>
2021-08-19 13:39:57 +0100

    Flesh out and style one hardcoded user card #1010

--- src/group-page/render-followers.ts
+++ src/group-page/render-followers.ts
@@ -14,7 +14,12 @@ export const renderFollowers: RenderFollowers = flow(
     <ul class="group-page-followers-list">
       <li class="group-page-followers-list__item">
         <article class="user-card">
-          @ScietyHQ
+          <div class="user-card__body">
+            <h3 class="user-card__title"><a href="/users/scietyhq" class="user-card__link">Sciety</a></h3>
+            <div class="user-card__handle">@ScietyHQ</div>
+            <span class="user-card__meta"><span class="visually-hidden">This user has </span><span>1 list</span><span>13 groups followed</span></span>
+          </div>
+          <img class="user-card__avatar" src="https://pbs.twimg.com/profile_images/1323645945179967488/DIp-lv6v_normal.png" alt="">
         </article>
       </li>
     </ul>
