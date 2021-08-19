f0e3f4bfb3dd743bf63235f8523be6c733df2219
Giorgio Sironi <g.sironi@elifesciences.org>
2021-08-19 14:49:20 +0200

    Extract View Model #1010

--- src/group-page/render-followers.ts
+++ src/group-page/render-followers.ts
@@ -3,6 +3,15 @@ import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
 type RenderFollowers = (followers: number) => HtmlFragment;
 
+const userCardViewModel = {
+  link: '/users/scietyhq',
+  title: 'Sciety',
+  handle: 'scietyHQ',
+  listCount: 1,
+  followedGroupCount: 13,
+  avatarUrl: 'https://pbs.twimg.com/profile_images/1323645945179967488/DIp-lv6v_normal.png',
+};
+
 export const renderFollowers: RenderFollowers = flow(
   (followerCount) => `
     <p>
@@ -15,11 +24,11 @@ export const renderFollowers: RenderFollowers = flow(
       <li class="group-page-followers-list__item">
         <article class="user-card">
           <div class="user-card__body">
-            <h3 class="user-card__title"><a href="/users/scietyhq" class="user-card__link">Sciety</a></h3>
-            <div class="user-card__handle">@ScietyHQ</div>
-            <span class="user-card__meta"><span class="visually-hidden">This user has </span><span>1 list</span><span>13 groups followed</span></span>
+            <h3 class="user-card__title"><a href="${userCardViewModel.link}" class="user-card__link">${userCardViewModel.title}</a></h3>
+            <div class="user-card__handle">@${userCardViewModel.handle}</div>
+            <span class="user-card__meta"><span class="visually-hidden">This user has </span><span>${userCardViewModel.listCount} list</span><span>${userCardViewModel.followedGroupCount} groups followed</span></span>
           </div>
-          <img class="user-card__avatar" src="https://pbs.twimg.com/profile_images/1323645945179967488/DIp-lv6v_normal.png" alt="">
+          <img class="user-card__avatar" src="${userCardViewModel.avatarUrl}" alt="">
         </article>
       </li>
     </ul>
