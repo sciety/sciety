2b46de9a10e3ca0dd48594781cdbc8fc934d6e31
Kevin Rutherford <kevin@rutherford-software.com>
2021-08-20 13:34:35 +0100

    Extract a function to fetch reviews from Crossref #859

--- src/ingest/fetch-reviews-from-crossref-via-biorxiv.ts
+++ src/ingest/fetch-reviews-from-crossref-via-biorxiv.ts
@@ -6,16 +6,18 @@ import { pipe } from 'fp-ts/function';
 import { Evaluation } from './evaluations';
 import { FetchEvaluations } from './update-all';
 
+type BiorxivItem = {
+  biorxiv_doi: string,
+  published_doi: string,
+};
+
 type BiorxivResponse = {
   messages: Array<{
     cursor: number | string,
     count: number,
     total: number,
   }>,
-  collection: Array<{
-    biorxiv_doi: string,
-    published_doi: string,
-  }>,
+  collection: Array<BiorxivItem>,
 };
 
 type CrossrefResponse = {
@@ -31,6 +33,33 @@ type CrossrefResponse = {
       },
     ],
   },
+};
+
+const getReviews = (reviewDoiPrefix: string) => async (biorxivItem: BiorxivItem) => {
+  const result: Array<Evaluation> = [];
+  const publishedDoi = biorxivItem.published_doi;
+  const biorxivDoi = biorxivItem.biorxiv_doi;
+  const headers: Record<string, string> = {
+    'User-Agent': 'Sciety (http://sciety.org; mailto:team@sciety.org)',
+  };
+  if (process.env.CROSSREF_API_BEARER_TOKEN !== undefined) {
+    headers['Crossref-Plus-API-Token'] = `Bearer ${process.env.CROSSREF_API_BEARER_TOKEN}`;
+  }
+  const { data } = await axios.get<CrossrefResponse>(
+    `https://api.crossref.org/prefixes/${reviewDoiPrefix}/works?rows=1000&filter=type:peer-review,relation.object:${publishedDoi}`,
+    { headers },
+  );
+  data.message.items.forEach((item) => {
+    const [year, month, day] = item['published-print']['date-parts'][0];
+    const date = new Date(year, month - 1, day);
+    const reviewDoi = item.DOI;
+    result.push({
+      date,
+      articleDoi: biorxivDoi,
+      evaluationLocator: `doi:${reviewDoi}`,
+    });
+  });
+  return result;
 };
 
 const identifyCandidates = (
@@ -49,28 +78,7 @@ const identifyCandidates = (
     const { count } = biorxivData.messages[0];
     total = biorxivData.messages[0].total;
     for (const biorxivItem of biorxivData.collection) {
-      const publishedDoi = biorxivItem.published_doi;
-      const biorxivDoi = biorxivItem.biorxiv_doi;
-      const headers: Record<string, string> = {
-        'User-Agent': 'Sciety (http://sciety.org; mailto:team@sciety.org)',
-      };
-      if (process.env.CROSSREF_API_BEARER_TOKEN !== undefined) {
-        headers['Crossref-Plus-API-Token'] = `Bearer ${process.env.CROSSREF_API_BEARER_TOKEN}`;
-      }
-      const { data } = await axios.get<CrossrefResponse>(
-        `https://api.crossref.org/prefixes/${reviewDoiPrefix}/works?rows=1000&filter=type:peer-review,relation.object:${publishedDoi}`,
-        { headers },
-      );
-      data.message.items.forEach((item) => {
-        const [year, month, day] = item['published-print']['date-parts'][0];
-        const date = new Date(year, month - 1, day);
-        const reviewDoi = item.DOI;
-        result.push({
-          date,
-          articleDoi: biorxivDoi,
-          evaluationLocator: `doi:${reviewDoi}`,
-        });
-      });
+      result.concat(await getReviews(reviewDoiPrefix)(biorxivItem));
     }
     offset += count;
   } while (offset < total);
