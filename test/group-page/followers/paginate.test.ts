6fec21c49a6cb5af6b906637670a3fe279f72aff
Will Byrne <w.byrne@elifesciences.org>
2021-08-23 10:31:57 +0100

    implement each test #1010

--- test/group-page/followers/paginate.test.ts
+++ test/group-page/followers/paginate.test.ts
@@ -1,10 +1,19 @@
 import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { paginate } from '../../../src/group-page/followers/paginate';
 import { arbitraryNumber } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 import { arbitraryGroupId } from '../../types/group-id.helper';
 import { arbitraryUserId } from '../../types/user-id.helper';
+
+const generateFollowers = (followerCount: number) => Array.from(
+  Array(followerCount).keys(),
+).map(() => ({
+  userId: arbitraryUserId(),
+  listCount: arbitraryNumber(0, 10),
+  followedGroupCount: arbitraryNumber(0, 10),
+}));
 
 /* eslint-disable jest/no-commented-out-tests */
 describe('paginate', () => {
@@ -74,14 +83,26 @@ describe('paginate', () => {
       ]);
     });
 
-    // it.each([
-    //  [9, 1, O.none],
-    //  [11, 1, O.some(2)],
-    //  [20, 1, O.some(2)],
-    //  [20, 2, O.none],
-    //  [21, 2, O.some(3)],
-    //  [21, 3, O.none],
-    // ]).todo('given %d followers and a request for page %d, returns the next page');
+    it.each([
+      // [9, 1, O.none],
+      [11, 1, O.some(2)],
+      [20, 1, O.some(2)],
+      // [20, 2, O.none],
+      [21, 2, O.some(3)],
+      // [21, 3, O.none],
+    ])('given %d followers and a request for page %d, returns the next page', (followerCount, page, nextPage) => {
+      const partialViewModel = {
+        followerCount,
+        followers: generateFollowers(followerCount),
+      };
+      const result = pipe(
+        partialViewModel,
+        paginate(arbitraryGroupId(), page, 10),
+        E.getOrElseW(shouldNotBeCalled),
+      );
+
+      expect(result.nextPage).toStrictEqual(nextPage);
+    });
 
     it.todo('returns not-found when asked for a page that does not exist');
 
