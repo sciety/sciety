a2b3fcd64c32eedd3c9ea8468c121ee7033f9658
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-08-24 15:51:00 +0100

    inject getTwitterResponse into batch get #1010

--- src/infrastructure/get-twitter-user-details-batch.ts
+++ src/infrastructure/get-twitter-user-details-batch.ts
@@ -1,4 +1,5 @@
 import * as TE from 'fp-ts/TaskEither';
+import { GetTwitterResponse } from './get-twitter-response';
 import * as DE from '../types/data-error';
 import { UserId } from '../types/user-id';
 
@@ -9,8 +10,10 @@ type UserDetails = {
 };
 
 type GetTwitterUserDetailsBatch = (
+  getTwitterResponse: GetTwitterResponse
+) => (
   userIds: ReadonlyArray<UserId>
 ) => TE.TaskEither<DE.DataError, ReadonlyArray<UserDetails>>;
 
 // ts-unused-exports:disable-next-line
-export const getTwitterUserDetailsBatch: GetTwitterUserDetailsBatch = () => TE.right([]);
+export const getTwitterUserDetailsBatch: GetTwitterUserDetailsBatch = () => () => TE.right([]);
--- test/infrastructure/get-twitter-user-details-batch.test.ts
+++ test/infrastructure/get-twitter-user-details-batch.test.ts
@@ -1,7 +1,9 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { getTwitterUserDetailsBatch } from '../../src/infrastructure/get-twitter-user-details-batch';
+import { arbitraryWord } from '../helpers';
 import { shouldNotBeCalled } from '../should-not-be-called';
+import { arbitraryUserId } from '../types/user-id.helper';
 
 describe('get-twitter-user-details-batch', () => {
   describe('when given no user ids', () => {
@@ -10,7 +12,7 @@ describe('get-twitter-user-details-batch', () => {
     it('returns an empty array', async () => {
       const result = await pipe(
         [],
-        getTwitterUserDetailsBatch,
+        getTwitterUserDetailsBatch(shouldNotBeCalled),
         TE.getOrElse(shouldNotBeCalled),
       )();
 
@@ -19,7 +21,44 @@ describe('get-twitter-user-details-batch', () => {
   });
 
   describe('when given one or more user ids', () => {
-    it.todo('returns details for each user id');
+    it.skip('returns details for each user id', async () => {
+      const handleA = arbitraryWord();
+      const handleB = arbitraryWord();
+      const getTwitterResponse = async () => (
+        {
+          data: [
+            {
+              id: '2244994945',
+              username: handleA,
+              name: 'Twitter Dev',
+            },
+            {
+              id: '783214',
+              username: handleB,
+              name: 'Twitter',
+            },
+          ],
+        }
+      );
+
+      const result = await pipe(
+        [
+          arbitraryUserId(),
+          arbitraryUserId(),
+        ],
+        getTwitterUserDetailsBatch(getTwitterResponse),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+
+      expect(result).toStrictEqual(expect.arrayContaining([
+        expect.objectContaining({
+          handle: handleA,
+        }),
+        expect.objectContaining({
+          handle: handleB,
+        }),
+      ]));
+    });
 
     it.todo('returns details in the same order as that supplied');
 
