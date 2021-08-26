e176f81694333e7e01ea88a085effd82a2a38934
Giorgio Sironi <g.sironi@elifesciences.org>
2021-08-26 12:24:53 +0200

    Write test list #1010

---
+++ test/group-page/followers/augment-with-user-details.test.ts
@@ -0,0 +1,7 @@
+describe('augment-with-user-details', () => {
+  describe('not all user details are returned from the port', () => {
+    it.todo('returns a shorter array of user card view models');
+  });
+
+  it.todo('returns the user card view models in the same order as the input followers');
+});
