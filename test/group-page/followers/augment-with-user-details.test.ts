6e7b2b845275383bfaad29e5a78fbbcf38b9e80c
David Moulton <d.moulton@elifesciences.org>
2021-08-26 11:48:01 +0100

    Add skipped test #1010

--- test/group-page/followers/augment-with-user-details.test.ts
+++ test/group-page/followers/augment-with-user-details.test.ts
@@ -1,6 +1,70 @@
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { augmentWithUserDetails } from '../../../src/group-page/followers/augment-with-user-details';
+import {
+  arbitraryNumber, arbitraryString, arbitraryUri, arbitraryWord,
+} from '../../helpers';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+import { arbitraryUserId } from '../../types/user-id.helper';
+
 describe('augment-with-user-details', () => {
   describe('not all user details are returned from the port', () => {
-    it.todo('returns a shorter array of user card view models');
+    it.skip('returns a shorter array of user card view models', async () => {
+      const userId1 = arbitraryUserId();
+      const listCount1 = 1;
+      const handle1 = arbitraryWord();
+      const userId3 = arbitraryUserId();
+      const listCount3 = 3;
+      const handle3 = arbitraryWord();
+      const ports = {
+        getUserDetailsBatch: () => TE.right([
+          {
+            userId: userId1,
+            handle: handle1,
+            avatarUrl: arbitraryUri(),
+            displayName: arbitraryString(),
+          },
+          {
+            userId: userId3,
+            handle: handle3,
+            avatarUrl: arbitraryUri(),
+            displayName: arbitraryString(),
+          },
+        ]),
+      };
+      const followers = [
+        {
+          userId: userId1,
+          listCount: listCount1,
+          followedGroupCount: arbitraryNumber(0, 10),
+        },
+        {
+          userId: arbitraryUserId(),
+          listCount: arbitraryNumber(0, 10),
+          followedGroupCount: arbitraryNumber(0, 10),
+        },
+        {
+          userId: userId3,
+          listCount: listCount3,
+          followedGroupCount: arbitraryNumber(0, 10),
+        },
+      ];
+      const results = await pipe(
+        followers,
+        augmentWithUserDetails(ports),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+
+      expect(results).toHaveLength(2);
+      expect(results[0]).toStrictEqual(expect.objectContaining({
+        listCount: listCount1,
+        handle: handle1,
+      }));
+      expect(results[1]).toStrictEqual(expect.objectContaining({
+        listCount: listCount3,
+        handle: handle3,
+      }));
+    });
   });
 
   it.todo('returns the user card view models in the same order as the input followers');
