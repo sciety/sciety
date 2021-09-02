d65c576a174b50b2b6a8f14ed94477a0abbff5bc
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-09-02 10:54:23 +0100

    implement todo test #1043

--- test/docmaps/docmap-index/generate-docmap-index.test.ts
+++ test/docmaps/docmap-index/generate-docmap-index.test.ts
@@ -2,9 +2,31 @@ import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
 import { generateDocmapIndex } from '../../../src/docmaps/docmap-index/generate-docmap-index';
+import { editorialCommunityReviewedArticle } from '../../../src/domain-events';
+import * as GID from '../../../src/types/group-id';
+import { arbitraryDoi } from '../../types/doi.helper';
+import { arbitraryReviewId } from '../../types/review-id.helper';
 
 describe('generate-docmap-index', () => {
-  it.todo('includes an absolute url for each docmap in the index');
+  it('includes an absolute url for each docmap in the index', async () => {
+    const doi = arbitraryDoi();
+    const ncrcGroupId = GID.fromValidatedString('62f9b0d0-8d43-4766-a52a-ce02af61bc6a');
+    const result = await pipe(
+      { updatedAfter: O.none },
+      generateDocmapIndex({
+        getAllEvents: T.of([
+          editorialCommunityReviewedArticle(ncrcGroupId, doi, arbitraryReviewId()),
+        ]),
+      }),
+      T.map(({ articles }) => articles),
+    )();
+
+    expect(result).toStrictEqual(expect.arrayContaining([
+      expect.objectContaining({
+        docmap: expect.stringMatching(`https://.*${doi.value}\\.docmap\\.json`),
+      }),
+    ]));
+  });
 
   describe('when no group identifier is supplied', () => {
     it('includes the doi of the hardcoded Review Commons docmap', async () => {
