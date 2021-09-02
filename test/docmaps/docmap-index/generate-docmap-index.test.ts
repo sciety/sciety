937846849a141530343d68d52d0fbfb7b68e7e64
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-09-02 10:22:40 +0100

    test list for generate docmap index #1043

---
+++ test/docmaps/docmap-index/generate-docmap-index.test.ts
@@ -0,0 +1,7 @@
+describe('generate-docmap-index', () => {
+  it.todo('includes a url to the hardcoded Review Commons docmap');
+
+  it.todo('includes urls to the NCRC docmaps');
+
+  it.todo('includes an absolute url for each docmap in the index');
+});
