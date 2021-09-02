22703b6466893adaf03e180514e81ba00bb3abb4
David Moulton <d.moulton@elifesciences.org>
2021-09-02 10:33:05 +0100

    Add todo tests for docmap group filtering #1043

--- test/docmaps/docmap-index/generate-docmap-index.test.ts
+++ test/docmaps/docmap-index/generate-docmap-index.test.ts
@@ -1,7 +1,21 @@
 describe('generate-docmap-index', () => {
-  it.todo('includes a url to the hardcoded Review Commons docmap');
-
-  it.todo('includes urls to the NCRC docmaps');
-
   it.todo('includes an absolute url for each docmap in the index');
+
+  describe('when no group identifier is supplied', () => {
+    it.todo('includes a url to the hardcoded Review Commons docmap');
+
+    it.todo('includes urls to the NCRC docmaps');
+  });
+
+  describe('when passed a group identifier for NCRC', () => {
+    it.todo('only returns urls for NCRC docmaps');
+  });
+
+  describe('when passed a group identifier for Review Commons', () => {
+    it.todo('only returns urls for Review Commons docmaps');
+  });
+
+  describe('when passed anything else as the group argument', () => {
+    it.todo('returns an empty index');
+  });
 });
