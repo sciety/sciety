9656f8e656e0468aad6e420772eb69a1ce74f57c
Giorgio Sironi <g.sironi@elifesciences.org>
2021-09-02 10:44:05 +0100

    Implement simpler test #1043

--- test/docmaps/docmap-index/generate-docmap-index.test.ts
+++ test/docmaps/docmap-index/generate-docmap-index.test.ts
@@ -1,8 +1,27 @@
+import * as O from 'fp-ts/Option';
+import * as T from 'fp-ts/Task';
+import { pipe } from 'fp-ts/function';
+import { generateDocmapIndex } from '../../../src/docmaps/docmap-index/generate-docmap-index';
+
 describe('generate-docmap-index', () => {
   it.todo('includes an absolute url for each docmap in the index');
 
   describe('when no group identifier is supplied', () => {
-    it.todo('includes a url to the hardcoded Review Commons docmap');
+    it('includes the doi of the hardcoded Review Commons docmap', async () => {
+      const result = await pipe(
+        { updatedAfter: O.none },
+        generateDocmapIndex({
+          getAllEvents: T.of([]),
+        }),
+        T.map(({ articles }) => articles),
+      )();
+
+      expect(result).toStrictEqual(expect.arrayContaining([
+        expect.objectContaining({
+          doi: '10.1101/2021.04.25.441302',
+        }),
+      ]));
+    });
 
     it.todo('includes urls to the NCRC docmaps');
   });
