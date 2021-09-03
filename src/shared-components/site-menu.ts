ffc8e293006ca07dec802478799fa859b66157fb
David Moulton <d.moulton@elifesciences.org>
2021-09-03 15:44:10 +0100

    Reorder drawer menu items #1045

--- src/shared-components/site-menu.ts
+++ src/shared-components/site-menu.ts
@@ -15,11 +15,11 @@ const myFeedMenuItem = () => toHtmlFragment(`
 export const siteMenuItems = (user: O.Option<User>): HtmlFragment => toHtmlFragment(`
   <ul role="list" class="site-menu__links">
     <li><a href="/" class="site-menu__link site-menu__link--home"><span class="site-menu__link_text">Home</span></a></li>
+    ${O.fold(constant(''), myFeedMenuItem)(user)}
+    ${O.fold(constant(''), myProfileMenuItem)(user)}
     <li><a href="/groups" class="site-menu__link site-menu__link--groups"><span class="site-menu__link_text">Groups</span></a></li>
     <li><a href="/blog" class="site-menu__link site-menu__link--blog"><span class="site-menu__link_text">Blog</span></a></li>
     <li><a href="/about" class="site-menu__link site-menu__link--about"><span class="site-menu__link_text">About</span></a></li>
-    ${O.fold(constant(''), myFeedMenuItem)(user)}
-    ${O.fold(constant(''), myProfileMenuItem)(user)}
     <li><a href="/contact-us" class="site-menu__link site-menu__link--contact-us"><span class="site-menu__link_text">Contact us</span></a></li>
   </ul>
 `);
