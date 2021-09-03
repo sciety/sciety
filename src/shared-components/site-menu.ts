606bde5e6ac8d7d89dee77c2e7e53840fd373a50
Will Byrne <w.byrne@elifesciences.org>
2021-09-03 15:10:13 +0100

    Add my feed link to side menu #1045

--- src/shared-components/site-menu.ts
+++ src/shared-components/site-menu.ts
@@ -8,12 +8,17 @@ const myProfileMenuItem = (user: User) => toHtmlFragment(`
   <li><a href="/users/${user.handle ?? user.id}" class="site-menu__link site-menu__link--profile"><span class="site-menu__link_text">My profile</span></a></li>
 `);
 
+const yourFeedMenuItem = () => toHtmlFragment(`
+  <li><a href="/" class="site-menu__link site-menu__link--feed"><span class="site-menu__link_text">My feed</span></a></li>
+`);
+
 export const siteMenuItems = (user: O.Option<User>): HtmlFragment => toHtmlFragment(`
   <ul role="list" class="site-menu__links">
     <li><a href="/" class="site-menu__link site-menu__link--home"><span class="site-menu__link_text">Home</span></a></li>
     <li><a href="/groups" class="site-menu__link site-menu__link--groups"><span class="site-menu__link_text">Groups</span></a></li>
     <li><a href="/blog" class="site-menu__link site-menu__link--blog"><span class="site-menu__link_text">Blog</span></a></li>
     <li><a href="/about" class="site-menu__link site-menu__link--about"><span class="site-menu__link_text">About</span></a></li>
+    ${O.fold(constant(''), yourFeedMenuItem)(user)}
     ${O.fold(constant(''), myProfileMenuItem)(user)}
     <li><a href="/contact-us" class="site-menu__link site-menu__link--contact-us"><span class="site-menu__link_text">Contact us</span></a></li>
   </ul>
