27457704da74c885145ad108a7a18d50a605ffcf
Giorgio Sironi <g.sironi@elifesciences.org>
2021-09-06 13:59:22 +0100

    Use consistent page header for my feed #1045

--- src/logged-in-home-page/render-page-header.ts
+++ src/logged-in-home-page/render-page-header.ts
@@ -1,12 +1,8 @@
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
 export const renderPageHeader = (): HtmlFragment => toHtmlFragment(`
-  <header class="logged-in-home-page-header">
-    <h1>
-      Welcome to Sciety
-    </h1>
-    <p>
-      Where research is evaluated and curated by the groups you trust.<br><a href="/about">Learn more about the platform.</a>
-    </p>
+  <header class="page-header">
+    <h1>My Feed</h1>
+    <p>Never miss a preprint evaluation.</p>
   </header>
 `);
