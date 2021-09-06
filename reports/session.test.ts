0220c77ee0ed947d9272c3443bb87f85feaee076
Kevin Rutherford <kevin@rutherford-software.com>
2021-09-06 15:29:48 +0100

    Add a passing test #1044

--- reports/session.test.ts
+++ reports/session.test.ts
@@ -1,5 +1,5 @@
 import * as S from './session';
-import { arbitraryWord } from '../test/helpers';
+import { arbitraryDate, arbitraryString, arbitraryWord } from '../test/helpers';
 
 describe('session', () => {
   describe('split', () => {
@@ -8,6 +8,26 @@ describe('session', () => {
         const session = {
           visitorId: arbitraryWord(),
           pageViews: [],
+        };
+
+        expect(S.split(session)).toStrictEqual([session]);
+      });
+    });
+
+    describe('when the pageViews all fall into a single chunk of time', () => {
+      it('returns a single session', () => {
+        const session = {
+          visitorId: arbitraryWord(),
+          pageViews: [
+            {
+              time_local: new Date('2021-09-06 11:00'),
+              request: arbitraryString(),
+            },
+            {
+              time_local: new Date('2021-09-06 11:05'),
+              request: arbitraryString(),
+            },
+          ],
         };
 
         expect(S.split(session)).toStrictEqual([session]);
