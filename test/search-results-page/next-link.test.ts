4fe0372e722e48e0994dfbb2d1cbcf33046d218b
Will Byrne <w.byrne@elifesciences.org>
2021-09-08 14:21:13 +0100

    Add skipped test for next link escaping #1041

---
+++ test/search-results-page/next-link.test.ts
@@ -0,0 +1,21 @@
+import * as O from 'fp-ts/Option';
+import { JSDOM } from 'jsdom';
+import { nextLink } from '../../src/search-results-page/next-link';
+
+describe('next-link', () => {
+  it.skip('encodes the cursor for http', () => {
+    const nextLinkAnchor = nextLink({
+      pageNumber: 1,
+      category: 'articles',
+      query: 'bats',
+      nextCursor: O.some('foo+/bar'),
+    });
+
+    const rendered = JSDOM.fragment(nextLinkAnchor);
+    const linkHref = rendered.querySelector('a')?.getAttribute('href');
+
+    const someString = '/search?query=bats&category=articles&cursor=foo%2B%2Fbar&page=1';
+
+    expect(linkHref).toStrictEqual(someString);
+  });
+});
