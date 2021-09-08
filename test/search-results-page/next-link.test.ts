d50fbf57a9a0aa7f120dd432a0e4ea1475f1c00a
Will Byrne <w.byrne@elifesciences.org>
2021-09-08 14:48:10 +0100

    add skipped test for query encoding #1041

--- test/search-results-page/next-link.test.ts
+++ test/search-results-page/next-link.test.ts
@@ -5,7 +5,7 @@ import { nextLink } from '../../src/search-results-page/next-link';
 describe('next-link', () => {
   it('encodes the cursor for http', () => {
     const nextLinkAnchor = nextLink({
-      pageNumber: 1,
+      pageNumber: 2,
       category: 'articles',
       query: 'bats',
       nextCursor: O.some('foo+/bar'),
@@ -14,8 +14,24 @@ describe('next-link', () => {
     const rendered = JSDOM.fragment(nextLinkAnchor);
     const linkHref = rendered.querySelector('a')?.getAttribute('href');
 
-    const someString = '/search?query=bats&category=articles&cursor=foo%2B%2Fbar&page=1';
+    const expectedHref = '/search?query=bats&category=articles&cursor=foo%2B%2Fbar&page=2';
 
-    expect(linkHref).toStrictEqual(someString);
+    expect(linkHref).toStrictEqual(expectedHref);
+  });
+
+  it.skip('encodes the query for http', () => {
+    const nextLinkAnchor = nextLink({
+      pageNumber: 2,
+      category: 'articles',
+      query: 'bats+bugs',
+      nextCursor: O.some('foo'),
+    });
+
+    const rendered = JSDOM.fragment(nextLinkAnchor);
+    const linkHref = rendered.querySelector('a')?.getAttribute('href');
+
+    const expectedHref = '/search?query=bats%2Bbugs&category=articles&cursor=foo&page=2';
+
+    expect(linkHref).toStrictEqual(expectedHref);
   });
 });
