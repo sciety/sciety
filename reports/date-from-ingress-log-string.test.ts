598f8f998dd78cd5d10990555280e98562cb9086
Will Byrne <w.byrne@elifesciences.org>
2021-09-09 11:30:54 +0100

    implement date-from-ingress-log #1044

--- reports/date-from-ingress-log-string.test.ts
+++ reports/date-from-ingress-log-string.test.ts
@@ -5,7 +5,7 @@ import { dateFromIngressLogString } from './date-from-ingress-log-string';
 describe('date-from-ingress-log-string', () => {
   it.todo('encodes to an ISO string');
 
-  it.skip('decodes to the correct Date object', () => {
+  it('decodes to the correct Date object', () => {
     const input = '08/Aug/2021:22:31:46 +0000';
     const expectedDate = new Date('08/Aug/2021 22:31:46 +0000');
     const decoded = pipe(
--- reports/date-from-ingress-log-string.ts
+++ reports/date-from-ingress-log-string.ts
@@ -1,9 +1,18 @@
+import * as E from 'fp-ts/Either';
+import { flow, pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
 
+// ts-unused-exports:disable-next-line
 export const dateFromIngressLogString = new t.Type(
   'dateFromIngressLogString ',
-  (u): u is Date => u instanceof Date,
-  tt.DateFromISOString.decode,
-  (d) => d.toString(),
+  (input): input is Date => input instanceof Date,
+  (input, context) => pipe(
+    t.string.validate(input, context),
+    E.chain(flow(
+      (str) => str.replace(/:/, ' '),
+      tt.DateFromISOString.decode,
+    )),
+  ),
+  (date) => date.toString(),
 );
