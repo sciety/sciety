2a020aa9526b6da763c2c5911d77f5fa6fb0e77d
David Moulton <d.moulton@elifesciences.org>
2021-09-10 11:29:02 +0100

    Add skipped tests for isAllowedRequest #1044

--- reports/is-allowed-request.test.ts
+++ reports/is-allowed-request.test.ts
@@ -1,15 +1,27 @@
 import { isAllowedRequest } from './is-allowed-request';
 
-describe('is-allowed-request', () => {
+describe.skip('is-allowed-request', () => {
   it('returns true if request does not start with POST', () => {
     const result = isAllowedRequest('GET / HTTP/1.1');
 
     expect(result).toBe(true);
   });
 
-  it.todo('returns true if POST, and path is whitelisted');
+  it('returns true if POST, and path is whitelisted', () => {
+    const result = isAllowedRequest('POST /follow HTTP/1.1');
 
-  it.todo('returns false if POST, and path is not whitelisted');
+    expect(result).toBe(true);
+  });
 
-  it.todo('returns false if POST, and request is malformed');
+  it('returns false if POST, and path is not whitelisted', () => {
+    const result = isAllowedRequest('POST /foo HTTP/1.1');
+
+    expect(result).toBe(false);
+  });
+
+  it('returns false if POST, and request is malformed', () => {
+    const result = isAllowedRequest('POST +bar');
+
+    expect(result).toBe(false);
+  });
 });
