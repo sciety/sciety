bf8c1b110e77002e05714e5011a06eb4e3e5c3d9
Will Byrne <w.byrne@elifesciences.org>
2021-09-10 11:03:16 +0100

    add todo tests #1044

---
+++ reports/does-not-contain-invalid-post.test.ts
@@ -0,0 +1,9 @@
+describe('does-not-contain-invalid-post', () => {
+  it.todo('returns true if request does not start with POST');
+
+  it.todo('returns false if POST, and path is not whitelisted');
+
+  it.todo('returns true if POST, and path is whitelisted');
+
+  it.todo('returns false if POST, and request is malformed');
+});
