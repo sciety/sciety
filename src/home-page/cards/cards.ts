2a42d484e7f3ea19381d5cda62492ac7a7b7038d
David Moulton <d.moulton@elifesciences.org>
2021-09-10 09:48:09 +0100

    Extract hard coded user list card data for better symmetry with evaluation cards #1059

--- src/home-page/cards/cards.ts
+++ src/home-page/cards/cards.ts
@@ -2,24 +2,24 @@ import { sequenceS } from 'fp-ts/Apply';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { card1, card2, card3 } from './hardcoded-user-list-cards';
 import { renderCardsSection } from './render-cards-section';
 import { Ports, userListCard } from './user-list-card';
 import { HtmlFragment } from '../../types/html-fragment';
-import { toUserId } from '../../types/user-id';
 
 export const cards = (ports: Ports): T.Task<HtmlFragment> => pipe(
   {
     first: userListCard(ports)(
-      toUserId('1417520401282854918'),
-      'Some interesting preprints on ion channel proteins',
+      card1.userId,
+      card1.description,
     ),
     second: userListCard(ports)(
-      toUserId('1412019815619911685'),
-      'See what researchers at Prachee Avasthi’s lab are reading to discover some interesting new work',
+      card2.userId,
+      card2.description,
     ),
     third: userListCard(ports)(
-      toUserId('1223116442549145601'),
-      'A list of papers on innate immunology curated by Ailís O’Carroll',
+      card3.userId,
+      card3.description,
     ),
   },
   sequenceS(TE.ApplyPar),
---
+++ src/home-page/cards/hardcoded-user-list-cards.ts
@@ -0,0 +1,16 @@
+import { toUserId } from '../../types/user-id';
+
+export const card1 = {
+  userId: toUserId('1417520401282854918'),
+  description: 'Some interesting preprints on ion channel proteins',
+};
+
+export const card2 = {
+  userId: toUserId('1412019815619911685'),
+  description: 'See what researchers at Prachee Avasthi’s lab are reading to discover some interesting new work',
+};
+
+export const card3 = {
+  userId: toUserId('1223116442549145601'),
+  description: 'A list of papers on innate immunology curated by Ailís O’Carroll',
+};
