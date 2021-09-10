53f943c3bbdbef78cfb2eff5eb6bb3c541de30f6
Giorgio Sironi <g.sironi@elifesciences.org>
2021-09-10 15:01:34 +0100

    Introduce markup to show one event #1062

--- src/all-events-page/index.ts
+++ src/all-events-page/index.ts
@@ -3,7 +3,14 @@ import { toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 import { RenderPageError } from '../types/render-page-error';
 
+const renderContent = (item: string) => toHtmlFragment(`
+  <h1>All events</h1>
+  <ol>
+    <li>${item}</li>
+  </ol>
+`);
+
 export const allEventsPage = (): TE.TaskEither<RenderPageError, Page> => TE.right({
   title: 'All events',
-  content: toHtmlFragment('<h1>All events</h1>'),
+  content: renderContent('The item'),
 });
