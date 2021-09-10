5f79deed4da1dcf47cf1a6886704dc3b5251c8c5
David Moulton <d.moulton@elifesciences.org>
2021-09-10 15:35:06 +0100

    Pluck date from a DomainEvent #1062

--- src/all-events-page/index.ts
+++ src/all-events-page/index.ts
@@ -2,9 +2,13 @@ import * as E from 'fp-ts/Either';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { DomainEvent } from '../domain-events';
+import { Doi } from '../types/doi';
+import { GroupId } from '../types/group-id';
 import { toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 import { RenderPageError } from '../types/render-page-error';
+import { ReviewId } from '../types/review-id';
 
 const renderContent = (item: string) => toHtmlFragment(`
   <h1>All events</h1>
@@ -14,7 +18,14 @@ const renderContent = (item: string) => toHtmlFragment(`
 `);
 
 export const allEventsPage = (): TE.TaskEither<RenderPageError, Page> => pipe(
-  T.of(new Date('2021-09-10')),
+  T.of({
+    type: 'EditorialCommunityReviewedArticle' as const,
+    date: new Date('2021-09-10'),
+    editorialCommunityId: 'kdasjkd' as GroupId,
+    articleId: new Doi('10.1101/1111111'),
+    reviewId: 'doi:10.1101/9999999' as ReviewId,
+  }),
+  T.map((event: DomainEvent) => event.date),
   T.map((eventDate: Date) => E.right({
     title: 'All events',
     content: renderContent(eventDate.toISOString()),
