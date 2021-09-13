33b3fef4391bdc5a23e21d854e7e4427ddfc4d77
Giorgio Sironi <g.sironi@elifesciences.org>
2021-09-13 14:11:30 +0100

    Differentiate EditorialCommunityReviewedArticle events #1062

--- src/all-events-page/collapse-close-events.ts
+++ src/all-events-page/collapse-close-events.ts
@@ -1,4 +1,7 @@
+import * as B from 'fp-ts/boolean';
+import { pipe } from 'fp-ts/function';
 import { DomainEvent } from '../domain-events';
+import { isEditorialCommunityReviewedArticleEvent } from '../domain-events/type-guards';
 import { Doi } from '../types/doi';
 import { GroupId } from '../types/group-id';
 
@@ -8,7 +11,14 @@ type CollapsedGroupEvaluatedArticle = {
   articleId: Doi,
 };
 
-const processEvent = (state: ReadonlyArray<DomainEvent>, event: DomainEvent) => state.concat([event]);
+const processEvent = (state: ReadonlyArray<DomainEvent>, event: DomainEvent) => pipe(
+  event,
+  isEditorialCommunityReviewedArticleEvent,
+  B.fold(
+    () => [...state, event],
+    () => [...state, event],
+  ),
+);
 
 export const collapseCloseEvents = (
   events: ReadonlyArray<DomainEvent>,
