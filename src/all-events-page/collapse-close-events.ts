5a0d8e8cda80484876aa6c409cfd777dc7048da7
David Moulton <d.moulton@elifesciences.org>
2021-09-13 13:51:06 +0100

    Begin implementation of collapseCloseEvents #1062

--- src/all-events-page/collapse-close-events.ts
+++ src/all-events-page/collapse-close-events.ts
@@ -1,3 +1,13 @@
-import { identity } from 'fp-ts/function';
+import { DomainEvent } from '../domain-events';
+import { Doi } from '../types/doi';
+import { GroupId } from '../types/group-id';
 
-export const collapseCloseEvents = identity;
+type CollapsedGroupEvaluatedArticle = {
+  type: 'CollapsedGroupEvaluatedArticle',
+  groupId: GroupId,
+  articleId: Doi,
+};
+
+export const collapseCloseEvents = (
+  events: ReadonlyArray<DomainEvent>,
+): ReadonlyArray<CollapsedGroupEvaluatedArticle | DomainEvent> => events;
