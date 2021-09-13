4891b11879525b09a272e3ba66a0649649d40e90
Giorgio Sironi <g.sironi@elifesciences.org>
2021-09-13 11:54:11 +0100

    Write highest value test #1062

--- test/all-events-page/collapse-close-events.test.ts
+++ test/all-events-page/collapse-close-events.test.ts
@@ -1,6 +1,32 @@
+import { pipe } from 'fp-ts/function';
+import { collapseCloseEvents } from '../../src/all-events-page/collapse-close-events';
+import { editorialCommunityReviewedArticle } from '../../src/domain-events/editorial-community-reviewed-article-event';
+import { arbitraryDoi } from '../types/doi.helper';
+import { arbitraryGroupId } from '../types/group-id.helper';
+import { arbitraryReviewId } from '../types/review-id.helper';
+
 describe('collapse-close-events', () => {
   describe('given consecutive events in which the same group evaluated an article', () => {
-    it.todo('collapses the events into a single feed item');
+    it.skip('collapses the events into a single feed item', () => {
+      const groupId = arbitraryGroupId();
+      const articleId = arbitraryDoi();
+
+      const result = pipe(
+        [
+          editorialCommunityReviewedArticle(groupId, articleId, arbitraryReviewId()),
+          editorialCommunityReviewedArticle(groupId, articleId, arbitraryReviewId()),
+        ],
+        collapseCloseEvents,
+      );
+
+      expect(result).toStrictEqual([
+        {
+          type: 'CollapsedGroupEvaluatedArticle',
+          groupId,
+          articleId,
+        },
+      ]);
+    });
   });
 
   describe('given consecutive events in which the same group evaluated different articles', () => {
