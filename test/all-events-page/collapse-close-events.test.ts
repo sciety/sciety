062a0d7d42f1a9355d39deac739837d49522643b
Will Byrne <w.byrne@elifesciences.org>
2021-09-14 14:21:49 +0100

    update tests to reflect new understanding of colapse rules #1062

--- test/all-events-page/collapse-close-events.test.ts
+++ test/all-events-page/collapse-close-events.test.ts
@@ -7,7 +7,7 @@ import { arbitraryReviewId } from '../types/review-id.helper';
 
 describe('collapse-close-events', () => {
   describe('given consecutive events in which the same group evaluated an article', () => {
-    it('collapses the events into a single feed item', () => {
+    it.skip('collapses the events into a single feed item', () => {
       const groupId = arbitraryGroupId();
       const articleId = arbitraryDoi();
       const date = new Date('2021-09-14 12:00');
@@ -26,13 +26,13 @@ describe('collapse-close-events', () => {
           type: 'CollapsedGroupEvaluatedArticle',
           groupId,
           articleId,
-          count: 2,
+          reviewCount: 2,
           date,
         },
       ]);
     });
 
-    it('collapses three events into a single feed item', () => {
+    it.skip('collapses three events into a single feed item', () => {
       const groupId = arbitraryGroupId();
       const articleId = arbitraryDoi();
 
@@ -50,82 +50,15 @@ describe('collapse-close-events', () => {
           type: 'CollapsedGroupEvaluatedArticle',
           groupId,
           articleId,
-          count: 3,
+          reviewCount: 3,
           date: expect.any(Date),
         },
-      ]);
-    });
-  });
-
-  describe('given two consecutive series of events in which the same group evaluated two different articles', () => {
-    it('collapses into two feed items', () => {
-      const groupId = arbitraryGroupId();
-      const firstArticleId = arbitraryDoi();
-      const secondArticleId = arbitraryDoi();
-
-      const result = pipe(
-        [
-          editorialCommunityReviewedArticle(groupId, firstArticleId, arbitraryReviewId()),
-          editorialCommunityReviewedArticle(groupId, firstArticleId, arbitraryReviewId()),
-          editorialCommunityReviewedArticle(groupId, secondArticleId, arbitraryReviewId()),
-          editorialCommunityReviewedArticle(groupId, secondArticleId, arbitraryReviewId()),
-        ],
-        collapseCloseEvents,
-      );
-
-      expect(result).toStrictEqual([
-        {
-          type: 'CollapsedGroupEvaluatedArticle',
-          groupId,
-          articleId: firstArticleId,
-          count: 2,
-          date: expect.any(Date),
-        },
-        {
-          type: 'CollapsedGroupEvaluatedArticle',
-          groupId,
-          articleId: secondArticleId,
-          count: 2,
-          date: expect.any(Date),
-        },
-      ]);
-    });
-  });
-
-  describe('given a group reviewing article 1 twice, then article 2 once, and then article 1 again', () => {
-    it('returns three feed items', () => {
-      const groupId = arbitraryGroupId();
-      const firstArticleId = arbitraryDoi();
-      const secondArticleId = arbitraryDoi();
-      const event3 = editorialCommunityReviewedArticle(groupId, secondArticleId, arbitraryReviewId());
-      const event4 = editorialCommunityReviewedArticle(groupId, firstArticleId, arbitraryReviewId());
-
-      const result = pipe(
-        [
-          editorialCommunityReviewedArticle(groupId, firstArticleId, arbitraryReviewId()),
-          editorialCommunityReviewedArticle(groupId, firstArticleId, arbitraryReviewId()),
-          event3,
-          event4,
-        ],
-        collapseCloseEvents,
-      );
-
-      expect(result).toStrictEqual([
-        {
-          type: 'CollapsedGroupEvaluatedArticle',
-          groupId,
-          articleId: firstArticleId,
-          count: 2,
-          date: expect.any(Date),
-        },
-        event3,
-        event4,
       ]);
     });
   });
 
   describe('given consecutive events in which the same group evaluated different articles', () => {
-    it('does not collapse the events', () => {
+    it.skip('collapses into one feed item', () => {
       const groupId = arbitraryGroupId();
 
       const events = [
@@ -137,7 +70,66 @@ describe('collapse-close-events', () => {
         collapseCloseEvents,
       );
 
-      expect(result).toStrictEqual(events);
+      expect(result).toStrictEqual([{
+        type: 'CollapsedGroupEvaluatedMultipleArticles',
+        groupId,
+        articleCount: 2,
+        date: expect.any(Date),
+      }]);
+    });
+  });
+
+  describe('given two consecutive series of events in which the same group evaluated two different articles', () => {
+    it.skip('collapses into one feed item', () => {
+      const groupId = arbitraryGroupId();
+      const firstArticleId = arbitraryDoi();
+      const secondArticleId = arbitraryDoi();
+
+      const result = pipe(
+        [
+          editorialCommunityReviewedArticle(groupId, firstArticleId, arbitraryReviewId()),
+          editorialCommunityReviewedArticle(groupId, firstArticleId, arbitraryReviewId()),
+          editorialCommunityReviewedArticle(groupId, secondArticleId, arbitraryReviewId()),
+          editorialCommunityReviewedArticle(groupId, secondArticleId, arbitraryReviewId()),
+        ],
+        collapseCloseEvents,
+      );
+
+      expect(result).toStrictEqual([
+        {
+          type: 'CollapsedGroupEvaluatedMultipleArticles',
+          groupId,
+          articleCount: 2,
+          date: expect.any(Date),
+        },
+      ]);
+    });
+  });
+
+  describe('given a group reviewing article 1 twice, then article 2 once, and then article 1 again', () => {
+    it.skip('collapses into one feed item', () => {
+      const groupId = arbitraryGroupId();
+      const firstArticleId = arbitraryDoi();
+      const secondArticleId = arbitraryDoi();
+
+      const result = pipe(
+        [
+          editorialCommunityReviewedArticle(groupId, firstArticleId, arbitraryReviewId()),
+          editorialCommunityReviewedArticle(groupId, firstArticleId, arbitraryReviewId()),
+          editorialCommunityReviewedArticle(groupId, secondArticleId, arbitraryReviewId()),
+          editorialCommunityReviewedArticle(groupId, firstArticleId, arbitraryReviewId()),
+        ],
+        collapseCloseEvents,
+      );
+
+      expect(result).toStrictEqual([
+        {
+          type: 'CollapsedGroupEvaluatedMultipleArticles',
+          groupId,
+          articleCount: 2,
+          date: expect.any(Date),
+        },
+      ]);
     });
   });
 
