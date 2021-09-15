e0e3fa01b00f25dd2127feebea529a27039ca70a
Giorgio Sironi <g.sironi@elifesciences.org>
2021-09-15 16:18:48 +0100

    Remove redundant error logging from fetchData #1071

--- src/infrastructure/fetchers.ts
+++ src/infrastructure/fetchers.ts
@@ -6,10 +6,6 @@ export const fetchData = (
 ) => async <D>(uri: string, headers: Record<string, string> = {}): Promise<AxiosResponse<D>> => {
   const startTime = new Date();
   return axios.get<D>(uri, { headers })
-    .catch((error) => {
-      logger('error', 'fetchData exception', { uri, error });
-      throw error;
-    })
     .finally(() => {
       const durationInMs = new Date().getTime() - startTime.getTime();
       logger('debug', 'Response time', { uri, durationInMs });
