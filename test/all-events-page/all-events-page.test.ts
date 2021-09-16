a74ea87eecb967dd8b635b7f793754a66b264fd8
Will Byrne <w.byrne@elifesciences.org>
2021-09-16 10:22:26 +0100

    add todo tests #1077

---
+++ test/all-events-page/all-events-page.test.ts
@@ -0,0 +1,9 @@
+describe('all-events-page', () => {
+  it.todo('renders collapsed single article evaluated events as a single card');
+
+  it.todo('renders collapsed multiple article evaluated events as a single card');
+
+  it.todo('renders a single evaluation as a card');
+
+  it.todo('renders at most 20 cards at a time');
+});
