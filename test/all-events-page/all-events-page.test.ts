b733418bdac9d8af978913965cc061964c67e115
David Moulton <d.moulton@elifesciences.org>
2021-09-16 10:46:19 +0100

    Implement todo test for multiple article collapsing #1077

--- test/all-events-page/all-events-page.test.ts
+++ test/all-events-page/all-events-page.test.ts
@@ -1,7 +1,34 @@
+import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
+import * as TO from 'fp-ts/TaskOption';
+import { pipe } from 'fp-ts/function';
+import { allEventsPage } from '../../src/all-events-page/all-events-page';
+import { groupEvaluatedArticle } from '../../src/domain-events';
+import { shouldNotBeCalled } from '../should-not-be-called';
+import { arbitraryDoi } from '../types/doi.helper';
+import { arbitraryGroup } from '../types/group.helper';
+import { arbitraryReviewId } from '../types/review-id.helper';
+
 describe('all-events-page', () => {
   it.todo('renders collapsed single article evaluated events as a single card');
 
-  it.todo('renders collapsed multiple article evaluated events as a single card');
+  it('renders collapsed multiple article evaluated events as a single card', async () => {
+    const group = arbitraryGroup();
+    const ports = {
+      getGroup: () => TO.some(group),
+      getAllEvents: T.of([
+        groupEvaluatedArticle(group.id, arbitraryDoi(), arbitraryReviewId()),
+        groupEvaluatedArticle(group.id, arbitraryDoi(), arbitraryReviewId()),
+      ]),
+    };
+    const renderedPage = await pipe(
+      allEventsPage(ports)({ page: 1 }),
+      T.map(E.getOrElseW(shouldNotBeCalled)),
+      T.map((page) => page.content),
+    )();
+
+    expect(renderedPage).toContain(`${group.name} evaluated 2 articles`);
+  });
 
   it.todo('renders a single evaluation as a card');
 
