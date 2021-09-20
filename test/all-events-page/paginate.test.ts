237bab5ce80e2ff4fd2665a361f5274bc5320550
Will Byrne <w.byrne@elifesciences.org>
2021-09-20 14:58:55 +0100

    add tests for paginate #1090

---
+++ test/all-events-page/paginate.test.ts
@@ -0,0 +1,50 @@
+import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { paginate } from '../../src/all-events-page/paginate';
+
+const generateItems = (eventCount: number): ReadonlyArray<number> => pipe(
+  Array(eventCount).keys(),
+  Array.from,
+  RA.map((k) => k as number),
+);
+
+describe('paginate', () => {
+  describe('when there are multiple items', () => {
+    it('limits the number of items to the requested page size', () => {
+      const result = pipe(
+        ['a', 'b', 'c'],
+        paginate(2, 1),
+      );
+
+      expect(result.items).toHaveLength(2);
+    });
+
+    it('returns the specified page of the items', () => {
+      const result = pipe(
+        ['a', 'b', 'c'],
+        paginate(1, 2),
+      );
+
+      expect(result.items).toStrictEqual(['b']);
+    });
+
+    it.skip.each([
+      [9, 1, O.none],
+      [11, 1, O.some(2)],
+      [20, 1, O.some(2)],
+      [20, 2, O.none],
+      [21, 2, O.some(3)],
+      [21, 3, O.none],
+    ])('given %d items and a request for page %d, returns the next page', (itemCount, page, nextPage) => {
+      const result = pipe(
+        generateItems(itemCount),
+        paginate(page, 10),
+      );
+
+      expect(result.nextPage).toStrictEqual(nextPage);
+    });
+
+    it.todo('returns not-found when asked for a page that does not exist');
+  });
+});
