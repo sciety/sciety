ac3f8f72be5a511f88efbc43fefc971060a2c3bc
Giorgio Sironi <g.sironi@elifesciences.org>
2021-09-21 15:02:36 +0100

    Add menu link to Sciety feed #1090

--- src/shared-components/site-menu.ts
+++ src/shared-components/site-menu.ts
@@ -15,6 +15,7 @@ const myFeedMenuItem = () => toHtmlFragment(`
 export const siteMenuItems = (user: O.Option<User>): HtmlFragment => toHtmlFragment(`
   <ul role="list" class="site-menu__links">
     <li><a href="/" class="site-menu__link site-menu__link--home"><span class="site-menu__link_text">Home</span></a></li>
+    <li><a href="/sciety-feed" class="site-menu__link site-menu__link--sciety-feed"><span class="site-menu__link_text">Sciety feed</span></a></li>
     ${O.fold(constant(''), myFeedMenuItem)(user)}
     ${O.fold(constant(''), myProfileMenuItem)(user)}
     <li><a href="/groups" class="site-menu__link site-menu__link--groups"><span class="site-menu__link_text">Groups</span></a></li>
