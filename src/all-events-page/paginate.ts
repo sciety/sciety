af57f161e44a308e22a5216a43b16361f2b06e20
Giorgio Sironi <g.sironi@elifesciences.org>
2021-09-21 11:56:31 +0100

    Refactor to build objects only once #1090

--- src/all-events-page/paginate.ts
+++ src/all-events-page/paginate.ts
@@ -12,28 +12,21 @@ type Paginate = <I>(pageSize: number, page: number) => (items: ReadonlyArray<I>)
 }>;
 
 export const paginate: Paginate = (pageSize, page) => (items) => pipe(
-  { pageNumber: page },
-  ({ pageNumber }) => pipe(
-    items,
-    RA.chunksOf(pageSize),
-    (chunks) => ({
-      chunks,
-      pageNumber,
+  items,
+  RA.chunksOf(pageSize),
+  (chunks) => pipe(
+    chunks,
+    RA.lookup(page - 1),
+    E.fromOption(() => DE.notFound),
+    E.map((pageOfItems) => ({
+      items: pageOfItems,
+      pageNumber: page,
       numberOfPages: chunks.length,
       nextPage: pipe(
         page + 1,
         O.some,
         O.filter((nextPage) => nextPage <= chunks.length),
       ),
-    }),
-  ),
-  ({ chunks, ...rest }) => pipe(
-    chunks,
-    RA.lookup(page - 1),
-    E.fromOption(() => DE.notFound),
-    E.map((pageOfItems) => ({
-      items: pageOfItems,
-      ...rest,
     })),
   ),
 );
