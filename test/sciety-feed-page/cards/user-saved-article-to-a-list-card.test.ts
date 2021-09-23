a0c1b221cff1a361e01e9920cad3c52d3426801d
David Moulton <d.moulton@elifesciences.org>
2021-09-23 09:52:33 +0100

    Add todo tests for user saved article to a list card #1101

---
+++ test/sciety-feed-page/cards/user-saved-article-to-a-list-card.test.ts
@@ -0,0 +1,11 @@
+describe('user-saved-article-to-a-list-card', () => {
+  it.todo('includes the user\'s handle');
+
+  it.todo('includes the user\'s avatar');
+
+  it.todo('includes the event date');
+
+  it.todo('includes the link to the list page');
+
+  it.todo('includes title and description of the list');
+});
