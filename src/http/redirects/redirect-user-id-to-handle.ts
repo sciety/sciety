966c82a3aeb4eaf55e700d63b8ddb880b35797c5
David Moulton <d.moulton@elifesciences.org>
2021-09-24 10:36:57 +0100

    Handle when a userid is not found #1105

--- src/http/redirects/redirect-user-id-to-handle.ts
+++ src/http/redirects/redirect-user-id-to-handle.ts
@@ -1,11 +1,14 @@
 import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { StatusCodes } from 'http-status-codes';
 import { Middleware } from 'koa';
 import * as DE from '../../types/data-error';
+import { toHtmlFragment } from '../../types/html-fragment';
 import { UserId } from '../../types/user-id';
+import { toErrorResponse } from '../page-handler';
 
 type Ports = {
   getUserDetails: (userID: UserId) => TE.TaskEither<DE.DataError, { handle: string }>,
@@ -16,7 +19,12 @@ export const redirectUserIdToHandle = (ports: Ports, path: string): Middleware =
     ports.getUserDetails(context.params.id as UserId),
     T.map(E.fold(
       () => {
-        context.status = StatusCodes.NOT_FOUND;
+        const response = toErrorResponse(O.fromNullable(context.state.user))({
+          type: DE.notFound,
+          message: toHtmlFragment('Sorry, we can\'t find this user.'),
+        });
+        context.response.type = 'html';
+        Object.assign(context.response, response);
       },
       ({ handle }) => {
         context.status = StatusCodes.PERMANENT_REDIRECT;
