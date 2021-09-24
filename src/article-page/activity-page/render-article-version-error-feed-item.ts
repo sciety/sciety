80c9f1773fd8bd9038093aacffa7a2d409624906
Giorgio Sironi <g.sironi@elifesciences.org>
2021-09-24 15:03:10 +0100

    Fix markup of error feed items #1107

--- src/article-page/activity-page/render-article-version-error-feed-item.ts
+++ src/article-page/activity-page/render-article-version-error-feed-item.ts
@@ -2,28 +2,32 @@ import { toHtmlFragment } from '../../types/html-fragment';
 
 export const biorxivArticleVersionErrorFeedItem = toHtmlFragment(`
   <div class="activity-feed__item_contents">
-    <img class="activity-feed__item__avatar" src="/static/images/biorxiv.jpg" alt="">
-    <div>
-      <p class="activity-feed__item__title">
-        Published on bioRxiv
-      </p>
-      <p>
-        We couldn't get version information from bioRxiv. Please try refreshing this page.
-      </p>
-    </div>
+    <header class="activity-feed__item_header">
+      <img class="activity-feed__item__avatar" src="/static/images/biorxiv.jpg" alt="">
+      <div class="activity-feed__item__meta">
+        <div class="activity-feed__item__title">
+          Published on bioRxiv
+        </div>
+      </div>
+    </header>
+    <p>
+      We couldn't get version information from bioRxiv. Please try refreshing this page.
+    </p>
   </div>
 `);
 
 export const medrxivArticleVersionErrorFeedItem = toHtmlFragment(`
   <div class="activity-feed__item_contents">
-    <img class="activity-feed__item__avatar" src="/static/images/medrxiv.jpg" alt="">
-    <div>
-      <p class="activity-feed__item__title">
-        Published on medRxiv
-      </p>
-      <p>
-        We couldn't get version information from medRxiv. Please try refreshing this page.
-      </p>
-    </div>
+    <header class="activity-feed__item_header">
+      <img class="activity-feed__item__avatar" src="/static/images/medrxiv.jpg" alt="">
+      <div class="activity-feed__item__meta">
+        <div class="activity-feed__item__title">
+          Published on medRxiv
+        </div>
+      </div>
+    </header>
+    <p>
+      We couldn't get version information from medRxiv. Please try refreshing this page.
+    </p>
   </div>
 `);
