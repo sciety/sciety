f7927176a17347cbed69cd73e468891d0760ae1d
Kevin Rutherford <kevin@rutherford-software.com>
2021-09-24 11:37:08 +0100

    Create a test list for the new feed card #1102

---
+++ test/sciety-feed-page/cards/user-followed-group-card.test.ts
@@ -0,0 +1,9 @@
+describe('user-followed-group-card', () => {
+  it.todo('displays the user\'s avatar');
+
+  it.todo('displays the user\'s handle');
+
+  it.todo('displays the date of the event');
+
+  it.todo('links to the group page about tab');
+});
