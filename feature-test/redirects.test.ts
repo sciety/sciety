7169b572d9a1b72b0ea4de73de3d4a6dab4b465d
Giorgio Sironi <g.sironi@elifesciences.org>
2021-09-24 10:21:57 +0100

    Add skipped test for error on redirect #1105

--- feature-test/redirects.test.ts
+++ feature-test/redirects.test.ts
@@ -32,7 +32,14 @@ describe('legacy redirects', () => {
 
   describe('user list page', () => {
     describe('when the user ID is not found', () => {
-      it.todo('displays a formatted error page');
+      it.skip('displays a formatted error page', async () => {
+        // Taiko doesn't seem to be able to load 404 pages with this call
+        await goto('localhost:8080/users/432143214321/lists/saved-articles');
+
+        const hasErrorHeading = await text('Oops!', within($('.error-page'))).exists();
+
+        expect(hasErrorHeading).toBe(true);
+      });
     });
   });
 });
