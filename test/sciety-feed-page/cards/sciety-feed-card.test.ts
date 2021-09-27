92f4fe4fb124bc6dc6486ee6b0c5797fc49641ac
David Moulton <d.moulton@elifesciences.org>
2021-09-27 10:32:20 +0100

    Add todo tests for sciety feed card renderer #1102

---
+++ test/sciety-feed-page/cards/sciety-feed-card.test.ts
@@ -0,0 +1,9 @@
+describe('sciety-feed-card', () => {
+  describe('when the view model doesn\'t contain details', () => {
+    it.todo('it doesn\'t include a details section');
+  });
+
+  describe('when the view model contains details', () => {
+    it.todo('it contains a details section');
+  });
+});
