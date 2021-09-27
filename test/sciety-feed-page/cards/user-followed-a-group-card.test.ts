418833cba7d95253f1adad8b2b820a4fec36ebba
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-09-27 14:38:07 +0100

    extract common setup for followed group happy path #1102

--- test/sciety-feed-page/cards/user-followed-a-group-card.test.ts
+++ test/sciety-feed-page/cards/user-followed-a-group-card.test.ts
@@ -3,6 +3,7 @@ import * as TO from 'fp-ts/TaskOption';
 import { pipe } from 'fp-ts/function';
 import { userFollowedEditorialCommunity } from '../../../src/domain-events';
 import { userFollowedAGroupCard } from '../../../src/sciety-feed-page/cards';
+import { HtmlFragment } from '../../../src/types/html-fragment';
 import { arbitraryUri } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 import { arbitraryGroupId } from '../../types/group-id.helper';
@@ -11,53 +12,42 @@ import { arbitraryUserId } from '../../types/user-id.helper';
 
 describe('user-followed-a-group-card', () => {
   const userId = arbitraryUserId();
-  const avatarUrl = arbitraryUri();
-  const handle = 'handle';
-  const getUserDetails = () => TE.right({
-    handle,
-    avatarUrl,
-  });
-  const group = arbitraryGroup();
-  const getGroup = () => TO.some(group);
   const event = userFollowedEditorialCommunity(userId, arbitraryGroupId(), new Date('2021-08-12'));
 
-  it('displays the user\'s avatar', async () => {
-    const result = await pipe(
-      event,
-      userFollowedAGroupCard(getUserDetails, getGroup),
-      TE.getOrElseW(shouldNotBeCalled),
-    )();
+  describe('happy path', () => {
+    const avatarUrl = arbitraryUri();
+    const handle = 'handle';
+    const getUserDetails = () => TE.right({
+      handle,
+      avatarUrl,
+    });
+    const group = arbitraryGroup();
+    const getGroup = () => TO.some(group);
 
-    expect(result).toContain(avatarUrl);
-  });
+    let result: HtmlFragment;
 
-  it('displays the user\'s handle', async () => {
-    const result = await pipe(
-      event,
-      userFollowedAGroupCard(getUserDetails, getGroup),
-      TE.getOrElseW(shouldNotBeCalled),
-    )();
+    beforeEach(async () => {
+      result = await pipe(
+        event,
+        userFollowedAGroupCard(getUserDetails, getGroup),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+    });
 
-    expect(result).toContain(handle);
-  });
+    it('displays the user\'s avatar', async () => {
+      expect(result).toContain(avatarUrl);
+    });
 
-  it('displays the date of the event', async () => {
-    const result = await pipe(
-      event,
-      userFollowedAGroupCard(getUserDetails, getGroup),
-      TE.getOrElseW(shouldNotBeCalled),
-    )();
+    it('displays the user\'s handle', async () => {
+      expect(result).toContain(handle);
+    });
 
-    expect(result).toContain('Aug 12, 2021');
-  });
+    it('displays the date of the event', async () => {
+      expect(result).toContain('Aug 12, 2021');
+    });
 
-  it.skip('links to the group page about tab', async () => {
-    const result = await pipe(
-      event,
-      userFollowedAGroupCard(getUserDetails, getGroup),
-      TE.getOrElseW(shouldNotBeCalled),
-    )();
-
-    expect(result).toContain(`href="/groups/${group.slug}/about"`);
+    it.skip('links to the group page about tab', async () => {
+      expect(result).toContain(`href="/groups/${group.slug}/about"`);
+    });
   });
 });
