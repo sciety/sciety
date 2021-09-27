5808583047743fc87aabaab8c457577db434bf2a
Kevin Rutherford <kevin@rutherford-software.com>
2021-09-27 10:01:20 +0100

    Add a skipped test for linking to the group page #1102

--- src/sciety-feed-page/cards/user-followed-a-group-card.ts
+++ src/sciety-feed-page/cards/user-followed-a-group-card.ts
@@ -1,10 +1,15 @@
 import * as TE from 'fp-ts/TaskEither';
+import * as TO from 'fp-ts/TaskOption';
 import { flow, pipe } from 'fp-ts/function';
 import { UserFollowedEditorialCommunityEvent } from '../../domain-events';
 import { templateDate } from '../../shared-components/date';
 import * as DE from '../../types/data-error';
+import { Group } from '../../types/group';
+import { GroupId } from '../../types/group-id';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 import { UserId } from '../../types/user-id';
+
+type GetGroup = (id: GroupId) => TO.TaskOption<Group>;
 
 type GetUserDetails = (userId: UserId) => TE.TaskEither<DE.DataError, {
   handle: string,
@@ -12,7 +17,8 @@ type GetUserDetails = (userId: UserId) => TE.TaskEither<DE.DataError, {
 }>;
 
 type UserFollowedAGroupCard = (
-  getUserDetails: GetUserDetails
+  getUserDetails: GetUserDetails,
+  getGroup: GetGroup,
 ) => (event: UserFollowedEditorialCommunityEvent) => TE.TaskEither<DE.DataError, HtmlFragment>;
 
 // ts-unused-exports:disable-next-line
--- test/sciety-feed-page/cards/user-followed-a-group-card.test.ts
+++ test/sciety-feed-page/cards/user-followed-a-group-card.test.ts
@@ -1,10 +1,12 @@
 import * as TE from 'fp-ts/TaskEither';
+import * as TO from 'fp-ts/TaskOption';
 import { pipe } from 'fp-ts/function';
 import { userFollowedEditorialCommunity } from '../../../src/domain-events';
 import { userFollowedAGroupCard } from '../../../src/sciety-feed-page/cards';
 import { arbitraryUri } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 import { arbitraryGroupId } from '../../types/group-id.helper';
+import { arbitraryGroup } from '../../types/group.helper';
 import { arbitraryUserId } from '../../types/user-id.helper';
 
 describe('user-followed-a-group-card', () => {
@@ -15,12 +17,14 @@ describe('user-followed-a-group-card', () => {
     handle,
     avatarUrl,
   });
+  const group = arbitraryGroup();
+  const getGroup = () => TO.some(group);
   const event = userFollowedEditorialCommunity(userId, arbitraryGroupId(), new Date('2021-08-12'));
 
   it('displays the user\'s avatar', async () => {
     const result = await pipe(
       event,
-      userFollowedAGroupCard(getUserDetails),
+      userFollowedAGroupCard(getUserDetails, getGroup),
       TE.getOrElseW(shouldNotBeCalled),
     )();
 
@@ -30,7 +34,7 @@ describe('user-followed-a-group-card', () => {
   it('displays the user\'s handle', async () => {
     const result = await pipe(
       event,
-      userFollowedAGroupCard(getUserDetails),
+      userFollowedAGroupCard(getUserDetails, getGroup),
       TE.getOrElseW(shouldNotBeCalled),
     )();
 
@@ -40,12 +44,20 @@ describe('user-followed-a-group-card', () => {
   it('displays the date of the event', async () => {
     const result = await pipe(
       event,
-      userFollowedAGroupCard(getUserDetails),
+      userFollowedAGroupCard(getUserDetails, getGroup),
       TE.getOrElseW(shouldNotBeCalled),
     )();
 
     expect(result).toContain('Aug 12, 2021');
   });
 
-  it.todo('links to the group page about tab');
+  it.skip('links to the group page about tab', async () => {
+    const result = await pipe(
+      event,
+      userFollowedAGroupCard(getUserDetails, getGroup),
+      TE.getOrElseW(shouldNotBeCalled),
+    )();
+
+    expect(result).toContain(`href="/groups/${group.slug}/about"`);
+  });
 });
