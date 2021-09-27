23789106a315415b9223e96b667784f94b21589e
Will Byrne <w.byrne@elifesciences.org>
2021-09-27 10:39:43 +0100

    add test for not rendering details on sciety feed cards #1102

--- test/sciety-feed-page/cards/sciety-feed-card.test.ts
+++ test/sciety-feed-page/cards/sciety-feed-card.test.ts
@@ -1,9 +1,21 @@
+import { scietyFeedCard } from '../../../src/sciety-feed-page/cards/sciety-feed-card';
+import { arbitraryDate, arbitraryString, arbitraryUri } from '../../helpers';
+
 describe('sciety-feed-card', () => {
   describe('when the view model doesn\'t contain details', () => {
-    it.todo('it doesn\'t include a details section');
+    it('doesn\'t contain a details section', () => {
+      const rendered = scietyFeedCard({
+        titleText: arbitraryString(),
+        avatarUrl: arbitraryUri(),
+        date: arbitraryDate(),
+        linkUrl: arbitraryUri(),
+      });
+
+      expect(rendered).not.toContain('sciety-feed-card__details');
+    });
   });
 
   describe('when the view model contains details', () => {
-    it.todo('it contains a details section');
+    it.todo('contains a details section');
   });
 });
