af109ef0f9801e285320ab71cf963960e79c4fb4
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-09-27 10:42:45 +0100

    test for details section #1102

--- test/sciety-feed-page/cards/sciety-feed-card.test.ts
+++ test/sciety-feed-page/cards/sciety-feed-card.test.ts
@@ -1,4 +1,5 @@
 import { scietyFeedCard } from '../../../src/sciety-feed-page/cards/sciety-feed-card';
+import { toHtmlFragment } from '../../../src/types/html-fragment';
 import { arbitraryDate, arbitraryString, arbitraryUri } from '../../helpers';
 
 describe('sciety-feed-card', () => {
@@ -16,6 +17,19 @@ describe('sciety-feed-card', () => {
   });
 
   describe('when the view model contains details', () => {
-    it.todo('contains a details section');
+    it('contains a details section', () => {
+      const rendered = scietyFeedCard({
+        titleText: arbitraryString(),
+        avatarUrl: arbitraryUri(),
+        date: arbitraryDate(),
+        linkUrl: arbitraryUri(),
+        details: {
+          title: toHtmlFragment(arbitraryString()),
+          content: toHtmlFragment(arbitraryString()),
+        },
+      });
+
+      expect(rendered).toContain('sciety-feed-card__details');
+    });
   });
 });
