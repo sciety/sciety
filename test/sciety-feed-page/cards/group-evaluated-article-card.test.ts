d852bc1ec14a32e871c4ea88040f6e675f510c20
Kevin Rutherford <kevin@rutherford-software.com>
2021-09-28 13:56:49 +0100

    Implement a todo test #1102

--- test/sciety-feed-page/cards/group-evaluated-article-card.test.ts
+++ test/sciety-feed-page/cards/group-evaluated-article-card.test.ts
@@ -5,6 +5,7 @@ import { pipe } from 'fp-ts/function';
 import { groupEvaluatedArticle } from '../../../src/domain-events';
 import { groupEvaluatedArticleCard } from '../../../src/sciety-feed-page/cards';
 import * as DE from '../../../src/types/data-error';
+import { arbitraryHtmlFragment } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 import { arbitraryDoi } from '../../types/doi.helper';
 import { arbitraryGroupId } from '../../types/group-id.helper';
@@ -12,6 +13,31 @@ import { arbitraryGroup } from '../../types/group.helper';
 import { arbitraryReviewId } from '../../types/review-id.helper';
 
 describe('group-evaluated-article-card', () => {
+  describe('when the article can be fetched', () => {
+    const article = {
+      doi: arbitraryDoi(),
+      title: arbitraryHtmlFragment(),
+      authors: [],
+    };
+    const fetchArticle = () => TE.right(article);
+    const createCard = pipe(
+      groupEvaluatedArticle(arbitraryGroupId(), arbitraryDoi(), arbitraryReviewId()),
+      groupEvaluatedArticleCard({
+        getGroup: () => TO.some(arbitraryGroup()),
+        fetchArticle,
+      }),
+      TE.getOrElse(shouldNotBeCalled),
+    );
+
+    it('adds the article title to the card details', async () => {
+      const viewModel = await createCard();
+
+      expect(viewModel.details?.title).toStrictEqual(article.title);
+    });
+
+    it.todo('adds the group name to the titleText');
+  });
+
   describe('when the article cannot be fetched', () => {
     const fetchArticle = () => TE.left(DE.unavailable);
     const createCard = pipe(
@@ -36,11 +62,5 @@ describe('group-evaluated-article-card', () => {
 
       expect(viewModel.details).toBeUndefined();
     });
-  });
-
-  describe('when the article can be fetched', () => {
-    it.todo('adds the article title to the card details');
-
-    it.todo('adds the group name to the titleText');
   });
 });
