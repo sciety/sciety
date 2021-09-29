b73839820ee1fc51535a570fd3a3883dcff99f60
David Moulton <d.moulton@elifesciences.org>
2021-09-29 13:49:00 +0100

    Add skipped test #1123

--- test/group-evaluations-page/evaluated-articles-list/evaluated-articles-list.test.ts
+++ test/group-evaluations-page/evaluated-articles-list/evaluated-articles-list.test.ts
@@ -1,16 +1,52 @@
 import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
+import * as TO from 'fp-ts/TaskOption';
 import { pipe } from 'fp-ts/function';
+import { JSDOM } from 'jsdom';
 import { evaluatedArticlesList } from '../../../src/group-evaluations-page/evaluated-articles-list';
 import * as DE from '../../../src/types/data-error';
-import { arbitraryDate, arbitraryNumber } from '../../helpers';
+import { arbitraryDate, arbitraryNumber, arbitrarySanitisedHtmlFragment } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
+import { arbitraryArticleServer } from '../../types/article-server.helper';
 import { arbitraryDoi } from '../../types/doi.helper';
 import { arbitraryGroup } from '../../types/group.helper';
 
+const generateArticles = (count: number) => (
+  [...Array(count).keys()].map(() => ({
+    doi: arbitraryDoi(),
+    evaluationCount: arbitraryNumber(1, 5),
+    latestActivityDate: arbitraryDate(),
+  }))
+);
+
 describe('evaluated-articles-list', () => {
   describe('when some of the article details can\'t be retrieved', () => {
-    it.todo('returns the successful article cards');
+    it.skip('returns the successful article cards', async () => {
+      const articles = generateArticles(4);
+      const fetchArticle = jest.fn().mockImplementation(
+        (doi) => {
+          if (doi.value === articles[0].doi.value || doi.value === articles[1].doi.value) {
+            return TE.left(DE.unavailable);
+          }
+          return TE.right({
+            title: arbitrarySanitisedHtmlFragment(),
+            server: arbitraryArticleServer(),
+            authors: [],
+          });
+        },
+      );
+      const html = await pipe(
+        evaluatedArticlesList({
+          fetchArticle,
+          findVersionsForArticleDoi: () => TO.some([{ occurredAt: arbitraryDate() }]),
+        })(articles, arbitraryGroup(), 1, 1),
+        TE.map(JSDOM.fragment),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+      const cardCount = Array.from(html.querySelectorAll('.article-card')).length;
+
+      expect(cardCount).toBe(2);
+    });
   });
 
   describe('when none of the article details can be retrieved', () => {
