2c150c8487cc2e073a3747db750dac8ac08c2999
Giorgio Sironi <g.sironi@elifesciences.org>
2021-09-29 10:51:03 +0100

    Move test list to the component level #1123

---
+++ test/group-evaluations-page/evaluated-articles-list/evaluated-articles-list.test.ts
@@ -0,0 +1,13 @@
+describe('evaluated-articles-list', () => {
+  describe('when some of the article details can\'t be retrieved', () => {
+    it.todo('returns the successful article cards');
+  });
+
+  describe('when none of the article details can be retrieved', () => {
+    it.todo('returns "this information can\'t be found" message');
+  });
+
+  describe('when the requested page is out of bounds', () => {
+    it.todo('returns unavailable');
+  });
+});
