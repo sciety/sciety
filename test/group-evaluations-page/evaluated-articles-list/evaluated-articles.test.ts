8bc60969931c9e65025722bd839448905dee8320
Will Byrne <w.byrne@elifesciences.org>
2021-09-29 10:41:48 +0100

    add todo tests #1123

--- test/group-evaluations-page/evaluated-articles-list/evaluated-articles.test.ts
+++ test/group-evaluations-page/evaluated-articles-list/evaluated-articles.test.ts
@@ -277,4 +277,16 @@ describe('evaluated-articles', () => {
       expect(endTime - startTime).toBeLessThan(350);
     });
   });
+
+  describe('when some of the article details can\'t be retrieved', () => {
+    it.todo('returns the successful article cards');
+  });
+
+  describe('when none of the article details can be retrieved', () => {
+    it.todo('returns "this information can\'t be found" message');
+  });
+
+  describe('when the requested page is out of bounds', () => {
+    it.todo('returns unavailable');
+  });
 });
