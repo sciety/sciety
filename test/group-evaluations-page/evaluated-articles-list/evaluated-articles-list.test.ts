8454694aaa6470729b81cc3f39f0195f020569dd
David Moulton <d.moulton@elifesciences.org>
2021-09-29 11:22:49 +0100

    Implement test for list page being out of bounds #1123

--- test/group-evaluations-page/evaluated-articles-list/evaluated-articles-list.test.ts
+++ test/group-evaluations-page/evaluated-articles-list/evaluated-articles-list.test.ts
@@ -1,3 +1,11 @@
+import * as E from 'fp-ts/Either';
+import { evaluatedArticlesList } from '../../../src/group-evaluations-page/evaluated-articles-list';
+import * as DE from '../../../src/types/data-error';
+import { arbitraryDate, arbitraryNumber } from '../../helpers';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+import { arbitraryDoi } from '../../types/doi.helper';
+import { arbitraryGroup } from '../../types/group.helper';
+
 describe('evaluated-articles-list', () => {
   describe('when some of the article details can\'t be retrieved', () => {
     it.todo('returns the successful article cards');
@@ -8,6 +16,24 @@ describe('evaluated-articles-list', () => {
   });
 
   describe('when the requested page is out of bounds', () => {
-    it.todo('returns unavailable');
+    it('returns not found', async () => {
+      const pageNumber = 2;
+      const pageSize = 1;
+      const result = await evaluatedArticlesList({
+        fetchArticle: shouldNotBeCalled,
+        findVersionsForArticleDoi: shouldNotBeCalled,
+      })(
+        [{
+          doi: arbitraryDoi(),
+          evaluationCount: arbitraryNumber(1, 5),
+          latestActivityDate: arbitraryDate(),
+        }],
+        arbitraryGroup(),
+        pageNumber,
+        pageSize,
+      )();
+
+      expect(result).toStrictEqual(E.left(DE.notFound));
+    });
   });
 });
