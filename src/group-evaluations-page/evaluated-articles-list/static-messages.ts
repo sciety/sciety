d0c1232b5fd3c657d234802fe6e8b70c426110b4
Giorgio Sironi <g.sironi@elifesciences.org>
2021-10-01 11:41:32 +0100

    Unify static messages for this component #1123

--- src/group-evaluations-page/evaluated-articles-list/static-messages.ts
+++ src/group-evaluations-page/evaluated-articles-list/static-messages.ts
@@ -1,5 +1,11 @@
 import { toHtmlFragment } from '../../types/html-fragment';
 
-export const noEvaluatedArticlesMessage = toHtmlFragment('<p class="evaluated-articles__empty">It looks like this group hasn’t evaluated any articles yet. Try coming back later!</p>');
+const staticMessage = (message: string) => toHtmlFragment(`<p class="evaluated-articles__static_message">${message}</p>`);
 
-export const noArticlesCanBeFetchedMessage = toHtmlFragment('<p class="static-message">This information can\'t be fetched right now. Please try again later.</p>');
+export const noEvaluatedArticlesMessage = staticMessage(
+  'It looks like this group hasn’t evaluated any articles yet. Try coming back later!',
+);
+
+export const noArticlesCanBeFetchedMessage = staticMessage(
+  'This information can\'t be fetched right now. Please try again later.',
+);
