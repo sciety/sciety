6cfc3450f7cbeea0ca222fb61b1d56dc442a89ad
Will Byrne <w.byrne@elifesciences.org>
2021-10-04 15:35:17 +0100

    add skipped test #1099

--- test/docmaps/docmap-index/docmap-index.test.ts
+++ test/docmaps/docmap-index/docmap-index.test.ts
@@ -6,6 +6,7 @@ import { StatusCodes } from 'http-status-codes';
 import { docmapIndex } from '../../../src/docmaps/docmap-index';
 import { FindVersionsForArticleDoi } from '../../../src/docmaps/docmap/docmap';
 import { groupEvaluatedArticle } from '../../../src/domain-events/group-evaluated-article-event';
+import * as DE from '../../../src/types/data-error';
 import * as GID from '../../../src/types/group-id';
 import { arbitraryDate, arbitraryUri } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
@@ -15,6 +16,8 @@ import { arbitraryGroup } from '../../types/group.helper';
 import { arbitraryReviewId } from '../../types/review-id.helper';
 
 describe('docmap-index', () => {
+  const ncrcGroupId = GID.fromValidatedString('62f9b0d0-8d43-4766-a52a-ce02af61bc6a');
+
   describe('when all ports work', () => {
     describe('and there are no docmaps', () => {
       let response: { body: { articles: ReadonlyArray<unknown> }, status: StatusCodes };
@@ -44,7 +47,6 @@ describe('docmap-index', () => {
       let response: { body: { articles: ReadonlyArray<unknown> }, status: StatusCodes };
 
       beforeEach(async () => {
-        const ncrcGroupId = GID.fromValidatedString('62f9b0d0-8d43-4766-a52a-ce02af61bc6a');
         const review = (groupId: GID.GroupId, date: Date) => ({
           reviewId: arbitraryReviewId(),
           groupId,
@@ -83,9 +85,27 @@ describe('docmap-index', () => {
   });
 
   describe('when any docmap fails to generate', () => {
+    let response: { body: { articles: ReadonlyArray<unknown> }, status: StatusCodes };
+
+    beforeEach(async () => {
+      const ports = {
+        getAllEvents: T.of([
+          groupEvaluatedArticle(ncrcGroupId, arbitraryDoi(), arbitraryReviewId()),
+        ]),
+        fetchReview: () => TE.left(DE.unavailable),
+        findReviewsForArticleDoi: () => TE.left(DE.unavailable),
+        findVersionsForArticleDoi: () => TO.none,
+        getGroup: () => TO.none,
+        fetchArticle: () => TE.left(DE.unavailable),
+      };
+      response = await docmapIndex(ports)({})();
+    });
+
     it.todo('returns a body containing an error object');
 
-    it.todo('returns a 500 status code');
+    it.skip('returns a 500 status code', () => {
+      expect(response.status).toStrictEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    });
   });
 
   describe('when the query parameters are invalid', () => {
