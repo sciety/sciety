d9aa1fa6babc3371a31a0702dca3cb388f0eaa82
Giorgio Sironi <g.sironi@elifesciences.org>
2021-10-04 10:42:32 +0100

    Remove unnecessary explicit coupling to allDocmapDois #1099

--- src/docmaps/docmap/generate-docmap.ts
+++ src/docmaps/docmap/generate-docmap.ts
@@ -3,20 +3,24 @@ import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import * as TO from 'fp-ts/TaskOption';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
 import { docmap, Ports as DocmapPorts } from './docmap';
-import { DomainEvent } from '../../domain-events';
+import { DomainEvent, isGroupEvaluatedArticleEvent } from '../../domain-events';
 import { DoiFromString } from '../../types/codecs/DoiFromString';
 import * as DE from '../../types/data-error';
 import { Doi } from '../../types/doi';
 import * as GID from '../../types/group-id';
-import { allDocmapDois } from '../all-docmap-dois';
 
 type GetAllEvents = T.Task<ReadonlyArray<DomainEvent>>;
 
-const isInIndex = (getAllEvents: GetAllEvents) => (doi: Doi) => pipe(
+const ncrcGroupId = GID.fromValidatedString('62f9b0d0-8d43-4766-a52a-ce02af61bc6a');
+
+const isEvaluatedByNcrc = (getAllEvents: GetAllEvents) => (doi: Doi) => pipe(
   getAllEvents,
-  T.map(allDocmapDois),
+  T.map(flow(
+    RA.filter(isGroupEvaluatedArticleEvent),
+    RA.filter(({ groupId }) => ncrcGroupId === groupId),
+  )),
   T.map(RA.findFirst(({ articleId }) => articleId.value === doi.value)),
   TO.map(({ articleId }) => articleId),
   TE.fromTaskOption(() => DE.notFound),
@@ -30,14 +34,11 @@ export const generateDocmap = (
   ports: Ports,
 ) => (
   input: unknown,
-): TE.TaskEither<DE.DataError, Record<string, unknown>> => {
-  const ncrcGroupId = GID.fromValidatedString('62f9b0d0-8d43-4766-a52a-ce02af61bc6a');
-  return pipe(
-    input,
-    DoiFromString.decode,
-    E.mapLeft(() => DE.notFound),
-    TE.fromEither,
-    TE.chain(isInIndex(ports.getAllEvents)),
-    TE.chain(docmap(ports, ncrcGroupId)),
-  );
-};
+): TE.TaskEither<DE.DataError, Record<string, unknown>> => pipe(
+  input,
+  DoiFromString.decode,
+  E.mapLeft(() => DE.notFound),
+  TE.fromEither,
+  TE.chain(isEvaluatedByNcrc(ports.getAllEvents)),
+  TE.chain(docmap(ports, ncrcGroupId)),
+);
