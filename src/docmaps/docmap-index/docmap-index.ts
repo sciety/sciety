51c2d8b2e816f4fba36de0e6fb9babee27ee2d07
Kevin Rutherford <kevin@rutherford-software.com>
2021-10-11 15:32:46 +0100

    Name some error cases #1137

--- src/docmaps/docmap-index/docmap-index.ts
+++ src/docmaps/docmap-index/docmap-index.ts
@@ -3,6 +3,7 @@ import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { flow } from 'fp-ts/function';
 import { StatusCodes } from 'http-status-codes';
+import { Errors } from 'io-ts';
 import * as PR from 'io-ts/PathReporter';
 import { generateDocmapDois, Ports as GenerateDocmapDoisPorts, paramsCodec } from './generate-docmap-dois';
 import { docmap, Ports as DocmapPorts } from '../docmap/docmap';
@@ -19,29 +20,31 @@ type DocmapIndex = (ports: Ports) => (query: unknown) => T.Task<{
   status: StatusCodes,
 }>;
 
+const avoidRateLimitingWithDummyValues = (ports: Ports): Ports => ({
+  ...ports,
+  fetchReview: () => TE.right({
+    url: new URL(`https://example.com/source-url-of-evaluation-${Math.random()}`),
+  }),
+});
+
+const toBadRequestResponse = (errors: Errors) => ({
+  body: { error: PR.failure(errors).join('\n') },
+  status: StatusCodes.BAD_REQUEST,
+});
+
+const toInternalServerErrorResponse = () => ({
+  body: { error: 'Internal server error while generating Docmaps' },
+  status: StatusCodes.INTERNAL_SERVER_ERROR,
+});
+
 export const docmapIndex: DocmapIndex = (ports) => flow(
   paramsCodec.decode,
   TE.fromEither,
-  TE.mapLeft(
-    (errors) => ({
-      body: { error: PR.failure(errors).join('\n') },
-      status: StatusCodes.BAD_REQUEST,
-    }),
-  ),
+  TE.mapLeft(toBadRequestResponse),
   TE.chainW(generateDocmapDois(ports)),
   TE.chainW(flow(
-    TE.traverseArray(docmap({
-      ...ports,
-      fetchReview: () => TE.right({
-        url: new URL(`https://example.com/source-url-of-evaluation-${Math.random()}`),
-      }),
-    })),
-    TE.mapLeft(
-      () => ({
-        body: { error: 'Internal server error while generating Docmaps' },
-        status: StatusCodes.INTERNAL_SERVER_ERROR,
-      }),
-    ),
+    TE.traverseArray(docmap(avoidRateLimitingWithDummyValues(ports))),
+    TE.mapLeft(toInternalServerErrorResponse),
   )),
   TE.map((docmaps) => ({
     body: { articles: docmaps },
