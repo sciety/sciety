2c146e28397aae66b7bdba3f8240faf26040a93f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-10-11 15:19:14 +0100

    add rapid reviews to the docmap index #1137

--- src/docmaps/docmap-index/generate-docmap-dois.ts
+++ src/docmaps/docmap-index/generate-docmap-dois.ts
@@ -49,8 +49,9 @@ const filterByUpdatedAfter = (
 );
 
 const ncrcGroupId = GID.fromValidatedString('62f9b0d0-8d43-4766-a52a-ce02af61bc6a');
+const rapidReviewsGroupId = GID.fromValidatedString('5142a5bc-6b18-42b1-9a8d-7342d7d17e94');
 
-const supportedGroups = [ncrcGroupId];
+const supportedGroups = [ncrcGroupId, rapidReviewsGroupId];
 
 export const generateDocmapDois = (
   ports: Ports,
