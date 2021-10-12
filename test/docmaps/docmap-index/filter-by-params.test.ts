e4b097e4594798249860216420f73c4b4401fab0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-10-12 11:58:05 +0100

    remove beforeEach in setups that dont involve async #1137

--- test/docmaps/docmap-index/filter-by-params.test.ts
+++ test/docmaps/docmap-index/filter-by-params.test.ts
@@ -1,12 +1,11 @@
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
-import { DocmapIndexEntryModel } from '../../../src/docmaps/docmap-index/docmap-index-entry-models';
+import { StatusCodes } from 'http-status-codes';
 import { filterByParams } from '../../../src/docmaps/docmap-index/filter-by-params';
 import { arbitraryDate } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 import { arbitraryDoi } from '../../types/doi.helper';
 import { arbitraryGroupId } from '../../types/group-id.helper';
-import { StatusCodes } from "http-status-codes";
 
 describe('filter-by-params', () => {
   describe('when no params are given', () => {
@@ -23,15 +22,11 @@ describe('filter-by-params', () => {
       },
     ];
 
-    let result: ReadonlyArray<DocmapIndexEntryModel>;
-
-    beforeEach(() => {
-      result = pipe(
-        input,
-        filterByParams(''),
-        E.getOrElseW(shouldNotBeCalled),
-      );
-    });
+    const result = pipe(
+      input,
+      filterByParams(''),
+      E.getOrElseW(shouldNotBeCalled),
+    );
 
     it('returns unmodified input', () => {
       expect(result).toStrictEqual(input);
@@ -63,15 +58,11 @@ describe('filter-by-params', () => {
       },
     ];
 
-    let result: ReadonlyArray<DocmapIndexEntryModel>;
-
-    beforeEach(() => {
-      result = pipe(
-        input,
-        filterByParams(`group=${requestedGroupId}`),
-        E.getOrElseW(shouldNotBeCalled),
-      );
-    });
+    const result = pipe(
+      input,
+      filterByParams(`group=${requestedGroupId}`),
+      E.getOrElseW(shouldNotBeCalled),
+    );
 
     it.skip('only returns entries by that group', () => {
       expect(result).toStrictEqual([
