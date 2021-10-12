5a364e1a59f62431732fd99fcd5e54869f98d8e2
Will Byrne <w.byrne@elifesciences.org>
2021-10-12 14:26:47 +0100

    consistent test layout and naming #1137

--- test/docmaps/docmap-index/identify-all-possible-index-entries.test.ts
+++ test/docmaps/docmap-index/identify-all-possible-index-entries.test.ts
@@ -8,19 +8,19 @@ describe('identify-all-possible-index-entries', () => {
   const supportedGroupIds = [arbitraryGroupId(), arbitraryGroupId()];
 
   describe('when there are evaluated events by a supported group', () => {
+    const articleId1 = arbitraryDoi();
+    const articleId2 = arbitraryDoi();
+    const earlierDate = new Date('1990');
+    const laterDate = new Date('2000');
+    const events = [
+      groupEvaluatedArticle(supportedGroupIds[0], articleId1, arbitraryReviewId(), earlierDate),
+      groupEvaluatedArticle(supportedGroupIds[0], articleId2, arbitraryReviewId(), laterDate),
+    ];
+
+    const result = identifyAllPossibleIndexEntries(supportedGroupIds)(events);
+
     it('returns a list of all the evaluated index entry models', () => {
-      const articleId1 = arbitraryDoi();
-      const articleId2 = arbitraryDoi();
-      const earlierDate = new Date('1990');
-      const laterDate = new Date('2000');
-      const events = [
-        groupEvaluatedArticle(supportedGroupIds[0], articleId1, arbitraryReviewId(), earlierDate),
-        groupEvaluatedArticle(supportedGroupIds[0], articleId2, arbitraryReviewId(), laterDate),
-      ];
-
-      const dois = identifyAllPossibleIndexEntries(supportedGroupIds)(events);
-
-      expect(dois).toStrictEqual([
+      expect(result).toStrictEqual([
         {
           articleId: articleId2,
           groupId: supportedGroupIds[0],
@@ -62,19 +62,19 @@ describe('identify-all-possible-index-entries', () => {
   });
 
   describe('when there are evaluated events by both supported and unsupported groups', () => {
+    const articleId1 = arbitraryDoi();
+    const articleId2 = arbitraryDoi();
+    const events = [
+      groupEvaluatedArticle(supportedGroupIds[0], articleId1, arbitraryReviewId()),
+      groupEvaluatedArticle(supportedGroupIds[1], articleId2, arbitraryReviewId()),
+      groupEvaluatedArticle(arbitraryGroupId(), arbitraryDoi(), arbitraryReviewId()),
+    ];
+
+    const result = identifyAllPossibleIndexEntries(supportedGroupIds)(events);
+
     it('excludes articles evaluated by the unsupported group', () => {
-      const articleId1 = arbitraryDoi();
-      const articleId2 = arbitraryDoi();
-      const events = [
-        groupEvaluatedArticle(supportedGroupIds[0], articleId1, arbitraryReviewId()),
-        groupEvaluatedArticle(supportedGroupIds[1], articleId2, arbitraryReviewId()),
-        groupEvaluatedArticle(arbitraryGroupId(), arbitraryDoi(), arbitraryReviewId()),
-      ];
-
-      const dois = identifyAllPossibleIndexEntries(supportedGroupIds)(events);
-
-      expect(dois).toHaveLength(2);
-      expect(dois).toStrictEqual(expect.arrayContaining([
+      expect(result).toHaveLength(2);
+      expect(result).toStrictEqual(expect.arrayContaining([
         expect.objectContaining({
           groupId: supportedGroupIds[0],
           articleId: articleId1,
