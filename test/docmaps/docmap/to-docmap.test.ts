f40dc62764bbaebead5b7a7738234df4e3cb302e
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-10-14 20:56:40 +0100

    reimplement test for single evaluation #1137

--- test/docmaps/docmap/to-docmap.test.ts
+++ test/docmaps/docmap/to-docmap.test.ts
@@ -1,60 +1,15 @@
 import { URL } from 'url';
-import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
-import * as TE from 'fp-ts/TaskEither';
-import * as TO from 'fp-ts/TaskOption';
 import { pipe } from 'fp-ts/function';
 import * as S from 'fp-ts/string';
-import { docmap } from '../../../src/docmaps/docmap/docmap';
-import { Docmap } from '../../../src/docmaps/docmap/docmap-type';
-import { FindVersionsForArticleDoi } from '../../../src/docmaps/docmap/generate-docmap-view-model';
 import { toDocmap } from '../../../src/docmaps/docmap/to-docmap';
-import { GroupId } from '../../../src/types/group-id';
-import { ReviewId } from '../../../src/types/review-id';
 import { arbitraryDate, arbitraryUri } from '../../helpers';
-import { shouldNotBeCalled } from '../../should-not-be-called';
-import { arbitraryArticleServer } from '../../types/article-server.helper';
 import { arbitraryDoi } from '../../types/doi.helper';
-import { arbitraryGroupId } from '../../types/group-id.helper';
 import { arbitraryGroup } from '../../types/group.helper';
 import { arbitraryReviewId } from '../../types/review-id.helper';
 
-const indexedGroupId = arbitraryGroupId();
 const articleId = arbitraryDoi();
-
-const review = (groupId: GroupId, date: Date) => ({
-  reviewId: arbitraryReviewId(),
-  groupId,
-  occurredAt: date,
-});
-
-const defaultPorts = {
-  fetchReview: (id: ReviewId) => TE.right({ url: new URL(`https://reviews.example.com/${id}`) }),
-  findReviewsForArticleDoi: () => TE.right([review(indexedGroupId, arbitraryDate())]),
-  findVersionsForArticleDoi: (): ReturnType<FindVersionsForArticleDoi> => TO.some([
-    {
-      source: new URL(arbitraryUri()),
-      occurredAt: arbitraryDate(),
-      version: 1,
-    },
-  ]),
-  getGroup: () => TE.right({
-    ...arbitraryGroup(),
-    id: indexedGroupId,
-  }),
-  fetchArticle: () => TE.right({ server: arbitraryArticleServer() }),
-};
-
-const expectOutputs = (ex: Record<string, unknown>) => E.right(expect.objectContaining({
-  steps: expect.objectContaining({
-    '_:b0': expect.objectContaining({
-      actions: [expect.objectContaining({
-        outputs: [expect.objectContaining(ex)],
-      })],
-    }),
-  }),
-}));
 
 describe('to-docmap', () => {
   describe('docmap meta data', () => {
@@ -252,128 +207,71 @@ describe('to-docmap', () => {
 
   describe('when there is a single evaluation by the selected group', () => {
     describe('in the first step', () => {
-      it('assertions are empty', async () => {
-        const result = await docmap(defaultPorts)({ articleId, groupId: indexedGroupId })();
+      const occurredAt = new Date('1900');
+      const reviewId = arbitraryReviewId();
+      const sourceUrl = new URL(`https://reviews.example.com/${reviewId}`);
+      const firstStep = '_:b0';
 
-        expect(result).toStrictEqual(E.right(expect.objectContaining({
-          steps: expect.objectContaining({
-            '_:b0': expect.objectContaining({
-              assertions: [],
-            }),
-          }),
-        })));
+      const result = toDocmap({
+        articleId,
+        group: arbitraryGroup(),
+        inputPublishedDate: O.none,
+        evaluations: [
+          {
+            sourceUrl,
+            reviewId,
+            occurredAt,
+          },
+        ],
+      });
+
+      it('assertions are empty', async () => {
+        expect(result.steps[firstStep].assertions).toStrictEqual([]);
       });
 
       describe('the inputs', () => {
         it('include the uri and doi', async () => {
-          const result = await docmap(defaultPorts)({ articleId, groupId: indexedGroupId })();
-
-          expect(result).toStrictEqual(E.right(expect.objectContaining({
-            steps: expect.objectContaining({
-              '_:b0': expect.objectContaining({
-                inputs: [expect.objectContaining(
-                  {
-                    doi: articleId.value,
-                    url: expect.stringContaining(articleId.value),
-                  },
-                )],
-              }),
-            }),
-          })));
+          expect(result.steps[firstStep].inputs).toStrictEqual([
+            expect.objectContaining(
+              {
+                doi: articleId.value,
+                url: expect.stringContaining(articleId.value),
+              },
+            )]);
         });
       });
 
       describe('the only action', () => {
         describe('the only participant', () => {
           it('is anonymous', async () => {
-            const result = await docmap(defaultPorts)({ articleId, groupId: indexedGroupId })();
-
-            expect(result).toStrictEqual(E.right(expect.objectContaining({
-              steps: expect.objectContaining({
-                '_:b0': expect.objectContaining({
-                  actions: [expect.objectContaining({
-                    participants: [
-                      { actor: { name: 'anonymous', type: 'person' }, role: 'peer-reviewer' },
-                    ],
-                  })],
-                }),
-              }),
-            })));
+            expect(result.steps[firstStep].actions[0].participants[0]).toStrictEqual(
+              { actor: { name: 'anonymous', type: 'person' }, role: 'peer-reviewer' },
+            );
           });
         });
 
         describe('the only output', () => {
           it('is always of type review-article', async () => {
-            const result = await docmap(defaultPorts)({ articleId, groupId: indexedGroupId })();
-
-            expect(result).toStrictEqual(expectOutputs({
-              type: 'review-article',
-            }));
+            expect(result.steps[firstStep].actions[0].outputs[0].type).toStrictEqual('review-article');
           });
 
           it('includes the published date of the evaluation', async () => {
-            const evaluationDate = arbitraryDate();
-            const ports = {
-              ...defaultPorts,
-              findReviewsForArticleDoi: () => TE.right([review(indexedGroupId, evaluationDate)]),
-            };
-
-            const result = await docmap(ports)({ articleId, groupId: indexedGroupId })();
-
-            expect(result).toStrictEqual(expectOutputs({
-              published: evaluationDate,
-            }));
+            expect(result.steps[firstStep].actions[0].outputs[0].published).toStrictEqual(occurredAt);
           });
 
           it('includes the url to the original evaluation source', async () => {
-            const evaluationId = arbitraryReviewId();
-            const sourceUrl = arbitraryUri();
-            const ports = {
-              ...defaultPorts,
-              findReviewsForArticleDoi: () => TE.right(
-                [
-                  {
-                    reviewId: evaluationId,
-                    groupId: indexedGroupId,
-                    occurredAt: arbitraryDate(),
-                  },
-                ],
-              ),
-              fetchReview: () => TE.right({ url: new URL(sourceUrl) }),
-            };
-            const result = await docmap(ports)({ articleId, groupId: indexedGroupId })();
-
-            expect(result).toStrictEqual(expectOutputs({
-              content: expect.arrayContaining([{
-                type: 'web-page',
-                url: sourceUrl,
-              }]),
-            }));
+            expect(result.steps[firstStep].actions[0].outputs[0].content).toStrictEqual(expect.arrayContaining([{
+              type: 'web-page',
+              url: sourceUrl.toString(),
+            }]));
           });
+        });
 
-          it('includes the url to the evaluation on sciety', async () => {
-            const evaluationId = arbitraryReviewId();
-            const ports = {
-              ...defaultPorts,
-              findReviewsForArticleDoi: () => TE.right(
-                [
-                  {
-                    reviewId: evaluationId,
-                    groupId: indexedGroupId,
-                    occurredAt: arbitraryDate(),
-                  },
-                ],
-              ),
-            };
-            const result = await docmap(ports)({ articleId, groupId: indexedGroupId })();
-
-            expect(result).toStrictEqual(expectOutputs({
-              content: expect.arrayContaining([{
-                type: 'web-page',
-                url: `https://sciety.org/articles/activity/${articleId.value}#${evaluationId}`,
-              }]),
-            }));
-          });
+        it('includes the url to the evaluation on sciety', async () => {
+          expect(result.steps[firstStep].actions[0].outputs[0].content).toStrictEqual(expect.arrayContaining([{
+            type: 'web-page',
+            url: `https://sciety.org/articles/activity/${articleId.value}#${reviewId}`,
+          }]));
         });
       });
     });
