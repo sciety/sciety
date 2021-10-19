95bf078fc989cea72bf1c903fb76adf1e3d3f688
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-10-19 11:18:33 +0100

    add PreLights and PeerJ to Docmap index #1133

--- src/docmaps/docmap-index/docmap-index.ts
+++ src/docmaps/docmap-index/docmap-index.ts
@@ -34,10 +34,18 @@ const avoidRateLimitingWithDummyValues = (ports: Ports): Ports => ({
 });
 
 const ncrcGroupId = GID.fromValidatedString('62f9b0d0-8d43-4766-a52a-ce02af61bc6a');
-const rapidReviewsGroupId = GID.fromValidatedString('5142a5bc-6b18-42b1-9a8d-7342d7d17e94');
+const peerJGroupId = GID.fromValidatedString('53ed5364-a016-11ea-bb37-0242ac130002');
+const preLightsGroupId = GID.fromValidatedString('f97bd177-5cb6-4296-8573-078318755bf2');
 const preReviewGroupId = GID.fromValidatedString('10360d97-bf52-4aef-b2fa-2f60d319edd7');
+const rapidReviewsGroupId = GID.fromValidatedString('5142a5bc-6b18-42b1-9a8d-7342d7d17e94');
 
-const supportedGroups = [ncrcGroupId, rapidReviewsGroupId, preReviewGroupId];
+const supportedGroups = [
+  ncrcGroupId,
+  peerJGroupId,
+  preLightsGroupId,
+  preReviewGroupId,
+  rapidReviewsGroupId,
+];
 
 export const docmapIndex: DocmapIndex = (ports) => (query) => pipe(
   ports.getAllEvents,
