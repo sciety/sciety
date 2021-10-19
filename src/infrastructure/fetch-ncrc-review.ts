5bb6e4599d57b1b14b0500fee8b9c4b6e8f13bca
David Moulton <d.moulton@elifesciences.org>
2021-10-19 14:52:42 +0100

    Use data from sheet rather than 2 individual calls #1179

--- src/infrastructure/fetch-ncrc-review.ts
+++ src/infrastructure/fetch-ncrc-review.ts
@@ -1,12 +1,9 @@
 import * as E from 'fp-ts/Either';
-import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
-import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import * as TE from 'fp-ts/TaskEither';
 import { flow, pipe } from 'fp-ts/function';
 import { google, sheets_v4 } from 'googleapis';
 import * as t from 'io-ts';
-import * as tt from 'io-ts-types';
 import * as PR from 'io-ts/PathReporter';
 import { constructNcrcReview } from './construct-ncrc-review';
 import { EvaluationFetcher } from './fetch-review';
@@ -31,35 +28,7 @@ type TupleFn = <TCodecs extends readonly [t.Mixed, ...Array<t.Mixed>]>(
 }>;
 const tuple: TupleFn = t.tuple as never;
 
-const columnType = t.tuple([tt.readonlyNonEmptyArray(t.string)]); // TODO use a uuid
-
-const rowType = t.tuple([tuple([
-  t.unknown, // A uuid
-  t.unknown, // B title_journal
-  t.string, // C Title
-  t.unknown, // D Topic
-  t.unknown, // E First Author
-  t.unknown, // F Date Published
-  t.unknown, // G link
-  t.string, // H Our Take
-  t.string, // I value_added
-  t.string, // J study_population_setting
-  t.string, // K main_findings
-  t.string, // L study_strength
-  t.string, // M limitations
-  t.unknown, // N (hidden)
-  t.unknown, // O journal
-  t.unknown, // P cross_post
-  t.unknown, // Q edit_finished
-  t.unknown, // R reviewer
-  t.unknown, // S edit_date
-  t.unknown, // T final_take_wordcount
-  t.unknown, // U compendium_feature
-  t.string, // V Study_Design
-  t.unknown, // W Subtopic_Tag
-])]);
-
-const sheetType = t.array(tuple([
+const ncrcSheet = t.array(tuple([
   t.string, // A uuid
   t.unknown, // B title_journal
   t.string, // C Title
@@ -118,51 +87,11 @@ const querySheet = (logger: Logger) => <A>(
   );
 };
 
-const getRowNumber = (logger: Logger) => (key: string) => pipe(
-  querySheet(logger)({
-    spreadsheetId: '1RJ_Neh1wwG6X0SkYZHjD-AEC9ykgAcya_8UCVNoE3SA',
-    range: 'Sheet1!A:A', // TODO don't select the header
-    majorDimension: 'COLUMNS',
-  }, columnType),
-  TE.chainEitherKW(flow(
-    RNEA.head,
-    RA.findIndex((uuid) => uuid === key),
-    O.map((n) => n + 1),
-    O.altW(() => {
-      logger('error', 'Cannot find NcrcId in NCRC sheet', {
-        ncrcId: key,
-      });
-      return O.none;
-    }),
-    E.fromOption(() => DE.notFound),
-  )),
-);
-
-const getNcrcReview = (logger: Logger) => flow(
-  (rowNumber: number) => querySheet(logger)({
-    spreadsheetId: '1RJ_Neh1wwG6X0SkYZHjD-AEC9ykgAcya_8UCVNoE3SA',
-    range: `Sheet1!A${rowNumber}:AF${rowNumber}`,
-  }, rowType),
-  TE.map(flow(
-    RNEA.head,
-    (row) => ({
-      title: row[2],
-      ourTake: row[7],
-      studyDesign: row[21],
-      studyPopulationSetting: row[9],
-      mainFindings: row[10],
-      studyStrength: row[11],
-      limitations: row[12],
-      valueAdded: row[8],
-    }),
-  )),
-);
-
 const getSheet = (logger: Logger) => flow(
   () => querySheet(logger)({
     spreadsheetId: '1RJ_Neh1wwG6X0SkYZHjD-AEC9ykgAcya_8UCVNoE3SA',
     range: 'Sheet1!A:AF',
-  }, sheetType),
+  }, ncrcSheet),
   TE.map(
     RA.map((row) => ({
       uuid: row[0],
@@ -180,7 +109,9 @@ const getSheet = (logger: Logger) => flow(
 
 export const fetchNcrcReview = (logger: Logger): EvaluationFetcher => (evaluationUuid: string) => pipe(
   getSheet(logger)(),
-  TE.chain(() => getRowNumber(logger)(evaluationUuid)),
-  TE.chainW(getNcrcReview(logger)),
+  TE.chainEitherKW(flow(
+    RA.findFirst((row) => row.uuid === evaluationUuid),
+    E.fromOption(() => DE.notFound),
+  )),
   TE.map(constructNcrcReview),
 );
