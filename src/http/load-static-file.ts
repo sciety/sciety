8e97c9a07a0b9b2b8555c58ffa12d50c04588899
Kevin Rutherford <kevin@rutherford-software.com>
2021-10-22 14:41:06 +0100

    Log missing static files as warnings #1190

--- src/http/load-static-file.ts
+++ src/http/load-static-file.ts
@@ -2,26 +2,33 @@ import path from 'path';
 import { Middleware } from '@koa/router';
 import * as O from 'fp-ts/Option';
 import { StatusCodes } from 'http-status-codes';
-import { HttpError } from 'koa';
 import send from 'koa-send';
 import { renderErrorPage } from './render-error-page';
+import { Logger } from '../infrastructure';
 import { standardPageLayout } from '../shared-components/standard-page-layout';
 import { toHtmlFragment } from '../types/html-fragment';
 
-type Logger = (level: 'error', message: string, payload: Record<string, unknown>) => void;
+type KoaSendError = {
+  status: number,
+};
+
+const isKoaSendError = (variableToCheck: unknown): variableToCheck is KoaSendError => (
+  (variableToCheck as KoaSendError).status !== undefined
+);
 
 export const loadStaticFile = (logger: Logger): Middleware => async (context) => {
+  let pageMessage = 'Something went wrong, please try again.';
   const file = context.params.file.replace('editorial-communities', 'groups');
   const staticFolder = path.resolve(__dirname, '../../static');
   try {
     await send(context, file, { root: staticFolder });
   } catch (error: unknown) {
-    logger('error', 'Static file could not be read', { error });
-    let pageMessage = 'Something went wrong, please try again.';
-    if (error instanceof HttpError && error.status === 404) {
+    if (isKoaSendError(error) && error.status === 404) {
+      logger('warn', 'Static file not found', { error });
       pageMessage = 'File not found';
       context.response.status = 404;
     } else {
+      logger('error', 'Static file could not be read', { error });
       context.response.status = StatusCodes.INTERNAL_SERVER_ERROR;
     }
     context.response.body = standardPageLayout(O.none)({
