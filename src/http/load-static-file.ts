962f7a3f8c89bd7d4c32e7373245b1128fcbf3d3
Giorgio Sironi <g.sironi@elifesciences.org>
2021-10-22 14:12:07 +0100

    Inline catch-static-file-errors middleware #1190

--- src/http/load-static-file.ts
+++ src/http/load-static-file.ts
@@ -1,9 +1,32 @@
 import path from 'path';
 import { Middleware } from '@koa/router';
+import * as O from 'fp-ts/Option';
+import { StatusCodes } from 'http-status-codes';
+import { HttpError } from 'koa';
 import send from 'koa-send';
+import { renderErrorPage } from './render-error-page';
+import { standardPageLayout } from '../shared-components/standard-page-layout';
+import { toHtmlFragment } from '../types/html-fragment';
 
-export const loadStaticFile: Middleware = async (context, next) => {
+type Logger = (level: 'error', message: string, payload: Record<string, unknown>) => void;
+
+export const loadStaticFile = (logger: Logger): Middleware => async (context) => {
   const file = context.params.file.replace('editorial-communities', 'groups');
-  await send(context, file, { root: path.resolve(__dirname, '../../static') });
-  await next();
+  const staticFolder = path.resolve(__dirname, '../../static');
+  try {
+    await send(context, file, { root: staticFolder });
+  } catch (error: unknown) {
+    logger('error', 'Static file could not be read', { error });
+    let pageMessage = 'Something went wrong, please try again.';
+    if (error instanceof HttpError && error.status === 404) {
+      pageMessage = 'File not found';
+      context.response.status = 404;
+    } else {
+      context.response.status = StatusCodes.INTERNAL_SERVER_ERROR;
+    }
+    context.response.body = standardPageLayout(O.none)({
+      title: 'Error',
+      content: renderErrorPage(toHtmlFragment(pageMessage)),
+    });
+  }
 };
