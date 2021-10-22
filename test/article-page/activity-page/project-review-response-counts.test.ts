1b145f0285c1df354c2a2ce3025061a1e7d38e29
Kevin Rutherford <kevin@rutherford-software.com>
2021-10-22 08:28:49 +0100

    Remove some unnecessary temporary variables #1188

--- test/article-page/activity-page/project-review-response-counts.test.ts
+++ test/article-page/activity-page/project-review-response-counts.test.ts
@@ -8,11 +8,11 @@ import {
 import { arbitraryReviewId } from '../../types/review-id.helper';
 import { arbitraryUserId } from '../../types/user-id.helper';
 
+const reviewId = arbitraryReviewId();
+
 describe('project-review-response-counts', () => {
   describe('given no events', () => {
     it('returns 0 `helpful` and 0 `not helpful`', async () => {
-      const reviewId = arbitraryReviewId();
-
       const projected = await projectReviewResponseCounts(async () => [])(reviewId)();
 
       expect(projected).toStrictEqual({ helpfulCount: 0, notHelpfulCount: 0 });
@@ -21,9 +21,7 @@ describe('project-review-response-counts', () => {
 
   describe('given a user responded to a different review', () => {
     it('returns 0 `helpful` and 0 `not helpful`', async () => {
-      const reviewId = arbitraryReviewId();
       const differentReviewId = arbitraryReviewId();
-
       const projected = await projectReviewResponseCounts(async () => [
         userFoundReviewHelpful(arbitraryUserId(), differentReviewId),
       ])(reviewId)();
@@ -34,15 +32,10 @@ describe('project-review-response-counts', () => {
 
   describe('given N different users responded helpful events', () => {
     it('returns N `helpful` and 0 `not helpful`', async () => {
-      const reviewId = arbitraryReviewId();
-      const userA = arbitraryUserId();
-      const userB = arbitraryUserId();
-      const userC = arbitraryUserId();
-
       const projected = await projectReviewResponseCounts(async () => [
-        userFoundReviewHelpful(userA, reviewId),
-        userFoundReviewHelpful(userB, reviewId),
-        userFoundReviewHelpful(userC, reviewId),
+        userFoundReviewHelpful(arbitraryUserId(), reviewId),
+        userFoundReviewHelpful(arbitraryUserId(), reviewId),
+        userFoundReviewHelpful(arbitraryUserId(), reviewId),
       ])(reviewId)();
 
       expect(projected).toStrictEqual({ helpfulCount: 3, notHelpfulCount: 0 });
@@ -51,9 +44,7 @@ describe('project-review-response-counts', () => {
 
   describe('given a single user responded helpful and revoked the helpful response', () => {
     it('returns 0 `helpful` and 0 `not helpful`', async () => {
-      const reviewId = arbitraryReviewId();
       const userId = arbitraryUserId();
-
       const projected = await projectReviewResponseCounts(async () => [
         userFoundReviewHelpful(userId, reviewId),
         userRevokedFindingReviewHelpful(userId, reviewId),
@@ -65,15 +56,10 @@ describe('project-review-response-counts', () => {
 
   describe('given N different users responded not helpful events', () => {
     it('returns 0 `helpful` and N `not helpful`', async () => {
-      const reviewId = arbitraryReviewId();
-      const userA = arbitraryUserId();
-      const userB = arbitraryUserId();
-      const userC = arbitraryUserId();
-
       const projected = await projectReviewResponseCounts(async () => [
-        userFoundReviewNotHelpful(userA, reviewId),
-        userFoundReviewNotHelpful(userB, reviewId),
-        userFoundReviewNotHelpful(userC, reviewId),
+        userFoundReviewNotHelpful(arbitraryUserId(), reviewId),
+        userFoundReviewNotHelpful(arbitraryUserId(), reviewId),
+        userFoundReviewNotHelpful(arbitraryUserId(), reviewId),
       ])(reviewId)();
 
       expect(projected).toStrictEqual({ helpfulCount: 0, notHelpfulCount: 3 });
@@ -82,9 +68,7 @@ describe('project-review-response-counts', () => {
 
   describe('given a single user responded: helpful, revoke helpful, not helpful, revoke not helpful', () => {
     it('returns 0 `helpful` and 0 `not helpful`', async () => {
-      const reviewId = arbitraryReviewId();
       const userId = arbitraryUserId();
-
       const projected = await projectReviewResponseCounts(async () => [
         userFoundReviewHelpful(userId, reviewId),
         userRevokedFindingReviewHelpful(userId, reviewId),
