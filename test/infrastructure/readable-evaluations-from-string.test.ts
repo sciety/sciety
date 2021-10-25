d1172575c207aa8ffd839669340c09df29b0fa4c
Giorgio Sironi <g.sironi@elifesciences.org>
2021-10-25 13:39:47 +0100

    Write one-direction happy path test #1186

--- test/infrastructure/readable-evaluations-from-string.test.ts
+++ test/infrastructure/readable-evaluations-from-string.test.ts
@@ -1,6 +1,19 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import { readableEvaluationsFromString } from '../../src/infrastructure/readable-evaluations-from-string';
+import { shouldNotBeCalled } from '../should-not-be-called';
+
 describe('readable-evalutions-from-string', () => {
   describe('when all lines are valid json that match domain model', () => {
-    it.todo('returns all evaluations');
+    const fileContents = '{"date":"2018-09-22T00:00:00.000Z","articleDoi":"10.1101/318121","evaluationLocator":"doi:10.24072/pci.paleo.100001"}\n{"date":"2019-10-15T00:00:00.000Z","articleDoi":"10.1101/352609","evaluationLocator":"doi:10.24072/pci.paleo.100002"}';
+    const result = pipe(
+      readableEvaluationsFromString(fileContents),
+      E.getOrElseW(shouldNotBeCalled),
+    );
+
+    it('returns all evaluations', () => {
+      expect(result).toHaveLength(2);
+    });
   });
 
   describe('when a line is empty', () => {
