3977c2e17d8848d808e48e02156d9fff5e8fc701
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-10-25 12:01:21 +0100

    write test list for write side #1186

---
+++ test/infrastructure/readable-evaluations-from-string.test.ts
@@ -0,0 +1,17 @@
+describe('readable-evalutions-from-string', () => {
+  describe('when all lines are valid json that match domain model', () => {
+    it.todo('returns all evaluations');
+  });
+
+  describe('when a line is empty', () => {
+    it.todo('does not return an evaluation for that line');
+  });
+
+  describe('when a line is invalid json', () => {
+    it.todo('returns a left');
+  });
+
+  describe('when a line does not match the domain model', () => {
+    it.todo('returns a left');
+  });
+});
