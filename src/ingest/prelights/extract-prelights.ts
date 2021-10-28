2c6448930d1770c9c4b818f29d2dffdeefb78afa
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-10-28 11:45:57 +0100

    extract and name share code for creating skipped item #1186

--- src/ingest/prelights/extract-prelights.ts
+++ src/ingest/prelights/extract-prelights.ts
@@ -11,21 +11,23 @@ export type Prelight = {
   author: string,
 };
 
+const skipWithReason = (item: Prelight, reason: string) => ({ item: item.guid, reason });
+
 export const extractPrelights = (items: ReadonlyArray<Prelight>): FeedData => pipe(
   items,
   RA.map(flow(
     E.right,
     E.filterOrElse(
       (i) => i.category.includes('highlight'),
-      (i) => ({ item: i.guid, reason: `Category was '${i.category}` }),
+      (i) => skipWithReason(i, `Category was '${i.category}`),
     ),
     E.filterOrElse(
       (i) => i.preprintDoi !== '',
-      (i) => ({ item: i.guid, reason: 'preprintDoi field is empty' }),
+      (i) => skipWithReason(i, 'preprintDoi field is empty'),
     ),
     E.filterOrElse(
       (i) => i.preprintDoi.startsWith('10.1101/'),
-      (i) => ({ item: i.guid, reason: `${i.preprintDoi} is not a biorxiv or medrxiv DOI` }),
+      (i) => skipWithReason(i, `${i.preprintDoi} is not a biorxiv or medrxiv DOI`),
     ),
     E.map(({
       pubDate, preprintDoi, guid, author,
