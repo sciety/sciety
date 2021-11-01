3339404fcafd25490a4d44c17d93ba7b712cf149
Kevin Rutherford <kevin@rutherford-software.com>
2021-11-01 14:41:12 +0000

    Match radius of drop shadow to that of the card #1211

--- src/sciety-feed-page/cards/sciety-feed-card.ts
+++ src/sciety-feed-page/cards/sciety-feed-card.ts
@@ -27,8 +27,8 @@ export const scietyFeedCard = (viewModel: ScietyFeedCard): HtmlFragment => pipe(
     : ''),
   toHtmlFragment,
   (details) => `
-    <article class="sciety-feed-card">
-      <a href="${viewModel.linkUrl}" class="sciety-feed-card__link">
+    <article>
+      <a href="${viewModel.linkUrl}" class="sciety-feed-card sciety-feed-card__link">
         <div class="sciety-feed-card__event_title">
           <img class="sciety-feed-card__avatar" src="${viewModel.avatarUrl}" alt="">
           <h2 class="sciety-feed-card__event_title_text">${viewModel.titleText}</h2>
