94c682799ebc5668b36c580bcc822233a62a663e
Kevin Rutherford <kevin@rutherford-software.com>
2021-11-02 10:29:43 +0000

    Make all list cards clickable. Closes #1212

--- src/shared-components/list-card/render-list-card.ts
+++ src/shared-components/list-card/render-list-card.ts
@@ -18,12 +18,12 @@ const lastUpdated = O.fold(
 
 export const renderListCard = (viewModel: ListCardViewModel): HtmlFragment => toHtmlFragment(`
   <article class="list-card">
-    <h3 class="list-card__title">
-      <a href="${viewModel.href}" class="list-card__link">${viewModel.title}</a>
-    </h3>
-    <p>${viewModel.description}</p>
-    <div class="list-card__meta">
-      <span class="visually-hidden">${viewModel.articleCountLabel} </span><span>${viewModel.articleCount} article${viewModel.articleCount === 1 ? '' : 's'}</span>${lastUpdated(viewModel.lastUpdated)}
-    </div>
+    <a href="${viewModel.href}" class="list-card__link">
+      <h3 class="list-card__title">${viewModel.title}</h3>
+      <p>${viewModel.description}</p>
+      <div class="list-card__meta">
+        <span class="visually-hidden">${viewModel.articleCountLabel} </span><span>${viewModel.articleCount} article${viewModel.articleCount === 1 ? '' : 's'}</span>${lastUpdated(viewModel.lastUpdated)}
+      </div>
+    </a>
   </article>
 `);
