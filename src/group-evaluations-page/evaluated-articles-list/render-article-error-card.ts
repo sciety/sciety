0eaa0cf847e21288f10abc3f64ff51b8e60959c4
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-11-05 15:00:27 +0000

    give error message breathing space #1234

--- src/group-evaluations-page/evaluated-articles-list/render-article-error-card.ts
+++ src/group-evaluations-page/evaluated-articles-list/render-article-error-card.ts
@@ -30,7 +30,9 @@ export const renderArticleErrorCard = (viewModel: ArticleErrorCardViewModel): Ht
   toHtmlFragment(`
     <article class="article-card">
       <a class="article-card__link" href="${viewModel.href}">
-        Can't currently display this article.
+        <p class="article-card__error_message">
+          Can't currently display this article.
+        </p>
         <footer class="article-card__footer">
           <div class="article-card__meta">
             <span class="visually-hidden">This article has </span>${renderEvaluationCount(viewModel.evaluationCount)}${renderArticleLatestActivityDate(viewModel.latestActivityDate)}
