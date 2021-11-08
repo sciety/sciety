857c19268080c270bd17d3e8f8111ec752668a9e
Giorgio Sironi <g.sironi@elifesciences.org>
2021-11-08 15:32:27 +0000

    Separate launch and polling of CloudWatch export #1238

--- scripts/launch-cloud-watch-export.sh
+++ scripts/launch-cloud-watch-export.sh
@@ -18,20 +18,3 @@ cloudWatchTaskId=$(aws logs create-export-task \
  	--destination-prefix "ingress-nginx-controller-$from-$to" \
      | jq -r '.taskId')
 echo "Started export with id: $cloudWatchTaskId"
-
-timeout="1800"
-
-timeout --foreground "$timeout" bash << EOT
-    while true; do
-      taskStatus=$(aws logs describe-export-tasks --task-id=$cloudWatchTaskId \
-        | jq -r '.exportTasks[0].status.code')
-      echo \$taskStatus
-      if [[ \$taskStatus == COMPLETED ]]; then
-       break
-      fi
-      if [[ \$taskStatus == FAILED ]]; then
-       exit 1
-      fi
-      sleep 30
-    done
-EOT
