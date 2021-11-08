bbe6e399ed126b60c3de1de374a8d0c87b463308
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-11-08 14:53:02 +0000

    create export task from script #1238

--- scripts/export-new-logs.sh
+++ scripts/export-new-logs.sh
@@ -3,4 +3,16 @@
 set -euo pipefail
 
 from=$(bq query --format=prettyjson --use_legacy_sql=false 'SELECT DATE_ADD(DATE(MAX(request_timestamp)), INTERVAL 1 DAY) AS day_from FROM `elife-data-pipeline.de_proto.v_sciety_ingress`' | jq -r '.[0].day_from')
-echo "Exporting from $from"
+to=$(date '+%Y-%m-%d')
+
+echo "Exporting from $from 00:00 to $to 00:00"
+from_epoch_ms="$(date --date "$from 00:00:00 +0000" +%s)000"
+to_epoch_ms="$(date --date "$to 00:00:00 +0000" +%s)000"
+
+aws logs create-export-task \
+	--log-group-name '/aws/containerinsights/libero-eks--franklin/application' \
+ 	--log-stream-name-prefix 'ingress-nginx-controller-' \
+ 	--from  $from_epoch_ms \
+ 	--to  $to_epoch_ms \
+ 	--destination 'sciety-data-extractions' \
+ 	--destination-prefix "ingress-nginx-controller-$from-$to"
