b960645ec2ca71b375a4530cf464af66e1e2262a
Kevin Rutherford <kevin@rutherford-software.com>
2021-11-08 15:09:10 +0000

    Inline a function #1236

--- src/group-evaluations-page/evaluated-articles-list/component.ts
+++ src/group-evaluations-page/evaluated-articles-list/component.ts
@@ -1,17 +1,28 @@
+import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
 import { evaluatedArticles } from './evaluated-articles';
-import { evaluatedArticlesList, Ports as EvaluatedArticlesListPorts } from './evaluated-articles-list';
+import { toPageOfCards, Ports as ToPageOfCardsPorts } from './to-page-of-cards';
 import { DomainEvent } from '../../domain-events';
+import { paginate } from '../../shared-components/paginate';
 import * as DE from '../../types/data-error';
 import { Group } from '../../types/group';
 import { HtmlFragment } from '../../types/html-fragment';
 
-export type Ports = EvaluatedArticlesListPorts & {
+export type Ports = ToPageOfCardsPorts & {
   getAllEvents: T.Task<ReadonlyArray<DomainEvent>>,
 };
+
+const emptyPage = (pageNumber: number) => E.right({
+  items: [],
+  nextPage: O.none,
+  pageNumber,
+  numberOfOriginalItems: 0,
+  numberOfPages: 0,
+});
 
 export const component = (
   ports: Ports,
@@ -21,5 +32,14 @@ export const component = (
   ports.getAllEvents,
   T.map(evaluatedArticles(group.id)),
   TE.rightTask,
-  TE.chainW(evaluatedArticlesList(ports, group, O.getOrElse(() => 1)(page), 20)),
+  TE.chainW(
+    flow(
+      RA.match(
+        () => emptyPage(O.getOrElse(() => 1)(page)),
+        paginate(20, O.getOrElse(() => 1)(page)),
+      ),
+      TE.fromEither,
+    ),
+  ),
+  TE.chainTaskK(toPageOfCards(ports, group)),
 );
