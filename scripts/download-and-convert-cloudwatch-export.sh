32404d50c1b8b1862cc07a9f6d3f70513a192b83
Daniel Ecer <de-code@users.noreply.github.com>
2021-11-09 14:22:03 +0000

    renamed variable name to cloudWatchTaskId

--- scripts/download-and-convert-cloudwatch-export.sh
+++ scripts/download-and-convert-cloudwatch-export.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 from=$(bq query --format=prettyjson --use_legacy_sql=false 'SELECT DATE_ADD(DATE(MAX(request_timestamp)), INTERVAL 1 DAY) AS day_from FROM `elife-data-pipeline.de_proto.v_sciety_ingress`' | jq -r '.[0].day_from')
 to=$(date '+%Y-%m-%d')
 
-taskId=$1
+cloudWatchTaskId=$1
 
-logsUri="s3://sciety-data-extractions/ingress-nginx-controller-$from-$to/$taskId"
+logsUri="s3://sciety-data-extractions/ingress-nginx-controller-$from-$to/$cloudWatchTaskId"
 echo $logsUri
 
 rm -rf logs
--- scripts/update-datastudio.sh
+++ scripts/update-datastudio.sh
@@ -4,10 +4,10 @@ set -euox pipefail
 
 scripts=$(dirname $0)
 
-CLOUDWATCH_EXPORT_ID=$($scripts/launch-cloud-watch-export.sh)
+cloudWatchTaskId=$($scripts/launch-cloud-watch-export.sh)
 
-echo "CLOUDWATCH_EXPORT_ID: ${CLOUDWATCH_EXPORT_ID}"
+echo "cloudWatchTaskId: ${cloudWatchTaskId}"
 
-$scripts/wait-for-cloud-watch-task-completion.sh $CLOUDWATCH_EXPORT_ID
+$scripts/wait-for-cloud-watch-task-completion.sh $cloudWatchTaskId
 
-$scripts/download-and-convert-cloudwatch-export.sh $CLOUDWATCH_EXPORT_ID
+$scripts/download-and-convert-cloudwatch-export.sh $cloudWatchTaskId
