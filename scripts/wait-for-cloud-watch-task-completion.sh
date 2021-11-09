c397e8ddfbf71bc24d1decbb529c269d248c0a67
Giorgio Sironi <g.sironi@elifesciences.org>
2021-11-09 14:03:22 +0000

    Fix infinite wait on task status #1238

--- scripts/wait-for-cloud-watch-task-completion.sh
+++ scripts/wait-for-cloud-watch-task-completion.sh
@@ -4,19 +4,15 @@ set -euo pipefail
 
 cloudWatchTaskId=$1
 
-timeout="1800"
-
-timeout --foreground "$timeout" bash << EOT
-    while true; do
-      taskStatus=$(aws logs describe-export-tasks --task-id=$cloudWatchTaskId \
-        | jq -r '.exportTasks[0].status.code')
-      echo \$taskStatus
-      if [[ \$taskStatus == COMPLETED ]]; then
-       break
-      fi
-      if [[ \$taskStatus == FAILED ]]; then
-       exit 1
-      fi
-      sleep 30
-    done
-EOT
+while true; do
+  taskStatus=$(aws logs describe-export-tasks --task-id=$cloudWatchTaskId \
+    | jq -r '.exportTasks[0].status.code')
+  echo $taskStatus
+  if [[ $taskStatus == COMPLETED ]]; then
+   break
+  fi
+  if [[ $taskStatus == FAILED ]]; then
+   exit 1
+  fi
+  sleep 30
+done
