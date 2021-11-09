22c5ff5df8bad9526ceb1e13a9609e4ebd4a513e
Giorgio Sironi <g.sironi@elifesciences.org>
2021-11-09 13:08:37 +0000

    Make date commands portable #1238

--- scripts/launch-cloud-watch-export.sh
+++ scripts/launch-cloud-watch-export.sh
@@ -2,12 +2,20 @@
 
 set -euo pipefail
 
+if ! command -v gdate &> /dev/null
+then
+    DATE=date
+else
+    DATE=gdate
+fi
+
+
 from=$(bq query --format=prettyjson --use_legacy_sql=false 'SELECT DATE_ADD(DATE(MAX(request_timestamp)), INTERVAL 1 DAY) AS day_from FROM `elife-data-pipeline.de_proto.v_sciety_ingress`' | jq -r '.[0].day_from')
-to=$(date '+%Y-%m-%d')
+to=$($DATE '+%Y-%m-%d')
 
 echo "Exporting from $from 00:00 to $to 00:00"
-from_epoch_ms="$(date --date "$from 00:00:00 +0000" +%s)000"
-to_epoch_ms="$(date --date "$to 00:00:00 +0000" +%s)000"
+from_epoch_ms="$($DATE --date "$from 00:00:00 +0000" +%s)000"
+to_epoch_ms="$($DATE --date "$to 00:00:00 +0000" +%s)000"
 
 cloudWatchTaskId=$(aws logs create-export-task \
 	--log-group-name '/aws/containerinsights/libero-eks--franklin/application' \
