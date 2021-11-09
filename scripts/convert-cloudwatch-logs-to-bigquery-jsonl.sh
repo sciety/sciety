6d7d366ef12c725782858379b82926ddeb2fed33
David Moulton <d.moulton@elifesciences.org>
2021-11-09 11:51:20 +0000

    Make script portable to Mac #1238

--- scripts/convert-cloudwatch-logs-to-bigquery-jsonl.sh
+++ scripts/convert-cloudwatch-logs-to-bigquery-jsonl.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 cat $1 \
-    | grep -o -P '{.*' \
+    | sed -e 's/[^ ]* //' \
     | jq --compact-output 'del(.kubernetes) | del(.docker) | del(.app_upstream_response_time)' \
     > $1.jsonl
