728b8a2dfeaf2b0fdd927cfc77790379f74ed46e
Daniel Ecer <de-code@users.noreply.github.com>
2021-11-09 14:15:31 +0000

    calling download and convert cloudwatch export

--- scripts/update-datastudio.sh
+++ scripts/update-datastudio.sh
@@ -9,3 +9,5 @@ CLOUDWATCH_EXPORT_ID=$($scripts/launch-cloud-watch-export.sh)
 echo "CLOUDWATCH_EXPORT_ID: ${CLOUDWATCH_EXPORT_ID}"
 
 $scripts/wait-for-cloud-watch-task-completion.sh $CLOUDWATCH_EXPORT_ID
+
+$scripts/download-and-convert-cloudwatch-export.sh $CLOUDWATCH_EXPORT_ID
