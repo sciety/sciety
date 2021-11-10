fe757439b7e498b429dc6e40a13f95df7d119088
David Moulton <d.moulton@elifesciences.org>
2021-11-10 08:52:05 +0000

    Reword tests for precision #1243

--- test/user-account/create-account-if-necessary.test.ts
+++ test/user-account/create-account-if-necessary.test.ts
@@ -1,15 +1,15 @@
 describe('create-account-if-necessary', () => {
   describe('when the user has already created an account', () => {
-    describe('because there is an account creation event', () => {
+    describe('because there is a UserCreatedAccount event', () => {
       it.todo('raises no events');
     });
 
-    describe('because there are breadcrumb events and there is no account creation event', () => {
+    describe('because there are already events initiated by this user, but no UserCreatedAccount event', () => {
       it.todo('raises no events');
     });
   });
 
   describe('when the user has not already created an account', () => {
-    it.todo('raises an account creation event');
+    it.todo('raises a UserCreatedAccount event');
   });
 });
