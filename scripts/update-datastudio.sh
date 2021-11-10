8e0b5a71aaface47dd6ef2008e2311dec074cd13
Giorgio Sironi <g.sironi@elifesciences.org>
2021-11-10 08:56:57 +0000

    Print link of Data Studio dashboard at end of update

--- scripts/update-datastudio.sh
+++ scripts/update-datastudio.sh
@@ -11,3 +11,5 @@ echo "cloudWatchTaskId: ${cloudWatchTaskId}"
 $scripts/wait-for-cloud-watch-task-completion.sh $cloudWatchTaskId
 
 $scripts/download-and-convert-from-cloudwatch-and-upload-to-bigquery.sh $cloudWatchTaskId
+
+echo "See https://datastudio.google.com/reporting/bc7fa747-9d10-4272-836d-f40425b93c95"
