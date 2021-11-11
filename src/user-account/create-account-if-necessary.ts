ae059f1e62fbc54eeb8595660c60f3fe8bd007e7
Giorgio Sironi <g.sironi@elifesciences.org>
2021-11-11 11:41:36 +0000

    Spike an implementation #1243

--- src/user-account/create-account-if-necessary.ts
+++ src/user-account/create-account-if-necessary.ts
@@ -1,5 +1,10 @@
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
-import { DomainEvent, RuntimeGeneratedEvent } from '../domain-events';
+import { pipe } from 'fp-ts/function';
+import {
+  DomainEvent, isUserCreatedAccountEvent, isUserFollowedEditorialCommunityEvent, RuntimeGeneratedEvent,
+} from '../domain-events';
+import { userCreatedAccount } from '../domain-events/user-created-account-event';
 import { UserId } from '../types/user-id';
 
 type GetAllEvents = T.Task<ReadonlyArray<DomainEvent>>;
@@ -21,6 +26,19 @@ export type UserAccount = {
 
 type CreateAccountIfNecessary = (ports: Ports) => (userAccount: UserAccount) => T.Task<void>;
 
-export const createAccountIfNecessary: CreateAccountIfNecessary = ({ commitEvents }) => () => (
-  commitEvents([])
+const isInterestingEvent = (userId: UserId) => (event: DomainEvent) => (
+  isUserFollowedEditorialCommunityEvent(event) || isUserCreatedAccountEvent(event)
+) && event.userId === userId;
+
+export const createAccountIfNecessary: CreateAccountIfNecessary = ({
+  getAllEvents,
+  commitEvents,
+}) => (
+  user,
+) => pipe(
+  getAllEvents,
+  T.map(RA.filter(isInterestingEvent(user.id))),
+  T.map(RA.isNonEmpty),
+  T.map((hasAlreadyLoggedInBefore) => (hasAlreadyLoggedInBefore ? [] : [userCreatedAccount(user.id, user.handle, '', '')])),
+  T.chain(commitEvents),
 );
