219a141df65d53bbc3f77772b57ebd404cce7da5
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-11-18 11:36:09 +0000

    hardcode header for featured articles list #1263

--- src/ncrc-featured-articles-page/page.ts
+++ src/ncrc-featured-articles-page/page.ts
@@ -1,8 +1,23 @@
+import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
-import { toHtmlFragment } from '../types/html-fragment';
+import { pipe } from 'fp-ts/function';
+import { renderComponent } from '../list-page/header/render-component';
 import { Page } from '../types/page';
 
+const header = {
+  name: 'Featured articles',
+  description: 'Articles that have been identified as high-impact by NCRC editors.',
+  ownerName: 'NCRC',
+  ownerHref: '/groups/ncrc',
+  ownerAvatarPath: '/static/groups/ncrc--62f9b0d0-8d43-4766-a52a-ce02af61bc6a.jpg',
+  articleCount: 0,
+  lastUpdated: O.some(new Date('2021-11-18T11:33:00Z')),
+};
+
 export const page = (): TE.TaskEither<never, Page> => TE.right({
-  title: 'Articles featured by NCRC',
-  content: toHtmlFragment(''),
+  title: 'Featured articles',
+  content: pipe(
+    header,
+    renderComponent,
+  ),
 });
