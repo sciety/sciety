a5f4bc83ae539432fcf8a67d323af1944b48e532
Kevin Rutherford <kevin@rutherford-software.com>
2021-11-18 11:47:05 +0000

    Add a section around the articles list #1263

--- src/ncrc-featured-articles-page/page.ts
+++ src/ncrc-featured-articles-page/page.ts
@@ -2,6 +2,7 @@ import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { renderComponent } from '../list-page/header/render-component';
+import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 
 const header = {
@@ -14,10 +15,28 @@ const header = {
   lastUpdated: O.some(new Date('2021-11-18T11:33:00Z')),
 };
 
+type Components = {
+  header: HtmlFragment,
+  articlesList: HtmlFragment,
+};
+
+const render = (components: Components) => toHtmlFragment(`
+  ${components.header}
+  <section class="evaluated-articles">
+    ${components.articlesList}
+  </section>
+`);
+
 export const page = (): TE.TaskEither<never, Page> => TE.right({
   title: 'Featured articles',
   content: pipe(
-    header,
-    renderComponent,
+    {
+      header: pipe(
+        header,
+        renderComponent,
+      ),
+      articlesList: toHtmlFragment(''),
+    },
+    render,
   ),
 });
