c38001be871bc04e6e771e04119194e5f55a458e
Giorgio Sironi <g.sironi@elifesciences.org>
2021-11-18 14:00:38 +0000

    Add first hardcoded article to list #1263

--- src/ncrc-featured-articles-page/articles-list/articlesList.ts
+++ src/ncrc-featured-articles-page/articles-list/articlesList.ts
@@ -5,6 +5,7 @@ import { toPageOfCards, Ports as ToPageOfCardsPorts } from './to-page-of-cards';
 import { noEvaluatedArticlesMessage } from '../../list-page/evaluated-articles-list/static-messages';
 import { paginate } from '../../shared-components/paginate';
 import * as DE from '../../types/data-error';
+import { Doi } from '../../types/doi';
 import { HtmlFragment } from '../../types/html-fragment';
 
 export type Ports = ToPageOfCardsPorts;
@@ -14,7 +15,13 @@ export const articlesList = (
   listId: string,
   pageNumber: number,
 ): TE.TaskEither<DE.DataError, HtmlFragment> => pipe(
-  [],
+  [
+    {
+      doi: new Doi('10.1101/2021.05.20.21257512'),
+      latestActivityDate: new Date('2021-07-09'),
+      evaluationCount: 2,
+    },
+  ],
   TE.right,
   TE.chain(RA.match(
     () => TE.right(noEvaluatedArticlesMessage),
