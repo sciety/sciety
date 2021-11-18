17b9687fae8c1b5f9b5944fbc8c9b37d30e8c046
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-11-18 17:25:36 +0000

    split refinements from predicates in follows

--- src/infrastructure/follows.ts
+++ src/infrastructure/follows.ts
@@ -1,8 +1,15 @@
 import * as O from 'fp-ts/Option';
-import * as A from 'fp-ts/ReadonlyArray';
+import * as RA from 'fp-ts/ReadonlyArray';
+import * as Refinement from 'fp-ts/Refinement';
 import * as T from 'fp-ts/Task';
 import { flow, pipe } from 'fp-ts/function';
-import { DomainEvent, isUserFollowedEditorialCommunityEvent, isUserUnfollowedEditorialCommunityEvent } from '../domain-events';
+import {
+  DomainEvent,
+  isUserFollowedEditorialCommunityEvent,
+  isUserUnfollowedEditorialCommunityEvent,
+  UserFollowedEditorialCommunityEvent,
+  UserUnfollowedEditorialCommunityEvent,
+} from '../domain-events';
 import { GroupId } from '../types/group-id';
 import { UserId } from '../types/user-id';
 
@@ -10,22 +17,23 @@ export type Follows = (u: UserId, g: GroupId) => T.Task<boolean>;
 
 type GetAllEvents = T.Task<ReadonlyArray<DomainEvent>>;
 
-const isSignificantTo = (
+const involves = (
   userId: UserId,
   groupId: GroupId,
-) => (event: DomainEvent) => (
-  (isUserFollowedEditorialCommunityEvent(event)
-    && event.editorialCommunityId === groupId
-    && event.userId === userId)
-  || (isUserUnfollowedEditorialCommunityEvent(event)
-    && event.editorialCommunityId === groupId
-    && event.userId === userId)
+) => (
+  event: UserFollowedEditorialCommunityEvent | UserUnfollowedEditorialCommunityEvent,
+) => event.editorialCommunityId === groupId && event.userId === userId;
+
+const isEventAboutFollowingAGroup = pipe(
+  isUserFollowedEditorialCommunityEvent,
+  Refinement.or(isUserUnfollowedEditorialCommunityEvent),
 );
 
 export const follows = (getAllEvents: GetAllEvents): Follows => (userId, groupId) => pipe(
   getAllEvents,
   T.map(flow(
-    A.findLast(isSignificantTo(userId, groupId)),
+    RA.filter(isEventAboutFollowingAGroup),
+    RA.findLast(involves(userId, groupId)),
     O.filter(isUserFollowedEditorialCommunityEvent),
     O.isSome,
   )),
