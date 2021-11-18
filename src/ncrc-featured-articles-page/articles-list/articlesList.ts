5c6cc39f02da96fecfc3835508b79368ef53d5c3
Kevin Rutherford <kevin@rutherford-software.com>
2021-11-18 14:23:47 +0000

    Separate the list of articles from the article activity read model #1263

--- src/ncrc-featured-articles-page/articles-list/articlesList.ts
+++ src/ncrc-featured-articles-page/articles-list/articlesList.ts
@@ -4,11 +4,20 @@ import { flow, pipe } from 'fp-ts/function';
 import { toPageOfCards, Ports as ToPageOfCardsPorts } from './to-page-of-cards';
 import { noEvaluatedArticlesMessage } from '../../list-page/evaluated-articles-list/static-messages';
 import { paginate } from '../../shared-components/paginate';
+import { ArticleActivity } from '../../types/article-activity';
 import * as DE from '../../types/data-error';
 import { Doi } from '../../types/doi';
 import { HtmlFragment } from '../../types/html-fragment';
 
 export type Ports = ToPageOfCardsPorts;
+
+const allArticleActivity: Record<string, ArticleActivity> = {
+  '10.1101/2021.05.20.21257512': {
+    doi: new Doi('10.1101/2021.05.20.21257512'),
+    latestActivityDate: new Date('2021-07-09'),
+    evaluationCount: 2,
+  },
+};
 
 export const articlesList = (
   ports: Ports,
@@ -16,12 +25,9 @@ export const articlesList = (
   pageNumber: number,
 ): TE.TaskEither<DE.DataError, HtmlFragment> => pipe(
   [
-    {
-      doi: new Doi('10.1101/2021.05.20.21257512'),
-      latestActivityDate: new Date('2021-07-09'),
-      evaluationCount: 2,
-    },
+    new Doi('10.1101/2021.05.20.21257512'),
   ],
+  RA.map((doi) => allArticleActivity[doi.value]),
   TE.right,
   TE.chain(RA.match(
     () => TE.right(noEvaluatedArticlesMessage),
