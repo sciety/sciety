4490a57648c2fd0083a4bd5c7950c95e3c753f03
David Moulton <d.moulton@elifesciences.org>
2021-11-19 11:34:59 +0000

    Refactor for readability #1264

--- src/ingest/medrxiv-or-biorxiv-link-to-doi.ts
+++ src/ingest/medrxiv-or-biorxiv-link-to-doi.ts
@@ -1,4 +1,17 @@
-export const medrxivOrBiorxivLinkToDoi = (link: string): string => {
+import { pipe } from 'fp-ts/function';
+
+const stripTrailingDot = (s: string) => s.replace(/\.$/, '');
+
+const addMedrxivOrBiorxivPrefix = (s: string) => `10.1101/${s}`;
+
+const extractDoiSuffix = (link: string) => {
   const [, doiSuffix] = /.*\/([^/a-z]*).*$/.exec(link) ?? [];
-  return `10.1101/${doiSuffix.replace(/\.$/, '')}`;
+  return doiSuffix;
 };
+
+export const medrxivOrBiorxivLinkToDoi = (link: string): string => pipe(
+  link,
+  extractDoiSuffix,
+  stripTrailingDot,
+  addMedrxivOrBiorxivPrefix,
+);
