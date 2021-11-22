273ae012852a434f8a1e99b9273840c55d3816e6
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-11-22 11:40:20 +0000

    prepare rendering to take multiple cards #1277

--- src/group-page/lists/lists.ts
+++ src/group-page/lists/lists.ts
@@ -3,6 +3,7 @@ import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { DomainEvent } from '../../domain-events';
 import { renderListCard } from '../../shared-components/list-card/render-list-card';
+import { templateListItems } from '../../shared-components/list-items';
 import { allLists, Ports as GroupListPorts } from '../../shared-read-models/all-lists';
 import * as DE from '../../types/data-error';
 import { Group } from '../../types/group';
@@ -14,13 +15,18 @@ export type Ports = GroupListPorts & {
   getAllEvents: GetAllEvents,
 };
 
-const renderLists = (evaluatedArticlesListCard: HtmlFragment) => toHtmlFragment(`
-  <section class="group-page-lists">
-    <ul class="group-page-followers-list" role="list">
-      <li class="group-page-followers-list__item" role="listitem">${evaluatedArticlesListCard}</li>
-    </ul>
-  </section>
-`);
+const renderCards = (cards: ReadonlyArray<HtmlFragment>) => pipe(
+  cards,
+  (items) => templateListItems(items, 'group-page-followers-list__item'),
+  (listContent) => `
+    <section class="group-page-lists">
+      <ul class="group-page-followers-list" role="list">
+        ${listContent}
+      </ul>
+    </section>
+  `,
+  toHtmlFragment,
+);
 
 export const lists = (ports: Ports) => (group: Group): TE.TaskEither<DE.DataError, HtmlFragment> => pipe(
   ports.getAllEvents,
@@ -33,5 +39,6 @@ export const lists = (ports: Ports) => (group: Group): TE.TaskEither<DE.DataErro
     articleCountLabel: 'This group has evaluated',
   })),
   TE.map(renderListCard),
-  TE.map(renderLists),
+  TE.map((card) => [card]),
+  TE.map(renderCards),
 );
