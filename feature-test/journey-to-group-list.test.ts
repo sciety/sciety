a24ab45dc992ce45182888c6c7e7d91735bba907
David Moulton <d.moulton@elifesciences.org>
2021-11-22 14:57:54 +0000

    Add taiko test for group page list user journey #1278

---
+++ feature-test/journey-to-group-list.test.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import {
+  $, click, goto, openBrowser,
+} from 'taiko';
+import { screenshotTeardown } from './utilities';
+
+describe('journey-to-group-list', () => {
+  beforeEach(async () => {
+    dotenv.config();
+    await openBrowser();
+  });
+
+  afterEach(screenshotTeardown);
+
+  it('navigates to a group list page via that group\'s page', async () => {
+    await goto('localhost:8080/groups/ncrc');
+    await click('Featured articles');
+    const pageTitle = await $('h1').text();
+
+    expect(pageTitle).toContain('Featured articles');
+  });
+});
