6b9b93919018d6f8f8d4b53b33fac45e13a9189b
Giorgio Sironi <g.sironi@elifesciences.org>
2021-11-22 11:00:04 +0000

    Consolidate URL to DOI extraction for Hypothesis group ingestion #1264

--- src/ingest/fetch-reviews-from-hypothesis-group.ts
+++ src/ingest/fetch-reviews-from-hypothesis-group.ts
@@ -4,26 +4,25 @@ import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { Evaluation } from './evaluations';
 import { FetchData } from './fetch-data';
+import { medrxivOrBiorxivLinkToDoi } from './medrxiv-or-biorxiv-link-to-doi';
 import { daysAgo } from './time';
 import { FetchEvaluations, SkippedItem } from './update-all';
 import * as Hyp from '../third-parties/hypothesis';
 
 // ts-unused-exports:disable-next-line
-export const toEvaluation = (row: Hyp.Annotation): E.Either<SkippedItem, Evaluation> => {
-  const doiRegex = '(10\\.[0-9]{4,}(?:\\.[1-9][0-9]*)*/(?:[^%"#?\\s])+)';
-  // eslint-disable-next-line no-useless-escape
-  const matches = new RegExp(`https://www.(bio|med)rxiv.org/content/${doiRegex}v[0-9]+\.*$`).exec(row.uri);
-  if (matches === null) {
-    return E.left({ item: row.uri, reason: 'Cannot parse into a biorxiv DOI' });
-  }
-  const doi = matches[2];
-  return E.right({
-    date: new Date(row.created),
-    articleDoi: doi,
-    evaluationLocator: `hypothesis:${row.id}`,
-    authors: [],
-  });
-};
+export const toEvaluation = (row: Hyp.Annotation): E.Either<SkippedItem, Evaluation> => pipe(
+  row.uri,
+  medrxivOrBiorxivLinkToDoi,
+  E.bimap(
+    (reason) => ({ item: row.uri, reason }),
+    (articleDoi) => ({
+      date: new Date(row.created),
+      articleDoi,
+      evaluationLocator: `hypothesis:${row.id}`,
+      authors: [],
+    }),
+  ),
+);
 
 type Ports = {
   fetchData: FetchData,
