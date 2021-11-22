834d9c49c4602d7e850622168c2caea1a27771bd
Giorgio Sironi <g.sironi@elifesciences.org>
2021-11-22 11:55:00 +0000

    Add a new list card for a particular group #1277

--- src/group-page/lists/lists.ts
+++ src/group-page/lists/lists.ts
@@ -1,3 +1,5 @@
+import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
@@ -38,7 +40,20 @@ export const lists = (ports: Ports) => (group: Group): TE.TaskEither<DE.DataErro
     title: details.name,
     articleCountLabel: 'This group has evaluated',
   })),
-  TE.map(renderListCard),
-  TE.map((card) => [card]),
+  TE.map((cardViewModel) => [cardViewModel]),
+  TE.map((cardViewModels) => ((group.slug === 'ncrc')
+    ? [...cardViewModels, {
+      href: '/lists/cbd478fe-3ff7-4125-ac9f-c94ff52ae0f7',
+      title: 'Featured articles',
+      articleCountLabel: 'This list contains',
+      description: 'Articles that have been identified as high-impact by NCRC editors.',
+      ownerName: 'NCRC',
+      ownerHref: '/groups/ncrc',
+      ownerAvatarPath: '/static/groups/ncrc--62f9b0d0-8d43-4766-a52a-ce02af61bc6a.jpg',
+      articleCount: 1,
+      lastUpdated: O.some(new Date('2021-11-18T11:33:00Z')),
+    }]
+    : cardViewModels)),
+  TE.map(RA.map(renderListCard)),
   TE.map(renderCards),
 );
