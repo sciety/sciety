ec5bd2db24d9967219701829c3715c16ae9bca2f
Kevin Rutherford <kevin@rutherford-software.com>
2021-11-22 08:18:41 +0000

    Replace a use of getGroup by the read model version #1269

--- src/http/redirects/redirect-group-id-to-slug.ts
+++ src/http/redirects/redirect-group-id-to-slug.ts
@@ -1,17 +1,22 @@
+import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { StatusCodes } from 'http-status-codes';
 import { Middleware } from 'koa';
+import { DomainEvent } from '../../domain-events';
+import { getGroup } from '../../shared-read-models/all-groups';
 import * as DE from '../../types/data-error';
 import { GroupId } from '../../types/group-id';
 
 type Ports = {
+  getAllEvents: T.Task<ReadonlyArray<DomainEvent>>,
   getGroup: (groupId: GroupId) => TE.TaskEither<DE.DataError, { slug: string }>,
 };
 
 export const redirectGroupIdToSlug = (ports: Ports, path: string): Middleware => async (context, next) => {
   await pipe(
-    ports.getGroup(context.params.id as GroupId),
+    ports.getAllEvents,
+    T.map(getGroup(context.params.id as GroupId)),
     TE.match(
       () => {
         context.status = StatusCodes.NOT_FOUND;
