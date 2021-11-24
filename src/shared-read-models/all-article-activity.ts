07af018b3a8981a5b0b88f0a38a638d1a28690db
David Moulton <d.moulton@elifesciences.org>
2021-11-24 10:41:19 +0000

    Add more articles to NCRC high impact list #1281

--- src/shared-read-models/all-article-activity.ts
+++ src/shared-read-models/all-article-activity.ts
@@ -7,9 +7,30 @@ type AllArticleActivityReadModel = Record<string, ArticleActivity>;
 type AllArticleActivity = (events: ReadonlyArray<DomainEvent>) => AllArticleActivityReadModel;
 
 export const allArticleActivity: AllArticleActivity = () => ({
+  // NCRC High impact:
   '10.1101/2021.11.04.467308': {
     doi: new Doi('10.1101/2021.11.04.467308'),
     latestActivityDate: new Date('2021-11-19'),
+    evaluationCount: 2,
+  },
+  '10.1101/2021.09.15.21263613': {
+    doi: new Doi('10.1101/2021.09.15.21263613'),
+    latestActivityDate: new Date('2021-10-18'),
+    evaluationCount: 2,
+  },
+  '10.1101/2021.08.30.21262866': {
+    doi: new Doi('10.1101/2021.08.30.21262866'),
+    latestActivityDate: new Date('2021-09-20'),
+    evaluationCount: 2,
+  },
+  '10.1101/2021.07.31.21261387': {
+    doi: new Doi('10.1101/2021.07.31.21261387'),
+    latestActivityDate: new Date('2021-09-10'),
+    evaluationCount: 2,
+  },
+  '10.1101/2021.08.03.21261496': {
+    doi: new Doi('10.1101/2021.08.03.21261496'),
+    latestActivityDate: new Date('2021-08-23'),
     evaluationCount: 2,
   },
   '10.1101/2021.05.20.21257512': {
@@ -17,6 +38,27 @@ export const allArticleActivity: AllArticleActivity = () => ({
     latestActivityDate: new Date('2021-07-09'),
     evaluationCount: 2,
   },
+  '10.1101/2021.06.02.21258076': {
+    doi: new Doi('10.1101/2021.06.02.21258076'),
+    latestActivityDate: new Date('2021-06-18'),
+    evaluationCount: 2,
+  },
+  '10.1101/2021.05.13.21256639': {
+    doi: new Doi('10.1101/2021.05.13.21256639'),
+    latestActivityDate: new Date('2021-05-28'),
+    evaluationCount: 2,
+  },
+  '10.1101/2021.02.26.21252554': {
+    doi: new Doi('10.1101/2021.02.26.21252554'),
+    latestActivityDate: new Date('2021-04-07'),
+    evaluationCount: 2,
+  },
+  '10.1101/2021.02.27.21252597': {
+    doi: new Doi('10.1101/2021.02.27.21252597'),
+    latestActivityDate: new Date('2021-03-22'),
+    evaluationCount: 2,
+  },
+  // Biophysics Colab:
   '10.1101/2021.03.21.436299': {
     doi: new Doi('10.1101/2021.03.21.436299'),
     latestActivityDate: new Date('2021-09-27'),
