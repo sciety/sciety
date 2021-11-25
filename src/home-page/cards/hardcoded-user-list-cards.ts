7fa4d325a3eca4e50aa3248fbb627d8b5ab785ee
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-11-25 14:32:59 +0000

    avoid calling twitter for user avatar and handle #1285

--- src/home-page/cards/hardcoded-user-list-cards.ts
+++ src/home-page/cards/hardcoded-user-list-cards.ts
@@ -3,14 +3,20 @@ import { toUserId } from '../../types/user-id';
 export const card1 = {
   userId: toUserId('1449681648019255299'),
   description: 'Selected papers from the Global South curated by Stefano Vianello',
+  avatarUrl: 'https://pbs.twimg.com/profile_images/1449682082482032640/xTHORs-g_bigger.jpg',
+  handle: 'ZonaPellucida_',
 };
 
 export const card2 = {
   userId: toUserId('1412019815619911685'),
   description: 'See what researchers at Prachee Avasthi’s lab are reading to discover some interesting new work',
+  avatarUrl: 'https://pbs.twimg.com/profile_images/1417079202973638657/VrQKBTkw_bigger.jpg',
+  handle: 'AvasthiReading',
 };
 
 export const card3 = {
   userId: toUserId('1223116442549145601'),
   description: 'A list of papers on innate immunology curated by Ailís O’Carroll',
+  avatarUrl: 'https://pbs.twimg.com/profile_images/1223116710820974592/-b-9R-UX_bigger.jpg',
+  handle: 'AilisDr',
 };
