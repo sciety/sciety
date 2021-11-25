b2a3f808066497cf6daaf6c333b7285782f8b640
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-11-25 11:02:07 +0000

    write a test list for the article activity read model #1281

---
+++ test/shared-read-models/construct-all-article-activity-read-model.test.ts
@@ -0,0 +1,9 @@
+describe('construct-all-article-activity-read-model', () => {
+  describe('when an article has no evaluations', () => {
+    it.todo('article is not in the read model');
+  });
+
+  describe('when an article has one or more evaluations', () => {
+    it.todo('returns the activity for that article');
+  });
+});
