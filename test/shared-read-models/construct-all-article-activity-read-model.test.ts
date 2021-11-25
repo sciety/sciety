835473d8fab03c9ea0467b71a2105faf780e6788
Kevin Rutherford <kevin@rutherford-software.com>
2021-11-25 11:16:10 +0000

    Add a passing test for an article with no activity #1281

--- test/shared-read-models/construct-all-article-activity-read-model.test.ts
+++ test/shared-read-models/construct-all-article-activity-read-model.test.ts
@@ -1,6 +1,22 @@
+import * as O from 'fp-ts/Option';
+import * as R from 'fp-ts/Record';
+import { pipe } from 'fp-ts/function';
+import { constructAllArticleActivityReadModel } from '../../src/shared-read-models/construct-all-article-activity-read-model';
+import { arbitraryDoi } from '../types/doi.helper';
+
 describe('construct-all-article-activity-read-model', () => {
+  const articleId = arbitraryDoi();
+
   describe('when an article has no evaluations', () => {
-    it.todo('article is not in the read model');
+    const articleActivity = pipe(
+      [],
+      constructAllArticleActivityReadModel,
+      R.lookup(articleId.value),
+    );
+
+    it('article is not in the read model', () => {
+      expect(O.isNone(articleActivity)).toBe(true);
+    });
   });
 
   describe('when an article has one or more evaluations', () => {
