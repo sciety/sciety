680b585bc7a1d6f77fdf965c1942b873ea69c9ff
Kevin Rutherford <kevin@rutherford-software.com>
2021-11-26 14:39:35 +0000

    Build lists from events #1286

--- src/shared-read-models/lists/construct-lists-read-model.ts
+++ src/shared-read-models/lists/construct-lists-read-model.ts
@@ -1,9 +1,11 @@
 import * as M from 'fp-ts/Map';
+import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { createListFromEvaluationEvents } from './create-list-from-evaluation-events';
 import { List } from './list';
 import { DomainEvent, GroupEvaluatedArticleEvent } from '../../domain-events';
+import { ListCreatedEvent } from '../../domain-events/list-created-event';
 import { GroupId } from '../../types/group-id';
 
 type ReadModel = Map<GroupId, List>;
@@ -12,6 +14,13 @@ export const constructListsReadModel = (
   events: ReadonlyArray<DomainEvent>,
 ): ReadModel => pipe(
   events,
+  RA.filter((event): event is ListCreatedEvent => event.type === 'ListCreated'),
+  RA.map((event): List => ({
+    ...event,
+    articleCount: 0,
+    lastUpdated: O.some(event.date),
+  })),
+  () => events,
   RA.filter((event): event is GroupEvaluatedArticleEvent => event.type === 'GroupEvaluatedArticle'),
   RA.reduce(
     new Map<GroupId, Array<GroupEvaluatedArticleEvent>>(),
