65de2f6f2fc0ef5881a6a891a6b394678e6a1849
Kevin Rutherford <kevin@rutherford-software.com>
2021-11-26 11:11:23 +0000

    Fix lint oversight #1286

--- src/shared-read-models/lists/select-all-lists-owned-by.ts
+++ src/shared-read-models/lists/select-all-lists-owned-by.ts
@@ -8,7 +8,9 @@ import { List } from './list';
 import { DomainEvent } from '../../domain-events';
 import { GroupId } from '../../types/group-id';
 
-export const selectAllListsOwnedBy = (groupId: GroupId) => (events: ReadonlyArray<DomainEvent>): ReadonlyArray<List> => pipe(
+type SelectAllListsOwnedBy = (groupId: GroupId) => (events: ReadonlyArray<DomainEvent>) => ReadonlyArray<List>;
+
+export const selectAllListsOwnedBy: SelectAllListsOwnedBy = (groupId) => (events) => pipe(
   events,
   constructListsReadModel,
   RM.lookup(S.Eq)(groupId),
