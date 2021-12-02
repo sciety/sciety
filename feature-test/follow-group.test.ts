c12dd2f1573693e3c5f4d851715185c8e23baf99
Kevin Rutherford <kevin@rutherford-software.com>
2021-12-02 16:05:24 +0000

    Test follow with a group that is usually above the fold #1299

--- feature-test/follow-group.test.ts
+++ feature-test/follow-group.test.ts
@@ -20,7 +20,7 @@ describe('follow a group', () => {
     });
 
     it('adds the group to my profile page', async () => {
-      const groupToBeFollowed = 'Biophysics Colab';
+      const groupToBeFollowed = 'ScreenIT';
       await goto('localhost:8080/groups');
       await click(groupToBeFollowed);
       await click('Follow');
