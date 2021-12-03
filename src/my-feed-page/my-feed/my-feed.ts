e96ab4dbb4e9e199a5b81aea9a525ac415edd9fe
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-12-03 15:15:21 +0000

    show which page of total pages is being shown #1301

--- src/my-feed-page/my-feed/my-feed.ts
+++ src/my-feed-page/my-feed/my-feed.ts
@@ -72,7 +72,10 @@ const renderArticleCardList = (pageofItems: PageOfItems<unknown>) => flow(
   RNEA.map(renderArticleCard(O.none)),
   RNEA.map((card) => `<li class="my-feed__list_item">${card}</li>`),
   (cards) => `
-    <ul class="my-feed__list" role="list">${cards.join('')}</ul>
+    <p class="sciety-feed-page-numbers">
+      Showing page ${pageofItems.pageNumber} of ${pageofItems.numberOfPages}<span class="visually-hidden"> pages of articles that have been evaluated by group that you follow</span>
+    </p>
+    <ol class="my-feed__list" role="list">${cards.join('')}</ol>
     ${pipe(
     pageofItems.nextPage,
     O.fold(
