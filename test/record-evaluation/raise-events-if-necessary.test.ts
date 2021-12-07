fe1c750d596bb6072c483e8362a03dacc0fc11ee
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-12-07 15:10:27 +0000

    skipped test for raising events #1304

--- test/record-evaluation/raise-events-if-necessary.test.ts
+++ test/record-evaluation/raise-events-if-necessary.test.ts
@@ -7,18 +7,32 @@ import { arbitraryGroupId } from '../types/group-id.helper';
 import { arbitraryReviewId } from '../types/review-id.helper';
 
 describe('raise-events-if-necessary', () => {
+  const evaluationLocator = arbitraryReviewId();
+  const input = {
+    groupId: arbitraryGroupId(),
+    articleId: arbitraryDoi(),
+    evaluationLocator,
+    publishedAt: arbitraryDate(),
+  };
+
   describe('when the evaluation locator has NOT already been recorded', () => {
-    it.todo('returns an EvaluationRecorded event');
+    const events = pipe(
+      [],
+      raiseEventsIfNecessary(input),
+    );
+
+    it.skip('returns an EvaluationRecorded event', () => {
+      expect(events).toStrictEqual([expect.objectContaining({
+        type: 'EvaluationRecorded',
+        groupId: input.groupId,
+        articleId: input.articleId,
+        evaluationLocator: input.evaluationLocator,
+        publishedAt: input.publishedAt,
+      })]);
+    });
   });
 
   describe('when the evaluation locator has already been recorded', () => {
-    const evaluationLocator = arbitraryReviewId();
-    const input = {
-      groupId: arbitraryGroupId(),
-      articleId: arbitraryDoi(),
-      evaluationLocator,
-      publishedAt: arbitraryDate(),
-    };
     const events = pipe(
       [
         evaluationRecorded(arbitraryGroupId(), arbitraryDoi(), evaluationLocator),
