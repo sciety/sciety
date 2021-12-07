96aaa644032b03b3fa6c15683387a6ae4bad41bb
Kevin Rutherford <kevin@rutherford-software.com>
2021-12-07 10:37:56 +0000

    Sketch the testable steps in the command #1304

--- src/record-evaluation/index.ts
+++ src/record-evaluation/index.ts
@@ -1,5 +1,17 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+
+const validateInputShape = (): E.Either<unknown, unknown> => E.right('');
+
+const executeCommand = () => E.right([]);
+
+const commitEvents = () => {};
+
 type RecordEvaluation = (input: unknown) => void;
 
-export const recordEvaluation: RecordEvaluation = () => {
-
-};
+export const recordEvaluation: RecordEvaluation = (input) => pipe(
+  input,
+  validateInputShape,
+  E.chainW(executeCommand),
+  E.map(commitEvents),
+);
