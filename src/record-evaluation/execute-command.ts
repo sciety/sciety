685fcc1fe6511fa396710ad2e09397ddfd721f37
Giorgio Sironi <g.sironi@elifesciences.org>
2021-12-07 15:03:02 +0000

    Introduce validation of GroupId in command #1304

--- src/record-evaluation/execute-command.ts
+++ src/record-evaluation/execute-command.ts
@@ -3,13 +3,25 @@ import { pipe } from 'fp-ts/function';
 import { raiseEventsIfNecessary } from './raise-events-if-necessary';
 import { DomainEvent } from '../domain-events';
 import { RuntimeGeneratedEvent } from '../domain-events/runtime-generated-event';
+import { GroupId } from '../types/group-id';
 
-type ExecuteCommand = (validatedInput: unknown)
+export type Command = {
+  groupId: GroupId,
+};
+
+type ExecuteCommand = (command: Command)
 => (events: ReadonlyArray<DomainEvent>)
 => E.Either<unknown, ReadonlyArray<RuntimeGeneratedEvent>>;
 
-export const executeCommand: ExecuteCommand = (validatedInput) => (events) => pipe(
+type ConfirmGroupExists = (groupId: GroupId)
+=> (events: ReadonlyArray<DomainEvent>)
+=> E.Either<void, void>;
+
+const confirmGroupExists: ConfirmGroupExists = () => () => E.right(undefined);
+
+export const executeCommand: ExecuteCommand = (command) => (events) => pipe(
   events,
-  raiseEventsIfNecessary(validatedInput),
   E.right,
+  E.chainFirst(confirmGroupExists(command.groupId)),
+  E.map(raiseEventsIfNecessary(command)),
 );
--- src/record-evaluation/index.ts
+++ src/record-evaluation/index.ts
@@ -2,8 +2,9 @@ import * as E from 'fp-ts/Either';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import { executeCommand } from './execute-command';
+import { Command, executeCommand } from './execute-command';
 import { DomainEvent, RuntimeGeneratedEvent } from '../domain-events';
+import * as GID from '../types/group-id';
 
 type CommitEvents = (event: ReadonlyArray<RuntimeGeneratedEvent>) => T.Task<void>;
 
@@ -12,7 +13,9 @@ type Ports = {
   commitEvents: CommitEvents,
 };
 
-const validateInputShape = (): E.Either<unknown, unknown> => E.right('');
+const validateInputShape = (): E.Either<unknown, Command> => E.right({
+  groupId: GID.fromValidatedString(''),
+});
 
 type RecordEvaluation = (ports: Ports) => (input: unknown) => TE.TaskEither<unknown, void>;
 
@@ -20,9 +23,9 @@ export const recordEvaluation: RecordEvaluation = (ports) => (input) => pipe(
   input,
   validateInputShape,
   TE.fromEither,
-  TE.chainW((validatedInput) => pipe(
+  TE.chainW((command) => pipe(
     ports.getAllEvents,
-    T.map(executeCommand(validatedInput)),
+    T.map(executeCommand(command)),
   )),
   TE.chainTaskK(ports.commitEvents),
 );
