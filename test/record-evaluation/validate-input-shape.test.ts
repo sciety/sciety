c3995eb474e19d9a139c4d0839c4ded5bef31ab4
David Moulton <d.moulton@elifesciences.org>
2021-12-08 11:39:06 +0000

    Write skipped test for validate-input-shape codec #1304

--- test/record-evaluation/validate-input-shape.test.ts
+++ test/record-evaluation/validate-input-shape.test.ts
@@ -1,6 +1,33 @@
+import * as E from 'fp-ts/Either';
+import { validateInputShape } from '../../src/record-evaluation/validate-input-shape';
+import * as RI from '../../src/types/review-id';
+import { arbitraryDate } from '../helpers';
+import { arbitraryDoi } from '../types/doi.helper';
+import { arbitraryGroupId } from '../types/group-id.helper';
+import { arbitraryReviewId } from '../types/review-id.helper';
+
 describe('validate-input-shape', () => {
   describe('when the input is valid', () => {
-    it.todo('returns a Command');
+    it.skip('returns a Command', () => {
+      const groupId = arbitraryGroupId();
+      const publishedAt = arbitraryDate();
+      const evaluationLocator = arbitraryReviewId();
+      const articleId = arbitraryDoi();
+
+      const result = validateInputShape({
+        groupId: groupId.toString(),
+        publishedAt: publishedAt.toISOString(),
+        evaluationLocator: RI.serialize(evaluationLocator),
+        articleId: articleId.value,
+      });
+
+      expect(result).toStrictEqual(E.right({
+        groupId,
+        publishedAt,
+        evaluationLocator,
+        articleId,
+      }));
+    });
   });
 
   describe('when the input is invalid', () => {
