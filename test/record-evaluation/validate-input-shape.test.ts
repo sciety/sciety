72b565dde686dcf5d59e1b087f4c1991b4312c3b
Giorgio Sironi <g.sironi@elifesciences.org>
2021-12-08 11:29:45 +0000

    Add test list #1304

---
+++ test/record-evaluation/validate-input-shape.test.ts
@@ -0,0 +1,23 @@
+describe('validate-input-shape', () => {
+  describe('when the input is valid', () => {
+    it.todo('returns a Command');
+  });
+
+  describe('when the input is invalid', () => {
+    describe('because the groupId is invalid', () => {
+      it.todo('returns an error message');
+    });
+
+    describe('because the publishedAt is invalid', () => {
+      it.todo('returns an error message');
+    });
+
+    describe('because the evaluationLocator is invalid', () => {
+      it.todo('returns an error message');
+    });
+
+    describe('because the articleId is invalid', () => {
+      it.todo('returns an error message');
+    });
+  });
+});
