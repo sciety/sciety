9f765f4991b9b24dc84449542e7fec952493660b
David Moulton <d.moulton@elifesciences.org>
2021-12-14 11:54:53 +0000

    Handle missing preLights feed key during ingestion #1317

--- src/third-parties/prelights/fetch-prelights-evaluations.ts
+++ src/third-parties/prelights/fetch-prelights-evaluations.ts
@@ -1,3 +1,4 @@
+import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { extractPrelights } from './extract-prelights';
@@ -5,14 +6,20 @@ import { identifyCandidates } from './identify-candidates';
 import { FetchData } from '../../ingest/fetch-data';
 import { FetchEvaluations } from '../../ingest/update-all';
 
-const key = process.env.PRELIGHTS_FEED_KEY ?? '';
+const keyFromEnv = pipe(
+  process.env.PRELIGHTS_FEED_KEY,
+  E.fromNullable('No preLights feed key provided'),
+);
 
 type Ports = {
   fetchData: FetchData,
 };
 
 export const fetchPrelightsEvaluations = (): FetchEvaluations => (ports: Ports) => pipe(
-  ports.fetchData<string>(`https://prelights.biologists.com/feed/sciety/?key=${key}&hours=120`),
+  keyFromEnv,
+  TE.fromEither,
+  TE.map((key) => `https://prelights.biologists.com/feed/sciety/?key=${key}&hours=120`),
+  TE.chain((url) => ports.fetchData<string>(url)),
   TE.chainEitherK(identifyCandidates),
   TE.map(extractPrelights),
 );
