e1e916f8e7fe5f0069faff03ed85a35efb931dc9
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-12-15 15:57:52 +0000

    Fix link

--- src/home-page/calls-to-action.ts
+++ src/home-page/calls-to-action.ts
@@ -13,7 +13,7 @@ export const callsToAction = (user: O.Option<User>): HtmlFragment => toHtmlFragm
     <p class="home-page-call-to-action__text">Follow your first group, save interesting articles and keep up to date with the latest trends.</p>
     <div class="home-page-calls-to-action__buttons">
       ${loginButtonIfNotLoggedIn(user)}
-      <a class="home-page-calls-to-action__subscribe_button" href="https://blog.sciety.org/subscribe">Subscribe for updates</a>
+      <a class="home-page-calls-to-action__subscribe_button" href="/signup">Subscribe for updates</a>
     </div>
     <p class="home-page-calls-to-action__byline">Follow the journey through <a href="/blog">our blog</a>.</p>
   </section>
