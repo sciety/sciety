18bcee8fe19985ec600ad22aed283c0c06cc1295
Giorgio Sironi <g.sironi@elifesciences.org>
2021-12-17 15:23:26 +0000

    Introduce script to update dates of an entire data file #1326

---
+++ scripts/update-recorded-at-dates.ts
@@ -0,0 +1,49 @@
+import { execSync } from 'child_process';
+import fs from 'fs';
+import { promisify } from 'util';
+import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { decodeEvaluationsFromJsonl } from '../src/infrastructure/evaluations-as-jsonl';
+
+const filename = './data/reviews/7a9e97d1-c1fe-4ac2-9572-4ecfe28f9f84.jsonl';
+
+const readFromFile = promisify(fs.readFile);
+
+const readTextFile = (path: string) => TE.tryCatch(
+  async () => readFromFile(path, 'utf-8'),
+  E.toError,
+);
+
+const runScript = (evaluationLocator: string): TE.TaskEither<unknown, Date> => pipe(
+  TE.tryCatch(
+    async () => execSync(`./scripts/first-commit-date-for-evaluation-locator.sh ${evaluationLocator}`),
+    String,
+  ),
+  TE.map((buffer) => buffer.toString('utf-8')),
+  TE.map((string) => string.trim()),
+  TE.map((string) => new Date(string)),
+);
+
+const updateDate = (partialEvent: { evaluationLocator: string }) => pipe(
+  partialEvent.evaluationLocator,
+  runScript,
+  TE.map((newDate) => ({
+    ...partialEvent,
+    date: newDate,
+  })),
+);
+
+const processFile = (filePath: string) => pipe(
+  filePath,
+  readTextFile,
+  T.map(E.orElse(() => E.right(''))),
+  TE.chainEitherKW(decodeEvaluationsFromJsonl),
+  TE.chainW(TE.traverseArray(updateDate)),
+  TE.map((foo) => process.stdout.write(JSON.stringify(foo))),
+);
+
+void (async (): Promise<unknown> => pipe(
+  processFile(filename),
+)())();
