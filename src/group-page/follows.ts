5d5d96ac0ad343186236d1a328a22277be15dc93
Kevin Rutherford <kevin@rutherford-software.com>
2021-12-20 15:45:03 +0000

    Use the shared readmodel to set the state of the Follow button #1325

--- src/group-page/follows.ts
+++ src/group-page/follows.ts
@@ -1,27 +1,16 @@
 import * as O from 'fp-ts/Option';
 import * as A from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import { DomainEvent, isUserFollowedEditorialCommunityEvent, isUserUnfollowedEditorialCommunityEvent } from '../domain-events';
+import { DomainEvent } from '../domain-events';
+import { getGroupIdsFollowedBy } from '../shared-read-models/followings';
 import { GroupId } from '../types/group-id';
 import { UserId } from '../types/user-id';
-
-const isSignificantTo = (
-  userId: UserId,
-  groupId: GroupId,
-) => (event: DomainEvent) => (
-  (isUserFollowedEditorialCommunityEvent(event)
-    && event.editorialCommunityId === groupId
-    && event.userId === userId)
-  || (isUserUnfollowedEditorialCommunityEvent(event)
-    && event.editorialCommunityId === groupId
-    && event.userId === userId)
-);
 
 type Follows = (u: UserId, g: GroupId) => (events: ReadonlyArray<DomainEvent>) => boolean;
 
 export const follows: Follows = (userId, groupId) => (events) => pipe(
   events,
-  A.findLast(isSignificantTo(userId, groupId)),
-  O.filter(isUserFollowedEditorialCommunityEvent),
+  getGroupIdsFollowedBy(userId),
+  A.findFirst((gid) => gid === groupId),
   O.isSome,
 );
