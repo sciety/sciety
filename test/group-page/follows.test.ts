8ddd3ab4432d0b83a0f0e3a860cd419ce7a9d70d
Kevin Rutherford <kevin@rutherford-software.com>
2021-12-20 09:29:15 +0000

    Express projection tests according to current codebase style #1325

--- test/group-page/follows.test.ts
+++ test/group-page/follows.test.ts
@@ -1,3 +1,4 @@
+import { pipe } from 'fp-ts/function';
 import { userFollowedEditorialCommunity, userUnfollowedEditorialCommunity } from '../../src/domain-events';
 import { follows } from '../../src/group-page/follows';
 import { groupIdFromString } from '../types/group-id.helper';
@@ -9,61 +10,68 @@ describe('follows', () => {
   const group2 = groupIdFromString('group-2');
 
   describe('when there are no events', () => {
-    it('is not following the group', () => {
-      const result = follows(someone, groupIdFromString('group-1'))([]);
+    const result = pipe(
+      [],
+      follows(someone, groupIdFromString('group-1')),
+    );
 
+    it('is not following the group', () => {
       expect(result).toBe(false);
     });
   });
 
   describe('when there is one follow event', () => {
-    const events = [
-      userFollowedEditorialCommunity(someone, group1),
-    ];
+    const result = pipe(
+      [
+        userFollowedEditorialCommunity(someone, group1),
+      ],
+      follows(someone, groupIdFromString('group-1')),
+    );
 
     it('is following the group', () => {
-      const result = follows(someone, groupIdFromString('group-1'))(events);
-
       expect(result).toBe(true);
     });
   });
 
   describe('when there is a follow event followed by unfollow event', () => {
-    const events = [
-      userFollowedEditorialCommunity(someone, group1),
-      userUnfollowedEditorialCommunity(someone, group1),
-    ];
+    const result = pipe(
+      [
+        userFollowedEditorialCommunity(someone, group1),
+        userUnfollowedEditorialCommunity(someone, group1),
+      ],
+      follows(someone, groupIdFromString('group-1')),
+    );
 
     it('not following the group', () => {
-      const result = follows(someone, groupIdFromString('group-1'))(events);
-
       expect(result).toBe(false);
     });
   });
 
   describe('when another user has a follow event', () => {
     const someoneElse = arbitraryUserId();
-    const events = [
-      userFollowedEditorialCommunity(someoneElse, group1),
-    ];
+    const result = pipe(
+      [
+        userFollowedEditorialCommunity(someoneElse, group1),
+      ],
+      follows(someone, groupIdFromString('group-1')),
+    );
 
     it('not following the group', () => {
-      const result = follows(someone, groupIdFromString('group-1'))(events);
-
       expect(result).toBe(false);
     });
   });
 
   describe('when a second group has both follow and unfollow events and the first has only follow event', () => {
-    const events = [
-      userFollowedEditorialCommunity(someone, group2),
-      userFollowedEditorialCommunity(someone, group1),
-      userUnfollowedEditorialCommunity(someone, group2),
-    ];
+    const result = pipe(
+      [
+        userFollowedEditorialCommunity(someone, group2),
+        userFollowedEditorialCommunity(someone, group1),
+        userUnfollowedEditorialCommunity(someone, group2),
+      ],
+      follows(someone, groupIdFromString('group-1')),
+    );
 
     it('is following the group', () => {
-      const result = follows(someone, groupIdFromString('group-1'))(events);
-
       expect(result).toBe(true);
     });
   });
