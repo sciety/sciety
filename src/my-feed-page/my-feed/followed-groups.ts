979d61696d7d96250dc5a733ef75d3f2bb07868e
Kevin Rutherford <kevin@rutherford-software.com>
2021-12-20 13:46:40 +0000

    Confirm that the projections in my-feed and user-page pass the same tests #1325

--- src/my-feed-page/my-feed/followed-groups.ts
+++ src/my-feed-page/my-feed/followed-groups.ts
@@ -1,44 +1,12 @@
-import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import {
-  DomainEvent,
-  isUserFollowedEditorialCommunityEvent,
-  isUserUnfollowedEditorialCommunityEvent, UserFollowedEditorialCommunityEvent, UserUnfollowedEditorialCommunityEvent,
-} from '../../domain-events';
+import { DomainEvent } from '../../domain-events';
 import { GroupId } from '../../types/group-id';
 import { UserId } from '../../types/user-id';
-
-type FollowOrUnfollowEvent = UserFollowedEditorialCommunityEvent | UserUnfollowedEditorialCommunityEvent;
-
-const reduceFollowOrUnfollowEventToGroupIds = (
-  state: ReadonlyArray<GroupId>,
-  event: FollowOrUnfollowEvent,
-): ReadonlyArray<GroupId> => pipe(
-  event.editorialCommunityId,
-  (groupId) => {
-    switch (event.type) {
-      case 'UserFollowedEditorialCommunity':
-        return [...state, groupId];
-      case 'UserUnfollowedEditorialCommunity':
-        return pipe(
-          state,
-          RA.filter((existing) => existing !== groupId),
-        );
-    }
-  },
-);
-
-const isFollowOrUnfollowEventForUser = (userId: UserId) => (event: DomainEvent): event is FollowOrUnfollowEvent => (
-  (
-    isUserFollowedEditorialCommunityEvent(event) || isUserUnfollowedEditorialCommunityEvent(event)
-  )
-  && event.userId === userId
-);
+import { followedGroupIds } from '../../user-page/follow-list/project-followed-group-ids';
 
 type FollowedGroups = (userId: UserId) => (events: ReadonlyArray<DomainEvent>) => ReadonlyArray<GroupId>;
 
 export const followedGroups: FollowedGroups = (userId) => (events) => pipe(
   events,
-  RA.filter(isFollowOrUnfollowEventForUser(userId)),
-  RA.reduce(RA.empty, reduceFollowOrUnfollowEventToGroupIds),
+  followedGroupIds(userId),
 );
