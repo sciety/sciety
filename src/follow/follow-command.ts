ef5a11cd92f6632bc78c11cf23b2fdae822bc982
Kevin Rutherford <kevin@rutherford-software.com>
2021-12-20 10:55:56 +0000

    Tidy up a function signature #1325

--- src/follow/follow-command.ts
+++ src/follow/follow-command.ts
@@ -5,20 +5,14 @@ import { DomainEvent, UserFollowedEditorialCommunityEvent } from '../domain-even
 import { GroupId } from '../types/group-id';
 import { User } from '../types/user';
 
-type CommitEvents = (events: ReadonlyArray<UserFollowedEditorialCommunityEvent>) => T.Task<void>;
-
 export type Ports = {
   getAllEvents: T.Task<ReadonlyArray<DomainEvent>>,
-  commitEvents: CommitEvents,
+  commitEvents: (events: ReadonlyArray<UserFollowedEditorialCommunityEvent>) => T.Task<void>,
 };
 
-type FollowCommand = (user: User, groupId: GroupId) => T.Task<void>;
-
-export const followCommand = (ports: Ports): FollowCommand => (
-  (user, groupId) => pipe(
-    ports.getAllEvents,
-    T.map(createEventSourceFollowListRepository(user.id)),
-    T.map((followList) => followList.follow(groupId)),
-    T.chain(ports.commitEvents),
-  )
+export const followCommand = (ports: Ports) => (user: User, groupId: GroupId): T.Task<void> => pipe(
+  ports.getAllEvents,
+  T.map(createEventSourceFollowListRepository(user.id)),
+  T.map((followList) => followList.follow(groupId)),
+  T.chain(ports.commitEvents),
 );
