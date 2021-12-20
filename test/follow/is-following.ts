da1daf5f551ac7341a732e77429e18bfe1e961bb
Kevin Rutherford <kevin@rutherford-software.com>
2021-12-20 13:09:55 +0000

    Tidy up the tests of the aggregate #1325

--- test/follow/is-following.ts
+++ test/follow/is-following.ts
@@ -1,50 +1,49 @@
+import { pipe } from 'fp-ts/function';
 import { userFollowedEditorialCommunity, userUnfollowedEditorialCommunity } from '../../src/domain-events';
 import { isFollowing } from '../../src/follow/is-following';
 import { arbitraryGroupId } from '../types/group-id.helper';
 import { arbitraryUserId } from '../types/user-id.helper';
 
 const groupId1 = arbitraryGroupId();
-const groupId2 = arbitraryGroupId();
 const userId1 = arbitraryUserId();
 const userId2 = arbitraryUserId();
 
-describe('event-sourced-follow-list-repository', () => {
-  it('builds a follow list from events', () => {
-    const events = [
-      userFollowedEditorialCommunity(userId1, groupId1),
-    ];
-    const actual = isFollowing(userId1, groupId1)(events);
+const unimportantEvents = [
+  userFollowedEditorialCommunity(arbitraryUserId(), arbitraryGroupId()),
+  userFollowedEditorialCommunity(userId2, groupId1),
+  userUnfollowedEditorialCommunity(userId2, groupId1),
+];
 
-    expect(actual).toBe(true);
+describe('is-following', () => {
+  describe('when the user is following the group', () => {
+    const result = pipe(
+      [
+        ...unimportantEvents,
+        userFollowedEditorialCommunity(userId1, groupId1),
+        ...unimportantEvents,
+      ],
+      isFollowing(userId1, groupId1),
+    );
+
+    it('returns true', () => {
+      expect(result).toBe(true);
+    });
   });
 
-  it('ignores groups that the user has unfollowed', () => {
-    const events = [
-      userFollowedEditorialCommunity(userId1, groupId1),
-      userUnfollowedEditorialCommunity(userId1, groupId1),
-    ];
-    const actual = isFollowing(userId1, groupId1)(events);
+  describe('when the user has unfollowed the group', () => {
+    const result = pipe(
+      [
+        ...unimportantEvents,
+        userFollowedEditorialCommunity(userId1, groupId1),
+        ...unimportantEvents,
+        userUnfollowedEditorialCommunity(userId1, groupId1),
+        ...unimportantEvents,
+      ],
+      isFollowing(userId1, groupId1),
+    );
 
-    expect(actual).toBe(false);
-  });
-
-  it('ignores groups that other users have followed', () => {
-    const events = [
-      userFollowedEditorialCommunity(userId1, groupId1),
-      userFollowedEditorialCommunity(userId2, groupId2),
-    ];
-    const actual = isFollowing(userId1, groupId1)(events);
-
-    expect(actual).toBe(true);
-  });
-
-  it('ignores groups followed by the user that other users have unfollowed', () => {
-    const events = [
-      userFollowedEditorialCommunity(userId1, groupId1),
-      userUnfollowedEditorialCommunity(userId2, groupId1),
-    ];
-    const actual = isFollowing(userId1, groupId1)(events);
-
-    expect(actual).toBe(true);
+    it('ignores groups that the user has unfollowed', () => {
+      expect(result).toBe(false);
+    });
   });
 });
