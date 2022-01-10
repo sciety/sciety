3261d75f48a7bada3eabe33928aa5963a3ee4862
Giorgio Sironi <g.sironi@elifesciences.org>
2022-01-10 15:27:19 +0000

    Turn headers into function #1355

--- src/ncrc-featured-articles-page/page.ts
+++ src/ncrc-featured-articles-page/page.ts
@@ -15,27 +15,32 @@ import * as DE from '../types/data-error';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 import { RenderPageError } from '../types/render-page-error';
+import { ListId } from '../types/list-id';
 
-const headers = {
-  'cbd478fe-3ff7-4125-ac9f-c94ff52ae0f7': {
-    name: 'High interest articles',
-    description: 'Articles that have been identified as high interest by NCRC editors.',
-    ownerName: 'NCRC',
-    ownerHref: '/groups/ncrc',
-    ownerAvatarPath: '/static/groups/ncrc--62f9b0d0-8d43-4766-a52a-ce02af61bc6a.jpg',
-    articleCount: lists['cbd478fe-3ff7-4125-ac9f-c94ff52ae0f7'].length,
-    lastUpdated: O.some(new Date('2021-11-24')),
+const headers = (listId: ListId) => pipe(
+  {
+    'cbd478fe-3ff7-4125-ac9f-c94ff52ae0f7': {
+      name: 'High interest articles',
+      description: 'Articles that have been identified as high interest by NCRC editors.',
+      ownerName: 'NCRC',
+      ownerHref: '/groups/ncrc',
+      ownerAvatarPath: '/static/groups/ncrc--62f9b0d0-8d43-4766-a52a-ce02af61bc6a.jpg',
+      articleCount: lists['cbd478fe-3ff7-4125-ac9f-c94ff52ae0f7'].length,
+      lastUpdated: O.some(new Date('2021-11-24')),
+    },
+    '5ac3a439-e5c6-4b15-b109-92928a740812': {
+      name: 'Endorsed articles',
+      description: 'Articles that have been endorsed by Biophysics Colab.',
+      ownerName: 'Biophysics Colab',
+      ownerHref: '/groups/biophysics-colab',
+      ownerAvatarPath: '/static/groups/biophysics-colab--4bbf0c12-629b-4bb8-91d6-974f4df8efb2.png',
+      articleCount: lists['5ac3a439-e5c6-4b15-b109-92928a740812'].length,
+      lastUpdated: O.some(new Date('2021-11-22T15:09:00Z')),
+    },
   },
-  '5ac3a439-e5c6-4b15-b109-92928a740812': {
-    name: 'Endorsed articles',
-    description: 'Articles that have been endorsed by Biophysics Colab.',
-    ownerName: 'Biophysics Colab',
-    ownerHref: '/groups/biophysics-colab',
-    ownerAvatarPath: '/static/groups/biophysics-colab--4bbf0c12-629b-4bb8-91d6-974f4df8efb2.png',
-    articleCount: lists['5ac3a439-e5c6-4b15-b109-92928a740812'].length,
-    lastUpdated: O.some(new Date('2021-11-22T15:09:00Z')),
-  },
-};
+  R.lookup(listId),
+  E.fromOption(() => DE.notFound),
+);
 
 type Components = {
   header: HtmlFragment,
@@ -63,17 +68,13 @@ type Params = t.TypeOf<typeof paramsCodec>;
 export const page = (ports: Ports) => (params: Params): TE.TaskEither<RenderPageError, Page> => pipe(
   {
     header: pipe(
-      headers,
-      R.lookup(params.id),
-      E.fromOption(() => DE.notFound),
+      headers(params.id),
       E.map(renderComponent),
       T.of,
     ),
     articlesList: articlesList(ports, params.id, params.page),
     title: pipe(
-      headers,
-      R.lookup(params.id),
-      E.fromOption(() => DE.notFound),
+      headers(params.id),
       E.map((header) => header.name),
       T.of,
     ),
