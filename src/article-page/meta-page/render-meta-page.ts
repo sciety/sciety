9145ed690272534bcfafa186f96511e353164812
David Moulton <d.moulton@elifesciences.org>
2022-01-11 14:28:13 +0000

    Extract function to file #1364

--- src/article-page/meta-page/render-meta-page.ts
+++ src/article-page/meta-page/render-meta-page.ts
@@ -1,9 +1,9 @@
 import { pipe } from 'fp-ts/function';
-import { renderAuthors } from './render-authors';
+import { renderMetaContent } from './render-meta-content';
 import { tabs } from '../../shared-components/tabs';
 import { ArticleAuthors } from '../../types/article-authors';
 import { Doi } from '../../types/doi';
-import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
+import { HtmlFragment } from '../../types/html-fragment';
 import { renderPage } from '../render-page';
 import { tabList } from '../tab-list';
 
@@ -11,30 +11,6 @@ type ArticleDetails = {
   abstract: HtmlFragment,
   authors: ArticleAuthors,
 };
-
-const renderMetaContent = (articleDetails: ArticleDetails, doi: Doi) => pipe(
-  `
-    <div class="article-meta-wrapper">
-      <section class="article-meta">
-        ${renderAuthors(articleDetails.authors)}
-        <ul aria-label="Publication details" class="article-meta-data-list" role="list">
-          <li>
-            <a href="https://doi.org/${doi.value}" target="_blank">https://doi.org/${doi.value}</a>
-          </li>
-        </ul>
-      </section>
-
-      <section class="article-abstract" role="doc-abstract">
-        ${articleDetails.abstract}
-      </section>
-
-      <a href="https://doi.org/${doi.value}" class="full-article-button" target="_blank">
-        Read the full article
-      </a>
-    </div>
-  `,
-  toHtmlFragment,
-);
 
 export const renderMetaPage = (components: {
   doi: Doi,
