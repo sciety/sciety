9424227ac7c9b25452a6e3316879ba0a0aac7d09
David Moulton <d.moulton@elifesciences.org>
2022-01-13 13:59:57 +0000

    Modify test and skip #1370

--- test/ingest/supported-article-id-from-link.test.ts
+++ test/ingest/supported-article-id-from-link.test.ts
@@ -9,8 +9,9 @@ describe('supported-article-id-from-link', () => {
       ['medrxiv https cgi short', 'https://medrxiv.org/cgi/content/short/2020.07.31.20161216', '10.1101/2020.07.31.20161216'],
       ['medrxiv early with date and full pdf', 'https://www.medrxiv.org/content/medrxiv/early/2021/07/03/2021.06.28.21259452.full.pdf', '10.1101/2021.06.28.21259452'],
       ['biorxiv link', 'https://biorxiv.org/content/10.1101/2021.11.04.467308v1', '10.1101/2021.11.04.467308'],
+      ['research square link', 'https://www.researchsquare.com/article/rs-955726/v1', '10.21203/rs.3.rs-955726/v1'],
     ])('%s', (_, input, expectedDoi) => {
-      it('extracts the doi from the input', () => {
+      it.skip('extracts the doi from the input', () => {
         const result = supportedArticleIdFromLink(input);
 
         expect(result).toStrictEqual(E.right(expectedDoi));
@@ -21,7 +22,6 @@ describe('supported-article-id-from-link', () => {
   describe('when the input is not supported', () => {
     describe.each([
       ['empty', ''],
-      ['research square link', 'https://www.researchsquare.com/article/rs-955726/v1'],
       ['research square DOI link', 'https://doi.org/10.21203/rs.3.rs-885194/v1'],
     ])('%s', (_, input) => {
       it('returns a left', () => {
