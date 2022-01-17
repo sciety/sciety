2ae10ee7bf3e5a56002d631d7f6ed197dd766591
Giorgio Sironi <g.sironi@elifesciences.org>
2022-01-17 15:36:24 +0000

    Acknowledge message processing #1376

--- src/policies/log-event.ts
+++ src/policies/log-event.ts
@@ -1,17 +1,35 @@
-import { ReceiveMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
+import { ReceiveMessageCommand, DeleteMessageCommand, SQSClient, ReceiveMessageCommandOutput } from '@aws-sdk/client-sqs';
 import { pipe } from 'fp-ts/function';
+import * as T from 'fp-ts/Task';
 
 const queueUrl = 'http://localhost:4566/000000000000/queue123';
 
-const foo = (url: string) => async () => {
+const getMessage = (url: string) => async () => {
   const sqsClient = new SQSClient({ region: 'us-east-1', endpoint: 'http://localhost:4566' });
   const command = new ReceiveMessageCommand({ QueueUrl: url });
-  const data = await sqsClient.send(command);
+  return sqsClient.send(command);
+};
 
+const logEvent = (data: ReceiveMessageCommandOutput) => {
   process.stdout.write(JSON.stringify(data));
+  return data;
+};
+
+const deleteMessageFromQueue = (data: ReceiveMessageCommandOutput) => async () => {
+  const sqsClient = new SQSClient({ region: 'us-east-1', endpoint: 'http://localhost:4566' });
+  if (data.Messages) {
+    const receiptHandle = data.Messages ? data.Messages[0].ReceiptHandle : '';
+    const command = new DeleteMessageCommand({ 
+      QueueUrl: queueUrl,
+      ReceiptHandle: receiptHandle,
+    });
+    await sqsClient.send(command);
+  }
 };
 
 void (async (): Promise<void> => pipe(
   queueUrl,
-  foo,
+  getMessage,
+  T.map(logEvent),
+  T.chain(deleteMessageFromQueue),
 )())();
