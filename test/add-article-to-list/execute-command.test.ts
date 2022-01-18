b4d4ac04a67da48a3789d65fccc09e9e9ba57677
Giorgio Sironi <g.sironi@elifesciences.org>
2022-01-18 10:58:50 +0000

    Write test list #1385

---
+++ test/add-article-to-list/execute-command.test.ts
@@ -0,0 +1,68 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import { executeCommand } from '../../src/add-article-to-list/execute-command';
+import { articleAddedToList } from '../../src/domain-events/article-added-to-list-event';
+import { listCreated } from '../../src/domain-events/list-created-event';
+import { arbitraryString } from '../helpers';
+import { arbitraryDoi } from '../types/doi.helper';
+import { arbitraryGroupId } from '../types/group-id.helper';
+import { arbitraryListId } from '../types/list-id.helper';
+
+describe('execute-command', () => {
+  const listId = arbitraryListId();
+  const articleId = arbitraryDoi();
+
+  describe('when the list exists', () => {
+    describe('and the article is already on the list', () => {
+      const result = pipe(
+        [
+          listCreated(listId, arbitraryString(), arbitraryString(), arbitraryGroupId()),
+          articleAddedToList(articleId, listId),
+        ],
+        executeCommand({
+          listId,
+          articleId,
+        }),
+      );
+
+      it('succeeds with no events raised', () => {
+        expect(result).toStrictEqual(E.right([]));
+      });
+    });
+
+    describe('and the article is not on the list', () => {
+      const result = pipe(
+        [
+          listCreated(listId, arbitraryString(), arbitraryString(), arbitraryGroupId()),
+        ],
+        executeCommand({
+          listId,
+          articleId,
+        }),
+      );
+
+      it.skip('succeeds and raises an event', () => {
+        expect(result).toStrictEqual(E.right([expect.objectContaining({
+          type: 'ArticleAddedToList',
+          articleId,
+          listId,
+        })]));
+      });
+    });
+  });
+
+  describe('when the list does not exist', () => {
+    const result = pipe(
+      [
+      ],
+      executeCommand({
+        listId,
+        articleId,
+      }),
+    );
+
+    it.skip('fails with no events raised', () => {
+      expect(result).toStrictEqual(E.left(expect.anything));
+    });
+  });
+});
