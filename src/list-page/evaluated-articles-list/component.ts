45a6b7bdd8a920556bb27d25b5f8311d622fe828
David Moulton <d.moulton@elifesciences.org>
2022-01-19 11:45:30 +0000

    Populate article activities operates on DOIs #1389

--- src/list-page/evaluated-articles-list/component.ts
+++ src/list-page/evaluated-articles-list/component.ts
@@ -8,8 +8,8 @@ import { toPageOfCards, Ports as ToPageOfCardsPorts } from './to-page-of-cards';
 import { DomainEvent } from '../../domain-events';
 import { PageOfItems, paginate } from '../../shared-components/paginate';
 import { getActivityForDois } from '../../shared-read-models/article-activity/get-activity-for-dois';
-import { ArticleActivity } from '../../types/article-activity';
 import * as DE from '../../types/data-error';
+import { Doi } from '../../types/doi';
 import { Group } from '../../types/group';
 import { HtmlFragment } from '../../types/html-fragment';
 
@@ -17,9 +17,8 @@ export type Ports = ToPageOfCardsPorts & {
   getAllEvents: T.Task<ReadonlyArray<DomainEvent>>,
 };
 
-const overrideArticleActivities = (ports: Ports) => (pageOfItems: PageOfItems<ArticleActivity>) => pipe(
+const populateArticleActivities = (ports: Ports) => (pageOfItems: PageOfItems<Doi>) => pipe(
   pageOfItems.items,
-  RA.map((item) => item.doi),
   (dois) => pipe(
     ports.getAllEvents,
     T.map(getActivityForDois(dois)),
@@ -41,9 +40,10 @@ export const component = (
   TE.chain(RA.match(
     () => TE.right(noEvaluatedArticlesMessage),
     flow(
+      RA.map((item) => item.doi),
       paginate(20, pageNumber),
       TE.fromEither,
-      TE.chainTaskK(overrideArticleActivities(ports)),
+      TE.chainTaskK(populateArticleActivities(ports)),
       TE.chainTaskK(toPageOfCards(ports, group)),
     ),
   )),
