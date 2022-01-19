31f62ed0c90fc72ab25ad41c0ebb7458fd6182e7
Giorgio Sironi <g.sironi@elifesciences.org>
2022-01-19 13:41:00 +0000

    Load featured articles page in 300ms rather than 5000ms #1389

--- src/ncrc-featured-articles-page/articles-list/articlesList.ts
+++ src/ncrc-featured-articles-page/articles-list/articlesList.ts
@@ -4,12 +4,11 @@ import * as TE from 'fp-ts/TaskEither';
 import { flow, pipe } from 'fp-ts/function';
 import { toPageOfCards, Ports as ToPageOfCardsPorts } from './to-page-of-cards';
 import { DomainEvent } from '../../domain-events';
+import { populateArticleActivities } from '../../list-page/evaluated-articles-list/populate-article-activities';
 import { noEvaluatedArticlesMessage } from '../../list-page/evaluated-articles-list/static-messages';
 import { paginate } from '../../shared-components/paginate';
-import { getActivityForDoi } from '../../shared-read-models/article-activity';
 import { selectArticlesBelongingToList } from '../../shared-read-models/list-articles';
 import * as DE from '../../types/data-error';
-import { Doi } from '../../types/doi';
 import { HtmlFragment } from '../../types/html-fragment';
 
 export type Ports = ToPageOfCardsPorts & {
@@ -23,15 +22,12 @@ export const articlesList = (
 ): TE.TaskEither<DE.DataError, HtmlFragment> => pipe(
   ports.getAllEvents,
   T.map(selectArticlesBelongingToList(listId)),
-  TE.chainTaskK((dois) => pipe(
-    ports.getAllEvents,
-    T.map((events) => RA.map((doi: Doi) => getActivityForDoi(doi)(events))(dois)),
-  )),
   TE.chain(RA.match(
     () => TE.right(noEvaluatedArticlesMessage),
     flow(
       paginate(20, pageNumber),
       TE.fromEither,
+      TE.chainTaskK(populateArticleActivities(ports)),
       TE.chainTaskK(toPageOfCards(ports, listId)),
     ),
   )),
