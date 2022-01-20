d6b3f37b9c7271be02bcf4d97970b503fda8ae0d
David Moulton <d.moulton@elifesciences.org>
2022-01-20 12:00:38 +0000

    Add todo test for list tab count #1392

--- test/group-page/content-component.test.ts
+++ test/group-page/content-component.test.ts
@@ -3,40 +3,43 @@ import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { JSDOM } from 'jsdom';
 import { userFollowedEditorialCommunity } from '../../src/domain-events';
-import { listCreated } from '../../src/domain-events/list-created-event';
 import { contentComponent } from '../../src/group-page/content-component';
-import { arbitraryString } from '../helpers';
 import { shouldNotBeCalled } from '../should-not-be-called';
 import { arbitraryGroup } from '../types/group.helper';
-import { arbitraryListId } from '../types/list-id.helper';
 import { arbitraryUserId } from '../types/user-id.helper';
 
 describe('content-component', () => {
   const group = arbitraryGroup();
-  const events = [
-    listCreated(arbitraryListId(), arbitraryString(), arbitraryString(), group.id),
-    userFollowedEditorialCommunity(arbitraryUserId(), group.id),
-    userFollowedEditorialCommunity(arbitraryUserId(), group.id),
-  ];
-  const ports = {
-    fetchStaticFile: () => TE.right(''),
-    getUserDetailsBatch: () => TE.right([]),
-    getAllEvents: T.of(events),
-    getGroup: () => TE.right(group),
-  };
 
-  it.each([
-    [0 as const],
-    [1 as const],
-    [2 as const],
-  ])('when loading tab index %d displays the followers count on the followers tab label', async (activeTabIndex) => {
-    const content = await pipe(
-      contentComponent(ports)(group, 1, activeTabIndex),
-      TE.getOrElse(shouldNotBeCalled),
-      T.map(JSDOM.fragment),
-    )();
-    const followersTabLabel = content.querySelector('.tab:nth-child(3)')?.textContent;
+  describe('the followers tab', () => {
+    const events = [
+      userFollowedEditorialCommunity(arbitraryUserId(), group.id),
+      userFollowedEditorialCommunity(arbitraryUserId(), group.id),
+    ];
+    const ports = {
+      fetchStaticFile: () => TE.right(''),
+      getUserDetailsBatch: () => TE.right([]),
+      getAllEvents: T.of(events),
+      getGroup: () => TE.right(group),
+    };
 
-    expect(followersTabLabel).toContain('(2)');
+    it.each([
+      [0 as const],
+      [1 as const],
+      [2 as const],
+    ])('when loading tab index %d displays the followers count on the followers tab label', async (activeTabIndex) => {
+      const content = await pipe(
+        contentComponent(ports)(group, 1, activeTabIndex),
+        TE.getOrElse(shouldNotBeCalled),
+        T.map(JSDOM.fragment),
+      )();
+      const followersTabLabel = content.querySelector('.tab:nth-child(3)')?.textContent;
+
+      expect(followersTabLabel).toContain('(2)');
+    });
+  });
+
+  describe('the lists tab', () => {
+    it.todo('when loading tab index 1 displays the lists count on the lists tab label');
   });
 });
