b2564f32ac2fe343d6852e065ef05644d1013c4f
David Moulton <d.moulton@elifesciences.org>
2022-01-26 15:00:02 +0000

    Remove code that read events from data files #1306

--- src/infrastructure/create-infrastructure.ts
+++ src/infrastructure/create-infrastructure.ts
@@ -17,7 +17,6 @@ import { fetchReview } from './fetch-review';
 import { fetchStaticFile } from './fetch-static-file';
 import { fetchData } from './fetchers';
 import { getCachedAxiosRequest } from './get-cached-axios-request';
-import { getEventsFromDataFiles } from './get-events-from-data-files';
 import { getEventsFromDatabase } from './get-events-from-database';
 import { getHtml } from './get-html';
 import {
@@ -26,7 +25,7 @@ import {
 import { needsToBeAdded } from './needs-to-be-added';
 import { bootstrapGroups } from '../data/bootstrap-groups';
 import * as DomainEvent from '../domain-events';
-import { isEvaluationRecordedEvent } from '../domain-events/type-guards';
+import { isEvaluationRecordedEvent } from '../domain-events';
 import { articleAddedToListEvents } from '../shared-read-models/lists/article-added-to-list-events';
 import { listCreationEvents } from '../shared-read-models/lists/list-creation-data';
 import { getArticleVersionEventsFromBiorxiv } from '../third-parties/biorxiv';
@@ -37,35 +36,6 @@ import { fetchPrelightsHighlight } from '../third-parties/prelights';
 import {
   getTwitterResponse, getTwitterUserDetails, getTwitterUserDetailsBatch, getTwitterUserId,
 } from '../third-parties/twitter';
-import * as Gid from '../types/group-id';
-
-const pciPaleontologyGroupId = Gid.fromValidatedString('7a9e97d1-c1fe-4ac2-9572-4ecfe28f9f84');
-const pciArchaeologyGroupId = Gid.fromValidatedString('b90854bf-795c-42ba-8664-8257b9c68b0c');
-const pciNeuroscienceGroupId = Gid.fromValidatedString('af792cd3-1600-465c-89e5-250c48f793aa');
-const pciZoologyGroupId = Gid.fromValidatedString('74fd66e9-3b90-4b5a-a4ab-5be83db4c5de');
-
-const groupIdsCurrentlyBeingPortedToDatabase = [
-] as ReadonlyArray<Gid.GroupId>;
-
-const groupIdsToSkipWhenLoadingEventsDirectlyFromDataFiles = [
-  pciPaleontologyGroupId,
-  pciArchaeologyGroupId,
-  pciNeuroscienceGroupId,
-  pciZoologyGroupId,
-  Gid.fromValidatedString('4eebcec9-a4bb-44e1-bde3-2ae11e65daaa'),
-  Gid.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2'),
-  Gid.fromValidatedString('50401e46-b764-47b7-8557-6bb35444b7c8'),
-  Gid.fromValidatedString('10360d97-bf52-4aef-b2fa-2f60d319edd7'),
-  Gid.fromValidatedString('32025f28-0506-480e-84a0-b47ef1e92ec5'),
-  Gid.fromValidatedString('62f9b0d0-8d43-4766-a52a-ce02af61bc6a'),
-  Gid.fromValidatedString('19b7464a-edbe-42e8-b7cc-04d1eb1f7332'),
-  Gid.fromValidatedString('5142a5bc-6b18-42b1-9a8d-7342d7d17e94'),
-  Gid.fromValidatedString('316db7d9-88cc-4c26-b386-f067e0f56334'),
-  Gid.fromValidatedString('f97bd177-5cb6-4296-8573-078318755bf2'),
-  Gid.fromValidatedString('53ed5364-a016-11ea-bb37-0242ac130002'),
-  Gid.fromValidatedString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'),
-  Gid.fromValidatedString('8ccea9c2-e6c8-4dd7-bf1d-37c3fa86ff65'),
-];
 
 type Dependencies = {
   prettyLog: boolean,
@@ -97,13 +67,12 @@ export const createInfrastructure = (dependencies: Dependencies): TE.TaskEither<
     `),
     identity,
   )),
-  TE.chain(({ pool, logger }) => pipe(
+  TE.chainW(({ pool, logger }) => pipe(
     {
       eventsFromDatabase: pipe(
         getEventsFromDatabase(pool, loggerIO(logger)),
-        TE.chain((events) => pipe(
-          groupIdsCurrentlyBeingPortedToDatabase,
-          getEventsFromDataFiles,
+        TE.chainW((events) => pipe(
+          TE.right([]),
           TE.map(RA.filter(isEvaluationRecordedEvent)),
           TE.map(RA.filter(needsToBeAdded(events))),
           TE.chainFirstTaskK(T.traverseArray(writeEventToDatabase(pool))),
@@ -113,23 +82,16 @@ export const createInfrastructure = (dependencies: Dependencies): TE.TaskEither<
           ]),
         )),
       ),
-      eventsFromDataFiles: pipe(
-        bootstrapGroups,
-        RA.map(({ groupId }) => groupId),
-        RA.filter((groupId) => !groupIdsToSkipWhenLoadingEventsDirectlyFromDataFiles.includes(groupId)),
-        getEventsFromDataFiles,
-      ),
       groupEvents: pipe(
         bootstrapGroups,
         TE.right,
       ),
     },
     sequenceS(TE.ApplyPar),
-    TE.map(({ eventsFromDataFiles, eventsFromDatabase, groupEvents }) => (
+    TE.map(({ eventsFromDatabase, groupEvents }) => (
       {
         events: pipe(
           [
-            ...eventsFromDataFiles,
             ...eventsFromDatabase,
             ...groupEvents,
             ...listCreationEvents,
