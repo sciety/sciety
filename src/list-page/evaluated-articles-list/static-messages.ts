76c9d550cda75f1224b45ac5c0424e772984fe03
David Moulton <d.moulton@elifesciences.org>
2022-01-27 14:47:20 +0000

    Update wording for accuracy #1409

--- src/list-page/evaluated-articles-list/static-messages.ts
+++ src/list-page/evaluated-articles-list/static-messages.ts
@@ -3,7 +3,7 @@ import { toHtmlFragment } from '../../types/html-fragment';
 const staticMessage = (message: string) => toHtmlFragment(`<p class="evaluated-articles__static_message">${message}</p>`);
 
 export const noEvaluatedArticlesMessage = staticMessage(
-  'It looks like this group hasn’t evaluated any articles yet. Try coming back later!',
+  'This list is currently empty. Try coming back later!',
 );
 
 export const noArticlesCanBeFetchedMessage = staticMessage(
