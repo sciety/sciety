c9c569a393988a97dbfdb88d9c0913531ab31f78
David Moulton <d.moulton@elifesciences.org>
2022-02-07 15:26:02 +0000

    Add test list for upcoming fetch-medrxiv-subject-area #1435

---
+++ test/third-parties/biorxiv/fetch-medrxiv-subject-area.test.ts
@@ -0,0 +1,13 @@
+describe('fetch-medrxiv-subject-area', () => {
+  describe('when one article version is returned', () => {
+    it.todo('returns the subject area');
+  });
+
+  describe('when there are multiple article versions', () => {
+    it.todo('returns the subject area of the most recent version');
+  });
+
+  describe('when no article versions are returned', () => {
+    it.todo('returns a left');
+  });
+});
