a09b256cdc99d15726a726f3215f783cff12aa6e
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-02-11 15:40:07 +0000

    Add test list for getList query

---
+++ test/shared-read-models/lists/get-list.test.ts
@@ -0,0 +1,15 @@
+describe('get-list', () => {
+  describe('when the listId does not exist', () => {
+    it.todo('returns not-found');
+  });
+
+  describe('when the listId does exist', () => {
+    describe('and it refers to a hardcoded list', () => {
+      it.todo('returns the correct List');
+    });
+
+    describe('and it refers to a non-hardcoded list', () => {
+      it.todo('returns the correct List');
+    });
+  });
+});
