f23f720ec7178fa1bd1ef040629a10d068d0c098
Giorgio Sironi <g.sironi@elifesciences.org>
2022-02-17 14:51:44 +0000

    Construct list read model, and query it by inlining select-all-lists-owned-by query #1450

--- src/lists-service/owned-by.ts
+++ src/lists-service/owned-by.ts
@@ -1,10 +1,25 @@
+import * as O from 'fp-ts/Option';
+import * as RM from 'fp-ts/ReadonlyMap';
+import * as T from 'fp-ts/Task';
+import { pipe } from 'fp-ts/function';
+import * as S from 'fp-ts/string';
 import { StatusCodes } from 'http-status-codes';
 import { Middleware } from 'koa';
+import { constructListsReadModel } from '../shared-read-models/lists/construct-lists-read-model';
 
-export const ownedBy: Middleware = async ({ response }, next) => {
+export const ownedBy: Middleware = async ({ params, response }, next) => {
   response.set({ 'Content-Type': 'application/json' });
   response.status = StatusCodes.OK;
-  response.body = { items: [] };
+  const items = await pipe(
+    [],
+    constructListsReadModel,
+    T.map(RM.lookup(S.Eq)(params.groupId)),
+    T.map(O.fold(
+      () => [],
+      (list) => [list],
+    )),
+  )();
+  response.body = { items };
 
   await next();
 };
