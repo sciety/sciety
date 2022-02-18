22ff0bf1a5683f69002de2f2166f0a1d7ea33873
David Moulton <d.moulton@elifesciences.org>
2022-02-18 11:02:24 +0000

    Wait for events table to exist before querying database #1450

--- src/infrastructure/get-events-from-database.ts
+++ src/infrastructure/get-events-from-database.ts
@@ -2,6 +2,7 @@ import * as E from 'fp-ts/Either';
 import * as IO from 'fp-ts/IO';
 import { JsonRecord } from 'fp-ts/Json';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { flow, pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
@@ -24,7 +25,22 @@ export const getEventsFromDatabase = (
   pool: Pool,
   logger: L.LoggerIO,
 ): TE.TaskEither<Error, ReadonlyArray<RuntimeGeneratedEvent>> => pipe(
-  TE.tryCatch(async () => pool.query<EventRow>('SELECT id, type, date::text, payload FROM events'), E.toError),
+  TE.tryCatch(async () => {
+    pipe(
+      {},
+      L.debug('Waiting for events table to exist'),
+      IO.chain(logger),
+    )();
+    // eslint-disable-next-line no-loops/no-loops
+    while (true) {
+      const queryResult = await pool.query<{ exists: boolean }>('SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = \'public\' AND tablename = \'events\')');
+      if (queryResult.rows[0].exists) {
+        break;
+      }
+      await T.delay(1000)(T.of(''))();
+    }
+    return pool.query<EventRow>('SELECT id, type, date::text, payload FROM events');
+  }, E.toError),
   TE.map((result) => result.rows),
   TE.chainFirstIOK(flow(
     (rows) => ({ count: rows.length }),
