49fe248a2ea63850df40cd50e7ad16532a7c4492
David Moulton <d.moulton@elifesciences.org>
2022-02-21 10:11:00 +0000

    Add skipped test for list updated date #1455

--- test/shared-read-models/lists/get-list.test.ts
+++ test/shared-read-models/lists/get-list.test.ts
@@ -1,3 +1,12 @@
+import * as O from 'fp-ts/Option';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { articleAddedToList } from '../../../src/domain-events';
+import { getList, List } from '../../../src/shared-read-models/lists';
+import { arbitraryDate } from '../../helpers';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+import { arbitraryDoi } from '../../types/doi.helper';
+
 describe('get-list', () => {
   describe('when the listId does not exist', () => {
     it.todo('returns not-found');
@@ -5,7 +14,28 @@ describe('get-list', () => {
 
   describe('when the listId does exist', () => {
     describe('and it refers to a hardcoded list', () => {
-      it.todo('returns the correct List');
+      describe('when the list is non-empty', () => {
+        const latestDate = arbitraryDate();
+        const eLifeMedicineListId = 'c7237468-aac1-4132-9598-06e9ed68f31d';
+        let result: List;
+
+        beforeEach(async () => {
+          result = await pipe(
+            [
+              articleAddedToList(arbitraryDoi(), eLifeMedicineListId, arbitraryDate()),
+              articleAddedToList(arbitraryDoi(), eLifeMedicineListId, latestDate),
+            ],
+            getList(eLifeMedicineListId),
+            TE.getOrElse(shouldNotBeCalled),
+          )();
+        });
+
+        it.todo('returns the correct List');
+
+        it.skip('with the correct last updated date', () => {
+          expect(result.lastUpdated).toStrictEqual(O.some(latestDate));
+        });
+      });
     });
 
     describe('and it refers to a non-hardcoded list', () => {
