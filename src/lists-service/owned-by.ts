0d0f33a2fd325f8f7a1daf292a7e7e5871ff10b0
Giorgio Sironi <g.sironi@elifesciences.org>
2022-02-25 10:30:20 +0000

    Introduce performance logs for read model query #1460

--- src/lists-service/owned-by.ts
+++ src/lists-service/owned-by.ts
@@ -6,17 +6,28 @@ import * as S from 'fp-ts/string';
 import { StatusCodes } from 'http-status-codes';
 import { Middleware } from 'koa';
 import { GetAllEvents } from './get-all-events';
+import { Logger } from '../infrastructure/logger';
 import { constructListsReadModel } from '../shared-read-models/lists/construct-lists-read-model';
 
 type Ports = {
   getAllEvents: GetAllEvents,
+  logger: Logger,
 };
 
 export const ownedBy = (ports: Ports): Middleware => async ({ params, response }, next) => {
   response.set({ 'Content-Type': 'application/json' });
+  ports.logger('debug', 'Started ownedBy query');
   await pipe(
     ports.getAllEvents,
+    TE.chainFirst(() => {
+      ports.logger('debug', 'Loaded all events');
+      return TE.right('everything is ok');
+    }),
     TE.chainTaskK(constructListsReadModel),
+    TE.chainFirst(() => {
+      ports.logger('debug', 'Constructed read model');
+      return TE.right('everything is ok');
+    }),
     TE.map(RM.lookup(S.Eq)(params.groupId)),
     TE.map(O.fold(
       () => [],
