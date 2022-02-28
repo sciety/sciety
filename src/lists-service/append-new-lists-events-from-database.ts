bdced40eb360b3a7d841107025cb9f15b7a5f4ef
Giorgio Sironi <g.sironi@elifesciences.org>
2022-02-28 10:45:56 +0000

    Write story as a pipe #1464

--- src/lists-service/append-new-lists-events-from-database.ts
+++ src/lists-service/append-new-lists-events-from-database.ts
@@ -1,7 +1,18 @@
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import { ListsEvent } from './lists-event';
 
 type AppendNewListsEventsFromDatabase = (listEvents: ReadonlyArray<ListsEvent>)
-=> TE.TaskEither<never, ReadonlyArray<ListsEvent>>;
+=> TE.TaskEither<Error, ReadonlyArray<ListsEvent>>;
 
-export const appendNewListsEventsFromDatabase: AppendNewListsEventsFromDatabase = (listEvents) => TE.right(listEvents);
+const queryDatabaseForEventsWithNewerDate = (): TE.TaskEither<Error, ReadonlyArray<ListsEvent>> => TE.right([]);
+
+export const appendNewListsEventsFromDatabase: AppendNewListsEventsFromDatabase = (sortedListEvents) => pipe(
+  sortedListEvents,
+  RA.last,
+  TE.fromOption(() => new Error('no lists events')),
+  TE.map((event) => event.date),
+  TE.chainW(queryDatabaseForEventsWithNewerDate),
+  TE.map((newEvents) => RA.concat(newEvents)(sortedListEvents)),
+);
