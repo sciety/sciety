941e875d5d0a08d3c6bdb40b77b700f9f0f65d43
David Moulton <d.moulton@elifesciences.org>
2022-02-28 15:29:02 +0000

    Prevent application failure when no events in db #1464

--- src/lists-service/append-new-lists-events-from-database.ts
+++ src/lists-service/append-new-lists-events-from-database.ts
@@ -1,3 +1,4 @@
+import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
@@ -5,6 +6,8 @@ import { Pool } from 'pg';
 import { ListsEvent } from './lists-event';
 import { queryDatabaseForEventsWithNewerDate } from './query-database-for-events-with-newer-date';
 import { Logger, loggerIO } from '../infrastructure/logger';
+
+const defaultCheckpoint = () => new Date('1970');
 
 type AppendNewListsEventsFromDatabase = (pool: Pool, logger: Logger) => (listEvents: ReadonlyArray<ListsEvent>)
 => TE.TaskEither<Error, ReadonlyArray<ListsEvent>>;
@@ -14,8 +17,8 @@ export const appendNewListsEventsFromDatabase: AppendNewListsEventsFromDatabase 
 ) => (sortedListEvents) => pipe(
   sortedListEvents,
   RA.last,
-  TE.fromOption(() => new Error('no lists events')),
-  TE.map((event) => event.date),
-  TE.chainW(queryDatabaseForEventsWithNewerDate(pool, loggerIO(logger))),
+  O.map((event) => event.date),
+  O.getOrElse(defaultCheckpoint),
+  queryDatabaseForEventsWithNewerDate(pool, loggerIO(logger)),
   TE.map((newEvents) => RA.concat(newEvents)(sortedListEvents)),
 );
