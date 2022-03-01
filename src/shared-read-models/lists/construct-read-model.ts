33bca79bc09abced3018c14bcdf739fa8f0617a1
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-01 14:45:25 +0000

    Extract update func for readmodel to reduce indentation #1466

--- src/shared-read-models/lists/construct-read-model.ts
+++ src/shared-read-models/lists/construct-read-model.ts
@@ -7,37 +7,39 @@ import { ListId } from '../../types/list-id';
 
 type ReadModel = Map<ListId, List>;
 
+const updateReadmodel = (state: ReadModel, event: DomainEvent) => {
+  switch (event.type) {
+    case 'ListCreated':
+      return state.set(event.listId, {
+        name: event.name,
+        description: event.description,
+        ownerId: event.ownerId,
+        articleCount: 0,
+        lastUpdated: event.date,
+      });
+    case 'ArticleAddedToList':
+      // eslint-disable-next-line no-case-declarations
+      const existing: List = pipe(
+        state.get(event.listId),
+        O.fromNullable,
+        O.getOrElseW(() => { throw new Error(`Can't find list with following listId in the read model: ${event.listId}`); }),
+      );
+      return state.set(event.listId, {
+        ...existing,
+        articleCount: existing.articleCount + 1,
+        lastUpdated: event.date,
+      });
+    default:
+      return state;
+  }
+};
+
 export const constructReadModel = (
   events: ReadonlyArray<DomainEvent>,
 ): ReadModel => pipe(
   events,
   RA.reduce(
     new Map<ListId, List>(),
-    (state, event) => {
-      switch (event.type) {
-        case 'ListCreated':
-          return state.set(event.listId, {
-            name: event.name,
-            description: event.description,
-            ownerId: event.ownerId,
-            articleCount: 0,
-            lastUpdated: event.date,
-          });
-        case 'ArticleAddedToList':
-          // eslint-disable-next-line no-case-declarations
-          const existing: List = pipe(
-            state.get(event.listId),
-            O.fromNullable,
-            O.getOrElseW(() => { throw new Error(`Can't find list with following listId in the read model: ${event.listId}`); }),
-          );
-          return state.set(event.listId, {
-            ...existing,
-            articleCount: existing.articleCount + 1,
-            lastUpdated: event.date,
-          });
-        default:
-          return state;
-      }
-    },
+    updateReadmodel,
   ),
 );
