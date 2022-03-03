1c725e1c67370d2944b17e6f4ae816bcc3f10afc
Giorgio Sironi <g.sironi@elifesciences.org>
2022-03-03 15:07:04 +0000

    Isolate rendering and fallback for testing #1469

--- src/group-page/lists/lists.ts
+++ src/group-page/lists/lists.ts
@@ -1,8 +1,10 @@
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { callListsReadModelService } from './call-lists-read-model-service';
-import { toListOfListCards } from './to-list-of-list-cards';
+import { renderListOfListCardsWithFallback } from './render-list-of-list-cards-with-fallback';
+import { toListCardViewModel } from './to-list-card-view-model';
 import { DomainEvent } from '../../domain-events';
 import { Logger } from '../../infrastructure/logger';
 import * as DE from '../../types/data-error';
@@ -20,5 +22,6 @@ export const lists = (ports: Ports) => (group: Group): TE.TaskEither<DE.DataErro
   ports.getAllEvents,
   TE.rightTask,
   TE.chain(callListsReadModelService(ports.logger, group.id)),
-  TE.map(toListOfListCards),
+  TE.map(RA.map(toListCardViewModel)),
+  TE.map(renderListOfListCardsWithFallback),
 );
--- src/group-page/lists/render-list-of-list-cards-with-fallback.ts
+++ src/group-page/lists/render-list-of-list-cards-with-fallback.ts
@@ -1,9 +1,7 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { flow, pipe } from 'fp-ts/function';
-import { toListCardViewModel } from './to-list-card-view-model';
-import { renderListCard } from '../../shared-components/list-card/render-list-card';
+import { ListCardViewModel, renderListCard } from '../../shared-components/list-card/render-list-card';
 import { templateListItems } from '../../shared-components/list-items';
-import { List } from '../../shared-read-models/lists';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 
 const renderCards = (cards: ReadonlyArray<HtmlFragment>) => pipe(
@@ -19,17 +17,13 @@ const renderCards = (cards: ReadonlyArray<HtmlFragment>) => pipe(
   toHtmlFragment,
 );
 
-type ToListOfListCards = (lists: ReadonlyArray<List>)
+type RenderListOfListCardsWithFallback = (lists: ReadonlyArray<ListCardViewModel>)
 => HtmlFragment;
 
-export const toListOfListCards: ToListOfListCards = (lists) => pipe(
-  lists,
-  RA.map(toListCardViewModel),
-  RA.match(
-    () => toHtmlFragment('<p class="static-message">This group doesn\'t have any lists yet.</p>'),
-    flow(
-      RA.map(renderListCard),
-      renderCards,
-    ),
+export const renderListOfListCardsWithFallback: RenderListOfListCardsWithFallback = RA.match(
+  () => toHtmlFragment('<p class="static-message">This group doesn\'t have any lists yet.</p>'),
+  flow(
+    RA.map(renderListCard),
+    renderCards,
   ),
 );
