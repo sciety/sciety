5f5cac8ecadfe1dd4594a63408de19e81e172e72
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-10 15:12:08 +0000

    Extract function to wait for db table to exist #1481

--- src/infrastructure/get-events-from-database.ts
+++ src/infrastructure/get-events-from-database.ts
@@ -20,20 +20,24 @@ type EventRow = {
 
 const domainEventsCodec = t.readonlyArray(domainEventCodec);
 
+const waitForTableToExist = async (pool: Pool, logger: Logger) => {
+  logger('debug', 'Waiting for events table to exist');
+  // eslint-disable-next-line no-loops/no-loops, no-constant-condition
+  while (true) {
+    const queryResult = await pool.query<{ exists: boolean }>('SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = \'public\' AND tablename = \'events\')');
+    if (queryResult.rows[0].exists) {
+      break;
+    }
+    await T.delay(1000)(T.of(''))();
+  }
+};
+
 export const getEventsFromDatabase = (
   pool: Pool,
   logger: Logger,
 ): TE.TaskEither<Error, ReadonlyArray<RuntimeGeneratedEvent>> => pipe(
   TE.tryCatch(async () => {
-    logger('debug', 'Waiting for events table to exist');
-    // eslint-disable-next-line no-loops/no-loops, no-constant-condition
-    while (true) {
-      const queryResult = await pool.query<{ exists: boolean }>('SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = \'public\' AND tablename = \'events\')');
-      if (queryResult.rows[0].exists) {
-        break;
-      }
-      await T.delay(1000)(T.of(''))();
-    }
+    await waitForTableToExist(pool, logger);
     return pool.query<EventRow>('SELECT id, type, date::text, payload FROM events');
   }, E.toError),
   TE.map((result) => result.rows),
