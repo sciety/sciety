6eaf15324ab9a4c82f58210e903a6030a2515950
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-14 14:35:18 +0000

    Increase apparent symmetry between app and lists-service db code for clarity

--- src/lists-service/get-lists-events-from-database.ts
+++ src/lists-service/get-lists-events-from-database.ts
@@ -9,20 +9,28 @@ import { EventRow, listsEventsCodec, selectAllListsEvents } from './events-table
 import { ListsEvent } from './lists-event';
 import { Logger } from '../infrastructure/logger';
 
+const waitForTableToExist = async (pool: Pool, logger: Logger) => {
+  logger('debug', 'Waiting for events table to exist');
+  // eslint-disable-next-line no-loops/no-loops, no-constant-condition
+  while (true) {
+    const queryResult = await pool.query<{ exists: boolean }>(`
+      SELECT EXISTS (
+        SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'events'
+      )
+    `);
+    if (queryResult.rows[0].exists) {
+      break;
+    }
+    await T.delay(1000)(T.of(''))();
+  }
+};
+
 export const getListsEventsFromDatabase = (
   pool: Pool,
   logger: Logger,
 ): TE.TaskEither<Error, ReadonlyArray<ListsEvent>> => pipe(
   TE.tryCatch(async () => {
-    logger('debug', 'Waiting for events table to exist');
-    // eslint-disable-next-line no-loops/no-loops, no-constant-condition
-    while (true) {
-      const queryResult = await pool.query<{ exists: boolean }>('SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = \'public\' AND tablename = \'events\')');
-      if (queryResult.rows[0].exists) {
-        break;
-      }
-      await T.delay(1000)(T.of(''))();
-    }
+    await waitForTableToExist(pool, logger);
     return pool.query<EventRow>(selectAllListsEvents);
   }, E.toError),
   TE.chainFirstTaskK((result) => T.of(logger('debug', 'Reading events from database', { count: result.rows.length }))),
