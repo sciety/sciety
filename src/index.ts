d864ce17a545e68b7169b5b98c9e518e82910019
Giorgio Sironi <g.sironi@elifesciences.org>
2022-03-16 10:37:54 +0000

    Populate Ecology eLife list on startup #1492

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ type ExecuteBackgroundPolicies = (adapters: Adapters) => T.Task<void>;
 
 const executeBackgroundPolicies: ExecuteBackgroundPolicies = (adapters) => async () => {
   const events = await adapters.getAllEvents();
-  const amountOfEventsToProcess = 0;
+  const amountOfEventsToProcess = events.length;
   const start = performance.now();
   // eslint-disable-next-line no-loops/no-loops
   for (let i = 0; i < amountOfEventsToProcess; i += 1) {
