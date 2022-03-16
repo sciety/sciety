6f2eae62709bd9fc644a3e374e9dea3d71627cd4
Giorgio Sironi <g.sironi@elifesciences.org>
2022-03-16 11:40:42 +0000

    Add todo test to drive a CreateList command #1492

---
+++ feature-test/create-list.test.ts
@@ -0,0 +1,5 @@
+describe('create a list', () => {
+  describe('when creating a list using the API', () => {
+    it.todo('displays the list on the lists tab of the owning group\'s page');
+  });
+})
