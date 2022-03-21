51f1c96ebdc715950aca3a3e6c52cfa8adffca0a
Giorgio Sironi <g.sironi@elifesciences.org>
2022-03-21 10:52:20 +0000

    Stop running background policy. Closes #1498

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ const executeBackgroundPolicies: ExecuteBackgroundPolicies = (adapters) => async
   });
 
   const events = await adapters.getAllEvents();
-  const amountOfEventsToProcess = events.length;
+  const amountOfEventsToProcess = 0;
   const start = performance.now();
   // eslint-disable-next-line no-loops/no-loops
   for (let i = 0; i < amountOfEventsToProcess; i += 1) {
