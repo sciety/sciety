04c7d61a0204648aaa3ab9777bf3a0f89ccdb63d
Giorgio Sironi <g.sironi@elifesciences.org>
2022-03-29 11:22:46 +0100

    Release checkbox for filtering evaluated articles #1512

--- src/shared-components/render-search-form.ts
+++ src/shared-components/render-search-form.ts
@@ -8,8 +8,8 @@ export const renderSearchForm = (query: string, evaluatedOnly: boolean): HtmlFra
       <input type="hidden" name="category" value="articles">
       <label for="searchText" class="visually-hidden">Search term</label>
       ${htmlEscape`<input value="${query}" id="searchText" name="query" placeholder="Find articles and evaluating groups…" class="search-form__text">`}
-      <!-- label for="searchEvaluatedOnlyFilter">Search only evaluated articles</label -->
-      <!-- input type="checkbox" name="evaluatedOnly" value="true" id="searchEvaluatedOnlyFilter"${evaluatedOnly ? ' checked' : ''} -->
+      <label for="searchEvaluatedOnlyFilter">Search only evaluated articles</label>
+      <input type="checkbox" name="evaluatedOnly" value="true" id="searchEvaluatedOnlyFilter"${evaluatedOnly ? ' checked' : ''}>
       <button type="reset" id="clearSearchText" class="search-form__clear visually-hidden">
         <img src="/static/images/clear-search-text-icon.svg" class="search-form__clear_icon" alt="">
       </button>
