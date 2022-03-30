57d7ee773e3e2e31369c4da98e2d353f569302c0
Giorgio Sironi <g.sironi@elifesciences.org>
2022-03-30 10:59:59 +0100

    Update canned searches with filter #1513

--- src/shared-components/render-example-searches.ts
+++ src/shared-components/render-example-searches.ts
@@ -1,5 +1,5 @@
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
 export const renderExampleSearches = (): HtmlFragment => toHtmlFragment(`
-  <p>Search examples: <a href="/search?query=COVID-19">COVID-19</a>, <a href="/search?query=C. elegans">C. elegans</a></p>
+  <p>Search examples: <a href="/search?query=COVID-19&evaluatedOnly=true">COVID-19 evaluated articles</a>, <a href="/search?query=C. elegans">All C. elegans articles</a></p>
 `);
