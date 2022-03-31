7134fa338cdc3d613ca3c62ff10ed76c69eec2f9
Giorgio Sironi <g.sironi@elifesciences.org>
2022-03-31 11:59:51 +0100

    Reduce duplication of arbitrary inputs in tests #1517

--- test/search-results-page/build-page-url.test.ts
+++ test/search-results-page/build-page-url.test.ts
@@ -1,14 +1,19 @@
 import { buildPageUrl } from '../../src/search-results-page/build-page-url';
 import { arbitraryBoolean, arbitraryString } from '../helpers';
 
-const arbitraryCategory = () => (arbitraryBoolean() ? 'articles' : 'groups');
+const arbitraryCategory = (): 'articles' | 'groups' => (arbitraryBoolean() ? 'articles' : 'groups');
 
 describe('build-page-url', () => {
+  const defaultParams = {
+    category: arbitraryCategory(),
+    query: arbitraryString(),
+    evaluatedOnly: arbitraryBoolean(),
+  };
+
   it('builds the URL with the correct category', () => {
     const result = buildPageUrl({
+      ...defaultParams,
       category: 'articles',
-      query: arbitraryString(),
-      evaluatedOnly: arbitraryBoolean(),
     });
 
     expect(result).toContain('category=articles');
@@ -17,9 +22,8 @@ describe('build-page-url', () => {
   it('builds the URL with the correct query', () => {
     const query = 'a search term';
     const result = buildPageUrl({
+      ...defaultParams,
       query,
-      category: arbitraryCategory(),
-      evaluatedOnly: arbitraryBoolean(),
     });
 
     expect(result).toContain(`query=${query}`);
@@ -28,9 +32,8 @@ describe('build-page-url', () => {
   describe('when evaluatedOnly is true', () => {
     it('builds the URL with the evaluatedOnly filter set', () => {
       const result = buildPageUrl({
+        ...defaultParams,
         evaluatedOnly: true,
-        query: arbitraryString(),
-        category: arbitraryCategory(),
       });
 
       expect(result).toContain('evaluatedOnly=true');
@@ -40,9 +43,8 @@ describe('build-page-url', () => {
   describe('when evaluatedOnly is false', () => {
     it('builds the URL without the evaluatedOnly filter set', () => {
       const result = buildPageUrl({
+        ...defaultParams,
         evaluatedOnly: false,
-        query: arbitraryString(),
-        category: arbitraryCategory(),
       });
 
       expect(result).not.toContain('evaluatedOnly');
