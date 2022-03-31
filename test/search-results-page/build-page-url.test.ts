395d848aded89a6e8721f991abe6bd73c49b96ef
Giorgio Sironi <g.sironi@elifesciences.org>
2022-03-31 11:51:11 +0100

    Implement test for query, staying clear of unclear HTML escaping #1517

--- test/search-results-page/build-page-url.test.ts
+++ test/search-results-page/build-page-url.test.ts
@@ -1,6 +1,8 @@
 import { buildPageUrl } from '../../src/search-results-page/build-page-url';
 import { arbitraryBoolean, arbitraryString } from '../helpers';
 
+const arbitraryCategory = () => (arbitraryBoolean() ? 'articles' : 'groups');
+
 describe('build-page-url', () => {
   it('builds the URL with the correct category', () => {
     const result = buildPageUrl({
@@ -12,7 +14,16 @@ describe('build-page-url', () => {
     expect(result).toContain('category=articles');
   });
 
-  it.todo('builds the URL with the correct query');
+  it('builds the URL with the correct query', () => {
+    const query = 'a search term';
+    const result = buildPageUrl({
+      query,
+      category: arbitraryCategory(),
+      evaluatedOnly: arbitraryBoolean(),
+    });
+
+    expect(result).toContain(`query=${query}`);
+  });
 
   describe('when evaluatedOnly is true', () => {
     it.todo('builds the URL with the evaluatedOnly filter set');
