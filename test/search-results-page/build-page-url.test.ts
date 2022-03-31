25005f3a156a7b94a24cf40898078a71eea7fdf1
Giorgio Sironi <g.sironi@elifesciences.org>
2022-03-31 11:32:36 +0100

    Substitute test list for renderer with test list for a pure function #1517

---
+++ test/search-results-page/build-page-url.test.ts
@@ -0,0 +1,13 @@
+describe('build-page-url', () => {
+  it.todo('builds the URL with the correct category');
+
+  it.todo('builds the URL with the correct query');
+
+  describe('when evaluatedOnly is true', () => {
+    it.todo('builds the URL with the evaluatedOnly filter set');
+  });
+
+  describe('when evaluatedOnly is false', () => {
+    it.todo('builds the URL without the evaluatedOnly filter set');
+  });
+});
