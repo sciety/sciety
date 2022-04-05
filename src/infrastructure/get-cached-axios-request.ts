1b0cd9fabcb7b4ea3188a38cb75c0d25c0fcb216
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-04-05 13:49:49 +0100

    Replace axios-cache-adapter with axios-cache-interceptor (only in-memory cache for now)
    Required upgrade of axios to get types to work.

--- src/infrastructure/get-cached-axios-request.ts
+++ src/infrastructure/get-cached-axios-request.ts
@@ -1,30 +1,15 @@
-import axios from 'axios';
-import { RedisStore, setupCache } from 'axios-cache-adapter';
-import redis from 'redis';
+import Axios from 'axios';
+import { setupCache } from 'axios-cache-interceptor';
 import { Logger } from './logger';
 
-let store;
-if (process.env.APP_CACHE === 'redis') {
-  const client = redis.createClient({
-    host: 'sciety_cache',
-  });
-  store = new RedisStore(client);
-}
-
-const cache = setupCache({
-  maxAge: 24 * 60 * 60 * 1000,
-  store,
-});
-const api = axios.create({
-  adapter: cache.adapter,
-});
+const cachedAxios = setupCache(Axios.create());
 
 export const getCachedAxiosRequest = (
   logger: Logger,
 ) => async <U>(url: string, headers: Record<string, string>): Promise<U> => {
   const startTime = new Date();
-  const response = await api.get<U>(url, { headers });
-  if (response.request.fromCache) {
+  const response = await cachedAxios.get<U>(url, { headers });
+  if (response.cached) {
     logger('debug', 'Axios cache hit', {
       url,
     });
