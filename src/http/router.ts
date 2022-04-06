8908f8eba8c9c38bed42eb0dadd24f081b141cf9
Giorgio Sironi <g.sironi@elifesciences.org>
2022-04-06 14:24:20 +0100

    Tidy up annotation form layout #1529

--- src/http/router.ts
+++ src/http/router.ts
@@ -382,15 +382,11 @@ export const createRouter = (adapters: Adapters): Router => {
       content: toHtmlFragment(`
         <h1>Create an annotation for an article on <a href="https://sciety.org/users/AvasthiReading/lists/saved-articles">Prachee's list</a></h1>
         <form>
-          <div class="annotation-form-row">
-            <label for="annotationContent">Annotation content</label>
-            <textarea id="annotationContent" name="annotationContent" cols="100" rows="10"></textarea>
-          </div>
-          <div class="annotation-form-row">
-            <label for="articleId">Article DOI</label>
-            <input type="text" name="articleId" id="articleId" placeholder="10.1101/2022.04.01.486801">
-          </div>
-          <div>
+          <label for="annotationContent">Annotation content</label>
+          <textarea id="annotationContent" name="annotationContent" rows="10" class="annotation-form-content"></textarea>
+          <label for="articleId">Article DOI</label>
+          <input type="text" name="articleId" id="articleId" placeholder="10.1101/2022.04.01.486801" class="annotation-form-article-id">
+          <div class="annotation-form-controls">
             <button class="annotation-form-submit">Create annotation</button>
             <button type="reset" class="annotation-form-reset">Reset</button>
           </div>
