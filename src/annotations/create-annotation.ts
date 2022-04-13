2cd9e10600a8d72bf91d8737a40eb54b73c6338f
Giorgio Sironi <g.sironi@elifesciences.org>
2022-04-13 15:25:25 +0100

    Move translation and logging into CommandHandler #1534

--- src/annotations/create-annotation.ts
+++ src/annotations/create-annotation.ts
@@ -1,7 +1,38 @@
+import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import { Adapters } from '../infrastructure';
 import { CommandResult } from '../types/command-result';
 
+type CreateAnnotationCommand = {
+  content: string,
+  target: {
+    articleId: string,
+  },
+};
+
+type Body = {
+  annotationContent: string,
+  articleId: string,
+};
+
+const translateCommand = ({ annotationContent, articleId }: Body): CreateAnnotationCommand => ({
+  content: annotationContent,
+  target: {
+    articleId,
+  },
+});
+
 type CreateAnnotation = (adapters: Adapters) => (input: unknown) => TE.TaskEither<string, CommandResult>;
 
-export const createAnnotation: CreateAnnotation = () => () => TE.left('');
+export const createAnnotation: CreateAnnotation = (adapters) => (input) => pipe(
+  input as Body,
+  translateCommand,
+  TE.right,
+  TE.chainFirstTaskK(
+    (command) => T.of(
+      adapters.logger('debug', 'Received CreateAnnotation command', { command }),
+    ),
+  ),
+  TE.map(() => 'no-events-created'),
+);
--- src/http/handle-create-annotation-command.ts
+++ src/http/handle-create-annotation-command.ts
@@ -4,59 +4,22 @@ import { Middleware } from 'koa';
 import bodyParser from 'koa-bodyparser';
 import compose from 'koa-compose';
 import { redirectBack } from './redirect-back';
-import { Adapters, Logger } from '../infrastructure';
+import { Adapters } from '../infrastructure';
 import { CommandResult } from '../types/command-result';
 
 type ScietyApiCommandHandler = (adapters: Adapters) => (input: unknown) => TE.TaskEither<string, CommandResult>;
 
 type HandleCreateAnnotationCommand = (adapters: Adapters, handler: ScietyApiCommandHandler) => Middleware;
 
-type CreateAnnotationCommand = {
-  content: string,
-  target: {
-    articleId: string,
-  },
-};
-
-const logCommand = (logger: Logger, url: string) => (command: CreateAnnotationCommand) => logger(
-  'debug',
-  'Received command',
-  {
-    command,
-    url,
-  },
-);
-
-type Body = {
-  annotationContent: string,
-  articleId: string,
-};
-
-const translateCommand = ({ annotationContent, articleId }: Body): CreateAnnotationCommand => ({
-  content: annotationContent,
-  target: {
-    articleId,
-  },
-});
-
-const translateAndLogCommand = (logger: Logger): Middleware => async (context, next) => {
-  pipe(
-    context.request.body,
-    translateCommand,
-    logCommand(logger, context.request.url),
-  );
-
-  await next();
-};
-
 export const handleCreateAnnotationCommand: HandleCreateAnnotationCommand = (adapters, handler) => compose([
   bodyParser({ enableTypes: ['form'] }),
-  translateAndLogCommand(adapters.logger),
-  async (context) => {
+  async (context, next) => {
     await pipe(
       context.request.body,
       handler(adapters),
     )();
+
+    await next();
   },
   redirectBack,
 ]);
