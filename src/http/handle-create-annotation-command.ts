3e5725dd3278359a8d4a14a0d8c0f61cb47f4adc
David Moulton <d.moulton@elifesciences.org>
2022-04-13 10:59:34 +0100

    Log & discard command derived from create annotation form POST #1534

--- src/http/handle-create-annotation-command.ts
+++ src/http/handle-create-annotation-command.ts
@@ -1,18 +1,40 @@
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import { Middleware } from 'koa';
 import bodyParser from 'koa-bodyparser';
 import compose from 'koa-compose';
-import { logRequestBody } from './log-request-body';
 import { redirectBack } from './redirect-back';
-import { Adapters } from '../infrastructure';
+import { Adapters, Logger } from '../infrastructure';
 import { CommandResult } from '../types/command-result';
 
 type ScietyApiCommandHandler = (adapters: Adapters) => (input: unknown) => TE.TaskEither<string, CommandResult>;
 
 type HandleCreateAnnotationCommand = (adapters: Adapters, handler: ScietyApiCommandHandler) => Middleware;
 
+const logCommand = (logger: Logger): Middleware => async (context, next) => {
+  pipe(
+    context.request.body,
+    (body) => ({
+      content: body.annotationContent,
+      target: {
+        articleId: body.articleId,
+      },
+    }),
+    (command) => logger(
+      'debug',
+      'Received command',
+      {
+        command,
+        url: context.request.url,
+      },
+    ),
+  );
+
+  await next();
+};
+
 export const handleCreateAnnotationCommand: HandleCreateAnnotationCommand = (adapters) => compose([
   bodyParser({ enableTypes: ['form'] }),
-  logRequestBody(adapters.logger),
+  logCommand(adapters.logger),
   redirectBack,
 ]);
