70d48a2afef76040f650aea7e7ccc6836fe8e3b5
David Moulton <d.moulton@elifesciences.org>
2022-04-14 09:53:36 +0100

    Add the five latest annotations to articles in AvastiReading's list #1536

--- src/user-list-page/saved-articles/saved-articles.ts
+++ src/user-list-page/saved-articles/saved-articles.ts
@@ -66,6 +66,22 @@ const getAnnotationContentByUserListTarget: GetAnnotationContentByUserListTarget
   if (articleId.value === '10.1101/2022.03.31.486371') {
     content = 'Whoa! Successful ID of early endosomal cargoes via pull-down of an early endosomal marker followed by proteomics.';
   }
+
+  if (articleId.value === '10.1101/2022.04.06.487275') {
+    content = 'How interesting! A peptide screen to identify degrons with E3 ubiquitin ligase specificity in yeast followed by extraction of conserved properties identifies transmembrane domain containing peptides as likely degrons.';
+  }
+  if (articleId.value === '10.1101/2022.04.05.487209') {
+    content = 'Recognizing the utility of not using <strong>exclusively</strong> cutting edge methods for everything, this important study combines bulk RNAseq (high sensitivity due to capturing more low expression and noncoding RNAs) and single cell RNAseq (high specificity) for a more comprehensive transcriptomic picture.';
+  }
+  if (articleId.value === '10.1101/2021.05.19.444861') {
+    content = 'This is wild! Chlamydomonas flagella were isolated, demembranated, attached to polystyrene beads and reactivated to reconstitute flagellar movement. This yields some interesting insights through experimentation and simulation: Increased calcium concentration affects flagellar waveform and trajectory of bead movement and surprisingly, larger beads have increased rather than decreased velocity.';
+  }
+  if (articleId.value === '10.1101/2022.04.07.487451') {
+    content = 'Interesting role for ARP2/3 complex in peroxisome autophagy in plants!';
+  }
+  if (articleId.value === '10.1101/2022.04.06.487340') {
+    content = 'This polarity of actin networks and myosin traffic is interesting in light of the parallels between cilium biogenesis and the immune synapse function.';
+  }
   return content;
 };
 
