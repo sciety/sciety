e8514c75b16c83fe6c1f29743044d601549d2d5d
David Moulton <d.moulton@elifesciences.org>
2022-04-14 11:26:06 +0100

    Extract query function to shared read model #1536

--- src/user-list-page/saved-articles/saved-articles.ts
+++ src/user-list-page/saved-articles/saved-articles.ts
@@ -12,10 +12,13 @@ import { DomainEvent } from '../../domain-events';
 import { renderUnsaveForm } from '../../save-article/render-unsave-form';
 import { FindVersionsForArticleDoi, getLatestArticleVersionDate, renderArticleCardWithControlsAndOptionalAnnotation } from '../../shared-components/article-card';
 import { populateArticleViewModel } from '../../shared-components/article-card/populate-article-view-model';
+import {
+  getAnnotationContentByUserListTarget,
+} from '../../shared-read-models/annotations';
 import { ArticleAuthors } from '../../types/article-authors';
 import { ArticleServer } from '../../types/article-server';
 import { Doi } from '../../types/doi';
-import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
+import { HtmlFragment } from '../../types/html-fragment';
 import { SanitisedHtmlFragment } from '../../types/sanitised-html-fragment';
 import { UserId } from '../../types/user-id';
 
@@ -43,49 +46,6 @@ const getArticleCardControls = (loggedInUserId: O.Option<UserId>, listOwnerId: U
   O.filter((userId) => userId === listOwnerId),
   O.map(() => renderUnsaveForm(articleId)),
 );
-
-type GetAnnotationContentByUserListTarget = (articleId: Doi, listOwnerId: UserId)
-=> (events: ReadonlyArray<DomainEvent>)
-=> HtmlFragment | undefined;
-
-const getAnnotationContentByUserListTarget: GetAnnotationContentByUserListTarget = (articleId, listOwnerId) => () => {
-  if (listOwnerId !== '1412019815619911685') {
-    return undefined;
-  }
-  let content: string | undefined;
-  if (articleId.value === '10.1101/2022.03.29.486216') {
-    content = 'A 2.2 angstrom resolution structures of muscle actin filaments in ATP, ADP-Pi and ADP states. Many new insights here about the surprising stability of ADP actin, mechanism of ATP hydrolysis, cofilin binding and more.';
-  }
-  if (articleId.value === '10.1101/2021.05.26.445751') {
-    content = 'Truly exquisite characterization of kinesins in the malaria parasite. Check out the spectacular expansion microscopy images in figure 7A!';
-  }
-  if (articleId.value === '10.1101/2022.03.31.486578') {
-    content = 'This is a beautiful characterization of caveolae. In addition to platinum replica EM of these plasma membrane invaginations in various cell types, this work includes molecular identification of proteins associated with caveolae of different curvatures by STED-CLEM (very high-resolution correlative light electron microscopy).';
-  }
-  if (articleId.value === '10.1101/2022.03.31.486371') {
-    content = 'Whoa! Successful ID of early endosomal cargoes via pull-down of an early endosomal marker followed by proteomics.';
-  }
-
-  if (articleId.value === '10.1101/2022.04.06.487275') {
-    content = 'How interesting! A peptide screen to identify degrons with E3 ubiquitin ligase specificity in yeast followed by extraction of conserved properties identifies transmembrane domain containing peptides as likely degrons.';
-  }
-  if (articleId.value === '10.1101/2022.04.05.487209') {
-    content = 'Recognizing the utility of not using <strong>exclusively</strong> cutting edge methods for everything, this important study combines bulk RNAseq (high sensitivity due to capturing more low expression and noncoding RNAs) and single cell RNAseq (high specificity) for a more comprehensive transcriptomic picture.';
-  }
-  if (articleId.value === '10.1101/2021.05.19.444861') {
-    content = 'This is wild! Chlamydomonas flagella were isolated, demembranated, attached to polystyrene beads and reactivated to reconstitute flagellar movement. This yields some interesting insights through experimentation and simulation: Increased calcium concentration affects flagellar waveform and trajectory of bead movement and surprisingly, larger beads have increased rather than decreased velocity.';
-  }
-  if (articleId.value === '10.1101/2022.04.07.487451') {
-    content = 'Interesting role for ARP2/3 complex in peroxisome autophagy in plants!';
-  }
-  if (articleId.value === '10.1101/2022.04.06.487340') {
-    content = 'This polarity of actin networks and myosin traffic is interesting in light of the parallels between cilium biogenesis and the immune synapse function.';
-  }
-  if (!content) {
-    return undefined;
-  }
-  return toHtmlFragment(content);
-};
 
 export const savedArticles: SavedArticles = (ports) => (dois, loggedInUser, listOwnerId) => pipe(
   dois,
