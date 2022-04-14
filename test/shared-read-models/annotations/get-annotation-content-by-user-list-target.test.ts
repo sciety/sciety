34a065f0e101076163d502e0da2ce28a67b7ab69
Giorgio Sironi <g.sironi@elifesciences.org>
2022-04-14 11:46:24 +0100

    Implement the more concrete current behavior test #1536

--- test/shared-read-models/annotations/get-annotation-content-by-user-list-target.test.ts
+++ test/shared-read-models/annotations/get-annotation-content-by-user-list-target.test.ts
@@ -1,6 +1,18 @@
+import { getAnnotationContentByUserListTarget } from '../../../src/shared-read-models/annotations';
+import { Doi } from '../../../src/types/doi';
+import { toUserId } from '../../../src/types/user-id';
+
 describe('get-annotation-content-by-user-list-target', () => {
   describe('when an article in the AvasthiReading list has been annotated', () => {
-    it.todo('returns the annotation content as HTML');
+    const annotationContent = 'Interesting role for ARP2/3 complex in peroxisome autophagy in plants!';
+    const result = getAnnotationContentByUserListTarget(
+      new Doi('10.1101/2022.04.07.487451'),
+      toUserId('1412019815619911685'),
+    )([]);
+
+    it('returns the annotation content as HTML', () => {
+      expect(result).toBe(annotationContent);
+    });
   });
 
   describe('when an article in the AvasthiReading list has not been annotated', () => {
