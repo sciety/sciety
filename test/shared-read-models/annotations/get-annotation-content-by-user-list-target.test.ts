3dafe3ee0e9bded87b6dc57d7ed644f4dfdca386
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-04-14 11:41:23 +0100

    Test list to cover existing behaviour of the query #1536

---
+++ test/shared-read-models/annotations/get-annotation-content-by-user-list-target.test.ts
@@ -0,0 +1,13 @@
+describe('get-annotation-content-by-user-list-target', () => {
+  describe('when an article in the AvasthiReading list has been annotated', () => {
+    it.todo('returns the annotation content as HTML');
+  });
+
+  describe('when an article in the AvasthiReading list has not been annotated', () => {
+    it.todo('returns undefined');
+  });
+
+  describe('when an article in any other list is the target', () => {
+    it.todo('returns undefined');
+  });
+});
