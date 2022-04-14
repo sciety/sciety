b0040d62431b81898afe16548912ba13624453f5
Giorgio Sironi <g.sironi@elifesciences.org>
2022-04-14 13:11:14 +0100

    Replicate first test, based on events #1536

--- test/shared-read-models/annotations/get-annotation-content-by-user-list-target.test.ts
+++ test/shared-read-models/annotations/get-annotation-content-by-user-list-target.test.ts
@@ -1,3 +1,4 @@
+import { pipe } from 'fp-ts/function';
 import { getAnnotationContentByUserListTarget } from '../../../src/shared-read-models/annotations';
 import { Doi } from '../../../src/types/doi';
 import { toUserId } from '../../../src/types/user-id';
@@ -7,37 +8,55 @@ import { arbitraryUserId } from '../../types/user-id.helper';
 describe('get-annotation-content-by-user-list-target', () => {
   const avasthiReadingUserId = toUserId('1412019815619911685');
 
-  describe('when an article in the AvasthiReading list has been annotated', () => {
-    const annotationContent = 'Interesting role for ARP2/3 complex in peroxisome autophagy in plants!';
-    const result = getAnnotationContentByUserListTarget(
-      new Doi('10.1101/2022.04.07.487451'),
-      avasthiReadingUserId,
-    )([]);
+  describe('existing hardcoded behavior', () => {
+    describe('when an article in the AvasthiReading list has been annotated', () => {
+      const annotationContent = 'Interesting role for ARP2/3 complex in peroxisome autophagy in plants!';
+      const result = getAnnotationContentByUserListTarget(
+        new Doi('10.1101/2022.04.07.487451'),
+        avasthiReadingUserId,
+      )([]);
 
-    it('returns the annotation content as HTML', () => {
-      expect(result).toBe(annotationContent);
+      it('returns the annotation content as HTML', () => {
+        expect(result).toBe(annotationContent);
+      });
+    });
+
+    describe('when an article in the AvasthiReading list has not been annotated', () => {
+      const result = getAnnotationContentByUserListTarget(
+        arbitraryDoi(),
+        avasthiReadingUserId,
+      )([]);
+
+      it('returns undefined', () => {
+        expect(result).toBeUndefined();
+      });
+    });
+
+    describe('when an article in any other list is the target', () => {
+      const result = getAnnotationContentByUserListTarget(
+        arbitraryDoi(),
+        arbitraryUserId(),
+      )([]);
+
+      it('returns undefined', () => {
+        expect(result).toBeUndefined();
+      });
     });
   });
 
-  describe('when an article in the AvasthiReading list has not been annotated', () => {
-    const result = getAnnotationContentByUserListTarget(
-      arbitraryDoi(),
-      avasthiReadingUserId,
-    )([]);
+  describe('hardcoded knowledge of user ids, but content and target coming from events', () => {
+    describe('when an article in the AvasthiReading list has not been annotated', () => {
+      const result = pipe(
+        [],
+        getAnnotationContentByUserListTarget(
+          arbitraryDoi(),
+          avasthiReadingUserId,
+        ),
+      );
 
-    it('returns undefined', () => {
-      expect(result).toBeUndefined();
-    });
-  });
-
-  describe('when an article in any other list is the target', () => {
-    const result = getAnnotationContentByUserListTarget(
-      arbitraryDoi(),
-      arbitraryUserId(),
-    )([]);
-
-    it('returns undefined', () => {
-      expect(result).toBeUndefined();
+      it('returns undefined', () => {
+        expect(result).toBeUndefined();
+      });
     });
   });
 });
