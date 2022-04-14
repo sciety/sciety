54385a761f1278071c903d19b2f7ae562f6c6588
David Moulton <d.moulton@elifesciences.org>
2022-04-14 11:51:31 +0100

    Implement another test for current behaviour #1536

--- test/shared-read-models/annotations/get-annotation-content-by-user-list-target.test.ts
+++ test/shared-read-models/annotations/get-annotation-content-by-user-list-target.test.ts
@@ -1,13 +1,16 @@
 import { getAnnotationContentByUserListTarget } from '../../../src/shared-read-models/annotations';
 import { Doi } from '../../../src/types/doi';
 import { toUserId } from '../../../src/types/user-id';
+import { arbitraryDoi } from '../../types/doi.helper';
 
 describe('get-annotation-content-by-user-list-target', () => {
+  const avasthiReadingUserId = toUserId('1412019815619911685');
+
   describe('when an article in the AvasthiReading list has been annotated', () => {
     const annotationContent = 'Interesting role for ARP2/3 complex in peroxisome autophagy in plants!';
     const result = getAnnotationContentByUserListTarget(
       new Doi('10.1101/2022.04.07.487451'),
-      toUserId('1412019815619911685'),
+      avasthiReadingUserId,
     )([]);
 
     it('returns the annotation content as HTML', () => {
@@ -16,7 +19,14 @@ describe('get-annotation-content-by-user-list-target', () => {
   });
 
   describe('when an article in the AvasthiReading list has not been annotated', () => {
-    it.todo('returns undefined');
+    const result = getAnnotationContentByUserListTarget(
+      arbitraryDoi(),
+      avasthiReadingUserId,
+    )([]);
+
+    it('returns undefined', () => {
+      expect(result).toBeUndefined();
+    });
   });
 
   describe('when an article in any other list is the target', () => {
