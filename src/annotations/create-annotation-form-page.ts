2ba06b3bc4f6ead7bad3bb059dacb27b64cedb67
Giorgio Sironi <g.sironi@elifesciences.org>
2022-04-20 11:59:20 +0100

    Populate form using params #1541

--- src/annotations/create-annotation-form-page.ts
+++ src/annotations/create-annotation-form-page.ts
@@ -3,9 +3,9 @@ import { toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 import { RenderPageError } from '../types/render-page-error';
 
-type CreateAnnotationFormPage = () => TE.TaskEither<RenderPageError, Page>;
+type CreateAnnotationFormPage = (queryParams: { articleId: string }) => TE.TaskEither<RenderPageError, Page>;
 
-export const createAnnotationFormPage: CreateAnnotationFormPage = () => TE.right({
+export const createAnnotationFormPage: CreateAnnotationFormPage = ({ articleId }) => TE.right({
   title: 'Create an annotation',
   content: toHtmlFragment(`
     <h1>Create an annotation for an article on <a href="https://sciety.org/users/AvasthiReading/lists/saved-articles">Prachee's list</a></h1>
@@ -13,7 +13,7 @@ export const createAnnotationFormPage: CreateAnnotationFormPage = () => TE.right
       <label for="annotationContent">Annotation content</label>
       <textarea id="annotationContent" name="annotationContent" rows="10" class="annotation-form-content"></textarea>
       <label for="articleId">Article DOI</label>
-      <input type="text" name="articleId" id="articleId" placeholder="10.1101/2022.04.01.486801" class="annotation-form-article-id">
+      <input type="text" name="articleId" id="articleId" placeholder="10.1101/2022.04.01.486801" class="annotation-form-article-id" value="${articleId}">
       <div class="annotation-form-controls">
         <button class="annotation-form-submit">Create annotation</button>
         <button type="reset" class="annotation-form-reset">Reset</button>
