fb101eef644fb5aa00eb08384b712db9f2cb40a4
Giorgio Sironi <g.sironi@elifesciences.org>
2022-04-20 15:33:27 +0100

    Allow form to be loaded without a articleId #1541

--- src/annotations/create-annotation-form-page.ts
+++ src/annotations/create-annotation-form-page.ts
@@ -5,10 +5,10 @@ import { Page } from '../types/page';
 import { RenderPageError } from '../types/render-page-error';
 
 export const paramsCodec = t.type({
-  articleId: t.string,
+  articleId: t.union([t.string, t.undefined]),
 });
 
-type CreateAnnotationFormPage = (queryParams: { articleId: string }) => TE.TaskEither<RenderPageError, Page>;
+type CreateAnnotationFormPage = (queryParams: { articleId?: string }) => TE.TaskEither<RenderPageError, Page>;
 
 export const createAnnotationFormPage: CreateAnnotationFormPage = ({ articleId }) => TE.right({
   title: 'Create an annotation',
@@ -18,7 +18,7 @@ export const createAnnotationFormPage: CreateAnnotationFormPage = ({ articleId }
       <label for="annotationContent">Annotation content</label>
       <textarea id="annotationContent" name="annotationContent" rows="10" class="annotation-form-content"></textarea>
       <label for="articleId">Article DOI</label>
-      <input type="text" name="articleId" id="articleId" placeholder="10.1101/2022.04.01.486801" class="annotation-form-article-id" value="${articleId}">
+      <input type="text" name="articleId" id="articleId" placeholder="10.1101/2022.04.01.486801" class="annotation-form-article-id" value="${articleId ?? ''}">
       <div class="annotation-form-controls">
         <button class="annotation-form-submit">Create annotation</button>
         <button type="reset" class="annotation-form-reset">Reset</button>
