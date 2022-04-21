bd31934fc80b16fd9674c9e0d6dca03c58f564ce
Giorgio Sironi <g.sironi@elifesciences.org>
2022-04-21 14:23:43 +0100

    Introduce an Option for articleId prepopulated value #1541

--- src/annotations/create-annotation-form-page.ts
+++ src/annotations/create-annotation-form-page.ts
@@ -1,14 +1,17 @@
+import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
+import * as tt from 'io-ts-types';
 import { toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 import { RenderPageError } from '../types/render-page-error';
 
 export const paramsCodec = t.type({
-  articleId: t.union([t.string, t.undefined]),
+  articleId: tt.optionFromNullable(t.string),
 });
 
-type CreateAnnotationFormPage = (params: { articleId?: string }) => TE.TaskEither<RenderPageError, Page>;
+type CreateAnnotationFormPage = (params: { articleId: O.Option<string> }) => TE.TaskEither<RenderPageError, Page>;
 
 export const createAnnotationFormPage: CreateAnnotationFormPage = ({ articleId }) => TE.right({
   title: 'Create an annotation',
@@ -18,7 +21,10 @@ export const createAnnotationFormPage: CreateAnnotationFormPage = ({ articleId }
       <label for="annotationContent">Annotation content</label>
       <textarea id="annotationContent" name="annotationContent" rows="10" class="annotation-form-content"></textarea>
       <label for="articleId">Article DOI</label>
-      <input type="text" name="articleId" id="articleId" placeholder="10.1101/2022.04.01.486801" class="annotation-form-article-id" value="${articleId ?? ''}">
+      <input type="text" name="articleId" id="articleId" placeholder="10.1101/2022.04.01.486801" class="annotation-form-article-id" value="${pipe(articleId, O.fold(
+        () => '',
+        (id) => id,
+      ))}">
       <div class="annotation-form-controls">
         <button class="annotation-form-submit">Create annotation</button>
         <button type="reset" class="annotation-form-reset">Reset</button>
