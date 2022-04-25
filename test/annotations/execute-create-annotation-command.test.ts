8f6b5faa961e5b00ef10f3307225f81eb89526eb
LinaKind <l.kinduryte@elifesciences.org>
2022-04-25 10:29:53 +0100

    Start test list #1547

---
+++ test/annotations/execute-create-annotation-command.test.ts
@@ -0,0 +1,5 @@
+describe('execute-create-annotation-command', () => {
+  describe('given the target does not have an annotation, when the command is executed', () => {
+    it.todo('returns an AnnotationCreated event');
+  });
+});
