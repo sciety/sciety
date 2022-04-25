e380c6b970fb735a50548655482fcdce1a6ca75e
David Moulton <d.moulton@elifesciences.org>
2022-04-25 10:37:17 +0100

    Add new todo test for creating annotations #1547

--- test/annotations/execute-create-annotation-command.test.ts
+++ test/annotations/execute-create-annotation-command.test.ts
@@ -2,4 +2,8 @@ describe('execute-create-annotation-command', () => {
   describe('given the target does not have an annotation, when the command is executed', () => {
     it.todo('returns an AnnotationCreated event');
   });
+
+  describe('given the target already has an annotation, when the command is executed', () => {
+    it.todo('returns no events');
+  });
 });
