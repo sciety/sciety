b0556538f43c48b45463a3e81800aff568e93944
LinaKind <l.kinduryte@elifesciences.org>
2022-04-27 15:14:30 +0100

    Change fetchDataciteReview to use JSON rather than RDF #1555

--- src/third-parties/datacite/fetch-datacite-review.ts
+++ src/third-parties/datacite/fetch-datacite-review.ts
@@ -1,48 +1,35 @@
 import { URL } from 'url';
-import { namedNode } from '@rdfjs/data-model';
-import { schema } from '@tpluscode/rdf-ns-builders';
-import * as E from 'fp-ts/Either';
+import axios from 'axios';
+import { Json } from 'fp-ts/Json';
 import * as TE from 'fp-ts/TaskEither';
 import { flow, pipe } from 'fp-ts/function';
-import type { NamedNode } from 'rdf-js';
 import { FetchDataset } from '../../infrastructure/fetch-dataset';
 import { EvaluationFetcher } from '../../infrastructure/fetch-review';
 import { Logger } from '../../infrastructure/logger';
 import * as DE from '../../types/data-error';
 import { toHtmlFragment } from '../../types/html-fragment';
 
-const fetchReviewContent = (
-  fetchDataset: FetchDataset,
-  logger: Logger,
-  reviewIri: NamedNode,
-) => pipe(
-  TE.tryCatch(
-    async () => fetchDataset(reviewIri),
-    () => DE.unavailable, // TODO might be DE.notFound
-  ),
-  TE.chainEitherK(flow(
-    (graph) => graph.out(schema.description).value,
-    E.fromNullable(DE.unavailable),
-    E.map(flow(
-      toHtmlFragment,
-      (fullText) => ({
-        fullText,
-        url: new URL(reviewIri.value),
-      }),
-    )),
-  )),
-  TE.map((evaluation) => {
-    logger('debug', 'Retrieved evaluation', { evaluation });
-    return evaluation;
-  }),
-);
-
 export const fetchDataciteReview = (fetchDataset: FetchDataset, logger: Logger): EvaluationFetcher => flow(
-  (key) => `https://doi.org/${key}`,
+  (key) => `https://data.crosscite.org/${encodeURIComponent(key)}`,
   (url) => {
     logger('debug', 'Fetching evaluation from Datacite', { url });
     return url;
   },
-  namedNode,
-  (reviewIri) => fetchReviewContent(fetchDataset, logger, reviewIri),
+  (url) => {
+    const res = TE.tryCatch(
+      async () => axios.get <Json>(url, {
+        headers: {
+          Accept: 'application/vnd.codemeta.ld+json',
+        },
+      }),
+      () => DE.unavailable,
+    );
+    return pipe(
+      res,
+      TE.map((axiosResponse) => ({
+        fullText: toHtmlFragment(axiosResponse.data.description),
+        url: new URL('http://sciety.org'),
+      })),
+    );
+  },
 );
