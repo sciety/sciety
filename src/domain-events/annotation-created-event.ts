8dcef4eb591f9e43c27200bc8d5138f255af5d48
Giorgio Sironi <g.sironi@elifesciences.org>
2022-05-03 13:31:02 +0100

    Decode annotation content as HTML #1570

--- src/domain-events/annotation-created-event.ts
+++ src/domain-events/annotation-created-event.ts
@@ -4,7 +4,7 @@ import { DoiFromString } from '../types/codecs/DoiFromString';
 import { EventIdFromString } from '../types/codecs/EventIdFromString';
 import { ListIdFromString } from '../types/codecs/ListIdFromString';
 import { generate } from '../types/event-id';
-import { HtmlFragment } from '../types/html-fragment';
+import { HtmlFragment, htmlFragmentCodec } from '../types/html-fragment';
 
 const targetCodec = t.type({
   articleId: DoiFromString,
@@ -17,7 +17,7 @@ export const annotationCreatedEventCodec = t.type({
   id: EventIdFromString,
   type: t.literal('AnnotationCreated'),
   date: tt.DateFromISOString,
-  content: t.string,
+  content: htmlFragmentCodec,
   target: targetCodec,
 });
 
