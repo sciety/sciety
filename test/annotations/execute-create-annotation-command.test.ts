f3c62d01c60e2ab3027ad48a417455c08b3f4d01
Giorgio Sironi <g.sironi@elifesciences.org>
2022-05-03 10:42:38 +0100

    Write skipped test #1569

--- test/annotations/execute-create-annotation-command.test.ts
+++ test/annotations/execute-create-annotation-command.test.ts
@@ -6,27 +6,37 @@ import { arbitraryArticleId } from '../types/article-id.helper';
 import { arbitraryListId } from '../types/list-id.helper';
 
 describe('execute-create-annotation-command', () => {
+  const target = {
+    articleId: arbitraryArticleId(),
+    listId: arbitraryListId(),
+  };
+  const content = arbitraryHtmlFragment();
+  const command = {
+    content,
+    target,
+  };
+
   describe('given the target does not have an annotation, when the command is executed', () => {
-    const target = {
-      articleId: arbitraryArticleId(),
-      listId: arbitraryListId(),
-    };
-    const content = arbitraryHtmlFragment();
-    const command = {
-      content,
-      target,
-    };
     const result = pipe(
       [],
       executeCreateAnnotationCommand(command),
     );
 
     it.skip('returns an AnnotationCreated event', () => {
-      expect(result).toStrictEqual(annotationCreated(target, content));
+      expect(result).toStrictEqual([annotationCreated(target, content)]);
     });
   });
 
   describe('given the target already has an annotation, when the command is executed', () => {
-    it.todo('returns no events');
+    const result = pipe(
+      [
+        annotationCreated(target, arbitraryHtmlFragment()),
+      ],
+      executeCreateAnnotationCommand(command),
+    );
+
+    it.skip('returns no events', () => {
+      expect(result).toStrictEqual([]);
+    });
   });
 });
