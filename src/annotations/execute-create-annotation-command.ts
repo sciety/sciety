e8550bc445cf7ff5ef04f6ca4c78a68afcf59cf2
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-05-03 11:10:05 +0100

    Extract comparison functionality for AnnotationTarget #1569

--- src/annotations/execute-create-annotation-command.ts
+++ src/annotations/execute-create-annotation-command.ts
@@ -1,18 +1,13 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
+import { AnnotationTarget, eqAnnotationTarget } from './annotation-target';
 import { AnnotationCreatedEvent, DomainEvent } from '../domain-events';
 import { annotationCreated, isAnnotationCreatedEvent } from '../domain-events/annotation-created-event';
-import { Doi } from '../types/doi';
 import { HtmlFragment } from '../types/html-fragment';
-import { ListId } from '../types/list-id';
 
 export type CreateAnnotationCommand = {
   content: HtmlFragment,
-  target: {
-    articleId: Doi,
-    listId: ListId,
-  },
-
+  target: AnnotationTarget,
 };
 
 type ExecuteCreateAnnotationCommand = (command: CreateAnnotationCommand)
@@ -22,10 +17,7 @@ type ExecuteCreateAnnotationCommand = (command: CreateAnnotationCommand)
 export const executeCreateAnnotationCommand: ExecuteCreateAnnotationCommand = (command) => (events) => pipe(
   events,
   RA.filter(isAnnotationCreatedEvent),
-  RA.filter((event) => (
-    event.target.articleId.value === command.target.articleId.value
-     && event.target.listId === command.target.listId
-  )),
+  RA.filter((event) => eqAnnotationTarget.equals(event.target, command.target)),
   RA.match(
     () => [annotationCreated(command.target, command.content)],
     () => [],
--- src/types/list-id.ts
+++ src/types/list-id.ts
@@ -1,5 +1,10 @@
+import * as Eq from 'fp-ts/Eq';
+import * as S from 'fp-ts/string';
+
 export type ListId = string & { readonly ListId: unique symbol };
 
 export const fromValidatedString = (value: string): ListId => value as ListId;
 
 export const isListId = (value: unknown): value is ListId => typeof value === 'string';
+
+export const eqListId: Eq.Eq<ListId> = S.Eq;
