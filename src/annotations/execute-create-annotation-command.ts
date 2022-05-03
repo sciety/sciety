64e0a2fa4209634c66d80b32249f6854c6323946
Giorgio Sironi <g.sironi@elifesciences.org>
2022-05-03 13:40:24 +0100

    Follow up to fix linter #1570

--- src/annotations/execute-create-annotation-command.ts
+++ src/annotations/execute-create-annotation-command.ts
@@ -1,8 +1,8 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import { AnnotationTarget, eqAnnotationTarget } from '../types/annotation-target';
 import { AnnotationCreatedEvent, DomainEvent } from '../domain-events';
 import { annotationCreated, isAnnotationCreatedEvent } from '../domain-events/annotation-created-event';
+import { AnnotationTarget, eqAnnotationTarget } from '../types/annotation-target';
 import { HtmlFragment } from '../types/html-fragment';
 
 export type CreateAnnotationCommand = {
