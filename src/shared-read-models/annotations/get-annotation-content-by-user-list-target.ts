27d8bdac378eb1b52418eb6e9093c65f69fa3817
David Moulton <d.moulton@elifesciences.org>
2022-05-04 14:25:57 +0100

    Remove obsolete hard coded annotations #1571
    These annotations have been added to the prod database using the annotation creation form, so there's no need for them to exist in code anymore.

--- src/shared-read-models/annotations/get-annotation-content-by-user-list-target.ts
+++ src/shared-read-models/annotations/get-annotation-content-by-user-list-target.ts
@@ -25,37 +25,6 @@ export const getAnnotationContentByUserListTarget: GetAnnotationContentByUserLis
     return undefined;
   }
   let content: string | undefined;
-  if (articleId.value === '10.1101/2022.03.29.486216') {
-    content = 'A 2.2 angstrom resolution structures of muscle actin filaments in ATP, ADP-Pi and ADP states. Many new insights here about the surprising stability of ADP actin, mechanism of ATP hydrolysis, cofilin binding and more.';
-  }
-  if (articleId.value === '10.1101/2021.05.26.445751') {
-    content = 'Truly exquisite characterization of kinesins in the malaria parasite. Check out the spectacular expansion microscopy images in figure 7A!';
-  }
-  if (articleId.value === '10.1101/2022.03.31.486578') {
-    content = 'This is a beautiful characterization of caveolae. In addition to platinum replica EM of these plasma membrane invaginations in various cell types, this work includes molecular identification of proteins associated with caveolae of different curvatures by STED-CLEM (very high-resolution correlative light electron microscopy).';
-  }
-  if (articleId.value === '10.1101/2022.03.31.486371') {
-    content = 'Whoa! Successful ID of early endosomal cargoes via pull-down of an early endosomal marker followed by proteomics.';
-  }
-
-  if (articleId.value === '10.1101/2022.04.06.487275') {
-    content = 'How interesting! A peptide screen to identify degrons with E3 ubiquitin ligase specificity in yeast followed by extraction of conserved properties identifies transmembrane domain containing peptides as likely degrons.';
-  }
-  if (articleId.value === '10.1101/2021.05.19.444861') {
-    content = 'This is wild! Chlamydomonas flagella were isolated, demembranated, attached to polystyrene beads and reactivated to reconstitute flagellar movement. This yields some interesting insights through experimentation and simulation: Increased calcium concentration affects flagellar waveform and trajectory of bead movement and surprisingly, larger beads have increased rather than decreased velocity.';
-  }
-  if (articleId.value === '10.1101/2022.04.07.487451') {
-    content = 'Interesting role for ARP2/3 complex in peroxisome autophagy in plants!';
-  }
-  if (articleId.value === '10.1101/2022.04.06.487340') {
-    content = 'This polarity of actin networks and myosin traffic is interesting in light of the parallels between cilium biogenesis and the immune synapse function.';
-  }
-  if (articleId.value === '10.1101/2022.04.13.488127') {
-    content = 'I love resources like this. A comprehensive look at expression levels and localization data for all 38 mitotic regulators in fission yeast during division.';
-  }
-  if (articleId.value === '10.1101/2022.04.15.488517') {
-    content = 'Huh clever. Leveraging incomplete dissociation in scRNA-seq experiments to infer tissue architecture/cell-cell interactions.';
-  }
   if (!content) {
     return pipe(
       events,
--- test/shared-read-models/annotations/get-annotation-content-by-user-list-target.test.ts
+++ test/shared-read-models/annotations/get-annotation-content-by-user-list-target.test.ts
@@ -1,8 +1,6 @@
 import { pipe } from 'fp-ts/function';
 import { annotationCreated } from '../../../src/domain-events';
 import { getAnnotationContentByUserListTarget } from '../../../src/shared-read-models/annotations';
-import { Doi } from '../../../src/types/doi';
-import { toHtmlFragment } from '../../../src/types/html-fragment';
 import * as LID from '../../../src/types/list-id';
 import { toUserId } from '../../../src/types/user-id';
 import { arbitraryHtmlFragment } from '../../helpers';
@@ -11,42 +9,6 @@ import { arbitraryUserId } from '../../types/user-id.helper';
 
 describe('get-annotation-content-by-user-list-target', () => {
   const avasthiReadingUserId = toUserId('1412019815619911685');
-
-  describe('existing hardcoded behavior', () => {
-    describe('when an article in the AvasthiReading list has been annotated', () => {
-      const annotationContent = toHtmlFragment('Interesting role for ARP2/3 complex in peroxisome autophagy in plants!');
-      const result = getAnnotationContentByUserListTarget(
-        new Doi('10.1101/2022.04.07.487451'),
-        avasthiReadingUserId,
-      )([]);
-
-      it('returns the annotation content as HTML', () => {
-        expect(result).toBe(annotationContent);
-      });
-    });
-
-    describe('when an article in the AvasthiReading list has not been annotated', () => {
-      const result = getAnnotationContentByUserListTarget(
-        arbitraryArticleId(),
-        avasthiReadingUserId,
-      )([]);
-
-      it('returns undefined', () => {
-        expect(result).toBeUndefined();
-      });
-    });
-
-    describe('when an article in any other user list is the target', () => {
-      const result = getAnnotationContentByUserListTarget(
-        arbitraryArticleId(),
-        arbitraryUserId(),
-      )([]);
-
-      it('returns undefined', () => {
-        expect(result).toBeUndefined();
-      });
-    });
-  });
 
   describe('hardcoded knowledge of user ids and user list ids, but content from events', () => {
     const listIdForAvasthiReadingUser = LID.fromValidatedString('1af5b971-162e-4cf3-abdf-57e3bbfcd0d7');
