3fa5e14d8f98fccaeed69af5d9ec7005532512eb
Giorgio Sironi <g.sironi@elifesciences.org>
2022-05-09 14:58:05 +0100

    Move test to follow production code equivalent path #1574

--- test/article-page/activity-page/activity-feed/render-review-responses.test.ts
+++ test/article-page/activity-page/activity-feed/render-review-responses.test.ts
@@ -1,6 +1,6 @@
 import * as O from 'fp-ts/Option';
-import { renderReviewResponses } from '../../../src/article-page/activity-page/activity-feed/render-review-responses';
-import { arbitraryReviewId } from '../../types/review-id.helper';
+import { renderReviewResponses } from '../../../../src/article-page/activity-page/activity-feed/render-review-responses';
+import { arbitraryReviewId } from '../../../types/review-id.helper';
 
 describe('render-review-responses', () => {
   it('displays the response counts by type', () => {
