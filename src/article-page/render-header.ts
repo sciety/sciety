9f4d03ea46f505f1273ae7dc22f2edd86442fb91
Giorgio Sironi <g.sironi@elifesciences.org>
2022-05-10 13:15:22 +0100

    Extract renderer for button #1576

--- src/article-page/render-header.ts
+++ src/article-page/render-header.ts
@@ -1,6 +1,10 @@
 import { Doi } from '../types/doi';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
+const renderFullArticleButton = (doi: Doi) => `
+  <a href="https://doi.org/${doi.value}" class="full-article-button">Read the full article</a>
+`;
+
 type ArticleDetails = {
   doi: Doi,
   title: string,
@@ -18,7 +22,7 @@ export const renderHeader = (viewModel: HeaderViewModel): HtmlFragment => toHtml
   </header>
 
   <div class="article-actions">
-    <a href="https://doi.org/${viewModel.articleDetails.doi.value}" class="full-article-button">Read the full article</a>
+    ${renderFullArticleButton(viewModel.articleDetails.doi)}
     <div class="tweet-and-save-buttons">
       ${viewModel.tweetThis}
       ${viewModel.saveArticle}
