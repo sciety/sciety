766a0867262252241a4973fba3bfd131f7ba62b5
David Moulton <d.moulton@elifesciences.org>
2022-05-12 11:09:02 +0100

    Update homepage evaluation cards

--- src/home-page/cards/hardcoded-evaluation-cards.ts
+++ src/home-page/cards/hardcoded-evaluation-cards.ts
@@ -1,26 +1,26 @@
 export const card1 = {
-  articleId: '10.1101/2020.09.14.297143',
-  articleTitle: 'Contextual effects in sensorimotor adaptation adhere to associative learning rules',
-  date: new Date('2022-03-15'),
-  groupSlug: 'elife',
-  groupName: 'eLife',
-  avatarPath: '/static/groups/elife--b560187e-f2fb-4ff9-a861-a204f3fc0fb0.png',
+  articleId: '10.1101/2022.04.04.486901',
+  articleTitle: 'All-optical visualization of specific molecules in the ultrastructural context of brain tissue',
+  date: new Date('2022-05-06'),
+  groupSlug: 'prelights',
+  groupName: 'preLights',
+  avatarPath: '/static/groups/prelights--f97bd177-5cb6-4296-8573-078318755bf2.jpg',
 };
 
 export const card2 = {
-  articleId: '10.1101/2021.08.12.456105',
-  articleTitle: 'Microbial colonization of tannin-rich tropical plants: interplay between degradability, methane production and tannin disappearance in the rumen',
-  date: new Date('2022-01-28'),
-  groupSlug: 'pci-animal-science',
-  groupName: 'Peer Community in Animal Science',
-  avatarPath: '/static/groups/pci-animal-science--4eebcec9-a4bb-44e1-bde3-2ae11e65daaa.png',
+  articleId: '10.1101/2022.02.15.480489',
+  articleTitle: 'Centriole growth is not limited by a finite pool of components, but is limited by the Cdk1/Cyclin-dependent phosphorylation of Ana2/STIL',
+  date: new Date('2022-05-10'),
+  groupSlug: 'review-commons',
+  groupName: 'Review Commons',
+  avatarPath: '/static/groups/review-commons--316db7d9-88cc-4c26-b386-f067e0f56334.jpg',
 };
 
 export const card3 = {
-  articleId: '10.1101/2021.10.05.21264567',
-  articleTitle: 'Efficacy and Safety of NVX-CoV2373 in Adults in the United States and Mexico',
-  date: new Date('2022-03-16'),
-  groupSlug: 'rapid-reviews-covid-19',
-  groupName: 'Rapid Reviews COVID-19',
-  avatarPath: '/static/groups/rapid-reviews-covid-19--5142a5bc-6b18-42b1-9a8d-7342d7d17e94.png',
+  articleId: '10.1101/2022.04.10.22273674',
+  articleTitle: 'COVID-19 and Curcumin: Using VOSviewer software to explore scientific landscapes – A bibliometric analysis',
+  date: new Date('2022-05-09'),
+  groupSlug: 'peerref',
+  groupName: 'PeerRef',
+  avatarPath: '/static/groups/peerref--b5f31635-d32b-4df9-92a5-0325a1524343.png',
 };
