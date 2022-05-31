e2f2d0436b3b0a666dd32d03ce65533f8e44c2a9
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-05-31 10:31:55 +0100

    Extract http status from doi.org and biorxiv #1602

---
+++ scripts/investigate-doi.sh
@@ -0,0 +1,5 @@
+doi=$1
+doi_dot_org=$(curl -s -o /dev/null -w "%{http_code}\n" https://doi.org/$doi)
+biorxiv=$(curl -s https://api.biorxiv.org/details/biorxiv/$doi | jq '.messages[0].status')
+
+echo $doi,$doi_dot_org,$biorxiv
