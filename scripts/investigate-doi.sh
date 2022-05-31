e8523ebf973d0b8d0331b2cbe88129e8479f19f1
LinaKind <l.kinduryte@elifesciences.org>
2022-05-31 10:44:03 +0100

    Add crossref status check

--- scripts/investigate-doi.sh
+++ scripts/investigate-doi.sh
@@ -2,5 +2,6 @@ doi=$1
 doi_dot_org=$(curl -s -o /dev/null -w "%{http_code}\n" https://doi.org/$doi)
 biorxiv=$(curl -s https://api.biorxiv.org/details/biorxiv/$doi | jq '.messages[0].status')
 medrxiv=$(curl -s https://api.biorxiv.org/details/medrxiv/$doi | jq '.messages[0].status')
+crossref=$(curl -s -o /dev/null -w "%{http_code}\n" https://api.crossref.org/works/$doi/transform -H "Accept: application/vnd.crossref.unixref+xml")
 
-echo $doi,$doi_dot_org,$biorxiv,$medrxiv
+echo $doi,$doi_dot_org,$biorxiv,$medrxiv,$crossref
