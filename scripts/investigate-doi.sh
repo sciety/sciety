6c65dfdd3ce93d8f842585dc69f324b180ea894f
LinaKind <l.kinduryte@elifesciences.org>
2022-05-31 10:37:50 +0100

    Get status from medrxiv #1602

--- scripts/investigate-doi.sh
+++ scripts/investigate-doi.sh
@@ -1,5 +1,6 @@
 doi=$1
 doi_dot_org=$(curl -s -o /dev/null -w "%{http_code}\n" https://doi.org/$doi)
 biorxiv=$(curl -s https://api.biorxiv.org/details/biorxiv/$doi | jq '.messages[0].status')
+medrxiv=$(curl -s https://api.biorxiv.org/details/medrxiv/$doi | jq '.messages[0].status')
 
-echo $doi,$doi_dot_org,$biorxiv
+echo $doi,$doi_dot_org,$biorxiv,$medrxiv
