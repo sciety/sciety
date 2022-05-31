29c79d75f1c3273ed97e80868aa902ba922c8b90
David Moulton <d.moulton@elifesciences.org>
2022-05-31 10:52:03 +0100

    Count number of instances of DOI in database #1602

--- scripts/investigate-doi.sh
+++ scripts/investigate-doi.sh
@@ -3,5 +3,6 @@ doi_dot_org=$(curl -s -o /dev/null -w "%{http_code}\n" https://doi.org/$doi)
 biorxiv=$(curl -s https://api.biorxiv.org/details/biorxiv/$doi | jq '.messages[0].status')
 medrxiv=$(curl -s https://api.biorxiv.org/details/medrxiv/$doi | jq '.messages[0].status')
 crossref=$(curl -s -o /dev/null -w "%{http_code}\n" https://api.crossref.org/works/$doi/transform -H "Accept: application/vnd.crossref.unixref+xml")
+number_of_events=$(grep $doi ./data/exploratory-test-from-prod.csv | wc -l)
 
-echo $doi,$doi_dot_org,$biorxiv,$medrxiv,$crossref
+echo $doi,$doi_dot_org,$biorxiv,$medrxiv,$crossref,$number_of_events
