b45eee18b33555cff515098f2edf6afbfade4e2a
David Moulton <d.moulton@elifesciences.org>
2022-06-07 09:51:15 +0100

    Update hard coded evaluation cards on home page

--- src/home-page/cards/hardcoded-evaluation-cards.ts
+++ src/home-page/cards/hardcoded-evaluation-cards.ts
@@ -1,26 +1,26 @@
 export const card1 = {
-  articleId: '10.1101/2022.04.04.486901',
-  articleTitle: 'All-optical visualization of specific molecules in the ultrastructural context of brain tissue',
-  date: new Date('2022-05-06'),
-  groupSlug: 'prelights',
-  groupName: 'preLights',
-  avatarPath: '/static/groups/prelights--f97bd177-5cb6-4296-8573-078318755bf2.jpg',
+  articleId: '10.1101/2021.10.09.463765',
+  articleTitle: 'Annotation of Hox cluster and Hox cofactor genes in the Asian citrus psyllid, Diaphorina citri, reveals novel features',
+  date: new Date('2022-05-31'),
+  groupSlug: 'gigabyte',
+  groupName: 'GigaByte',
+  avatarPath: '/static/groups/gigabyte--f7a7aec3-8b1c-4b81-b098-f3f2e4eefe58.png',
 };
 
 export const card2 = {
-  articleId: '10.1101/2022.02.15.480489',
-  articleTitle: 'Centriole growth is not limited by a finite pool of components, but is limited by the Cdk1/Cyclin-dependent phosphorylation of Ana2/STIL',
-  date: new Date('2022-05-10'),
-  groupSlug: 'review-commons',
-  groupName: 'Review Commons',
-  avatarPath: '/static/groups/review-commons--316db7d9-88cc-4c26-b386-f067e0f56334.jpg',
+  articleId: '10.1101/2022.02.14.480364',
+  articleTitle: 'The Transcription Factor Bach2 Negatively Regulates Natural Killer Cell Maturation and Function',
+  date: new Date('2022-06-01'),
+  groupSlug: 'prereview',
+  groupName: 'PREreview',
+  avatarPath: '/static/groups/prereview-community--10360d97-bf52-4aef-b2fa-2f60d319edd7.jpg',
 };
 
 export const card3 = {
-  articleId: '10.1101/2022.04.10.22273674',
-  articleTitle: 'COVID-19 and Curcumin: Using VOSviewer software to explore scientific landscapes – A bibliometric analysis',
-  date: new Date('2022-05-09'),
-  groupSlug: 'peerref',
-  groupName: 'PeerRef',
-  avatarPath: '/static/groups/peerref--b5f31635-d32b-4df9-92a5-0325a1524343.png',
+  articleId: '10.1101/2022.06.01.22275778',
+  articleTitle: 'A comparison of sleep-wake patterns among school-age children and adolescents in Hong Kong before and during the COVID-19 pandemic',
+  date: new Date('2022-06-05'),
+  groupSlug: 'screenit',
+  groupName: 'ScreenIT',
+  avatarPath: '/static/groups/screenit--8ccea9c2-e6c8-4dd7-bf1d-37c3fa86ff65.png',
 };
