56a41dc0de1b566cf870f38ebe1970b10d7abb4a
LinaKind <l.kinduryte@elifesciences.org>
2022-06-13 10:25:51 +0100

    Reflect structure of footer in BEM class names #1615

--- src/shared-components/page-footer/page-footer.ts
+++ src/shared-components/page-footer/page-footer.ts
@@ -3,8 +3,8 @@ import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 export const pageFooter: HtmlFragment = toHtmlFragment(`
   <footer class="page-footer">
     <div class="pre-footer">
-      <div class="page-footer__slogan">Stay Updated. Get Involved.</div>
-      <a href="https://staging.sciety.org/signup" class="page-footer__call_to_action">Subscribe to Mailing List</a>
+      <div class="pre-footer__slogan">Stay Updated. Get Involved.</div>
+      <a href="https://staging.sciety.org/signup" class="pre-footer__call_to_action">Subscribe to Mailing List</a>
     </div>
     <div class="main-footer">
       <ul class="main-footer__navigation">
