292effeed1f6add8a81b6f9984695a4c86b96090
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-06-14 12:36:58 +0100

    Introduce ts-adt reduce boilerplate in data-error

--- src/types/data-error.ts
+++ src/types/data-error.ts
@@ -1,17 +1,25 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import { Lazy } from 'fp-ts/function';
+import { ADT } from 'ts-adt';
 
-export const notFound = { _type: 'not-found' as const };
-export const unavailable = { _type: 'unavailable' as const };
+export type DataError = ADT<{
+  notFound: {},
+  unavailable: {},
+}>;
 
-export type DataError = typeof notFound | typeof unavailable;
+type DataErrorTypes = DataError['_type'];
+
+const create = (type: DataErrorTypes): DataError => ({ _type: type });
+
+export const notFound = create('notFound');
+export const unavailable = create('unavailable');
 
 export const isNotFound = (de: DataError): boolean => de === notFound;
-
 export const isUnavailable = (de: DataError): boolean => de === unavailable;
 
 export const fold = <B>(opts: Record<'notFound' | 'unavailable', Lazy<B>>) => (e: DataError): B => {
   switch (e._type) {
-    case 'not-found':
+    case 'notFound':
       return opts.notFound();
     case 'unavailable':
       return opts.unavailable();
