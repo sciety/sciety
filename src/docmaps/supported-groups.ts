1d3d0857594fe9c382b592a96c012fce5f58e1e6
Giorgio Sironi <g.sironi@elifesciences.org>
2022-06-15 10:54:55 +0100

    Enable Docmaps for ASAPbio crowd review #1618

--- src/docmaps/supported-groups.ts
+++ src/docmaps/supported-groups.ts
@@ -1,5 +1,6 @@
 import * as GID from '../types/group-id';
 
+const asapBioGroupId = GID.fromValidatedString('50401e46-b764-47b7-8557-6bb35444b7c8');
 const biophysicsColabGroupId = GID.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2');
 const ncrcGroupId = GID.fromValidatedString('62f9b0d0-8d43-4766-a52a-ce02af61bc6a');
 const peerJGroupId = GID.fromValidatedString('53ed5364-a016-11ea-bb37-0242ac130002');
@@ -8,6 +9,7 @@ const preReviewGroupId = GID.fromValidatedString('10360d97-bf52-4aef-b2fa-2f60d3
 const rapidReviewsGroupId = GID.fromValidatedString('5142a5bc-6b18-42b1-9a8d-7342d7d17e94');
 
 export const supportedGroups = [
+  asapBioGroupId,
   biophysicsColabGroupId,
   ncrcGroupId,
   peerJGroupId,
