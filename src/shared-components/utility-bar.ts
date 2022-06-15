84712030207f5134b93d907740815b25b56b30cf
Giorgio Sironi <g.sironi@elifesciences.org>
2022-06-15 14:55:44 +0100

    Add sign up link to header #1619

--- src/shared-components/utility-bar.ts
+++ src/shared-components/utility-bar.ts
@@ -1,4 +1,5 @@
 import * as O from 'fp-ts/Option';
+import { constant } from 'fp-ts/function';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { User } from '../types/user';
 
@@ -14,6 +15,12 @@ const logOutMenuItem = () => toHtmlFragment(`
   </li>
 `);
 
+const signUpMenuItem = () => toHtmlFragment(`
+  <li class="utility-bar__list_item">
+    <a href="/log-in" class="utility-bar__list_link_button">Sign up</a>
+  </li>
+`);
+
 export const utilityBar = (user: O.Option<User>): HtmlFragment => toHtmlFragment(`
   <nav class="utility-bar" aria-describedby="application-utilities">
       <div id="application-utilities" class="hidden">Sciety application utilities</div>
@@ -24,6 +31,7 @@ export const utilityBar = (user: O.Option<User>): HtmlFragment => toHtmlFragment
           </a>
         </li>
         ${O.fold(logInMenuItem, logOutMenuItem)(user)}
+        ${O.fold(signUpMenuItem, constant(''))(user)}
       </ul>
     </nav>
 `);
