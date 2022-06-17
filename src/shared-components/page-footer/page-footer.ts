f7b02deba1fa759785e612d7ca35cbb473a2163f
LinaKind <l.kinduryte@elifesciences.org>
2022-06-17 11:11:44 +0100

    Fix accidentally hard coded link #1623

--- src/shared-components/page-footer/page-footer.ts
+++ src/shared-components/page-footer/page-footer.ts
@@ -4,7 +4,7 @@ export const pageFooter: HtmlFragment = toHtmlFragment(`
   <footer class="page-footer">
     <div class="pre-footer">
       <div class="pre-footer__slogan">Stay Updated. Get Involved.</div>
-      <a href="https://staging.sciety.org/signup" class="pre-footer__call_to_action">Subscribe to Mailing List</a>
+      <a href="/signup" class="pre-footer__call_to_action">Subscribe to Mailing List</a>
     </div>
     <div class="main-footer">
       <ul class="main-footer__navigation">
