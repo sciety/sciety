82233909f9492ff57a69d195942a60c85f4bdcd1
Giorgio Sironi <g.sironi@elifesciences.org>
2022-06-17 10:57:12 +0100

    Align capitalization of sign up page to the rest of the site #1623

--- src/sign-up-page/sign-up-page.ts
+++ src/sign-up-page/sign-up-page.ts
@@ -2,37 +2,37 @@ import { toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 
 export const signUpPage: Page = {
-  title: 'Sign Up for Sciety',
+  title: 'Sign up for Sciety',
   content: toHtmlFragment(`
     <header class="page-header">
-      <h1>Sign Up for Sciety</h1>
+      <h1>Sign up for Sciety</h1>
     </header>
     <div class="sign-up-page-content">
-      <p><a href="/sign-up-call-to-action" class="sign-up-page-call-to-action">Sign Up With Your Twitter Account</a></p>
+      <p><a href="/sign-up-call-to-action" class="sign-up-page-call-to-action">Sign up with your Twitter account</a></p>
   
-      <h2>Don't Have a Twitter Account?</h2>
-      <p>You will need to create a twitter account before you can sign up for Sciety.</p>
-      <p><a href="https://twitter.com/">Create a Twitter Account</a></p>
+      <h2>Don't have a Twitter account?</h2>
+      <p>You will need to create a Twitter account before you can sign up for Sciety.</p>
+      <p><a href="https://twitter.com/">Create a Twitter account</a></p>
   
-      <h3>Don't Want to Create a Twitter Account?</h3>
+      <h3>Don't want to create a Twitter account?</h3>
       <p>We will be adding other sign-in methods soon. <a href="/signup">Subscribe to our newsletter</a> to be notified when we do.</p>
   
-      <h2>Why Should I Make a Sciety Account?</h2>
+      <h2>Why should I make a Sciety account?</h2>
       <p>You don't need an account to use Sciety, but having an account unlocks new features.</p>
       <ul>
-        <li><b>Save Your Favourite Preprints</b>: Never lose the preprints that interest you most.</li>
-        <li><b>Follow Trusted Groups</b>: Stay updated on what your favourite groups are evaluating.</li>
-        <li><b>Track Recent Activity</b>: Always see new evaluations on saved preprints.</li>
+        <li><b>Save your favourite preprints</b>: never lose the preprints that interest you most.</li>
+        <li><b>Follow trusted groups</b>: stay updated on what your favourite groups are evaluating.</li>
+        <li><b>Track recent activity</b>: always see new evaluations on saved preprints.</li>
       </ul>
   
       <p><a href="/about">Learn more about Sciety</a></p>
     </div>
 
     <aside class="sign-up-page-supplementary">
-      <h2>What We Do With Your Data</h2>
-      <h3>Twitter Data</h3>
+      <h2>What we do with your data</h2>
+      <h3>Twitter data</h3>
       <p>We only use your user name. We never post on your behalf.</p>
-      <h3>Sciety Data</h3>
+      <h3>Sciety data</h3>
       <p>We use website data to inform our improvements.</p>
       <p>Read more on our <a href="/legal">legal information page</a>.</p>
     </aside>
