0d9758d16f81f09daa8fc43ad8394fe2de2980a8
David Moulton <d.moulton@elifesciences.org>
2022-06-23 15:59:52 +0100

    Rename for intent #1634

--- src/shared-components/site-header/site-header.ts
+++ src/shared-components/site-header/site-header.ts
@@ -4,7 +4,7 @@ import { User } from '../../types/user';
 import { utilityBar } from '../utility-bar';
 
 export const siteHeader = (user: O.Option<User>): HtmlFragment => toHtmlFragment(`<header class="site-header">
-    <div class="site-header__inner">
+    <div class="site-header__grey_box">
     <a href="/menu" class="site-header__menu_link">
         <img src="/static/images/menu-icon.svg" alt="" />
     </a>
