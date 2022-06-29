7ab31aac2ad521eac4a04f1952b9d3b977ad4c1c
LinaKind <l.kinduryte@elifesciences.org>
2022-06-29 13:32:02 +0100

    Remove empty divs by reducing grid column number

--- src/shared-components/site-header/site-header.ts
+++ src/shared-components/site-header/site-header.ts
@@ -3,8 +3,7 @@ import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 import { User } from '../../types/user';
 import { utilityBar } from '../utility-bar';
 
-export const siteHeader = (user: O.Option<User>): HtmlFragment => toHtmlFragment(`<header class="site-header">
-  <div class="site-header__white_box_padding"></div>  
+export const siteHeader = (user: O.Option<User>): HtmlFragment => toHtmlFragment(`<header class="site-header"> 
   <div class="site-header__white_box">
       <a href="/" class="site-header__logo-link" aria-hidden="true">
         <img src="/static/images/sciety-logo-blue-text.svg " alt="Sciety" class="">
@@ -19,5 +18,4 @@ export const siteHeader = (user: O.Option<User>): HtmlFragment => toHtmlFragment
     <div class="site-header__grey_box">
     ${utilityBar(user)}
     </div>
-    <div></div> 
 </header>`);
