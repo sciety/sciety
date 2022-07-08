5aff7594ce70ea02ca702e4c9ae515f8a5c6f07a
David Moulton <d.moulton@elifesciences.org>
2022-07-08 10:55:14 +0100

    Add copy and call to action  #1660

--- src/learn-about-page/learn-about-page.ts
+++ src/learn-about-page/learn-about-page.ts
@@ -7,8 +7,22 @@ export const learnAboutPage: Page = {
     <header class="page-header">
       <h1>Learn about Sciety</h1>
     </header>
-      <video controls preload="metadata" class="learn-about-page-video">
-        <source src="/static/video/sciety-animation-video.mp4" type="video/mp4"></source>
-      </video>
+
+    <video controls preload="metadata" class="learn-about-page-video">
+      <source src="/static/video/sciety-animation-video.mp4" type="video/mp4"></source>
+    </video>
+
+    <section>
+      <h2>Join Sciety</h2>
+      <p>Start a new way of curating and sharing evaluated preprints. <a href="/sign-up">Sign up</a> today.</p>
+
+      <h2>What Sciety offers</h2>
+      <p>Sciety's changing the landscape of preprint curation.</p>
+      <ul>
+        <li>Discover evaluated preprints</li>
+        <li>Curate lists of preprints you're interested in</li>
+        <li>Follow trusted groups</li>
+      </ul>
+    </section>
   `),
 };
