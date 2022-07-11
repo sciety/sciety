9a6745a822a9b8e1ca0d7ff1d8b1144f3b77ab18
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-07-11 10:04:26 +0100

    Add transcript for video #1660

--- src/learn-about-page/learn-about-page.ts
+++ src/learn-about-page/learn-about-page.ts
@@ -8,9 +8,27 @@ export const learnAboutPage: Page = {
       <h1>Learn about Sciety</h1>
     </header>
 
-    <video controls preload="metadata" class="learn-about-page-video">
-      <source src="/static/video/sciety-animation-video.mp4" type="video/mp4"></source>
-    </video>
+    <section>
+      <video controls preload="metadata" class="learn-about-page-video">
+        <source src="/static/video/sciety-animation-video.mp4" type="video/mp4"></source>
+      </video>
+
+      <h2>Video transcript</h2>
+      <p>Do you work in biomedical and life sciences?</p>
+      <p>How do you keep on top of the growing number of preprints now being made available?</p>
+      <p>How can you tell if a preprint is worth your time?</p>
+      <p>How can you and your peers share the preprints you find interesting or important?</p>
+      <p>How about discovering evaluated preprints, especially from underrepresented labs?</p>
+      <p>There is an answer…</p>
+      <p>Sciety aggregates evaluations of preprints in one place, helping you organise your preprints of choice into your own shareable lists.</p>
+      <p>Gone are the days when curation is for the selected few…</p>
+      <p>On Sciety, you can find open reviews, assessments and evaluations by groups of experts you can trust...</p>
+      <p>Organise lists of articles in your own space on Sciety</p>
+      <p>Follow the groups evaluating articles you care about to receive their latest reviews in your personal feed.</p>
+      <p>Or form your own group to share your own evaluations of preprints.</p>
+      <p>Visit Sciety now to discover your next important read. </p>
+      <p>Sciety; for the shared discovery, evaluation and organisation of preprints.</p>
+    </section>
 
     <section class="learn-about-page-copy">
       <h2>Join Sciety</h2>
