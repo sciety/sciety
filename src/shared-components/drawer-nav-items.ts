4e1d64809e4081fe07a2dea96c263b3e531fa80b
LinaKind <l.kinduryte@elifesciences.org>
2022-07-13 16:47:12 +0100

    Hide sidebar #1667

--- src/shared-components/drawer-nav-items.ts
+++ src/shared-components/drawer-nav-items.ts
@@ -1,7 +1,3 @@
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
-export const drawerNavItems = (): HtmlFragment => toHtmlFragment(`
-  <ul role="list" class="site-menu__links">
-    <li><a href="/sciety-feed" class="site-menu__link site-menu__link--sciety-feed"><span class="site-menu__link_text">Sciety feed</span></a></li>
-  </ul>
-`);
+export const drawerNavItems = (): HtmlFragment => toHtmlFragment('');
--- src/shared-components/drawer.ts
+++ src/shared-components/drawer.ts
@@ -2,7 +2,7 @@ import { drawerNavItems } from './drawer-nav-items';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
 export const drawer = (): HtmlFragment => toHtmlFragment(`
-  <nav class="drawer">
+  <nav class="drawer" aria-hidden="true">
     ${drawerNavItems()}
   </nav>
   `);
