6ffd3bbacf90edb2dbae1630250450c6826dbeec
David Moulton <d.moulton@elifesciences.org>
2022-07-18 10:02:00 +0100

    Use Vimeo player with localy-hosted video as  non-js fallback #1677

--- src/learn-about-page/learn-about-page.ts
+++ src/learn-about-page/learn-about-page.ts
@@ -9,9 +9,16 @@ export const learnAboutPage: Page = {
     </header>
 
     <section>
-      <video controls preload="metadata" class="learn-about-page-video">
-        <source src="/static/video/sciety-animation-video.mp4" type="video/mp4">
-      </video>
+      <noscript>
+        <video controls preload="metadata" class="learn-about-page-video">
+          <source src="/static/video/sciety-animation-video.mp4" type="video/mp4">
+        </video>
+      </noscript>
+
+      <div style="padding:56.25% 0 0 0;position:relative;">
+        <iframe src="https://player.vimeo.com/video/727774522?h=6e8839dc6b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Sciety animation video"></iframe>
+      </div>
+      <script src="https://player.vimeo.com/api/player.js"></script>
 
       <section class="learn-about-page-transcript">
         <h2>Video transcript</h2>
