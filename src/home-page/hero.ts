3f3138e5b8e245ee477c29f4ac1b43933232d8cd
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-07-18 14:52:35 +0100

    Remove UI elements from header to make space for new copy #1678

--- src/home-page/hero.ts
+++ src/home-page/hero.ts
@@ -7,34 +7,6 @@ import { renderExampleSearches } from './render-example-searches';
 import { DomainEvent, isUserSavedArticleEvent } from '../domain-events';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
-const renderStatistics = (listCount: number) => toHtmlFragment(`
-  <p class="visually-hidden">On Sciety you can find:</p>
-  <ul role="list" class="home-page-hero__statistics">
-    <li class="home-page-hero__statistic">
-      <a href="https://blog.sciety.org/lists-on-sciety/" class="home-page-hero__statistic_link">
-        <span class="home-page-hero__statistic_number">${listCount}</span><span class="home-page-hero__statistic_title"> user curated lists</span>
-        <span class="visually-hidden">. Find out more about lists on Sciety.</span>
-      </a>
-    </li>
-    <li class="home-page-hero__statistic">
-      <a href="/groups" class="home-page-hero__statistic_link">
-        <span class="home-page-hero__statistic_number">30k</span><span class="home-page-hero__statistic_title"> evaluations</span>
-        <span class="visually-hidden">. Find out more about the groups producing them.</span>
-      </a>
-    </li>
-    <li class="home-page-hero__statistic">
-      <a href="/groups" class="home-page-hero__statistic_link">
-        <span class="home-page-hero__statistic_number">20k</span><span class="home-page-hero__statistic_title"> evaluated articles</span>
-        <span class="visually-hidden">. Find out more about the groups evaluating them.</span>
-      </a>
-    </li>
-  </ul>
-`);
-
-const renderScietyFeedCTA = (): HtmlFragment => toHtmlFragment(
-  '<a class="home-page-hero__sciety_feed_link" href="/sciety-feed">What\'s happening on Sciety?<a/>',
-);
-
 const renderVideoCallToAction = () => `
   <div class="home-page-hero__video_call_to_action">
     <div class="home-page-hero__video_cta_text_wrapper">
@@ -51,21 +23,16 @@ export type Ports = {
 
 type Hero = (ports: Ports) => T.Task<HtmlFragment>;
 
-const renderHeroWithVideo = (listCount: number) => `
+const renderHeroWithVideo = () => `
 <section class="home-page-hero">
   <div class="home-page-hero__content">
     <div class="home-page-hero__left_wrapper">
       <h1 class="home-page-hero__content_title">
-        Sciety: the home of public preprint evaluation
+        The home of public preprint evaluation
       </h1>
       <p class="home-page-hero__content_byline">
-        Open evaluation and curation together in one place.
-        <br>
-        Let Sciety help you navigate the preprint landscape.
+        Explore and curate evaluated preprints.
       </p>
-      <p class="home-page-hero__content_byline">Follow the journey through <a href="/blog">our blog</a>.</p>
-      ${renderStatistics(listCount)}
-      ${renderScietyFeedCTA()}
       <form class="home-page-hero__search_form" action="/search" method="get">
         <input type="hidden" name="category" value="articles">
         <label for="searchText" class="visually-hidden">Search term</label>
@@ -94,7 +61,7 @@ export const hero: Hero = (ports) => pipe(
     RS.size,
   )),
   T.map(flow(
-    (listCount) => renderHeroWithVideo(listCount),
+    () => renderHeroWithVideo(),
     toHtmlFragment,
   )),
 );
