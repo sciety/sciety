ac0decf773888ca34d0368fc9f56d9dbb7a6f7de
David Moulton <d.moulton@elifesciences.org>
2022-07-19 13:34:35 +0100

    Remove old calls to action from home page #1680

--- src/home-page/home-page.ts
+++ src/home-page/home-page.ts
@@ -1,17 +1,14 @@
 import { sequenceS } from 'fp-ts/Apply';
-import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
-import { callsToAction } from './calls-to-action';
 import { cards, Ports as CardsPorts } from './cards';
 import { hero } from './hero';
 import { personas } from './personas';
 import { UserIdFromString } from '../types/codecs/UserIdFromString';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
-import { User } from '../types/user';
 
 export const homePageParams = t.type({
   user: tt.optionFromNullable(t.type({
@@ -23,7 +20,6 @@ type Components = {
   hero: HtmlFragment,
   cards: HtmlFragment,
   personas: HtmlFragment,
-  callsToAction: HtmlFragment,
 };
 
 type Ports = CardsPorts;
@@ -33,16 +29,14 @@ const renderContent = (components: Components) => toHtmlFragment(`
     ${components.hero}
     ${components.cards}
     ${components.personas}
-    ${components.callsToAction}
   </div>
 `);
 
-export const homePage = (ports: Ports) => (user: O.Option<User>): T.Task<Page> => pipe(
+export const homePage = (ports: Ports) => (): T.Task<Page> => pipe(
   {
     hero: T.of(hero),
     cards: cards(ports),
     personas: T.of(personas),
-    callsToAction: pipe(user, callsToAction, T.of),
   },
   sequenceS(T.ApplyPar),
   T.map(renderContent),
