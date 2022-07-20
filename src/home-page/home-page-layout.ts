591c1678a45ab8ce5114384a832e85562f32ca1b
Giorgio Sironi <g.sironi@elifesciences.org>
2022-07-20 16:47:37 +0100

    Add common footer to homepage #1684

--- src/home-page/home-page-layout.ts
+++ src/home-page/home-page-layout.ts
@@ -1,6 +1,7 @@
 import * as O from 'fp-ts/Option';
 import { googleTagManagerNoScript } from '../shared-components/analytics';
 import { head } from '../shared-components/head';
+import { pageFooter } from '../shared-components/page-footer';
 import { siteHeader } from '../shared-components/site-header';
 import { Page } from '../types/page';
 import { User } from '../types/user';
@@ -17,15 +18,7 @@ export const homePageLayout = (user: O.Option<User>) => (page: Page): string => 
       ${page.content}
     </main>
 
-    <footer class="home-page-footer">
-      <ul class="home-page-footer__links" role="list">
-        <li><a href="/about" class="home-page-footer__link">About</a></li>
-        <li><a href="/contact-us" class="home-page-footer__link">Contact us</a></li>
-        <li><a href="/blog" class="home-page-footer__link">Blog</a></li>
-        <li><a href="https://twitter.com/scietyHQ" class="home-page-footer__link"><img src="/static/images/twitter-bird.svg" alt="Follow us on Twitter"/></a></li>
-        <li><a href="https://www.facebook.com/ScietyHQ/" class="home-page-footer__link"><img src="/static/images/facebook.svg" alt="Follow us on Facebook"/></a></li>
-      </ul>
-    </footer>
+    ${pageFooter}
   </div>
 
   <script src="/static/behaviour.js"></script>
