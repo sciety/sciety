1d6d30d7afec83b08e2213b13c24ba73395b04f0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-07-22 11:34:24 +0100

    Add a link for users whose embedded playback fails due to statistics cookie not being accepted #1688

--- src/learn-about-page/learn-about-page.ts
+++ src/learn-about-page/learn-about-page.ts
@@ -15,6 +15,7 @@ const renderVimeoVideo = () => `
     <iframe src="https://player.vimeo.com/video/727774522?h=6e8839dc6b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Sciety animation video"></iframe>
   </div>
   <script src="https://player.vimeo.com/api/player.js"></script>
+  <p><a href="https://vimeo.com/727774522">Watch video on Vimeo</a></p>
 `;
 
 const renderVideoTranscript = () => `
