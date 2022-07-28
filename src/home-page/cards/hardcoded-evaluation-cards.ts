b9c8b9a6ebf694747bf9ba0946b15f49f80a9bd7
Giorgio Sironi <g.sironi@elifesciences.org>
2022-07-28 11:39:09 +0100

    Update home page evaluation cards for July 26th

--- src/home-page/cards/hardcoded-evaluation-cards.ts
+++ src/home-page/cards/hardcoded-evaluation-cards.ts
@@ -1,26 +1,26 @@
 export const card1 = {
-  articleId: '10.1101/2020.03.10.984989',
-  articleTitle: 'Dopamine pathway characterization during the reproductive mode switch in the pea aphid',
-  date: new Date('2022-06-24'),
-  groupSlug: 'pci-zoology',
-  groupName: 'Peer Community In Zoology',
-  avatarPath: '/static/groups/pci-zoology--74fd66e9-3b90-4b5a-a4ab-5be83db4c5de.jpg',
+  articleId: '10.1101/2021.06.04.447151',
+  articleTitle: 'Single-cell intracellular pH dynamics regulate the cell cycle by timing G1 exit and the G2 transition',
+  date: new Date('2022-07-26'),
+  groupSlug: 'asapbio-crowd-review',
+  groupName: 'ASAPbio crowd review',
+  avatarPath: '/static/groups/asapbio-crowd-review--50401e46-b764-47b7-8557-6bb35444b7c8.png',
 };
 
 export const card2 = {
-  articleId: '10.1101/2022.01.11.475879',
-  articleTitle: 'Structural basis of ion – substrate coupling in the Na <sup>+</sup> -dependent dicarboxylate transporter VcINDY',
-  date: new Date('2022-06-28'),
-  groupSlug: 'biophysics-colab',
-  groupName: 'Biophysics Colab',
-  avatarPath: '/static/groups/biophysics-colab--4bbf0c12-629b-4bb8-91d6-974f4df8efb2.png',
+  articleId: '10.1101/2020.11.12.379909',
+  articleTitle: 'Functional correlates of immediate early gene expression in mouse visual cortex',
+  date: new Date('2022-07-26'),
+  groupSlug: 'pci-neuroscience',
+  groupName: 'Peer Community in Neuroscience',
+  avatarPath: '/static/groups/pci-neuroscience--af792cd3-1600-465c-89e5-250c48f793aa.png',
 };
 
 export const card3 = {
-  articleId: '10.1101/215855',
-  articleTitle: 'A practical tool for Maximal Information Coefficient analysis',
-  date: new Date('2022-06-23'),
-  groupSlug: 'gigascience',
-  groupName: 'GigaScience',
-  avatarPath: '/static/groups/gigascience--d6e1a913-76f8-40dc-9074-8eac033e1bc8.png',
+  articleId: '10.1101/2022.06.01.494423',
+  articleTitle: 'Long-read HiFi Sequencing Correctly Assembles Repetitive <i>heavy fibroin</i> Silk Genes in New Moth and Caddisfly Genomes',
+  date: new Date('2022-07-22'),
+  groupSlug: 'gigabyte',
+  groupName: 'GigaByte',
+  avatarPath: '/static/groups/gigabyte--f7a7aec3-8b1c-4b81-b098-f3f2e4eefe58.png',
 };
