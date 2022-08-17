bf3242ff2920958b907d0783e7c3bc137377bcd7
LinaKind <l.kinduryte@elifesciences.org>
2022-08-17 12:03:40 +0100

    Remove obsolete hard-coded data #1725

--- src/shared-read-models/lists/get-list.ts
+++ src/shared-read-models/lists/get-list.ts
@@ -1,7 +1,6 @@
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as RM from 'fp-ts/ReadonlyMap';
-import * as R from 'fp-ts/Record';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as S from 'fp-ts/string';
@@ -11,7 +10,6 @@ import { DomainEvent } from '../../domain-events';
 import { isArticleAddedToListEvent } from '../../domain-events/article-added-to-list-event';
 import * as DE from '../../types/data-error';
 import * as Gid from '../../types/group-id';
-import * as Lid from '../../types/list-id';
 import { ListId } from '../../types/list-id';
 
 type GetList = (listId: ListId) => (events: ReadonlyArray<DomainEvent>) => TE.TaskEither<DE.DataError, List>;
@@ -49,49 +47,7 @@ const listFromEvents = (
 );
 
 export const getList: GetList = (listId) => (events) => pipe(
-  {
-    'cbd478fe-3ff7-4125-ac9f-c94ff52ae0f7': {
-      id: Lid.fromValidatedString('cbd478fe-3ff7-4125-ac9f-c94ff52ae0f7'),
-      name: 'High interest articles',
-      description: 'Articles that have been identified as high interest by NCRC editors.',
-      createdOn: new Date('2021-11-24'),
-      ownerId: Gid.fromValidatedString('62f9b0d0-8d43-4766-a52a-ce02af61bc6a'),
-      articleCount: 0,
-    },
-    '5ac3a439-e5c6-4b15-b109-92928a740812': {
-      id: Lid.fromValidatedString('5ac3a439-e5c6-4b15-b109-92928a740812'),
-      name: 'Endorsed articles',
-      description: 'Articles that have been endorsed by Biophysics Colab.',
-      createdOn: new Date('2021-11-22T15:09:00Z'),
-      ownerId: Gid.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2'),
-      articleCount: 0,
-    },
-    'c7237468-aac1-4132-9598-06e9ed68f31d': {
-      id: Lid.fromValidatedString('c7237468-aac1-4132-9598-06e9ed68f31d'),
-      name: 'Medicine',
-      description: 'Medicine articles that have been evaluated by eLife.',
-      createdOn: new Date('2022-02-01T13:14:00Z'),
-      ownerId: Gid.fromValidatedString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'),
-      articleCount: 0,
-    },
-    'cb15ef21-944d-44d6-b415-a3d8951e9e8b': {
-      id: Lid.fromValidatedString('cb15ef21-944d-44d6-b415-a3d8951e9e8b'),
-      name: 'Cell Biology',
-      description: 'Cell Biology articles that have been evaluated by eLife.',
-      createdOn: new Date('2022-02-09T09:43:00Z'),
-      ownerId: Gid.fromValidatedString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'),
-      articleCount: 0,
-    },
-    '3253c905-8083-4f3d-9e1f-0a8085e64ee5': {
-      id: Lid.fromValidatedString('3253c905-8083-4f3d-9e1f-0a8085e64ee5'),
-      name: 'Neuroscience',
-      description: 'Neuroscience articles that have been evaluated by eLife.',
-      createdOn: new Date('2022-03-03T09:25:00Z'),
-      ownerId: Gid.fromValidatedString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'),
-      articleCount: 0,
-    },
-  },
-  R.lookup(listId),
+  O.none,
   TE.fromOption(() => DE.notFound),
   TE.map(addLastUpdatedFromEvents(events, listId)),
   TE.alt(listFromEvents(events, listId)),
--- test/shared-read-models/lists/get-list.test.ts
+++ test/shared-read-models/lists/get-list.test.ts
@@ -2,7 +2,6 @@ import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { articleAddedToList, listCreated } from '../../../src/domain-events';
 import { getList, List } from '../../../src/shared-read-models/lists';
-import * as Lid from '../../../src/types/list-id';
 import { arbitraryDate, arbitraryString } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 import { arbitraryArticleId } from '../../types/article-id.helper';
@@ -15,37 +14,6 @@ describe('get-list', () => {
   });
 
   describe('when the listId does exist', () => {
-    describe('and it refers to a hardcoded list', () => {
-      describe('when the list is empty', () => {
-        it.todo('returns the correct List');
-
-        it.todo('with the correct last updated date');
-      });
-
-      describe('when the list is non-empty', () => {
-        const latestDate = arbitraryDate();
-        const eLifeMedicineListId = Lid.fromValidatedString('c7237468-aac1-4132-9598-06e9ed68f31d');
-        let result: List;
-
-        beforeEach(async () => {
-          result = await pipe(
-            [
-              articleAddedToList(arbitraryArticleId(), eLifeMedicineListId, arbitraryDate()),
-              articleAddedToList(arbitraryArticleId(), eLifeMedicineListId, latestDate),
-            ],
-            getList(eLifeMedicineListId),
-            TE.getOrElse(shouldNotBeCalled),
-          )();
-        });
-
-        it.todo('returns the correct List');
-
-        it('with the correct last updated date', () => {
-          expect(result.lastUpdated).toStrictEqual(latestDate);
-        });
-      });
-    });
-
     describe('and it refers to a non-hardcoded list', () => {
       const listId = arbitraryListId();
       const name = arbitraryString();
