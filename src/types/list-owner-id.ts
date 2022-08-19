47ba3b224ca3881759fa334c702b03eec18d72ca
Giorgio Sironi <g.sironi@elifesciences.org>
2022-08-19 09:37:37 +0100

    Make coupling explicit between type and codec #1727

--- src/types/list-owner-id.ts
+++ src/types/list-owner-id.ts
@@ -1,9 +1,7 @@
 import * as t from 'io-ts';
 import { GroupIdFromString } from './codecs/GroupIdFromString';
 import { UserIdFromString } from './codecs/UserIdFromString';
-import { GroupId } from './group-id';
-import { UserId } from './user-id';
 
 export const listOwnerIdCodec = t.union([GroupIdFromString, UserIdFromString]);
 
-export type ListOwnerId = GroupId | UserId;
+export type ListOwnerId = t.TypeOf<typeof listOwnerIdCodec>;
