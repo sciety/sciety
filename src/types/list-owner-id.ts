4a2ed084e9ddc2c22b1d2838f65eda953b29454c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-08-19 10:34:40 +0100

    Encapsulate construction concerns in the type #1727

--- src/types/list-owner-id.ts
+++ src/types/list-owner-id.ts
@@ -1,7 +1,13 @@
 import * as t from 'io-ts';
 import { GroupIdFromString } from './codecs/GroupIdFromString';
 import { UserIdFromString } from './codecs/UserIdFromString';
+import { GroupId } from './group-id';
+import { UserId } from './user-id';
 
 export const listOwnerIdCodec = t.union([GroupIdFromString, UserIdFromString]);
 
 export type ListOwnerId = t.TypeOf<typeof listOwnerIdCodec>;
+
+export const fromGroupId = (groupId: GroupId): ListOwnerId => groupId;
+
+export const fromUserId = (userId: UserId): ListOwnerId => userId;
