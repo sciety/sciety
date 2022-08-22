b96e71b2ac4af6b719658eed0c5dd6bda8b02823
LinaKind <l.kinduryte@elifesciences.org>
2022-08-22 10:14:04 +0100

    Get list-owner-id file to compile with a discriminated union #1727

--- src/types/list-owner-id.ts
+++ src/types/list-owner-id.ts
@@ -6,16 +6,22 @@ import { UserIdFromString } from './codecs/UserIdFromString';
 import { GroupId } from './group-id';
 import { UserId } from './user-id';
 
-export const listOwnerIdCodec = t.union([GroupIdFromString, UserIdFromString]);
+export const listOwnerIdCodec = t.union([
+  t.type({ value: GroupIdFromString, tag: t.literal('group-id') }),
+  t.type({ value: UserIdFromString, tag: t.literal('user-id') }),
+]);
 
 export type ListOwnerId = t.TypeOf<typeof listOwnerIdCodec>;
 
-export const fromGroupId = (groupId: GroupId): ListOwnerId => groupId;
+export const fromGroupId = (groupId: GroupId): ListOwnerId => ({ value: groupId, tag: 'group-id' });
 
-export const fromUserId = (userId: UserId): ListOwnerId => userId;
+export const fromUserId = (userId: UserId): ListOwnerId => ({ value: userId, tag: 'user-id' });
 
-export const eqListOwnerId: Eq.Eq<ListOwnerId> = S.Eq;
+export const eqListOwnerId: Eq.Eq<ListOwnerId> = Eq.struct({ value: S.Eq, tag: S.Eq });
 
-export const toString = (listOwnerId: ListOwnerId): string => listOwnerId;
+export const toString = (listOwnerId: ListOwnerId): string => `${listOwnerId.tag}:${listOwnerId.value}`;
 
-export const fromValidatedString = (str: string): ListOwnerId => str as GroupId;
+export const fromValidatedString = (str: string): ListOwnerId => {
+  const firstPart = str.split(':')[0];
+  return firstPart === 'group-id' ? fromGroupId(str.split(':')[1] as GroupId) : fromUserId(str.split(':')[1] as UserId);
+};
