dd2f059d1174202e6283756e755076df7e89c419
Giorgio Sironi <g.sironi@elifesciences.org>
2022-08-23 13:12:05 +0100

    Write test list for ListOwnerId serialization #1727

---
+++ test/types/list-owner-id.test.ts
@@ -0,0 +1,21 @@
+describe('list-owner-id', () => {
+  describe('given a list owner id that contains a user id', () => {
+    it.todo('encodes and decodes to a right of the same value');
+  });
+
+  describe('given a list owner id that contains a group id', () => {
+    it.todo('encodes and decodes to a right of the same value');
+  });
+
+  describe('given an invalid tag', () => {
+    it.todo('returns on the left');
+  });
+
+  describe('given an invalid value', () => {
+    it.todo('returns on the left');
+  });
+
+  describe('given an invalid separator', () => {
+    it.todo('returns on the left');
+  });
+});
