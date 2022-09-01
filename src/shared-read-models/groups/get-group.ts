8b7f05d16fa174ffa289571c662edf9f63ac5998
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-09-01 11:51:44 +0100

    Use shared GroupsReadModel type #1751

--- src/shared-read-models/groups/get-group.ts
+++ src/shared-read-models/groups/get-group.ts
@@ -2,13 +2,12 @@ import * as E from 'fp-ts/Either';
 import * as RM from 'fp-ts/ReadonlyMap';
 import { flow } from 'fp-ts/function';
 import * as S from 'fp-ts/string';
+import { GroupsReadModel } from './read-model';
 import * as DE from '../../types/data-error';
 import { Group } from '../../types/group';
 import { GroupId } from '../../types/group-id';
 
-type ReadModel = Map<GroupId, Group>;
-
-type GetGroup = (groupId: GroupId) => (groupsReadModel: ReadModel) => E.Either<DE.DataError, Group>;
+type GetGroup = (groupId: GroupId) => (groupsReadModel: GroupsReadModel) => E.Either<DE.DataError, Group>;
 
 export const getGroup: GetGroup = (groupId: GroupId) => flow(
   RM.lookup(S.Eq)(groupId),
