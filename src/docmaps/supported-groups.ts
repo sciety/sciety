60a8e143f66a0f59a9a2df500ead73308f8b48a0
Giorgio Sironi <g.sironi@elifesciences.org>
2022-09-01 10:04:32 +0100

    Enable Arcadia Science group on Docmaps #1750

--- src/docmaps/supported-groups.ts
+++ src/docmaps/supported-groups.ts
@@ -1,5 +1,6 @@
 import * as GID from '../types/group-id';
 
+const arcadiaScienceGroupId = GID.fromValidatedString('bc1f956b-12e8-4f5c-aadc-70f91347bd18');
 const asapBioGroupId = GID.fromValidatedString('50401e46-b764-47b7-8557-6bb35444b7c8');
 const biophysicsColabGroupId = GID.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2');
 const ncrcGroupId = GID.fromValidatedString('62f9b0d0-8d43-4766-a52a-ce02af61bc6a');
@@ -9,6 +10,7 @@ const preReviewGroupId = GID.fromValidatedString('10360d97-bf52-4aef-b2fa-2f60d3
 const rapidReviewsGroupId = GID.fromValidatedString('5142a5bc-6b18-42b1-9a8d-7342d7d17e94');
 
 export const supportedGroups = [
+  arcadiaScienceGroupId,
   asapBioGroupId,
   biophysicsColabGroupId,
   ncrcGroupId,
