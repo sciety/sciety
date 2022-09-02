62efd4f9c0518466b6e8c4f468d7c9401496784a
LinaKind <l.kinduryte@elifesciences.org>
2022-09-02 11:33:46 +0100

    Couple collected ports to shared ports rather than adapters #1746

--- src/infrastructure/collected-ports.ts
+++ src/infrastructure/collected-ports.ts
@@ -1,12 +1,10 @@
 import * as O from 'fp-ts/Option';
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
-import { CommitEvents } from './commit-events';
 import { FetchReview } from './fetch-review';
 import { FetchStaticFile } from './fetch-static-file';
-import { Logger } from './logger';
-import { DomainEvent } from '../domain-events';
-import { AddArticleToList } from '../shared-ports';
+import {
+  AddArticleToList, CommitEvents, GetAllEvents, Logger,
+} from '../shared-ports';
 import { List } from '../shared-read-models/lists';
 import { GetArticleVersionEventsFromBiorxiv } from '../third-parties/biorxiv';
 import { GetBiorxivOrMedrxivSubjectArea } from '../third-parties/biorxiv/get-biorxiv-or-medrxiv-subject-area';
@@ -23,7 +21,7 @@ export type CollectedPorts = {
   fetchReview: FetchReview,
   fetchStaticFile: FetchStaticFile,
   findVersionsForArticleDoi: GetArticleVersionEventsFromBiorxiv,
-  getAllEvents: T.Task<ReadonlyArray<DomainEvent>>,
+  getAllEvents: GetAllEvents,
   getBiorxivOrMedrxivSubjectArea: GetBiorxivOrMedrxivSubjectArea,
   getListsOwnedBy: (ownerId: ListOwnerId) => TE.TaskEither<DE.DataError, ReadonlyArray<List>>,
   getUserDetails: GetTwitterUserDetails,
