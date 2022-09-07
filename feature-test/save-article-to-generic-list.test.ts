0e8c1b38d6cb6c74c7080e30c827e518892a192d
David Moulton <d.moulton@elifesciences.org>
2022-09-07 11:00:20 +0100

    Write a partial failing test #1757

--- feature-test/save-article-to-generic-list.test.ts
+++ feature-test/save-article-to-generic-list.test.ts
@@ -11,13 +11,24 @@ describe('save-article-to-generic-list', () => {
   afterEach(screenshotTeardown);
 
   describe('when logged in', () => {
+    const testUserId = '1338873008283377664';
+
     beforeEach(async () => {
       await goto('localhost:8080/');
       await click('Log in');
     });
 
     describe('when the user saves the article', () => {
-      it.todo('the article should appear in the user\'s generic list page');
+      beforeEach(async () => {
+        await goto('localhost:8080/articles/activity/10.1101/2021.12.06.471423');
+        await click('Save to my list');
+      });
+
+      it.skip('the article should appear in the user\'s generic list page', async () => {
+        await goto(`localhost:8080/lists/list-id-${testUserId}`);
+
+        expect(true).toBe(false);
+      });
     });
   });
 });
