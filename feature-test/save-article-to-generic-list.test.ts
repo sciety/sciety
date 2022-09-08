2c7e30b052768431034d1420b9860a669014d8fa
LinaKind <l.kinduryte@elifesciences.org>
2022-09-08 13:32:29 +0100

    Implement first test for existing behaviour #1757

--- feature-test/save-article-to-generic-list.test.ts
+++ feature-test/save-article-to-generic-list.test.ts
@@ -52,6 +52,38 @@ describe('save-article-to-list', () => {
     });
 
     describe('and the user only has the default user list', () => {
+      const userHandle = 'account27775998';
+
+      describe('when the user saves an article that isn\'t in any list', () => {
+        const articleId = '10.1101/2021.12.06.471423';
+
+        beforeEach(async () => {
+          await goto(`localhost:8080/articles/activity/${articleId}`);
+          await click('Save to my list');
+        });
+
+        it('the article should appear in the list page', async () => {
+          await goto(`localhost:8080/users/${userHandle}/lists/saved-articles`);
+
+          const articleIsDisplayed = await $(`.article-card__link[href="/articles/activity/${articleId}"]`).exists();
+
+          expect(articleIsDisplayed).toBe(true);
+        });
+
+        it.todo('the article is counted in the list card on the user account page');
+
+        it.todo('the user\'s action appears in the Sciety feed');
+
+        it.todo('the list count of the article card on the search page increases by one');
+
+        it.todo('the list count of the article card on the list page it is in increases by one');
+
+        it.todo('the last updated date of the list card on the user\'s list page is updated');
+
+        it.todo('the save article button on the article\'s page is replaced with link to the list');
+
+        it.todo('the article card on the list page offers a delete button');
+      });
     });
   });
 });
