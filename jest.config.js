51cd0dde9f508560c7142b4413f42bfd3109b4fe
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-09-09 13:29:36 +0100

    Bump jest ecosystem to version 29

--- jest.config.js
+++ jest.config.js
@@ -5,18 +5,15 @@ module.exports = {
   ],
   coverageDirectory: './coverage',
   coverageReporters: ['json', 'lcov', 'html', 'text'],
-  globals: {
-    'ts-jest': {
-      diagnostics: false,
-      isolatedModules: true,
-      tsconfig: 'tsconfig.dev.json',
-    },
-  },
   roots: ['./src/', './test/'],
   testEnvironment: 'node',
   testRunner: 'jest-circus/runner',
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': ['ts-jest', {
+      diagnostics: false,
+      isolatedModules: true,
+      tsconfig: 'tsconfig.dev.json',
+    }],
   },
   verbose: true,
 };
