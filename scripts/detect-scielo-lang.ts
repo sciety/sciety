b356a645f53793e725efeca19d6fbdd11b30072d
David Moulton <d.moulton@elifesciences.org>
2022-09-13 11:59:08 +0100

    Expand with examples for all three Scielo-supported languages

--- scripts/detect-scielo-lang.ts
+++ scripts/detect-scielo-lang.ts
@@ -1,7 +1,14 @@
 import { detect } from 'tinyld';
 
-const text = 'Evolución de casos confirmados de COVID-19 en cinco países con transmisión comunitaria de la enfermedad';
+const titleEn = 'Overview of confirmed cases of COVID-19 in five countries facing community transmission';
+const titleEs = 'Evolución de casos confirmados de COVID-19 en cinco países con transmisión comunitaria de la enfermedad';
+const titlePt = 'Evolução de casos confirmados de COVID-19 em cinco países com transmissão comunitária da doença';
 
 const detectLanguage = (t: string): string => detect(t, { only: ['en', 'es', 'pt'] });
 
-console.log(detectLanguage(text));
+console.log('English text:', titleEn);
+console.log('detected:', detectLanguage(titleEn));
+console.log('Spanish text:', titleEs);
+console.log('detected:', detectLanguage(titleEs));
+console.log('Portuguese text:', titlePt);
+console.log('detected:', detectLanguage(titlePt));
