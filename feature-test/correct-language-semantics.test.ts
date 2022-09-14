16766c3930dbacf52632dc14afe4763bc0bfde0d
LinaKind <l.kinduryte@elifesciences.org>
2022-09-14 15:08:03 +0100

    Make title used in test customisable #1778

--- feature-test/correct-language-semantics.test.ts
+++ feature-test/correct-language-semantics.test.ts
@@ -1,6 +1,13 @@
-import {
-  $, closeBrowser, goto, openBrowser,
-} from 'taiko';
+import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import * as TO from 'fp-ts/TaskOption';
+import { closeBrowser, openBrowser } from 'taiko';
+import { articleActivityPage } from '../src/article-page';
+import { SanitisedHtmlFragment } from '../src/types/sanitised-html-fragment';
+import { arbitrarySanitisedHtmlFragment } from '../test/helpers';
+import { arbitraryDoi } from '../test/types/doi.helper';
 
 describe('correct-language-semantics', () => {
   beforeAll(async () => {
@@ -13,8 +20,6 @@ describe('correct-language-semantics', () => {
 
   describe('in the article page', () => {
     describe('the article title', () => {
-      const articleTitleSelector = 'h1 > span:first-child';
-
       describe('when detected as Portuguese', () => {
         it.todo('is marked up as Portuguese');
       });
@@ -25,10 +30,28 @@ describe('correct-language-semantics', () => {
 
       describe('when detected as English', () => {
         it('is marked up as English', async () => {
-          await goto('localhost:8080/articles/activity/10.1101/2020.11.12.379909');
-          const languageAttribute = await $(articleTitleSelector).attribute('lang');
+          const getArticleDetails = () => (TE.right({
+            doi: arbitraryDoi(),
+            title: 'Arbitrary title in English' as SanitisedHtmlFragment,
+            abstract: arbitrarySanitisedHtmlFragment(),
+            server: 'biorxiv' as const,
+            authors: O.none,
+          }));
+          const ports = {
+            fetchArticle: getArticleDetails,
+            fetchReview: () => TE.left(undefined),
+            findVersionsForArticleDoi: () => TO.none,
+            getAllEvents: T.of([]),
+          };
+          const renderPage = articleActivityPage(ports);
+          const rendered = await renderPage({
+            doi: arbitraryDoi(),
+            user: O.none,
+          })();
 
-          expect(languageAttribute).toBe('en');
+          expect(rendered).toStrictEqual(E.right(expect.objectContaining({
+            content: expect.stringContaining('<h1><span lang="en">Arbitrary title in English</span></h1>'),
+          })));
         });
       });
     });
