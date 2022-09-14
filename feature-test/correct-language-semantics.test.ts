a2e4bd4b0284bf5529ab66092f4bf79201938b88
Giorgio Sironi <g.sironi@elifesciences.org>
2022-09-14 10:57:29 +0100

    Begin to express intention with a customer test #1778

---
+++ feature-test/correct-language-semantics.test.ts
@@ -0,0 +1,9 @@
+describe('correct-language-semantics', () => {
+  describe('the article page', () => {
+    it.todo('does not need to mark up English content as the HTML document is already marked up as being in English');
+
+    it.todo('marks up Portuguese content as Portuguese');
+
+    it.todo('marks up Spanish content as Spanish');
+  });
+});
