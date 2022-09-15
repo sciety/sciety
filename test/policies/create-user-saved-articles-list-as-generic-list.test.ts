dd500e5aa51e1e198ed96bf3d326f6ea3259756d
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-09-15 11:15:42 +0100

    Decide on default list name and description with product manager #1773

--- test/policies/create-user-saved-articles-list-as-generic-list.test.ts
+++ test/policies/create-user-saved-articles-list-as-generic-list.test.ts
@@ -5,9 +5,9 @@ describe('create-user-saved-articles-list-as-generic-list', () => {
 
       it.todo('calls the command with the user as the owner');
 
-      it.todo('calls the command with "Saved Articles" as a name');
+      it.todo('calls the command with "@{handle}\'s saved articles" as a name');
 
-      it.todo('calls the command with "Articles that have been saved by @ZonaPellucida_" as a description');
+      it.todo('calls the command with "Articles that have been saved by @{handle}" as a description');
     });
   });
 });
