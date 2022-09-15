e8a00f8bca1afaa52dc7af63ee70cfc06f49ee23
Giorgio Sironi <g.sironi@elifesciences.org>
2022-09-15 10:39:12 +0100

    Sketch happy path test list for policy #1773

---
+++ test/policies/create-user-saved-articles-list-as-generic-list.test.ts
@@ -0,0 +1,13 @@
+describe('create-user-saved-articles-list-as-generic-list', () => {
+  describe('when a UserSavedArticle event is received', () => {
+    describe('and that user owns no generic list', () => {
+      it.todo('calls the CreateList command');
+
+      it.todo('calls the command with the user as the owner');
+
+      it.todo('calls the command with "Saved Articles" as a name');
+
+      it.todo('calls the command with "Articles that have been saved by @ZonaPellucida_" as a description');
+    });
+  });
+});
