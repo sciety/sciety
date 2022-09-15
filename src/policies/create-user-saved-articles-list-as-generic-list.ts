73dd00ac92f69544a1d7d92845da5d1a7bb20ff1
Giorgio Sironi <g.sironi@elifesciences.org>
2022-09-15 11:55:14 +0100

    Make test pass #1773

--- src/policies/create-user-saved-articles-list-as-generic-list.ts
+++ src/policies/create-user-saved-articles-list-as-generic-list.ts
@@ -2,6 +2,8 @@ import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { DomainEvent } from '../domain-events';
+import { isUserSavedArticleEvent } from '../domain-events/user-saved-article-event';
+import * as LOID from '../types/list-owner-id';
 
 // ts-unused-exports:disable-next-line
 export type Ports = {
@@ -11,9 +13,12 @@ export type Ports = {
 type CreateUserSavedArticlesListAsGenericList = (ports: Ports) => (event: DomainEvent) => T.Task<undefined>;
 
 // ts-unused-exports:disable-next-line
-export const createUserSavedArticlesListAsGenericList: CreateUserSavedArticlesListAsGenericList = (ports) => () => pipe(
-  'foo',
-  TE.right,
+export const createUserSavedArticlesListAsGenericList: CreateUserSavedArticlesListAsGenericList = (
+  ports,
+) => (event) => pipe(
+  event,
+  TE.fromPredicate(isUserSavedArticleEvent, () => 'event not of interest'),
+  TE.map(({ userId }) => ({ ownerId: LOID.fromUserId(userId) })),
   TE.chain(ports.createList),
   TE.match(
     () => undefined,
