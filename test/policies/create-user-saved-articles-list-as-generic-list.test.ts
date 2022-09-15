38314efad3b239d009b684fc561113b20f7244b5
Giorgio Sironi <g.sironi@elifesciences.org>
2022-09-15 11:25:52 +0100

    Expand scenarios in test list #1773

--- test/policies/create-user-saved-articles-list-as-generic-list.test.ts
+++ test/policies/create-user-saved-articles-list-as-generic-list.test.ts
@@ -1,13 +1,27 @@
 describe('create-user-saved-articles-list-as-generic-list', () => {
   describe('when a UserSavedArticle event is received', () => {
     describe('and that user owns no generic list', () => {
-      it.todo('calls the CreateList command');
+      describe('if the command succeeds', () => {
+        it.todo('calls the CreateList command');
 
-      it.todo('calls the command with the user as the owner');
+        it.todo('calls the command with the user as the owner');
 
-      it.todo('calls the command with "@{handle}\'s saved articles" as a name');
+        it.todo('calls the command with "@{handle}\'s saved articles" as a name');
 
-      it.todo('calls the command with "Articles that have been saved by @{handle}" as a description');
+        it.todo('calls the command with "Articles that have been saved by @{handle}" as a description');
+      });
+
+      describe('if the command fails', () => {
+        it.todo('logs an error');
+      });
     });
+
+    describe('and the user already owns a generic list', () => {
+      it.todo('does not call the CreateList command');
+    });
+  });
+
+  describe('when any other event is received', () => {
+    it.todo('does not call the CreateList command');
   });
 });
