d24aaa483815d01f9935e3add13a592c6990e837
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-09-15 11:40:12 +0100

    Ignore unused exports #1773

--- src/policies/create-user-saved-articles-list-as-generic-list.ts
+++ src/policies/create-user-saved-articles-list-as-generic-list.ts
@@ -2,11 +2,13 @@ import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { DomainEvent } from '../domain-events';
 
+// ts-unused-exports:disable-next-line
 export type Ports = {
   createList: () => TE.TaskEither<unknown, void>,
 };
 
 type CreateUserSavedArticlesListAsGenericList = (ports: Ports) => (event: DomainEvent) => T.Task<undefined>;
 
+// ts-unused-exports:disable-next-line
 export const createUserSavedArticlesListAsGenericList: CreateUserSavedArticlesListAsGenericList = (
 ) => () => T.of(undefined);
