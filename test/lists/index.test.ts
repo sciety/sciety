2d08b9041b510e3b56bfb9cab7aba502321bfa6b
Giorgio Sironi <g.sironi@elifesciences.org>
2022-09-16 10:50:33 +0100

    Write passing test to capture decision about behavior #1773

--- test/lists/index.test.ts
+++ test/lists/index.test.ts
@@ -1,4 +1,5 @@
 import { pipe } from 'fp-ts/function';
+import { validate as isUuid } from 'uuid';
 import { isListCreatedEvent } from '../../src/domain-events/list-created-event';
 import { executeCreateListCommand } from '../../src/lists';
 import { arbitraryString } from '../helpers';
@@ -18,6 +19,10 @@ describe('index', () => {
     it('returns a ListCreated event', () => {
       expect(result).toHaveLength(1);
       expect(isListCreatedEvent(result[0])).toBe(true);
+    });
+
+    it('returns a ListCreated event with a uuid as its listId', () => {
+      expect(isUuid(result[0].listId)).toBe(true);
     });
 
     it.todo('returns a ListCreated event containing the requested owner');
