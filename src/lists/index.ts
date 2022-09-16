23047f05efe619cc47209777ae83072ca8a41d60
Giorgio Sironi <g.sironi@elifesciences.org>
2022-09-16 14:20:48 +0100

    Try treating dependency on commands like we treat dependency on events #1773

--- src/lists/index.ts
+++ src/lists/index.ts
@@ -1,13 +1,7 @@
 import { v4 } from 'uuid';
+import { CreateListCommand } from '../commands';
 import { listCreated, ListCreatedEvent } from '../domain-events';
 import * as LID from '../types/list-id';
-import * as LOID from '../types/list-owner-id';
-
-type CreateListCommand = {
-  ownerId: LOID.ListOwnerId,
-  name: string,
-  description: string,
-};
 
 type ExecuteCreateListCommand = (command: CreateListCommand) => ReadonlyArray<ListCreatedEvent>;
 
--- src/shared-ports/create-list.ts
+++ src/shared-ports/create-list.ts
@@ -1,11 +1,5 @@
 import * as TE from 'fp-ts/TaskEither';
+import { CreateListCommand } from '../commands';
 import * as DE from '../types/data-error';
-import * as LOID from '../types/list-owner-id';
-
-type CreateListCommand = {
-  ownerId: LOID.ListOwnerId,
-  name: string,
-  description: string,
-};
 
 export type CreateList = (command: CreateListCommand) => TE.TaskEither<DE.DataError, void>;
