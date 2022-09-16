758ff8a8c0973996987e72f95eb910dd95bc76b2
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-09-16 10:33:29 +0100

    Write test list for create list command execution #1773

---
+++ test/lists/index.test.ts
@@ -0,0 +1,11 @@
+describe('index', () => {
+  describe('when a command is received', () => {
+    it.todo('returns a ListCreated event');
+
+    it.todo('returns a ListCreated event containing the requested owner');
+
+    it.todo('returns a ListCreated event containing the requested name');
+
+    it.todo('returns a ListCreated event containing the requested description');
+  });
+});
