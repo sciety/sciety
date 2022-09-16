3bb355d5ff338dbd578124c2bfdc0ff966badba9
Giorgio Sironi <g.sironi@elifesciences.org>
2022-09-16 10:39:30 +0100

    Write first failing test #1773

--- test/lists/index.test.ts
+++ test/lists/index.test.ts
@@ -1,6 +1,24 @@
+import { pipe } from 'fp-ts/function';
+import { isListCreatedEvent } from '../../src/domain-events/list-created-event';
+import { executeCreateListCommand } from '../../src/lists';
+import { arbitraryString } from '../helpers';
+import { arbitraryListOwnerId } from '../types/list-owner-id.helper';
+
 describe('index', () => {
   describe('when a command is received', () => {
-    it.todo('returns a ListCreated event');
+    const result = pipe(
+      {
+        ownerId: arbitraryListOwnerId(),
+        name: arbitraryString(),
+        description: arbitraryString(),
+      },
+      executeCreateListCommand,
+    );
+
+    it.skip('returns a ListCreated event', () => {
+      expect(result).toHaveLength(1);
+      expect(isListCreatedEvent(result[0])).toBe(true);
+    });
 
     it.todo('returns a ListCreated event containing the requested owner');
 
