bee4b60f522f38dc7359b9a15e9b273f654e0c10
Kevin Rutherford <kevin@rutherford-software.com>
2022-09-20 14:15:31 +0100

    Move article server definitions to one place #1781

--- src/article-page/activity-feed/render-article-version-error-feed-item.ts
+++ src/article-page/activity-feed/render-article-version-error-feed-item.ts
@@ -1,39 +1,10 @@
 import { pipe } from 'fp-ts/function';
 import { retryLater } from './static-messages';
-import { ArticleServer } from '../../types/article-server';
+import { ArticleServer, articleServers } from '../../types/article-server';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 
-type ServerInfo = {
-  name: string,
-  avatarUrl: string,
-  versionsSupported: boolean,
-};
-
-const servers: Record<ArticleServer, ServerInfo> = {
-  biorxiv: {
-    name: 'bioRxiv',
-    avatarUrl: '/static/images/biorxiv.jpg',
-    versionsSupported: true,
-  },
-  medrxiv: {
-    name: 'medRxiv',
-    avatarUrl: '/static/images/medrxiv.jpg',
-    versionsSupported: true,
-  },
-  researchsquare: {
-    name: 'Research Square',
-    avatarUrl: '/static/images/researchsquare.png',
-    versionsSupported: false,
-  },
-  scielopreprints: {
-    name: 'SciELO Preprints',
-    avatarUrl: '/static/images/scielo.svg',
-    versionsSupported: false,
-  },
-};
-
 export const renderVersionErrorFeedItem = (server: ArticleServer): HtmlFragment => pipe(
-  servers[server],
+  articleServers[server],
   (viewModel) => `
     <div class="activity-feed__item__contents">
       <header class="activity-feed__item__header">
--- src/types/article-server.ts
+++ src/types/article-server.ts
@@ -1,3 +1,32 @@
+type ServerInfo = {
+  name: string,
+  avatarUrl: string,
+  versionsSupported: boolean,
+};
+
+export const articleServers: Record<ArticleServer, ServerInfo> = {
+  biorxiv: {
+    name: 'bioRxiv',
+    avatarUrl: '/static/images/biorxiv.jpg',
+    versionsSupported: true,
+  },
+  medrxiv: {
+    name: 'medRxiv',
+    avatarUrl: '/static/images/medrxiv.jpg',
+    versionsSupported: true,
+  },
+  researchsquare: {
+    name: 'Research Square',
+    avatarUrl: '/static/images/researchsquare.png',
+    versionsSupported: false,
+  },
+  scielopreprints: {
+    name: 'SciELO Preprints',
+    avatarUrl: '/static/images/scielo.svg',
+    versionsSupported: false,
+  },
+};
+
 export type ArticleServer = 'biorxiv'
 | 'medrxiv'
 | 'researchsquare'
