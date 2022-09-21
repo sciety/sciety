19599e1dd89cec37a9199ed189ca6dd8c2b86766
Kevin Rutherford <kevin@rutherford-software.com>
2022-09-21 11:01:03 +0100

    Add failing implementation for first test of /add-group #1782

--- feature-test/add-group.test.ts
+++ feature-test/add-group.test.ts
@@ -1,5 +1,48 @@
-describe('add-group', () => {
-  it.todo('appears in the list of groups on the Groups page');
+import axios from 'axios';
+import {
+  $, closeBrowser, goto, openBrowser, text, within,
+} from 'taiko';
+import { arbitraryString, arbitraryWord } from '../test/helpers';
+
+describe.skip('add-group', () => {
+  const newGroup = {
+    name: arbitraryWord(),
+    shortDescription: arbitraryString(),
+  };
+
+  beforeAll(async () => {
+    await openBrowser();
+    await axios.post(
+      'http://localhost:8080/add-group',
+      JSON.stringify(newGroup),
+      {
+        headers: {
+          Authorization: 'Bearer secret',
+          'Content-Type': 'application/json',
+        },
+        timeout: 5000,
+      },
+    );
+  });
+
+  afterAll(async () => {
+    await closeBrowser();
+  });
+
+  describe('the list of groups on the groups page', () => {
+    beforeAll(async () => {
+      await goto('localhost:8080/groups');
+    });
+
+    it.each([
+      [newGroup.name],
+      [newGroup.shortDescription],
+    ])('the new group appears', async (expectedString) => {
+      const itemExists = await text(expectedString, within($('.group-list'))).exists();
+
+      expect(itemExists).toBe(true);
+    });
+  });
 
   it.todo('the group now has its own page');
 
