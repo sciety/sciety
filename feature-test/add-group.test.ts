2144779f132173229e8a00d902db05f6264542dc
David Moulton <d.moulton@elifesciences.org>
2022-09-21 10:26:54 +0100

    Express intention to add a command to add groups #1782

---
+++ feature-test/add-group.test.ts
@@ -0,0 +1,9 @@
+describe('add-group', () => {
+  it.todo('appears in the list of groups on the Groups page');
+
+  it.todo('the group now has its own page');
+
+  it.todo('can be searched for');
+
+  it.todo('creation of the group appears in the Sciety feed');
+});
