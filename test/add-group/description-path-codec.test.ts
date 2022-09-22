0ea202bc2fe2fd5627be14e67966e303dbc090fb
LinaKind <l.kinduryte@elifesciences.org>
2022-09-22 13:29:47 +0100

    Transform test list for a new seam #1782

---
+++ test/add-group/description-path-codec.test.ts
@@ -0,0 +1,13 @@
+describe('description-path-codec', () => {
+  describe('that is a single file name with a markdown file extension', () => {
+    it.todo('is valid');
+  });
+
+  describe('that contains a folder', () => {
+    it.todo('is not valid');
+  });
+
+  describe('that does not have a markdown file extension', () => {
+    it.todo('is not valid');
+  });
+});
