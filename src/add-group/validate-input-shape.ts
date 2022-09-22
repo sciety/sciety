7a066dac1cfbf062161473591f5fcb4697941078
LinaKind <l.kinduryte@elifesciences.org>
2022-09-22 11:56:52 +0100

    Introduce more testable seam #1782

--- src/add-group/validate-input-shape.ts
+++ src/add-group/validate-input-shape.ts
@@ -2,6 +2,7 @@ import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 import * as PR from 'io-ts/PathReporter';
+import { descriptionPathCodec } from './description-path-codec';
 import { Command } from './execute-command';
 
 const inputCodec = t.type({
@@ -9,7 +10,7 @@ const inputCodec = t.type({
   shortDescription: t.string,
   homepage: t.string,
   avatarPath: t.string,
-  descriptionPath: t.string,
+  descriptionPath: descriptionPathCodec,
   slug: t.string,
 });
 
