aba3a3dded5c37c8753b0ba4ca6f2e9b616b9248
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-09-22 11:30:43 +0100

    Capture contraints for group descriptionPath #1782

---
+++ test/add-group/validate-input-shape.test.ts
@@ -0,0 +1,15 @@
+describe('validate-input-shape', () => {
+  describe('description path', () => {
+    describe('is a markdown file name', () => {
+      it.todo('returns on the right');
+    });
+
+    describe('contains multiple path segments', () => {
+      it.todo('returns on the left');
+    });
+
+    describe('does not have a markdown file extension', () => {
+      it.todo('returns on the left');
+    });
+  });
+});
