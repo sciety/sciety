035851a5bd4e7c78a35aa6450e8b9ba12095daaa
Giorgio Sironi <g.sironi@elifesciences.org>
2022-09-23 08:30:13 +0100

    Rewrite replay as a reduce over relevant events #1785

--- src/add-article-to-list/replay-aggregate.ts
+++ src/add-article-to-list/replay-aggregate.ts
@@ -1,28 +1,35 @@
 import * as E from 'fp-ts/Either';
 import * as RA from 'fp-ts/ReadonlyArray';
-import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
 import { ListAggregate } from './list-aggregate';
-import { DomainEvent, isArticleAddedToListEvent, isListCreatedEvent } from '../domain-events';
+import {
+  ArticleAddedToListEvent, DomainEvent, isArticleAddedToListEvent, isListCreatedEvent,
+} from '../domain-events';
+import { ListCreatedEvent } from '../domain-events/list-created-event';
 import { ListId } from '../types/list-id';
-
-const listAggregateByListId = (listId: ListId) => (events: ReadonlyArray<DomainEvent>): ListAggregate => pipe(
-  events,
-  RA.filter(isArticleAddedToListEvent),
-  RA.filter((event) => event.listId === listId),
-  RA.map((event) => event.articleId),
-  (articleIds) => ({ articleIds }),
-);
 
 type ReplayAggregate = (listId: ListId) => (events: ReadonlyArray<DomainEvent>) => E.Either<string, ListAggregate>;
 
+type RelevantEvent = ListCreatedEvent | ArticleAddedToListEvent;
+
+const isARelevantEventForTheWriteModel = (event: DomainEvent): event is RelevantEvent => isListCreatedEvent(event)
+ || isArticleAddedToListEvent(event);
+
+const isAnEventOfThisAggregate = (listId: ListId) => (event: RelevantEvent) => event.listId === listId;
+
 export const replayAggregate: ReplayAggregate = (listId) => (events) => pipe(
   events,
-  RA.filter(isListCreatedEvent),
-  RA.some((event) => event.listId === listId),
-  B.fold(
-    () => E.left(`List "${listId}" not found`),
-    () => E.right(undefined),
-  ),
-  E.map(() => listAggregateByListId(listId)(events)),
+  RA.filter(isARelevantEventForTheWriteModel),
+  RA.filter(isAnEventOfThisAggregate(listId)),
+  RA.reduce(E.left(`List with list id ${listId} not found`), (aggregate, event) => {
+    switch (event.type) {
+      case 'ListCreated':
+        return E.right({ articleIds: [] });
+      case 'ArticleAddedToList':
+        return pipe(
+          aggregate,
+          E.map(({ articleIds }) => ({ articleIds: [...articleIds, event.articleId] })),
+        );
+    }
+  }),
 );
