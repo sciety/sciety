0c4b6a2229a262d78d0a3b72c1c99bfbeed4b911
Giorgio Sironi <g.sironi@elifesciences.org>
2022-09-23 08:00:01 +0100

    Extract replay-aggregate #1785

--- src/add-article-to-list/index.ts
+++ src/add-article-to-list/index.ts
@@ -1,40 +1,17 @@
-import * as E from 'fp-ts/Either';
-import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
-import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
 import { executeCommand } from './execute-command';
-import { ListAggregate } from './list-aggregate';
+import { replayAggregate } from './replay-aggregate';
 import { validateInputShape } from './validate-input-shape';
-import { DomainEvent, isArticleAddedToListEvent, isListCreatedEvent } from '../domain-events';
+import { DomainEvent } from '../domain-events';
 import { CommitEvents } from '../shared-ports';
 import { CommandResult } from '../types/command-result';
-import { ListId } from '../types/list-id';
 
 export type Ports = {
   getAllEvents: T.Task<ReadonlyArray<DomainEvent>>,
   commitEvents: CommitEvents,
 };
-
-const listAggregateByListId = (listId: ListId) => (events: ReadonlyArray<DomainEvent>): ListAggregate => pipe(
-  events,
-  RA.filter(isArticleAddedToListEvent),
-  RA.filter((event) => event.listId === listId),
-  RA.map((event) => event.articleId),
-  (articleIds) => ({ articleIds }),
-);
-
-const confirmListExistsAndReturnListAggregate = (listId: ListId) => (events: ReadonlyArray<DomainEvent>) => pipe(
-  events,
-  RA.filter(isListCreatedEvent),
-  RA.some((event) => event.listId === listId),
-  B.fold(
-    () => E.left(`List "${listId}" not found`),
-    () => E.right(undefined),
-  ),
-  E.map(() => listAggregateByListId(listId)(events)),
-);
 
 type AddArticleToListCommandHandler = (
   ports: Ports
@@ -55,7 +32,7 @@ export const addArticleToListCommandHandler: AddArticleToListCommandHandler = (
   TE.chainW((command) => pipe(
     ports.getAllEvents,
     TE.rightTask,
-    TE.chainEitherK(confirmListExistsAndReturnListAggregate(command.listId)),
+    TE.chainEitherK(replayAggregate(command.listId)),
     TE.chainEitherK(executeCommand(command, date)),
   )),
   TE.chainTaskK(ports.commitEvents),
