23e2062b651ff087b9eef06cea266b29ecdcf7d1
Giorgio Sironi <g.sironi@elifesciences.org>
2022-09-23 07:52:25 +0100

    Move list existence check before the command execution #1785
    We should not try to execute a Command on an Aggregate that does not exist.

--- src/add-article-to-list/index.ts
+++ src/add-article-to-list/index.ts
@@ -1,11 +1,13 @@
+import * as E from 'fp-ts/Either';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
+import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
 import { executeCommand } from './execute-command';
 import { ListAggregate } from './list-aggregate';
 import { validateInputShape } from './validate-input-shape';
-import { DomainEvent, isArticleAddedToListEvent } from '../domain-events';
+import { DomainEvent, isArticleAddedToListEvent, isListCreatedEvent } from '../domain-events';
 import { CommitEvents } from '../shared-ports';
 import { CommandResult } from '../types/command-result';
 import { ListId } from '../types/list-id';
@@ -14,6 +16,16 @@ export type Ports = {
   getAllEvents: T.Task<ReadonlyArray<DomainEvent>>,
   commitEvents: CommitEvents,
 };
+
+const confirmListExists = (listId: ListId) => (events: ReadonlyArray<DomainEvent>) => pipe(
+  events,
+  RA.filter(isListCreatedEvent),
+  RA.some((event) => event.listId === listId),
+  B.fold(
+    () => E.left(`List "${listId}" not found`),
+    () => E.right(undefined),
+  ),
+);
 
 const listAggregateByListId = (listId: ListId) => (events: ReadonlyArray<DomainEvent>): ListAggregate => pipe(
   events,
@@ -41,8 +53,10 @@ export const addArticleToListCommandHandler: AddArticleToListCommandHandler = (
   TE.fromEither,
   TE.chainW((command) => pipe(
     ports.getAllEvents,
-    T.map(listAggregateByListId(command.listId)),
-    T.map(executeCommand(command, date)),
+    TE.rightTask,
+    TE.chainFirstEitherK(confirmListExists(command.listId)),
+    TE.map(listAggregateByListId(command.listId)),
+    TE.chainEitherK(executeCommand(command, date)),
   )),
   TE.chainTaskK(ports.commitEvents),
 );
