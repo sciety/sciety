4b0a400f2a41a17ece7a44d4f4861daf334e3282
LinaKind <l.kinduryte@elifesciences.org>
2022-09-23 11:50:40 +0100

    Prepare hard coded data for narrowing of type #1782

--- src/commands/description-path-codec.ts
+++ src/commands/description-path-codec.ts
@@ -12,3 +12,5 @@ export const descriptionPathCodec = t.brand(
 
 // ts-unused-exports:disable-next-line
 export type DescriptionPath = t.TypeOf<typeof descriptionPathCodec>;
+
+export const fromValidatedString = (input: string): DescriptionPath => input as DescriptionPath;
