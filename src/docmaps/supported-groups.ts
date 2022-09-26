84d999752bc07882e7320e8cd479a95b6aad7270
LinaKind <l.kinduryte@elifesciences.org>
2022-09-26 13:15:20 +0100

    Allow docmaps for eLife on staging and local #1788

--- src/docmaps/supported-groups.ts
+++ src/docmaps/supported-groups.ts
@@ -8,8 +8,9 @@ const peerJGroupId = GID.fromValidatedString('53ed5364-a016-11ea-bb37-0242ac1300
 const preLightsGroupId = GID.fromValidatedString('f97bd177-5cb6-4296-8573-078318755bf2');
 const preReviewGroupId = GID.fromValidatedString('10360d97-bf52-4aef-b2fa-2f60d319edd7');
 const rapidReviewsGroupId = GID.fromValidatedString('5142a5bc-6b18-42b1-9a8d-7342d7d17e94');
+const elifeGroupId = GID.fromValidatedString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0');
 
-export const supportedGroups = [
+const supportedGroups = [
   arcadiaScienceGroupId,
   asapBioGroupId,
   biophysicsColabGroupId,
@@ -19,3 +20,8 @@ export const supportedGroups = [
   preReviewGroupId,
   rapidReviewsGroupId,
 ];
+if (process.env.EXPERIMENT_ENABLED === 'true') {
+  supportedGroups.push(elifeGroupId);
+}
+
+export { supportedGroups };
