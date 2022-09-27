df6d5ac94ccfccc03385bab7cdab586ffd104ae6
LinaKind <l.kinduryte@elifesciences.org>
2022-09-27 14:23:39 +0100

    Fetch the evaluation content #1790

--- src/evaluation-content/evaluation-content.ts
+++ src/evaluation-content/evaluation-content.ts
@@ -1,6 +1,8 @@
 import { URL } from 'url';
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
+import * as DE from '../types/data-error';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 import { RenderPageError } from '../types/render-page-error';
@@ -12,7 +14,7 @@ export const paramsCodec = t.type({
   reviewid: RID.reviewIdCodec,
 });
 
-type FetchReview = (id: RID.ReviewId) => TE.TaskEither<unknown, {
+type FetchReview = (id: RID.ReviewId) => TE.TaskEither<DE.DataError, {
   fullText: HtmlFragment,
   url: URL,
 }>;
@@ -23,8 +25,17 @@ export type Ports = {
 
 type Params = t.TypeOf<typeof paramsCodec>;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const evaluationContent = (ports: Ports) => (params: Params): EvaluationContent => TE.right({
-  title: 'evaluation',
-  content: toHtmlFragment('evaluation'),
-});
+export const evaluationContent = (ports: Ports) => (params: Params): EvaluationContent => pipe(
+  params.reviewid,
+  ports.fetchReview,
+  TE.bimap(
+    (error) => ({
+      type: error,
+      message: toHtmlFragment('Could not fetch evaluation'),
+    }),
+    (review) => ({
+      title: 'Evaluation',
+      content: toHtmlFragment(review.fullText),
+    }),
+  ),
+);
