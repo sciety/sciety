4c86e1039a03054547b60f00c3855f40c8992b7a
David Moulton <d.moulton@elifesciences.org>
2022-09-28 14:21:48 +0100

    List wrangling

--- src/group-page/about/about.ts
+++ src/group-page/about/about.ts
@@ -1,13 +1,19 @@
+import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { FetchStaticFile, renderDescription } from './render-description';
 import { renderLists } from './render-lists';
+import { List } from '../../shared-read-models/lists';
 import * as DE from '../../types/data-error';
 import { Group } from '../../types/group';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
+import * as LOID from '../../types/list-owner-id';
+import { ListOwnerId } from '../../types/list-owner-id';
 
 export type Ports = {
   fetchStaticFile: FetchStaticFile,
+  getListsOwnedBy: (ownerId: ListOwnerId) => TE.TaskEither<DE.DataError, ReadonlyArray<List>>,
 };
 
 type RenderAbout = (about: { lists: HtmlFragment, description: HtmlFragment }) => HtmlFragment;
@@ -19,13 +25,37 @@ const renderAbout: RenderAbout = (about) => toHtmlFragment(`
   </div>
 `);
 
+type ListViewModel = {
+  articleCount: number,
+  lastUpdated: O.Option<Date>,
+  href: string,
+  title: string,
+  description: string,
+};
+
+type ToListViewModel = (list: List) => ListViewModel;
+
+export const toListViewModel: ToListViewModel = (list) => ({
+  ...list,
+  href: `/lists/${list.id}`,
+  title: list.name,
+  lastUpdated: O.some(list.lastUpdated),
+});
+
+const lists = (ports: Ports) => (group: Group): TE.TaskEither<DE.DataError, ReadonlyArray<ListViewModel>> => pipe(
+  group.id,
+  LOID.fromGroupId,
+  ports.getListsOwnedBy,
+  TE.map(RA.map(toListViewModel)),
+);
+
 export const about = (ports: Ports) => (group: Group): TE.TaskEither<DE.DataError, HtmlFragment> => pipe(
   `groups/${group.descriptionPath}`,
   ports.fetchStaticFile,
   TE.map(renderDescription),
   TE.map((renderedDescription) => ({
     description: renderedDescription,
-    lists: renderLists(),
+    lists: renderLists(lists),
   })),
   TE.map(renderAbout),
 );
--- src/group-page/about/render-lists.ts
+++ src/group-page/about/render-lists.ts
@@ -1,9 +1,37 @@
+import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import * as DE from '../../types/data-error';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 
-export const renderLists = (): HtmlFragment => {
+type ListViewModel = {
+  articleCount: number,
+  lastUpdated: O.Option<Date>,
+  href: string,
+  title: string,
+};
+
+type RenderList = (list: ListViewModel) => HtmlFragment;
+
+const renderList: RenderList = (list) => toHtmlFragment(`<li>${list.title} ${list.articleCount} Articles Updated: ${list.lastUpdated.toString()}</li>`);
+
+export const renderLists = (lists: ReadonlyArray<ListViewModel>): TE.TaskEither<DE.DataError, HtmlFragment> => {
   if (process.env.EXPERIMENT_ENABLED === 'true') {
-    return toHtmlFragment('Placeholder for group lists');
+    return pipe(
+      lists,
+      RA.map(renderList),
+      (list) => list.join(''),
+      (renderedLists) => `
+        <h3>Our lists</h3>
+        <ul>
+          ${renderedLists}
+        </ul>
+      `,
+      toHtmlFragment,
+      TE.right,
+    );
   }
 
-  return toHtmlFragment('');
+  return TE.right(toHtmlFragment(''));
 };
