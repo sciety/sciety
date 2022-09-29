9e39ebcf43549199d0e7ae37e991ed5b17467aef
Kevin Rutherford <kevin@rutherford-software.com>
2022-09-29 15:21:10 +0100

    Sketch out a test list for the Our Lists section #1793

---
+++ test/group-page/about/render-our-lists.test.ts
@@ -0,0 +1,19 @@
+describe('render-our-lists', () => {
+  describe('when the group has more than three lists', () => {
+    it.todo('renders a slimline card for only three lists');
+
+    it.todo('displays a View All Lists button');
+
+    it.todo('the View All Lists button links to the lists tab');
+  });
+
+  describe('when the group has three or fewer lists', () => {
+    it.todo('renders a slimline card for each list');
+
+    it.todo('does not display a View All Lists button');
+  });
+
+  describe('when the group has no lists', () => {
+    it.todo('tbd');
+  });
+});
