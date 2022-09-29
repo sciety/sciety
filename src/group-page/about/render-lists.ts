e513b32d1b51663165cb07bd79fae716abc5936a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-09-29 10:41:16 +0100

    Remove placeholder text #1793

--- src/group-page/about/render-lists.ts
+++ src/group-page/about/render-lists.ts
@@ -2,7 +2,7 @@ import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 
 export const renderLists = (): HtmlFragment => {
   if (process.env.EXPERIMENT_ENABLED === 'true') {
-    return toHtmlFragment('Placeholder for group lists');
+    return toHtmlFragment('');
   }
 
   return toHtmlFragment('');
