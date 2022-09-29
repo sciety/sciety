4c53dea879d49a65f2e465dd625ec3cdcbbdf42e
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-09-29 18:43:32 +0100

    fix save-save-article-command

--- src/save-article/save-save-article-command.ts
+++ src/save-article/save-save-article-command.ts
@@ -1,9 +1,28 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
 import { Middleware } from 'koa';
 
 export const articleIdFieldName = 'articleid';
 
+const bodyCodec = t.type({
+  [articleIdFieldName]: t.string,
+});
+
 export const saveSaveArticleCommand: Middleware = async (context, next) => {
-  context.session.command = 'save-article';
-  context.session.articleId = context.request.body[articleIdFieldName];
-  await next();
+  const middlewareSucceeded = pipe(
+    context.request.body,
+    bodyCodec.decode,
+    E.map((body) => body),
+    E.match(
+      () => false,
+      (body) => {
+        context.session.command = 'save-article';
+        context.session.articleId = body[articleIdFieldName];
+        return true;
+      },
+    ),
+  );
+
+  if (middlewareSucceeded) { await next(); }
 };
