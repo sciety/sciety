eb37bf745d58a7f8a0c835a7b994404cf13fb3e2
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-09-29 14:40:43 +0100

    Display last updated info for lists on group about page #1793

--- src/group-page/about/render-lists.ts
+++ src/group-page/about/render-lists.ts
@@ -1,13 +1,20 @@
+import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
+import { templateDate } from '../../shared-components/date';
 import { ListCardViewModel } from '../../shared-components/list-card/render-list-card';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
+
+const renderLastUpdatedDate = O.fold(
+  () => '',
+  (date: Date) => `<span>Updated: ${templateDate(date)}</span>`,
+);
 
 export const renderLists = (listViewModels: ReadonlyArray<ListCardViewModel>): HtmlFragment => {
   if (process.env.EXPERIMENT_ENABLED === 'true') {
     return pipe(
       listViewModels,
-      RA.map((viewModel) => `<li><span>${viewModel.title}</span><span>${viewModel.articleCount} articles</span></li>`),
+      RA.map((viewModel) => `<li><span>${viewModel.title}</span><span>${viewModel.articleCount} articles</span>${renderLastUpdatedDate(viewModel.lastUpdated)}</li>`),
       (fragments) => fragments.join(''),
       (slimlineCards) => `<ul>${slimlineCards}</ul>`,
       toHtmlFragment,
