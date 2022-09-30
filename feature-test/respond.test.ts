80f39c0f7c521bec4c3af2340048c253fef15543
LinaKind <l.kinduryte@elifesciences.org>
2022-09-30 15:33:36 +0100

    Reduce amount of hardcoded data in taiko tests

--- feature-test/respond.test.ts
+++ feature-test/respond.test.ts
@@ -4,16 +4,19 @@ import {
 import { callApi } from './call-api.helper';
 import { screenshotTeardown } from './utilities';
 import * as GID from '../src/types/group-id';
+import { arbitraryArticleId } from '../test/types/article-id.helper';
 import { arbitraryReviewId } from '../test/types/review-id.helper';
 
 describe('respond', () => {
+  const articleId = arbitraryArticleId();
+
   beforeEach(async () => {
     await openBrowser();
     await callApi('record-evaluation', {
       groupId: GID.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2'),
       publishedAt: new Date(),
       evaluationLocator: arbitraryReviewId(),
-      articleId: 'doi:10.1101/722579',
+      articleId: articleId.toString(),
       authors: [],
     });
   });
@@ -22,7 +25,7 @@ describe('respond', () => {
 
   describe('when not logged in', () => {
     it('authenticates via twitter, returns and displays increased response count', async () => {
-      await goto('localhost:8080/articles/10.1101/722579');
+      await goto(`localhost:8080/articles/${articleId.value}`);
       await click($('.activity-feed__item:first-child button[value="respond-helpful"]'));
       const result = await text('1', within($('.activity-feed__item:first-child button[value="revoke-response"]'))).exists();
 
