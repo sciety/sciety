433cdddee7e86e5e6959d50fe8635898664580b5
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-09-30 16:51:23 +0100

    integrate Middleware world into pipe

--- src/save-article/save-save-article-command.ts
+++ src/save-article/save-save-article-command.ts
@@ -1,4 +1,5 @@
-import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 import { Middleware } from 'koa';
@@ -10,19 +11,21 @@ const bodyCodec = t.type({
 });
 
 export const saveSaveArticleCommand: Middleware = async (context, next) => {
-  const middlewareSucceeded = pipe(
+  await pipe(
     context.request.body,
     bodyCodec.decode,
-    E.map((body) => body),
-    E.match(
-      () => false,
-      (body) => {
-        context.session.command = 'save-article';
-        context.session.articleId = body[articleIdFieldName];
-        return true;
-      },
-    ),
-  );
-
-  if (middlewareSucceeded) { await next(); }
+    TE.fromEither,
+    TE.map((body) => ({
+      articleId: body[articleIdFieldName],
+      command: 'save-article',
+    })),
+    TE.chainFirstTaskK((session) => {
+      context.session = {
+        ...context.session,
+        ...session,
+      };
+      return T.of(undefined);
+    }),
+    TE.chainTaskK(() => next),
+  )();
 };
