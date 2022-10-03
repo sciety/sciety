7450819473c586fc44b811c9e6ba61154c9e24e3
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-10-03 14:49:32 +0100

    Improve legibility with whitespace and better linebreaks #1805

--- src/group-page/about/render-our-lists.ts
+++ src/group-page/about/render-our-lists.ts
@@ -16,8 +16,8 @@ const renderLists = (lists: ReadonlyArray<ListViewModel>) => pipe(
   RA.map((viewModel) => (`
     <tr>
       <td><a href=${viewModel.href}>${viewModel.title}</a></td>
-      <td>${viewModel.articleCount}<span aria-hidden="true"> articles</span></td>
-      <td><span aria-hidden="true">Updated </span>${templateDate(viewModel.lastUpdated)}</td>
+      <td class="our-lists__article_count">${viewModel.articleCount}<span aria-hidden="true"> articles</span></td>
+      <td><span aria-hidden="true" class="our-lists__updated_label">Updated </span>${templateDate(viewModel.lastUpdated)}</td>
     </tr>
   `)),
   (listItems) => `
