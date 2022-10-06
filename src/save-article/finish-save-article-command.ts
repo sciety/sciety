08449b3260c3b2078bc06d40151f96a7c1389770
Kevin Rutherford <kevin@rutherford-software.com>
2022-10-06 11:18:11 +0100

    Save an article before checking the feed #1811

--- src/save-article/finish-save-article-command.ts
+++ src/save-article/finish-save-article-command.ts
@@ -6,6 +6,7 @@ import { Middleware } from 'koa';
 import { articleSaveState } from './article-save-state';
 import { commandHandler } from './command-handler';
 import { CommitEvents, GetAllEvents } from '../shared-ports';
+import { CommandResult } from '../types/command-result';
 import * as Doi from '../types/doi';
 import { User } from '../types/user';
 
@@ -16,8 +17,21 @@ type Ports = {
   commitEvents: CommitEvents,
 };
 
+export const executeSaveArticle = (ports: Ports) => (user: User, articleId: Doi.Doi): T.Task<CommandResult> => pipe(
+  ports.getAllEvents,
+  T.chain(flow(
+    articleSaveState(user.id, articleId),
+    commandHandler({
+      articleId,
+      userId: user.id,
+      type: 'SaveArticle' as const,
+    }),
+    ports.commitEvents,
+  )),
+);
+
 export const finishSaveArticleCommand = (
-  { getAllEvents, commitEvents }: Ports,
+  ports: Ports,
 ): Middleware => async (context, next) => {
   const user = context.state.user as User;
   await pipe(
@@ -29,16 +43,7 @@ export const finishSaveArticleCommand = (
     O.fold(
       () => T.of(undefined),
       ({ articleId }) => pipe(
-        getAllEvents,
-        T.chain(flow(
-          articleSaveState(user.id, articleId),
-          commandHandler({
-            articleId,
-            userId: user.id,
-            type: 'SaveArticle' as const,
-          }),
-          commitEvents,
-        )),
+        executeSaveArticle(ports)(user, articleId),
         T.map(() => {
           delete context.session.command;
           delete context.session.articleId;
--- test/coupling/save-article-to-list.test.ts
+++ test/coupling/save-article-to-list.test.ts
@@ -1,12 +1,63 @@
 import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { RuntimeGeneratedEvent } from '../../src/domain-events';
+import { createListCommandHandler } from '../../src/lists';
+import { executePolicies } from '../../src/policies/execute-policies';
+import { executeSaveArticle } from '../../src/save-article/finish-save-article-command';
 import { generateViewModel } from '../../src/sciety-feed-page/sciety-feed-page';
 import { toHtmlFragment } from '../../src/types/html-fragment';
-import { arbitraryString, arbitraryUri, arbitraryWord } from '../helpers';
+import {
+  arbitraryDate, arbitraryNumber, arbitraryString, arbitraryUri, arbitraryWord,
+} from '../helpers';
 import { shouldNotBeCalled } from '../should-not-be-called';
 import { arbitraryArticleId } from '../types/article-id.helper';
+import { arbitraryListId } from '../types/list-id.helper';
+import { arbitraryListOwnerId } from '../types/list-owner-id.helper';
+import { arbitraryUserId } from '../types/user-id.helper';
+
+// eslint-disable-next-line jest/require-hook
+let events: ReadonlyArray<RuntimeGeneratedEvent> = [];
+
+const getAllEvents = T.of(events);
+
+const commitEvents = (eventsToCommit: ReadonlyArray<RuntimeGeneratedEvent>) => {
+  events = events.concat(eventsToCommit);
+  return pipe(
+    T.of(eventsToCommit),
+    T.map(RA.match(
+      () => 'no-events-created',
+      () => 'events-created',
+    )),
+    T.chainFirst(() => pipe(
+      eventsToCommit,
+      T.traverseArray(executePolicies({
+        commitEvents,
+        getAllEvents,
+        logger: () => {},
+        getBiorxivOrMedrxivSubjectArea: () => TE.right(arbitraryString()),
+        getListsOwnedBy: () => TE.right([{
+          id: arbitraryListId(),
+          name: arbitraryString(),
+          description: arbitraryString(),
+          articleCount: arbitraryNumber(1, 10),
+          lastUpdated: arbitraryDate(),
+          ownerId: arbitraryListOwnerId(),
+        }]),
+        getUserDetails: () => TE.right({
+          avatarUrl: arbitraryUri(),
+          displayName: arbitraryString(),
+          handle: arbitraryWord(),
+          userId: arbitraryUserId(),
+        }),
+        addArticleToList: () => TE.right(undefined),
+        createList: createListCommandHandler({ commitEvents }),
+      })),
+    )),
+  );
+};
 
 describe('save-article-to-list', () => {
   describe('given the user is logged in', () => {
@@ -17,13 +68,22 @@ describe('save-article-to-list', () => {
 
       describe('when the user saves an article that isn\'t in any list', () => {
         beforeEach(async () => {
-          // await goto(articlePage);
-          // await click('Save to my list');
+          const ports = {
+            getAllEvents,
+            commitEvents,
+          };
+          const user = {
+            id: arbitraryUserId(),
+            handle: arbitraryWord(),
+            avatarUrl: arbitraryUri(),
+          };
+          const articleId = arbitraryArticleId();
+          await executeSaveArticle(ports)(user, articleId)();
         });
 
         it.failing('the user\'s action appears in the Sciety feed', async () => {
           const adapters = {
-            getAllEvents: T.of([]),
+            getAllEvents,
             getUserDetails: () => TE.right({
               handle: arbitraryWord(),
               avatarUrl: arbitraryUri(),
