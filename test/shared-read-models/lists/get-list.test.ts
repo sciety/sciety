2fc59cf471a1228574e88344fc2e4142856666c9
LinaKind <l.kinduryte@elifesciences.org>
2022-10-10 14:35:51 +0100

    Write failing tests for remove article from list #1819

--- test/shared-read-models/lists/get-list.test.ts
+++ test/shared-read-models/lists/get-list.test.ts
@@ -1,6 +1,6 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import { articleAddedToList, listCreated } from '../../../src/domain-events';
+import { articleAddedToList, articleRemovedFromList, listCreated } from '../../../src/domain-events';
 import { getList, List } from '../../../src/shared-read-models/lists';
 import { arbitraryDate, arbitraryString } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
@@ -21,37 +21,77 @@ describe('get-list', () => {
       const ownerId = arbitraryListOwnerId();
 
       describe('when the list is empty', () => {
-        const creationDate = arbitraryDate();
-        let result: List;
+        describe('because it is a new list', () => {
+          const creationDate = arbitraryDate();
+          let result: List;
 
-        beforeEach(async () => {
-          result = await pipe(
-            [
-              listCreated(listId, name, description, ownerId, creationDate),
-            ],
-            getList(listId),
-            TE.getOrElse(shouldNotBeCalled),
-          )();
+          beforeEach(async () => {
+            result = await pipe(
+              [
+                listCreated(listId, name, description, ownerId, creationDate),
+              ],
+              getList(listId),
+              TE.getOrElse(shouldNotBeCalled),
+            )();
+          });
+
+          it('returns non-dynamic metadata sourced from list creation event', () => {
+            expect(result).toStrictEqual(expect.objectContaining({
+              name,
+              description,
+              ownerId,
+            }));
+          });
+
+          it('returns the list id', () => {
+            expect(result.id).toStrictEqual(listId);
+          });
+
+          it('returns the list creation date as the last updated date', () => {
+            expect(result.lastUpdated).toStrictEqual(creationDate);
+          });
+
+          it('returns an articleCount of 0', () => {
+            expect(result.articleCount).toBe(0);
+          });
         });
 
-        it('returns non-dynamic metadata sourced from list creation event', () => {
-          expect(result).toStrictEqual(expect.objectContaining({
-            name,
-            description,
-            ownerId,
-          }));
-        });
+        describe('an was added and then removed', () => {
+          const creationDate = arbitraryDate();
+          const articleId = arbitraryArticleId();
+          let result: List;
 
-        it('returns the list id', () => {
-          expect(result.id).toStrictEqual(listId);
-        });
+          beforeEach(async () => {
+            result = await pipe(
+              [
+                listCreated(listId, name, description, ownerId, creationDate),
+                articleAddedToList(articleId, listId),
+                articleRemovedFromList(articleId, listId),
+              ],
+              getList(listId),
+              TE.getOrElse(shouldNotBeCalled),
+            )();
+          });
 
-        it('returns the list creation date as the last updated date', () => {
-          expect(result.lastUpdated).toStrictEqual(creationDate);
-        });
+          it('returns non-dynamic metadata sourced from list creation event', () => {
+            expect(result).toStrictEqual(expect.objectContaining({
+              name,
+              description,
+              ownerId,
+            }));
+          });
 
-        it('returns an articleCount of 0', () => {
-          expect(result.articleCount).toBe(0);
+          it('returns the list id', () => {
+            expect(result.id).toStrictEqual(listId);
+          });
+
+          it.failing('returns the list creation date as the last updated date', () => {
+            expect(result.lastUpdated).toStrictEqual(creationDate);
+          });
+
+          it.failing('returns an articleCount of 0', () => {
+            expect(result.articleCount).toBe(0);
+          });
         });
       });
 
