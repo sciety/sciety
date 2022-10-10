2ec931215ba4b0014c2abbabf0373023df2d90e5
Kevin Rutherford <kevin@rutherford-software.com>
2022-10-10 15:48:08 +0100

    Introduce a reducer to help with future refactoring #1819

--- src/shared-read-models/list-articles/select-articles-belonging-to-list.ts
+++ src/shared-read-models/list-articles/select-articles-belonging-to-list.ts
@@ -2,7 +2,9 @@ import * as E from 'fp-ts/Either';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
-import { DomainEvent, isArticleAddedToListEvent, isListCreatedEvent } from '../../domain-events';
+import {
+  ArticleAddedToListEvent, DomainEvent, isArticleAddedToListEvent, isListCreatedEvent,
+} from '../../domain-events';
 import * as DE from '../../types/data-error';
 import { Doi } from '../../types/doi';
 import { ListId } from '../../types/list-id';
@@ -10,6 +12,11 @@ import { ListId } from '../../types/list-id';
 type SelectArticlesBelongingToList = (listId: ListId)
 => (events: ReadonlyArray<DomainEvent>)
 => E.Either<DE.DataError, ReadonlyArray<Doi>>;
+
+const calculateListContents = (state: Array<Doi>, event: ArticleAddedToListEvent): Array<Doi> => {
+  state.push(event.articleId);
+  return state;
+};
 
 export const selectArticlesBelongingToList: SelectArticlesBelongingToList = (listId) => (events) => pipe(
   events,
@@ -21,7 +28,7 @@ export const selectArticlesBelongingToList: SelectArticlesBelongingToList = (lis
       events,
       RA.filter(isArticleAddedToListEvent),
       RA.filter((event) => event.listId === listId),
-      RA.map((event) => event.articleId),
+      RA.reduce([], calculateListContents),
       RA.reverse,
       E.right,
     ),
