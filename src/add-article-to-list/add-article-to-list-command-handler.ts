680710ad85a16fb911f0a54cbd3ba0f7ce3be30e
Kevin Rutherford <kevin@rutherford-software.com>
2022-10-10 12:00:53 +0100

    Make existence of command handler explicit #1819

---
+++ src/add-article-to-list/add-article-to-list-command-handler.ts
@@ -0,0 +1,39 @@
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { executeCommand } from './execute-command';
+import { validateInputShape } from './validate-input-shape';
+import { DomainEvent } from '../domain-events';
+import { CommitEvents } from '../shared-ports';
+import { replayListAggregate } from '../shared-write-models/replay-list-aggregate';
+import { CommandResult } from '../types/command-result';
+
+export type Ports = {
+  getAllEvents: T.Task<ReadonlyArray<DomainEvent>>,
+  commitEvents: CommitEvents,
+};
+
+type AddArticleToListCommandHandler = (
+  ports: Ports
+) => (
+  input: unknown,
+  date?: Date
+) => TE.TaskEither<string, CommandResult>;
+
+export const addArticleToListCommandHandler: AddArticleToListCommandHandler = (
+  ports,
+) => (
+  input,
+  date = new Date(),
+) => pipe(
+  input,
+  validateInputShape,
+  TE.fromEither,
+  TE.chainW((command) => pipe(
+    ports.getAllEvents,
+    TE.rightTask,
+    TE.chainEitherK(replayListAggregate(command.listId)),
+    TE.map(executeCommand(command, date)),
+  )),
+  TE.chainTaskK(ports.commitEvents),
+);
