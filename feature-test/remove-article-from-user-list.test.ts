37f0c0304c095a414fec4877cdf887a1268607e7
LinaKind <l.kinduryte@elifesciences.org>
2022-10-10 14:01:47 +0100

    Add feature tests for removing an article from generic list #1819

---
+++ feature-test/remove-article-from-user-list.test.ts
@@ -0,0 +1,13 @@
+describe('when the user removes an article from a generic list', () => {
+  it.todo('the article no longer appears on the generic list page');
+
+  describe('on the list card on the user profile page', () => {
+    it.todo('the article count is reduced by one');
+
+    it.todo('the last updated date changes');
+  });
+
+  describe('on the article card', () => {
+    it.todo('on the search results page the list count is decreased by one');
+  });
+});
