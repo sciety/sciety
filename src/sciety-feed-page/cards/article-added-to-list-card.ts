0ec8c5ad87e09feb50e715595711e208850a1028
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-10-11 13:12:35 +0100

    Add list details to feed card #1823

--- src/sciety-feed-page/cards/article-added-to-list-card.ts
+++ src/sciety-feed-page/cards/article-added-to-list-card.ts
@@ -1,27 +1,34 @@
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import { ScietyFeedCard } from './sciety-feed-card';
 import { ArticleAddedToListEvent } from '../../domain-events';
+import { GetAllEvents } from '../../shared-ports';
+import { getList } from '../../shared-read-models/lists';
 import * as DE from '../../types/data-error';
-import { UserId } from '../../types/user-id';
-
-type GetUserDetails = (userId: UserId) => TE.TaskEither<DE.DataError, {
-  handle: string,
-  avatarUrl: string,
-}>;
+import { toHtmlFragment } from '../../types/html-fragment';
 
 type Ports = {
-  getUserDetails: GetUserDetails,
+  getAllEvents: GetAllEvents,
 };
 
 type ArticleAddedToListCard = (
   ports: Ports,
 ) => (event: ArticleAddedToListEvent) => TE.TaskEither<DE.DataError, ScietyFeedCard>;
 
-export const articleAddedToListCard: ArticleAddedToListCard = () => (event) => TE.right(
-  {
-    titleText: 'Somebody added an article to a list',
-    linkUrl: `/lists/${event.listId}`,
-    avatarUrl: '/static/images/sciety-logo.jpg',
-    date: event.date,
-  },
+export const articleAddedToListCard: ArticleAddedToListCard = (ports) => (event) => pipe(
+  ports.getAllEvents,
+  TE.rightTask,
+  TE.chain(getList(event.listId)),
+  TE.map(
+    (list) => ({
+      titleText: 'Somebody added an article to a list',
+      linkUrl: `/lists/${event.listId}`,
+      avatarUrl: '/static/images/sciety-logo.jpg',
+      date: event.date,
+      details: {
+        title: toHtmlFragment(list.name),
+        content: toHtmlFragment(list.description),
+      },
+    }),
+  ),
 );
