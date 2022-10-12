0d957f30edc0502cab5bca7c1155098c917ddfa9
David Moulton <d.moulton@elifesciences.org>
2022-10-12 10:37:55 +0100

    Make validateInputShape generic ready for reuse #1819

--- src/remove-article-from-list/validate-input-shape.ts
+++ src/remove-article-from-list/validate-input-shape.ts
@@ -2,9 +2,8 @@ import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 import * as PR from 'io-ts/PathReporter';
-import { Command } from './execute-command';
 
-type ValidateInputShape = (codec: t.Decoder<unknown, Command>) => (input: unknown) => E.Either<string, Command>;
+type ValidateInputShape = <C>(codec: t.Decoder<unknown, C>) => (input: unknown) => E.Either<string, C>;
 
 export const validateInputShape: ValidateInputShape = (codec) => (input) => pipe(
   input,
