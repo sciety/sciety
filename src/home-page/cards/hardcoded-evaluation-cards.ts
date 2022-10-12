a6b59fc6720ab6de81979c281fe99bf3b10fdc16
David Moulton <d.moulton@elifesciences.org>
2022-10-12 14:10:49 +0100

    Update home page evaluation cards

--- src/home-page/cards/hardcoded-evaluation-cards.ts
+++ src/home-page/cards/hardcoded-evaluation-cards.ts
@@ -1,26 +1,26 @@
 export const card1 = {
-  articleId: '10.1101/2021.06.04.447151',
-  articleTitle: 'Single-cell intracellular pH dynamics regulate the cell cycle by timing G1 exit and the G2 transition',
-  date: new Date('2022-07-26'),
-  groupSlug: 'asapbio-crowd-review',
-  groupName: 'ASAPbio crowd review',
-  avatarPath: '/static/groups/asapbio-crowd-review--50401e46-b764-47b7-8557-6bb35444b7c8.png',
+  articleId: '10.1101/2022.10.01.510447',
+  articleTitle: 'Modulation of tooth regeneration through opposing responses to Wnt and BMP signals in teleosts',
+  date: new Date('2022-10-02'),
+  groupSlug: 'arcadia-science',
+  groupName: 'Arcadia Science',
+  avatarPath: '/static/groups/arcadia-science--bc1f956b-12e8-4f5c-aadc-70f91347bd18.png',
 };
 
 export const card2 = {
-  articleId: '10.1101/2020.11.12.379909',
-  articleTitle: 'Functional correlates of immediate early gene expression in mouse visual cortex',
-  date: new Date('2022-07-26'),
-  groupSlug: 'pci-neuroscience',
-  groupName: 'Peer Community in Neuroscience',
-  avatarPath: '/static/groups/pci-neuroscience--af792cd3-1600-465c-89e5-250c48f793aa.png',
+  articleId: '10.1101/2022.06.26.496535',
+  articleTitle: 'Position representations of moving objects align with real-time position in the early visual response',
+  date: new Date('2022-06-29'),
+  groupSlug: 'elife',
+  groupName: 'eLife',
+  avatarPath: '/static/groups/elife--b560187e-f2fb-4ff9-a861-a204f3fc0fb0.png',
 };
 
 export const card3 = {
-  articleId: '10.1101/2022.06.01.494423',
-  articleTitle: 'Long-read HiFi Sequencing Correctly Assembles Repetitive <i>heavy fibroin</i> Silk Genes in New Moth and Caddisfly Genomes',
-  date: new Date('2022-07-22'),
-  groupSlug: 'gigabyte',
-  groupName: 'GigaByte',
-  avatarPath: '/static/groups/gigabyte--f7a7aec3-8b1c-4b81-b098-f3f2e4eefe58.png',
+  articleId: '10.1101/2021.07.05.451165',
+  articleTitle: 'Message in a Bottle – Metabarcoding Enables Biodiversity Comparisons Across Ecoregions',
+  date: new Date('2021-07-06'),
+  groupSlug: 'gigascience',
+  groupName: 'GigaScience',
+  avatarPath: '/static/groups/gigascience--d6e1a913-76f8-40dc-9074-8eac033e1bc8.png',
 };
