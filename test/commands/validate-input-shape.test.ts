9c4d0e392fed8400ccc74355fe396f4b34706c3d
LinaKind <l.kinduryte@elifesciences.org>
2022-10-12 11:35:35 +0100

    Make tests generic #1819

---
+++ test/commands/validate-input-shape.test.ts
@@ -0,0 +1,34 @@
+import * as E from 'fp-ts/Either';
+import * as t from 'io-ts';
+import { validateInputShape } from '../../src/commands/validate-input-shape';
+import { arbitraryNumber, arbitraryString } from '../helpers';
+
+describe('validate-input-shape', () => {
+  const fakeFieldName = arbitraryNumber(1, 100);
+
+  const fakeCommandCodec = t.type({
+    fakeFieldName: t.number,
+  });
+
+  describe('when the input is valid', () => {
+    it('returns a Command', () => {
+      const result = validateInputShape(fakeCommandCodec)({
+        fakeFieldName,
+      });
+
+      expect(result).toStrictEqual(E.right({
+        fakeFieldName,
+      }));
+    });
+  });
+
+  describe('when the input is invalid', () => {
+    it('returns an error message', () => {
+      const result = validateInputShape(fakeCommandCodec)({
+        fakeFieldName: arbitraryString(),
+      });
+
+      expect(result).toStrictEqual(E.left(expect.stringMatching(/.+/)));
+    });
+  });
+});
