c375e97d39e91ab69bd4aa2700be974af8020c46
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-10-13 11:41:24 +0100

    Write a failing test #1825

--- test/policies/add-article-to-generic-list-from-user-saved-article.test.ts
+++ test/policies/add-article-to-generic-list-from-user-saved-article.test.ts
@@ -1,7 +1,45 @@
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import { DomainEvent, userSavedArticle } from '../../src/domain-events';
+import { AddArticleToList } from '../../src/shared-ports';
+import { dummyLogger } from '../dummy-logger';
+import { arbitraryArticleId } from '../types/article-id.helper';
+import { arbitraryUserId } from '../types/user-id.helper';
+
+type Ports = {
+  addArticleToList: AddArticleToList,
+};
+
+type AddArticleToGenericListFromUserSavedArticle = (ports: Ports) => (event: DomainEvent) => T.Task<undefined>;
+
+const addArticleToGenericListFromUserSavedArticle: AddArticleToGenericListFromUserSavedArticle = (
+) => () => T.of(undefined);
+
 describe('add-article-to-generic-list-from-user-saved-article', () => {
+  const defaultPorts = {
+    addArticleToList: () => TE.right(undefined),
+    logger: dummyLogger,
+  };
+
+  let ports: Ports;
+
   describe('when a UserSavedArticle event is received', () => {
     describe('and the user has a generic list', () => {
-      it.todo('calls the AddArticleToList command');
+      const userId = arbitraryUserId();
+      const articleId = arbitraryArticleId();
+
+      const event = userSavedArticle(userId, articleId);
+
+      beforeEach(async () => {
+        ports = {
+          addArticleToList: jest.fn(defaultPorts.addArticleToList),
+        };
+        await addArticleToGenericListFromUserSavedArticle(ports)(event)();
+      });
+
+      it.failing('calls the AddArticleToList command', () => {
+        expect(ports.addArticleToList).toHaveBeenCalledWith(expect.anything());
+      });
 
       it.todo('calls the command with the generic list id owned by that user');
 
