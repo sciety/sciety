16e081afe08700dac41ebaab20d63af678774f79
Giorgio Sironi <g.sironi@elifesciences.org>
2022-10-13 15:21:04 +0100

    Improve readability #1825

--- test/policies/add-article-to-generic-list-from-user-saved-article.test.ts
+++ test/policies/add-article-to-generic-list-from-user-saved-article.test.ts
@@ -1,4 +1,3 @@
-import * as E from 'fp-ts/Either';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
@@ -10,7 +9,6 @@ import * as LOID from '../../src/types/list-owner-id';
 import { dummyLogger } from '../dummy-logger';
 import { arbitraryList } from '../group-page/about/to-our-lists-view-model.test';
 import { arbitraryArticleId } from '../types/article-id.helper';
-import { arbitraryListId } from '../types/list-id.helper';
 import { arbitraryUserId } from '../types/user-id.helper';
 
 type Ports = {
@@ -31,8 +29,7 @@ const addArticleToGenericListFromUserSavedArticle: AddArticleToGenericListFromUs
   ports,
 ) => (event) => pipe(
   event,
-  E.fromPredicate(isUserSavedArticleEvent, () => 'not interesting'),
-  TE.fromEither,
+  TE.fromPredicate(isUserSavedArticleEvent, () => 'not interesting'),
   TE.chainW(toCommand(ports)),
   TE.chain(ports.addArticleToList),
   TE.match(
@@ -53,17 +50,14 @@ describe('add-article-to-generic-list-from-user-saved-article', () => {
     describe('and the user has a generic list', () => {
       const userId = arbitraryUserId();
       const articleId = arbitraryArticleId();
-      const listId = arbitraryListId();
+      const genericListOwnedByUser = arbitraryList();
 
       const event = userSavedArticle(userId, articleId);
 
       beforeEach(async () => {
         ports = {
           addArticleToList: jest.fn(defaultPorts.addArticleToList),
-          getListsOwnedBy: () => TE.right([{
-            ...arbitraryList(),
-            id: listId,
-          }]),
+          getListsOwnedBy: () => TE.right([genericListOwnedByUser]),
         };
         await addArticleToGenericListFromUserSavedArticle(ports)(event)();
       });
@@ -74,14 +68,14 @@ describe('add-article-to-generic-list-from-user-saved-article', () => {
 
       it('calls the command with the generic list id owned by that user', () => {
         expect(ports.addArticleToList).toHaveBeenCalledWith(
-          expect.objectContaining({ listId }),
+          expect.objectContaining({ listId: genericListOwnedByUser.id }),
         );
       });
 
       it('calls the command with the article id in the UserSavedArticle event', () => {
-        expect(ports.addArticleToList).toHaveBeenCalledWith(expect.objectContaining({
-          articleId,
-        }));
+        expect(ports.addArticleToList).toHaveBeenCalledWith(
+          expect.objectContaining({ articleId }),
+        );
       });
     });
 
