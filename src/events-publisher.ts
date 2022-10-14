8a592eff6fdbd0d2234bbd781fefb738b36a9a7c
Giorgio Sironi <g.sironi@elifesciences.org>
2022-10-14 08:59:44 +0100

    Publish multiple events #1829

--- src/events-publisher.ts
+++ src/events-publisher.ts
@@ -1,16 +1,19 @@
-import { Pool } from "pg";
+import { Pool } from 'pg';
 
 void (async (): Promise<void> => {
   process.stdout.write('I am the events publisher.\n');
   const pool = new Pool();
 
-  const queryText = 'SELECT * FROM outbox ORDER BY date LIMIT 1';
-  const res = await pool.query(queryText);
-  if (res.rowCount > 0) {
-    const event = res.rows[0];
-    console.log('Publishing event', event);
-    await pool.query('DELETE FROM outbox WHERE id = $1', [event.id]);
-  }
+  // eslint-disable-next-line no-loops/no-loops
+  while (true) {
+    const queryText = 'SELECT * FROM outbox ORDER BY date LIMIT 1';
+    const res = await pool.query(queryText);
+    if (res.rowCount > 0) {
+      const event = res.rows[0];
+      console.log('Publishing event', event);
+      await pool.query('DELETE FROM outbox WHERE id = $1', [event.id]);
+    }
 
-  await new Promise((resolve) => { setTimeout(resolve, 60 * 60 * 1000); });
+    await new Promise((resolve) => { setTimeout(resolve, 5 * 1000); });
+  }
 })();
