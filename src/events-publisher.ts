7f6145503da368e4d8be9a81b356506884d095cf
Giorgio Sironi <g.sironi@elifesciences.org>
2022-10-14 08:57:26 +0100

    Publish one event #1829

--- src/events-publisher.ts
+++ src/events-publisher.ts
@@ -1,4 +1,16 @@
+import { Pool } from "pg";
+
 void (async (): Promise<void> => {
   process.stdout.write('I am the events publisher.\n');
+  const pool = new Pool();
+
+  const queryText = 'SELECT * FROM outbox ORDER BY date LIMIT 1';
+  const res = await pool.query(queryText);
+  if (res.rowCount > 0) {
+    const event = res.rows[0];
+    console.log('Publishing event', event);
+    await pool.query('DELETE FROM outbox WHERE id = $1', [event.id]);
+  }
+
   await new Promise((resolve) => { setTimeout(resolve, 60 * 60 * 1000); });
 })();
