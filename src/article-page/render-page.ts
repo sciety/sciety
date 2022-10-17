993083efc2215ebff3a782783dd2a587a09aed0b
Kevin Rutherford <kevin@rutherford-software.com>
2022-10-17 09:36:36 +0100

    Identify an existing type as the contractual viewmodel #1834

--- src/article-page/render-page.ts
+++ src/article-page/render-page.ts
@@ -1,17 +1,11 @@
+import { ViewModel } from './view-model';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
-type Components = {
-  header: HtmlFragment,
-  articleActions: HtmlFragment,
-  articleAbstract: HtmlFragment,
-  mainContent: HtmlFragment,
-};
-
-export const renderPage = (components: Components): HtmlFragment => toHtmlFragment(`
-  ${components.header}
-  ${components.articleActions}
-  ${components.articleAbstract}
+export const renderPage = (viewmodel: ViewModel): HtmlFragment => toHtmlFragment(`
+  ${viewmodel.header}
+  ${viewmodel.articleActions}
+  ${viewmodel.articleAbstract}
   <div class="main-content">
-    ${components.mainContent}
+    ${viewmodel.mainContent}
   </div>
 `);
---
+++ src/article-page/view-model.ts
@@ -0,0 +1,8 @@
+import { HtmlFragment } from '../types/html-fragment';
+
+export type ViewModel = {
+  header: HtmlFragment,
+  articleActions: HtmlFragment,
+  articleAbstract: HtmlFragment,
+  mainContent: HtmlFragment,
+};
