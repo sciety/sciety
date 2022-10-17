a5aaebdf5140e833ab7ac27905596a017e29db29
David Moulton <d.moulton@elifesciences.org>
2022-10-17 14:37:48 +0100

    Extract function to its own file for better reuse #1823

--- src/sciety-feed-page/cards/article-added-to-list-card.ts
+++ src/sciety-feed-page/cards/article-added-to-list-card.ts
@@ -1,77 +1,16 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { addListOwnershipInformation, Ports as AddListOwnershipInformationPorts } from './add-list-ownership-information';
 import { ScietyFeedCard } from './sciety-feed-card';
 import { ArticleAddedToListEvent } from '../../domain-events';
 import { GetAllEvents } from '../../shared-ports';
-import { getGroup } from '../../shared-read-models/groups';
-import { getList, List } from '../../shared-read-models/lists';
+import { getList } from '../../shared-read-models/lists';
 import * as DE from '../../types/data-error';
 import { toHtmlFragment } from '../../types/html-fragment';
-import { UserId } from '../../types/user-id';
-
-type GetUserDetails = (userId: UserId) => TE.TaskEither<DE.DataError, {
-  handle: string,
-  avatarUrl: string,
-}>;
 
 type Ports = {
   getAllEvents: GetAllEvents,
-  getUserDetails: GetUserDetails,
-};
-
-type ListWithAddedOwnershipInformation = {
-  name: string,
-  description: string,
-  ownerName: string,
-  ownerAvatarUrl: string,
-  linkUrl: string,
-};
-
-export const addListOwnershipInformation = (
-  ports: Ports,
-) => (
-  list: List,
-): TE.TaskEither<DE.DataError, ListWithAddedOwnershipInformation> => {
-  switch (list.ownerId.tag) {
-    case 'group-id':
-      return pipe(
-        ports.getAllEvents,
-        TE.rightTask,
-        TE.chainEitherK(getGroup(list.ownerId.value)),
-        TE.map((group) => ({
-          ...list,
-          ownerName: group.name,
-          ownerAvatarUrl: group.avatarPath,
-          linkUrl: `/lists/${list.id}`,
-        })),
-      );
-    case 'user-id':
-      return pipe(
-        list.ownerId.value,
-        ports.getUserDetails,
-        TE.match(
-          () => (
-            {
-              ...list,
-              ownerName: 'A user',
-              ownerAvatarUrl: '/static/images/sciety-logo.jpg',
-              linkUrl: `/users/${list.ownerId.value}/lists/saved-articles`,
-            }
-          ),
-          (userDetails) => (
-            {
-              ...list,
-              ownerName: userDetails.handle,
-              ownerAvatarUrl: userDetails.avatarUrl,
-              linkUrl: `/users/${userDetails.handle}/lists/saved-articles`,
-
-            }
-          ),
-        ),
-        TE.rightTask,
-      );
-  }
-};
+} & AddListOwnershipInformationPorts;
 
 type ArticleAddedToListCard = (
   ports: Ports,
--- src/sciety-feed-page/cards/collapsed-articles-added-to-list-card.ts
+++ src/sciety-feed-page/cards/collapsed-articles-added-to-list-card.ts
@@ -1,23 +1,16 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import { addListOwnershipInformation } from './article-added-to-list-card';
+import { addListOwnershipInformation, Ports as AddListOwnershipInformationPorts } from './add-list-ownership-information';
 import { ScietyFeedCard } from './sciety-feed-card';
 import { GetAllEvents } from '../../shared-ports';
 import { getList } from '../../shared-read-models/lists';
 import * as DE from '../../types/data-error';
 import { toHtmlFragment } from '../../types/html-fragment';
-import { UserId } from '../../types/user-id';
 import { CollapsedArticlesAddedToList } from '../collapse-close-events';
-
-type GetUserDetails = (userId: UserId) => TE.TaskEither<DE.DataError, {
-  handle: string,
-  avatarUrl: string,
-}>;
 
 type Ports = {
   getAllEvents: GetAllEvents,
-  getUserDetails: GetUserDetails,
-};
+} & AddListOwnershipInformationPorts;
 
 type CollapsedArticlesAddedToListCard = (
   ports: Ports,
@@ -27,7 +20,6 @@ export const collapsedArticlesAddedToListCard: CollapsedArticlesAddedToListCard 
   ports.getAllEvents,
   TE.rightTask,
   TE.chain(getList(collapsedEvents.listId)),
-  // TE.chain(addListOwnerName(ports)),
   TE.chain(addListOwnershipInformation(ports)),
   TE.map((extendedListMetadata) => ({
     ownerName: extendedListMetadata.ownerName,
