a8471fc24c6eb9d8acf07a333ea2f5d387be451f
LinaKind <l.kinduryte@elifesciences.org>
2022-10-25 14:51:35 +0100

    Write a failing test #1850

--- test/shared-read-models/elife-articles-missing-from-subject-area-lists/handle-event.test.ts
+++ test/shared-read-models/elife-articles-missing-from-subject-area-lists/handle-event.test.ts
@@ -1,9 +1,10 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import { listCreated } from '../../../src/domain-events';
+import { articleAddedToList, listCreated } from '../../../src/domain-events';
 import { evaluationRecorded } from '../../../src/domain-events/evaluation-recorded-event';
 import { handleEvent } from '../../../src/shared-read-models/elife-articles-missing-from-subject-area-lists/handle-event';
 import * as GroupId from '../../../src/types/group-id';
+import * as LID from '../../../src/types/list-id';
 import { arbitraryString } from '../../helpers';
 import { arbitraryArticleId } from '../../types/article-id.helper';
 import { arbitraryGroupId } from '../../types/group-id.helper';
@@ -61,6 +62,23 @@ describe('handle-event', () => {
 
       expect(readModel).toHaveLength(1);
       expect(readModel[0].value).toStrictEqual(articleId.value);
+    });
+  });
+
+  describe('when there is an evaluation by eLife on an article that has been added to an eLife subject area list', () => {
+    it.failing('does not include the article in the read model', () => {
+      const articleId = arbitraryArticleId();
+      const elifeBiochemistryAndChemicalBiologyListId = LID.fromValidatedString('3792ee73-6a7d-4c54-b6ee-0abc18cb8bc4');
+      const elifeGroupId = GroupId.fromValidatedString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0');
+      const readModel = pipe(
+        [
+          evaluationRecorded(elifeGroupId, articleId, arbitraryReviewId()),
+          articleAddedToList(articleId, elifeBiochemistryAndChemicalBiologyListId),
+        ],
+        RA.reduce([], handleEvent),
+      );
+
+      expect(readModel).toHaveLength(0);
     });
   });
 
