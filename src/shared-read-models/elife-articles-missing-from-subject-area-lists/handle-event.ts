6f41bc5670585abd40e269637b0403ce4d3ec5b2
LinaKind <l.kinduryte@elifesciences.org>
2022-10-25 11:34:15 +0100

    Test via reducing to allow multiple events in the test #1850

--- src/shared-read-models/elife-articles-missing-from-subject-area-lists/handle-event.ts
+++ src/shared-read-models/elife-articles-missing-from-subject-area-lists/handle-event.ts
@@ -1,13 +1,11 @@
-import {EvaluationRecordedEvent} from '../../domain-events';
+import { EvaluationRecordedEvent } from '../../domain-events';
 import { Doi } from '../../types/doi';
 
 export type MissingArticles = { articleIds: ReadonlyArray<Doi> };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const handleEvent = (readmodel: MissingArticles, event: EvaluationRecordedEvent): MissingArticles => {
-  return {
-    articleIds: [
-      event.articleId,
-    ],
-  };
-};
+export const handleEvent = (readmodel: MissingArticles, event: EvaluationRecordedEvent): MissingArticles => ({
+  articleIds: [
+    event.articleId,
+  ],
+});
--- test/shared-read-models/elife-articles-missing-from-subject-area-lists/handle-event.test.ts
+++ test/shared-read-models/elife-articles-missing-from-subject-area-lists/handle-event.test.ts
@@ -1,3 +1,5 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
 import { evaluationRecorded } from '../../../src/domain-events/evaluation-recorded-event';
 import { handleEvent } from '../../../src/shared-read-models/elife-articles-missing-from-subject-area-lists/handle-event';
 import * as GroupId from '../../../src/types/group-id';
@@ -5,18 +7,20 @@ import { arbitraryArticleId } from '../../types/article-id.helper';
 import { arbitraryReviewId } from '../../types/review-id.helper';
 
 describe('handle-event', () => {
-  describe('when there is no articles missing', () => {
-    describe('there is an EvaluationRecorded by eLife event', () => {
+  describe('when there are currently no articles missing', () => {
+    describe('and next there is an EvaluationRecorded by eLife event', () => {
       describe('and it has not been added to an eLife subject area list', () => {
         it('appears in the read model', () => {
           const articleId = arbitraryArticleId();
-          const readModel = handleEvent(
-            { articleIds: [] },
-            evaluationRecorded(
-              GroupId.fromValidatedString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'),
-              articleId,
-              arbitraryReviewId(),
-            ),
+          const readModel = pipe(
+            [
+              evaluationRecorded(
+                GroupId.fromValidatedString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'),
+                articleId,
+                arbitraryReviewId(),
+              ),
+            ],
+            RA.reduce({ articleIds: [] }, handleEvent),
           );
 
           expect(readModel.articleIds).toHaveLength(1);
