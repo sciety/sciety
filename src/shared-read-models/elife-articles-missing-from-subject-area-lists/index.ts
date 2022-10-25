12dd78dd1e3adc6ff8f5cf69eb9cd44c5b2e7160
Kevin Rutherford <kevin@rutherford-software.com>
2022-10-25 10:36:55 +0100

    Introduce a reducer seam to enable future TDD #1850

--- src/shared-read-models/elife-articles-missing-from-subject-area-lists/index.ts
+++ src/shared-read-models/elife-articles-missing-from-subject-area-lists/index.ts
@@ -1,7 +1,17 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { DomainEvent } from '../../domain-events';
 import { Doi } from '../../types/doi';
 
 type MissingArticles = { articleIds: ReadonlyArray<Doi> };
 
-export const elifeArticleMissingFromSubjectAreaLists = (): MissingArticles => (
-  { articleIds: [new Doi('10.1101/1234')] }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const handleEvent = (readmodel: MissingArticles, event: DomainEvent) => readmodel;
+
+export const elifeArticleMissingFromSubjectAreaLists = (): MissingArticles => pipe(
+  [],
+  RA.reduce(
+    { articleIds: [] },
+    handleEvent,
+  ),
 );
