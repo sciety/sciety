80bb9779047383e8fac47d5af9d40522862faf1d
David Moulton <d.moulton@elifesciences.org>
2022-10-26 15:21:42 +0100

    Extract a formatter #1850

--- src/elife-articles-missing-from-subject-area-lists-json/index.ts
+++ src/elife-articles-missing-from-subject-area-lists-json/index.ts
@@ -2,13 +2,19 @@ import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
 import { elifeArticleMissingFromSubjectAreaLists, Ports } from '../shared-read-models/elife-articles-missing-from-subject-area-lists';
+import { Doi } from '../types/doi';
+
+const formatForJson = (articleIds: ReadonlyArray<Doi>) => pipe(
+  articleIds,
+  RA.map((articleId) => articleId.value),
+  (ids) => ({
+    articleIds: ids,
+    articleCount: ids.length,
+  }),
+);
 
 export const elifeArticlesMissingFromSubjectAreaListsJson = (ports: Ports):
 T.Task<{ articleIds: ReadonlyArray<string>, articleCount: number }> => pipe(
   elifeArticleMissingFromSubjectAreaLists(ports),
-  T.map(RA.map((articleId) => articleId.value)),
-  T.map((articleIds) => ({
-    articleIds,
-    articleCount: articleIds.length,
-  })),
+  T.map(formatForJson),
 );
