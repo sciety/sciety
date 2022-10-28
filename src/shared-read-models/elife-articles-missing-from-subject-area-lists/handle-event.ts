ea4a617e783f0244730971c4e5dc96351a93659b
Giorgio Sironi <g.sironi@elifesciences.org>
2022-10-28 14:22:35 +0100

    Explicitly couple to canonical group id #1850

--- src/shared-read-models/elife-articles-missing-from-subject-area-lists/handle-event.ts
+++ src/shared-read-models/elife-articles-missing-from-subject-area-lists/handle-event.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-param-reassign */
-import { elifeSubjectAreaLists } from './data';
+import { elifeGroupId, elifeSubjectAreaLists } from './data';
 import { DomainEvent, isArticleAddedToListEvent, isEvaluationRecordedEvent } from '../../domain-events';
-import * as GroupId from '../../types/group-id';
 
 export type ArticleState = 'missing' | 'added';
 export type ReadModel = Record<string, ArticleState>;
@@ -10,7 +9,7 @@ export const initialState = (): ReadModel => ({});
 
 export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel => {
   if (isEvaluationRecordedEvent(event)) {
-    if (event.groupId === GroupId.fromValidatedString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0')) {
+    if (event.groupId === elifeGroupId) {
       const key = event.articleId.value;
       if (readmodel[key] === undefined) {
         readmodel[key] = 'missing' as ArticleState;
