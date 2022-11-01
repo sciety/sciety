2768e4a1450216a56d275948cfe6063ff370c7b7
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-01 15:11:24 +0000

    Fix build #1859

--- src/add-article-to-elife-subject-area-list/index.ts
+++ src/add-article-to-elife-subject-area-list/index.ts
@@ -1,2 +1,3 @@
 export { readModelStatus, ArticleIdsByState } from './read-model-status';
+// ts-unused-exports:disable-next-line
 export { discoverElifeArticleCategory } from './discover-elife-article-category';
