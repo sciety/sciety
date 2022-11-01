6dd6e0ab0b94894c8b23863ba85ea99230eb49a4
Kevin Rutherford <kevin@rutherford-software.com>
2022-11-01 14:47:27 +0000

    Write a test list for the new saga #1859

---
+++ test/add-article-to-elife-subject-area-list/discover-elife-article-category.test.ts
@@ -0,0 +1,19 @@
+describe('discover-elife-article-category', () => {
+  describe('when there is work to do', () => {
+    describe('when the category can be retrieved', () => {
+      it.todo('records the category via a command');
+
+      it.todo('invokes only one command');
+    });
+
+    describe('when the category cannot be retrieved', () => {
+      it.todo('does not invoke a command');
+
+      it.todo('logs a warning');
+    });
+  });
+
+  describe('when there is no work to do', () => {
+    it.todo('does not invoke a command');
+  });
+});
