a9dd6afe90973f3c4e195754e750f83f6133d496
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-02 15:19:41 +0000

    Update home page list cards

--- src/home-page/cards/hardcoded-user-list-cards.ts
+++ src/home-page/cards/hardcoded-user-list-cards.ts
@@ -1,22 +1,22 @@
 import { toUserId } from '../../types/user-id';
 
 export const card1 = {
-  userId: toUserId('1449681648019255299'),
-  description: 'Selected papers from the Global South curated by Stefano Vianello',
-  avatarUrl: 'https://pbs.twimg.com/profile_images/1544360429744447488/IHZY2BeW_bigger.jpg',
-  handle: 'ZonaPellucida_',
+  userId: toUserId('1384541806231175172'),
+  description: 'Articles that are being read by Biophysics Colab.',
+  avatarUrl: 'https://pbs.twimg.com/profile_images/1417582635040317442/jYHfOlh6_bigger.jpg',
+  handle: 'BiophysicsColab',
 };
 
 export const card2 = {
   userId: toUserId('1412019815619911685'),
-  description: 'See what researchers at Prachee Avasthi’s lab are reading to discover some interesting new work',
+  description: 'See what researchers at Prachee Avasthi’s lab are reading to discover some interesting new work.',
   avatarUrl: 'https://pbs.twimg.com/profile_images/1417079202973638657/VrQKBTkw_bigger.jpg',
   handle: 'AvasthiReading',
 };
 
 export const card3 = {
-  userId: toUserId('1384541806231175172'),
-  description: 'Articles that are being read by Biophysics Colab.',
-  avatarUrl: 'https://pbs.twimg.com/profile_images/1417582635040317442/jYHfOlh6_bigger.jpg',
-  handle: 'BiophysicsColab',
+  userId: toUserId('1122522190791028737'),
+  description: 'Articles that are being read by Dr. Maria Eichel-Vogel.',
+  avatarUrl: 'https://pbs.twimg.com/profile_images/1584205611247583234/aN5oC7iZ_bigger.jpg',
+  handle: 'maria_eichel',
 };
