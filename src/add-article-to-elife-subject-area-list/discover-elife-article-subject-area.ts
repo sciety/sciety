339d7b91d378af3de1004b5a6cacebc5340c64e9
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-03 10:36:14 +0000

    Call command from saga #1859

--- src/add-article-to-elife-subject-area-list/discover-elife-article-subject-area.ts
+++ src/add-article-to-elife-subject-area-list/discover-elife-article-subject-area.ts
@@ -1,4 +1,5 @@
 import { Logger, RecordSubjectArea } from '../shared-ports';
+import { Doi } from '../types/doi';
 
 type Ports = {
   logger: Logger,
@@ -7,5 +8,12 @@ type Ports = {
 
 export const discoverElifeArticleSubjectArea = async (ports: Ports): Promise<void> => {
   ports.logger('info', 'discoverElifeArticleSubjectArea starting');
+  ports.recordSubjectArea({
+    articleId: new Doi('10.1101/1234'),
+    subjectArea: {
+      value: 'something',
+      server: 'biorxiv',
+    },
+  });
   ports.logger('info', 'discoverElifeArticleSubjectArea finished');
 };
