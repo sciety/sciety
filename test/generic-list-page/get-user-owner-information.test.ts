e5a3d477e77afec65717986d2e090a5de346334f
David Moulton <d.moulton@elifesciences.org>
2022-11-03 11:32:05 +0000

    Write a failing test #1863

--- test/generic-list-page/get-user-owner-information.test.ts
+++ test/generic-list-page/get-user-owner-information.test.ts
@@ -1,6 +1,37 @@
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { getUserOwnerInformation } from '../../src/generic-list-page/get-user-owner-information';
+import { arbitraryString, arbitraryUri, arbitraryWord } from '../helpers';
+import { shouldNotBeCalled } from '../should-not-be-called';
+import { arbitraryUserId } from '../types/user-id.helper';
+
 describe('get-user-owner-information', () => {
   describe('when Twitter finds the given user', () => {
-    it.todo('returns the corresponding owner info');
+    it.failing('returns the corresponding owner info', async () => {
+      const userDisplayName = arbitraryString();
+      const userAvatarUrl = arbitraryUri().toString();
+      const userHandle = arbitraryWord();
+      const userId = arbitraryUserId();
+      const ports = {
+        getUserDetails: () => TE.right({
+          displayName: userDisplayName,
+          handle: userHandle,
+          avatarUrl: userAvatarUrl,
+        }),
+      };
+
+      const ownerInfo = await pipe(
+        userId,
+        getUserOwnerInformation(ports),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+
+      expect(ownerInfo).toStrictEqual({
+        ownerName: userDisplayName,
+        ownerAvatarPath: userAvatarUrl,
+        ownerHref: `/users/${userHandle}`,
+      });
+    });
   });
 
   describe('when Twitter does not find the given user', () => {
