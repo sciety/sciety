24e6b5b9a7767810498c152916284a876aae3235
Kevin Rutherford <kevin@rutherford-software.com>
2022-11-03 11:20:36 +0000

    Write a test list #1863

---
+++ test/generic-list-page/get-user-owner-information.test.ts
@@ -0,0 +1,9 @@
+describe('get-user-owner-information', () => {
+  describe('when Twitter finds the given user', () => {
+    it.todo('returns the corresponding owner info');
+  });
+
+  describe('when Twitter does not find the given user', () => {
+    it.todo('returns a not-found error');
+  });
+});
