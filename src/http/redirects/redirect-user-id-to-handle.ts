c034ff8039942516a6f03414c3db32c63ba0575e
David Moulton <d.moulton@elifesciences.org>
2022-11-03 14:24:43 +0000

    Extend use of shared port #1863

--- src/http/redirects/redirect-user-id-to-handle.ts
+++ src/http/redirects/redirect-user-id-to-handle.ts
@@ -1,17 +1,17 @@
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
-import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { StatusCodes } from 'http-status-codes';
 import { Middleware } from 'koa';
+import { GetUserDetails } from '../../shared-ports/get-user-details';
 import * as DE from '../../types/data-error';
 import { toHtmlFragment } from '../../types/html-fragment';
 import { UserId } from '../../types/user-id';
 import { toErrorResponse } from '../page-handler';
 
 type Ports = {
-  getUserDetails: (userID: UserId) => TE.TaskEither<DE.DataError, { handle: string }>,
+  getUserDetails: GetUserDetails,
 };
 
 export const redirectUserIdToHandle = (ports: Ports, path: string): Middleware => async (context, next) => {
