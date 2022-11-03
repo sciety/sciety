3c717d0bc32576738a00330d87af36d33cb661b6
Kevin Rutherford <kevin@rutherford-software.com>
2022-11-03 11:40:01 +0000

    Write a failing test #1863

--- test/generic-list-page/get-user-owner-information.test.ts
+++ test/generic-list-page/get-user-owner-information.test.ts
@@ -1,17 +1,19 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { getUserOwnerInformation } from '../../src/generic-list-page/get-user-owner-information';
+import * as DE from '../../src/types/data-error';
 import { arbitraryString, arbitraryUri, arbitraryWord } from '../helpers';
 import { shouldNotBeCalled } from '../should-not-be-called';
 import { arbitraryUserId } from '../types/user-id.helper';
 
 describe('get-user-owner-information', () => {
+  const userId = arbitraryUserId();
+
   describe('when Twitter finds the given user', () => {
     it.failing('returns the corresponding owner info', async () => {
       const userDisplayName = arbitraryString();
       const userAvatarUrl = arbitraryUri().toString();
       const userHandle = arbitraryWord();
-      const userId = arbitraryUserId();
       const ports = {
         getUserDetails: () => TE.right({
           displayName: userDisplayName,
@@ -35,6 +37,19 @@ describe('get-user-owner-information', () => {
   });
 
   describe('when Twitter does not find the given user', () => {
-    it.todo('returns a not-found error');
+    it.failing('returns a not-found error', async () => {
+      const ports = {
+        getUserDetails: () => TE.left(DE.notFound),
+      };
+
+      const ownerInfo = await pipe(
+        userId,
+        getUserOwnerInformation(ports),
+        TE.swap,
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+
+      expect(ownerInfo).toStrictEqual(DE.notFound);
+    });
   });
 });
