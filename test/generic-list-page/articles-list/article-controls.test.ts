a670df821ae5292f3bece2c4dc0fedfc778058bf
LinaKind <l.kinduryte@elifesciences.org>
2022-11-04 15:21:15 +0000

    Write a failing test #1867

--- test/generic-list-page/articles-list/article-controls.test.ts
+++ test/generic-list-page/articles-list/article-controls.test.ts
@@ -1,7 +1,18 @@
+import * as O from 'fp-ts/Option';
+import { articleControls } from '../../../src/generic-list-page/articles-list/article-controls';
+import { fromUserId } from '../../../src/types/list-owner-id';
+import { arbitraryUserId } from '../../types/user-id.helper';
+
 describe('article-controls', () => {
   describe('when the list owner is a user', () => {
     describe('when the logged in user is the list owner', () => {
-      it.todo('has controls');
+      const userId = arbitraryUserId();
+      const listOwnerId = fromUserId(userId);
+      const loggedInUserId = O.some(userId);
+
+      it.failing('has controls', () => {
+        expect(articleControls(listOwnerId, loggedInUserId)).toBe(true);
+      });
     });
 
     describe('when the logged in user is not the list owner', () => {
