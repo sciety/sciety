943ec3e508ea68785aa6499167fcc2e443776441
LinaKind <l.kinduryte@elifesciences.org>
2022-11-04 14:52:39 +0000

    Introduce renderer for the form #1867

--- src/generic-list-page/articles-list/to-page-of-cards.ts
+++ src/generic-list-page/articles-list/to-page-of-cards.ts
@@ -11,7 +11,9 @@ import { ArticleViewModel } from '../../shared-components/article-card';
 import { PageOfItems } from '../../shared-components/paginate';
 import { paginationControls } from '../../shared-components/pagination-controls';
 import { ArticleActivity } from '../../types/article-activity';
+import { Doi } from '../../types/doi';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
+import { ListId } from '../../types/list-id';
 import { ListOwnerId } from '../../types/list-owner-id';
 import { UserId } from '../../types/user-id';
 
@@ -42,16 +44,31 @@ const articleControls = (
   loggedInUserId: O.Option<UserId>,
 ) => O.none;
 
-const renderRemoveArticleForm = () => toHtmlFragment('');
+const renderRemoveArticleForm = (articleId: Doi, listId: ListId) => () => pipe(
+  articleId.value,
+  (id) => `<form method="post" action="/remove-article-from-list-from-form">
+      <input type="hidden" name="articleid" value="${id}">
+      <input type="hidden" name="listid" value="${listId}">
+      <button aria-label="Remove this article from the list" class="saved-articles-control">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" class="saved-articles-control__icon">
+          <desc>Remove this article from the list</desc>
+          <path d="M0 0h24v24H0V0z" fill="none"/>
+          <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/>
+        </svg>
+      </button>
+    </form>`,
+  toHtmlFragment,
+);
 
 const toArticleCardWithControlsViewModel = (
   listOwnerId: ListOwnerId,
   loggedInUserId: O.Option<UserId>,
+  listId: ListId,
 ) => (articleViewModel: ArticleViewModel) => ({
   articleViewModel,
   controls: pipe(
     articleControls(listOwnerId, loggedInUserId),
-    O.map(renderRemoveArticleForm),
+    O.map(renderRemoveArticleForm(articleViewModel.articleId, listId)),
   ),
 });
 
@@ -60,13 +77,14 @@ export const toPageOfCards = (
   basePath: string,
   listOwnerId: ListOwnerId,
   loggedInUserId: O.Option<UserId>,
+  listId: ListId,
 ) => (pageOfArticles: PageOfItems<ArticleActivity>): T.Task<HtmlFragment> => pipe(
   pageOfArticles.items,
   T.traverseArray(toCardViewModel(ports)),
   T.map(E.fromPredicate(RA.some(E.isRight), () => noArticlesCanBeFetchedMessage)),
   TE.map(RA.map(E.bimap(
     identity,
-    toArticleCardWithControlsViewModel(listOwnerId, loggedInUserId),
+    toArticleCardWithControlsViewModel(listOwnerId, loggedInUserId, listId),
   ))),
   TE.map(flow(
     renderComponent,
