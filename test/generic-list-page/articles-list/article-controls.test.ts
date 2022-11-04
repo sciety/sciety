e61be270b4cf4a59740f1878be24e8b2bc9ee17a
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-04 15:27:28 +0000

    Write all remaining passing tests #1867

--- test/generic-list-page/articles-list/article-controls.test.ts
+++ test/generic-list-page/articles-list/article-controls.test.ts
@@ -1,6 +1,7 @@
 import * as O from 'fp-ts/Option';
 import { articleControls } from '../../../src/generic-list-page/articles-list/article-controls';
-import { fromUserId } from '../../../src/types/list-owner-id';
+import { fromGroupId, fromUserId } from '../../../src/types/list-owner-id';
+import { arbitraryGroupId } from '../../types/group-id.helper';
 import { arbitraryUserId } from '../../types/user-id.helper';
 
 describe('article-controls', () => {
@@ -16,15 +17,30 @@ describe('article-controls', () => {
     });
 
     describe('when the logged in user is not the list owner', () => {
-      it.todo('has no controls');
+      const listOwnerId = fromUserId(arbitraryUserId());
+      const loggedInUserId = O.some(arbitraryUserId());
+
+      it('has no controls', () => {
+        expect(articleControls(listOwnerId, loggedInUserId)).toBe(false);
+      });
     });
 
     describe('when the user is not logged in', () => {
-      it.todo('has no controls');
+      const listOwnerId = fromUserId(arbitraryUserId());
+      const loggedInUserId = O.none;
+
+      it('has no controls', () => {
+        expect(articleControls(listOwnerId, loggedInUserId)).toBe(false);
+      });
     });
   });
 
   describe('when the list owner is a group', () => {
-    it.todo('has no controls');
+    const listOwnerId = fromGroupId(arbitraryGroupId());
+    const irrelevantLoggedInUserId = O.none;
+
+    it('has no controls', () => {
+      expect(articleControls(listOwnerId, irrelevantLoggedInUserId)).toBe(false);
+    });
   });
 });
