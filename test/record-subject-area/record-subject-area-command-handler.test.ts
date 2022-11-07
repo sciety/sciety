5a0ba96918a43e889a1f86e693efc90707b0783c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-11-07 10:55:13 +0000

    Write test list for command handler #1871

---
+++ test/record-subject-area/record-subject-area-command-handler.test.ts
@@ -0,0 +1,17 @@
+describe('record-subject-area-command-handler', () => {
+  describe('given no events', () => {
+    it.todo('raises an event');
+  });
+
+  describe('when an evaluation was recorded', () => {
+    it.todo('raises an event');
+  });
+
+  describe('the same subject area was recorded', () => {
+    it.todo('raises no events');
+  });
+
+  describe('a different subject area was recorded', () => {
+    it.todo('returns an error message');
+  });
+});
