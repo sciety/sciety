afbc1fbb72cce2c4fe7b33249b163dbc106a20e8
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-11-07 15:41:36 +0000

    Extract and name behaviour to reveal intent more clearly #1871

--- src/record-subject-area/execute-command.ts
+++ src/record-subject-area/execute-command.ts
@@ -1,24 +1,30 @@
 import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
 import { RecordSubjectAreaCommand } from '../commands';
 import { DomainEvent } from '../domain-events/domain-event';
 import { isSubjectAreaRecordedEvent, subjectAreaRecorded } from '../domain-events/subject-area-recorded-event';
-import { eqDoi } from '../types/doi';
+import { Doi, eqDoi } from '../types/doi';
 import { ErrorMessage, toErrorMessage } from '../types/error-message';
+
+const buildUpArticleSubjectAreaResourceFor = (articleId: Doi) => flow(
+  RA.filter(isSubjectAreaRecordedEvent),
+  RA.filter((event) => eqDoi.equals(event.articleId, articleId)),
+  RA.head,
+  O.map((event) => event.subjectArea),
+);
 
 type ExecuteCommand = (command: RecordSubjectAreaCommand)
 => (events: ReadonlyArray<DomainEvent>) => E.Either<ErrorMessage, ReadonlyArray<DomainEvent>> ;
 
 export const executeCommand: ExecuteCommand = (command) => (events) => pipe(
   events,
-  RA.filter(isSubjectAreaRecordedEvent),
-  RA.filter((event) => eqDoi.equals(event.articleId, command.articleId)),
-  RA.head,
+  buildUpArticleSubjectAreaResourceFor(command.articleId),
   E.fromOption(() => 'no subject area recorded for this article'),
   E.match(
     () => E.right([subjectAreaRecorded(command.articleId, command.subjectArea)]),
-    (event) => (event.subjectArea === command.subjectArea
+    (subjectArea) => (subjectArea === command.subjectArea
       ? E.right([])
       : E.left(toErrorMessage('changing of subject area not possible according to domain model'))),
   ),
