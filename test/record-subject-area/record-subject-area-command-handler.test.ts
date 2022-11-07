26b0c2332fc73b57085755400c3a4921315caa1b
LinaKind <l.kinduryte@elifesciences.org>
2022-11-07 11:20:51 +0000

    Write a failing test #1871

--- test/record-subject-area/record-subject-area-command-handler.test.ts
+++ test/record-subject-area/record-subject-area-command-handler.test.ts
@@ -1,6 +1,29 @@
+import { pipe } from 'fp-ts/function';
+import { RecordSubjectAreaCommand } from '../../src/commands';
+import { DomainEvent } from '../../src/domain-events/domain-event';
+import { arbitraryArticleId } from '../types/article-id.helper';
+import { arbitrarySubjectArea } from '../types/subject-area.helper';
+
+type ExecuteCommand = (command: RecordSubjectAreaCommand)
+=> (events: ReadonlyArray<DomainEvent>) => ReadonlyArray<DomainEvent> ;
+
+const executeCommand: ExecuteCommand = () => () => [];
+
 describe('record-subject-area-command-handler', () => {
   describe('given no events', () => {
-    it.todo('raises an event');
+    const command = {
+      articleId: arbitraryArticleId(),
+      subjectArea: arbitrarySubjectArea(),
+    };
+
+    const result = pipe(
+      [],
+      executeCommand(command),
+    );
+
+    it.failing('raises an event', () => {
+      expect(result).toStrictEqual([expect.objectContaining({ type: 'SubjectAreaRecorded' })]);
+    });
   });
 
   describe('when an evaluation was recorded', () => {
