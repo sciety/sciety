dd02d76e93b373d22342137d0e68b82394f54bf8
LinaKind <l.kinduryte@elifesciences.org>
2022-11-08 15:29:57 +0000

    Write a failing test #1878

--- feature-test/remove-article-from-list-from-form.test.ts
+++ feature-test/remove-article-from-list-from-form.test.ts
@@ -1,10 +1,24 @@
+import {
+  $,
+  goto,
+} from 'taiko';
+import { noArticlesMessage } from '../src/generic-list-page/articles-list/static-messages';
+
 describe('remove-article-from-list-from-form', () => {
   describe('when the user is logged in', () => {
     describe('and has saved an article', () => {
       describe('and they click the trash can', () => {
+        const contentSelector = 'main';
+        const genericListPage = 'localhost:8080/lists/12345';
+
         it.todo('they should be redirected to the generic list page');
 
-        it.todo('the article should no longer be in the list');
+        it.failing('the article should no longer be in the list', async () => {
+          await goto(genericListPage);
+          const content = await $(contentSelector).text();
+
+          expect(content).toContain(noArticlesMessage);
+        });
       });
     });
   });
