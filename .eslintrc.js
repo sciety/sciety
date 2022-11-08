e7042046e7e190f0c441fa1b5f4d28373309177f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-11-08 08:51:11 +0000

    Simplify eslintrc by extending the no-compile ruleset #1869

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,133 +1,21 @@
 module.exports = {
-  env: {
-    es2020: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript',
-    'plugin:fp-ts/all',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: './.eslintrc-no-compilation.js',
   parserOptions: {
     project: './tsconfig.json',
   },
-  plugins: [
-    '@typescript-eslint',
-    'node',
-    'no-loops',
-    'unused-imports',
-  ],
-  root: true,
   rules: {
-    '@typescript-eslint/array-type': ['error', {
-      default: 'generic',
-    }],
-    '@typescript-eslint/brace-style': 'error',
-    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-    '@typescript-eslint/member-delimiter-style': ['error', {
-      multiline: {
-        delimiter: 'comma',
-        requireLast: true,
-      },
-      singleline: {
-        delimiter: 'comma',
-        requireLast: false,
-      },
-    }],
     '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/no-implicit-any-catch': 'error',
     '@typescript-eslint/no-misused-promises': ['error', {
       checksVoidReturn: false,
     }],
+    '@typescript-eslint/dot-notation': 'error',
+    '@typescript-eslint/no-implied-eval': 'error',
+    '@typescript-eslint/no-throw-literal': 'error',
     '@typescript-eslint/no-unsafe-return': 'error',
-    '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/prefer-nullish-coalescing': 'error',
     '@typescript-eslint/promise-function-async': 'error',
     '@typescript-eslint/restrict-template-expressions': 'error',
+    '@typescript-eslint/return-await': 'error',
     '@typescript-eslint/switch-exhaustiveness-check': 'error',
-    '@typescript-eslint/type-annotation-spacing': 'error',
-    'consistent-return': 'off',
-    'default-case': 'off',
-    'fp-ts/no-module-imports': 'off',
-    'function-paren-newline': ['error', 'consistent'],
-    'import/no-useless-path-segments': ['error', {
-      noUselessIndex: true,
-    }],
-    'import/order': ['error', {
-      alphabetize: {
-        order: 'asc',
-      },
-      groups: [
-        'builtin',
-        'external',
-        'internal',
-        'index',
-        'sibling',
-        'parent',
-      ],
-    }],
-    'import/prefer-default-export': 'off',
-    'max-len': ['error', 120, 2, {
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-      ignoreUrls: true,
-    }],
-    'no-await-in-loop': 'off',
-    'no-loops/no-loops': 2,
-    'no-restricted-syntax': ['error', ...[
-      'ForInStatement',
-      'LabeledStatement',
-      'WithStatement',
-    ]],
-    'no-underscore-dangle': 'off',
-    'no-unreachable': 'error',
-    'no-void': ['error', {
-      allowAsStatement: true,
-    }],
-    'node/prefer-global/buffer': ['error', 'never'],
-    'node/prefer-global/text-decoder': ['error', 'never'],
-    'node/prefer-global/text-encoder': ['error', 'never'],
-    'node/prefer-global/url': ['error', 'never'],
-    'node/prefer-global/url-search-params': ['error', 'never'],
-    'sort-imports': ['error', {
-      ignoreCase: true,
-      ignoreDeclarationSort: true,
-    }],
-    'unused-imports/no-unused-imports-ts': 'error',
-    'import/no-extraneous-dependencies': ['error', { devDependencies: ['feature-test/**/*.ts', 'test/**/*.ts'] }],
-  },
-  overrides: [
-    {
-      files: ['**/*.test.ts'],
-      extends: [
-        'plugin:jest/all',
-        'plugin:jest-formatting/strict',
-      ],
-      plugins: [
-        'jest',
-        'jest-formatting',
-      ],
-      rules: {
-        'jest/no-conditional-in-test': 'off',
-        'jest/no-disabled-tests': 'off',
-        'jest/no-hooks': 'off',
-        'jest/prefer-expect-assertions': 'off',
-        'jest/prefer-to-be': 'error',
-        'jest/prefer-expect-resolves': 'off',
-        'jest/unbound-method': 'off',
-      },
-    },
-  ],
-  settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-      },
-    },
   },
 };
