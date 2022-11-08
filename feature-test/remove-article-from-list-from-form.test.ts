92ba0b76a20e88186eb55168c65b0bbcca6edafe
David Moulton <d.moulton@elifesciences.org>
2022-11-08 15:03:00 +0000

    Write todo feature tests for wiring the trash can for generic lists #1878

---
+++ feature-test/remove-article-from-list-from-form.test.ts
@@ -0,0 +1,11 @@
+describe('remove-article-from-list-from-form', () => {
+  describe('when the user is logged in', () => {
+    describe('and has saved an article', () => {
+      describe('and they click the trash can', () => {
+        it.todo('they should be redirected to the generic list page');
+
+        it.todo('the article should no longer be in the list');
+      });
+    });
+  });
+});
