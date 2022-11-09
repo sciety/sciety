81798e6029b57f9db844d4409b92a833a88cb848
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-09 11:44:13 +0000

    Check list ownership #1878

--- src/remove-article-from-list-from-form/index.ts
+++ src/remove-article-from-list-from-form/index.ts
@@ -1,12 +1,21 @@
 import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as PR from 'io-ts/PathReporter';
 import { Middleware } from 'koa';
 import { removeArticleFromListCommandCodec } from '../commands/remove-article-from-list';
-import { Logger } from '../shared-ports';
+import { GetAllEvents, Logger } from '../shared-ports';
+import { getList } from '../shared-read-models/lists';
+import * as LOID from '../types/list-owner-id';
+import { User } from '../types/user';
 
-export const removeArticleFromListFromForm = (adapters: { logger: Logger }): Middleware => async (context, next) => {
-  pipe(
+type Ports = { logger: Logger, getAllEvents: GetAllEvents };
+
+export const removeArticleFromListFromForm = (adapters: Ports): Middleware => async (context, next) => {
+  const user = context.state.user as User;
+  const userId = user.id;
+  await pipe(
     {
       articleId: context.request.body.articleid,
       listId: context.request.body.listid,
@@ -18,8 +27,24 @@ export const removeArticleFromListFromForm = (adapters: { logger: Logger }): Mid
         PR.failure,
         (fails) => adapters.logger('error', 'invalid remove article from list form command', { fails }),
       ),
-      (command) => adapters.logger('info', 'received remove article from list form command', { command }),
+      (command) => {
+        adapters.logger('info', 'received remove article from list form command', { command });
+        return command;
+      },
     ),
-  );
+    TE.fromEither,
+    TE.chainW((command) => pipe(
+      adapters.getAllEvents,
+      T.chain(getList(command.listId)),
+      TE.map((list) => {
+        if (!LOID.eqListOwnerId.equals(list.ownerId, LOID.fromUserId(userId))) {
+          adapters.logger('error', 'List owner id does not match user id', { list, userId });
+        } else {
+          adapters.logger('info', 'List owner id matches user id', { list, userId });
+        }
+        return command;
+      }),
+    )),
+  )();
   await next();
 };
