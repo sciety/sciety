214d1a251cc047af0ba43c62ef2f7426ab935366
LinaKind <l.kinduryte@elifesciences.org>
2022-11-09 10:48:45 +0000

    Decode form input for remove article from list form #1878

--- src/remove-article-from-list-from-form/index.ts
+++ src/remove-article-from-list-from-form/index.ts
@@ -1,6 +1,25 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import * as PR from 'io-ts/PathReporter';
 import { Middleware } from 'koa';
+import { removeArticleFromListCommandCodec } from '../commands/remove-article-from-list';
+import { Logger } from '../shared-ports';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const removeArticleFromListFromForm = (adapters: unknown): Middleware => async (context, next) => {
+export const removeArticleFromListFromForm = (adapters: { logger: Logger }): Middleware => async (context, next) => {
+  pipe(
+    {
+      articleId: context.request.body.articleid,
+      listId: context.request.body.listid,
+    },
+    removeArticleFromListCommandCodec.decode,
+    E.bimap(
+      (errors) => pipe(
+        errors,
+        PR.failure,
+        (fails) => adapters.logger('error', 'invalid remove article from list form command', { fails }),
+      ),
+      (command) => adapters.logger('info', 'received remove article from list form command', { command }),
+    ),
+  );
   await next();
 };
