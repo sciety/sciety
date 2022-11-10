8cc4e4a6b314a464bbdfe80d16fe0d7e09d714e1
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-11-10 10:36:42 +0000

    Write regression test #1884

--- feature-test/create-annotation.test.ts
+++ feature-test/create-annotation.test.ts
@@ -1,8 +1,49 @@
-describe('create-annotation', () => {
+import {
+  $, click, closeBrowser, goto, into, openBrowser, textBox, write,
+} from 'taiko';
+import { toUserId } from '../src/types/user-id';
+import { arbitraryString } from '../test/helpers';
+
+describe.skip('create-annotation', () => {
+  beforeAll(async () => {
+    await openBrowser();
+  });
+
+  afterAll(async () => {
+    await closeBrowser();
+  });
+
   describe('when logged in as AvasthiReading', () => {
+    const avasthiReadingUserId = toUserId('1412019815619911685');
+
+    beforeAll(async () => {
+      await goto(`localhost:8080/log-in-as?userId=${avasthiReadingUserId}`);
+    });
+
     describe('and having saved an article', () => {
+      const articleId = '10.1101/2022.09.20.508647';
+
+      beforeAll(async () => {
+        await goto(`localhost:8080/articles/${articleId}}`);
+        await click('Save to my list');
+      });
+
       describe('and having annotated that article', () => {
-        it.todo('the article card on the user saved articles list page has the annotation attached');
+        const annotationText = arbitraryString();
+
+        beforeAll(async () => {
+          await goto('localhost:8080/annotations/create-annotation-form-avasthi-reading');
+          await write(annotationText, into(textBox('Annotation content')));
+          await write(articleId, into(textBox('Article DOI')));
+          await click('Create annotation');
+          await goto('localhost:8080/users/AvasthiReading/lists/saved-articles');
+        });
+
+        it('the article card on the user saved articles list page has the annotation attached', async () => {
+          const annotationSectionText = await $('.article-card-annotation').text();
+
+          expect(annotationSectionText).toContain(annotationText);
+        });
       });
     });
   });
