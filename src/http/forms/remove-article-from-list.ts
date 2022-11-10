aa2cb1d75138f6fe1c40946f4aa3d8150fd8a6a9
Kevin Rutherford <kevin@rutherford-software.com>
2022-11-10 10:58:32 +0000

    Separate form handler from koa #1878

--- src/http/forms/remove-article-from-list.ts
+++ src/http/forms/remove-article-from-list.ts
@@ -11,45 +11,55 @@ import { getList } from '../../shared-read-models/lists';
 import * as DE from '../../types/data-error';
 import * as LOID from '../../types/list-owner-id';
 import { User } from '../../types/user';
+import { UserId } from '../../types/user-id';
 
 type Ports = { logger: Logger, getAllEvents: GetAllEvents, commitEvents: CommitEvents };
 
+type FormBody = {
+  articleid: unknown,
+  listid: unknown,
+};
+
+const handleFormSubmission = (adapters: Ports, userId: UserId) => (formBody: FormBody) => pipe(
+  {
+    articleId: formBody.articleid,
+    listId: formBody.listid,
+  },
+  removeArticleFromListCommandCodec.decode,
+  E.bimap(
+    (errors) => pipe(
+      errors,
+      PR.failure,
+      (fails) => adapters.logger('error', 'invalid remove article from list form command', { fails }),
+    ),
+    (command) => {
+      adapters.logger('info', 'received remove article from list form command', { command });
+      return command;
+    },
+  ),
+  TE.fromEither,
+  TE.chainW((command) => pipe(
+    adapters.getAllEvents,
+    T.chain(getList(command.listId)),
+    TE.chainEitherK((list) => {
+      if (!LOID.eqListOwnerId.equals(list.ownerId, LOID.fromUserId(userId))) {
+        adapters.logger('error', 'List owner id does not match user id', { list, userId });
+
+        return E.left(DE.unavailable);
+      }
+      adapters.logger('info', 'List owner id matches user id', { list, userId });
+
+      return E.right(command);
+    }),
+  )),
+  TE.chainW(removeArticleFromListCommandHandler(adapters)),
+);
+
 export const removeArticleFromList = (adapters: Ports): Middleware => async (context, next) => {
   const user = context.state.user as User;
-  const userId = user.id;
   await pipe(
-    {
-      articleId: context.request.body.articleid,
-      listId: context.request.body.listid,
-    },
-    removeArticleFromListCommandCodec.decode,
-    E.bimap(
-      (errors) => pipe(
-        errors,
-        PR.failure,
-        (fails) => adapters.logger('error', 'invalid remove article from list form command', { fails }),
-      ),
-      (command) => {
-        adapters.logger('info', 'received remove article from list form command', { command });
-        return command;
-      },
-    ),
-    TE.fromEither,
-    TE.chainW((command) => pipe(
-      adapters.getAllEvents,
-      T.chain(getList(command.listId)),
-      TE.chainEitherK((list) => {
-        if (!LOID.eqListOwnerId.equals(list.ownerId, LOID.fromUserId(userId))) {
-          adapters.logger('error', 'List owner id does not match user id', { list, userId });
-
-          return E.left(DE.unavailable);
-        }
-        adapters.logger('info', 'List owner id matches user id', { list, userId });
-
-        return E.right(command);
-      }),
-    )),
-    TE.chainW(removeArticleFromListCommandHandler(adapters)),
+    context.request.body,
+    handleFormSubmission(adapters, user.id),
     TE.mapLeft(() => {
       context.redirect('/action-failed');
     }),
