7fb803bb6d5860bc69d6177b7dc053c709c0d6b7
David Moulton <d.moulton@elifesciences.org>
2022-11-10 10:12:32 +0000

    Write a regression test list for creating annotations against the supported user list #1884

---
+++ feature-test/create-annotation.test.ts
@@ -0,0 +1,9 @@
+describe('create-annotation', () => {
+  describe('when logged in as AvasthiReading', () => {
+    describe('and having saved an article', () => {
+      describe('and having annotated that article', () => {
+        it.todo('the article card on the user saved articles list page has the annotation attached');
+      });
+    });
+  });
+});
