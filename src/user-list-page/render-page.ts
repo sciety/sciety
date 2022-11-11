f327f864dff836cc16ddc9c405c90a253ed33ed9
David Moulton <d.moulton@elifesciences.org>
2022-11-11 15:37:56 +0000

    Remove unused #1884

--- src/user-list-page/render-page.ts
+++ src/user-list-page/render-page.ts
@@ -1,8 +1,6 @@
 import { pipe } from 'fp-ts/function';
-import * as DE from '../types/data-error';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
-import { RenderPageError } from '../types/render-page-error';
 
 type ViewModel = {
   title: string,
@@ -20,19 +18,6 @@ const render: Render = ({ header, content, supplementary = toHtmlFragment('') })
   </section>
   ${supplementary}
 `);
-
-export const renderErrorPage = (e: DE.DataError): RenderPageError => pipe(
-  e,
-  DE.fold({
-    notFound: () => 'We couldn\'t find this information.',
-    unavailable: () => 'We couldn\'t retrieve this information. Please try again.',
-  }),
-  toHtmlFragment,
-  (message) => ({
-    type: e,
-    message,
-  }),
-);
 
 export const renderPage = (viewModel: ViewModel): Page => ({
   title: viewModel.title,
