d313ab5313239d027dbc18f9d45f137c50c2fb90
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-14 15:16:27 +0000

    Handle article added to list #1888

--- src/shared-read-models/lists-owned-by-users/handle-event.ts
+++ src/shared-read-models/lists-owned-by-users/handle-event.ts
@@ -1,14 +1,28 @@
 import { DomainEvent } from '../../domain-events';
+import { isArticleAddedToListEvent } from '../../domain-events/article-added-to-list-event';
+/* eslint-disable no-param-reassign */
+import { isListCreatedEvent } from '../../domain-events/list-created-event';
 import { ListId } from '../../types/list-id';
 import { ListOwnerId } from '../../types/list-owner-id';
 
 type ListState = {
   ownerId: ListOwnerId,
-  articleIds: ReadonlyArray<string>,
+  articleIds: Array<string>,
 };
 export type ReadModel = Record<ListId, ListState>;
 
 export const initialState = (): ReadModel => ({});
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel => readmodel;
+export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel => {
+  if (isListCreatedEvent(event)) {
+    readmodel[event.listId] = {
+      ownerId: event.ownerId,
+      articleIds: [],
+    };
+  } else if (isArticleAddedToListEvent(event)) {
+    readmodel[event.listId].articleIds.push(event.articleId.value);
+  }
+
+  return readmodel;
+};
