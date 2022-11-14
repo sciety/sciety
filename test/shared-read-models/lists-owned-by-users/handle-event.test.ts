ab74c0c57dbd34a7ad4f15ad153d7124371abea6
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-14 14:24:05 +0000

    Write a failing test for new read model #1888

--- test/shared-read-models/lists-owned-by-users/handle-event.test.ts
+++ test/shared-read-models/lists-owned-by-users/handle-event.test.ts
@@ -1,10 +1,31 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { articleAddedToList } from '../../../src/domain-events/article-added-to-list-event';
+import { listCreated } from '../../../src/domain-events/list-created-event';
+import { handleEvent, initialState } from '../../../src/shared-read-models/lists-owned-by-users/handle-event';
+import * as LOID from '../../../src/types/list-owner-id';
+import { arbitraryString } from '../../helpers';
+import { arbitraryArticleId } from '../../types/article-id.helper';
+import { arbitraryListId } from '../../types/list-id.helper';
+import { arbitraryUserId } from '../../types/user-id.helper';
+
 describe('handle-event', () => {
-  describe('when there are no events', () => {
-    it.todo('the read model is empty');
-  });
+  const articleId = arbitraryArticleId();
+  const listId = arbitraryListId();
+  const userId = arbitraryUserId();
 
   describe('when the user has added an article to the list', () => {
-    it.todo('the article id is listed against the user id');
+    const readModel = pipe(
+      [
+        listCreated(listId, arbitraryString(), arbitraryString(), LOID.fromUserId(userId)),
+        articleAddedToList(articleId, listId),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it.failing('the article id is listed against the user id', () => {
+      expect(readModel[userId]).toBe([articleId]);
+    });
   });
 
   describe('when the user has added and removed an article', () => {
