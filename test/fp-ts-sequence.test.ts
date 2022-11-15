e211e12c167de7f4af52864fb6e8fa740ca48e33
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-15 17:00:31 +0000

    Compare with sequencing arrays

--- test/fp-ts-sequence.test.ts
+++ test/fp-ts-sequence.test.ts
@@ -1,4 +1,5 @@
 import * as E from 'fp-ts/Either';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
@@ -22,7 +23,17 @@ describe('fp-ts-sequence', () => {
     });
   });
 
-  describe('sequence', () => {
+  describe('.sequence', () => {
+    it('takes out a Task from inside a ReadonlyArray', async () => {
+      const result = pipe(
+        [T.of(42)],
+        RA.sequence(T.ApplicativeSeq),
+        T.map(RA.map((value) => `the number is ${value}`)),
+      );
+
+      expect(await result()).toStrictEqual(['the number is 42']);
+    });
+
     it('takes out a Task from inside an Either', async () => {
       const result = pipe(
         E.right(T.of(42)),
