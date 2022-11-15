d912afc748b60076461db5ead7a241b4ec926da7
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-15 16:57:24 +0000

    Compare fold and sequence

--- test/fp-ts-sequence.test.ts
+++ test/fp-ts-sequence.test.ts
@@ -1,9 +1,36 @@
 import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+
 describe('fp-ts-sequence', () => {
-    it('takes out a Task from inside an Either', () => {
+  describe('folding', () => {
+    it('takes out a Task from inside an Either', async () => {
       const result = pipe(
         E.right(T.of(42)),
-      )
-      expect(await result()).toStrictEqual(E.right(42));
+        E.foldW(
+          (left) => TE.left(left),
+          (right) => pipe(
+            right,
+            T.map((value) => `the number is ${value}`),
+            TE.rightTask,
+          ),
+        ),
+      );
+
+      expect(await result()).toStrictEqual(E.right('the number is 42'));
     });
-})
+  });
+
+  describe('sequence', () => {
+    it('takes out a Task from inside an Either', async () => {
+      const result = pipe(
+        E.right(T.of(42)),
+        E.sequence(T.ApplicativeSeq),
+        TE.map((value) => `the number is ${value}`),
+      );
+
+      expect(await result()).toStrictEqual(E.right('the number is 42'));
+    });
+  });
+});
