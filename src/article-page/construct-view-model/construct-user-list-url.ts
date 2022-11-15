6f047ceb7f14ebb7092994d5ef5f2bd5e4cf762e
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-15 11:57:45 +0000

    Simplify pipe thanks to new query shape #1888

--- src/article-page/construct-view-model/construct-user-list-url.ts
+++ src/article-page/construct-view-model/construct-user-list-url.ts
@@ -1,7 +1,6 @@
 import * as O from 'fp-ts/Option';
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
-import { constant, pipe } from 'fp-ts/function';
+import { pipe } from 'fp-ts/function';
 import { IsArticleOnTheListOwnedBy } from '../../shared-ports';
 import { Doi } from '../../types/doi';
 import { User } from '../../types/user';
@@ -15,15 +14,11 @@ export const constructUserListUrl = (ports: Ports) => (
   user: O.Option<User>,
 ): TE.TaskEither<never, O.Option<string>> => pipe(
   user,
-  O.fold(
-    constant(T.of(O.none)),
-    (u) => pipe(
-      doi,
-      ports.isArticleOnTheListOwnedBy(u.id),
-      T.of,
-      T.map(O.guard),
-      T.map(O.map(() => `/users/${u.id}/lists`)),
-    ),
-  ),
-  TE.rightTask,
+  O.chain((u) => pipe(
+    doi,
+    ports.isArticleOnTheListOwnedBy(u.id),
+    O.guard,
+    O.map(() => `/users/${u.id}/lists`),
+  )),
+  TE.right,
 );
