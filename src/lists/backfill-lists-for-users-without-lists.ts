7daa0e8ae7ede5527cca84d816677abe3eeb82a6
Kevin Rutherford <kevin@rutherford-software.com>
2022-11-17 15:48:26 +0000

    Implement and disable backfilling #1891

--- src/lists/backfill-lists-for-users-without-lists.ts
+++ src/lists/backfill-lists-for-users-without-lists.ts
@@ -2,9 +2,10 @@ import * as RA from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
 import { executeCreateListCommand } from './execute-create-list-command';
-import { DomainEvent } from '../domain-events';
+import { DomainEvent, isUserCreatedAccountEvent } from '../domain-events';
 import { constructCommand } from '../policies/create-user-saved-articles-list-as-generic-list';
 import { CommitEvents, Logger, SelectAllListsOwnedBy } from '../shared-ports';
+import * as LOID from '../types/list-owner-id';
 import { User } from '../types/user';
 
 type Queries = {
@@ -19,7 +20,17 @@ type Ports = {
 type DetermineUsersInNeedOfLists = (ports: Queries) => (events: ReadonlyArray<DomainEvent>) => ReadonlyArray<User>;
 
 // ts-unused-exports:disable-next-line
-export const determineUsersInNeedOfLists: DetermineUsersInNeedOfLists = () => () => [];
+export const determineUsersInNeedOfLists: DetermineUsersInNeedOfLists = (ports) => (events) => pipe(
+  events,
+  RA.filter(isUserCreatedAccountEvent),
+  RA.filter((event) => ports.selectAllListsOwnedBy(LOID.fromUserId(event.userId)).length === 0),
+  RA.map((event) => ({
+    id: event.userId,
+    handle: event.handle,
+    avatarUrl: event.avatarUrl,
+  })),
+  () => [],
+);
 
 const createListEvent = (user: User) => pipe(
   constructCommand({ userId: user.id, handle: user.handle }),
