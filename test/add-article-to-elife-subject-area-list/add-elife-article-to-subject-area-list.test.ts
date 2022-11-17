28d69273bb2a2db24436e5f2b15d9dbaa3fa4fdb
LinaKind <l.kinduryte@elifesciences.org>
2022-11-17 10:09:47 +0000

    Write initial test list #1892

---
+++ test/add-article-to-elife-subject-area-list/add-elife-article-to-subject-area-list.test.ts
@@ -0,0 +1,9 @@
+describe('add-elife-article-to-subject-area-list', () => {
+  describe('when there is work to do', () => {
+    it.todo('invokes addArticleToList command');
+  });
+
+  describe('when there is no work to do', () => {
+    it.todo('does not invoke a command');
+  });
+});
