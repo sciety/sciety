6c6746ae61ba12ed95d4eacb7df1d86d0ba632ff
LinaKind <l.kinduryte@elifesciences.org>
2022-11-17 11:13:58 +0000

    Write a failing test for query #1892

---
+++ test/add-article-to-elife-subject-area-list/read-model/get-one-article-id-in-evaluated-and-subject-area-known-state.test.ts
@@ -0,0 +1,36 @@
+import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import {
+  elifeGroupId, handleEvent, initialState, ReadModel,
+} from '../../../src/add-article-to-elife-subject-area-list/read-model';
+import { evaluationRecorded } from '../../../src/domain-events/evaluation-recorded-event';
+import { subjectAreaRecorded } from '../../../src/domain-events/subject-area-recorded-event';
+import { arbitraryArticleId } from '../../types/article-id.helper';
+import { arbitraryReviewId } from '../../types/review-id.helper';
+import { arbitrarySubjectArea } from '../../types/subject-area.helper';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const getOneArticleIdInEvaluatedAndSubjectAreaKnownState = (readModel: ReadModel) => () => O.none;
+
+describe('get-one-article-id-in-evaluated-and-subject-area-known-state', () => {
+  describe('given a bunch of events', () => {
+    const articleIdA = arbitraryArticleId();
+    const subjectArea = arbitrarySubjectArea();
+
+    const readModel = pipe(
+      [
+        evaluationRecorded(elifeGroupId, articleIdA, arbitraryReviewId()),
+        subjectAreaRecorded(articleIdA, subjectArea),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it.failing('returns one article', () => {
+      expect(getOneArticleIdInEvaluatedAndSubjectAreaKnownState(readModel)()).toStrictEqual(O.some({
+        articleId: articleIdA,
+        subjectArea,
+      }));
+    });
+  });
+});
