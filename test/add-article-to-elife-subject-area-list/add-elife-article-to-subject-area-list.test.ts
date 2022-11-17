63011cb8d87a4b222c10d2ac3913f4a8e3c20fcb
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-17 10:21:24 +0000

    Write first failing test #1892

--- test/add-article-to-elife-subject-area-list/add-elife-article-to-subject-area-list.test.ts
+++ test/add-article-to-elife-subject-area-list/add-elife-article-to-subject-area-list.test.ts
@@ -1,6 +1,31 @@
+import { AddArticleToList } from '../../src/shared-ports';
+import { arbitraryArticleId } from '../types/article-id.helper';
+import { arbitraryListId } from '../types/list-id.helper';
+
+type Ports = {
+  addArticleToList: AddArticleToList,
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const addELifeArticleToSubjectAreaList = async (adapters: Ports): Promise<void> => {
+};
+
 describe('add-elife-article-to-subject-area-list', () => {
   describe('when there is work to do', () => {
-    it.todo('invokes addArticleToList command');
+    const adapters = {
+      addArticleToList: jest.fn(),
+    };
+
+    beforeAll(async () => {
+      await addELifeArticleToSubjectAreaList(adapters);
+    });
+
+    it.failing('invokes addArticleToList command', () => {
+      expect(adapters.addArticleToList).toHaveBeenCalledWith({
+        articleId: arbitraryArticleId(),
+        listId: arbitraryListId(),
+      });
+    });
   });
 
   describe('when there is no work to do', () => {
