14807ed6c706b1220218d52f71fde4262299ebf2
LinaKind <l.kinduryte@elifesciences.org>
2022-11-17 10:44:59 +0000

    Specify where listId comes from #1892

--- test/add-article-to-elife-subject-area-list/add-elife-article-to-subject-area-list.test.ts
+++ test/add-article-to-elife-subject-area-list/add-elife-article-to-subject-area-list.test.ts
@@ -1,9 +1,9 @@
 import * as O from 'fp-ts/Option';
+import { getCorrespondingListId } from '../../src/add-article-to-elife-subject-area-list/read-model';
 import { AddArticleToList } from '../../src/shared-ports';
 import { Doi } from '../../src/types/doi';
 import { SubjectArea } from '../../src/types/subject-area';
 import { arbitraryArticleId } from '../types/article-id.helper';
-import { arbitraryListId } from '../types/list-id.helper';
 import { arbitrarySubjectArea } from '../types/subject-area.helper';
 
 type GetOneArticleInEvaluatedAndSubjectAreaKnownState = () => O.Option<{
@@ -23,11 +23,13 @@ const addELifeArticleToSubjectAreaList = async (adapters: Ports): Promise<void> 
 describe('add-elife-article-to-subject-area-list', () => {
   describe('when there is work to do', () => {
     const articleId = arbitraryArticleId();
+    const knownSubjectAreaValue = 'neuroscience';
+    const listId = getCorrespondingListId(knownSubjectAreaValue);
     const adapters = {
       addArticleToList: jest.fn(),
       getOneArticleIdInEvaluatedAndSubjectAreaKnownState: () => O.some({
         articleId,
-        subjectArea: arbitrarySubjectArea('neuroscience'),
+        subjectArea: arbitrarySubjectArea(knownSubjectAreaValue),
       }),
     };
 
@@ -38,7 +40,7 @@ describe('add-elife-article-to-subject-area-list', () => {
     it.failing('invokes addArticleToList command', () => {
       expect(adapters.addArticleToList).toHaveBeenCalledWith({
         articleId,
-        listId: arbitraryListId(),
+        listId,
       });
     });
   });
