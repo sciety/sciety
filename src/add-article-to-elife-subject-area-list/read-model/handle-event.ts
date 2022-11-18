a33124133fd32f431860d5b35bd5a035e78d658a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-11-18 16:50:07 +0000

    Move initial state and transitions closer together

--- src/add-article-to-elife-subject-area-list/read-model/handle-event.ts
+++ src/add-article-to-elife-subject-area-list/read-model/handle-event.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable quote-props */
 /* eslint-disable no-param-reassign */
+import * as O from 'fp-ts/Option';
+import * as R from 'fp-ts/Record';
 import { pipe } from 'fp-ts/function';
 import { ADT, match } from 'ts-adt';
 import { elifeGroupId, elifeSubjectAreaLists } from './data';
@@ -34,28 +36,25 @@ export type ArticleStateWithSubjectArea =
 | SubjectAreaKnown
 | EvaluatedAndSubjectAreaKnown;
 
-const transition = (state: ArticleState): ArticleState => pipe(
-  state,
-  match({
-    'subject-area-known': (s) => ({ _type: 'evaluated-and-subject-area-known' as const, subjectArea: s.subjectArea }),
-    'evaluated': (s) => s,
-    'evaluated-and-subject-area-known': (s) => s,
-    'listed': (s) => s,
-  }),
-);
-
 export const initialState = (): ReadModel => ({});
 
 export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel => {
   if (isEvaluationRecordedEvent(event)) {
     if (event.groupId === elifeGroupId) {
       const key = event.articleId.value;
-      const currentState = readmodel[key];
-      if (currentState === undefined) {
-        readmodel[key] = { _type: 'evaluated' };
-      } else {
-        readmodel[key] = transition(currentState);
-      }
+      readmodel[key] = pipe(
+        readmodel,
+        R.lookup(key),
+        O.fold(
+          () => ({ _type: 'evaluated' }),
+          match({
+            'subject-area-known': (s) => ({ _type: 'evaluated-and-subject-area-known' as const, subjectArea: s.subjectArea }),
+            'evaluated': (s) => s,
+            'evaluated-and-subject-area-known': (s) => s,
+            'listed': (s) => s,
+          }),
+        ),
+      );
     }
   } else if (isArticleAddedToListEvent(event)) {
     if (elifeSubjectAreaLists.includes(event.listId)) {
