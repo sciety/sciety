aee6b399287a3bc56e8bb4efca6a51f6242b2900
LinaKind <l.kinduryte@elifesciences.org>
2022-11-18 15:51:37 +0000

    Update home page evaluation cards

--- src/home-page/cards/hardcoded-evaluation-cards.ts
+++ src/home-page/cards/hardcoded-evaluation-cards.ts
@@ -1,26 +1,26 @@
 export const card1 = {
-  articleId: '10.1101/2022.10.01.510447',
-  articleTitle: 'Modulation of tooth regeneration through opposing responses to Wnt and BMP signals in teleosts',
-  date: new Date('2022-10-02'),
+  articleId: '10.1101/2022.10.31.514445',
+  articleTitle: 'How do Vampires Suck Blood?',
+  date: new Date('2022-11-09'),
   groupSlug: 'arcadia-science',
   groupName: 'Arcadia Science',
   avatarPath: '/static/groups/arcadia-science--bc1f956b-12e8-4f5c-aadc-70f91347bd18.png',
 };
 
 export const card2 = {
-  articleId: '10.1101/2022.06.26.496535',
-  articleTitle: 'Position representations of moving objects align with real-time position in the early visual response',
-  date: new Date('2022-06-29'),
-  groupSlug: 'elife',
-  groupName: 'eLife',
-  avatarPath: '/static/groups/elife--b560187e-f2fb-4ff9-a861-a204f3fc0fb0.png',
+  articleId: '10.1101/2022.09.02.22279345',
+  articleTitle: 'Online training in manuscript peer review: a systematic review',
+  date: new Date('2022-11-07'),
+  groupSlug: 'prereview',
+  groupName: 'PREreview',
+  avatarPath: '/static/groups/prereview-community--10360d97-bf52-4aef-b2fa-2f60d319edd7.jpg',
 };
 
 export const card3 = {
-  articleId: '10.1101/2021.07.05.451165',
-  articleTitle: 'Message in a Bottle – Metabarcoding Enables Biodiversity Comparisons Across Ecoregions',
-  date: new Date('2021-07-06'),
-  groupSlug: 'gigascience',
-  groupName: 'GigaScience',
-  avatarPath: '/static/groups/gigascience--d6e1a913-76f8-40dc-9074-8eac033e1bc8.png',
+  articleId: '10.1101/2021.12.09.471922',
+  articleTitle: 'A striatal circuit balances learned fear in the presence and absence of sensory cues',
+  date: new Date('2021-11-08'),
+  groupSlug: 'elife',
+  groupName: 'eLife',
+  avatarPath: '/static/groups/elife--b560187e-f2fb-4ff9-a861-a204f3fc0fb0.png',
 };
