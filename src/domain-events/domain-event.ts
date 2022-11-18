3dccd5bd0d2f9631cebb95f8e6d473a72a9bae6d
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-18 08:57:45 +0000

    Derive DomainEvent from codec #1893
    Side-effect is the UserCreatedAccountEvent type is temporarily unused outside of its file.

--- src/domain-events/domain-event.ts
+++ src/domain-events/domain-event.ts
@@ -4,22 +4,22 @@ import * as Ord from 'fp-ts/Ord';
 import { pipe } from 'fp-ts/function';
 import * as S from 'fp-ts/string';
 import * as t from 'io-ts';
-import { AnnotationCreatedEvent, annotationCreatedEventCodec } from './annotation-created-event';
-import { ArticleAddedToListEvent, articleAddedToListEventCodec } from './article-added-to-list-event';
-import { ArticleRemovedFromListEvent, articleRemovedFromListEventCodec } from './article-removed-from-list-event';
-import { EvaluationRecordedEvent, evaluationRecordedEventCodec } from './evaluation-recorded-event';
-import { GroupJoinedEvent, groupJoinedEventCodec } from './group-joined-event';
-import { ListCreatedEvent, listCreatedEventCodec } from './list-created-event';
-import { SubjectAreaRecordedEvent, subjectAreaRecordedEventCodec } from './subject-area-recorded-event';
-import { UserCreatedAccountEvent, userCreatedAccountEventCodec } from './user-created-account-event';
-import { UserFollowedEditorialCommunityEvent, userFollowedEditorialCommunityEventCodec } from './user-followed-editorial-community-event';
-import { UserFoundReviewHelpfulEvent, userFoundReviewHelpfulEventCodec } from './user-found-review-helpful-event';
-import { UserFoundReviewNotHelpfulEvent, userFoundReviewNotHelpfulEventCodec } from './user-found-review-not-helpful-event';
-import { UserRevokedFindingReviewHelpfulEvent, userRevokedFindingReviewHelpfulEventCodec } from './user-revoked-finding-review-helpful-event';
-import { UserRevokedFindingReviewNotHelpfulEvent, userRevokedFindingReviewNotHelpfulEventCodec } from './user-revoked-finding-review-not-helpful-event';
-import { UserSavedArticleEvent, userSavedArticleEventCodec } from './user-saved-article-event';
-import { UserUnfollowedEditorialCommunityEvent, userUnfollowedEditorialCommunityEventCodec } from './user-unfollowed-editorial-community-event';
-import { UserUnsavedArticleEvent, userUnsavedArticleEventCodec } from './user-unsaved-article-event';
+import { annotationCreatedEventCodec } from './annotation-created-event';
+import { articleAddedToListEventCodec } from './article-added-to-list-event';
+import { articleRemovedFromListEventCodec } from './article-removed-from-list-event';
+import { evaluationRecordedEventCodec } from './evaluation-recorded-event';
+import { groupJoinedEventCodec } from './group-joined-event';
+import { listCreatedEventCodec } from './list-created-event';
+import { subjectAreaRecordedEventCodec } from './subject-area-recorded-event';
+import { userCreatedAccountEventCodec } from './user-created-account-event';
+import { userFollowedEditorialCommunityEventCodec } from './user-followed-editorial-community-event';
+import { userFoundReviewHelpfulEventCodec } from './user-found-review-helpful-event';
+import { userFoundReviewNotHelpfulEventCodec } from './user-found-review-not-helpful-event';
+import { userRevokedFindingReviewHelpfulEventCodec } from './user-revoked-finding-review-helpful-event';
+import { userRevokedFindingReviewNotHelpfulEventCodec } from './user-revoked-finding-review-not-helpful-event';
+import { userSavedArticleEventCodec } from './user-saved-article-event';
+import { userUnfollowedEditorialCommunityEventCodec } from './user-unfollowed-editorial-community-event';
+import { userUnsavedArticleEventCodec } from './user-unsaved-article-event';
 
 export const domainEventCodec = t.union([
   annotationCreatedEventCodec,
@@ -40,23 +40,7 @@ export const domainEventCodec = t.union([
   userUnsavedArticleEventCodec,
 ], 'type');
 
-export type DomainEvent =
-  AnnotationCreatedEvent |
-  ArticleAddedToListEvent |
-  ArticleRemovedFromListEvent |
-  SubjectAreaRecordedEvent |
-  GroupJoinedEvent |
-  EvaluationRecordedEvent |
-  ListCreatedEvent |
-  UserSavedArticleEvent |
-  UserUnsavedArticleEvent |
-  UserFollowedEditorialCommunityEvent |
-  UserUnfollowedEditorialCommunityEvent |
-  UserFoundReviewHelpfulEvent |
-  UserRevokedFindingReviewHelpfulEvent |
-  UserFoundReviewNotHelpfulEvent |
-  UserRevokedFindingReviewNotHelpfulEvent |
-  UserCreatedAccountEvent;
+export type DomainEvent = t.TypeOf<typeof domainEventCodec>;
 
 const byDate: Ord.Ord<DomainEvent> = pipe(
   D.Ord,
