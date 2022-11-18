03bc92ad138b33603ba50164fa8914102571833c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-11-18 10:08:20 +0000

    Remove no longer needed policy as all users now have a list #1891

--- src/policies/create-user-saved-articles-list-as-generic-list.ts
+++ src/policies/create-user-saved-articles-list-as-generic-list.ts
@@ -1,70 +1,9 @@
-import * as RA from 'fp-ts/ReadonlyArray';
-import * as T from 'fp-ts/Task';
-import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
 import { CreateListCommand } from '../commands';
-import { DomainEvent } from '../domain-events';
-import { isUserSavedArticleEvent, UserSavedArticleEvent } from '../domain-events/user-saved-article-event';
-import { Logger } from '../shared-ports';
-import { CreateList } from '../shared-ports/create-list';
-import { GetUserDetails } from '../shared-ports/get-user-details';
-import * as DE from '../types/data-error';
-import { ListId } from '../types/list-id';
 import * as LOID from '../types/list-owner-id';
 import { UserId } from '../types/user-id';
-
-// ts-unused-exports:disable-next-line
-export type Ports = {
-  createList: CreateList,
-  getUserDetails: GetUserDetails,
-  getListsOwnedBy: (ownerId: LOID.ListOwnerId) => TE.TaskEither<DE.DataError, ReadonlyArray<{ id: ListId }>>,
-  logger: Logger,
-};
-
-const filterOutUsersWithGenericLists = (getListsOwnedBy: Ports['getListsOwnedBy']) => (userSavedArticleEvent: UserSavedArticleEvent) => pipe(
-  userSavedArticleEvent.userId,
-  LOID.fromUserId,
-  getListsOwnedBy,
-  TE.filterOrElseW(RA.isEmpty, () => 'user already owns a list' as const),
-);
 
 export const constructCommand = (userDetails: { userId: UserId, handle: string }): CreateListCommand => ({
   ownerId: LOID.fromUserId(userDetails.userId),
   name: `@${userDetails.handle}'s saved articles`,
   description: `Articles that have been saved by @${userDetails.handle}`,
 });
-
-type CreateUserSavedArticlesListAsGenericList = (ports: Ports) => (event: DomainEvent) => T.Task<undefined>;
-
-export const createUserSavedArticlesListAsGenericList: CreateUserSavedArticlesListAsGenericList = (
-  ports,
-) => (event) => pipe(
-  event,
-  TE.fromPredicate(isUserSavedArticleEvent, () => 'event not of interest' as const),
-  TE.chainFirstW(filterOutUsersWithGenericLists(ports.getListsOwnedBy)),
-  TE.map(({ userId }) => userId),
-  TE.chainW(ports.getUserDetails),
-  TE.map(constructCommand),
-  TE.chainW(ports.createList),
-  TE.match(
-    (reason) => {
-      switch (reason) {
-        case 'user already owns a list':
-          ports.logger('debug', 'createUserSavedArticlesListAsGenericList policy produced no action', { reason, event });
-          break;
-        case 'event not of interest':
-          break;
-        case DE.notFound:
-          ports.logger('error', 'createUserSavedArticlesListAsGenericList policy failed', { reason, event });
-          break;
-        case DE.unavailable:
-          ports.logger('error', 'createUserSavedArticlesListAsGenericList policy failed', { reason, event });
-          break;
-        default:
-          ports.logger('error', 'createUserSavedArticlesListAsGenericList policy failed', { reason, event });
-      }
-      return undefined;
-    },
-    () => undefined,
-  ),
-);
--- src/policies/execute-policies.ts
+++ src/policies/execute-policies.ts
@@ -2,7 +2,6 @@ import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
 import { addArticleToElifeSubjectAreaLists, Ports as AddArticleToElifeSubjectAreaListsPorts } from './add-article-to-elife-subject-area-lists';
 import { Ports as AddArticleToEvaluatedArticlePorts, addArticleToEvaluatedArticlesList } from './add-article-to-evaluated-articles-list';
-import { createUserSavedArticlesListAsGenericList, Ports as CreateUserSavedArticlesListAsGenericListPorts } from './create-user-saved-articles-list-as-generic-list';
 import { replicateUserSavedArticlesListAsGenericList, Ports as ReplicateUserSavedArticlesListAsGenericListPorts } from './replicate-user-saved-articles-list-as-generic-list';
 import {
   DomainEvent,
@@ -10,7 +9,6 @@ import {
 
 type PoliciesPorts = AddArticleToEvaluatedArticlePorts
 & AddArticleToElifeSubjectAreaListsPorts
-& CreateUserSavedArticlesListAsGenericListPorts
 & ReplicateUserSavedArticlesListAsGenericListPorts;
 
 type ExecutePolicies = (ports: PoliciesPorts) => (event: DomainEvent) => T.Task<void>;
@@ -19,7 +17,6 @@ export const executePolicies: ExecutePolicies = (ports) => (event) => pipe(
   [
     addArticleToEvaluatedArticlesList(ports)(event),
     addArticleToElifeSubjectAreaLists(ports)(event),
-    createUserSavedArticlesListAsGenericList(ports)(event),
     replicateUserSavedArticlesListAsGenericList(ports)(event),
   ],
   T.sequenceSeqArray,
