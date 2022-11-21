67d9d14735663f7eccefd9e06c63e15e3f1ba3f4
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-21 11:35:56 +0000

    Update and flatten existing tests to drive out both page and query #1896

--- test/user-page/user-list-card/user-list-card.test.ts
+++ test/user-page/user-list-card/user-list-card.test.ts
@@ -1,7 +1,6 @@
 import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
 import { JSDOM } from 'jsdom';
-import { userSavedArticle } from '../../../src/domain-events';
 import { userListCard } from '../../../src/user-page/user-list-card';
 import { arbitraryWord } from '../../helpers';
 import { arbitraryArticleId } from '../../types/article-id.helper';
@@ -16,6 +15,12 @@ const arbitraryList = () => ({
 });
 
 describe('user-list-card', () => {
+  const list = {
+    ...arbitraryList(),
+    articleIds: [arbitraryArticleId().value, arbitraryArticleId().value],
+    lastUpdated: new Date('2021-07-23'),
+  };
+
   it('displays the title of the list', async () => {
     const rendered = await pipe(
       userListCard(T.of([]))(arbitraryWord(), arbitraryUserId(), arbitraryList()),
@@ -36,66 +41,27 @@ describe('user-list-card', () => {
     expect(description?.textContent).toContain(handle);
   });
 
-  describe('when list contains articles', () => {
-    it('displays when the list was last updated', async () => {
-      const handle = arbitraryWord();
-      const userId = arbitraryUserId();
-      const events = [
-        userSavedArticle(userId, arbitraryArticleId(), new Date('2021-01-01')),
-        userSavedArticle(userId, arbitraryArticleId(), new Date('2021-07-23')),
-      ];
-      const rendered = await pipe(
-        userListCard(T.of(events))(handle, userId, arbitraryList()),
-        T.map(JSDOM.fragment),
-      )();
-      const meta = rendered.querySelector('.list-card__meta');
-
-      expect(meta?.textContent).toContain('Last updated Jul 23, 2021');
-    });
-
-    it('displays the number of articles in the list', async () => {
-      const handle = arbitraryWord();
-      const userId = arbitraryUserId();
-      const list = {
-        ...arbitraryList(),
-        articleIds: [arbitraryArticleId().value, arbitraryArticleId().value],
-      };
-      const rendered = await pipe(
-        userListCard(T.of([]))(handle, userId, list),
-        T.map(JSDOM.fragment),
-      )();
-      const meta = rendered.querySelector('.list-card__meta');
-
-      expect(meta?.textContent).toContain('2 articles');
-    });
-  });
-
-  describe('when list is empty', () => {
+  it.failing('displays when the list was last updated', async () => {
     const handle = arbitraryWord();
     const userId = arbitraryUserId();
+    const rendered = await pipe(
+      userListCard(T.of([]))(handle, userId, list),
+      T.map(JSDOM.fragment),
+    )();
+    const meta = rendered.querySelector('.list-card__meta');
 
-    it('does not display last updated date', async () => {
-      const rendered = await pipe(
-        userListCard(T.of([]))(handle, userId, arbitraryList()),
-        T.map(JSDOM.fragment),
-      )();
-      const meta = rendered.querySelector('.list-card__meta');
+    expect(meta?.textContent).toContain('Last updated Jul 23, 2021');
+  });
 
-      expect(meta?.textContent).not.toContain('Last updated');
-    });
+  it('displays the number of articles in the list', async () => {
+    const handle = arbitraryWord();
+    const userId = arbitraryUserId();
+    const rendered = await pipe(
+      userListCard(T.of([]))(handle, userId, list),
+      T.map(JSDOM.fragment),
+    )();
+    const meta = rendered.querySelector('.list-card__meta');
 
-    it('displays an article count of 0', async () => {
-      const list = {
-        ...arbitraryList(),
-        articleIds: [],
-      };
-      const rendered = await pipe(
-        userListCard(T.of([]))(handle, userId, list),
-        T.map(JSDOM.fragment),
-      )();
-      const meta = rendered.querySelector('.list-card__meta');
-
-      expect(meta?.textContent).toContain('0 articles');
-    });
+    expect(meta?.textContent).toContain('2 articles');
   });
 });
