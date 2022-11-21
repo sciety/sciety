1e076126e88d1f95aa59e65218f31305288c4424
LinaKind <l.kinduryte@elifesciences.org>
2022-11-21 11:31:10 +0000

    Remove obsolete policy #1892

--- src/policies/execute-policies.ts
+++ src/policies/execute-policies.ts
@@ -1,6 +1,5 @@
 import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
-import { addArticleToElifeSubjectAreaLists, Ports as AddArticleToElifeSubjectAreaListsPorts } from './add-article-to-elife-subject-area-lists';
 import { Ports as AddArticleToEvaluatedArticlePorts, addArticleToEvaluatedArticlesList } from './add-article-to-evaluated-articles-list';
 import { replicateUserSavedArticlesListAsGenericList, Ports as ReplicateUserSavedArticlesListAsGenericListPorts } from './replicate-user-saved-articles-list-as-generic-list';
 import {
@@ -8,7 +7,6 @@ import {
 } from '../domain-events';
 
 type PoliciesPorts = AddArticleToEvaluatedArticlePorts
-& AddArticleToElifeSubjectAreaListsPorts
 & ReplicateUserSavedArticlesListAsGenericListPorts;
 
 type ExecutePolicies = (ports: PoliciesPorts) => (event: DomainEvent) => T.Task<void>;
@@ -16,7 +14,6 @@ type ExecutePolicies = (ports: PoliciesPorts) => (event: DomainEvent) => T.Task<
 export const executePolicies: ExecutePolicies = (ports) => (event) => pipe(
   [
     addArticleToEvaluatedArticlesList(ports)(event),
-    addArticleToElifeSubjectAreaLists(ports)(event),
     replicateUserSavedArticlesListAsGenericList(ports)(event),
   ],
   T.sequenceSeqArray,
