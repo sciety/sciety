90274e6a27b34c4e1f37f6799c86edda69536949
Giorgio Sironi <g.sironi@elifesciences.org>
2022-11-22 11:06:53 +0000

    Extract function from renderer #1898

--- src/shared-components/list-card/render-list-card.ts
+++ src/shared-components/list-card/render-list-card.ts
@@ -18,9 +18,16 @@ const lastUpdated = O.fold(
   (date: Date) => `<span>Last updated ${templateDate(date)}</span>`,
 );
 
+type ListPageLinkHrefViewModel = {
+  listId: ListId,
+  userHandle?: string,
+};
+
+const renderListPageLinkHref = (viewModel: ListPageLinkHrefViewModel) => (viewModel.userHandle ? `/users/${viewModel.userHandle}/lists/saved-articles` : `/lists/${viewModel.listId}`);
+
 export const renderListCard = (viewModel: ListCardViewModel): HtmlFragment => toHtmlFragment(`
   <article class="list-card">
-      <h3 class="list-card__title"><a href="${viewModel.userHandle ? `/users/${viewModel.userHandle}/lists/saved-articles` : `/lists/${viewModel.listId}`}" class="list-card__link">${viewModel.title}</a></h3>
+      <h3 class="list-card__title"><a href="${renderListPageLinkHref({ listId: viewModel.listId, userHandle: viewModel.userHandle })}" class="list-card__link">${viewModel.title}</a></h3>
       <p>${viewModel.description}</p>
       <div class="list-card__meta">
         <span class="visually-hidden">${viewModel.articleCountLabel} </span><span>${viewModel.articleCount} article${viewModel.articleCount === 1 ? '' : 's'}</span>${lastUpdated(viewModel.lastUpdated)}
