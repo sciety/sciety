25cd0be52362af2f348a7167200b9beede4584ac
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-11-25 15:16:28 +0000

    remove unused shared port

--- src/shared-ports/get-lists-owned-by.ts
+++
@@ -1,7 +0,0 @@
-import * as TE from 'fp-ts/TaskEither';
-import { List } from '../shared-read-models/lists';
-import * as DE from '../types/data-error';
-import { ListOwnerId } from '../types/list-owner-id';
-
-// ts-unused-exports:disable-next-line
-export type GetListsOwnedBy = (ownerId: ListOwnerId) => TE.TaskEither<DE.DataError, ReadonlyArray<List>>;
