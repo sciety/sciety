03e382ea7e4c86cc81c53a0d1ea3ff6e5eac925c
Kevin Rutherford <kevin@rutherford-software.com>
2022-11-29 08:44:27 +0000

    Write a test list for execute-command #1906

---
+++ test/edit-list-details/execute-command.test.ts
@@ -0,0 +1,9 @@
+describe('execute-command', () => {
+  describe('when the new name is different from the current name', () => {
+    it.todo('raises an event with the new name');
+  });
+
+  describe('when the new name is the same as the current name', () => {
+    it.todo('raises no events');
+  });
+});
