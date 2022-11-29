7bf204282f8ed8ced49bffef3b878231056e9295
LinaKind <l.kinduryte@elifesciences.org>
2022-11-29 10:15:29 +0000

    Write a failing test #1906

--- test/edit-list-details/execute-command.test.ts
+++ test/edit-list-details/execute-command.test.ts
@@ -1,11 +1,34 @@
+import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
+import { listCreated } from '../../src/domain-events/list-created-event';
 import { executeCommand } from '../../src/edit-list-details/execute-command';
+import { replayListAggregate } from '../../src/shared-write-models/replay-list-aggregate';
 import { arbitraryString } from '../helpers';
+import { shouldNotBeCalled } from '../should-not-be-called';
 import { arbitraryListId } from '../types/list-id.helper';
+import { arbitraryListOwnerId } from '../types/list-owner-id.helper';
 
 describe('execute-command', () => {
   describe('when the new name is different from the current name', () => {
-    it.todo('raises an event with the new name');
+    const newName = arbitraryString();
+    const listId = arbitraryListId();
+    const command = {
+      name: newName,
+      listId,
+    };
+
+    const raisedEvents = pipe(
+      [
+        listCreated(listId, arbitraryString(), arbitraryString(), arbitraryListOwnerId()),
+      ],
+      replayListAggregate(listId),
+      E.map(executeCommand(command)),
+      E.getOrElseW(shouldNotBeCalled),
+    );
+
+    it.failing('raises an event with the new name', () => {
+      expect(raisedEvents).toStrictEqual([expect.objectContaining({ name: newName })]);
+    });
   });
 
   describe('when the new name is the same as the current name', () => {
