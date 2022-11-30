4baa007195ef8c52be547076db531a8f0daf9bd2
LinaKind <l.kinduryte@elifesciences.org>
2022-11-30 14:46:44 +0000

    Delete unused policy code #1906

--- src/policies/run-elife-subject-area-policy.ts
+++
@@ -1,9 +0,0 @@
-import * as T from 'fp-ts/Task';
-import { pipe } from 'fp-ts/function';
-
-const replayEventsForPolicy = () => T.of(undefined);
-
-void (async (): Promise<void> => pipe(
-  replayEventsForPolicy(),
-  T.chainFirst(() => T.of(process.stdout.write('Completed\n'))),
-)())();
