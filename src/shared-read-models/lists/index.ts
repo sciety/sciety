59082db81201af8f781196c1d622b97626781bd8
LinaKind <l.kinduryte@elifesciences.org>
2022-11-30 11:36:42 +0000

    Remove legacy read model #1906

--- src/shared-read-models/lists/index.ts
+++
@@ -1,6 +0,0 @@
-// ts-unused-exports:disable-next-line
-export { List } from './list';
-// ts-unused-exports:disable-next-line
-export { selectAllListsOwnedBy } from './select-all-lists-owned-by';
-// ts-unused-exports:disable-next-line
-export { getList } from './get-list';
