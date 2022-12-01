665c7e38af8eaf17acf4bcde2495668e7595b888
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-12-01 10:55:20 +0000

    Remove unused query #1907

--- src/shared-read-models/groups/index.ts
+++ src/shared-read-models/groups/index.ts
@@ -1,4 +1,2 @@
 export { getAllGroups } from './get-all-groups';
-// ts-unused-exports:disable-next-line
-export { getGroup } from './get-group';
 export { getGroupBySlug } from './get-group-by-slug';
