a7bb5c3c51c8a11c7eb8ad429914aa202a8e4700
David Moulton <d.moulton@elifesciences.org>
2022-12-02 11:54:33 +0000

    Add a naive form implementation for editing list details #1913

--- src/edit-list-details-form-page/page.ts
+++ src/edit-list-details-form-page/page.ts
@@ -3,5 +3,20 @@ import { toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 import { RenderPageError } from '../types/render-page-error';
 
+export const editListDetailsFormPage = (
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const editListDetailsFormPage = (params: unknown): TE.TaskEither<RenderPageError, Page> => TE.right({ title: 'Edit details form', content: toHtmlFragment('<h1>My form</h1>') });
+  params: unknown,
+): TE.TaskEither<RenderPageError, Page> => TE.right({
+  title: 'Edit details form',
+  content: toHtmlFragment(`
+<h1>My form</h1>
+<form action="/forms/edit-list-details" method="post">
+  <label for="">List name</label>
+  <input type="text">
+  <label for="">Description</label>
+  <textarea name="" id="" cols="30" rows="10" placeholder="This is a description of my list. It tells you about the lists I have made."></textarea>
+  <p>Max 250 characters.</p>
+  <button>Save</button>
+</form>
+`),
+});
