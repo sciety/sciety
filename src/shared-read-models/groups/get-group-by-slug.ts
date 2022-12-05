c8728985fb96419312ea6b6e2f270592578fb64d
Kevin Rutherford <kevin@rutherford-software.com>
2022-12-05 14:46:12 +0000

    Remove unnecessary sorting #1916

--- src/shared-read-models/groups/get-group-by-slug.ts
+++ src/shared-read-models/groups/get-group-by-slug.ts
@@ -1,23 +1,14 @@
 import * as E from 'fp-ts/Either';
-import * as Ord from 'fp-ts/Ord';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import * as S from 'fp-ts/string';
 import { ReadModel } from './handle-event';
 import * as DE from '../../types/data-error';
 import { Group } from '../../types/group';
 
-const byName: Ord.Ord<Group> = pipe(
-  S.Ord,
-  Ord.contramap((group) => group.name),
-);
-
 type GetGroupBySlug = (slug: string) => E.Either<DE.DataError, Group>;
 
-// ts-unused-exports:disable-next-line
-export const getGroupBySlug = (r: ReadModel): GetGroupBySlug => (slug) => pipe(
-  Object.values(r),
-  RA.sort(byName),
+export const getGroupBySlug = (readmodel: ReadModel): GetGroupBySlug => (slug) => pipe(
+  Object.values(readmodel),
   RA.findFirst((group) => group.slug === slug),
   E.fromOption(() => DE.notFound),
 );
