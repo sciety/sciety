81d22aa4aa521bd8d37a40d5b531534f418cf5b3
Giorgio Sironi <g.sironi@elifesciences.org>
2022-12-06 15:42:47 +0000

    Write another passing test for existing functionality #1922

--- test/policies/add-article-to-evaluated-articles-list.test.ts
+++ test/policies/add-article-to-evaluated-articles-list.test.ts
@@ -6,17 +6,18 @@ import * as Gid from '../../src/types/group-id';
 import * as Lid from '../../src/types/list-id';
 import { dummyLogger } from '../dummy-logger';
 import { arbitraryArticleId } from '../types/article-id.helper';
+import { arbitraryGroupId } from '../types/group-id.helper';
 import { arbitraryReviewId } from '../types/review-id.helper';
 
 describe('add-article-to-evaluated-articles-list', () => {
+  const ports = {
+    logger: dummyLogger,
+  };
+
   describe('when the group has an evaluated articles list', () => {
     const articleId = arbitraryArticleId();
     const listId = Lid.fromValidatedString('f8459240-f79c-4bb2-bb55-b43eae25e4f6');
     const groupId = Gid.fromValidatedString('bc1f956b-12e8-4f5c-aadc-70f91347bd18');
-
-    const ports = {
-      logger: dummyLogger,
-    };
 
     const command = pipe(
       evaluationRecorded(groupId, articleId, arbitraryReviewId()),
@@ -32,6 +33,13 @@ describe('add-article-to-evaluated-articles-list', () => {
   });
 
   describe('when the group does not have an evaluated articles list', () => {
-    it.todo('returns nothing to do');
+    const command = pipe(
+      evaluationRecorded(arbitraryGroupId(), arbitraryArticleId(), arbitraryReviewId()),
+      constructCommand(ports),
+    );
+
+    it('returns nothing to do', () => {
+      expect(E.isLeft(command)).toBe(true);
+    });
   });
 });
