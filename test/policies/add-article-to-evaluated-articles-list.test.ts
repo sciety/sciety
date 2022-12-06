36147a6f59295d53e26eac3eb71ea316f82753df
Kevin Rutherford <kevin@rutherford-software.com>
2022-12-06 15:19:20 +0000

    Sketch a test list for the logic of the policy #1922

---
+++ test/policies/add-article-to-evaluated-articles-list.test.ts
@@ -0,0 +1,9 @@
+describe('add-article-to-evaluated-articles-list', () => {
+  describe('when the group has an evaluated articles list', () => {
+    it.todo('returns a command');
+  });
+
+  describe('when the group does not have an evaluated articles list', () => {
+    it.todo('returns nothing to do');
+  });
+});
