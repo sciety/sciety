7b0e82541ade9f82d2270db9d6c1719288006c6e
Giorgio Sironi <g.sironi@elifesciences.org>
2022-12-07 13:38:52 +0000

    Write passing test #1913

--- test/types/codecs/user-generated-input-codec.test.ts
+++ test/types/codecs/user-generated-input-codec.test.ts
@@ -2,9 +2,15 @@ import * as E from 'fp-ts/Either';
 import { userGeneratedInputCodec } from '../../../src/types/codecs/user-generated-input-codec';
 
 describe('user-generated-input-codec', () => {
-  const result = userGeneratedInputCodec.decode('<script>');
-
   it('fails when supplied with script tag', () => {
+    const result = userGeneratedInputCodec.decode('<script>');
+
     expect(E.isLeft(result)).toBe(true);
+  });
+
+  it('passes when supplied with a whitelisted special character', () => {
+    const result = userGeneratedInputCodec.decode('Some articles?');
+
+    expect(E.isRight(result)).toBe(true);
   });
 });
