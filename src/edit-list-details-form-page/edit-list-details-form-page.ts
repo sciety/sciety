53e975f7db5b865cbac86d21568bbe597fc7a99b
LinaKind <l.kinduryte@elifesciences.org>
2022-12-07 10:47:13 +0000

    Provide correct list id when editing list details #1913

--- src/edit-list-details-form-page/edit-list-details-form-page.ts
+++ src/edit-list-details-form-page/edit-list-details-form-page.ts
@@ -1,19 +1,19 @@
 import * as TE from 'fp-ts/TaskEither';
 import { toHtmlFragment } from '../types/html-fragment';
+import { ListId } from '../types/list-id';
 import { Page } from '../types/page';
 import { RenderPageError } from '../types/render-page-error';
 
 export const editListDetailsFormPage = (
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-  params: unknown,
+  params: { id: ListId },
 ): TE.TaskEither<RenderPageError, Page> => TE.right({
   title: 'Edit details form',
   content: toHtmlFragment(`
 <h1>My form</h1>
 <form action="/forms/edit-list-details" method="post">
+  <input type="hidden" value="${params.id}" name="listId">
   <label for="listName">List name</label>
   <input type="text" id="listName" name="name">
-  <input type="text" name="listId">
   <label for="listDescription">Description</label>
   <textarea id="listDescription" name="description" cols="30" rows="10" placeholder="This is a description of my list. It tells you about the lists I have made."></textarea>
   <p>Max 250 characters.</p>
