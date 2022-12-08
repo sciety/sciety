5a6e5d3bb7ff7dcf49c6f67da473327b6fe20e91
LinaKind <l.kinduryte@elifesciences.org>
2022-12-08 09:54:51 +0000

    Express ticket scope in test list #1925

---
+++ test/edit-list-details-form-page/construct-view-model.test.ts
@@ -0,0 +1,13 @@
+describe('construct-view-model', () => {
+  describe('when the list exists', () => {
+    it.todo('includes the list id');
+
+    it.todo('includes the current list name');
+
+    it.todo('includes the current list description');
+  });
+
+  describe('when the list does not exist', () => {
+    it.todo('returns on left');
+  });
+});
