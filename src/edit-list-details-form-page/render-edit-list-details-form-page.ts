d084f472ccb63f98097d9153fa0bc5a8c79246b7
David Moulton <d.moulton@elifesciences.org>
2022-12-09 11:26:18 +0000

    Make list description a required field when user editing #1934

--- src/edit-list-details-form-page/render-edit-list-details-form-page.ts
+++ src/edit-list-details-form-page/render-edit-list-details-form-page.ts
@@ -18,7 +18,7 @@ export const renderEditListDetailsFormPage = (viewModel: ViewModel): Page => (
   <label for="listName">List name</label>
   <input type="text" id="listName" name="name" value="${viewModel.listName}" pattern="[^<>]+" required>
   <label for="listDescription">Description</label>
-  <textarea id="listDescription" name="description" cols="30" rows="10" placeholder="This is a description of my list. It tells you about the lists I have made.">${viewModel.listDescription}</textarea>
+  <textarea id="listDescription" name="description" cols="30" rows="10" placeholder="This is a description of my list. It tells you about the lists I have made." required>${viewModel.listDescription}</textarea>
   <p>Max 250 characters.</p>
   <button>Save</button>
 </form>
