214d31716869de273c5d51ff0708523874134d8f
David Moulton <d.moulton@elifesciences.org>
2022-12-09 15:40:21 +0000

    Lay out edit list details form members in a single column #1934

--- src/edit-list-details-form-page/render-edit-list-details-form-page.ts
+++ src/edit-list-details-form-page/render-edit-list-details-form-page.ts
@@ -15,10 +15,10 @@ export const renderEditListDetailsFormPage = (viewModel: ViewModel): Page => (
 <h1>Edit list details</h1>
 <form action="/forms/edit-list-details" method="post" class="edit-list-details-form">
   <input type="hidden" value="${viewModel.listId}" name="listId">
-  <label for="listName">List name</label>
-  <input type="text" id="listName" name="name" value="${viewModel.listName}" pattern="[^<>]+" required>
-  <label for="listDescription">Description</label>
-  <textarea id="listDescription" name="description" cols="30" rows="10" placeholder="This is a description of my list. It tells you about the lists I have made." required>${viewModel.listDescription}</textarea>
+  <label for="listName" class="edit-list-details-form__label">List name</label>
+  <input type="text" id="listName" name="name" class="edit-list-details-form__field" value="${viewModel.listName}" pattern="[^<>]+" required>
+  <label for="listDescription" class="edit-list-details-form__label">Description</label>
+  <textarea id="listDescription" name="description" cols="30" rows="10" class="edit-list-details-form__field" placeholder="This is a description of my list. It tells you about the lists I have made." required>${viewModel.listDescription}</textarea>
   <p>Max 250 characters.</p>
   <a href="/lists/${viewModel.listId}">Cancel</a>
   <button>Save</button>
