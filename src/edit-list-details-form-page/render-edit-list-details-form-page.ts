cd0fff12c9a8a30d26480942872c6b96926e597a
LinaKind <l.kinduryte@elifesciences.org>
2022-12-09 11:14:38 +0000

    Add client input validation to list name field #1934

--- src/edit-list-details-form-page/render-edit-list-details-form-page.ts
+++ src/edit-list-details-form-page/render-edit-list-details-form-page.ts
@@ -16,7 +16,7 @@ export const renderEditListDetailsFormPage = (viewModel: ViewModel): Page => (
 <form action="/forms/edit-list-details" method="post">
   <input type="hidden" value="${viewModel.listId}" name="listId">
   <label for="listName">List name</label>
-  <input type="text" id="listName" name="name" value="${viewModel.listName}">
+  <input type="text" id="listName" name="name" value="${viewModel.listName}" pattern="[^<>]+" required>
   <label for="listDescription">Description</label>
   <textarea id="listDescription" name="description" cols="30" rows="10" placeholder="This is a description of my list. It tells you about the lists I have made.">${viewModel.listDescription}</textarea>
   <p>Max 250 characters.</p>
