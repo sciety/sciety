1d674993f82d6e91422261614c9c3f372ec1d7d9
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-12-09 11:44:51 +0000

    Further reduction of noise #1933

--- src/infrastructure/dispatcher.ts
+++ src/infrastructure/dispatcher.ts
@@ -5,15 +5,6 @@ import { DomainEvent } from '../domain-events';
 import * as groups from '../shared-read-models/groups';
 import * as lists from '../shared-read-models/lists';
 
-type DispatchToAllReadModels = (events: ReadonlyArray<DomainEvent>) => void;
-
-type Dispatcher = {
-  queries: elife.Queries
-  & lists.Queries
-  & groups.Queries,
-  dispatchToAllReadModels: DispatchToAllReadModels,
-};
-
 type ReadModelWithInstance<I, Q> = {
   instance: I,
   handleEvent: (state: I, event: DomainEvent) => I,
@@ -28,10 +19,22 @@ const updateInstance = (
   events: ReadonlyArray<DomainEvent>,
 ) => <I, Q>(
   readModelWithInstance: ReadModelWithInstance<I, Q>,
-): I => pipe(
+) => {
+  // eslint-disable-next-line no-param-reassign
+  readModelWithInstance.instance = pipe(
     events,
     RA.reduce(readModelWithInstance.instance, readModelWithInstance.handleEvent),
   );
+};
+
+type DispatchToAllReadModels = (events: ReadonlyArray<DomainEvent>) => void;
+
+type Dispatcher = {
+  queries: elife.Queries
+  & lists.Queries
+  & groups.Queries,
+  dispatchToAllReadModels: DispatchToAllReadModels,
+};
 
 export const dispatcher = (): Dispatcher => {
   const readModelsWithInstances = {
@@ -41,9 +44,9 @@ export const dispatcher = (): Dispatcher => {
   };
 
   const dispatchToAllReadModels: DispatchToAllReadModels = (events) => {
-    readModelsWithInstances.elife.instance = updateInstance(events)(readModelsWithInstances.elife);
-    readModelsWithInstances.lists.instance = updateInstance(events)(readModelsWithInstances.lists);
-    readModelsWithInstances.groups.instance = updateInstance(events)(readModelsWithInstances.groups);
+    updateInstance(events)(readModelsWithInstances.elife);
+    updateInstance(events)(readModelsWithInstances.lists);
+    updateInstance(events)(readModelsWithInstances.groups);
   };
 
   const queries = {
