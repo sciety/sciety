31ea365861ef9f93fe48c78079865ebbfaf0fe08
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-12-09 11:34:23 +0000

    Improve legibility #1933

--- src/infrastructure/dispatcher.ts
+++ src/infrastructure/dispatcher.ts
@@ -1,6 +1,6 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import * as addArticleToElifeSubjectAreaList from '../add-article-to-elife-subject-area-list/read-model';
+import * as elife from '../add-article-to-elife-subject-area-list/read-model';
 import { DomainEvent } from '../domain-events';
 import * as groups from '../shared-read-models/groups';
 import * as lists from '../shared-read-models/lists';
@@ -8,21 +8,21 @@ import * as lists from '../shared-read-models/lists';
 type DispatchToAllReadModels = (events: ReadonlyArray<DomainEvent>) => void;
 
 type Dispatcher = {
-  queries: addArticleToElifeSubjectAreaList.Queries
+  queries: elife.Queries
   & lists.Queries
   & groups.Queries,
   dispatchToAllReadModels: DispatchToAllReadModels,
 };
-
-const wireUpQueries = <I, Q>(
-  readModelWithInstance: ReadModelWithInstance<I, Q>,
-): Q => readModelWithInstance.queryBuilder(readModelWithInstance.instance);
 
 type ReadModelWithInstance<I, Q> = {
   instance: I,
   handleEvent: (state: I, event: DomainEvent) => I,
   queryBuilder: (instance: I) => Q,
 };
+
+const wireUpQueries = <I, Q>(
+  readModelWithInstance: ReadModelWithInstance<I, Q>,
+): Q => readModelWithInstance.queryBuilder(readModelWithInstance.instance);
 
 const updateInstance = (
   events: ReadonlyArray<DomainEvent>,
@@ -34,18 +34,10 @@ const updateInstance = (
   );
 
 export const dispatcher = (): Dispatcher => {
-  const addArticleToElifeSubjectAreaListReadModel = addArticleToElifeSubjectAreaList.initialState();
-  const listsReadModel = lists.initialState();
-  const groupsReadModel = groups.initialState();
-
   const readModelsWithInstances = {
-    elife: {
-      instance: addArticleToElifeSubjectAreaListReadModel,
-      handleEvent: addArticleToElifeSubjectAreaList.handleEvent,
-      queryBuilder: addArticleToElifeSubjectAreaList.queries,
-    },
-    lists: { instance: listsReadModel, handleEvent: lists.handleEvent, queryBuilder: lists.queries },
-    groups: { instance: groupsReadModel, handleEvent: groups.handleEvent, queryBuilder: groups.queries },
+    elife: { instance: elife.initialState(), handleEvent: elife.handleEvent, queryBuilder: elife.queries },
+    lists: { instance: lists.initialState(), handleEvent: lists.handleEvent, queryBuilder: lists.queries },
+    groups: { instance: groups.initialState(), handleEvent: groups.handleEvent, queryBuilder: groups.queries },
   };
 
   const dispatchToAllReadModels: DispatchToAllReadModels = (events) => {
