28cb9b9b76dff7a4fc4cde89a48c60e639c52ea2
Giorgio Sironi <g.sironi@elifesciences.org>
2022-12-12 15:48:40 +0000

    Write a test list for editing capability #1934

---
+++ test/html-pages/generic-list-page/headers.test.ts
@@ -0,0 +1,13 @@
+describe('headers', () => {
+  describe('when the logged in user owns the list', () => {
+    it.todo('includes editing capability');
+  });
+
+  describe('when the logged in user does not own the list', () => {
+    it.todo('does not include editing capability');
+  });
+
+  describe('when there is no logged in user', () => {
+    it.todo('does not include editing capability');
+  });
+});
