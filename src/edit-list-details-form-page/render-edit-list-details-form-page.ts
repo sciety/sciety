a3e46bb7b67cca9fbd5efa7ef065757efbd46aed
David Moulton <d.moulton@elifesciences.org>
2022-12-12 14:26:47 +0000

    Make form constraint visible to the user #1934

--- src/edit-list-details-form-page/render-edit-list-details-form-page.ts
+++ src/edit-list-details-form-page/render-edit-list-details-form-page.ts
@@ -19,9 +19,10 @@ export const renderEditListDetailsFormPage = (viewModel: ViewModel): Page => (
   <input type="hidden" value="${viewModel.listId}" name="listId">
   <label for="listName" class="edit-list-details-form__label">List name</label>
   <input type="text" id="listName" name="name" class="edit-list-details-form__field" value="${viewModel.listName}" pattern="[^<>]+" required maxlength="100">
+  <p class="edit-list-details-form__constraints">Max 100 characters.</p>
   <label for="listDescription" class="edit-list-details-form__label">Description</label>
   <textarea id="listDescription" name="description" cols="30" rows="10" class="edit-list-details-form__field edit-list-details-form__field--description" placeholder="This is a description of my list. It tells you about the lists I have made." required maxlength="250">${viewModel.listDescription}</textarea>
-  <p class="edit-list-details-form__description_constraints">Max 250 characters.</p>
+  <p class="edit-list-details-form__constraints">Max 250 characters.</p>
   <a href="/lists/${viewModel.listId}" class="edit-list-details-form__cancel">Cancel</a><button class="edit-list-details-form__save">Save</button>
 </form>
 `),
