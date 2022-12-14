cbdfb88013f3fab61b2c724782d02c36d8f08c07
David Moulton <d.moulton@elifesciences.org>
2022-12-14 10:00:09 +0000

    Begin to write a fast test for list editing capability #1940

--- test/html-pages/generic-list-page/headers.test.ts
+++ test/html-pages/generic-list-page/headers.test.ts
@@ -1,6 +1,37 @@
+import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { headers } from '../../../src/html-pages/generic-list-page/headers';
+import * as DE from '../../../src/types/data-error';
+import { UserId } from '../../../src/types/user-id';
+import { arbitraryString, arbitraryUri, arbitraryWord } from '../../helpers';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+import { arbitraryList } from '../../types/list-helper';
+
 describe('headers', () => {
   describe('when the logged in user owns the list', () => {
-    it.todo('includes editing capability');
+    it.skip('includes editing capability', async () => {
+      const ports = {
+        getAllEvents: T.of([]),
+        getGroup: () => E.left(DE.notFound),
+        getUserDetails: (userId: UserId) => TE.right({
+          avatarUrl: arbitraryUri(),
+          displayName: arbitraryString(),
+          handle: arbitraryWord(),
+          userId,
+        }),
+      };
+      const list = arbitraryList();
+      const viewModel = await pipe(
+        list,
+        headers(ports),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+
+      expect(viewModel.editCapability).toStrictEqual(O.some(list.listId));
+    });
   });
 
   describe('when the logged in user does not own the list', () => {
