98fdcc2fb322f1c1cf4eeaec46502493bbec52ed
David Moulton <d.moulton@elifesciences.org>
2022-12-15 13:56:41 +0000

    Express intention through todo tests #1941

---
+++ test/shared-read-models/get-evaluated-articles-list-id-for-group-from-hardcoded-data-and-events.test.ts
@@ -0,0 +1,13 @@
+describe('get-evaluated-articles-list-id-for-group-from-hardcoded-data-and-events', () => {
+  describe('given a group Id for which the information is hard coded', () => {
+    it.todo('returns the list Id');
+  });
+
+  describe('given a group Id for which the information is stored in an event', () => {
+    it.todo('returns the list Id');
+  });
+
+  describe('given a group Id for which no evaluated articles list is known', () => {
+    it.todo('returns nothing');
+  });
+});
