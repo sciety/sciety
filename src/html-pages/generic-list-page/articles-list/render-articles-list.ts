73e25c3975b017ebe8ead9e8e205c9f4fd7acd3c
LinaKind <l.kinduryte@elifesciences.org>
2022-12-16 14:46:02 +0000

    Remove controls html fragment from view model #1940

--- src/html-pages/generic-list-page/articles-list/render-articles-list.ts
+++ src/html-pages/generic-list-page/articles-list/render-articles-list.ts
@@ -1,14 +1,16 @@
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
-import { flow } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
 import { ArticleErrorCardViewModel, renderArticleErrorCard } from './render-article-error-card';
 import { ArticleViewModel, renderArticleCardWithControlsAndOptionalAnnotation } from '../../../shared-components/article-card';
+import { Doi } from '../../../types/doi';
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
+import { ListId } from '../../../types/list-id';
 
 export type ArticleCardWithControlsViewModel = {
   articleViewModel: ArticleViewModel,
-  controls: O.Option<HtmlFragment>,
+  controls: O.Option<{ articleId: Doi, listId: ListId }>,
   annotationContent?: HtmlFragment,
 };
 
@@ -16,11 +18,30 @@ type RenderArticlesList = (
   articleViewModels: ReadonlyArray<E.Either<ArticleErrorCardViewModel, ArticleCardWithControlsViewModel>>,
 ) => HtmlFragment;
 
+const renderRemoveArticleForm = (articleId: Doi, listId: ListId) => pipe(
+  articleId.value,
+  (id) => `<form method="post" action="/forms/remove-article-from-list">
+      <input type="hidden" name="articleid" value="${id}">
+      <input type="hidden" name="listid" value="${listId}">
+      <button aria-label="Remove this article from the list" class="saved-articles-control">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" class="saved-articles-control__icon">
+          <desc>Remove this article from the list</desc>
+          <path d="M0 0h24v24H0V0z" fill="none"/>
+          <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/>
+        </svg>
+      </button>
+    </form>`,
+  toHtmlFragment,
+);
+
 export const renderArticlesList: RenderArticlesList = flow(
   RA.map(E.fold(
     renderArticleErrorCard,
     (viewModel) => renderArticleCardWithControlsAndOptionalAnnotation(
-      viewModel.controls,
+      pipe(
+        viewModel.controls,
+        O.map(({ articleId, listId }) => renderRemoveArticleForm(articleId, listId)),
+      ),
       viewModel.annotationContent,
     )(viewModel.articleViewModel),
   )),
