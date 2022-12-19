19128da84c9ff5462f1215cc92209815db3940bf
David Moulton <d.moulton@elifesciences.org>
2022-12-19 12:00:24 +0000

    Don't overwrite already spread key #1940

--- src/html-pages/generic-list-page/page.ts
+++ src/html-pages/generic-list-page/page.ts
@@ -87,7 +87,6 @@ export const page = (ports: Ports) => (params: Params): TE.TaskEither<RenderPage
       title: list.name,
       articleCount: list.articleIds.length,
       listOwnerId: list.ownerId,
-      listId: list.listId,
       editCapability: userHasEditCapability(getLoggedInUserIdFromParam(params.user), list.ownerId),
     })),
   )),
