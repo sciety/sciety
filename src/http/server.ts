fe146840584303f11866c207365ba457e8e91de6
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-12-20 11:07:01 +0000

    Revert "Follow first steps of auth0 passport tutorial #1953"
    This reverts commit b89b4c59a49b157da2fe219dd4f5c68e69e0e6a6.

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,7 +6,6 @@ import Koa, { Middleware } from 'koa';
 import koaPassport from 'koa-passport';
 import koaSession from 'koa-session';
 import { Strategy as LocalStrategy } from 'passport-local';
-import { OpenIDConnectStrategy } from 'passport-openidconnect';
 import { Strategy as TwitterStrategy } from 'passport-twitter';
 import { routeNotFound } from './route-not-found';
 import { CollectedPorts } from '../infrastructure';
@@ -94,7 +93,7 @@ export const createApplicationServer = (router: Router, ports: CollectedPorts): 
           .then(() => cb(null, user));
       },
     ));
-  } else if (process.env.AUTHENTICATION_STRATEGY === 'twitter') {
+  } else {
     koaPassport.use(
       new TwitterStrategy(
         {
@@ -124,17 +123,6 @@ export const createApplicationServer = (router: Router, ports: CollectedPorts): 
         },
       ),
     );
-  } else {
-    koaPassport.use(new OpenIDConnectStrategy({
-      issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-      authorizationURL: `https://${process.env.AUTH0_DOMAIN}/authorize`,
-      tokenURL: `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
-      userInfoURL: `https://${process.env.AUTH0_DOMAIN}/userinfo`,
-      clientID: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      callbackURL: '/oauth2/redirect',
-      scope: ['profile'],
-    }, ((issuer, profile, cb) => cb(null, profile))));
   }
 
   app.use(koaPassport.initialize());
