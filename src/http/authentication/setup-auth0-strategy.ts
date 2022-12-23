d1af3437ea3fa94cba35667976820f07a7c43b02
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-12-23 10:22:56 +0000

    Rename and taskify for clarity #1953

--- src/http/authentication/setup-auth0-strategy.ts
+++ src/http/authentication/setup-auth0-strategy.ts
@@ -1,10 +1,13 @@
+import { sequenceS } from 'fp-ts/Apply';
+import { pipe } from 'fp-ts/lib/function';
+import * as T from 'fp-ts/Task';
 import Auth0Strategy from 'passport-auth0';
 import { fetchData } from '../../infrastructure/fetchers';
 import { CommitEvents, GetAllEvents, Logger } from '../../shared-ports';
 import { toUserId } from '../../types/user-id';
 import { createAccountIfNecessary } from '../../user-account/create-account-if-necessary';
 
-const callAuth0ManagementApi = (logger: Logger) => async (id: string) => {
+const getTwitterScreenNameViaAuth0 = (logger: Logger) => (id: string) => async () => {
   const token = process.env.AUTH0_MANAGEMENT_API_SECRET;
   const response: { data: { screen_name: string } } = await fetchData(logger)(
     `https://dev-sqa2k3wwnhpxk36d.eu.auth0.com/api/v2/users/${id}`,
@@ -29,24 +32,27 @@ const auth0Config = {
   callbackURL: process.env.AUTH0_CALLBACK_URL ?? '',
 };
 
-const deriveHandle = async (profile: Auth0Strategy.Profile, logger: Logger): Promise<string> => {
+const deriveHandle = (profile: Auth0Strategy.Profile, logger: Logger): T.Task<string> => {
   const isAuthdViaTwitter = (id: string) => id.includes('twitter');
-  const screenName = await callAuth0ManagementApi(logger)(profile.id);
-  const handle = isAuthdViaTwitter(profile.id) ? screenName : profile.nickname;
-  return handle as string;
+  const screenName = getTwitterScreenNameViaAuth0(logger)(profile.id);
+  const handle = isAuthdViaTwitter(profile.id) ? screenName : T.of(profile.nickname as string);
+  return handle;
 };
 
-const createUserAccountData = async (profile: Auth0Strategy.Profile, logger: Logger) => ({
-  id: toUserId(profile.id.substring(profile.id.indexOf('|') + 1)),
-  handle: await deriveHandle(profile, logger),
-  avatarUrl: profile.picture,
-  displayName: profile.displayName,
-});
+const createUserAccountData = (profile: Auth0Strategy.Profile, logger: Logger) => pipe(
+  {
+    id: T.of(toUserId(profile.id.substring(profile.id.indexOf('|') + 1))),
+    handle: deriveHandle(profile, logger),
+    avatarUrl: T.of(profile.picture as string),
+    displayName: T.of(profile.displayName),
+  },
+  sequenceS(T.ApplicativePar),
+);
 
 export const setupAuth0Strategy = (ports: Ports) => new Auth0Strategy(
   auth0Config,
   (async (accessToken, refreshToken, extraParams, profile, done) => {
-    const userAccount = await createUserAccountData(profile, ports.logger);
+    const userAccount = await createUserAccountData(profile, ports.logger)();
     void createAccountIfNecessary(ports)(userAccount)()
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       .then(() => done(
