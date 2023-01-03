1f39fb2de3bba116070b39890389face311e4201
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-01-03 14:29:22 +0000

    Write test list for querying a single user #1966

---
+++ test/shared-read-models/users/get-user.test.ts
@@ -0,0 +1,9 @@
+describe('get-user', () => {
+  describe('when user exists', () => {
+    it.todo('returns the correct user details');
+  });
+
+  describe('when user does not exist', () => {
+    it.todo('returns None');
+  });
+});
