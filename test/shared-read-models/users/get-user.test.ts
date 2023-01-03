6632f3cc43c11d50b39cf2ea9976f14ed9c3def8
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-01-03 15:01:57 +0000

    Make tests pass with local implementation #1966

--- test/shared-read-models/users/get-user.test.ts
+++ test/shared-read-models/users/get-user.test.ts
@@ -1,19 +1,41 @@
+/* eslint-disable no-param-reassign */
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as R from 'fp-ts/Record';
 import { pipe } from 'fp-ts/function';
 import { DomainEvent } from '../../../src/domain-events/domain-event';
-import { userCreatedAccount } from '../../../src/domain-events/user-created-account-event';
+import { isUserCreatedAccountEvent, userCreatedAccount } from '../../../src/domain-events/user-created-account-event';
 import { UserId } from '../../../src/types/user-id';
 import { arbitraryString, arbitraryUri, arbitraryWord } from '../../helpers';
 import { arbitraryUserId } from '../../types/user-id.helper';
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-type ReadModel = {};
-const initialState = () => ({});
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const handleEvent = (state: ReadModel, event: DomainEvent) => state;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const getUser = (readModel: ReadModel) => (userId: UserId) => O.none;
+type UserDetails = {
+  avatarUrl: string,
+  displayName: string,
+  handle: string,
+  userId: UserId,
+};
+
+type ReadModel = Record<UserId, UserDetails>;
+
+const initialState = (): ReadModel => ({});
+
+const handleEvent = (readModel: ReadModel, event: DomainEvent): ReadModel => {
+  if (isUserCreatedAccountEvent(event)) {
+    readModel[event.userId] = {
+      avatarUrl: event.avatarUrl,
+      displayName: event.displayName,
+      handle: event.handle,
+      userId: event.userId,
+    };
+  }
+  return readModel;
+};
+
+const getUser = (readModel: ReadModel) => (userId: UserId) => pipe(
+  readModel,
+  R.lookup(userId),
+);
 
 describe('get-user', () => {
   const userId = arbitraryUserId();
@@ -29,7 +51,7 @@ describe('get-user', () => {
       RA.reduce(initialState(), handleEvent),
     );
 
-    it.failing('returns the correct user details', () => {
+    it('returns the correct user details', () => {
       expect(getUser(readModel)(userId)).toStrictEqual(O.some({
         avatarUrl,
         displayName,
