2df035da1ccaf249318635eaf78c04ed389ed9b7
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-01-09 11:20:11 +0000

    Prevent fallbacks from silently hiding unacceptable config arguments

--- src/app-config.ts
+++ src/app-config.ts
@@ -1,18 +1,32 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
 import { strategyCodec } from './http/authentication/login-middlewares';
 import { CacheStrategyCodec } from './infrastructure/get-cached-axios-request';
 import { levelNameCodec } from './infrastructure/logger';
 
-export const appConfigCodec = t.type({
+const withDefaultIfEmpty = <C extends t.Any>(
+  codec: C,
+  ifEmpty: t.TypeOf<C>,
+) => tt.withValidate(
+    codec,
+    (input, context) => pipe(
+      tt.NonEmptyString.validate(input, context),
+      E.orElse(() => t.success(ifEmpty)),
+      E.chain((nonEmptyString) => codec.validate(nonEmptyString, context)),
+    ),
+  );
+
+export const appConfigCodec = t.strict({
   // Logging
   PRETTY_LOG: tt.withFallback(tt.BooleanFromString, false),
-  LOG_LEVEL: tt.withFallback(levelNameCodec, 'debug'),
+  LOG_LEVEL: withDefaultIfEmpty(levelNameCodec, 'debug'),
 
   // Sciety Application
   APP_ORIGIN: tt.NonEmptyString,
   APP_SECRET: tt.NonEmptyString,
-  APP_CACHE: tt.withFallback(CacheStrategyCodec, 'memory'),
+  APP_CACHE: withDefaultIfEmpty(CacheStrategyCodec, 'memory'),
   ALLOW_SITE_CRAWLERS: tt.withFallback(tt.BooleanFromString, false),
   SCIETY_TEAM_API_BEARER_TOKEN: tt.NonEmptyString,
   USE_STUB_ADAPTERS: tt.withFallback(tt.BooleanFromString, false),
@@ -28,7 +42,7 @@ export const appConfigCodec = t.type({
   PGDATABASE: tt.NonEmptyString,
 
   // User authentication
-  AUTHENTICATION_STRATEGY: tt.withFallback(strategyCodec, 'twitter'),
+  AUTHENTICATION_STRATEGY: withDefaultIfEmpty(strategyCodec, 'twitter'),
   TWITTER_API_KEY: tt.NonEmptyString,
   TWITTER_API_SECRET_KEY: tt.NonEmptyString,
 });
