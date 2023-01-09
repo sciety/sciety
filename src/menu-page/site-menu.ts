3b1114df80cdbd18609b0ba2b41771cbd77fc944
Giorgio Sironi <g.sironi@elifesciences.org>
2023-01-09 10:46:02 +0000

    Remove menu icons to get closer to navigation design #1979

--- src/menu-page/site-menu.ts
+++ src/menu-page/site-menu.ts
@@ -5,19 +5,19 @@ import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 import { User } from '../types/user';
 
 const myProfileMenuItem = (user: User) => toHtmlFragment(`
-  <li><a href="/users/${user.handle}" class="site-menu__link site-menu__link--profile"><span class="site-menu__link_text">My Profile</span></a></li>
+  <li><a href="/users/${user.handle}" class="site-menu__link"><span class="site-menu__link_text">My Profile</span></a></li>
 `);
 
 const myFeedMenuItem = () => toHtmlFragment(`
-  <li><a href="/my-feed" class="site-menu__link site-menu__link--feed"><span class="site-menu__link_text">My Feed</span></a></li>
+  <li><a href="/my-feed" class="site-menu__link"><span class="site-menu__link_text">My Feed</span></a></li>
 `);
 
 export const siteMenuItems = (user: O.Option<User>): HtmlFragment => toHtmlFragment(`
   <ul role="list" class="site-menu__links">
-    <li><a href="/" class="site-menu__link site-menu__link--home"><span class="site-menu__link_text">Home</span></a></li>
-    <li><a href="/sciety-feed" class="site-menu__link site-menu__link--sciety-feed"><span class="site-menu__link_text">Sciety Feed</span></a></li>
+    <li><a href="/" class="site-menu__link"><span class="site-menu__link_text">Home</span></a></li>
+    <li><a href="/sciety-feed" class="site-menu__link"><span class="site-menu__link_text">Sciety Feed</span></a></li>
     ${O.fold(constant(''), myFeedMenuItem)(user)}
     ${O.fold(constant(''), myProfileMenuItem)(user)}
-    <li><a href="/groups" class="site-menu__link site-menu__link--groups"><span class="site-menu__link_text">Groups</span></a></li>
+    <li><a href="/groups" class="site-menu__link"><span class="site-menu__link_text">Groups</span></a></li>
   </ul>
 `);
