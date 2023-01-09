b7f6ee54ef98623919864d604e84aafff6483b33
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-09 08:18:06 +0000

    Express intention with a test that doesnt compile #1978

---
+++ test/infrastructure/dispatcher.test.ts
@@ -0,0 +1,21 @@
+import { userFollowedEditorialCommunity } from '../../src/domain-events';
+import { dispatcher } from '../../src/infrastructure/dispatcher';
+import { arbitraryGroupId } from '../types/group-id.helper';
+import { arbitraryUserId } from '../types/user-id.helper';
+
+describe('dispatcher', () => {
+  it('sends a new event to every readmodel', () => {
+    const {
+      dispatchToAllReadModels,
+      queries,
+    } = dispatcher();
+    dispatchToAllReadModels([
+      userFollowedEditorialCommunity(arbitraryUserId(), arbitraryGroupId()),
+    ]);
+
+    expect(queries.q1()).toStrictEqual({
+      initial: 1,
+      calledWith: 'UserFollowedEditorialCommunity',
+    });
+  });
+});
