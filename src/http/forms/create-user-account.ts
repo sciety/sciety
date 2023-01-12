fb5a39429338edab7b5bbca89a34624169bc2408
LinaKind <l.kinduryte@elifesciences.org>
2023-01-12 14:49:54 +0000

    Decode form fields #1985

--- src/http/forms/create-user-account.ts
+++ src/http/forms/create-user-account.ts
@@ -1,8 +1,18 @@
+import { pipe } from 'fp-ts/function';
 import { Middleware } from 'koa';
+import * as t from 'io-ts';
+import { userGeneratedInputCodec } from '../../types/codecs/user-generated-input-codec';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Ports = {};
 
+const createUserAccountFormCodec = t.type({
+  displayName: userGeneratedInputCodec(30),
+  handle: userGeneratedInputCodec(30),
+});
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const createUserAccount = (adapters: Ports): Middleware => async (context) => {
-};
+export const createUserAccount = (adapters: Ports): Middleware => async (context) => pipe(
+  context.request.body,
+  createUserAccountFormCodec.decode,
+
+);
