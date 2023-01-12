faf6806b0cb0014da88e870aa72f2f5b6ee923d9
Giorgio Sironi <g.sironi@elifesciences.org>
2023-01-12 10:54:03 +0000

    Display a submittable form #1985

--- src/create-user-account-form-page/create-user-account-form-page.ts
+++ src/create-user-account-form-page/create-user-account-form-page.ts
@@ -3,5 +3,15 @@ import { Page } from '../types/page';
 
 export const createUserAccountFormPage: Page = {
   title: 'Create user account',
-  content: toHtmlFragment(''),
+  content: toHtmlFragment(`
+    <h1>Create user account</h1>
+    <form action="/forms/create-user-account" method="post">
+      <label for="displayName">Display name</label>
+      <input type="text" id="displayName" name="displayName">
+      <label for="handle">Handle</label>
+      <input type="text" id="handle" name="handle">
+      <button>Create account</button>
+      <button type="reset">Clear form</button>
+    </form>
+  `),
 };
