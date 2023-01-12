84c2dac0ef8339e39325e32023d4d10b002e34f4
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-12 14:34:49 +0000

    Add tests expressing the intended outcomes of the addGroup command #1991

--- test/add-group/execute-command.test.ts
+++ test/add-group/execute-command.test.ts
@@ -1,13 +1,16 @@
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import { executeCommand } from '../../src/add-group/execute-command';
-import { groupJoined } from '../../src/domain-events';
+import { groupJoined, ListCreatedEvent } from '../../src/domain-events';
 import { DescriptionPath } from '../../src/types/description-path';
 import { arbitraryString, arbitraryUri, arbitraryWord } from '../helpers';
+import { shouldNotBeCalled } from '../should-not-be-called';
 import { arbitraryGroupId } from '../types/group-id.helper';
+import * as LOID from '../../src/types/list-owner-id';
 
 describe('execute-command', () => {
   const newGroup = {
+    id: arbitraryGroupId(),
     name: arbitraryWord(),
     shortDescription: arbitraryString(),
     homepage: arbitraryUri(),
@@ -20,23 +23,36 @@ describe('execute-command', () => {
     const result = pipe(
       [],
       executeCommand(newGroup),
+      E.getOrElseW(shouldNotBeCalled),
     );
 
-    it('succeeds and raises an event', () => {
-      expect(result).toStrictEqual(E.right([expect.objectContaining({
+    it.failing('creates the group', () => {
+      expect(result[0]).toStrictEqual(expect.objectContaining({
         type: 'GroupJoined',
         ...newGroup,
-      })]));
+      }));
+    });
+
+    it.failing('creates a list owned by the group', () => {
+      expect(result[1]).toStrictEqual(expect.objectContaining({
+        type: 'ListCreated',
+        ownerId: LOID.fromGroupId(newGroup.id),
+      }));
+    });
+
+    it.failing('identifes the list as the target for ingestion', () => {
+      expect(result[2]).toStrictEqual(expect.objectContaining({
+        type: 'EvaluatedArticlesListSpecified',
+        groupId: newGroup.id,
+        listId: (result[1] as ListCreatedEvent).listId,
+      }));
     });
   });
 
   describe('when the group already exists', () => {
     const result = pipe(
       [
-        groupJoined({
-          id: arbitraryGroupId(),
-          ...newGroup,
-        }),
+        groupJoined(newGroup),
       ],
       executeCommand(newGroup),
     );
