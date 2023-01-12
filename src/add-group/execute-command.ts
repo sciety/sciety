7697e8f1ee19abc55125ba06c4be4312386ee19e
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-01-12 15:31:35 +0000

    Reshape to introduce concept of AllGroupsResource #1991

--- src/add-group/execute-command.ts
+++ src/add-group/execute-command.ts
@@ -1,30 +1,33 @@
 import * as E from 'fp-ts/Either';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import { Eq as stringEq } from 'fp-ts/string';
 import { AddGroupCommand } from '../commands';
 import {
   DomainEvent, groupJoined, GroupJoinedEvent, isGroupJoinedEvent,
 } from '../domain-events';
 
-const isSlugEqualIn = (
-  command: AddGroupCommand,
-) => (
-  event: GroupJoinedEvent,
-) => stringEq.equals(command.slug, event.slug);
+type AllGroupsResource = ReadonlyArray<GroupJoinedEvent>;
+
+const replayAllGroupsResource = (events: ReadonlyArray<DomainEvent>): AllGroupsResource => pipe(
+  events,
+  RA.filter(isGroupJoinedEvent),
+);
+
+const check = (command: AddGroupCommand) => (resource: AllGroupsResource): E.Either<string, unknown> => pipe(
+  resource,
+  RA.filter((event) => command.slug === event.slug),
+  RA.match(
+    () => E.right(undefined),
+    () => E.left(`Group with slug ${command.slug} already exists`),
+  ),
+);
 
 type ExecuteCommand = (command: AddGroupCommand)
 => (events: ReadonlyArray<DomainEvent>)
 => E.Either<string, ReadonlyArray<DomainEvent>>;
 
 export const executeCommand: ExecuteCommand = (command) => (events) => pipe(
-  events,
-  RA.filter(isGroupJoinedEvent),
-  RA.filter(isSlugEqualIn(command)),
-  RA.match(
-    () => E.right([
-      groupJoined(command),
-    ]),
-    () => E.left(`Group with slug ${command.slug} already exists`),
-  ),
+  replayAllGroupsResource(events),
+  check(command),
+  E.map(() => [groupJoined(command)]),
 );
