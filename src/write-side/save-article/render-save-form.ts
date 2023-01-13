98748fbe92b729c0ac26a2ea2bbe3d01e663be11
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-13 14:52:42 +0000

    Fix image path broken by recent folder move #1991

--- src/write-side/save-article/render-save-form.ts
+++ src/write-side/save-article/render-save-form.ts
@@ -6,7 +6,7 @@ export const renderSaveForm = (doi: Doi): HtmlFragment => toHtmlFragment(`
   <form method="post" action="/write-side/save-article">
     <input type="hidden" name="${articleIdFieldName}" value="${doi.value}">
     <button type="submit" class="save-article-button">
-      <img class="save-article-button__icon" src="/images/playlist_add-24px.svg" alt=""> Save to my list
+      <img class="save-article-button__icon" src="/static/images/playlist_add-24px.svg" alt=""> Save to my list
     </button>
   </form>
 `);
