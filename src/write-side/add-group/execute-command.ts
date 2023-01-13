c05e99df35b590b041922b73fc8630ae922415e3
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-13 14:44:51 +0000

    Extract an AllGroups resource #1991

--- src/write-side/add-group/execute-command.ts
+++ src/write-side/add-group/execute-command.ts
@@ -1,41 +1,20 @@
 import * as E from 'fp-ts/Either';
-import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import * as LID from '../../types/list-id';
 import * as LOID from '../../types/list-owner-id';
 import { AddGroupCommand } from '../commands';
 import {
-  DomainEvent, evaluatedArticlesListSpecified, groupJoined, GroupJoinedEvent, isGroupJoinedEvent, listCreated,
+  DomainEvent, evaluatedArticlesListSpecified, groupJoined, listCreated,
 } from '../../domain-events';
-
-type AllGroupsResource = ReadonlyArray<GroupJoinedEvent>;
-
-const replayAllGroupsResource = (events: ReadonlyArray<DomainEvent>): AllGroupsResource => pipe(
-  events,
-  RA.filter(isGroupJoinedEvent),
-);
-
-const check = (command: AddGroupCommand) => (resource: AllGroupsResource): E.Either<string, unknown> => pipe(
-  resource,
-  E.right,
-  E.filterOrElse(
-    RA.every((event) => event.slug !== command.slug),
-    () => `Group with slug ${command.slug} already exists`,
-  ),
-  E.filterOrElse(
-    RA.every((event) => event.groupId !== command.groupId),
-    () => `Group with id ${command.groupId} already exists`,
-  ),
-  E.map(() => undefined),
-);
+import * as AG from '../resources/all-groups';
 
 type ExecuteCommand = (command: AddGroupCommand)
 => (events: ReadonlyArray<DomainEvent>)
 => E.Either<string, ReadonlyArray<DomainEvent>>;
 
 export const executeCommand: ExecuteCommand = (command) => (events) => pipe(
-  replayAllGroupsResource(events),
-  check(command),
+  AG.replay(events),
+  AG.check(command),
   E.map(LID.generate),
   E.map((listId) => [
     groupJoined({
