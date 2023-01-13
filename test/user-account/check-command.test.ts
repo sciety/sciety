5319e0551f2546c9159d3bf1aaa7c4fa650dc874
LinaKind <l.kinduryte@elifesciences.org>
2023-01-13 10:47:03 +0000

    Write a test list #1985

---
+++ test/user-account/check-command.test.ts
@@ -0,0 +1,9 @@
+describe('check-command', () => {
+  describe('when the handle in the command is unique', () => {
+    it.todo('returns the command');
+  });
+
+  describe('when the handle in the command is not unique', () => {
+    it.todo('returns an error message');
+  });
+});
