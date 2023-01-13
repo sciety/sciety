968c3640d613c08aaee152d9734cbc00bee6fe38
Giorgio Sironi <g.sironi@elifesciences.org>
2023-01-13 11:52:49 +0000

    Complete the test list for checkCommand #1985

--- test/user-account/check-command.test.ts
+++ test/user-account/check-command.test.ts
@@ -1,5 +1,6 @@
 import * as E from 'fp-ts/Either';
 import * as T from 'fp-ts/Task';
+import { userCreatedAccount } from '../../src/domain-events/user-created-account-event';
 import { checkCommand } from '../../src/http/forms/create-user-account';
 import { UserHandle } from '../../src/types/user-handle';
 import { arbitraryWord, arbitraryString, arbitraryUri } from '../helpers';
@@ -24,6 +25,22 @@ describe('check-command', () => {
   });
 
   describe('when the handle in the command is not unique', () => {
-    it.todo('returns an error message');
+    const userHandle = arbitraryWord() as UserHandle;
+    const command = {
+      id: arbitraryUserId(),
+      handle: userHandle,
+      displayName: arbitraryString(),
+      avatarUrl: arbitraryUri(),
+    };
+    const adapters = {
+      getAllEvents: T.of([
+        userCreatedAccount(arbitraryUserId(), userHandle, arbitraryUri(), arbitraryString()),
+      ]),
+    };
+    const result = checkCommand(adapters)(command);
+
+    it('fails', () => {
+      expect(E.isLeft(result)).toBe(true);
+    });
   });
 });
