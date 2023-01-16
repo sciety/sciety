ec86a1c0af9d67cb53941093717093db6a63ff88
LinaKind <l.kinduryte@elifesciences.org>
2023-01-16 15:50:07 +0000

    Complete the failing test #1996

--- test/policies/backfill-group-joined-events.test.ts
+++ test/policies/backfill-group-joined-events.test.ts
@@ -3,12 +3,11 @@ import { backfillGroupJoinedEvents } from '../../src/policies/backfill-group-joi
 import { arbitraryGroupId } from '../types/group-id.helper';
 import { arbitraryDate, arbitraryString } from '../helpers';
 import { arbitraryDescriptionPath } from '../types/description-path.helper';
-import { DomainEvent } from '../../src/domain-events';
+import { DomainEvent, groupJoined } from '../../src/domain-events';
 
 describe('backfill-group-joined-events', () => {
   describe('when there is no pre-existing groupJoined event for the supplied group', () => {
     it.failing('raises a groupJoined event for the supplied group', () => {
-      const events: ReadonlyArray<DomainEvent> = [];
       const groupToBeAdded = {
         groupId: arbitraryGroupId(),
         name: arbitraryString(),
@@ -19,6 +18,28 @@ describe('backfill-group-joined-events', () => {
         slug: arbitraryString(),
         date: arbitraryDate(),
       };
+      const aDifferentGroup = {
+        groupId: arbitraryGroupId(),
+        name: arbitraryString(),
+        avatarPath: arbitraryString(),
+        descriptionPath: arbitraryDescriptionPath(),
+        shortDescription: arbitraryString(),
+        homepage: arbitraryString(),
+        slug: arbitraryString(),
+        date: arbitraryDate(),
+      };
+      const events: ReadonlyArray<DomainEvent> = [
+        groupJoined(
+          aDifferentGroup.groupId,
+          aDifferentGroup.name,
+          aDifferentGroup.avatarPath,
+          aDifferentGroup.descriptionPath,
+          aDifferentGroup.shortDescription,
+          aDifferentGroup.homepage,
+          aDifferentGroup.slug,
+          aDifferentGroup.date,
+        ),
+      ];
       const eventsToCommit = backfillGroupJoinedEvents(events)([groupToBeAdded]);
 
       expect(eventsToCommit).toStrictEqual([expect.objectContaining({
