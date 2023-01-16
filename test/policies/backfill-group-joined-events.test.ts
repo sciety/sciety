bc7a1212e0d84a62b2ab4d763bb776cb56a8e999
David Moulton <d.moulton@elifesciences.org>
2023-01-16 14:29:37 +0000

    Express intent #1996

---
+++ test/policies/backfill-group-joined-events.test.ts
@@ -0,0 +1,9 @@
+describe('backfill-group-joined-events', () => {
+  describe('when given information about an unknown group', () => {
+    it.todo('raises a groupJoined event');
+  });
+
+  describe('when given information about an known group', () => {
+    it.todo('does not raise a groupJoined event');
+  });
+});
