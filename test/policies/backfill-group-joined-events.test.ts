0733aabd5b2269949ad93cb7fa8cb2fb0405f341
LinaKind <l.kinduryte@elifesciences.org>
2023-01-16 14:48:55 +0000

    Refine intent #1996

--- test/policies/backfill-group-joined-events.test.ts
+++ test/policies/backfill-group-joined-events.test.ts
@@ -1,9 +1,9 @@
 describe('backfill-group-joined-events', () => {
-  describe('when given information about an unknown group', () => {
-    it.todo('raises a groupJoined event');
+  describe('when there is no pre-existing groupJoined event for the supplied group', () => {
+    it.todo('raises a groupJoined event for the supplied group');
   });
 
-  describe('when given information about an known group', () => {
-    it.todo('does not raise a groupJoined event');
+  describe('when there is a pre-existing groupJoined event for the supplied group', () => {
+    it.todo('does not raise a groupJoined event for the supplied group');
   });
 });
