d89f841d3b71675f713fd4ffde955de616e91738
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-17 15:26:09 +0000

    Encapsulate another use of context.state.user #1985

--- src/write-side/follow/finish-unfollow-command.ts
+++ src/write-side/follow/finish-unfollow-command.ts
@@ -2,8 +2,14 @@ import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { StatusCodes } from 'http-status-codes';
 import { Middleware } from 'koa';
-import { Ports, unfollowCommand } from './unfollow-command';
+import { Ports as UnfollowCommandPorts, unfollowCommand } from './unfollow-command';
 import * as GroupId from '../../types/group-id';
+import { getLoggedInScietyUser, Ports as GetLoggedInScietyUserPorts } from '../../http/get-logged-in-sciety-user';
+import { Logger } from '../../shared-ports';
+
+type Ports = GetLoggedInScietyUserPorts & UnfollowCommandPorts & {
+  logger: Logger,
+};
 
 export const finishUnfollowCommand = (ports: Ports): Middleware => async (context, next) => {
   if (context.session.command === 'unfollow') {
@@ -12,10 +18,18 @@ export const finishUnfollowCommand = (ports: Ports): Middleware => async (contex
       GroupId.fromNullable,
       O.fold(
         () => context.throw(StatusCodes.BAD_REQUEST),
-        async (groupId) => {
-          const { user } = context.state;
-          return unfollowCommand(ports)(user, groupId)();
-        },
+        async (groupId) => pipe(
+          getLoggedInScietyUser(ports, context),
+          O.match(
+            () => {
+              ports.logger('error', 'Logged in user not found', { context });
+              context.response.status = StatusCodes.INTERNAL_SERVER_ERROR;
+            },
+            async (userDetails) => {
+              await unfollowCommand(ports)(userDetails, groupId)();
+            },
+          ),
+        ),
       ),
     );
   }
