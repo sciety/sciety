edf7b452139bcae56ef45d7ab519e75809099182
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-17 10:37:08 +0000

    Encapsulate another use of context.state.user #1985

--- src/http/forms/remove-article-from-list.ts
+++ src/http/forms/remove-article-from-list.ts
@@ -1,4 +1,5 @@
 import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
 import { flow, pipe } from 'fp-ts/function';
 import * as PR from 'io-ts/PathReporter';
@@ -9,17 +10,22 @@ import { removeArticleFromListCommandHandler } from '../../write-side/remove-art
 import {
   CommitEvents, GetAllEvents, GetList, Logger,
 } from '../../shared-ports';
-import { User } from '../../types/user';
-import { UserId } from '../../types/user-id';
+import { getLoggedInScietyUser, Ports as GetLoggedInScietyUserPorts } from '../get-logged-in-sciety-user';
+import { UserDetails } from '../../types/user-details';
 
-type Ports = { logger: Logger, getAllEvents: GetAllEvents, commitEvents: CommitEvents, getList: GetList };
+type Ports = GetLoggedInScietyUserPorts & {
+  logger: Logger,
+  getAllEvents: GetAllEvents,
+  commitEvents: CommitEvents,
+  getList: GetList,
+};
 
 type FormBody = {
   articleid: unknown,
   listid: unknown,
 };
 
-const handleFormSubmission = (adapters: Ports, userId: UserId) => (formBody: FormBody) => pipe(
+const handleFormSubmission = (adapters: Ports, userDetails: O.Option<UserDetails>) => (formBody: FormBody) => pipe(
   {
     articleId: formBody.articleid,
     listId: formBody.listid,
@@ -36,23 +42,36 @@ const handleFormSubmission = (adapters: Ports, userId: UserId) => (formBody: For
       return command;
     },
   ),
+  E.chainW((command) => pipe(
+    userDetails,
+    O.match(
+      () => {
+        adapters.logger('error', 'Logged in user not found', { command });
+        return E.left(undefined);
+      },
+      (user) => E.right({
+        command,
+        userId: user.id,
+      }),
+    ),
+  )),
   TE.fromEither,
   TE.chainFirstW(flow(
-    (command) => checkUserOwnsList(adapters, command.listId, userId),
+    ({ command, userId }) => checkUserOwnsList(adapters, command.listId, userId),
     TE.mapLeft((logEntry) => {
       adapters.logger('error', logEntry.message, logEntry.payload);
       return logEntry;
     }),
   )),
+  TE.map(({ command }) => command),
   TE.chainW(removeArticleFromListCommandHandler(adapters)),
-
 );
 
 export const removeArticleFromList = (adapters: Ports): Middleware => async (context, next) => {
-  const user = context.state.user as User;
+  const user = getLoggedInScietyUser(adapters, context);
   await pipe(
     context.request.body,
-    handleFormSubmission(adapters, user.id),
+    handleFormSubmission(adapters, user),
     TE.mapLeft(() => {
       context.redirect('/action-failed');
     }),
