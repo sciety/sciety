9210ec6560809a835486ee291975e1720d304c56
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-01-17 13:55:14 +0000

    Stop another taiko test from relying on hardcoded groups #1996

--- feature-test/api/remove-article-from-list.test.ts
+++ feature-test/api/remove-article-from-list.test.ts
@@ -1,7 +1,11 @@
 import {
   $, goto, openBrowser,
 } from 'taiko';
+import { arbitraryString } from '../../test/helpers';
+import { arbitraryDescriptionPath } from '../../test/types/description-path.helper';
+import { arbitraryGroupId } from '../../test/types/group-id.helper';
 import { callApi } from '../call-api.helper';
+import { getFirstListOwnedByGroup } from '../get-first-list-owned-by.helper';
 import { screenshotTeardown } from '../utilities';
 
 describe('remove an article from a list', () => {
@@ -13,9 +17,20 @@ describe('remove an article from a list', () => {
 
   describe('when an article is removed from a list via the API', () => {
     const articleId = '10.1101/813451';
-    const listId = '5498e813-ddad-414d-88df-d1f84696cecd';
+    let listId: string;
 
     beforeEach(async () => {
+      const groupId = arbitraryGroupId();
+      await callApi('api/add-group', {
+        groupId,
+        name: arbitraryString(),
+        shortDescription: arbitraryString(),
+        homepage: arbitraryString(),
+        avatarPath: 'http://somethingthatproducesa404',
+        descriptionPath: arbitraryDescriptionPath(),
+        slug: arbitraryString(),
+      });
+      listId = await getFirstListOwnedByGroup(groupId);
       await callApi('api/add-article-to-list', { articleId, listId });
       await callApi('api/remove-article-from-list', { articleId, listId });
     });
