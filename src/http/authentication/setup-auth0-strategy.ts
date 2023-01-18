ec46e63591c9c21db318ce157f9a5a94eb3c805c
LinaKind <l.kinduryte@elifesciences.org>
2023-01-18 14:25:00 +0000

    Simplify auth0 strategy given its reduced responsibility #1985

--- src/http/authentication/setup-auth0-strategy.ts
+++ src/http/authentication/setup-auth0-strategy.ts
@@ -1,7 +1,4 @@
-import * as E from 'fp-ts/Either';
-import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import * as t from 'io-ts';
 import Auth0Strategy from 'passport-auth0';
 import { toUserId } from '../../types/user-id';
 import { writeUserIdToState } from '../get-logged-in-sciety-user';
@@ -13,33 +10,11 @@ const auth0Config = {
   callbackURL: process.env.AUTH0_CALLBACK_URL ?? '',
 };
 
-const profileCodec = t.type({
-  id: t.string,
-  displayName: t.string,
-  nickname: t.string,
-  picture: t.string,
-});
-
-type Profile = t.TypeOf<typeof profileCodec>;
-
-const toUserAccount = (profile: Profile) => ({
-  id: toUserId(profile.id),
-  handle: profile.nickname,
-  avatarUrl: profile.picture,
-  displayName: profile.displayName,
-});
-
 export const setupAuth0Strategy = () => new Auth0Strategy(
   auth0Config,
-  (async (accessToken, refreshToken, extraParams, profile, done) => pipe(
-    profile,
-    profileCodec.decode,
-    E.map(toUserAccount),
-    TE.fromEither,
-    TE.match(
-      () => done('could-not-derive-user-account-from-profile'),
-      (userAccount) => writeUserIdToState(done)(userAccount.id),
-    ),
-  )()
+  async (accessToken, refreshToken, extraParams, profile, done) => pipe(
+    profile.id,
+    toUserId,
+    writeUserIdToState(done),
   ),
 );
