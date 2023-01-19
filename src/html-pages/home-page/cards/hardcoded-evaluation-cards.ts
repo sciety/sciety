cf38af2dc817a82c80f46531561b6b7dd424afdb
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-19 08:21:11 +0000

    Update home-page evaluated article cards #2001

--- src/html-pages/home-page/cards/hardcoded-evaluation-cards.ts
+++ src/html-pages/home-page/cards/hardcoded-evaluation-cards.ts
@@ -1,26 +1,26 @@
 export const card1 = {
-  articleId: '10.1101/2022.10.31.514445',
-  articleTitle: 'How do Vampires Suck Blood?',
-  date: new Date('2022-11-09'),
-  groupSlug: 'arcadia-science',
-  groupName: 'Arcadia Science',
-  avatarPath: '/static/groups/arcadia-science--bc1f956b-12e8-4f5c-aadc-70f91347bd18.png',
-};
-
-export const card2 = {
-  articleId: '10.1101/2022.09.02.22279345',
-  articleTitle: 'Online training in manuscript peer review: a systematic review',
-  date: new Date('2022-11-07'),
-  groupSlug: 'prereview',
-  groupName: 'PREreview',
-  avatarPath: '/static/groups/prereview-community--10360d97-bf52-4aef-b2fa-2f60d319edd7.jpg',
-};
-
-export const card3 = {
-  articleId: '10.1101/2021.12.09.471922',
-  articleTitle: 'A striatal circuit balances learned fear in the presence and absence of sensory cues',
-  date: new Date('2021-11-08'),
+  articleId: '10.1101/2022.08.20.504530',
+  articleTitle: 'Universal gut microbial relationships in the gut microbiome of wild baboons',
+  date: new Date('2023-01-11'),
   groupSlug: 'elife',
   groupName: 'eLife',
   avatarPath: '/static/groups/elife--b560187e-f2fb-4ff9-a861-a204f3fc0fb0.png',
+};
+
+export const card2 = {
+  articleId: '10.1101/2022.04.21.22274150',
+  articleTitle: 'Epidemiological impact and cost-effectiveness analysis of COVID-19 vaccination in Kenya',
+  date: new Date('2023-01-03'),
+  groupSlug: 'rapid-reviews-covid-19',
+  groupName: 'Rapid Reviews COVID-19',
+  avatarPath: '/static/groups/rapid-reviews-covid-19--5142a5bc-6b18-42b1-9a8d-7342d7d17e94.png',
+};
+
+export const card3 = {
+  articleId: '10.1101/2022.12.15.520598',
+  articleTitle: 'Mapping current and future thermal limits to suitability for malaria transmission by the invasive mosquito Anopheles stephens i',
+  date: new Date('2023-01-02'),
+  groupSlug: 'prelights',
+  groupName: 'preLights',
+  avatarPath: '/static/groups/prelights--f97bd177-5cb6-4296-8573-078318755bf2.jpg',
 };
