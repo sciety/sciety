457f7a65f5c7d9f6082c00d39e39937308a1375d
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-19 15:37:58 +0000

    Log out of auth0 layer when logging out #2002

--- src/http/log-out.ts
+++ src/http/log-out.ts
@@ -2,7 +2,15 @@ import { Middleware } from 'koa';
 
 export const logOut: Middleware = async (context, next) => {
   context.logout();
-  context.redirect('back');
+  if (process.env.EXPERIMENT_ENABLED === 'true') {
+    const domain = process.env.AUTH0_DOMAIN ?? '';
+    const clientId = process.env.AUTH0_CLIENT_ID ?? '';
+    const app = process.env.APP_ORIGIN ?? '';
+    const auth0logout = `https://${domain}/v2/logout?client_id=${clientId}&returnTo=${app}/`;
+    context.redirect(auth0logout);
+  } else {
+    context.redirect('back');
+  }
 
   await next();
 };
