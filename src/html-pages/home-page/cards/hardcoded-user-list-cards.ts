443c6bff1296cb2f99b1fc0cb4e3cdb738edc702
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-20 08:35:38 +0000

    Delete an obsolete property from hardcoded home-page data #2001

--- src/html-pages/home-page/cards/hardcoded-user-list-cards.ts
+++ src/html-pages/home-page/cards/hardcoded-user-list-cards.ts
@@ -2,20 +2,16 @@ import { toUserId, UserId } from '../../../types/user-id';
 
 type Card = {
   userId: UserId,
-  description: string,
 };
 
 export const card1: Card = {
   userId: toUserId('1384541806231175172'),
-  description: 'Articles that are being read by Biophysics Colab.',
 };
 
 export const card2: Card = {
   userId: toUserId('1412019815619911685'),
-  description: 'See what researchers at Prachee Avasthi’s lab are reading to discover some interesting new work.',
 };
 
 export const card3: Card = {
   userId: toUserId('1122522190791028737'),
-  description: 'Articles that are being read by Dr. Maria Eichel-Vogel.',
 };
