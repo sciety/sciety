282fa1629a9117533df7ba35bb1cf1825869675f
David Moulton <d.moulton@elifesciences.org>
2023-01-20 13:28:25 +0000

    Begin to express intent for auth0 authentication journeys #2002

---
+++ feature-test/auth0-authentication-journeys.test.ts
@@ -0,0 +1,19 @@
+describe('auth0 authentication journeys', () => {
+  describe('sign up', () => {
+    describe('after completing the sign up journey', () => {
+      describe('the user is logged in', () => {
+        it.todo('the login button says "Log out"');
+
+        it.todo('i can navigate to my lists from the nav bar');
+
+        it.todo('i can see my handle in the nav bar');
+
+        it.todo('i can see my avatar in the nav bar');
+      });
+    });
+  });
+
+  describe('log in', () => {});
+
+  describe('log out', () => {});
+});
