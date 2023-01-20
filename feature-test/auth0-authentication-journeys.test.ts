3a92cff4eb2a6689278443c0c6c37f1283cff262
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-20 13:40:08 +0000

    Continue outlining auth0 journey tests #2002

--- feature-test/auth0-authentication-journeys.test.ts
+++ feature-test/auth0-authentication-journeys.test.ts
@@ -1,6 +1,6 @@
 describe('auth0 authentication journeys', () => {
   describe('sign up', () => {
-    describe('after completing the sign up journey', () => {
+    describe('after successfully completing the sign up journey', () => {
       describe('the user is logged in', () => {
         it.todo('the login button says "Log out"');
 
@@ -13,7 +13,21 @@ describe('auth0 authentication journeys', () => {
     });
   });
 
-  describe('log in', () => {});
+  describe('log in', () => {
+    describe('given the user has signed up successfully and is logged out', () => {
+      describe('when the user logs in', () => {
+        describe('the user is logged in', () => {
+          it.todo('the login button says "Log out"');
+
+          it.todo('i can navigate to my lists from the nav bar');
+
+          it.todo('i can see my handle in the nav bar');
+
+          it.todo('i can see my avatar in the nav bar');
+        });
+      });
+    });
+  });
 
   describe('log out', () => {});
 });
