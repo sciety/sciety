8947820b5196ab138642c8185cc96a433dfa5e5b
David Moulton <d.moulton@elifesciences.org>
2023-01-20 13:48:26 +0000

    More auth0 journey test expression #2002

--- feature-test/auth0-authentication-journeys.test.ts
+++ feature-test/auth0-authentication-journeys.test.ts
@@ -1,7 +1,7 @@
 describe('auth0 authentication journeys', () => {
   describe('sign up', () => {
     describe('after successfully completing the sign up journey', () => {
-      describe('the user is logged in', () => {
+      describe('i am logged in', () => {
         it.todo('the login button says "Log out"');
 
         it.todo('i can navigate to my lists from the nav bar');
@@ -9,14 +9,16 @@ describe('auth0 authentication journeys', () => {
         it.todo('i can see my handle in the nav bar');
 
         it.todo('i can see my avatar in the nav bar');
+
+        it.todo('i am on the home page');
       });
     });
   });
 
   describe('log in', () => {
-    describe('given the user has signed up successfully and is logged out', () => {
-      describe('when the user logs in', () => {
-        describe('the user is logged in', () => {
+    describe('given that I have signed up successfully and am logged out', () => {
+      describe('when I log in', () => {
+        describe('i am logged in', () => {
           it.todo('the login button says "Log out"');
 
           it.todo('i can navigate to my lists from the nav bar');
@@ -24,10 +26,26 @@ describe('auth0 authentication journeys', () => {
           it.todo('i can see my handle in the nav bar');
 
           it.todo('i can see my avatar in the nav bar');
+
+          it.todo('i am on the home page');
         });
       });
     });
   });
 
-  describe('log out', () => {});
+  describe('log out', () => {
+    describe('given that I have signed up successfully and am logged in', () => {
+      describe('when I log out', () => {
+        it.todo('the login button says "Log in"');
+
+        it.todo('my lists do not appear in the nav bar');
+
+        it.todo('my handle is not in the nav bar');
+
+        it.todo('my avatar is not in the nav bar');
+
+        it.todo('i am on the home page');
+      });
+    });
+  });
 });
