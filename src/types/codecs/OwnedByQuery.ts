a78ed22ce4867009bb119b7ec00a9b6da6b78ff7
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-23 08:51:20 +0000

    Extract a common definition of List #2008

--- src/types/codecs/OwnedByQuery.ts
+++ src/types/codecs/OwnedByQuery.ts
@@ -1,15 +1,6 @@
 import * as t from 'io-ts';
-import * as tt from 'io-ts-types';
-import { ListIdFromString } from './ListIdFromString';
-import * as LOID from '../list-owner-id';
+import { listCodec } from '../list';
 
 export const OwnedByQuery = t.type({
-  items: t.readonlyArray(t.type({
-    id: ListIdFromString,
-    name: t.string,
-    description: t.string,
-    articleIds: t.array(t.string),
-    lastUpdated: tt.DateFromISOString,
-    ownerId: LOID.fromStringCodec,
-  })),
+  items: t.readonlyArray(listCodec),
 });
--- src/types/list.ts
+++ src/types/list.ts
@@ -1,11 +1,15 @@
-import { ListId } from './list-id';
-import { ListOwnerId } from './list-owner-id';
+import * as t from 'io-ts';
+import * as tt from 'io-ts-types';
+import { ListIdFromString } from './codecs/ListIdFromString';
+import * as LOID from './list-owner-id';
 
-export type List = {
-  id: ListId,
-  ownerId: ListOwnerId,
-  articleIds: Array<string>,
-  lastUpdated: Date,
-  name: string,
-  description: string,
-};
+export const listCodec = t.type({
+  id: ListIdFromString,
+  name: t.string,
+  description: t.string,
+  articleIds: t.array(t.string),
+  lastUpdated: tt.DateFromISOString,
+  ownerId: LOID.fromStringCodec,
+});
+
+export type List = t.TypeOf<typeof listCodec>;
