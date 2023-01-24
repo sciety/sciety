7c13d627796850cb1d57b9dc8816f5eab7dbdebb
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-24 08:47:58 +0000

    Use a codec to parse API responses in taiko tests #2008

--- feature-test/get-first-list-owned-by.helper.ts
+++ feature-test/get-first-list-owned-by.helper.ts
@@ -1,15 +1,38 @@
+import * as PR from 'io-ts/PathReporter';
 import axios from 'axios';
+import { flow, pipe } from 'fp-ts/function';
+import * as TE from 'fp-ts/TaskEither';
+import * as E from 'fp-ts/Either';
+import * as t from 'io-ts';
+import * as tt from 'io-ts-types';
+import { listCodec } from '../src/types/list';
 
-export const getFirstListOwnedByUser = async (userId: string): Promise<string> => {
-  const userList = await axios.get(`http://localhost:8080/api/lists/owned-by/user-id:${userId}`);
-  expect(userList.data.items).toHaveLength(1);
-  const listId = userList.data.items[0].id as unknown as string;
-  return listId;
-};
+const responseCodec = t.type({
+  data: t.type({
+    items: tt.nonEmptyArray(listCodec),
+  }),
+});
 
-export const getFirstListOwnedByGroup = async (groupId: string): Promise<string> => {
-  const userList = await axios.get(`http://localhost:8080/api/lists/owned-by/group-id:${groupId}`);
-  expect(userList.data.items).toHaveLength(1);
-  const listId = userList.data.items[0].id as unknown as string;
-  return listId;
-};
+const fetchFirstListOwnedBy = async (ownerId: string): Promise<string> => pipe(
+  TE.tryCatch(
+    async () => axios.get(`http://localhost:8080/api/lists/owned-by/${ownerId}`),
+    String,
+  ),
+  TE.chainEitherK(flow(
+    responseCodec.decode,
+    E.mapLeft((errors) => PR.failure(errors).join('')),
+  )),
+  TE.map((response) => response.data),
+  TE.match(
+    (error) => { throw new Error(error); },
+    (response) => response.items[0].id,
+  ),
+)();
+
+export const getFirstListOwnedByUser = async (userId: string): Promise<string> => (
+  fetchFirstListOwnedBy(`user-id:${userId}`)
+);
+
+export const getFirstListOwnedByGroup = async (groupId: string): Promise<string> => (
+  fetchFirstListOwnedBy(`group-id:${groupId}`)
+);
