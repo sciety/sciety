98a5ad29e1093ea419a44bf835a5dded050f5bf6
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-24 14:02:42 +0000

    Extract a function for constructing the viewmodel #2012

---
+++ src/html-pages/user-page/construct-view-model/construct-view-model.ts
@@ -0,0 +1,68 @@
+import { sequenceS } from 'fp-ts/Apply';
+import * as E from 'fp-ts/Either';
+import * as RA from 'fp-ts/ReadonlyArray';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { followList, Ports as FollowListPorts } from '../follow-list/follow-list';
+import { tabList } from '../tab-list';
+import { userListCard } from '../user-list-card';
+import { tabs } from '../../../shared-components/tabs';
+import { GetUserViaHandle, SelectAllListsOwnedBy } from '../../../shared-ports';
+import { getGroupIdsFollowedBy } from '../../../shared-read-models/followings';
+import * as DE from '../../../types/data-error';
+import * as LOID from '../../../types/list-owner-id';
+import { ViewModel } from '../view-model';
+
+// ts-unused-exports:disable-next-line
+export type Ports = FollowListPorts & {
+  getUserViaHandle: GetUserViaHandle,
+  selectAllListsOwnedBy: SelectAllListsOwnedBy,
+};
+
+type Params = {
+  handle: string,
+};
+
+type ConstructViewModel = (tab: string, ports: Ports) => (params: Params) => TE.TaskEither<DE.DataError, ViewModel>;
+
+export const constructViewModel: ConstructViewModel = (tab, ports) => (params) => pipe(
+  params.handle,
+  ports.getUserViaHandle,
+  TE.fromOption(() => DE.notFound),
+  TE.chainW((user) => pipe(
+    {
+      groupIds: pipe(
+        ports.getAllEvents,
+        T.map(getGroupIdsFollowedBy(user.id)),
+        TE.rightTask,
+      ),
+      userDetails: TE.right(user),
+      activeTabIndex: TE.right(tab === 'lists' ? 0 as const : 1 as const),
+      userId: TE.right(user.id),
+      list: pipe(
+        user.id,
+        LOID.fromUserId,
+        ports.selectAllListsOwnedBy,
+        RA.head,
+        E.fromOption(() => DE.notFound),
+        T.of,
+      ),
+    },
+    sequenceS(TE.ApplyPar),
+  )),
+  TE.chainTaskK((inputs) => pipe(
+    (inputs.activeTabIndex === 0)
+      ? T.of(userListCard(inputs.list))
+      : followList(ports)(inputs.groupIds),
+    T.map(tabs({
+      tabList: tabList(inputs.userDetails.handle, inputs.groupIds.length),
+      activeTabIndex: inputs.activeTabIndex,
+    })),
+    T.map((mainContent) => ({
+      ...inputs.userDetails,
+      groupIds: inputs.groupIds,
+      mainContent,
+    })),
+  )),
+);
--- src/html-pages/user-page/user-page.ts
+++ src/html-pages/user-page/user-page.ts
@@ -1,21 +1,12 @@
-import { sequenceS } from 'fp-ts/Apply';
-import * as E from 'fp-ts/Either';
-import * as RA from 'fp-ts/ReadonlyArray';
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import { followList, Ports as FollowListPorts } from './follow-list/follow-list';
+import { Ports as FollowListPorts } from './follow-list/follow-list';
 import { renderErrorPage } from './render-error-page';
-import { tabList } from './tab-list';
-import { userListCard } from './user-list-card';
-import { tabs } from '../../shared-components/tabs';
 import { GetUserViaHandle, SelectAllListsOwnedBy } from '../../shared-ports';
-import { getGroupIdsFollowedBy } from '../../shared-read-models/followings';
-import * as DE from '../../types/data-error';
-import * as LOID from '../../types/list-owner-id';
 import { Page } from '../../types/page';
 import { RenderPageError } from '../../types/render-page-error';
 import { renderAsHtml } from './render-as-html';
+import { constructViewModel } from './construct-view-model';
 
 // ts-unused-exports:disable-next-line
 export type Ports = FollowListPorts & {
@@ -30,43 +21,7 @@ type Params = {
 type UserPage = (tab: string) => (params: Params) => TE.TaskEither<RenderPageError, Page>;
 
 export const userPage = (ports: Ports): UserPage => (tab) => (params) => pipe(
-  params.handle,
-  ports.getUserViaHandle,
-  TE.fromOption(() => DE.notFound),
-  TE.chainW((user) => pipe(
-    {
-      groupIds: pipe(
-        ports.getAllEvents,
-        T.map(getGroupIdsFollowedBy(user.id)),
-        TE.rightTask,
-      ),
-      userDetails: TE.right(user),
-      activeTabIndex: TE.right(tab === 'lists' ? 0 as const : 1 as const),
-      userId: TE.right(user.id),
-      list: pipe(
-        user.id,
-        LOID.fromUserId,
-        ports.selectAllListsOwnedBy,
-        RA.head,
-        E.fromOption(() => DE.notFound),
-        T.of,
-      ),
-    },
-    sequenceS(TE.ApplyPar),
-  )),
-  TE.chainTaskK((inputs) => pipe(
-    (inputs.activeTabIndex === 0)
-      ? T.of(userListCard(inputs.list))
-      : followList(ports)(inputs.groupIds),
-    T.map(tabs({
-      tabList: tabList(inputs.userDetails.handle, inputs.groupIds.length),
-      activeTabIndex: inputs.activeTabIndex,
-    })),
-    T.map((mainContent) => ({
-      ...inputs.userDetails,
-      groupIds: inputs.groupIds,
-      mainContent,
-    })),
-  )),
+  params,
+  constructViewModel(tab, ports),
   TE.bimap(renderErrorPage, renderAsHtml),
 );
