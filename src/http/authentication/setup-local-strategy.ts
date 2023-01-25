77ad1675204b02d5ce8290a3f6b4309e9c13e52e
LinaKind <l.kinduryte@elifesciences.org>
2023-01-25 15:32:35 +0000

    Apply twitter strategy bug fix to local strategy #2002

--- src/http/authentication/setup-local-strategy.ts
+++ src/http/authentication/setup-local-strategy.ts
@@ -3,14 +3,13 @@ import { Strategy as LocalStrategy } from 'passport-local';
 import * as E from 'fp-ts/Either';
 import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
+import { setUpUserIfNecessary } from '../../write-side/create-user-account/set-up-user-if-necessary';
 import { UserHandle } from '../../types/user-handle';
 import { toUserId } from '../../types/user-id';
-import { createUserAccountCommandHandler, Ports as CreateUserAccountCommandHandlerPorts } from '../../write-side/create-user-account';
+import { Ports as CreateUserAccountCommandHandlerPorts } from '../../write-side/create-user-account';
 import { CreateUserAccountCommand } from '../../write-side/commands';
 import { writeUserIdToState } from '../authentication-and-logging-in-of-sciety-users';
 import { Logger } from '../../shared-ports';
-import { CommandResult } from '../../types/command-result';
-import { ErrorMessage } from '../../types/error-message';
 
 type Ports = CreateUserAccountCommandHandlerPorts & {
   logger: Logger,
@@ -18,27 +17,32 @@ type Ports = CreateUserAccountCommandHandlerPorts & {
 
 const createUserAccountForLocalStrategy = (
   ports: Ports,
-) => async (username: string): Promise<E.Either<ErrorMessage, CommandResult>> => {
+) => async (username: string): Promise<string> => {
   const command: CreateUserAccountCommand = {
     userId: toUserId(username),
     handle: `H${username}` as UserHandle,
     avatarUrl: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png',
     displayName: '',
   };
-  return createUserAccountCommandHandler(ports)(command)();
+  return pipe(
+    ports.getAllEvents,
+    T.map(setUpUserIfNecessary(command)),
+    T.chain(ports.commitEvents),
+  )();
 };
 
 const noop = (
   ports: Ports,
-) => async (username: string): Promise<E.Either<ErrorMessage, CommandResult>> => T.of(E.right('no-events-created' as CommandResult))();
+) => async (username: string): Promise<string> => T.of('')();
 
 export const setupLocalStrategy = (ports: Ports) => new LocalStrategy(
   (username, _password, cb) => {
     void createUserAccountForLocalStrategy(ports)(username)
     // void noop(ports)(username)
-      .then((commandResult) => pipe(
-        commandResult,
-        E.map(() => toUserId(username)),
+      .then(() => pipe(
+        username,
+        toUserId,
+        E.right,
         writeUserIdToState(cb),
       ));
   },
