988a4492d34d6289f51c98c32fb3a80c54313de2
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-26 10:05:15 +0000

    Use the UserHandle codec when redirecting away from saved-articles list #2016

--- src/http/redirects/redirect-user-list-page-to-generic-list-page.ts
+++ src/http/redirects/redirect-user-list-page-to-generic-list-page.ts
@@ -1,6 +1,7 @@
+import * as t from 'io-ts';
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
 import { StatusCodes } from 'http-status-codes';
 import { Middleware } from 'koa';
 import { GetUserViaHandle, SelectAllListsOwnedBy } from '../../shared-ports';
@@ -9,20 +10,37 @@ import { toHtmlFragment } from '../../types/html-fragment';
 import * as LOID from '../../types/list-owner-id';
 import { toErrorResponse } from '../page-handler';
 import { getLoggedInScietyUser, Ports as GetLoggedInScietyUserPorts } from '../authentication-and-logging-in-of-sciety-users';
+import { userHandleCodec } from '../../types/user-handle';
+import { UserDetails } from '../../types/user-details';
 
 type Ports = GetLoggedInScietyUserPorts & {
   getUserViaHandle: GetUserViaHandle,
   selectAllListsOwnedBy: SelectAllListsOwnedBy,
 };
 
+const params = t.type({
+  handle: userHandleCodec,
+});
+
+const toUserHandle = flow(
+  params.decode,
+  O.fromEither,
+  O.map((p) => p.handle),
+);
+
+const toFirstOwnedList = (adapters: Ports) => (user: UserDetails) => pipe(
+  user.id,
+  LOID.fromUserId,
+  adapters.selectAllListsOwnedBy,
+  RA.head,
+);
+
 export const redirectUserListPageToGenericListPage = (adapters: Ports): Middleware => async (context) => {
   pipe(
-    context.params.handle as string,
-    adapters.getUserViaHandle,
-    O.map((user) => user.id),
-    O.map(LOID.fromUserId),
-    O.map(adapters.selectAllListsOwnedBy),
-    O.chain(RA.head),
+    context.params,
+    toUserHandle,
+    O.chain(adapters.getUserViaHandle),
+    O.chain(toFirstOwnedList(adapters)),
     O.match(
       () => {
         const loggedInUser = getLoggedInScietyUser(adapters, context);
