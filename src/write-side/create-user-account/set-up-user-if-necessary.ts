bcf185ffe248de1dbf736849093b05b0a66eae6d
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-26 09:39:30 +0000

    Adopt UserHandle when creating a user account #2016

--- src/write-side/create-user-account/set-up-user-if-necessary.ts
+++ src/write-side/create-user-account/set-up-user-if-necessary.ts
@@ -12,6 +12,7 @@ import { isListCreatedEvent } from '../../domain-events/list-created-event';
 import { executeCreateListCommand } from '../../lists/execute-create-list-command';
 import * as LOID from '../../types/list-owner-id';
 import { UserId } from '../../types/user-id';
+import { UserHandle } from '../../types/user-handle';
 
 const isAccountCreatedBy = (userId: UserId) => (event: DomainEvent) => (
   isUserCreatedAccountEvent(event)
@@ -33,7 +34,7 @@ const shouldCreateList = (userId: UserId) => (events: ReadonlyArray<DomainEvent>
   RA.isEmpty,
 );
 
-const constructCommand = (userDetails: { userId: UserId, handle: string }): CreateListCommand => ({
+const constructCommand = (userDetails: { userId: UserId, handle: UserHandle }): CreateListCommand => ({
   ownerId: LOID.fromUserId(userDetails.userId),
   name: `@${userDetails.handle}'s saved articles`,
   description: `Articles that have been saved by @${userDetails.handle}`,
