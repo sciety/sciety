5aab6cbd87098baa5eb6b8f0ec011a5170693ba7
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-26 11:00:10 +0000

    Adopt UserHandle in all remaining places #2016

--- src/html-pages/user-page/construct-view-model/tab-list.ts
+++ src/html-pages/user-page/construct-view-model/tab-list.ts
@@ -1,7 +1,8 @@
 import { Tab } from '../../../shared-components/tabs';
 import { toHtmlFragment } from '../../../types/html-fragment';
+import { UserHandle } from '../../../types/user-handle';
 
-export const tabList = (userHandle: string, followingCount: number): [Tab, Tab] => [
+export const tabList = (userHandle: UserHandle, followingCount: number): [Tab, Tab] => [
   {
     label: toHtmlFragment('Lists (1)'),
     url: `/users/${userHandle}/lists`,
--- src/shared-read-models/users/get-user-via-handle.ts
+++ src/shared-read-models/users/get-user-via-handle.ts
@@ -1,9 +1,10 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { ReadModel } from './handle-event';
+import { GetUserViaHandle } from '../../shared-ports';
 
 // ts-unused-exports:disable-next-line
-export const getUserViaHandle = (readModel: ReadModel) => (handle: string) => pipe(
+export const getUserViaHandle = (readModel: ReadModel): GetUserViaHandle => (handle) => pipe(
   Object.values(readModel),
   RA.findFirst((user) => user.handle === handle),
 );
