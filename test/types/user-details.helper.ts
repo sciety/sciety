80189cc7d4a751ebfc6305b77fadf365795e9002
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-26 08:46:31 +0000

    Always create a valid arbitrary user handle #2016

--- test/types/user-details.helper.ts
+++ test/types/user-details.helper.ts
@@ -1,10 +1,11 @@
 import { arbitraryUserId } from './user-id.helper';
 import { UserDetails } from '../../src/types/user-details';
-import { arbitraryString, arbitraryUri, arbitraryWord } from '../helpers';
+import { arbitraryString, arbitraryUri } from '../helpers';
+import { arbitraryUserHandle } from './user-handle.helper';
 
 export const arbitraryUserDetails = (): UserDetails => ({
   id: arbitraryUserId(),
   avatarUrl: arbitraryUri(),
   displayName: arbitraryString(),
-  handle: arbitraryWord(),
+  handle: arbitraryUserHandle(),
 });
--- test/types/user-handle.helper.ts
+++ test/types/user-handle.helper.ts
@@ -1,11 +1,11 @@
+import * as PR from 'io-ts/PathReporter';
 import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
 import * as UH from '../../src/types/user-handle';
 import { arbitraryWord } from '../helpers';
-import { shouldNotBeCalled } from '../should-not-be-called';
 
 export const arbitraryUserHandle = (): UH.UserHandle => pipe(
-  arbitraryWord(),
+  arbitraryWord(10),
   UH.userHandleCodec.decode,
-  E.getOrElseW(shouldNotBeCalled),
+  E.getOrElseW((errors) => { throw new Error(PR.failure(errors).join('')); }),
 );
