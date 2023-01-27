eb47570063061e078892210f4483ee3182740066
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-27 14:32:34 +0000

    Separate viewmodel construction from rendering in the about tab #2012

--- src/html-pages/group-page/about/about.ts
+++ src/html-pages/group-page/about/about.ts
@@ -2,11 +2,10 @@ import { sequenceS } from 'fp-ts/Apply';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { renderDescription } from './render-description';
-import { renderOurLists } from './render-our-lists';
+import { OurListsViewModel, renderOurLists } from './render-our-lists';
 import { toOurListsViewModel } from './to-our-lists-view-model';
 import { FetchStaticFile } from '../../../shared-ports';
 import * as DE from '../../../types/data-error';
-import { Group } from '../../../types/group';
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 import { ContentModel } from '../content-model';
 
@@ -14,34 +13,36 @@ export type Ports = {
   fetchStaticFile: FetchStaticFile,
 };
 
-const getRenderedLists = (contentModel: ContentModel) => pipe(
-  contentModel.lists,
-  TE.right,
-  TE.map(toOurListsViewModel(contentModel.group.slug)),
-  TE.map(renderOurLists),
-);
+type AboutTabViewModel = {
+  lists: OurListsViewModel,
+  markdown: string,
+};
 
-const getRenderedDescription = (ports: Ports) => (group: Group): TE.TaskEither<DE.DataError, HtmlFragment> => pipe(
-  `groups/${group.descriptionPath}`,
-  ports.fetchStaticFile,
-  TE.map(renderDescription),
-);
-
-export const about = (ports: Ports) => (contentModel: ContentModel): TE.TaskEither<DE.DataError, HtmlFragment> => pipe(
+const constructAboutTab = (ports: Ports) => (contentModel: ContentModel) => pipe(
   {
-    lists: getRenderedLists(contentModel),
-    description: getRenderedDescription(ports)(contentModel.group),
+    lists: pipe(
+      contentModel.lists,
+      toOurListsViewModel(contentModel.group.slug),
+      TE.right,
+    ),
+    markdown: ports.fetchStaticFile(`groups/${contentModel.group.descriptionPath}`),
   },
   sequenceS(TE.ApplyPar),
-  TE.map(({ lists, description }) => `
-    <div class="group-page-about">
-      <section>
-        ${lists}
-      </section>
-      <section>
-        ${description}
-      </section>
-    </div>
-  `),
-  TE.map(toHtmlFragment),
+);
+
+const renderAboutTab = (viewmodel: AboutTabViewModel) => toHtmlFragment(`
+  <div class="group-page-about">
+    <section>
+      ${renderOurLists(viewmodel.lists)}
+    </section>
+    <section>
+      ${renderDescription(viewmodel.markdown)}
+    </section>
+  </div>
+`);
+
+export const about = (ports: Ports) => (contentModel: ContentModel): TE.TaskEither<DE.DataError, HtmlFragment> => pipe(
+  contentModel,
+  constructAboutTab(ports),
+  TE.map(renderAboutTab),
 );
