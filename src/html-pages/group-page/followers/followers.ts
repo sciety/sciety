0ffd9c6d0ddfeadcf41bba197d0a0038a4574d1b
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-27 09:06:01 +0000

    Simplify pipe steps #2012

--- src/html-pages/group-page/followers/followers.ts
+++ src/html-pages/group-page/followers/followers.ts
@@ -1,12 +1,11 @@
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import { augmentWithUserDetails, Ports as AugmentWithUserDetailsPorts, Follower } from './augment-with-user-details';
+import { augmentWithUserDetails, Ports as AugmentWithUserDetailsPorts } from './augment-with-user-details';
 import { findFollowers } from './find-followers';
 import { paginate } from './paginate';
 import { renderFollowers } from './render-followers';
 import { DomainEvent } from '../../../domain-events';
-import { PageOfItems } from '../../../shared-components/paginate';
 import { paginationControls } from '../../../shared-components/pagination-controls';
 import * as DE from '../../../types/data-error';
 import { GroupId } from '../../../types/group-id';
@@ -15,19 +14,6 @@ import { HtmlFragment } from '../../../types/html-fragment';
 export type Ports = AugmentWithUserDetailsPorts & {
   getAllEvents: T.Task<ReadonlyArray<DomainEvent>>,
 };
-
-const augmentFollowersWithUserDetails = (
-  ports: Ports,
-) => (pageOfFollowers: PageOfItems<Follower>) => pipe(
-  {
-    followerCount: pageOfFollowers.numberOfOriginalItems,
-    nextPage: pageOfFollowers.nextPage,
-    followers: pipe(
-      pageOfFollowers.items,
-      augmentWithUserDetails(ports),
-    ),
-  },
-);
 
 const pageSize = 10;
 
@@ -40,10 +26,13 @@ export const followers = (
   ports.getAllEvents,
   T.map(findFollowers(group.id)),
   T.map(paginate(pageNumber, pageSize)),
-  TE.map(augmentFollowersWithUserDetails(ports)),
-  TE.map((partial) => ({
-    ...partial,
-    nextLink: paginationControls(`/groups/${group.slug}/followers?`, partial.nextPage),
+  TE.map((pageOfFollowers) => ({
+    followerCount: pageOfFollowers.numberOfOriginalItems,
+    followers: pipe(
+      pageOfFollowers.items,
+      augmentWithUserDetails(ports),
+    ),
+    nextLink: paginationControls(`/groups/${group.slug}/followers?`, pageOfFollowers.nextPage),
   })),
   TE.map(renderFollowers),
 );
