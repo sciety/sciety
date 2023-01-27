80f32edd380e467ac2a434eae7c7e4a8314c7db9
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-27 15:02:03 +0000

    Separate viewmodel construction from rendering in the lists tab #2012

--- src/html-pages/group-page/lists/lists.ts
+++ src/html-pages/group-page/lists/lists.ts
@@ -1,15 +1,40 @@
+import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
 import { renderListOfListCardsWithFallback } from './render-list-of-list-cards-with-fallback';
-import { toListCardViewModel } from './to-list-card-view-model';
 import { HtmlFragment } from '../../../types/html-fragment';
 import { ContentModel } from '../content-model';
+import { ListCardViewModel } from '../../../shared-components/list-card/render-list-card';
+import { List } from '../../../types/list';
 
-export const lists = (contentModel: ContentModel): TE.TaskEither<never, HtmlFragment> => pipe(
+const toListCardViewModel = (list: List): ListCardViewModel => ({
+  ...list,
+  listId: list.id,
+  title: list.name,
+  articleCount: list.articleIds.length,
+  articleCountLabel: 'This list contains',
+  lastUpdated: O.some(list.lastUpdated),
+});
+
+type ListsTabViewModel = {
+  lists: ReadonlyArray<ListCardViewModel>,
+};
+
+const constructListsTab = (contentModel: ContentModel): TE.TaskEither<never, ListsTabViewModel> => pipe(
   contentModel.lists,
   RA.reverse,
   RA.map(toListCardViewModel),
-  renderListOfListCardsWithFallback,
+  (lists) => ({ lists }),
   TE.right,
+);
+
+const renderListsTab = (viewmodel: ListsTabViewModel): HtmlFragment => (
+  renderListOfListCardsWithFallback(viewmodel.lists)
+);
+
+export const lists = (contentModel: ContentModel): TE.TaskEither<never, HtmlFragment> => pipe(
+  contentModel,
+  constructListsTab,
+  TE.map(renderListsTab),
 );
