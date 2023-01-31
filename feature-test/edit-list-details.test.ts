a18c9f8ae2b1bf6e3e27dc418cbd272f7b0d57ea
Kevin Rutherford <kevin@rutherford-software.com>
2023-01-31 13:44:17 +0000

    Create a Taiko helper to create a user account and log in #2002

--- feature-test/edit-list-details.test.ts
+++ feature-test/edit-list-details.test.ts
@@ -2,15 +2,32 @@ import {
   $, click, closeBrowser, currentURL, goto, into, openBrowser, textBox, write,
 } from 'taiko';
 import { getFirstListOwnedByUser } from './get-first-list-owned-by.helper';
-import { arbitraryString, arbitraryWord } from '../test/helpers';
+import { arbitraryString, arbitraryUri, arbitraryWord } from '../test/helpers';
+import { arbitraryUserId } from '../test/types/user-id.helper';
+import { UserId } from '../src/types/user-id';
+import { arbitraryUserHandle } from '../test/types/user-handle.helper';
+import { callApi } from './call-api.helper';
+
+const createUserAccountAndLogIn = async (userId: UserId) => {
+  await callApi('api/create-user', {
+    userId,
+    handle: arbitraryUserHandle(),
+    avatarUrl: arbitraryUri(),
+    displayName: arbitraryString(),
+  });
+  await goto('localhost:8080/');
+  await click('Log In');
+  await write(userId, into(textBox('User id')));
+  await click('Log in');
+};
 
 describe('edit-list-details', () => {
   let listId: string;
 
   beforeAll(async () => {
-    const testUserId = '153571843';
+    const testUserId = arbitraryUserId();
     await openBrowser();
-    await goto(`localhost:8080/log-in-as?userId=${testUserId}`);
+    await createUserAccountAndLogIn(testUserId);
     listId = await getFirstListOwnedByUser(testUserId);
   });
 
