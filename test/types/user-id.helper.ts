dc64500a40bfd418da4a0588fbc91afa60d3b763
Giorgio Sironi <g.sironi@elifesciences.org>
2023-02-02 11:39:33 +0000

    Dogfood codec for test generated data #2029

--- test/types/user-id.helper.ts
+++ test/types/user-id.helper.ts
@@ -1,4 +1,11 @@
-import { toUserId, UserId } from '../../src/types/user-id';
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
+import { shouldNotBeCalled } from '../should-not-be-called';
+import { UserId, userIdCodec } from '../../src/types/user-id';
 import { arbitraryWord } from '../helpers';
 
-export const arbitraryUserId = (): UserId => toUserId(arbitraryWord());
+export const arbitraryUserId = (): UserId => pipe(
+  arbitraryWord(),
+  userIdCodec.decode,
+  E.getOrElseW(shouldNotBeCalled),
+);
