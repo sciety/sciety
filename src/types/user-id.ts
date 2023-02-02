1bc4ab9b120608d64498dffb37bf1126e1423900
Giorgio Sironi <g.sironi@elifesciences.org>
2023-02-02 14:41:58 +0000

    Inline everything in the codec #2029

--- src/types/user-id.ts
+++ src/types/user-id.ts
@@ -1,31 +1,22 @@
-import * as O from 'fp-ts/Option';
 import * as t from 'io-ts';
-import { flow, identity, pipe } from 'fp-ts/function';
+import { identity, pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
 
 export type UserId = string & { readonly UserId: unique symbol };
 
 const isUserId = (value: unknown): value is UserId => typeof value === 'string' && value !== '';
 
-const fromString = (value: string): O.Option<UserId> => {
-  if (isUserId(value)) {
-    return O.some(value);
-  }
-  return O.none;
-};
-
 export const userIdCodec = new t.Type(
   'UserIdFromString',
   isUserId,
   (u, c) => pipe(
     t.string.validate(u, c),
-    E.chain(flow(
-      fromString,
-      O.fold(
-        () => t.failure(u, c),
-        t.success,
-      ),
-    )),
+    E.chain((value) => {
+      if (isUserId(value)) {
+        return t.success(value);
+      }
+      return t.failure(u, c);
+    }),
   ),
   identity,
 );
