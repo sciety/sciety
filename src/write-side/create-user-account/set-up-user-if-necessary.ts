1cdfe259d8daba868ccef9b8dc1be8bfe0fc3802
LinaKind <l.kinduryte@elifesciences.org>
2023-02-02 14:52:54 +0000

    Clearly separate user and list creation handlers

--- src/write-side/create-user-account/set-up-user-if-necessary.ts
+++ src/write-side/create-user-account/set-up-user-if-necessary.ts
@@ -2,19 +2,16 @@ import * as RA from 'fp-ts/ReadonlyArray';
 import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
 import { v4 } from 'uuid';
+import { listCreated, isListCreatedEvent } from '../../domain-events/list-created-event';
 import * as LID from '../../types/list-id';
 import { CreateUserAccountCommand } from '../commands/create-user-account';
-import { CreateListCommand } from '../commands';
 import {
   DomainEvent,
   isUserCreatedAccountEvent,
   userCreatedAccount,
 } from '../../domain-events';
-import { isListCreatedEvent } from '../../domain-events/list-created-event';
-import { executeCreateListCommand } from '../../lists/execute-create-list-command';
 import * as LOID from '../../types/list-owner-id';
 import { UserId } from '../../types/user-id';
-import { UserHandle } from '../../types/user-handle';
 
 const isAccountCreatedBy = (userId: UserId) => (event: DomainEvent) => (
   isUserCreatedAccountEvent(event)
@@ -36,13 +33,6 @@ const shouldCreateList = (userId: UserId) => (events: ReadonlyArray<DomainEvent>
   RA.isEmpty,
 );
 
-const constructCommand = (userDetails: { userId: UserId, handle: UserHandle }): CreateListCommand => ({
-  listId: LID.fromValidatedString(v4()),
-  ownerId: LOID.fromUserId(userDetails.userId),
-  name: `@${userDetails.handle}'s saved articles`,
-  description: `Articles that have been saved by @${userDetails.handle}`,
-});
-
 type SetUpUserIfNecessary = (command: CreateUserAccountCommand)
 => (events: ReadonlyArray<DomainEvent>)
 => ReadonlyArray<DomainEvent>;
@@ -63,10 +53,14 @@ export const setUpUserIfNecessary: SetUpUserIfNecessary = (command) => (events) 
     shouldCreateList(command.userId),
     B.fold(
       () => [],
-      () => pipe(
-        constructCommand({ userId: command.userId, handle: command.handle }),
-        executeCreateListCommand([]),
-      ),
+      () => [
+        listCreated(
+          LID.fromValidatedString(v4()),
+          `@${command.handle}'s saved articles`,
+          `Articles that have been saved by @${command.handle}`,
+          LOID.fromUserId(command.userId),
+        ),
+      ],
     ),
   ),
 ];
