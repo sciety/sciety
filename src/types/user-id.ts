da790a8d1d9f9ebb612b2d4a5a5809fd26752afb
David Moulton <d.moulton@elifesciences.org>
2023-02-02 10:35:37 +0000

    Move codec for better cohesion #2029

--- src/types/user-id.ts
+++ src/types/user-id.ts
@@ -1,8 +1,11 @@
 import * as O from 'fp-ts/Option';
+import * as t from 'io-ts';
+import { flow, identity, pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
 
 export type UserId = string & { readonly UserId: unique symbol };
 
-export const isUserId = (value: unknown): value is UserId => typeof value === 'string' && value !== '';
+const isUserId = (value: unknown): value is UserId => typeof value === 'string' && value !== '';
 
 export const toUserId = (value: string): UserId => {
   if (isUserId(value)) {
@@ -11,6 +14,23 @@ export const toUserId = (value: string): UserId => {
   throw new Error();
 };
 
-export const fromValidatedString = (value: string): UserId => value as UserId;
+const fromString = (value: string): O.Option<UserId> => O.tryCatch(() => toUserId(value));
 
-export const fromString = (value: string): O.Option<UserId> => O.tryCatch(() => toUserId(value));
+export const UserIdFromString = new t.Type(
+  'UserIdFromString',
+  isUserId,
+  (u, c) => pipe(
+    t.string.validate(u, c),
+    E.chain(flow(
+      fromString,
+      O.fold(
+        () => t.failure(u, c),
+        t.success,
+      ),
+    )),
+  ),
+  identity,
+);
+
+// ts-unused-exports:disable-next-line
+export const fromValidatedString = (value: string): UserId => value as UserId;
