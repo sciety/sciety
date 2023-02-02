a852559fdaf5d92236d5b34e0036b70108e4b2de
LinaKind <l.kinduryte@elifesciences.org>
2023-02-02 15:35:39 +0000

    Use resource in command execution

--- src/write-side/create-list/execute-create-list-command.ts
+++ src/write-side/create-list/execute-create-list-command.ts
@@ -1,14 +1,18 @@
+import { AllListsResource } from '../resources/all-lists';
 import { CreateListCommand } from '../commands';
 import { listCreated, ListCreatedEvent } from '../../domain-events';
-import { AllListsResource } from '../resources/all-lists';
 
 type ExecuteCreateListCommand = (command: CreateListCommand)
 => (resource: AllListsResource)
 => ReadonlyArray<ListCreatedEvent>;
 
-export const executeCreateListCommand: ExecuteCreateListCommand = (command) => () => [listCreated(
-  command.listId,
-  command.name,
-  command.description,
-  command.ownerId,
-)];
+export const executeCreateListCommand: ExecuteCreateListCommand = (command) => (resource) => (
+  resource.includes(command.listId)
+    ? []
+    : [listCreated(
+      command.listId,
+      command.name,
+      command.description,
+      command.ownerId,
+    )]
+);
