224e7e5eba283f076f8e40e2a61314affb6279f0
David Moulton <d.moulton@elifesciences.org>
2023-02-02 14:30:39 +0000

    Express intention with more test cases for user id #2029

--- test/types/user-id.test.ts
+++ test/types/user-id.test.ts
@@ -1,14 +1,32 @@
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import { userIdCodec } from '../../src/types/user-id';
+import { shouldNotBeCalled } from '../should-not-be-called';
 
 describe('user-id', () => {
+  it.each([
+    ['auth0|63d8f06993ff1642c77e0e91', 'auth0|63d8f06993ff1642c77e0e91'],
+    ['twitter|1295307136415735808', 'twitter|1295307136415735808'],
+    // ['1295307136415735808', 'twitter|1295307136415735808'],
+  ])('encodes %s to a canonical value', (input, expectedValue) => {
+    expect(pipe(
+      input,
+      userIdCodec.decode,
+      E.map(userIdCodec.encode),
+      E.getOrElseW(shouldNotBeCalled),
+    )).toBe(expectedValue);
+  });
+
   it.each([
     43,
     null,
     undefined,
     '',
-  ])('cannot decode an invalid UserId', (input) => {
+    // 'unsupportedconnection|63d8f06993ff1642c77e0e91',
+    // 'unsupportedconnection|',
+    // '|',
+    // 'auth0|twitter|63d8f06993ff1642c77e0e91',
+  ])('cannot decode %s as a UserId', (input) => {
     expect(pipe(
       input,
       userIdCodec.decode,
