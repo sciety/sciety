130a53c017f0669da241a70d91de547ae0ec4d63
Giorgio Sironi <g.sironi@elifesciences.org>
2023-02-02 14:13:48 +0000

    Delete old, outdated test #2029

--- test/types/user-id.test.ts
+++ test/types/user-id.test.ts
@@ -1,19 +1,8 @@
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import { userIdCodec } from '../../src/types/user-id';
-import { arbitraryUserId } from './user-id.helper';
 
 describe('user-id', () => {
-  it('encodes and decodes back to the same value', () => {
-    const id = arbitraryUserId();
-
-    expect(pipe(
-      id,
-      userIdCodec.encode,
-      userIdCodec.decode,
-    )).toStrictEqual(E.right(id));
-  });
-
   it.each([
     43,
     null,
