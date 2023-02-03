11a9f18e50ddc6df0e8a52350e78c7a70b28ac57
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-03 10:13:18 +0000

    Use realistic auth0-related userIds in tests #2029

--- test/types/user-id.helper.ts
+++ test/types/user-id.helper.ts
@@ -2,10 +2,14 @@ import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
 import { shouldNotBeCalled } from '../should-not-be-called';
 import { UserId, userIdCodec } from '../../src/types/user-id';
-import { arbitraryWord } from '../helpers';
+import { arbitraryNumber, arbitraryWord } from '../helpers';
+
+const prefixes = ['auth0|', 'twitter|', ''];
+
+const generatePrefix = () => prefixes[arbitraryNumber(0, prefixes.length - 1)];
 
 export const arbitraryUserId = (): UserId => pipe(
-  arbitraryWord(),
+  `${generatePrefix()}${arbitraryWord()}`,
   userIdCodec.decode,
   E.getOrElseW(shouldNotBeCalled),
 );
