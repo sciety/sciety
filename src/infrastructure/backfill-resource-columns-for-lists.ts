4b6ab7843b7956d230872f8e14252edc42feb3cc
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-02-03 19:38:36 +0000

    dynamically determine resource version

--- src/infrastructure/backfill-resource-columns-for-lists.ts
+++ src/infrastructure/backfill-resource-columns-for-lists.ts
@@ -3,6 +3,9 @@ import { pipe } from 'fp-ts/function';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
 import * as E from 'fp-ts/Either';
+import * as M from 'fp-ts/Map';
+import { Eq as stringEq } from 'fp-ts/string';
+import * as O from 'fp-ts/Option';
 import { DomainEvent, isListCreatedEvent, ListCreatedEvent } from '../domain-events';
 import { EventId } from '../types/event-id';
 import { ListId } from '../types/list-id';
@@ -19,7 +22,23 @@ const updateListResourceBackfillData = (
   state: BackfillDataForAllList,
   event: ListCreatedEvent,
 ): BackfillDataForAllList => {
-  state.set(event.listId, [{ resourceId: event.listId, resourceVersion: 0, eventId: event.id }]);
+  const updatedResource = pipe(
+    state,
+    M.lookup(stringEq)(event.listId),
+    O.fold(
+      () => [{
+        resourceId: event.listId,
+        resourceVersion: 0,
+        eventId: event.id,
+      }],
+      (oldResource) => [...oldResource, {
+        resourceId: event.listId,
+        resourceVersion: oldResource.length,
+        eventId: event.id,
+      }],
+    ),
+  );
+  state.set(event.listId, updatedResource);
   return state;
 };
 
