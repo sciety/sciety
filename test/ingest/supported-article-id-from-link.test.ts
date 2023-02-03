fb8f0d46446818ffc4130990a050a192d061e591
LinaKind <l.kinduryte@elifesciences.org>
2023-02-03 11:39:54 +0000

    Add a failing test case for a new preprint server #2036

--- test/ingest/supported-article-id-from-link.test.ts
+++ test/ingest/supported-article-id-from-link.test.ts
@@ -16,6 +16,7 @@ describe('supported-article-id-from-link', () => {
       ['research square link', 'https://www.researchsquare.com/article/rs-955726/v1', '10.21203/rs.3.rs-955726/v1'],
       ['research square DOI link', 'https://doi.org/10.21203/rs.3.rs-885194/v1', '10.21203/rs.3.rs-885194/v1'],
       ['SciELO link', 'https://preprints.scielo.org/index.php/scielo/preprint/download/4639/8936/9328', '10.1590/SciELOPreprints.4639'],
+      // ['OSF link', 'https://osf.io/vrmpf/', '10.31219/osf.io/vrmpf'],
     ])('%s', (_, input, expectedDoi) => {
       it('extracts the doi from the input', () => {
         const result = supportedArticleIdFromLink(input);
