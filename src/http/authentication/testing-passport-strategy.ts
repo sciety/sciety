0baef6cb6615247253a0e8e519e8cef2a1d02d96
Giorgio Sironi <g.sironi@elifesciences.org>
2023-02-06 10:57:17 +0000

    Extract and name function #2029

--- src/http/authentication/testing-passport-strategy.ts
+++ src/http/authentication/testing-passport-strategy.ts
@@ -1,14 +1,14 @@
-import { Strategy as LocalStrategy } from 'passport-local';
+import { Strategy as LocalStrategy, VerifyFunction } from 'passport-local';
 import { pipe } from 'fp-ts/function';
 import { userIdCodec } from '../../types/user-id';
 import { authenticateWithUserId } from '../authentication-and-logging-in-of-sciety-users';
 
-export const testingPassportStrategy = new LocalStrategy(
-  (candidateUserId, _password, cb) => {
-    pipe(
-      candidateUserId,
-      userIdCodec.decode,
-      authenticateWithUserId(cb),
-    );
-  },
-);
+const callback: VerifyFunction = (candidateUserId, _password, cb) => {
+  pipe(
+    candidateUserId,
+    userIdCodec.decode,
+    authenticateWithUserId(cb),
+  );
+};
+
+export const testingPassportStrategy = new LocalStrategy(callback);
