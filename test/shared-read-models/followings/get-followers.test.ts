d60114717c5cbc6f32c443800dc502ac8caf7480
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-07 08:39:12 +0000

    Implement all tests for get-followers #2040

--- test/shared-read-models/followings/get-followers.test.ts
+++ test/shared-read-models/followings/get-followers.test.ts
@@ -1,8 +1,10 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
+import { userFollowedEditorialCommunity, userUnfollowedEditorialCommunity } from '../../../src/domain-events';
 import { handleEvent, initialState } from '../../../src/shared-read-models/followings';
 import { getFollowers } from '../../../src/shared-read-models/followings/get-followers';
 import { arbitraryGroupId } from '../../types/group-id.helper';
+import { arbitraryUserId } from '../../types/user-id.helper';
 
 describe('get-users-following', () => {
   const groupId = arbitraryGroupId();
@@ -19,18 +21,62 @@ describe('get-users-following', () => {
   });
 
   describe('when 1 user has followed the group', () => {
-    it.todo('returns a list containing them as a follower');
+    const userId = arbitraryUserId();
+    const readmodel = pipe(
+      [
+        userFollowedEditorialCommunity(userId, groupId),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it.failing('returns a list containing them as a follower', () => {
+      expect(getFollowers(readmodel)(groupId)).toStrictEqual([userId]);
+    });
   });
 
   describe('when 1 user has followed then unfollowed the group', () => {
-    it.todo('returns empty list');
+    const userId = arbitraryUserId();
+    const readmodel = pipe(
+      [
+        userFollowedEditorialCommunity(userId, groupId),
+        userUnfollowedEditorialCommunity(userId, groupId),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it('returns an empty list', () => {
+      expect(getFollowers(readmodel)(groupId)).toStrictEqual([]);
+    });
   });
 
   describe('when 1 user has followed the group and another group', () => {
-    it.todo('returns a list containing them as a follower');
+    const userId = arbitraryUserId();
+    const readmodel = pipe(
+      [
+        userFollowedEditorialCommunity(userId, groupId),
+        userFollowedEditorialCommunity(userId, arbitraryGroupId()),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it.failing('returns a list containing them as a follower', () => {
+      expect(getFollowers(readmodel)(groupId)).toStrictEqual([userId]);
+    });
   });
 
   describe('when multiple users have followed the group', () => {
-    it.todo('returns a list containing them as followers');
+    const userId1 = arbitraryUserId();
+    const userId2 = arbitraryUserId();
+    const readmodel = pipe(
+      [
+        userFollowedEditorialCommunity(userId1, groupId),
+        userFollowedEditorialCommunity(userId2, groupId),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it.failing('returns a list containing them as followers', () => {
+      expect(getFollowers(readmodel)(groupId)).toStrictEqual([userId1, userId2]);
+    });
   });
 });
