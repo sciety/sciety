0f3c4448c77a7aa56f8ddfd7c38e5c3c1987b6c5
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-07 08:51:16 +0000

    Implement all tests for get-groups-followed-by #2040

---
+++ src/shared-read-models/followings/get-groups-followed-by.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ReadModel } from './handle-event';
+import { GroupId } from '../../types/group-id';
+import { UserId } from '../../types/user-id';
+
+type GetGroupsFollowedBy = (userId: UserId) => ReadonlyArray<GroupId>;
+
+// ts-unused-exports:disable-next-line
+export const getGroupsFollowedBy = (readmodel: ReadModel): GetGroupsFollowedBy => (userId) => [];
---
+++ test/shared-read-models/followings/get-groups-followed-by.test.ts
@@ -0,0 +1,72 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { userFollowedEditorialCommunity, userUnfollowedEditorialCommunity } from '../../../src/domain-events';
+import { handleEvent, initialState } from '../../../src/shared-read-models/followings';
+import { getGroupsFollowedBy } from '../../../src/shared-read-models/followings/get-groups-followed-by';
+import { arbitraryGroupId } from '../../types/group-id.helper';
+import { arbitraryUserId } from '../../types/user-id.helper';
+
+describe('get-group-ids-followed-by', () => {
+  const userId = arbitraryUserId();
+
+  describe('when multiple groups are followed', () => {
+    const groupId1 = arbitraryGroupId();
+    const groupId2 = arbitraryGroupId();
+    const readmodel = pipe(
+      [
+        userFollowedEditorialCommunity(userId, groupId1),
+        userFollowedEditorialCommunity(userId, groupId2),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it.failing('returns all groups in unspecified order', () => {
+      expect(getGroupsFollowedBy(readmodel)(userId)).toStrictEqual([groupId1, groupId2]);
+    });
+  });
+
+  describe('when a group is unfollowed', () => {
+    const groupId = arbitraryGroupId();
+    const readmodel = pipe(
+      [
+        userFollowedEditorialCommunity(userId, groupId),
+        userUnfollowedEditorialCommunity(userId, groupId),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it('does not list that group', () => {
+      expect(getGroupsFollowedBy(readmodel)(userId)).toStrictEqual([]);
+    });
+  });
+
+  describe('when a group is unfollowed and followed again', () => {
+    const groupId = arbitraryGroupId();
+    const readmodel = pipe(
+      [
+        userFollowedEditorialCommunity(userId, groupId),
+        userUnfollowedEditorialCommunity(userId, groupId),
+        userFollowedEditorialCommunity(userId, groupId),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it.failing('lists that group', () => {
+      expect(getGroupsFollowedBy(readmodel)(userId)).toStrictEqual([groupId]);
+    });
+  });
+
+  describe('when a different user has followed a group', () => {
+    const groupId = arbitraryGroupId();
+    const readmodel = pipe(
+      [
+        userFollowedEditorialCommunity(arbitraryUserId(), groupId),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it('that group is not returned', () => {
+      expect(getGroupsFollowedBy(readmodel)(userId)).toStrictEqual([]);
+    });
+  });
+});
