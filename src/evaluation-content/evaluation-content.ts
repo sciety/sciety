bb4e6ce55758d5db365771c50bb9ada327b241a3
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-09 15:18:01 +0000

    Reuse FetchReview from shared-ports #2055

--- src/evaluation-content/evaluation-content.ts
+++ src/evaluation-content/evaluation-content.ts
@@ -1,23 +1,17 @@
-import { URL } from 'url';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
-import * as DE from '../types/data-error';
-import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
+import { toHtmlFragment } from '../types/html-fragment';
 import { Page } from '../types/page';
 import { RenderPageError } from '../types/render-page-error';
 import * as RID from '../types/review-id';
+import { FetchReview } from '../shared-ports';
 
 type EvaluationContent = TE.TaskEither<RenderPageError, Page>;
 
 export const paramsCodec = t.type({
   reviewid: RID.reviewIdCodec,
 });
-
-type FetchReview = (id: RID.ReviewId) => TE.TaskEither<DE.DataError, {
-  fullText: HtmlFragment,
-  url: URL,
-}>;
 
 type Ports = {
   fetchReview: FetchReview,
