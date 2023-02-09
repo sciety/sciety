4198ca60980d1db10eb9df0f47c9426118b3d75c
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-09 14:53:12 +0000

    Fix compiler error #2055

--- src/shared-ports/fetch-review.ts
+++ src/shared-ports/fetch-review.ts
@@ -1,6 +1,6 @@
 import * as TE from 'fp-ts/TaskEither';
 import * as RI from '../types/review-id';
 import * as DE from '../types/data-error';
-import { Evaluation } from '../infrastructure/evaluation';
+import { Evaluation } from '../types/evaluation';
 
 export type FetchReview = (id: RI.ReviewId) => TE.TaskEither<DE.DataError, Evaluation>;
