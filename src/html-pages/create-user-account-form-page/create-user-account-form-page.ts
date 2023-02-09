3ad622eb9fa595dbba01acce8b7552cbcaa6887b
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-02-09 11:23:59 +0000

    Start improving language of form to help users fill it in #2051

--- src/html-pages/create-user-account-form-page/create-user-account-form-page.ts
+++ src/html-pages/create-user-account-form-page/create-user-account-form-page.ts
@@ -2,17 +2,17 @@ import { toHtmlFragment } from '../../types/html-fragment';
 import { Page } from '../../types/page';
 
 export const createUserAccountFormPage: Page = {
-  title: 'Create user account',
+  title: 'Sign up',
   content: toHtmlFragment(`
     <div class="create-user-account-form-wrapper">
       <header class="page-header">
-        <h1>Create user account</h1>
+        <h1>Sign up</h1>
       </header>
       <form action="/forms/create-user-account" method="post" class="create-user-account-form">
         <label for="displayName" class="create-user-account-form__label">Display name</label>
-        <input type="text" id="displayName" name="displayName" class="create-user-account-form__input">
+        <input type="text" id="displayName" name="displayName" placeholder="Ada Lovelace" class="create-user-account-form__input">
         <label for="handle" class="create-user-account-form__label">Handle</label>
-        <input type="text" id="handle" name="handle" class="create-user-account-form__input">
+        <input type="text" id="handle" name="handle" placeholder="ada_lovelace42" class="create-user-account-form__input">
         <button id="createAccountButton" class="create-user-account-form__submit">Sign Up</button>
         <button type="reset" class="create-user-account-form__reset">Clear form</button>
       </form>
