6ec806e58873cb0dde8353cc03f2173dba8836b1
David Moulton <d.moulton@elifesciences.org>
2023-02-10 15:42:26 +0000

    Add new hardcoded docmap for eLife https://github.com/elifesciences/enhanced-preprints-issues/issues/484

--- src/docmaps/hardcoded-elife-docmaps/index.ts
+++ src/docmaps/hardcoded-elife-docmaps/index.ts
@@ -19,6 +19,7 @@ import { hardcodedElifeArticle as elife_10_1101_2022_10_28_514241 } from './elif
 import { hardcodedElifeArticle as elife_10_1101_2022_12_01_518662 } from './elife-10.1101-2022.12.01.518662.docmap';
 import { hardcodedElifeArticle as elife_10_1101_2021_06_21_449261 } from './elife-10.1101-2021.06.21.449261.docmap';
 import { hardcodedElifeArticle as elife_10_1101_2022_07_22_501195 } from './elife-10.1101-2022.07.22.501195.docmap';
+import { hardcodedElifeArticle as elife_10_1101_2022_11_01_514717 } from './elife-10.1101-2022.11.01.514717.docmap';
 import { Docmap } from '../docmap/docmap-type';
 
 export const hardcodedDocmaps: Record<string, Docmap> = {
@@ -43,5 +44,5 @@ export const hardcodedDocmaps: Record<string, Docmap> = {
   '10.1101/2022.12.01.518662': elife_10_1101_2022_12_01_518662,
   '10.1101/2021.06.21.449261': elife_10_1101_2021_06_21_449261,
   '10.1101/2022.07.22.501195': elife_10_1101_2022_07_22_501195,
-
+  '10.1101/2022.11.01.514717': elife_10_1101_2022_11_01_514717,
 };
