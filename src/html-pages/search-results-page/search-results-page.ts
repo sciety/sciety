84d5f25ddb5b0a7cee411d1bae5b28017e14fa26
Giorgio Sironi <g.sironi@elifesciences.org>
2023-02-13 14:40:48 +0000

    Instrument main pipe of the page #2064

--- src/html-pages/search-results-page/search-results-page.ts
+++ src/html-pages/search-results-page/search-results-page.ts
@@ -1,5 +1,6 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { performance } from 'perf_hooks';
 import {
   fetchExtraDetails,
   Ports as FetchExtraDetailsPorts,
@@ -19,11 +20,31 @@ type SearchResultsPage = (ports: Ports) => (pageSize: number) => (params: Params
 
 export const searchResultsPage: SearchResultsPage = (ports) => (pageSize) => (params) => pipe(
   params,
+  (input) => {
+    console.log('before performAllSearches', performance.now());
+    return input;
+  },
   performAllSearches(ports)(pageSize),
+  TE.map((input) => {
+    console.log('before selectSubsetToDisplay', performance.now());
+    return input;
+  }),
   TE.map(selectSubsetToDisplay),
+  TE.map((input) => {
+    console.log('before fetchExtraDetails', performance.now());
+    return input;
+  }),
   TE.chainTaskK(fetchExtraDetails({
     ...ports,
     getLatestArticleVersionDate: getLatestArticleVersionDate(ports.findVersionsForArticleDoi),
   })),
+  TE.map((input) => {
+    console.log('before renderPage', performance.now());
+    return input;
+  }),
   TE.bimap(renderErrorPage, renderPage),
+  TE.map((input) => {
+    console.log('after renderPage', performance.now());
+    return input;
+  }),
 );
