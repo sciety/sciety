a05b67392a3a1296a399b9f07afa388f30b159dd
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-02-13 10:00:35 +0000

    Hardcode another elife docmap elifesciences/enhanced-preprints-issues#487 #2061

--- src/docmaps/hardcoded-elife-docmaps/index.ts
+++ src/docmaps/hardcoded-elife-docmaps/index.ts
@@ -21,6 +21,7 @@ import { hardcodedElifeArticle as elife_10_1101_2021_06_21_449261 } from './elif
 import { hardcodedElifeArticle as elife_10_1101_2022_07_22_501195 } from './elife-10.1101-2022.07.22.501195.docmap';
 import { hardcodedElifeArticle as elife_10_1101_2022_11_01_514717 } from './elife-10.1101-2022.11.01.514717.docmap';
 import { hardcodedElifeArticle as elife_10_1101_2022_12_07_519455 } from './elife-10.1101-2022.12.07.519455.docmap';
+import { hardcodedElifeArticle as elife_10_1101_2022_11_25_517922 } from './elife-10.1101-2022.11.25.517922.docmap';
 import { Docmap } from '../docmap/docmap-type';
 
 export const hardcodedDocmaps: Record<string, Docmap> = {
@@ -47,4 +48,5 @@ export const hardcodedDocmaps: Record<string, Docmap> = {
   '10.1101/2022.07.22.501195': elife_10_1101_2022_07_22_501195,
   '10.1101/2022.11.01.514717': elife_10_1101_2022_11_01_514717,
   '10.1101/2022.12.07.519455': elife_10_1101_2022_12_07_519455,
+  '10.1101/2022.11.25.517922': elife_10_1101_2022_11_25_517922,
 };
