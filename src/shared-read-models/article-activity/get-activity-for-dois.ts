fc4b5a9fce2271415c4eaac9f242b6cb828b2283
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-14 13:33:15 +0000

    Introduce a type for the other query #2065

--- src/shared-read-models/article-activity/get-activity-for-dois.ts
+++ src/shared-read-models/article-activity/get-activity-for-dois.ts
@@ -1,12 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { pipe } from 'fp-ts/function';
 import * as RA from 'fp-ts/ReadonlyArray';
+import { ArticleActivity } from '../../types/article-activity';
 import { Doi } from '../../types/doi';
 import { getActivityForDoi } from './get-activity-for-doi';
 import { ReadModel } from './handle-event';
 
+type GetActivityForDois = (articleIds: ReadonlyArray<Doi>) => ReadonlyArray<ArticleActivity>;
+
 // ts-unused-exports:disable-next-line
-export const getActivityForDois = (readmodel: ReadModel) => (articleIds: ReadonlyArray<Doi>) => pipe(
+export const getActivityForDois = (readmodel: ReadModel): GetActivityForDois => (articleIds) => pipe(
   articleIds,
   RA.map(getActivityForDoi(readmodel)),
 );
