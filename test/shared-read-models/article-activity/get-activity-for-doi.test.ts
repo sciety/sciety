1afdf86ec12cd0b62955677bf477092e347f1611
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-14 10:34:45 +0000

    Copy the old readmodel tests to be a test lists for the new readmodel #2065

---
+++ test/shared-read-models/article-activity/get-activity-for-doi.test.ts
@@ -0,0 +1,197 @@
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import {
+  articleAddedToList, articleRemovedFromList, evaluationRecorded,
+} from '../../../src/domain-events';
+import { userSavedArticle } from '../../../src/domain-events/user-saved-article-event';
+import { getActivityForDoi } from '../../../src/shared-read-models/article-activity-stateless';
+import { arbitraryDate } from '../../helpers';
+import { arbitraryArticleId } from '../../types/article-id.helper';
+import { arbitraryGroupId } from '../../types/group-id.helper';
+import { arbitraryListId } from '../../types/list-id.helper';
+import { arbitraryReviewId } from '../../types/review-id.helper';
+import { arbitraryUserId } from '../../types/user-id.helper';
+
+describe('get-activity-for-doi', () => {
+  const articleId = arbitraryArticleId();
+
+  describe('when an article has no evaluations and is in no list', () => {
+    describe('because it has never been added to a list', () => {
+      const articleActivity = pipe(
+        [],
+        getActivityForDoi(articleId),
+      );
+
+      it('article has no activity', () => {
+        expect(articleActivity).toStrictEqual({
+          articleId,
+          latestActivityDate: O.none,
+          evaluationCount: 0,
+          listMembershipCount: 0,
+        });
+      });
+    });
+
+    describe('because it has been added and removed from a list', () => {
+      const listId = arbitraryListId();
+      const articleActivity = pipe(
+        [
+          articleAddedToList(articleId, listId),
+          articleRemovedFromList(articleId, listId),
+        ],
+        getActivityForDoi(articleId),
+      );
+
+      it('has a listMemberShipCount of 0', () => {
+        expect(articleActivity.listMembershipCount).toBe(0);
+      });
+    });
+  });
+
+  describe('when an article has one or more evaluations', () => {
+    const earlierPublishedDate = new Date(1900);
+    const laterPublishedDate = new Date(2000);
+
+    describe('and the evaluations are recorded in order of publication', () => {
+      const articleActivity = pipe(
+        [
+          evaluationRecorded(
+            arbitraryGroupId(),
+            articleId,
+            arbitraryReviewId(),
+            [],
+            earlierPublishedDate,
+            arbitraryDate(),
+          ),
+          evaluationRecorded(
+            arbitraryGroupId(),
+            articleId,
+            arbitraryReviewId(),
+            [],
+            laterPublishedDate,
+            arbitraryDate(),
+          ),
+        ],
+        getActivityForDoi(articleId),
+      );
+
+      it('returns the activity for that article', () => {
+        expect(articleActivity).toStrictEqual(expect.objectContaining({
+          latestActivityDate: O.some(laterPublishedDate),
+          evaluationCount: 2,
+        }));
+      });
+    });
+
+    describe('and the evaluations are NOT recorded in order of publication', () => {
+      const articleActivity = pipe(
+        [
+          evaluationRecorded(
+            arbitraryGroupId(),
+            articleId,
+            arbitraryReviewId(),
+            [],
+            laterPublishedDate,
+            arbitraryDate(),
+          ),
+          evaluationRecorded(
+            arbitraryGroupId(),
+            articleId,
+            arbitraryReviewId(),
+            [],
+            earlierPublishedDate,
+            arbitraryDate(),
+          ),
+        ],
+        getActivityForDoi(articleId),
+      );
+
+      it('returns the activity for that article', () => {
+        expect(articleActivity).toStrictEqual(expect.objectContaining({
+          latestActivityDate: O.some(laterPublishedDate),
+          evaluationCount: 2,
+        }));
+      });
+    });
+  });
+
+  describe('when an article appears in one list', () => {
+    describe('and the article has been added to a single list and not removed', () => {
+      const articleActivity = pipe(
+        [
+          articleAddedToList(articleId, arbitraryListId()),
+        ],
+        getActivityForDoi(articleId),
+      );
+
+      it('has a listMemberShipCount of 1', () => {
+        expect(articleActivity.listMembershipCount).toBe(1);
+      });
+    });
+
+    describe('and the article was added and removed from a different list', () => {
+      const listAId = arbitraryListId();
+      const listBId = arbitraryListId();
+      const articleActivity = pipe(
+        [
+          articleAddedToList(articleId, listAId),
+          articleRemovedFromList(articleId, listAId),
+          articleAddedToList(articleId, listBId),
+        ],
+        getActivityForDoi(articleId),
+      );
+
+      it('has a listMemberShipCount of 1', () => {
+        expect(articleActivity.listMembershipCount).toBe(1);
+      });
+    });
+
+    describe('and the article was saved by a user with a legacy command', () => {
+      const articleActivity = pipe(
+        [
+          userSavedArticle(arbitraryUserId(), articleId),
+          articleAddedToList(articleId, arbitraryListId()),
+        ],
+        getActivityForDoi(articleId),
+      );
+
+      it('has a listMemberShipCount of 1', () => {
+        expect(articleActivity.listMembershipCount).toBe(1);
+      });
+    });
+
+    describe('added to a list, after being evaluated', () => {
+      const articleActivity = pipe(
+        [
+          evaluationRecorded(arbitraryGroupId(), articleId, arbitraryReviewId()),
+          articleAddedToList(articleId, arbitraryListId()),
+        ],
+        getActivityForDoi(articleId),
+      );
+
+      it('has a listMemberShipCount of 1', () => {
+        expect(articleActivity.listMembershipCount).toBe(1);
+      });
+
+      it('has an evaluationCount of 1', () => {
+        expect(articleActivity.evaluationCount).toBe(1);
+      });
+    });
+  });
+
+  describe('when an article appears in multiple lists', () => {
+    describe('in two different lists', () => {
+      const articleActivity = pipe(
+        [
+          articleAddedToList(articleId, arbitraryListId()),
+          articleAddedToList(articleId, arbitraryListId()),
+        ],
+        getActivityForDoi(articleId),
+      );
+
+      it('has a listMemberShipCount of 2', () => {
+        expect(articleActivity.listMembershipCount).toBe(2);
+      });
+    });
+  });
+});
---
+++ test/shared-read-models/article-activity/get-activity-for-dois.test.ts
@@ -0,0 +1,64 @@
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { evaluationRecorded } from '../../../src/domain-events';
+import { articleAddedToList } from '../../../src/domain-events/article-added-to-list-event';
+import { getActivityForDois } from '../../../src/shared-read-models/article-activity-stateless/get-activity-for-dois';
+import { arbitraryDate } from '../../helpers';
+import { arbitraryDoi } from '../../types/doi.helper';
+import { arbitraryGroupId } from '../../types/group-id.helper';
+import { arbitraryListId } from '../../types/list-id.helper';
+import { arbitraryReviewId } from '../../types/review-id.helper';
+
+describe('get-activity-for-dois', () => {
+  describe('when the articleIds are in the read model', () => {
+    it('returns the activity for those articles', () => {
+      const articleId1 = arbitraryDoi();
+      const articleId2 = arbitraryDoi();
+      const date1 = arbitraryDate();
+      const date2 = arbitraryDate();
+      const activity = pipe(
+        [
+          evaluationRecorded(arbitraryGroupId(), articleId1, arbitraryReviewId(), [], date1),
+          articleAddedToList(articleId1, arbitraryListId()),
+          evaluationRecorded(arbitraryGroupId(), articleId2, arbitraryReviewId(), [], date2),
+          articleAddedToList(articleId2, arbitraryListId()),
+        ],
+        getActivityForDois([articleId1, articleId2]),
+      );
+
+      expect(activity).toStrictEqual([
+        {
+          articleId: articleId1,
+          latestActivityDate: O.some(date1),
+          evaluationCount: 1,
+          listMembershipCount: 1,
+        },
+        {
+          articleId: articleId2,
+          latestActivityDate: O.some(date2),
+          evaluationCount: 1,
+          listMembershipCount: 1,
+        },
+      ]);
+    });
+  });
+
+  describe('when an articleId is not in the read model', () => {
+    it('returns empty activity for that article', () => {
+      const articleId = arbitraryDoi();
+      const activity = pipe(
+        [],
+        getActivityForDois([articleId]),
+      );
+
+      expect(activity).toStrictEqual([
+        {
+          articleId,
+          latestActivityDate: O.none,
+          evaluationCount: 0,
+          listMembershipCount: 0,
+        },
+      ]);
+    });
+  });
+});
