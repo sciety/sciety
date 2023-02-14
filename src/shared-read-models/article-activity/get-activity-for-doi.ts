fa1e06f4e8f195dc9afe098b2b61840ad9a460d6
LinaKind <l.kinduryte@elifesciences.org>
2023-02-14 13:29:34 +0000

    Provide a return type for the new query #2065

--- src/shared-read-models/article-activity/get-activity-for-doi.ts
+++ src/shared-read-models/article-activity/get-activity-for-doi.ts
@@ -3,11 +3,14 @@ import * as RM from 'fp-ts/ReadonlyMap';
 import * as O from 'fp-ts/Option';
 import * as S from 'fp-ts/string';
 import { pipe } from 'fp-ts/function';
+import { ArticleActivity } from '../../types/article-activity';
 import { Doi } from '../../types/doi';
 import { ReadModel } from './handle-event';
 
+type GetActivityForDoi = (articleId: Doi) => ArticleActivity;
+
 // ts-unused-exports:disable-next-line
-export const getActivityForDoi = (readmodel: ReadModel) => (articleId: Doi) => pipe(
+export const getActivityForDoi = (readmodel: ReadModel): GetActivityForDoi => (articleId) => pipe(
   readmodel,
   RM.lookup(S.Eq)(articleId.value),
   O.match(
