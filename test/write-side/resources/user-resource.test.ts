6c56b88d68103013ab648d07ca202652edc989ca
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-16 14:37:43 +0000

    Prepare a test list for a new User resource #2073

---
+++ test/write-side/resources/user-resource.test.ts
@@ -0,0 +1,17 @@
+describe('user-resource', () => {
+  describe('exists', () => {
+    describe('when the user exists', () => {
+      describe('with an identical handle', () => {
+        it.todo('returns the user');
+      });
+
+      describe('with a handle matching except for case', () => {
+        it.todo('returns the user');
+      });
+    });
+
+    describe('when the user does not exist', () => {
+      it.todo('returns not-found');
+    });
+  });
+});
