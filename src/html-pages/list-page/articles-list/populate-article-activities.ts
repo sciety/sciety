b118e331c003b0ad291b0c3c68274a670f15ebbe
David Moulton <d.moulton@elifesciences.org>
2023-02-16 10:19:05 +0000

    Remove unnecessary port #2065

--- src/html-pages/list-page/articles-list/populate-article-activities.ts
+++ src/html-pages/list-page/articles-list/populate-article-activities.ts
@@ -1,12 +1,9 @@
-import * as T from 'fp-ts/Task';
-import { DomainEvent } from '../../../domain-events';
 import { PageOfItems } from '../../../shared-components/paginate';
 import { ArticleActivity } from '../../../types/article-activity';
 import { Doi } from '../../../types/doi';
 import { GetActivityForDois } from '../../../shared-ports';
 
 export type Ports = {
-  getAllEvents: T.Task<ReadonlyArray<DomainEvent>>,
   getActivityForDois: GetActivityForDois,
 };
 
