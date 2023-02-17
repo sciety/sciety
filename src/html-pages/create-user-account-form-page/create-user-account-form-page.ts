5657b9de124a407a66275600d73eee63e53a4a71
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-17 10:43:21 +0000

    Ask for full name instead of display name #2075

--- src/html-pages/create-user-account-form-page/create-user-account-form-page.ts
+++ src/html-pages/create-user-account-form-page/create-user-account-form-page.ts
@@ -10,8 +10,8 @@ export const createUserAccountFormPage: Page = {
       </header>
       <form action="/forms/create-user-account" method="post" class="create-user-account-form">
         <h2>Sign up &ndash; Step 2 of 2</h2>
-        <label for="displayName" class="create-user-account-form__label">Create a display name</label>
-        <input type="text" id="displayName" name="displayName" placeholder="Ada Lovelace" class="create-user-account-form__input">
+        <label for="fullName" class="create-user-account-form__label">Full name</label>
+        <input type="text" id="fullName" name="fullName" placeholder="Ada Lovelace" class="create-user-account-form__input">
         <label for="handle" class="create-user-account-form__label">Create a handle</label>
         <input type="text" id="handle" name="handle" placeholder="ada_lovelace42" class="create-user-account-form__input">
         <button id="createAccountButton" class="create-user-account-form__submit">Sign Up</button>
