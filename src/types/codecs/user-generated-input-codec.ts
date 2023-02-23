14bf806cfe484dc49aee2d7102566bd828107aea
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-23 14:03:13 +0000

    Generate a type for user generated input #2090

--- src/types/codecs/user-generated-input-codec.ts
+++ src/types/codecs/user-generated-input-codec.ts
@@ -1,22 +1,16 @@
-import * as E from 'fp-ts/Either';
-import * as B from 'fp-ts/boolean';
-import { identity, pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 
 const regex = /^[^<>]+$/;
 
-export const userGeneratedInputCodec = (maxLength: number) => new t.Type(
-  'userGeneratedInputCodec',
-  (u): u is string => typeof u === 'string',
-  (u, c) => pipe(
-    t.string.validate(u, c),
-    E.chain((inputString) => pipe(
-      !!regex.exec(inputString) && inputString.length <= maxLength,
-      B.fold(
-        () => t.failure(u, c),
-        () => t.success(inputString),
-      ),
-    )),
-  ),
-  identity,
+type UserGeneratedInputBrand = {
+  readonly UserGeneratedInput: unique symbol,
+};
+
+export const userGeneratedInputCodec = (maxLength: number) => t.brand(
+  t.string,
+  (input): input is t.Branded<string, UserGeneratedInputBrand> => !!regex.exec(input) && input.length <= maxLength,
+  'UserGeneratedInput',
 );
+
+// ts-unused-exports:disable-next-line
+export type UserGeneratedInput = t.TypeOf<ReturnType<typeof userGeneratedInputCodec>>;
