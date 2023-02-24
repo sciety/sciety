d510b255baefa846179d0bc5198deed2f450eea5
LinaKind <l.kinduryte@elifesciences.org>
2023-02-24 14:18:27 +0000

    Remove unused dummy logger

--- test/dummy-logger.ts
+++ test/dummy-logger.ts
@@ -3,5 +3,3 @@ import * as L from '../src/infrastructure/logger';
 const loggerStub: L.Logger = () => {};
 
 export const dummyLogger = Object.assign(loggerStub, { bindToRequestId: () => loggerStub });
-
-export const dummyLoggerIO = L.loggerIO(dummyLogger);
