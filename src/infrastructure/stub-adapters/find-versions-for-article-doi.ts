3d89ff55ed9e265672828e044be9fc10d1f19ec0
LinaKind <l.kinduryte@elifesciences.org>
2023-02-24 11:36:36 +0000

    Extract stub to its own file

---
+++ src/infrastructure/stub-adapters/find-versions-for-article-doi.ts
@@ -0,0 +1,23 @@
+import * as T from 'fp-ts/Task';
+import * as O from 'fp-ts/Option';
+import { URL } from 'url';
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
+
+type ArticleVersionInformation = {
+  source: URL,
+  publishedAt: Date,
+  version: number,
+};
+
+export const findVersionsForArticleDoi = () => T.of(O.some([
+  {
+    source: new URL('https://www.biorxiv.org/content/10.1101/2022.08.20.504530v1'),
+    publishedAt: new Date('1970'),
+    version: 1,
+  },
+  {
+    source: new URL('https://www.biorxiv.org/content/10.1101/2022.08.20.504530v2'),
+    publishedAt: new Date('1980'),
+    version: 2,
+  },
+] as RNEA.ReadonlyNonEmptyArray<ArticleVersionInformation>));
