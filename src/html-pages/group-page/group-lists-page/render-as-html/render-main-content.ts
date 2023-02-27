d34e3507cf68ca507444523695b7a1263ba0c40d
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-27 14:00:12 +0000

    Remove more unreachable code from the lists tab #2098

--- src/html-pages/group-page/group-lists-page/render-as-html/render-main-content.ts
+++ src/html-pages/group-page/group-lists-page/render-as-html/render-main-content.ts
@@ -1,29 +1,16 @@
 import { pipe } from 'fp-ts/function';
 import { renderTabs } from '../../../../shared-components/tabs';
 import { HtmlFragment } from '../../../../types/html-fragment';
-import { renderFollowers } from './render-followers';
 import { ViewModel } from '../view-model';
 import { tabList } from './tab-list';
 import { renderListOfListCardsWithFallback } from './render-list-of-list-cards-with-fallback';
-import { renderAboutTab } from './render-about-tab';
 
 const tabProps = (viewmodel: ViewModel) => ({
   tabList: tabList(viewmodel),
   activeTabIndex: 0,
 });
 
-const renderActiveTabContents = (viewmodel: ViewModel) => {
-  switch (viewmodel.activeTab.selector) {
-    case 'lists':
-      return renderListOfListCardsWithFallback(viewmodel.activeTab.lists);
-    case 'about':
-      return renderAboutTab(viewmodel.activeTab);
-    default:
-      return renderFollowers(viewmodel.activeTab);
-  }
-};
-
 export const renderMainContent = (viewmodel: ViewModel): HtmlFragment => pipe(
-  renderActiveTabContents(viewmodel),
+  renderListOfListCardsWithFallback(viewmodel.activeTab.lists),
   renderTabs(tabProps(viewmodel)),
 );
