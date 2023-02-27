97a3325559e8a2b43a5c7da93483b23a7cf9269c
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-27 09:48:21 +0000

    Move some rendering out of a viewmodel construction module #2012

--- src/html-pages/group-page/about/about.ts
+++ src/html-pages/group-page/about/about.ts
@@ -4,10 +4,7 @@ import { pipe } from 'fp-ts/function';
 import { toOurListsViewModel } from './to-our-lists-view-model';
 import { FetchStaticFile } from '../../../shared-ports';
 import * as DE from '../../../types/data-error';
-import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 import { ContentModel } from '../content-model';
-import { renderOurLists } from '../render-as-html/render-our-lists';
-import { renderDescription } from '../render-as-html/render-description';
 import { AboutTab } from '../view-model';
 
 export type Ports = {
@@ -28,14 +25,3 @@ export const constructAboutTab = (
   },
   sequenceS(TE.ApplyPar),
 );
-
-export const renderAboutTab = (viewmodel: AboutTab): HtmlFragment => toHtmlFragment(`
-  <div class="group-page-about">
-    <section>
-      ${renderOurLists(viewmodel.lists)}
-    </section>
-    <section>
-      ${renderDescription(viewmodel.markdown)}
-    </section>
-  </div>
-`);
--- src/html-pages/group-page/render-as-html/render-main-content.ts
+++ src/html-pages/group-page/render-as-html/render-main-content.ts
@@ -1,11 +1,11 @@
 import { pipe } from 'fp-ts/function';
 import { renderTabs } from '../../../shared-components/tabs';
 import { HtmlFragment } from '../../../types/html-fragment';
-import { renderAboutTab } from '../about/about';
 import { renderFollowers } from './render-followers';
 import { ViewModel } from '../view-model';
 import { tabList } from './tab-list';
 import { renderListOfListCardsWithFallback } from './render-list-of-list-cards-with-fallback';
+import { renderAboutTab } from './render-about-tab';
 
 const tabProps = (viewmodel: ViewModel) => ({
   tabList: tabList(viewmodel),
