8733bc4691046d478a8d9e86c2e0bccd3b89b712
Kevin Rutherford <kevin@rutherford-software.com>
2023-02-27 13:01:33 +0000

    Remove unreachable code #2098

--- src/html-pages/group-page/render-as-html/render-as-html.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '../../../types/page';
-import { ViewModel } from '../view-model';
-import { renderPage } from './render-page';
-
-// ts-unused-exports:disable-next-line
-export const renderAsHtml = (viewmodel: ViewModel): Page => ({
-  title: viewmodel.group.name,
-  content: renderPage(viewmodel),
-});
