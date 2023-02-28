84694dd7d06b7a62a6916b927fb1558152bc781d
ShaneAlsop <99194567+ShaneAlsop@users.noreply.github.com>
2023-02-28 10:58:37 +0000

    Update hardcoded-evaluation-cards.ts

--- src/html-pages/home-page/cards/hardcoded-evaluation-cards.ts
+++ src/html-pages/home-page/cards/hardcoded-evaluation-cards.ts
@@ -8,19 +8,19 @@ export const card1 = {
 };
 
 export const card2 = {
-  articleId: '10.1101/2022.04.21.22274150',
-  articleTitle: 'Epidemiological impact and cost-effectiveness analysis of COVID-19 vaccination in Kenya',
-  date: new Date('2023-01-03'),
-  groupSlug: 'rapid-reviews-covid-19',
-  groupName: 'Rapid Reviews COVID-19',
-  avatarPath: '/static/groups/rapid-reviews-covid-19--5142a5bc-6b18-42b1-9a8d-7342d7d17e94.png',
+  articleId: '10.1101/2023.01.13.523998',
+  articleTitle: 'Maintained imbalance of triglycerides, apolipoproteins, energy metabolites and cytokines in long-term COVID-19 syndrome (LTCS) patients',
+  date: new Date('2023-02-20'),
+  groupSlug: 'peerref',
+  groupName: 'PeerRef',
+  avatarPath: '/static/groups/peerref--b5f31635-d32b-4df9-92a5-0325a1524343.png',
 };
 
 export const card3 = {
-  articleId: '10.1101/2022.12.15.520598',
-  articleTitle: 'Mapping current and future thermal limits to suitability for malaria transmission by the invasive mosquito Anopheles stephens i',
-  date: new Date('2023-01-02'),
-  groupSlug: 'prelights',
-  groupName: 'preLights',
-  avatarPath: '/static/groups/prelights--f97bd177-5cb6-4296-8573-078318755bf2.jpg',
+  articleId: '10.1101/2023.01.11.523614',
+  articleTitle: 'Characterization of the REC114-MEI4-IHO1 complex regulating meiotic DNA double-strand break formation',
+  date: new Date('2023-02-23'),
+  groupSlug: 'review-commons',
+  groupName: 'Review Commons',
+  avatarPath: '/static/groups/review-commons--316db7d9-88cc-4c26-b386-f067e0f56334.jpg',
 };
