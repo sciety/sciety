b177b40590700a25d5eb158609bc9d9cb0734788
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-02-28 15:33:59 +0000

    Stop checking for data on state that is no longer stored there #2104

--- src/http/require-logged-in-user.ts
+++ src/http/require-logged-in-user.ts
@@ -3,21 +3,13 @@ import { Middleware, ParameterizedContext } from 'koa';
 import { pipe } from 'fp-ts/function';
 import { getLoggedInScietyUser, Ports as GetLoggedInScietyUserPorts } from './authentication-and-logging-in-of-sciety-users';
 
-type State = {
-  targetFragmentId?: string,
-};
-
-export const constructRedirectUrl = (context: ParameterizedContext<State>): string => {
-  const result = context.request.headers.referer ?? '/';
-  if (context.state.targetFragmentId) {
-    return `${result}#${context.state.targetFragmentId}`;
-  }
-  return result;
-};
+export const constructRedirectUrl = (context: ParameterizedContext): string => (
+  context.request.headers.referer ?? '/'
+);
 
 export const requireLoggedInUser = (
   adapters: GetLoggedInScietyUserPorts,
-): Middleware<State> => async (context, next) => {
+): Middleware => async (context, next) => {
   await pipe(
     getLoggedInScietyUser(adapters, context),
     O.match(
