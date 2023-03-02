4327736a70bff76efff297fd26034e85b02ef2d4
Kevin Rutherford <kevin@rutherford-software.com>
2023-03-02 07:57:10 +0000

    Add redundant information for tabs to one group page #2098

--- src/html-pages/group-page/group-lists-page/construct-view-model/construct-view-model.ts
+++ src/html-pages/group-page/group-lists-page/construct-view-model/construct-view-model.ts
@@ -3,12 +3,36 @@ import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
-import { GetGroupBySlug, IsFollowing } from '../../../../shared-ports';
+import * as RA from 'fp-ts/ReadonlyArray';
+import {
+  GetFollowers, GetGroupBySlug, IsFollowing, SelectAllListsOwnedBy,
+} from '../../../../shared-ports';
 import { userIdCodec } from '../../../../types/user-id';
 import * as DE from '../../../../types/data-error';
-import { ViewModel } from '../view-model';
+import { TabsViewModel, ViewModel } from '../view-model';
 import { findFollowers, Ports as FindFollowersPorts } from './find-followers';
 import { constructListCards, Ports as ConstructListCardsPorts } from './construct-list-cards';
+import { Group } from '../../../../types/group';
+import * as LOID from '../../../../types/list-owner-id';
+
+type TabsViewModelPorts = {
+  getFollowers: GetFollowers,
+  selectAllListsOwnedBy: SelectAllListsOwnedBy,
+};
+
+const constructTabsViewModel = (ports: TabsViewModelPorts, group: Group): TabsViewModel => ({
+  groupSlug: group.slug,
+  listCount: pipe(
+    group.id,
+    LOID.fromGroupId,
+    ports.selectAllListsOwnedBy,
+    RA.size,
+  ),
+  followerCount: pipe(
+    ports.getFollowers(group.id),
+    RA.size,
+  ),
+});
 
 export type Ports = FindFollowersPorts & ConstructListCardsPorts & {
   getGroupBySlug: GetGroupBySlug,
@@ -41,6 +65,7 @@ export const constructViewModel: ConstructViewModel = (ports) => (params) => pip
       followers: findFollowers(ports)(group.id),
       lists: constructListCards(ports, group),
       listCards: constructListCards(ports, group),
+      tabs: constructTabsViewModel(ports, group),
     },
   )),
   TE.fromOption(() => DE.notFound),
--- src/html-pages/group-page/group-lists-page/view-model.ts
+++ src/html-pages/group-page/group-lists-page/view-model.ts
@@ -4,6 +4,12 @@ import { UserId } from '../../../types/user-id';
 import { PageHeaderViewModel } from '../common-components/page-header';
 import { TabListViewModel } from '../common-components/tab-list';
 
+export type TabsViewModel = {
+  groupSlug: string,
+  listCount: number,
+  followerCount: number,
+};
+
 export type Follower = {
   userId: UserId,
   listCount: number,
@@ -14,4 +20,5 @@ export type ViewModel = PageHeaderViewModel & TabListViewModel & {
   followers: ReadonlyArray<Follower>,
   group: Group,
   listCards: ReadonlyArray<ListCardViewModel>,
+  tabs: TabsViewModel,
 };
