9f90f3ee0cf93613f7c39b003badc38f987ca358
David Moulton <d.moulton@elifesciences.org>
2023-03-03 14:43:48 +0000

    Derive type ready for use

--- src/http/environment-variables-codec.ts
+++ src/http/environment-variables-codec.ts
@@ -4,3 +4,6 @@ import * as tt from 'io-ts-types';
 export const environmentVariablesCodec = t.type({
   APP_ORIGIN: tt.NonEmptyString,
 });
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type EnvironmentVariables = t.TypeOf<typeof environmentVariablesCodec>;
