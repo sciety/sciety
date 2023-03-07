1f0a15895b7e7f043c27eff17e701f0d9a0c5dd8
Kevin Rutherford <kevin@rutherford-software.com>
2023-03-07 15:09:14 +0000

    Create a test list for the toggle #2115

---
+++ test/html-pages/user-page/construct-view-model/construct-lists-tab.test.ts
@@ -0,0 +1,17 @@
+describe('construct-lists-tab', () => {
+  describe('showCreateNewList', () => {
+    describe('when there is a logged in user', () => {
+      describe('and the logged in user also owns the page', () => {
+        it.todo('the button should be shown');
+      });
+
+      describe('and the logged in user does not own the page', () => {
+        it.todo('the button should not be shown');
+      });
+    });
+
+    describe('when there is no logged in user', () => {
+      it.todo('the button should not be shown');
+    });
+  });
+});
