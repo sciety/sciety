ccaa3e185ba18e4a43a71d8cb26f2fc5be016f43
Kevin Rutherford <kevin@rutherford-software.com>
2023-03-07 08:11:14 +0000

    Write a test list for adding a second user list #2114

---
+++ test/coupling/create-user-list.test.ts
@@ -0,0 +1,15 @@
+describe('create user list', () => {
+  describe('given a user who is following a group', () => {
+    describe('when the user creates a new list', () => {
+      describe('on the user-lists page', () => {
+        it.todo('the tabs count the list');
+
+        it.todo('there is a card for the list');
+      });
+
+      describe('on the group-followers page', () => {
+        it.todo('the user card counts the extra list');
+      });
+    });
+  });
+});
