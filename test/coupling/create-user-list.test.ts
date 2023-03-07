1918c7dd7d9695d6c6801cd0c7ea1df37e8b52d7
Kevin Rutherford <kevin@rutherford-software.com>
2023-03-07 08:20:05 +0000

    Express intention with a test expectation #2114

--- test/coupling/create-user-list.test.ts
+++ test/coupling/create-user-list.test.ts
@@ -1,14 +1,38 @@
+import { ViewModel as GroupFollowersPage } from '../../src/html-pages/group-page/group-followers-page/view-model';
+
 describe('create user list', () => {
   describe('given a user who is following a group', () => {
+    beforeEach(() => {
+      console.log('COMMAND: create user');
+      console.log('COMMAND: create group');
+      console.log('COMMAND: user follows group');
+    });
+
     describe('when the user creates a new list', () => {
+      beforeEach(() => {
+        console.log('COMMAND: create user list');
+      });
+
       describe('on the user-lists page', () => {
+        beforeEach(() => {
+          console.log('VIEWMODEL: user-lists page');
+        });
+
         it.todo('the tabs count the list');
 
         it.todo('there is a card for the list');
       });
 
       describe('on the group-followers page', () => {
-        it.todo('the user card counts the extra list');
+        let groupFollowersPage: GroupFollowersPage;
+
+        beforeEach(() => {
+          console.log('VIEWMODEL: group-followers page');
+        });
+
+        it.failing('the user card counts the extra list', () => {
+          expect(groupFollowersPage.followers[0].listCount).toBe(2);
+        });
       });
     });
   });
