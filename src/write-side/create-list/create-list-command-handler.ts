07ff35919eb7b32b9eda328e7ad6956f85781e6d
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-03-07 13:54:21 +0000

    Move code out of index file #2115

---
+++ src/write-side/create-list/create-list-command-handler.ts
@@ -0,0 +1,19 @@
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import * as T from 'fp-ts/Task';
+import { executeCreateListCommand } from './execute-create-list-command';
+import { CommitEvents, CreateList, GetAllEvents } from '../../shared-ports';
+import { replayAllLists } from '../resources/all-lists';
+
+type Ports = {
+  commitEvents: CommitEvents,
+  getAllEvents: GetAllEvents,
+};
+
+export const createListCommandHandler = (ports: Ports): CreateList => (command) => pipe(
+  ports.getAllEvents,
+  T.map(replayAllLists),
+  T.map(executeCreateListCommand(command)),
+  T.chain(ports.commitEvents),
+  TE.rightTask,
+);
