c8fecd745d79fc108ea5c94c55ad6d1c32ed4cab
David Moulton <d.moulton@elifesciences.org>
2023-03-08 14:39:01 +0000

    Remove hard coding of user list count in page meta descriptions #2118

--- src/html-pages/user-page/render-as-html/render-description.ts
+++ src/html-pages/user-page/render-as-html/render-description.ts
@@ -1,9 +1,5 @@
-import { pipe } from 'fp-ts/function';
 import { ViewModel } from '../view-model';
 
 const pluralise = (count: number) => (count === 1 ? '' : 's');
 
-export const renderDescription = (viewmodel: ViewModel): string => pipe(
-  viewmodel.groupIds.length,
-  (groupCount) => `1 list | Following ${groupCount} group${pluralise(groupCount)}`,
-);
+export const renderDescription = (viewmodel: ViewModel): string => `${viewmodel.listCount} list${pluralise(viewmodel.listCount)} | Following ${viewmodel.groupIds.length} group${pluralise(viewmodel.groupIds.length)}`;
