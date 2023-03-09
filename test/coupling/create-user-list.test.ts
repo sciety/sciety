8ae261e43b60954f893f350dcc02ebc2d25e5360
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-03-09 11:55:32 +0000

    Wire up dispatcher to event store #2121

--- test/coupling/create-user-list.test.ts
+++ test/coupling/create-user-list.test.ts
@@ -3,6 +3,7 @@ import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
 import * as T from 'fp-ts/Task';
+import * as RA from 'fp-ts/ReadonlyArray';
 import {
   constructViewModel as constructGroupFollowersPage,
   Ports as GroupFollowersPagePorts,
@@ -14,24 +15,45 @@ import { arbitraryGroup } from '../types/group.helper';
 import { arbitraryUserDetails } from '../types/user-details.helper';
 import { DomainEvent } from '../../src/domain-events';
 import { GetAllEvents, CommitEvents } from '../../src/shared-ports';
+import { CommandResult } from '../../src/types/command-result';
 
 type EventStore = {
   getAllEvents: GetAllEvents,
   commitEvents: CommitEvents,
 };
 
+const commitEvents = (
+  inMemoryEvents: Array<DomainEvent>,
+  dispatchToAllReadModels: (events: ReadonlyArray<DomainEvent>) => void,
+): CommitEvents => (events) => pipe(
+  events,
+  RA.match(
+    () => ('no-events-created' as CommandResult),
+    (es) => {
+      pipe(
+        es,
+        RA.map((event) => { inMemoryEvents.push(event); return event; }),
+      );
+      dispatchToAllReadModels(es);
+      return 'events-created' as CommandResult;
+    },
+  ),
+  T.of,
+);
+
 describe('create user list', () => {
   let queries: GroupFollowersPagePorts;
+  let dispatchToAllReadModels: (events: ReadonlyArray<DomainEvent>) => void;
   let allEvents: Array<DomainEvent>;
   let eventStore: EventStore;
 
   beforeEach(() => {
     allEvents = [];
+    ({ dispatchToAllReadModels, queries } = dispatcher());
     eventStore = {
       getAllEvents: T.of(allEvents),
-      commitEvents: () => T.of('no-events-created' as const),
+      commitEvents: commitEvents(allEvents, dispatchToAllReadModels),
     };
-    ({ queries } = dispatcher());
   });
 
   describe('given a user who is following a group', () => {
@@ -68,7 +90,7 @@ describe('create user list', () => {
         it.todo('there is a card for the list');
       });
 
-      describe.skip('on the group-followers page', () => {
+      describe('on the group-followers page', () => {
         let groupFollowersPage: GroupFollowersPage;
 
         beforeEach(async () => {
