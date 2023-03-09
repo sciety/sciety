453e204982159e830058ef5950021ceaf5c8f8d6
Giorgio Sironi <g.sironi@elifesciences.org>
2023-03-09 11:16:48 +0000

    Duplicate form to be able to change it #2120

--- src/html-pages/article-page/render-as-html/render-save-article.ts
+++ src/html-pages/article-page/render-as-html/render-save-article.ts
@@ -4,6 +4,7 @@ import { renderSaveForm } from '../../../write-side/save-article/render-save-for
 import { Doi } from '../../../types/doi';
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 import { ListId } from '../../../types/list-id';
+import { renderSaveMultipleListsForm } from '../../../write-side/save-article/render-save-multiple-lists-form';
 
 type ViewModel = {
   doi: Doi,
@@ -13,7 +14,7 @@ type ViewModel = {
 export const renderSaveArticle = (viewmodel: ViewModel): HtmlFragment => pipe(
   viewmodel.isArticleInList,
   O.fold(
-    () => (process.env.EXPERIMENT_ENABLED === 'true' ? renderSaveForm(viewmodel.doi) : renderSaveForm(viewmodel.doi)),
+    () => (process.env.EXPERIMENT_ENABLED === 'true' ? renderSaveMultipleListsForm(viewmodel.doi) : renderSaveForm(viewmodel.doi)),
     (listId) => `
       <a class="saved-to-list" href="/lists/${listId}">
         <img src="/static/images/playlist_add_check-24px.svg" alt="" class="saved-to-list__icon">
