67695e4a7542e9c77104dec5a72df1ee7cd60c45
Giorgio Sironi <g.sironi@elifesciences.org>
2023-03-13 14:37:49 +0000

    Write test list while looking at save-article-to-list test #2120

---
+++ test/html-pages/article-page/construct-view-model/construct-view-model.test.ts
@@ -0,0 +1,9 @@
+describe('construct-view-model', () => {
+  describe('when the article is saved to a list', () => {
+    it.todo('list management has access to list id');
+
+    it.todo('list management has access to list name');
+
+    it.todo('list management marks the article as being saved in the list');
+  });
+});
