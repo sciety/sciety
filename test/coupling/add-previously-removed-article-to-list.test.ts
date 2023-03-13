6e14f8fac697553da5123db6c69db96e141441c2
LinaKind <l.kinduryte@elifesciences.org>
2023-03-13 10:18:49 +0000

    Write a test list #2120

---
+++ test/coupling/add-previously-removed-article-to-list.test.ts
@@ -0,0 +1,11 @@
+describe('add previously removed article to list', () => {
+  describe('given an article that has been removed from a list', () => {
+    describe('when that article is added to the list again', () => {
+      it.todo('is marked as saved on the article page');
+
+      it.todo('appears on the list page');
+
+      it.todo('sciety feed shows the article being added to the list');
+    });
+  });
+});
