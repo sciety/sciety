5352785ab7f350c4805a71490a1726cc21f81084
Kevin Rutherford <kevin@rutherford-software.com>
2023-03-13 10:39:25 +0000

    Write partially valid expectation for a coupling test #2120

--- test/coupling/add-previously-removed-article-to-list.test.ts
+++ test/coupling/add-previously-removed-article-to-list.test.ts
@@ -1,7 +1,30 @@
+import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
+import * as TE from 'fp-ts/TaskEither';
+import { constructViewModel as constructArticlePage, Ports } from '../../src/html-pages/article-page/construct-view-model';
+import { shouldNotBeCalled } from '../should-not-be-called';
+import { arbitraryArticleId } from '../types/article-id.helper';
+import { arbitraryUserId } from '../types/user-id.helper';
+
 describe('add previously removed article to list', () => {
   describe('given an article that has been removed from a list', () => {
     describe('when that article is added to the list again', () => {
-      it.todo('is marked as saved on the article page');
+      it.failing('is marked as saved on the article page as seen by the list owner', async () => {
+        const adapters = {
+        } as Ports;
+        const articlePage = await pipe(
+          {
+            doi: arbitraryArticleId(),
+            user: O.some({ id: arbitraryUserId() }),
+          },
+          constructArticlePage(adapters),
+          TE.getOrElse(shouldNotBeCalled),
+        )();
+
+        expect(articlePage.userListManagement).toStrictEqual(O.some(expect.objectContaining({
+          isArticleInList: true,
+        })));
+      });
 
       it.todo('appears on the list page');
 
