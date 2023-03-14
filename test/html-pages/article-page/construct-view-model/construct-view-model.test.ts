cd432c4ce2fdad8e8b90b860f13c98a6e9733e9f
LinaKind <l.kinduryte@elifesciences.org>
2023-03-14 13:49:02 +0000

    Remove duplication in test set up #2132

--- test/html-pages/article-page/construct-view-model/construct-view-model.test.ts
+++ test/html-pages/article-page/construct-view-model/construct-view-model.test.ts
@@ -1,6 +1,7 @@
 import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
+import { UserDetails } from '../../../../src/types/user-details';
 import { arbitraryArticleId } from '../../../types/article-id.helper';
 import { shouldNotBeCalled } from '../../../should-not-be-called';
 import { constructViewModel, Ports } from '../../../../src/html-pages/article-page/construct-view-model';
@@ -24,104 +25,109 @@ describe('construct-view-model', () => {
     };
   });
 
-  describe('when the article is not saved to any user list', () => {
-    let list: List;
-    let viewModel: ViewModel;
+  describe('when the user is logged in', () => {
+    let userDetails: UserDetails;
 
     beforeEach(async () => {
-      const userDetails = arbitraryUserDetails();
-      const articleId = arbitraryArticleId();
-      await framework.commandHelpers.createUserAccount(userDetails);
-      // eslint-disable-next-line prefer-destructuring
-      list = framework.queries.selectAllListsOwnedBy(LOID.fromUserId(userDetails.id))[0];
-      viewModel = await pipe(
-        {
-          doi: articleId,
-          user: O.some({ id: userDetails.id }),
-        },
-        constructViewModel(adapters),
-        TE.getOrElse(shouldNotBeCalled),
-      )();
+      userDetails = arbitraryUserDetails();
     });
 
-    it('list management has access to the default user list id', () => {
-      expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ listId: list.id })));
+    describe('when the article is not saved to any user list', () => {
+      let list: List;
+      let viewModel: ViewModel;
+
+      beforeEach(async () => {
+        const articleId = arbitraryArticleId();
+        await framework.commandHelpers.createUserAccount(userDetails);
+        // eslint-disable-next-line prefer-destructuring
+        list = framework.queries.selectAllListsOwnedBy(LOID.fromUserId(userDetails.id))[0];
+        viewModel = await pipe(
+          {
+            doi: articleId,
+            user: O.some({ id: userDetails.id }),
+          },
+          constructViewModel(adapters),
+          TE.getOrElse(shouldNotBeCalled),
+        )();
+      });
+
+      it('list management has access to the default user list id', () => {
+        expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ listId: list.id })));
+      });
+
+      it('list management has access to the default user list name', () => {
+        expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ listName: list.name })));
+      });
+
+      it('list management marks the article as not being saved in the default user list', () => {
+        expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ isArticleInList: false })));
+      });
     });
 
-    it('list management has access to the default user list name', () => {
-      expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ listName: list.name })));
+    describe('when the article is saved to the default user list', () => {
+      let list: List;
+      let viewModel: ViewModel;
+
+      beforeEach(async () => {
+        const articleId = arbitraryArticleId();
+        await framework.commandHelpers.createUserAccount(userDetails);
+        // eslint-disable-next-line prefer-destructuring
+        list = framework.queries.selectAllListsOwnedBy(LOID.fromUserId(userDetails.id))[0];
+        await framework.commandHelpers.addArticleToList(articleId, list.id);
+        viewModel = await pipe(
+          {
+            doi: articleId,
+            user: O.some({ id: userDetails.id }),
+          },
+          constructViewModel(adapters),
+          TE.getOrElse(shouldNotBeCalled),
+        )();
+      });
+
+      it('list management has access to list id', async () => {
+        expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ listId: list.id })));
+      });
+
+      it('list management has access to list name', () => {
+        expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ listName: list.name })));
+      });
+
+      it('list management marks the article as being saved in the list', () => {
+        expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ isArticleInList: true })));
+      });
     });
 
-    it('list management marks the article as not being saved in the default user list', () => {
-      expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ isArticleInList: false })));
-    });
-  });
+    describe('when the article is saved to another user list', () => {
+      let list: List;
+      let viewModel: ViewModel;
 
-  describe('when the article is saved to the default user list', () => {
-    let list: List;
-    let viewModel: ViewModel;
+      beforeEach(async () => {
+        const articleId = arbitraryArticleId();
+        await framework.commandHelpers.createUserAccount(userDetails);
+        list = { ...arbitraryList(), ownerId: LOID.fromUserId(userDetails.id) };
+        await framework.commandHelpers.createList(list);
+        await framework.commandHelpers.addArticleToList(articleId, list.id);
+        viewModel = await pipe(
+          {
+            doi: articleId,
+            user: O.some({ id: userDetails.id }),
+          },
+          constructViewModel(adapters),
+          TE.getOrElse(shouldNotBeCalled),
+        )();
+      });
 
-    beforeEach(async () => {
-      const userDetails = arbitraryUserDetails();
-      const articleId = arbitraryArticleId();
-      await framework.commandHelpers.createUserAccount(userDetails);
-      // eslint-disable-next-line prefer-destructuring
-      list = framework.queries.selectAllListsOwnedBy(LOID.fromUserId(userDetails.id))[0];
-      await framework.commandHelpers.addArticleToList(articleId, list.id);
-      viewModel = await pipe(
-        {
-          doi: articleId,
-          user: O.some({ id: userDetails.id }),
-        },
-        constructViewModel(adapters),
-        TE.getOrElse(shouldNotBeCalled),
-      )();
-    });
+      it('list management has access to list id', () => {
+        expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ listId: list.id })));
+      });
 
-    it('list management has access to list id', async () => {
-      expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ listId: list.id })));
-    });
+      it('list management has access to list name', () => {
+        expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ listName: list.name })));
+      });
 
-    it('list management has access to list name', () => {
-      expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ listName: list.name })));
-    });
-
-    it('list management marks the article as being saved in the list', () => {
-      expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ isArticleInList: true })));
-    });
-  });
-
-  describe('when the article is saved to another user list', () => {
-    let list: List;
-    let viewModel: ViewModel;
-
-    beforeEach(async () => {
-      const userDetails = arbitraryUserDetails();
-      const articleId = arbitraryArticleId();
-      await framework.commandHelpers.createUserAccount(userDetails);
-      list = { ...arbitraryList(), ownerId: LOID.fromUserId(userDetails.id) };
-      await framework.commandHelpers.createList(list);
-      await framework.commandHelpers.addArticleToList(articleId, list.id);
-      viewModel = await pipe(
-        {
-          doi: articleId,
-          user: O.some({ id: userDetails.id }),
-        },
-        constructViewModel(adapters),
-        TE.getOrElse(shouldNotBeCalled),
-      )();
-    });
-
-    it('list management has access to list id', () => {
-      expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ listId: list.id })));
-    });
-
-    it('list management has access to list name', () => {
-      expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ listName: list.name })));
-    });
-
-    it('list management marks the article as being saved in the list', () => {
-      expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ isArticleInList: true })));
+      it('list management marks the article as being saved in the list', () => {
+        expect(viewModel.userListManagement).toStrictEqual(O.some(expect.objectContaining({ isArticleInList: true })));
+      });
     });
   });
 });
