9a6a275e98672c9d83ac29bbc50c3d4dccc5a049
David Moulton <d.moulton@elifesciences.org>
2023-03-14 11:56:14 +0000

    Extract helper function for better code habitability #2121

--- test/framework/create-test-framework.ts
+++ test/framework/create-test-framework.ts
@@ -1,26 +1,6 @@
-import * as O from 'fp-ts/Option';
-import * as TE from 'fp-ts/TaskEither';
 import { createReadAndWriteSides, ReadAndWriteSides } from './create-read-and-write-sides';
 import { CommandHelpers, createCommandHelpers } from './create-command-helpers';
-import { FetchArticle } from '../../src/shared-ports';
-import { ArticleServer } from '../../src/types/article-server';
-import { toHtmlFragment } from '../../src/types/html-fragment';
-import { sanitise } from '../../src/types/sanitised-html-fragment';
-import { arbitraryString } from '../helpers';
-
-type HappyPathThirdPartyAdapters = {
-  fetchArticle: FetchArticle,
-};
-
-const createHappyPathThirdPartyAdapters = (): HappyPathThirdPartyAdapters => ({
-  fetchArticle: (doi) => TE.right({
-    doi,
-    authors: O.none,
-    title: sanitise(toHtmlFragment(arbitraryString())),
-    abstract: sanitise(toHtmlFragment(arbitraryString())),
-    server: 'biorxiv' as ArticleServer,
-  }),
-});
+import { createHappyPathThirdPartyAdapters, HappyPathThirdPartyAdapters } from './happy-path-third-party-adapters';
 
 export type TestFramework = ReadAndWriteSides & {
   commandHelpers: CommandHelpers,
