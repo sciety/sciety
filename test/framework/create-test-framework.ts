7bd4654de9133ebc84a019152ddea867c94eeb97
Kevin Rutherford <kevin@rutherford-software.com>
2023-03-14 11:52:21 +0000

    Add a happy-path stub for fetch-article #2121

--- test/framework/create-test-framework.ts
+++ test/framework/create-test-framework.ts
@@ -1,8 +1,30 @@
+import * as O from 'fp-ts/Option';
+import * as TE from 'fp-ts/TaskEither';
 import { createReadAndWriteSides, ReadAndWriteSides } from './create-read-and-write-sides';
 import { CommandHelpers, createCommandHelpers } from './create-command-helpers';
+import { FetchArticle } from '../../src/shared-ports';
+import { ArticleServer } from '../../src/types/article-server';
+import { toHtmlFragment } from '../../src/types/html-fragment';
+import { sanitise } from '../../src/types/sanitised-html-fragment';
+import { arbitraryString } from '../helpers';
+
+type HappyPathThirdPartyAdapters = {
+  fetchArticle: FetchArticle,
+};
+
+const createHappyPathThirdPartyAdapters = (): HappyPathThirdPartyAdapters => ({
+  fetchArticle: (doi) => TE.right({
+    doi,
+    authors: O.none,
+    title: sanitise(toHtmlFragment(arbitraryString())),
+    abstract: sanitise(toHtmlFragment(arbitraryString())),
+    server: 'biorxiv' as ArticleServer,
+  }),
+});
 
 export type TestFramework = ReadAndWriteSides & {
   commandHelpers: CommandHelpers,
+  happyPathThirdParties: HappyPathThirdPartyAdapters,
 };
 
 export const createTestFramework = (): TestFramework => {
@@ -10,5 +32,6 @@ export const createTestFramework = (): TestFramework => {
   return {
     ...framework,
     commandHelpers: createCommandHelpers(framework.commandHandlers),
+    happyPathThirdParties: createHappyPathThirdPartyAdapters(),
   };
 };
