c67a246e59cd16ec63ef0ebd34bb22e58b68d2ae
David Moulton <d.moulton@elifesciences.org>
2023-03-15 13:38:32 +0000

    Begin to implement a test for the list page view model

--- test/html-pages/list-page/construct-view-model/construct-view-model.test.ts
+++ test/html-pages/list-page/construct-view-model/construct-view-model.test.ts
@@ -1,5 +1,21 @@
+import * as E from 'fp-ts/Either';
+import { arbitraryArticleId } from '../../../types/article-id.helper';
+
 describe('construct-view-model', () => {
   describe('when a user saves an article that is not in any list', () => {
-    it.todo('the article details are included in the page content');
+    it.skip('the article details are included in the page content', () => {
+      const viewmodel = { contentViewModel: undefined };
+      const articleId = arbitraryArticleId();
+
+      expect(viewmodel.contentViewModel).toStrictEqual(expect.objectContaining({
+        articles: [
+          E.right(expect.objectContaining({
+            articleViewModel: expect.objectContaining({
+              articleId,
+            }),
+          })),
+        ],
+      }));
+    });
   });
 });
