a1be20c828f6b6e87cf023ee1a4b246911e57550
LinaKind <l.kinduryte@elifesciences.org>
2023-03-15 13:27:32 +0000

    Express intent to replace taiko test with a fast test

---
+++ test/html-pages/list-page/construct-view-model/construct-view-model.test.ts
@@ -0,0 +1,5 @@
+describe('construct-view-model', () => {
+  describe('when a user saves an article that is not in any list', () => {
+    it.todo('the article details are included in the page content');
+  });
+});
