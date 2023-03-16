3eab5e1d7ab8ff57f0c86dc6e525ed7a49b125b5
Giorgio Sironi <g.sironi@elifesciences.org>
2023-03-16 11:23:32 +0000

    Write a happy path journey test with Taiko #2141

---
+++ feature-test/journey-to-create-new-list.test.ts
@@ -0,0 +1,7 @@
+describe('journey-to-create-new-list', () => {
+  describe('when the user is on their My Lists page', () => {
+    describe('when they create a new list', () => {
+      it.todo('they end up on the My Lists page with a new list, and a customized name and a description');
+    });
+  });
+});
