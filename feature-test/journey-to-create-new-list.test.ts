1012c607ae98a09b9238e3a590a8ed0e1e91113d
LinaKind <l.kinduryte@elifesciences.org>
2023-03-16 11:31:39 +0000

    Add user creation to set up #2141

--- feature-test/journey-to-create-new-list.test.ts
+++ feature-test/journey-to-create-new-list.test.ts
@@ -1,5 +1,25 @@
+import { openBrowser } from 'taiko';
+import { arbitraryString } from '../test/helpers';
+import { arbitraryUserHandle } from '../test/types/user-handle.helper';
+import { arbitraryUserId } from '../test/types/user-id.helper';
+import { callApi } from './helpers/call-api.helper';
+import { screenshotTeardown } from './utilities';
+
 describe('journey-to-create-new-list', () => {
   describe('when the user is on their My Lists page', () => {
+    beforeEach(async () => {
+      const userId = arbitraryUserId();
+      await callApi('api/create-user', {
+        userId,
+        handle: arbitraryUserHandle(),
+        avatarUrl: 'http://somethingthatproducesa404',
+        displayName: arbitraryString(),
+      });
+      await openBrowser();
+    });
+
+    afterEach(screenshotTeardown);
+
     describe('when they create a new list', () => {
       it.todo('they end up on the My Lists page with a new list, and a customized name and a description');
     });
