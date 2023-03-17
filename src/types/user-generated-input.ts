4f8679927e982a4cfadfe3ee9965273896c1e636
David Moulton <d.moulton@elifesciences.org>
2023-03-17 14:52:46 +0000

    Extract function to reveal intent #2142

--- src/types/user-generated-input.ts
+++ src/types/user-generated-input.ts
@@ -12,13 +12,15 @@ type Config = {
   allowEmptyInput?: boolean,
 };
 
+const areInputCharactersSafe = (
+  config: Config,
+  input: string,
+) => !!((config.allowEmptyInput ? emptyRegex : notEmptyRegex).exec(input));
+
 export const userGeneratedInputCodec = (config: Config) => t.brand(
   t.string,
   (input): input is t.Branded<string, UserGeneratedInputBrand> => (
-    !!(
-      (config.allowEmptyInput ? emptyRegex : notEmptyRegex).exec(input)
-    )
-    && input.length <= config.maxInputLength
+    areInputCharactersSafe(config, input) && input.length <= config.maxInputLength
   ),
   'UserGeneratedInput',
 );
