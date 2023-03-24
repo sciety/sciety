914012338fbfa6265741311bd1beb89c097b9e9a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-03-24 15:38:24 +0000

    Safely access request body for unfollow-handler #2163

--- src/write-side/follow/unfollow-handler.ts
+++ src/write-side/follow/unfollow-handler.ts
@@ -2,19 +2,28 @@ import { Middleware } from '@koa/router';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { StatusCodes } from 'http-status-codes';
+import * as t from 'io-ts';
 import { getLoggedInScietyUser, Ports as GetLoggedInScietyUserPorts } from '../../http/authentication-and-logging-in-of-sciety-users';
 import { Ports as UnfollowCommandPorts, unfollowCommand } from './unfollow-command';
-import * as GroupId from '../../types/group-id';
 import { Logger } from '../../shared-ports';
+import { GroupIdFromString } from '../../types/codecs/GroupIdFromString';
 
 type Ports = GetLoggedInScietyUserPorts & UnfollowCommandPorts & {
   logger: Logger,
 };
 
+const requestCodec = t.type({
+  body: t.type({
+    editorialcommunityid: GroupIdFromString,
+  }),
+});
+
 export const unfollowHandler = (ports: Ports): Middleware => async (context, next) => {
   await pipe(
-    context.request.body.editorialcommunityid,
-    GroupId.fromNullable,
+    context.request,
+    requestCodec.decode,
+    O.fromEither,
+    O.map((req) => req.body.editorialcommunityid),
     O.fold(
       () => context.throw(StatusCodes.BAD_REQUEST),
       async (groupId) => pipe(
