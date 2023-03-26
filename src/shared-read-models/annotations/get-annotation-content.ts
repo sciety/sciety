af69f12ebbd7f9c8ec906e4b2138bf8debc239a9
Kevin Rutherford <kevin@rutherford-software.com>
2023-03-26 11:46:44 +0100

    Implement the readmodel query #2166

--- src/shared-read-models/annotations/get-annotation-content.ts
+++ src/shared-read-models/annotations/get-annotation-content.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import * as R from 'fp-ts/Record';
 import { ReadModel } from './handle-event';
 import { ListId } from '../../types/list-id';
 import { Doi } from '../../types/doi';
@@ -8,4 +10,8 @@ import { HtmlFragment } from '../../types/html-fragment';
 type GetAnnotationContent = (listId: ListId, articleId: Doi) => O.Option<HtmlFragment>;
 
 // ts-unused-exports:disable-next-line
-export const getAnnotationContent = (readModel: ReadModel): GetAnnotationContent => (listId, articleId) => O.none;
+export const getAnnotationContent = (readModel: ReadModel): GetAnnotationContent => (listId, articleId) => pipe(
+  readModel,
+  R.lookup(listId),
+  O.chain(R.lookup(articleId.value)),
+);
