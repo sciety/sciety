f5b318913622843b2392408a126ad3275fa229cf
LinaKind <l.kinduryte@elifesciences.org>
2023-03-28 16:47:08 +0100

    Only allow one application host name #2179

--- src/http/check-referer.ts
+++ src/http/check-referer.ts
@@ -2,10 +2,10 @@ import { pipe } from 'fp-ts/function';
 import { URL } from 'url';
 import * as O from 'fp-ts/Option';
 
-export const checkReferer = (referer: string | undefined, hostname: string) => pipe(
+export const checkReferer = (referer: string | undefined, applicationHostname: string) => pipe(
   referer,
   O.fromNullable,
   O.map((url) => new URL(url)),
-  O.map((url) => ([hostname, 'localhost', 'staging.sciety.org'].includes(url.hostname) ? url.toString() : '/')),
+  O.map((url) => (url.hostname === applicationHostname ? url.toString() : '/')),
   O.getOrElse(() => '/'),
 );
