5852d16ee79339d4fc5f69b7103623b6802ec7fc
LinaKind <l.kinduryte@elifesciences.org>
2023-03-28 16:12:26 +0100

    Ensure code can be tested on multiple environments #2179

--- src/http/check-referer.ts
+++ src/http/check-referer.ts
@@ -6,6 +6,6 @@ export const checkReferer = (referer: string | undefined) => pipe(
   referer,
   O.fromNullable,
   O.map((url) => new URL(url)),
-  O.map((url) => (url.hostname === 'sciety.org' ? url.toString() : '/')),
+  O.map((url) => (['sciety.org', 'localhost', 'staging.sciety.org'].includes(url.hostname) ? url.toString() : '/')),
   O.getOrElse(() => '/'),
 );
