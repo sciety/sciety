88d9af744998148f0b98c40e3c67021905fff5ee
Giorgio Sironi <g.sironi@elifesciences.org>
2023-03-29 10:53:05 +0100

    Replace duplication with named intent #2179

--- src/http/authentication-destination/calculate-authentication-destination.ts
+++ src/http/authentication-destination/calculate-authentication-destination.ts
@@ -2,10 +2,13 @@ import { pipe } from 'fp-ts/function';
 import { URL } from 'url';
 import * as O from 'fp-ts/Option';
 
+// ts-unused-exports:disable-next-line
+export const defaultDestination = '/';
+
 export const calculateAuthenticationDestination = (referer: string | undefined, applicationHostname: string) => pipe(
   referer,
   O.fromNullable,
   O.map((url) => new URL(url)),
-  O.map((url) => (url.hostname === applicationHostname ? url.toString() : '/')),
-  O.getOrElse(() => '/'),
+  O.map((url) => (url.hostname === applicationHostname ? url.toString() : defaultDestination)),
+  O.getOrElse(() => defaultDestination),
 );
--- test/http/authentication-destination/calculate-authentication-destination.test.ts
+++ test/http/authentication-destination/calculate-authentication-destination.test.ts
@@ -1,9 +1,9 @@
-import { calculateAuthenticationDestination } from '../../../src/http/authentication-destination/calculate-authentication-destination';
+import { defaultDestination, calculateAuthenticationDestination } from '../../../src/http/authentication-destination/calculate-authentication-destination';
 
 describe('calculate-authentication-destination', () => {
   describe('when a referer is not provided', () => {
     it('defaults to homepage', () => {
-      expect(calculateAuthenticationDestination(undefined, 'sciety.org')).toBe('/');
+      expect(calculateAuthenticationDestination(undefined, 'sciety.org')).toBe(defaultDestination);
     });
   });
 
@@ -16,7 +16,7 @@ describe('calculate-authentication-destination', () => {
 
     describe('and it does not match the application hostname', () => {
       it('defaults to the homepage', () => {
-        expect(calculateAuthenticationDestination('https://t.co/j2ZLHZRBXb', 'sciety.org')).toBe('/');
+        expect(calculateAuthenticationDestination('https://t.co/j2ZLHZRBXb', 'sciety.org')).toBe(defaultDestination);
       });
     });
   });
