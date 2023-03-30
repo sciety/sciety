386e1e62c3cfa3317e16be806d34579ee7b08d69
Kevin Rutherford <kevin@rutherford-software.com>
2023-03-30 14:53:13 +0100

    Remove unnecessary category lift #2183

--- src/html-pages/home-page/cards/cards.ts
+++ src/html-pages/home-page/cards/cards.ts
@@ -1,5 +1,6 @@
 import { sequenceS } from 'fp-ts/Apply';
 import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import { renderCardsSection } from './render-cards-section';
 import { Ports, userListCard } from './user-list-card';
@@ -7,14 +8,14 @@ import { HtmlFragment } from '../../../types/html-fragment';
 import { ListId, listIdCodec } from '../../../types/list-id';
 
 type Card = {
-  listId: O.Option<ListId>,
+  listId: ListId,
 };
 
 const card1: Card = {
   listId: pipe(
     '454ba80f-e0bc-47ed-ba76-c8f872c303d2', // Biophysics Colab reading list
     listIdCodec.decode,
-    O.fromEither,
+    E.getOrElseW((errors) => { throw new Error(JSON.stringify(errors)); }),
   ),
 };
 
@@ -22,7 +23,7 @@ const card2: Card = {
   listId: pipe(
     'dcc7c864-6630-40e7-8eeb-9fb6f012e92b', // @AvasthiReading's saved articles
     listIdCodec.decode,
-    O.fromEither,
+    E.getOrElseW((errors) => { throw new Error(JSON.stringify(errors)); }),
   ),
 };
 
@@ -30,7 +31,7 @@ const card3: Card = {
   listId: pipe(
     '154c0659-5310-4dcc-9da3-1de24d99a542', // @LSE_Angela's List of Preprints to Read
     listIdCodec.decode,
-    O.fromEither,
+    E.getOrElseW((errors) => { throw new Error(JSON.stringify(errors)); }),
   ),
 };
 
@@ -38,15 +39,15 @@ export const cards = (ports: Ports): HtmlFragment => pipe(
   {
     first: pipe(
       card1.listId,
-      O.chain(userListCard(ports)),
+      userListCard(ports),
     ),
     second: pipe(
       card2.listId,
-      O.chain(userListCard(ports)),
+      userListCard(ports),
     ),
     third: pipe(
       card3.listId,
-      O.chain(userListCard(ports)),
+      userListCard(ports),
     ),
   },
   sequenceS(O.Apply),
