7b5a3b91deabf67f76391799de8b90239c4517db
Kevin Rutherford <kevin@rutherford-software.com>
2023-03-30 11:22:51 +0100

    Test group lists ordering behaviour #2183

---
+++ test/html-pages/group-page/group-lists-page/construct-view-model/construct-view-model.test.ts
@@ -0,0 +1,57 @@
+import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
+import * as TE from 'fp-ts/TaskEither';
+import { createTestFramework, TestFramework } from '../../../../framework';
+import { arbitraryGroup } from '../../../../types/group.helper';
+import { arbitraryList } from '../../../../types/list-helper';
+import * as LOID from '../../../../../src/types/list-owner-id';
+import { ViewModel } from '../../../../../src/html-pages/group-page/group-lists-page/view-model';
+import { constructViewModel } from '../../../../../src/html-pages/group-page/group-lists-page/construct-view-model/construct-view-model';
+import { shouldNotBeCalled } from '../../../../should-not-be-called';
+import { List } from '../../../../../src/types/list';
+
+describe('construct-view-model', () => {
+  let framework: TestFramework;
+  const group = arbitraryGroup();
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
+  describe('when the group has more than one list', () => {
+    let firstList: List;
+    const secondList = arbitraryList(LOID.fromGroupId(group.id));
+    const thirdList = arbitraryList(LOID.fromGroupId(group.id));
+    let viewmodel: ViewModel;
+
+    beforeEach(async () => {
+      await framework.commandHelpers.createGroup(group);
+      // eslint-disable-next-line prefer-destructuring
+      firstList = framework.queries.selectAllListsOwnedBy(LOID.fromGroupId(group.id))[0];
+      await framework.commandHelpers.createList(secondList);
+      await framework.commandHelpers.createList(thirdList);
+      viewmodel = await pipe(
+        {
+          slug: group.slug,
+          user: O.none,
+        },
+        constructViewModel(framework.queries),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+    });
+
+    it('returns lists in descending order of updated date', () => {
+      expect(viewmodel.listCards).toStrictEqual([
+        expect.objectContaining({
+          listId: thirdList.id,
+        }),
+        expect.objectContaining({
+          listId: secondList.id,
+        }),
+        expect.objectContaining({
+          listId: firstList.id,
+        }),
+      ]);
+    });
+  });
+});
