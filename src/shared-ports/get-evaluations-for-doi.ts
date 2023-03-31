f9c9f20404d0a5990c7d389206461d478cf0cd8b
Kevin Rutherford <kevin@rutherford-software.com>
2023-03-31 15:47:18 +0100

    Allow the evaluations readmodel to be queried #2187

---
+++ src/shared-ports/get-evaluations-for-doi.ts
@@ -0,0 +1,13 @@
+import { Doi } from '../types/doi';
+import { GroupId } from '../types/group-id';
+import { ReviewId } from '../types/review-id';
+
+type RecordedEvaluation = {
+  reviewId: ReviewId,
+  groupId: GroupId,
+  recordedAt: Date,
+  publishedAt: Date,
+  authors: ReadonlyArray<string>,
+};
+
+export type GetEvaluationsForDoi = (articleDoi: Doi) => ReadonlyArray<RecordedEvaluation>;
--- src/shared-ports/index.ts
+++ src/shared-ports/index.ts
@@ -12,6 +12,7 @@ import { GetAnnotationContent } from './get-annotation-content';
 import { GetArticleIdsByState } from './get-article-ids-by-state';
 import { GetArticleSubjectArea } from './get-article-subject-area';
 import { GetEvaluatedArticlesListIdForGroup } from './get-evaluated-articles-list-id-for-group';
+import { GetEvaluationsForDoi } from './get-evaluations-for-doi';
 import { GetFollowers } from './get-followers';
 import { GetGroup } from './get-group';
 import { GetGroupBySlug } from './get-group-by-slug';
@@ -44,6 +45,8 @@ export { GetGroupBySlug } from './get-group-by-slug';
 export { GetAllGroups } from './get-all-groups';
 // ts-unused-exports:disable-next-line
 export { GetAnnotationContent } from './get-annotation-content';
+// ts-unused-exports:disable-next-line
+export { GetEvaluationsForDoi } from './get-evaluations-for-doi';
 export { LookupList } from './lookup-list';
 export { LookupUser } from './lookup-user';
 export { LookupUserByHandle } from './lookup-user-by-handle';
@@ -79,6 +82,7 @@ export type SharedPorts = {
   getAnnotationContent: GetAnnotationContent,
   getArticleIdsByState: GetArticleIdsByState,
   getArticleSubjectArea: GetArticleSubjectArea,
+  getEvaluationsForDoi: GetEvaluationsForDoi,
   getFollowers: GetFollowers,
   getGroup: GetGroup,
   getGroupBySlug: GetGroupBySlug,
