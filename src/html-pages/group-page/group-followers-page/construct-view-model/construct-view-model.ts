981591c33cbd198b8c41eb2c08790e032bef0935
Kevin Rutherford <kevin@rutherford-software.com>
2023-03-31 13:15:01 +0100

    Inline a function to improve readability #2098

--- src/html-pages/group-page/group-followers-page/construct-view-model/construct-view-model.ts
+++ src/html-pages/group-page/group-followers-page/construct-view-model/construct-view-model.ts
@@ -1,18 +1,20 @@
+import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
+import { paginationControls } from '../../../../shared-components/pagination-controls';
+import { paginate } from './paginate';
+import { augmentWithUserDetails, Ports as AugmentWithUserDetailsPorts } from './augment-with-user-details';
 import { GetGroupBySlug, IsFollowing } from '../../../../shared-ports';
 import { userIdCodec } from '../../../../types/user-id';
 import * as DE from '../../../../types/data-error';
 import { ViewModel } from '../view-model';
 import { findFollowers, Ports as FindFollowersPorts } from './find-followers';
-import { constructFollowersTab, Ports as FollowersPorts } from './followers';
 import { constructTabsViewModel, Ports as TabsViewModelPorts } from '../../common-components/tabs-view-model';
 
-export type Ports = FindFollowersPorts & FollowersPorts & TabsViewModelPorts & {
+export type Ports = FindFollowersPorts & AugmentWithUserDetailsPorts & TabsViewModelPorts & {
   getGroupBySlug: GetGroupBySlug,
   isFollowing: IsFollowing,
 };
@@ -26,6 +28,8 @@ export const paramsCodec = t.type({
 });
 
 export type Params = t.TypeOf<typeof paramsCodec>;
+
+const pageSize = 10;
 
 type ConstructViewModel = (ports: Ports) => (params: Params) => TE.TaskEither<DE.DataError, ViewModel>;
 
@@ -48,8 +52,16 @@ export const constructViewModel: ConstructViewModel = (ports) => (params) => pip
   )),
   E.fromOption(() => DE.notFound),
   E.chain((partial) => pipe(
-    partial,
-    constructFollowersTab(ports),
+    partial.followers,
+    paginate(partial.pageNumber, pageSize),
+    E.map((pageOfFollowers) => ({
+      followerCount: pageOfFollowers.numberOfOriginalItems,
+      followers: pipe(
+        pageOfFollowers.items,
+        augmentWithUserDetails(ports),
+      ),
+      nextLink: paginationControls(`/groups/${partial.group.slug}/followers?`, pageOfFollowers.nextPage),
+    })),
     E.map((activeTab) => ({
       ...partial,
       activeTab,
