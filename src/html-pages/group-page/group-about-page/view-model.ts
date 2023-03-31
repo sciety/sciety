81ccfad8380936ae0a8c971ca4f1de0467cf624e
Kevin Rutherford <kevin@rutherford-software.com>
2023-03-31 09:01:59 +0100

    Remove an unnecessary property from the viewmodel #2098

--- src/html-pages/group-page/group-about-page/view-model.ts
+++ src/html-pages/group-page/group-about-page/view-model.ts
@@ -2,7 +2,6 @@ import * as O from 'fp-ts/Option';
 import { ListId } from '../../../types/list-id';
 import { PageHeaderViewModel } from '../common-components/page-header';
 import { TabsViewModel } from '../common-components/tabs-view-model';
-import { Group } from '../../../types/group';
 
 export type ListViewModel = {
   listId: ListId,
@@ -19,6 +18,5 @@ export type OurListsViewModel = {
 export type ViewModel = PageHeaderViewModel & {
   ourLists: OurListsViewModel,
   markdown: string,
-  group: Group,
   tabs: TabsViewModel,
 };
