60247d91be9f0bab6dbf401d642b3d66072167f1
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-04-03 11:42:20 +0100

    Extract function for readability #2188

--- src/html-pages/search-results-page/render-as-html/pagination.ts
+++ src/html-pages/search-results-page/render-as-html/pagination.ts
@@ -8,6 +8,18 @@ export type PaginationViewModel = SearchParameters & {
   numberOfPages: number,
 };
 
+const renderArticlesSearchResultsHeader = (pageNumber: number, numberOfPages: number) => `
+  <header class="search-results__header">
+    <h3 class="search-results__page_count">
+      Showing page <b>${pageNumber}</b> of <b>${numberOfPages}</b><span class="visually-hidden"> pages of search results</span>
+    </h3>
+    <div class="search-results__header_details">
+      <div class="search-results__header_details_item">Results from <b>bioRxiv</b>, <b>medRxiv</b></div>
+      <div class="search-results__header_details_item">Sorted by <b>publication date</b></div>
+    </div>
+  </header>
+`;
+
 type Pagination = (viewModel: PaginationViewModel) => (content: O.Option<HtmlFragment>) => HtmlFragment;
 
 export const pagination: Pagination = (viewModel) => (content) => pipe(
@@ -16,15 +28,7 @@ export const pagination: Pagination = (viewModel) => (content) => pipe(
     () => '',
     (c: HtmlFragment) => (viewModel.category === 'articles'
       ? `
-      <header class="search-results__header">
-        <h3 class="search-results__page_count">
-          Showing page <b>${viewModel.pageNumber}</b> of <b>${viewModel.numberOfPages}</b><span class="visually-hidden"> pages of search results</span>
-        </h3>
-        <div class="search-results__header_details">
-          <div class="search-results__header_details_item">Results from <b>bioRxiv</b>, <b>medRxiv</b></div>
-          <div class="search-results__header_details_item">Sorted by <b>publication date</b></div>
-        </div>
-      </header>
+      ${renderArticlesSearchResultsHeader(viewModel.pageNumber, viewModel.numberOfPages)}
       ${c}
       ${renderNextLinkOrCallsToAction({ ...viewModel, pageNumber: viewModel.pageNumber + 1 })}
     `
