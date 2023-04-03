8359f0110bfa22380f299e0595540e1f56c48220
Kevin Rutherford <kevin@rutherford-software.com>
2023-04-03 15:38:49 +0100

    Convert a couple more docmap tests to use the framework #2187

--- test/docmaps/docmap/generate-docmap-view-model.test.ts
+++ test/docmaps/docmap/generate-docmap-view-model.test.ts
@@ -1,45 +1,27 @@
 import { URL } from 'url';
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
-import * as TO from 'fp-ts/TaskOption';
 import { pipe } from 'fp-ts/function';
 import {
   DocmapModel,
   generateDocmapViewModel,
   Ports,
 } from '../../../src/docmaps/docmap/generate-docmap-view-model';
-import { evaluationRecorded, groupJoined } from '../../../src/domain-events';
 import * as DE from '../../../src/types/data-error';
-import { inferredSourceUrl, ReviewId } from '../../../src/types/review-id';
-import { arbitraryDate, arbitraryUri } from '../../helpers';
+import { inferredSourceUrl } from '../../../src/types/review-id';
+import { arbitraryUri } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 import { arbitraryArticleId } from '../../types/article-id.helper';
-import { arbitraryArticleServer } from '../../types/article-server.helper';
 import { arbitraryGroupId } from '../../types/group-id.helper';
 import { arbitraryGroup } from '../../types/group.helper';
-import { arbitraryNcrcId, arbitraryReviewDoi, arbitraryReviewId } from '../../types/review-id.helper';
+import { arbitraryNcrcId, arbitraryReviewDoi } from '../../types/review-id.helper';
 import { TestFramework, createTestFramework } from '../../framework';
 import { RecordedEvaluation } from '../../../src/types/recorded-evaluation';
 import { arbitraryRecordedEvaluation } from '../../types/recorded-evaluation.helper';
 
 const indexedGroupId = arbitraryGroupId();
 const articleId = arbitraryArticleId();
-
-const defaultPorts: Ports = {
-  fetchReview: (id: ReviewId) => TE.right({ url: new URL(`https://reviews.example.com/${id}`) }),
-  findVersionsForArticleDoi: () => TO.some([
-    {
-      source: new URL(arbitraryUri()),
-      publishedAt: arbitraryDate(),
-      version: 1,
-    },
-  ]),
-  getAllEvents: T.of([]),
-  fetchArticle: () => TE.right({ server: arbitraryArticleServer() }),
-  getGroup: () => O.some(arbitraryGroup()),
-};
 
 describe('generate-docmap-view-model', () => {
   let framework: TestFramework;
@@ -54,86 +36,55 @@ describe('generate-docmap-view-model', () => {
     };
   });
 
-  it('includes the article id', async () => {
+  describe('when there is an evaluation by the selected group', () => {
     const group = arbitraryGroup();
-    const ports: Ports = {
-      ...defaultPorts,
-      getAllEvents: T.of([
-        groupJoined(
-          group.id,
-          group.name,
-          group.avatarPath,
-          group.descriptionPath,
-          group.shortDescription,
-          group.homepage,
-          group.slug,
-        ),
-        evaluationRecorded(group.id, articleId, arbitraryReviewId()),
-      ]),
+    const evaluation = {
+      ...arbitraryRecordedEvaluation(),
+      articleId,
+      groupId: group.id,
     };
-    const result = await pipe(
-      { articleId, groupId: group.id },
-      generateDocmapViewModel(ports),
-      TE.getOrElse(framework.abortTest('generateDocmapViewModel')),
-    )();
+    let result: DocmapModel;
 
-    expect(result).toStrictEqual(expect.objectContaining({ articleId }));
-  });
+    beforeEach(async () => {
+      await framework.commandHelpers.createGroup(group);
+      await framework.commandHelpers.recordEvaluation(evaluation);
+      result = await pipe(
+        { articleId, groupId: group.id },
+        generateDocmapViewModel(defaultAdapters),
+        TE.getOrElse(framework.abortTest('generateDocmapViewModel')),
+      )();
+    });
 
-  it('includes the group', async () => {
-    const group = arbitraryGroup();
-    const ports: Ports = {
-      ...defaultPorts,
-      getAllEvents: T.of([
-        groupJoined(
-          group.id,
-          group.name,
-          group.avatarPath,
-          group.descriptionPath,
-          group.shortDescription,
-          group.homepage,
-          group.slug,
-        ),
-        evaluationRecorded(group.id, articleId, arbitraryReviewId()),
-      ]),
-      getGroup: () => O.some(group),
-    };
-    const result = await pipe(
-      { articleId, groupId: group.id },
-      generateDocmapViewModel(ports),
-      TE.getOrElse(framework.abortTest('generateDocmapViewModel')),
-    )();
+    it('includes the article id', async () => {
+      expect(result).toStrictEqual(expect.objectContaining({ articleId }));
+    });
 
-    expect(result).toStrictEqual(expect.objectContaining({ group }));
+    it('includes the group', async () => {
+      expect(result).toStrictEqual(expect.objectContaining({ group }));
+    });
   });
 
   describe('when there are multiple evaluations by the selected group', () => {
     const group = arbitraryGroup();
-    const earlierDate = new Date('1900');
-    const earlierReviewId = arbitraryReviewId();
-    const laterDate = new Date('2000');
-    const laterReviewId = arbitraryReviewId();
+    const firstEvaluation = {
+      ...arbitraryRecordedEvaluation(),
+      articleId,
+      groupId: group.id,
+    };
+    const secondEvaluation = {
+      ...arbitraryRecordedEvaluation(),
+      articleId,
+      groupId: group.id,
+    };
+
     let result: DocmapModel;
 
     beforeEach(async () => {
-      const ports: Ports = {
-        ...defaultPorts,
-        getAllEvents: T.of([
-          groupJoined(
-            indexedGroupId,
-            group.name,
-            group.avatarPath,
-            group.descriptionPath,
-            group.shortDescription,
-            group.homepage,
-            group.slug,
-          ),
-          evaluationRecorded(indexedGroupId, articleId, earlierReviewId, [], new Date(), earlierDate),
-          evaluationRecorded(indexedGroupId, articleId, laterReviewId, [], new Date(), laterDate),
-        ]),
-      };
+      await framework.commandHelpers.createGroup(group);
+      await framework.commandHelpers.recordEvaluation(firstEvaluation);
+      await framework.commandHelpers.recordEvaluation(secondEvaluation);
       result = await pipe(
-        generateDocmapViewModel(ports)({ articleId, groupId: indexedGroupId }),
+        generateDocmapViewModel(defaultAdapters)({ articleId, groupId: group.id }),
         TE.getOrElse(framework.abortTest('generateDocmapViewModel')),
       )();
     });
@@ -141,12 +92,10 @@ describe('generate-docmap-view-model', () => {
     it('returns all evaluations', () => {
       expect(result.evaluations).toStrictEqual([
         expect.objectContaining({
-          recordedAt: earlierDate,
-          reviewId: earlierReviewId,
+          reviewId: firstEvaluation.reviewId,
         }),
         expect.objectContaining({
-          recordedAt: laterDate,
-          reviewId: laterReviewId,
+          reviewId: secondEvaluation.reviewId,
         }),
       ]);
     });
@@ -162,24 +111,20 @@ describe('generate-docmap-view-model', () => {
 
     beforeEach(async () => {
       const group = arbitraryGroup();
-      const ports: Ports = {
-        ...defaultPorts,
-        fetchReview: shouldNotBeCalled,
-        getAllEvents: T.of([
-          groupJoined(
-            indexedGroupId,
-            group.name,
-            group.avatarPath,
-            group.descriptionPath,
-            group.shortDescription,
-            group.homepage,
-            group.slug,
-          ),
-          evaluationRecorded(indexedGroupId, articleId, reviewIdWithInferrableSourceUrl),
-        ]),
+      const evaluation = {
+        ...arbitraryRecordedEvaluation(),
+        groupId: group.id,
+        reviewId: reviewIdWithInferrableSourceUrl,
+        articleId,
       };
+      const ports: Ports = {
+        ...defaultAdapters,
+        fetchReview: shouldNotBeCalled,
+      };
+      await framework.commandHelpers.createGroup(group);
+      await framework.commandHelpers.recordEvaluation(evaluation);
       result = await pipe(
-        generateDocmapViewModel(ports)({ articleId, groupId: indexedGroupId }),
+        generateDocmapViewModel(ports)({ articleId, groupId: group.id }),
         TE.getOrElse(framework.abortTest('generateDocmapViewModel')),
       )();
     });
