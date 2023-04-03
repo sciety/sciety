bc37411978e7340d72070df52b244b59957abbf0
Giorgio Sironi <g.sironi@elifesciences.org>
2023-04-03 17:16:35 +0100

    Send commands and verify they don't produce duplicate events #2193

--- src/ingest/record-article-versions-for-day.ts
+++ src/ingest/record-article-versions-for-day.ts
@@ -2,7 +2,10 @@ import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
+import axios from 'axios';
+import * as E from 'fp-ts/Either';
 import { axiosGet } from './fetch-data';
+import { batchTaskTraverse } from './batch-traverse';
 
 type BiorxivResponse = {
   collection: ReadonlyArray<{
@@ -11,6 +14,45 @@ type BiorxivResponse = {
     version: string,
   }>,
 };
+
+const send = (command: unknown) => TE.tryCatch(
+  async () => axios.post(`${process.env.INGESTION_TARGET_APP ?? 'http://app'}/api/record-article-version`, JSON.stringify(command), {
+    headers: {
+      Authorization: `Bearer ${process.env.SCIETY_TEAM_API_BEARER_TOKEN ?? 'secret'}`,
+      'Content-Type': 'application/json',
+    },
+    timeout: 10000,
+  }),
+  (error) => {
+    if (axios.isAxiosError(error)) {
+      return `Failed to post evaluation command: ${String(error)}. Response data is: "${String(error.response?.data)}"`;
+    }
+    return `Failed to post evaluation command: ${String(error)}`;
+  },
+);
+
+const sendRecordArticleVersionCommands = (commands: ReadonlyArray<unknown>) => pipe(
+  commands,
+  batchTaskTraverse(send, 1),
+  T.map((array) => {
+    const leftsCount = RA.lefts(array).length;
+    const lefts = pipe(
+      array,
+      RA.lefts,
+      RA.size,
+    );
+    const rightsCount = RA.rights(array).length;
+    const summaryOfRequests = {
+      lefts,
+      leftsTotal: leftsCount,
+      rightsTotal: rightsCount,
+    };
+    if (leftsCount > 0) {
+      return E.left(summaryOfRequests);
+    }
+    return E.right(summaryOfRequests);
+  }),
+);
 
 export const recordAll = (): TE.TaskEither<unknown, unknown> => pipe(
   TE.tryCatch(
@@ -25,10 +67,7 @@ export const recordAll = (): TE.TaskEither<unknown, unknown> => pipe(
       publishedAt: biorxivArticleVersion.date,
     })),
   )),
-  TE.map((commands) => {
-    console.log(commands);
-    return;
-  }),
+  TE.chainW(sendRecordArticleVersionCommands),
 );
 
 void (async (): Promise<unknown> => pipe(
