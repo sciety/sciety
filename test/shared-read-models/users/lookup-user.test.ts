d276b98dad58468c94c9a4907c863b9c5a4ef818
LinaKind <l.kinduryte@elifesciences.org>
2023-04-04 14:54:09 +0100

    Write a failing test #2197

--- test/shared-read-models/users/lookup-user.test.ts
+++ test/shared-read-models/users/lookup-user.test.ts
@@ -2,6 +2,7 @@ import * as O from 'fp-ts/Option';
 import { arbitraryUserId } from '../../types/user-id.helper';
 import { TestFramework, createTestFramework } from '../../framework';
 import { arbitraryUserDetails } from '../../types/user-details.helper';
+import { arbitraryUri } from '../../helpers';
 
 describe('lookup-user', () => {
   let framework: TestFramework;
@@ -25,7 +26,21 @@ describe('lookup-user', () => {
   });
 
   describe('when avatarUrl has been updated', () => {
-    it.todo('returns the updated avatarUrl');
+    const user = arbitraryUserDetails();
+    const newAvatarUrl = arbitraryUri();
+
+    beforeEach(async () => {
+      await framework.commandHelpers.createUserAccount(user);
+    });
+
+    it.failing('returns the updated avatarUrl', () => {
+      const result = framework.queries.lookupUser(user.id);
+
+      expect(result).toStrictEqual(O.some({
+        ...user,
+        avatarUrl: newAvatarUrl,
+      }));
+    });
   });
 
   describe('when user does not exist', () => {
