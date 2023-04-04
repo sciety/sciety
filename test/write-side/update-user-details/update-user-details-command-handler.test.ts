78f12c2995004507bc5c31c7ba1eebd59b4fcd6f
LinaKind <l.kinduryte@elifesciences.org>
2023-04-04 14:01:27 +0100

    Provide initial test set up #2197

--- test/write-side/update-user-details/update-user-details-command-handler.test.ts
+++ test/write-side/update-user-details/update-user-details-command-handler.test.ts
@@ -1,9 +1,43 @@
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+import { updateUserDetailsCommandHandler } from '../../../src/write-side/update-user-details/update-user-details-command-handler';
+import { arbitraryUserDetails } from '../../types/user-details.helper';
+import { createTestFramework, TestFramework } from '../../framework';
+
 describe('update user details command handler', () => {
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
   describe('given a new avatarUrl in the command', () => {
     it.todo('raises an UpdateUserDetails event');
   });
 
   describe('given an avatarUrl in the command that matches the current avatarUrl', () => {
+    const userDetails = arbitraryUserDetails();
+
+    beforeEach(async () => {
+      await framework.commandHelpers.createUserAccount(userDetails);
+      const command = {
+        id: userDetails.id,
+        avatarUrl: O.some(userDetails.avatarUrl),
+        displayName: O.none,
+      };
+      const adapters = {
+        getAllEvents: framework.getAllEvents,
+        commitEvents: jest.fn(),
+      };
+      await pipe(
+        command,
+        updateUserDetailsCommandHandler(adapters),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+    });
+
     it.todo('does not raise an UpdateUserDetails event');
   });
 });
