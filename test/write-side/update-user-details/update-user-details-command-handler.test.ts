68da19d0dc0e7aedaee610a3962879c6f69c887f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-04-04 13:47:18 +0100

    Write a subset of the test list #2197

---
+++ test/write-side/update-user-details/update-user-details-command-handler.test.ts
@@ -0,0 +1,9 @@
+describe('update user details command handler', () => {
+  describe('given a new avatarUrl in the command', () => {
+    it.todo('raises an UpdateUserDetails event');
+  });
+
+  describe('given an avatarUrl in the command that matches the current avatarUrl', () => {
+    it.todo('does not raise an UpdateUserDetails event');
+  });
+});
