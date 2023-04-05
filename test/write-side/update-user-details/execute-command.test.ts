e6862584cbca241d255e01d8259690d3d8ba95a0
LinaKind <l.kinduryte@elifesciences.org>
2023-04-05 11:35:00 +0100

    Express intent with to do tests #2196

---
+++ test/write-side/update-user-details/execute-command.test.ts
@@ -0,0 +1,21 @@
+describe('execute-command', () => {
+  describe('when passed a new avatar url', () => {
+    it.todo('raises an event to update avatar url');
+  });
+
+  describe('when passed a new display name', () => {
+    it.todo('raises an event to update display name');
+  });
+
+  describe('when passed a new display name and a new avatar url', () => {
+    it.todo('raises an event to update display name and avatar url');
+  });
+
+  describe('when passed the existing avatar url', () => {
+    it.todo('raises no events');
+  });
+
+  describe('when passed the existing display name', () => {
+    it.todo('raises no events');
+  });
+});
