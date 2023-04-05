3a4e09fa866580f3f8e3972e24b1f894fd07bde9
David Moulton <d.moulton@elifesciences.org>
2023-04-05 10:54:16 +0100

    Introduce a pre-existing command handler structure #2196

--- src/write-side/update-user-details/update-user-details-command-handler.ts
+++ src/write-side/update-user-details/update-user-details-command-handler.ts
@@ -1,20 +1,42 @@
+import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
+import * as T from 'fp-ts/Task';
+import * as E from 'fp-ts/Either';
 import { UpdateUserDetailsCommand } from '../commands';
-import { toErrorMessage } from '../../types/error-message';
 import { CommandHandler } from '../../types/command-handler';
 import { CommitEvents, GetAllEvents } from '../../shared-ports';
+import { DomainEvent } from '../../domain-events';
+import { ErrorMessage } from '../../types/error-message';
+import { UserId } from '../../types/user-id';
 
 type Ports = {
   getAllEvents: GetAllEvents,
   commitEvents: CommitEvents,
 };
 
+type UserResource = unknown;
+
+type ReplayUserResource = (userId: UserId)
+=> (events: ReadonlyArray<DomainEvent>)
+=> E.Either<ErrorMessage, UserResource>;
+
+const replayUserResource: ReplayUserResource = () => () => E.left('nope' as ErrorMessage);
+
+type ExecuteCommand = (command: UpdateUserDetailsCommand)
+=> (userResource: UserResource)
+=> ReadonlyArray<DomainEvent>;
+
+const executeCommand: ExecuteCommand = () => () => [];
+
 type UpdateUserDetailsCommandHandler = (
   adapters: Ports
 ) => CommandHandler<UpdateUserDetailsCommand>;
 
 export const updateUserDetailsCommandHandler: UpdateUserDetailsCommandHandler = (
-
-) => (
-
-) => TE.left(toErrorMessage('no-events-raised'));
+  adapters,
+) => (command) => pipe(
+  adapters.getAllEvents,
+  T.map(replayUserResource(command.id)),
+  TE.map(executeCommand(command)),
+  TE.chainTaskK(adapters.commitEvents),
+);
