ab0447695130a92656e58beca5c23b254b470edb
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-04-05 07:54:28 +0100

    Extract event store creation in framework for reuse

---
+++ test/framework/create-event-store.ts
@@ -0,0 +1,38 @@
+import { pipe } from 'fp-ts/function';
+import * as T from 'fp-ts/Task';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { DomainEvent } from '../../src/domain-events';
+import { GetAllEvents, CommitEvents } from '../../src/shared-ports';
+import { CommandResult } from '../../src/types/command-result';
+
+const commitEvents = (
+  inMemoryEvents: Array<DomainEvent>,
+  dispatchToAllReadModels: (events: ReadonlyArray<DomainEvent>) => void,
+): CommitEvents => (events) => pipe(
+  events,
+  RA.match(
+    () => ('no-events-created' as CommandResult),
+    (es) => {
+      pipe(
+        es,
+        RA.map((event) => { inMemoryEvents.push(event); return event; }),
+      );
+      dispatchToAllReadModels(es);
+      return 'events-created' as CommandResult;
+    },
+  ),
+  T.of,
+);
+
+export type EventStore = {
+  getAllEvents: GetAllEvents,
+  commitEvents: CommitEvents,
+};
+
+export const createEventStore = (dispatchToAllReadModels: (events: ReadonlyArray<DomainEvent>) => void): EventStore => {
+  const allEvents: Array<DomainEvent> = [];
+  return {
+    getAllEvents: T.of(allEvents),
+    commitEvents: commitEvents(allEvents, dispatchToAllReadModels),
+  };
+};
