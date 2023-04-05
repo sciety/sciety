511910c5a6d93b39fe4be65d269c8a2ac48cedb8
Giorgio Sironi <g.sironi@elifesciences.org>
2023-04-05 13:06:12 +0100

    Extract to its designated place #2196

--- src/write-side/resources/user-resource.ts
+++ src/write-side/resources/user-resource.ts
@@ -1,7 +1,10 @@
 import { pipe } from 'fp-ts/function';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as E from 'fp-ts/Either';
 import { UserHandle } from '../../types/user-handle';
 import { DomainEvent, isUserCreatedAccountEvent } from '../../domain-events';
+import { UserId } from '../../types/user-id';
+import { ErrorMessage } from '../../types/error-message';
 
 export const exists = (userHandle: UserHandle) => (events: ReadonlyArray<DomainEvent>) => pipe(
   events,
@@ -13,3 +16,11 @@ export const exists = (userHandle: UserHandle) => (events: ReadonlyArray<DomainE
     () => true,
   ),
 );
+
+export type UserResource = unknown;
+
+type ReplayUserResource = (userId: UserId)
+=> (events: ReadonlyArray<DomainEvent>)
+=> E.Either<ErrorMessage, UserResource>;
+
+export const replayUserResource: ReplayUserResource = () => () => E.left('nope' as ErrorMessage);
--- src/write-side/update-user-details/update-user-details-command-handler.ts
+++ src/write-side/update-user-details/update-user-details-command-handler.ts
@@ -1,26 +1,16 @@
 import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
 import * as T from 'fp-ts/Task';
-import * as E from 'fp-ts/Either';
 import { UpdateUserDetailsCommand } from '../commands';
 import { CommandHandler } from '../../types/command-handler';
 import { CommitEvents, GetAllEvents } from '../../shared-ports';
 import { DomainEvent } from '../../domain-events';
-import { ErrorMessage } from '../../types/error-message';
-import { UserId } from '../../types/user-id';
+import { replayUserResource, UserResource } from '../resources/user-resource';
 
 type Ports = {
   getAllEvents: GetAllEvents,
   commitEvents: CommitEvents,
 };
-
-type UserResource = unknown;
-
-type ReplayUserResource = (userId: UserId)
-=> (events: ReadonlyArray<DomainEvent>)
-=> E.Either<ErrorMessage, UserResource>;
-
-const replayUserResource: ReplayUserResource = () => () => E.left('nope' as ErrorMessage);
 
 type ExecuteCommand = (command: UpdateUserDetailsCommand)
 => (userResource: UserResource)
