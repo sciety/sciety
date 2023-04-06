8680c18226ae5f29988b919f4e5597b4cfe3722d
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-04-06 14:36:17 +0100

    Write a failing test for updating the displayName #2196

--- test/shared-read-models/users/lookup-user.test.ts
+++ test/shared-read-models/users/lookup-user.test.ts
@@ -3,7 +3,7 @@ import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { arbitraryUserId } from '../../types/user-id.helper';
 import { arbitraryUserDetails } from '../../types/user-details.helper';
-import { arbitraryUri } from '../../helpers';
+import { arbitraryString, arbitraryUri } from '../../helpers';
 import { userCreatedAccount, userDetailsUpdated } from '../../../src/domain-events';
 import { handleEvent, initialState } from '../../../src/shared-read-models/users';
 import { lookupUser } from '../../../src/shared-read-models/users/lookup-user';
@@ -38,6 +38,25 @@ describe('lookup-user', () => {
       expect(lookupUser(readModel)(user.id)).toStrictEqual(O.some({
         ...user,
         avatarUrl: newAvatarUrl,
+      }));
+    });
+  });
+
+  describe('when displayName has been updated', () => {
+    const user = arbitraryUserDetails();
+    const newDisplayName = arbitraryString();
+    const readModel = pipe(
+      [
+        userCreatedAccount(user.id, user.handle, user.avatarUrl, user.displayName),
+        userDetailsUpdated(user.id, undefined, newDisplayName),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it.failing('returns the updated displayName', () => {
+      expect(lookupUser(readModel)(user.id)).toStrictEqual(O.some({
+        ...user,
+        displayName: newDisplayName,
       }));
     });
   });
