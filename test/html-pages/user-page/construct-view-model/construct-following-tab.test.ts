8e53671da88bce4e4cd1626e2d1123ca7a1c638f
Kevin Rutherford <kevin@rutherford-software.com>
2023-04-06 10:12:39 +0100

    Write a failnig test #2200

--- test/html-pages/user-page/construct-view-model/construct-following-tab.test.ts
+++ test/html-pages/user-page/construct-view-model/construct-following-tab.test.ts
@@ -1,5 +1,38 @@
+import * as O from 'fp-ts/Option';
+import { arbitraryGroup } from '../../../types/group.helper';
+import { TestFramework, createTestFramework } from '../../../framework';
+import { constructFollowingTab } from '../../../../src/html-pages/user-page/construct-view-model/construct-following-tab';
+
 describe('construct-following-tab', () => {
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
   describe('when the user follows three groups', () => {
-    it.todo('returns them in order of most recently followed first');
+    const group1 = arbitraryGroup();
+    const group2 = arbitraryGroup();
+    const group3 = arbitraryGroup();
+
+    beforeEach(async () => {
+      await framework.commandHelpers.createGroup(group1);
+      await framework.commandHelpers.createGroup(group2);
+      await framework.commandHelpers.createGroup(group3);
+    });
+
+    it.failing('returns them in order of most recently followed first', async () => {
+      const ports = {
+        ...framework.queries,
+        getAllEvents: framework.getAllEvents,
+      };
+      const viewmodel = await constructFollowingTab(ports, [group1.id, group2.id, group3.id])();
+
+      expect(viewmodel.followedGroups).toStrictEqual(O.some([
+        expect.objectContaining({ id: group3.id }),
+        expect.objectContaining({ id: group2.id }),
+        expect.objectContaining({ id: group1.id }),
+      ]));
+    });
   });
 });
