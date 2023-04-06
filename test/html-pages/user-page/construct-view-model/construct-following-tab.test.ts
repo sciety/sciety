6367b622ad5741e3b56ec54305341d9976984a07
LinaKind <l.kinduryte@elifesciences.org>
2023-04-06 09:54:04 +0100

    Write a test list expressing a requirement #2200

---
+++ test/html-pages/user-page/construct-view-model/construct-following-tab.test.ts
@@ -0,0 +1,5 @@
+describe('construct-following-tab', () => {
+  describe('when the user follows three groups', () => {
+    it.todo('returns them in order of most recently followed first');
+  });
+});
