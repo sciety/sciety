f9be142a28284e6b3b103e924c62843dfa75ad13
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-04-11 15:24:06 +0100

    Extract a function that matches the resource action pattern #2210

--- src/write-side/resources/list/edit-list-details-command-handler.ts
+++ src/write-side/resources/list/edit-list-details-command-handler.ts
@@ -1,16 +1,29 @@
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
 import { executeCommand } from './execute-command';
 import { EditListDetailsCommand } from '../../commands';
 import { CommitEvents, GetAllEvents } from '../../../shared-ports';
 import { replayListResource } from './replay-list-resource';
 import { CommandHandler } from '../../../types/command-handler';
+import { DomainEvent } from '../../../domain-events';
+import { ErrorMessage } from '../../../types/error-message';
 
 type Ports = {
   getAllEvents: GetAllEvents,
   commitEvents: CommitEvents,
 };
+
+type ResourceAction = (command: EditListDetailsCommand)
+=> (events: ReadonlyArray<DomainEvent>)
+=> E.Either<ErrorMessage, ReadonlyArray<DomainEvent>>;
+
+const resourceAction: ResourceAction = (command) => (events) => pipe(
+  events,
+  replayListResource(command.listId),
+  E.map(executeCommand(command)),
+);
 
 type EditListDetailsCommandHandler = (
   adapters: Ports
@@ -22,7 +35,6 @@ export const editListDetailsCommandHandler: EditListDetailsCommandHandler = (
   command,
 ) => pipe(
   adapters.getAllEvents,
-  T.map(replayListResource(command.listId)),
-  TE.map(executeCommand(command)),
+  T.map(resourceAction(command)),
   TE.chainTaskK(adapters.commitEvents),
 );
