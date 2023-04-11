e287e096f49ff8cdcaca0ef972d0496eb222f4b4
Giorgio Sironi <g.sironi@elifesciences.org>
2023-04-11 15:28:46 +0100

    Move resource action to dedicated file #2210

--- src/write-side/resources/list/edit-list-details-command-handler.ts
+++ src/write-side/resources/list/edit-list-details-command-handler.ts
@@ -1,29 +1,15 @@
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import * as E from 'fp-ts/Either';
-import { executeCommand } from './execute-command';
+import { resourceAction } from './update';
 import { EditListDetailsCommand } from '../../commands';
 import { CommitEvents, GetAllEvents } from '../../../shared-ports';
-import { replayListResource } from './replay-list-resource';
 import { CommandHandler } from '../../../types/command-handler';
-import { DomainEvent } from '../../../domain-events';
-import { ErrorMessage } from '../../../types/error-message';
 
 type Ports = {
   getAllEvents: GetAllEvents,
   commitEvents: CommitEvents,
 };
-
-type ResourceAction = (command: EditListDetailsCommand)
-=> (events: ReadonlyArray<DomainEvent>)
-=> E.Either<ErrorMessage, ReadonlyArray<DomainEvent>>;
-
-const resourceAction: ResourceAction = (command) => (events) => pipe(
-  events,
-  replayListResource(command.listId),
-  E.map(executeCommand(command)),
-);
 
 type EditListDetailsCommandHandler = (
   adapters: Ports
---
+++ src/write-side/resources/list/update.ts
@@ -0,0 +1,17 @@
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
+import { executeCommand } from './execute-command';
+import { EditListDetailsCommand } from '../../commands';
+import { replayListResource } from './replay-list-resource';
+import { DomainEvent } from '../../../domain-events';
+import { ErrorMessage } from '../../../types/error-message';
+
+type ResourceAction = (command: EditListDetailsCommand)
+=> (events: ReadonlyArray<DomainEvent>)
+=> E.Either<ErrorMessage, ReadonlyArray<DomainEvent>>;
+
+export const resourceAction: ResourceAction = (command) => (events) => pipe(
+  events,
+  replayListResource(command.listId),
+  E.map(executeCommand(command)),
+);
