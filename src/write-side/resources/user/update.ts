b7ac0a768700d9d2892123969a8e8dcee990f606
Giorgio Sironi <g.sironi@elifesciences.org>
2023-04-12 11:34:04 +0100

    Encapsulate function that is only used once #2196

--- src/write-side/resources/user/update.ts
+++ src/write-side/resources/user/update.ts
@@ -1,10 +1,21 @@
 import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
 import { UpdateUserDetailsCommand } from '../../commands';
-import { replayUserResource } from './replay-user-resource';
-import { executeCommand } from './execute-command';
-import { DomainEvent } from '../../../domain-events';
+import { UserResource, replayUserResource } from './replay-user-resource';
 import { ErrorMessage } from '../../../types/error-message';
+import { DomainEvent, userDetailsUpdated } from '../../../domain-events';
+
+type ExecuteCommand = (command: UpdateUserDetailsCommand)
+=> (userResource: UserResource)
+=> ReadonlyArray<DomainEvent>;
+
+const executeCommand: ExecuteCommand = (command) => (userResource) => {
+  const avatarUrl = (userResource.avatarUrl === command.avatarUrl) ? undefined : command.avatarUrl;
+  const displayName = (userResource.displayName === command.displayName) ? undefined : command.displayName;
+  return (avatarUrl === undefined && displayName === undefined)
+    ? []
+    : [userDetailsUpdated(command.userId, avatarUrl, displayName)];
+};
 
 type Update = (command: UpdateUserDetailsCommand)
 => (events: ReadonlyArray<DomainEvent>)
