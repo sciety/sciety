9aac609065221894fbfd050a89bbfd2e9e7f1471
LinaKind <l.kinduryte@elifesciences.org>
2023-04-12 11:59:22 +0100

    Define a test list to replace resource testing #2196

--- test/write-side/resources/user/update.test.ts
+++ test/write-side/resources/user/update.test.ts
@@ -10,148 +10,164 @@ import { update } from '../../../../src/write-side/resources/user';
 
 describe('update', () => {
   describe('when the user exists', () => {
-    const originalUserDetails = arbitraryUserDetails();
-    const existingEvents = [
-      userCreatedAccount(
-        originalUserDetails.id,
-        originalUserDetails.handle,
-        originalUserDetails.avatarUrl,
-        originalUserDetails.displayName,
-      ),
-    ];
-    let events: ReadonlyArray<DomainEvent>;
+    describe('and they have never updated their details', () => {
+      const originalUserDetails = arbitraryUserDetails();
+      const existingEvents = [
+        userCreatedAccount(
+          originalUserDetails.id,
+          originalUserDetails.handle,
+          originalUserDetails.avatarUrl,
+          originalUserDetails.displayName,
+        ),
+      ];
+      let events: ReadonlyArray<DomainEvent>;
 
-    describe('when passed a new avatar url', () => {
-      const newAvatarUrl = arbitraryUri();
-      const command = constructUpdateUserDetailsCommand({
-        userId: originalUserDetails.id,
-        avatarUrl: newAvatarUrl,
+      describe('when passed a new avatar url', () => {
+        const newAvatarUrl = arbitraryUri();
+        const command = constructUpdateUserDetailsCommand({
+          userId: originalUserDetails.id,
+          avatarUrl: newAvatarUrl,
+        });
+
+        beforeEach(() => {
+          events = pipe(
+            update(command)(existingEvents),
+            E.getOrElseW(shouldNotBeCalled),
+          );
+        });
+
+        it('raises an event to update avatar url', () => {
+          expect(events).toStrictEqual([
+            expect.objectContaining({
+              userId: originalUserDetails.id,
+              avatarUrl: newAvatarUrl,
+              displayName: undefined,
+            }),
+          ]);
+        });
       });
 
-      beforeEach(() => {
-        events = pipe(
-          update(command)(existingEvents),
-          E.getOrElseW(shouldNotBeCalled),
-        );
+      describe('when passed a new display name', () => {
+        const newDisplayName = arbitraryString();
+        const command = constructUpdateUserDetailsCommand({
+          userId: originalUserDetails.id,
+          displayName: newDisplayName,
+        });
+
+        beforeEach(() => {
+          events = pipe(
+            update(command)(existingEvents),
+            E.getOrElseW(shouldNotBeCalled),
+          );
+        });
+
+        it('raises an event to update display name', () => {
+          expect(events).toStrictEqual([
+            expect.objectContaining({
+              userId: originalUserDetails.id,
+              avatarUrl: undefined,
+              displayName: newDisplayName,
+            }),
+          ]);
+        });
       });
 
-      it('raises an event to update avatar url', () => {
-        expect(events).toStrictEqual([
-          expect.objectContaining({
-            userId: originalUserDetails.id,
-            avatarUrl: newAvatarUrl,
-            displayName: undefined,
-          }),
-        ]);
+      describe('when passed a new display name and a new avatar url', () => {
+        const newAvatarUrl = arbitraryUri();
+        const newDisplayName = arbitraryString();
+        const command = constructUpdateUserDetailsCommand({
+          userId: originalUserDetails.id,
+          avatarUrl: newAvatarUrl,
+          displayName: newDisplayName,
+        });
+
+        beforeEach(() => {
+          events = pipe(
+            update(command)(existingEvents),
+            E.getOrElseW(shouldNotBeCalled),
+          );
+        });
+
+        it('raises an event to update display name and avatar url', () => {
+          expect(events).toStrictEqual([
+            expect.objectContaining({
+              userId: originalUserDetails.id,
+              avatarUrl: newAvatarUrl,
+              displayName: newDisplayName,
+            }),
+          ]);
+        });
+      });
+
+      describe('when passed the existing avatar url', () => {
+        const command = constructUpdateUserDetailsCommand({
+          userId: originalUserDetails.id,
+          avatarUrl: originalUserDetails.avatarUrl,
+        });
+
+        beforeEach(() => {
+          events = pipe(
+            update(command)(existingEvents),
+            E.getOrElseW(shouldNotBeCalled),
+          );
+        });
+
+        it('raises no events', () => {
+          expect(events).toStrictEqual([]);
+        });
+      });
+
+      describe('when passed the existing display name', () => {
+        const command = {
+          userId: originalUserDetails.id,
+          avatarUrl: undefined,
+          displayName: originalUserDetails.displayName,
+        };
+
+        beforeEach(() => {
+          events = pipe(
+            update(command)(existingEvents),
+            E.getOrElseW(shouldNotBeCalled),
+          );
+        });
+
+        it('raises no events', () => {
+          expect(events).toStrictEqual([]);
+        });
+      });
+
+      describe('when passed no avatarUrl and no displayName', () => {
+        const command = {
+          userId: originalUserDetails.id,
+          avatarUrl: undefined,
+          displayName: undefined,
+        };
+
+        beforeEach(() => {
+          events = pipe(
+            update(command)(existingEvents),
+            E.getOrElseW(shouldNotBeCalled),
+          );
+        });
+
+        it('raises no events', () => {
+          expect(events).toStrictEqual([]);
+        });
       });
     });
 
-    describe('when passed a new display name', () => {
-      const newDisplayName = arbitraryString();
-      const command = constructUpdateUserDetailsCommand({
-        userId: originalUserDetails.id,
-        displayName: newDisplayName,
+    describe('and they have previously updated their details', () => {
+      describe('when passed a new avatar url', () => {
+        it.todo('raises an event to update avatar url');
       });
 
-      beforeEach(() => {
-        events = pipe(
-          update(command)(existingEvents),
-          E.getOrElseW(shouldNotBeCalled),
-        );
-      });
-
-      it('raises an event to update display name', () => {
-        expect(events).toStrictEqual([
-          expect.objectContaining({
-            userId: originalUserDetails.id,
-            avatarUrl: undefined,
-            displayName: newDisplayName,
-          }),
-        ]);
+      describe('when passed a new display name', () => {
+        it.todo('raises an event to update display name');
       });
     });
+  });
 
-    describe('when passed a new display name and a new avatar url', () => {
-      const newAvatarUrl = arbitraryUri();
-      const newDisplayName = arbitraryString();
-      const command = constructUpdateUserDetailsCommand({
-        userId: originalUserDetails.id,
-        avatarUrl: newAvatarUrl,
-        displayName: newDisplayName,
-      });
-
-      beforeEach(() => {
-        events = pipe(
-          update(command)(existingEvents),
-          E.getOrElseW(shouldNotBeCalled),
-        );
-      });
-
-      it('raises an event to update display name and avatar url', () => {
-        expect(events).toStrictEqual([
-          expect.objectContaining({
-            userId: originalUserDetails.id,
-            avatarUrl: newAvatarUrl,
-            displayName: newDisplayName,
-          }),
-        ]);
-      });
-    });
-
-    describe('when passed the existing avatar url', () => {
-      const command = constructUpdateUserDetailsCommand({
-        userId: originalUserDetails.id,
-        avatarUrl: originalUserDetails.avatarUrl,
-      });
-
-      beforeEach(() => {
-        events = pipe(
-          update(command)(existingEvents),
-          E.getOrElseW(shouldNotBeCalled),
-        );
-      });
-
-      it('raises no events', () => {
-        expect(events).toStrictEqual([]);
-      });
-    });
-
-    describe('when passed the existing display name', () => {
-      const command = {
-        userId: originalUserDetails.id,
-        avatarUrl: undefined,
-        displayName: originalUserDetails.displayName,
-      };
-
-      beforeEach(() => {
-        events = pipe(
-          update(command)(existingEvents),
-          E.getOrElseW(shouldNotBeCalled),
-        );
-      });
-
-      it('raises no events', () => {
-        expect(events).toStrictEqual([]);
-      });
-    });
-
-    describe('when passed no avatarUrl and no displayName', () => {
-      const command = {
-        userId: originalUserDetails.id,
-        avatarUrl: undefined,
-        displayName: undefined,
-      };
-
-      beforeEach(() => {
-        events = pipe(
-          update(command)(existingEvents),
-          E.getOrElseW(shouldNotBeCalled),
-        );
-      });
-
-      it('raises no events', () => {
-        expect(events).toStrictEqual([]);
-      });
-    });
+  describe('when the user does not exist', () => {
+    it.todo('fails');
   });
 });
