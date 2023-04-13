561372d64f35e81889963082f0a7f66a4f84186e
Kevin Rutherford <kevin@rutherford-software.com>
2023-04-13 08:39:38 +0100

    Add a test list and dummy implementation for a new readmodel query #2187

---
+++ src/shared-read-models/evaluations/get-evaluations-by-group.ts
@@ -0,0 +1,17 @@
+import * as O from 'fp-ts/Option';
+import * as R from 'fp-ts/Record';
+import { identity, pipe } from 'fp-ts/function';
+import { ReadModel, RecordedEvaluation } from './handle-event';
+import { GroupId } from '../../types/group-id';
+
+type GetEvaluationsByGroup = (groupId: GroupId) => ReadonlyArray<RecordedEvaluation>;
+
+// ts-unused-exports:disable-next-line
+export const getEvaluationsByGroup = (readmodel: ReadModel): GetEvaluationsByGroup => (groupId) => pipe(
+  readmodel.byGroupId,
+  R.lookup(groupId),
+  O.match(
+    () => [],
+    identity,
+  ),
+);
---
+++ test/shared-read-models/evaluations/get-evaluations-by-group.test.ts
@@ -0,0 +1,40 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { evaluationRecorded } from '../../../src/domain-events';
+import { arbitraryDoi } from '../../types/doi.helper';
+import { arbitraryGroupId } from '../../types/group-id.helper';
+import { arbitraryReviewId } from '../../types/review-id.helper';
+import { handleEvent, initialState } from '../../../src/shared-read-models/evaluations';
+import { getEvaluationsByGroup } from '../../../src/shared-read-models/evaluations/get-evaluations-by-group';
+
+describe.skip('get-evaluations-by-group', () => {
+  const article1 = arbitraryDoi();
+  const article2 = arbitraryDoi();
+  const group1 = arbitraryGroupId();
+  const group2 = arbitraryGroupId();
+  const reviewId1 = arbitraryReviewId();
+  const reviewId2 = arbitraryReviewId();
+  const reviewId3 = arbitraryReviewId();
+
+  it.each([
+    ['two evaluations', group1, [reviewId1, reviewId2]],
+    ['one evaluation', group2, [reviewId3]],
+    ['no evaluations', arbitraryGroupId(), []],
+  ])('finds the correct evaluations when the group has %s', async (_, groupId, expectedEvaluations) => {
+    const readmodel = pipe(
+      [
+        evaluationRecorded(group1, article1, reviewId1, [], new Date(), new Date('2020-05-19T00:00:00Z')),
+        evaluationRecorded(group1, article2, reviewId2, [], new Date(), new Date('2020-05-21T00:00:00Z')),
+        evaluationRecorded(group2, article1, reviewId3, [], new Date(), new Date('2020-05-20T00:00:00Z')),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+    const actualEvaluations = pipe(
+      groupId,
+      getEvaluationsByGroup(readmodel),
+      RA.map((evaluation) => evaluation.reviewId),
+    );
+
+    expect(actualEvaluations).toStrictEqual(expectedEvaluations);
+  });
+});
