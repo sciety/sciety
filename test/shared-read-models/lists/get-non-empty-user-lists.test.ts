51169179b34d0981f3870be69e5861c08b0f9701
Giorgio Sironi <g.sironi@elifesciences.org>
2023-04-13 14:47:48 +0100

    Express intent for a new query with a test list #2212

---
+++ test/shared-read-models/lists/get-non-empty-user-lists.test.ts
@@ -0,0 +1,17 @@
+describe('get-non-empty-user-lists', () => {
+  describe('when there are populated user lists', () => {
+    it.todo('returns only the populated user lists');
+  });
+
+  describe('when the only user lists are empty', () => {
+    it.todo('returns an empty result');
+  });
+
+  describe('when there are only group lists', () => {
+    it.todo('returns an empty result');
+  });
+
+  describe('when there are no lists', () => {
+    it.todo('returns an empty result');
+  });
+});
