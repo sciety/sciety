f75c33befee6da307fdd9b7e50b4d9f3ec63e34b
Kevin Rutherford <kevin@rutherford-software.com>
2023-04-13 15:51:52 +0100

    Sketch a test for ordering on the page #2212

---
+++ test/html-pages/list-of-user-lists-page/construct-view-model/construct-view-model.test.ts
@@ -0,0 +1,40 @@
+import { TestFramework, createTestFramework } from '../../../framework';
+import * as LOID from '../../../../src/types/list-owner-id';
+import { List } from '../../../../src/types/list';
+import { arbitraryList } from '../../../types/list-helper';
+import { arbitraryUserDetails } from '../../../types/user-details.helper';
+import { constructViewModel } from '../../../../src/html-pages/list-of-user-lists-page/construct-view-model/construct-view-model';
+import { ViewModel } from '../../../../src/html-pages/list-of-user-lists-page/view-model';
+import { arbitraryArticleId } from '../../../types/article-id.helper';
+
+describe('construct-view-model', () => {
+  let framework: TestFramework;
+  const user = arbitraryUserDetails();
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
+  describe.skip('when there are two populated user lists', () => {
+    let initialUserList: List;
+    const updatedList = arbitraryList(LOID.fromUserId(user.id));
+    let viewmodel: ViewModel;
+
+    beforeEach(async () => {
+      await framework.commandHelpers.createUserAccount(user);
+      initialUserList = framework.queries.selectAllListsOwnedBy(LOID.fromUserId(user.id))[0];
+      await framework.commandHelpers.addArticleToList(arbitraryArticleId(), initialUserList.id);
+      await framework.commandHelpers.createList(updatedList);
+      await framework.commandHelpers.addArticleToList(arbitraryArticleId(), updatedList.id);
+
+      viewmodel = constructViewModel(framework.queries);
+    });
+
+    it('the most recently updated list is shown first', async () => {
+      expect(viewmodel).toStrictEqual([
+        expect.objectContaining({ listId: updatedList.id }),
+        expect.objectContaining({ listId: initialUserList.id }),
+      ]);
+    });
+  });
+});
