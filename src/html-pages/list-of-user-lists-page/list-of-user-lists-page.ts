cc2ea881bc0750a348fb53fcaded287aadd8cebc
Kevin Rutherford <kevin@rutherford-software.com>
2023-04-13 13:44:23 +0100

    Design basic page architecture #2212

--- src/html-pages/list-of-user-lists-page/list-of-user-lists-page.ts
+++ src/html-pages/list-of-user-lists-page/list-of-user-lists-page.ts
@@ -1,13 +1,24 @@
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import { toHtmlFragment } from '../../types/html-fragment';
 import { Page } from '../../types/page';
 import { RenderPageError } from '../../types/render-page-error';
+import { ListCardViewModel } from '../../shared-components/list-card/render-list-card';
+
+type ViewModel = ReadonlyArray<ListCardViewModel>;
+
+const constructViewModel = () => [];
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const renderAsHtml = (viewModel: ViewModel): Page => ({
+  title: 'Most active user lists',
+  content: toHtmlFragment('Hello World!'),
+});
 
 type ListOfUserListsPage = TE.TaskEither<RenderPageError, Page>;
 
-export const listOfUserListsPage = (): ListOfUserListsPage => (
-  TE.right({
-    title: 'Most active user lists',
-    content: toHtmlFragment('Hello World!'),
-
-  }));
+export const listOfUserListsPage = (): ListOfUserListsPage => pipe(
+  constructViewModel(),
+  renderAsHtml,
+  TE.right,
+);
