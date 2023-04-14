0bb3974eed55e2220d6acb3e596463ee0fe6877f
LinaKind <l.kinduryte@elifesciences.org>
2023-04-14 15:02:00 +0100

    Write a test list for the query #2218

---
+++ test/shared-read-models/lists/select-all-lists-containing-article.test.ts
@@ -0,0 +1,17 @@
+describe('select-all-lists-containing-article', () => {
+  describe('when the article is not in any list', () => {
+    it.todo('returns an empty result');
+  });
+
+  describe('when the article appears in one list', () => {
+    it.todo('returns one list');
+  });
+
+  describe('when the article appears in a user and a group list', () => {
+    it.todo('returns two lists');
+  });
+
+  describe('when only other articles appear in lists', () => {
+    it.todo('returns an empty result');
+  });
+});
