9edbf1606e0c1a607e0cbf1007b9e15d8af3c9c0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-04-17 13:52:45 +0100

    Make test pass by providing a group name #2218

--- src/html-pages/article-page/construct-view-model/construct-listed-in.ts
+++ src/html-pages/article-page/construct-view-model/construct-listed-in.ts
@@ -1,13 +1,21 @@
 import { pipe } from 'fp-ts/function';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as O from 'fp-ts/Option';
 import { SelectAllListsContainingArticle } from '../../../shared-ports/select-all-lists-containing-article';
 import { Doi } from '../../../types/doi';
 import { ListOwnerId } from '../../../types/list-owner-id';
+import { GetGroup } from '../../../shared-ports';
 
-const getListOwnerName = (ownerId: ListOwnerId) => {
+const getListOwnerName = (ports: Ports) => (ownerId: ListOwnerId) => {
   switch (ownerId.tag) {
     case 'group-id':
-      return 'Hardcoded list owner name';
+      return pipe(
+        ownerId.value,
+        ports.getGroup,
+        O.map((group) => group.name),
+        O.getOrElseW(() => { throw new Error(`Failed to get group ${ownerId.value}`); }),
+      );
+
     case 'user-id':
       return 'Hardcoded list owner name';
   }
@@ -16,6 +24,7 @@ const getListOwnerName = (ownerId: ListOwnerId) => {
 // ts-unused-exports:disable-next-line
 export type Ports = {
   selectAllListsContainingArticle: SelectAllListsContainingArticle,
+  getGroup: GetGroup,
 };
 
 export const constructListedIn = (ports: Ports) => (articleId: Doi) => pipe(
@@ -24,6 +33,6 @@ export const constructListedIn = (ports: Ports) => (articleId: Doi) => pipe(
   RA.map((list) => ({
     listId: list.id,
     listName: list.name,
-    listOwnerName: getListOwnerName(list.ownerId),
+    listOwnerName: getListOwnerName(ports)(list.ownerId),
   })),
 );
