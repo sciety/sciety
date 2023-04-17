6bdb12ac1996d6a93ec31af712fa6d5120397e5e
Giorgio Sironi <g.sironi@elifesciences.org>
2023-04-17 13:52:53 +0100

    Ensure the avatar sits in the correct position on the list card #2221

--- src/shared-components/list-card/render-list-card.ts
+++ src/shared-components/list-card/render-list-card.ts
@@ -20,10 +20,12 @@ const lastUpdated = O.fold(
 
 export const renderListCard = (viewModel: ListCardViewModel): HtmlFragment => toHtmlFragment(`
   <article class="list-card">
-    <h3 class="list-card__title"><a href="${renderListPageLinkHref(viewModel.listId)}" class="list-card__link">${viewModel.title}</a></h3>
-    <p>${viewModel.description}</p>
-    <div class="list-card__meta">
-      <span class="visually-hidden">This list contains </span><span>${viewModel.articleCount} article${viewModel.articleCount === 1 ? '' : 's'}</span>${lastUpdated(viewModel.updatedAt)}
+    <div class="list-card__body">
+      <h3 class="list-card__title"><a href="${renderListPageLinkHref(viewModel.listId)}" class="list-card__link">${viewModel.title}</a></h3>
+      <p>${viewModel.description}</p>
+      <div class="list-card__meta">
+        <span class="visually-hidden">This list contains </span><span>${viewModel.articleCount} article${viewModel.articleCount === 1 ? '' : 's'}</span>${lastUpdated(viewModel.updatedAt)}
+      </div>
     </div>
     ${viewModel.avatarUrl !== undefined ? `<img class="user-list-card__avatar" src="${viewModel.avatarUrl}" alt="" />` : ''}
   </article>
