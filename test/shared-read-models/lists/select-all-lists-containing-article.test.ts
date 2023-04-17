7eade50577a8b1ab02d23eadc48a6b1951897ae2
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-04-17 10:01:54 +0100

    Implement already passing test #2218

--- test/shared-read-models/lists/select-all-lists-containing-article.test.ts
+++ test/shared-read-models/lists/select-all-lists-containing-article.test.ts
@@ -4,7 +4,10 @@ import { arbitraryList } from '../../types/list-helper';
 import { handleEvent, initialState } from '../../../src/shared-read-models/lists';
 import { articleAddedToList, listCreated } from '../../../src/domain-events';
 import { arbitraryArticleId } from '../../types/article-id.helper';
+import * as LOID from '../../../src/types/list-owner-id';
 import { selectAllListsContainingArticle } from '../../../src/shared-read-models/lists/select-all-lists-containing-article';
+import { arbitraryUserId } from '../../types/user-id.helper';
+import { arbitraryGroupId } from '../../types/group-id.helper';
 
 describe('select-all-lists-containing-article', () => {
   describe('when the article is not in any list', () => {
@@ -34,7 +37,25 @@ describe('select-all-lists-containing-article', () => {
   });
 
   describe('when the article appears in a user and a group list', () => {
-    it.todo('returns two lists');
+    const userList = arbitraryList(LOID.fromUserId(arbitraryUserId()));
+    const groupList = arbitraryList(LOID.fromGroupId(arbitraryGroupId()));
+    const articleId = arbitraryArticleId();
+    const readModel = pipe(
+      [
+        listCreated(userList.id, userList.name, userList.description, userList.ownerId),
+        articleAddedToList(articleId, userList.id),
+        listCreated(groupList.id, groupList.name, groupList.description, groupList.ownerId),
+        articleAddedToList(articleId, groupList.id),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it('returns two lists', () => {
+      expect(selectAllListsContainingArticle(readModel)(articleId)).toStrictEqual([
+        expect.objectContaining({ id: userList.id }),
+        expect.objectContaining({ id: groupList.id }),
+      ]);
+    });
   });
 
   describe('when only other articles appear in lists', () => {
