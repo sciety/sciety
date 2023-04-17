00f84e35d757b69f0933631f33646caca174222a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-04-17 14:55:56 +0100

    Express intention for new getUser query #2218

---
+++ test/shared-read-models/users/get-user.test.ts
@@ -0,0 +1,9 @@
+describe('get user', () => {
+  describe('when the user exists', () => {
+    it.todo('returns relevant user details');
+  });
+
+  describe('when the user does not exist', () => {
+    it.todo('throws an error');
+  });
+});
