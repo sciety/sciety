ff0f0083ff930c7248cff6d5e206150b080048d5
Giorgio Sironi <g.sironi@elifesciences.org>
2023-04-17 14:09:55 +0100

    Extract list card view model construction #2221

--- src/html-pages/lists-page/construct-view-model/construct-view-model.ts
+++ src/html-pages/lists-page/construct-view-model/construct-view-model.ts
@@ -2,18 +2,21 @@ import { pipe } from 'fp-ts/function';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as O from 'fp-ts/Option';
 import { GetNonEmptyUserLists } from '../../../shared-ports';
+import { List } from '../../../types/list';
+import { ListCardViewModel } from '../../../shared-components/list-card/render-list-card';
 
 export type Ports = {
   getNonEmptyUserLists: GetNonEmptyUserLists,
 };
+const constructListCardViewModel = (list: List): ListCardViewModel => ({
+  listId: list.id,
+  articleCount: list.articleIds.length,
+  updatedAt: O.some(list.updatedAt),
+  title: list.name,
+  description: list.description,
+});
 
 export const constructViewModel = (ports: Ports) => pipe(
   ports.getNonEmptyUserLists(),
-  RA.map((list) => ({
-    listId: list.id,
-    articleCount: list.articleIds.length,
-    updatedAt: O.some(list.updatedAt),
-    title: list.name,
-    description: list.description,
-  })),
+  RA.map(constructListCardViewModel),
 );
