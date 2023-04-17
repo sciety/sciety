08073d7436d48a60c444daec2a95ae7802dc8625
David Moulton <d.moulton@elifesciences.org>
2023-04-17 10:59:46 +0100

    Start a test list for listed-in view model #2218

---
+++ test/html-pages/article-page/construct-view-model/construct-listed-in.test.ts
@@ -0,0 +1,21 @@
+describe('construct-listed-in', () => {
+  describe('when the article is not in any list', () => {
+    it.todo('returns empty');
+  });
+
+  describe('when the article is in a list owned by a user', () => {
+    it.todo('returns the list id');
+
+    it.todo('returns the list name');
+
+    it.todo('returns the list owner name');
+  });
+
+  describe('when the article is in a list owned by a group', () => {
+    it.todo('returns the list id');
+
+    it.todo('returns the list name');
+
+    it.todo('returns the list owner name');
+  });
+});
