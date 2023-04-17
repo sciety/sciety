1ca9819b567d836b7336ccd7f576cfbaef92a538
David Moulton <d.moulton@elifesciences.org>
2023-04-17 13:40:36 +0100

    Make it possible to handle different types of owner id separately #2218

--- src/html-pages/article-page/construct-view-model/construct-listed-in.ts
+++ src/html-pages/article-page/construct-view-model/construct-listed-in.ts
@@ -2,6 +2,16 @@ import { pipe } from 'fp-ts/function';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { SelectAllListsContainingArticle } from '../../../shared-ports/select-all-lists-containing-article';
 import { Doi } from '../../../types/doi';
+import { ListOwnerId } from '../../../types/list-owner-id';
+
+const getListOwnerName = (ownerId: ListOwnerId) => {
+  switch (ownerId.tag) {
+    case 'group-id':
+      return 'Hardcoded list owner name';
+    case 'user-id':
+      return 'Hardcoded list owner name';
+  }
+};
 
 // ts-unused-exports:disable-next-line
 export type Ports = {
@@ -14,6 +24,6 @@ export const constructListedIn = (ports: Ports) => (articleId: Doi) => pipe(
   RA.map((list) => ({
     listId: list.id,
     listName: list.name,
-    listOwnerName: 'Hardcoded list owner name',
+    listOwnerName: getListOwnerName(list.ownerId),
   })),
 );
