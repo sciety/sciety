059b1e3144451baabde3c9cc3c991b4b20cbb397
LinaKind <l.kinduryte@elifesciences.org>
2023-04-18 14:32:08 +0100

    Further siplify pipe #2221

--- src/html-pages/home-page/cards/user-list-card.ts
+++ src/html-pages/home-page/cards/user-list-card.ts
@@ -1,16 +1,13 @@
-import { sequenceS } from 'fp-ts/Apply';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
-import { LookupList, LookupUser } from '../../../shared-ports';
+import { LookupList } from '../../../shared-ports';
 import { HtmlFragment } from '../../../types/html-fragment';
-import { userIdCodec } from '../../../types/user-id';
 import { ListId } from '../../../types/list-id';
 import { renderListCard } from '../../../shared-components/list-card/render-list-card';
 import { constructListCardViewModel, Ports as ConstructListCardViewModelPorts } from '../../../shared-components/list-card/construct-list-card-view-model';
 
 export type Ports = ConstructListCardViewModelPorts & {
   lookupList: LookupList,
-  lookupUser: LookupUser,
 };
 
 export const userListCard = (
@@ -18,20 +15,6 @@ export const userListCard = (
 ) => (listId: ListId): O.Option<HtmlFragment> => pipe(
   listId,
   ports.lookupList,
-  O.chain((list) => pipe(
-    {
-      list: O.some(list),
-      listOwner: pipe(
-        list.ownerId.value,
-        userIdCodec.decode,
-        O.fromEither,
-        O.chain(ports.lookupUser),
-      ),
-    },
-    sequenceS(O.Apply),
-  )),
-  O.map(({ list }) => (
-    constructListCardViewModel(ports)(list)
-  )),
+  O.map(constructListCardViewModel(ports)),
   O.map(renderListCard),
 );
