1353325d61ecefb6dacfe91f37a51eb83f19b3ba
David Moulton <d.moulton@elifesciences.org>
2023-04-18 14:30:22 +0100

    Allow arbitrary lists to have either kind of owner #2218

--- test/types/list-owner-id.helper.ts
+++ test/types/list-owner-id.helper.ts
@@ -1,6 +1,11 @@
 import { arbitraryUserId } from './user-id.helper';
 import * as LOID from '../../src/types/list-owner-id';
+import { arbitraryNumber } from '../helpers';
+import { arbitraryGroupId } from './group-id.helper';
 
 type ArbitraryListOwnerId = () => LOID.ListOwnerId;
 
-export const arbitraryListOwnerId: ArbitraryListOwnerId = () => LOID.fromUserId(arbitraryUserId());
+export const arbitraryListOwnerId: ArbitraryListOwnerId = () => {
+  const choice = arbitraryNumber(0, 1);
+  return choice === 0 ? LOID.fromUserId(arbitraryUserId()) : LOID.fromGroupId(arbitraryGroupId());
+};
