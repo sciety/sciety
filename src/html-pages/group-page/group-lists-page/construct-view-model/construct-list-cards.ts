3631e8203eafb5062a946a9324ad453ce916da27
Giorgio Sironi <g.sironi@elifesciences.org>
2023-04-19 09:38:32 +0100

    Reuse appropriate list card view model in another place #2221

--- src/html-pages/group-page/group-lists-page/construct-view-model/construct-list-cards.ts
+++ src/html-pages/group-page/group-lists-page/construct-view-model/construct-list-cards.ts
@@ -1,10 +1,9 @@
 import { pipe } from 'fp-ts/function';
-import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
+import { constructListCardViewModelWithoutAvatar } from '../../../../shared-components/list-card';
 import { ListCardViewModel } from '../../../../shared-components/list-card/render-list-card';
 import { SelectAllListsOwnedBy } from '../../../../shared-ports';
 import { Group } from '../../../../types/group';
-import { List } from '../../../../types/list';
 import * as LOID from '../../../../types/list-owner-id';
 import { sortByDefaultListOrdering } from '../../../sort-by-default-list-ordering';
 
@@ -12,19 +11,10 @@ export type Ports = {
   selectAllListsOwnedBy: SelectAllListsOwnedBy,
 };
 
-const toListCardViewModel = (list: List): ListCardViewModel => ({
-  ...list,
-  listId: list.id,
-  title: list.name,
-  articleCount: list.articleIds.length,
-  updatedAt: O.some(list.updatedAt),
-  avatarUrl: O.none,
-});
-
 export const constructListCards = (ports: Ports, group: Group): ReadonlyArray<ListCardViewModel> => pipe(
   group.id,
   LOID.fromGroupId,
   ports.selectAllListsOwnedBy,
   sortByDefaultListOrdering,
-  RA.map(toListCardViewModel),
+  RA.map(constructListCardViewModelWithoutAvatar),
 );
