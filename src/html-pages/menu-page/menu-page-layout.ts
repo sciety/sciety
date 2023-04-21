f063018b9aad74a9ccd6d2e7edaf456de610f427
David Moulton <d.moulton@elifesciences.org>
2023-04-21 17:03:34 +0100

    Extract rendering of seach icon to a function & remove obsolete standalone SVG

--- src/html-pages/menu-page/menu-page-layout.ts
+++ src/html-pages/menu-page/menu-page-layout.ts
@@ -8,6 +8,7 @@ import { utilityBar } from '../../shared-components/utility-bar';
 import { Page } from '../../types/page';
 import { toHtmlFragment } from '../../types/html-fragment';
 import { UserDetails } from '../../types/user-details';
+import { renderSearchIcon } from '../render-search-icon';
 
 // TODO: return a more specific type e.g. HtmlDocument
 export const menuPage = (user: O.Option<UserDetails>) => (referer: O.Option<string>) => ({
@@ -40,25 +41,7 @@ export const menuPageLayout = (user: O.Option<UserDetails>) => (page: Page): str
 
   ${page.content}
   <header class="menu-page-header">
-    <a href="/search" class="site-header__search_link">
-      <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-          <g id="Designs" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-              <g id="results-mobile-broken-down" transform="translate(-367.000000, -169.000000)">
-                  <g id="Group" transform="translate(95.000000, 112.000000)">
-                      <g id="new/navbar-mobile" transform="translate(0.000000, 36.000000)">
-                          <g id="page-tools" transform="translate(168.000000, 12.000000)">
-                              <g id="baseline-search-24px" transform="translate(104.000000, 9.000000)">
-                                  <path class="site-header__search_icon_path" d="M15.5,14 L14.71,14 L14.43,13.73 C15.41,12.59 16,11.11 16,9.5 C16,5.91 13.09,3 9.5,3 C5.91,3 3,5.91 3,9.5 C3,13.09 5.91,16 9.5,16 C11.11,16 12.59,15.41 13.73,14.43 L14,14.71 L14,15.5 L19,20.49 L20.49,19 L15.5,14 Z M9.5,14 C7.01,14 5,11.99 5,9.5 C5,7.01 7.01,5 9.5,5 C11.99,5 14,7.01 14,9.5 C14,11.99 11.99,14 9.5,14 Z" id="Shape" fill="#34434A" fill-rule="nonzero"/>
-                                  <polygon id="Path" points="0 0 24 0 24 24 0 24"/>
-                              </g>
-                          </g>
-                      </g>
-                  </g>
-              </g>
-          </g>
-      </svg>
-<span class="site-header__search_label">Search</span>
-    </a>
+    <a href="/search" class="site-header__search_link">${renderSearchIcon()}<span class="site-header__search_label">Search</span></a>
     ${utilityBar(user)}
   </header>
 
--- src/html-pages/render-search-icon.ts
+++ src/html-pages/render-search-icon.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" encoding="UTF-8"?>
+import { toHtmlFragment } from '../types/html-fragment';
+
+export const renderSearchIcon = () => toHtmlFragment(`<?xml version="1.0" encoding="UTF-8"?>
 <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <g id="Designs" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="results-mobile-broken-down" transform="translate(-367.000000, -169.000000)">
@@ -15,3 +17,4 @@
         </g>
     </g>
 </svg>
+`);
