208bffe9ca6d2c51f66dd5f26faa5475ed609096
Kevin Rutherford <kevin@rutherford-software.com>
2023-04-21 15:15:46 +0100

    Stop making cross-resource consistency check #2187

--- src/write-side/record-evaluation/record-evaluation-command-handler.ts
+++ src/write-side/record-evaluation/record-evaluation-command-handler.ts
@@ -1,4 +1,3 @@
-import * as E from 'fp-ts/Either';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
@@ -6,7 +5,7 @@ import { executeCommand } from './execute-command';
 import { RecordEvaluationCommand } from '../commands';
 import { CommitEvents, GetAllEvents, GetGroup } from '../../shared-ports';
 import { CommandResult } from '../../types/command-result';
-import { ErrorMessage, toErrorMessage } from '../../types/error-message';
+import { ErrorMessage } from '../../types/error-message';
 
 export type Ports = {
   getAllEvents: GetAllEvents,
@@ -14,20 +13,13 @@ export type Ports = {
   getGroup: GetGroup,
 };
 
-const confirmGroupExists = (ports: Ports) => (command: RecordEvaluationCommand) => pipe(
-  command.groupId,
-  ports.getGroup,
-  E.fromOption(() => toErrorMessage(`Group "${command.groupId}" not found`)),
-);
-
 type RecordEvaluationCommandHandler = (ports: Ports)
 => (command: RecordEvaluationCommand)
 => TE.TaskEither<ErrorMessage, CommandResult>;
 
 export const recordEvaluationCommandHandler: RecordEvaluationCommandHandler = (ports) => (command) => pipe(
   command,
-  confirmGroupExists(ports),
-  TE.fromEither,
+  TE.right,
   TE.chainTaskK(() => pipe(
     ports.getAllEvents,
     T.map(executeCommand(command)),
