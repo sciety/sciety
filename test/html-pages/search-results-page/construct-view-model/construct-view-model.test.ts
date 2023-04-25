cd13f7352d9f9d2fd30451744e7689261168709a
David Moulton <d.moulton@elifesciences.org>
2023-04-25 13:59:00 +0100

    Write partial first draft of test list to substitute legacy tests relying of stubbing of a query we're trying to replace #2238

---
+++ test/html-pages/search-results-page/construct-view-model/construct-view-model.test.ts
@@ -0,0 +1,25 @@
+describe('construct-view-model', () => {
+  describe('when the category is "articles"', () => {
+    describe('and there are results', () => {
+      it.todo('write many of these');
+    });
+
+    describe('but there are no results', () => {
+      it.todo('write many of these');
+    });
+  });
+
+  describe('when the category is "groups"', () => {
+    describe('and there are results', () => {
+      it.todo('write many of these');
+    });
+
+    describe('but there are no results', () => {
+      it.todo('write many of these');
+    });
+  });
+
+  describe('when the category is not provided', () => {
+    it.todo('the category defaults to articles');
+  });
+});
