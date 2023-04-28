978390c43c557e64170bcd36adcbd59fb2dccf26
David Moulton <d.moulton@elifesciences.org>
2023-04-28 10:19:02 +0100

    Write happy path test list #2238

---
+++ test/shared-components/group-card/construct-group-card-view-model.test.ts
@@ -0,0 +1,21 @@
+describe('construct-group-card-view-model', () => {
+  describe('when all data is available', () => {
+    it.todo('contains the group id');
+
+    it.todo('contains the group name');
+
+    it.todo('contains the group description');
+
+    it.todo('contains the group avatar path');
+
+    it.todo('contains the group slug');
+
+    it.todo('contains the list count');
+
+    it.todo('contains the follower count');
+
+    it.todo('contains the evaluation count');
+
+    it.todo('contains the latest activity');
+  });
+});
