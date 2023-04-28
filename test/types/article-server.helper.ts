62c05bd94d9b7e7d47387d3c7abb0b15b0cd5eaa
Giorgio Sironi <g.sironi@elifesciences.org>
2023-04-28 11:51:04 +0100

    Fix test helper to offer all values of the type #2238

--- test/types/article-server.helper.ts
+++ test/types/article-server.helper.ts
@@ -1,7 +1,13 @@
 import { ArticleServer } from '../../src/types/article-server';
+import { arbitraryNumber } from '../helpers';
 
-export const arbitraryArticleServer = (): ArticleServer => (
-  Math.random() > 0.5
-    ? 'biorxiv' as const
-    : 'medrxiv' as const
-);
+export const arbitraryArticleServer = (): ArticleServer => {
+  const articleServers: ReadonlyArray<ArticleServer> = [
+    'biorxiv',
+    'medrxiv',
+    'researchsquare',
+    'scielopreprints',
+    'osf',
+  ];
+  return articleServers[arbitraryNumber(0, articleServers.length - 1)];
+};
