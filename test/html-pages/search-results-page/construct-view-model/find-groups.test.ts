5a6f775f473072bed5a47a0c3c6e3295ea3d7ddb
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-05-02 13:56:17 +0100

    Rewrite a viewmodel construction test to use the framework to make it easier to use Queries type wholesale #2262

--- test/html-pages/search-results-page/construct-view-model/find-groups.test.ts
+++ test/html-pages/search-results-page/construct-view-model/find-groups.test.ts
@@ -2,21 +2,31 @@ import * as TE from 'fp-ts/TaskEither';
 import { GroupId } from '../../../../src/types/group-id';
 import { arbitraryString } from '../../../helpers';
 import { arbitraryGroup } from '../../../types/group.helper';
-import { findGroups } from '../../../../src/html-pages/search-results-page/construct-view-model/find-groups';
+import { Ports, findGroups } from '../../../../src/html-pages/search-results-page/construct-view-model/find-groups';
+import { TestFramework, createTestFramework } from '../../../framework';
 
 describe('find-groups', () => {
-  const group1 = arbitraryGroup();
-  const group2 = arbitraryGroup();
-  const ports = {
-    fetchStaticFile: () => TE.right(''),
-    getAllGroups: () => [group1, group2],
-  };
+  let framework: TestFramework;
+  let adapters: Ports;
+
+  beforeEach(async () => {
+    framework = createTestFramework();
+    adapters = {
+      fetchStaticFile: () => TE.right(''),
+      ...framework.queries,
+    };
+  });
 
   describe('when there are matching groups', () => {
+    const group1 = arbitraryGroup();
+    const group2 = arbitraryGroup();
+
     let result: ReadonlyArray<GroupId>;
 
     beforeEach(async () => {
-      result = await findGroups(ports, group1.name)();
+      await framework.commandHelpers.createGroup(group1);
+      await framework.commandHelpers.createGroup(group2);
+      result = await findGroups(adapters, group1.name)();
     });
 
     it('returns an array containing the groupId', async () => {
@@ -28,7 +38,7 @@ describe('find-groups', () => {
     let result: ReadonlyArray<GroupId>;
 
     beforeEach(async () => {
-      result = await findGroups(ports, arbitraryString())();
+      result = await findGroups(adapters, arbitraryString())();
     });
 
     it('returns an empty array', async () => {
