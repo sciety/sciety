433d744cf161ef3649379f3200981905503b313a
Giorgio Sironi <g.sironi@elifesciences.org>
2023-05-03 15:36:27 +0100

    Implement desired command type via codec #2257

--- src/write-side/commands/erase-evaluation.ts
+++ src/write-side/commands/erase-evaluation.ts
@@ -1,7 +1,8 @@
 import * as t from 'io-ts';
+import { evaluationLocatorCodec } from '../../types/evaluation-locator';
 
 export const eraseEvaluationCommandCodec = t.type({
-
+  evaluationLocator: evaluationLocatorCodec,
 });
 
 export type EraseEvaluationCommand = t.TypeOf<typeof eraseEvaluationCommandCodec>;
