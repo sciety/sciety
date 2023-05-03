4ce28949499141e2ffb2349b9062309a4baa7f87
David Moulton <d.moulton@elifesciences.org>
2023-05-03 15:50:34 +0100

    Write a test list for the write side #2257

---
+++ test/write-side/resources/evaluation/erase.test.ts
@@ -0,0 +1,13 @@
+describe('erase', () => {
+  describe('when an evaluation has been incorrectly recorded', () => {
+    describe('and the action is executed', () => {
+      it.todo('raises an IncorrectlyRecordedEvaluationErased event');
+    });
+  });
+
+  describe('when evaluation has not been recorded', () => {
+    describe('and the action is executed', () => {
+      it.todo('raises no event');
+    });
+  });
+});
