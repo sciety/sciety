2a86fcae652f080e10121b6ad845ccd8e2888d7b
David Moulton <d.moulton@elifesciences.org>
2023-05-03 14:25:24 +0100

    Refactor to use type guards #2257

--- src/shared-read-models/article-activity/handle-event.ts
+++ src/shared-read-models/article-activity/handle-event.ts
@@ -2,7 +2,12 @@
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { ListId } from '../../types/list-id';
-import { DomainEvent } from '../../domain-events';
+import {
+  DomainEvent,
+  isArticleAddedToListEvent, isArticleRemovedFromListEvent,
+  isEvaluationRecordedEvent,
+  isIncorrectlyRecordedEvaluationErasedEvent,
+} from '../../domain-events';
 import { Doi } from '../../types/doi';
 import { ReviewId } from '../../types/review-id';
 
@@ -27,72 +32,71 @@ export type ReadModel = Map<string, ArticleState>;
 export const initialState = (): ReadModel => new Map();
 
 export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel => {
-  switch (event.type) {
-    case 'ArticleAddedToList':
-      return pipe(
-        readmodel.get(event.articleId.value),
-        O.fromNullable,
-        O.fold(
-          () => readmodel.set(event.articleId.value, {
-            articleId: event.articleId,
-            evaluationStates: [],
-            lists: new Set([event.listId]),
-          }),
-          (entry) => readmodel.set(event.articleId.value, {
-            ...entry,
-            lists: entry.lists.add(event.listId),
-          }),
-        ),
-      );
-
-    case 'EvaluationRecorded':
-      return pipe(
-        readmodel.get(event.articleId.value),
-        O.fromNullable,
-        O.fold(
-          () => readmodel.set(event.articleId.value, {
-            articleId: event.articleId,
-            evaluationStates: [{
-              evaluationLocator: event.evaluationLocator,
-              publishedAt: event.publishedAt,
-            }],
-            lists: new Set(),
-          }),
-          (entry) => readmodel.set(event.articleId.value, {
-            ...entry,
-            evaluationStates: [...entry.evaluationStates, {
-              evaluationLocator: event.evaluationLocator,
-              publishedAt: event.publishedAt,
-            }],
-          }),
-        ),
-      );
-
-    case 'IncorrectlyRecordedEvaluationErased':
-      readmodel.forEach((articleState) => {
-        const i = articleState.evaluationStates.findIndex(
-          (evaluationState) => evaluationState.evaluationLocator === event.evaluationLocator,
-        );
-        if (i > -1) {
-          articleState.evaluationStates.splice(i, 1);
-        }
-      });
-      return readmodel;
-
-    case 'ArticleRemovedFromList':
-      return pipe(
-        readmodel.get(event.articleId.value),
-        O.fromNullable,
-        O.fold(
-          () => readmodel,
-          (entry) => readmodel.set(event.articleId.value, {
-            ...entry,
-            lists: deleteFromSet(entry.lists, event.listId),
-          }),
-        ),
-      );
-
-    default:
-      return readmodel;
+  if (isArticleAddedToListEvent(event)) {
+    pipe(
+      readmodel.get(event.articleId.value),
+      O.fromNullable,
+      O.fold(
+        () => readmodel.set(event.articleId.value, {
+          articleId: event.articleId,
+          evaluationStates: [],
+          lists: new Set([event.listId]),
+        }),
+        (entry) => readmodel.set(event.articleId.value, {
+          ...entry,
+          lists: entry.lists.add(event.listId),
+        }),
+      ),
+    );
   }
+
+  if (isEvaluationRecordedEvent(event)) {
+    pipe(
+      readmodel.get(event.articleId.value),
+      O.fromNullable,
+      O.fold(
+        () => readmodel.set(event.articleId.value, {
+          articleId: event.articleId,
+          evaluationStates: [{
+            evaluationLocator: event.evaluationLocator,
+            publishedAt: event.publishedAt,
+          }],
+          lists: new Set(),
+        }),
+        (entry) => readmodel.set(event.articleId.value, {
+          ...entry,
+          evaluationStates: [...entry.evaluationStates, {
+            evaluationLocator: event.evaluationLocator,
+            publishedAt: event.publishedAt,
+          }],
+        }),
+      ),
+    );
+  }
+
+  if (isIncorrectlyRecordedEvaluationErasedEvent(event)) {
+    readmodel.forEach((articleState) => {
+      const i = articleState.evaluationStates.findIndex(
+        (evaluationState) => evaluationState.evaluationLocator === event.evaluationLocator,
+      );
+      if (i > -1) {
+        articleState.evaluationStates.splice(i, 1);
+      }
+    });
+  }
+
+  if (isArticleRemovedFromListEvent(event)) {
+    pipe(
+      readmodel.get(event.articleId.value),
+      O.fromNullable,
+      O.fold(
+        () => readmodel,
+        (entry) => readmodel.set(event.articleId.value, {
+          ...entry,
+          lists: deleteFromSet(entry.lists, event.listId),
+        }),
+      ),
+    );
+  }
+  return readmodel;
 };
