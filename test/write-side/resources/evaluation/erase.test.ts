086d673838fa3494d64041cae2cc486f215fa7f3
David Moulton <d.moulton@elifesciences.org>
2023-05-04 10:25:41 +0100

    Write a failing test for the new resource #2257

--- test/write-side/resources/evaluation/erase.test.ts
+++ test/write-side/resources/evaluation/erase.test.ts
@@ -1,7 +1,41 @@
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
+import { erase } from '../../../../src/write-side/resources/evaluation';
+import { shouldNotBeCalled } from '../../../should-not-be-called';
+import { arbitraryReviewId } from '../../../types/review-id.helper';
+import { DomainEvent, evaluationRecorded } from '../../../../src/domain-events';
+import { arbitraryGroupId } from '../../../types/group-id.helper';
+import { arbitraryDoi } from '../../../types/doi.helper';
+
 describe('erase', () => {
   describe('when an evaluation has been incorrectly recorded', () => {
     describe('and the action is executed', () => {
-      it.todo('raises an IncorrectlyRecordedEvaluationErased event');
+      const evaluationLocator = arbitraryReviewId();
+      let eventsRaised: ReadonlyArray<DomainEvent>;
+
+      beforeEach(() => {
+        eventsRaised = pipe(
+          [
+            evaluationRecorded(
+              arbitraryGroupId(),
+              arbitraryDoi(),
+              evaluationLocator,
+              [],
+              new Date(),
+            ),
+          ],
+          erase({ evaluationLocator }),
+          E.getOrElseW(shouldNotBeCalled),
+        );
+      });
+
+      it.failing('raises an IncorrectlyRecordedEvaluationErased event', () => {
+        expect(eventsRaised).toStrictEqual([
+          expect.objectContaining({
+            evaluationLocator,
+          }),
+        ]);
+      });
     });
   });
 
