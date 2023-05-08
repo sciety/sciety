cc123113becb0172fabd606217c0e238bee447c6
Kevin Rutherford <kevin@rutherford-software.com>
2023-05-08 13:33:20 +0100

    Convert another test to use the framework #2269

--- test/html-pages/sciety-feed-page/sciety-feed-page.test.ts
+++ test/html-pages/sciety-feed-page/sciety-feed-page.test.ts
@@ -3,28 +3,14 @@ import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
 import { JSDOM } from 'jsdom';
-import {
-  articleAddedToList,
-  groupJoined, listCreated,
-  userFoundReviewHelpful,
-  userFoundReviewNotHelpful,
-  userRevokedFindingReviewHelpful,
-  userRevokedFindingReviewNotHelpful,
-  userSavedArticle,
-  userUnfollowedEditorialCommunity,
-  userUnsavedArticle,
-} from '../../../src/domain-events';
 import { scietyFeedPage } from '../../../src/html-pages/sciety-feed-page/sciety-feed-page';
 import { Ports } from '../../../src/html-pages/sciety-feed-page/construct-view-model/construct-view-model';
-import * as LOID from '../../../src/types/list-owner-id';
 import { dummyLogger } from '../../dummy-logger';
 import { arbitraryString, arbitraryUri } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 import { arbitraryArticleId } from '../../types/article-id.helper';
-import { arbitraryGroupId } from '../../types/group-id.helper';
 import { arbitraryGroup } from '../../types/group.helper';
 import { arbitraryList } from '../../types/list-helper';
-import { arbitraryListId } from '../../types/list-id.helper';
 import { arbitraryEvaluationLocator } from '../../types/evaluation-locator.helper';
 import { arbitraryUserId } from '../../types/user-id.helper';
 import { arbitraryUserHandle } from '../../types/user-handle.helper';
@@ -109,29 +95,22 @@ describe('sciety-feed-page', () => {
   });
 
   it('does not render uninteresting events', async () => {
-    const listId = arbitraryListId();
+    const articleId = arbitraryArticleId();
+    const list = arbitraryList();
+    const userId = arbitraryUserId();
+    const evaluationLocator = arbitraryEvaluationLocator();
+    await framework.commandHelpers.createGroup(group);
+    await framework.commandHelpers.createList(list);
+    await framework.commandHelpers.addArticleToList(articleId, list.id);
+    await framework.commandHelpers.removeArticleFromList(articleId, list.id);
+    await framework.commandHelpers.respond('respond-helpful', evaluationLocator, userId);
+    await framework.commandHelpers.respond('revoke-response', evaluationLocator, userId);
+    await framework.commandHelpers.respond('respond-not-helpful', evaluationLocator, userId);
+    await framework.commandHelpers.respond('revoke-response', evaluationLocator, userId);
+    await framework.commandHelpers.unfollowGroup(userId, group.id);
     const ports = {
       ...defaultPorts,
-      getAllEvents: T.of([
-        groupJoined(
-          group.id,
-          group.name,
-          group.avatarPath,
-          group.descriptionPath,
-          group.shortDescription,
-          group.homepage,
-          group.slug,
-        ),
-        listCreated(listId, arbitraryString(), arbitraryString(), LOID.fromGroupId(group.id)),
-        articleAddedToList(arbitraryArticleId(), listId),
-        userUnsavedArticle(arbitraryUserId(), arbitraryArticleId()),
-        userUnfollowedEditorialCommunity(arbitraryUserId(), arbitraryGroupId()),
-        userFoundReviewHelpful(arbitraryUserId(), arbitraryEvaluationLocator()),
-        userFoundReviewNotHelpful(arbitraryUserId(), arbitraryEvaluationLocator()),
-        userRevokedFindingReviewHelpful(arbitraryUserId(), arbitraryEvaluationLocator()),
-        userRevokedFindingReviewNotHelpful(arbitraryUserId(), arbitraryEvaluationLocator()),
-        userSavedArticle(arbitraryUserId(), arbitraryArticleId()),
-      ]),
+      getAllEvents: framework.getAllEvents,
     };
     const renderedPage = await pipe(
       scietyFeedPage(ports)(10)({ page: 1 }),
