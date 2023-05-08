980e1482c87ee24dec7b8d34d36eeca5bcf75cf1
Kevin Rutherford <kevin@rutherford-software.com>
2023-05-08 10:25:37 +0100

    Rename a variable to match most other tests #2269

--- test/html-pages/article-page/construct-view-model/project-review-response-counts.test.ts
+++ test/html-pages/article-page/construct-view-model/project-review-response-counts.test.ts
@@ -15,107 +15,107 @@ describe('project-review-response-counts', () => {
   });
 
   describe('given no events', () => {
-    let projected: ResponseCounts;
+    let result: ResponseCounts;
 
     beforeEach(async () => {
-      projected = await pipe(
+      result = await pipe(
         framework.getAllEvents,
         T.map(projectReviewResponseCounts(evaluationLocator)),
       )();
     });
 
     it('returns 0 `helpful` and 0 `not helpful`', () => {
-      expect(projected).toStrictEqual({ helpfulCount: 0, notHelpfulCount: 0 });
+      expect(result).toStrictEqual({ helpfulCount: 0, notHelpfulCount: 0 });
     });
   });
 
   describe('given a user responded to a different evaluation', () => {
-    let projected: ResponseCounts;
+    let result: ResponseCounts;
 
     beforeEach(async () => {
       await framework.commandHelpers.respond('respond-helpful', arbitraryEvaluationLocator(), arbitraryUserId());
-      projected = await pipe(
+      result = await pipe(
         framework.getAllEvents,
         T.map(projectReviewResponseCounts(evaluationLocator)),
       )();
     });
 
     it('returns 0 `helpful` and 0 `not helpful`', () => {
-      expect(projected).toStrictEqual({ helpfulCount: 0, notHelpfulCount: 0 });
+      expect(result).toStrictEqual({ helpfulCount: 0, notHelpfulCount: 0 });
     });
   });
 
   describe('given N different users responded helpful events', () => {
-    let projected: ResponseCounts;
+    let result: ResponseCounts;
 
     beforeEach(async () => {
       await framework.commandHelpers.respond('respond-helpful', evaluationLocator, arbitraryUserId());
       await framework.commandHelpers.respond('respond-helpful', evaluationLocator, arbitraryUserId());
       await framework.commandHelpers.respond('respond-helpful', evaluationLocator, arbitraryUserId());
-      projected = await pipe(
+      result = await pipe(
         framework.getAllEvents,
         T.map(projectReviewResponseCounts(evaluationLocator)),
       )();
     });
 
     it('returns N `helpful` and 0 `not helpful`', () => {
-      expect(projected).toStrictEqual({ helpfulCount: 3, notHelpfulCount: 0 });
+      expect(result).toStrictEqual({ helpfulCount: 3, notHelpfulCount: 0 });
     });
   });
 
   describe('given a single user responded helpful and revoked the helpful response', () => {
     const userId = arbitraryUserId();
-    let projected: ResponseCounts;
+    let result: ResponseCounts;
 
     beforeEach(async () => {
       await framework.commandHelpers.respond('respond-helpful', evaluationLocator, userId);
       await framework.commandHelpers.respond('revoke-response', evaluationLocator, userId);
-      projected = await pipe(
+      result = await pipe(
         framework.getAllEvents,
         T.map(projectReviewResponseCounts(evaluationLocator)),
       )();
     });
 
     it('returns 0 `helpful` and 0 `not helpful`', () => {
-      expect(projected).toStrictEqual({ helpfulCount: 0, notHelpfulCount: 0 });
+      expect(result).toStrictEqual({ helpfulCount: 0, notHelpfulCount: 0 });
     });
   });
 
   describe('given N different users responded not helpful events', () => {
-    let projected: ResponseCounts;
+    let result: ResponseCounts;
 
     beforeEach(async () => {
       await framework.commandHelpers.respond('respond-not-helpful', evaluationLocator, arbitraryUserId());
       await framework.commandHelpers.respond('respond-not-helpful', evaluationLocator, arbitraryUserId());
       await framework.commandHelpers.respond('respond-not-helpful', evaluationLocator, arbitraryUserId());
-      projected = await pipe(
+      result = await pipe(
         framework.getAllEvents,
         T.map(projectReviewResponseCounts(evaluationLocator)),
       )();
     });
 
     it('returns 0 `helpful` and N `not helpful`', () => {
-      expect(projected).toStrictEqual({ helpfulCount: 0, notHelpfulCount: 3 });
+      expect(result).toStrictEqual({ helpfulCount: 0, notHelpfulCount: 3 });
     });
   });
 
   describe('given a single user responded: helpful, revoke helpful, not helpful, revoke not helpful', () => {
     const userId = arbitraryUserId();
-    let projected: ResponseCounts;
+    let result: ResponseCounts;
 
     beforeEach(async () => {
       await framework.commandHelpers.respond('respond-helpful', evaluationLocator, userId);
       await framework.commandHelpers.respond('revoke-response', evaluationLocator, userId);
       await framework.commandHelpers.respond('respond-not-helpful', evaluationLocator, userId);
       await framework.commandHelpers.respond('revoke-response', evaluationLocator, userId);
-      projected = await pipe(
+      result = await pipe(
         framework.getAllEvents,
         T.map(projectReviewResponseCounts(evaluationLocator)),
       )();
     });
 
     it('returns 0 `helpful` and 0 `not helpful`', () => {
-      expect(projected).toStrictEqual({ helpfulCount: 0, notHelpfulCount: 0 });
+      expect(result).toStrictEqual({ helpfulCount: 0, notHelpfulCount: 0 });
     });
   });
 });
