6c8460a3dff412e8a948971772a902f40b1ec1cf
Kevin Rutherford <kevin@rutherford-software.com>
2023-05-08 08:05:25 +0100

    Extract the same header rendering from both user pages #2261

--- src/html-pages/user-page/user-following-page/render-as-html/render-page.ts
+++ src/html-pages/user-page/user-following-page/render-as-html/render-page.ts
@@ -1,18 +1,10 @@
-import { htmlEscape } from 'escape-goat';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 import { ViewModel } from '../view-model';
 import { renderMainContent } from './render-main-content';
+import { renderAsHtml as renderUserPageHeader } from '../../user-page-header/render-as-html';
 
 export const renderPage = (viewmodel: ViewModel): HtmlFragment => toHtmlFragment(`
-  <header class="page-header page-header--user page-header__identity">
-    <img src="${viewmodel.userDetails.avatarUrl}" alt="" class="page-header__avatar">
-    <h1>
-      <span class="visually-hidden">Sciety user </span>${htmlEscape(viewmodel.userDetails.displayName)}
-      <div class="page-header__handle">
-        <span class="visually-hidden">Sciety handle </span>@${viewmodel.userDetails.handle}
-      </div>
-    </h1>
-  </header>
+  ${renderUserPageHeader(viewmodel.userDetails)}
   <div class="main-content main-content--user">
     ${renderMainContent(viewmodel)}
   </div>
