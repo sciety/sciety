673402a4ab6a3e20900612e5f6bf35b7e9a103b1
LinaKind <l.kinduryte@elifesciences.org>
2023-05-09 14:12:08 +0100

    Use codec to decode response #2273

--- src/html-pages/article-page/construct-view-model/construct-related-articles.ts
+++ src/html-pages/article-page/construct-view-model/construct-related-articles.ts
@@ -3,6 +3,7 @@ import * as TO from 'fp-ts/TaskOption';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
+import * as E from 'fp-ts/Either';
 import { Doi } from '../../../types/doi';
 import { sanitise } from '../../../types/sanitised-html-fragment';
 import { toHtmlFragment } from '../../../types/html-fragment';
@@ -45,8 +46,9 @@ const hardcodedResponse: SemanticScholarRecommendedPapersResponse = {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const constructRelatedArticles = (doi: Doi): TO.TaskOption<ReadonlyArray<ArticleViewModel>> => pipe(
   hardcodedResponse,
-  (response) => response.recommendedPapers,
-  RA.map((recommendedPaper) => ({
+  semanticScholarRecommendedPapersResponseCodec.decode,
+  E.map((response) => response.recommendedPapers),
+  E.map(RA.map((recommendedPaper) => ({
     articleId: recommendedPaper.externalIds.DOI,
     title: sanitise(toHtmlFragment(recommendedPaper.title)),
     authors: pipe(
@@ -58,6 +60,6 @@ export const constructRelatedArticles = (doi: Doi): TO.TaskOption<ReadonlyArray<
     latestActivityAt: O.none,
     evaluationCount: 0,
     listMembershipCount: 0,
-  })),
-  TO.some,
+  }))),
+  TO.fromEither,
 );
