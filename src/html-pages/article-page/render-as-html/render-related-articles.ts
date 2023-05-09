28ad14e94d2e0a06045e55192cf942f559591363
LinaKind <l.kinduryte@elifesciences.org>
2023-05-09 10:48:54 +0100

    Introduce an article card (approving backstop noise) #2273

--- src/html-pages/article-page/render-as-html/render-related-articles.ts
+++ src/html-pages/article-page/render-as-html/render-related-articles.ts
@@ -1,8 +1,22 @@
+import * as O from 'fp-ts/Option';
+import { toHtmlFragment } from '../../../types/html-fragment';
+import { renderArticleCard } from '../../../shared-components/article-card/render-article-card';
 import { ViewModel } from '../view-model';
+import { Doi } from '../../../types/doi';
+import { sanitise } from '../../../types/sanitised-html-fragment';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const renderRelatedArticles = (viewmodel: ViewModel) => `
   <div>
     <h3>Related articles</h3>
+    ${renderArticleCard({
+    articleId: new Doi('10.1101/2023.03.24.534097'),
+    title: sanitise(toHtmlFragment('Replication fork plasticity upon replication stress requires rapid nuclear actin polymerization')),
+    authors: O.some(['Maria Dilia Palumbieri', 'C. Merigliano']),
+    latestVersionDate: O.none,
+    latestActivityAt: O.none,
+    evaluationCount: 0,
+    listMembershipCount: 0,
+  })}
   </div>
 `;
