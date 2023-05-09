63fd6844f6bd428be484a6a7bd5bfea4809b1d72
Giorgio Sironi <g.sironi@elifesciences.org>
2023-05-09 11:51:33 +0100

    Start to introduce a type for related articles data #2273

--- src/html-pages/article-page/construct-view-model/construct-related-articles.ts
+++ src/html-pages/article-page/construct-view-model/construct-related-articles.ts
@@ -1,20 +1,30 @@
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
 import { Doi } from '../../../types/doi';
 import { sanitise } from '../../../types/sanitised-html-fragment';
 import { toHtmlFragment } from '../../../types/html-fragment';
+import { DoiFromString } from '../../../types/codecs/DoiFromString';
 
-const hardcodedRelatedArticles = [
+const relatedArticlesCodec = t.array(t.type({
+  articleId: DoiFromString,
+  title: t.string,
+  authors: t.array(t.string),
+}));
+
+type RelatedArticles = t.TypeOf<typeof relatedArticlesCodec>;
+
+const hardcodedRelatedArticles: RelatedArticles = [
   {
     articleId: new Doi('10.1101/2023.03.24.534097'),
-    title: sanitise(toHtmlFragment('Replication fork plasticity upon replication stress requires rapid nuclear actin polymerization')),
-    authors: O.some(['Maria Dilia Palumbieri', 'C. Merigliano']),
+    title: 'Replication fork plasticity upon replication stress requires rapid nuclear actin polymerization',
+    authors: ['Maria Dilia Palumbieri', 'C. Merigliano'],
   },
   {
     articleId: new Doi('10.1101/2023.03.21.533689'),
-    title: sanitise(toHtmlFragment('An endocytic myosin essential for plasma membrane invagination powers motility against resistance')),
-    authors: O.some(['Ross T A Pedersen', 'Aaron Snoberger']),
+    title: 'An endocytic myosin essential for plasma membrane invagination powers motility against resistance',
+    authors: ['Ross T A Pedersen', 'Aaron Snoberger'],
   },
 ];
 
@@ -22,7 +32,9 @@ const hardcodedRelatedArticles = [
 export const constructRelatedArticles = (doi: Doi) => pipe(
   hardcodedRelatedArticles,
   RA.map((relatedArticle) => ({
-    ...relatedArticle,
+    articleId: relatedArticle.articleId,
+    title: sanitise(toHtmlFragment(relatedArticle.title)),
+    authors: O.some(relatedArticle.authors),
     latestVersionDate: O.none,
     latestActivityAt: O.none,
     evaluationCount: 0,
