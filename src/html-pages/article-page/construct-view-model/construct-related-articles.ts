65a6a2791a3385c295b93cb66e21c05d4b9049a0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-05-09 13:18:37 +0100

    Continue shaping the hardcoded data to match the expected response from semantic scholar #2273

--- src/html-pages/article-page/construct-view-model/construct-related-articles.ts
+++ src/html-pages/article-page/construct-view-model/construct-related-articles.ts
@@ -7,36 +7,41 @@ import { sanitise } from '../../../types/sanitised-html-fragment';
 import { toHtmlFragment } from '../../../types/html-fragment';
 import { DoiFromString } from '../../../types/codecs/DoiFromString';
 
-const semanticScholarRecommendedPapersCodec = t.array(t.type({
-  externalIds: t.type({
-    DOI: DoiFromString,
-  }),
-  title: t.string,
-  authors: t.array(t.string),
-}));
+const semanticScholarRecommendedPapersCodec = t.type({
+  recommendedPapers: t.array(t.type({
+    externalIds: t.type({
+      DOI: DoiFromString,
+    }),
+    title: t.string,
+    authors: t.array(t.string),
+  })),
+});
 
 type RelatedArticles = t.TypeOf<typeof semanticScholarRecommendedPapersCodec>;
 
-const hardcodedRelatedArticles: RelatedArticles = [
-  {
-    externalIds: {
-      DOI: new Doi('10.1101/2023.03.24.534097'),
+const hardcodedRelatedArticles: RelatedArticles = {
+  recommendedPapers: [
+    {
+      externalIds: {
+        DOI: new Doi('10.1101/2023.03.24.534097'),
+      },
+      title: 'Replication fork plasticity upon replication stress requires rapid nuclear actin polymerization',
+      authors: ['Maria Dilia Palumbieri', 'C. Merigliano'],
     },
-    title: 'Replication fork plasticity upon replication stress requires rapid nuclear actin polymerization',
-    authors: ['Maria Dilia Palumbieri', 'C. Merigliano'],
-  },
-  {
-    externalIds: {
-      DOI: new Doi('10.1101/2023.03.21.533689'),
+    {
+      externalIds: {
+        DOI: new Doi('10.1101/2023.03.21.533689'),
+      },
+      title: 'An endocytic myosin essential for plasma membrane invagination powers motility against resistance',
+      authors: ['Ross T A Pedersen', 'Aaron Snoberger'],
     },
-    title: 'An endocytic myosin essential for plasma membrane invagination powers motility against resistance',
-    authors: ['Ross T A Pedersen', 'Aaron Snoberger'],
-  },
-];
+  ],
+};
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const constructRelatedArticles = (doi: Doi) => pipe(
   hardcodedRelatedArticles,
+  (response) => response.recommendedPapers,
   RA.map((relatedArticle) => ({
     articleId: relatedArticle.externalIds.DOI,
     title: sanitise(toHtmlFragment(relatedArticle.title)),
