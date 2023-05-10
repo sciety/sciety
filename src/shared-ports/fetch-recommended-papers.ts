3f787fe808d548e2a262f6e8b839d253c89c0c23
LinaKind <l.kinduryte@elifesciences.org>
2023-05-10 10:49:02 +0100

    Define type rather than the codec #2273

--- src/shared-ports/fetch-recommended-papers.ts
+++ src/shared-ports/fetch-recommended-papers.ts
@@ -1,18 +1,14 @@
-import * as t from 'io-ts';
 import * as TE from 'fp-ts/TaskEither';
 import { Doi } from '../types/doi';
-import { DoiFromString } from '../types/codecs/DoiFromString';
 import * as DE from '../types/data-error';
 
-const semanticScholarRecommendedPapersResponseCodec = t.readonlyArray(t.type({
-  articleId: DoiFromString,
-  title: t.string,
-  authors: t.array(t.type({
-    name: t.string,
-  })),
-}));
-
-type SemanticScholarRecommendedPapersResponse = t.TypeOf<typeof semanticScholarRecommendedPapersResponseCodec>;
+type RecommendedPapers = ReadonlyArray<{
+  articleId: Doi,
+  title: string,
+  authors: ReadonlyArray<{
+    name: string,
+  }>,
+}>;
 
 export type FetchRecommendedPapers = (doi: Doi)
-=> TE.TaskEither<DE.DataError, SemanticScholarRecommendedPapersResponse>;
+=> TE.TaskEither<DE.DataError, RecommendedPapers>;
