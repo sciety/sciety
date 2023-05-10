7ca3a3e8e70cdedd323addaf049585f6179d17e3
Giorgio Sironi <g.sironi@elifesciences.org>
2023-05-10 14:15:18 +0100

    Write a failing test for filtering unsupported DOIs #2273

--- test/third-parties/semantic-scholar/fetch-recommended-papers.test.ts
+++ test/third-parties/semantic-scholar/fetch-recommended-papers.test.ts
@@ -3,17 +3,17 @@ import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { RelatedArticles } from '../../../src/shared-ports/fetch-related-articles';
 import { arbitraryArticleId } from '../../types/article-id.helper';
-import { arbitrarySanitisedHtmlFragment, arbitraryString } from '../../helpers';
+import { arbitrarySanitisedHtmlFragment, arbitraryString, arbitraryWord } from '../../helpers';
 import { Ports, fetchRecommendedPapers } from '../../../src/third-parties/semantic-scholar/fetch-recommended-papers';
 import { dummyLogger } from '../../dummy-logger';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 
 describe('fetch-recommended-papers', () => {
-  describe('when a good response is returned', () => {
-    const articleId = arbitraryArticleId();
-    const articleTitle = arbitrarySanitisedHtmlFragment();
-    const articleAuthors = [arbitraryString(), arbitraryString()];
+  const articleId = arbitraryArticleId();
+  const articleTitle = arbitrarySanitisedHtmlFragment();
+  const articleAuthors = [arbitraryString(), arbitraryString()];
 
+  describe('when a good response is returned', () => {
     it('translates to RelatedArticles', async () => {
       const ports: Ports = {
         logger: dummyLogger,
@@ -52,7 +52,50 @@ describe('fetch-recommended-papers', () => {
   });
 
   describe('when a response containing unsuported articles is returned', () => {
-    it.todo('removes the unsupported articles');
+    it.failing('removes the unsupported articles', async () => {
+      const ports: Ports = {
+        logger: dummyLogger,
+        getJson: async () => ({
+          recommendedPapers: [
+            {
+              externalIds: {
+                DOI: articleId.value,
+              },
+              title: articleTitle.toString(),
+              authors: [
+                {
+                  name: articleAuthors[0],
+                },
+                {
+                  name: articleAuthors[1],
+                },
+              ],
+            },
+            {
+              externalIds: {
+                DOI: '10.26434/not-a-supported-doi',
+              },
+              title: arbitraryString(),
+              authors: [
+                {
+                  name: arbitraryWord(),
+                },
+              ],
+            },
+          ],
+        }),
+      };
+      const result = await pipe(
+        arbitraryArticleId(),
+        fetchRecommendedPapers(ports),
+        TE.getOrElseW(shouldNotBeCalled),
+      )();
+      const expected: RelatedArticles = [expect.objectContaining({
+        articleId,
+      })];
+
+      expect(result).toStrictEqual(expected);
+    });
   });
 
   describe('when we cannot access Semantic Scholar', () => {
