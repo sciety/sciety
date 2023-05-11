dabd32b333d0be049077e4950e15f4dc2f6dd0c5
Kevin Rutherford <kevin@rutherford-software.com>
2023-05-11 15:35:59 +0100

    Derive a function return type instead of repeating it #2273

--- src/html-pages/article-page/construct-view-model/construct-related-articles.ts
+++ src/html-pages/article-page/construct-view-model/construct-related-articles.ts
@@ -1,11 +1,12 @@
 import * as O from 'fp-ts/Option';
+import * as T from 'fp-ts/Task';
 import * as TO from 'fp-ts/TaskOption';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
 import { Doi } from '../../../types/doi';
-import { ArticleViewModel } from '../../../shared-components/article-card';
 import { FetchRelatedArticles } from '../../../shared-ports';
+import { ViewModel } from '../view-model';
 
 export type Ports = {
   fetchRelatedArticles: FetchRelatedArticles,
@@ -13,7 +14,7 @@ export type Ports = {
 
 export const constructRelatedArticles = (
   doi: Doi, ports: Ports,
-): TO.TaskOption<ReadonlyArray<ArticleViewModel>> => pipe(
+): T.Task<ViewModel['relatedArticles']> => pipe(
   ports.fetchRelatedArticles(doi),
   TE.map(RA.takeLeft(3)),
   TE.map(RA.map((recommendedPaper) => ({
