e689ad1691d4d62ad7a2d46b91d2514f1ed6fd01
LinaKind <l.kinduryte@elifesciences.org>
2023-05-11 15:26:20 +0100

    Change arbitrary article id to reflect more biorxiv/medrxiv preprint dois #2273

--- test/types/article-id.helper.ts
+++ test/types/article-id.helper.ts
@@ -1,4 +1,4 @@
 import { Doi } from '../../src/types/doi';
-import { arbitraryWord } from '../helpers';
+import { arbitraryNumber } from '../helpers';
 
-export const arbitraryArticleId = (prefix = '10.1101'): Doi => new Doi(`${prefix}/${arbitraryWord(8)}`);
+export const arbitraryArticleId = (prefix = '10.1101'): Doi => new Doi(`${prefix}/${arbitraryNumber(100000, 999999)}`);
