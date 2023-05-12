b80c9a1010eb1daace915ed47e5b3bc60c4cf4f3
LinaKind <l.kinduryte@elifesciences.org>
2023-05-12 11:02:26 +0100

    Reduce noise when a test fails due to a lack of a browser #2283

--- feature-test/utilities.ts
+++ feature-test/utilities.ts
@@ -1,7 +1,11 @@
 import { closeBrowser, screenshot } from 'taiko';
 
 export const screenshotTeardown = async (): Promise<void> => {
-  const currentTestName = expect.getState().currentTestName ?? 'unknown-test';
-  await screenshot({ path: `./feature-test/screenshots/${currentTestName}.png` });
-  await closeBrowser();
+  try {
+    const currentTestName = expect.getState().currentTestName ?? 'unknown-test';
+    await screenshot({ path: `./feature-test/screenshots/${currentTestName}.png` });
+    await closeBrowser();
+  } catch (error: unknown) {
+    process.stderr.write('Warning: cannot take a screenshot\n');
+  }
 };
