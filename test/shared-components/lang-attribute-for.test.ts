a04fe81d8770517690bccdd539333c35f81466fe
Kevin Rutherford <kevin@rutherford-software.com>
2023-05-15 11:43:41 +0100

    Remove tests of deleted code #2286

--- test/shared-components/lang-attribute-for.test.ts
+++
@@ -1,9 +0,0 @@
-import { langAttributeFor } from '../../src/shared-components/lang-attribute-for';
-
-describe('lang-attribute-for', () => {
-  describe('when the language cannot be inferred', () => {
-    it('does not return a lang attribute', async () => {
-      expect(langAttributeFor('12345')).toBe('');
-    });
-  });
-});
