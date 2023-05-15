11943dc1568ce2f176c5cc0bd221b2766036e0a5
Kevin Rutherford <kevin@rutherford-software.com>
2023-05-15 08:40:31 +0100

    Convert another test to use the framework #2269

--- test/html-pages/list-page/construct-view-model/get-user-owner-information.test.ts
+++ test/html-pages/list-page/construct-view-model/get-user-owner-information.test.ts
@@ -1,49 +1,43 @@
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
-import { getUserOwnerInformation, Ports } from '../../../../src/html-pages/list-page/construct-view-model/get-user-owner-information';
-import { arbitraryString, arbitraryUri } from '../../../helpers';
+import { getUserOwnerInformation } from '../../../../src/html-pages/list-page/construct-view-model/get-user-owner-information';
 import { arbitraryUserId } from '../../../types/user-id.helper';
-import { arbitraryUserHandle } from '../../../types/user-handle.helper';
+import { TestFramework, createTestFramework } from '../../../framework';
+import { arbitraryUserDetails } from '../../../types/user-details.helper';
 
 describe('get-user-owner-information', () => {
-  const userId = arbitraryUserId();
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
 
   describe('when given user exists', () => {
-    it('returns the corresponding owner info', () => {
-      const userDisplayName = arbitraryString();
-      const userAvatarUrl = arbitraryUri().toString();
-      const userHandle = arbitraryUserHandle();
-      const ports: Ports = {
-        lookupUser: () => O.some({
-          displayName: userDisplayName,
-          handle: userHandle,
-          avatarUrl: userAvatarUrl,
-          id: userId,
-        }),
-      };
+    const user = arbitraryUserDetails();
 
+    beforeEach(async () => {
+      await framework.commandHelpers.createUserAccount(user);
+    });
+
+    it('returns the corresponding owner info', () => {
       const ownerInfo = pipe(
-        userId,
-        getUserOwnerInformation(ports),
+        user.id,
+        getUserOwnerInformation(framework.queries),
       );
 
       expect(ownerInfo).toStrictEqual(O.some({
-        ownerName: userDisplayName,
-        ownerAvatarPath: userAvatarUrl,
-        ownerHref: `/users/${userHandle}`,
+        ownerName: user.displayName,
+        ownerAvatarPath: user.avatarUrl,
+        ownerHref: `/users/${user.handle}`,
       }));
     });
   });
 
   describe('when the given user does not exist', () => {
     it('returns a not-found error', () => {
-      const ports = {
-        lookupUser: () => O.none,
-      };
-
       const ownerInfo = pipe(
-        userId,
-        getUserOwnerInformation(ports),
+        arbitraryUserId(),
+        getUserOwnerInformation(framework.queries),
       );
 
       expect(ownerInfo).toStrictEqual(O.none);
