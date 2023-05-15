c4792a698292785c22fa07d13dde4160261cdc64
Kevin Rutherford <kevin@rutherford-software.com>
2023-05-15 08:29:15 +0100

    Convert another test to use the framework #2269

--- test/html-pages/article-page/construct-view-model/get-feed-events-content.test.ts
+++ test/html-pages/article-page/construct-view-model/get-feed-events-content.test.ts
@@ -1,42 +1,41 @@
-import { URL } from 'url';
 import * as O from 'fp-ts/Option';
-import * as T from 'fp-ts/Task';
-import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
 import {
   FeedEvent,
   getFeedEventsContent,
 } from '../../../../src/html-pages/article-page/construct-view-model/get-feed-events-content';
-import { toHtmlFragment } from '../../../../src/types/html-fragment';
 import { arbitraryGroupId } from '../../../types/group-id.helper';
-import { arbitraryGroup } from '../../../types/group.helper';
 import { arbitraryEvaluationLocator } from '../../../types/evaluation-locator.helper';
+import { TestFramework, createTestFramework } from '../../../framework';
 
 describe('get-feed-events-content', () => {
-  describe('when there are reviews', () => {
-    it('creates a view model for the reviews', async () => {
-      const groupId = arbitraryGroupId();
-      const feedEvents: ReadonlyArray<FeedEvent> = [
-        {
-          type: 'review',
-          groupId,
-          reviewId: arbitraryEvaluationLocator(),
-          publishedAt: new Date(),
-        },
-        {
-          type: 'review',
-          groupId,
-          reviewId: arbitraryEvaluationLocator(),
-          publishedAt: new Date(),
-        },
-      ];
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
+  describe('when there are evaluations', () => {
+    const groupId = arbitraryGroupId();
+    const feedEvents: ReadonlyArray<FeedEvent> = [
+      {
+        type: 'review',
+        groupId,
+        reviewId: arbitraryEvaluationLocator(),
+        publishedAt: new Date(),
+      },
+      {
+        type: 'review',
+        groupId,
+        reviewId: arbitraryEvaluationLocator(),
+        publishedAt: new Date(),
+      },
+    ];
+
+    it('creates a view model for the evaluations', async () => {
       const ports = {
-        getAllEvents: T.of([]),
-        fetchReview: () => TE.right({
-          fullText: pipe('some text', toHtmlFragment),
-          url: new URL('http://example.com'),
-        }),
-        getGroup: () => O.some(arbitraryGroup()),
+        ...framework.queries,
+        ...framework.happyPathThirdParties,
+        getAllEvents: framework.getAllEvents,
       };
       const viewModel = await getFeedEventsContent(ports, 'biorxiv', O.none)(feedEvents)();
 
