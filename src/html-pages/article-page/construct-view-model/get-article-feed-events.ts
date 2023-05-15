2fec309aeca1e2b03006b5484954b0b1bfee79ba
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-05-15 14:39:56 +0100

    Improve code legibility #2286

--- src/html-pages/article-page/construct-view-model/get-article-feed-events.ts
+++ src/html-pages/article-page/construct-view-model/get-article-feed-events.ts
@@ -3,9 +3,11 @@ import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import * as T from 'fp-ts/Task';
 import * as TO from 'fp-ts/TaskOption';
 import { constant, pipe } from 'fp-ts/function';
-import { getFeedEventsContent, Ports as GetFeedEventsContentPorts } from './get-feed-events-content';
+import * as D from 'fp-ts/Date';
+import * as Ord from 'fp-ts/Ord';
+import { sequenceS } from 'fp-ts/Apply';
+import { FeedEvent, getFeedEventsContent, Ports as GetFeedEventsContentPorts } from './get-feed-events-content';
 import { handleArticleVersionErrors } from './handle-article-version-errors';
-import { mergeFeeds } from './merge-feeds';
 import { ArticleServer } from '../../../types/article-server';
 import { Doi } from '../../../types/doi';
 import { FeedItem } from '../view-model';
@@ -17,6 +19,16 @@ export type Ports = GetFeedEventsContentPorts & {
   getEvaluationsForDoi: Queries['getEvaluationsForDoi'],
 };
 
+const byDate: Ord.Ord<FeedEvent> = pipe(
+  D.Ord,
+  Ord.contramap((event) => event.publishedAt),
+);
+
+const byDateDescending: Ord.Ord<FeedEvent> = pipe(
+  byDate,
+  Ord.reverse,
+);
+
 type GetArticleFeedEventsByDateDescending = (adapters: Ports)
 => (doi: Doi, server: ArticleServer)
 => T.Task<RNEA.ReadonlyNonEmptyArray<FeedItem>>;
@@ -26,21 +38,29 @@ export const getArticleFeedEventsByDateDescending: GetArticleFeedEventsByDateDes
 ) => (
   doi, server,
 ) => pipe(
-  [
-    pipe(
+  {
+    evaluations: pipe(
       adapters.getEvaluationsForDoi(doi),
       T.of,
-      T.map(RA.map((review) => ({ type: 'review', ...review } as const))),
+      T.map(RA.map((review) => ({
+        type: 'review' as const,
+        ...review,
+      }))),
     ),
-    pipe(
+    versions: pipe(
       adapters.findVersionsForArticleDoi(doi, server),
       TO.matchW(
         constant([]),
-        RNEA.map((version) => ({ type: 'article-version', ...version } as const)),
+        RNEA.map((version) => ({
+          type: 'article-version' as const,
+          ...version,
+        })),
       ),
     ),
-  ] as const,
-  mergeFeeds,
+  },
+  sequenceS(T.ApplyPar),
+  T.map((feeds) => [...feeds.evaluations, ...feeds.versions]),
+  T.map(RA.sort(byDateDescending)),
   T.chain(getFeedEventsContent(adapters, server)),
   T.map(handleArticleVersionErrors(server)),
 );
