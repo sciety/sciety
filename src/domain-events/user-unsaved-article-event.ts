4c1497a71464ccfeac252296f32932f4bbe9837f
LinaKind <l.kinduryte@elifesciences.org>
2023-05-16 13:49:52 +0100

    Simplify definition of two obsolete events #2286

--- src/domain-events/user-unsaved-article-event.ts
+++ src/domain-events/user-unsaved-article-event.ts
@@ -1,13 +1,10 @@
 import * as t from 'io-ts';
-import * as tt from 'io-ts-types';
 import { DoiFromString } from '../types/codecs/DoiFromString';
-import { EventIdFromString } from '../types/codecs/EventIdFromString';
 import { userIdCodec } from '../types/user-id';
+import { eventBaseCodec } from './event-base';
 
 export const userUnsavedArticleEventCodec = t.type({
-  id: EventIdFromString,
-  type: t.literal('UserUnsavedArticle'),
-  date: tt.DateFromISOString,
+  ...eventBaseCodec('UserUnsavedArticle'),
   userId: userIdCodec,
   articleId: DoiFromString,
 });
