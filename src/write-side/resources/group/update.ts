4396a5328acc78323bc44f4afc4212dda50a7032
LinaKind <l.kinduryte@elifesciences.org>
2023-05-17 15:51:21 +0100

    Partially implement name check for group details updated event #2169

--- src/write-side/resources/group/update.ts
+++ src/write-side/resources/group/update.ts
@@ -1,17 +1,30 @@
 import * as E from 'fp-ts/Either';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
+import { toErrorMessage } from '../../../types/error-message';
+import { isEventOfType } from '../../../domain-events/domain-event';
 import { UpdateGroupDetailsCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
 import { constructEvent } from '../../../domain-events';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const update: ResourceAction<UpdateGroupDetailsCommand> = (command) => (events) => (
-  E.right([constructEvent('GroupDetailsUpdated')({
-    groupId: command.groupId,
-    name: command.name,
-    shortDescription: undefined,
-    homepage: undefined,
-    avatarPath: undefined,
-    descriptionPath: undefined,
-    slug: undefined,
-  })])
+export const update: ResourceAction<UpdateGroupDetailsCommand> = (command) => (events) => pipe(
+  events,
+  RA.filter(isEventOfType('GroupJoined')),
+  RA.filter((event) => event.groupId === command.groupId),
+  RA.head,
+  O.match(
+    () => E.left(toErrorMessage('not implemented')),
+    () => E.right([constructEvent('GroupDetailsUpdated')({
+      groupId: command.groupId,
+      name: command.name,
+      shortDescription: undefined,
+      homepage: undefined,
+      avatarPath: undefined,
+      descriptionPath: undefined,
+      slug: undefined,
+    })]),
+  )
+  ,
 );
