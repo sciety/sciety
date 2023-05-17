ad15981f7f98c4d7bab6d2348a747c41e5a7d735
David Moulton <d.moulton@elifesciences.org>
2023-05-17 14:44:12 +0100

    Write happy path failing test #2169

--- test/write-side/resources/group/update.test.ts
+++ test/write-side/resources/group/update.test.ts
@@ -1,8 +1,56 @@
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
+import { update } from '../../../../src/write-side/resources/group';
+import { shouldNotBeCalled } from '../../../should-not-be-called';
+import { arbitraryString } from '../../../helpers';
+import { DomainEvent, groupJoined } from '../../../../src/domain-events';
+import { arbitraryGroupId } from '../../../types/group-id.helper';
+import { arbitraryDescriptionPath } from '../../../types/description-path.helper';
+
 describe('update', () => {
   describe('when the group has joined', () => {
     describe('and they have never updated their details', () => {
-      describe('when passed a new name for the group', () => {
-        it.todo('raises an event to update the group name');
+      const groupId = arbitraryGroupId();
+      const existingEvents = [
+        groupJoined(
+          groupId,
+          arbitraryString(),
+          arbitraryString(),
+          arbitraryDescriptionPath(),
+          arbitraryString(),
+          arbitraryString(),
+          arbitraryString(),
+        ),
+      ];
+
+      describe.skip('when passed a new name for the group', () => {
+        const newName = arbitraryString();
+        const command = {
+          groupId,
+          name: newName,
+          shortDescription: undefined,
+          homepage: undefined,
+          avatarPath: undefined,
+          descriptionPath: undefined,
+          slug: undefined,
+        };
+        let events: ReadonlyArray<DomainEvent>;
+
+        beforeEach(() => {
+          events = pipe(
+            update(command)(existingEvents),
+            E.getOrElseW(shouldNotBeCalled),
+          );
+        });
+
+        it('raises an event to update the group name', () => {
+          expect(events).toStrictEqual([
+            expect.objectContaining({
+              groupId,
+              name: newName,
+            }),
+          ]);
+        });
       });
 
       describe('when passed the group\'s existing name', () => {
