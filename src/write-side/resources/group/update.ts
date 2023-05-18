efff3e7c231e514ecba74c1ce835d6afc27dc9fb
David Moulton <d.moulton@elifesciences.org>
2023-05-18 11:02:10 +0100

    Begin to shape the construction of a private read model #2169

--- src/write-side/resources/group/update.ts
+++ src/write-side/resources/group/update.ts
@@ -1,18 +1,42 @@
 import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { toErrorMessage } from '../../../types/error-message';
-import { isEventOfType } from '../../../domain-events/domain-event';
+import { isEventOfType, constructEvent, DomainEvent } from '../../../domain-events';
 import { UpdateGroupDetailsCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
-import { constructEvent } from '../../../domain-events';
+import { GroupId } from '../../../types/group-id';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const update: ResourceAction<UpdateGroupDetailsCommand> = (command) => (events) => pipe(
+type ReadModel = {
+  groupToUpdate: O.Option<{
+    name: string,
+    slug: string,
+  }>,
+};
+
+const buildReadModel = (groupId: GroupId) => (events: ReadonlyArray<DomainEvent>): ReadModel => pipe(
   events,
   RA.filter(isEventOfType('GroupJoined')),
-  RA.filter((event) => event.groupId === command.groupId),
+  RA.filter((event) => event.groupId === groupId),
   RA.head,
+  O.match(
+    () => ({
+      groupToUpdate: O.none,
+    }),
+    (groupToUpdate) => ({
+      groupToUpdate: O.some({
+        name: groupToUpdate.name,
+        slug: groupToUpdate.slug,
+      }),
+    }),
+  ),
+);
+
+export const update: ResourceAction<UpdateGroupDetailsCommand> = (command) => (events) => pipe(
+  events,
+  buildReadModel(command.groupId),
+  (readModel) => readModel.groupToUpdate,
   E.fromOption(() => toErrorMessage('not implemented')),
   E.map(
     (group) => ((group.name === command.name) ? [] : [constructEvent('GroupDetailsUpdated')({
