0ce4e81a2ed84f08f3aa904acf6cc838146f6bce
Kevin Rutherford <kevin@rutherford-software.com>
2023-05-19 08:55:18 +0100

    Improve encapsulation of third-parties #2292

--- src/third-parties/biorxiv/index.ts
+++ src/third-parties/biorxiv/index.ts
@@ -1,3 +1,2 @@
-export {
-  getArticleVersionEventsFromBiorxiv,
-} from './get-article-version-events-from-biorxiv';
+export { getArticleVersionEventsFromBiorxiv } from './get-article-version-events-from-biorxiv';
+export { getBiorxivOrMedrxivCategory } from './get-biorxiv-or-medrxiv-category';
