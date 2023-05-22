156fd11ab902497157deef575ed9e31b7aec435c
David Moulton <d.moulton@elifesciences.org>
2023-05-22 14:31:27 +0100

    Show one large group logo on home page #2311

--- src/html-pages/home-page/groups.ts
+++ src/html-pages/home-page/groups.ts
@@ -1,13 +1,11 @@
 import { toHtmlFragment } from '../../types/html-fragment';
 
-export const groups = process.env.EXPERIMENT_ENABLED === 'true'
-  ? toHtmlFragment(`
+export const groups = toHtmlFragment(`
 <section class="home-page-groups">
   <h2>Groups evaluating preprints on Sciety</h2>
-  <ul>
-    <li><a href="/groups/biophysics-colab">Biophysics Colab</a></li>
+  <ul class="home-page-groups-list">
+    <li><a href="/groups/biophysics-colab" class="home-page-groups-list__link"><span class="visually-hidden">Biophysics Colab</span></a></li>
   </ul>
   <a href="/groups">See all evaluating groups</a>
 </section>
-`)
-  : toHtmlFragment('');
+`);
