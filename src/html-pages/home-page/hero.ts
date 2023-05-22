fe48a166484b4b284089e65b8a375b8b5de4f9ca
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-05-22 11:31:31 +0100

    Refactor for legibility #2305

--- src/html-pages/home-page/hero.ts
+++ src/html-pages/home-page/hero.ts
@@ -2,14 +2,14 @@ import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 
 const renderSearchForm = () => `
   <form class="home-page-hero-search-form" action="/search" method="get">
-    <input type="hidden" name="category" value="articles">
     <label for="searchText" class="home-page-hero-search-form__label">Search for keywords, groups or DOIs</label>
     <div class="home-page-hero-search-form__visible_interactive_elements">
       <input id="searchText" name="query" class="home-page-hero-search-form__text">
       <button type="submit" class="home-page-hero-search-form__button">Search</button>
     </div>
-    <input type="hidden" name="evaluatedOnly" value="true">
     <button type="reset" class="visually-hidden">Reset</button>
+    <input type="hidden" name="evaluatedOnly" value="true">
+    <input type="hidden" name="category" value="articles">
   </form>
 `;
 
