0899a52f9ac4d545a17f2119869477077975e2ca
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-05-22 14:46:33 +0100

    Add a second large logo to the homepage #2311

--- src/html-pages/home-page/groups.ts
+++ src/html-pages/home-page/groups.ts
@@ -4,7 +4,8 @@ export const groups = toHtmlFragment(`
 <section class="home-page-groups">
   <h2>Groups evaluating preprints on Sciety</h2>
   <ul class="home-page-groups-list">
-    <li><a href="/groups/biophysics-colab" class="home-page-groups-list__link"><span class="visually-hidden">Biophysics Colab</span></a></li>
+    <li><a href="/groups/biophysics-colab" class="home-page-groups-list__link home-page-groups-list__link--biophysics-colab"><span class="visually-hidden">Biophysics Colab</span></a></li>
+    <li><a href="/groups/elife" class="home-page-groups-list__link home-page-groups-list__link--elife"><span class="visually-hidden">eLife</span></a></li>
   </ul>
   <a href="/groups">See all evaluating groups</a>
 </section>
