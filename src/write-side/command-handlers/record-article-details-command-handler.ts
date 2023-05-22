070d717ffce67ec0183aeeb04cc98bf3fb920828
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-05-22 20:38:11 +0100

    Make command handler easier to read

--- src/write-side/command-handlers/record-article-details-command-handler.ts
+++ src/write-side/command-handlers/record-article-details-command-handler.ts
@@ -1,20 +1,53 @@
-import { pipe, flow } from 'fp-ts/function';
+import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as O from 'fp-ts/Option';
 import { RecordArticleDetailsCommand } from '../commands';
 import { CommandHandler } from '../../types/command-handler';
 import { CommitEvents, FetchArticle, GetAllEvents } from '../../shared-ports';
-import { DomainEvent, constructEvent, isEventOfType } from '../../domain-events';
-import { toErrorMessage } from '../../types/error-message';
-
-const ONE_DAY = 24 * 60 * 60 * 1000;
+import {
+  DomainEvent, EventOfType, constructEvent, isEventOfType,
+} from '../../domain-events';
+import { ErrorMessage, toErrorMessage } from '../../types/error-message';
+import { Doi } from '../../types/doi';
 
 type Ports = {
   getAllEvents: GetAllEvents,
   commitEvents: CommitEvents,
   fetchArticle: FetchArticle,
 };
+
+const ONE_DAY = 24 * 60 * 60 * 1000;
+
+const getDetailsYoungerThanOneDay = (articleId: Doi) => (
+  events: ReadonlyArray<DomainEvent>,
+): O.Option<EventOfType<'ArticleDetailsRecorded'>> => pipe(
+  events,
+  RA.filter(isEventOfType('ArticleDetailsRecorded')),
+  RA.filter((event) => event.doi.value === articleId.value),
+  RA.last,
+  O.filter((event) => (new Date().getHours() - event.date.getHours()) < ONE_DAY),
+);
+
+const prepareNewEvent = (fetchArticle: Ports['fetchArticle']) => (articleId: Doi): TE.TaskEither<ErrorMessage, EventOfType<'ArticleDetailsRecorded'>> => pipe(
+  articleId,
+  fetchArticle,
+  TE.map((details) => ({
+    doi: details.doi,
+    title: details.title,
+    abstract: details.abstract,
+    server: details.server,
+    authors: pipe(
+      details.authors,
+      O.getOrElseW(() => []),
+      (authors) => [...authors],
+    ),
+  })),
+  TE.bimap(
+    toErrorMessage,
+    constructEvent('ArticleDetailsRecorded'),
+  ),
+);
 
 type RecordArticleDetailsCommandHandler = (
   adapters: Ports
@@ -27,34 +60,14 @@ export const recordArticleDetailsCommandHandler: RecordArticleDetailsCommandHand
 ) => (command) => pipe(
   adapters.getAllEvents,
   TE.rightTask,
-  TE.map(RA.filter(isEventOfType('ArticleDetailsRecorded'))),
-  TE.map(RA.filter((event) => event.doi.value === command.articleId.value)),
-  TE.chainW(flow(
-    RA.last,
-    O.filter((event) => (new Date().getHours() - event.date.getHours()) < ONE_DAY),
-    O.matchW(
-      () => pipe(
-        command.articleId,
-        adapters.fetchArticle,
-        TE.map((details) => ({
-          doi: details.doi,
-          title: details.title,
-          abstract: details.abstract,
-          server: details.server,
-          authors: pipe(
-            details.authors,
-            O.getOrElseW(() => []),
-            (authors) => [...authors],
-          ),
-        })),
-        TE.bimap(
-          toErrorMessage,
-          constructEvent('ArticleDetailsRecorded'),
-        ),
-        TE.map((newEvent) => [newEvent]),
-      ),
-      () => TE.right([] as ReadonlyArray<DomainEvent>),
+  TE.map(getDetailsYoungerThanOneDay(command.articleId)),
+  TE.chainW(O.match(
+    () => pipe(
+      command.articleId,
+      prepareNewEvent(adapters.fetchArticle),
+      TE.map((newEvent) => [newEvent]),
     ),
+    () => TE.right([] as ReadonlyArray<DomainEvent>),
   )),
   TE.chainTaskK(adapters.commitEvents),
 );
