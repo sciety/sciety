49df3c728e753007be88c8a8c98978f1f9ea8fa8
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-05-22 15:44:37 +0100

    Add large logos for the remaining groups to be featured on the homepage #2311

--- src/html-pages/home-page/groups.ts
+++ src/html-pages/home-page/groups.ts
@@ -7,6 +7,11 @@ export const groups = toHtmlFragment(`
     <li><a href="/groups/biophysics-colab" class="home-page-groups-list__link home-page-groups-list__link--biophysics-colab"><span class="visually-hidden">Biophysics Colab</span></a></li>
     <li><a href="/groups/elife" class="home-page-groups-list__link home-page-groups-list__link--elife"><span class="visually-hidden">eLife</span></a></li>
     <li><a href="/groups/prelights" class="home-page-groups-list__link home-page-groups-list__link--prelights"><span class="visually-hidden">preLights</span></a></li>
+    <li><a href="/groups/review-commons" class="home-page-groups-list__link home-page-groups-list__link--review-commons"><span class="visually-hidden">Review Commons</span></a></li>
+    <li><a href="/groups/asapbio-crowd-review" class="home-page-groups-list__link home-page-groups-list__link--asapbio"><span class="visually-hidden">ASAPbio crowd review</span></a></li>
+    <li><a href="/groups/rapid-reviews-covid-19" class="home-page-groups-list__link home-page-groups-list__link--rrid"><span class="visually-hidden">Rapid Reviews Infectious Diseases</span></a></li>
+    <li><a href="/groups/arcadia-science" class="home-page-groups-list__link home-page-groups-list__link--arcadia-science"><span class="visually-hidden">Arcadia Science</span></a></li>
+    <li><a href="/groups/prereview" class="home-page-groups-list__link home-page-groups-list__link--prereview"><span class="visually-hidden">PREreview</span></a></li>
   </ul>
   <a href="/groups">See all evaluating groups</a>
 </section>
