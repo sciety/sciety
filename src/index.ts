3a4775721ecf18deea862bd5f5c2c485bdcda4ad
Kevin Rutherford <kevin@rutherford-software.com>
2023-05-23 08:30:00 +0100

    Create an empty saga #2312

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import {
   CollectedPorts, createInfrastructure, Logger, replaceError,
 } from './infrastructure';
 import { environmentVariablesCodec } from './http/environment-variables-codec';
+import { ensureEvaluationsAreListed } from './sagas/ensure-evaluations-are-listed/ensure-evaluations-are-listed';
 
 const terminusOptions = (logger: Logger): TerminusOptions => ({
   onShutdown: async () => {
@@ -51,6 +52,7 @@ const startSagas = (ports: CollectedPorts) => async () => {
   ports.logger('info', 'Starting sagas');
   setInterval(async () => discoverElifeArticleSubjectArea(ports), 661 * 1000);
   setInterval(async () => addArticleToElifeSubjectAreaList(ports), 13 * 60 * 1000);
+  setInterval(async () => ensureEvaluationsAreListed(ports), 3601 * 1000);
   ports.logger('info', 'Sagas started');
 };
 
