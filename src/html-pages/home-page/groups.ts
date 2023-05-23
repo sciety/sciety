961655b28a92a5690d39911f4560910abe967669
Giorgio Sironi <g.sironi@elifesciences.org>
2023-05-23 10:12:07 +0100

    Start extracting list item view model #2311

--- src/html-pages/home-page/groups.ts
+++ src/html-pages/home-page/groups.ts
@@ -1,10 +1,16 @@
 import { toHtmlFragment } from '../../types/html-fragment';
 
+const listItem = {
+  link: '/groups/biophysics-colab',
+  logoPath: '/static/images/home-page/biophysics-collab.png',
+  name: 'Biophysics Colab',
+};
+
 export const groups = toHtmlFragment(`
 <section class="home-page-groups">
   <h2 class="home-page-groups__title">Groups evaluating preprints on Sciety</h2>
   <ul class="home-page-groups-list">
-    <li><a href="/groups/biophysics-colab" class="home-page-groups-list__link home-page-groups-list__link--biophysics-colab"><span class="visually-hidden">Biophysics Colab</span></a></li>
+    <li><a href="${listItem.link}" class="home-page-groups-list__link" style="background-image: url('${listItem.logoPath}');"><span class="visually-hidden">${listItem.name}</span></a></li>
     <li><a href="/groups/elife" class="home-page-groups-list__link home-page-groups-list__link--elife"><span class="visually-hidden">eLife</span></a></li>
     <li><a href="/groups/prelights" class="home-page-groups-list__link home-page-groups-list__link--prelights"><span class="visually-hidden">preLights</span></a></li>
     <li><a href="/groups/review-commons" class="home-page-groups-list__link home-page-groups-list__link--review-commons"><span class="visually-hidden">Review Commons</span></a></li>
