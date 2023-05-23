c1ebe41f94835ed6f681f6563dc3c41bed1b7042
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-05-23 10:19:26 +0100

    Isolate link rendering #2311

--- src/html-pages/home-page/groups.ts
+++ src/html-pages/home-page/groups.ts
@@ -1,16 +1,26 @@
 import { toHtmlFragment } from '../../types/html-fragment';
 
-const listItem = {
-  link: '/groups/biophysics-colab',
-  logoPath: '/static/images/home-page/biophysics-collab.png',
-  name: 'Biophysics Colab',
+type GroupLinkViewModel = {
+  link: string,
+  logoPath: string,
+  name: string,
 };
+
+const groupLinks = [
+  {
+    link: '/groups/biophysics-colab',
+    logoPath: '/static/images/home-page/biophysics-collab.png',
+    name: 'Biophysics Colab',
+  },
+];
+
+const renderGroup = (groupLink: GroupLinkViewModel) => `<a href="${groupLink.link}" class="home-page-groups-list__link" style="background-image: url('${groupLink.logoPath}');"><span class="visually-hidden">${groupLink.name}</span></a>`;
 
 export const groups = toHtmlFragment(`
 <section class="home-page-groups">
   <h2 class="home-page-groups__title">Groups evaluating preprints on Sciety</h2>
   <ul class="home-page-groups-list">
-    <li><a href="${listItem.link}" class="home-page-groups-list__link" style="background-image: url('${listItem.logoPath}');"><span class="visually-hidden">${listItem.name}</span></a></li>
+    <li>${renderGroup(groupLinks[0])}</li>
     <li><a href="/groups/elife" class="home-page-groups-list__link home-page-groups-list__link--elife"><span class="visually-hidden">eLife</span></a></li>
     <li><a href="/groups/prelights" class="home-page-groups-list__link home-page-groups-list__link--prelights"><span class="visually-hidden">preLights</span></a></li>
     <li><a href="/groups/review-commons" class="home-page-groups-list__link home-page-groups-list__link--review-commons"><span class="visually-hidden">Review Commons</span></a></li>
