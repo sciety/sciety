85aef84854deb41368851822791765c806a6f951
Giorgio Sironi <g.sironi@elifesciences.org>
2023-05-23 11:51:00 +0100

    Write a test list for groups component #2311

---
+++ test/html-pages/home-page/construct-view-model/construct-view-model.test.ts
@@ -0,0 +1,15 @@
+describe('construct-view-model', () => {
+  describe('groups', () => {
+    describe('when all groups can be found', () => {
+      it.todo('returns the links to their group pages');
+
+      it.todo('returns their names');
+
+      it.todo('returns their provided logo paths');
+    });
+
+    describe('when any of the groups can not be found', () => {
+      it.todo('does not include the groups component');
+    });
+  });
+});
