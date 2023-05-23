518b28db551065c8a596a62fb0698ed75ffa5775
Kevin Rutherford <kevin@rutherford-software.com>
2023-05-23 14:21:33 +0100

    Inline a function to consolidate API route handler code #2169

--- src/http/create-api-route-for-command.ts
+++ src/http/create-api-route-for-command.ts
@@ -1,19 +1,40 @@
 import { Middleware } from 'koa';
 import * as TE from 'fp-ts/TaskEither';
-import { flow } from 'fp-ts/function';
+import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
-import { handleScietyApiCommand } from './api/handle-sciety-api-command';
-import { validateInputShape } from '../write-side/commands/validate-input-shape';
-
-import { CollectedPorts } from '../infrastructure';
+import { StatusCodes } from 'http-status-codes';
+import bodyParser from 'koa-bodyparser';
+import compose from 'koa-compose';
 import { CommandHandler, GenericCommand } from '../types/command-handler';
+import { CollectedPorts } from '../infrastructure';
+import { validateInputShape } from '../write-side/commands/validate-input-shape';
+import { logRequestBody } from './api/log-request-body';
+import { requireBearerToken } from './api/require-bearer-token';
 
 export const createApiRouteForCommand = <C extends GenericCommand>(
-  adapters: CollectedPorts,
+  ports: CollectedPorts,
   codec: t.Decoder<unknown, C>,
   commandHandler: CommandHandler<C>,
-): Middleware => handleScietyApiCommand(adapters, flow(
-    validateInputShape(codec),
-    TE.fromEither,
-    TE.chain(commandHandler),
-  ));
+): Middleware => compose([
+    bodyParser({ enableTypes: ['json'] }),
+    logRequestBody(ports.logger),
+    requireBearerToken,
+    async (context) => {
+      await pipe(
+        context.request.body,
+        validateInputShape(codec),
+        TE.fromEither,
+        TE.chain(commandHandler),
+        TE.match(
+          (error) => {
+            context.response.status = StatusCodes.BAD_REQUEST;
+            context.response.body = error;
+          },
+          (eventsCreated) => {
+            context.response.status = eventsCreated === 'events-created' ? StatusCodes.CREATED : StatusCodes.OK;
+            context.response.body = '';
+          },
+        ),
+      )();
+    },
+  ]);
