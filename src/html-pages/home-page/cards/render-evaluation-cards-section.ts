55618c585f09028c2ab7e690ce94b506fda48257
Giorgio Sironi <g.sironi@elifesciences.org>
2023-05-24 15:50:37 +0100

    Separate content from HTML structure #2317

--- src/html-pages/home-page/cards/render-evaluation-cards-section.ts
+++ src/html-pages/home-page/cards/render-evaluation-cards-section.ts
@@ -1,14 +1,28 @@
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 
-const renderEvaluationCard = () => `
+type EvaluationCardViewModel = {
+  articleLink: string,
+  quote: HtmlFragment,
+  caption: string,
+  articleTitle: HtmlFragment,
+};
+
+const evaluationCard = {
+  articleLink: '/articles/activity/10.1101/2022.06.22.497259',
+  quote: toHtmlFragment('The preprint by Yang et al. asks how the shape of the membrane influences the localization of mechanosensitive Piezo channels. The authors use a creative approach involving methods that distort the plasma membrane by generating blebs and artificial filopodia. They convincingly show that curvature of the lipid environment influences Piezo1 localization, such that increased curvature causes channel depletion, and that application of the chemical modulator Yoda1 is sufficient to allow channels to enter filopodia. The study provides support for a provocative “flattening model” of Yoda1 action, and should inspire future studies by researchers interested in mechanosensitive channels and membrane curvature.'),
+  caption: 'Curated by Biophysics Colab',
+  articleTitle: toHtmlFragment('Membrane curvature governs the distribution of Piezo1 in live cells'),
+};
+
+const renderEvaluationCard = (viewModel: EvaluationCardViewModel) => `
   <article class="evaluation-card">
     <figure>
-      <blockquote cite="/articles/activity/10.1101/2022.06.22.497259">
-        The preprint by Yang et al. asks how the shape of the membrane influences the localization of mechanosensitive Piezo channels. The authors use a creative approach involving methods that distort the plasma membrane by generating blebs and artificial filopodia. They convincingly show that curvature of the lipid environment influences Piezo1 localization, such that increased curvature causes channel depletion, and that application of the chemical modulator Yoda1 is sufficient to allow channels to enter filopodia. The study provides support for a provocative “flattening model” of Yoda1 action, and should inspire future studies by researchers interested in mechanosensitive channels and membrane curvature.
+      <blockquote cite="${viewModel.articleLink}">
+        ${viewModel.quote}
       </blockquote>
       <figcaption>
-        <p>Curated by Biophysics Colab</p>
-        <cite><a href="/articles/activity/10.1101/2022.06.22.497259">Membrane curvature governs the distribution of Piezo1 in live cells</a></cite>
+        <p>${viewModel.caption}</p>
+        <cite><a href="${viewModel.articleLink}">${viewModel.articleTitle}</a></cite>
       </figcaption>
     </figure>
   </article>
@@ -18,13 +32,13 @@ const evaluationCards = () => toHtmlFragment(`
   <h2 class="home-page-cards__title">Most recent evaluations</h2>
   <ul class="home-page-cards__cards">
     <li>
-      ${renderEvaluationCard()}
+      ${renderEvaluationCard(evaluationCard)}
     </li>
     <li>
-      ${renderEvaluationCard()}
+      ${renderEvaluationCard(evaluationCard)}
     </li>
     <li>
-      ${renderEvaluationCard()}
+      ${renderEvaluationCard(evaluationCard)}
     </li>
   </ul>
 `);
