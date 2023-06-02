5e1743096d4cb2a8db66100fcec1dfab18619789
LinaKind <l.kinduryte@elifesciences.org>
2023-06-02 15:20:55 +0100

    Write a failing test to avoid articles, on a not supported preprint server, becoming stuck in the evaluated state #2334

--- test/add-article-to-elife-subject-area-list/read-model/get-one-article-id-in-evaluated-state.test.ts
+++ test/add-article-to-elife-subject-area-list/read-model/get-one-article-id-in-evaluated-state.test.ts
@@ -11,7 +11,7 @@ import { getOneArticleIdInEvaluatedState } from '../../../src/add-article-to-eli
 import { Doi } from '../../../src/types/doi';
 
 describe('get-one-article-id-in-evaluated-state', () => {
-  describe('given a bunch of events', () => {
+  describe('given a biorxiv article that has been evaluated by eLife', () => {
     const articleId = new Doi('10.1101/2022.06.22.497259');
 
     const readModel = pipe(
@@ -29,6 +29,27 @@ describe('get-one-article-id-in-evaluated-state', () => {
 
     it('returns one article in evaluated state', () => {
       expect(getOneArticleIdInEvaluatedState(readModel)()).toStrictEqual(O.some(articleId));
+    });
+  });
+
+  describe('given a researchsquare article that has been evaluated by eLife', () => {
+    const articleId = new Doi('10.21203/rs.3.rs-2407778/v1');
+
+    const readModel = pipe(
+      [
+        constructEvent('EvaluationRecorded')({
+          groupId: elifeGroupId,
+          articleId,
+          evaluationLocator: arbitraryEvaluationLocator(),
+          authors: [],
+          publishedAt: arbitraryDate(),
+        }),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it.failing('returns no articles in the evaluated state', () => {
+      expect(getOneArticleIdInEvaluatedState(readModel)()).toStrictEqual(O.none);
     });
   });
 });
