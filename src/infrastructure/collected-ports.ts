3ce6df68edf4dededd15d9e1bd6428ab07d00043
Giorgio Sironi <g.sironi@elifesciences.org>
2023-06-02 10:51:46 +0100

    Remove unnecessary inlined type in favor of shared port #2334

--- src/infrastructure/collected-ports.ts
+++ src/infrastructure/collected-ports.ts
@@ -1,21 +1,4 @@
-import * as T from 'fp-ts/Task';
-import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
-import * as O from 'fp-ts/Option';
-import { URL } from 'url';
 import { SharedPorts } from '../shared-ports';
 import { Queries } from '../shared-read-models';
-import { Doi } from '../types/doi';
-import { ArticleServer } from '../types/article-server';
 
-type ArticleVersion = {
-  source: URL,
-  publishedAt: Date,
-  version: number,
-};
-
-export type CollectedPorts = SharedPorts & Queries & {
-  findVersionsForArticleDoi: (
-    doi: Doi,
-    server: ArticleServer,
-  ) => T.Task<O.Option<RNEA.ReadonlyNonEmptyArray<ArticleVersion>>>,
-};
+export type CollectedPorts = SharedPorts & Queries;
