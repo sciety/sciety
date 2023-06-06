cbb7479a0ecb39087ba3d07eb7ac57b0b02c90c6
Kevin Rutherford <kevin@rutherford-software.com>
2023-06-06 08:34:44 +0100

    Express intent by writing a test #2312

--- test/sagas/ensure-evaluations-are-listed/ensure-evaluations-are-listed.test.ts
+++ test/sagas/ensure-evaluations-are-listed/ensure-evaluations-are-listed.test.ts
@@ -1,6 +1,33 @@
+import { ensureEvaluationsAreListed } from '../../../src/sagas/ensure-evaluations-are-listed/ensure-evaluations-are-listed';
+import { dummyLogger } from '../../dummy-logger';
+import { TestFramework, createTestFramework } from '../../framework';
+
 describe('ensure-evaluations-are-listed', () => {
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
   describe('when there are no unlisted evaluations', () => {
-    it.todo('does nothing');
+    const resources = {
+      list: {
+        addArticle: jest.fn(),
+      },
+    };
+
+    beforeEach(async () => {
+      const ports = {
+        ...framework.queries,
+        logger: dummyLogger,
+        resources,
+      };
+      await ensureEvaluationsAreListed(ports);
+    });
+
+    it('does nothing', () => {
+      expect(resources.list.addArticle).not.toHaveBeenCalled();
+    });
   });
 
   describe('when there are listed evaluations', () => {
