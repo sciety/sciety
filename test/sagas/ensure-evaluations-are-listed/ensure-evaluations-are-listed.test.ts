4e6f40e03e7a62ca18002b874fe7207d6e30c0dd
Kevin Rutherford <kevin@rutherford-software.com>
2023-06-06 08:21:31 +0100

    Sketch an outline test list for the saga #2312

---
+++ test/sagas/ensure-evaluations-are-listed/ensure-evaluations-are-listed.test.ts
@@ -0,0 +1,9 @@
+describe('ensure-evaluations-are-listed', () => {
+  describe('when there are no unlisted evaluations', () => {
+    it.todo('does nothing');
+  });
+
+  describe('when there are listed evaluations', () => {
+    it.todo('adds exactly one missing article to the appropriate list');
+  });
+});
