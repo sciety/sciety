5092e3a939705a4a97f6a22854b6eca9a2a05505
LinaKind <l.kinduryte@elifesciences.org>
2023-06-07 10:30:40 +0100

    Extract construction of curation statements for encapsulation #2346

--- src/html-pages/article-page/construct-view-model/construct-view-model.ts
+++ src/html-pages/article-page/construct-view-model/construct-view-model.ts
@@ -13,12 +13,13 @@ import { ArticleServer } from '../../../types/article-server';
 import * as DE from '../../../types/data-error';
 import { Doi } from '../../../types/doi';
 import { SanitisedHtmlFragment } from '../../../types/sanitised-html-fragment';
-import { CurationStatement, ViewModel } from '../view-model';
+import { ViewModel } from '../view-model';
 import { UserId } from '../../../types/user-id';
 import { constructListedIn, Ports as ConstructListedInPorts } from './construct-listed-in';
 import { constructUserListManagement, Ports as ConstructUserListManagementPorts } from './construct-user-list-management';
 import { constructRelatedArticles, Ports as ConstructRelatedArticlesPorts } from './construct-related-articles';
 import { detectLanguage } from '../../../shared-components/lang-attribute';
+import { constructCurationStatements } from './construct-curation-statements';
 
 export type Params = {
   doi: Doi,
@@ -41,30 +42,6 @@ export type Ports = GetArticleFeedEventsPorts
   fetchArticle: GetArticleDetails,
 };
 
-const curationStatements: ReadonlyArray<CurationStatement> = [
-  {
-    groupName: 'eLife',
-    groupLargeLogo: '/static/images/home-page/elife.svg',
-    statement: `
-      <div lang="en">
-        <p><strong>eLife assessment</strong></p>
-        <p>This fundamental study presents solid evidence for T1r (sweet /umami) taste receptors as chloride (Cl-) receptors, based on a combination of state-of-the-art techniques to demonstrate that T1r receptors from Medaka fish bind chloride and that this binding induces a conformational change in the heteromeric receptor. This conformational change leads to low-concentration chloride-specific action potential firing in nerves from neurons containing these receptors in mice, results that represent an important advance in our understanding of the logic of taste perception.</p>
-      </div>
-    `,
-  },
-  {
-    groupName: 'Biophysics Colab',
-    groupLargeLogo: '/static/images/home-page/biophysics-colab.png',
-    statement: `
-      <div lang="en">
-        <p><strong>Endorsement statement (17 November 2022)</strong></p>
-        <p>The preprint by Atsumi <em>et al</em>. describes how chloride binding to sweet- and umami-sensing proteins (T1R taste receptors) can evoke taste sensation. The authors use an elegant combination of structural, biophysical and electrophysiological approaches to locate a chloride binding site in the ligand-binding domain of medaka fish T1r2a/3 receptors. They convincingly show that low mM concentrations of chloride induce conformational changes and, using single fiber recordings, establish that mouse chorda tympani nerves are activated by chloride in a T1R-dependent manner. This suggests that chloride binding to sweet receptors could mediate the commonly reported sweet taste sensation following ingestion of low concentrations of table salt. The findings will be of broad relevance to those studying taste sensation and ligand recognition in GPCRs.</p>
-        <p><em>(This endorsement by Biophysics Colab refers to version 2 of this preprint, which has been revised in response to peer review of version 1.)</em></p>
-      </div>
-    `,
-  },
-];
-
 type ConstructViewModel = (ports: Ports) => (params: Params) => TE.TaskEither<DE.DataError, ViewModel>;
 
 export const constructViewModel: ConstructViewModel = (ports) => (params) => pipe(
@@ -86,7 +63,7 @@ export const constructViewModel: ConstructViewModel = (ports) => (params) => pip
       ...feedSummary(feedItemsByDateDescending),
       listedIn: constructListedIn(ports)(params.doi),
       relatedArticles,
-      curationStatements: (params.doi.value === '10.1101/2022.02.23.481615') ? curationStatements : [],
+      curationStatements: constructCurationStatements(params.doi),
     })),
   )),
 );
