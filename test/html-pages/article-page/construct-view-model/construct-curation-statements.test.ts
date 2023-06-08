13ec8b1c6d2e3d52648d3116527859c8a56c4e21
Kevin Rutherford <kevin@rutherford-software.com>
2023-06-08 14:50:50 +0100

    Express intent with a test list #2355

---
+++ test/html-pages/article-page/construct-view-model/construct-curation-statements.test.ts
@@ -0,0 +1,9 @@
+describe('construct-curation-statements', () => {
+  describe('when the group exists', () => {
+    it.todo('the curation statement is returned');
+  });
+
+  describe('when the group does not exist', () => {
+    it.todo('the curation statement is skipped');
+  });
+});
