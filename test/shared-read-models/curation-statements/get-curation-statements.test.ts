be939f8edc61a6e43a0461296d6f32a07a4e3643
LinaKind <l.kinduryte@elifesciences.org>
2023-06-09 14:12:45 +0100

    Write a test list for the new query #2355

---
+++ test/shared-read-models/curation-statements/get-curation-statements.test.ts
@@ -0,0 +1,9 @@
+describe('get-curation-statements', () => {
+  describe('when there are curation statements for the article', () => {
+    it.todo('returns them');
+  });
+
+  describe('when there are no curation statements for the article', () => {
+    it.todo('returns an empty array');
+  });
+});
