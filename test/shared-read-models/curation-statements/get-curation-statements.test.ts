c2b8fdeb627072ee203d84f6aef788b0b8f0b8b0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-06-09 14:40:31 +0100

    Write a failing test #2355

--- test/shared-read-models/curation-statements/get-curation-statements.test.ts
+++ test/shared-read-models/curation-statements/get-curation-statements.test.ts
@@ -3,10 +3,35 @@ import * as RA from 'fp-ts/ReadonlyArray';
 import { getCurationStatements } from '../../../src/shared-read-models/curation-statements/get-curation-statements';
 import { arbitraryArticleId } from '../../types/article-id.helper';
 import { handleEvent, initialState } from '../../../src/shared-read-models/curation-statements/handle-event';
+import { constructEvent } from '../../../src/domain-events';
+import { arbitraryGroupId } from '../../types/group-id.helper';
+import { arbitraryEvaluationLocator } from '../../types/evaluation-locator.helper';
 
 describe('get-curation-statements', () => {
   describe('when there are curation statements for the article', () => {
-    it.todo('returns them');
+    const articleId = arbitraryArticleId();
+    const groupId = arbitraryGroupId();
+    const evaluationLocator = arbitraryEvaluationLocator();
+    const readmodel = pipe(
+      [
+        constructEvent('CurationStatementRecorded')({
+          articleId,
+          evaluationLocator,
+          groupId,
+        }),
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+    const result = pipe(
+      articleId,
+      getCurationStatements(readmodel),
+    );
+
+    it.failing('returns them', () => {
+      const curationStatement = { articleId, evaluationLocator, groupId };
+
+      expect(result).toStrictEqual([curationStatement]);
+    });
   });
 
   describe('when there are no curation statements for the article', () => {
