8ee728d3b36df62cd829d011ed18a7f518138233
LinaKind <l.kinduryte@elifesciences.org>
2023-06-12 14:56:19 +0100

    Introduce a placeholder image for the homepage value statements #2363

--- src/html-pages/home-page/render-as-html/render-value-statements.ts
+++ src/html-pages/home-page/render-as-html/render-value-statements.ts
@@ -10,21 +10,21 @@ export const renderValueStatements = process.env.EXPERIMENT_ENABLED === 'true'
                 <h3>A central place for preprints, reviews and recommendations</h3>
                 <p>Sciety brings together groups of researchers who are evaluating and curating the preprints they find interesting or important.</p>
             </div>
-            <img src="#" alt="placeholder image">
+            <img src="/static/images/close-icon.svg" alt="placeholder image" class="home-page-value-statement__image">
         </article>
         <article>
             <div>
                 <h3>Discover the next advancement in your field</h3>
                 <p>Search and sort through the vast preprint literature and find relevant and impactful studies in your area of expertise, alongside evaluations from trusted experts.</p>
             </div>
-            <img src="#" alt="placeholder image">
+            <img src="/static/images/close-icon.svg" alt="placeholder image" class="home-page-value-statement__image">
         </article>
         <article>
             <div>
                 <h3>Organise and share what you are reading</h3>
                 <p>Create lists of the preprints you find interesting that you can return to in the future or share with your research community.</p>
             </div>
-            <img src="#" alt="placeholder image">
+            <img src="/static/images/close-icon.svg" alt="placeholder image" class="home-page-value-statement__image">
         </article>
         </div>
     </section>
