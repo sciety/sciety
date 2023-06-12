f49e87ca472767399e16e16e9477ac78efd8d7f3
LinaKind <l.kinduryte@elifesciences.org>
2023-06-12 13:53:12 +0100

    Establish html semantics for value statements on the home page #2363

--- src/html-pages/home-page/render-as-html/render-value-statements.ts
+++ src/html-pages/home-page/render-as-html/render-value-statements.ts
@@ -1,5 +1,30 @@
 import { toHtmlFragment } from '../../../types/html-fragment';
 
 export const renderValueStatements = process.env.EXPERIMENT_ENABLED === 'true'
-  ? toHtmlFragment('Value statement')
+  ? toHtmlFragment(`
+    <section>
+        <h2>How does Sciety work?</h2>
+        <article>
+            <div>
+                <h3>A central place for preprints, reviews and recommendations</h3>
+                <p>Sciety brings together groups of researchers who are evaluating and curating the preprints they find interesting or important.</p>
+            </div>
+            <img src="#" alt="placeholder image">
+        </article>
+        <article>
+            <div>
+                <h3>Discover the next advancement in your field</h3>
+                <p>Search and sort through the vast preprint literature and find relevant and impactful studies in your area of expertise, alongside evaluations from trusted experts.</p>
+            </div>
+            <img src="#" alt="placeholder image">
+        </article>
+        <article>
+            <div>
+                <h3>Organise and share what you are reading</h3>
+                <p>Create lists of the preprints you find interesting that you can return to in the future or share with your research community.</p>
+            </div>
+            <img src="#" alt="placeholder image">
+        </article>
+    </section>
+  `)
   : toHtmlFragment('');
