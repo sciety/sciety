22cb10cc016b2a77c7faec338af5a676fd60662f
Kevin Rutherford <kevin@rutherford-software.com>
2023-06-12 13:57:32 +0100

    Make a pipe easier to read by avoiding a boolean #2355

--- src/write-side/resources/curation-statement/record.ts
+++ src/write-side/resources/curation-statement/record.ts
@@ -1,24 +1,23 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import * as B from 'fp-ts/boolean';
 import * as E from 'fp-ts/Either';
 import { RecordCurationStatementCommand } from '../../commands/record-curation-statement';
 import { ResourceAction } from '../resource-action';
 import { DomainEvent, constructEvent, isEventOfType } from '../../../domain-events';
 import { EvaluationLocator } from '../../../types/evaluation-locator';
 
-const hasEvaluationLocatorAlreadyBeenRecordedInCurationStatement = (
+const existingCurationStatementsUsing = (
   evaluationLocator: EvaluationLocator,
 ) => (events: ReadonlyArray<DomainEvent>) => pipe(
   events,
   RA.filter(isEventOfType('CurationStatementRecorded')),
-  RA.some((event) => event.evaluationLocator === evaluationLocator),
+  RA.filter((event) => event.evaluationLocator === evaluationLocator),
 );
 
 export const record: ResourceAction<RecordCurationStatementCommand> = (command) => (events) => pipe(
   events,
-  hasEvaluationLocatorAlreadyBeenRecordedInCurationStatement(command.evaluationLocator),
-  B.fold(
+  existingCurationStatementsUsing(command.evaluationLocator),
+  RA.match(
     () => [
       constructEvent('CurationStatementRecorded')({
         articleId: command.articleId,
