563f3df5b198b179d70224344b51b55c457b06de
Kevin Rutherford <kevin@rutherford-software.com>
2023-06-12 10:56:06 +0100

    Remove the hardcoded curation statements from the article page #2355

--- src/shared-read-models/curation-statements/get-curation-statements.ts
+++ src/shared-read-models/curation-statements/get-curation-statements.ts
@@ -2,34 +2,12 @@ import * as R from 'fp-ts/Record';
 import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import { Doi } from '../../types/doi';
-import * as GID from '../../types/group-id';
-import { toEvaluationLocator } from '../../types/evaluation-locator';
 import { CurationStatement, ReadModel } from './handle-event';
 
-// ts-unused-exports:disable-next-line
-export const magicArticleId = '10.1101/2022.02.23.481615';
+type GetCurationStatements = (readmodel: ReadModel) => (articleId: Doi) => ReadonlyArray<CurationStatement>;
 
-// ts-unused-exports:disable-next-line
-export const curationStatements: ReadonlyArray<CurationStatement> = [
-  {
-    articleId: new Doi(magicArticleId),
-    groupId: GID.fromValidatedString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'),
-    evaluationLocator: toEvaluationLocator('hypothesis:YSEnWoeKEe2O2nN67bYvsA'),
-  },
-  {
-    articleId: new Doi(magicArticleId),
-    groupId: GID.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2'),
-    evaluationLocator: toEvaluationLocator('hypothesis:TfCA6maDEe2zm3_n3MyxjA'),
-  },
-];
-
-export const getCurationStatements = (readmodel: ReadModel) => (articleId: Doi): ReadonlyArray<CurationStatement> => {
-  if (articleId.value === magicArticleId) {
-    return curationStatements;
-  }
-  return pipe(
-    readmodel,
-    R.lookup(articleId.value),
-    O.getOrElseW(() => []),
-  );
-};
+export const getCurationStatements: GetCurationStatements = (readmodel) => (articleId) => pipe(
+  readmodel,
+  R.lookup(articleId.value),
+  O.getOrElseW(() => []),
+);
