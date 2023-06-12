8b9bf4cfe1ee00ff89bcb319a4b102992311de7f
Kevin Rutherford <kevin@rutherford-software.com>
2023-06-12 15:45:59 +0100

    Revert "Move a third party query into the third party folder #2355"
    This reverts commit d32793e56079cf1b90088599d172d59f7bc60611.

--- src/infrastructure/fetch-hypothesis-annotation.ts
+++ src/infrastructure/fetch-hypothesis-annotation.ts
@@ -7,15 +7,14 @@ import { linkify } from 'remarkable/linkify';
 import { formatValidationErrors } from 'io-ts-reporters';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as O from 'fp-ts/Option';
-import { hypothesisAnnotation, HypothesisAnnotation } from '../../infrastructure/codecs/HypothesisAnnotation';
-import { EvaluationFetcher } from '../fetch-review';
-import { Logger } from '../../infrastructure/logger';
-import * as DE from '../../types/data-error';
-import { toHtmlFragment } from '../../types/html-fragment';
-import { Evaluation } from '../../types/evaluation';
-import { GetJson } from '../../shared-ports';
-import { getJsonAndLog } from '../get-json-and-log';
-import { sanitise } from '../../types/sanitised-html-fragment';
+import { Logger, GetJson } from '../shared-ports';
+import { EvaluationFetcher } from '../third-parties/fetch-review';
+import { getJsonAndLog } from '../third-parties/get-json-and-log';
+import { Evaluation } from '../types/evaluation';
+import { toHtmlFragment } from '../types/html-fragment';
+import { sanitise } from '../types/sanitised-html-fragment';
+import { HypothesisAnnotation, hypothesisAnnotation } from './codecs/HypothesisAnnotation';
+import * as DE from '../types/data-error';
 
 const converter = new Remarkable({ html: true }).use(linkify);
 
--- src/third-parties/hypothesis/index.ts
+++ src/third-parties/hypothesis/index.ts
@@ -2,4 +2,3 @@ export { fetchEvaluationsByGroupSince } from './fetch-evaluations-by-group';
 export { fetchEvaluationsByUserSince } from './fetch-evaluations-by-user';
 export { Annotation } from './annotation';
 export { Response } from './response';
-export { fetchHypothesisAnnotation } from './fetch-hypothesis-annotation';
