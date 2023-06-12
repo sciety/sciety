5a5c6f9ceae7dc67fe218465b41c9eeb19e6b85a
Kevin Rutherford <kevin@rutherford-software.com>
2023-06-12 09:39:48 +0100

    Make the test list easier to read #2355

--- test/write-side/resources/curation-statement/record.test.ts
+++ test/write-side/resources/curation-statement/record.test.ts
@@ -5,23 +5,24 @@ import { arbitraryArticleId } from '../../../types/article-id.helper';
 import { arbitraryGroupId } from '../../../types/group-id.helper';
 import { arbitraryEvaluationLocator } from '../../../types/evaluation-locator.helper';
 import { record } from '../../../../src/write-side/resources/curation-statement';
+import { RecordCurationStatementCommand } from '../../../../src/write-side/commands/record-curation-statement';
 
 describe('record', () => {
   const evaluationLocator = arbitraryEvaluationLocator();
-  const input = {
+  const input: RecordCurationStatementCommand = {
     groupId: arbitraryGroupId(),
     articleId: arbitraryArticleId(),
     evaluationLocator,
   };
 
-  describe('when no curation statement with the given evaluation locator has been recorded', () => {
-    const events = pipe(
+  describe('when no curation statement with the given evaluation locator has already been recorded', () => {
+    const eventsToBeRaised = pipe(
       [],
       record(input),
     );
 
     it.failing('returns an CurationStatementRecorded event', () => {
-      expect(events).toStrictEqual(E.right([expect.objectContaining({
+      expect(eventsToBeRaised).toStrictEqual(E.right([expect.objectContaining({
         type: 'CurationStatementRecorded',
         groupId: input.groupId,
         articleId: input.articleId,
@@ -31,7 +32,7 @@ describe('record', () => {
   });
 
   describe('when a curation statement with given evaluation locator has already been recorded', () => {
-    const events = pipe(
+    const eventsToBeRaised = pipe(
       [
         constructEvent('CurationStatementRecorded')({
           groupId: arbitraryGroupId(),
@@ -43,7 +44,7 @@ describe('record', () => {
     );
 
     it('returns no events', () => {
-      expect(events).toStrictEqual(E.right([]));
+      expect(eventsToBeRaised).toStrictEqual(E.right([]));
     });
   });
 });
