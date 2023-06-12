281c9ceac42daba9fc544033754479362eb2dc41
Kevin Rutherford <kevin@rutherford-software.com>
2023-06-12 13:46:06 +0100

    Prefer shared third party type over local copy #2355

--- src/html-pages/article-page/construct-view-model/dependencies.ts
+++ src/html-pages/article-page/construct-view-model/dependencies.ts
@@ -1,24 +1,14 @@
-import * as TE from 'fp-ts/TaskEither';
 import { Queries } from '../../../shared-read-models';
-import { Doi } from '../../../types/doi';
-import * as DE from '../../../types/data-error';
-import { SanitisedHtmlFragment } from '../../../types/sanitised-html-fragment';
-import { ArticleServer } from '../../../types/article-server';
-import { ArticleAuthors } from '../../../types/article-authors';
 import {
-  FetchRelatedArticles, FetchReview, FindVersionsForArticleDoi, Logger,
+  FetchArticle,
+  FetchRelatedArticles,
+  FetchReview,
+  FindVersionsForArticleDoi,
+  Logger,
 } from '../../../shared-ports';
 
-type GetArticleDetails = (doi: Doi) => TE.TaskEither<DE.DataError, {
-  doi: Doi,
-  title: SanitisedHtmlFragment,
-  abstract: SanitisedHtmlFragment, // TODO Use HtmlFragment as the HTML is stripped
-  server: ArticleServer,
-  authors: ArticleAuthors,
-}>;
-
 export type Dependencies = Queries & {
-  fetchArticle: GetArticleDetails,
+  fetchArticle: FetchArticle,
   fetchRelatedArticles: FetchRelatedArticles,
   fetchReview: FetchReview,
   findVersionsForArticleDoi: FindVersionsForArticleDoi,
