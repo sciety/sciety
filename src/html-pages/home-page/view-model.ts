8e00301db8df02eb240a86f7ee253e79ab12ce2d
Kevin Rutherford <kevin@rutherford-software.com>
2023-06-13 10:36:35 +0100

    Prepare to derive group information from a query #2364

--- src/html-pages/home-page/view-model.ts
+++ src/html-pages/home-page/view-model.ts
@@ -1,9 +1,11 @@
 import * as O from 'fp-ts/Option';
 import { GroupsViewModel } from './render-as-html/render-groups';
 import { HtmlFragment } from '../../types/html-fragment';
+import { GroupId } from '../../types/group-id';
 
 type CurationTeaser = {
   articleLink: string,
+  groupId: GroupId,
   quote: HtmlFragment,
   caption: string,
   articleTitle: HtmlFragment,
