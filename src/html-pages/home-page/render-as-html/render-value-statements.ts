657455209fb23c9bbd0229cfbbdfb7a6d975d18a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-06-13 10:27:08 +0100

    Add value statements to homepage #2363

--- src/html-pages/home-page/render-as-html/render-value-statements.ts
+++ src/html-pages/home-page/render-as-html/render-value-statements.ts
@@ -1,7 +1,6 @@
 import { toHtmlFragment } from '../../../types/html-fragment';
 
-export const renderValueStatements = process.env.EXPERIMENT_ENABLED === 'true'
-  ? toHtmlFragment(`
+export const renderValueStatements = toHtmlFragment(`
     <section class="home-page-value-statements-wrapper">
       <div class="home-page-value-statements">
         <h2>How does Sciety work?</h2>
@@ -10,23 +9,22 @@ export const renderValueStatements = process.env.EXPERIMENT_ENABLED === 'true'
                 <h3>A central place for preprints, reviews and recommendations</h3>
                 <p>Sciety brings together groups of researchers who are evaluating and curating the preprints they find interesting or important.</p>
             </div>
-            <img src="/static/images/generic-placeholder-490x281.jpg" alt="placeholder image" class="home-page-value-statement__image">
+            <img src="/static/images/home-page/first-value-placeholder.svg" alt="placeholder image" class="home-page-value-statement__image">
         </article>
         <article class="home-page-value-statement">
             <div class="home-page-value-statement__copy">
                 <h3>Discover the next advancement in your field</h3>
                 <p>Search and sort through the vast preprint literature and find relevant and impactful studies in your area of expertise, alongside evaluations from trusted experts.</p>
             </div>
-            <img src="/static/images/generic-placeholder-490x281.jpg" alt="placeholder image" class="home-page-value-statement__image">
+            <img src="/static/images/home-page/second-value-placeholder.svg" alt="placeholder image" class="home-page-value-statement__image">
         </article>
         <article class="home-page-value-statement">
             <div class="home-page-value-statement__copy">
                 <h3>Organise and share what you are reading</h3>
                 <p>Create lists of the preprints you find interesting that you can return to in the future or share with your research community.</p>
             </div>
-            <img src="/static/images/generic-placeholder-490x281.jpg" alt="placeholder image" class="home-page-value-statement__image">
+            <img src="/static/images/home-page/third-value-placeholder.svg" alt="placeholder image" class="home-page-value-statement__image">
         </article>
         </div>
     </section>
-  `)
-  : toHtmlFragment('');
+  `);
