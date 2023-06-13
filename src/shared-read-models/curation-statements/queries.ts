8c5e765e3be001b256ed71cab960ecca5c20dff5
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-06-13 09:01:54 +0100

    Derive Queries and queries from object of queryBuilders

--- src/shared-read-models/curation-statements/queries.ts
+++ src/shared-read-models/curation-statements/queries.ts
@@ -1,10 +1,17 @@
+import { pipe } from 'fp-ts/function';
+import * as R from 'fp-ts/Record';
 import { getCurationStatements } from './get-curation-statements';
 import { ReadModel } from './handle-event';
 
-export type Queries = {
-  getCurationStatements: ReturnType<typeof getCurationStatements>,
+const queryBuilders = {
+  getCurationStatements,
 };
 
-export const queries = (instance: ReadModel): Queries => ({
-  getCurationStatements: getCurationStatements(instance),
-});
+export type Queries = {
+  [K in keyof typeof queryBuilders]: ReturnType<typeof queryBuilders[K]>
+};
+
+export const queries = (instance: ReadModel): Queries => pipe(
+  queryBuilders,
+  R.map((builder) => builder(instance)),
+);
