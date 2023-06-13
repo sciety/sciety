0d0b89216c928894336c634b1e2a7e832d83ad8c
LinaKind <l.kinduryte@elifesciences.org>
2023-06-13 11:54:06 +0100

    Increase symmetry of resource action tests

--- test/write-side/resources/list/create.test.ts
+++ test/write-side/resources/list/create.test.ts
@@ -1,51 +1,33 @@
 import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
-import * as RA from 'fp-ts/ReadonlyArray';
 import { create } from '../../../../src/write-side/resources/list/create';
 import { arbitraryString } from '../../../helpers';
 import { arbitraryListId } from '../../../types/list-id.helper';
 import { arbitraryListOwnerId } from '../../../types/list-owner-id.helper';
-import { EventOfType, constructEvent, isEventOfType } from '../../../../src/domain-events';
-import { shouldNotBeCalled } from '../../../should-not-be-called';
+import { constructEvent } from '../../../../src/domain-events';
 
 describe('create', () => {
-  const listId = arbitraryListId();
-  const ownerId = arbitraryListOwnerId();
-  const name = arbitraryString();
-  const description = arbitraryString();
+  const input = {
+    listId: arbitraryListId(),
+    ownerId: arbitraryListOwnerId(),
+    name: arbitraryString(),
+    description: arbitraryString(),
+  };
 
-  describe('when a command is received', () => {
-    const result: ReadonlyArray<EventOfType<'ListCreated'>> = pipe(
+  describe('when the listId in the command does not yet exist', () => {
+    const result = pipe(
       [],
-      create({
-        listId,
-        ownerId,
-        name,
-        description,
-      }),
-      E.getOrElseW(shouldNotBeCalled),
-      RA.filter(isEventOfType('ListCreated')),
+      create(input),
     );
 
     it('returns a ListCreated event', () => {
-      expect(result).toHaveLength(1);
-      expect(isEventOfType('ListCreated')(result[0])).toBe(true);
-    });
-
-    it('returns a ListCreated event with the specified listId', () => {
-      expect(result[0].listId).toStrictEqual(listId);
-    });
-
-    it('returns a ListCreated event containing the requested owner', () => {
-      expect(result[0].ownerId).toStrictEqual(ownerId);
-    });
-
-    it('returns a ListCreated event containing the requested name', () => {
-      expect(result[0].name).toStrictEqual(name);
-    });
-
-    it('returns a ListCreated event containing the requested description', () => {
-      expect(result[0].description).toStrictEqual(description);
+      expect(result).toStrictEqual(E.right([expect.objectContaining({
+        type: 'ListCreated',
+        listId: input.listId,
+        ownerId: input.ownerId,
+        name: input.name,
+        description: input.description,
+      })]));
     });
   });
 
@@ -53,23 +35,17 @@ describe('create', () => {
     const result = pipe(
       [
         constructEvent('ListCreated')({
-          listId,
+          listId: input.listId,
           name: arbitraryString(),
           description: arbitraryString(),
           ownerId: arbitraryListOwnerId(),
         }),
       ],
-      create({
-        listId,
-        ownerId,
-        name,
-        description,
-      }),
-      E.getOrElseW(shouldNotBeCalled),
+      create(input),
     );
 
-    it('does not return an event', () => {
-      expect(result).toHaveLength(0);
+    it('returns no events', () => {
+      expect(result).toStrictEqual(E.right([]));
     });
   });
 });
