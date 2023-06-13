7d63a6ae150733942577623c7c4ad1563c08622e
David Moulton <d.moulton@elifesciences.org>
2023-06-13 11:55:27 +0100

    Build the links from the article IDs #2364

--- src/html-pages/home-page/construct-view-model/construct-curation-teaser.ts
+++ src/html-pages/home-page/construct-view-model/construct-curation-teaser.ts
@@ -2,21 +2,30 @@ import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { ViewModel } from '../view-model';
 import { Dependencies } from '../dependencies';
+import { GroupId } from '../../../types/group-id';
+import { HtmlFragment } from '../../../types/html-fragment';
+import { Doi } from '../../../types/doi';
 
-export type HardcodedData = Omit<ViewModel['curationTeasers'][number], 'caption'>;
+export type CurationStatement = {
+  articleId: Doi,
+  groupId: GroupId,
+  quote: HtmlFragment,
+  articleTitle: HtmlFragment,
+};
 
-export const constructCurationTeaser = (dependencies: Dependencies) => (hardcodedData: HardcodedData): ViewModel['curationTeasers'][number] => pipe(
-  hardcodedData.groupId,
+export const constructCurationTeaser = (dependencies: Dependencies) => (curationStatement: CurationStatement): ViewModel['curationTeasers'][number] => pipe(
+  curationStatement.groupId,
   dependencies.getGroup,
   O.match(
     () => {
-      dependencies.logger('error', 'Group missing from readmodel', { groupId: hardcodedData.groupId });
+      dependencies.logger('error', 'Group missing from readmodel', { groupId: curationStatement.groupId });
       return 'Curated by unknown';
     },
     (group) => `Curated by ${group.name}`,
   ),
   (caption) => ({
-    ...hardcodedData,
+    ...curationStatement,
     caption,
+    articleLink: `/articles/${curationStatement.articleId.value}`,
   }),
 );
