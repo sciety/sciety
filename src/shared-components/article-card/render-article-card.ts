51ba091eef5fcdf0a96fb4b89ce99b33749e1813
Kevin Rutherford <kevin@rutherford-software.com>
2023-06-15 15:45:22 +0100

    Distinguish curation teasers with a background colour #2379

--- src/shared-components/article-card/render-article-card.ts
+++ src/shared-components/article-card/render-article-card.ts
@@ -73,7 +73,7 @@ const renderCurationStatement = (articleId: Doi) => {
       <ul class="card-teasers__teasers">
       <li>
         <h4>Curated by <a href="/groups/biophysics-colab">Biophysics Colab</a></h4>
-        <div lang="en" class="curation-teaser__quote curation-teaser__quote_card">
+        <div lang="en" class="curation-teaser__quote_card">
           <p><strong>Endorsement statement (17 November 2022)</strong></p>
           <p>The preprint by Atsumi <em>et al</em>. describes how chloride binding to sweet- and umami-sensing proteins (T1R taste receptors) can evoke taste sensation. The authors use an elegant combination of structural, biophysical and electrophysiological approaches to locate a chloride binding site in the ligand-binding domain of medaka fish T1r2a/3 receptors. They convincingly show that low mM concentrations of chloride induce conformational changes and, using single fiber recordings, establish that mouse chorda tympani nerves are activated by chloride in a T1R-dependent manner. This suggests that chloride binding to sweet receptors could mediate the commonly reported sweet taste sensation following ingestion of low concentrations of table salt. The findings will be of broad relevance to those studying taste sensation and ligand recognition in GPCRs.</p>
           <p><em>(This endorsement by Biophysics Colab refers to version 2 of this preprint, which has been revised in response to peer review of version 1.)</em></p>
@@ -81,7 +81,7 @@ const renderCurationStatement = (articleId: Doi) => {
       </li>
       <li>
         <h4>Curated by <a href="/groups/elife">eLife</a></h4>
-        <div lang="en" class="curation-teaser__quote curation-teaser__quote_card">
+        <div lang="en" class="curation-teaser__quote_card">
           <p><strong>eLife assessment</strong></p>
           <p>This fundamental study presents solid evidence for T1r (sweet /umami) taste receptors as chloride (Cl-) receptors, based on a combination of state-of-the-art techniques to demonstrate that T1r receptors from Medaka fish bind chloride and that this binding induces a conformational change in the heteromeric receptor. This conformational change leads to low-concentration chloride-specific action potential firing in nerves from neurons containing these receptors in mice, results that represent an important advance in our understanding of the logic of taste perception.</p>
         </div>
