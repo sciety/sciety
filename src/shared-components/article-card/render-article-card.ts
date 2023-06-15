dba53a85f19830dd033ee3e3e74531b8ef9931e2
Kevin Rutherford <kevin@rutherford-software.com>
2023-06-15 15:06:23 +0100

    Hardcode curation statements for an article with two #2379

--- src/shared-components/article-card/render-article-card.ts
+++ src/shared-components/article-card/render-article-card.ts
@@ -66,16 +66,23 @@ const renderCurationStatement = (articleId: Doi) => {
   if (process.env.EXPERIMENT_ENABLED !== 'true') {
     return '';
   }
-  if (articleId.value !== '10.1101/2022.06.22.497259') {
+  if (articleId.value !== '10.1101/2022.02.23.481615') {
     return '';
   }
   return `
       <section>
         <h4>Curated by <a href="/groups/biophysics-colab">Biophysics Colab</a></h4>
         <div lang="en" class="curation-statement-text">
-          <p><strong>Endorsement statement (6 December 2022)</strong></p>
-          <p>The preprint by Yang <em>et al</em>. asks how the shape of the membrane influences the localization of mechanosensitive Piezo channels. The authors use a creative approach involving methods that distort the plasma membrane by generating blebs and artificial filopodia. They convincingly show that curvature of the lipid environment influences Piezo1 localization, such that increased curvature causes channel depletion, and that application of the chemical modulator Yoda1 is sufficient to allow channels to enter filopodia. The study provides support for a provocative “flattening model” of Yoda1 action, and should inspire future studies by researchers interested in mechanosensitive channels and membrane curvature.</p>
+          <p><strong>Endorsement statement (17 November 2022)</strong></p>
+          <p>The preprint by Atsumi <em>et al</em>. describes how chloride binding to sweet- and umami-sensing proteins (T1R taste receptors) can evoke taste sensation. The authors use an elegant combination of structural, biophysical and electrophysiological approaches to locate a chloride binding site in the ligand-binding domain of medaka fish T1r2a/3 receptors. They convincingly show that low mM concentrations of chloride induce conformational changes and, using single fiber recordings, establish that mouse chorda tympani nerves are activated by chloride in a T1R-dependent manner. This suggests that chloride binding to sweet receptors could mediate the commonly reported sweet taste sensation following ingestion of low concentrations of table salt. The findings will be of broad relevance to those studying taste sensation and ligand recognition in GPCRs.</p>
           <p><em>(This endorsement by Biophysics Colab refers to version 2 of this preprint, which has been revised in response to peer review of version 1.)</em></p>
+        </div>
+      </section>
+      <section>
+        <h4>Curated by <a href="/groups/elife">eLife</a></h4>
+        <div lang="en" class="curation-statement-text">
+          <p><strong>eLife assessment</strong></p>
+          <p>This fundamental study presents solid evidence for T1r (sweet /umami) taste receptors as chloride (Cl-) receptors, based on a combination of state-of-the-art techniques to demonstrate that T1r receptors from Medaka fish bind chloride and that this binding induces a conformational change in the heteromeric receptor. This conformational change leads to low-concentration chloride-specific action potential firing in nerves from neurons containing these receptors in mice, results that represent an important advance in our understanding of the logic of taste perception.</p>
         </div>
       </section>
     `;
