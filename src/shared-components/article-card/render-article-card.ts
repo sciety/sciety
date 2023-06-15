6e014ae7fe165f345928421628e76f0a203d1530
LinaKind <l.kinduryte@elifesciences.org>
2023-06-15 14:44:59 +0100

    Hardcode html from the article page #2379

--- src/shared-components/article-card/render-article-card.ts
+++ src/shared-components/article-card/render-article-card.ts
@@ -62,6 +62,28 @@ const renderArticleLatestActivityDate = O.fold(
   ),
 );
 
+const renderCurationStatement = (articleId: Doi) => {
+  if (process.env.EXPERIMENT_ENABLED !== 'true') {
+    return '';
+  }
+  if (articleId.value !== '10.1101/2022.06.22.497259') {
+    return '';
+  }
+  return `
+      <section>
+        <header class="curation-statement-header">
+          <h4>Curated by <a href="/groups/biophysics-colab">Biophysics Colab</a></h4>
+          <img src="/static/images/home-page/biophysics-colab.png" alt="Biophysics Colab logo">
+        </header>
+        <div lang="en" class="curation-statement-text">
+          <p><strong>Endorsement statement (6 December 2022)</strong></p>
+          <p>The preprint by Yang <em>et al</em>. asks how the shape of the membrane influences the localization of mechanosensitive Piezo channels. The authors use a creative approach involving methods that distort the plasma membrane by generating blebs and artificial filopodia. They convincingly show that curvature of the lipid environment influences Piezo1 localization, such that increased curvature causes channel depletion, and that application of the chemical modulator Yoda1 is sufficient to allow channels to enter filopodia. The study provides support for a provocative “flattening model” of Yoda1 action, and should inspire future studies by researchers interested in mechanosensitive channels and membrane curvature.</p>
+          <p><em>(This endorsement by Biophysics Colab refers to version 2 of this preprint, which has been revised in response to peer review of version 1.)</em></p>
+        </div>
+      </section>
+    `;
+};
+
 const renderAnnotationContent = (content: O.Option<HtmlFragment>) => pipe(
   content,
   O.match(
@@ -78,6 +100,7 @@ const renderAnnotationContent = (content: O.Option<HtmlFragment>) => pipe(
 const renderArticleCardContents = (model: ArticleViewModel): HtmlFragment => toHtmlFragment(`
   <h3 class="article-card__title"><a class="article-card__link" href="/articles/activity/${model.articleId.value}">${model.title}</a></h3>
   ${renderAuthors(model.authors, `article-card-author-list-${model.articleId.value}`)}
+  ${renderCurationStatement(model.articleId)}
   <footer class="article-card__footer">
     <div class="article-card__meta">
       <span class="visually-hidden">This article has ${model.evaluationCount === 0 ? 'no evaluations' : ''}</span>${renderEvaluationCount(model.evaluationCount)}${renderListMembershipCount(model.listMembershipCount)}${renderArticleVersionDate(model.latestVersionDate)}${renderArticleLatestActivityDate(model.latestActivityAt)}
