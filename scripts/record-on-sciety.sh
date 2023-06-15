6ae974272fb6d2583603779901d8aed51e30a173
David Moulton <d.moulton@elifesciences.org>
2023-06-15 11:21:07 +0100

    Record eLife curation statements on staging and production #2371

--- scripts/record-on-sciety.sh
+++ scripts/record-on-sciety.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-curl localhost:8080/api/record-curation-statement \
+curl https://sciety.org/api/record-curation-statement \
   -w "%{http_code}," \
-  -H "Authorization: Bearer secret" \
+  -H "Authorization: Bearer $SCIETY_TEAM_API_BEARER_TOKEN" \
   -X POST \
   -H "Content-type: application/json" \
   -d '{"evaluationLocator": "'$1'", "articleId": "'$2'", "groupId": "'$3'"}'
