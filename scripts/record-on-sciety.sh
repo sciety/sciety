5c9384aa5fb1257481ffcf0c820b78295f82c3ff
Giorgio Sironi <g.sironi@elifesciences.org>
2023-06-15 11:00:03 +0100

    Modify a script to record curation statements locally #2371

--- scripts/record-on-sciety.sh
+++ scripts/record-on-sciety.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
-echo $*
+curl -v localhost:8080/api/record-curation-statement \
+  -H "Authorization: Bearer secret" \
+  -X POST \
+  -H "Content-type: application/json" \
+  -d '{"evaluationLocator": "'$1'", "articleId": "'$2'", "groupId": "'$3'"}'
+
