14f7c301ec63d593f3c4e7dd7f002e15fb6b69ee
LinaKind <l.kinduryte@elifesciences.org>
2023-06-15 11:53:20 +0100

    Remove no longer used code

--- src/html-pages/article-page/construct-view-model/construct-curation-statements.ts
+++ src/html-pages/article-page/construct-view-model/construct-curation-statements.ts
@@ -9,30 +9,13 @@ import { Doi } from '../../../types/doi';
 import { detectLanguage } from '../../../shared-components/lang-attribute';
 import { ViewModel } from '../view-model';
 import { Dependencies } from './dependencies';
-import { EvaluationLocator, toEvaluationLocator } from '../../../types/evaluation-locator';
-
-// ts-unused-exports:disable-next-line
-export const magicArticleId = '10.1101/2022.02.23.481615';
+import { EvaluationLocator } from '../../../types/evaluation-locator';
 
 type CurationStatement = {
   articleId: Doi,
   evaluationLocator: EvaluationLocator,
   groupId: GID.GroupId,
 };
-
-// ts-unused-exports:disable-next-line
-export const curationStatements: ReadonlyArray<CurationStatement> = [
-  {
-    articleId: new Doi(magicArticleId),
-    groupId: GID.fromValidatedString('b560187e-f2fb-4ff9-a861-a204f3fc0fb0'),
-    evaluationLocator: toEvaluationLocator('hypothesis:YSEnWoeKEe2O2nN67bYvsA'),
-  },
-  {
-    articleId: new Doi(magicArticleId),
-    groupId: GID.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2'),
-    evaluationLocator: toEvaluationLocator('hypothesis:TfCA6maDEe2zm3_n3MyxjA'),
-  },
-];
 
 const addGroupInformation = (dependencies: Dependencies) => (statement: CurationStatement) => pipe(
   statement.groupId,
