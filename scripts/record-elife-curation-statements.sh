45cdb6b85726411823938518e5d787cf33fde12e
Kevin Rutherford <kevin@rutherford-software.com>
2023-06-15 10:46:49 +0100

    Invoke a new script with 3 arguments for each curation statement #2371

--- scripts/record-elife-curation-statements.sh
+++ scripts/record-elife-curation-statements.sh
@@ -3,7 +3,7 @@
 curl "https://api.hypothes.is/api/search?user=public_reviews;tag=Summary" | \
   jq -r '.rows | .[] | "hypothesis:" + (.id) + " " + (.uri)' | \
   grep 10.1101 | \
-  sed -e 's/^/"evaluationLocator":"/' | \
-  sed -e 's/ https.*content\//","articleId":"/' | \
-  sed -e 's/v[0-9]*$/","groupId":"b560187e-f2fb-4ff9-a861-a204f3fc0fb0"/'
+  sed -e 's/ https.*content\// /' | \
+  sed -e 's/v[0-9]*$/ b560187e-f2fb-4ff9-a861-a204f3fc0fb0/' | \
+  xargs -n3 ./scripts/record-on-sciety.sh
 
