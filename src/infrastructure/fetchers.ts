bd8b0a7453e578c367982e974ce89194fd94d3a8
Giorgio Sironi <g.sironi@elifesciences.org>
2023-06-16 10:48:32 +0100

    Send the Sciety user agent on all remaining third party queries #2380

--- src/infrastructure/fetchers.ts
+++ src/infrastructure/fetchers.ts
@@ -4,9 +4,9 @@ import { Logger } from './logger';
 export const fetchData = (
   logger: Logger,
   timeout?: number,
-) => async <D>(url: string, headers: Record<string, string> = {}): Promise<AxiosResponse<D>> => {
+) => async <D>(url: string): Promise<AxiosResponse<D>> => {
   const startTime = new Date();
-  return axios.get<D>(url, { headers, timeout })
+  return axios.get<D>(url, { headers: { 'User-Agent': 'Sciety (http://sciety.org; mailto:team@sciety.org)' }, timeout })
     .finally(() => {
       const durationInMs = new Date().getTime() - startTime.getTime();
       logger('debug', 'Response time', { url, durationInMs });
