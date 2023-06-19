4a7666791a2c2e01fdc880cd25032e6d56a53737
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-06-19 09:39:43 +0100

    Prepare queryExternalService to be used by searchEuropePmc #2380

--- src/third-parties/query-external-service.ts
+++ src/third-parties/query-external-service.ts
@@ -4,13 +4,15 @@ import * as DE from '../types/data-error';
 import { logAndTransformToDataError } from './get-json-and-log';
 import { Logger } from '../shared-ports';
 import { getCachedAxiosRequest } from '../infrastructure/get-cached-axios-request';
+import { LevelName } from '../infrastructure/logger';
 
 export type QueryExternalService = (url: string) => TE.TaskEither<DE.DataError, unknown>;
 
 export const queryExternalService = (
   logger: Logger,
   cacheMaxAgeSeconds = 5 * 60,
+  notFoundLogLevel: LevelName = 'warn',
 ): QueryExternalService => (url) => pipe(
   TE.tryCatch(async () => getCachedAxiosRequest(logger, cacheMaxAgeSeconds * 1000)<unknown>(url), identity),
-  TE.mapLeft(logAndTransformToDataError(logger, url)),
+  TE.mapLeft(logAndTransformToDataError(logger, url, notFoundLogLevel)),
 );
