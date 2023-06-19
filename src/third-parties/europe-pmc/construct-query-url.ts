5979300d307c2f1a4a0bdfdd9ac01c32c6534f4f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-06-19 10:27:44 +0100

    Clarify the construction of EuropePMC query params #2380

--- src/third-parties/europe-pmc/construct-query-url.ts
+++ src/third-parties/europe-pmc/construct-query-url.ts
@@ -1,25 +1,6 @@
 import { URLSearchParams } from 'url';
 import * as O from 'fp-ts/Option';
-import {
-  constant, pipe, tupled,
-} from 'fp-ts/function';
-
-const constructQueryParams = (
-  pageSize: number,
-) => (
-  query: string,
-  cursor: O.Option<string>,
-  evaluatedOnly: boolean,
-) => (
-  new URLSearchParams({
-    query: `(${query}) (PUBLISHER:"bioRxiv" OR PUBLISHER:"medRxiv" OR PUBLISHER:"Research Square" OR PUBLISHER:"SciELO Preprints")${evaluatedOnly ? ' (LABS_PUBS:"2112")' : ''} sort_date:y`,
-    format: 'json',
-    pageSize: pageSize.toString(),
-    resultType: 'core',
-    cursorMark: O.getOrElse(constant('*'))(cursor),
-  }));
-
-const constructSearchUrl = (queryParams: URLSearchParams) => `https://www.ebi.ac.uk/europepmc/webservices/rest/search?${queryParams.toString()}`;
+import { pipe } from 'fp-ts/function';
 
 export const constructQueryUrl = (
   query: string,
@@ -27,7 +8,16 @@ export const constructQueryUrl = (
   evaluatedOnly: boolean,
   pageSize: number,
 ): string => pipe(
-  [query, cursor, evaluatedOnly],
-  tupled(constructQueryParams(pageSize)),
-  constructSearchUrl,
+  evaluatedOnly ? ' (LABS_PUBS:"2112")' : '',
+  (evaluatedOnSciety) => new URLSearchParams({
+    query: `(${query}) (PUBLISHER:"bioRxiv" OR PUBLISHER:"medRxiv" OR PUBLISHER:"Research Square" OR PUBLISHER:"SciELO Preprints")${evaluatedOnSciety} sort_date:y`,
+    format: 'json',
+    pageSize: pageSize.toString(),
+    resultType: 'core',
+    cursorMark: pipe(
+      cursor,
+      O.getOrElse(() => '*'),
+    ),
+  }),
+  (queryParams) => `https://www.ebi.ac.uk/europepmc/webservices/rest/search?${queryParams.toString()}`,
 );
