b3e358e0690bd4b00b376b5ec3908f2218c1d938
LinaKind <l.kinduryte@elifesciences.org>
2023-06-21 14:37:43 +0100

    Consistently use the same article cards for related articles #2386

--- src/html-pages/article-page/construct-view-model/construct-related-articles.ts
+++ src/html-pages/article-page/construct-view-model/construct-related-articles.ts
@@ -1,28 +1,21 @@
-import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
 import * as TO from 'fp-ts/TaskOption';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
+import { constructArticleCardViewModel } from '../../../shared-components/article-card/construct-article-card-view-model';
 import { Doi } from '../../../types/doi';
 import { ViewModel } from '../view-model';
 import { Dependencies } from './dependencies';
-import { getCurationStatements } from '../../../shared-components/article-card/get-curation-statements';
 
 export const constructRelatedArticles = (
   doi: Doi, dependencies: Dependencies,
 ): T.Task<ViewModel['relatedArticles']> => pipe(
   dependencies.fetchRelatedArticles(doi),
   TE.map(RA.takeLeft(3)),
-  TE.map(RA.map((recommendedPaper) => ({
-    articleId: recommendedPaper.articleId,
-    title: recommendedPaper.title,
-    authors: recommendedPaper.authors,
-    latestVersionDate: O.none,
-    latestActivityAt: O.none,
-    evaluationCount: 0,
-    listMembershipCount: 0,
-    curationStatements: getCurationStatements(doi),
-  }))),
+  TE.chainW(TE.traverseArray((recommendedPaper) => pipe(
+    recommendedPaper.articleId,
+    constructArticleCardViewModel(dependencies),
+  ))),
   TO.fromTaskEither,
 );
