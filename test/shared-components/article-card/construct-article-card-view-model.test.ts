03f8a0e5a0e6ad24924db49576c33a01489345a4
David Moulton <d.moulton@elifesciences.org>
2023-06-21 15:11:23 +0100

    Begin to set up the test using the testing framework #2386

--- test/shared-components/article-card/construct-article-card-view-model.test.ts
+++ test/shared-components/article-card/construct-article-card-view-model.test.ts
@@ -1,55 +1,37 @@
 import * as O from 'fp-ts/Option';
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
-import * as TO from 'fp-ts/TaskOption';
 import { pipe } from 'fp-ts/function';
-import { URL } from 'url';
-import { arbitrarySanitisedHtmlFragment, arbitraryUri } from '../../helpers';
-import { constructArticleCardViewModel, Ports } from '../../../src/shared-components/article-card/construct-article-card-view-model';
-import { toHtmlFragment } from '../../../src/types/html-fragment';
-import { sanitise } from '../../../src/types/sanitised-html-fragment';
+import { constructArticleCardViewModel } from '../../../src/shared-components/article-card/construct-article-card-view-model';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 import { arbitraryArticleId } from '../../types/article-id.helper';
+import { createTestFramework, TestFramework } from '../../framework';
+import { ArticleCardViewModel } from '../../../src/shared-components/article-card';
 
 describe('construct-article-card-view-model', () => {
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
   describe('when all information is fetched successfully', () => {
-    it('returns a correct view model', async () => {
-      const articleId = arbitraryArticleId();
-      const latestVersionDate = new Date();
-      const laterPublicationDate = new Date('2020');
-      const article = {
+    const articleId = arbitraryArticleId();
+    const latestVersionDate = new Date();
+    const laterPublicationDate = new Date('2020');
+    let viewModel: ArticleCardViewModel;
+
+    beforeEach(async () => {
+      viewModel = await pipe(
         articleId,
-        server: 'biorxiv' as const,
-        title: pipe('', toHtmlFragment, sanitise),
-        authors: O.none,
-      };
-      const ports: Ports = {
-        fetchArticle: () => TE.right({
-          abstract: arbitrarySanitisedHtmlFragment(),
-          authors: article.authors,
-          doi: articleId,
-          title: article.title,
-          server: article.server,
+        constructArticleCardViewModel({
+          ...framework.queries,
+          ...framework.happyPathThirdParties,
         }),
-        findVersionsForArticleDoi: () => TO.some([{
-          source: new URL(arbitraryUri()),
-          publishedAt: latestVersionDate,
-          version: 1,
-        }]),
-        getActivityForDoi: (a) => ({
-          articleId: a,
-          latestActivityAt: O.some(laterPublicationDate),
-          evaluationCount: 2,
-          listMembershipCount: 0,
-        }),
-      };
-
-      const viewModel = await pipe(
-        article.articleId,
-        constructArticleCardViewModel(ports),
-        TE.getOrElseW(() => T.of(shouldNotBeCalled)),
+        TE.getOrElse(shouldNotBeCalled),
       )();
+    });
 
+    it.skip('returns a correct view model', async () => {
       expect(viewModel).toStrictEqual(expect.objectContaining({
         evaluationCount: 2,
         latestVersionDate: O.some(latestVersionDate),
