f405e7e37a4772639d6ab27cf1cb0240bc0fb0ee
LinaKind <l.kinduryte@elifesciences.org>
2023-06-21 10:40:47 +0100

    Use populate article view model in the list page #2386

--- src/html-pages/list-page/construct-view-model/to-card-view-model.ts
+++ src/html-pages/list-page/construct-view-model/to-card-view-model.ts
@@ -1,13 +1,13 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { populateArticleViewModel, Ports as PopulateArticleViewModelPorts } from '../../../shared-components/article-card/populate-article-view-model';
 import { FetchArticle } from '../../../shared-ports/fetch-article';
 import { ArticleCardViewModel, Ports as ArticleCardPorts, getLatestArticleVersionDate } from '../../../shared-components/article-card';
 import { fetchArticleDetails } from '../../../shared-components/article-card/fetch-article-details';
 import { ArticleActivity } from '../../../types/article-activity';
 import { ArticleErrorCardViewModel } from '../render-as-html/render-article-error-card';
-import { getCurationStatements } from '../../../shared-components/article-card/get-curation-statements';
 
-export type Ports = ArticleCardPorts & {
+export type Ports = ArticleCardPorts & PopulateArticleViewModelPorts & {
   fetchArticle: FetchArticle,
 };
 
@@ -23,18 +23,10 @@ export const toCardViewModel = (
 ): TE.TaskEither<ArticleErrorCardViewModel, ArticleCardViewModel> => pipe(
   articleActivity.articleId,
   getArticleDetails(ports),
-  TE.bimap(
-    (error) => ({
-      ...articleActivity,
-      href: `/articles/${articleActivity.articleId.value}`,
-      error,
-    }),
-    (articleDetails) => ({
-      ...articleActivity,
-      ...articleDetails,
-      authors: articleDetails.authors,
-      latestVersionDate: articleDetails.latestVersionDate,
-      curationStatements: getCurationStatements(articleActivity.articleId),
-    }),
-  ),
+  TE.mapLeft((error) => ({
+    ...articleActivity,
+    href: `/articles/${articleActivity.articleId.value}`,
+    error,
+  })),
+  TE.chain(populateArticleViewModel(ports)),
 );
