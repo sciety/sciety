17737f1979e53c57b52f193e7e744454e3485163
David Moulton <d.moulton@elifesciences.org>
2023-06-22 15:37:14 +0100

    Inline the caching code to help clarify names #2399

--- src/third-parties/query-external-service.ts
+++ src/third-parties/query-external-service.ts
@@ -1,10 +1,40 @@
 import * as TE from 'fp-ts/TaskEither';
 import { identity, pipe } from 'fp-ts/function';
+import Axios from 'axios';
+import { setupCache, type HeaderInterpreter, AxiosCacheInstance } from 'axios-cache-interceptor';
 import * as DE from '../types/data-error';
 import { logAndTransformToDataError } from './log-and-transform-to-data-error';
 import { Logger } from '../shared-ports';
-import { getCachedAxiosRequest } from './get-cached-axios-request';
 import { LevelName } from '../infrastructure/logger';
+
+const headerInterpreterWithFixedMaxAge = (maxAge: number): HeaderInterpreter => () => maxAge;
+
+const createCacheAdapter = (maxAge: number) => setupCache(
+  Axios.create(),
+  { headerInterpreter: headerInterpreterWithFixedMaxAge(maxAge) },
+);
+
+const cachedGetter = (
+  cachedAxios: AxiosCacheInstance,
+  logger: Logger,
+) => async <U>(url: string, headers: Record<string, string> = {}): Promise<U> => {
+  const startTime = new Date();
+  const response = await cachedAxios.get<U>(url, {
+    headers: {
+      ...headers,
+      'User-Agent': 'Sciety (http://sciety.org; mailto:team@sciety.org)',
+    },
+    timeout: 10 * 1000,
+  });
+  if (response.cached) {
+    logger('debug', 'Axios cache hit', { url });
+  } else {
+    logger('debug', 'Axios cache miss', { url });
+    const durationInMs = new Date().getTime() - startTime.getTime();
+    logger('debug', 'Response time', { url, durationInMs, responseStatus: response.status });
+  }
+  return response.data;
+};
 
 export type CallXYZ = (
   notFoundLogLevel?: LevelName,
@@ -15,7 +45,8 @@ export const callXYZ = (
   logger: Logger,
   cacheMaxAgeSeconds = 5 * 60,
 ): CallXYZ => {
-  const get = getCachedAxiosRequest(logger, cacheMaxAgeSeconds * 1000);
+  const cachedAxios = createCacheAdapter(cacheMaxAgeSeconds * 1000);
+  const get = cachedGetter(cachedAxios, logger);
   return (
     notFoundLogLevel: LevelName = 'warn',
     headers = {},
