bdcdcf0d0e5bb76c5da07be04f1cd69e08cd3b0c
David Moulton <d.moulton@elifesciences.org>
2023-06-22 11:03:14 +0100

    Extract a check that changes the logging level for readability #2395

--- src/third-parties/log-and-transform-to-data-error.ts
+++ src/third-parties/log-and-transform-to-data-error.ts
@@ -1,12 +1,18 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { Logger } from '../shared-ports';
 import * as DE from '../types/data-error';
 import { LevelName } from '../infrastructure/logger';
 
+const notFoundResponseStatuses = [404, 410];
+
+const isANotFoundResponse = (
+  response: AxiosResponse | undefined,
+) => response?.status !== undefined && notFoundResponseStatuses.includes(response?.status);
+
 export const logAndTransformToDataError = (logger: Logger, url: string, notFoundLogLevel: LevelName = 'warn') => (error: unknown): DE.DataError => {
   if (axios.isAxiosError(error)) {
     const logPayload = { error, response: error.response?.data };
-    if (error.response?.status === 404 || error.response?.status === 410) {
+    if (isANotFoundResponse(error.response)) {
       logger(notFoundLogLevel, 'Third party data not found', logPayload);
       return DE.notFound;
     }
