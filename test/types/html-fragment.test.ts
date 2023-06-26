c21a3417ec405bfb04c0d46a0b145a6cc3a58ccd
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-06-26 08:23:57 +0100

    Write a failing set of tests for the string concatenation

---
+++ test/types/html-fragment.test.ts
@@ -0,0 +1,13 @@
+import { html } from '../../src/types/html-fragment';
+
+describe('html', () => {
+  const eggs = 'eggs';
+
+  it.failing('concatenates literals and substitutions', () => {
+    expect(html`spam`).toBe('spam');
+    expect(html`${eggs}`).toBe('eggs');
+    expect(html`spam${eggs}`).toBe('spameggs');
+    expect(html`${eggs}spam${eggs}`).toBe('eggsspameggs');
+    expect(html`${eggs}spam`).toBe('eggsspam');
+  });
+});
