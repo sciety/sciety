3a4c327d7456da5418401bb3fdfb8c2c15c54863
Giorgio Sironi <g.sironi@elifesciences.org>
2023-06-26 11:02:27 +0100

    Increase encapsulation within a renderer #2386

--- src/html-pages/list-page/render-as-html/render-articles-list.ts
+++ src/html-pages/list-page/render-as-html/render-articles-list.ts
@@ -1,15 +1,12 @@
 import * as E from 'fp-ts/Either';
 import * as RA from 'fp-ts/ReadonlyArray';
-import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
 import { ArticleErrorCardViewModel, renderArticleErrorCard } from '../../../shared-components/article-card/render-article-error-card';
 import {
   ArticleCardWithControlsAndOptionalAnnotationViewModel,
   renderArticleCardWithControlsAndOptionalAnnotation,
 } from '../../../shared-components/article-card';
-import { Doi } from '../../../types/doi';
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
-import { ListId } from '../../../types/list-id';
 
 type RenderArticlesList = (
   articleViewModels: ReadonlyArray<E.Either<
@@ -18,43 +15,11 @@ type RenderArticlesList = (
   >>,
 ) => HtmlFragment;
 
-const renderRemoveArticleForm = (articleId: Doi, listId: ListId) => pipe(
-  articleId.value,
-  (id) => `
-    <div class="article-card__controls">
-      <form method="post" action="/forms/remove-article-from-list">
-        <input type="hidden" name="articleid" value="${id}">
-        <input type="hidden" name="listid" value="${listId}">
-        <button aria-label="Remove this article from the list" class="saved-articles-control">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" class="saved-articles-control__icon">
-            <desc>Remove this article from the list</desc>
-            <path d="M0 0h24v24H0V0z" fill="none"/>
-            <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/>
-          </svg>
-        </button>
-      </form>
-    </div>
-  `,
-  toHtmlFragment,
-);
-
-const renderControls = (viewModel: ArticleCardWithControlsAndOptionalAnnotationViewModel) => pipe(
-  viewModel.hasControls,
-  B.fold(
-    () => toHtmlFragment(''),
-    () => renderRemoveArticleForm(viewModel.articleCard.articleId, viewModel.listId),
-  ),
-);
-
 export const renderArticlesList: RenderArticlesList = (articles) => pipe(
   articles,
   RA.map(E.fold(
     renderArticleErrorCard,
-    (viewModel) => renderArticleCardWithControlsAndOptionalAnnotation(
-      viewModel.articleCard,
-      renderControls(viewModel),
-      viewModel.annotationContent,
-    ),
+    renderArticleCardWithControlsAndOptionalAnnotation,
   )),
   RA.map((activity) => `<li>${activity}</li>`),
   (renderedActivities) => `
--- src/shared-components/article-card/render-article-card-with-controls-and-optional-annotation.ts
+++ src/shared-components/article-card/render-article-card-with-controls-and-optional-annotation.ts
@@ -1,8 +1,10 @@
+import * as B from 'fp-ts/boolean';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 import { ListId } from '../../types/list-id';
 import { ArticleCardViewModel, renderArticleCardContents } from './render-article-card';
+import { Doi } from '../../types/doi';
 
 type AnnotationContent = O.Option<HtmlFragment>;
 
@@ -12,6 +14,34 @@ export type ArticleCardWithControlsAndOptionalAnnotationViewModel = {
   annotationContent: O.Option<HtmlFragment>,
   listId: ListId,
 };
+
+const renderRemoveArticleForm = (articleId: Doi, listId: ListId) => pipe(
+  articleId.value,
+  (id) => `
+    <div class="article-card__controls">
+      <form method="post" action="/forms/remove-article-from-list">
+        <input type="hidden" name="articleid" value="${id}">
+        <input type="hidden" name="listid" value="${listId}">
+        <button aria-label="Remove this article from the list" class="saved-articles-control">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" class="saved-articles-control__icon">
+            <desc>Remove this article from the list</desc>
+            <path d="M0 0h24v24H0V0z" fill="none"/>
+            <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/>
+          </svg>
+        </button>
+      </form>
+    </div>
+  `,
+  toHtmlFragment,
+);
+
+const renderControls = (viewModel: ArticleCardWithControlsAndOptionalAnnotationViewModel) => pipe(
+  viewModel.hasControls,
+  B.fold(
+    () => toHtmlFragment(''),
+    () => renderRemoveArticleForm(viewModel.articleCard.articleId, viewModel.listId),
+  ),
+);
 
 const renderAnnotationContent = (content: AnnotationContent) => pipe(
   content,
@@ -26,12 +56,12 @@ const renderAnnotationContent = (content: AnnotationContent) => pipe(
   ),
 );
 
-export const renderArticleCardWithControlsAndOptionalAnnotation = (model: ArticleCardViewModel, controls: HtmlFragment, annotationContent: AnnotationContent): HtmlFragment => toHtmlFragment(`
+export const renderArticleCardWithControlsAndOptionalAnnotation = (viewModel: ArticleCardWithControlsAndOptionalAnnotationViewModel): HtmlFragment => toHtmlFragment(`
   <article>
     <section class="article-card">
-      ${renderArticleCardContents(model)}
-      ${controls}
+      ${renderArticleCardContents(viewModel.articleCard)}
+      ${renderControls(viewModel)}
     </section>
-    ${renderAnnotationContent(annotationContent)}
+    ${renderAnnotationContent(viewModel.annotationContent)}
   </article>
 `);
