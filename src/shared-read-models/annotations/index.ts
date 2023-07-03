03107a7f0f2c4ff26d16e6b48a98c0d5a122614c
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-03 11:34:09 +0100

    Report the number of annotations #2419

--- src/shared-read-models/annotations/index.ts
+++ src/shared-read-models/annotations/index.ts
@@ -1,8 +1,10 @@
+import { annotationsStatus } from './annotations-status';
 import { getAnnotationContent } from './get-annotation-content';
 import { handleEvent, initialState } from './handle-event';
 
 export const annotations = {
   queries: {
+    annotationsStatus,
     getAnnotationContent,
   },
   initialState,
--- src/views/status/construct-view-model.ts
+++ src/views/status/construct-view-model.ts
@@ -3,6 +3,7 @@ import { Queries } from '../../shared-read-models';
 
 export const constructViewModel = (queries: Queries): Json => ({
   data: {
+    annotations: queries.annotationsStatus(),
     evaluations: queries.evaluationsStatus(),
     followings: queries.followingsStatus(),
     groups: {
