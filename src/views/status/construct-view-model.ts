63fda6a04163b6944a1b15b0a6aa3bec2e236d47
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-03 10:13:07 +0100

    Report basic info about followings #2419

--- src/views/status/construct-view-model.ts
+++ src/views/status/construct-view-model.ts
@@ -2,6 +2,7 @@ import { Json } from 'fp-ts/Json';
 import { Queries } from '../../shared-read-models';
 
 export const constructViewModel = (queries: Queries): Json => ({
+  followings: queries.followingsStatus(),
   groups: {
     total: queries.getAllGroups().length,
   },
