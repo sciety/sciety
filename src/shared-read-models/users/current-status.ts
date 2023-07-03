01ada6d803d325b46fc6df9bea3905cf405274d1
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-03 09:50:20 +0100

    Report number of users using each kind of auth method #2419

---
+++ src/shared-read-models/users/current-status.ts
@@ -0,0 +1,18 @@
+import * as R from 'fp-ts/Record';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { Json } from 'fp-ts/Json';
+import { ReadModel } from './handle-event';
+
+type CurrentStatus = () => Json;
+
+export const currentStatus = (readModel: ReadModel): CurrentStatus => () => pipe(
+  readModel,
+  R.toEntries,
+  RA.map((entry) => entry[1]),
+  RA.partition((user) => user.id.startsWith('auth0')),
+  ({ left, right }) => ({
+    twitter: left.length,
+    auth0: right.length,
+  }),
+);
--- src/shared-read-models/users/index.ts
+++ src/shared-read-models/users/index.ts
@@ -1,11 +1,13 @@
 import { handleEvent, initialState } from './handle-event';
 import { lookupUserByHandle } from './lookup-user-by-handle';
 import { lookupUser } from './lookup-user';
+import { currentStatus } from './current-status';
 
 export const users = {
   queries: {
     lookupUserByHandle,
     lookupUser,
+    usersStatus: currentStatus,
   },
   initialState,
   handleEvent,
