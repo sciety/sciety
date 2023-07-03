5efec7d4d6529881675b866a2408e16b44d3646e
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-03 15:49:00 +0100

    Reshape a command handler to match the other #2423

--- src/write-side/create-user-account/create-user-account-command-handler.ts
+++ src/write-side/create-user-account/create-user-account-command-handler.ts
@@ -1,5 +1,6 @@
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
+import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import { CreateUserAccountCommand } from '../commands/create-user-account';
 import { setUpUserIfNecessary } from './set-up-user-if-necessary';
@@ -7,6 +8,7 @@ import { DomainEvent } from '../../domain-events';
 import { CommitEvents } from '../../shared-ports';
 import { CommandHandler } from '../../types/command-handler';
 import { checkCommand } from './check-command';
+import { ResourceAction } from '../resources/resource-action';
 
 type GetAllEvents = T.Task<ReadonlyArray<DomainEvent>>;
 
@@ -15,14 +17,19 @@ export type Ports = {
   commitEvents: CommitEvents,
 };
 
+const executeCommand: ResourceAction<CreateUserAccountCommand> = (command) => (events) => pipe(
+  events,
+  checkCommand(command),
+  E.map(() => pipe(
+    events,
+    setUpUserIfNecessary(command),
+  )),
+);
+
 type CreateUserAccountCommandHandler = (ports: Ports) => CommandHandler<CreateUserAccountCommand>;
 
 export const createUserAccountCommandHandler: CreateUserAccountCommandHandler = (ports) => (command) => pipe(
   ports.getAllEvents,
-  T.map(checkCommand(command)),
-  TE.chainTaskK(() => pipe(
-    ports.getAllEvents,
-    T.map(setUpUserIfNecessary(command)),
-    T.chain(ports.commitEvents),
-  )),
+  T.map(executeCommand(command)),
+  TE.chainTaskK(ports.commitEvents),
 );
