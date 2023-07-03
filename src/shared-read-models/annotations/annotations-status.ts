20aac1c4f855049ece3d25079a52b3a5f3d72de2
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-03 11:34:09 +0100

    Report the number of annotations #2419

---
+++ src/shared-read-models/annotations/annotations-status.ts
@@ -0,0 +1,12 @@
+import { pipe } from 'fp-ts/function';
+import { Json } from 'fp-ts/Json';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { ReadModel } from './handle-event';
+
+export const annotationsStatus = (readModel: ReadModel) => (): Json => pipe(
+  Object.values(readModel),
+  RA.chain((listState) => Object.values(listState)),
+  (annotations) => ({
+    total: annotations.length,
+  }),
+);
