d79322148a0555a92ae9d73a56ddaa44aae306fe
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-03 10:50:03 +0100

    Report counts of evaluation types #2419

--- src/shared-read-models/evaluations/index.ts
+++ src/shared-read-models/evaluations/index.ts
@@ -1,9 +1,11 @@
+import { evaluationsStatus } from './evaluations-status';
 import { getEvaluationsByGroup } from './get-evaluations-by-group';
 import { getEvaluationsForDoi } from './get-evaluations-for-doi';
 import { handleEvent, initialState } from './handle-event';
 
 export const evaluations = {
   queries: {
+    evaluationsStatus,
     getEvaluationsForDoi,
     getEvaluationsByGroup,
   },
--- src/views/status/construct-view-model.ts
+++ src/views/status/construct-view-model.ts
@@ -2,6 +2,7 @@ import { Json } from 'fp-ts/Json';
 import { Queries } from '../../shared-read-models';
 
 export const constructViewModel = (queries: Queries): Json => ({
+  evaluations: queries.evaluationsStatus(),
   followings: queries.followingsStatus(),
   groups: {
     total: queries.getAllGroups().length,
