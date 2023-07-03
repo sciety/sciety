4863fdb7f43ddb4db670ec427e15ec0ed7068fdc
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-03 09:32:18 +0100

    Return the actual count of groups

--- src/views/status/application-status.ts
+++ src/views/status/application-status.ts
@@ -1,13 +1,7 @@
 import { identity, pipe } from 'fp-ts/function';
 import { Json } from 'fp-ts/Json';
 import { Queries } from '../../shared-read-models';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const constructViewModel = (queries: Queries) => ({
-  groups: {
-    total: 23,
-  },
-});
+import { constructViewModel } from './construct-view-model';
 
 const renderAsJson = identity;
 
