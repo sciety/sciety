71c8f91ee3059de31e2c7129d85c9dc33ee124de
David Moulton <2893480+davidcmoulton@users.noreply.github.com>
2023-07-04 15:38:51 +0100

    Remove duplicate export #2419

--- src/write-side/command-handlers/index.ts
+++ src/write-side/command-handlers/index.ts
@@ -4,4 +4,3 @@ export { recordEvaluationCommandHandler } from './record-evaluation-command-hand
 export { createListCommandHandler } from './create-list-command-handler';
 export { recordSubjectAreaCommandHandler } from './record-subject-area-command-handler';
 export { removeArticleFromListCommandHandler } from './remove-article-from-list-command-handler';
-export { recordSubjectAreaCommandHandler } from './record-subject-area-command-handler';
