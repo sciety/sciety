89bbfe150a6f4f5cca638b989e30d34da76b4b31
David Moulton <d.moulton@elifesciences.org>
2023-07-05 15:40:26 +0100

    Introduce the start of todo tests to express intent #2416

---
+++ test/sagas/discover-elife-evaluation-type.test.ts
@@ -0,0 +1,9 @@
+describe('discover-elife-evaluation-type', () => {
+  describe('when there is an eLife evaluation missing its evaluation type', () => {
+    it.todo('do stuff');
+  });
+
+  describe('when there is an eLife evaluation that has an evaluation type', () => {
+    it.todo('do not do stuff');
+  });
+});
