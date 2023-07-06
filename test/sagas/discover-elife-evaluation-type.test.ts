ec7a7eaad39d8a3f978adc71a7274e8093011d2f
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-06 09:33:28 +0100

    Begin implementing a test for a new saga #2416

--- test/sagas/discover-elife-evaluation-type.test.ts
+++ test/sagas/discover-elife-evaluation-type.test.ts
@@ -1,9 +1,28 @@
+import * as O from 'fp-ts/Option';
+import { EvaluationType } from '../../src/types/recorded-evaluation';
+import { TestFramework, createTestFramework } from '../framework';
+import { arbitraryDoi } from '../types/doi.helper';
+
 describe('discover-elife-evaluation-type', () => {
-  describe('when there is an eLife evaluation missing its evaluation type', () => {
-    it.todo('do stuff');
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
   });
 
-  describe('when there is an eLife evaluation that has an evaluation type', () => {
-    it.todo('do not do stuff');
+  describe('when there is an eLife evaluation missing its evaluation type', () => {
+    const knownType: EvaluationType = 'review';
+    const articleId = arbitraryDoi();
+    let result: ReturnType<typeof framework.queries.getEvaluationsForDoi>;
+
+    beforeEach(() => {
+      result = framework.queries.getEvaluationsForDoi(articleId);
+    });
+
+    it.failing('the evaluation now has a known type', () => {
+      expect(result[0]).toStrictEqual(expect.objectContaining({
+        evaluationType: O.some(knownType),
+      }));
+    });
   });
 });
