da663caf4adb9f7f794efff2d997532c41f6247e
David Moulton <d.moulton@elifesciences.org>
2023-07-10 15:40:07 +0100

    Return tags from hypothesis fetcher #2439

--- src/third-parties/hypothesis/fetch-hypothesis-annotation.ts
+++ src/third-parties/hypothesis/fetch-hypothesis-annotation.ts
@@ -41,7 +41,7 @@ const toReview = (logger: Logger) => (response: HypothesisAnnotation) => {
       sanitise,
     ),
     url: new URL(response.links.incontext),
-    tags: [],
+    tags: response.tags,
   };
   logger('debug', 'Retrieved evaluation', { ...evaluation, fullText: '[text]' });
   return evaluation;
