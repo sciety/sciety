6d7663633c610278ffa1d1bd7a012610ff175667
David Moulton <d.moulton@elifesciences.org>
2023-07-12 14:39:35 +0100

    Extract a function for readability #2439

--- src/write-side/resources/evaluation/update.ts
+++ src/write-side/resources/evaluation/update.ts
@@ -4,17 +4,23 @@ import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { ResourceAction } from '../resource-action';
 import { UpdateEvaluationCommand } from '../../commands';
-import { constructEvent, isEventOfType } from '../../../domain-events';
+import { constructEvent, DomainEvent, isEventOfType } from '../../../domain-events';
 import { toErrorMessage } from '../../../types/error-message';
+import { EvaluationLocator } from '../../../types/evaluation-locator';
 
-export const update: ResourceAction<UpdateEvaluationCommand> = (command) => (allEvents) => pipe(
-  allEvents,
+const findInterestingEvents = (evaluationLocator: EvaluationLocator) => (events: ReadonlyArray<DomainEvent>) => pipe(
+  events,
   RA.filter(isEventOfType('EvaluationRecorded')),
-  RA.filter((event) => event.evaluationLocator === command.evaluationLocator),
+  RA.filter((event) => event.evaluationLocator === evaluationLocator),
   RA.match(
     () => E.left(toErrorMessage('no recorded evaluation found')),
     (es) => E.right(es),
   ),
+);
+
+export const update: ResourceAction<UpdateEvaluationCommand> = (command) => (allEvents) => pipe(
+  allEvents,
+  findInterestingEvents(command.evaluationLocator),
   E.map((events) => pipe(
     events,
     RA.filter((e) => e.evaluationType !== command.evaluationType),
