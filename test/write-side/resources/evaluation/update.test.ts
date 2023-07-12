1abff9e8feb52e3b94247bc7f2eaeecce32df906
Giorgio Sironi <g.sironi@elifesciences.org>
2023-07-12 10:09:55 +0100

    Write a failing test #2439

--- test/write-side/resources/evaluation/update.test.ts
+++ test/write-side/resources/evaluation/update.test.ts
@@ -1,7 +1,40 @@
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
+import { update } from '../../../../src/write-side/resources/evaluation';
+import { evaluationRecordedHelper } from '../../../types/evaluation-recorded-event.helper';
+import { arbitraryRecordedEvaluation } from '../../../types/recorded-evaluation.helper';
+
 describe('update', () => {
   describe('when the evaluation locator has been recorded', () => {
     describe('when the evaluation type has not been recorded', () => {
-      it.todo('returns an EvaluationUpdated event');
+      const evaluation = arbitraryRecordedEvaluation();
+      const command = {
+        evaluationLocator: evaluation.evaluationLocator,
+        evaluationType: 'author-response' as const,
+      };
+
+      const events = pipe(
+        [
+          evaluationRecordedHelper(
+            evaluation.groupId,
+            evaluation.articleId,
+            evaluation.evaluationLocator,
+            evaluation.authors,
+            evaluation.publishedAt,
+            new Date(),
+            undefined,
+          ),
+        ],
+        update(command),
+      );
+
+      it.failing('returns an EvaluationUpdated event', () => {
+        expect(events).toStrictEqual(E.right([expect.objectContaining({
+          type: 'EvaluationUpdated',
+          evaluationLocator: command.evaluationLocator,
+          evaluationType: command.evaluationType,
+        })]));
+      });
     });
 
     describe('when the evaluation type has been recorded', () => {
