50ac75edeb7bdc5003a2acdbec4b09b159cd4dd3
LinaKind <l.kinduryte@elifesciences.org>
2023-07-12 09:40:57 +0100

    Describe intent with the test list #2439

---
+++ test/write-side/resources/evaluation/update.test.ts
@@ -0,0 +1,7 @@
+describe('update', () => {
+  describe('when the evaluation locator has been recorded', () => {
+    describe('when the evaluation type has not been recorded', () => {
+      it.todo('returns an EvaluationUpdated event');
+    });
+  });
+});
