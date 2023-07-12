5f85c6b5d474d96383eab6c5d66317ac55910256
David Moulton <d.moulton@elifesciences.org>
2023-07-12 14:05:31 +0100

    Make the update resource action idempotent #2439

--- src/write-side/resources/evaluation/update.ts
+++ src/write-side/resources/evaluation/update.ts
@@ -1,3 +1,4 @@
+import * as B from 'fp-ts/boolean';
 import * as E from 'fp-ts/Either';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
@@ -6,18 +7,26 @@ import { UpdateEvaluationCommand } from '../../commands';
 import { constructEvent, isEventOfType } from '../../../domain-events';
 import { toErrorMessage } from '../../../types/error-message';
 
-export const update: ResourceAction<UpdateEvaluationCommand> = (command) => (events) => pipe(
-  events,
+export const update: ResourceAction<UpdateEvaluationCommand> = (command) => (allEvents) => pipe(
+  allEvents,
   RA.filter(isEventOfType('EvaluationRecorded')),
   RA.filter((event) => event.evaluationLocator === command.evaluationLocator),
   RA.match(
     () => E.left(toErrorMessage('no recorded evaluation found')),
     (es) => E.right(es),
   ),
-  E.map(() => [
-    constructEvent('EvaluationUpdated')({
-      evaluationLocator: command.evaluationLocator,
-      evaluationType: command.evaluationType,
-    }),
-  ]),
+  E.map((events) => pipe(
+    events,
+    RA.filter((e) => e.evaluationType !== command.evaluationType),
+    RA.isNonEmpty,
+    B.fold(
+      () => [],
+      () => [
+        constructEvent('EvaluationUpdated')({
+          evaluationLocator: command.evaluationLocator,
+          evaluationType: command.evaluationType,
+        }),
+      ],
+    ),
+  )),
 );
