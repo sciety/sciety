45c1050639b3cde570521e99d5a5a19515bcf0f5
David Moulton <d.moulton@elifesciences.org>
2023-07-12 09:52:34 +0100

    Write a complete test list #2439

--- test/write-side/resources/evaluation/update.test.ts
+++ test/write-side/resources/evaluation/update.test.ts
@@ -3,5 +3,19 @@ describe('update', () => {
     describe('when the evaluation type has not been recorded', () => {
       it.todo('returns an EvaluationUpdated event');
     });
+
+    describe('when the evaluation type has been recorded', () => {
+      describe('and it is the same value as the one being passed in', () => {
+        it.todo('returns no events');
+      });
+
+      describe('and it is not the same value as the one being passed in', () => {
+        it.todo('returns an EvaluationUpdated event');
+      });
+    });
+  });
+
+  describe('when the evaluation locator has not been recorded', () => {
+    it.todo('fails');
   });
 });
