7471c51e4a99c7f9e75b68f9904e4373a5453037
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-14 08:21:19 +0100

    Inline a Ports type into Dependencies #2436

--- src/html-pages/sciety-feed-page/construct-view-model/construct-event-card.ts
+++ src/html-pages/sciety-feed-page/construct-view-model/construct-event-card.ts
@@ -3,19 +3,15 @@ import {
   CollapsedArticlesAddedToList, isArticleAddedToListEvent,
   isCollapsedArticlesAddedToList,
 } from './feed-item';
-import { userFollowedAGroupCard, Ports as UserFollowedAGroupCardPorts } from './user-followed-a-group-card';
-import { articleAddedToListCard, Ports as ArticleAddedToListCardPorts } from './article-added-to-list-card';
-import { collapsedArticlesAddedToListCard, Ports as CollapsedArticlesAddedToListCardPorts } from './collapsed-articles-added-to-list-card';
+import { userFollowedAGroupCard } from './user-followed-a-group-card';
+import { articleAddedToListCard } from './article-added-to-list-card';
+import { collapsedArticlesAddedToListCard } from './collapsed-articles-added-to-list-card';
 import { ScietyFeedCard } from '../view-model';
 import { DomainEvent, isEventOfType } from '../../../domain-events';
-
-export type Ports =
-  UserFollowedAGroupCardPorts
-  & ArticleAddedToListCardPorts
-  & CollapsedArticlesAddedToListCardPorts;
+import { Dependencies } from './dependencies';
 
 export const constructEventCard = (
-  dependencies: Ports,
+  dependencies: Dependencies,
 ) => (
   event: DomainEvent | CollapsedArticlesAddedToList,
 ): O.Option<ScietyFeedCard> => {
--- src/html-pages/sciety-feed-page/construct-view-model/dependencies.ts
+++ src/html-pages/sciety-feed-page/construct-view-model/dependencies.ts
@@ -1,6 +1,12 @@
-import { Ports as EventCardDependencies } from './construct-event-card';
 import { GetAllEvents } from '../../../shared-ports';
+import { Ports as UserFollowedAGroupCardPorts } from './user-followed-a-group-card';
+import { Ports as ArticleAddedToListCardPorts } from './article-added-to-list-card';
+import { Ports as CollapsedArticlesAddedToListCardPorts } from './collapsed-articles-added-to-list-card';
 
-export type Dependencies = EventCardDependencies & {
-  getAllEvents: GetAllEvents,
-};
+export type Dependencies =
+  UserFollowedAGroupCardPorts
+  & ArticleAddedToListCardPorts
+  & CollapsedArticlesAddedToListCardPorts
+  & {
+    getAllEvents: GetAllEvents,
+  };
