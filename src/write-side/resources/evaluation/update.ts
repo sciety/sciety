d0ebd282e4d92783519080d9884a63a86a3f12f3
David Moulton <d.moulton@elifesciences.org>
2023-07-14 09:17:20 +0100

    Improve an error message #2439

--- src/write-side/resources/evaluation/update.ts
+++ src/write-side/resources/evaluation/update.ts
@@ -5,7 +5,7 @@ import { pipe } from 'fp-ts/function';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import {
   EventOfType, constructEvent, DomainEvent, isEventOfType,
-} from '../../../domain-events/index';
+} from '../../../domain-events';
 import { ResourceAction } from '../resource-action';
 import { UpdateEvaluationCommand } from '../../commands';
 import { toErrorMessage } from '../../../types/error-message';
@@ -18,7 +18,7 @@ const findInterestingEvents = (evaluationLocator: EvaluationLocator) => (events:
     || isEventOfType('EvaluationUpdated')(event)),
   RA.filter((event) => event.evaluationLocator === evaluationLocator),
   RA.match(
-    () => E.left(toErrorMessage('no recorded evaluation found')),
+    () => E.left(toErrorMessage('Evaluation to be updated does not exist')),
     (es) => E.right(es),
   ),
 );
