0e9094fc11ab70fccac2790ea10fd46c9822fff7
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-14 16:04:25 +0100

    Extract an in-memory eventstore from the test framework #2458

--- test/framework/create-read-and-write-sides.ts
+++ test/framework/create-read-and-write-sides.ts
@@ -1,12 +1,9 @@
 import { pipe } from 'fp-ts/function';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
-import * as RA from 'fp-ts/ReadonlyArray';
 import { dispatcher, Queries } from '../../src/shared-read-models';
 import * as groupResource from '../../src/write-side/resources/group';
-import { DomainEvent } from '../../src/domain-events';
 import { GetAllEvents, CommitEvents } from '../../src/shared-ports';
-import { CommandResult } from '../../src/types/command-result';
 import { createUserAccountCommandHandler } from '../../src/write-side/create-user-account';
 import { followCommandHandler } from '../../src/write-side/follow/follow-command-handler';
 import {
@@ -19,25 +16,7 @@ import { unfollowCommandHandler } from '../../src/write-side/follow/unfollow-com
 import { CommandHandler } from '../../src/types/command-handler';
 import { AddGroupCommand, UpdateGroupDetailsCommand } from '../../src/write-side/commands';
 import { addArticleToListCommandHandler } from '../../src/write-side/command-handlers/add-article-to-list-command-handler';
-
-const commitEvents = (
-  inMemoryEvents: Array<DomainEvent>,
-  dispatchToAllReadModels: (events: ReadonlyArray<DomainEvent>) => void,
-): CommitEvents => (events) => pipe(
-  events,
-  RA.match(
-    () => ('no-events-created' as CommandResult),
-    (es) => {
-      pipe(
-        es,
-        RA.map((event) => { inMemoryEvents.push(event); return event; }),
-      );
-      dispatchToAllReadModels(es);
-      return 'events-created' as CommandResult;
-    },
-  ),
-  T.of,
-);
+import { createInMemoryEventStore } from './create-in-memory-event-store';
 
 type EventStore = {
   getAllEvents: GetAllEvents,
@@ -81,12 +60,8 @@ export type ReadAndWriteSides = {
 };
 
 export const createReadAndWriteSides = (): ReadAndWriteSides => {
-  const allEvents: Array<DomainEvent> = [];
   const { dispatchToAllReadModels, queries } = dispatcher();
-  const eventStore: EventStore = {
-    getAllEvents: T.of(allEvents),
-    commitEvents: commitEvents(allEvents, dispatchToAllReadModels),
-  };
+  const eventStore = createInMemoryEventStore(dispatchToAllReadModels);
   const commandHandlers = instantiateCommandHandlers(eventStore, queries);
   return {
     commandHandlers,
