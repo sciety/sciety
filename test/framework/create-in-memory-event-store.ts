8750c8f125addaaadc5d73e7b14d1e427e425777
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-14 16:04:25 +0100

    Extract an in-memory eventstore from the test framework #2458

---
+++ test/framework/create-in-memory-event-store.ts
@@ -0,0 +1,40 @@
+import { pipe } from 'fp-ts/function';
+import * as T from 'fp-ts/Task';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { DomainEvent } from '../../src/domain-events';
+import { GetAllEvents, CommitEvents } from '../../src/shared-ports';
+import { CommandResult } from '../../src/types/command-result';
+
+const commitEvents = (
+  inMemoryEvents: Array<DomainEvent>,
+  dispatchToAllReadModels: (events: ReadonlyArray<DomainEvent>) => void,
+): CommitEvents => (events) => pipe(
+  events,
+  RA.match(
+    () => ('no-events-created' as CommandResult),
+    (es) => {
+      pipe(
+        es,
+        RA.map((event) => { inMemoryEvents.push(event); return event; }),
+      );
+      dispatchToAllReadModels(es);
+      return 'events-created' as CommandResult;
+    },
+  ),
+  T.of,
+);
+
+type DispatchToAllReadModels = (events: ReadonlyArray<DomainEvent>) => void;
+
+type EventStore = {
+  getAllEvents: GetAllEvents,
+  commitEvents: CommitEvents,
+};
+
+export const createInMemoryEventStore = (dispatchToAllReadModels: DispatchToAllReadModels): EventStore => {
+  const allEvents: Array<DomainEvent> = [];
+  return {
+    getAllEvents: T.of(allEvents),
+    commitEvents: commitEvents(allEvents, dispatchToAllReadModels),
+  };
+};
