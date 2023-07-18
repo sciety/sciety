ffad602791aeb56b9fabb95e692d338cbb92279f
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-18 11:30:58 +0100

    Return a more useful status code on failure #2462

--- src/html-pages/list-page/list-feed.ts
+++ src/html-pages/list-page/list-feed.ts
@@ -1,16 +1,13 @@
 import { Middleware } from '@koa/router';
 import { HttpStatusCode } from 'axios';
 import { pipe } from 'fp-ts/function';
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { renderAsAtom } from './render-as-atom';
 import { Dependencies, constructViewModel, paramsCodec } from './construct-view-model';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const listFeed = (dependencies: Dependencies): Middleware => async (context, next) => {
-  context.response.status = HttpStatusCode.Ok;
   context.response.type = 'application/atom+xml';
-  context.response.body = await pipe(
+  const result = await pipe(
     {
       ...context.params,
       ...context.query,
@@ -19,8 +16,19 @@ export const listFeed = (dependencies: Dependencies): Middleware => async (conte
     TE.fromEither,
     TE.chainW(constructViewModel(dependencies)),
     TE.map(renderAsAtom),
-    TE.getOrElse(() => T.of('')),
+    TE.match(
+      () => ({
+        status: HttpStatusCode.BadRequest,
+        body: '',
+      }),
+      (body) => ({
+        status: HttpStatusCode.Ok,
+        body,
+      }),
+    ),
   )();
+  context.response.status = result.status;
+  context.response.body = result.body;
 
   await next();
 };
