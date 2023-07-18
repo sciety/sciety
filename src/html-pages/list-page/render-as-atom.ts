be7e97e609d09273ed89ad5621daa97f19114daf
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-07-18 11:11:29 +0100

    Fix validation errors by removing whitespace from the start of the document #2462

--- src/html-pages/list-page/render-as-atom.ts
+++ src/html-pages/list-page/render-as-atom.ts
@@ -1,15 +1,14 @@
 import { ViewModel } from './view-model';
 
-export const renderAsAtom = (viewModel: ViewModel): string => `
-  <?xml version="1.0" encoding="utf-8"?>
-  <feed xmlns="http://www.w3.org/2005/Atom">
-    <title>${viewModel.name}</title>
-    <link rel="alternate" href="${viewModel.listPageAbsoluteUrl.toString()}"/>
-    <link rel="self" href="${viewModel.listPageAbsoluteUrl.toString()}/feed.atom"/>
-    <updated>${viewModel.updatedAt.toISOString()}</updated>
-    <author>
-      <name>${viewModel.ownerName}</name>
-    </author>
-    <id>urn:uuid:${viewModel.listId}</id>
-  </feed>
+export const renderAsAtom = (viewModel: ViewModel): string => `<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <title>${viewModel.name}</title>
+  <link rel="alternate" href="${viewModel.listPageAbsoluteUrl.toString()}"/>
+  <link rel="self" href="${viewModel.listPageAbsoluteUrl.toString()}/feed.atom"/>
+  <updated>${viewModel.updatedAt.toISOString()}</updated>
+  <author>
+    <name>${viewModel.ownerName}</name>
+  </author>
+  <id>urn:uuid:${viewModel.listId}</id>
+</feed>
 `;
