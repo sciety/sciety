d9f2ba2dcf4c153344509a600865a078ba2e2d05
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-18 13:54:32 +0100

    Set up for the rendering of individual feed entries #2462

--- src/views/list/render-as-atom.ts
+++ src/views/list/render-as-atom.ts
@@ -1,4 +1,21 @@
+import { pipe } from 'fp-ts/function';
+import * as RA from 'fp-ts/ReadonlyArray';
 import { ViewModel } from './view-model';
+
+const renderEntries = (entries: ViewModel['content']['articles']) => pipe(
+  entries,
+  RA.map(() => `
+  <entry>
+    <title>{{ENTRY.TITLE}}</title>
+    <link rel="alternate" type="text/html" href="{{ENTRY.HTML_URL}}"/>
+    <id>{{ENTRY.PERMALINK}}</id>
+    <published>{{ENTRY.FIRST_POST_TIME in RFC3339 format}}</published>
+    <updated>{{ENTRY.LAST_UPDATE_TIME in RFC3339 format}}</updated>
+    <content type="html">{{ENTRY.HTML}}</content>
+  </entry>
+  `),
+  (items) => items.join('\n'),
+);
 
 export const renderAsAtom = (viewModel: ViewModel): string => `<?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
@@ -10,5 +27,8 @@ export const renderAsAtom = (viewModel: ViewModel): string => `<?xml version="1.
     <name>${viewModel.ownerName}</name>
   </author>
   <id>urn:uuid:${viewModel.listId}</id>
+
+  ${renderEntries(viewModel.content.articles)}
+
 </feed>
 `;
