684128f25d0e6861fe73ac61ed9a49168e07f16a
David Moulton <d.moulton@elifesciences.org>
2023-07-18 09:35:54 +0100

    Use existing view model constructor #2462

--- src/html-pages/list-page/list-feed.ts
+++ src/html-pages/list-page/list-feed.ts
@@ -2,31 +2,27 @@ import { Middleware } from '@koa/router';
 import { HttpStatusCode } from 'axios';
 import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
 import { renderAsAtom } from './render-as-atom';
 import { ListId } from '../../types/list-id';
 import { Dependencies } from './construct-view-model/dependencies';
+import { constructViewModel } from './construct-view-model/construct-view-model';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const listFeed = (dependencies: Dependencies): Middleware => async (context, next) => {
   context.response.status = HttpStatusCode.Ok;
   context.response.type = 'application/atom+xml';
-  context.response.body = pipe(
+  context.response.body = await pipe(
     {
-      name: '',
-      description: '',
-      ownerName: '',
-      ownerHref: '',
-      ownerAvatarPath: '',
-      articleCount: 0,
-      updatedAt: new Date(),
-      editCapability: false,
-      listId: '' as ListId,
-      basePath: '',
-      content: 'no-articles',
-      relatedArticlesLink: O.none,
+      page: 1,
+      id: '' as ListId,
+      user: O.none,
     },
-    renderAsAtom,
-  );
+    constructViewModel(dependencies),
+    TE.map(renderAsAtom),
+    TE.getOrElse(() => T.of('')),
+  )();
 
   await next();
 };
