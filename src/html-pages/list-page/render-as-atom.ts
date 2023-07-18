8ebd6de6f3d8093ca99741d0b0f79833a54cacda
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-18 10:08:25 +0100

    Get the list author from the viewmodel #2462

--- src/html-pages/list-page/render-as-atom.ts
+++ src/html-pages/list-page/render-as-atom.ts
@@ -1,14 +1,14 @@
 import { ViewModel } from './view-model';
 
 export const renderAsAtom = (viewModel: ViewModel): string => `
-    <?xml version="1.0" encoding="utf-8"?>
-    <feed xmlns="http://www.w3.org/2005/Atom">
-      <title>${viewModel.name}</title>
-      <link href="http://example.org/"/>
-      <updated>${viewModel.updatedAt.toISOString()}</updated>
-      <author>
-        <name>John Doe</name>
-      </author>
-      <id>urn:uuid:60a76c80-d399-11d9-b93C-0003939e0af6</id>
-    </feed>
-  `;
+  <?xml version="1.0" encoding="utf-8"?>
+  <feed xmlns="http://www.w3.org/2005/Atom">
+    <title>${viewModel.name}</title>
+    <link href="http://example.org/"/>
+    <updated>${viewModel.updatedAt.toISOString()}</updated>
+    <author>
+      <name>${viewModel.ownerName}</name>
+    </author>
+    <id>urn:uuid:60a76c80-d399-11d9-b93C-0003939e0af6</id>
+  </feed>
+`;
