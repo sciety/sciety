e4b6ba2eab435d6530ebcb9bff0d816c9f429a63
David Moulton <d.moulton@elifesciences.org>
2023-07-19 14:51:23 +0100

    Simplify interfaces to reflect failure modes #2462

--- src/views/list/construct-view-model/construct-content-with-pagination-view-model.ts
+++ src/views/list/construct-view-model/construct-content-with-pagination-view-model.ts
@@ -1,9 +1,7 @@
-import * as TE from 'fp-ts/TaskEither';
 import * as T from 'fp-ts/Task';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { toPageOfCards } from './to-page-of-cards';
-import * as DE from '../../../types/data-error';
 import { Doi } from '../../../types/doi';
 import { ContentWithPaginationViewModel } from '../view-model';
 import { ListId } from '../../../types/list-id';
@@ -11,16 +9,11 @@ import { Dependencies } from './dependencies';
 
 export const constructContentWithPaginationViewModel = (
   dependencies: Dependencies,
-  pageNumber: number,
   listId: ListId,
-) => (articleIds: ReadonlyArray<Doi>): TE.TaskEither<DE.DataError, ContentWithPaginationViewModel> => pipe(
+) => (articleIds: ReadonlyArray<Doi>): T.Task<ContentWithPaginationViewModel> => pipe(
   articleIds,
   RA.takeLeft(20),
   RA.map(dependencies.getActivityForDoi),
-  TE.right,
-  TE.chainTaskK((activities) => pipe(
-    activities,
-    toPageOfCards(dependencies, listId),
-    T.map((articles) => ({ articles, pagination: activities })),
-  )),
+  toPageOfCards(dependencies, listId),
+  T.map((articles) => ({ articles })),
 );
--- src/views/list/construct-view-model/construct-view-model.ts
+++ src/views/list/construct-view-model/construct-view-model.ts
@@ -1,25 +1,20 @@
 import { URL } from 'url';
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
-import * as tt from 'io-ts-types';
 import { constructContentWithPaginationViewModel } from './construct-content-with-pagination-view-model';
 import { getOwnerName } from './get-owner-name';
 import { listIdCodec } from '../../../types/list-id';
-import { userIdCodec } from '../../../types/user-id';
 import * as DE from '../../../types/data-error';
 import { Doi } from '../../../types/doi';
 import { Dependencies } from './dependencies';
 import { ViewModel } from '../view-model';
 
 export const paramsCodec = t.type({
-  page: tt.withFallback(tt.NumberFromString, 1),
   id: listIdCodec,
-  user: tt.optionFromNullable(t.type({
-    id: userIdCodec,
-  })),
 });
 
 type Params = t.TypeOf<typeof paramsCodec>;
@@ -42,12 +37,11 @@ export const constructViewModel = (
     })),
   )),
   TE.fromOption(() => DE.notFound),
-  TE.chainW((partialPageViewModel) => pipe(
+  TE.chainTaskK((partialPageViewModel) => pipe(
     partialPageViewModel.articleIds,
     RA.map((articleId) => new Doi(articleId)),
-    constructContentWithPaginationViewModel(dependencies, params.page, partialPageViewModel.listId),
-    TE.bimap(
-      () => DE.unavailable,
+    constructContentWithPaginationViewModel(dependencies, partialPageViewModel.listId),
+    T.map(
       (content) => ({
         ...content,
         ...partialPageViewModel,
