dfe02eb3a48d4642c1a5412dc10adcc1acc2e00b
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-21 09:38:56 +0100

    Increase the validity of the test by calling more resource actions #2468

--- test/write-side/resources/evaluation/lifecycle.test.ts
+++ test/write-side/resources/evaluation/lifecycle.test.ts
@@ -1,17 +1,31 @@
+import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
-import { update } from '../../../../src/write-side/resources/evaluation';
+import { erase, update } from '../../../../src/write-side/resources/evaluation';
 import { arbitraryEvaluationType } from '../../../types/evaluation-type.helper';
 import { arbitraryEvaluationLocator } from '../../../types/evaluation-locator.helper';
 import { DomainEvent } from '../../../../src/domain-events';
+import { shouldNotBeCalled } from '../../../should-not-be-called';
 
 describe('lifecycle', () => {
   describe('record -> erase -> update', () => {
+    let result: unknown;
     const evaluationLocator = arbitraryEvaluationLocator();
-    const events: ReadonlyArray<DomainEvent> = [];
-    const result = update({
-      evaluationLocator,
-      evaluationType: arbitraryEvaluationType(),
-    })(events);
+
+    beforeEach(() => {
+      const events: Array<DomainEvent> = [];
+      const eventsFromErase = pipe(
+        events,
+        erase({
+          evaluationLocator,
+        }),
+        E.getOrElseW(shouldNotBeCalled),
+      );
+      events.push(...eventsFromErase);
+      result = update({
+        evaluationLocator,
+        evaluationType: arbitraryEvaluationType(),
+      })(events);
+    });
 
     it('errors with not found', () => {
       expect(result).toStrictEqual(E.left('Evaluation to be updated does not exist'));
