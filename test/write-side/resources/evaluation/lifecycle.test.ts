619edd4739893e3e2c19aea87d0a3aa7062f9e6c
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-21 17:25:04 +0100

    Increase the symmetry when invoking several resource actions in sequence #2475

--- test/write-side/resources/evaluation/lifecycle.test.ts
+++ test/write-side/resources/evaluation/lifecycle.test.ts
@@ -2,8 +2,6 @@ import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
 import { erase, update, record } from '../../../../src/write-side/resources/evaluation';
 import { arbitraryEvaluationType } from '../../../types/evaluation-type.helper';
-import { DomainEvent } from '../../../../src/domain-events';
-import { shouldNotBeCalled } from '../../../should-not-be-called';
 import { arbitraryDate } from '../../../helpers';
 import { arbitraryArticleId } from '../../../types/article-id.helper';
 import { arbitraryEvaluationLocator } from '../../../types/evaluation-locator.helper';
@@ -11,7 +9,6 @@ import { arbitraryGroupId } from '../../../types/group-id.helper';
 
 describe('lifecycle', () => {
   describe('record -> erase -> update', () => {
-    let result: unknown;
     const recordCommand = {
       groupId: arbitraryGroupId(),
       publishedAt: arbitraryDate(),
@@ -20,27 +17,25 @@ describe('lifecycle', () => {
       authors: [],
     };
 
-    beforeEach(() => {
-      const events: Array<DomainEvent> = [];
-      const eventsFromRecord = pipe(
+    const result = pipe(
+      [],
+      E.right,
+      E.chain((events) => pipe(
         events,
         record(recordCommand),
-        E.getOrElseW(shouldNotBeCalled),
-      );
-      events.push(...eventsFromRecord);
-      const eventsFromErase = pipe(
+        E.map((outputEvents) => [...events, ...outputEvents]),
+      )),
+      E.chain((events) => pipe(
         events,
-        erase({
-          evaluationLocator: recordCommand.evaluationLocator,
-        }),
-        E.getOrElseW(shouldNotBeCalled),
-      );
-      events.push(...eventsFromErase);
-      result = update({
-        evaluationLocator: recordCommand.evaluationLocator,
-        evaluationType: arbitraryEvaluationType(),
-      })(events);
-    });
+        erase({ evaluationLocator: recordCommand.evaluationLocator }),
+        E.map((outputEvents) => [...events, ...outputEvents]),
+      )),
+      E.chain((events) => pipe(
+        events,
+        update({ evaluationLocator: recordCommand.evaluationLocator, evaluationType: arbitraryEvaluationType() }),
+        E.map((outputEvents) => [...events, ...outputEvents]),
+      )),
+    );
 
     it('errors with not found', () => {
       expect(result).toStrictEqual(E.left('Evaluation to be updated does not exist'));
