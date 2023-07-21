87f418429d145abe8bfe4ad9159549833df3303f
David Moulton <d.moulton@elifesciences.org>
2023-07-21 09:28:56 +0100

    Begin sketching lifecycle test setup #2468

--- test/write-side/resources/evaluation/lifecycle.test.ts
+++ test/write-side/resources/evaluation/lifecycle.test.ts
@@ -1,5 +1,20 @@
+import * as E from 'fp-ts/Either';
+import { update } from '../../../../src/write-side/resources/evaluation';
+import { arbitraryEvaluationType } from '../../../types/evaluation-type.helper';
+import { arbitraryEvaluationLocator } from '../../../types/evaluation-locator.helper';
+import { DomainEvent } from '../../../../src/domain-events';
+
 describe('lifecycle', () => {
   describe('record -> erase -> update', () => {
-    it.todo('errors with not found');
+    const evaluationLocator = arbitraryEvaluationLocator();
+    const events: ReadonlyArray<DomainEvent> = [];
+    const result = update({
+      evaluationLocator,
+      evaluationType: arbitraryEvaluationType(),
+    })(events);
+
+    it('errors with not found', () => {
+      expect(result).toStrictEqual(E.left('Evaluation to be updated does not exist'));
+    });
   });
 });
