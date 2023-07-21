0985b18087238b7195695f06c812713b21b0c230
Giorgio Sironi <g.sironi@elifesciences.org>
2023-07-21 09:45:57 +0100

    Characterize a bug with a skipped test #2468

--- test/write-side/resources/evaluation/lifecycle.test.ts
+++ test/write-side/resources/evaluation/lifecycle.test.ts
@@ -1,18 +1,31 @@
 import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
-import { erase, update } from '../../../../src/write-side/resources/evaluation';
+import { erase, update, record } from '../../../../src/write-side/resources/evaluation';
 import { arbitraryEvaluationType } from '../../../types/evaluation-type.helper';
-import { arbitraryEvaluationLocator } from '../../../types/evaluation-locator.helper';
 import { DomainEvent } from '../../../../src/domain-events';
 import { shouldNotBeCalled } from '../../../should-not-be-called';
+import { arbitraryRecordedEvaluation } from '../../../types/recorded-evaluation.helper';
 
 describe('lifecycle', () => {
   describe('record -> erase -> update', () => {
     let result: unknown;
-    const evaluationLocator = arbitraryEvaluationLocator();
+    const evaluation = arbitraryRecordedEvaluation();
+    const evaluationLocator = evaluation.evaluationLocator;
 
     beforeEach(() => {
       const events: Array<DomainEvent> = [];
+      const eventsFromRecord = pipe(
+        events,
+        record({
+          groupId: evaluation.groupId,
+          publishedAt: evaluation.publishedAt,
+          evaluationLocator: evaluation.evaluationLocator,
+          articleId: evaluation.articleId,
+          authors: evaluation.authors,
+        }),
+        E.getOrElseW(shouldNotBeCalled),
+      );
+      events.push(...eventsFromRecord);
       const eventsFromErase = pipe(
         events,
         erase({
@@ -27,7 +40,7 @@ describe('lifecycle', () => {
       })(events);
     });
 
-    it('errors with not found', () => {
+    it.skip('errors with not found', () => {
       expect(result).toStrictEqual(E.left('Evaluation to be updated does not exist'));
     });
   });
