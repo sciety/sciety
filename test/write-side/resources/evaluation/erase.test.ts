b3077d19208f276db439eb3d7c255fba48308002
LinaKind <l.kinduryte@elifesciences.org>
2023-07-24 10:31:13 +0100

    Simplify test set up #2468

--- test/write-side/resources/evaluation/erase.test.ts
+++ test/write-side/resources/evaluation/erase.test.ts
@@ -5,7 +5,6 @@ import { erase } from '../../../../src/write-side/resources/evaluation';
 import { shouldNotBeCalled } from '../../../should-not-be-called';
 import { arbitraryEvaluationLocator } from '../../../types/evaluation-locator.helper';
 import {
-  DomainEvent,
   constructEvent,
 } from '../../../../src/domain-events';
 import { arbitraryEvaluationRecordedEvent, evaluationRecordedHelper } from '../../../types/evaluation-recorded-event.helper';
@@ -21,20 +20,16 @@ const expectIncorrectlyRecordedEvaluationErasedEvent = (evaluationLocator: Evalu
 describe('erase', () => {
   describe('when an evaluation has been incorrectly recorded', () => {
     const evaluationLocator = arbitraryEvaluationLocator();
-    let eventsRaised: ReadonlyArray<DomainEvent>;
-
-    beforeEach(() => {
-      eventsRaised = pipe(
-        [
-          {
-            ...arbitraryEvaluationRecordedEvent(),
-            evaluationLocator,
-          },
-        ],
-        erase({ evaluationLocator }),
-        E.getOrElseW(shouldNotBeCalled),
-      );
-    });
+    const eventsRaised = pipe(
+      [
+        {
+          ...arbitraryEvaluationRecordedEvent(),
+          evaluationLocator,
+        },
+      ],
+      erase({ evaluationLocator }),
+      E.getOrElseW(shouldNotBeCalled),
+    );
 
     it('raises an IncorrectlyRecordedEvaluationErased event', () => {
       expect(eventsRaised).toStrictEqual([
@@ -44,15 +39,11 @@ describe('erase', () => {
   });
 
   describe('when the evaluation has not been recorded', () => {
-    let eventsRaised: ReadonlyArray<DomainEvent>;
-
-    beforeEach(() => {
-      eventsRaised = pipe(
-        [],
-        erase({ evaluationLocator: arbitraryEvaluationLocator() }),
-        E.getOrElseW(shouldNotBeCalled),
-      );
-    });
+    const eventsRaised = pipe(
+      [],
+      erase({ evaluationLocator: arbitraryEvaluationLocator() }),
+      E.getOrElseW(shouldNotBeCalled),
+    );
 
     it('raises no event', () => {
       expect(eventsRaised).toStrictEqual([]);
@@ -61,21 +52,17 @@ describe('erase', () => {
 
   describe('when the evaluation has been recorded and erased', () => {
     const evaluationLocator = arbitraryEvaluationLocator();
-    let eventsRaised: ReadonlyArray<DomainEvent>;
-
-    beforeEach(() => {
-      eventsRaised = pipe(
-        [
-          {
-            ...arbitraryEvaluationRecordedEvent(),
-            evaluationLocator,
-          },
-          constructEvent('IncorrectlyRecordedEvaluationErased')({ evaluationLocator }),
-        ],
-        erase({ evaluationLocator }),
-        E.getOrElseW(shouldNotBeCalled),
-      );
-    });
+    const eventsRaised = pipe(
+      [
+        {
+          ...arbitraryEvaluationRecordedEvent(),
+          evaluationLocator,
+        },
+        constructEvent('IncorrectlyRecordedEvaluationErased')({ evaluationLocator }),
+      ],
+      erase({ evaluationLocator }),
+      E.getOrElseW(shouldNotBeCalled),
+    );
 
     it('raises no event', () => {
       expect(eventsRaised).toStrictEqual([]);
@@ -84,31 +71,27 @@ describe('erase', () => {
 
   describe('when the evaluation has been recorded, erased and recorded again', () => {
     const evaluationLocator = arbitraryEvaluationLocator();
-    let eventsRaised: ReadonlyArray<DomainEvent>;
-
-    beforeEach(() => {
-      eventsRaised = pipe(
-        [
-          evaluationRecordedHelper(
-            arbitraryGroupId(),
-            arbitraryDoi(),
-            evaluationLocator,
-            [],
-            new Date(),
-          ),
-          constructEvent('IncorrectlyRecordedEvaluationErased')({ evaluationLocator }),
-          evaluationRecordedHelper(
-            arbitraryGroupId(),
-            arbitraryDoi(),
-            evaluationLocator,
-            [],
-            new Date(),
-          ),
-        ],
-        erase({ evaluationLocator }),
-        E.getOrElseW(shouldNotBeCalled),
-      );
-    });
+    const eventsRaised = pipe(
+      [
+        evaluationRecordedHelper(
+          arbitraryGroupId(),
+          arbitraryDoi(),
+          evaluationLocator,
+          [],
+          new Date(),
+        ),
+        constructEvent('IncorrectlyRecordedEvaluationErased')({ evaluationLocator }),
+        evaluationRecordedHelper(
+          arbitraryGroupId(),
+          arbitraryDoi(),
+          evaluationLocator,
+          [],
+          new Date(),
+        ),
+      ],
+      erase({ evaluationLocator }),
+      E.getOrElseW(shouldNotBeCalled),
+    );
 
     it('raises one IncorrectlyRecordedEvaluationErased event', () => {
       expect(eventsRaised).toStrictEqual([
