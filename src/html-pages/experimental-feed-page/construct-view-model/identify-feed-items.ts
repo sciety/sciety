98c4b3b85243cd3291ac47008860dba222eeaa22
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-07-25 12:01:14 +0100

    Introduce a seam to allow filtering of user lists #2481

--- src/html-pages/experimental-feed-page/construct-view-model/identify-feed-items.ts
+++ src/html-pages/experimental-feed-page/construct-view-model/identify-feed-items.ts
@@ -1,27 +1,29 @@
 import * as E from 'fp-ts/Either';
 import * as RA from 'fp-ts/ReadonlyArray';
-import { flow } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import { collapseCloseListEvents } from './collapse-close-list-events';
 import { FeedItem } from './feed-item';
-import { DomainEvent, isEventOfType } from '../../../domain-events';
+import { DomainEvent, EventOfType, isEventOfType } from '../../../domain-events';
 import { PageOfItems, paginate } from '../../../shared-components/paginate';
 import * as DE from '../../../types/data-error';
 import { Dependencies } from './dependencies';
 
-const isFeedRelevantEvent = (event: DomainEvent) => (
-  isEventOfType('ArticleAddedToList')(event)
+const isActivityOnUserList = (dependencies: Dependencies) => (event: EventOfType<'ArticleAddedToList'>) => pipe(
+  event.listId,
+  dependencies.lookupList,
+  O.match(
+    () => false,
+    () => true,
+  ),
 );
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const isActivityOnUserList = (dependencies: Dependencies) => (event: DomainEvent) => true;
 
 type IdentifyFeedItems = (dependencies: Dependencies, pageSize: number, page: number)
 => (events: ReadonlyArray<DomainEvent>)
 => E.Either<DE.DataError, PageOfItems<FeedItem>>;
 
 export const identifyFeedItems: IdentifyFeedItems = (dependencies, pageSize, page) => flow(
-  RA.filter(isFeedRelevantEvent),
+  RA.filter(isEventOfType('ArticleAddedToList')),
   RA.filter(isActivityOnUserList(dependencies)),
   RA.match(
     () => E.right({
