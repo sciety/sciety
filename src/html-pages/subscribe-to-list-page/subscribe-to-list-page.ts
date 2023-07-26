f10f77b60df5643d4cbebcd31249c9652abb7216
Giorgio Sironi <g.sironi@elifesciences.org>
2023-07-26 15:45:07 +0100

    Improve the information provided to the user #2485

--- src/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
+++ src/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
@@ -7,6 +7,10 @@ export const subscribeToListPage: Page = {
     <header class="page-header">
       <h1>Subscribe to a list</h1>
     </header>
-    <p>Hello! You caught us before we're ready. If you want us to tell you when it's possible to follow this list, <a href="/contact-us">let us know</a>.</p>
+    <p>Hello! Sorry, you've caught us before we're ready. 
+    
+    Once this feature is available, subscribing to a list will let you receive a regular update when this list is updated.
+
+    If you want us to tell you when it's possible to follow this list, <a href="/contact-us">let us know</a>.</p>
   `),
 };
