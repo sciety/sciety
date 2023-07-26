93329b41af56a7ad88af7c15fd1f217903c87903
David Moulton <d.moulton@elifesciences.org>
2023-07-26 15:26:18 +0100

    Provide user with some information on a feature that hasn't been implemented yet #2485

--- src/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
+++ src/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
@@ -2,10 +2,11 @@ import { toHtmlFragment } from '../../types/html-fragment';
 import { Page } from '../../types/page';
 
 export const subscribeToListPage: Page = {
-  title: 'Subscribe to list page',
+  title: 'Subscribe to a list',
   content: toHtmlFragment(`
     <header class="page-header">
-      <h1>Subscribe to list page</h1>
+      <h1>Subscribe to a list</h1>
     </header>
+    <p>Hello! You caught us before we're ready. If you want us to tell you when it's possible to follow this list, <a href="/contact-us">let us know</a>.</p>
   `),
 };
