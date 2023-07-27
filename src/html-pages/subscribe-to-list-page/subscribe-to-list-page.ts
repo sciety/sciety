8111bed30a97e700ca065f2fe2fe336363843245
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-27 14:20:52 +0100

    Reshape for clearer seams #2491

--- src/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
+++ src/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
@@ -1,20 +1,28 @@
+import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
 import { HandlePage } from '../../http/page-handler';
 import { toHtmlFragment } from '../../types/html-fragment';
 
-export const subscribeToListPage: HandlePage = () => TE.right({
-  title: 'Subscribe to a list',
-  content: toHtmlFragment(`
-    <header class="page-header">
+const renderAsHtml = () => toHtmlFragment(`
+  <header class="page-header">
 
-      <h1>Subscribe to a list</h1>
-    </header>
+    <h1>Subscribe to a list</h1>
+  </header>
 
-    <p>
-      Please note that this is an experimental feature and may be a bit rough around the edges
-      while we get things ship-shape.
-    </p>
+  <p>
+    Please note that this is an experimental feature and may be a bit rough around the edges
+    while we get things ship-shape.
+  </p>
 
-    <script type="text/javascript" src="https://form.jotform.com/jsform/232072517707050"></script>
-  `),
-});
+  <script type="text/javascript" src="https://form.jotform.com/jsform/232072517707050"></script>
+`);
+
+export const subscribeToListPage: HandlePage = (params: unknown) => pipe(
+  params,
+  renderAsHtml,
+  (content) => ({
+    title: 'Subscribe to a list',
+    content,
+  }),
+  TE.right,
+);
