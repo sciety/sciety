629b72eebb22834ddcbbf5f4d781a1bb25827b53
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-07-27 14:25:18 +0100

    Use a view model in the subscribe to list page #2491

--- src/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
+++ src/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
@@ -2,8 +2,13 @@ import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
 import { HandlePage } from '../../http/page-handler';
 import { toHtmlFragment } from '../../types/html-fragment';
+import { ListId } from '../../types/list-id';
 
-const renderAsHtml = () => toHtmlFragment(`
+type ViewModel = {
+  listId: ListId,
+};
+
+const renderAsHtml = (viewModel: ViewModel) => toHtmlFragment(`
   <header class="page-header">
 
     <h1>Subscribe to a list</h1>
@@ -14,11 +19,12 @@ const renderAsHtml = () => toHtmlFragment(`
     while we get things ship-shape.
   </p>
 
-  <script type="text/javascript" src="https://form.jotform.com/jsform/232072517707050"></script>
+  <script type="text/javascript" src="https://form.jotform.com/jsform/232072517707050?listId=${viewModel.listId}"></script>
 `);
 
 export const subscribeToListPage: HandlePage = (params: unknown) => pipe(
   params,
+  () => ({ listId: '' as ListId }),
   renderAsHtml,
   (content) => ({
     title: 'Subscribe to a list',
