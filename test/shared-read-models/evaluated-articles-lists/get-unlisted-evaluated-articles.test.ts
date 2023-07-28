1715e95b31dfc62c9e3459246f4deaa04df6a8cc
Kevin Rutherford <kevin@rutherford-software.com>
2023-07-28 15:12:18 +0100

    Create a test list for a new readmodel #2496

---
+++ test/shared-read-models/evaluated-articles-lists/get-unlisted-evaluated-articles.test.ts
@@ -0,0 +1,65 @@
+describe('get-unlisted-evaluated-articles', () => {
+  describe('given an article and a group with no previous activity', () => {
+    it.todo('the article IS NOT reported as work');
+
+    describe('when the group\'s list is identified', () => {
+      it.todo('the article IS NOT reported as work');
+
+      describe('and then the article is added to the group\'s list', () => {
+        it.todo('the article IS NOT reported as work');
+
+        describe('and then the group evaluates the article', () => {
+          it.todo('the article IS NOT reported as work');
+        });
+      });
+
+      describe('and then the article is evaluated by the group', () => {
+        it.todo('the article IS reported as work');
+
+        describe('and then the article is added to the list', () => {
+          it.todo('the article IS NOT reported as work');
+        });
+      });
+    });
+
+    describe('when the article is added to the group\'s list', () => {
+      it.todo('the article IS NOT reported as work');
+
+      describe('and then the group\'s list is identified', () => {
+        it.todo('the article IS NOT reported as work');
+
+        describe('and then the group evaluates the article', () => {
+          it.todo('the article IS NOT reported as work');
+        });
+      });
+
+      describe('and then the article is evaluated by the group', () => {
+        it.todo('the article IS NOT reported as work');
+
+        describe('and then the group\'s list is identified', () => {
+          it.todo('the article IS NOT reported as work');
+        });
+      });
+    });
+
+    describe('when the article is evaluated by the group', () => {
+      it.todo('the article IS NOT reported as work');
+
+      describe('and then the group\'s list is identified', () => {
+        it.todo('the article IS reported as work');
+
+        describe('and then the article is added to the group\'s list', () => {
+          it.todo('the article IS NOT reported as work');
+        });
+      });
+
+      describe('and then the article is added to the group\'s list', () => {
+        it.todo('the article IS NOT reported as work');
+
+        describe('and then the group\'s list is identified', () => {
+          it.todo('the article IS NOT reported as work');
+        });
+      });
+    });
+  });
+});
