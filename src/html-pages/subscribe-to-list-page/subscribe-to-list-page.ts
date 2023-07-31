91f5f228f05e54d5cca4d430e108f30c5245e386
David Moulton <d.moulton@elifesciences.org>
2023-07-31 13:48:12 +0100

    Extract a type for subsequent reuse #2491

--- src/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
+++ src/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
@@ -1,11 +1,11 @@
 import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
-import { HandlePage } from '../../http/page-handler';
 import { renderAsHtml, toErrorPage } from './render-as-html';
 import { Queries } from '../../shared-read-models';
 import { constructViewModel } from './construct-view-model';
+import { ConstructPage } from '../construct-page';
 
-export const subscribeToListPage = (dependencies: Queries): HandlePage => (params: unknown) => pipe(
+export const subscribeToListPage = (dependencies: Queries): ConstructPage => (params: unknown) => pipe(
   params,
   constructViewModel(dependencies),
   TE.bimap(toErrorPage, renderAsHtml),
