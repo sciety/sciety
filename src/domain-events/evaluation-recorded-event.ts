ae0739e61c5297aab082bdca2347a20172fe34f0
Giorgio Sironi <g.sironi@elifesciences.org>
2023-08-02 08:50:51 +0100

    Wrapping the original codec with intention, but no implementation #2510

--- src/domain-events/evaluation-recorded-event.ts
+++ src/domain-events/evaluation-recorded-event.ts
@@ -6,9 +6,19 @@ import { GroupIdFromString } from '../types/codecs/GroupIdFromString';
 import { evaluationLocatorCodec } from '../types/evaluation-locator';
 import { evaluationTypeCodec } from './types/evaluation-type';
 
+const eventTypePropertyCodec = (currentValue: string, deprecatedValue: string) => {
+  const currentPropertyCodec = t.literal('EvaluationPublicationRecorded');
+  return new t.Type(
+    'UpgradedType',
+    currentPropertyCodec.is,
+    (u, c) => currentPropertyCodec.validate(u, c),
+    currentPropertyCodec.encode,
+  );
+};
+
 export const evaluationRecordedEventCodec = t.type({
   id: EventIdFromString,
-  type: t.literal('EvaluationPublicationRecorded'),
+  type: eventTypePropertyCodec('EvaluationPublicationRecorded', 'EvaluationRecorded'),
   date: tt.DateFromISOString,
   groupId: GroupIdFromString,
   evaluationLocator: evaluationLocatorCodec,
