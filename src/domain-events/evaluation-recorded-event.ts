5f280e33e81803abbcd737ebc47816d67bf1e824
Giorgio Sironi <g.sironi@elifesciences.org>
2023-08-02 08:56:35 +0100

    Delegate to original codec, only passing in the current value #2510

--- src/domain-events/evaluation-recorded-event.ts
+++ src/domain-events/evaluation-recorded-event.ts
@@ -1,5 +1,7 @@
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
 import { DoiFromString } from '../types/codecs/DoiFromString';
 import { EventIdFromString } from '../types/codecs/EventIdFromString';
 import { GroupIdFromString } from '../types/codecs/GroupIdFromString';
@@ -11,7 +13,18 @@ const eventTypePropertyCodec = (currentValue: string, deprecatedValue: string) =
   return new t.Type(
     'UpgradedType',
     currentPropertyCodec.is,
-    (u, c) => currentPropertyCodec.validate(u, c),
+    (u, c) => pipe(
+      t.string.validate(u, c),
+      E.chain((value) => {
+        if (value === deprecatedValue) {
+          return currentPropertyCodec.validate(currentValue, c);
+        }
+        if (value === currentValue) {
+          return currentPropertyCodec.validate(currentValue, c);
+        }
+        return t.failure(u, c);
+      }),
+    ),
     currentPropertyCodec.encode,
   );
 };
