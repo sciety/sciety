2d24c5063badf7c483c7484c13144377f7b385db
Giorgio Sironi <g.sironi@elifesciences.org>
2023-08-04 08:39:53 +0100

    Represent intention of codec #2510

--- src/domain-events/evaluation-publication-recorded-event.ts
+++ src/domain-events/evaluation-publication-recorded-event.ts
@@ -8,30 +8,30 @@ import { GroupIdFromString } from '../types/codecs/GroupIdFromString';
 import { evaluationLocatorCodec } from '../types/evaluation-locator';
 import { evaluationTypeCodec } from './types/evaluation-type';
 
-const eventTypePropertyCodec = (currentValue: string, deprecatedValue: string) => {
-  const currentPropertyCodec = t.literal('EvaluationPublicationRecorded');
+const upgradedEventType = (currentValue: string, deprecatedValues: ReadonlyArray<string>) => {
+  const currentEventTypeCodec = t.literal('EvaluationPublicationRecorded');
   return new t.Type(
     'UpgradedType',
-    currentPropertyCodec.is,
+    currentEventTypeCodec.is,
     (u, c) => pipe(
       t.string.validate(u, c),
       E.chain((value) => {
-        if (value === deprecatedValue) {
-          return currentPropertyCodec.validate(currentValue, c);
+        if (deprecatedValues.includes(value)) {
+          return currentEventTypeCodec.validate(currentValue, c);
         }
         if (value === currentValue) {
-          return currentPropertyCodec.validate(currentValue, c);
+          return currentEventTypeCodec.validate(currentValue, c);
         }
         return t.failure(u, c);
       }),
     ),
-    currentPropertyCodec.encode,
+    currentEventTypeCodec.encode,
   );
 };
 
 export const evaluationRecordedEventCodec = t.type({
   id: EventIdFromString,
-  type: eventTypePropertyCodec('EvaluationPublicationRecorded', 'EvaluationRecorded'),
+  type: upgradedEventType('EvaluationPublicationRecorded', ['EvaluationRecorded']),
   date: tt.DateFromISOString,
   groupId: GroupIdFromString,
   evaluationLocator: evaluationLocatorCodec,
