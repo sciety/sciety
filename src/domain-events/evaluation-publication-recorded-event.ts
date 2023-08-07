308cfa8a8bda252f01a0b5ce59e734f662e3b160
Giorgio Sironi <g.sironi@elifesciences.org>
2023-08-07 10:44:07 +0100

    Provide the capability to upgrade event type #2511

--- src/domain-events/evaluation-publication-recorded-event.ts
+++ src/domain-events/evaluation-publication-recorded-event.ts
@@ -1,14 +1,37 @@
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
 import { DoiFromString } from '../types/codecs/DoiFromString';
 import { EventIdFromString } from '../types/codecs/EventIdFromString';
 import { GroupIdFromString } from '../types/codecs/GroupIdFromString';
 import { evaluationLocatorCodec } from '../types/evaluation-locator';
 import { evaluationTypeCodec } from './types/evaluation-type';
 
+const upgradedEventType = <T extends string>(currentValue: T, deprecatedValues: ReadonlyArray<string>) => {
+  const currentEventTypeCodec = t.literal(currentValue);
+  return new t.Type(
+    'UpgradedType',
+    currentEventTypeCodec.is,
+    (u, c) => pipe(
+      t.string.validate(u, c),
+      E.chain((value) => {
+        if (deprecatedValues.includes(value)) {
+          return currentEventTypeCodec.validate(currentValue, c);
+        }
+        if (value === currentValue) {
+          return currentEventTypeCodec.validate(currentValue, c);
+        }
+        return t.failure(u, c);
+      }),
+    ),
+    currentEventTypeCodec.encode,
+  );
+};
+
 export const evaluationPublicationRecordedEventCodec = t.type({
   id: EventIdFromString,
-  type: t.literal('EvaluationPublicationRecorded'),
+  type: upgradedEventType('EvaluationPublicationRecorded', []),
   date: tt.DateFromISOString,
   groupId: GroupIdFromString,
   evaluationLocator: evaluationLocatorCodec,
