bba44f108a850117ac4f54a1ebe5fe94e0a47585
LinaKind <l.kinduryte@elifesciences.org>
2023-08-07 10:33:30 +0100

    Specify legacy events should be upgraded to the current ones on loading #2511

--- test/domain-events/evaluation-recorded-event.test.ts
+++ test/domain-events/evaluation-recorded-event.test.ts
@@ -1,14 +1,12 @@
 import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
-import { evaluationRecordedEventCodec } from '../../src/domain-events/evaluation-recorded-event';
 import { arbitraryEvaluationRecordedEvent } from './evaluation-recorded-event.helper';
-import { EventOfType, constructEvent } from '../../src/domain-events';
+import { EventOfType, constructEvent, domainEventCodec } from '../../src/domain-events';
 import { arbitraryDoi } from '../types/doi.helper';
 import { arbitraryEvaluationLocator } from '../types/evaluation-locator.helper';
 import { arbitraryDate } from '../helpers';
 import { arbitraryEvaluationType } from '../types/evaluation-type.helper';
 import { arbitraryGroupId } from '../types/group-id.helper';
-import { evaluationPublicationRecordedEventCodec } from '../../src/domain-events/evaluation-publication-recorded-event';
 
 const arbitraryEvaluationPublicationRecordedEvent = (): EventOfType<'EvaluationPublicationRecorded'> => constructEvent('EvaluationPublicationRecorded')({
   groupId: arbitraryGroupId(),
@@ -21,25 +19,28 @@ const arbitraryEvaluationPublicationRecordedEvent = (): EventOfType<'EvaluationP
 });
 
 describe('evaluation-recorded-event', () => {
-  describe('when there is a legacy event to be stored', () => {
+  describe('when there is a legacy event to be loaded', () => {
     const evaluationRecordedEvent = arbitraryEvaluationRecordedEvent();
     const result = pipe(
       evaluationRecordedEvent,
-      evaluationRecordedEventCodec.encode,
-      evaluationRecordedEventCodec.decode,
+      domainEventCodec.encode,
+      domainEventCodec.decode,
     );
 
-    it('encodes and decodes the event successfully', () => {
-      expect(result).toStrictEqual(E.right(evaluationRecordedEvent));
+    it.skip('encodes and decodes the event successfully', () => {
+      expect(result).toStrictEqual(E.right({
+        ...evaluationRecordedEvent,
+        type: 'EvaluationPublicationRecorded',
+      }));
     });
   });
 
-  describe('when there is a current event to be stored', () => {
+  describe('when there is a current event to be loaded', () => {
     const evaluationPublicationRecordedEvent = arbitraryEvaluationPublicationRecordedEvent();
     const result = pipe(
       evaluationPublicationRecordedEvent,
-      evaluationPublicationRecordedEventCodec.encode,
-      evaluationPublicationRecordedEventCodec.decode,
+      domainEventCodec.encode,
+      domainEventCodec.decode,
     );
 
     it('encodes and decodes the event successfully', () => {
