02ba1d1e6220a475fd8405a9198dac2e50b694a6
LinaKind <l.kinduryte@elifesciences.org>
2023-08-07 10:50:26 +0100

    Provide testing helpers for the current event, in addition to the legacy event #2511

--- test/domain-events/evaluation-publication-recorded-event.helper.ts
+++ test/domain-events/evaluation-publication-recorded-event.helper.ts
@@ -1,8 +1,4 @@
 import { EventOfType, constructEvent } from '../../src/domain-events';
-import { Doi } from '../../src/types/doi';
-import { EvaluationLocator } from '../../src/types/evaluation-locator';
-import { GroupId } from '../../src/types/group-id';
-import { EvaluationType } from '../../src/types/recorded-evaluation';
 import { arbitraryDate } from '../helpers';
 import { arbitraryDoi } from '../types/doi.helper';
 import { arbitraryEvaluationLocator } from '../types/evaluation-locator.helper';
@@ -19,20 +15,12 @@ export const arbitraryEvaluationRecordedEvent = (): EventOfType<'EvaluationRecor
   evaluationType: arbitraryEvaluationType(),
 });
 
-export const evaluationRecordedHelper = (
-  groupId: GroupId,
-  doi: Doi,
-  evaluationLocator: EvaluationLocator,
-  authors: ReadonlyArray<string>,
-  publishedAt: Date,
-  date: Date = new Date(),
-  evaluationType?: EvaluationType,
-): EventOfType<'EvaluationRecorded'> => constructEvent('EvaluationRecorded')({
-  date,
-  groupId,
-  articleId: doi,
-  evaluationLocator,
-  publishedAt,
-  authors,
-  evaluationType,
+export const arbitraryEvaluationPublicationRecordedEvent = (): EventOfType<'EvaluationPublicationRecorded'> => constructEvent('EvaluationPublicationRecorded')({
+  groupId: arbitraryGroupId(),
+  articleId: arbitraryDoi(),
+  evaluationLocator: arbitraryEvaluationLocator(),
+  authors: [],
+  publishedAt: arbitraryDate(),
+  date: arbitraryDate(),
+  evaluationType: arbitraryEvaluationType(),
 });
