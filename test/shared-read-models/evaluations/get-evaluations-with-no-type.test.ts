86714f6b30775acc195d082dbaa93d9a60c68f0e
Giorgio Sironi <g.sironi@elifesciences.org>
2023-08-07 11:32:37 +0100

    Stop using a deprecated helper in one more file #2511

--- test/shared-read-models/evaluations/get-evaluations-with-no-type.test.ts
+++ test/shared-read-models/evaluations/get-evaluations-with-no-type.test.ts
@@ -1,7 +1,7 @@
 import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
-import { evaluationRecordedHelper } from '../../domain-events/evaluation-publication-recorded-event.helper';
+import { arbitraryEvaluationRecordedEvent } from '../../domain-events/evaluation-publication-recorded-event.helper';
 import { handleEvent, initialState } from '../../../src/shared-read-models/evaluations/handle-event';
 import { arbitraryRecordedEvaluation } from '../../types/recorded-evaluation.helper';
 import { getEvaluationsWithNoType } from '../../../src/shared-read-models/evaluations/get-evaluations-with-no-type';
@@ -15,24 +15,26 @@ describe('get-evaluations-with-no-type', () => {
     };
     const readModel = pipe(
       [
-        evaluationRecordedHelper(
-          evaluation1.groupId,
-          evaluation1.articleId,
-          evaluation1.evaluationLocator,
-          evaluation1.authors,
-          evaluation1.publishedAt,
-          evaluation1.recordedAt,
-          'author-response',
-        ),
-        evaluationRecordedHelper(
-          evaluation2.groupId,
-          evaluation2.articleId,
-          evaluation2.evaluationLocator,
-          evaluation2.authors,
-          evaluation2.publishedAt,
-          evaluation2.recordedAt,
-          undefined,
-        ),
+        {
+          ...arbitraryEvaluationRecordedEvent(),
+          groupId: evaluation1.groupId,
+          articleId: evaluation1.articleId,
+          evaluationLocator: evaluation1.evaluationLocator,
+          authors: evaluation1.authors,
+          publishedAt: evaluation1.publishedAt,
+          date: evaluation1.recordedAt,
+          evaluationType: 'author-response',
+        },
+        {
+          ...arbitraryEvaluationRecordedEvent(),
+          groupId: evaluation2.groupId,
+          articleId: evaluation2.articleId,
+          evaluationLocator: evaluation2.evaluationLocator,
+          authors: evaluation2.authors,
+          publishedAt: evaluation2.publishedAt,
+          date: evaluation2.recordedAt,
+          evaluationType: undefined,
+        },
       ],
       RA.reduce(initialState(), handleEvent),
     );
