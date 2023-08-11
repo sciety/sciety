9725fc9e1c1da7f0f818381052c248882afcc834
Giorgio Sironi <g.sironi@elifesciences.org>
2023-08-11 11:17:14 +0100

    Write a confirmatory test for the case when there are evaluated articles #2525

--- test/html-pages/group-page/group-feed-page/construct-view-model/construct-content.test.ts
+++ test/html-pages/group-page/group-feed-page/construct-view-model/construct-content.test.ts
@@ -1,4 +1,5 @@
 import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 import { dummyLogger } from '../../../../dummy-logger';
 import { ViewModel } from '../../../../../src/html-pages/group-page/group-feed-page/view-model';
@@ -6,6 +7,9 @@ import { createTestFramework, TestFramework } from '../../../../framework';
 import { arbitraryGroup } from '../../../../types/group.helper';
 import { shouldNotBeCalled } from '../../../../should-not-be-called';
 import { constructContent } from '../../../../../src/html-pages/group-page/group-feed-page/construct-view-model/construct-content';
+import { List } from '../../../../../src/types/list';
+import * as LOID from '../../../../../src/types/list-owner-id';
+import { arbitraryArticleId } from '../../../../types/article-id.helper';
 
 describe('construct-content', () => {
   let framework: TestFramework;
@@ -16,7 +20,38 @@ describe('construct-content', () => {
   });
 
   describe('when the group has evaluated articles', () => {
-    it.todo('contains article cards');
+    let viewModel: ViewModel['content'];
+    let initialGroupList: List;
+    const article1 = arbitraryArticleId();
+    const article2 = arbitraryArticleId();
+
+    beforeEach(async () => {
+      await framework.commandHelpers.createGroup(group);
+      initialGroupList = framework.queries.selectAllListsOwnedBy(LOID.fromGroupId(group.id))[0];
+      await framework.commandHelpers.addArticleToList(article1, initialGroupList.id);
+      await framework.commandHelpers.addArticleToList(article2, initialGroupList.id);
+
+      viewModel = await pipe(
+        constructContent({
+          ...framework.queries,
+          ...framework.happyPathThirdParties,
+          logger: dummyLogger,
+        },
+        group.id),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+    });
+
+    it('contains article cards', () => {
+      expect(viewModel).toStrictEqual([
+        E.right(expect.objectContaining({
+          articleId: article2,
+        })),
+        E.right(expect.objectContaining({
+          articleId: article1,
+        })),
+      ]);
+    });
   });
 
   describe('when the group has no evaluated articles', () => {
