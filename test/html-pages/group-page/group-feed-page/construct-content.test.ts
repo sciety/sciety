cf6aa1ac0720695734e01153f0d0d7fb00beb8ab
Giorgio Sironi <g.sironi@elifesciences.org>
2023-08-11 10:50:07 +0100

    Describe current functionality of the view model with todo tests #2525

---
+++ test/html-pages/group-page/group-feed-page/construct-content.test.ts
@@ -0,0 +1,9 @@
+describe('construct-content', () => {
+  describe('when the group has evaluated articles', () => {
+    it.todo('contains article cards');
+  });
+
+  describe('when the group has no evaluated articles', () => {
+    it.todo('contains a no-activity-yet message');
+  });
+});
