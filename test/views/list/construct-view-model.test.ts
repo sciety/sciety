d567c41d2cd39a5b4a55bbd58bd0a78842e27d77
LinaKind <l.kinduryte@elifesciences.org>
2023-08-14 13:27:04 +0100

    Write a characterisation test of the ordering inside the Atom feed view model #2532

--- test/views/list/construct-view-model.test.ts
+++ test/views/list/construct-view-model.test.ts
@@ -1,5 +1,51 @@
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+import { arbitraryArticleId } from '../../types/article-id.helper';
+import { createTestFramework, TestFramework } from '../../framework';
+import { arbitraryUserDetails } from '../../types/user-details.helper';
+import * as LOID from '../../../src/types/list-owner-id';
+import { constructViewModel } from '../../../src/views/list/construct-view-model';
+import { ViewModel } from '../../../src/views/list/view-model';
+
 describe('construct-view-model', () => {
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
   describe('when the list contains two articles', () => {
-    it.todo('sorts the articles in reverse order of being added to the list');
+    const articleId1 = arbitraryArticleId();
+    const articleId2 = arbitraryArticleId();
+    let viewModel: ViewModel;
+    const createList = async () => {
+      const userDetails = arbitraryUserDetails();
+      await framework.commandHelpers.createUserAccount(userDetails);
+      const list = framework.queries.selectAllListsOwnedBy(LOID.fromUserId(userDetails.id))[0];
+      return list.id;
+    };
+
+    beforeEach(async () => {
+      const listId = await createList();
+      await framework.commandHelpers.addArticleToList(articleId1, listId);
+      await framework.commandHelpers.addArticleToList(articleId2, listId);
+      viewModel = await pipe(
+        { id: listId },
+        constructViewModel(framework.dependenciesForViews),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+    });
+
+    it('sorts the articles in reverse order of being added to the list', () => {
+      expect(viewModel.articles).toStrictEqual([
+        expect.objectContaining({
+          articleId: articleId2,
+        }),
+        expect.objectContaining({
+          articleId: articleId1,
+        }),
+      ]);
+    });
   });
 });
