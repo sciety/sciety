7231a6027b3a72db4d403bde4978dcf862aa5694
Giorgio Sironi <g.sironi@elifesciences.org>
2023-08-14 13:12:28 +0100

    Express intent with a todo test #2532

---
+++ test/views/list/construct-view-model.test.ts
@@ -0,0 +1,5 @@
+describe('construct-view-model', () => {
+  describe('when the list contains two articles', () => {
+    it.todo('sorts the articles in reverse order of being added to the list');
+  });
+});
