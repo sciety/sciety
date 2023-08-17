08daac4b2bb7b5620cd1ee13e9e59666e30bf023
Giorgio Sironi <g.sironi@elifesciences.org>
2023-08-17 16:21:42 +0100

    Explicitly upgrade a deprecated event type after decoding #2649

--- src/domain-events/evaluation-publication-recorded-event.ts
+++ src/domain-events/evaluation-publication-recorded-event.ts
@@ -1,37 +1,26 @@
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
-import * as E from 'fp-ts/Either';
-import { pipe } from 'fp-ts/function';
 import { DoiFromString } from '../types/codecs/DoiFromString';
 import { EventIdFromString } from '../types/codecs/EventIdFromString';
 import { GroupIdFromString } from '../types/codecs/GroupIdFromString';
 import { evaluationLocatorCodec } from '../types/evaluation-locator';
 import { evaluationTypeCodec } from './types/evaluation-type';
 
-const upgradedEventType = <T extends string>(currentValue: T, deprecatedValues: ReadonlyArray<string>) => {
-  const currentEventTypeCodec = t.literal(currentValue);
-  return new t.Type(
-    'UpgradedType',
-    currentEventTypeCodec.is,
-    (u, c) => pipe(
-      t.string.validate(u, c),
-      E.chain((value) => {
-        if (deprecatedValues.includes(value)) {
-          return currentEventTypeCodec.validate(currentValue, c);
-        }
-        if (value === currentValue) {
-          return currentEventTypeCodec.validate(currentValue, c);
-        }
-        return t.failure(u, c);
-      }),
-    ),
-    currentEventTypeCodec.encode,
-  );
-};
+export const evaluationRecordedEventCodec = t.type({
+  id: EventIdFromString,
+  type: t.literal('EvaluationRecorded'),
+  date: tt.DateFromISOString,
+  groupId: GroupIdFromString,
+  evaluationLocator: evaluationLocatorCodec,
+  articleId: DoiFromString,
+  publishedAt: tt.DateFromISOString,
+  authors: t.readonlyArray(t.string),
+  evaluationType: evaluationTypeCodec,
+});
 
 export const evaluationPublicationRecordedEventCodec = t.type({
   id: EventIdFromString,
-  type: upgradedEventType('EvaluationPublicationRecorded', ['EvaluationRecorded']),
+  type: t.literal('EvaluationPublicationRecorded'),
   date: tt.DateFromISOString,
   groupId: GroupIdFromString,
   evaluationLocator: evaluationLocatorCodec,
