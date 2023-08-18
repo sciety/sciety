870ef213d2e4341958859bb2557f40cbbdddfb93
Kevin Rutherford <kevin@rutherford-software.com>
2023-08-18 15:51:47 +0100

    Show unlisted articles in /status #2496

--- src/shared-read-models/evaluated-articles-lists/unlisted-articles-status.ts
+++ src/shared-read-models/evaluated-articles-lists/unlisted-articles-status.ts
@@ -1,6 +1,7 @@
 import { Json } from 'fp-ts/Json';
+import { getUnlistedEvaluatedArticles } from './get-unlisted-evaluated-articles';
 import { ReadModel } from './handle-event';
 
 export const unlistedArticlesStatus = (readmodel: ReadModel) => (): Json => ({
-  total: Object.values(readmodel).length,
+  articles: getUnlistedEvaluatedArticles(readmodel)(),
 });
