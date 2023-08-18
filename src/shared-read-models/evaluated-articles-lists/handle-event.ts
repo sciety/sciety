b9b5983b0db1c26235e20d2a1f6720e09864c1e2
Kevin Rutherford <kevin@rutherford-software.com>
2023-08-18 13:32:15 +0100

    Sketch a starting structure for the readmodel #2496

--- src/shared-read-models/evaluated-articles-lists/handle-event.ts
+++ src/shared-read-models/evaluated-articles-lists/handle-event.ts
@@ -1,9 +1,22 @@
 /* eslint-disable no-param-reassign */
 import { DomainEvent } from '../../domain-events';
+import { GroupId } from '../../types/group-id';
+import { ListId } from '../../types/list-id';
 
-export type ReadModel = ReadonlyArray<string>;
+type ArticleState = {
+  listedIn: ReadonlyArray<ListId>,
+  evaluatedBy: ReadonlyArray<GroupId>,
+};
 
-export const initialState = (): ReadModel => [];
+export type ReadModel = {
+  articles: Map<string, ArticleState>,
+  groups: Map<GroupId, ListId>,
+};
+
+export const initialState = (): ReadModel => ({
+  articles: new Map(),
+  groups: new Map(),
+});
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel => readmodel;
