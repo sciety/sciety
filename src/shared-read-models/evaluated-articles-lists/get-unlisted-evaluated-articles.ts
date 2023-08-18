deae792d05d04cc5e415cf0a6d2a8d46dfef8356
Kevin Rutherford <kevin@rutherford-software.com>
2023-08-18 14:10:35 +0100

    Sketch a possible query #2496

--- src/shared-read-models/evaluated-articles-lists/get-unlisted-evaluated-articles.ts
+++ src/shared-read-models/evaluated-articles-lists/get-unlisted-evaluated-articles.ts
@@ -1,10 +1,20 @@
 import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
 import * as S from 'fp-ts/string';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as RM from 'fp-ts/ReadonlyMap';
 import { ArticleState, ReadModel } from './handle-event';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const articleIsNotListed = (groups: ReadModel['groups']) => (a: ArticleState) => false;
+const articleIsNotListed = (groups: ReadModel['groups']) => (a: ArticleState): boolean => pipe(
+  a.evaluatedBy,
+  RA.filter((groupId) => groups.has(groupId)),
+  RA.map((groupId) => groups.get(groupId)),
+  RA.map(O.fromNullable),
+  RA.compact,
+  RA.filter((listId) => !a.listedIn.includes(listId)),
+  RA.isEmpty,
+);
 
 export const getUnlistedEvaluatedArticles = (readmodel: ReadModel) => (): ReadonlyArray<string> => pipe(
   readmodel.articles,
