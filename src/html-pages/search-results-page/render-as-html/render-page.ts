19840d5dcad0c5cb6a4835cb11f9a23076b50d40
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-08-21 13:47:58 +0100

    Break down a renderer into individual components #2550

--- src/html-pages/search-results-page/render-as-html/render-page.ts
+++ src/html-pages/search-results-page/render-as-html/render-page.ts
@@ -1,9 +1,9 @@
 import { pipe } from 'fp-ts/function';
-import * as RA from 'fp-ts/ReadonlyArray';
 import { renderSearchResults } from './render-search-results';
 import { renderSearchForm } from '../../../shared-components/search-form';
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 import { ViewModel } from '../view-model';
+import { renderRelatedGroups } from './render-related-groups';
 
 export const renderPage = (viewModel: ViewModel): HtmlFragment => pipe(
   `
@@ -11,25 +11,7 @@ export const renderPage = (viewModel: ViewModel): HtmlFragment => pipe(
       <h1>Search Sciety</h1>
     </header>
     ${renderSearchForm(viewModel.query, viewModel.evaluatedOnly)}
-    ${viewModel.relatedGroups.length !== 0
-    ? pipe(
-      viewModel.relatedGroups,
-      RA.map((relatedGroup) => `
-        <li role="listitem">
-          <a href="${relatedGroup.path}">${relatedGroup.name}</a>
-        </li>
-      `),
-      (items) => items.join(''),
-      (listContent) => `
-        <section class="related-groups">
-          <h2>Related groups</h2>
-          <ul role="list" class="related-groups__list">
-          ${listContent}
-          </ul>
-        </section>
-      `,
-    )
-    : ''}
+    ${renderRelatedGroups(viewModel.relatedGroups)}
     <section class="search-results">
       ${renderSearchResults(viewModel)}
     </section>
---
+++ src/html-pages/search-results-page/render-as-html/render-related-groups.ts
@@ -0,0 +1,25 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { ViewModel } from '../view-model';
+import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
+
+export const renderRelatedGroups = (relatedGroups: ViewModel['relatedGroups']): HtmlFragment => (relatedGroups.length !== 0
+  ? pipe(
+    relatedGroups,
+    RA.map((relatedGroup) => `
+      <li role="listitem">
+        <a href="${relatedGroup.path}">${relatedGroup.name}</a>
+      </li>
+    `),
+    (items) => items.join(''),
+    (listContent) => `
+      <section class="related-groups">
+        <h2>Related groups</h2>
+        <ul role="list" class="related-groups__list">
+        ${listContent}
+        </ul>
+      </section>
+    `,
+    toHtmlFragment,
+  )
+  : toHtmlFragment(''));
