4f191516fe4215e3fb9a0569332f6fe343933a21
David Moulton <d.moulton@elifesciences.org>
2023-08-24 16:03:17 +0100

    Remove gutters from search results footer, and delete now unused class and selector

--- src/html-pages/search-results-page/render-as-html/render-next-link-or-calls-to-action.ts
+++ src/html-pages/search-results-page/render-as-html/render-next-link-or-calls-to-action.ts
@@ -6,7 +6,7 @@ import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 export const renderNextLinkOrCallsToAction = (pageNumber: number, basePath: O.Option<string>): HtmlFragment => pipe(
   basePath,
   O.fold(
-    () => '<footer class="search-results__footer">Not what you were hoping for? Try our <a href="https://blog.sciety.org/sciety-search/">advanced search tips</a>, or <a href="/contact-us">leave us a suggestion</a>.</footer>',
+    () => '<footer>Not what you were hoping for? Try our <a href="https://blog.sciety.org/sciety-search/">advanced search tips</a>, or <a href="/contact-us">leave us a suggestion</a>.</footer>',
     (base) => renderPaginationControls({
       nextPageHref: O.some(`${base}page=${pageNumber}`),
     }),
