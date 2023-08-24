b53b608db2f3166370972afbf3f59445f285babe
Giorgio Sironi <g.sironi@elifesciences.org>
2023-08-24 11:33:46 +0100

    Log an error if any related group is missing from the read model #2550

--- src/html-pages/search-results-page/construct-view-model/construct-related-groups.ts
+++ src/html-pages/search-results-page/construct-view-model/construct-related-groups.ts
@@ -1,16 +1,26 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
 import { Doi } from '../../../types/doi';
 import { ArticlesCategoryViewModel } from '../view-model';
 import { Dependencies } from './dependencies';
 import * as GID from '../../../types/group-id';
+
+const getGroup = (dependencies: Dependencies) => (groupId: GID.GroupId) => pipe(
+  groupId,
+  dependencies.getGroup,
+  O.orElse(() => {
+    dependencies.logger('error', 'Group missing from readmodel', { groupId });
+    return O.none;
+  }),
+);
 
 export const constructRelatedGroups = (dependencies: Dependencies) => (articleIds: ReadonlyArray<Doi>): ArticlesCategoryViewModel['relatedGroups'] => pipe(
   articleIds,
   RA.flatMap(dependencies.getEvaluationsForDoi),
   RA.map((recordedEvaluation) => recordedEvaluation.groupId),
   RA.uniq(GID.eq),
-  RA.map(dependencies.getGroup),
+  RA.map(getGroup(dependencies)),
   RA.compact,
   RA.matchW(
     () => ({ tag: 'no-groups-evaluated-the-found-articles' as const }),
