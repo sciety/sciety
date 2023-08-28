258e7936e25bcd7b4e776a3d3ab5ce87b67c464e
Kevin Rutherford <kevin@rutherford-software.com>
2023-08-28 10:43:37 +0100

    Use modern terminology for dependencies #2496

--- src/sagas/ensure-evaluations-are-listed/ensure-evaluations-are-listed.ts
+++ src/sagas/ensure-evaluations-are-listed/ensure-evaluations-are-listed.ts
@@ -3,15 +3,15 @@ import { pipe } from 'fp-ts/function';
 import { Queries } from '../../shared-read-models';
 import { Logger } from '../../shared-ports';
 
-type Ports = Queries & {
+type Dependencies = Queries & {
   logger: Logger,
 };
 
-export const ensureEvaluationsAreListed = async (adapters: Ports): Promise<void> => {
-  adapters.logger('info', 'ensureEvaluationsAreListed starting');
+export const ensureEvaluationsAreListed = async (dependencies: Dependencies): Promise<void> => {
+  dependencies.logger('info', 'ensureEvaluationsAreListed starting');
   await pipe(
     [],
     T.of,
   )();
-  adapters.logger('info', 'ensureEvaluationsAreListed finished');
+  dependencies.logger('info', 'ensureEvaluationsAreListed finished');
 };
