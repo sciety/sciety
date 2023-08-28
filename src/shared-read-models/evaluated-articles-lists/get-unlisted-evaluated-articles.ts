169ff0093a660515d2249fa40197238c12ac27d7
Kevin Rutherford <kevin@rutherford-software.com>
2023-08-28 09:43:13 +0100

    Return command pairs from the readmodel query #2496

--- src/shared-read-models/evaluated-articles-lists/get-unlisted-evaluated-articles.ts
+++ src/shared-read-models/evaluated-articles-lists/get-unlisted-evaluated-articles.ts
@@ -4,22 +4,37 @@ import * as S from 'fp-ts/string';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as RM from 'fp-ts/ReadonlyMap';
 import { ArticleState, ReadModel } from './handle-event';
+import { ListId } from '../../types/list-id';
 
 const hasBeenEvaluated = (a: ArticleState): boolean => a.evaluatedBy.length > 0;
 
-const articleIsNotListed = (groups: ReadModel['groups']) => (a: ArticleState): boolean => pipe(
+const missingLists = (groups: ReadModel['groups']) => (a: ArticleState) => pipe(
   a.evaluatedBy,
   RA.filter((groupId) => groups.has(groupId)),
   RA.map((groupId) => groups.get(groupId)),
   RA.map(O.fromNullable),
   RA.compact,
   RA.filter((listId) => !a.listedIn.includes(listId)),
-  RA.isNonEmpty,
 );
 
-export const getUnlistedEvaluatedArticles = (readmodel: ReadModel) => (): ReadonlyArray<string> => pipe(
+type MissingArticle = {
+  articleId: string,
+  listId: ListId,
+};
+
+const toPairs = (articleId: string, listIds: ReadonlyArray<ListId>): ReadonlyArray<MissingArticle> => pipe(
+  listIds,
+  RA.map((listId) => ({
+    articleId,
+    listId,
+  })),
+);
+
+export const getUnlistedEvaluatedArticles = (readmodel: ReadModel) => (): ReadonlyArray<MissingArticle> => pipe(
   readmodel.articles,
   RM.filter(hasBeenEvaluated),
-  RM.filter(articleIsNotListed(readmodel.groups)),
-  RM.keys(S.Ord),
+  RM.map(missingLists(readmodel.groups)),
+  RM.filter((lists) => lists.length > 0),
+  RM.collect(S.Ord)(toPairs),
+  RA.flatten,
 );
