24853848dc700f4d9a7c834cec7c16d9b5d1cbce
Kevin Rutherford <kevin@rutherford-software.com>
2023-08-31 13:54:04 +0100

    Stop using a deprecated command helper in one more place #2582

--- test/sagas/ensure-evaluations-are-listed/ensure-evaluations-are-listed.test.ts
+++ test/sagas/ensure-evaluations-are-listed/ensure-evaluations-are-listed.test.ts
@@ -1,9 +1,9 @@
 import { ensureEvaluationsAreListed } from '../../../src/sagas/ensure-evaluations-are-listed/ensure-evaluations-are-listed';
 import { dummyLogger } from '../../dummy-logger';
 import { TestFramework, createTestFramework } from '../../framework';
-import { arbitraryRecordedEvaluation } from '../../types/recorded-evaluation.helper';
 import * as LOID from '../../../src/types/list-owner-id';
 import { arbitraryAddGroupCommand } from '../../write-side/commands/add-group-command.helper';
+import { arbitraryRecordEvaluationPublicationCommand } from '../../write-side/commands/record-evaluation-publication-command.helper';
 
 describe('ensure-evaluations-are-listed', () => {
   let framework: TestFramework;
@@ -14,15 +14,15 @@ describe('ensure-evaluations-are-listed', () => {
 
   describe('when there are listed evaluations', () => {
     const addGroupCommand = arbitraryAddGroupCommand();
-    const evaluation = {
-      ...arbitraryRecordedEvaluation(),
+    const recordEvaluationPublicationCommand = {
+      ...arbitraryRecordEvaluationPublicationCommand(),
       groupId: addGroupCommand.groupId,
     };
     let listedArticleIds: ReadonlyArray<string>;
 
     beforeEach(async () => {
       await framework.commandHelpers.addGroup(addGroupCommand);
-      await framework.commandHelpers.deprecatedRecordEvaluation(evaluation);
+      await framework.commandHelpers.recordEvaluationPublication(recordEvaluationPublicationCommand);
 
       await ensureEvaluationsAreListed({
         ...framework.queries,
@@ -36,7 +36,7 @@ describe('ensure-evaluations-are-listed', () => {
     });
 
     it('adds the article to the appropriate list', () => {
-      expect(listedArticleIds).toContain(evaluation.articleId.value);
+      expect(listedArticleIds).toContain(recordEvaluationPublicationCommand.articleId.value);
     });
   });
 });
