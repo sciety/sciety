79cd2918a55fe1d26fc47d6d05cba77435931cb4
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-09-01 14:59:36 +0100

    Inline another function to see similarity #2589

--- src/html-pages/search-results-page/construct-view-model/construct-view-model.ts
+++ src/html-pages/search-results-page/construct-view-model/construct-view-model.ts
@@ -1,9 +1,9 @@
 import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
+import { pipe, tupled } from 'fp-ts/function';
 import * as DE from '../../../types/data-error';
 import { fetchExtraDetails } from './fetch-extra-details';
-import { Params, performSearch } from './perform-search';
+import { Params } from './perform-search';
 import { ViewModel } from '../view-model';
 import { Dependencies } from './dependencies';
 
@@ -11,8 +11,15 @@ export const constructViewModel = (
   dependencies: Dependencies,
   pageSize: number,
 ) => (params: Params): TE.TaskEither<DE.DataError, ViewModel> => pipe(
-  params,
-  performSearch(dependencies)(pageSize),
+  [
+    params.query,
+    params.cursor,
+    pipe(
+      params.evaluatedOnly,
+      O.isSome,
+    ),
+  ],
+  tupled(dependencies.searchForArticles(pageSize)),
   TE.map((articles) => ({
     evaluatedOnly: pipe(
       params.evaluatedOnly,
--- src/html-pages/search-results-page/construct-view-model/perform-search.ts
+++ src/html-pages/search-results-page/construct-view-model/perform-search.ts
@@ -1,11 +1,5 @@
-import * as O from 'fp-ts/Option';
-import * as TE from 'fp-ts/TaskEither';
-import { pipe, tupled } from 'fp-ts/function';
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
-import * as DE from '../../../types/data-error';
-import { Dependencies } from './dependencies';
-import { SearchResults } from '../../../shared-ports/search-for-articles';
 
 export const paramsCodec = t.type({
   query: t.string,
@@ -15,19 +9,3 @@ export const paramsCodec = t.type({
 });
 
 export type Params = t.TypeOf<typeof paramsCodec>;
-
-type PerformSearch = (
-  dependencies: Dependencies,
-) => (pageSize: number) => (params: Params) => TE.TaskEither<DE.DataError, SearchResults>;
-
-export const performSearch: PerformSearch = (dependencies) => (pageSize) => (params) => pipe(
-  [
-    params.query,
-    params.cursor,
-    pipe(
-      params.evaluatedOnly,
-      O.isSome,
-    ),
-  ],
-  tupled(dependencies.searchForArticles(pageSize)),
-);
