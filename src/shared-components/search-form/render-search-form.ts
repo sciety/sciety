530635506872c9a52a770e5fbf184b0d09206118
Giorgio Sironi <g.sironi@elifesciences.org>
2023-09-01 11:02:16 +0100

    Clarify what the user can search for #2589

--- src/shared-components/search-form/render-search-form.ts
+++ src/shared-components/search-form/render-search-form.ts
@@ -4,10 +4,9 @@ import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 export const renderSearchForm = (query: string, evaluatedOnly: boolean): HtmlFragment => toHtmlFragment(`
   <form action="/search" method="get" class="search-form">
     <input type="hidden" name="category" value="articles">
-    <label for="searchText" class="search-form__text_input_label">Find preprints and evaluating groups</label>
+    <label for="searchText" class="search-form__text_input_label">Find preprints</label>
     <div class="search-form__helper_text">
-      Search articles by DOI, author or keyword;
-      search groups by keyword.
+      Search articles by DOI, author or keyword.
     </div>
     <div class="search-form__positioning_context">
       ${htmlEscape`<input value="${query}" id="searchText" name="query" class="search-form__text">`}
