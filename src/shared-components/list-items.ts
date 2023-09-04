8d24ce2acfb50a7874bcef7e0f7cb9ef1f302151
Giorgio Sironi <g.sironi@elifesciences.org>
2023-09-04 14:38:42 +0100

    Stop using a non-existent CSS class #2590

--- src/shared-components/list-items.ts
+++ src/shared-components/list-items.ts
@@ -1,8 +1,8 @@
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
-export const templateListItems = (items: ReadonlyArray<HtmlFragment>, itemClass = 'item'): HtmlFragment => (
+export const templateListItems = (items: ReadonlyArray<HtmlFragment>, itemClass?: string): HtmlFragment => (
   toHtmlFragment(
-    items.map((item: HtmlFragment) => `<li class="${itemClass}" role="listitem">${item}</li>\n`)
+    items.map((item: HtmlFragment) => `<li${itemClass !== undefined ? ` class="${itemClass}` : ''} role="listitem">${item}</li>\n`)
       .join(''),
   )
 );
