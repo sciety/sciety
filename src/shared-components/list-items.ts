7b119b18abb47fd6cebdbe54ece9dc27b57a597a
LinaKind <l.kinduryte@elifesciences.org>
2023-09-04 14:47:53 +0100

    Refactor to fp-ts usage #2590

--- src/shared-components/list-items.ts
+++ src/shared-components/list-items.ts
@@ -1,10 +1,12 @@
+import { pipe } from 'fp-ts/function';
+import * as RA from 'fp-ts/ReadonlyArray';
 import { HtmlFragment, toHtmlFragment } from '../types/html-fragment';
 
 const renderClassAttribute = (itemClass: string | undefined) => (itemClass !== undefined ? ` class="${itemClass}` : '');
 
-export const templateListItems = (items: ReadonlyArray<HtmlFragment>, itemClass?: string): HtmlFragment => (
-  toHtmlFragment(
-    items.map((item: HtmlFragment) => `<li${renderClassAttribute(itemClass)} role="listitem">${item}</li>\n`)
-      .join(''),
-  )
+export const templateListItems = (items: ReadonlyArray<HtmlFragment>, itemClass?: string): HtmlFragment => pipe(
+  items,
+  RA.map((item: HtmlFragment) => `<li${renderClassAttribute(itemClass)} role="listitem">${item}</li>\n`),
+  (listItems) => listItems.join(''),
+  toHtmlFragment,
 );
