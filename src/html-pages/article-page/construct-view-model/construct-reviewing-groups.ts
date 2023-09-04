250daaa5841c6cf2496293c63f0d241589e620d9
LinaKind <l.kinduryte@elifesciences.org>
2023-09-04 11:28:15 +0100

    Display a group name for each evaluation on each article #2590

--- src/html-pages/article-page/construct-view-model/construct-reviewing-groups.ts
+++ src/html-pages/article-page/construct-view-model/construct-reviewing-groups.ts
@@ -1,3 +1,5 @@
+import { pipe } from 'fp-ts/function';
+import * as RA from 'fp-ts/ReadonlyArray';
 import { Doi } from '../../../types/doi';
 import { ViewModel } from '../view-model';
 import { Dependencies } from './dependencies';
@@ -7,4 +9,11 @@ export const constructReviewingGroups = (
   dependencies: Dependencies,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   articleId: Doi,
-): ViewModel['reviewingGroups'] => ['eLife'];
+): ViewModel['reviewingGroups'] => pipe(
+  articleId,
+  dependencies.getEvaluationsForDoi,
+  RA.map((evaluation) => evaluation.groupId),
+  RA.map((groupId) => dependencies.getGroup(groupId)),
+  RA.compact,
+  RA.map((group) => group.name),
+);
