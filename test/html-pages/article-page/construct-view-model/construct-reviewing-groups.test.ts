868fe9436a6830b76ff9dde73179d8875c84c9f4
David Moulton <d.moulton@elifesciences.org>
2023-09-04 11:05:01 +0100

    Write a failing test #2590

--- test/html-pages/article-page/construct-view-model/construct-reviewing-groups.test.ts
+++ test/html-pages/article-page/construct-view-model/construct-reviewing-groups.test.ts
@@ -1,6 +1,42 @@
+import { arbitraryString } from '../../../helpers';
+import { createTestFramework, TestFramework } from '../../../framework';
+import { ViewModel } from '../../../../src/html-pages/article-page/view-model';
+import { arbitraryAddGroupCommand } from '../../../write-side/commands/add-group-command.helper';
+import {
+  arbitraryRecordEvaluationPublicationCommand,
+} from '../../../write-side/commands/record-evaluation-publication-command.helper';
+import { arbitraryArticleId } from '../../../types/article-id.helper';
+import {
+  constructReviewingGroups,
+} from '../../../../src/html-pages/article-page/construct-view-model/construct-reviewing-groups';
+
 describe('construct-reviewing-groups', () => {
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
   describe('when an article has been reviewed by one group once', () => {
-    it.todo('returns an array containing the reviewing group\'s name once');
+    const reviewingGroupName = arbitraryString();
+    const reviewedArticle = arbitraryArticleId();
+    let result: ViewModel['reviewingGroups'];
+
+    beforeEach(async () => {
+      await framework.commandHelpers.addGroup({
+        ...arbitraryAddGroupCommand(),
+        name: reviewingGroupName,
+      });
+      await framework.commandHelpers.recordEvaluationPublication({
+        ...arbitraryRecordEvaluationPublicationCommand(),
+        articleId: reviewedArticle,
+      });
+      result = constructReviewingGroups(framework.dependenciesForViews, reviewedArticle);
+    });
+
+    it.skip('returns an array containing the reviewing group\'s name once', () => {
+      expect(result).toStrictEqual([reviewingGroupName]);
+    });
   });
 
   describe('when an article has been reviewed by one group twice', () => {
