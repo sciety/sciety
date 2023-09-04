5dbc25ffaaaca8cc4ceb52f573a849ecc079aefb
Giorgio Sironi <g.sironi@elifesciences.org>
2023-09-04 10:30:57 +0100

    Allow rendering of more than one reviewing group for an article #2589

--- src/html-pages/article-page/render-as-html/render-page.ts
+++ src/html-pages/article-page/render-as-html/render-page.ts
@@ -1,3 +1,5 @@
+import { pipe } from 'fp-ts/function';
+import * as RA from 'fp-ts/ReadonlyArray';
 import { renderFeed } from './render-feed';
 import { renderSaveArticle } from './render-save-article';
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
@@ -7,22 +9,37 @@ import { renderRelatedArticles } from './render-related-articles';
 import { renderLangAttribute } from '../../../shared-components/lang-attribute';
 import { renderHeader } from './render-header';
 import { renderRelatedArticlesLink } from './render-related-articles-link';
+import { templateListItems } from '../../../shared-components/list-items';
 
-const renderReviewingGroups = () => (process.env.EXPERIMENT_ENABLED === 'true' ? `
-  <section>
-    <h2 class="article-actions-heading">Reviewed by</h2>
-    <ul role="list" class="article-actions-reviewing-groups">
-      <li role="listitem">eLife</li>
-    </ul>
-  </section>
-` : '');
+const renderReviewingGroups = (reviewingGroups: ReadonlyArray<string>) => {
+  if (process.env.EXPERIMENT_ENABLED === 'true') {
+    if (reviewingGroups.length === 0) {
+      return toHtmlFragment('');
+    }
+    return pipe(
+      reviewingGroups,
+      RA.map(toHtmlFragment),
+      (listItems) => templateListItems(listItems),
+      (listItems) => `
+        <section>
+          <h2 class="article-actions-heading">Reviewed by</h2>
+          <ul role="list" class="article-actions-reviewing-groups">
+            ${listItems}
+          </ul>
+        </section>
+      `,
+      toHtmlFragment,
+    );
+  }
+  return '';
+};
 
 export const renderPage = (viewmodel: ViewModel): HtmlFragment => toHtmlFragment(`
   <div class="article-page-wrapper">
     ${renderHeader(viewmodel)}
     <div class="sciety-grid-two-columns">
       <section class="article-actions">
-        ${renderReviewingGroups()}
+        ${renderReviewingGroups(['eLife'])}
         <a href="${viewmodel.fullArticleUrl}" class="full-article-button">Read the full article</a>
         ${renderRelatedArticlesLink(viewmodel.relatedArticles)}
         <div class="list-management">
