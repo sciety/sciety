c118280df50c3d153dbb67bcaaf9ac772579bd9b
Giorgio Sironi <g.sironi@elifesciences.org>
2023-09-04 10:53:44 +0100

    Define a test list to describe intent #2590

---
+++ test/html-pages/article-page/construct-view-model/construct-reviewing-groups.test.ts
@@ -0,0 +1,21 @@
+describe('construct-reviewing-groups', () => {
+  describe('when an article has been reviewed by one group once', () => {
+    it.todo('returns an array containing the reviewing group\'s name once');
+  });
+
+  describe('when an article has been reviewed by one group twice', () => {
+    it.todo('returns an array containing the reviewing group\'s name once');
+  });
+
+  describe('when an article has been reviewed by more than one group', () => {
+    it.todo('returns an array containing all the reviewing groups\' names once');
+  });
+
+  describe('when an article has not been reviewed by any groups', () => {
+    it.todo('returns an empty array');
+  });
+
+  describe('when an article has a curation statement by a group', () => {
+    it.todo('the group\'s name is not in the array returned');
+  });
+});
