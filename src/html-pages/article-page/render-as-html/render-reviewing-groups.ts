303693bc617afb8038d3bb5e033250e91df0e153
LinaKind <l.kinduryte@elifesciences.org>
2023-09-05 14:44:19 +0100

    Create a reviewing group logo renderer on the article page #2600

--- src/html-pages/article-page/render-as-html/render-reviewing-groups.ts
+++ src/html-pages/article-page/render-as-html/render-reviewing-groups.ts
@@ -1,11 +1,20 @@
 import { pipe } from 'fp-ts/function';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as O from 'fp-ts/Option';
 import { ViewModel } from '../view-model';
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 import { renderListItems } from '../../../shared-components/render-list-items';
 
+const renderGroupLogoWithTextFallback = (largeLogo: O.Option<string>, name: string) => pipe(
+  largeLogo,
+  O.fold(
+    () => name,
+    (logoPath) => `<img src="${logoPath}"alt="${name}">`,
+  ),
+);
+
 const createGroupLink = (group: ViewModel['reviewingGroups'][number]) => `
-<a href="${group.href}">${group.name}</a>
+<a href="${group.href}">${renderGroupLogoWithTextFallback(O.none, group.name)}</a>
 `;
 
 export const renderReviewingGroups = (reviewingGroups: ViewModel['reviewingGroups']): HtmlFragment => {
