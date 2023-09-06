9bea9449ba109ea5cd1ce048712565c8655939a2
Giorgio Sironi <g.sironi@elifesciences.org>
2023-09-06 14:59:05 +0100

    Refactor to improve symmetry and readability #2600

--- src/shared-components/group-link-with-logo/render-group-link-with-logo.ts
+++ src/shared-components/group-link-with-logo/render-group-link-with-logo.ts
@@ -2,11 +2,11 @@ import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 
-const renderGroupLogoWithTextFallback = (largeLogo: O.Option<string>, name: string) => pipe(
-  largeLogo,
+const renderGroupLogoWithTextFallback = (viewModel: GroupLinkWithLogoViewModel) => pipe(
+  viewModel.logoPath,
   O.fold(
-    () => name,
-    (logoPath) => `<img src="${logoPath}"alt="${name}" class="group-link-with-logo__logo">`,
+    () => viewModel.groupName,
+    (logoPath) => `<img src="${logoPath}"alt="${viewModel.groupName}" class="group-link-with-logo__logo">`,
   ),
 );
 
@@ -16,12 +16,20 @@ export type GroupLinkWithLogoViewModel = {
   logoPath: O.Option<string>,
 };
 
-const renderLinkClass = (logoHorizontalAlignment: 'left' | 'center') => (
-  logoHorizontalAlignment === 'center'
-    ? 'class="group-link-with-logo group-link-with-logo--center-align"'
-    : 'class="group-link-with-logo group-link-with-logo--left-align"'
-);
+const renderAlignmentClass = (logoHorizontalAlignment: 'left' | 'center') => {
+  switch (logoHorizontalAlignment) {
+    case 'left':
+      return 'group-link-with-logo--left-align';
+    case 'center':
+      return 'group-link-with-logo--center-align';
+  }
+};
 
-export const renderGroupLinkWithLogo = (logoHorizontalAlignment: 'left' | 'center') => (viewModel: GroupLinkWithLogoViewModel): HtmlFragment => toHtmlFragment(`
-  <a href="${viewModel.href}" ${renderLinkClass(logoHorizontalAlignment)}>${renderGroupLogoWithTextFallback(viewModel.logoPath, viewModel.groupName)}</a>
-`);
+export const renderGroupLinkWithLogo = (logoHorizontalAlignment: 'left' | 'center') => (viewModel: GroupLinkWithLogoViewModel): HtmlFragment => pipe(
+  viewModel,
+  renderGroupLogoWithTextFallback,
+  (logoWithTextFallback) => `
+    <a href="${viewModel.href}" class="group-link-with-logo ${renderAlignmentClass(logoHorizontalAlignment)}">${logoWithTextFallback}</a>
+  `,
+  toHtmlFragment,
+);
