83e6f9252a6e2756f62f8a439052878077767ca6
Giorgio Sironi <g.sironi@elifesciences.org>
2023-09-06 15:26:47 +0100

    More closely align the construction of two different view models in preparation for extraction #2600

--- src/html-pages/article-page/construct-view-model/construct-reviewing-groups.ts
+++ src/html-pages/article-page/construct-view-model/construct-reviewing-groups.ts
@@ -5,11 +5,21 @@ import { RecordedEvaluation } from '../../../types/recorded-evaluation';
 import { Doi } from '../../../types/doi';
 import { ViewModel } from '../view-model';
 import { Dependencies } from './dependencies';
+import * as GID from '../../../types/group-id';
 
 const isNotCurationStatement = (evaluation: RecordedEvaluation) => pipe(
   evaluation.type,
   O.getOrElseW(() => undefined),
 ) !== 'curation-statement';
+
+const getGroup = (dependencies: Dependencies) => (groupId: GID.GroupId) => pipe(
+  groupId,
+  dependencies.getGroup,
+  O.orElse(() => {
+    dependencies.logger('error', 'Group missing from readmodel', { groupId });
+    return O.none;
+  }),
+);
 
 const unique = <A>(input: ReadonlyArray<A>) => [...new Set(input)];
 
@@ -24,7 +34,7 @@ export const constructReviewingGroups = (
   unique,
   RA.map((groupId) => pipe(
     groupId,
-    dependencies.getGroup,
+    getGroup(dependencies),
     O.map((group) => ({ groupName: group.name, href: `/groups/${group.slug}`, logoPath: group.largeLogoPath })),
   )),
   RA.compact,
