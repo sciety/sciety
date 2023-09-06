793613f6d8688f53dd12727f5b1a70a5e48e586d
David Moulton <d.moulton@elifesciences.org>
2023-09-06 10:40:17 +0100

    Reuse a shared component to reduce code duplication #2600

--- src/html-pages/search-results-page/render-as-html/render-related-groups.ts
+++ src/html-pages/search-results-page/render-as-html/render-related-groups.ts
@@ -1,25 +1,15 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import * as O from 'fp-ts/Option';
 import { ViewModel } from '../view-model';
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
-
-const renderLargeLogoLink = (relatedGroup: (ViewModel['relatedGroups'] & { tag: 'some-related-groups' })['items'][number]) => pipe(
-  relatedGroup.largeLogoUrl,
-  O.match(
-    () => '',
-    (largeLogoUrl) => `<a href="${relatedGroup.groupPageHref}" class="related-groups__link">
-    <img src="${largeLogoUrl}" alt="${relatedGroup.groupName}">
-    </a>`,
-  ),
-);
+import { renderGroupLinkWithLogo } from '../../../shared-components/group-link-with-logo';
 
 export const renderRelatedGroups = (relatedGroups: ViewModel['relatedGroups']): HtmlFragment => (relatedGroups.tag === 'some-related-groups'
   ? pipe(
     relatedGroups.items,
     RA.map((relatedGroup) => `
       <li role="listitem">
-       ${renderLargeLogoLink(relatedGroup)}
+       ${renderGroupLinkWithLogo({ name: relatedGroup.groupName, href: relatedGroup.groupPageHref, largeLogo: relatedGroup.largeLogoUrl })}
       </li>
     `),
     (items) => items.join(''),
