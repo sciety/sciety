d13e3d06906a0762e66d118c21ab9cc3780c2192
LinaKind <l.kinduryte@elifesciences.org>
2023-09-12 13:50:32 +0100

    Completely hide illustrations from screen readers #2617

--- src/html-pages/home-page/render-as-html/render-value-statements.ts
+++ src/html-pages/home-page/render-as-html/render-value-statements.ts
@@ -9,21 +9,21 @@ export const renderValueStatements = toHtmlFragment(`
                 <h3 class="home-page-value-statement__heading">Explore preprints, reviews and recommendations</h3>
                 <p>Sciety brings together groups of researchers who are evaluating and curating the preprints they find interesting or important in a central place.</p>
             </div>
-            <img src="/static/images/home-page/value-statements/illustration-1.svg" alt="placeholder image" class="home-page-value-statement__image">
+            <img src="/static/images/home-page/value-statements/illustration-1.svg" alt="" class="home-page-value-statement__image">
         </article>
         <article class="home-page-value-statement home-page-value-statement--reverse-order">
             <div class="home-page-value-statement__copy">
                 <h3 class="home-page-value-statement__heading">Discover the next advancement in your field</h3>
                 <p>Search and sort through the vast preprint literature and find relevant and impactful studies in your area of expertise, alongside evaluations from trusted experts.</p>
             </div>
-            <img src="/static/images/home-page/value-statements/illustration-2.svg" alt="placeholder image" class="home-page-value-statement__image">
+            <img src="/static/images/home-page/value-statements/illustration-2.svg" alt="" class="home-page-value-statement__image">
         </article>
         <article class="home-page-value-statement">
             <div class="home-page-value-statement__copy">
                 <h3 class="home-page-value-statement__heading">Organise and share what you are reading</h3>
                 <p>Create lists of the preprints you find interesting that you can return to in the future or share with your research community.</p>
             </div>
-            <img src="/static/images/home-page/value-statements/illustration-3.svg" alt="placeholder image" class="home-page-value-statement__image">
+            <img src="/static/images/home-page/value-statements/illustration-3.svg" alt="" class="home-page-value-statement__image">
         </article>
         </div>
     </section>
