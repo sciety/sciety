8314a38b545ae914683fc9e34f4f8f135e868985
LinaKind <l.kinduryte@elifesciences.org>
2023-09-13 13:16:39 +0100

    Inline dependencies to prepare change of file location #2618

--- src/html-pages/article-page/construct-view-model/construct-reviewing-groups.ts
+++ src/html-pages/article-page/construct-view-model/construct-reviewing-groups.ts
@@ -1,11 +1,14 @@
 import { pipe } from 'fp-ts/function';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as O from 'fp-ts/Option';
-import { constructGroupLinkWithLogo } from '../../../shared-components/group-link-with-logo/construct-group-link-with-logo';
+import { constructGroupLinkWithLogo, ConstructGroupLinkWithLogoDependencies } from '../../../shared-components/group-link-with-logo/construct-group-link-with-logo';
 import { RecordedEvaluation } from '../../../types/recorded-evaluation';
 import { Doi } from '../../../types/doi';
 import { ViewModel } from '../view-model';
-import { Dependencies } from './dependencies';
+import { Queries } from '../../../read-models';
+
+type Dependencies = Queries
+& ConstructGroupLinkWithLogoDependencies;
 
 const isNotCurationStatement = (evaluation: RecordedEvaluation) => pipe(
   evaluation.type,
