d151cbf4738cf674e36d457a045908e4f7895109
Giorgio Sironi <g.sironi@elifesciences.org>
2023-09-13 13:56:08 +0100

    Surface separate renderer #2618

--- src/shared-components/article-card/render-reviewing-groups-with-link.ts
+++ src/shared-components/article-card/render-reviewing-groups-with-link.ts
@@ -3,13 +3,15 @@ import * as RA from 'fp-ts/ReadonlyArray';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 import { ViewModel } from './view-model';
 
+const renderGroupLinkAsText = (group: ViewModel['reviewingGroups'][number]) => `<a href="${group.groupPageHref}">${group.groupName}</a>`;
+
 export const renderReviewingGroupsWithLink = (reviewingGroups: ViewModel['reviewingGroups']): HtmlFragment => {
   if (reviewingGroups.length === 0) {
     return toHtmlFragment('');
   }
   return pipe(
     reviewingGroups,
-    RA.map((group) => `<a href="${group.groupPageHref}">${group.groupName}</a>`),
+    RA.map(renderGroupLinkAsText),
     (links) => links.join(', '),
     (groupNamesWithLinks) => `<p class="article-card__reviewing_groups">Reviewed by ${groupNamesWithLinks}</p>`,
     toHtmlFragment,
