3577a1e49657b3180e8167d5734ed0bf4fbbb8b4
LinaKind <l.kinduryte@elifesciences.org>
2023-09-14 11:44:30 +0100

    Extract inline styles to a sass file #2619

--- src/html-pages/about-page/content.ts
+++ src/html-pages/about-page/content.ts
@@ -17,21 +17,21 @@ export const content = `
 
 <p>As more and more research is posted as preprints it can be difficult to trust the accuracy of the claims in the research without peer review, or narrow them down to the most interesting or impactful.</p>
 
-<figure>
-<img src="/static/images/about-page/preprints-in-europe-pmc.png" style="width: 100%" alt="graph of preprints in Europe PMC showing massive growth">
-<figcaption style="text-align: center">Preprints in Europe PMC</figcaption>
+<figure class="about-page-figure">
+<img src="/static/images/about-page/preprints-in-europe-pmc.png" alt="graph of preprints in Europe PMC showing massive growth">
+<figcaption>Preprints in Europe PMC</figcaption>
 </figure>
 
 <p>Societies, journals and other groups of scientists are helping by highlighting the preprints that they think are the most important and providing public review and curation to share with their peers. Sciety brings the review and curation of preprints from multiple groups into one place to make them easier to find.</p>
 
-<figure>
-<img src="/static/images/about-page/number-of-evaluations-on-sciety.png" style="width: 100%" alt="graph showing increasing amount of evaluations on Sciety">
-<figcaption style="text-align: center">Number of evaluations on Sciety</figcaption>
+<figure class="about-page-figure">
+<img src="/static/images/about-page/number-of-evaluations-on-sciety.png" alt="graph showing increasing amount of evaluations on Sciety">
+<figcaption>Number of evaluations on Sciety</figcaption>
 </figure>
 
-<figure>
-<img src="/static/images/about-page/number-of-evaluated-articles-on-sciety.png" style="width: 100%" alt="graph showing increasing amount of evaluated articles on Sciety">
-<figcaption style="text-align: center">Number of evaluated articles on Sciety</figcaption>
+<figure class="about-page-figure">
+<img src="/static/images/about-page/number-of-evaluated-articles-on-sciety.png" alt="graph showing increasing amount of evaluated articles on Sciety">
+<figcaption>Number of evaluated articles on Sciety</figcaption>
 </figure>
 
 <p>Sciety also enables scientists to share the evaluated preprints that they think are interesting or relevant to their work, which in turn helps their fellow researchers discover the latest and best new research.</p>
