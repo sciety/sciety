468345d1fd89f29711b575437a918d13999f679f
LinaKind <l.kinduryte@elifesciences.org>
2023-09-18 14:45:28 +0100

    Write a test list for construct annotation #2639

---
+++ test/shared-components/article-card-with-controls-and-annotation/construct-annotation.test.ts
@@ -0,0 +1,11 @@
+describe('construct-annotation', () => {
+  describe('when there is no annotation', () => {
+    it.todo('returns none');
+  });
+
+  describe('when there is an annotation', () => {
+    it.todo('returns its content');
+
+    it.todo('returns its author');
+  });
+});
