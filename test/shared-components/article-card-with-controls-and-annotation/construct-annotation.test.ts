afedc0b7c8de58c190413c5d8ed33cd19df14da8
David Moulton <d.moulton@elifesciences.org>
2023-09-18 15:15:31 +0100

    Write a missing characterisation test #2639

--- test/shared-components/article-card-with-controls-and-annotation/construct-annotation.test.ts
+++ test/shared-components/article-card-with-controls-and-annotation/construct-annotation.test.ts
@@ -1,6 +1,20 @@
+import * as O from 'fp-ts/Option';
+
+import { createTestFramework } from '../../framework';
+import {
+  constructAnnotation,
+} from '../../../src/shared-components/article-card-with-controls-and-annotation/construct-annotation';
+import { arbitraryArticleId } from '../../types/article-id.helper';
+import { arbitraryListId } from '../../types/list-id.helper';
+
 describe('construct-annotation', () => {
   describe('when there is no annotation', () => {
-    it.todo('returns none');
+    it('returns none', () => {
+      const framework = createTestFramework();
+      const result = constructAnnotation(framework.dependenciesForViews)(arbitraryListId(), arbitraryArticleId());
+
+      expect(result).toStrictEqual(O.none);
+    });
   });
 
   describe('when there is an annotation', () => {
