8ce905108e7c8a6172d951b2688cb52c3b815997
LinaKind <l.kinduryte@elifesciences.org>
2023-09-19 13:50:37 +0100

    Reduce coupling by extracting fallback to a single place #2639

--- src/shared-components/article-card-with-controls-and-annotation/construct-annotation.ts
+++ src/shared-components/article-card-with-controls-and-annotation/construct-annotation.ts
@@ -1,42 +1,40 @@
 import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
+import { unknownAuthor } from './static-messages';
 import { ListId } from '../../types/list-id';
 import { Doi } from '../../types/doi';
 import { Queries } from '../../read-models';
 import { ArticleCardWithControlsAndAnnotationViewModel } from './article-card-with-controls-and-annotation-view-model';
-import { unknownAuthor } from './static-messages';
 
 const getAnnotationAuthorDisplayName = (dependencies: Queries, listId: ListId) => pipe(
   listId,
   dependencies.lookupList,
   O.map((list) => list.ownerId),
-  O.match(
-    () => unknownAuthor,
-    (ownerId) => {
-      switch (ownerId.tag) {
-        case 'group-id':
-          return pipe(
-            ownerId.value,
-            dependencies.getGroup,
-            O.map((group) => group.name),
-            O.getOrElse(() => unknownAuthor),
-          );
-        case 'user-id':
-          return pipe(
-            ownerId.value,
-            dependencies.lookupUser,
-            O.map((user) => user.displayName),
-            O.getOrElse(() => unknownAuthor),
-          );
-      }
-    },
-  ),
+  O.chain((ownerId) => {
+    switch (ownerId.tag) {
+      case 'group-id':
+        return pipe(
+          ownerId.value,
+          dependencies.getGroup,
+          O.map((group) => group.name),
+        );
+      case 'user-id':
+        return pipe(
+          ownerId.value,
+          dependencies.lookupUser,
+          O.map((user) => user.displayName),
+        );
+    }
+  }),
 );
 
 export const constructAnnotation = (dependencies: Queries) => (listId: ListId, articleId: Doi): ArticleCardWithControlsAndAnnotationViewModel['annotation'] => pipe(
   dependencies.getAnnotationContent(listId, articleId),
   O.map((content) => ({
     content,
-    author: getAnnotationAuthorDisplayName(dependencies, listId),
+    author: pipe(
+      getAnnotationAuthorDisplayName(dependencies, listId),
+      O.getOrElse(() => unknownAuthor),
+    ),
   })),
 );
