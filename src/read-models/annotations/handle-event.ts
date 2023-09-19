a54aaa4a1f7ba863178e8de20d09b960caf07a48
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-09-19 15:28:39 +0100

    Extract a function for code readability #2644

--- src/read-models/annotations/handle-event.ts
+++ src/read-models/annotations/handle-event.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign */
-import { DomainEvent, isEventOfType } from '../../domain-events';
+import { DomainEvent, EventOfType, isEventOfType } from '../../domain-events';
 import { HtmlFragment } from '../../types/html-fragment';
 import * as LID from '../../types/list-id';
 
@@ -12,16 +12,20 @@ export const initialState = (): ReadModel => ({});
 const targetListIdForAvasthiReadingUser = LID.fromValidatedString('1af5b971-162e-4cf3-abdf-57e3bbfcd0d7');
 const actualListIdForAvasthiReadingUser = LID.fromValidatedString('dcc7c864-6630-40e7-8eeb-9fb6f012e92b');
 
+const handleLegacyAnnotations = (readmodel: ReadModel, event: EventOfType<'AnnotationCreated'>) => {
+  if (event.target.listId === targetListIdForAvasthiReadingUser) {
+    const actualListState = readmodel[actualListIdForAvasthiReadingUser] ?? {};
+    actualListState[event.target.articleId.value] = event.content;
+    readmodel[actualListIdForAvasthiReadingUser] = actualListState;
+  }
+};
+
 export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel => {
   if (isEventOfType('AnnotationCreated')(event)) {
     const listState = readmodel[event.target.listId] ?? {};
     listState[event.target.articleId.value] = event.content;
     readmodel[event.target.listId] = listState;
-    if (event.target.listId === targetListIdForAvasthiReadingUser) {
-      const actualListState = readmodel[actualListIdForAvasthiReadingUser] ?? {};
-      actualListState[event.target.articleId.value] = event.content;
-      readmodel[actualListIdForAvasthiReadingUser] = actualListState;
-    }
+    handleLegacyAnnotations(readmodel, event);
   }
   return readmodel;
 };
