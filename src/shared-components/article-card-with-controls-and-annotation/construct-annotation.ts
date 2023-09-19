069d6d8994ad0bd10dccc5937a7d664987bdb1e3
David Moulton <d.moulton@elifesciences.org>
2023-09-19 11:02:02 +0100

    Extract a seam to understand behaviour requirements #2639

--- src/shared-components/article-card-with-controls-and-annotation/construct-annotation.ts
+++ src/shared-components/article-card-with-controls-and-annotation/construct-annotation.ts
@@ -5,10 +5,20 @@ import { Doi } from '../../types/doi';
 import { Queries } from '../../read-models';
 import { ArticleCardWithControlsAndAnnotationViewModel } from './article-card-with-controls-and-annotation-view-model';
 
+const getAnnotationAuthorDisplayName = (ports: Queries, listId: ListId) => pipe(
+  listId,
+  ports.lookupList,
+  O.map((list) => list.ownerId),
+  O.match(
+    () => '',
+    () => 'AvasthiReading',
+  ),
+);
+
 export const constructAnnotation = (ports: Queries) => (listId: ListId, articleId: Doi): ArticleCardWithControlsAndAnnotationViewModel['annotation'] => pipe(
   ports.getAnnotationContent(listId, articleId),
   O.map((content) => ({
     content,
-    author: 'AvasthiReading',
+    author: getAnnotationAuthorDisplayName(ports, listId),
   })),
 );
