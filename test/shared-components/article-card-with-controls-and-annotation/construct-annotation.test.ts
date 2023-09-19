0c4f18e5e55eca8dc7ce53cb4895f7c27cecc645
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-09-19 09:57:34 +0100

    Add a missing characterisation test #2639

--- test/shared-components/article-card-with-controls-and-annotation/construct-annotation.test.ts
+++ test/shared-components/article-card-with-controls-and-annotation/construct-annotation.test.ts
@@ -1,11 +1,15 @@
 import * as O from 'fp-ts/Option';
 
+import { pipe } from 'fp-ts/function';
 import { createTestFramework } from '../../framework';
 import {
   constructAnnotation,
 } from '../../../src/shared-components/article-card-with-controls-and-annotation/construct-annotation';
 import { arbitraryArticleId } from '../../types/article-id.helper';
 import { arbitraryListId } from '../../types/list-id.helper';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+import { arbitraryCreateListCommand } from '../../write-side/commands/create-list-command.helper';
+import { arbitraryHtmlFragment } from '../../helpers';
 
 describe('construct-annotation', () => {
   describe('when there is no annotation', () => {
@@ -18,7 +22,24 @@ describe('construct-annotation', () => {
   });
 
   describe('when there is an annotation', () => {
-    it.todo('returns its content');
+    it('returns its content', async () => {
+      const framework = createTestFramework();
+      const createListCommand = arbitraryCreateListCommand();
+      const articleId = arbitraryArticleId();
+      const content = arbitraryHtmlFragment();
+      await framework.commandHelpers.createList(createListCommand);
+      await framework.commandHelpers.addArticleToList(articleId, createListCommand.listId);
+      await framework.commandHelpers.createAnnotation({
+        content,
+        target: { listId: createListCommand.listId, articleId },
+      });
+      const result = pipe(
+        constructAnnotation(framework.dependenciesForViews)(createListCommand.listId, articleId),
+        O.getOrElseW(shouldNotBeCalled),
+      );
+
+      expect(result.content).toStrictEqual(content);
+    });
 
     it.todo('returns its author');
   });
