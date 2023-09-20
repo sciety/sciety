6feedbc4f3fb49e1dbe5444a2230778a4a4e4de2
David Moulton <d.moulton@elifesciences.org>
2023-09-20 16:03:33 +0100

    Restrict the ability to annotate an article on a list to the list owner

--- src/annotations/supply-form-submission-to.ts
+++ src/annotations/supply-form-submission-to.ts
@@ -6,29 +6,46 @@ import { Middleware } from 'koa';
 import bodyParser from 'koa-bodyparser';
 import compose from 'koa-compose';
 import { sequenceS } from 'fp-ts/Apply';
+import * as t from 'io-ts';
 import { redirectBack } from '../http/redirect-back';
 import { CommandResult } from '../types/command-result';
-import { userIdCodec } from '../types/user-id';
 import { getLoggedInScietyUser, Ports as GetLoggedInScietyUserPorts } from '../http/authentication-and-logging-in-of-sciety-users';
+import { htmlFragmentCodec } from '../types/html-fragment';
+import { DoiFromString } from '../types/codecs/DoiFromString';
+import * as LID from '../types/list-id';
+import { Queries } from '../read-models';
 
 type CommandHandler = (input: unknown) => TE.TaskEither<unknown, CommandResult>;
 
-const requireUserToBeAvasthiReading = (adapters: GetLoggedInScietyUserPorts): Middleware => async (context, next) => {
+const bodyCodec = t.type({
+  annotationContent: htmlFragmentCodec,
+  articleId: DoiFromString,
+  listId: LID.listIdCodec,
+});
+
+type Dependencies = Queries & GetLoggedInScietyUserPorts;
+
+const requireUserToOwnTheList = (adapters: Dependencies): Middleware => async (context, next) => {
   pipe(
     {
       loggedInUser: getLoggedInScietyUser(adapters, context),
-      avasthiReadingUserId: pipe(
-        '1412019815619911685',
-        userIdCodec.decode,
+      listOwnerId: pipe(
+        context.request.body,
+        bodyCodec.decode,
         O.fromEither,
+        O.map((body) => body.listId),
+        O.map(LID.fromValidatedString),
+        O.chain(adapters.lookupList),
+        O.chainNullableK((list) => list.ownerId.value),
       ),
     },
+    (foo) => foo,
     sequenceS(O.Apply),
-    O.filter(({ loggedInUser, avasthiReadingUserId }) => loggedInUser.id === avasthiReadingUserId),
+    O.filter(({ loggedInUser, listOwnerId }) => loggedInUser.id === listOwnerId),
     O.match(
       () => {
         context.response.status = StatusCodes.FORBIDDEN;
-        context.response.body = 'Only @AvasthiReading is allowed to annotate their list.';
+        context.response.body = 'Only the list owner is allowed to annotate their list.';
       },
       async () => { await next(); },
     ),
@@ -36,13 +53,13 @@ const requireUserToBeAvasthiReading = (adapters: GetLoggedInScietyUserPorts): Mi
 };
 
 type SupplyFormSubmissionTo = (
-  adapters: GetLoggedInScietyUserPorts,
+  adapters: Dependencies,
   handler: CommandHandler,
 ) => Middleware;
 
 export const supplyFormSubmissionTo: SupplyFormSubmissionTo = (adapters, handler) => compose([
   bodyParser({ enableTypes: ['form'] }),
-  requireUserToBeAvasthiReading(adapters),
+  requireUserToOwnTheList(adapters),
   async (context, next) => {
     await pipe(
       context.request.body,
