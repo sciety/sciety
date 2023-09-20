024b077bf0f0786c1f3540d70b299ea01ccf3d24
David Moulton <d.moulton@elifesciences.org>
2023-09-20 14:22:14 +0100

    Allow listId to be passed in the create annotation form

--- src/annotations/create-annotation-form-page.ts
+++ src/annotations/create-annotation-form-page.ts
@@ -9,6 +9,7 @@ import { RenderPageError } from '../types/render-page-error';
 
 export const paramsCodec = t.type({
   articleId: tt.optionFromNullable(t.string),
+  listId: tt.optionFromNullable(t.string),
 });
 
 const renderArticleIdInput = (articleId: O.Option<string>) => pipe(
@@ -21,15 +22,29 @@ const renderArticleIdInput = (articleId: O.Option<string>) => pipe(
 `,
 );
 
-type CreateAnnotationFormPage = (params: { articleId: O.Option<string> }) => TE.TaskEither<RenderPageError, Page>;
+const renderListIdInput = (listId: O.Option<string>) => pipe(
+  listId,
+  O.fold(
+    () => '',
+    (id) => id,
+  ),
+  (value) => `<input type="text" name="listId" id="listId" placeholder="" class="annotation-form-article-id" value="${value}">
+`,
+);
 
-export const createAnnotationFormPage: CreateAnnotationFormPage = ({ articleId }) => TE.right({
+type CreateAnnotationFormPage = (
+  params: { articleId: O.Option<string>, listId: O.Option<string> }
+) => TE.TaskEither<RenderPageError, Page>;
+
+export const createAnnotationFormPage: CreateAnnotationFormPage = ({ articleId, listId }) => TE.right({
   title: 'Create an annotation',
   content: toHtmlFragment(`
     <h1>Create an annotation for an article on <a href="https://sciety.org/lists/dcc7c864-6630-40e7-8eeb-9fb6f012e92b">Prachee's list</a></h1>
     <form method="POST" action="/annotations/create-annotation">
       <label for="annotationContent">Annotation content</label>
       <textarea id="annotationContent" name="annotationContent" rows="10" class="annotation-form-content"></textarea>
+      <label for="listId">List Id</label>
+      ${renderListIdInput(listId)}
       <label for="articleId">Article DOI</label>
       ${renderArticleIdInput(articleId)}
       <div class="annotation-form-controls">
