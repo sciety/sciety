c169bf8e3b70d59549c0cffe4b5a189363ca6b2b
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-09-21 08:46:13 +0100

    Introduce generic Prism for unions with 'tag' as discriminant

--- test/html-pages/search-results-page/construct-view-model/construct-related-groups.test.ts
+++ test/html-pages/search-results-page/construct-view-model/construct-related-groups.test.ts
@@ -8,6 +8,7 @@ import { Doi } from '../../../../src/types/doi';
 import { arbitraryRecordEvaluationPublicationCommand } from '../../../write-side/commands/record-evaluation-publication-command.helper';
 import { arbitraryAddGroupCommand } from '../../../write-side/commands/add-group-command.helper';
 import { constructRelatedGroups } from '../../../../src/html-pages/search-results-page/construct-view-model/construct-related-groups';
+import { GetTag } from '../../../type-optics';
 
 const isSomeRelatedGroups = (value: ViewModel['relatedGroups']): value is SomeRelatedGroups => value.tag === 'some-related-groups';
 
@@ -34,7 +35,7 @@ describe('construct-related-groups', () => {
     RA.map((item) => item.groupName),
   );
 
-  type GroupNames = ReadonlyArray<(ViewModel['relatedGroups'] & { tag: 'some-related-groups' })['items'][number]['groupName']>;
+  type GroupNames = ReadonlyArray<(GetTag<ViewModel['relatedGroups'], 'some-related-groups' >)['items'][number]['groupName']>;
 
   describe('when the results consist of one article evaluated once by two different groups', () => {
     const articleId = arbitraryDoi();
--- test/type-optics.ts
+++ test/type-optics.ts
@@ -1,1 +1,5 @@
 export type GetSome<T> = (T & { _tag: 'Some', value: unknown })['value'];
+
+type Tagged = Record<string, unknown> & { tag: string };
+
+export type GetTag<A extends Tagged, T extends A['tag']> = (A & { tag: T });
