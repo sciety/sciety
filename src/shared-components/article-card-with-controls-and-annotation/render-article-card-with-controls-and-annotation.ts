ff01c1305e5badc44513dafb5c6d039adc5c7657
David Moulton <d.moulton@elifesciences.org>
2023-09-26 14:43:51 +0100

    Make it clear to the user when they're about to remove an article from a list #2684

--- src/shared-components/article-card-with-controls-and-annotation/render-article-card-with-controls-and-annotation.ts
+++ src/shared-components/article-card-with-controls-and-annotation/render-article-card-with-controls-and-annotation.ts
@@ -13,11 +13,7 @@ const renderRemoveArticleForm = (articleId: Doi, listId: ListId) => pipe(
         <input type="hidden" name="articleid" value="${id}">
         <input type="hidden" name="listid" value="${listId}">
         <button aria-label="Remove this article from the list" class="saved-articles-control">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" class="saved-articles-control__icon">
-            <desc>Remove this article from the list</desc>
-            <path d="M0 0h24v24H0V0z" fill="none"/>
-            <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/>
-          </svg>
+        Remove article
         </button>
       </form>
   `,
