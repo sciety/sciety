f167b4419bc7254e2bdd9ee061ec2b736c35171a
Giorgio Sironi <g.sironi@elifesciences.org>
2023-09-26 13:34:14 +0100

    Sketch a feature test for creating annotations #2683

---
+++ feature-test/create-annotation.test.ts
@@ -0,0 +1,11 @@
+describe('create-annotation', () => {
+  describe('given I am logged in', () => {
+    describe('with an article on my list', () => {
+      describe('when I annotate the article', () => {
+        it.todo('i am back on the list page');
+
+        it.todo('the annotation is visible');
+      });
+    });
+  });
+});
