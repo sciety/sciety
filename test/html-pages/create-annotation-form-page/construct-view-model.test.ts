1d0ea5263c770a540975609d4bcea0c56fe80e95
LinaKind <l.kinduryte@elifesciences.org>
2023-09-27 10:04:50 +0100

    Write a test list for the form view model construction #2685

---
+++ test/html-pages/create-annotation-form-page/construct-view-model.test.ts
@@ -0,0 +1,17 @@
+describe('construct-view-model', () => {
+  describe('when the article title is available', () => {
+    it.todo('returns the article title');
+  });
+
+  describe('when the list title is available', () => {
+    it.todo('returns the list title');
+  });
+
+  describe('when the article title is not available', () => {
+    it.todo('returns on the left');
+  });
+
+  describe('when the list title is not available', () => {
+    it.todo('returns on the left');
+  });
+});
