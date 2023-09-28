adafd9a5618f3788d4f94331bfd5fd2f84c5dd27
Giorgio Sironi <g.sironi@elifesciences.org>
2023-09-28 13:29:36 +0100

    Partially revert accidental changes to renderer from "Ensure construction of view model fails correctly when list name is not available #2685"
    This reverts commit 91aa3877ceef2412759320895efaa7dc876e9447.

--- src/html-pages/create-annotation-form-page/render-page.ts
+++ src/html-pages/create-annotation-form-page/render-page.ts
@@ -5,8 +5,7 @@ export const listIdInputName = 'listId';
 export const articleIdInputName = 'articleId';
 
 export const renderPage = (viewModel: ViewModel): HtmlFragment => toHtmlFragment(`
-    <h1>Create an annotation</h1>
-    <p>Create an annotation for <a href="/articles/${viewModel.articleId.value}" class="annotation-form-article-title">${viewModel.articleTitle}</a> on <a href="/lists/${viewModel.listId}" class="annotation-form-list-name">${viewModel.listName}</a></p>
+    <h1>Create an annotation for <span class="annotation-form-article-title">${viewModel.articleTitle}</span> on <span class="annotation-form-list-name">${viewModel.listName}</span></h1>
     <form method="POST" action="/annotations/create-annotation">
       <label for="annotationContent">Annotation content</label>
       <textarea id="annotationContent" name="annotationContent" rows="10" class="annotation-form-content"></textarea>
