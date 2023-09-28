91aa3877ceef2412759320895efaa7dc876e9447
Giorgio Sironi <g.sironi@elifesciences.org>
2023-09-28 11:43:47 +0100

    Ensure construction of view model fails correctly when list name is not available #2685

--- src/html-pages/create-annotation-form-page/construct-view-model.ts
+++ src/html-pages/create-annotation-form-page/construct-view-model.ts
@@ -1,10 +1,14 @@
 import * as O from 'fp-ts/Option';
+import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { sequenceS } from 'fp-ts/Apply';
 import { Queries } from '../../read-models';
 import { ExternalQueries } from '../../third-parties/external-queries';
 import { ViewModel } from './view-model';
 import { DataError } from '../../types/data-error';
+import * as DE from '../../types/data-error';
 import { Doi } from '../../types/doi';
 import { ListId } from '../../types/list-id';
 import * as LID from '../../types/list-id';
@@ -21,6 +25,8 @@ const getListName = (dependencies: Dependencies, listId: ListId) => pipe(
   listId,
   dependencies.lookupList,
   O.map((list) => list.name),
+  E.fromOption(() => DE.notFound),
+  T.of,
 );
 
 export const constructViewModel = (
@@ -28,17 +34,14 @@ export const constructViewModel = (
   listId: string,
   dependencies: Dependencies,
 ): TE.TaskEither<DataError, ViewModel> => pipe(
-  getArticleTitle(dependencies, new Doi(articleId)),
-  TE.map((articleTitle) => ({
+  {
+    articleTitle: getArticleTitle(dependencies, new Doi(articleId)),
+    listName: getListName(dependencies, LID.fromValidatedString(listId)),
+  },
+  sequenceS(TE.ApplyPar),
+  TE.map((partial) => ({
+    ...partial,
     articleId,
     listId,
-    articleTitle,
-    listName: pipe(
-      getListName(dependencies, LID.fromValidatedString(listId)),
-      O.match(
-        () => '',
-        (listName) => listName,
-      ),
-    ),
   })),
 );
