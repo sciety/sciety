6f6eaa9e15030b2e297903f4d273ab595b53a2cb
David Moulton <d.moulton@elifesciences.org>
2023-10-02 10:53:12 +0100

    Experiment with a feature-flagged embedded annotation content form field #2699

--- src/write-side/save-article/render-save-to-list-form.ts
+++ src/write-side/save-article/render-save-to-list-form.ts
@@ -3,11 +3,18 @@ import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 import { ListId } from '../../types/list-id';
 import { articleIdFieldName } from './save-article-handler';
 
+const renderAnnotationContentTextarea = (listId: ListId) => (process.env.EXPERIMENT_ENABLED === 'true' ? `
+    <label for="annotationContent-${listId}">Annotation content</label>
+    <textarea id="annotationContent-${listId}" name="annotationContent"></textarea>
+`
+  : '');
+
 export const renderSaveToListForm = (doi: Doi, listId: ListId, listName: string): HtmlFragment => toHtmlFragment(`
   <form class="save-article-form" method="post" action="/save-article">
     <input type="hidden" name="${articleIdFieldName}" value="${doi.value}">
     <input type="hidden" name="listId" value="${listId}">
     <div class="list-name">${listName}</div>
+    ${renderAnnotationContentTextarea(listId)}
     <button type="submit" class="save-article-button">
       Save article
     </button>
