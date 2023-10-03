58657cad8429d8e2e1d159b50c41c7f6d25f7c85
Kevin Rutherford <kevin@rutherford-software.com>
2023-10-03 13:57:36 +0100

    Prepare viewmodel construction to be able to retrieve the user lists #2704

--- src/html-pages/save-article-form-page/dependencies.ts
+++ src/html-pages/save-article-form-page/dependencies.ts
@@ -1,3 +1,4 @@
+import { Queries } from '../../read-models';
 import { ExternalQueries } from '../../third-parties/external-queries';
 
-export type Dependencies = ExternalQueries;
+export type Dependencies = Queries & ExternalQueries;
--- src/html-pages/save-article-form-page/params.ts
+++ src/html-pages/save-article-form-page/params.ts
@@ -1,8 +1,11 @@
 import * as t from 'io-ts';
+import * as tt from 'io-ts-types';
 import { DoiFromString } from '../../types/codecs/DoiFromString';
+import { userIdCodec } from '../../types/user-id';
 
 export const paramsCodec = t.strict({
   articleId: DoiFromString,
+  user: tt.optionFromNullable(t.type({ id: userIdCodec })),
 });
 
 export type Params = t.TypeOf<typeof paramsCodec>;
