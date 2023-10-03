d7c28a424b5f1e09e2ccf1a535bd11f0fd984b1b
Giorgio Sironi <g.sironi@elifesciences.org>
2023-10-03 14:37:33 +0100

    Display radio buttons to select the user list #2704

--- src/html-pages/save-article-form-page/render-as-html.ts
+++ src/html-pages/save-article-form-page/render-as-html.ts
@@ -1,8 +1,20 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
 import { Page } from '../../types/page';
 import { toHtmlFragment } from '../../types/html-fragment';
 import { articleIdFieldName } from '../../write-side/save-article/save-article-handler';
 import { ViewModel } from './view-model';
-import { renderListItems } from '../../shared-components/render-list-items';
+
+const renderLists = (listNames: ViewModel['userListNames']) => pipe(
+  listNames,
+  RA.map((listName) => `
+    <div>
+      <input type="radio" id="${listName}" name="drone" value="fake-list-id" />
+      <label for="${listName}">${listName}</label>
+    </div>
+  `),
+  (divs) => divs.join(''),
+);
 
 export const renderAsHtml = (viewModel: ViewModel): Page => ({
   title: 'Save article page',
@@ -13,11 +25,11 @@ export const renderAsHtml = (viewModel: ViewModel): Page => ({
   <p>
     ${viewModel.articleTitle}
   </p>
-  <ul>
-    ${renderListItems(viewModel.userListNames)}
-  </ul>
   <form class="save-article-form" method="post" action="/save-article">
     <input type="hidden" name="${articleIdFieldName}" value="${viewModel.articleId.value}">
+    <div>
+      ${renderLists(viewModel.userListNames)}
+    </div>
     <button type="submit" class="save-article-button">
       Save article
     </button>
