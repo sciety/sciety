90912696ac251345aaeea1feb80dbc05434e6801
LinaKind <l.kinduryte@elifesciences.org>
2023-10-04 15:35:57 +0100

    Enforce a common interface between pageHandler and a page #2704

--- src/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
+++ src/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
@@ -5,7 +5,7 @@ import { Queries } from '../../read-models';
 import { constructViewModel } from './construct-view-model';
 import { ConstructPage } from '../construct-page';
 
-export const subscribeToListPage = (dependencies: Queries): ConstructPage => (params: unknown) => pipe(
+export const subscribeToListPage = (dependencies: Queries): ConstructPage => (params) => pipe(
   params,
   constructViewModel(dependencies),
   TE.bimap(toErrorPage, renderAsHtml),
