546e406db19d25e742901f599fc16f492ddeb2de
David Moulton <d.moulton@elifesciences.org>
2023-10-04 10:35:35 +0100

    Depend on the ExternalQueries type rather than individual queries #2704

--- src/html-pages/article-page/construct-view-model/dependencies.ts
+++ src/html-pages/article-page/construct-view-model/dependencies.ts
@@ -1,21 +1,13 @@
 import { Queries } from '../../../read-models';
-import {
-  FetchArticle,
-  FetchRelatedArticles,
-  FetchReview,
-  FindVersionsForArticleDoi,
-  Logger,
-} from '../../../shared-ports';
+import { Logger } from '../../../shared-ports';
 import { Dependencies as ConstructCurationStatementsDependencies } from '../../../shared-components/curation-statements/construct-curation-statements';
 import { ConstructReviewingGroupsDependencies } from '../../../shared-components/reviewing-groups';
+import { ExternalQueries } from '../../../third-parties';
 
 export type Dependencies = Queries
 & ConstructCurationStatementsDependencies
 & ConstructReviewingGroupsDependencies
+& ExternalQueries
 & {
-  fetchArticle: FetchArticle,
-  fetchRelatedArticles: FetchRelatedArticles,
-  fetchReview: FetchReview,
-  findVersionsForArticleDoi: FindVersionsForArticleDoi,
   logger: Logger,
 };
