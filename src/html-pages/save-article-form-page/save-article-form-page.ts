0a3b197c80c9dc54741536fc856977ac8cc69f76
David Moulton <d.moulton@elifesciences.org>
2023-10-04 10:02:04 +0100

    Rely on the ConstructPage type #2704

--- src/html-pages/save-article-form-page/save-article-form-page.ts
+++ src/html-pages/save-article-form-page/save-article-form-page.ts
@@ -1,21 +1,18 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import { Page } from '../../types/page';
-import { RenderPageError } from '../../types/render-page-error';
 import { renderAsHtml } from './render-as-html';
 import { constructViewModel } from './construct-view-model';
 import * as DE from '../../types/data-error';
 import { toHtmlFragment } from '../../types/html-fragment';
 import { paramsCodec } from './params';
 import { Dependencies } from './dependencies';
-
-type SaveArticleFormPage = TE.TaskEither<RenderPageError, Page>;
+import { ConstructPage } from '../construct-page';
 
 export const saveArticleFormPage = (
   dependencies: Dependencies,
-) => (
-  input: Record<string, unknown>,
-): SaveArticleFormPage => pipe(
+): ConstructPage => (
+  input,
+) => pipe(
   input,
   paramsCodec.decode,
   TE.fromEither,
