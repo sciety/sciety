476a55685c29aa1e793899b5f95296589b6871e8
David Moulton <d.moulton@elifesciences.org>
2023-10-05 10:53:23 +0100

    Replace another usage of a class method with a function #2710

--- src/types/codecs/DoiFromString.ts
+++ src/types/codecs/DoiFromString.ts
@@ -2,20 +2,20 @@ import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import { flow, pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
-import * as Doi from '../article-id';
+import * as AID from '../article-id';
 
 export const DoiFromString = new t.Type(
   'DoiFromString',
-  (u): u is Doi.ArticleId => u instanceof Doi.ArticleId,
+  (u): u is AID.ArticleId => u instanceof AID.ArticleId,
   (u, c) => pipe(
     t.string.validate(u, c),
     E.chain(flow(
-      Doi.fromString,
+      AID.fromString,
       O.fold(
         () => t.failure(u, c),
         t.success,
       ),
     )),
   ),
-  (a) => a.toString(),
+  (a) => AID.toString(a),
 );
