e0d550d3f5cb7b9b2e009d85bbcc688bce23e231
Giorgio Sironi <g.sironi@elifesciences.org>
2023-10-05 10:27:43 +0100

    Improve visual separation between form inputs and form button #2709

--- src/html-pages/save-article-form-page/render-as-html.ts
+++ src/html-pages/save-article-form-page/render-as-html.ts
@@ -24,10 +24,10 @@ export const renderAsHtml = (viewModel: ViewModel): HtmlPage => ({
   </header>
   <form class="save-article-page-form" method="post" action="/save-article">
     <input type="hidden" name="${articleIdFieldName}" value="${viewModel.articleId.value}">
-    <fieldset>
-    <legend class="save-article-page-form__legend">
-    Which list do you want to save this article to?
-    </legend>
+    <fieldset class="save-article-page-form__fieldset">
+      <legend class="save-article-page-form__legend">
+      Which list do you want to save this article to?
+      </legend>
       ${renderLists(viewModel.userLists)}
     </fieldset>
     <button type="submit" class="save-article-page-form__button">
