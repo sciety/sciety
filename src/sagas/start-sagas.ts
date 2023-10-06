a92868cac99afaf036bec936b2c72ae16a1ba897
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-10-06 13:13:07 +0100

    Higher frequency saga to populate Evaluated Articles lists

--- src/sagas/start-sagas.ts
+++ src/sagas/start-sagas.ts
@@ -7,6 +7,6 @@ export const startSagas = (ports: CollectedPorts) => async (): Promise<void> => 
   ports.logger('info', 'Starting sagas');
   setInterval(async () => discoverElifeArticleSubjectArea(ports), 307 * 1000);
   setInterval(async () => addArticleToElifeSubjectAreaList(ports), 311 * 1000);
-  setInterval(async () => ensureEvaluationsAreListed(ports), 317 * 1000);
+  setInterval(async () => ensureEvaluationsAreListed(ports), 3 * 1000);
   ports.logger('info', 'Sagas started');
 };
