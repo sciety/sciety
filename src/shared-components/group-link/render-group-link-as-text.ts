0352e8cb6d25d0ad908e7c0a1ef6b2f4ae68201e
David Moulton <d.moulton@elifesciences.org>
2023-10-06 15:18:28 +0100

    htmlEscape the rendering of the reviewing group name in the article card #2714

--- src/shared-components/group-link/render-group-link-as-text.ts
+++ src/shared-components/group-link/render-group-link-as-text.ts
@@ -1,4 +1,5 @@
+import { htmlEscape } from 'escape-goat';
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 import { GroupLinkAsTextViewModel } from './group-link-as-text-view-model';
 
-export const renderGroupLinkAsText = (group: GroupLinkAsTextViewModel): HtmlFragment => toHtmlFragment(`<a href="${group.href}">${group.groupName}</a>`);
+export const renderGroupLinkAsText = (group: GroupLinkAsTextViewModel): HtmlFragment => toHtmlFragment(`<a href="${group.href}">${htmlEscape(group.groupName)}</a>`);
