2e106b708f394b8aa04cae07e9b750d0c323ad98
LinaKind <l.kinduryte@elifesciences.org>
2023-10-06 10:56:29 +0100

    Remove obsolete forms #2712

--- src/html-pages/article-page/render-as-html/render-save-article.ts
+++ src/html-pages/article-page/render-as-html/render-save-article.ts
@@ -1,10 +1,8 @@
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
-import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 import { ListId } from '../../../types/list-id';
-import { renderSaveToListForm } from '../../../write-side/save-article/render-save-to-list-form';
 import { SaveToAListForms, ViewModel } from '../view-model';
 
 const renderLinkToUserListArticleIsInto = (listId: ListId, listName: string) => `
@@ -19,24 +17,18 @@ const renderLinkToUserListArticleIsInto = (listId: ListId, listName: string) => 
 
 const renderLoggedOutCallToAction = () => '<a href="/log-in" class="logged-out-call-to-action">Log in to save this article</a>';
 
-const renderSaveToListSection = (href: string) => `
-  <section>
-    <a href="${href}" class="article-actions__save_article">Save this article</a>
-  </section>
+const renderSaveToList = (notInAnyList: SaveToAListForms) => `
+<section>
+  <a href="${notInAnyList.saveArticleHref}" class="article-actions__save_article">Save this article</a>
+</section>
 `;
-
-const renderSaveToList = (viewmodel: ViewModel) => (notInAnyList: SaveToAListForms) => pipe(
-  notInAnyList.lists,
-  RA.map((list) => renderSaveToListForm(viewmodel.doi, list.listId, list.listName)),
-  () => renderSaveToListSection(notInAnyList.saveArticleHref),
-);
 
 export const renderSaveArticle = (viewmodel: ViewModel): HtmlFragment => pipe(
   viewmodel.userListManagement,
   O.match(
     renderLoggedOutCallToAction,
     E.match(
-      renderSaveToList(viewmodel),
+      renderSaveToList,
       (savedToThisList) => renderLinkToUserListArticleIsInto(savedToThisList.listId, savedToThisList.listName),
     ),
   ),
