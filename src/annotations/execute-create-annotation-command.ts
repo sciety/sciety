3d1fb3d84332c13a6a94b01e8c8a6949c9c6665f
David Moulton <d.moulton@elifesciences.org>
2023-10-09 11:59:31 +0100

    Enforce the resource action signature #2715

--- src/annotations/execute-create-annotation-command.ts
+++ src/annotations/execute-create-annotation-command.ts
@@ -2,16 +2,11 @@ import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
 import { eqAnnotationTarget } from '../types/annotation-target';
-import {
-  DomainEvent, EventOfType, isEventOfType, constructEvent,
-} from '../domain-events';
+import { isEventOfType, constructEvent } from '../domain-events';
 import { CreateAnnotationCommand } from '../write-side/commands';
+import { ResourceAction } from '../write-side/resources/resource-action';
 
-type ExecuteCreateAnnotationCommand = (command: CreateAnnotationCommand)
-=> (events: ReadonlyArray<DomainEvent>)
-=> E.Either<never, ReadonlyArray<EventOfType<'AnnotationCreated'>>>;
-
-export const executeCreateAnnotationCommand: ExecuteCreateAnnotationCommand = (command) => (events) => pipe(
+export const executeCreateAnnotationCommand: ResourceAction<CreateAnnotationCommand> = (command) => (events) => pipe(
   events,
   RA.filter(isEventOfType('AnnotationCreated')),
   RA.filter((event) => eqAnnotationTarget.equals(event.target, command.target)),
