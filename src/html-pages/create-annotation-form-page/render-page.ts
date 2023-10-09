7d533303114193e8b401931ca404c81d2b00f753
Kevin Rutherford <kevin@rutherford-software.com>
2023-10-09 11:32:18 +0100

    Apply standard page header styling #2721

--- src/html-pages/create-annotation-form-page/render-page.ts
+++ src/html-pages/create-annotation-form-page/render-page.ts
@@ -6,12 +6,14 @@ export const listIdInputName = 'listId';
 export const articleIdInputName = 'articleId';
 
 export const renderPage = (viewModel: ViewModel): HtmlFragment => toHtmlFragment(`
+  <header class="page-header">
     <h1>Create an annotation for <span class="annotation-form-article-title">${viewModel.articleTitle}</span> on <span class="annotation-form-list-name">${htmlEscape(viewModel.listName)}</span></h1>
-    <form class="standard-form" method="POST" action="/annotations/create-annotation">
-      <label for="annotationContent">Annotation content</label>
-      <textarea id="annotationContent" name="annotationContent" rows="10"></textarea>
-      <input type="hidden" name="${listIdInputName}" value="${viewModel.listId}">
-      <input type="hidden" name="${articleIdInputName}" value="${viewModel.articleId.value}">
-      <button type="submit">Create annotation</button>
-    </form>
-  `);
+  </header>
+  <form class="standard-form" method="POST" action="/annotations/create-annotation">
+    <label for="annotationContent">Annotation content</label>
+    <textarea id="annotationContent" name="annotationContent" rows="10"></textarea>
+    <input type="hidden" name="${listIdInputName}" value="${viewModel.listId}">
+    <input type="hidden" name="${articleIdInputName}" value="${viewModel.articleId.value}">
+    <button type="submit">Create annotation</button>
+  </form>
+`);
