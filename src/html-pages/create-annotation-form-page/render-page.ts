9f5d83cf7b5005b4a87d9e89fc974be6954d0e41
Kevin Rutherford <kevin@rutherford-software.com>
2023-10-09 11:22:09 +0100

    Prefer standard form styling over custom classes #2721

--- src/html-pages/create-annotation-form-page/render-page.ts
+++ src/html-pages/create-annotation-form-page/render-page.ts
@@ -7,11 +7,11 @@ export const articleIdInputName = 'articleId';
 
 export const renderPage = (viewModel: ViewModel): HtmlFragment => toHtmlFragment(`
     <h1>Create an annotation for <span class="annotation-form-article-title">${viewModel.articleTitle}</span> on <span class="annotation-form-list-name">${htmlEscape(viewModel.listName)}</span></h1>
-    <form method="POST" action="/annotations/create-annotation">
+    <form class="standard-form" method="POST" action="/annotations/create-annotation">
       <label for="annotationContent">Annotation content</label>
       <textarea id="annotationContent" name="annotationContent" rows="10"></textarea>
       <input type="hidden" name="${listIdInputName}" value="${viewModel.listId}">
       <input type="hidden" name="${articleIdInputName}" value="${viewModel.articleId.value}">
-      <button class="annotation-form-submit">Create annotation</button>
+      <button type="submit">Create annotation</button>
     </form>
   `);
