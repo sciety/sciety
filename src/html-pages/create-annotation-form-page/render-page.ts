9f7a1f603ab9f44717babba6724119576ebc2eb4
LinaKind <l.kinduryte@elifesciences.org>
2023-10-10 11:48:34 +0100

    Start rolling out standard form summary list form pattern to accelerate learning #2723

--- src/html-pages/create-annotation-form-page/render-page.ts
+++ src/html-pages/create-annotation-form-page/render-page.ts
@@ -10,10 +10,16 @@ export const renderPage = (viewModel: ViewModel): HtmlFragment => toHtmlFragment
     <h1>Create an annotation for <span class="annotation-form-article-title">${viewModel.articleTitle}</span> on <span class="annotation-form-list-name">${htmlEscape(viewModel.listName)}</span></h1>
   </header>
   <form class="standard-form" method="POST" action="/annotations/create-annotation">
+    <input type="hidden" name="${articleIdInputName}" value="${viewModel.articleId.value}">
+    <input type="hidden" name="${listIdInputName}" value="${viewModel.listId}">
+    <dl>
+      <dt>Article</dt>
+      <dd>${viewModel.articleTitle}</dd>
+      <dt>List</dt>
+      <dd>${viewModel.listName}</dd>
+    </dl>
     <label for="annotationContent">Annotation content</label>
     <textarea id="annotationContent" name="annotationContent" rows="10"></textarea>
-    <input type="hidden" name="${listIdInputName}" value="${viewModel.listId}">
-    <input type="hidden" name="${articleIdInputName}" value="${viewModel.articleId.value}">
     <button type="submit">Create annotation</button>
   </form>
 `);
