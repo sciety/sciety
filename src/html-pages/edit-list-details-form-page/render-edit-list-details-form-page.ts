a10bbec30dd00ce226eb567a46b1c8e461a698e2
David Moulton <d.moulton@elifesciences.org>
2023-10-10 15:40:41 +0100

    Delete custom styling in favour of equivalent standard form styling #2715

--- src/html-pages/edit-list-details-form-page/render-edit-list-details-form-page.ts
+++ src/html-pages/edit-list-details-form-page/render-edit-list-details-form-page.ts
@@ -21,13 +21,13 @@ export const renderEditListDetailsFormPage = (viewModel: ViewModel): HtmlPage =>
 <form action="/forms/edit-list-details" method="post" class="edit-list-details-form standard-form">
   <input type="hidden" value="${viewModel.listId}" name="listId">
   <section>
-    <label for="listName" class="edit-list-details-form__label">List name</label>
+    <label for="listName" class="standard-form__sub_heading">List name</label>
     <p class="edit-list-details-form__helper_text">Give the list a descriptive title.</p>
     <input type="text" id="listName" name="name" class="edit-list-details-form__field" value="${htmlEscape(viewModel.listName)}" pattern="[^<>]+" required maxlength="${viewModel.listNameMaxLength}">
     <p class="edit-list-details-form__constraints">Maximum ${viewModel.listNameMaxLength} characters</p>
   </section>
   <section>
-    <label for="listDescription" class="edit-list-details-form__label">Description (optional)</label>
+    <label for="listDescription" class="standard-form__sub_heading">Description (optional)</label>
     <p class="edit-list-details-form__helper_text">Add further context to help readers understand your list.</p>
     <textarea id="listDescription" name="description" cols="30" rows="5" maxlength="${viewModel.listDescriptionMaxLength}">${viewModel.listDescription}</textarea>
     <p class="edit-list-details-form__constraints">Maximum ${viewModel.listDescriptionMaxLength} characters</p>
