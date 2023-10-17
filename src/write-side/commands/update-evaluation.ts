3e5e68a76005770442430bda8df03ebfc11d7188
LinaKind <l.kinduryte@elifesciences.org>
2023-10-17 10:27:28 +0100

    Prepare update evaluation command for expansion #2742

--- src/write-side/commands/update-evaluation.ts
+++ src/write-side/commands/update-evaluation.ts
@@ -2,9 +2,13 @@ import * as t from 'io-ts';
 import { evaluationLocatorCodec } from '../../types/evaluation-locator';
 import { evaluationTypeCodec } from './record-evaluation-publication';
 
-export const updateEvaluationCommandCodec = t.strict({
-  evaluationLocator: evaluationLocatorCodec,
-  evaluationType: evaluationTypeCodec,
-});
+export const updateEvaluationCommandCodec = t.intersection([
+  t.strict({
+    evaluationLocator: evaluationLocatorCodec,
+  }),
+  t.partial({
+    evaluationType: evaluationTypeCodec,
+  }),
+]);
 
 export type UpdateEvaluationCommand = t.TypeOf<typeof updateEvaluationCommandCodec>;
