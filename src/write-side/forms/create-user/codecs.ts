5b2eb4b7c99ef763276424913acd0e64c54f0b26
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-10-18 10:44:21 +0100

    Make coupling between happy path and loosened form field codec explicit

--- src/write-side/forms/create-user/codecs.ts
+++ src/write-side/forms/create-user/codecs.ts
@@ -1,16 +1,20 @@
 import * as tt from 'io-ts-types';
+import * as R from 'fp-ts/Record';
 import * as t from 'io-ts';
+import { pipe } from 'fp-ts/function';
 import { userGeneratedInputCodec, UserGeneratedInput } from '../../../types/user-generated-input';
 import { userHandleCodec } from '../../../types/user-handle';
+
+const toFieldsCodec = <P extends t.Props>(props: P) => pipe(
+  props,
+  R.map(() => t.string),
+  (stringProps) => stringProps as { [K in keyof P]: t.StringC },
+  t.type,
+);
 
 export const unvalidatedFormDetailsCodec = t.type({
   fullName: tt.withFallback(userGeneratedInputCodec({ maxInputLength: 1000 }), '' as UserGeneratedInput),
   handle: tt.withFallback(userGeneratedInputCodec({ maxInputLength: 1000 }), '' as UserGeneratedInput),
-});
-
-export const formFieldsCodec = t.type({
-  fullName: t.string,
-  handle: t.string,
 });
 
 export const createUserAccountFormCodec = t.type({
@@ -19,3 +23,5 @@ export const createUserAccountFormCodec = t.type({
 });
 
 export type CreateUserAccountForm = t.TypeOf<typeof createUserAccountFormCodec>;
+
+export const formFieldsCodec = toFieldsCodec(createUserAccountFormCodec.props);
