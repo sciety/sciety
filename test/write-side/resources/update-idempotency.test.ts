a4c9e418f0111cee8ec077caf745a414efd76a13
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-10-24 08:22:17 +0100

    Group test cases by type #2769

--- test/write-side/resources/update-idempotency.test.ts
+++ test/write-side/resources/update-idempotency.test.ts
@@ -12,11 +12,15 @@ describe('update-idempotency', () => {
 
   describe('changedFields', () => {
     it.each([
+      // trivial
       [{}, {}, {}],
       [{ a: 1 }, { a: 1 }, { a: undefined }],
+      // involving objects
       [{ a: ['1'] }, { a: ['1'] }, { a: undefined }],
       [{ a: ['2'] }, { a: ['1'] }, { a: ['2'] }],
+      // combinations of changed and unchanged
       [{ a: 1, b: 2 }, { a: 1, b: 1 }, { a: undefined, b: 2 }],
+      // combinations of changed and undefined
       [{ a: undefined, b: 2 }, { a: 1, b: 1 }, { a: undefined, b: 2 }],
       [{ a: undefined, b: 2 }, { a: undefined, b: 1 }, { a: undefined, b: 2 }],
       [{ a: 1, b: 2 }, { a: undefined, b: 1 }, { a: 1, b: 2 }],
