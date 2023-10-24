42d99d934ab2457bef04bdf77ec6fdf0bc97a726
LinaKind <l.kinduryte@elifesciences.org>
2023-10-24 15:34:52 +0100

    Delete legacy test cases which are now the responsibility of the view model construction #2771

--- test/docmaps/docmap-index/docmap-index.test.ts
+++ test/docmaps/docmap-index/docmap-index.test.ts
@@ -5,7 +5,6 @@ import { Dependencies } from '../../../src/docmaps/docmap-index/docmap-index';
 import { TestFramework, createTestFramework } from '../../framework';
 import { dummyLogger } from '../../dummy-logger';
 import { arbitraryRecordEvaluationPublicationCommand } from '../../write-side/commands/record-evaluation-publication-command.helper';
-import { arbitraryAddGroupCommand } from '../../write-side/commands/add-group-command.helper';
 
 describe('docmap-index', () => {
   const ncrcGroupId = GID.fromValidatedString('62f9b0d0-8d43-4766-a52a-ce02af61bc6a');
@@ -23,50 +22,6 @@ describe('docmap-index', () => {
       ...framework.happyPathThirdParties,
       logger: dummyLogger,
     };
-  });
-
-  describe('when all ports work', () => {
-    describe('and there are no docmaps', () => {
-      let response: { body: DocmapIndexBody, status: StatusCodes };
-
-      beforeEach(async () => {
-        response = await docmapIndex(defaultAdapters)({})();
-      });
-
-      it('return an empty list in the articles field', async () => {
-        expect(response.body).toStrictEqual({ articles: [] });
-      });
-
-      it('return a 200 status code', () => {
-        expect(response.status).toStrictEqual(StatusCodes.OK);
-      });
-    });
-
-    describe('when there are docmaps', () => {
-      const addGroupCommand = {
-        ...arbitraryAddGroupCommand(),
-        groupId: ncrcGroupId,
-      };
-      const recordEvaluationPublicationCommand = {
-        ...arbitraryRecordEvaluationPublicationCommand(),
-        groupId: addGroupCommand.groupId,
-      };
-      let response: { body: DocmapIndexBody, status: StatusCodes };
-
-      beforeEach(async () => {
-        await framework.commandHelpers.addGroup(addGroupCommand);
-        await framework.commandHelpers.recordEvaluationPublication(recordEvaluationPublicationCommand);
-        response = await docmapIndex(defaultAdapters)({})();
-      });
-
-      it('return them as a list in the articles field', () => {
-        expect(response.body.articles).toHaveLength(1);
-      });
-
-      it('return a 200 status code', () => {
-        expect(response.status).toStrictEqual(StatusCodes.OK);
-      });
-    });
   });
 
   describe('when any docmap fails to generate', () => {
