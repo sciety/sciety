1d1db8ba37892ac84e7a7ecd5cdb8065e8ca0c4d
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-10-24 08:50:32 +0100

    Ignore undefined when checking if changedAttributes is empty and make it possible to ignore never changing id field #2769

--- src/write-side/resources/update-idempotency.ts
+++ src/write-side/resources/update-idempotency.ts
@@ -3,7 +3,19 @@
 /* eslint-disable no-loops/no-loops */
 import deepEqual from 'deep-equal';
 
-export const isEmpty = <T extends Record<string, unknown>>(object: T): boolean => Object.keys(object).length === 0;
+const removeUndefined = <T extends Record<string, unknown>>(input: T): Partial<T> => {
+  const result = input;
+  for (const key in result) {
+    if (result[key] === undefined) {
+      delete result[key];
+    }
+  }
+  return result;
+};
+
+export const isEmpty = <
+T extends Record<string, unknown>,
+>(object: T): boolean => Object.keys(removeUndefined(object)).length === 0;
 
 type KeysOfType<T, U> = { [K in keyof T]: T[K] extends U ? K : never }[keyof T];
 
@@ -11,13 +23,15 @@ type RequiredKeys<T> = Exclude<KeysOfType<T, Exclude<T[keyof T], undefined>>, un
 
 type PropertiesThatCanBeUndefined<T extends Record<string, unknown>> = Omit<T, RequiredKeys<T>>;
 
-type ChangedFields = <C extends Record<string, unknown>>(command: C)
+type ChangedFields = <I extends string, C extends Record<string, unknown> & Record<I, unknown>>(command: C, idField: I)
 => (state: PropertiesThatCanBeUndefined<C>)
 => Required<PropertiesThatCanBeUndefined<C>>;
 
 // @ts-ignore
-export const changedFields: ChangedFields = (command) => (state) => {
-  const result = command;
+export const changedFields: ChangedFields = (command, idField) => (state) => {
+  const result = structuredClone(command);
+  delete result[idField];
+
   for (const key in state) {
     // @ts-ignore
     if (deepEqual(command[key], state[key])) { result[key] = undefined; }
--- test/write-side/resources/update-idempotency.test.ts
+++ test/write-side/resources/update-idempotency.test.ts
@@ -4,6 +4,7 @@ describe('update-idempotency', () => {
   describe('isEmpty', () => {
     it.each([
       [{}, true],
+      [{ a: undefined }, true],
       [{ foo: 'bar' }, false],
     ])('given %s returns %s', (input, expected) => {
       expect(UI.isEmpty(input)).toBe(expected);
@@ -13,19 +14,20 @@ describe('update-idempotency', () => {
   describe('changedFields', () => {
     it.each([
       // trivial
-      [{}, {}, {}],
-      [{ a: 1 }, { a: 1 }, { a: undefined }],
+      [{ id: 'foo' }, {}, {}],
+      [{ id: 'foo', a: 1 }, { a: 1 }, { a: undefined }],
       // involving objects
-      [{ a: ['1'] }, { a: ['1'] }, { a: undefined }],
-      [{ a: ['2'] }, { a: ['1'] }, { a: ['2'] }],
+      [{ id: 'foo', a: ['1'] }, { a: ['1'] }, { a: undefined }],
+      [{ id: 'foo', a: ['2'] }, { a: ['1'] }, { a: ['2'] }],
       // combinations of changed and unchanged
-      [{ a: 1, b: 2 }, { a: 1, b: 1 }, { a: undefined, b: 2 }],
+      [{ id: 'foo', a: 1, b: 2 }, { a: 1, b: 1 }, { a: undefined, b: 2 }],
       // combinations of changed and undefined
-      [{ a: undefined, b: 2 }, { a: 1, b: 1 }, { a: undefined, b: 2 }],
-      [{ a: undefined, b: 2 }, { a: undefined, b: 1 }, { a: undefined, b: 2 }],
-      [{ a: 1, b: 2 }, { a: undefined, b: 1 }, { a: 1, b: 2 }],
+      [{ id: 'foo', a: undefined, b: 2 }, { a: 1, b: 1 }, { a: undefined, b: 2 }],
+      [{ id: 'foo', a: undefined, b: 2 }, { a: undefined, b: 1 }, { a: undefined, b: 2 }],
+      [{ id: 'foo', a: 1, b: 2 }, { a: undefined, b: 1 }, { a: 1, b: 2 }],
     ])('input: %s and state: %s returns %s', (input, state, expected) => {
-      expect(UI.changedFields(input)(state)).toStrictEqual(expected);
+      // eslint-disable-next-line jest/prefer-strict-equal
+      expect(UI.changedFields(input, 'id')(state)).toEqual(expected);
     });
   });
 });
