7d6d0c3301fcd815013bf8b93645e109dc7d1de9
Giorgio Sironi <g.sironi@elifesciences.org>
2023-10-26 11:52:24 +0100

    Write the first pass of a test list to express intent #2782

--- test/third-parties/fetch-doi-evaluation-by-publisher.test.ts
+++ test/third-parties/fetch-doi-evaluation-by-publisher.test.ts
@@ -1,6 +1,12 @@
 describe('fetch-doi-evaluation-by-publisher', () => {
   describe('when a doi with a configured prefix is passed in', () => {
-    it.todo('delegates to the doi fetcher for that publisher');
+    describe('when the delegated doi fetcher returns a right', () => {
+      it.todo('returns a right');
+    });
+
+    describe('when the delegated doi fetcher returns a left', () => {
+      it.todo('returns a left');
+    });
   });
 
   describe('when a doi with an unknown prefix is passed in', () => {
