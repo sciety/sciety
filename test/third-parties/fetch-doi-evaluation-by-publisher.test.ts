c8f162bc76fac44735adf371ac8bd55b0ebc9fa9
Giorgio Sironi <g.sironi@elifesciences.org>
2023-10-26 13:18:16 +0100

    Write a simpler failing test #2782

--- test/third-parties/fetch-doi-evaluation-by-publisher.test.ts
+++ test/third-parties/fetch-doi-evaluation-by-publisher.test.ts
@@ -1,11 +1,10 @@
 import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 import { URL } from 'url';
-
+import * as DE from '../../src/types/data-error';
 import { fetchDoiEvaluationByPublisher } from '../../src/third-parties/fetch-doi-evaluation-by-publisher';
-
 import { Evaluation } from '../../src/types/evaluation';
-import { arbitrarySanitisedHtmlFragment, arbitraryUri } from '../helpers';
+import { arbitrarySanitisedHtmlFragment, arbitraryString, arbitraryUri } from '../helpers';
 
 const arbitraryEvaluation: Evaluation = {
   fullText: arbitrarySanitisedHtmlFragment(),
@@ -14,12 +13,18 @@ const arbitraryEvaluation: Evaluation = {
 
 describe('fetch-doi-evaluation-by-publisher', () => {
   describe('when a doi with a configured prefix is passed in', () => {
-    describe('when the delegated doi fetcher returns a right', () => {
-      const result = fetchDoiEvaluationByPublisher({
-        doiPrefix: () => TE.right(arbitraryEvaluation),
+    describe.skip('when the delegated doi fetcher returns a right', () => {
+      const doiPrefix = arbitraryString();
+      const evaluationFetchersConfiguration = {
+        [doiPrefix]: () => TE.right(arbitraryEvaluation),
+      };
+      let result: E.Either<DE.DataError, Evaluation>;
+
+      beforeEach(async () => {
+        result = await fetchDoiEvaluationByPublisher(evaluationFetchersConfiguration)(`${doiPrefix}/123`)();
       });
 
-      it.skip('returns a right', () => {
+      it('returns a right', () => {
         expect(result).toStrictEqual(E.right);
       });
     });
@@ -29,7 +34,19 @@ describe('fetch-doi-evaluation-by-publisher', () => {
     });
   });
 
-  describe('when a doi with an unknown prefix is passed in', () => {
-    it.todo('returns unavailable');
+  describe.skip('when a doi with an unknown prefix is passed in', () => {
+    const doiPrefix = arbitraryString();
+    const evaluationFetchersConfiguration = {
+      [arbitraryString()]: () => TE.right(arbitraryEvaluation),
+    };
+    let result: E.Either<DE.DataError, Evaluation>;
+
+    beforeEach(async () => {
+      result = await fetchDoiEvaluationByPublisher(evaluationFetchersConfiguration)(`${doiPrefix}/123`)();
+    });
+
+    it('returns unavailable', () => {
+      expect(result).toStrictEqual(E.left(DE.unavailable));
+    });
   });
 });
