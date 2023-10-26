a8e6b9899fbc97a120f702a32c6838c37c89f111
David Moulton <d.moulton@elifesciences.org>
2023-10-26 12:02:23 +0100

    Begin to set up a test #2782

--- test/third-parties/fetch-doi-evaluation-by-publisher.test.ts
+++ test/third-parties/fetch-doi-evaluation-by-publisher.test.ts
@@ -1,7 +1,27 @@
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import { URL } from 'url';
+
+import { fetchDoiEvaluationByPublisher } from '../../src/third-parties/fetch-doi-evaluation-by-publisher';
+
+import { Evaluation } from '../../src/types/evaluation';
+import { arbitrarySanitisedHtmlFragment, arbitraryUri } from '../helpers';
+
+const arbitraryEvaluation: Evaluation = {
+  fullText: arbitrarySanitisedHtmlFragment(),
+  url: new URL(arbitraryUri()),
+};
+
 describe('fetch-doi-evaluation-by-publisher', () => {
   describe('when a doi with a configured prefix is passed in', () => {
     describe('when the delegated doi fetcher returns a right', () => {
-      it.todo('returns a right');
+      const result = fetchDoiEvaluationByPublisher({
+        doiPrefix: () => TE.right(arbitraryEvaluation),
+      });
+
+      it.skip('returns a right', () => {
+        expect(result).toStrictEqual(E.right);
+      });
     });
 
     describe('when the delegated doi fetcher returns a left', () => {
