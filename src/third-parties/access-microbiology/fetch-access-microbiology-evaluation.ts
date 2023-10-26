8d0b140d675f4cfa41b8ac76f1b60ed5ab8400d6
LinaKind <l.kinduryte@elifesciences.org>
2023-10-26 14:36:34 +0100

    Display full text, copied from XML #2782
    https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000530.v1/acmi.0.000530.v1.xml

--- src/third-parties/access-microbiology/fetch-access-microbiology-evaluation.ts
+++ src/third-parties/access-microbiology/fetch-access-microbiology-evaluation.ts
@@ -1,9 +1,20 @@
 import * as TE from 'fp-ts/TaskEither';
+import { URL } from 'url';
 import { Logger } from '../../shared-ports';
 import * as DE from '../../types/data-error';
 import { EvaluationFetcher } from '../fetch-review';
+import { toHtmlFragment } from '../../types/html-fragment';
+import { sanitise } from '../../types/sanitised-html-fragment';
 
 export const fetchAccessMicrobiologyEvaluation = (logger: Logger): EvaluationFetcher => (key: string) => {
   logger('debug', 'calling fetchAccessMicrobiology', { key });
+  if (key === '10.1099/acmi.0.000530.v1.3') {
+    return TE.right({
+      url: new URL(`https://doi.org/${key}`),
+      fullText: sanitise(toHtmlFragment(`
+      <p>In this manuscript, the authors present a case report of pulmonary nocardiosis, describing the course of the disease and treatment and the microbiological traits of the pathogen Nocardia otitidiscaviarum. As such, the report is a valuable contribution to the medical literature. However, after different consultations with the Editorial Office, we agree that the section about the literature review should be improved before progressing the manuscript to peer-review. In particular, we believe that the paper would greatly benefit from a deeper discussion of the differences and commonalities between previous reports and the one your present here, either where you state that you have done a literature review (line 179) or in the discussion. This would be much more appreciated by the potential readers.</p>    
+      `)),
+    });
+  }
   return TE.left(DE.unavailable);
 };
