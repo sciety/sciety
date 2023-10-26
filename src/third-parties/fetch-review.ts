cb41ff579273ae4bed1b211d7286962f7ad392cc
Giorgio Sironi <g.sironi@elifesciences.org>
2023-10-26 14:55:55 +0100

    Prefer to use ExternalQueries to better conform to current standards #2782

--- src/third-parties/fetch-review.ts
+++ src/third-parties/fetch-review.ts
@@ -1,11 +1,12 @@
 import * as R from 'fp-ts/Record';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { ExternalQueries } from './external-queries';
 import * as DE from '../types/data-error';
 import * as EL from '../types/evaluation-locator';
 import { FetchReview } from '../shared-ports';
 
-export type EvaluationFetcher = (key: string) => ReturnType<FetchReview>;
+export type EvaluationFetcher = (key: string) => ReturnType<ExternalQueries['fetchReview']>;
 
 export const fetchReview = (fetchers: Record<string, EvaluationFetcher>): FetchReview => (id) => pipe(
   fetchers,
