a97660cd9bc3f3d58f8bf68d1ed90e32da688e41
LinaKind <l.kinduryte@elifesciences.org>
2023-10-26 11:36:33 +0100

    Write a test list #2782

---
+++ test/third-parties/fetch-doi-evaluation-by-publisher.test.ts
@@ -0,0 +1,9 @@
+describe('fetch-doi-evaluation-by-publisher', () => {
+  describe('when a doi with a configured prefix is passed in', () => {
+    it.todo('delegates to the doi fetcher for that publisher');
+  });
+
+  describe('when a doi with an unknown prefix is passed in', () => {
+    it.todo('returns unavailable');
+  });
+});
