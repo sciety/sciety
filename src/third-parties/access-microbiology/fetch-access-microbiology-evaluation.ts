0f7142412f26613cf760bddbbe6d02344592c86e
Giorgio Sironi <g.sironi@elifesciences.org>
2023-10-27 15:27:22 +0100

    Display an evaluation that is not a curation statement to the user #2785

--- src/third-parties/access-microbiology/fetch-access-microbiology-evaluation.ts
+++ src/third-parties/access-microbiology/fetch-access-microbiology-evaluation.ts
@@ -4,12 +4,13 @@ import { XMLBuilder, XMLParser } from 'fast-xml-parser';
 import * as t from 'io-ts';
 import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
+import { accessMicrobiologyXmlResponse000569 } from './acmi.0.000569.v1';
 import { Logger } from '../../shared-ports';
 import * as DE from '../../types/data-error';
 import { EvaluationFetcher } from '../evaluation-fetcher';
 import { toHtmlFragment } from '../../types/html-fragment';
 import { sanitise } from '../../types/sanitised-html-fragment';
-import { accessMicrobiologyXmlResponse } from './acmi.0.000530.v1';
+import { accessMicrobiologyXmlResponse000530 } from './acmi.0.000530.v1';
 
 const parser = new XMLParser({});
 const builder = new XMLBuilder();
@@ -31,7 +32,7 @@ export const fetchAccessMicrobiologyEvaluation = (logger: Logger): EvaluationFet
   logger('debug', 'calling fetchAccessMicrobiology', { key });
   if (key === '10.1099/acmi.0.000530.v1.3') {
     return pipe(
-      parser.parse(accessMicrobiologyXmlResponse),
+      parser.parse(accessMicrobiologyXmlResponse000530),
       xmlCodec.decode,
       E.map((response) => builder.build(response.article['sub-article'][2].body).toString() as string),
       E.map((text) => ({
@@ -44,7 +45,7 @@ export const fetchAccessMicrobiologyEvaluation = (logger: Logger): EvaluationFet
   }
   if (key === '10.1099/acmi.0.000569.v1.4') {
     return pipe(
-      parser.parse(''),
+      parser.parse(accessMicrobiologyXmlResponse000569),
       xmlCodec.decode,
       E.map((response) => builder.build(response.article['sub-article'][3].body).toString() as string),
       E.map((text) => ({
