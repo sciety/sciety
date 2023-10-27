a0bf2b01d502cd24e0f1480df28aea37026ca495
LinaKind <l.kinduryte@elifesciences.org>
2023-10-27 09:52:11 +0100

    Consistently link to graphs' sources

--- src/html-pages/about-page/content.ts
+++ src/html-pages/about-page/content.ts
@@ -1,3 +1,5 @@
+const scietyEvaluationGraphsSourceHref = 'https://lookerstudio.google.com/reporting/b09cf3e8-88c7-4928-b639-2c48dc786191/page/p_kr9n81etoc';
+
 export const blogLinks = `
 <ul class="about-page-blog-links">
   <li><a href="https://blog.sciety.org/what-is-public-preprint-evaluation/">What are evaluations and where do they come from?</a></li>
@@ -19,19 +21,19 @@ export const content = `
 
 <figure class="about-page-figure">
 <img src="/static/images/about-page/preprints-in-europe-pmc.png" alt="graph of preprints in Europe PMC showing massive growth">
-<figcaption>Preprints in Europe PMC (source: <a href="https://europepmc.org/Preprints">Europe PMC</a>)</figcaption>
+<figcaption><a href="https://europepmc.org/Preprints">Preprints in Europe PMC</a></figcaption>
 </figure>
 
 <p>Societies, journals and other groups of scientists are helping by highlighting the preprints that they think are the most important and providing public review and curation to share with their peers. Sciety brings the review and curation of preprints from multiple groups into one place to make them easier to find.</p>
 
 <figure class="about-page-figure">
 <img src="/static/images/about-page/number-of-evaluations-on-sciety.png" alt="graph showing increasing amount of evaluations on Sciety">
-<figcaption>Number of evaluations on Sciety</figcaption>
+<figcaption><a href="${scietyEvaluationGraphsSourceHref}">Number of evaluations on Sciety</a></figcaption>
 </figure>
 
 <figure class="about-page-figure">
 <img src="/static/images/about-page/number-of-evaluated-articles-on-sciety.png" alt="graph showing increasing amount of evaluated articles on Sciety">
-<figcaption>Number of evaluated articles on Sciety</figcaption>
+<figcaption><a href="${scietyEvaluationGraphsSourceHref}">Number of evaluated articles on Sciety</a></figcaption>
 </figure>
 
 <p>Sciety also enables scientists to share the evaluated preprints that they think are interesting or relevant to their work, which in turn helps their fellow researchers discover the latest and best new research.</p>
