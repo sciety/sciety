a5890571f8d212f71a82ab83720d43519f36f40e
David Moulton <d.moulton@elifesciences.org>
2023-11-01 13:13:10 +0000

    Write a test list with both existing and desired behaviour #2801

--- test/write-side/resources/list/annotate.test.ts
+++ test/write-side/resources/list/annotate.test.ts
@@ -1,3 +1,15 @@
 describe('annotate', () => {
-  it.todo('tbd');
+  describe('when the list exists', () => {
+    describe('and the article is in the list', () => {
+      it.todo('succeeds, raising a new event');
+    });
+
+    describe('and the article is not in the list', () => {
+      it.todo('fails');
+    });
+  });
+
+  describe('when the list does not exist', () => {
+    it.todo('fails');
+  });
 });
