90e1a834a681e4f5e8c70521703e7f8ac69a536e
Giorgio Sironi <g.sironi@elifesciences.org>
2023-11-03 09:37:44 +0000

    Reduce confusion about write models by moving behavior into a private function #2801

--- src/write-side/resources/list/create.ts
+++ src/write-side/resources/list/create.ts
@@ -1,14 +1,20 @@
-import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
-import { replayAllLists } from './all-lists';
+import { pipe } from 'fp-ts/function';
+import * as RA from 'fp-ts/ReadonlyArray';
 import { CreateListCommand } from '../../commands';
-import { constructEvent } from '../../../domain-events';
+import { constructEvent, DomainEvent, isEventOfType } from '../../../domain-events';
 import { ResourceAction } from '../resource-action';
+
+const getListIdsInUse = (events: ReadonlyArray<DomainEvent>) => pipe(
+  events,
+  RA.filter(isEventOfType('ListCreated')),
+  RA.map(({ listId }) => listId),
+);
 
 export const create: ResourceAction<CreateListCommand> = (command) => (events) => pipe(
   events,
-  replayAllLists,
-  (resource) => (resource.includes(command.listId)
+  getListIdsInUse,
+  (listIdsInUse) => (listIdsInUse.includes(command.listId)
     ? []
     : [constructEvent('ListCreated')(command)]),
   E.right,
