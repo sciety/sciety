04e78fc46880b1cc83f32ef3a258bdafcbdef718
Kevin Rutherford <kevin@rutherford-software.com>
2023-11-06 10:41:37 +0000

    Show a consistent error page when redisplaying of a form fails #2801

--- src/http/form-submission-handlers/create-annotation-handler.ts
+++ src/http/form-submission-handlers/create-annotation-handler.ts
@@ -1,9 +1,10 @@
 import * as O from 'fp-ts/Option';
 import * as PR from 'io-ts/PathReporter';
-import { identity, pipe } from 'fp-ts/function';
+import { pipe } from 'fp-ts/function';
 import { StatusCodes } from 'http-status-codes';
 import { Middleware } from 'koa';
 import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import * as t from 'io-ts';
 import { getLoggedInScietyUser, Ports as GetLoggedInScietyUserPorts } from '../authentication-and-logging-in-of-sciety-users';
@@ -17,7 +18,8 @@ import { createAnnotationFormPage, paramsCodec } from '../../html-pages/create-a
 import { ExternalQueries } from '../../third-parties';
 import { standardPageLayout } from '../../shared-components/standard-page-layout';
 import { UserDetails } from '../../types/user-details';
-import { renderErrorPage } from '../render-error-page';
+import { toWebPage } from '../page-handler';
+import { toHtmlFragment } from '../../types/html-fragment';
 
 type Dependencies = Queries & GetLoggedInScietyUserPorts & HandleCreateAnnotationCommandDependencies & ExternalQueries;
 
@@ -42,11 +44,13 @@ const redisplayFormPage = (
 ) => pipe(
   createAnnotationFormPage(dependencies)(params, 'article-not-in-list'),
   TE.map(prependErrorToTitleForAccessibility),
-  TE.map(standardPageLayout(user)),
-  TE.match(
-    (renderPageError) => `Something went wrong when you submitted your annotation. ${renderErrorPage(renderPageError.message)}`,
-    identity,
+  TE.mapLeft(
+    (renderPageError) => ({
+      type: renderPageError.type,
+      message: toHtmlFragment(`Something went wrong when you submitted your annotation. ${renderPageError.message}`),
+    }),
   ),
+  T.map(toWebPage(user, standardPageLayout)),
 );
 
 type CreateAnnotationHandler = (adapters: Dependencies) => Middleware;
@@ -90,13 +94,14 @@ export const createAnnotationHandler: CreateAnnotationHandler = (adapters) => as
           context.redirect(`/lists/${command.right.listId}`);
           return;
         }
-        context.response.status = StatusCodes.BAD_REQUEST;
-        context.response.type = 'html';
-        context.response.body = await redisplayFormPage(
+        const htmlResponse = await redisplayFormPage(
           adapters,
           { listId: command.right.listId, articleId: command.right.articleId },
           loggedInUser,
         )();
+        context.response.status = htmlResponse.status;
+        context.response.type = 'html';
+        context.response.body = htmlResponse.body;
       },
     ),
   );
