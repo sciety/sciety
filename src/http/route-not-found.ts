58c7782054d956de5616386d81d3a576e0d72814
Giorgio Sironi <g.sironi@elifesciences.org>
2023-11-06 14:30:23 +0000

    Use constructHtmlResponse in one more place instead of manually crafting the status code #2801

--- src/http/route-not-found.ts
+++ src/http/route-not-found.ts
@@ -1,21 +1,25 @@
 import { Middleware } from '@koa/router';
-import { pipe } from 'fp-ts/function';
-import { StatusCodes } from 'http-status-codes';
-import { renderErrorPage } from '../html-pages/render-error-page';
+import * as E from 'fp-ts/Either';
 import { standardPageLayout } from '../shared-components/standard-page-layout';
 import { toHtmlFragment } from '../types/html-fragment';
 import { getLoggedInScietyUser, Ports as GetLoggedInScietyUserPorts } from './authentication-and-logging-in-of-sciety-users';
+import { constructHtmlResponse } from '../html-pages/construct-html-response';
+import * as DE from '../types/data-error';
+import { getHttpStatusCode } from './get-http-status-code';
 
 export const routeNotFound = (adapters: GetLoggedInScietyUserPorts): Middleware => async (context, next) => {
   if (context._matchedRoute === undefined) {
-    context.status = StatusCodes.NOT_FOUND;
-    context.body = pipe(
-      {
-        title: 'Page not found',
-        content: renderErrorPage(toHtmlFragment('Page not found.')),
-      },
-      standardPageLayout(getLoggedInScietyUser(adapters, context)),
+    const htmlResponse = constructHtmlResponse(
+      getLoggedInScietyUser(adapters, context),
+      standardPageLayout,
+    )(
+      E.left({
+        type: DE.notFound,
+        message: toHtmlFragment('Page not found.'),
+      }),
     );
+    context.status = getHttpStatusCode(htmlResponse);
+    context.body = htmlResponse.content;
   }
   await next();
 };
