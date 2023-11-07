c4838c414a61d398688f96d4dadbba7c79aa6318
Kevin Rutherford <kevin@rutherford-software.com>
2023-11-07 15:46:43 +0000

    Inline to remove a confusing level of indirection #2808

--- src/shared-components/standard-page-layout.ts
+++ src/shared-components/standard-page-layout.ts
@@ -1,12 +1,25 @@
-import { fullWidthPageLayout } from './full-width-page-layout';
+import { pipe } from 'fp-ts/function';
 import { toHtmlFragment } from '../types/html-fragment';
 import { PageLayout } from '../html-pages/page-layout';
+import { wrapInHtmlDocument } from '../html-pages/wrap-in-html-document';
+import { siteFooter } from './site-footer';
+import { siteHeader } from './site-header';
 
-export const standardPageLayout: PageLayout = (user) => (page) => (
-  fullWidthPageLayout(user)(
-    {
-      ...page,
-      content: toHtmlFragment(`<div class="sciety-grid-two-columns">${page.content}</div>`),
-    },
-  )
+export const standardPageLayout: PageLayout = (user) => (page) => pipe(
+  `
+    <div class="standard-page-container">
+      ${siteHeader(user)}
+
+      <main id="mainContent">
+        <div class="page-content">
+          <div class="sciety-grid-two-columns">
+            ${page.content}
+          </div>
+        </div>
+      </main>
+      ${siteFooter(user)}
+    </div>
+  `,
+  toHtmlFragment,
+  wrapInHtmlDocument(user, page),
 );
