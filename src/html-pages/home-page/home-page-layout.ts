c033298301792019c345514ffe3d5fe8f5fa7b5a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-11-07 15:36:32 +0000

    Reshape home-page-layout to enable splitting of responsibilities #2808

--- src/html-pages/home-page/home-page-layout.ts
+++ src/html-pages/home-page/home-page-layout.ts
@@ -1,23 +1,12 @@
 import { pipe } from 'fp-ts/function';
-import * as O from 'fp-ts/Option';
-import { googleTagManagerNoScript } from '../../shared-components/analytics';
-import { head } from '../../shared-components/head';
 import { siteFooter } from '../../shared-components/site-footer';
 import { siteHeader } from '../../shared-components/site-header';
 import { PageLayout } from '../page-layout';
 import { toHtmlFragment } from '../../types/html-fragment';
+import { wrapInHtmlDocument } from '../wrap-in-html-document';
 
-export const homePageLayout: PageLayout = (user) => (page) => toHtmlFragment(`<!doctype html>
-<html lang="en" prefix="og: http://ogp.me/ns#">
-  ${head(
-    pipe(
-      user,
-      O.map((u) => u.id),
-    ),
-    page,
-  )}
-<body>
-  ${googleTagManagerNoScript()}
+export const homePageLayout: PageLayout = (user) => (page) => pipe(
+  `
   <div class="home-page-container">
     ${siteHeader(user, 'dark')}
 
@@ -27,9 +16,7 @@ export const homePageLayout: PageLayout = (user) => (page) => toHtmlFragment(`<!
 
     ${siteFooter(user)}
   </div>
-
-  <script src="/static/behaviour.js"></script>
-
-</body>
-</html>
-`);
+`,
+  toHtmlFragment,
+  wrapInHtmlDocument(user, page),
+);
