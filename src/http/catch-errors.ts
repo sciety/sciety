550872419e9df3c6e7a48b21c7a2942394c71971
LinaKind <l.kinduryte@elifesciences.org>
2023-11-07 13:39:38 +0000

    Keep rolling out toErrorHtmlDocument #2808

--- src/http/catch-errors.ts
+++ src/http/catch-errors.ts
@@ -1,9 +1,6 @@
 import { Middleware } from '@koa/router';
-import * as O from 'fp-ts/Option';
 import { StatusCodes } from 'http-status-codes';
-import { renderOopsMessage } from '../html-pages/render-oops-message';
-import { standardPageLayout } from '../shared-components/standard-page-layout';
-import { toHtmlFragment } from '../types/html-fragment';
+import { toErrorHtmlDocument } from '../html-pages/to-error-html-document';
 
 type Logger = (level: 'error', message: string, payload: Record<string, unknown>) => void;
 
@@ -15,10 +12,7 @@ export const catchErrors = (logger: Logger, logMessage: string, pageMessage: str
       logger('error', logMessage, { error });
 
       context.response.status = StatusCodes.INTERNAL_SERVER_ERROR;
-      context.response.body = standardPageLayout(O.none)({
-        title: 'Error',
-        content: renderOopsMessage(toHtmlFragment(pageMessage)),
-      });
+      context.response.body = toErrorHtmlDocument(pageMessage);
     }
   }
 );
