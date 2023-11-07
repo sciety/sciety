26c01e3329daba2c75409de1907aa66ff0f6f17e
Giorgio Sironi <g.sironi@elifesciences.org>
2023-11-07 15:39:36 +0000

    Remove duplication from another page layout #2808

--- src/html-pages/create-user-account-form-page/create-user-account-form-page-layout.ts
+++ src/html-pages/create-user-account-form-page/create-user-account-form-page-layout.ts
@@ -1,23 +1,12 @@
 import { pipe } from 'fp-ts/function';
-import * as O from 'fp-ts/Option';
-import { googleTagManagerNoScript } from '../../shared-components/analytics';
-import { head } from '../../shared-components/head';
 import { siteFooter } from '../../shared-components/site-footer';
 import { siteHeader } from '../../shared-components/site-header';
 import { PageLayout } from '../page-layout';
 import { toHtmlFragment } from '../../types/html-fragment';
+import { wrapInHtmlDocument } from '../wrap-in-html-document';
 
-export const createUserAccountFormPageLayout: PageLayout = (user) => (page) => toHtmlFragment(`<!doctype html>
-<html lang="en" prefix="og: http://ogp.me/ns#">
-  ${head(
-    pipe(
-      user,
-      O.map((u) => u.id),
-    ),
-    page,
-  )}
-<body>
-  ${googleTagManagerNoScript()}
+export const createUserAccountFormPageLayout: PageLayout = (user) => (page) => pipe(
+  `
   <div class="create-user-account-form-page__container">
     ${siteHeader(user)}
 
@@ -29,9 +18,7 @@ export const createUserAccountFormPageLayout: PageLayout = (user) => (page) => t
 
     ${siteFooter(user)}
   </div>
-
-  <script src="/static/behaviour.js"></script>
-
-</body>
-</html>
-`);
+  `,
+  toHtmlFragment,
+  wrapInHtmlDocument(user, page),
+);
