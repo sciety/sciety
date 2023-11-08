85d5760c61199034a880f9ceee3ecdb5fd917e9f
LinaKind <l.kinduryte@elifesciences.org>
2023-11-08 13:32:33 +0000

    Avoid dependency on complete html documents when deciding status code #2808

--- src/http/get-http-status-code.ts
+++ src/http/get-http-status-code.ts
@@ -2,18 +2,14 @@ import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { StatusCodes } from 'http-status-codes';
 import * as DE from '../types/data-error';
-import { HtmlResponse } from '../html-pages/construct-html-response';
 
-export const getHttpStatusCode = (htmlResponse: HtmlResponse): StatusCodes => pipe(
-  htmlResponse.error,
+export const getHttpStatusCode = (error: O.Option<DE.DataError>): StatusCodes => pipe(
+  error,
   O.match(
     () => StatusCodes.OK,
-    (error) => pipe(
-      error,
-      DE.match({
-        notFound: () => StatusCodes.NOT_FOUND,
-        unavailable: () => StatusCodes.SERVICE_UNAVAILABLE,
-      }),
-    ),
+    DE.match({
+      notFound: () => StatusCodes.NOT_FOUND,
+      unavailable: () => StatusCodes.SERVICE_UNAVAILABLE,
+    }),
   ),
 );
--- src/http/set-response-on-context.ts
+++ src/http/set-response-on-context.ts
@@ -3,7 +3,7 @@ import { HtmlResponse } from '../html-pages/construct-html-response';
 import { getHttpStatusCode } from './get-http-status-code';
 
 export const setResponseOnContext = (htmlResponse: HtmlResponse, context: ParameterizedContext): void => {
-  context.response.status = getHttpStatusCode(htmlResponse);
+  context.response.status = getHttpStatusCode(htmlResponse.error);
   context.response.type = 'html';
   context.response.body = htmlResponse.document;
 };
