18d0009dd2d2b2ff0a1f47d1d019fc6dcbe80889
Giorgio Sironi <g.sironi@elifesciences.org>
2023-11-08 12:02:56 +0000

    Inline to make it obvious what needs changing #2808

--- src/http/html-fragment-handler.ts
+++ src/http/html-fragment-handler.ts
@@ -1,11 +1,12 @@
 import { Middleware } from '@koa/router';
+import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
 import { pipe } from 'fp-ts/function';
 import { standardPageLayout } from '../shared-components/standard-page-layout';
 import { ConstructPage } from '../html-pages/construct-page';
 import { PageLayout } from '../html-pages/page-layout';
-import { constructHtmlResponse } from '../html-pages/construct-html-response';
+import { pageToSuccessResponse, toErrorResponse } from '../html-pages/construct-html-response';
 import { setResponseOnContext } from './set-response-on-context';
 
 export const htmlFragmentHandler = (
@@ -15,7 +16,12 @@ export const htmlFragmentHandler = (
   const response = await pipe(
     context.params,
     handler,
-    T.map(constructHtmlResponse(O.none, pageLayout)),
+    T.map(
+      E.fold(
+        toErrorResponse(O.none),
+        pageToSuccessResponse(O.none, pageLayout),
+      ),
+    ),
   )();
 
   setResponseOnContext(response, context);
