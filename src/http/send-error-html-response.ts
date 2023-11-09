0829c9d95a625d1033d47c3e4599674d6246f3b8
David Moulton <d.moulton@elifesciences.org>
2023-11-09 11:43:16 +0000

    Declare intended dependencies to achieve new responsibility #2808

--- src/http/send-error-html-response.ts
+++ src/http/send-error-html-response.ts
@@ -3,6 +3,10 @@ import { ParameterizedContext } from 'koa';
 import * as O from 'fp-ts/Option';
 import { toErrorHtmlDocument } from '../html-pages/to-error-html-document';
 import { detectClientClassification } from './detect-client-classification';
+import { Ports as GetLoggedInScietyUserPorts } from './authentication-and-logging-in-of-sciety-users';
+
+// ts-unused-exports:disable-next-line
+export type Dependencies = GetLoggedInScietyUserPorts;
 
 export const sendErrorHtmlResponse = (
   context: ParameterizedContext,
