c6dae76b88849ffe4408f50a1f2441e6050f10d9
Giorgio Sironi <g.sironi@elifesciences.org>
2023-11-09 10:46:49 +0000

    Extract responsibilities to set status code and body from one more place #2808

--- src/http/load-static-file.ts
+++ src/http/load-static-file.ts
@@ -2,9 +2,8 @@ import path from 'path';
 import { Middleware } from '@koa/router';
 import { StatusCodes } from 'http-status-codes';
 import send from 'koa-send';
-import { toErrorHtmlDocument } from '../html-pages/to-error-html-document';
 import { Logger } from '../infrastructure';
-import { detectClientClassification } from './detect-client-classification';
+import { sendErrorHtmlResponse } from './send-error-html-response';
 
 type KoaSendError = {
   status: number,
@@ -16,6 +15,7 @@ const isKoaSendError = (variableToCheck: unknown): variableToCheck is KoaSendErr
 
 export const loadStaticFile = (logger: Logger): Middleware => async (context) => {
   let pageMessage = 'Something went wrong, please try again.';
+  let errorStatus: StatusCodes;
   const file = context.params.file.replace('editorial-communities', 'groups');
   const staticFolder = path.resolve(__dirname, '../../static');
   try {
@@ -24,11 +24,11 @@ export const loadStaticFile = (logger: Logger): Middleware => async (context) =>
     if (isKoaSendError(error) && error.status === 404) {
       logger('warn', 'Static file not found', { error });
       pageMessage = 'File not found';
-      context.response.status = 404;
+      errorStatus = 404;
     } else {
       logger('error', 'Static file could not be read', { error });
-      context.response.status = StatusCodes.INTERNAL_SERVER_ERROR;
+      errorStatus = StatusCodes.INTERNAL_SERVER_ERROR;
     }
-    context.response.body = toErrorHtmlDocument(pageMessage, detectClientClassification(context));
+    sendErrorHtmlResponse(context, errorStatus, pageMessage);
   }
 };
