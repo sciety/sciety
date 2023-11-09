62913f487632003ad2c2d84e3797e7b5777a71b5
LinaKind <l.kinduryte@elifesciences.org>
2023-11-09 13:26:22 +0000

    Remove one dependency on html pages #2808

--- src/http/route-not-found.ts
+++ src/http/route-not-found.ts
@@ -1,25 +1,11 @@
 import { Middleware } from '@koa/router';
-import * as E from 'fp-ts/Either';
-import { standardPageLayout } from '../shared-components/standard-page-layout';
-import { toHtmlFragment } from '../types/html-fragment';
-import { getLoggedInScietyUser, Ports as GetLoggedInScietyUserPorts } from './authentication-and-logging-in-of-sciety-users';
-import { constructHtmlResponse } from '../html-pages/construct-html-response';
-import * as DE from '../types/data-error';
-import { getHttpStatusCode } from './get-http-status-code';
+import { StatusCodes } from 'http-status-codes';
+import { Ports as GetLoggedInScietyUserPorts } from './authentication-and-logging-in-of-sciety-users';
 import { sendErrorHtmlResponse } from './send-error-html-response';
 
 export const routeNotFound = (adapters: GetLoggedInScietyUserPorts): Middleware => async (context, next) => {
   if (context._matchedRoute === undefined) {
-    const htmlResponse = constructHtmlResponse(
-      getLoggedInScietyUser(adapters, context),
-      standardPageLayout,
-    )(
-      E.left({
-        type: DE.notFound,
-        message: toHtmlFragment('Page not found.'),
-      }),
-    );
-    sendErrorHtmlResponse(adapters, context, getHttpStatusCode(htmlResponse.error), 'Page not found.');
+    sendErrorHtmlResponse(adapters, context, StatusCodes.NOT_FOUND, 'Page not found.');
   }
   await next();
 };
