932816c6d9537b621a259b557b44de8f74328f27
LinaKind <l.kinduryte@elifesciences.org>
2023-11-13 14:39:32 +0000

    Create an as yet unused type for unsafe user input #2834

---
+++ src/types/unsafe-user-input.ts
@@ -0,0 +1,42 @@
+import { pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
+import * as B from 'fp-ts/boolean';
+
+const notEmptyRegex = /^[^<>"]+$/;
+const emptyRegex = /^[^<>"]*$/;
+
+type UnsafeUserInputBrand = {
+  readonly UnsafeUserInput: unique symbol,
+};
+
+type Config = {
+  maxInputLength: number,
+  allowEmptyInput?: boolean,
+};
+
+const areInputCharactersSafe = (
+  config: Config,
+  input: string,
+) => pipe(
+  config.allowEmptyInput ?? false,
+  B.fold(
+    () => !!notEmptyRegex.exec(input),
+    () => !!emptyRegex.exec(input),
+  ),
+);
+
+const isInputShortEnough = (config: Config, input: string) => input.length <= config.maxInputLength;
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+// ts-unused-exports:disable-next-line
+export const unsafeUserInputCodec = (config: Config) => t.brand(
+  t.string,
+  (input): input is t.Branded<string, UnsafeUserInputBrand> => (
+    areInputCharactersSafe(config, input) && isInputShortEnough(config, input)
+  ),
+  'UnsafeUserInput',
+);
+/* eslint-enable @typescript-eslint/explicit-module-boundary-types */
+
+// ts-unused-exports:disable-next-line
+export type UnsafeUserInput = t.TypeOf<ReturnType<typeof unsafeUserInputCodec>>;
