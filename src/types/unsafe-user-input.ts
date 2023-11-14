5fff71471481eef56b5fac4b7aca62f4845d6307
David Moulton <d.moulton@elifesciences.org>
2023-11-14 11:52:22 +0000

    Remove responsibility of forbidding special characters #2834

--- src/types/unsafe-user-input.ts
+++ src/types/unsafe-user-input.ts
@@ -1,19 +1,15 @@
 import * as t from 'io-ts';
 
-const emptyRegex = /^[^<>"]*$/;
-
 type UnsafeUserInputBrand = {
   readonly UnsafeUserInput: unique symbol,
 };
 
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 // ts-unused-exports:disable-next-line
-export const unsafeUserInputCodec = () => t.brand(
+export const unsafeUserInputCodec = t.brand(
   t.string,
-  (input): input is t.Branded<string, UnsafeUserInputBrand> => (!!emptyRegex.exec(input)),
+  (input): input is t.Branded<string, UnsafeUserInputBrand> => true,
   'UnsafeUserInput',
 );
-/* eslint-enable @typescript-eslint/explicit-module-boundary-types */
 
 // ts-unused-exports:disable-next-line
-export type UnsafeUserInput = t.TypeOf<ReturnType<typeof unsafeUserInputCodec>>;
+export type UnsafeUserInput = t.TypeOf<typeof unsafeUserInputCodec>;
