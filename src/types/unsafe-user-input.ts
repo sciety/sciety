1c3aac41757e85c270b65f724296a3832fdc054e
LinaKind <l.kinduryte@elifesciences.org>
2023-11-14 11:44:48 +0000

    Remove undesired responsibility from a codec #2834

--- src/types/unsafe-user-input.ts
+++ src/types/unsafe-user-input.ts
@@ -1,37 +1,16 @@
-import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
-import * as B from 'fp-ts/boolean';
 
-const notEmptyRegex = /^[^<>"]+$/;
 const emptyRegex = /^[^<>"]*$/;
 
 type UnsafeUserInputBrand = {
   readonly UnsafeUserInput: unique symbol,
 };
 
-type Config = {
-  maxInputLength: number,
-  allowEmptyInput?: boolean,
-};
-
-const areInputCharactersSafe = (
-  config: Config,
-  input: string,
-) => pipe(
-  config.allowEmptyInput ?? false,
-  B.fold(
-    () => !!notEmptyRegex.exec(input),
-    () => !!emptyRegex.exec(input),
-  ),
-);
-
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 // ts-unused-exports:disable-next-line
-export const unsafeUserInputCodec = (config: Config) => t.brand(
+export const unsafeUserInputCodec = () => t.brand(
   t.string,
-  (input): input is t.Branded<string, UnsafeUserInputBrand> => (
-    areInputCharactersSafe(config, input)
-  ),
+  (input): input is t.Branded<string, UnsafeUserInputBrand> => (!!emptyRegex.exec(input)),
   'UnsafeUserInput',
 );
 /* eslint-enable @typescript-eslint/explicit-module-boundary-types */
