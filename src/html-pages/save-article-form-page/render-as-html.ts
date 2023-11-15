06edd560fc384913cfa7c3bbb1762666e33121f4
Giorgio Sironi <g.sironi@elifesciences.org>
2023-11-15 10:23:41 +0000

    Improve language of save article form page to advise users annotating is public #2840

--- src/html-pages/save-article-form-page/render-as-html.ts
+++ src/html-pages/save-article-form-page/render-as-html.ts
@@ -55,7 +55,8 @@ export const renderAsHtml = (viewModel: ViewModel): HtmlPage => ({
     <input type="hidden" name="${articleIdFieldName}" value="${viewModel.article.id.value}">
     ${renderDependingOnUserListCount(viewModel.userLists, viewModel.article.name)}
     <section>
-      <label for="annotationContent" class="standard-form__sub_heading">Why are you saving this article (optional)?</label>
+      <label for="annotationContent" class="standard-form__sub_heading">Why are you saving this article? <span style="font-weight: normal">(optional)</span></label>
+      <p class="standard-form__helper_text">Add a public comment to share with others what's interesting or important about this article.</p>
       <textarea id="annotationContent" name="annotation" rows="10"></textarea>
     </section>
     <button type="submit">
