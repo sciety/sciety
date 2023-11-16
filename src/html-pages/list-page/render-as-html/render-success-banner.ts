e33dd8a2b6d8df4e54d8bb320455736b789e07dc
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-11-16 10:04:12 +0000

    Style the success banner #2841

--- src/html-pages/list-page/render-as-html/render-success-banner.ts
+++ src/html-pages/list-page/render-as-html/render-success-banner.ts
@@ -1,8 +1,10 @@
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 
 const banner = `
-  <div>
-    <h3>Success</h3>
+  <div class="success-banner">
+    <div class="success-banner__heading">
+      <h2>Success</h2>
+    </div>
     <p>
       Your comment has been added. You are contributing to a network of researchers advancing open science.
     </p>
