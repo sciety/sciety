a014689916c096efdfc6a5068ce804095c1c52bd
LinaKind <l.kinduryte@elifesciences.org>
2023-11-16 10:50:32 +0000

    Extract success banner to shared components as its existence would have made this feature easier to implement #2841

--- src/html-pages/list-page/render-as-html/render-success-banner.ts
+++ src/html-pages/list-page/render-as-html/render-success-banner.ts
@@ -1,19 +1,9 @@
+import { successBanner } from '../../../shared-components/success-banner/success-banner';
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 import { ViewModel } from '../view-model';
 
-const banner = `
-  <div class="success-banner">
-    <div class="success-banner__heading">
-      <h2>Success</h2>
-    </div>
-    <p>
-      Your comment has been added. You are contributing to a network of researchers advancing open science.
-    </p>
-  </div>
-`;
-
 export const renderSuccessBanner = (viewModel: ViewModel): HtmlFragment => (
   viewModel.showAnnotationSuccessBanner
-    ? toHtmlFragment(banner)
+    ? successBanner('Your comment has been added. You are contributing to a network of researchers advancing open science.')
     : toHtmlFragment('')
 );
---
+++ src/shared-components/success-banner/success-banner.ts
@@ -0,0 +1,12 @@
+import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
+
+export const successBanner = (message: string): HtmlFragment => toHtmlFragment(`
+  <div class="success-banner">
+    <div class="success-banner__heading">
+      <h2>Success</h2>
+    </div>
+    <p>
+      ${message}
+    </p>
+  </div>
+`);
