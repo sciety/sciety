d9f4a761c0f1cc1ac62183fb6d9b90a93dd8c0f0
LinaKind <l.kinduryte@elifesciences.org>
2023-11-16 09:47:46 +0000

    Write some affirming copy #2841

--- src/html-pages/list-page/render-as-html/render-success-banner.ts
+++ src/html-pages/list-page/render-as-html/render-success-banner.ts
@@ -1,7 +1,16 @@
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 
+const banner = `
+  <div>
+    <h3>Success</h3>
+    <p>
+      Your comment has been added. You are contributing to a network of researchers advancing open science.
+    </p>
+  </div>
+`;
+
 export const renderSuccessBanner = (): HtmlFragment => (
   process.env.EXPERIMENT_ENABLED
-    ? toHtmlFragment('foo')
+    ? toHtmlFragment(banner)
     : toHtmlFragment('')
 );
