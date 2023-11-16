8c7c5d71023d9188616d4b77f42461c0e00887be
LinaKind <l.kinduryte@elifesciences.org>
2023-11-16 11:27:59 +0000

    Fix borked rebase #2841

--- src/html-pages/list-page/construct-view-model/params.ts
+++ src/html-pages/list-page/construct-view-model/params.ts
@@ -2,7 +2,7 @@ import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
 import { listIdCodec } from '../../../types/list-id';
 import { userIdCodec } from '../../../types/user-id';
-import { externalInputFieldNames } from '../../../standards';
+import { inputFieldNames } from '../../../standards';
 
 export const paramsCodec = t.type({
   page: tt.withFallback(tt.NumberFromString, 1),
@@ -10,7 +10,7 @@ export const paramsCodec = t.type({
   user: tt.optionFromNullable(t.type({
     id: userIdCodec,
   })),
-  [externalInputFieldNames.success]: tt.withFallback(tt.BooleanFromString, false),
+  [inputFieldNames.success]: tt.withFallback(tt.BooleanFromString, false),
 });
 
 export type Params = t.TypeOf<typeof paramsCodec>;
