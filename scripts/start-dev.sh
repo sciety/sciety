0423e205518bfc0c4c6b853e9504fba1ab39f126
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-11-17 16:17:39 +0000

    Fix make dev by switching from ts-node-dev to tsx

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -5,7 +5,7 @@ rm -f static/style.css static/style.css.map
 node_modules/.bin/sass --watch --embed-sources --load-path=src src/sass/style.scss:static/style.css &
 pid1=$!
 
-node_modules/.bin/ts-node-dev --exit-child --transpile-only --watch data,static/groups ./src/index.ts &
+node_modules/.bin/tsx watch ./src/index.ts &
 pid2=$!
 
 trap "echo 'Caught signal; killing watchers'; kill ${pid1} ${pid2}; exit 1" INT QUIT TERM
