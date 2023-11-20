b9ba0a00d76d1e176fdc1b1bc151fdc2bf07a545
Kevin Rutherford <kevin@rutherford-software.com>
2023-11-20 10:45:49 +0000

    Extract and name inline behaviour #2844

--- src/html-pages/list-page/render-as-html/render-as-html.ts
+++ src/html-pages/list-page/render-as-html/render-as-html.ts
@@ -1,16 +1,19 @@
 import { pipe } from 'fp-ts/function';
 import striptags from 'striptags';
+import { RawUserInput } from '../../../read-models/annotations/handle-event';
 import { toHtmlFragment } from '../../../types/html-fragment';
 import { HtmlPage, NotHtml } from '../../html-page';
 import { ViewModel } from '../view-model';
 import { renderPage } from './render-page';
 
+const stripHtml = (input: RawUserInput): NotHtml => striptags(input.content) as NotHtml;
+
 export const renderAsHtml = (viewModel: ViewModel): HtmlPage => ({
   title: viewModel.name,
   openGraph: {
     title: viewModel.name,
-    description: striptags(viewModel.description.content) as NotHtml,
+    description: stripHtml(viewModel.description),
   },
-  description: striptags(viewModel.description.content) as NotHtml,
+  description: stripHtml(viewModel.description),
   content: pipe(viewModel, renderPage, toHtmlFragment),
 });
