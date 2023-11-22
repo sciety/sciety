dc1c5a643692c0110ccff66bc81670ccc3f50f4d
LinaKind <l.kinduryte@elifesciences.org>
2023-11-22 13:53:08 +0000

    Decouple DoiOFArticleExpression from the articleId type #2863

--- src/types/doi-of-article-expression.ts
+++ src/types/doi-of-article-expression.ts
@@ -1,3 +1,40 @@
-import { ArticleId } from './article-id';
+import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
+import { flow, pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
 
-export type DoiOfArticleExpression = ArticleId;
+export class DoiOfArticleExpression {
+  readonly value: string;
+
+  constructor(input: string) {
+    const doi = input.replace(/^doi:/, '');
+
+    if (!doi) {
+      throw new Error(`'${input}' is not a possible DOI`);
+    }
+
+    this.value = doi;
+  }
+}
+
+export const fromString = (
+  value: string,
+): O.Option<DoiOfArticleExpression> => O.tryCatch(() => new DoiOfArticleExpression(value));
+
+export const toString = (value: DoiOfArticleExpression): string => `doi:${value.value}`;
+
+export const doiOfArticleExpressionCodec = new t.Type(
+  'doiOfArticleExpressionCodec',
+  (u): u is DoiOfArticleExpression => u instanceof DoiOfArticleExpression,
+  (u, c) => pipe(
+    t.string.validate(u, c),
+    E.chain(flow(
+      fromString,
+      O.fold(
+        () => t.failure(u, c),
+        t.success,
+      ),
+    )),
+  ),
+  (a) => toString(a),
+);
