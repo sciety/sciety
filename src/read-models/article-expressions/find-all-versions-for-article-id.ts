2303c2ad66aa41c15b61ea02b0c18747473a418e
Giorgio Sironi <g.sironi@elifesciences.org>
2023-11-24 09:51:05 +0000

    Use the hardcoded read model for one query #2863

--- src/read-models/article-expressions/find-all-versions-for-article-id.ts
+++ src/read-models/article-expressions/find-all-versions-for-article-id.ts
@@ -2,6 +2,7 @@
 import { URL } from 'url';
 import * as O from 'fp-ts/Option';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
+import { pipe } from 'fp-ts/function';
 import { ReadModel } from './handle-event';
 import { ArticleId } from '../../types/article-id';
 
@@ -31,13 +32,7 @@ type FindAllVersionsForArticleId = (
 
 export const findAllVersionsForArticleId = (
   readModel: ReadModel,
-): FindAllVersionsForArticleId => () => O.some([{
-  source: new URL('https://doi.org/10.1099/acmi.0.000530.v1'),
-  publishedAt: new Date('2022-11-29'),
-  version: 1,
-},
-{
-  source: new URL('https://doi.org/10.1099/acmi.0.000530.v2'),
-  publishedAt: new Date('2023-10-20'),
-  version: 2,
-}]);
+): FindAllVersionsForArticleId => () => pipe(
+  hardcodedReadModel.get('uuid:30374f3c-92dc-4692-aac0-ed95883b9ea0'),
+  O.fromNullable,
+);
