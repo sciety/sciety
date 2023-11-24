c819aebdfb09b5e0ee074fd31b836c2b88599ded
LinaKind <l.kinduryte@elifesciences.org>
2023-11-24 09:44:38 +0000

    Introduce a read model type #2863

--- src/read-models/article-expressions/find-all-versions-for-article-id.ts
+++ src/read-models/article-expressions/find-all-versions-for-article-id.ts
@@ -5,9 +5,29 @@ import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import { ReadModel } from './handle-event';
 import { ArticleId } from '../../types/article-id';
 
+type ReadModelEntry = { source: URL, publishedAt: Date, version: number };
+
+type HardcodedReadModel = Map<string, RNEA.ReadonlyNonEmptyArray<ReadModelEntry>>;
+
+const hardcodedReadModel: HardcodedReadModel = new Map();
+
+hardcodedReadModel.set(
+  'uuid:30374f3c-92dc-4692-aac0-ed95883b9ea0',
+  [{
+    source: new URL('https://doi.org/10.1099/acmi.0.000530.v1'),
+    publishedAt: new Date('2022-11-29'),
+    version: 1,
+  },
+  {
+    source: new URL('https://doi.org/10.1099/acmi.0.000530.v2'),
+    publishedAt: new Date('2023-10-20'),
+    version: 2,
+  }],
+);
+
 type FindAllVersionsForArticleId = (
   articleId: ArticleId,
-) => O.Option<RNEA.ReadonlyNonEmptyArray<{ source: URL, publishedAt: Date, version: number }>>;
+) => O.Option<RNEA.ReadonlyNonEmptyArray<ReadModelEntry>>;
 
 export const findAllVersionsForArticleId = (
   readModel: ReadModel,
