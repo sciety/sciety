4ed261bdb8c45e0b1732e175aaeb7f04e0f7f88d
LinaKind <l.kinduryte@elifesciences.org>
2023-11-24 10:41:32 +0000

    Implement a real query #2863

--- src/read-models/article-expressions/find-expression-of-article-as-doi.ts
+++ src/read-models/article-expressions/find-expression-of-article-as-doi.ts
@@ -1,14 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import { ArticleId } from '../../types/article-id';
 import { ArticleExpressionDoi } from '../../types/article-expression-doi';
 import { ReadModel } from './handle-event';
+import { hardcodedReadModel } from './hardcoded-read-model';
 
 export const findExpressionOfArticleAsDoi = (
   readModel: ReadModel,
 ) => (articleId: ArticleId): O.Option<ArticleExpressionDoi> => {
   if (articleId.value.startsWith('uuid:')) {
-    return O.some(new ArticleExpressionDoi('10.1099/acmi.0.000530.v1'));
+    return pipe(
+      hardcodedReadModel.get(articleId.value),
+      O.fromNullable,
+      O.map(RNEA.last),
+      O.map((readModelEntry) => readModelEntry.articleExpressionDoi),
+    );
   }
   return O.some(articleId);
 };
