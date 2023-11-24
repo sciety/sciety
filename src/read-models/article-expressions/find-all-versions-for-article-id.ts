1b602f45fc58dbe8d2670370f010ef7a99c1861c
Giorgio Sironi <g.sironi@elifesciences.org>
2023-11-24 10:15:00 +0000

    Extract a hardcoded read model in preparation for reuse #2863

--- src/read-models/article-expressions/find-all-versions-for-article-id.ts
+++ src/read-models/article-expressions/find-all-versions-for-article-id.ts
@@ -1,38 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { URL } from 'url';
 import * as O from 'fp-ts/Option';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import { pipe } from 'fp-ts/function';
 import { ReadModel } from './handle-event';
 import { ArticleId } from '../../types/article-id';
-import { ArticleExpressionDoi } from '../../types/article-expression-doi';
-
-type ReadModelEntry = {
-  source: URL,
-  articleExpressionDoi: ArticleExpressionDoi,
-  publishedAt: Date,
-  version: number,
-};
-
-type HardcodedReadModel = Map<string, RNEA.ReadonlyNonEmptyArray<ReadModelEntry>>;
-
-const hardcodedReadModel: HardcodedReadModel = new Map();
-
-hardcodedReadModel.set(
-  'uuid:30374f3c-92dc-4692-aac0-ed95883b9ea0',
-  [{
-    source: new URL('https://doi.org/10.1099/acmi.0.000530.v1'),
-    articleExpressionDoi: new ArticleExpressionDoi('10.1099/acmi.0.000530.v1'),
-    publishedAt: new Date('2022-11-29'),
-    version: 1,
-  },
-  {
-    source: new URL('https://doi.org/10.1099/acmi.0.000530.v2'),
-    articleExpressionDoi: new ArticleExpressionDoi('10.1099/acmi.0.000530.v2'),
-    publishedAt: new Date('2023-10-20'),
-    version: 2,
-  }],
-);
+import { ReadModelEntry, hardcodedReadModel } from './hardcoded-read-model';
 
 type FindAllVersionsForArticleId = (
   articleId: ArticleId,
---
+++ src/read-models/article-expressions/hardcoded-read-model.ts
@@ -0,0 +1,30 @@
+import { URL } from 'url';
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
+import { ArticleExpressionDoi } from '../../types/article-expression-doi';
+
+export type ReadModelEntry = {
+  source: URL,
+  articleExpressionDoi: ArticleExpressionDoi,
+  publishedAt: Date,
+  version: number,
+};
+
+type HardcodedReadModel = Map<string, RNEA.ReadonlyNonEmptyArray<ReadModelEntry>>;
+
+export const hardcodedReadModel: HardcodedReadModel = new Map();
+
+hardcodedReadModel.set(
+  'uuid:30374f3c-92dc-4692-aac0-ed95883b9ea0',
+  [{
+    source: new URL('https://doi.org/10.1099/acmi.0.000530.v1'),
+    articleExpressionDoi: new ArticleExpressionDoi('10.1099/acmi.0.000530.v1'),
+    publishedAt: new Date('2022-11-29'),
+    version: 1,
+  },
+  {
+    source: new URL('https://doi.org/10.1099/acmi.0.000530.v2'),
+    articleExpressionDoi: new ArticleExpressionDoi('10.1099/acmi.0.000530.v2'),
+    publishedAt: new Date('2023-10-20'),
+    version: 2,
+  }],
+);
