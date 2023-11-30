dc2ec8b4c250de6ece63cbfde998296334b2c9ca
Giorgio Sironi <g.sironi@elifesciences.org>
2023-11-30 15:17:29 +0000

    Allow the variant of a paper id to be determined at runtime #2870

--- src/third-parties/paper-id.ts
+++ src/third-parties/paper-id.ts
@@ -3,10 +3,10 @@ import { ArticleId } from '../types/article-id';
 
 export type PaperIdThatIsADoi = string & { readonly PaperIdThatIsADoi: unique symbol };
 
-export const getDoiPortion = (paperId: PaperIdThatIsADoi): string => paperId;
+export const getDoiPortion = (paperId: PaperIdThatIsADoi): string => paperId.split(':')[1];
 
 export const toArticleId = (paperId: PaperIdThatIsADoi): ArticleId => new ArticleId(getDoiPortion(paperId));
 
 export type PaperId = PaperIdThatIsADoi;
 
-export const fromNonEmptyString = (candidate: NonEmptyString): PaperId => `${candidate}` as PaperIdThatIsADoi;
+export const fromNonEmptyString = (candidate: NonEmptyString): PaperId => `doi:${candidate}` as PaperIdThatIsADoi;
---
+++ test/third-parties/paper-id.test.ts
@@ -0,0 +1,19 @@
+import { pipe } from 'fp-ts/function';
+import { NonEmptyString } from 'io-ts-types';
+import { PaperId } from '../../src/third-parties';
+import { arbitraryString } from '../helpers';
+
+describe('paper-id', () => {
+  describe('when a construction is followed by a destruction', () => {
+    const original = arbitraryString() as NonEmptyString;
+    const result = pipe(
+      original,
+      PaperId.fromNonEmptyString,
+      PaperId.getDoiPortion,
+    );
+
+    it('results in the original value', () => {
+      expect(result).toBe(original);
+    });
+  });
+});
