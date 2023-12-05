8c9fde07b9baf82696261f45ef892010e3162413
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-12-05 09:30:15 +0000

    Nest older test in current structure #2884

--- test/third-parties/paper-id.test.ts
+++ test/third-parties/paper-id.test.ts
@@ -8,22 +8,6 @@ import { arbitraryArticleId } from '../types/article-id.helper';
 import { shouldNotBeCalled } from '../should-not-be-called';
 
 describe('paper-id', () => {
-  describe('when a construction is followed by a destruction', () => {
-    const original = arbitraryArticleId().value;
-    const result = pipe(
-      original,
-      PaperId.paperIdCodec.decode,
-      E.getOrElseW(shouldNotBeCalled),
-      O.fromPredicate(PaperId.isDoi),
-      O.getOrElseW(shouldNotBeCalled),
-      PaperId.getDoiPortion,
-    );
-
-    it('results in the original value', () => {
-      expect(result).toBe(original);
-    });
-  });
-
   describe('given a uuid', () => {
     const input = v4() as NonEmptyString;
 
@@ -42,6 +26,21 @@ describe('paper-id', () => {
 
   describe('given a doi', () => {
     const input = arbitraryArticleId().value as NonEmptyString;
+
+    describe('when decoded, followed by getDoiPortion', () => {
+      const result = pipe(
+        input,
+        PaperId.paperIdCodec.decode,
+        E.getOrElseW(shouldNotBeCalled),
+        O.fromPredicate(PaperId.isDoi),
+        O.getOrElseW(shouldNotBeCalled),
+        PaperId.getDoiPortion,
+      );
+
+      it('results in the original value', () => {
+        expect(result).toBe(input);
+      });
+    });
 
     describe('fromNonEmptyString', () => {
       const paperId = PaperId.fromNonEmptyString(input);
