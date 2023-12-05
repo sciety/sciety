162933fa27459d4243ac881ddc64a16ee17e5f33
David Moulton <d.moulton@elifesciences.org>
2023-12-05 11:42:01 +0000

    Make further improvements to cohesion #2884

--- src/third-parties/paper-id-that-is-a-doi.ts
+++ src/third-parties/paper-id-that-is-a-doi.ts
@@ -2,6 +2,7 @@ import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import { flow, pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
+import { ArticleId } from '../types/article-id';
 
 export const isDoi = (input: unknown): input is PaperIdThatIsADoi => typeof input === 'string' && input.startsWith('doi:');
 
@@ -24,3 +25,9 @@ export const paperIdThatIsADoiCodec = new t.Type<PaperIdThatIsADoi, string, unkn
 );
 
 export type PaperIdThatIsADoi = string & { readonly PaperIdThatIsADoi: unique symbol };
+
+export const getDoiPortion = (paperId: PaperIdThatIsADoi): string => paperId.split(':')[1];
+
+export const toArticleId = (paperId: PaperIdThatIsADoi): ArticleId => new ArticleId(getDoiPortion(paperId));
+
+export const fromArticleId = (articleId: ArticleId): PaperIdThatIsADoi => `doi:${articleId.value}` as PaperIdThatIsADoi;
--- src/third-parties/paper-id.ts
+++ src/third-parties/paper-id.ts
@@ -1,16 +1,11 @@
 import * as t from 'io-ts';
-import { ArticleId } from '../types/article-id';
 import { paperIdThatIsAUuidCodec } from './paper-id-that-is-a-uuid';
-import { PaperIdThatIsADoi, paperIdThatIsADoiCodec } from './paper-id-that-is-a-doi';
+import { paperIdThatIsADoiCodec } from './paper-id-that-is-a-doi';
 
 export { isUuid } from './paper-id-that-is-a-uuid';
-export { isDoi, PaperIdThatIsADoi } from './paper-id-that-is-a-doi';
-
-export const getDoiPortion = (paperId: PaperIdThatIsADoi): string => paperId.split(':')[1];
-
-export const toArticleId = (paperId: PaperIdThatIsADoi): ArticleId => new ArticleId(getDoiPortion(paperId));
-
-export const fromArticleId = (articleId: ArticleId): PaperIdThatIsADoi => `doi:${articleId.value}` as PaperIdThatIsADoi;
+export {
+  isDoi, PaperIdThatIsADoi, getDoiPortion, toArticleId, fromArticleId,
+} from './paper-id-that-is-a-doi';
 
 export const paperIdCodec = t.union([paperIdThatIsADoiCodec, paperIdThatIsAUuidCodec]);
 
