75eecbf3a400c3a1526f7a7e57ff2c7629925645
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-12-05 10:00:56 +0000

    Expose codec bug #2884

--- test/third-parties/paper-id.test.ts
+++ test/third-parties/paper-id.test.ts
@@ -1,31 +1,31 @@
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
-import { NonEmptyString } from 'io-ts-types';
 import { v4 } from 'uuid';
 import { PaperId } from '../../src/third-parties';
 import { arbitraryArticleId } from '../types/article-id.helper';
 import { shouldNotBeCalled } from '../should-not-be-called';
 
 describe('paper-id', () => {
-  describe('given a uuid', () => {
-    const input = v4() as NonEmptyString;
+  describe('when successfully decoding a UUID', () => {
+    const input = v4();
+    const decoded = pipe(
+      input,
+      PaperId.paperIdCodec.decode,
+      E.getOrElseW(shouldNotBeCalled),
+    );
 
-    describe('fromNonEmptyString', () => {
-      const paperId = PaperId.fromNonEmptyString(input);
+    it.failing('isDoi detects that the paper id is not a doi', () => {
+      expect(PaperId.isDoi(decoded)).toBe(false);
+    });
 
-      it('detects that the paper id is not a doi', () => {
-        expect(PaperId.isDoi(paperId)).toBe(false);
-      });
-
-      it('detects that the paper id is a uuid', () => {
-        expect(PaperId.isUuid(paperId)).toBe(true);
-      });
+    it.failing('isUuid detects that the paper id is a uuid', () => {
+      expect(PaperId.isUuid(decoded)).toBe(true);
     });
   });
 
   describe('when successfully decoding a DOI', () => {
-    const input = arbitraryArticleId().value as NonEmptyString;
+    const input = arbitraryArticleId().value;
     const decoded = pipe(
       input,
       PaperId.paperIdCodec.decode,
