0bbaf188ddb00e01f0303348c0693c7b89ba9898
Kevin Rutherford <kevin@rutherford-software.com>
2023-12-11 15:02:33 +0000

    Perform real requests to Crossref #2895

--- src/third-parties/crossref/fetch-all-paper-expressions-from-crossref.ts
+++ src/third-parties/crossref/fetch-all-paper-expressions-from-crossref.ts
@@ -3,6 +3,7 @@ import * as t from 'io-ts';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import * as TE from 'fp-ts/TaskEither';
+import * as E from 'fp-ts/Either';
 import * as TO from 'fp-ts/TaskOption';
 import { pipe } from 'fp-ts/function';
 import { ArticleVersion } from '../../types/article-version';
@@ -12,7 +13,7 @@ const crossrefRecordCodec = t.strict({
   message: t.strict({
     DOI: t.string,
     posted: t.strict({
-      'date-parts': t.tuple([t.number, t.number, t.number]),
+      'date-parts': t.readonlyArray(t.readonlyArray(t.number)),
     }),
     resource: t.strict({
       primary: t.strict({
@@ -24,68 +25,36 @@ const crossrefRecordCodec = t.strict({
 
 type CrossrefRecord = t.TypeOf<typeof crossrefRecordCodec>;
 
-const fetchIndividualRecord = (response: unknown) => pipe(
-  response,
-  crossrefRecordCodec.decode,
-  TE.fromEither,
+const fetchIndividualRecord = (queryCrossrefService: QueryExternalService) => (doi: string) => pipe(
+  `https://api.crossref.org/works/${doi}`,
+  queryCrossrefService(),
+  TE.chainEitherKW((response) => pipe(
+    response,
+    crossrefRecordCodec.decode,
+    E.mapLeft((e) => e),
+  )),
 );
 
 const toArticleVersion = (crossrefExpression: CrossrefRecord): ArticleVersion => ({
   version: parseInt(crossrefExpression.message.DOI.substring(crossrefExpression.message.DOI.length - 1), 10),
   publishedAt: new Date(
-    crossrefExpression.message.posted['date-parts'][0],
-    crossrefExpression.message.posted['date-parts'][1] - 1,
-    crossrefExpression.message.posted['date-parts'][2],
+    crossrefExpression.message.posted['date-parts'][0][0],
+    crossrefExpression.message.posted['date-parts'][0][1] - 1,
+    crossrefExpression.message.posted['date-parts'][0][2],
   ),
   source: new URL(crossrefExpression.message.resource.primary.URL),
 });
 
-type FetchAllPaperExpressionsFromCrossref = (queryCrossrefService: QueryExternalService, doi: string)
+type FetchAllPaperExpressions = (queryCrossrefService: QueryExternalService, doi: string)
 => TO.TaskOption<RNEA.ReadonlyNonEmptyArray<ArticleVersion>>;
 
-export const fetchAllPaperExpressionsFromCrossref: FetchAllPaperExpressionsFromCrossref = () => pipe(
+export const fetchAllPaperExpressionsFromCrossref: FetchAllPaperExpressions = (queryCrossrefService) => pipe(
   [
-    {
-      message: {
-        DOI: '10.1099/acmi.0.000667.v3',
-        posted: {
-          'date-parts': [2023, 12, 8],
-        },
-        resource: {
-          primary: {
-            URL: 'https://www.microbiologyresearch.org/content/journal/acmi/10.1099/acmi.0.000667.v3',
-          },
-        },
-      },
-    },
-    {
-      message: {
-        DOI: '10.1099/acmi.0.000667.v2',
-        posted: {
-          'date-parts': [2023, 11, 2],
-        },
-        resource: {
-          primary: {
-            URL: 'https://www.microbiologyresearch.org/content/journal/acmi/10.1099/acmi.0.000667.v2',
-          },
-        },
-      },
-    },
-    {
-      message: {
-        DOI: '10.1099/acmi.0.000667.v1',
-        posted: {
-          'date-parts': [2023, 7, 6],
-        },
-        resource: {
-          primary: {
-            URL: 'https://www.microbiologyresearch.org/content/journal/acmi/10.1099/acmi.0.000667.v1',
-          },
-        },
-      },
-    },
+    '10.1099/acmi.0.000667.v3',
+    '10.1099/acmi.0.000667.v2',
+    '10.1099/acmi.0.000667.v1',
   ],
-  TE.traverseArray(fetchIndividualRecord),
+  TE.traverseArray(fetchIndividualRecord(queryCrossrefService)),
   TO.fromTaskEither,
   TO.map(RA.map(toArticleVersion)),
   TO.chainOptionK(RNEA.fromReadonlyArray),
