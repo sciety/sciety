bc43ec33b5f38cab7951b5b273bfb1f56b935e55
Giorgio Sironi <g.sironi@elifesciences.org>
2023-12-11 11:36:42 +0000

    Add two more hardcoded version feed items #2895

--- src/third-parties/instantiate.ts
+++ src/third-parties/instantiate.ts
@@ -39,9 +39,19 @@ const findVersionsForArticleDoiFromSupportedServers = (
     return pipe(
       [
         {
-          version: 1,
+          version: 3,
           publishedAt: new Date('2023-12-08'),
           source: new URL('https://www.microbiologyresearch.org/content/journal/acmi/10.1099/acmi.0.000667.v3'),
+        },
+        {
+          version: 2,
+          publishedAt: new Date('2023-11-02'),
+          source: new URL('https://www.microbiologyresearch.org/content/journal/acmi/10.1099/acmi.0.000667.v2'),
+        },
+        {
+          version: 1,
+          publishedAt: new Date('2023-07-06'),
+          source: new URL('https://www.microbiologyresearch.org/content/journal/acmi/10.1099/acmi.0.000667.v1'),
         },
       ],
       RNEA.fromReadonlyArray,
