fb7745f967842ec5c2c28af0b482e5725100e3ab
Kevin Rutherford <kevin@rutherford-software.com>
2023-12-12 13:55:38 +0000

    Write a confirmatory test #2895

--- test/third-parties/crossref/enqueue-all-related-dois-not-yet-collected.test.ts
+++ test/third-parties/crossref/enqueue-all-related-dois-not-yet-collected.test.ts
@@ -1,8 +1,37 @@
 import { enqueueAllRelatedDoisNotYetCollected } from '../../../src/third-parties/crossref/fetch-all-paper-expressions-from-crossref';
+import { arbitraryString } from '../../helpers';
 
 describe('enqueue-all-related-dois-not-yet-collected', () => {
   describe('when there no records', () => {
     const initialRecords = new Map();
+    const result = enqueueAllRelatedDoisNotYetCollected({ collectedRecords: initialRecords, queue: [] });
+
+    it('the collected records are unchanged', () => {
+      expect(result.collectedRecords).toStrictEqual(initialRecords);
+    });
+
+    it('the queue is empty', () => {
+      expect(result.queue).toStrictEqual([]);
+    });
+  });
+
+  describe('when there is one record with no relations', () => {
+    const initialRecords = new Map([
+      ['10.21203/rs.3.rs-1828415/v2', {
+        message: {
+          DOI: '10.21203/rs.3.rs-1828415/v2',
+          posted: {
+            'date-parts': [[2023, 12, 7]],
+          },
+          resource: {
+            primary: {
+              URL: arbitraryString(),
+            },
+          },
+          relation: {},
+        },
+      }],
+    ]);
     const result = enqueueAllRelatedDoisNotYetCollected({ collectedRecords: initialRecords, queue: [] });
 
     it('the collected records are unchanged', () => {
