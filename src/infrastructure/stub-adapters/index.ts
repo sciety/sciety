d09569c3ae942eaf13806e9c4cf0273839e1030b
Kevin Rutherford <kevin@rutherford-software.com>
2023-12-20 15:43:27 +0000

    Stub all external queries #2905

--- src/infrastructure/stub-adapters/index.ts
+++ src/infrastructure/stub-adapters/index.ts
@@ -1,15 +1,21 @@
+import * as TE from 'fp-ts/TaskEither';
 import { fetchStaticFile } from './fetch-static-file';
 import { searchForPaperExpressions } from './search-for-paper-expressions';
 import { fetchReview } from './fetch-review';
 import { fetchRecommendedPapers } from './fetch-recommended-papers';
 import { localFetchPaperExpressionFrontMatter } from './local-fetch-paper-expression-front-matter';
 import { findAllExpressionsOfPaper } from './find-all-expressions-of-paper';
+import { ExternalQueries } from '../../third-parties';
 
-export const stubAdapters = {
-  fetchPaperExpressionFrontMatter: localFetchPaperExpressionFrontMatter,
+export const stubAdapters: ExternalQueries = {
+  fetchExpressionFrontMatter: localFetchPaperExpressionFrontMatter,
   fetchStaticFile,
   searchForPaperExpressions,
   findAllExpressionsOfPaper,
   fetchReview,
   fetchRecommendedPapers,
+  getArticleSubjectArea: () => TE.right({
+    value: 'Biology',
+    server: 'biorxiv',
+  }),
 };
