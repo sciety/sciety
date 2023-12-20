4559f83ef1d2a21dbe55c20163755ebcb49d5519
Kevin Rutherford <kevin@rutherford-software.com>
2023-12-20 13:19:33 +0000

    Shrink the interface of an external query #2905

--- src/html-pages/search-results-page/construct-view-model/data-types.ts
+++ src/html-pages/search-results-page/construct-view-model/data-types.ts
@@ -1,11 +1,5 @@
-import { ArticleAuthors } from '../../../types/article-authors';
-import { ArticleServer } from '../../../types/article-server';
 import { ArticleId } from '../../../types/article-id';
-import { SanitisedHtmlFragment } from '../../../types/sanitised-html-fragment';
 
 export type ArticleItem = {
   articleId: ArticleId,
-  server: ArticleServer,
-  title: SanitisedHtmlFragment,
-  authors: ArticleAuthors,
 };
--- src/shared-ports/search-for-articles.ts
+++ src/shared-ports/search-for-articles.ts
@@ -1,14 +1,8 @@
 import * as O from 'fp-ts/Option';
-import { ArticleAuthors } from '../types/article-authors';
-import { ArticleServer } from '../types/article-server';
 import { ArticleId } from '../types/article-id';
-import { SanitisedHtmlFragment } from '../types/sanitised-html-fragment';
 
 type SearchResult = {
   articleId: ArticleId,
-  server: ArticleServer,
-  title: SanitisedHtmlFragment,
-  authors: ArticleAuthors,
 };
 
 export type SearchResults = {
