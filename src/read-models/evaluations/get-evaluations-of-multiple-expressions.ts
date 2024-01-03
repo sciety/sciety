bb45d8764ca5578a002cafea2c0e0c88acfe66de
Giorgio Sironi <g.sironi@elifesciences.org>
2024-01-03 15:54:17 +0100

    Avoid showing duplicate evaluations to the user by returning only unique evaluation locators #2926

--- src/read-models/evaluations/get-evaluations-of-multiple-expressions.ts
+++ src/read-models/evaluations/get-evaluations-of-multiple-expressions.ts
@@ -1,8 +1,14 @@
 import { pipe } from 'fp-ts/function';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as Eq from 'fp-ts/Eq';
+import * as S from 'fp-ts/string';
 import { ReadModel } from './handle-event';
 import { RecordedEvaluation } from '../../types/recorded-evaluation';
 import { ExpressionDoi } from '../../types/expression-doi';
+
+const eqEntry: Eq.Eq<RecordedEvaluation> = Eq.struct({
+  evaluationLocator: S.Eq,
+});
 
 type GetEvaluationsOfMultipleExpressions = (
   expressionDois: ReadonlyArray<ExpressionDoi>
@@ -13,4 +19,5 @@ export const getEvaluationsOfMultipleExpressions = (
 ): GetEvaluationsOfMultipleExpressions => (expressionDois) => pipe(
   expressionDois,
   RA.flatMap((expressionDoi) => readmodel.byArticleId.get(expressionDoi) ?? []),
+  RA.uniq(eqEntry),
 );
