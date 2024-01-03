a233cf6f8b7c0b42fbd49b1d4466f9cc3f0f3927
Giorgio Sironi <g.sironi@elifesciences.org>
2024-01-03 16:39:46 +0100

    Fix the build by making stubs dynamic #2926

--- src/infrastructure/stub-adapters/find-all-expressions-of-paper.ts
+++ src/infrastructure/stub-adapters/find-all-expressions-of-paper.ts
@@ -2,19 +2,18 @@ import { URL } from 'url';
 import * as T from 'fp-ts/Task';
 import * as O from 'fp-ts/Option';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
-import * as EDOI from '../../types/expression-doi';
 import { PaperExpression } from '../../types/paper-expression';
 import { ExternalQueries } from '../../third-parties';
 
-export const findAllExpressionsOfPaper: ExternalQueries['findAllExpressionsOfPaper'] = () => T.of(O.some([
+export const findAllExpressionsOfPaper: ExternalQueries['findAllExpressionsOfPaper'] = (expressionDoi) => T.of(O.some([
   {
-    expressionDoi: EDOI.fromValidatedString('10.1101/2022.08.20.504530'),
+    expressionDoi,
     publisherHtmlUrl: new URL('https://www.biorxiv.org/content/10.1101/2022.08.20.504530v1'),
     publishedAt: new Date('1970'),
     version: 1,
   },
   {
-    expressionDoi: EDOI.fromValidatedString('10.1101/2022.08.20.504530'),
+    expressionDoi,
     publisherHtmlUrl: new URL('https://www.biorxiv.org/content/10.1101/2022.08.20.504530v2'),
     publishedAt: new Date('1980'),
     version: 2,
