08e318f36914cc53cc3911e4596c942c5c63309d
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-01-08 14:58:43 +0000

    Augment the PaperExpression to express intent #2936

--- src/types/paper-expression.ts
+++ src/types/paper-expression.ts
@@ -1,8 +1,10 @@
 import { URL } from 'url';
 import { ExpressionDoi } from './expression-doi';
+import { ArticleServer } from './article-server';
 
 export type PaperExpression = {
   expressionDoi: ExpressionDoi,
   publisherHtmlUrl: URL,
   publishedAt: Date,
+  server?: ArticleServer,
 };
