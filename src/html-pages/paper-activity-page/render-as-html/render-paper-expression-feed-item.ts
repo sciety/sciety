0548b975b9ef2e3a291a3fe89484712dae4eb001
Giorgio Sironi <g.sironi@elifesciences.org>
2024-01-09 09:39:32 +0000

    Prepare paper expression feed items for cases when we don't have a server avatar #2936

--- src/html-pages/paper-activity-page/render-as-html/render-paper-expression-feed-item.ts
+++ src/html-pages/paper-activity-page/render-as-html/render-paper-expression-feed-item.ts
@@ -1,15 +1,25 @@
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
 import { templateDate } from '../../../shared-components/date';
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 import { PaperExpressionFeedItem } from '../view-model';
 import { articleServers } from '../../../types/article-server';
+
+const renderServerAvatar = (server: O.Option<PaperExpressionFeedItem['server']>) => pipe(
+  server,
+  O.match(
+    () => '',
+    (serverKey) => `<img class="activity-feed__item__avatar" src="${articleServers[serverKey].avatarUrl}" alt="">`,
+  ),
+);
 
 type RenderPaperExpressionFeedItem = (feedItem: PaperExpressionFeedItem) => HtmlFragment;
 
 export const renderPaperExpressionFeedItem: RenderPaperExpressionFeedItem = (feedItem) => toHtmlFragment(`
   <div class="activity-feed__item__contents">
     <header class="activity-feed__item__header">
-      <img class="activity-feed__item__avatar" src="${articleServers[feedItem.server].avatarUrl}" alt="">
       <div class="activity-feed__item__meta">
+        ${renderServerAvatar(O.some(feedItem.server))}
         <div class="activity-feed__item__title">
           <a href="${feedItem.source.toString()}">
             Version published to ${feedItem.doi} on ${articleServers[feedItem.server].name}
