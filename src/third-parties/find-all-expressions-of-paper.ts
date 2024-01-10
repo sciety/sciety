6b74b46b7f4ddac4bc674e75d91c20632b6e13c9
David Moulton <d.moulton@elifesciences.org>
2024-01-10 14:27:22 +0000

    Always fetch paper expressions from Crossref, but only use them if the input's DOI is not on biorxiv or medrxiv #2941

--- src/third-parties/find-all-expressions-of-paper.ts
+++ src/third-parties/find-all-expressions-of-paper.ts
@@ -1,4 +1,5 @@
 import * as O from 'fp-ts/Option';
+import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { getArticleVersionEventsFromBiorxiv } from './biorxiv';
 import { fetchAllPaperExpressions as fetchAllPaperExpressionsFromCrossref } from './crossref';
@@ -13,19 +14,25 @@ export const findAllExpressionsOfPaper = (
   crossrefApiBearerToken: O.Option<string>,
   logger: Logger,
 ): ExternalQueries['findAllExpressionsOfPaper'] => (expressionDoi, server) => {
-  if (server !== 'biorxiv' && server !== 'medrxiv') {
-    const headers: Record<string, string> = { };
-    if (O.isSome(crossrefApiBearerToken)) {
-      headers['Crossref-Plus-API-Token'] = `Bearer ${crossrefApiBearerToken.value}`;
-    }
-    return pipe(
-      fetchAllPaperExpressionsFromCrossref(
-        queryCrossrefService(undefined, headers),
-        logger,
-        expressionDoi,
-      ),
-    );
+  const headers: Record<string, string> = { };
+  if (O.isSome(crossrefApiBearerToken)) {
+    headers['Crossref-Plus-API-Token'] = `Bearer ${crossrefApiBearerToken.value}`;
   }
-
-  return getArticleVersionEventsFromBiorxiv({ queryExternalService, logger })(new ArticleId(expressionDoi), server);
+  return pipe(
+    fetchAllPaperExpressionsFromCrossref(
+      queryCrossrefService(undefined, headers),
+      logger,
+      expressionDoi,
+    ),
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    TE.chain((expressionsFromCrossref) => pipe(
+      (server === 'biorxiv' || server === 'medrxiv')
+        ? getArticleVersionEventsFromBiorxiv({ queryExternalService, logger })(
+          new ArticleId(expressionDoi),
+          server,
+        )
+        : TE.right([]),
+      TE.map((expressionsFromBiorxiv) => expressionsFromBiorxiv),
+    )),
+  );
 };
