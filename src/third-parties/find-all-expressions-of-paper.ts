d3c4348b004d3ee6823618c648f9cd2146f87c89
Giorgio Sironi <g.sironi@elifesciences.org>
2024-01-10 14:05:26 +0000

    Reorder code to express a shifting of strategy #2941

--- src/third-parties/find-all-expressions-of-paper.ts
+++ src/third-parties/find-all-expressions-of-paper.ts
@@ -1,7 +1,7 @@
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { getArticleVersionEventsFromBiorxiv } from './biorxiv';
-import { fetchAllPaperExpressions } from './crossref';
+import { fetchAllPaperExpressions as fetchAllPaperExpressionsFromCrossref } from './crossref';
 import { QueryExternalService } from './query-external-service';
 import { Logger } from '../shared-ports';
 import { ArticleId } from '../types/article-id';
@@ -13,18 +13,19 @@ export const findAllExpressionsOfPaper = (
   crossrefApiBearerToken: O.Option<string>,
   logger: Logger,
 ): ExternalQueries['findAllExpressionsOfPaper'] => (expressionDoi, server) => {
-  if (server === 'biorxiv' || server === 'medrxiv') {
-    return getArticleVersionEventsFromBiorxiv({ queryExternalService, logger })(new ArticleId(expressionDoi), server);
+  if (server !== 'biorxiv' && server !== 'medrxiv') {
+    const headers: Record<string, string> = { };
+    if (O.isSome(crossrefApiBearerToken)) {
+      headers['Crossref-Plus-API-Token'] = `Bearer ${crossrefApiBearerToken.value}`;
+    }
+    return pipe(
+      fetchAllPaperExpressionsFromCrossref(
+        queryCrossrefService(undefined, headers),
+        logger,
+        expressionDoi,
+      ),
+    );
   }
-  const headers: Record<string, string> = { };
-  if (O.isSome(crossrefApiBearerToken)) {
-    headers['Crossref-Plus-API-Token'] = `Bearer ${crossrefApiBearerToken.value}`;
-  }
-  return pipe(
-    fetchAllPaperExpressions(
-      queryCrossrefService(undefined, headers),
-      logger,
-      expressionDoi,
-    ),
-  );
+
+  return getArticleVersionEventsFromBiorxiv({ queryExternalService, logger })(new ArticleId(expressionDoi), server);
 };
