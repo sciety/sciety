64e7b971bcacc56feeffc30bd0e370b4a287b54b
David Moulton <d.moulton@elifesciences.org>
2024-01-17 15:27:24 +0000

    Express intent with a test list #2957

---
+++ test/third-parties/crossref/is-at-least-one-crossref-work-posted-content.test.ts
@@ -0,0 +1,11 @@
+describe('is-at least-one-crossref-work-posted-content', () => {
+  describe('given a set of expressions for a paper', () => {
+    describe('when at least one of the expressions in this set has a type of "posted-content"', () => {
+      it.todo('returns the set');
+    });
+  });
+
+  describe('when none of the expressions in this set have a type of "posted-content"', () => {
+    it.todo('returns on the left');
+  });
+});
