d572c5d12be148f474ff03628d25584eb1362681
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-01-17 07:52:15 +0000

    Fix header

--- src/html-pages/markdown-preview-page/render-page.ts
+++ src/html-pages/markdown-preview-page/render-page.ts
@@ -6,14 +6,14 @@ export const renderPage = (): HtmlFragment => toHtmlFragment(
     <h1>
       Preview your Markdown content
     </h1>
-    <form class="standard-form" style="margin-bottom: 3rem">
-      <label for="markdown" class="standard-form__sub_heading">Your Markdown</label>
-      <textarea id="markdown" rows=10></textarea>
-    </form>
-    <h2>How it appears on Sciety</h2>
-    <hr>
-    <div class="activity-feed__item__body"></div>
-    <hr>
   </header>
+  <form class="standard-form" style="margin-bottom: 3rem">
+    <label for="markdown" class="standard-form__sub_heading">Your Markdown</label>
+    <textarea id="markdown" rows=10></textarea>
+  </form>
+  <h2>How it appears on Sciety</h2>
+  <hr>
+  <div class="activity-feed__item__body"></div>
+  <hr>
 `,
 );
