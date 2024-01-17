8d4d52d307ffa09e36956d53a276b26b8705c8f7
David Moulton <d.moulton@elifesciences.org>
2024-01-17 13:38:10 +0000

    Extract a seam #2956

--- src/third-parties/crossref/fetch-all-paper-expression/to-paper-expression.ts
+++ src/third-parties/crossref/fetch-all-paper-expression/to-paper-expression.ts
@@ -4,13 +4,15 @@ import { PaperExpression } from '../../../types/paper-expression';
 import * as EDOI from '../../../types/expression-doi';
 import { identifyExpressionServer } from './identify-expression-server';
 
+const determinePublicationDate = (crossrefWork: CrossrefWorkPostedContent) => new Date(
+  crossrefWork.posted['date-parts'][0][0],
+  crossrefWork.posted['date-parts'][0][1] - 1,
+  crossrefWork.posted['date-parts'][0][2],
+);
+
 export const toPaperExpression = (crossrefWork: CrossrefWorkPostedContent): PaperExpression => ({
   expressionDoi: EDOI.fromValidatedString(crossrefWork.DOI),
-  publishedAt: new Date(
-    crossrefWork.posted['date-parts'][0][0],
-    crossrefWork.posted['date-parts'][0][1] - 1,
-    crossrefWork.posted['date-parts'][0][2],
-  ),
+  publishedAt: determinePublicationDate(crossrefWork),
   publisherHtmlUrl: new URL(crossrefWork.resource.primary.URL),
   server: identifyExpressionServer(crossrefWork.resource.primary.URL),
 });
