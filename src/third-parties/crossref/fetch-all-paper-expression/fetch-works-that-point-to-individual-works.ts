ac1dbd96ede26a724162fca6c71d73868160a622
David Moulton <d.moulton@elifesciences.org>
2024-01-18 10:18:14 +0000

    Restrict a Crossref query to consider only some types of relations #2956

--- src/third-parties/crossref/fetch-all-paper-expression/fetch-works-that-point-to-individual-works.ts
+++ src/third-parties/crossref/fetch-all-paper-expression/fetch-works-that-point-to-individual-works.ts
@@ -23,7 +23,7 @@ export const fetchWorksThatPointToIndividualWorks = (
 ): TE.TaskEither<DE.DataError, ReadonlyArray<CrossrefWork>> => pipe(
   queue,
   TE.traverseArray((doi) => pipe(
-    `https://api.crossref.org/works?filter=relation.object:${doi},type:posted-content,type:journal-article`,
+    `https://api.crossref.org/works?filter=relation.object:${doi},type:posted-content,type:journal-article,relation.type:has-version,relation.type:is-version-of,relation.type:has-preprint,relation.type:is-preprint-of`,
     queryCrossrefService,
     TE.chainEitherKW((response) => pipe(
       response,
