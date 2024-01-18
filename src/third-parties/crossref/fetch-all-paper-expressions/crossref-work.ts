47b6e3780dda1971ae9854b65410308ce986a367
Lina Kinduryte <lina.kinduryte@gmail.com>
2024-01-18 14:49:46 +0000

    Extract codecs to remove duplication #2956

--- src/third-parties/crossref/fetch-all-paper-expressions/crossref-work.ts
+++ src/third-parties/crossref/fetch-all-paper-expressions/crossref-work.ts
@@ -1,50 +1,43 @@
 import * as t from 'io-ts';
 
-const relationCodec = t.partial({
-  'has-version': t.readonlyArray(t.strict({
-    'id-type': t.literal('doi'),
-    id: t.string,
-  })),
-  'is-version-of': t.readonlyArray(t.strict({
-    'id-type': t.literal('doi'),
-    id: t.string,
-  })),
-  'has-preprint': t.readonlyArray(t.strict({
-    'id-type': t.literal('doi'),
-    id: t.string,
-  })),
-  'is-preprint-of': t.readonlyArray(t.strict({
-    'id-type': t.literal('doi'),
-    id: t.string,
-  })),
+const relationCodec = t.strict({
+  'id-type': t.literal('doi'),
+  id: t.string,
+});
+
+const relationsCodec = t.readonlyArray(relationCodec);
+
+const relationshipsCodec = t.partial({
+  'has-version': relationsCodec,
+  'is-version-of': relationsCodec,
+  'has-preprint': relationsCodec,
+  'is-preprint-of': relationsCodec,
+});
+
+const datePartsCodec = t.strict({
+  'date-parts': t.readonlyArray(t.tuple([t.number, t.number, t.number])),
+});
+
+const resourceCodec = t.strict({
+  primary: t.strict({
+    URL: t.string,
+  }),
 });
 
 const journalArticleCodec = t.strict({
   type: t.literal('journal-article'),
   DOI: t.string,
-  published: t.strict({
-    'date-parts': t.readonlyArray(t.tuple([t.number, t.number, t.number])),
-  }),
-  resource: t.strict({
-    primary: t.strict({
-      URL: t.string,
-    }),
-  }),
-  relation: relationCodec,
+  published: datePartsCodec,
+  resource: resourceCodec,
+  relation: relationshipsCodec,
 });
 
 const postedContentCodec = t.strict({
   type: t.literal('posted-content'),
   DOI: t.string,
-  posted: t.strict({
-    'date-parts': t.readonlyArray(t.tuple([t.number, t.number, t.number])),
-  }),
-  resource: t.strict({
-    primary: t.strict({
-      URL: t.string,
-    }),
-  }),
-  relation: relationCodec,
+  posted: datePartsCodec,
+  resource: resourceCodec,
+  relation: relationshipsCodec,
 });
 
 type CrossrefWorkPostedContent = t.TypeOf<typeof postedContentCodec>;
