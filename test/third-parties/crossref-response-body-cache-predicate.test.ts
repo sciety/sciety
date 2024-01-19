d3fe358c9432d0684741da6ce50c26d922f55823
Giorgio Sironi <g.sironi@elifesciences.org>
2024-01-19 13:28:51 +0000

    Write another regression test #2956

--- test/third-parties/crossref-response-body-cache-predicate.test.ts
+++ test/third-parties/crossref-response-body-cache-predicate.test.ts
@@ -20,6 +20,25 @@ describe('crossref-response-body-cache-predicate', () => {
       });
     });
 
+    describe('when the body is a legitimate XML response', () => {
+      const result = crossrefResponseBodyCachePredicate(dummyLogger)(
+        `
+          <?xml version="1.0" encoding="UTF-8"?>
+          <doi_records>
+            <doi_record owner="10.1101" timestamp="2023-12-13 16:25:39">
+              <crossref>
+              </crossref>
+            </doi_record>
+          </doi_records>
+        `,
+        arbitraryUri(),
+      );
+
+      it('decides to cache', () => {
+        expect(result).toBe(true);
+      });
+    });
+
     describe('when the body is an empty string', () => {
       const result = crossrefResponseBodyCachePredicate(dummyLogger)('', arbitraryUri());
 
