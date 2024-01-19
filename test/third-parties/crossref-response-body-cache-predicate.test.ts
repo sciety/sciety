519a64944a47ac90f28829b22235837749b8ae1f
Lina Kinduryte <lina.kinduryte@gmail.com>
2024-01-19 13:21:34 +0000

    Add a regression test for json responses #2956

--- test/third-parties/crossref-response-body-cache-predicate.test.ts
+++ test/third-parties/crossref-response-body-cache-predicate.test.ts
@@ -4,10 +4,26 @@ import { arbitraryUri } from '../helpers';
 
 describe('crossref-response-body-cache-predicate', () => {
   describe('given a 200 response from Crossref', () => {
+    describe('when the body is a parsed json', () => {
+      const result = crossrefResponseBodyCachePredicate(dummyLogger)(
+        {
+          status: 'ok',
+          'message-type': 'work',
+          'message-version': '1.0.0',
+          message: {},
+        },
+        arbitraryUri(),
+      );
+
+      it('decides to cache', () => {
+        expect(result).toBe(true);
+      });
+    });
+
     describe('when the body is an empty string', () => {
       const result = crossrefResponseBodyCachePredicate(dummyLogger)('', arbitraryUri());
 
-      it('does not cache', () => {
+      it('decides not to cache', () => {
         expect(result).toBe(false);
       });
     });
@@ -15,7 +31,7 @@ describe('crossref-response-body-cache-predicate', () => {
     describe('when the body confusingly contains a report of a 503 status', () => {
       const result = crossrefResponseBodyCachePredicate(dummyLogger)('unexpected HTTP status: status=503', arbitraryUri());
 
-      it('does not cache', () => {
+      it('decides not to cache', () => {
         expect(result).toBe(false);
       });
     });
@@ -23,7 +39,7 @@ describe('crossref-response-body-cache-predicate', () => {
     describe('when the body contains an exception', () => {
       const result = crossrefResponseBodyCachePredicate(dummyLogger)('unexpected DoiDBrecord.get issue: null\n(org.crossref.common.exceptions.IORuntimeException occured 1/18/24 11:18 AM)', arbitraryUri());
 
-      it.failing('does not cache', () => {
+      it.failing('decides not to cache', () => {
         expect(result).toBe(false);
       });
     });
