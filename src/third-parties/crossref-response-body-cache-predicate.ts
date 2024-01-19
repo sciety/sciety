a0efdd41ece79c02e453141b9cd709a4f2dcf0b8
Giorgio Sironi <g.sironi@elifesciences.org>
2024-01-19 13:57:19 +0000

    Remove redundant code #2956

--- src/third-parties/crossref-response-body-cache-predicate.ts
+++ src/third-parties/crossref-response-body-cache-predicate.ts
@@ -5,14 +5,6 @@ export const crossrefResponseBodyCachePredicate = (
   logger: Logger,
 ): ResponseBodyCachePredicate => (responseBody, url) => {
   if (typeof responseBody === 'string') {
-    if (responseBody === '') {
-      logger('warn', 'Response from Crossref is an empty string, not caching', { responseBody, url });
-      return false;
-    }
-    if (responseBody.includes('unexpected HTTP status: status=503')) {
-      logger('warn', 'Response from Crossref is a Crossref error, not caching', { responseBody, url });
-      return false;
-    }
     if (responseBody.includes('<?xml version="1.0" encoding="UTF-8"?>') && responseBody.includes('<doi_records>')) {
       logger('debug', 'Response from Crossref looks like XML, caching', { responseBody, url });
       return true;
