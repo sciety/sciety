f1574bee23ddd3b546fb4cef9c7ff5aacdf66e36
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-01-23 10:59:40 +0000

    Sketch a test list for publication date parsing #2972

---
+++ test/third-parties/crossref/fetch-all-paper-expressions/determine-publication-date.test.ts
@@ -0,0 +1,9 @@
+describe('determine-publication-date', () => {
+  describe('when the date specifies a day', () => {
+    it.todo('returns a date matching the given day');
+  });
+
+  describe('when the date does not specify a day', () => {
+    it.todo('returns a date matching the first of the given month');
+  });
+});
