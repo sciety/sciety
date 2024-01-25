7c466debaad31df4c2c2a12274643d1070aac328
David Moulton <d.moulton@elifesciences.org>
2024-01-25 10:02:36 +0000

    Avoid cluttering logs after having confirmed that predicate works as intended #2956

--- src/third-parties/crossref-response-body-cache-predicate.ts
+++ src/third-parties/crossref-response-body-cache-predicate.ts
@@ -6,12 +6,12 @@ export const crossrefResponseBodyCachePredicate = (
 ): ResponseBodyCachePredicate => (responseBody, url) => {
   if (typeof responseBody === 'string') {
     if (responseBody.includes('<?xml version="1.0" encoding="UTF-8"?>') && responseBody.includes('<doi_records>')) {
-      logger('debug', 'Response from Crossref looks like XML, caching', { responseBody, url });
+      logger('debug', 'Response from Crossref looks like XML, caching', { url });
       return true;
     }
   }
   if (typeof responseBody === 'object') {
-    logger('debug', 'Response from Crossref looks like a javascript object, caching', { responseBody, url });
+    logger('debug', 'Response from Crossref looks like a javascript object, caching', { url });
     return true;
   }
   logger('warn', 'Response from Crossref is unrecognised, not caching', { responseBody, url });
