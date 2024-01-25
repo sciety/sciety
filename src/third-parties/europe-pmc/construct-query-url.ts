210aa5a3a67123c7e5f48fe1dbbf572e3bad507d
David Moulton <d.moulton@elifesciences.org>
2024-01-25 13:39:06 +0000

    Remove a feature flag. Closes #2981

--- src/third-parties/europe-pmc/construct-query-url.ts
+++ src/third-parties/europe-pmc/construct-query-url.ts
@@ -2,8 +2,6 @@ import { URLSearchParams } from 'url';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 
-const supportAccessMicrobiologySearchIfEnabled = process.env.EXPERIMENT_ENABLED === 'true' ? ' OR PUBLISHER:"Access Microbiology"' : '';
-
 export const constructQueryUrl = (
   query: string,
   cursor: O.Option<string>,
@@ -12,7 +10,7 @@ export const constructQueryUrl = (
 ): string => pipe(
   evaluatedOnly ? ' (LABS_PUBS:"2112")' : '',
   (evaluatedOnSciety) => new URLSearchParams({
-    query: `(${query}) (PUBLISHER:"bioRxiv" OR PUBLISHER:"medRxiv" OR PUBLISHER:"Research Square" OR PUBLISHER:"SciELO Preprints"${supportAccessMicrobiologySearchIfEnabled})${evaluatedOnSciety} sort_date:y`,
+    query: `(${query}) (PUBLISHER:"bioRxiv" OR PUBLISHER:"medRxiv" OR PUBLISHER:"Research Square" OR PUBLISHER:"SciELO Preprints" OR PUBLISHER:"Access Microbiology")${evaluatedOnSciety} sort_date:y`,
     format: 'json',
     pageSize: pageSize.toString(),
     resultType: 'core',
