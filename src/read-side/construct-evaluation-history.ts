328b92eddb5c0723e746cdaa0b942d9926af177a
Kevin Rutherford <kevin@rutherford-software.com>
2024-01-25 13:58:04 +0000

    Decouple shared read-side code from a particular page #2967

--- src/read-side/construct-evaluation-history.ts
+++ src/read-side/construct-evaluation-history.ts
@@ -1,10 +1,10 @@
 import { pipe } from 'fp-ts/function';
-import { Dependencies } from '../html-pages/paper-activity-page/construct-view-model/dependencies';
+import { Queries } from '../read-models';
 import * as PH from '../types/publishing-history';
 import { RecordedEvaluation } from '../types/recorded-evaluation';
 
 export const constructEvaluationHistory = (
-  dependencies: Dependencies,
+  dependencies: Queries,
   history: PH.PublishingHistory,
 ): ReadonlyArray<RecordedEvaluation> => pipe(
   history,
