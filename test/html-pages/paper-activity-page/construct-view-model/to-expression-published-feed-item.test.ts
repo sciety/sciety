83d46829ec38f3e156949352b7ee0e7cc5807d9c
David Moulton <d.moulton@elifesciences.org>
2024-01-25 15:38:59 +0000

    Write a failing test #2982

--- test/html-pages/paper-activity-page/construct-view-model/to-expression-published-feed-item.test.ts
+++ test/html-pages/paper-activity-page/construct-view-model/to-expression-published-feed-item.test.ts
@@ -3,10 +3,21 @@ import { arbitraryPaperExpression } from '../../../types/paper-expression.helper
 import {
   toExpressionPublishedFeedItem,
 } from '../../../../src/html-pages/paper-activity-page/construct-view-model/to-expression-published-feed-item';
+import { articleServers } from '../../../../src/types/article-server';
+import { arbitraryArticleServer } from '../../../types/article-server.helper';
 
 describe('to-expression-published-feed-item', () => {
   describe('given a paper expression from a known server', () => {
-    it.todo('publishedTo contains the server name');
+    const server = arbitraryArticleServer();
+    const expression = {
+      ...arbitraryPaperExpression(),
+      server: O.some(server),
+    };
+    const item = toExpressionPublishedFeedItem(expression);
+
+    it.failing('publishedTo contains the server name', () => {
+      expect(item.publishedTo).toContain(articleServers[server].name);
+    });
 
     describe('when the server is a ColdSpringHabor server', () => {
       it.todo('publishedTo contains the path for the expression\'s url on that server');
