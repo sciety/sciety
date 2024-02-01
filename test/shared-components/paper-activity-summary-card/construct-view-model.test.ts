357403fb1bdc693cba8f6901b9467c6d91101afe
Giorgio Sironi <g.sironi@elifesciences.org>
2024-02-01 13:40:32 +0000

    Use a standard in a test #2995

--- test/shared-components/paper-activity-summary-card/construct-view-model.test.ts
+++ test/shared-components/paper-activity-summary-card/construct-view-model.test.ts
@@ -18,6 +18,7 @@ import { RecordEvaluationPublicationCommand } from '../../../src/write-side/comm
 import { arbitraryExpressionDoi } from '../../types/expression-doi.helper';
 import * as PH from '../../../src/types/publishing-history';
 import { arbitraryPublishingHistoryOnlyPreprints } from '../../types/publishing-history.helper';
+import { paperActivityPagePath } from '../../../src/standards';
 
 describe('construct-view-model', () => {
   let framework: TestFramework;
@@ -63,7 +64,7 @@ describe('construct-view-model', () => {
 
       it('the article card links to the article page', () => {
         expect(viewModel).toStrictEqual(E.right(expect.objectContaining({
-          paperActivityPageHref: `/articles/activity/${latestExpressionDoi}`,
+          paperActivityPageHref: paperActivityPagePath(latestExpressionDoi),
         })));
       });
     });
