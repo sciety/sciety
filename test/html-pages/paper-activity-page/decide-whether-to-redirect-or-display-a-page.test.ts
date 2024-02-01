99d33ceb480ea9ca8f6999e1f34421f8400ee978
Giorgio Sironi <g.sironi@elifesciences.org>
2024-02-01 14:38:24 +0000

    Write an incomplete passing test #2995

--- test/html-pages/paper-activity-page/decide-whether-to-redirect-or-display-a-page.test.ts
+++ test/html-pages/paper-activity-page/decide-whether-to-redirect-or-display-a-page.test.ts
@@ -1,6 +1,37 @@
+import { pipe } from 'fp-ts/function';
+import * as TE from 'fp-ts/TaskEither';
+import * as O from 'fp-ts/Option';
+import { decideWhetherToRedirectOrDisplayAPage } from '../../../src/html-pages/paper-activity-page/decide-whether-to-redirect-or-display-a-page';
+import { arbitraryExpressionDoi } from '../../types/expression-doi.helper';
+import { TestFramework, createTestFramework } from '../../framework';
+import { ConstructPageResult } from '../../../src/html-pages/construct-page';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+
 describe('decide-whether-to-redirect-or-display-a-page', () => {
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
   describe('when the expressionDoi is the latest for the paper', () => {
-    it.todo('displays the page');
+    const expressionDoi = arbitraryExpressionDoi();
+    let result: ConstructPageResult;
+
+    beforeEach(async () => {
+      result = await pipe(
+        {
+          user: O.none,
+          expressionDoi,
+        },
+        decideWhetherToRedirectOrDisplayAPage(framework.dependenciesForViews),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+    });
+
+    it.skip('displays the page', () => {
+      expect(result.tag).toBe('html-page');
+    });
   });
 
   describe('when the expressionDoi is not the latest for the paper', () => {
