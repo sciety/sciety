1b77a25479340f38c64f00923dfa2e4e5f4ff0c3
David Moulton <d.moulton@elifesciences.org>
2024-02-01 14:21:28 +0000

    Describe intent with a test list #2995

---
+++ test/html-pages/paper-activity-page/decide-whether-to-redirect-or-display-a-page.test.ts
@@ -0,0 +1,9 @@
+describe('decide-whether-to-redirect-or-display-a-page', () => {
+  describe('when the expressionDoi is the latest for the paper', () => {
+    it.todo('displays the page');
+  });
+
+  describe('when the expressionDoi is not the latest for the paper', () => {
+    it.todo('redirects');
+  });
+});
