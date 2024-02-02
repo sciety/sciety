c246a202e04bbb2193ef9578fcb78cf0d5b3ca90
Lina Kinduryte <lina.kinduryte@gmail.com>
2024-02-02 14:24:03 +0000

    Log when a paper activity page contains expressions with duplicate dates #2995

--- src/html-pages/paper-activity-page/decide-whether-to-redirect-or-display-a-page.ts
+++ src/html-pages/paper-activity-page/decide-whether-to-redirect-or-display-a-page.ts
@@ -1,5 +1,7 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import * as RA from 'fp-ts/ReadonlyArray';
+import * as D from 'fp-ts/Date';
 import * as DE from '../../types/data-error';
 import { Dependencies } from './construct-view-model/dependencies';
 import { ConstructPageResult } from '../construct-page';
@@ -17,6 +19,19 @@ const identifyLatestExpressionDoiOfTheSamePaper = (
 ) => pipe(
   expressionDoi,
   dependencies.fetchPublishingHistory,
+  TE.map((history: PH.PublishingHistory) => {
+    pipe(
+      history.expressions,
+      RA.map((expression) => expression.publishedAt),
+      RA.uniq(D.Eq),
+      (uniqueDates) => {
+        if (uniqueDates.length !== history.expressions.length) {
+          dependencies.logger('debug', 'Expressions with duplicate dates found for a publishing history', { expressionDoi });
+        }
+      },
+    );
+    return history;
+  }),
   TE.map((publishingHistory) => PH.getLatestExpression(publishingHistory).expressionDoi),
 );
 
