fdbfbdc6da0213b6ef4a71963036021030a0df73
Giorgio Sironi <g.sironi@elifesciences.org>
2024-02-07 13:09:59 +0000

    Provide additional decoding for the original input #3009

--- src/html-pages/paper-activity-page/paper-activity-page.ts
+++ src/html-pages/paper-activity-page/paper-activity-page.ts
@@ -1,6 +1,8 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
+import * as t from 'io-ts';
+import * as tt from 'io-ts-types';
 import * as DE from '../../types/data-error';
 import { canonicalParamsCodec } from './construct-view-model';
 import { toErrorPage } from './render-as-html';
@@ -8,13 +10,22 @@ import { ErrorPageBodyViewModel } from '../../types/render-page-error';
 import { Dependencies } from './construct-view-model/dependencies';
 import { ConstructPageResult } from '../construct-page';
 import { decideWhetherToRedirectOrDisplayAPage } from './decide-whether-to-redirect-or-display-a-page';
+import { userIdCodec } from '../../types/user-id';
+
+const inputParamsCodec = t.type({
+  expressionDoi: t.string,
+  user: tt.optionFromNullable(t.type({ id: userIdCodec })),
+});
 
 const decodingAndCanonicalization = (params: unknown) => pipe(
   params,
   canonicalParamsCodec.decode,
   E.map((canonicalParams) => ({
     ...canonicalParams,
-    inputExpressionDoi: '',
+    inputExpressionDoi: pipe(
+      params,
+      inputParamsCodec.decode,
+    ),
   })),
 );
 
