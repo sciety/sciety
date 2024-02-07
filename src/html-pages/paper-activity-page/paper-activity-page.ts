85c46b0c25140a9cd40d277d81c395cd726ec6b4
Giorgio Sironi <g.sironi@elifesciences.org>
2024-02-07 13:51:33 +0000

    Remove unnecessary property #3009

--- src/html-pages/paper-activity-page/paper-activity-page.ts
+++ src/html-pages/paper-activity-page/paper-activity-page.ts
@@ -2,7 +2,6 @@ import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
 import * as t from 'io-ts';
-import * as tt from 'io-ts-types';
 import * as DE from '../../types/data-error';
 import { canonicalParamsCodec } from './construct-view-model';
 import { toErrorPage } from './render-as-html';
@@ -10,13 +9,11 @@ import { ErrorPageBodyViewModel } from '../../types/render-page-error';
 import { Dependencies } from './construct-view-model/dependencies';
 import { ConstructPageResult } from '../construct-page';
 import { decideWhetherToRedirectOrDisplayAPage } from './decide-whether-to-redirect-or-display-a-page';
-import { userIdCodec } from '../../types/user-id';
 import { paperActivityPagePath } from '../../standards';
 import { toRedirectTarget } from '../redirect-target';
 
 const inputParamsCodec = t.type({
   expressionDoi: t.string,
-  user: tt.optionFromNullable(t.type({ id: userIdCodec })),
 });
 
 const decodeCombinedParams = (params: unknown) => pipe(
