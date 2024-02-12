60214f8375f67595fa030363c83fd99389e495c5
David Moulton <d.moulton@elifesciences.org>
2024-02-12 10:49:43 +0000

    Write a characterisation test for decoding failure #3018

--- test/third-parties/redis-storage.test.ts
+++ test/third-parties/redis-storage.test.ts
@@ -22,7 +22,18 @@ describe('redis-storage', () => {
     });
   });
 
-  describe('when failing to decode a storage value', () => {
-    it.todo('returns on the left');
+  describe('when decode', () => {
+    describe('is passed invalid JSON', () => {
+      const value = 'foo';
+      let result: E.Either<unknown, StorageValue>;
+
+      beforeEach(() => {
+        result = decode(value);
+      });
+
+      it('returns on the left', () => {
+        expect(E.isLeft(result)).toBe(true);
+      });
+    });
   });
 });
