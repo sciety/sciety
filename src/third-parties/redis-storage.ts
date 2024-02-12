eaa1a6aa115a12282ca6631ff0e319df16c1f8a4
David Moulton <d.moulton@elifesciences.org>
2024-02-12 09:57:11 +0000

    Allow decode to fail #3018

--- src/third-parties/redis-storage.ts
+++ src/third-parties/redis-storage.ts
@@ -2,11 +2,13 @@ import {
   AxiosStorage, StorageValue, buildStorage, NotEmptyStorageValue,
 } from 'axios-cache-interceptor';
 import { createClient } from 'redis';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
 import { Logger } from '../shared-ports';
 
 const encode = (value: NotEmptyStorageValue) => JSON.stringify(value);
 
-const decode = (value: string) => JSON.parse(value) as StorageValue;
+const decode = (value: string) => E.right(JSON.parse(value) as StorageValue);
 
 export const redisStorage = (
   client: ReturnType<typeof createClient>,
@@ -15,13 +17,15 @@ export const redisStorage = (
 ): AxiosStorage => buildStorage({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async find(key, cacheRequestConfig) {
-    const storageValue = await client
-      .get(`axios-cache-${key}`)
-      .then((result) => (result ? decode(result) : undefined));
-    if (storageValue !== undefined) {
-      logger('debug', 'Found key in the cache', { key });
+    const result = await client.get(`axios-cache-${key}`);
+    if (!result) {
+      return undefined;
     }
-    return storageValue;
+    logger('debug', 'Found key in the cache', { key });
+    return pipe(
+      decode(result),
+      E.getOrElseW(() => undefined),
+    );
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
