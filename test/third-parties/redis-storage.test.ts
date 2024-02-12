4c5102a6849351f8db610206e158e39d61af5f25
David Moulton <d.moulton@elifesciences.org>
2024-02-12 10:21:51 +0000

    Write a test list for encoding and decoding of cached storage values #3018

---
+++ test/third-parties/redis-storage.test.ts
@@ -0,0 +1,9 @@
+describe('redis-storage', () => {
+  describe('when successfully decoding a storage value', () => {
+    it.todo('encodes to the original value');
+  });
+
+  describe('when failing to decode a storage value', () => {
+    it.todo('returns on the left');
+  });
+});
