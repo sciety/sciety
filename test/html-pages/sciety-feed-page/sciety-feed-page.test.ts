c033d047895f197b9a80a6dc9634191dceceeb69
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-13 15:45:57 +0000

    Delete tests covered by other tests

--- test/html-pages/sciety-feed-page/sciety-feed-page.test.ts
+++ test/html-pages/sciety-feed-page/sciety-feed-page.test.ts
@@ -1,9 +1,5 @@
-import * as E from 'fp-ts/Either';
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import { scietyFeedPage } from '../../../src/html-pages/sciety-feed-page/sciety-feed-page';
-import { dummyLogger } from '../../dummy-logger';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 import { arbitraryArticleId } from '../../types/article-id.helper';
 import { arbitraryUserId } from '../../types/user-id.helper';
@@ -16,38 +12,8 @@ describe('sciety-feed-page', () => {
   const addGroupCommand = arbitraryAddGroupCommand();
   let framework: TestFramework;
 
-  const renderPage = async (pageSize: number) => {
-    const dependencies = {
-      ...framework.queries,
-      logger: dummyLogger,
-      getAllEvents: framework.getAllEvents,
-    };
-    return pipe(
-      scietyFeedPage(dependencies)(pageSize)({ page: 1 }),
-      T.map(E.getOrElseW(shouldNotBeCalled)),
-      T.map((page) => page.content),
-    )();
-  };
-
   beforeEach(() => {
     framework = createTestFramework();
-  });
-
-  it('renders a single article added to a list as a card', async () => {
-    const createListCommand = arbitraryCreateListCommand();
-    await framework.commandHelpers.createList(createListCommand);
-    await framework.commandHelpers.addArticleToList(arbitraryArticleId(), createListCommand.listId);
-    const renderedPage = await renderPage(20);
-
-    expect(renderedPage).toContain('added an article to a list');
-  });
-
-  it('renders a single user followed editorial community as a card', async () => {
-    await framework.commandHelpers.addGroup(addGroupCommand);
-    await framework.commandHelpers.followGroup(arbitraryUserId(), addGroupCommand.groupId);
-    const renderedPage = await renderPage(20);
-
-    expect(renderedPage).toContain('followed a group');
   });
 
   it('displays at most a page of cards at a time', async () => {
