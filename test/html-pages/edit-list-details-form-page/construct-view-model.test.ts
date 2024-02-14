b84d67ef1eb88316d7bc2f99520e6801c78213cd
Giorgio Sironi <g.sironi@elifesciences.org>
2024-02-14 11:53:36 +0000

    Port an old test to use the framework #3024

--- test/html-pages/edit-list-details-form-page/construct-view-model.test.ts
+++ test/html-pages/edit-list-details-form-page/construct-view-model.test.ts
@@ -1,34 +1,43 @@
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
+import { arbitraryCreateListCommand } from '../../write-side/commands/create-list-command.helper';
+import { TestFramework, createTestFramework } from '../../framework/create-test-framework';
 import { constructViewModel } from '../../../src/html-pages/edit-list-details-form-page/construct-view-model';
 import { shouldNotBeCalled } from '../../should-not-be-called';
-import { arbitraryList } from '../../read-models/lists/list-helper';
 import { arbitraryListId } from '../../types/list-id.helper';
+import { ViewModel } from '../../../src/html-pages/edit-list-details-form-page/render-edit-list-details-form-page';
 
 describe('construct-view-model', () => {
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
   describe('when the list exists', () => {
-    const listId = arbitraryListId();
-    const list = arbitraryList();
-    const adapters = {
-      lookupList: () => O.some(list),
-    };
-    const viewModel = pipe(
-      listId,
-      constructViewModel(adapters),
-      E.getOrElseW(shouldNotBeCalled),
-    );
+    const command = arbitraryCreateListCommand();
+    let viewModel: ViewModel;
+
+    beforeEach(async () => {
+      await framework.commandHelpers.createList(command);
+      viewModel = pipe(
+        command.listId,
+        constructViewModel(framework.dependenciesForViews),
+        E.getOrElseW(shouldNotBeCalled),
+      );
+    });
 
     it('includes the list id', () => {
-      expect(viewModel.listId).toStrictEqual(listId);
+      expect(viewModel.listId).toStrictEqual(command.listId);
     });
 
     it('includes the current list name', () => {
-      expect(viewModel.listName).toStrictEqual(list.name);
+      expect(viewModel.listName).toStrictEqual(command.name);
     });
 
     it('includes the current list description', () => {
-      expect(viewModel.listDescription).toStrictEqual(list.description);
+      expect(viewModel.listDescription).toStrictEqual(command.description);
     });
   });
 
