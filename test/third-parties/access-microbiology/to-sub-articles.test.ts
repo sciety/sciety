10001b4c57adc65fd0bfba886e706e35d1d603a3
Giorgio Sironi <g.sironi@elifesciences.org>
2024-02-20 14:21:53 +0000

    Write the narrowest possible happy path #3042

--- test/third-parties/access-microbiology/to-sub-articles.test.ts
+++ test/third-parties/access-microbiology/to-sub-articles.test.ts
@@ -2,11 +2,19 @@ import * as E from 'fp-ts/Either';
 import { toSubArticles } from '../../../src/third-parties/access-microbiology/to-sub-articles';
 
 describe('to-sub-articles', () => {
-  describe('when the input is not a string', () => {
+  describe('given an input that is not a string', () => {
     const result = toSubArticles(undefined);
 
     it('fails', () => {
       expect(E.isLeft(result)).toBe(true);
     });
+  });
+
+  describe('given an input containing a single sub-article with a body containing a single paragraph', () => {
+    it.todo('returns one sub-article');
+
+    it.todo('returns the subArticleId');
+
+    it.todo('returns the body unchanged');
   });
 });
