d38fe1a409a5960fe2441a467da5daba728d74cb
David Moulton <d.moulton@elifesciences.org>
2024-02-20 09:48:37 +0000

    Create a test list expressing our wish to reject certain types of Crossref work #3039

---
+++ test/third-parties/crossref/fetch-all-paper-expressions/to-paper-expression.test.ts
@@ -0,0 +1,13 @@
+describe('to-paper-expression', () => {
+  describe('when the Crossref work is of type posted-content', () => {
+    it.todo('returns an expression of type preprint');
+  });
+
+  describe('when the Crossref work is of type journal-article', () => {
+    it.todo('returns an expression of type journal-article');
+  });
+
+  describe('when the Crossref work is of type component', () => {
+    it.todo('rejects the work');
+  });
+});
