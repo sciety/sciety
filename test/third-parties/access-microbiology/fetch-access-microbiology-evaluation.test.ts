bb66744a2628a16e753fe13b8c38892119b0a04e
Giorgio Sironi <g.sironi@elifesciences.org>
2024-02-20 10:45:15 +0000

    Start writing a characterization test to identify next steps #3042

--- test/third-parties/access-microbiology/fetch-access-microbiology-evaluation.test.ts
+++ test/third-parties/access-microbiology/fetch-access-microbiology-evaluation.test.ts
@@ -1,6 +1,30 @@
+import { pipe } from 'fp-ts/function';
+import * as TE from 'fp-ts/TaskEither';
+import { fetchAccessMicrobiologyEvaluation } from '../../../src/third-parties/access-microbiology/fetch-access-microbiology-evaluation';
+import { QueryExternalService } from '../../../src/third-parties/query-external-service';
+import { Evaluation } from '../../../src/types/evaluation';
+import { dummyLogger } from '../../dummy-logger';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+import { arbitraryHtmlFragment, arbitraryString } from '../../helpers';
+
 describe('fetch-access-microbiology-evaluation', () => {
-  describe('given an XML containing the relevant sub-article', () => {
-    it.todo('return a full text');
+  describe.skip('given an XML containing the relevant sub-article', () => {
+    const queryExternalService: QueryExternalService = () => () => TE.right('');
+    const key = arbitraryString();
+    const body = arbitraryHtmlFragment();
+    let result: Evaluation;
+
+    beforeEach(async () => {
+      result = await pipe(
+        key,
+        fetchAccessMicrobiologyEvaluation(queryExternalService, dummyLogger),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+    });
+
+    it('return a full text', () => {
+      expect(result.fullText).toStrictEqual(body);
+    });
 
     it.todo('return a url');
   });
