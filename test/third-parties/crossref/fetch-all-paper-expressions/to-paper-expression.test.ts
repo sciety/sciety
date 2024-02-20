9b734720c20bd10a26fd54649b4058372a3aa745
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-20 14:18:12 +0000

    Implement a characterisation test #3038

--- test/third-parties/crossref/fetch-all-paper-expressions/to-paper-expression.test.ts
+++ test/third-parties/crossref/fetch-all-paper-expressions/to-paper-expression.test.ts
@@ -1,13 +1,13 @@
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
-import { CrossrefWork, SupportedCrossrefWork } from '../../../../src/third-parties/crossref/fetch-all-paper-expressions/crossref-work';
+import { CrossrefWork } from '../../../../src/third-parties/crossref/fetch-all-paper-expressions/crossref-work';
 import { toPaperExpression } from '../../../../src/third-parties/crossref/fetch-all-paper-expressions/to-paper-expression';
 import { arbitraryString, arbitraryUri } from '../../../helpers';
 import { shouldNotBeCalled } from '../../../should-not-be-called';
 
 describe('to-paper-expression', () => {
   describe('when the Crossref work is of type posted-content', () => {
-    const crossrefWork: SupportedCrossrefWork = {
+    const crossrefWork: CrossrefWork = {
       type: 'posted-content',
       DOI: arbitraryString(),
       posted: { 'date-parts': [[2021, 10, 3]] },
@@ -27,7 +27,7 @@ describe('to-paper-expression', () => {
   });
 
   describe('when the Crossref work is of type journal-article', () => {
-    const crossrefWork: SupportedCrossrefWork = {
+    const crossrefWork: CrossrefWork = {
       type: 'journal-article',
       DOI: arbitraryString(),
       published: { 'date-parts': [[2021, 10, 3]] },
@@ -47,14 +47,12 @@ describe('to-paper-expression', () => {
   });
 
   describe('when the Crossref work is of unknown type', () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const crossrefWork: CrossrefWork = {
       type: 'other',
       DOI: arbitraryString(),
       relation: { },
     };
-    // const result = toPaperExpression(crossrefWork);
-    const result = E.left('incomplete-test');
+    const result = toPaperExpression(crossrefWork);
 
     it('rejects the work', () => {
       expect(E.isLeft(result)).toBe(true);
