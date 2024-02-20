87dd18882343023d823e97244cb07d609b6d8f19
David Moulton <d.moulton@elifesciences.org>
2024-02-20 12:00:31 +0000

    Allow the compiler to distinguish the various types of Crossref work #3038

--- src/third-parties/crossref/fetch-all-paper-expressions/to-paper-expression.ts
+++ src/third-parties/crossref/fetch-all-paper-expressions/to-paper-expression.ts
@@ -7,24 +7,25 @@ import { identifyExpressionServer } from './identify-expression-server';
 import * as crossrefDate from './date-stamp';
 
 export const toPaperExpression = (crossrefWork: SupportedCrossrefWork): E.Either<unknown, PaperExpression> => {
-  switch (crossrefWork.type) {
-    case 'posted-content':
-      return E.right({
-        expressionType: 'preprint',
-        expressionDoi: EDOI.fromValidatedString(crossrefWork.DOI),
-        publishedAt: crossrefDate.toDate(crossrefWork.posted),
-        publishedTo: crossrefWork.DOI,
-        publisherHtmlUrl: new URL(crossrefWork.resource.primary.URL),
-        server: identifyExpressionServer(crossrefWork.resource.primary.URL),
-      });
-    case 'journal-article':
-      return E.right({
-        expressionType: 'journal-article',
-        expressionDoi: EDOI.fromValidatedString(crossrefWork.DOI),
-        publishedAt: crossrefDate.toDate(crossrefWork.published),
-        publishedTo: crossrefWork.DOI,
-        publisherHtmlUrl: new URL(crossrefWork.resource.primary.URL),
-        server: identifyExpressionServer(crossrefWork.resource.primary.URL),
-      });
+  if (crossrefWork.type === 'posted-content') {
+    return E.right({
+      expressionType: 'preprint',
+      expressionDoi: EDOI.fromValidatedString(crossrefWork.DOI),
+      publishedAt: crossrefDate.toDate(crossrefWork.posted),
+      publishedTo: crossrefWork.DOI,
+      publisherHtmlUrl: new URL(crossrefWork.resource.primary.URL),
+      server: identifyExpressionServer(crossrefWork.resource.primary.URL),
+    });
   }
+  if (crossrefWork.type === 'journal-article') {
+    return E.right({
+      expressionType: 'journal-article',
+      expressionDoi: EDOI.fromValidatedString(crossrefWork.DOI),
+      publishedAt: crossrefDate.toDate(crossrefWork.published),
+      publishedTo: crossrefWork.DOI,
+      publisherHtmlUrl: new URL(crossrefWork.resource.primary.URL),
+      server: identifyExpressionServer(crossrefWork.resource.primary.URL),
+    });
+  }
+  return E.left('unrecognised Crossref work type');
 };
