9ce2a785c6827e1812460a689e92b1a6eb4a976d
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-20 10:09:42 +0000

    Write a characterisation test #3038

--- test/third-parties/crossref/fetch-all-paper-expressions/to-paper-expression.test.ts
+++ test/third-parties/crossref/fetch-all-paper-expressions/to-paper-expression.test.ts
@@ -1,6 +1,29 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import { SupportedCrossrefWork } from '../../../../src/third-parties/crossref/fetch-all-paper-expressions/crossref-work';
+import { toPaperExpression } from '../../../../src/third-parties/crossref/fetch-all-paper-expressions/to-paper-expression';
+import { arbitraryString, arbitraryUri } from '../../../helpers';
+import { shouldNotBeCalled } from '../../../should-not-be-called';
+
 describe('to-paper-expression', () => {
   describe('when the Crossref work is of type posted-content', () => {
-    it.todo('returns an expression of type preprint');
+    const crossrefWork: SupportedCrossrefWork = {
+      type: 'posted-content',
+      DOI: arbitraryString(),
+      posted: { 'date-parts': [[2021, 10, 3]] },
+      resource: { primary: { URL: arbitraryUri() } },
+      relation: { },
+    };
+    const expressionType = pipe(
+      crossrefWork,
+      toPaperExpression,
+      E.getOrElseW(shouldNotBeCalled),
+      (expression) => expression.expressionType,
+    );
+
+    it('returns an expression of type preprint', () => {
+      expect(expressionType).toBe('preprint');
+    });
   });
 
   describe('when the Crossref work is of type journal-article', () => {
