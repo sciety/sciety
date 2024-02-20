e440e21ac973539566bb6a6ddb15ca396f9b82ed
Giorgio Sironi <g.sironi@elifesciences.org>
2024-02-20 14:57:02 +0000

    Introduce another failure case test #3042

--- test/third-parties/access-microbiology/to-sub-articles.test.ts
+++ test/third-parties/access-microbiology/to-sub-articles.test.ts
@@ -24,6 +24,32 @@ describe('to-sub-articles', () => {
     });
   });
 
+  describe('given an input containing a single sub-article without a body', () => {
+    let result: ReadonlyArray<SubArticle>;
+
+    beforeEach(() => {
+      result = pipe(
+        `
+          <?xml version="1.0" encoding="UTF-8"?>
+          <!DOCTYPE article PUBLIC "-//NLM//DTD JATS (Z39.96) Journal Archiving and Interchange DTD v1.1 20151215//EN" "JATS-archivearticle1.dtd">
+          <article dtd-version="1.1" article-type="case-report" xml:lang="EN" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML" specific-use="pre-print">
+            <sub-article article-type="review-tool-report" id="report.acmi.0.000569.v1.1">
+              <front-stub>
+                <article-id pub-id-type="doi">10.1099/acmi.0.000569.v1.1</article-id>
+              </front-stub>
+            </sub-article>
+          </article>
+        `,
+        toSubArticles,
+        E.getOrElseW(shouldNotBeCalled),
+      );
+    });
+
+    it.failing('returns an empty array', () => {
+      expect(result).toHaveLength(0);
+    });
+  });
+
   describe.skip('given an input containing a single sub-article with a body containing a single paragraph', () => {
     let result: ReadonlyArray<SubArticle>;
 
