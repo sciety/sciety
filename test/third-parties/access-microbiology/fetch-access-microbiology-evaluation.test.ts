5ff76ec2af28572112a59af361155672719fcc9b
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-02-20 10:36:09 +0000

    Write a todo characterisation test #3042

---
+++ test/third-parties/access-microbiology/fetch-access-microbiology-evaluation.test.ts
@@ -0,0 +1,7 @@
+describe('fetch-access-microbiology-evaluation', () => {
+  describe('given an XML containing the relevant sub-article', () => {
+    it.todo('return a full text');
+
+    it.todo('return a url');
+  });
+});
