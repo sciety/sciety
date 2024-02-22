bf4c3c8b711314fd1fc669ce08944715604a9252
Giorgio Sironi <g.sironi@elifesciences.org>
2024-02-22 13:24:34 +0000

    Remove obsolete code #3045

--- src/third-parties/access-microbiology/fetch-access-microbiology-evaluation.ts
+++ src/third-parties/access-microbiology/fetch-access-microbiology-evaluation.ts
@@ -1,35 +1,13 @@
 import * as TE from 'fp-ts/TaskEither';
 import { URL } from 'url';
-import { XMLBuilder, XMLParser } from 'fast-xml-parser';
-import * as t from 'io-ts';
 import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
-import { decodeAndLogFailures } from '../decode-and-log-failures';
 import { Logger } from '../../shared-ports';
 import * as DE from '../../types/data-error';
 import { EvaluationFetcher } from '../evaluation-fetcher';
-import { toHtmlFragment } from '../../types/html-fragment';
-import { sanitise } from '../../types/sanitised-html-fragment';
 import { QueryExternalService } from '../query-external-service';
-import { acmiJatsCodec } from './acmi-jats';
 import * as AED from './acmi-evaluation-doi';
 import { toFullTextsOfEvaluations } from './to-full-texts-of-evaluations';
-
-const parser = new XMLParser({});
-const builder = new XMLBuilder();
-
-const parseXmlAsAJavascriptObject = (accessMicrobiologyXmlResponse: string) => pipe(
-  parser.parse(accessMicrobiologyXmlResponse),
-  acmiJatsCodec.decode,
-  E.mapLeft(() => DE.unavailable),
-);
-
-const decodeResponse = (logger: Logger) => (response: unknown) => pipe(
-  response,
-  decodeAndLogFailures(logger, t.string),
-  E.mapLeft(() => DE.unavailable),
-  E.chain(parseXmlAsAJavascriptObject),
-);
 
 export const fetchAccessMicrobiologyEvaluation = (
   queryExternalService: QueryExternalService,
@@ -40,11 +18,14 @@ export const fetchAccessMicrobiologyEvaluation = (
     return pipe(
       'https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000530.v1/acmi.0.000530.v1.xml',
       queryExternalService(),
-      TE.chainEitherKW(decodeResponse(logger)),
-      TE.map((response) => builder.build(response.article['sub-article'][2].body).toString() as string),
-      TE.map((text) => ({
+      TE.chainEitherK(toFullTextsOfEvaluations),
+      TE.chainEitherKW((fullTexts) => pipe(
+        fullTexts.get(AED.fromValidatedString(key)),
+        (fullText) => (fullText !== undefined ? E.right(fullText) : E.left(DE.notFound)),
+      )),
+      TE.map((fullText) => ({
         url: new URL(`https://doi.org/${key}`),
-        fullText: sanitise(toHtmlFragment(text)),
+        fullText,
       })),
     );
   }
