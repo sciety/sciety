73b1366f4e0a4babafac22f1d643e816e500a9ca
David Moulton <d.moulton@elifesciences.org>
2024-02-23 13:35:25 +0000

    Write a naive passing test for a codec #3053

--- src/third-parties/access-microbiology/acmi-evaluation-doi.ts
+++ src/third-parties/access-microbiology/acmi-evaluation-doi.ts
@@ -1,3 +1,7 @@
+import * as t from 'io-ts';
+
 export type AcmiEvaluationDoi = string & { readonly AcmiEvaluationDoi: unique symbol };
 
 export const fromValidatedString = (value: string): AcmiEvaluationDoi => value as AcmiEvaluationDoi;
+
+export const acmiEvaluationDoiCodec = t.string;
--- test/third-parties/access-microbiology/acmi-evaluation-doi.test.ts
+++ test/third-parties/access-microbiology/acmi-evaluation-doi.test.ts
@@ -1,7 +1,27 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import { abortTest } from '../../framework/abort-test';
+import {
+  acmiEvaluationDoiCodec,
+} from '../../../src/third-parties/access-microbiology/acmi-evaluation-doi';
+
 describe('acmi-evaluation-doi', () => {
   describe('acmiEvaluationDoiCodec', () => {
     describe('when decoding a value that represents a valid ACMI evaluation DOI', () => {
-      it.todo('returns the DOI');
+      const input = '10.1099/acmi.0.000569.v1.5';
+      let result: string;
+
+      beforeEach(() => {
+        result = pipe(
+          input,
+          acmiEvaluationDoiCodec.decode,
+          E.getOrElseW(abortTest('Codec returned on the left')),
+        );
+      });
+
+      it('returns the DOI', () => {
+        expect(result).toBe(input);
+      });
     });
 
     describe('when attempting to decode a value that does not represent a valid ACMI evaluation DOI', () => {
