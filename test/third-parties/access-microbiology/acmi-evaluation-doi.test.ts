439f849e83492b05513507bfd8a876e5c1894243
Giorgio Sironi <g.sironi@elifesciences.org>
2024-02-23 13:28:10 +0000

    Express intent with todo tests #3053

---
+++ test/third-parties/access-microbiology/acmi-evaluation-doi.test.ts
@@ -0,0 +1,11 @@
+describe('acmi-evaluation-doi', () => {
+  describe('acmiEvaluationDoiCodec', () => {
+    describe('when decoding a value that represents a valid ACMI evaluation DOI', () => {
+      it.todo('returns the DOI');
+    });
+
+    describe('when attempting to decode a value that does not represent a valid ACMI evaluation DOI', () => {
+      it.todo('returns on the left');
+    });
+  });
+});
