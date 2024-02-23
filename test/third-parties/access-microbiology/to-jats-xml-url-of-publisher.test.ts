44843960dcc86c4db44dd5c4091263a5929e9fad
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-02-23 11:31:36 +0000

    Collect test scenarios #3053

--- test/third-parties/access-microbiology/to-jats-xml-url-of-publisher.test.ts
+++ test/third-parties/access-microbiology/to-jats-xml-url-of-publisher.test.ts
@@ -1,3 +1,8 @@
 describe('to-jats-xml-url-of-publisher', () => {
-  it.todo('characterise hard-coded behaviour');
+  describe.each([
+    ['10.1099/acmi.0.000530.v1.3', 'https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000530.v1/acmi.0.000530.v1.xml'],
+    ['10.1099/acmi.0.000569.v1.4', 'https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000569.v1/acmi.0.000569.v1.xml'],
+  ])('given an ACMI evaluation DOI: %s', () => {
+    it.todo('returns an inferred url');
+  });
 });
