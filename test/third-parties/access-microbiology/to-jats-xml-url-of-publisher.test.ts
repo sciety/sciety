21f6b867e655e195c511e3ac685c42fd4401bb87
Giorgio Sironi <g.sironi@elifesciences.org>
2024-02-23 11:44:29 +0000

    Implement a characterization test #3053

--- test/third-parties/access-microbiology/to-jats-xml-url-of-publisher.test.ts
+++ test/third-parties/access-microbiology/to-jats-xml-url-of-publisher.test.ts
@@ -1,8 +1,25 @@
+import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
+import { toJatsXmlUrlOfPublisher } from '../../../src/third-parties/access-microbiology/to-jats-xml-url-of-publisher';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+
 describe('to-jats-xml-url-of-publisher', () => {
   describe.each([
     ['10.1099/acmi.0.000530.v1.3', 'https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000530.v1/acmi.0.000530.v1.xml'],
     ['10.1099/acmi.0.000569.v1.4', 'https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000569.v1/acmi.0.000569.v1.xml'],
-  ])('given an ACMI evaluation DOI: %s', () => {
-    it.todo('returns an inferred url');
+  ])('given an ACMI evaluation DOI: %s', (acmiEvaluationDoi, url) => {
+    let inferredUrl: string;
+
+    beforeEach(() => {
+      inferredUrl = pipe(
+        acmiEvaluationDoi,
+        toJatsXmlUrlOfPublisher,
+        O.getOrElseW(shouldNotBeCalled),
+      );
+    });
+
+    it('returns an inferred url', () => {
+      expect(inferredUrl).toBe(url);
+    });
   });
 });
