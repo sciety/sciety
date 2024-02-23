a103b0bd7ca3f106037ecbcafb6dc90eaeb856c9
David Moulton <d.moulton@elifesciences.org>
2024-02-23 11:57:15 +0000

    Introduce a test to drive removal of hard coding #3053

--- test/third-parties/access-microbiology/to-jats-xml-url-of-publisher.test.ts
+++ test/third-parties/access-microbiology/to-jats-xml-url-of-publisher.test.ts
@@ -7,7 +7,7 @@ describe('to-jats-xml-url-of-publisher', () => {
   describe.each([
     ['10.1099/acmi.0.000530.v1.3', 'https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000530.v1/acmi.0.000530.v1.xml'],
     ['10.1099/acmi.0.000569.v1.4', 'https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000569.v1/acmi.0.000569.v1.xml'],
-  ])('given an ACMI evaluation DOI: %s', (acmiEvaluationDoi, url) => {
+  ])('given a hardcoded ACMI evaluation DOI: %s', (acmiEvaluationDoi, url) => {
     let inferredUrl: string;
 
     beforeEach(() => {
@@ -20,6 +20,19 @@ describe('to-jats-xml-url-of-publisher', () => {
 
     it('returns an inferred url', () => {
       expect(inferredUrl).toBe(url);
+    });
+  });
+
+  describe.each([
+    ['10.1099/acmi.0.000569.v1.5', 'https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000569.v1/acmi.0.000569.v1.xml'],
+  ])('given an ACMI evaluation DOI: %s', (acmiEvaluationDoi, url) => {
+    const inferredUrl = pipe(
+      acmiEvaluationDoi,
+      toJatsXmlUrlOfPublisher,
+    );
+
+    it.failing('returns an inferred url', () => {
+      expect(inferredUrl).toBe(O.some(url));
     });
   });
 });
