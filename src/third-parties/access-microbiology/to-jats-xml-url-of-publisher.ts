4ebdf6f74879b05bfc2f51f8f9e61dbcf844ab59
David Moulton <d.moulton@elifesciences.org>
2024-02-23 15:23:21 +0000

    Hardcode all remaining url mappings for the ticket #3053

--- src/third-parties/access-microbiology/to-jats-xml-url-of-publisher.ts
+++ src/third-parties/access-microbiology/to-jats-xml-url-of-publisher.ts
@@ -4,7 +4,11 @@ export const toJatsXmlUrlOfPublisher = (key: string): O.Option<string> => {
   if (key === '10.1099/acmi.0.000530.v1.3') {
     return O.some('https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000530.v1/acmi.0.000530.v1.xml');
   }
-  if (key === '10.1099/acmi.0.000569.v1.4' || key === '10.1099/acmi.0.000569.v1.5') {
+  if (
+    key === '10.1099/acmi.0.000569.v1.4'
+    || key === '10.1099/acmi.0.000569.v1.5'
+    || key === '10.1099/acmi.0.000569.v1.3'
+    || key === '10.1099/acmi.0.000569.v1.6') {
     return O.some('https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000569.v1/acmi.0.000569.v1.xml');
   }
   return O.none;
