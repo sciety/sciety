04f7e052519e0d85d5f00d5a70fbb3f04f687c0e
David Moulton <d.moulton@elifesciences.org>
2024-02-26 14:41:52 +0000

    Safely access an array of undefined length #3053

--- src/third-parties/access-microbiology/to-jats-xml-url-of-publisher.ts
+++ src/third-parties/access-microbiology/to-jats-xml-url-of-publisher.ts
@@ -1,14 +1,15 @@
 import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { AcmiEvaluationDoi } from './acmi-evaluation-doi';
 
 const deriveExpressionDoiSuffix = (doi: AcmiEvaluationDoi) => {
   const acmiEvaluationDoiRegex = /^10.1099\/(acmi\.0\.[0-9]{6,}\.v[0-9]+)\.[0-9]+$/;
-  const result = acmiEvaluationDoiRegex.exec(doi);
-  if (result) {
-    return O.some(result[1]);
-  }
-  return O.none;
+  return pipe(
+    acmiEvaluationDoiRegex.exec(doi),
+    O.fromNullable,
+    O.chain(RA.lookup(1)),
+  );
 };
 
 export const toJatsXmlUrlOfPublisher = (key: AcmiEvaluationDoi): O.Option<string> => {
