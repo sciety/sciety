17d850f7ef5c5872a26171d3c7c262e7a998b814
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-02-26 14:43:58 +0000

    Remove special cases that are now addressed by the generic derivation #3053

--- src/third-parties/access-microbiology/to-jats-xml-url-of-publisher.ts
+++ src/third-parties/access-microbiology/to-jats-xml-url-of-publisher.ts
@@ -12,20 +12,8 @@ const deriveExpressionDoiSuffix = (doi: AcmiEvaluationDoi) => {
   );
 };
 
-export const toJatsXmlUrlOfPublisher = (key: AcmiEvaluationDoi): O.Option<string> => {
-  if (key === '10.1099/acmi.0.000530.v1.3') {
-    return O.some('https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000530.v1/acmi.0.000530.v1.xml');
-  }
-  if (
-    key === '10.1099/acmi.0.000569.v1.4'
-    || key === '10.1099/acmi.0.000569.v1.5'
-    || key === '10.1099/acmi.0.000569.v1.3'
-    || key === '10.1099/acmi.0.000569.v1.6') {
-    return O.some('https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000569.v1/acmi.0.000569.v1.xml');
-  }
-  return pipe(
-    key,
-    deriveExpressionDoiSuffix,
-    O.map((s) => `https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/${s}/${s}.xml`),
-  );
-};
+export const toJatsXmlUrlOfPublisher = (key: AcmiEvaluationDoi): O.Option<string> => pipe(
+  key,
+  deriveExpressionDoiSuffix,
+  O.map((s) => `https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/${s}/${s}.xml`),
+);
