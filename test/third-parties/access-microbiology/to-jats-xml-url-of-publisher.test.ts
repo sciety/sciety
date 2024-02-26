68f753a3f2de5adbad5266197eac0b99633cf6a8
David Moulton <d.moulton@elifesciences.org>
2024-02-26 11:45:04 +0000

    Expand coverage of failure cases #3053

--- test/third-parties/access-microbiology/to-jats-xml-url-of-publisher.test.ts
+++ test/third-parties/access-microbiology/to-jats-xml-url-of-publisher.test.ts
@@ -28,6 +28,7 @@ describe('to-jats-xml-url-of-publisher', () => {
 
   describe.each([
     ['10.1099/acmi.0.000569.v1.9999', 'https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.000569.v1/acmi.0.000569.v1.xml'],
+    ['10.1099/acmi.0.0005691234567.v1.9999', 'https://www.microbiologyresearch.org/docserver/fulltext/acmi/10.1099/acmi.0.0005691234567.v1/acmi.0.0005691234567.v1.xml'],
   ])('given an ACMI evaluation DOI: %s', (acmiEvaluationDoi, url) => {
     const inferredUrl = pipe(
       acmiEvaluationDoi,
@@ -40,8 +41,15 @@ describe('to-jats-xml-url-of-publisher', () => {
     });
   });
 
-  describe('given a string that does not represent an ACMI evaluation DOI', () => {
-    const result = toJatsXmlUrlOfPublisher(AED.fromValidatedString(arbitraryWord()));
+  describe.each([
+    [arbitraryWord()],
+    // ['10.1099/acmi.0.000569.v1.'],
+    ['10.1099/acmi.0.000569.'],
+    ['10.1099/acmi.0'],
+    [`10.1099/${arbitraryWord()}`],
+    // ['10.1234/acmi.0.000569.v1.9999'],
+  ])('given a string %s that does not represent an ACMI evaluation DOI', (input) => {
+    const result = toJatsXmlUrlOfPublisher(AED.fromValidatedString(input));
 
     it('returns an O.none', () => {
       expect(result).toStrictEqual(O.none);
