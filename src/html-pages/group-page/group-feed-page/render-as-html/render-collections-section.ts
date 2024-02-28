fe2eef1e214c507d5214f9494bf623c2fa81d28d
David Moulton <d.moulton@elifesciences.org>
2024-02-28 11:51:01 +0000

    Display an hard-coded list card #3062

--- src/html-pages/group-page/group-feed-page/render-as-html/render-collections-section.ts
+++ src/html-pages/group-page/group-feed-page/render-as-html/render-collections-section.ts
@@ -1,3 +1,20 @@
+import * as O from 'fp-ts/Option';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
+import { renderListCard } from '../../../../shared-components/list-card';
+import * as LID from '../../../../types/list-id';
+import { rawUserInput } from '../../../../read-models/annotations/handle-event';
 
-export const renderCollectionsSection = (): HtmlFragment => toHtmlFragment('<p>Hello, world!</p>');
+export const renderCollectionsSection = (): HtmlFragment => toHtmlFragment(`
+  <ol class="card-list" role="list">
+    <li role="listitem">
+      ${renderListCard({
+    listId: LID.fromValidatedString(''),
+    articleCount: 0,
+    updatedAt: O.none,
+    title: 'My collection',
+    description: rawUserInput('My description'),
+    avatarUrl: O.some('https://pbs.twimg.com/profile_images/1417582635040317442/jYHfOlh6_normal.jpg'),
+  })}
+    </li>
+  </ol>
+`);
