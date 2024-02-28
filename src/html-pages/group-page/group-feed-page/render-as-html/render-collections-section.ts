2c7cc6174bdffbd1795cbb8afa7c6a7f4ca7591e
David Moulton <d.moulton@elifesciences.org>
2024-02-28 14:48:46 +0000

    Push hardcoding out of one renderer #3062

--- src/html-pages/group-page/group-feed-page/render-as-html/render-collections-section.ts
+++ src/html-pages/group-page/group-feed-page/render-as-html/render-collections-section.ts
@@ -1,22 +1,12 @@
-import * as O from 'fp-ts/Option';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
-import { renderListCard } from '../../../../shared-components/list-card';
-import * as LID from '../../../../types/list-id';
-import { rawUserInput } from '../../../../read-models/annotations/handle-event';
+import { ListCardViewModel, renderListCard } from '../../../../shared-components/list-card';
 
-export const renderCollectionsSection = (): HtmlFragment => toHtmlFragment(`
+export const renderCollectionsSection = (listCard: ListCardViewModel): HtmlFragment => toHtmlFragment(`
   <section class="group-page-collections">
     <h2>Collections</h2>
     <ol class="card-list" role="list">
         <li role="listitem">
-        ${renderListCard({
-    listId: LID.fromValidatedString('454ba80f-e0bc-47ed-ba76-c8f872c303d2'),
-    articleCount: 706,
-    updatedAt: O.some(new Date('2024-02-22')),
-    title: 'Reading list',
-    description: rawUserInput('Articles that are being read by Biophysics Colab.'),
-    avatarUrl: O.some('https://pbs.twimg.com/profile_images/1417582635040317442/jYHfOlh6_normal.jpg'),
-  })}
+          ${renderListCard(listCard)}
         </li>
     </ol>
   </section>
--- src/html-pages/group-page/group-feed-page/render-as-html/render-main-content.ts
+++ src/html-pages/group-page/group-feed-page/render-as-html/render-main-content.ts
@@ -1,4 +1,5 @@
 import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
 import { renderTabs } from '../../../../shared-components/tabs';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 import { tabList } from '../../common-components/tab-list';
@@ -6,16 +7,28 @@ import { ViewModel } from '../view-model';
 import { renderListOfArticleCardsWithFallback } from './render-list-of-article-cards-with-fallback';
 import { renderCollectionsSection } from './render-collections-section';
 import { wrapperForTopSpace } from '../../wrapper-for-top-space';
+import { ListCardViewModel } from '../../../../shared-components/list-card';
+import * as LID from '../../../../types/list-id';
+import { rawUserInput } from '../../../../read-models/annotations/handle-event';
 
 const tabProps = (viewmodel: ViewModel) => ({
   tabList: tabList(viewmodel.tabs),
   activeTabIndex: 0,
 });
 
+const conciergedBiophysicsColabUserListCard: ListCardViewModel = {
+  listId: LID.fromValidatedString('454ba80f-e0bc-47ed-ba76-c8f872c303d2'),
+  articleCount: 706,
+  updatedAt: O.some(new Date('2024-02-22')),
+  title: 'Reading list',
+  description: rawUserInput('Articles that are being read by Biophysics Colab.'),
+  avatarUrl: O.some('https://pbs.twimg.com/profile_images/1417582635040317442/jYHfOlh6_normal.jpg'),
+};
+
 const augmentWithCollectionsSection = (groupId: ViewModel['group']['id']) => (otherContent: HtmlFragment) => {
   if (groupId === '4bbf0c12-629b-4bb8-91d6-974f4df8efb2') {
     return toHtmlFragment(`
-      ${renderCollectionsSection()}
+      ${renderCollectionsSection(conciergedBiophysicsColabUserListCard)}
       ${otherContent}
     `);
   }
