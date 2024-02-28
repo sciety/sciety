de36e9104e577fa835da0118bc53800897d3e1b8
Giorgio Sironi <g.sironi@elifesciences.org>
2024-02-28 13:49:39 +0000

    Hardcode collection data for Biophysics Colab reading list #3062

--- src/html-pages/group-page/group-feed-page/render-as-html/render-collections-section.ts
+++ src/html-pages/group-page/group-feed-page/render-as-html/render-collections-section.ts
@@ -10,11 +10,11 @@ export const renderCollectionsSection = (): HtmlFragment => toHtmlFragment(`
     <ol class="card-list" role="list">
         <li role="listitem">
         ${renderListCard({
-    listId: LID.fromValidatedString(''),
-    articleCount: 0,
-    updatedAt: O.none,
-    title: 'My collection',
-    description: rawUserInput('My description'),
+    listId: LID.fromValidatedString('454ba80f-e0bc-47ed-ba76-c8f872c303d2'),
+    articleCount: 706,
+    updatedAt: O.some(new Date('2024-02-22')),
+    title: 'Reading list',
+    description: rawUserInput('Articles that are being read by Biophysics Colab.'),
     avatarUrl: O.some('https://pbs.twimg.com/profile_images/1417582635040317442/jYHfOlh6_normal.jpg'),
   })}
         </li>
