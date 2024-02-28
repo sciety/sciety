1920b05fc0062f559f97c26f50fc0060a2e8766f
David Moulton <d.moulton@elifesciences.org>
2024-02-28 10:22:18 +0000

    Make a test valid #3053

--- test/third-parties/fetch-evaluation/access-microbiology/fetch-access-microbiology-evaluation.test.ts
+++ test/third-parties/fetch-evaluation/access-microbiology/fetch-access-microbiology-evaluation.test.ts
@@ -2,14 +2,14 @@ import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
 import { QueryExternalService } from '../../../../src/third-parties/query-external-service';
 import { Evaluation } from '../../../../src/types/evaluation';
-import { arbitraryString, arbitraryWord } from '../../../helpers';
+import { arbitraryString } from '../../../helpers';
 import { abortTest } from '../../../framework/abort-test';
 import { dummyLogger } from '../../../dummy-logger';
 import { fetchAccessMicrobiologyEvaluation } from '../../../../src/third-parties/fetch-evaluation/access-microbiology/fetch-access-microbiology-evaluation';
 
 describe('fetch-access-microbiology-evaluation', () => {
-  describe.skip('given an XML containing the relevant sub-article', () => {
-    const key = arbitraryWord();
+  describe('given an XML containing the relevant sub-article', () => {
+    const key = '10.1099/acmi.0.000569.v1.3';
     const text = arbitraryString();
     const queryExternalService: QueryExternalService = () => () => TE.right(`
       <article>
@@ -33,7 +33,7 @@ describe('fetch-access-microbiology-evaluation', () => {
       )();
     });
 
-    it.failing('return a full text', () => {
+    it('return a full text', () => {
       expect(result.fullText).toBe(`<p>${text}</p>`);
     });
 
