35689f6823d6c3db91249086f588034751646012
David Moulton <d.moulton@elifesciences.org>
2024-03-01 11:13:47 +0000

    Implement an additional test #3067

--- test/third-parties/crossref/parse-crossref-article.test.ts
+++ test/third-parties/crossref/parse-crossref-article.test.ts
@@ -399,6 +399,15 @@ describe('parse-crossref-article', () => {
   });
 
   describe('when the document', () => {
+    describe('does not contain a <crossref> tag', () => {
+      const input = '<?xml version="1.0" encoding="UTF-8"?>\n<doi_records>\r\n  <doi_record>\r\n      </doi_record>\r\n</doi_records>';
+      const result = containsUnrecoverableError(parser.parseFromString(input, 'text/xml'));
+
+      it.failing('detects an unrecoverable error', () => {
+        expect(result).toBe(true);
+      });
+    });
+
     describe('contains a <crossref> tag with its only child an <error> tag', () => {
       const input = '<?xml version="1.0" encoding="UTF-8"?>\n<doi_records>\r\n  <doi_record>\r\n    <crossref>\r\n      <error>doi:10.21203/rs.3.rs-3869684/v1</error>\r\n    </crossref>\r\n  </doi_record>\r\n</doi_records>';
       const result = containsUnrecoverableError(parser.parseFromString(input, 'text/xml'));
