ccd5b1c495beb3144ae18ddd1aa5cb15c63cc677
Giorgio Sironi <g.sironi@elifesciences.org>
2024-03-05 11:51:27 +0000

    Reduce duplication and corral hardcoding #3071

--- src/html-pages/group-page/group-feed-page/construct-view-model/construct-view-model.ts
+++ src/html-pages/group-page/group-feed-page/construct-view-model/construct-view-model.ts
@@ -10,6 +10,7 @@ import { Params } from './params';
 import * as LID from '../../../../types/list-id';
 import { ListCardViewModel, constructListCardViewModelWithAvatar } from '../../../../shared-components/list-card';
 import { GroupId } from '../../../../types/group-id';
+import * as GID from '../../../../types/group-id';
 
 const constructListCardForACollection = (dependencies: Dependencies) => (listId: LID.ListId) => pipe(
   listId,
@@ -17,16 +18,16 @@ const constructListCardForACollection = (dependencies: Dependencies) => (listId:
   O.map(constructListCardViewModelWithAvatar(dependencies)),
 );
 
+const hardcoded = new Map<GroupId, LID.ListId>([
+  [GID.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2'), LID.fromValidatedString('454ba80f-e0bc-47ed-ba76-c8f872c303d2')],
+  [GID.fromValidatedString('f7a7aec3-8b1c-4b81-b098-f3f2e4eefe58'), LID.fromValidatedString('729cab51-b47d-4ab5-bf2f-8282f1de445e')],
+]);
+
 const constructCollections = (dependencies: Dependencies, groupId: GroupId): O.Option<ListCardViewModel> => {
-  if (groupId === '4bbf0c12-629b-4bb8-91d6-974f4df8efb2') {
+  const hardcodedListId = hardcoded.get(groupId);
+  if (hardcodedListId) {
     return pipe(
-      LID.fromValidatedString('454ba80f-e0bc-47ed-ba76-c8f872c303d2'),
-      constructListCardForACollection(dependencies),
-    );
-  }
-  if (groupId === 'f7a7aec3-8b1c-4b81-b098-f3f2e4eefe58') {
-    return pipe(
-      LID.fromValidatedString('729cab51-b47d-4ab5-bf2f-8282f1de445e'),
+      hardcodedListId,
       constructListCardForACollection(dependencies),
     );
   }
