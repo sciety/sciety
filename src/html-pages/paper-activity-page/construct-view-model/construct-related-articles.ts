67a9bdcc13e8b804382f410ca3c9ae217bb1e4ce
Lina Kinduryte <lina.kinduryte@gmail.com>
2024-03-06 15:42:37 +0000

    Hide internal structure of publishing history #3073

--- src/html-pages/paper-activity-page/construct-view-model/construct-related-articles.ts
+++ src/html-pages/paper-activity-page/construct-view-model/construct-related-articles.ts
@@ -21,7 +21,7 @@ export const constructRelatedArticles = (
   TE.map(RA.takeLeft(3)),
   TE.chainW(TE.traverseArray(constructPaperActivitySummaryCard(dependencies))),
   TE.mapLeft((error) => {
-    dependencies.logger('error', 'constructRelatedArticles has failed', { error, paperExpressionDoi: history.expressions[0].expressionDoi });
+    dependencies.logger('error', 'constructRelatedArticles has failed', { error, paperExpressionDoi: PH.getLatestExpression(history).expressionDoi });
     return error;
   }),
   TO.fromTaskEither,
