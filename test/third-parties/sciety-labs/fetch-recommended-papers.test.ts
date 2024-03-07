146b42b61939053cdc19bc6756990e753cf6edda
Giorgio Sironi <g.sironi@elifesciences.org>
2024-03-07 14:00:11 +0000

    Remove duplication from a test suite for readability #3073

--- test/third-parties/sciety-labs/fetch-recommended-papers.test.ts
+++ test/third-parties/sciety-labs/fetch-recommended-papers.test.ts
@@ -102,106 +102,104 @@ describe('fetch-recommended-papers', () => {
     });
   });
 
-  describe('when a response contains an understandable DOI', () => {
-    const recommendedExpressionDoi = arbitraryExpressionDoi();
-    const queryExternalService = () => () => TE.right({
-      recommendedPapers: [
-        arbitraryRecommendedPaper(recommendedExpressionDoi),
-      ],
-    });
-    let recommendedPapers: ReadonlyArray<ExpressionDoi>;
+  describe('given an arbitrary publishing history', () => {
+    const getRecommendedPapers = (queryExternalService: QueryExternalService) => pipe(
+      arbitraryPublishingHistoryOnlyPreprints(),
+      fetchRecommendedPapers(queryExternalService, dummyLogger),
+    );
 
-    beforeEach(async () => {
-      recommendedPapers = await pipe(
-        arbitraryPublishingHistoryOnlyPreprints(),
-        fetchRecommendedPapers(queryExternalService, dummyLogger),
-        TE.getOrElse(shouldNotBeCalled),
-      )();
-    });
+    describe('when a response contains an understandable DOI', () => {
+      const recommendedExpressionDoi = arbitraryExpressionDoi();
+      const queryExternalService = () => () => TE.right({
+        recommendedPapers: [
+          arbitraryRecommendedPaper(recommendedExpressionDoi),
+        ],
+      });
+      let recommendedPapers: ReadonlyArray<ExpressionDoi>;
 
-    it('returns an array containing an expression DOI', () => {
-      expect(recommendedPapers).toStrictEqual([recommendedExpressionDoi]);
-    });
-  });
+      beforeEach(async () => {
+        recommendedPapers = await pipe(
+          getRecommendedPapers(queryExternalService),
+          TE.getOrElse(shouldNotBeCalled),
+        )();
+      });
 
-  describe('when a response contains an corrupt DOI', () => {
-    const corruptDoi = '10.1101/2023.01.15.524119 10.1101/123456';
-    const understandableDoi = '10.1101/123';
-    const queryExternalService = () => () => TE.right({
-      recommendedPapers: [
-        arbitraryRecommendedPaper(understandableDoi),
-        arbitraryRecommendedPaper(corruptDoi),
-      ],
+      it('returns an array containing an expression DOI', () => {
+        expect(recommendedPapers).toStrictEqual([recommendedExpressionDoi]);
+      });
     });
 
-    let result: ReadonlyArray<ExpressionDoi>;
+    describe('when a response contains an corrupt DOI', () => {
+      const corruptDoi = '10.1101/2023.01.15.524119 10.1101/123456';
+      const understandableDoi = '10.1101/123';
+      const queryExternalService = () => () => TE.right({
+        recommendedPapers: [
+          arbitraryRecommendedPaper(understandableDoi),
+          arbitraryRecommendedPaper(corruptDoi),
+        ],
+      });
 
-    beforeEach(async () => {
-      result = await pipe(
-        arbitraryPublishingHistoryOnlyPreprints(),
-        fetchRecommendedPapers(queryExternalService, dummyLogger),
-        TE.getOrElse(shouldNotBeCalled),
-      )();
+      let result: ReadonlyArray<ExpressionDoi>;
+
+      beforeEach(async () => {
+        result = await pipe(
+          getRecommendedPapers(queryExternalService),
+          TE.getOrElse(shouldNotBeCalled),
+        )();
+      });
+
+      it('returns an array that does not include the corrupted DOI', () => {
+        expect(result).toStrictEqual([EDOI.fromValidatedString(understandableDoi)]);
+      });
     });
 
-    it('returns an array that does not include the corrupted DOI', () => {
-      expect(result).toStrictEqual([EDOI.fromValidatedString(understandableDoi)]);
-    });
-  });
+    describe('when we cannot access the third-party', () => {
+      const queryExternalService = () => () => TE.left(DE.unavailable);
+      let result: E.Either<unknown, unknown>;
 
-  describe('when we cannot access the third-party', () => {
-    const queryExternalService = () => () => TE.left(DE.unavailable);
-    let result: E.Either<unknown, unknown>;
+      beforeEach(async () => {
+        result = await getRecommendedPapers(queryExternalService)();
+      });
 
-    beforeEach(async () => {
-      result = await pipe(
-        arbitraryPublishingHistoryOnlyPreprints(),
-        fetchRecommendedPapers(queryExternalService, dummyLogger),
-      )();
+      it('returns a left', () => {
+        expect(E.isLeft(result)).toBe(true);
+      });
     });
 
-    it('returns a left', () => {
-      expect(E.isLeft(result)).toBe(true);
-    });
-  });
+    describe('when we cannot decode the response', () => {
+      const queryExternalService = () => () => TE.right(arbitraryString());
+      let result: E.Either<unknown, unknown>;
 
-  describe('when we cannot decode the response', () => {
-    const queryExternalService = () => () => TE.right(arbitraryString());
-    let result: E.Either<unknown, unknown>;
+      beforeEach(async () => {
+        result = await getRecommendedPapers(queryExternalService)();
+      });
 
-    beforeEach(async () => {
-      result = await pipe(
-        arbitraryPublishingHistoryOnlyPreprints(),
-        fetchRecommendedPapers(queryExternalService, dummyLogger),
-      )();
+      it('returns a left', () => {
+        expect(E.isLeft(result)).toBe(true);
+      });
     });
 
-    it('returns a left', () => {
-      expect(E.isLeft(result)).toBe(true);
-    });
-  });
+    describe('when the response contains a paper with no DOI', () => {
+      const response = {
+        recommendedPapers: [
+          {
+            externalIds: {},
+          },
+        ],
+      };
+      const queryExternalService = () => () => TE.right(response);
+      let result: ReadonlyArray<ExpressionDoi>;
 
-  describe('when the response contains a paper with no DOI', () => {
-    const response = {
-      recommendedPapers: [
-        {
-          externalIds: {},
-        },
-      ],
-    };
-    const queryExternalService = () => () => TE.right(response);
-    let result: ReadonlyArray<ExpressionDoi>;
+      beforeEach(async () => {
+        result = await pipe(
+          getRecommendedPapers(queryExternalService),
+          TE.getOrElse(shouldNotBeCalled),
+        )();
+      });
 
-    beforeEach(async () => {
-      result = await pipe(
-        arbitraryPublishingHistoryOnlyPreprints(),
-        fetchRecommendedPapers(queryExternalService, dummyLogger),
-        TE.getOrElse(shouldNotBeCalled),
-      )();
-    });
-
-    it('ignores such papers', () => {
-      expect(result).toStrictEqual([]);
+      it('ignores such papers', () => {
+        expect(result).toStrictEqual([]);
+      });
     });
   });
 });
