538f0b73b591b71ac96d341c3577dc576eac576a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-03-07 14:30:25 +0000

    Inline some behaviour to prepare reshaping of responsibilities

--- src/http/form-submission-handlers/create-user-account.ts
+++ src/http/form-submission-handlers/create-user-account.ts
@@ -1,23 +1,72 @@
 import { pipe } from 'fp-ts/function';
 import { Middleware } from 'koa';
 import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
 import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
+import { sequenceS } from 'fp-ts/Apply';
+import { formatValidationErrors } from 'io-ts-reporters';
 import {
-  Ports as GetLoggedInScietyUserPorts, getLoggedInScietyUser,
+  Ports as GetLoggedInScietyUserPorts, getAuthenticatedUserIdFromContext, getLoggedInScietyUser,
 } from '../authentication-and-logging-in-of-sciety-users';
 import { createUserAccountFormPageLayout, renderFormPage } from '../../html-pages/create-user-account-form-page';
 import { constructHtmlResponse } from '../../html-pages/construct-html-response';
-import { validateAndExecuteCommand, Dependencies as ValidateAndExecuteCommandPorts } from './validate-and-execute-command';
+import { createUserAccountFormCodec, unvalidatedFormDetailsCodec } from './codecs';
 import { redirectToAuthenticationDestination } from '../authentication-destination';
 import { sendHtmlResponse } from '../send-html-response';
 import { detectClientClassification } from '../detect-client-classification';
+import { SanitisedUserInput } from '../../types/sanitised-user-input';
+import { createUserAccountCommandHandler } from '../../write-side/command-handlers';
+import { Logger } from '../../shared-ports';
+import { DependenciesForCommands } from '../../write-side/dependencies-for-commands';
 
-type Dependencies = GetLoggedInScietyUserPorts & ValidateAndExecuteCommandPorts;
+const defaultSignUpAvatarUrl = '/static/images/profile-dark.svg';
+
+type Dependencies = GetLoggedInScietyUserPorts & DependenciesForCommands & {
+  logger: Logger,
+};
 
 export const createUserAccount = (dependencies: Dependencies): Middleware => async (context, next) => {
   await pipe(
-    validateAndExecuteCommand(context, dependencies),
+    {
+      formUserDetails: pipe(
+        context.request.body,
+        createUserAccountFormCodec.decode,
+        E.mapLeft((errors) => {
+          dependencies.logger('error', 'createUserAccountFormCodec failed', { error: formatValidationErrors(errors) });
+          return 'validation-error';
+        }),
+      ),
+      authenticatedUserId: pipe(
+        context,
+        getAuthenticatedUserIdFromContext,
+        E.fromOption(() => 'no-authenticated-user-id'),
+      ),
+    },
+    sequenceS(E.Apply),
+    E.map(({ formUserDetails, authenticatedUserId }) => ({
+      ...formUserDetails,
+      displayName: formUserDetails.fullName,
+      userId: authenticatedUserId,
+      avatarUrl: defaultSignUpAvatarUrl,
+    })),
+    T.of,
+    TE.chainW((command) => pipe(
+      command,
+      createUserAccountCommandHandler(dependencies),
+      TE.mapLeft((error) => {
+        dependencies.logger('error', 'createUserAccountCommandHandler failed', { error, command });
+        return 'command-failed';
+      }),
+    )),
+    TE.mapLeft(() => pipe(
+      context.request.body,
+      unvalidatedFormDetailsCodec.decode,
+      E.getOrElse(() => ({
+        fullName: '' as SanitisedUserInput,
+        handle: '' as SanitisedUserInput,
+      })),
+    )),
     TE.bimap(
       (formDetails) => {
         const htmlResponse = pipe(
