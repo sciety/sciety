5152a063f8ce6f59ac58629d83e4f5d65383375a
Lina Kinduryte <lina.kinduryte@gmail.com>
2024-03-08 11:03:51 +0000

    Hide enum order in a function #3073

--- src/infrastructure/logger/stream-logger.ts
+++ src/infrastructure/logger/stream-logger.ts
@@ -1,5 +1,11 @@
 import { Serializer } from './serializer';
 import { Logger, LoggerLevelName, LoggerLevel } from '../../shared-ports';
+import { LevelName } from '../../shared-ports/logger';
+
+const shouldLogLineBeIgnored = (
+  requestedLogLevelName: LevelName,
+  configuredLogLevel: LoggerLevel,
+) => LoggerLevel[requestedLogLevelName] > configuredLogLevel;
 
 export const streamLogger = (
   stream: NodeJS.WritableStream,
@@ -7,13 +13,13 @@ export const streamLogger = (
   configuredLogLevelName: string,
 ): Logger => {
   const configuredLogLevel = LoggerLevel[configuredLogLevelName as LoggerLevelName] ?? LoggerLevel.debug;
-  return (requestedLogLevel, message, payload = {}, date = new Date()) => {
-    if (LoggerLevel[requestedLogLevel] > configuredLogLevel) {
+  return (requestedLogLevelName, message, payload = {}, date = new Date()) => {
+    if (shouldLogLineBeIgnored(requestedLogLevelName, configuredLogLevel)) {
       return;
     }
     const entry = {
       timestamp: date,
-      level: requestedLogLevel,
+      level: requestedLogLevelName,
       message,
       payload,
     };
