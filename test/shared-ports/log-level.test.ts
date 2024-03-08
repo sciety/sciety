8cfffbee1de9eebc057232b3c3889a531ea6211f
David Moulton <d.moulton@elifesciences.org>
2024-03-08 13:44:45 +0000

    Write a preliminary test list #3073

---
+++ test/shared-ports/log-level.test.ts
@@ -0,0 +1,9 @@
+describe('log-level', () => {
+  describe('when requested level is debug and the configured level is info', () => {
+    it.todo('gets ignored');
+  });
+
+  describe('when requested level is error and the configured level is verbose', () => {
+    it.todo('does not get ignored');
+  });
+});
