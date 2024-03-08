9fd895608c394042b49803ef2ffa7b7b8d1ac112
Lina Kinduryte <lina.kinduryte@gmail.com>
2024-03-08 13:47:16 +0000

    Write a characterisation test #3073

--- test/shared-ports/log-level.test.ts
+++ test/shared-ports/log-level.test.ts
@@ -1,6 +1,10 @@
+import { shouldLogLineBeIgnored } from '../../src/shared-ports/log-level';
+
 describe('log-level', () => {
   describe('when requested level is debug and the configured level is info', () => {
-    it.todo('gets ignored');
+    it('gets ignored', () => {
+      expect(shouldLogLineBeIgnored('debug', 'info')).toBe(true);
+    });
   });
 
   describe('when requested level is error and the configured level is verbose', () => {
