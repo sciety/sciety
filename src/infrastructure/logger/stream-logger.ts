77bebc10ab774f88c4a5cfefd6afe6fdab5a51a7
Giorgio Sironi <g.sironi@elifesciences.org>
2024-03-08 11:21:31 +0000

    Encapsulate the knowledge of LoggerLevel in one function #3073

--- src/infrastructure/logger/stream-logger.ts
+++ src/infrastructure/logger/stream-logger.ts
@@ -3,26 +3,26 @@ import { Logger, LoggerLevelName, LoggerLevel } from '../../shared-ports';
 
 const shouldLogLineBeIgnored = (
   requestedLogLevelName: LoggerLevelName,
-  configuredLogLevel: LoggerLevel,
-) => LoggerLevel[requestedLogLevelName] > configuredLogLevel;
+  configuredLogLevelName: string,
+) => {
+  const configuredLogLevel = LoggerLevel[configuredLogLevelName as LoggerLevelName] ?? LoggerLevel.debug;
+  return LoggerLevel[requestedLogLevelName] > configuredLogLevel;
+};
 
 export const streamLogger = (
   stream: NodeJS.WritableStream,
   serializer: Serializer,
   configuredLogLevelName: string,
-): Logger => {
-  const configuredLogLevel = LoggerLevel[configuredLogLevelName as LoggerLevelName] ?? LoggerLevel.debug;
-  return (requestedLogLevelName, message, payload = {}, date = new Date()) => {
-    if (shouldLogLineBeIgnored(requestedLogLevelName, configuredLogLevel)) {
-      return;
-    }
-    const entry = {
-      timestamp: date,
-      level: requestedLogLevelName,
-      message,
-      payload,
-    };
-
-    stream.write(`${serializer(entry)}\n`);
+): Logger => (requestedLogLevelName, message, payload = {}, date = new Date()) => {
+  if (shouldLogLineBeIgnored(requestedLogLevelName, configuredLogLevelName)) {
+    return;
+  }
+  const entry = {
+    timestamp: date,
+    level: requestedLogLevelName,
+    message,
+    payload,
   };
+
+  stream.write(`${serializer(entry)}\n`);
 };
