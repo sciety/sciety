bb26905296997fa858de680b3009cfb42ff1f111
Lina Kinduryte <lina.kinduryte@gmail.com>
2024-03-08 15:46:49 +0000

    Document responsibilities delegated to the cache unit of encapsulation #3073

--- src/third-parties/cache/index.ts
+++ src/third-parties/cache/index.ts
@@ -1,2 +1,7 @@
+// Knows how to
+// - Perform http request
+// - Cache responses
+// - Provide logging of 4xx, 5xx errors and timeouts
+
 export { CachingFetcherOptions, createCachingFetcher } from './caching-fetcher-factory';
 export { ResponseBodyCachePredicate } from './cached-getter';
