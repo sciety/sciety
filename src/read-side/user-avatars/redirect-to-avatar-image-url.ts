cfd9e90420b265d21f4d243316aa0bf9301e9553
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-11 15:21:51 +0000

    Always redirect to the default avatar #3089

--- src/read-side/user-avatars/redirect-to-avatar-image-url.ts
+++ src/read-side/user-avatars/redirect-to-avatar-image-url.ts
@@ -1,5 +1,9 @@
 import { Middleware } from '@koa/router';
+import { toRedirectTarget } from '../../html-pages/redirect-target';
+import { sendRedirect } from '../../http/send-redirect';
 
 export const redirectToAvatarImageUrl = (): Middleware => async (context, next) => {
+  const avatarUrl = toRedirectTarget('/static/images/profile-dark.svg');
+  sendRedirect(context, avatarUrl);
   await next();
 };
