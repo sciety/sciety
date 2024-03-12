85f254e8c19e8dcd2df7b751d1c38a13e37cd60d
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-12 10:39:42 +0000

    Use a more appropriate codec #3089

--- src/read-side/user-avatars/redirect-to-avatar-image-url.ts
+++ src/read-side/user-avatars/redirect-to-avatar-image-url.ts
@@ -1,4 +1,5 @@
 import * as E from 'fp-ts/Either';
+import * as t from 'io-ts';
 import { Middleware } from '@koa/router';
 import { pipe } from 'fp-ts/function';
 import { toRedirectTarget } from '../../html-pages/redirect-target';
@@ -6,10 +7,15 @@ import { sendRedirect } from '../../http/send-redirect';
 import { Queries } from '../../read-models';
 import { candidateUserHandleCodec } from '../../types/candidate-user-handle';
 
+const paramsCodec = t.type({
+  handle: candidateUserHandleCodec,
+});
+
 export const redirectToAvatarImageUrl = (queries: Queries): Middleware => async (context, next) => {
   const avatarUrl = pipe(
     context.params,
-    candidateUserHandleCodec.decode,
+    paramsCodec.decode,
+    E.map((params) => params.handle),
     E.map(queries.lookupUserByHandle),
     () => '/static/images/profile-dark.svg',
     toRedirectTarget,
