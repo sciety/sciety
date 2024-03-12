eee38d53aed5acecc80abed4fe3315f05fa081ff
David Moulton <d.moulton@elifesciences.org>
2024-03-12 10:50:11 +0000

    Add error handling to avatar url parameters #3089

--- src/read-side/user-avatars/redirect-to-avatar-image-url.ts
+++ src/read-side/user-avatars/redirect-to-avatar-image-url.ts
@@ -6,6 +6,7 @@ import { toRedirectTarget } from '../../html-pages/redirect-target';
 import { sendRedirect } from '../../http/send-redirect';
 import { Queries } from '../../read-models';
 import { candidateUserHandleCodec } from '../../types/candidate-user-handle';
+import * as DE from '../../types/data-error';
 
 const paramsCodec = t.type({
   handle: candidateUserHandleCodec,
@@ -16,7 +17,10 @@ export const redirectToAvatarImageUrl = (queries: Queries): Middleware => async 
     context.params,
     paramsCodec.decode,
     E.map((params) => params.handle),
-    E.map(queries.lookupUserByHandle),
+    E.flatMapOption(
+      queries.lookupUserByHandle,
+      () => DE.notFound,
+    ),
     () => '/static/images/profile-dark.svg',
     toRedirectTarget,
   );
