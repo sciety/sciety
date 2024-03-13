f8d1ddb1fda82875706fd9589bc555caebbdc32e
Giorgio Sironi <g.sironi@elifesciences.org>
2024-03-13 14:54:55 +0000

    Implement a characterization test #3094

--- test/read-models/groups/get-group-by-slug.test.ts
+++ test/read-models/groups/get-group-by-slug.test.ts
@@ -6,6 +6,7 @@ import { handleEvent, initialState } from '../../../src/read-models/groups/handl
 import { getGroupBySlug } from '../../../src/read-models/groups/get-group-by-slug';
 import { arbitraryGroup } from '../../types/group.helper';
 import { arbitraryGroupJoinedEvent } from '../../domain-events/group-resource-events.helper';
+import { arbitraryString } from '../../helpers';
 
 const group = arbitraryGroup();
 
@@ -29,7 +30,24 @@ describe('getGroupBySlug', () => {
   });
 
   describe('when the group joined with a large logo', () => {
-    it.todo('returns the group');
+    const groupJoinedEvent = {
+      ...arbitraryGroupJoinedEvent(),
+      largeLogoPath: arbitraryString(),
+    };
+    const readmodel = pipe(
+      [
+        ...arbitraryUninterestingEvents,
+        groupJoinedEvent,
+        ...arbitraryUninterestingEvents,
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+
+    it('returns the group', () => {
+      expect(getGroupBySlug(readmodel)(groupJoinedEvent.slug)).toStrictEqual(O.some(expect.objectContaining({
+        slug: groupJoinedEvent.slug,
+      })));
+    });
   });
 
   describe('when the group does not exist', () => {
