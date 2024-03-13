2ad37d35ccc525f64d1c688c52610035035a9739
Lina Kinduryte <lina.kinduryte@gmail.com>
2024-03-13 13:45:45 +0000

    Use the new arbitrary test helper in another test helper #3094

--- test/read-models/groups/arbitrary-uninteresting-events.helper.ts
+++ test/read-models/groups/arbitrary-uninteresting-events.helper.ts
@@ -1,24 +1,16 @@
 import { constructEvent } from '../../../src/domain-events';
 import { arbitraryGroupId } from '../../types/group-id.helper';
-import { arbitraryGroup } from '../../types/group.helper';
 import { arbitraryUserId } from '../../types/user-id.helper';
 import { arbitraryEvaluationPublicationRecordedEvent } from '../../domain-events/evaluation-resource-events.helper';
+import { arbitraryGroupJoinedEvent } from '../../domain-events/group-resource-events.helper';
 
-const group = arbitraryGroup();
+const groupId = arbitraryGroupId();
 
 export const arbitraryUninterestingEvents = [
-  constructEvent('GroupJoined')({
-    groupId: group.id,
-    name: group.name,
-    avatarPath: group.avatarPath,
-    descriptionPath: group.descriptionPath,
-    shortDescription: group.shortDescription,
-    homepage: group.homepage,
-    slug: group.slug,
-  }),
+  arbitraryGroupJoinedEvent(groupId),
   constructEvent('UserFollowedEditorialCommunity')({ userId: arbitraryUserId(), editorialCommunityId: arbitraryGroupId() }),
   {
     ...arbitraryEvaluationPublicationRecordedEvent(),
-    groupId: group.id,
+    groupId,
   },
 ];
