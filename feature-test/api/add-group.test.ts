00eaeebd2ea21d9a72dd01195f9b23508f01058f
Giorgio Sironi <g.sironi@elifesciences.org>
2024-03-14 14:29:21 +0000

    Roll out the usage of arbitrary command in one more test #3094

--- feature-test/api/add-group.test.ts
+++ feature-test/api/add-group.test.ts
@@ -1,21 +1,11 @@
 import {
   $, click, closeBrowser, goto, openBrowser, text, within,
 } from 'taiko';
-import { AddGroupCommand } from '../../src/write-side/commands';
-import { arbitraryGroup } from '../../test/types/group.helper';
 import { callApi } from '../helpers/call-api.helper';
+import { arbitraryAddGroupCommand } from '../../test/write-side/commands/add-group-command.helper';
 
 describe('add-group', () => {
-  const newGroup = arbitraryGroup();
-  const addGroupCommand: AddGroupCommand = {
-    groupId: newGroup.id,
-    name: newGroup.name,
-    shortDescription: newGroup.shortDescription,
-    homepage: newGroup.homepage,
-    avatarPath: newGroup.avatarPath,
-    descriptionPath: newGroup.descriptionPath,
-    slug: newGroup.slug,
-  };
+  const addGroupCommand = arbitraryAddGroupCommand();
 
   beforeAll(async () => {
     await openBrowser();
@@ -28,17 +18,17 @@ describe('add-group', () => {
 
   it('the group appears in the list on the groups page', async () => {
     await goto('localhost:8080/groups');
-    const groupCardExists = await text(newGroup.name, within($('.card-list'))).exists();
+    const groupCardExists = await text(addGroupCommand.name, within($('.card-list'))).exists();
 
     expect(groupCardExists).toBe(true);
   });
 
   it.failing('the group now has its own page', async () => {
     await goto('localhost:8080/groups');
-    const link = $(`[href="/groups/${newGroup.slug}"].group-card__link`);
+    const link = $(`[href="/groups/${addGroupCommand.slug}"].group-card__link`);
     await click(link);
     const groupName = await $('h1 img').attribute('alt');
 
-    expect(groupName).toBe(newGroup.name);
+    expect(groupName).toBe(addGroupCommand.name);
   });
 });
