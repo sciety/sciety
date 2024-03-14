2d9ec32f63335d2d5a679020a83089a273e3ccc8
David Moulton <d.moulton@elifesciences.org>
2024-03-14 14:50:41 +0000

    Update function signature to permit the next refactoring #3089

--- src/third-parties/fetch-user-avatar-url/fetch-user-avatar-url.ts
+++ src/third-parties/fetch-user-avatar-url/fetch-user-avatar-url.ts
@@ -5,8 +5,10 @@ import { ExternalQueries } from '../external-queries';
 import * as DE from '../../types/data-error';
 import { Logger } from '../../shared-ports';
 import { getAuth0ManagementApiToken } from './get-auth0-management-api-token';
+import { UserId } from '../../types/user-id';
 
-export const fetchUserAvatarUrl = (logger: Logger): ExternalQueries['fetchUserAvatarUrl'] => () => pipe(
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const fetchUserAvatarUrl = (logger: Logger): ExternalQueries['fetchUserAvatarUrl'] => (userId: UserId) => pipe(
   getAuth0ManagementApiToken(logger),
   T.map(() => E.left(DE.unavailable)),
 );
