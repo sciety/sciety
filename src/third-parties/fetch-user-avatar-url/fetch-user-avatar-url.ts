8fb0d8bec10da668fccbeac039c4fe4900137c09
David Moulton <d.moulton@elifesciences.org>
2024-03-14 14:20:48 +0000

    Ignore irrelevant parts of the Axios response #3089

--- src/third-parties/fetch-user-avatar-url/fetch-user-avatar-url.ts
+++ src/third-parties/fetch-user-avatar-url/fetch-user-avatar-url.ts
@@ -10,52 +10,47 @@ import * as DE from '../../types/data-error';
 import { Logger } from '../../shared-ports';
 
 const managementApiTokenCodec = t.type({
-  data: t.type({
-    access_token: t.string,
-    scope: t.literal('read:users'),
-    token_type: t.literal('Bearer'),
-  }),
+  access_token: t.string,
+  scope: t.literal('read:users'),
+  token_type: t.literal('Bearer'),
 });
 
-export const fetchUserAvatarUrl = (logger: Logger): ExternalQueries['fetchUserAvatarUrl'] => () => {
-  logger('debug', 'START fetchUserAvatarUrl');
-
-  return pipe(
-    TE.tryCatch(
-      async () => axios.post<unknown>(
-        `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
-        JSON.stringify({
-          grant_type: 'client_credentials',
-          client_id: process.env.AUTH0_CLIENT_ID,
-          client_secret: process.env.AUTH0_CLIENT_SECRET,
-          audience: `https://${process.env.AUTH0_DOMAIN}/api/v2/`,
-        }),
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
+export const fetchUserAvatarUrl = (logger: Logger): ExternalQueries['fetchUserAvatarUrl'] => () => pipe(
+  TE.tryCatch(
+    async () => axios.post<unknown>(
+      `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
+      JSON.stringify({
+        grant_type: 'client_credentials',
+        client_id: process.env.AUTH0_CLIENT_ID,
+        client_secret: process.env.AUTH0_CLIENT_SECRET,
+        audience: `https://${process.env.AUTH0_DOMAIN}/api/v2/`,
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/json',
         },
-      ),
-      (error) => {
-        if (axios.isAxiosError(error)) {
-          logger('error', 'Failed to get Management API token from Auth0', { error: String(error.response) });
-        } else {
-          logger('error', 'Failed to get Management API token from Auth0', { error: String(error) });
-        }
-
-        return DE.unavailable;
       },
     ),
-    TE.chainEitherK(flow(
-      managementApiTokenCodec.decode,
-      E.bimap(
-        (errors) => {
-          logger('error', 'Unreadable response from Auth0', { errors: formatValidationErrors(errors) });
-          return DE.unavailable;
-        },
-        (response) => response.data.access_token,
-      ),
-    )),
-    T.map(() => E.left(DE.unavailable)),
-  );
-};
+    (error) => {
+      if (axios.isAxiosError(error)) {
+        logger('error', 'Failed to get Management API token from Auth0', { error: String(error.response) });
+      } else {
+        logger('error', 'Failed to get Management API token from Auth0', { error: String(error) });
+      }
+
+      return DE.unavailable;
+    },
+  ),
+  TE.map((axiosResponse) => axiosResponse.data),
+  TE.chainEitherK(flow(
+    managementApiTokenCodec.decode,
+    E.bimap(
+      (errors) => {
+        logger('error', 'Unreadable response from Auth0', { errors: formatValidationErrors(errors) });
+        return DE.unavailable;
+      },
+      (response) => response.access_token,
+    ),
+  )),
+  T.map(() => E.left(DE.unavailable)),
+);
