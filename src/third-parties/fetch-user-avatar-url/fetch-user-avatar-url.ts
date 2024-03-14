5897778987f6a8b73e3ea4f94b42fa038cc1507b
David Moulton <d.moulton@elifesciences.org>
2024-03-14 10:56:37 +0000

    Call axios to get a management API token but ignore the results for now #3089

--- src/third-parties/fetch-user-avatar-url/fetch-user-avatar-url.ts
+++ src/third-parties/fetch-user-avatar-url/fetch-user-avatar-url.ts
@@ -1,7 +1,28 @@
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import axios from 'axios';
 import { ExternalQueries } from '../external-queries';
 import * as DE from '../../types/data-error';
 import { Logger } from '../../shared-ports';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const fetchUserAvatarUrl = (logger: Logger): ExternalQueries['fetchUserAvatarUrl'] => () => TE.left(DE.unavailable);
+export const fetchUserAvatarUrl = (logger: Logger): ExternalQueries['fetchUserAvatarUrl'] => () => pipe(
+  TE.tryCatch(
+    async () => axios.post(
+      `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
+      JSON.stringify({
+        grant_type: 'client_credentials',
+        client_id: process.env.AUTH0_CLIENT_ID,
+        client_secret: process.env.AUTH0_CLIENT_SECRET,
+        audience: `https://${process.env.AUTH0_DOMAIN}/api/v2/`,
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    ),
+    () => DE.unavailable,
+  ),
+  () => TE.left(DE.unavailable),
+);
