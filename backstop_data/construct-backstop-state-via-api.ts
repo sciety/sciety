b9a56c85def0af07ddb50fdfeb46c650f342e810
David Moulton <d.moulton@elifesciences.org>
2024-03-15 14:56:56 +0000

    Explicitly couple stubbed data to a specific command type #3094

--- backstop_data/construct-backstop-state-via-api.ts
+++ backstop_data/construct-backstop-state-via-api.ts
@@ -1,16 +1,18 @@
-import { AddGroupCommand } from '../src/write-side/commands';
 import { callApi } from './../feature-test/helpers/call-api.helper';
+import {DescriptionPath} from '../src/types/description-path';
+import {fromValidatedString} from '../src/types/group-id';
+import {AddGroupCommand} from '../src/write-side/commands';
 
 const constructBackstopStateViaApi = async () => {
-    const groupA = {
-        groupId: 'ba6327db-d783-49a4-af23-deece25d4053',
+    const groupA: AddGroupCommand = {
+        groupId: fromValidatedString('ba6327db-d783-49a4-af23-deece25d4053'),
         name: 'Group A',
         shortDescription: 'Group A description',
         homepage: 'http://example.com/group-a',
         avatarPath: '/static/images/profile-dark.svg',
-        descriptionPath: 'asapbio-scielo-preprint-crowd-review.md',
+        descriptionPath: 'asapbio-scielo-preprint-crowd-review.md' as DescriptionPath,
         slug: 'groupA',
-        largeLogoPath: '/static/groups/large-logos/access-microbiology.png'
+        largeLogoPath: '/static/groups/large-logos/access-microbiology.png',
       }
     await callApi('api/add-group', groupA)
 };
