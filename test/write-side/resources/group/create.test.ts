c71cf1ee62a473fd8733c75c4e0f895e6ded7616
David Moulton <d.moulton@elifesciences.org>
2024-03-15 13:10:06 +0000

    Clarify the language describing the testing scope #3094

--- test/write-side/resources/group/create.test.ts
+++ test/write-side/resources/group/create.test.ts
@@ -46,84 +46,84 @@ describe('create', () => {
     });
   });
 
-  describe('when passed a value taken by another group', () => {
+  describe('when passed a name value taken by another group', () => {
     const otherGroupJoinedEvent = arbitraryGroupJoinedEvent();
 
-    describe('name', () => {
-      describe('and the other group\'s details have never been updated', () => {
-        const result = pipe(
-          [
-            otherGroupJoinedEvent,
-          ],
-          create({ ...addGroupCommand, name: otherGroupJoinedEvent.name }),
-        );
+    describe('and the other group\'s details have never been updated', () => {
+      const result = pipe(
+        [
+          otherGroupJoinedEvent,
+        ],
+        create({ ...addGroupCommand, name: otherGroupJoinedEvent.name }),
+      );
 
-        it('fails with no events raised', () => {
-          expect(E.isLeft(result)).toBe(true);
-        });
-      });
-
-      describe('and the other group\'s name collides due to a previous update', () => {
-        const name = arbitraryString();
-        const result = pipe(
-          [
-            otherGroupJoinedEvent,
-            constructEvent('GroupDetailsUpdated')({
-              groupId: otherGroupJoinedEvent.groupId,
-              name,
-              avatarPath: undefined,
-              descriptionPath: undefined,
-              largeLogoPath: undefined,
-              shortDescription: undefined,
-              homepage: undefined,
-              slug: undefined,
-            }),
-          ],
-          create({ ...addGroupCommand, name }),
-        );
-
-        it('fails with no events raised', () => {
-          expect(E.isLeft(result)).toBe(true);
-        });
+      it('fails with no events raised', () => {
+        expect(E.isLeft(result)).toBe(true);
       });
     });
 
-    describe('slug', () => {
-      describe('and the other group\'s details have never been updated', () => {
-        const result = pipe(
-          [
-            otherGroupJoinedEvent,
-          ],
-          create({ ...addGroupCommand, slug: otherGroupJoinedEvent.slug }),
-        );
+    describe('and the other group\'s name collides due to a previous update', () => {
+      const name = arbitraryString();
+      const result = pipe(
+        [
+          otherGroupJoinedEvent,
+          constructEvent('GroupDetailsUpdated')({
+            groupId: otherGroupJoinedEvent.groupId,
+            name,
+            avatarPath: undefined,
+            descriptionPath: undefined,
+            largeLogoPath: undefined,
+            shortDescription: undefined,
+            homepage: undefined,
+            slug: undefined,
+          }),
+        ],
+        create({ ...addGroupCommand, name }),
+      );
 
-        it('fails with no events raised', () => {
-          expect(E.isLeft(result)).toBe(true);
-        });
+      it('fails with no events raised', () => {
+        expect(E.isLeft(result)).toBe(true);
       });
+    });
+  });
 
-      describe('and the other group\'s slug collides due to a previous update', () => {
-        const slug = arbitraryWord();
-        const result = pipe(
-          [
-            otherGroupJoinedEvent,
-            constructEvent('GroupDetailsUpdated')({
-              groupId: otherGroupJoinedEvent.groupId,
-              name: undefined,
-              avatarPath: undefined,
-              descriptionPath: undefined,
-              shortDescription: undefined,
-              homepage: undefined,
-              largeLogoPath: undefined,
-              slug,
-            }),
-          ],
-          create({ ...addGroupCommand, slug }),
-        );
+  describe('when passed a slug value taken by another group', () => {
+    const otherGroupJoinedEvent = arbitraryGroupJoinedEvent();
 
-        it('fails with no events raised', () => {
-          expect(E.isLeft(result)).toBe(true);
-        });
+    describe('and the other group\'s details have never been updated', () => {
+      const result = pipe(
+        [
+          otherGroupJoinedEvent,
+        ],
+        create({ ...addGroupCommand, slug: otherGroupJoinedEvent.slug }),
+      );
+
+      it('fails with no events raised', () => {
+        expect(E.isLeft(result)).toBe(true);
+      });
+    });
+
+    describe('and the other group\'s slug collides due to a previous update', () => {
+      const slug = arbitraryWord();
+      const result = pipe(
+        [
+          otherGroupJoinedEvent,
+          constructEvent('GroupDetailsUpdated')({
+            groupId: otherGroupJoinedEvent.groupId,
+            name: undefined,
+            avatarPath: undefined,
+            descriptionPath: undefined,
+            shortDescription: undefined,
+            homepage: undefined,
+            largeLogoPath: undefined,
+            slug,
+          }),
+        ],
+        create({ ...addGroupCommand, slug }),
+      );
+
+      it('fails with no events raised', () => {
+        expect(E.isLeft(result)).toBe(true);
       });
     });
   });
