772e7315fa6de04da6608ee698694bea3cc57669
David Moulton <d.moulton@elifesciences.org>
2024-03-15 15:28:24 +0000

    Use more correct test data in one more feature test #3094

--- feature-test/unfollow-group.test.ts
+++ feature-test/unfollow-group.test.ts
@@ -5,19 +5,14 @@ import { callApi } from './helpers/call-api.helper';
 import { screenshotTeardown } from './utilities';
 import { arbitraryUserId } from '../test/types/user-id.helper';
 import { createUserAccountAndLogIn } from './helpers/create-user-account-and-log-in.helper';
-import { arbitraryGroup } from '../test/types/group.helper';
-import { arbitraryString } from '../test/helpers';
+import { arbitraryAddGroupCommand } from '../test/write-side/commands/add-group-command.helper';
 
 describe('unfollow a group', () => {
-  const group = arbitraryGroup();
+  const command = arbitraryAddGroupCommand();
 
   beforeEach(async () => {
     await openBrowser();
-    await callApi('api/add-group', {
-      ...group,
-      groupId: group.id,
-      largeLogoPath: arbitraryString(),
-    });
+    await callApi('api/add-group', command);
   });
 
   afterEach(screenshotTeardown);
@@ -29,12 +24,12 @@ describe('unfollow a group', () => {
 
     describe('from a group page', () => {
       it('removes the group from user page', async () => {
-        await goto(`localhost:8080/groups/${group.slug}`);
+        await goto(`localhost:8080/groups/${command.slug}`);
         await click('Follow');
         await click('Unfollow');
         await click('My lists');
         await click('Following');
-        const groupExists = await text(group.name, within($('.followed-groups'))).exists();
+        const groupExists = await text(command.name, within($('.followed-groups'))).exists();
 
         expect(groupExists).toBe(false);
       });
