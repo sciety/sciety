c2cc95b32b1de863433e177793d623998db13799
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-19 11:23:55 +0000

    Remove use of asymmetric matchers from the home-page tests #3072

--- test/html-pages/home-page/construct-view-model/construct-view-model.test.ts
+++ test/html-pages/home-page/construct-view-model/construct-view-model.test.ts
@@ -1,10 +1,13 @@
 import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
 import { TestFramework, createTestFramework } from '../../../framework';
 import { constructViewModel } from '../../../../src/html-pages/home-page/construct-view-model/construct-view-model';
 import { ViewModel } from '../../../../src/html-pages/home-page/view-model';
 import { arbitraryString } from '../../../helpers';
 import { arbitraryAddGroupCommand } from '../../../write-side/commands/add-group-command.helper';
 import { arbitraryGroupId } from '../../../types/group-id.helper';
+import { GroupLinkWithLogoViewModel } from '../../../../src/shared-components/group-link';
+import { shouldNotBeCalled } from '../../../should-not-be-called';
 
 describe('construct-view-model', () => {
   let framework: TestFramework;
@@ -21,23 +24,25 @@ describe('construct-view-model', () => {
       const addGroup2 = arbitraryAddGroupCommand();
       const group1LogoPath = arbitraryString();
       const group2LogoPath = arbitraryString();
+      let groups: ReadonlyArray<GroupLinkWithLogoViewModel>;
 
       beforeEach(async () => {
         await framework.commandHelpers.addGroup(addGroup1);
         await framework.commandHelpers.updateGroupDetails(addGroup1.groupId, group1LogoPath);
         await framework.commandHelpers.addGroup(addGroup2);
         await framework.commandHelpers.updateGroupDetails(addGroup2.groupId, group2LogoPath);
-        viewModel = constructViewModel(framework.dependenciesForViews, [
-          { groupId: addGroup1.groupId },
-          { groupId: addGroup2.groupId },
-        ]);
+        groups = pipe(
+          constructViewModel(framework.dependenciesForViews, [
+            { groupId: addGroup1.groupId },
+            { groupId: addGroup2.groupId },
+          ]).groups,
+          O.getOrElseW(shouldNotBeCalled),
+        );
       });
 
       it('returns the groups in the specified order', () => {
-        expect(viewModel.groups).toStrictEqual(O.some([
-          expect.objectContaining({ groupName: addGroup1.name }),
-          expect.objectContaining({ groupName: addGroup2.name }),
-        ]));
+        expect(groups[0].groupName).toStrictEqual(addGroup1.name);
+        expect(groups[1].groupName).toStrictEqual(addGroup2.name);
       });
     });
 
