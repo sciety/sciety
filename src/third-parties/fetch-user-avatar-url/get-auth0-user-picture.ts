ac88303664f9cdf97b053a5a827581e69cfef9cb
David Moulton <d.moulton@elifesciences.org>
2024-03-20 11:56:07 +0000

    Ask Auth0 for our user ids without any translation #3089

--- src/third-parties/fetch-user-avatar-url/get-auth0-user-picture.ts
+++ src/third-parties/fetch-user-avatar-url/get-auth0-user-picture.ts
@@ -1,5 +1,6 @@
 import * as TE from 'fp-ts/TaskEither';
 import { URL } from 'url';
+import { pipe } from 'fp-ts/function';
 import { UserId } from '../../types/user-id';
 import * as DE from '../../types/data-error';
 
@@ -7,4 +8,7 @@ export const getAuth0UserPicture = (
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
   userId: UserId,
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-) => (managementApiToken: string): TE.TaskEither<DE.DataError, URL> => TE.right(new URL('http://example.com'));
+) => (managementApiToken: string): TE.TaskEither<DE.DataError, URL> => pipe(
+  `/api/v2/users/${userId}`,
+  () => TE.right(new URL('http://example.com')),
+);
