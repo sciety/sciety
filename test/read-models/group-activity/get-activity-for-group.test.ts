bee2eff615b475d2708c545c53af26e0a3a3d324
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-03-22 10:03:01 +0000

    Extract common setups in a test #3072

--- test/read-models/group-activity/get-activity-for-group.test.ts
+++ test/read-models/group-activity/get-activity-for-group.test.ts
@@ -4,7 +4,7 @@ import { pipe } from 'fp-ts/function';
 import { handleEvent, initialState } from '../../../src/read-models/group-activity/handle-event';
 import { arbitraryGroupId } from '../../types/group-id.helper';
 import { getActivityForGroup } from '../../../src/read-models/group-activity/get-activity-for-group';
-import { constructEvent } from '../../../src/domain-events';
+import { DomainEvent, constructEvent } from '../../../src/domain-events';
 import { arbitraryGroup } from '../../types/group.helper';
 import { arbitraryEvaluationPublicationRecordedEvent, arbitraryEvaluationRemovalRecordedEvent } from '../../domain-events/evaluation-resource-events.helper';
 import { arbitraryDate, arbitraryString } from '../../helpers';
@@ -12,17 +12,22 @@ import { arbitraryEvaluationLocator } from '../../types/evaluation-locator.helpe
 import { arbitraryExpressionDoi } from '../../types/expression-doi.helper';
 import { arbitraryGroupJoinedEvent } from '../../domain-events/group-resource-events.helper';
 import { shouldNotBeCalled } from '../../should-not-be-called';
+import { GroupId } from '../../../src/types/group-id';
 
 describe('get-activity-for-group', () => {
   const group = arbitraryGroup();
 
   describe('when the group has not joined', () => {
-    describe('and no group activity has been recorded', () => {
+    const getActivityForGroupHelper = (events: ReadonlyArray<DomainEvent>, groupId: GroupId) => {
       const readModel = pipe(
-        [],
+        events,
         RA.reduce(initialState(), handleEvent),
       );
-      const result = getActivityForGroup(readModel)(arbitraryGroupId());
+      return getActivityForGroup(readModel)(groupId);
+    };
+
+    describe('and no group activity has been recorded', () => {
+      const result = getActivityForGroupHelper([], arbitraryGroupId());
 
       it('returns O.none', () => {
         expect(result).toStrictEqual(O.none);
@@ -30,20 +35,13 @@ describe('get-activity-for-group', () => {
     });
 
     describe('and an evaluation\'s publication has been recorded', () => {
-      let result: O.Option<unknown>;
-
-      beforeEach(() => {
-        const readModel = pipe(
-          [
-            {
-              ...arbitraryEvaluationPublicationRecordedEvent(),
-              groupId: group.id,
-            },
-          ],
-          RA.reduce(initialState(), handleEvent),
-        );
-        result = getActivityForGroup(readModel)(group.id);
-      });
+      const events = [
+        {
+          ...arbitraryEvaluationPublicationRecordedEvent(),
+          groupId: group.id,
+        },
+      ];
+      const result = getActivityForGroupHelper(events, group.id);
 
       it('returns O.none', () => {
         expect(result).toStrictEqual(O.none);
@@ -51,23 +49,16 @@ describe('get-activity-for-group', () => {
     });
 
     describe('and an evaluation\'s publication has been recorded, and then erased by Sciety', () => {
-      let result: O.Option<unknown>;
-
-      beforeEach(() => {
-        const evaluationLocator = arbitraryEvaluationLocator();
-        const readModel = pipe(
-          [
-            {
-              ...arbitraryEvaluationPublicationRecordedEvent(),
-              groupId: group.id,
-              evaluationLocator,
-            },
-            constructEvent('IncorrectlyRecordedEvaluationErased')({ evaluationLocator }),
-          ],
-          RA.reduce(initialState(), handleEvent),
-        );
-        result = getActivityForGroup(readModel)(group.id);
-      });
+      const evaluationLocator = arbitraryEvaluationLocator();
+      const events = [
+        {
+          ...arbitraryEvaluationPublicationRecordedEvent(),
+          groupId: group.id,
+          evaluationLocator,
+        },
+        constructEvent('IncorrectlyRecordedEvaluationErased')({ evaluationLocator }),
+      ];
+      const result = getActivityForGroupHelper(events, group.id);
 
       it('returns O.none', () => {
         expect(result).toStrictEqual(O.none);
@@ -75,26 +66,19 @@ describe('get-activity-for-group', () => {
     });
 
     describe('and an evaluation\'s publication and removal have been recorded', () => {
-      let result: O.Option<unknown>;
-
-      beforeEach(() => {
-        const evaluationLocator = arbitraryEvaluationLocator();
-        const readModel = pipe(
-          [
-            {
-              ...arbitraryEvaluationPublicationRecordedEvent(),
-              groupId: group.id,
-              evaluationLocator,
-            },
-            {
-              ...arbitraryEvaluationRemovalRecordedEvent(),
-              evaluationLocator,
-            },
-          ],
-          RA.reduce(initialState(), handleEvent),
-        );
-        result = getActivityForGroup(readModel)(group.id);
-      });
+      const evaluationLocator = arbitraryEvaluationLocator();
+      const events = [
+        {
+          ...arbitraryEvaluationPublicationRecordedEvent(),
+          groupId: group.id,
+          evaluationLocator,
+        },
+        {
+          ...arbitraryEvaluationRemovalRecordedEvent(),
+          evaluationLocator,
+        },
+      ];
+      const result = getActivityForGroupHelper(events, group.id);
 
       it('returns O.none', () => {
         expect(result).toStrictEqual(O.none);
@@ -103,6 +87,18 @@ describe('get-activity-for-group', () => {
   });
 
   describe('when the group has joined', () => {
+    const getActivityForGroupHelperExpectingSome = (events: ReadonlyArray<DomainEvent>, groupId: GroupId) => {
+      const readModel = pipe(
+        events,
+        RA.reduce(initialState(), handleEvent),
+      );
+      return pipe(
+        groupId,
+        getActivityForGroup(readModel),
+        O.getOrElseW(shouldNotBeCalled),
+      );
+    };
+
     const groupJoinedEvent = {
       ...arbitraryGroupJoinedEvent(),
       groupId: group.id,
@@ -115,17 +111,7 @@ describe('get-activity-for-group', () => {
     };
 
     describe('when there are no recorded evaluations', () => {
-      const readModel = pipe(
-        [
-          groupJoinedEvent,
-        ],
-        RA.reduce(initialState(), handleEvent),
-      );
-      const activity = pipe(
-        group.id,
-        getActivityForGroup(readModel),
-        O.getOrElseW(shouldNotBeCalled),
-      );
+      const activity = getActivityForGroupHelperExpectingSome([groupJoinedEvent], group.id);
 
       it('returns an evaluationCount of 0', () => {
         expect(activity.evaluationCount).toBe(0);
@@ -138,22 +124,15 @@ describe('get-activity-for-group', () => {
 
     describe('when there is 1 recorded evaluation', () => {
       const publishedAt = new Date();
-      const readModel = pipe(
-        [
-          groupJoinedEvent,
-          {
-            ...arbitraryEvaluationPublicationRecordedEvent(),
-            groupId: group.id,
-            publishedAt,
-          },
-        ],
-        RA.reduce(initialState(), handleEvent),
-      );
-      const activity = pipe(
-        group.id,
-        getActivityForGroup(readModel),
-        O.getOrElseW(shouldNotBeCalled),
-      );
+      const events = [
+        groupJoinedEvent,
+        {
+          ...arbitraryEvaluationPublicationRecordedEvent(),
+          groupId: group.id,
+          publishedAt,
+        },
+      ];
+      const activity = getActivityForGroupHelperExpectingSome(events, group.id);
 
       it('returns an evaluationCount of 1', () => {
         expect(activity.evaluationCount).toBe(1);
@@ -167,28 +146,21 @@ describe('get-activity-for-group', () => {
     describe('when there are 2 evaluations', () => {
       describe('and the most recently recorded is the most recently published evaluation', () => {
         const mostRecentPublishedAt = new Date('2000');
-        const readModel = pipe(
-          [
-            groupJoinedEvent,
-            {
-              ...arbitraryEvaluationPublicationRecordedEvent(),
-              groupId: group.id,
-              publishedAt: new Date('1970'),
-            },
-            {
-              ...arbitraryEvaluationPublicationRecordedEvent(),
-              groupId: group.id,
-              publishedAt: mostRecentPublishedAt,
-            },
+        const events = [
+          groupJoinedEvent,
+          {
+            ...arbitraryEvaluationPublicationRecordedEvent(),
+            groupId: group.id,
+            publishedAt: new Date('1970'),
+          },
+          {
+            ...arbitraryEvaluationPublicationRecordedEvent(),
+            groupId: group.id,
+            publishedAt: mostRecentPublishedAt,
+          },
 
-          ],
-          RA.reduce(initialState(), handleEvent),
-        );
-        const activity = pipe(
-          group.id,
-          getActivityForGroup(readModel),
-          O.getOrElseW(shouldNotBeCalled),
-        );
+        ];
+        const activity = getActivityForGroupHelperExpectingSome(events, group.id);
 
         it('returns an evaluationCount of 2', () => {
           expect(activity.evaluationCount).toBe(2);
@@ -201,27 +173,20 @@ describe('get-activity-for-group', () => {
 
       describe('and the most recently recorded is not the most recently published evaluation', () => {
         const mostRecentPublishedAt = new Date('2000');
-        const readModel = pipe(
-          [
-            groupJoinedEvent,
-            {
-              ...arbitraryEvaluationPublicationRecordedEvent(),
-              groupId: group.id,
-              publishedAt: mostRecentPublishedAt,
-            },
-            {
-              ...arbitraryEvaluationPublicationRecordedEvent(),
-              groupId: group.id,
-              publishedAt: new Date('1970'),
-            },
-          ],
-          RA.reduce(initialState(), handleEvent),
-        );
-        const activity = pipe(
-          group.id,
-          getActivityForGroup(readModel),
-          O.getOrElseW(shouldNotBeCalled),
-        );
+        const events = [
+          groupJoinedEvent,
+          {
+            ...arbitraryEvaluationPublicationRecordedEvent(),
+            groupId: group.id,
+            publishedAt: mostRecentPublishedAt,
+          },
+          {
+            ...arbitraryEvaluationPublicationRecordedEvent(),
+            groupId: group.id,
+            publishedAt: new Date('1970'),
+          },
+        ];
+        const activity = getActivityForGroupHelperExpectingSome(events, group.id);
 
         it('returns an evaluationCount of 2', () => {
           expect(activity.evaluationCount).toBe(2);
@@ -235,23 +200,16 @@ describe('get-activity-for-group', () => {
 
     describe('when one evaluation has been recorded, and erased by Sciety', () => {
       const evaluationLocator = arbitraryEvaluationLocator();
-      const readModel = pipe(
-        [
-          groupJoinedEvent,
-          {
-            ...arbitraryEvaluationPublicationRecordedEvent(),
-            groupId: group.id,
-            evaluationLocator,
-          },
-          constructEvent('IncorrectlyRecordedEvaluationErased')({ evaluationLocator }),
-        ],
-        RA.reduce(initialState(), handleEvent),
-      );
-      const activity = pipe(
-        group.id,
-        getActivityForGroup(readModel),
-        O.getOrElseW(shouldNotBeCalled),
-      );
+      const events = [
+        groupJoinedEvent,
+        {
+          ...arbitraryEvaluationPublicationRecordedEvent(),
+          groupId: group.id,
+          evaluationLocator,
+        },
+        constructEvent('IncorrectlyRecordedEvaluationErased')({ evaluationLocator }),
+      ];
+      const activity = getActivityForGroupHelperExpectingSome(events, group.id);
 
       it('returns an evaluationCount of 0', () => {
         expect(activity.evaluationCount).toBe(0);
@@ -265,28 +223,21 @@ describe('get-activity-for-group', () => {
     describe('when two evaluations have been recorded, and one erased by Sciety', () => {
       const remainingEvaluationPublishedAt = new Date('1999');
       const evaluationToErase = arbitraryEvaluationLocator();
-      const readModel = pipe(
-        [
-          groupJoinedEvent,
-          {
-            ...arbitraryEvaluationPublicationRecordedEvent(),
-            groupId: group.id,
-            publishedAt: remainingEvaluationPublishedAt,
-          },
-          {
-            ...arbitraryEvaluationPublicationRecordedEvent(),
-            groupId: group.id,
-            evaluationLocator: evaluationToErase,
-          },
-          constructEvent('IncorrectlyRecordedEvaluationErased')({ evaluationLocator: evaluationToErase }),
-        ],
-        RA.reduce(initialState(), handleEvent),
-      );
-      const activity = pipe(
-        group.id,
-        getActivityForGroup(readModel),
-        O.getOrElseW(shouldNotBeCalled),
-      );
+      const events = [
+        groupJoinedEvent,
+        {
+          ...arbitraryEvaluationPublicationRecordedEvent(),
+          groupId: group.id,
+          publishedAt: remainingEvaluationPublishedAt,
+        },
+        {
+          ...arbitraryEvaluationPublicationRecordedEvent(),
+          groupId: group.id,
+          evaluationLocator: evaluationToErase,
+        },
+        constructEvent('IncorrectlyRecordedEvaluationErased')({ evaluationLocator: evaluationToErase }),
+      ];
+      const activity = getActivityForGroupHelperExpectingSome(events, group.id);
 
       it('returns an evaluationCount of 1', () => {
         expect(activity.evaluationCount).toBe(1);
@@ -300,31 +251,24 @@ describe('get-activity-for-group', () => {
     describe('when two evaluations have been recorded, and one evaluation removal has been recorded', () => {
       const remainingEvaluationPublishedAt = new Date('1999');
       const evaluationLocator = arbitraryEvaluationLocator();
-      const readModel = pipe(
-        [
-          groupJoinedEvent,
-          {
-            ...arbitraryEvaluationPublicationRecordedEvent(),
-            groupId: group.id,
-            publishedAt: remainingEvaluationPublishedAt,
-          },
-          {
-            ...arbitraryEvaluationPublicationRecordedEvent(),
-            groupId: group.id,
-            evaluationLocator,
-          },
-          {
-            ...arbitraryEvaluationRemovalRecordedEvent(),
-            evaluationLocator,
-          },
-        ],
-        RA.reduce(initialState(), handleEvent),
-      );
-      const activity = pipe(
-        group.id,
-        getActivityForGroup(readModel),
-        O.getOrElseW(shouldNotBeCalled),
-      );
+      const events = [
+        groupJoinedEvent,
+        {
+          ...arbitraryEvaluationPublicationRecordedEvent(),
+          groupId: group.id,
+          publishedAt: remainingEvaluationPublishedAt,
+        },
+        {
+          ...arbitraryEvaluationPublicationRecordedEvent(),
+          groupId: group.id,
+          evaluationLocator,
+        },
+        {
+          ...arbitraryEvaluationRemovalRecordedEvent(),
+          evaluationLocator,
+        },
+      ];
+      const activity = getActivityForGroupHelperExpectingSome(events, group.id);
 
       it('returns an evaluationCount of 1', () => {
         expect(activity.evaluationCount).toBe(1);
@@ -343,25 +287,18 @@ describe('get-activity-for-group', () => {
         authors: [arbitraryString()],
         publishedAt: arbitraryDate(),
       };
-      const readModel = pipe(
-        [
-          groupJoinedEvent,
-          {
-            ...arbitraryEvaluationPublicationRecordedEvent(),
-            ...eventProperties,
-          },
-          {
-            ...arbitraryEvaluationPublicationRecordedEvent(),
-            ...eventProperties,
-          },
-        ],
-        RA.reduce(initialState(), handleEvent),
-      );
-      const activity = pipe(
-        group.id,
-        getActivityForGroup(readModel),
-        O.getOrElseW(shouldNotBeCalled),
-      );
+      const events = [
+        groupJoinedEvent,
+        {
+          ...arbitraryEvaluationPublicationRecordedEvent(),
+          ...eventProperties,
+        },
+        {
+          ...arbitraryEvaluationPublicationRecordedEvent(),
+          ...eventProperties,
+        },
+      ];
+      const activity = getActivityForGroupHelperExpectingSome(events, group.id);
 
       it('returns an evaluationCount of 1', () => {
         expect(activity.evaluationCount).toBe(1);
