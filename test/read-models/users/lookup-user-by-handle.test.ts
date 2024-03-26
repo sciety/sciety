9201ec14bfce34fd9f56fd10427cd4fd16e425bf
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-26 10:01:31 +0000

    Roll out a new event helper to another test suite #3089

--- test/read-models/users/lookup-user-by-handle.test.ts
+++ test/read-models/users/lookup-user-by-handle.test.ts
@@ -2,38 +2,45 @@ import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { arbitraryCandidateUserHandle, candidateUserHandleFromString } from '../../types/candidate-user-handle.helper';
-import { constructEvent } from '../../../src/domain-events';
 import { handleEvent, initialState } from '../../../src/read-models/users/handle-event';
 import { lookupUserByHandle } from '../../../src/read-models/users/lookup-user-by-handle';
-import { arbitraryUserDetails } from '../../types/user-details.helper';
+import { arbitraryUserCreatedAccountEvent } from '../../domain-events/user-resource-events.helper';
+import { shouldNotBeCalled } from '../../should-not-be-called';
 
 describe('lookup-user-by-handle', () => {
-  const user = arbitraryUserDetails();
+  const userCreatedAccountEvent = arbitraryUserCreatedAccountEvent();
 
   describe('when the user exists', () => {
     const readmodel = pipe(
       [
-        constructEvent('UserCreatedAccount')({
-          ...user,
-          userId: user.id,
-        }),
+        userCreatedAccountEvent,
       ],
       RA.reduce(initialState(), handleEvent),
     );
 
     describe('and the requested handle is an identical match', () => {
-      const candidateHandle = candidateUserHandleFromString(user.handle);
+      const user = pipe(
+        userCreatedAccountEvent.handle,
+        candidateUserHandleFromString,
+        lookupUserByHandle(readmodel),
+        O.getOrElseW(shouldNotBeCalled),
+      );
 
       it('returns the user', () => {
-        expect(lookupUserByHandle(readmodel)(candidateHandle)).toStrictEqual(O.some(user));
+        expect(user.id).toStrictEqual(userCreatedAccountEvent.userId);
       });
     });
 
     describe('and the requested handle only differs in case', () => {
-      const candidateHandle = candidateUserHandleFromString(user.handle.toUpperCase());
+      const user = pipe(
+        userCreatedAccountEvent.handle.toUpperCase(),
+        candidateUserHandleFromString,
+        lookupUserByHandle(readmodel),
+        O.getOrElseW(shouldNotBeCalled),
+      );
 
       it('returns the user', () => {
-        expect(lookupUserByHandle(readmodel)(candidateHandle)).toStrictEqual(O.some(user));
+        expect(user.id).toStrictEqual(userCreatedAccountEvent.userId);
       });
     });
   });
