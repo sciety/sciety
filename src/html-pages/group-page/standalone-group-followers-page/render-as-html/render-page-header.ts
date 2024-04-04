9fd80d6531df6cae0eca3330dc7ad7031d78bdb7
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-04-04 14:45:51 +0100

    Start decoupling group page and group followers page headers #3143

---
+++ src/html-pages/group-page/standalone-group-followers-page/render-as-html/render-page-header.ts
@@ -0,0 +1,37 @@
+import { htmlEscape } from 'escape-goat';
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { Group } from '../../../../types/group';
+import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
+import { renderFollowToggle } from '../../common-components/render-follow-toggle';
+
+type PageHeaderViewModel = {
+  group: Group,
+  isFollowing: boolean,
+};
+
+const renderPageHeaderIdentity = (group: PageHeaderViewModel['group']) => pipe(
+  group.largeLogoPath,
+  O.match(
+    () => `<h1>${htmlEscape(group.name)}</h1>`,
+    (largeLogoPath) => htmlEscape`
+    <h1 class="page-header__visual_heading">
+      <img src="${largeLogoPath}" alt="${group.name}" class="page-header__large_logo">
+    </h1>
+  `,
+  ),
+);
+
+export const renderPageHeader = (viewmodel: PageHeaderViewModel): HtmlFragment => toHtmlFragment(`
+  <header class="page-header">
+    <div class="page-header__identity">
+      ${renderPageHeaderIdentity(viewmodel.group)}
+    </div>
+    <p class="group-page-short-description">
+      ${htmlEscape(viewmodel.group.shortDescription)}
+    </p>
+    <div class="group-page-actions">
+      ${renderFollowToggle(viewmodel.group.id, viewmodel.group.name)(viewmodel.isFollowing)}
+    </div>
+  </header>
+`);
