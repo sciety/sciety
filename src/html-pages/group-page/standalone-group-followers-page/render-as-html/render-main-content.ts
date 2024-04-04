91864060ada39111cab714768d34ba40ed1b15fc
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-04-04 13:44:42 +0100

    Remove tabs from standalone page #3143

--- src/html-pages/group-page/standalone-group-followers-page/render-as-html/render-main-content.ts
+++ src/html-pages/group-page/standalone-group-followers-page/render-as-html/render-main-content.ts
@@ -1,12 +1,9 @@
 import { htmlEscape } from 'escape-goat';
 import { flow, pipe } from 'fp-ts/function';
 import * as RA from 'fp-ts/ReadonlyArray';
-import { renderTabs } from '../../../../shared-components/tabs';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 import { ViewModel, UserCardViewModel } from '../view-model';
-import { tabList } from '../../common-components/tab-list';
 import { renderCountWithDescriptor } from '../../../../shared-components/render-count-with-descriptor';
-import { wrapperForTopSpace } from '../../common-components/wrapper-for-top-space';
 
 const renderUserCard = (userCard: UserCardViewModel): HtmlFragment => toHtmlFragment(`
   <article class="user-card">
@@ -32,11 +29,6 @@ const renderFollowersList = (userCards: ReadonlyArray<UserCardViewModel>) => pip
   `),
 );
 
-const tabProps = (viewmodel: ViewModel) => ({
-  tabList: tabList(viewmodel.tabs),
-  activeTabIndex: 3,
-});
-
 export const renderMainContent = (viewmodel: ViewModel): HtmlFragment => pipe(
   `
     <p>
@@ -46,6 +38,4 @@ export const renderMainContent = (viewmodel: ViewModel): HtmlFragment => pipe(
     ${viewmodel.nextLink}
   `,
   toHtmlFragment,
-  wrapperForTopSpace,
-  renderTabs(tabProps(viewmodel)),
 );
