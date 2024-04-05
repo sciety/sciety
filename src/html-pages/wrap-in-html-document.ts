f9ec3e14fe12c3576e698e80baa7ba0e8ecb9a00
Giorgio Sironi <g.sironi@elifesciences.org>
2024-04-05 13:24:42 +0100

    Allow developers to easily observe when server is being restarted #3143

--- src/html-pages/wrap-in-html-document.ts
+++ src/html-pages/wrap-in-html-document.ts
@@ -6,11 +6,14 @@ import { DynamicHeadViewModel, head } from '../shared-components/head';
 import { ContentWrappedInLayout } from './content-wrapped-in-layout';
 import { CompleteHtmlDocument, toCompleteHtmlDocument } from './complete-html-document';
 
+const serverStartupTimestamp = new Date();
+
 export const wrapInHtmlDocument = (user: O.Option<UserDetails>, dynamicHeadViewModel: DynamicHeadViewModel) => (contentWrappedInLayout: ContentWrappedInLayout): CompleteHtmlDocument => toCompleteHtmlDocument(`
   <!doctype html>
   <html lang="en" prefix="og: http://ogp.me/ns#">
     ${head(pipe(user, O.map((u) => u.id)), dynamicHeadViewModel)}
   <body>
+    ${process.env.DISPLAY_LAST_SERVER_STARTUP === 'true' ? `<div>Server restarted at: ${serverStartupTimestamp.toISOString()}</div>` : ''}
     ${googleTagManagerNoScript()}
     ${contentWrappedInLayout}
 
