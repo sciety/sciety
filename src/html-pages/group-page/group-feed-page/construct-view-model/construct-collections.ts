38b191357b649a515feccc32bc33e657a6570e27
David Moulton <d.moulton@elifesciences.org>
2024-04-05 11:47:58 +0100

    Display an image for one more collection card #3148

--- src/html-pages/group-page/group-feed-page/construct-view-model/construct-collections.ts
+++ src/html-pages/group-page/group-feed-page/construct-view-model/construct-collections.ts
@@ -10,6 +10,9 @@ const lookupListImage = (listId: LID.ListId) => {
   if (listId === '729cab51-b47d-4ab5-bf2f-8282f1de445e') {
     return O.some('/static/images/collections/endorsed-by-gigabyte.png');
   }
+  if (listId === '454ba80f-e0bc-47ed-ba76-c8f872c303d2') {
+    return O.some('/static/images/collections/biophysics-colab-collection-1.png');
+  }
   return O.none;
 };
 
