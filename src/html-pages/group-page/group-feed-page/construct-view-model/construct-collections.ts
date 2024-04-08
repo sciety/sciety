6469fc13263ab5f57105f4ca9d75eb304b8d3df6
Giorgio Sironi <g.sironi@elifesciences.org>
2024-04-08 11:28:15 +0100

    Display one more list in Biophysics Colab `Collections` section #3151

--- src/html-pages/group-page/group-feed-page/construct-view-model/construct-collections.ts
+++ src/html-pages/group-page/group-feed-page/construct-view-model/construct-collections.ts
@@ -19,7 +19,10 @@ const constructListCardForACollection = (dependencies: Dependencies) => (listId:
 );
 
 const hardcoded = new Map<GroupId, ReadonlyArray<LID.ListId>>([
-  [GID.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2'), [LID.fromValidatedString('454ba80f-e0bc-47ed-ba76-c8f872c303d2')]],
+  [GID.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2'), [
+    LID.fromValidatedString('454ba80f-e0bc-47ed-ba76-c8f872c303d2'),
+    LID.fromValidatedString('5ac3a439-e5c6-4b15-b109-92928a740812'),
+  ]],
   [GID.fromValidatedString('f7a7aec3-8b1c-4b81-b098-f3f2e4eefe58'), [LID.fromValidatedString('729cab51-b47d-4ab5-bf2f-8282f1de445e')]],
 ]);
 
