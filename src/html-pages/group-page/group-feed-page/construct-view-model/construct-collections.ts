6d4e0af644937284d7a90e2d81dc788e74735a3f
LinaKind <l.kinduryte@elifesciences.org>
2024-04-08 11:23:06 +0100

    Allow hardcoded data structure to contain more than one list per group #3151

--- src/html-pages/group-page/group-feed-page/construct-view-model/construct-collections.ts
+++ src/html-pages/group-page/group-feed-page/construct-view-model/construct-collections.ts
@@ -18,21 +18,22 @@ const constructListCardForACollection = (dependencies: Dependencies) => (listId:
   })),
 );
 
-const hardcoded = new Map<GroupId, LID.ListId>([
-  [GID.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2'), LID.fromValidatedString('454ba80f-e0bc-47ed-ba76-c8f872c303d2')],
-  [GID.fromValidatedString('f7a7aec3-8b1c-4b81-b098-f3f2e4eefe58'), LID.fromValidatedString('729cab51-b47d-4ab5-bf2f-8282f1de445e')],
+const hardcoded = new Map<GroupId, ReadonlyArray<LID.ListId>>([
+  [GID.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2'), [LID.fromValidatedString('454ba80f-e0bc-47ed-ba76-c8f872c303d2')]],
+  [GID.fromValidatedString('f7a7aec3-8b1c-4b81-b098-f3f2e4eefe58'), [LID.fromValidatedString('729cab51-b47d-4ab5-bf2f-8282f1de445e')]],
 ]);
 
 export const constructCollections = (
   dependencies: Dependencies,
   groupId: GroupId,
 ): ViewModel['collections'] => {
-  const hardcodedListId = hardcoded.get(groupId);
-  if (hardcodedListId) {
+  const hardcodedListIds = hardcoded.get(groupId);
+  if (hardcodedListIds) {
     return pipe(
-      hardcodedListId,
-      constructListCardForACollection(dependencies),
-      RA.fromOption,
+      hardcodedListIds,
+      RA.map(constructListCardForACollection(dependencies)),
+      RA.filter(O.isSome),
+      RA.map((listCardViewModel) => listCardViewModel.value),
     );
   }
   return [];
