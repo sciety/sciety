7d276d829db76e00938a9ad640950178b96ff0cf
David Moulton <d.moulton@elifesciences.org>
2024-04-09 10:36:53 +0100

    Remove no longer needed tab #3162

--- src/html-pages/group-page/common-components/tab-list.ts
+++ src/html-pages/group-page/common-components/tab-list.ts
@@ -12,8 +12,4 @@ export const tabList = (viewmodel: TabsViewModel): RNEA.ReadonlyNonEmptyArray<Ta
     label: toHtmlFragment(`<span class="visually-hidden">This group has ${viewmodel.listCount} </span>Lists<span aria-hidden="true"> (${viewmodel.listCount})</span>`),
     url: `/groups/${viewmodel.groupSlug}/lists`,
   },
-  {
-    label: toHtmlFragment('About'),
-    url: `/groups/${viewmodel.groupSlug}/about`,
-  },
 ];
