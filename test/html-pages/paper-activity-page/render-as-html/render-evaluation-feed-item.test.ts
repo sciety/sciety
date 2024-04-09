bf2142b4246f07016f4341d2604780a2a81ff176
Giorgio Sironi <g.sironi@elifesciences.org>
2024-04-09 15:10:57 +0100

    Finish replacing JSDOM in a test #3163

--- test/html-pages/paper-activity-page/render-as-html/render-evaluation-feed-item.test.ts
+++ test/html-pages/paper-activity-page/render-as-html/render-evaluation-feed-item.test.ts
@@ -1,5 +1,4 @@
 import { pipe } from 'fp-ts/function';
-import { JSDOM } from 'jsdom';
 import { load } from 'cheerio';
 import { renderEvaluationPublishedFeedItem } from '../../../../src/html-pages/paper-activity-page/render-as-html/render-evaluation-published-feed-item';
 import { missingFullTextAndSourceLink } from '../../../../src/html-pages/paper-activity-page/render-as-html/static-messages';
@@ -10,6 +9,7 @@ import * as RFI from '../evaluation-feed-item.helper';
 const toggleableContentSelector = '[data-behaviour="collapse_to_teaser"]';
 const teaserSelector = '[data-teaser]';
 const cheerioSafeIdSelector = (evaluationLocator: EvaluationLocator) => `#${evaluationLocatorCodec.encode(evaluationLocator).replace(':', '\\:')}`;
+const sourceLinkSelector = '.activity-feed__item__read_original_source';
 
 describe('render-evaluation-feed-item', () => {
   const fullText = arbitraryWord(100);
@@ -52,7 +52,7 @@ describe('render-evaluation-feed-item', () => {
       expect(parsedResult(toggleableContentSelector)).toHaveLength(0);
       expect(parsedResult(teaserSelector)).toHaveLength(0);
       expect(parsedResult('.activity-feed__item__body').text()).toStrictEqual(expect.stringContaining(fullText));
-      expect(parsedResult('.activity-feed__item__read_original_source').attr('href')).toStrictEqual(source);
+      expect(parsedResult(sourceLinkSelector).attr('href')).toStrictEqual(source);
     });
 
     it('renders an id tag with the correct value', async () => {
@@ -63,50 +63,36 @@ describe('render-evaluation-feed-item', () => {
   describe('when the evaluation has no full text', () => {
     describe('when there is a source link URL', () => {
       const source = 'http://example.com/source';
-      let rendered: DocumentFragment;
       const item = pipe(
         RFI.arbitrary(),
         RFI.withNoFullText,
         RFI.withSource(source),
       );
-
-      beforeEach(() => {
-        rendered = pipe(
-          renderEvaluationPublishedFeedItem(item, 6),
-          JSDOM.fragment,
-        );
-      });
+      const parsedResult = pipe(
+        renderEvaluationPublishedFeedItem(item, 6),
+        load,
+      );
 
       it('renders without a teaser', async () => {
-        const toggleableContent = rendered.querySelector('[data-behaviour="collapse_to_teaser"]');
-        const sourceLinkUrl = rendered.querySelector('.activity-feed__item__read_original_source')?.getAttribute('href');
-
-        expect(toggleableContent).toBeNull();
-        expect(sourceLinkUrl).toStrictEqual(source);
+        expect(parsedResult(toggleableContentSelector)).toHaveLength(0);
+        expect(parsedResult(sourceLinkSelector).attr('href')).toStrictEqual(source);
       });
 
       it('renders an id tag with the correct value', async () => {
-        expect(rendered.getElementById(evaluationLocatorCodec.encode(item.id))).not.toBeNull();
+        expect(parsedResult(cheerioSafeIdSelector(item.id))).toHaveLength(1);
       });
     });
 
     describe('when there is no source link URL', () => {
-      let rendered: DocumentFragment;
       const item = pipe(
         RFI.arbitrary(),
         RFI.withNoFullText,
         RFI.withNoSource,
       );
-
-      beforeEach(() => {
-        rendered = pipe(
-          renderEvaluationPublishedFeedItem(item, arbitraryNumber(6, 10)),
-          JSDOM.fragment,
-        );
-      });
+      const result = renderEvaluationPublishedFeedItem(item, arbitraryNumber(6, 10));
 
       it('displays a message that the evaluation is unavailable', () => {
-        expect(rendered.textContent).toContain(missingFullTextAndSourceLink);
+        expect(result).toContain(missingFullTextAndSourceLink);
       });
     });
   });
