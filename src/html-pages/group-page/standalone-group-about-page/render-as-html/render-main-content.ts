d3c2fe507b5fdea5098c6c4deaed17550887a385
David Moulton <d.moulton@elifesciences.org>
2024-04-09 09:56:22 +0100

    Stop rendering unnecessary tabs #3162

--- src/html-pages/group-page/standalone-group-about-page/render-as-html/render-main-content.ts
+++ src/html-pages/group-page/standalone-group-about-page/render-as-html/render-main-content.ts
@@ -1,16 +1,9 @@
 import { pipe } from 'fp-ts/function';
-import { renderTabs } from '../../../../shared-components/tabs';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
-import { tabList } from '../../common-components/tab-list';
 import { ViewModel } from '../view-model';
 import { renderOurLists } from './render-our-lists';
 import { renderDescription } from './render-description';
 import { wrapperForTopSpace } from '../../common-components/wrapper-for-top-space';
-
-const tabProps = (viewmodel: ViewModel) => ({
-  tabList: tabList(viewmodel.tabs),
-  activeTabIndex: 2,
-});
 
 export const renderMainContent = (viewmodel: ViewModel): HtmlFragment => pipe(
   `
@@ -23,5 +16,4 @@ export const renderMainContent = (viewmodel: ViewModel): HtmlFragment => pipe(
   `,
   toHtmlFragment,
   wrapperForTopSpace,
-  renderTabs(tabProps(viewmodel)),
 );
