531c78dfea711e1c06d282b8dc33e47059d4da94
David Moulton <d.moulton@elifesciences.org>
2024-04-09 11:59:29 +0100

    Remove the old version of the group about page and its corresponding tests #3162

--- src/html-pages/group-page/group-about-page/construct-view-model/dependencies.ts
+++
@@ -1,7 +0,0 @@
-import { Dependencies as TabsViewModelDependencies } from '../../common-components/tabs-view-model';
-import { Queries } from '../../../../read-models';
-import { ExternalQueries } from '../../../../third-parties';
-
-export type Dependencies = Pick<Queries, 'getGroupBySlug' | 'isFollowing' | 'selectAllListsOwnedBy'>
-& ExternalQueries
-& TabsViewModelDependencies;
--- test/html-pages/group-page/group-about-page/render-as-html/render-description.test.ts
+++
@@ -1,9 +0,0 @@
-import { renderDescription } from '../../../../../src/html-pages/group-page/group-about-page/render-as-html/render-description';
-
-describe('render-description', () => {
-  it('renders the group description', async () => {
-    const rendered = renderDescription('Something interesting');
-
-    expect(rendered).toStrictEqual(expect.stringContaining('Something interesting'));
-  });
-});
