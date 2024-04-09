6c8b1987ba8ff4bc8ee81b3ac0abceb5d68ef0a2
LinaKind <l.kinduryte@elifesciences.org>
2024-04-09 11:41:42 +0100

    Clone the existing tests and make them work for the new standalone page #3162

---
+++ test/html-pages/group-page/standalone-group-about-page/construct-view-model/to-our-lists-view-model.test.ts
@@ -0,0 +1,72 @@
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { arbitraryString } from '../../../../helpers';
+import { arbitraryGroup } from '../../../../types/group.helper';
+import { arbitraryList } from '../../../../read-models/lists/list-helper';
+import { toOurListsViewModel } from '../../../../../src/html-pages/group-page/standalone-group-about-page/construct-view-model/to-our-lists-view-model';
+
+describe('to-our-lists-view-model', () => {
+  const groupSlug = arbitraryGroup().slug;
+
+  describe('when the group has more than three lists', () => {
+    const nameOfMostRecentlyUpdatedList = arbitraryString();
+    const model = pipe(
+      [
+        arbitraryList(),
+        arbitraryList(),
+        arbitraryList(),
+        { ...arbitraryList(), name: nameOfMostRecentlyUpdatedList },
+      ],
+      toOurListsViewModel(groupSlug),
+    );
+
+    it('returns list view models for only three lists', () => {
+      expect(model.lists).toHaveLength(3);
+    });
+
+    it('the View All Lists button is set', () => {
+      expect(O.isSome(model.allListsUrl)).toBe(true);
+    });
+
+    it('the View All Lists button is a link to the lists tab', () => {
+      expect(model.allListsUrl).toStrictEqual(O.some(`/groups/${groupSlug}/lists`));
+    });
+  });
+
+  describe('when the group has two or three lists', () => {
+    const nameOfMostRecentlyUpdatedList = arbitraryString();
+    const model = pipe(
+      [
+        arbitraryList(),
+        arbitraryList(),
+        { ...arbitraryList(), name: nameOfMostRecentlyUpdatedList },
+      ],
+      toOurListsViewModel(groupSlug),
+    );
+
+    it('returns list view models for each list', () => {
+      expect(model.lists).toHaveLength(3);
+    });
+
+    it('the View All Lists button is not set', () => {
+      expect(O.isNone(model.allListsUrl)).toBe(true);
+    });
+  });
+
+  describe('when the group has one list', () => {
+    const model = pipe(
+      [
+        arbitraryList(),
+      ],
+      toOurListsViewModel(groupSlug),
+    );
+
+    it('returns list view models for each list', () => {
+      expect(model.lists).toHaveLength(1);
+    });
+
+    it('the View All Lists button is not set', () => {
+      expect(O.isNone(model.allListsUrl)).toBe(true);
+    });
+  });
+});
---
+++ test/html-pages/group-page/standalone-group-about-page/render-as-html/render-description.test.ts
@@ -0,0 +1,9 @@
+import { renderDescription } from '../../../../../src/html-pages/group-page/standalone-group-about-page/render-as-html/render-description';
+
+describe('render-description', () => {
+  it('renders the group description', async () => {
+    const rendered = renderDescription('Something interesting');
+
+    expect(rendered).toStrictEqual(expect.stringContaining('Something interesting'));
+  });
+});
