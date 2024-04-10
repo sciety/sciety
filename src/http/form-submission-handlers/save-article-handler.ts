bba1c05aee073bc0098c74d3f2cd0aec6cb2e8ca
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-04-10 10:53:45 +0100

    Introduce a seam for building a validation and associated recovery

--- src/http/form-submission-handlers/save-article-handler.ts
+++ src/http/form-submission-handlers/save-article-handler.ts
@@ -1,9 +1,11 @@
 import * as E from 'fp-ts/Either';
+import * as R from 'fp-ts/Record';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 import { Middleware } from 'koa';
 import { StatusCodes } from 'http-status-codes';
+import * as RA from 'fp-ts/ReadonlyArray';
 import { Logger } from '../../shared-ports';
 import { articleIdCodec } from '../../types/article-id';
 import { getAuthenticatedUserIdFromContext, Ports as GetLoggedInScietyUserPorts } from '../authentication-and-logging-in-of-sciety-users';
@@ -12,11 +14,13 @@ import { listIdCodec } from '../../types/list-id';
 import { AddArticleToListCommand } from '../../write-side/commands';
 import { addArticleToListCommandHandler } from '../../write-side/command-handlers';
 import { DependenciesForCommands } from '../../write-side/dependencies-for-commands';
-import { unsafeUserInputCodec } from '../../types/unsafe-user-input';
+import { UnsafeUserInput } from '../../types/unsafe-user-input';
 import { sendDefaultErrorHtmlResponse } from '../send-default-error-html-response';
 import { decodeAndLogFailures } from '../../third-parties/decode-and-log-failures';
 import * as LOID from '../../types/list-owner-id';
 import { UserId } from '../../types/user-id';
+import { ValidationRecovery } from '../../html-pages/validation-recovery';
+import { rawUserInputCodec } from '../../read-side/raw-user-input';
 
 export const articleIdFieldName = 'articleid';
 
@@ -30,7 +34,7 @@ const userInvisibleFormFieldsCodec = t.strict({
 });
 
 const userEditableFormFieldsCodec = t.strict({
-  annotation: unsafeUserInputCodec,
+  annotation: rawUserInputCodec,
 });
 
 const saveArticleFormBodyCodec = t.intersection([userInvisibleFormFieldsCodec, userEditableFormFieldsCodec], 'saveArticleFormBodyCodec');
@@ -42,7 +46,7 @@ const isAuthorised = (
 const toCommand = (form: t.TypeOf<typeof saveArticleFormBodyCodec>): AddArticleToListCommand => ({
   articleId: form[articleIdFieldName],
   listId: form.listId,
-  annotation: form.annotation.length === 0 ? undefined : form.annotation,
+  annotation: form.annotation.content.length === 0 ? undefined : form.annotation.content as UnsafeUserInput,
 });
 
 export const saveArticleHandler = (dependencies: Ports): Middleware => async (context) => {
@@ -75,6 +79,27 @@ export const saveArticleHandler = (dependencies: Ports): Middleware => async (co
   if (!isAuthorised(listOwnerId.value, authenticatedUserId.value)) {
     sendDefaultErrorHtmlResponse(dependencies, context, StatusCodes.UNAUTHORIZED, 'You are not the owner of the list you are trying to save to.');
     return;
+  }
+
+  const containsErrors = <A extends Record<string, unknown>>(recovery: ValidationRecovery<A>): boolean => pipe(
+    recovery,
+    R.map((entry) => entry.error),
+    R.toEntries,
+    RA.map((entry) => entry[1]),
+    RA.some(O.isSome),
+  );
+
+  const validationRecovery: ValidationRecovery<t.TypeOf<typeof userEditableFormFieldsCodec>> = pipe(
+    {
+      annotation: {
+        userInput: formBody.right.annotation,
+        error: O.none,
+      },
+    },
+  );
+
+  if (containsErrors(validationRecovery)) {
+    context.redirect('back');
   }
 
   const commandResult = await pipe(
