bc815b4e026521794f75e64897507af63ca14503
LinaKind <l.kinduryte@elifesciences.org>
2024-04-10 13:27:38 +0100

    Write some characterisation to do tests #3165

---
+++ test/html-pages/shared-components/pagination/construct-default-pagination-controls.test.ts
@@ -0,0 +1,9 @@
+describe('construct-default-pagination-controls', () => {
+  describe('given a path with no query string parameters', () => {
+    describe('when there is a nextPage of items', () => {
+      it.todo('the path of the forwardPageHref is this path');
+
+      it.todo('the query string of the forwardPageHref requests the page parameter to be the nextPage value');
+    });
+  });
+});
