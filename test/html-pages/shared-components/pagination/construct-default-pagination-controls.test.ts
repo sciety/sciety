921a70a534017bf9c33127a819e48130ef8b9dd2
David Moulton <d.moulton@elifesciences.org>
2024-04-10 13:54:51 +0100

    Write the first characterisation test #3165

--- test/html-pages/shared-components/pagination/construct-default-pagination-controls.test.ts
+++ test/html-pages/shared-components/pagination/construct-default-pagination-controls.test.ts
@@ -1,7 +1,33 @@
+import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
+import { constructDefaultPaginationControls } from '../../../../src/html-pages/shared-components/pagination';
+import { shouldNotBeCalled } from '../../../should-not-be-called';
+
 describe('construct-default-pagination-controls', () => {
   describe('given a path with no query string parameters', () => {
+    const path = '/foo';
+
     describe('when there is a forwardPage of items', () => {
-      it.todo('the path of the forwardPageHref is this path');
+      let result: string;
+
+      beforeEach(() => {
+        result = pipe(
+          constructDefaultPaginationControls(path, {
+            items: ['a', 'b'],
+            backwardPage: O.none,
+            forwardPage: O.some(2),
+            pageNumber: 1,
+            numberOfPages: 2,
+            numberOfOriginalItems: 4,
+          }),
+          (viewModel) => viewModel.forwardPageHref,
+          O.getOrElseW(shouldNotBeCalled),
+        );
+      });
+
+      it('the path of the forwardPageHref is this path', () => {
+        expect(result.startsWith('/foo?')).toBe(true);
+      });
 
       it.todo('the query string of the forwardPageHref requests the page parameter to be the forwardPage value');
     });
