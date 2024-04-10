9c1ca5b65bcdee2210bd9dd087ded3c37fd0e800
Giorgio Sironi <g.sironi@elifesciences.org>
2024-04-10 14:38:32 +0100

    Remove redundant code #3165

--- src/html-pages/sciety-feed-page/construct-view-model/identify-feed-items.ts
+++ src/html-pages/sciety-feed-page/construct-view-model/identify-feed-items.ts
@@ -1,7 +1,6 @@
 import * as E from 'fp-ts/Either';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { flow } from 'fp-ts/function';
-import * as O from 'fp-ts/Option';
 import { collapseCloseListEvents } from './collapse-close-list-events';
 import { FeedItem } from './feed-item';
 import { DomainEvent, isEventOfType } from '../../../domain-events';
@@ -19,19 +18,7 @@ type IdentifyFeedItems = (pageSize: number, page: number)
 
 export const identifyFeedItems: IdentifyFeedItems = (pageSize, page) => flow(
   RA.filter(isFeedRelevantEvent),
-  RA.match(
-    () => E.right({
-      items: [],
-      backwardPage: O.none,
-      forwardPage: O.none,
-      pageNumber: 1,
-      numberOfPages: 1,
-      numberOfOriginalItems: 0,
-    }),
-    flow(
-      collapseCloseListEvents,
-      RA.reverse,
-      paginate(pageSize, page),
-    ),
-  ),
+  collapseCloseListEvents,
+  RA.reverse,
+  paginate(pageSize, page),
 );
