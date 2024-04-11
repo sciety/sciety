2787b3cac558752b523f46445ecca1611b7a3bd3
LinaKind <l.kinduryte@elifesciences.org>
2024-04-11 14:31:39 +0100

    Deal with pluralisation in the list count #3169

--- src/html-pages/group-page/standalone-group-lists-page/render-as-html/render-main-content.ts
+++ src/html-pages/group-page/standalone-group-lists-page/render-as-html/render-main-content.ts
@@ -1,8 +1,9 @@
+import { renderCountWithDescriptor } from '../../../../shared-components/render-count-with-descriptor';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 import { ViewModel } from '../view-model';
 import { renderListOfListCardsWithFallback } from './render-list-of-list-cards-with-fallback';
 
-const renderListCount = () => '<p>This group has 1 list.</p>';
+const renderListCount = () => `<p>This group has ${renderCountWithDescriptor(1, 'list', 'lists')}.</p>`;
 
 export const renderMainContent = (viewmodel: ViewModel): HtmlFragment => toHtmlFragment(`
   ${renderListCount()}
