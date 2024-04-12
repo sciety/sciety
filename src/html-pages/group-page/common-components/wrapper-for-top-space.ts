84737623fb6b006430d425ad1157128c2a613b0f
David Moulton <d.moulton@elifesciences.org>
2024-04-12 14:30:51 +0100

    Delete unused code #3172

--- src/html-pages/group-page/common-components/wrapper-for-top-space.ts
+++
@@ -1,8 +0,0 @@
-import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
-
-// ts-unused-exports:disable-next-line
-export const wrapperForTopSpace = (wrapped: HtmlFragment): HtmlFragment => toHtmlFragment(`
-  <div class="group-page-tab-panel-content">
-    ${wrapped}
-  </div>
-`);
