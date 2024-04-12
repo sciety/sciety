e17df8755d41cc81955de8764086032b1ac84b3f
LinaKind <l.kinduryte@elifesciences.org>
2024-04-12 11:55:52 +0100

    Rename the file for precision #3169

--- src/html-pages/group-page/group-feed-page/render-as-html/render-page.ts
+++ src/html-pages/group-page/group-feed-page/render-as-html/render-page.ts
@@ -1,5 +1,5 @@
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
-import { renderPageHeader } from '../../common-components/page-header';
+import { renderPageHeader } from '../../common-components/render-page-header';
 import { ViewModel } from '../view-model';
 import { renderMainContent } from './render-main-content';
 
--- src/html-pages/group-page/group-feed-page/view-model.ts
+++ src/html-pages/group-page/group-feed-page/view-model.ts
@@ -2,7 +2,7 @@ import * as E from 'fp-ts/Either';
 import { PaperActivitySummaryCardViewModel, PaperActivityErrorCardViewModel } from '../../../shared-components/paper-activity-summary-card';
 import { PaginationControlsViewModel } from '../../shared-components/pagination';
 import { Group } from '../../../types/group';
-import { PageHeaderViewModel } from '../common-components/page-header';
+import { PageHeaderViewModel } from '../common-components/render-page-header';
 import { TabsViewModel } from '../common-components/tabs-view-model';
 import { ListCardViewModel } from '../../../shared-components/list-card';
 
