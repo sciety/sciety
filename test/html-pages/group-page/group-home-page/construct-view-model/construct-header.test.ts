00a616f78a11d6ac1395b31f542e2c07219a1ee2
LinaKind <l.kinduryte@elifesciences.org>
2024-04-15 15:31:37 +0100

    Write a test list for existing and desired behaviour #3178

---
+++ test/html-pages/group-page/group-home-page/construct-view-model/construct-header.test.ts
@@ -0,0 +1,9 @@
+describe('construct-header', () => {
+  describe('when the group has multiple lists', () => {
+    it.todo('displays a link to the group lists sub page');
+  });
+
+  describe('when the group has only one list', () => {
+    it.todo('does not display a link to the group lists sub page');
+  });
+});
