8201ffe7d495420dccaaa9168fbff67a30506fbb
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-16 11:02:30 +0100

    Reduce the levels of indentation to improve readability #3178

--- src/html-pages/group-page/group-home-page/construct-view-model/construct-header.ts
+++ src/html-pages/group-page/group-home-page/construct-view-model/construct-header.ts
@@ -6,21 +6,24 @@ import { Params } from './params';
 import { Group } from '../../../../types/group';
 import { ViewModel } from '../view-model';
 import { calculateListCount } from '../../common-components/calculate-list-count';
+import { GroupId } from '../../../../types/group-id';
 
 const constructGroupListsPageHref = (group: Group, listCount: number): O.Option<string> => (
   listCount === 1
     ? O.none
     : O.some(`/groups/${group.slug}/lists`));
 
+const checkFollowingStatus = (user: Params['user'], dependencies: Dependencies, groupId: GroupId) => pipe(
+  user,
+  O.fold(
+    () => false,
+    (u) => dependencies.isFollowing(groupId)(u.id),
+  ),
+);
+
 export const constructHeader = (dependencies: Dependencies, user: Params['user']) => (group: Group): ViewModel['header'] => ({
   group,
-  isFollowing: pipe(
-    user,
-    O.fold(
-      () => false,
-      (u) => dependencies.isFollowing(group.id)(u.id),
-    ),
-  ),
+  isFollowing: checkFollowingStatus(user, dependencies, group.id),
   followerCount: pipe(
     dependencies.getFollowers(group.id),
     RA.size,
