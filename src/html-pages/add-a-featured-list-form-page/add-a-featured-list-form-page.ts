0a638258625b83c57adbfdaaade65d7f4d1d5e56
LinaKind <l.kinduryte@elifesciences.org>
2024-04-18 15:01:12 +0100

    Allow construction of the viewmodel to fail #3187

--- src/html-pages/add-a-featured-list-form-page/add-a-featured-list-form-page.ts
+++ src/html-pages/add-a-featured-list-form-page/add-a-featured-list-form-page.ts
@@ -1,17 +1,30 @@
+import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 import { constructViewModel } from './construct-view-model';
 import { renderAddAFeaturedListFormPage } from './render-add-a-featured-list-form-page';
-import { ErrorPageBodyViewModel } from '../../types/error-page-body-view-model';
+import * as DE from '../../types/data-error';
+import { ErrorPageBodyViewModel, toErrorPageBodyViewModel } from '../../types/error-page-body-view-model';
+import { toHtmlFragment } from '../../types/html-fragment';
 import { HtmlPage } from '../html-page';
 
 export const addAFeaturedListFormPageParamsCodec = t.type({
   slug: t.string,
 });
 
+const renderNoSuchGroupError = (): ErrorPageBodyViewModel => toErrorPageBodyViewModel(
+  {
+    type: DE.notFound,
+    message: toHtmlFragment('The group does not exist.'),
+  },
+);
+
 export const addAFeaturedListFormPage = () => (): TE.TaskEither<ErrorPageBodyViewModel, HtmlPage> => pipe(
   constructViewModel(),
-  renderAddAFeaturedListFormPage,
-  TE.right,
+  E.bimap(
+    renderNoSuchGroupError,
+    renderAddAFeaturedListFormPage,
+  ),
+  TE.fromEither,
 );
--- src/html-pages/add-a-featured-list-form-page/construct-view-model.ts
+++ src/html-pages/add-a-featured-list-form-page/construct-view-model.ts
@@ -1,7 +1,9 @@
+import * as E from 'fp-ts/Either';
+
 export type ViewModel = {
   groupName: string,
 };
 
-export const constructViewModel = (): ViewModel => ({
+export const constructViewModel = (): E.Either<unknown, ViewModel> => E.right({
   groupName: 'Group',
 });
