1ca7860e241063dc52d2bc0b79b2efed427960ef
LinaKind <l.kinduryte@elifesciences.org>
2024-04-18 14:29:12 +0100

    Begin to change the form to look as intended #3187

--- src/html-pages/add-a-featured-list-form-page/render-add-a-featured-list-form-page.ts
+++ src/html-pages/add-a-featured-list-form-page/render-add-a-featured-list-form-page.ts
@@ -3,5 +3,16 @@ import { HtmlPage, toHtmlPage } from '../html-page';
 
 export const renderAddAFeaturedListFormPage = (): HtmlPage => toHtmlPage({
   title: 'Add a featured list form',
-  content: toHtmlFragment('Form'),
+  content: toHtmlFragment(`
+  <header class="page-header">
+    <h1>Add a featured list form</h1>
+  </header>
+  <form action="#" method="post" class="standard-form">
+    <section>
+      <label for="listId" class="standard-form__sub_heading">List Id</label>
+      <input type="text" id="listId" name="" value="">
+    </section>
+    <button type="submit">Save</button><a href="#">Cancel</a>
+  </form>
+  `),
 });
