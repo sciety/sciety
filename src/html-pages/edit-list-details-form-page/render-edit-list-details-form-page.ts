087d732d7100827645b35c7af2147a5d99e919f7
LinaKind <l.kinduryte@elifesciences.org>
2024-04-19 10:59:50 +0100

    Make it easier to style cancel buttons consistently #3187

--- src/html-pages/edit-list-details-form-page/render-edit-list-details-form-page.ts
+++ src/html-pages/edit-list-details-form-page/render-edit-list-details-form-page.ts
@@ -37,7 +37,7 @@ export const renderEditListDetailsFormPage = (viewModel: ViewModel): HtmlPage =>
     <textarea id="listDescription" name="description" rows="5" maxlength="${viewModel.listDescriptionMaxLength}">${safelyReflectRawUserInputForEditing(viewModel.listDescription)}</textarea>
     <p class="standard-form__constraints">Maximum ${viewModel.listDescriptionMaxLength} characters</p>
   </section>
-  <button type="submit">Save</button><a href="${viewModel.listHref}" class="edit-list-details-form__cancel">Cancel</a>
+  <button type="submit">Save</button><a href="${viewModel.listHref}" class="standard-form__cancel">Cancel</a>
 </form>
 `),
   },
