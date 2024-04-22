02c69fa491add8110c2cb74a758b9bb19b09717e
David Moulton <d.moulton@elifesciences.org>
2024-04-22 13:34:56 +0100

    Write a test list that surfaces a bigger responsibility #3189

---
+++ test/read-models/lists/select-all-lists-featured-for-group.test.ts
@@ -0,0 +1,9 @@
+describe('select-all-lists-featured-for-group', () => {
+  describe('when no lists have ever been featured for a group', () => {
+    it.todo('returns no lists');
+  });
+
+  describe('when a list has been featured for a group', () => {
+    it.todo('returns that list');
+  });
+});
