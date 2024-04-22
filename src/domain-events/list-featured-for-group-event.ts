994fb112629634b05b0ed8c703eea16b88a55f9d
Giorgio Sironi <g.sironi@elifesciences.org>
2024-04-22 14:24:25 +0100

    Add a codec defining the new event shape #3189

---
+++ src/domain-events/list-featured-for-group-event.ts
@@ -0,0 +1,14 @@
+import * as t from 'io-ts';
+import * as tt from 'io-ts-types';
+import { EventIdFromString } from '../types/codecs/EventIdFromString';
+import { GroupIdFromString } from '../types/codecs/GroupIdFromString';
+import { listIdCodec } from '../types/list-id';
+
+// ts-unused-exports:disable-next-line
+export const listFeaturedForGroupEventCodec = t.type({
+  id: EventIdFromString,
+  type: t.literal('ListFeaturedForGroup'),
+  date: tt.DateFromISOString,
+  listId: listIdCodec,
+  featuredFor: GroupIdFromString,
+});
