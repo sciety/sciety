d5908cc83f88d3303ecb41b70f6e402ef0f8098f
David Moulton <d.moulton@elifesciences.org>
2024-04-22 13:59:03 +0100

    Write a characterisation test #3189

--- test/read-models/lists/select-all-lists-featured-for-group.test.ts
+++ test/read-models/lists/select-all-lists-featured-for-group.test.ts
@@ -1,6 +1,22 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { handleEvent, initialState } from '../../../src/read-models/lists/handle-event';
+import { selectAllListsFeaturedForGroup } from '../../../src/read-models/lists/select-all-lists-featured-for-group';
+import { arbitraryGroupId } from '../../types/group-id.helper';
+
 describe('select-all-lists-featured-for-group', () => {
+  const groupId = arbitraryGroupId();
+
   describe('when no lists have ever been featured for a group', () => {
-    it.todo('returns no lists');
+    const readModel = pipe(
+      [],
+      RA.reduce(initialState(), handleEvent),
+    );
+    const result = selectAllListsFeaturedForGroup(readModel)(groupId);
+
+    it('returns no lists', () => {
+      expect(result).toStrictEqual([]);
+    });
   });
 
   describe('when a list has been featured for a group', () => {
