1c26c0e8b53f0fc476a9639bac1d3344010f432d
LinaKind <l.kinduryte@elifesciences.org>
2024-04-24 10:56:36 +0100

    Refactor to introduce a meaningful name #3189

--- src/write-side/resources/list-promotion/create.ts
+++ src/write-side/resources/list-promotion/create.ts
@@ -1,27 +1,26 @@
 import * as E from 'fp-ts/Either';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
 import { DomainEvent, constructEvent, isEventOfType } from '../../../domain-events';
 import { PromoteListCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
 
-const foo = (command: PromoteListCommand) => (events: ReadonlyArray<DomainEvent>) => pipe(
+const hasGroupAlreadyPromotedSameList = (command: PromoteListCommand) => (events: ReadonlyArray<DomainEvent>) => pipe(
   events,
   RA.filter(isEventOfType('ListPromotionCreated')),
-  RA.filter((event) => event.listId === command.listId && event.byGroup === command.forGroup),
+  RA.some((event) => event.listId === command.listId && event.byGroup === command.forGroup),
 );
 
 export const create: ResourceAction<PromoteListCommand> = (command) => (events) => pipe(
   events,
-  foo(command),
-  (arrayOfEvents) => {
-    if (arrayOfEvents.length === 0) {
-      return [constructEvent('ListPromotionCreated')({
-        listId: command.listId,
-        byGroup: command.forGroup,
-      })];
-    }
-    return [];
-  },
+  hasGroupAlreadyPromotedSameList(command),
+  B.fold(
+    () => [constructEvent('ListPromotionCreated')({
+      listId: command.listId,
+      byGroup: command.forGroup,
+    })],
+    () => [],
+  ),
   E.right,
 );
