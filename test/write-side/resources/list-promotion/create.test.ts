819d16d262e2bcac0a6b8fe300f4644245a0d348
LinaKind <l.kinduryte@elifesciences.org>
2024-04-24 09:54:00 +0100

    Express the intended happy path for the new resource action in the to do test #3189

---
+++ test/write-side/resources/list-promotion/create.test.ts
@@ -0,0 +1,5 @@
+describe('create', () => {
+  describe('when given the id of a list that the group has never before promoted', () => {
+    it.todo('raises exactly one ListPromotionCreated event');
+  });
+});
