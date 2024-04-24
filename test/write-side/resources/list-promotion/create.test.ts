a67653156f8a0b5ef59f879fa7a03c5cb0530a29
David Moulton <d.moulton@elifesciences.org>
2024-04-24 09:59:59 +0100

    Write a complete test list #3189

--- test/write-side/resources/list-promotion/create.test.ts
+++ test/write-side/resources/list-promotion/create.test.ts
@@ -2,4 +2,16 @@ describe('create', () => {
   describe('when given the id of a list that the group has never before promoted', () => {
     it.todo('raises exactly one ListPromotionCreated event');
   });
+
+  describe('when given the id of a list that the group has already promoted', () => {
+    it.todo('raises no events');
+  });
+
+  describe('when given the id of a list that a different group has already promoted', () => {
+    it.todo('raises exactly one ListPromotionCreated event');
+  });
+
+  describe('when the group has already promoted a different list', () => {
+    it.todo('raises exactly one ListPromotionCreated event');
+  });
 });
