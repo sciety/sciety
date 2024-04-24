0b6f31e6c660a99d455397747884b70eda10dae2
LinaKind <l.kinduryte@elifesciences.org>
2024-04-24 11:22:51 +0100

    Remove hardcoded promoted lists, that can now be populated via API #3189

--- src/read-models/lists/select-all-lists-promoted-by-group.ts
+++ src/read-models/lists/select-all-lists-promoted-by-group.ts
@@ -5,23 +5,12 @@ import { pipe } from 'fp-ts/function';
 import { ReadModel } from './handle-event';
 import { List } from './list';
 import { GroupId } from '../../types/group-id';
-import * as GID from '../../types/group-id';
-import * as LID from '../../types/list-id';
-
-const hardcoded = new Map<GroupId, ReadonlyArray<LID.ListId>>([
-  [GID.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2'), [
-    LID.fromValidatedString('454ba80f-e0bc-47ed-ba76-c8f872c303d2'),
-    LID.fromValidatedString('5ac3a439-e5c6-4b15-b109-92928a740812'),
-  ]],
-  [GID.fromValidatedString('f7a7aec3-8b1c-4b81-b098-f3f2e4eefe58'), [LID.fromValidatedString('729cab51-b47d-4ab5-bf2f-8282f1de445e')]],
-]);
 
 export const selectAllListsPromotedByGroup = (
   readModel: ReadModel,
 ) => (
   groupId: GroupId,
 ): ReadonlyArray<List> => {
-  const promotedListIdsThatAreHardcoded = hardcoded.get(groupId) ?? [];
   const promotedListIdsFromReadModel = pipe(
     readModel.byPromotingGroupId,
     R.lookup(groupId),
@@ -33,7 +22,6 @@ export const selectAllListsPromotedByGroup = (
   const promotedLists = pipe(
     [
       ...promotedListIdsFromReadModel,
-      ...promotedListIdsThatAreHardcoded,
     ],
     RA.map((listId) => pipe(
       readModel.byListId,
