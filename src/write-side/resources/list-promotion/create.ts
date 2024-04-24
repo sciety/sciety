444929de49341a059e4fdd47f8ee1d1bc2b75b10
LinaKind <l.kinduryte@elifesciences.org>
2024-04-24 15:35:21 +0100

    Extract a named function for readability #3189

--- src/write-side/resources/list-promotion/create.ts
+++ src/write-side/resources/list-promotion/create.ts
@@ -2,14 +2,20 @@ import * as E from 'fp-ts/Either';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
-import { DomainEvent, constructEvent, isEventOfType } from '../../../domain-events';
+import {
+  DomainEvent, EventOfType, constructEvent, isEventOfType,
+} from '../../../domain-events';
 import { PromoteListCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
+
+const promotionAlreadyExists = (
+  command: PromoteListCommand,
+) => (event: EventOfType<'ListPromotionCreated'>) => event.listId === command.listId && event.byGroup === command.forGroup;
 
 const hasGroupAlreadyPromotedSameList = (command: PromoteListCommand) => (events: ReadonlyArray<DomainEvent>) => pipe(
   events,
   RA.filter(isEventOfType('ListPromotionCreated')),
-  RA.some((event) => event.listId === command.listId && event.byGroup === command.forGroup),
+  RA.some(promotionAlreadyExists(command)),
 );
 
 export const create: ResourceAction<PromoteListCommand> = (command) => (events) => pipe(
