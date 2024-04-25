077b0cd57b925300b0f0518d348095594eb57903
David Moulton <d.moulton@elifesciences.org>
2024-04-25 11:41:37 +0100

    Fix linting error #3195

--- src/read-side/html-pages/mobile-menu/mobile-menu.ts
+++ src/read-side/html-pages/mobile-menu/mobile-menu.ts
@@ -1,8 +1,8 @@
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
-import { constructUserAvatarSrc } from '../../paths';
 import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
 import { UserDetails } from '../../../types/user-details';
+import { constructUserAvatarSrc } from '../../paths';
 
 const renderProfileLink = (user: O.Option<UserDetails>) => pipe(
   user,
