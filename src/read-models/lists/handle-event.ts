4ca15225b86b18450502f476213b1fd125cd2048
Giorgio Sironi <g.sironi@elifesciences.org>
2024-04-25 15:31:11 +0100

    Restructure the read model to use a single set of List objects #3189

--- src/read-models/lists/handle-event.ts
+++ src/read-models/lists/handle-event.ts
@@ -21,7 +21,7 @@ const registerUpdateToList = (readModel: ReadModel, listId: ListId, date: Date) 
 
 export type ReadModel = {
   byListId: Record<ListId, ListState>,
-  byPromotingGroupId: Record<GroupId, Array<ListId>>,
+  byPromotingGroupId: Record<GroupId, Array<List>>,
 };
 
 export const initialState = (): ReadModel => ({
@@ -59,10 +59,13 @@ export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel
     registerUpdateToList(readmodel, event.listId, event.date);
     readmodel.byListId[event.listId].description = rawUserInput(event.description);
   } else if (isEventOfType('ListPromotionCreated')(event)) {
-    if (readmodel.byPromotingGroupId[event.byGroup] === undefined) {
-      readmodel.byPromotingGroupId[event.byGroup] = [event.listId];
-    } else {
-      readmodel.byPromotingGroupId[event.byGroup].push(event.listId);
+    const list = readmodel.byListId[event.listId];
+    if (list !== undefined) {
+      if (readmodel.byPromotingGroupId[event.byGroup] === undefined) {
+        readmodel.byPromotingGroupId[event.byGroup] = [list];
+      } else {
+        readmodel.byPromotingGroupId[event.byGroup].push(list);
+      }
     }
   }
   return readmodel;
--- src/read-models/lists/select-all-lists-promoted-by-group.ts
+++ src/read-models/lists/select-all-lists-promoted-by-group.ts
@@ -1,37 +1,15 @@
 import * as O from 'fp-ts/Option';
-import * as RA from 'fp-ts/ReadonlyArray';
 import * as R from 'fp-ts/Record';
 import { pipe } from 'fp-ts/function';
 import { ReadModel } from './handle-event';
 import { List } from './list';
 import { GroupId } from '../../types/group-id';
-import { ListId } from '../../types/list-id';
 
-const getPromotedListIdsForGroup = (groupId: GroupId) => (index: ReadModel['byPromotingGroupId']): ReadonlyArray<ListId> => pipe(
-  index,
+export const selectAllListsPromotedByGroup = (readModel: ReadModel) => (groupId: GroupId): ReadonlyArray<List> => pipe(
+  readModel.byPromotingGroupId,
   R.lookup(groupId),
   O.match(
     () => [],
     (promotedListIds) => promotedListIds,
   ),
-);
-
-const filterOutNonExistentListIds = (index: ReadModel['byListId']) => (listIds: ReadonlyArray<ListId>) => pipe(
-  listIds,
-  RA.map((listId) => pipe(
-    index,
-    R.lookup(listId),
-  )),
-  RA.filter(O.isSome),
-);
-
-export const selectAllListsPromotedByGroup = (
-  readModel: ReadModel,
-) => (
-  groupId: GroupId,
-): ReadonlyArray<List> => pipe(
-  readModel.byPromotingGroupId,
-  getPromotedListIdsForGroup(groupId),
-  filterOutNonExistentListIds(readModel.byListId),
-  RA.map((option) => option.value),
 );
