1f7755c33d30c12150bd9b4da4d7bc15ef01f68f
LinaKind <l.kinduryte@elifesciences.org>
2024-04-26 14:52:30 +0100

    Start building a codec for the crossref response #3202

--- src/ingest/fetch-reviews-from-access-microbiology.ts
+++ src/ingest/fetch-reviews-from-access-microbiology.ts
@@ -1,10 +1,24 @@
+import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
+import { pipe, flow } from 'fp-ts/function';
+import * as t from 'io-ts';
 import { FetchEvaluations } from './update-all';
+
+const crossrefResponseCodec = t.strict({
+  message: t.strict({
+    items: t.readonlyArray(
+      t.unknown,
+    ),
+  }),
+});
 
 export const fetchReviewsFromAccessMicrobiology: FetchEvaluations = (dependencies) => pipe(
   'https://api.crossref.org/works?filter=prefix:10.1099,type:peer-review,relation.type:is-review-of',
   dependencies.fetchData,
+  TE.chainEitherK(flow(
+    crossrefResponseCodec.decode,
+    E.mapLeft(() => 'acmi: could not decode crossref response'),
+  )),
   TE.map(() => ({
     evaluations: [],
     skippedItems: [],
