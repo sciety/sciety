b3776c64d99ccd75eec7ee4890cfede16d1a87d3
Giorgio Sironi <g.sironi@elifesciences.org>
2024-04-26 16:02:17 +0100

    Separate upgrade functions definition #3200

--- src/infrastructure/upgrade-legacy-event-if-necessary.ts
+++ src/infrastructure/upgrade-legacy-event-if-necessary.ts
@@ -3,27 +3,28 @@ import {
   LegacyEventOfType,
 } from '../domain-events';
 
+const upgradeFunctions = {
+  EvaluationRecorded: (legacyEvent: LegacyEventOfType<'EvaluationRecorded'>) => ({
+    ...legacyEvent,
+    type: 'EvaluationPublicationRecorded' as const,
+  } satisfies EventOfType<'EvaluationPublicationRecorded'>),
+  CurationStatementRecorded: (legacyEvent: LegacyEventOfType<'CurationStatementRecorded'>) => ({
+    ...legacyEvent,
+    authors: undefined,
+    evaluationType: 'curation-statement',
+    type: 'EvaluationUpdated' as const,
+  } satisfies EventOfType<'EvaluationUpdated'>),
+  AnnotationCreated: (legacyEvent: LegacyEventOfType<'AnnotationCreated'>) => ({
+    id: legacyEvent.id,
+    type: 'ArticleInListAnnotated',
+    date: legacyEvent.date,
+    content: legacyEvent.content,
+    articleId: legacyEvent.target.articleId,
+    listId: legacyEvent.target.listId,
+  } satisfies EventOfType<'ArticleInListAnnotated'>),
+};
+
 export const upgradeLegacyEventIfNecessary = (event: CurrentOrLegacyDomainEvent): DomainEvent => {
-  const upgradeFunctions = {
-    EvaluationRecorded: (legacyEvent: LegacyEventOfType<'EvaluationRecorded'>) => ({
-      ...legacyEvent,
-      type: 'EvaluationPublicationRecorded' as const,
-    } satisfies EventOfType<'EvaluationPublicationRecorded'>),
-    CurationStatementRecorded: (legacyEvent: LegacyEventOfType<'CurationStatementRecorded'>) => ({
-      ...legacyEvent,
-      authors: undefined,
-      evaluationType: 'curation-statement',
-      type: 'EvaluationUpdated' as const,
-    } satisfies EventOfType<'EvaluationUpdated'>),
-    AnnotationCreated: (legacyEvent: LegacyEventOfType<'AnnotationCreated'>) => ({
-      id: legacyEvent.id,
-      type: 'ArticleInListAnnotated',
-      date: legacyEvent.date,
-      content: legacyEvent.content,
-      articleId: legacyEvent.target.articleId,
-      listId: legacyEvent.target.listId,
-    } satisfies EventOfType<'ArticleInListAnnotated'>),
-  };
   if (event.type === 'EvaluationRecorded') {
     return upgradeFunctions[event.type](event);
   }
