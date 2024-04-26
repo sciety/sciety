13fdebe81c0050b51491f7a6b315c4de65ec3b36
LinaKind <l.kinduryte@elifesciences.org>
2024-04-26 14:35:17 +0100

    Prepare for fetching data from crossref #3202

--- src/ingest/fetch-reviews-from-access-microbiology.ts
+++ src/ingest/fetch-reviews-from-access-microbiology.ts
@@ -1,7 +1,12 @@
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import { FetchEvaluations } from './update-all';
 
-export const fetchReviewsFromAccessMicrobiology: FetchEvaluations = () => TE.right({
-  evaluations: [],
-  skippedItems: [],
-});
+export const fetchReviewsFromAccessMicrobiology: FetchEvaluations = (dependencies) => pipe(
+  'example.com',
+  dependencies.fetchData,
+  () => TE.right({
+    evaluations: [],
+    skippedItems: [],
+  }),
+);
