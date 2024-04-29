3d1370cf445534dab04908b7a91bed5e6997058d
David Moulton <d.moulton@elifesciences.org>
2024-04-29 15:16:43 +0100

    Move the search form to be within read-side/html-pages/shared-components #3204

--- src/read-side/html-pages/search-page/search-page.ts
+++ src/read-side/html-pages/search-page/search-page.ts
@@ -1,6 +1,6 @@
-import { renderSearchForm } from '../../../shared-components/search-form';
 import { toHtmlFragment } from '../../../types/html-fragment';
 import { HtmlPage, toHtmlPage } from '../html-page';
+import { renderSearchForm } from '../shared-components/search-form';
 
 export const searchPage: HtmlPage = toHtmlPage({
   title: 'Search',
