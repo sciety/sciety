e2b9c96269533b35c3b87519d6d66779c687c1f2
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-04-29 13:46:10 +0100

    Inline dispatch and rely on inferred types rather than explicit ones

--- src/infrastructure/upgrade-legacy-event-if-necessary.ts
+++ src/infrastructure/upgrade-legacy-event-if-necessary.ts
@@ -1,31 +1,9 @@
 import {
   DomainEvent, CurrentOrLegacyDomainEvent, EventOfType,
-  LegacyEventOfType,
 } from '../domain-events';
 import { isEventOfSubset } from '../domain-events/current-or-legacy-domain-event';
 
 const isUpgradableEvent = isEventOfSubset(['EvaluationRecorded', 'CurationStatementRecorded', 'AnnotationCreated']);
-
-const upgradeFunctions = {
-  EvaluationRecorded: (legacyEvent: LegacyEventOfType<'EvaluationRecorded'>) => ({
-    ...legacyEvent,
-    type: 'EvaluationPublicationRecorded' as const,
-  } satisfies EventOfType<'EvaluationPublicationRecorded'>),
-  CurationStatementRecorded: (legacyEvent: LegacyEventOfType<'CurationStatementRecorded'>) => ({
-    ...legacyEvent,
-    authors: undefined,
-    evaluationType: 'curation-statement',
-    type: 'EvaluationUpdated' as const,
-  } satisfies EventOfType<'EvaluationUpdated'>),
-  AnnotationCreated: (legacyEvent: LegacyEventOfType<'AnnotationCreated'>) => ({
-    id: legacyEvent.id,
-    type: 'ArticleInListAnnotated',
-    date: legacyEvent.date,
-    content: legacyEvent.content,
-    articleId: legacyEvent.target.articleId,
-    listId: legacyEvent.target.listId,
-  } satisfies EventOfType<'ArticleInListAnnotated'>),
-};
 
 export const upgradeLegacyEventIfNecessary = (event: CurrentOrLegacyDomainEvent): DomainEvent => {
   if (!isUpgradableEvent(event)) {
@@ -34,10 +12,25 @@ export const upgradeLegacyEventIfNecessary = (event: CurrentOrLegacyDomainEvent)
 
   switch (event.type) {
     case 'EvaluationRecorded':
-      return upgradeFunctions.EvaluationRecorded(event);
-    case 'AnnotationCreated':
-      return upgradeFunctions.AnnotationCreated(event);
+      return ({
+        ...event,
+        type: 'EvaluationPublicationRecorded' as const,
+      } satisfies EventOfType<'EvaluationPublicationRecorded'>);
     case 'CurationStatementRecorded':
-      return upgradeFunctions.CurationStatementRecorded(event);
+      return ({
+        ...event,
+        authors: undefined,
+        evaluationType: 'curation-statement',
+        type: 'EvaluationUpdated' as const,
+      } satisfies EventOfType<'EvaluationUpdated'>);
+    case 'AnnotationCreated':
+      return ({
+        id: event.id,
+        type: 'ArticleInListAnnotated',
+        date: event.date,
+        content: event.content,
+        articleId: event.target.articleId,
+        listId: event.target.listId,
+      }) satisfies EventOfType<'ArticleInListAnnotated'>;
   }
 };
