7ea4dea12759815a5153a6b529ade8efbf3b335f
LinaKind <l.kinduryte@elifesciences.org>
2024-04-30 15:29:16 +0100

    Require the form to decide where to redirect on success #3201

--- src/http/form-submission-handlers/add-a-featured-list-handler.ts
+++ src/http/form-submission-handlers/add-a-featured-list-handler.ts
@@ -3,12 +3,21 @@ import * as O from 'fp-ts/Option';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
+import * as tt from 'io-ts-types';
 import { Middleware } from 'koa';
 import { decodeFormSubmission, Dependencies as DecodeFormSubmissionDependencies } from './decode-form-submission';
 import { ensureUserIsLoggedIn, Dependencies as EnsureUserIsLoggedInDependencies } from './ensure-user-is-logged-in';
 import { promoteListCommandCodec } from '../../write-side/commands';
 import { DependenciesForCommands } from '../../write-side/dependencies-for-commands';
 import * as listPromotion from '../../write-side/resources/list-promotion';
+
+const formBodyCodec = t.intersection([
+  promoteListCommandCodec,
+  t.strict({
+    successRedirectPath: tt.NonEmptyString,
+  }),
+]);
 
 type Dependencies = EnsureUserIsLoggedInDependencies
 & DecodeFormSubmissionDependencies & DependenciesForCommands;
@@ -22,7 +31,7 @@ export const addAFeaturedListHandler = (dependencies: Dependencies): Middleware 
     const formBody = decodeFormSubmission(
       dependencies,
       context,
-      promoteListCommandCodec,
+      formBodyCodec,
       loggedInUser.value.id,
     );
     if (E.isLeft(formBody)) {
@@ -39,6 +48,6 @@ export const addAFeaturedListHandler = (dependencies: Dependencies): Middleware 
       TE.chainW(dependencies.commitEvents),
     )();
 
-    context.redirect('/groups/prereview');
+    context.redirect(formBody.right.successRedirectPath);
   }
 };
