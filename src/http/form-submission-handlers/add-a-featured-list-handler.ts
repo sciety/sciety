c43916be15f0dbbeb8477ae6a4cf66e3170e1e41
Giorgio Sironi <g.sironi@elifesciences.org>
2024-05-02 15:19:36 +0100

    Rename to improve readability #3214

--- src/http/form-submission-handlers/add-a-featured-list-handler.ts
+++ src/http/form-submission-handlers/add-a-featured-list-handler.ts
@@ -22,7 +22,7 @@ const formBodyCodec = t.intersection([
   }),
 ]);
 
-const groupAdmins: Record<UserId, string> = {
+const groupAdministratedBy: Record<UserId, string> = {
   ['auth0|650a91161c07d3acf5ff7da5' as UserId]: 'd6e1a913-76f8-40dc-9074-8eac033e1bc8',
   ['twitter|1384541806231175172' as UserId]: '4bbf0c12-629b-4bb8-91d6-974f4df8efb2',
   ['auth0|65faae8fd0f034a2c4c72b7c' as UserId]: '10360d97-bf52-4aef-b2fa-2f60d319edd7',
@@ -31,7 +31,7 @@ const groupAdmins: Record<UserId, string> = {
 };
 
 const isUserAdminOfThisGroup = (userId: UserId, groupId: GroupId) => (
-  groupAdmins[userId] === groupId
+  groupAdministratedBy[userId] === groupId
 );
 
 type Dependencies = EnsureUserIsLoggedInDependencies
