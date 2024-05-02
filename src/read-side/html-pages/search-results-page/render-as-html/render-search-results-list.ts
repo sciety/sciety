974bf62e3fd6f16520ee0d1516beb4513578b2fd
David Moulton <d.moulton@elifesciences.org>
2024-05-02 15:02:31 +0100

    Extract common code to an article-list shared component

--- src/read-side/html-pages/search-results-page/render-as-html/render-search-results-list.ts
+++ src/read-side/html-pages/search-results-page/render-as-html/render-search-results-list.ts
@@ -3,16 +3,13 @@ import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import { pipe } from 'fp-ts/function';
 import { renderListItems } from '../../../../shared-components/render-list-items';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
+import { renderArticleList } from '../../shared-components/article-list';
 
 export const renderSearchResultsList = (cards: ReadonlyArray<HtmlFragment>): O.Option<HtmlFragment> => pipe(
   cards,
   RNEA.fromReadonlyArray,
   O.map(
-    (a) => `
-      <ol class="article-list" role="list">
-        ${renderListItems(a)}
-      </ol>
-    `,
+    (a) => renderArticleList(renderListItems(a)),
   ),
   O.map(toHtmlFragment),
 );
