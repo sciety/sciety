8f0d06b6f6896d1513d34cbf5575e249d587fa2d
David Moulton <d.moulton@elifesciences.org>
2024-05-03 14:19:54 +0100

    Improve the screen reader experience after adding an annotation #3219

--- src/read-side/html-pages/shared-components/success-banner/success-banner.ts
+++ src/read-side/html-pages/shared-components/success-banner/success-banner.ts
@@ -1,9 +1,9 @@
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 
 export const successBanner = (message: string): HtmlFragment => toHtmlFragment(`
-  <div class="success-banner">
+  <div class="success-banner" role="region" aria-labelledby="success-notification-banner-title" >
     <div class="success-banner__heading">
-      <h2>Success</h2>
+      <h2 id="success-notification-banner-title">Success</h2>
     </div>
     <p>
       ${message}
