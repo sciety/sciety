99a8a7983e7dca54231b422abdb60413075f2c0a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-05-07 09:51:15 +0100

    Remove an unnecessary level of indirection #3219

--- src/read-side/html-pages/list-page/render-as-html/render-success-banner.ts
+++ src/read-side/html-pages/list-page/render-as-html/render-success-banner.ts
@@ -1,19 +1,17 @@
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 import { ViewModel } from '../view-model';
 
-const successBanner = (message: string): HtmlFragment => toHtmlFragment(`
+export const renderSuccessBanner = (viewModel: ViewModel): HtmlFragment => (
+  viewModel.showAnnotationSuccessBanner
+    ? toHtmlFragment(`
   <div class="success-banner" role="region" aria-labelledby="success-notification-banner-title" >
     <div class="success-banner__heading">
       <h2 id="success-notification-banner-title">Success</h2>
     </div>
     <p>
-      ${message}
+      Your comment has been added. You are contributing to a network of researchers advancing open science.
     </p>
   </div>
-`);
-
-export const renderSuccessBanner = (viewModel: ViewModel): HtmlFragment => (
-  viewModel.showAnnotationSuccessBanner
-    ? successBanner('Your comment has been added. You are contributing to a network of researchers advancing open science.')
+`)
     : toHtmlFragment('')
 );
