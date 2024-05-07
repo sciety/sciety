91549c9d96b73662972d77d470eef16d2521b91e
Giorgio Sironi <g.sironi@elifesciences.org>
2024-05-07 09:47:10 +0100

    Improve encapsulation #3219

--- src/read-side/html-pages/list-page/render-as-html/render-success-banner.ts
+++ src/read-side/html-pages/list-page/render-as-html/render-success-banner.ts
@@ -1,6 +1,16 @@
-import { successBanner } from './success-banner';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 import { ViewModel } from '../view-model';
+
+const successBanner = (message: string): HtmlFragment => toHtmlFragment(`
+  <div class="success-banner" role="region" aria-labelledby="success-notification-banner-title" >
+    <div class="success-banner__heading">
+      <h2 id="success-notification-banner-title">Success</h2>
+    </div>
+    <p>
+      ${message}
+    </p>
+  </div>
+`);
 
 export const renderSuccessBanner = (viewModel: ViewModel): HtmlFragment => (
   viewModel.showAnnotationSuccessBanner
