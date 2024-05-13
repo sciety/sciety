cca6f1db58782dcf570b8506c77dbc7eeac34464
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-05-13 13:31:16 +0100

    Complete characterisation tests #3234

--- test/ingest/evaluation-discovery/ingestion-window-start-date.test.ts
+++ test/ingest/evaluation-discovery/ingestion-window-start-date.test.ts
@@ -18,12 +18,18 @@ describe('ingestion-window-start-date', () => {
   });
 
   describe('when both ingestDays and earliestAllowedStartDate are specified', () => {
+    const earliestAllowedStartDate = new Date('2019-01-01');
+
     describe('when ingestDays does not exceed the earliestAllowedStartDate', () => {
-      it.todo('returns a date in the past');
+      it('returns a date in the past', () => {
+        expect(ingestionWindowStartDate(5, earliestAllowedStartDate)).toStrictEqual(new Date('2020-01-01'));
+      });
     });
 
     describe('when ingestDays goes to far into the past', () => {
-      it.todo('returns the earliestAllowedStartDate');
+      it('returns the earliestAllowedStartDate', () => {
+        expect(ingestionWindowStartDate(5000, earliestAllowedStartDate)).toStrictEqual(new Date('2019-01-01'));
+      });
     });
   });
 });
