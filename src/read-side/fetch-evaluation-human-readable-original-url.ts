c83a0ec130dc666a98cd22688a98c3e91989d969
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-05-14 10:26:33 +0100

    Prefer inferred evaluation human readable original urls over contacting 3rd parties to get them #3239

--- src/read-side/fetch-evaluation-human-readable-original-url.ts
+++ src/read-side/fetch-evaluation-human-readable-original-url.ts
@@ -1,9 +1,10 @@
 import { URL } from 'url';
+import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { ExternalQueries } from '../third-parties';
 import * as DE from '../types/data-error';
-import { EvaluationLocator } from '../types/evaluation-locator';
+import { EvaluationLocator, inferredSourceUrl } from '../types/evaluation-locator';
 
 type Dependencies = ExternalQueries;
 
@@ -12,6 +13,13 @@ export const fetchEvaluationHumanReadableOriginalUrl = (
   dependencies: Dependencies,
 ): TE.TaskEither<DE.DataError, URL> => pipe(
   evaluationLocator,
-  dependencies.fetchEvaluationDigest,
-  TE.map(({ url }) => url),
+  inferredSourceUrl,
+  O.match(
+    () => pipe(
+      evaluationLocator,
+      dependencies.fetchEvaluationDigest,
+      TE.map(({ url }) => url),
+    ),
+    (url) => TE.right(url),
+  ),
 );
