0a16b70432bda109de2c125894e122719a48ec39
Giorgio Sironi <g.sironi@elifesciences.org>
2024-05-14 09:30:01 +0100

    Decouple return type of seam from third parties #3239

--- src/read-side/fetch-evaluation.ts
+++ src/read-side/fetch-evaluation.ts
@@ -1,11 +1,17 @@
+import { URL } from 'url';
 import * as TE from 'fp-ts/TaskEither';
 import { Queries } from '../read-models';
 import { ExternalQueries } from '../third-parties';
 import * as DE from '../types/data-error';
-import { Evaluation } from '../types/evaluation';
 import { EvaluationLocator } from '../types/evaluation-locator';
+import { SanitisedHtmlFragment } from '../types/sanitised-html-fragment';
 
 type Dependencies = Queries & ExternalQueries;
+
+type Evaluation = {
+  fullText: SanitisedHtmlFragment,
+  url: URL,
+};
 
 type FetchEvaluation = (evaluationLocator: EvaluationLocator) => TE.TaskEither<DE.DataError, Evaluation>;
 
