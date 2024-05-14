934e0b996e91879ffcaac6f5f11e825ded204f4c
Giorgio Sironi <g.sironi@elifesciences.org>
2024-05-14 13:26:36 +0100

    Write a test list prior to refactoring existing behavior #3239

---
+++ test/read-side/html-pages/paper-activity-page/construct-view-model/to-evaluation-published-feed-item.test.ts
@@ -0,0 +1,17 @@
+describe('to-evaluation-published-feed-item', () => {
+  describe('when the human readable original URL is available for the evaluation', () => {
+    it.todo('displays it as a link');
+  });
+
+  describe('when the human readable original URL is not available for the evaluation', () => {
+    it.todo('hides the link');
+  });
+
+  describe('when the evaluation digest is available', () => {
+    it.todo('displays the digest');
+  });
+
+  describe('when the evaluation digest is not available', () => {
+    it.todo('hides the digest');
+  });
+});
