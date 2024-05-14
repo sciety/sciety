2811692bcbbd485c8fcdaa55a18dd8f9b170763c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-05-14 13:34:59 +0100

    Write a characterisation test for a happy path #3239

--- test/read-side/html-pages/paper-activity-page/construct-view-model/to-evaluation-published-feed-item.test.ts
+++ test/read-side/html-pages/paper-activity-page/construct-view-model/to-evaluation-published-feed-item.test.ts
@@ -1,6 +1,39 @@
+import { URL } from 'url';
+import * as O from 'fp-ts/Option';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { toEvaluationPublishedFeedItem } from '../../../../../src/read-side/html-pages/paper-activity-page/construct-view-model/to-evaluation-published-feed-item';
+import { EvaluationPublishedFeedItem } from '../../../../../src/read-side/html-pages/paper-activity-page/view-model';
+import { TestFramework, createTestFramework } from '../../../../framework';
+import { arbitraryUri } from '../../../../helpers';
+import { arbitraryRecordedEvaluation } from '../../../../types/recorded-evaluation.helper';
+
 describe('to-evaluation-published-feed-item', () => {
+  let framework: TestFramework;
+
+  beforeEach(async () => {
+    framework = createTestFramework();
+  });
+
   describe('when the human readable original URL is available for the evaluation', () => {
-    it.todo('displays it as a link');
+    const humanReadableOriginalUrl = new URL(arbitraryUri());
+    let sourceHref: EvaluationPublishedFeedItem['sourceHref'];
+
+    beforeEach(async () => {
+      sourceHref = await pipe(
+        arbitraryRecordedEvaluation(),
+        toEvaluationPublishedFeedItem({
+          ...framework.dependenciesForViews,
+          fetchEvaluationHumanReadableOriginalUrl: () => TE.right(humanReadableOriginalUrl),
+        }),
+        T.map((feedItem) => feedItem.sourceHref),
+      )();
+    });
+
+    it('displays it as a link', () => {
+      expect(sourceHref).toStrictEqual(O.some(humanReadableOriginalUrl));
+    });
   });
 
   describe('when the human readable original URL is not available for the evaluation', () => {
