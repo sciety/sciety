e26ee330673a1ca26e072c40e54e99564feb6551
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-05-14 09:25:52 +0100

    Decouple the type of a read-side function from a particular external query #3239

--- src/read-side/fetch-evaluation.ts
+++ src/read-side/fetch-evaluation.ts
@@ -1,7 +1,12 @@
+import * as TE from 'fp-ts/TaskEither';
 import { Queries } from '../read-models';
 import { ExternalQueries } from '../third-parties';
+import * as DE from '../types/data-error';
+import { Evaluation } from '../types/evaluation';
+import { EvaluationLocator } from '../types/evaluation-locator';
 
 type Dependencies = Queries & ExternalQueries;
 
-// ts-unused-exports:disable-next-line
-export const fetchEvaluation = (dependencies: Dependencies): ExternalQueries['fetchEvaluation'] => dependencies.fetchEvaluation;
+type FetchEvaluation = (evaluationLocator: EvaluationLocator) => TE.TaskEither<DE.DataError, Evaluation>;
+
+export const fetchEvaluation = (dependencies: Dependencies): FetchEvaluation => dependencies.fetchEvaluation;
