6fda13056ac2dd3b819a4c7230a9bbbe5e44862d
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-05-14 09:33:53 +0100

    Capture intention to migrate away from a type #3239

--- src/types/evaluation.ts
+++ src/types/evaluation.ts
@@ -1,6 +1,9 @@
 import { URL } from 'url';
 import { SanitisedHtmlFragment } from './sanitised-html-fragment';
 
+/**
+ * @deprecated Refer to read-side/fetch-evaluation instead, which should be the only producer of this type
+ */
 export type Evaluation = {
   fullText: SanitisedHtmlFragment,
   url: URL,
