4510a300cf08f0e48935978f2cfe7c6e5812b9a5
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-05-14 13:59:52 +0100

    Simplify construction of digest #3239

--- src/read-side/html-pages/paper-activity-page/construct-view-model/to-evaluation-published-feed-item.ts
+++ src/read-side/html-pages/paper-activity-page/construct-view-model/to-evaluation-published-feed-item.ts
@@ -4,25 +4,10 @@ import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { Dependencies } from './dependencies';
-import * as EL from '../../../../types/evaluation-locator';
 import { RecordedEvaluation } from '../../../../types/recorded-evaluation';
 import { sanitise } from '../../../../types/sanitised-html-fragment';
 import { detectLanguage } from '../../shared-components/lang-attribute';
 import { EvaluationPublishedFeedItem } from '../view-model';
-
-const constructEvaluation = (
-  dependencies: Dependencies,
-) => (evaluationLocator: EL.EvaluationLocator) => pipe(
-  {
-    fullText: pipe(
-      evaluationLocator,
-      dependencies.fetchEvaluationDigest,
-      TE.map(({ fullText }) => fullText),
-    ),
-    url: dependencies.fetchEvaluationHumanReadableOriginalUrl(evaluationLocator),
-  },
-  sequenceS(TE.ApplySeq),
-);
 
 export const toEvaluationPublishedFeedItem = (dependencies: Dependencies) => (
   evaluation: RecordedEvaluation,
@@ -51,18 +36,16 @@ export const toEvaluationPublishedFeedItem = (dependencies: Dependencies) => (
     ),
     review: pipe(
       evaluation.evaluationLocator,
-      constructEvaluation(dependencies),
+      dependencies.fetchEvaluationDigest,
+      TE.map(({ fullText }) => fullText),
       TE.match(
         () => ({
-          url: EL.inferredSourceUrl(evaluation.evaluationLocator),
           fullText: O.none,
           fullTextLanguageCode: O.none,
         }),
-        (review) => ({
-          ...review,
-          url: O.some(review.url),
-          fullText: O.some(review.fullText),
-          fullTextLanguageCode: detectLanguage(review.fullText),
+        (digest) => ({
+          fullText: O.some(digest),
+          fullTextLanguageCode: detectLanguage(digest),
         }),
       ),
     ),
