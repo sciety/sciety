d92d849ae7311d5be2cc94e73cd85b4e9d61ac52
Giorgio Sironi <g.sironi@elifesciences.org>
2024-05-15 14:56:35 +0100

    Widen output type to provide a scenario for the contract #3245

--- src/read-side/non-html-views/docmaps/docmap/docmap.ts
+++ src/read-side/non-html-views/docmaps/docmap/docmap.ts
@@ -1,9 +1,9 @@
 import * as E from 'fp-ts/Either';
+import { Json } from 'fp-ts/Json';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { StatusCodes } from 'http-status-codes';
 import * as t from 'io-ts';
-import { Docmap } from './docmap-type';
 import { Ports, generateDocmaps } from './generate-docmaps';
 import { NonHtmlViewError } from '../../non-html-view-error';
 import { NonHtmlViewParams } from '../../non-html-view-params';
@@ -16,7 +16,7 @@ export const docmap = (
   ports: Ports,
 ) => (
   params: NonHtmlViewParams,
-): TE.TaskEither<NonHtmlViewError, ReadonlyArray<Docmap>> => pipe(
+): TE.TaskEither<NonHtmlViewError, Json> => pipe(
   params,
   paramsCodec.decode,
   E.mapLeft(() => ({ status: StatusCodes.BAD_REQUEST, message: 'Cannot understand the request' })),
