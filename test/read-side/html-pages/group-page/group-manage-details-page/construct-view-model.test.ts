968640b3c551c0e824eefda6b7bd6e029968180f
Giorgio Sironi <g.sironi@elifesciences.org>
2024-05-20 15:45:09 +0100

    Capture current responsibilities and intentions in a test list #3258

---
+++ test/read-side/html-pages/group-page/group-manage-details-page/construct-view-model.test.ts
@@ -0,0 +1,13 @@
+describe('construct-view-model', () => {
+  describe('when the user is not logged in', () => {
+    it.todo('undefined behavior');
+  });
+
+  describe('when the user is authorized to feature lists for this group', () => {
+    it.todo('returns on the right');
+  });
+
+  describe('when the user is not authorized to feature lists for this group', () => {
+    it.todo('returns an unauthorized status on the left');
+  });
+});
