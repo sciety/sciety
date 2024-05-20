9b3513a8bcdb01f629142482985a46b953af1699
David Moulton <d.moulton@elifesciences.org>
2024-05-20 14:51:45 +0100

    Create a seam for the authorisation decision #3258

--- src/read-side/html-pages/group-page/group-manage-details-page/construct-and-render-page.ts
+++ src/read-side/html-pages/group-page/group-manage-details-page/construct-and-render-page.ts
@@ -1,13 +1,22 @@
+import * as E from 'fp-ts/Either';
+import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { Params } from './params';
-import { ErrorPageBodyViewModel } from '../../../../types/error-page-body-view-model';
+import * as DE from '../../../../types/data-error';
+import { ErrorPageBodyViewModel, toErrorPageBodyViewModel } from '../../../../types/error-page-body-view-model';
 import { toHtmlFragment } from '../../../../types/html-fragment';
 import { HtmlPage, toHtmlPage } from '../../html-page';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const constructViewModel = (params: Params): E.Either<unknown, void> => E.right(undefined);
 
 type GroupPage = (params: Params) => TE.TaskEither<ErrorPageBodyViewModel, HtmlPage>;
 
 export const constructAndRenderPage: GroupPage = (params) => pipe(
   params,
-  () => TE.right(toHtmlPage({ title: 'Manage details', content: toHtmlFragment('') })),
+  constructViewModel,
+  E.mapLeft(() => (toErrorPageBodyViewModel({ type: DE.unavailable, message: toHtmlFragment('You are not authorised to proceed.') }))),
+  E.map(() => toHtmlPage({ title: 'Manage details', content: toHtmlFragment('') })),
+  T.of,
 );
