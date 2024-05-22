a8233a63d580fc091c18c04ebb85f4c846e07b7c
LinaKind <l.kinduryte@elifesciences.org>
2024-05-22 13:32:58 +0100

    Extract a seam for authorisation #3263

--- src/http/form-submission-handlers/add-a-featured-list-handler.ts
+++ src/http/form-submission-handlers/add-a-featured-list-handler.ts
@@ -7,8 +7,7 @@ import * as tt from 'io-ts-types';
 import { Middleware } from 'koa';
 import { decodeFormSubmission, Dependencies as DecodeFormSubmissionDependencies } from './decode-form-submission';
 import { ensureUserIsLoggedIn, Dependencies as EnsureUserIsLoggedInDependencies } from './ensure-user-is-logged-in';
-import { GroupId } from '../../types/group-id';
-import { UserId } from '../../types/user-id';
+import { isUserAdminOfThisGroup } from './is-user-admin-of-this-group';
 import { promoteListCommandCodec } from '../../write-side/commands';
 import { DependenciesForCommands } from '../../write-side/dependencies-for-commands';
 import { executeResourceAction } from '../../write-side/resources/execute-resource-action';
@@ -21,18 +20,6 @@ const formBodyCodec = t.intersection([
     successRedirectPath: tt.NonEmptyString,
   }),
 ]);
-
-const groupAdministratedBy: Record<UserId, string> = {
-  ['auth0|650a91161c07d3acf5ff7da5' as UserId]: 'd6e1a913-76f8-40dc-9074-8eac033e1bc8',
-  ['twitter|1384541806231175172' as UserId]: '4bbf0c12-629b-4bb8-91d6-974f4df8efb2',
-  ['auth0|65faae8fd0f034a2c4c72b7c' as UserId]: '10360d97-bf52-4aef-b2fa-2f60d319edd7',
-  ['twitter|1443469693621309444' as UserId]: 'b5f31635-d32b-4df9-92a5-0325a1524343',
-  ['twitter|380816062' as UserId]: 'b560187e-f2fb-4ff9-a861-a204f3fc0fb0',
-};
-
-const isUserAdminOfThisGroup = (userId: UserId, groupId: GroupId) => (
-  groupAdministratedBy[userId] === groupId
-);
 
 type Dependencies = EnsureUserIsLoggedInDependencies
 & DecodeFormSubmissionDependencies & DependenciesForCommands;
