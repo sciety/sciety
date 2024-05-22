abd1d76a25b9e791331523c90ce430743a95a54e
David Moulton <d.moulton@elifesciences.org>
2024-05-22 13:53:18 +0100

    Write a test list for both existing and desired behaviour #3263

---
+++ test/read-side/html-pages/group-page/group-add-a-featured-list-form-page/construct-view-model.test.ts
@@ -0,0 +1,15 @@
+describe('construct-view-model', () => {
+  describe('when the group can be found', () => {
+    describe('and the user is an admin of this group', () => {
+      it.todo('returns on the right');
+    });
+
+    describe('and the user is not an admin of this group', () => {
+      it.todo('returns on the left');
+    });
+  });
+
+  describe('when the group can not be found', () => {
+    it.todo('returns on the left');
+  });
+});
