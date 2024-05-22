7854f86a07847242da067ce334b7289b469fc805
Giorgio Sironi <g.sironi@elifesciences.org>
2024-05-22 14:01:26 +0100

    Write a passing characterization test #3263

--- test/read-side/html-pages/group-page/group-add-a-featured-list-form-page/construct-view-model.test.ts
+++ test/read-side/html-pages/group-page/group-add-a-featured-list-form-page/construct-view-model.test.ts
@@ -1,4 +1,17 @@
+import * as E from 'fp-ts/Either';
+import { constructViewModel } from '../../../../../src/read-side/html-pages/group-page/group-add-a-featured-list-form-page/construct-view-model';
+import { ViewModel } from '../../../../../src/read-side/html-pages/group-page/group-add-a-featured-list-form-page/view-model';
+import { TestFramework, createTestFramework } from '../../../../framework';
+import { arbitraryWord } from '../../../../helpers';
+
 describe('construct-view-model', () => {
+  let framework: TestFramework;
+  let result: E.Either<'no-such-group', ViewModel>;
+
+  beforeEach(async () => {
+    framework = createTestFramework();
+  });
+
   describe('when the group can be found', () => {
     describe('and the user is an admin of this group', () => {
       it.todo('returns on the right');
@@ -10,6 +23,13 @@ describe('construct-view-model', () => {
   });
 
   describe('when the group can not be found', () => {
-    it.todo('returns on the left');
+    beforeEach(() => {
+      const groupSlug = arbitraryWord();
+      result = constructViewModel(framework.dependenciesForViews)(groupSlug);
+    });
+
+    it('returns on the left', () => {
+      expect(E.isLeft(result)).toBe(true);
+    });
   });
 });
