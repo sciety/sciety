ed18179d2451ea79586a364414adaa5c536374d6
LinaKind <l.kinduryte@elifesciences.org>
2024-05-23 11:15:51 +0100

    Intruduce a seam to allow us to write a complicated algorithm #3263

--- src/read-side/html-pages/group-page/group-add-a-featured-list-form-page/construct-view-model.ts
+++ src/read-side/html-pages/group-page/group-add-a-featured-list-form-page/construct-view-model.ts
@@ -5,7 +5,12 @@ import { pipe } from 'fp-ts/function';
 import { Dependencies } from './dependencies';
 import { Params } from './params';
 import { ViewModel } from './view-model';
+import { Group } from '../../../../types/group';
+import { UserId } from '../../../../types/user-id';
 import { constructGroupPageHref } from '../../../paths';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const checkUserIsAdminOfGroup = (userId: UserId, group: Group) => E.right(group);
 
 export const constructViewModel = (dependencies: Dependencies) => (params: Params): E.Either<'no-such-group', ViewModel> => pipe(
   {
@@ -14,7 +19,8 @@ export const constructViewModel = (dependencies: Dependencies) => (params: Param
   },
   sequenceS(O.Apply),
   E.fromOption(() => 'no-such-group' as const),
-  E.map(({ group }) => ({
+  E.chainW(({ group, user }) => checkUserIsAdminOfGroup(user.id, group)),
+  E.map((group) => ({
     pageHeading: `Add a featured list for ${group.name}`,
     groupId: group.id,
     successRedirectPath: constructGroupPageHref(group),
