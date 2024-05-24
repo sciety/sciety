7a43b5ab38db303636c8daedeb9eb4d1d151aa6c
Giorgio Sironi <g.sironi@elifesciences.org>
2024-05-24 14:55:51 +0100

    Write a characterization test #3269

--- test/read-models/group-authorisations/is-user-admin-of-group.test.ts
+++ test/read-models/group-authorisations/is-user-admin-of-group.test.ts
@@ -1,6 +1,23 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { handleEvent, initialState } from '../../../src/read-models/group-authorisations/handle-event';
+import { isUserAdminOfGroup } from '../../../src/read-models/group-authorisations/is-user-admin-of-group';
+import { arbitraryGroupId } from '../../types/group-id.helper';
+import { arbitraryUserId } from '../../types/user-id.helper';
+
 describe('is-user-admin-of-group', () => {
   describe('when no user has been assigned as an admin of a group', () => {
-    it.todo('returns false');
+    const userId = arbitraryUserId();
+    const groupId = arbitraryGroupId();
+    const readModel = pipe(
+      [],
+      RA.reduce(initialState(), handleEvent),
+    );
+    const result = isUserAdminOfGroup(readModel)(userId, groupId);
+
+    it('returns false', () => {
+      expect(result).toBe(false);
+    });
   });
 
   describe('when a user has been assigned as an admin of a group', () => {
