ac80b4c12002816a122ba7b0c7150d3a45247146
David Moulton <d.moulton@elifesciences.org>
2024-05-24 14:47:36 +0100

    Write a test list for a query we want to remove hard coding from #3269

---
+++ test/read-models/group-authorisations/is-user-admin-of-group.test.ts
@@ -0,0 +1,15 @@
+describe('is-user-admin-of-group', () => {
+  describe('when no user has been assigned as an admin of a group', () => {
+    it.todo('returns false');
+  });
+
+  describe('when a user has been assigned as an admin of a group', () => {
+    it.todo('returns true');
+  });
+
+  describe('when multiple users have been assigned as admins of a group', () => {
+    it.todo('returns true for all those users');
+
+    it.todo('returns false for other users');
+  });
+});
