79ae947b0d424a86a0162be3fdc5f3c56f0a9604
LinaKind <l.kinduryte@elifesciences.org>
2024-05-27 11:01:42 +0100

    Extract a named resource action to better align with the write side #3274

--- src/write-side/command-handlers/unfollow-command-handler.ts
+++ src/write-side/command-handlers/unfollow-command-handler.ts
@@ -1,31 +1,34 @@
+import * as E from 'fp-ts/Either';
 import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
 import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
 import { constructEvent } from '../../domain-events';
 import { isFollowing } from '../../http/form-submission-handlers/is-following';
 import { CommandHandler } from '../../types/command-handler';
-import { GroupId } from '../../types/group-id';
-import { UserId } from '../../types/user-id';
+import { UnfollowCommand } from '../commands/unfollow';
 import { DependenciesForCommands } from '../dependencies-for-commands';
-
-type UnfollowCommand = {
-  userId: UserId,
-  groupId: GroupId,
-};
+import { ResourceAction } from '../resources/resource-action';
 
 type UnfollowCommandHandler = CommandHandler<UnfollowCommand>;
+
+const unfollow: ResourceAction<UnfollowCommand> = (command) => (events) => pipe(
+  events,
+  isFollowing(command.userId, command.groupId),
+  B.fold(
+    () => [constructEvent('UserUnfollowedEditorialCommunity')({
+      userId: command.userId,
+      editorialCommunityId: command.groupId,
+    })],
+    () => [],
+  ),
+  E.right,
+);
 
 export const unfollowCommandHandler = (
   dependencies: DependenciesForCommands,
 ): UnfollowCommandHandler => (command) => pipe(
   dependencies.getAllEvents,
-  T.map(isFollowing(command.userId, command.groupId)),
-  T.map(B.fold(
-    () => [],
-    () => [constructEvent('UserUnfollowedEditorialCommunity')({
-      userId: command.userId,
-      editorialCommunityId: command.groupId,
-    })],
-  )),
-  T.chain(dependencies.commitEvents),
+  T.map(unfollow(command)),
+  TE.chain(dependencies.commitEvents),
 );
