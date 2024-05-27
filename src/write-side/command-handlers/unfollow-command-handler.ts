cce8746746bb287ab2c87783a712b49fb66c779d
LinaKind <l.kinduryte@elifesciences.org>
2024-05-27 11:48:21 +0100

    Revert "Extract a named resource action to better align with the write side #3274"
    This reverts commit 79ae947b0d424a86a0162be3fdc5f3c56f0a9604.

--- src/write-side/command-handlers/unfollow-command-handler.ts
+++ src/write-side/command-handlers/unfollow-command-handler.ts
@@ -1,34 +1,31 @@
-import * as E from 'fp-ts/Either';
 import * as T from 'fp-ts/Task';
-import * as TE from 'fp-ts/TaskEither';
 import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
 import { constructEvent } from '../../domain-events';
 import { isFollowing } from '../../http/form-submission-handlers/is-following';
 import { CommandHandler } from '../../types/command-handler';
-import { UnfollowCommand } from '../commands/unfollow';
+import { GroupId } from '../../types/group-id';
+import { UserId } from '../../types/user-id';
 import { DependenciesForCommands } from '../dependencies-for-commands';
-import { ResourceAction } from '../resources/resource-action';
+
+type UnfollowCommand = {
+  userId: UserId,
+  groupId: GroupId,
+};
 
 type UnfollowCommandHandler = CommandHandler<UnfollowCommand>;
-
-const unfollow: ResourceAction<UnfollowCommand> = (command) => (events) => pipe(
-  events,
-  isFollowing(command.userId, command.groupId),
-  B.fold(
-    () => [constructEvent('UserUnfollowedEditorialCommunity')({
-      userId: command.userId,
-      editorialCommunityId: command.groupId,
-    })],
-    () => [],
-  ),
-  E.right,
-);
 
 export const unfollowCommandHandler = (
   dependencies: DependenciesForCommands,
 ): UnfollowCommandHandler => (command) => pipe(
   dependencies.getAllEvents,
-  T.map(unfollow(command)),
-  TE.chain(dependencies.commitEvents),
+  T.map(isFollowing(command.userId, command.groupId)),
+  T.map(B.fold(
+    () => [],
+    () => [constructEvent('UserUnfollowedEditorialCommunity')({
+      userId: command.userId,
+      editorialCommunityId: command.groupId,
+    })],
+  )),
+  T.chain(dependencies.commitEvents),
 );
--- src/write-side/commands/unfollow.ts
+++
@@ -1,7 +0,0 @@
-import { GroupId } from '../../types/group-id';
-import { UserId } from '../../types/user-id';
-
-export type UnfollowCommand = {
-  userId: UserId,
-  groupId: GroupId,
-};
