a084f31b239b2e406128c35196ce71fc985b708e
David Moulton <d.moulton@elifesciences.org>
2024-05-28 15:17:32 +0100

    Make createPageFromParams safer to use with a stricter type #3278

--- src/read-side/html-pages/create-page-from-params.ts
+++ src/read-side/html-pages/create-page-from-params.ts
@@ -2,7 +2,7 @@ import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
-import { HtmlPage } from './html-page';
+import { ConstructPage } from './construct-page';
 import * as DE from '../../types/data-error';
 import { ErrorPageBodyViewModel, toErrorPageBodyViewModel } from '../../types/error-page-body-view-model';
 import { toHtmlFragment } from '../../types/html-fragment';
@@ -17,15 +17,15 @@ export const toUnavailable = (): ErrorPageBodyViewModel => toErrorPageBodyViewMo
   message: toHtmlFragment('Sorry, something went wrong. Please try again later.'),
 });
 
-type GeneratePage<P> = (params: P) => TE.TaskEither<ErrorPageBodyViewModel, HtmlPage>;
+type ConstructPageFromDecodedParams<P> = (params: P) => ReturnType<ConstructPage>;
 
 export const createPageFromParams = <P>(
   codec: t.Decoder<unknown, P>,
-  generatePage: GeneratePage<P>,
-) => (input: unknown): ReturnType<GeneratePage<P>> => pipe(
+  constructPageFromDecodedParams: ConstructPageFromDecodedParams<P>,
+): ConstructPage => (input) => pipe(
     input,
     codec.decode,
     E.mapLeft(toNotFound),
     TE.fromEither,
-    TE.chain(generatePage),
+    TE.chain(constructPageFromDecodedParams),
   );
