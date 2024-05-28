0a39217828396098abedbbd4e1fcbaa2f1f57014
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-05-28 12:00:54 +0100

    Sketch a test list for the new resource action #3269

---
+++ test/write-side/resources/group-authorisation/create.test.ts
@@ -0,0 +1,9 @@
+describe('create', () => {
+  describe('given an assignment that does not already exist', () => {
+    it.todo('assigns the user as admin of the group');
+  });
+
+  describe('given an assignment that already exists', () => {
+    it.todo('succeeds and does nothing');
+  });
+});
