d6448ec4a1841e44d2aa991955b0ef9fe7e1aa3c
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-28 14:27:06 +0100

    Write a failing test #3269

--- test/write-side/resources/group-authorisation/create.test.ts
+++ test/write-side/resources/group-authorisation/create.test.ts
@@ -1,6 +1,26 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import { AssignUserAsGroupAdminCommand } from '../../../../src/write-side/commands';
+import { create } from '../../../../src/write-side/resources/group-authorisation/create';
+import { shouldNotBeCalled } from '../../../should-not-be-called';
+import { arbitraryGroupId } from '../../../types/group-id.helper';
+import { arbitraryUserId } from '../../../types/user-id.helper';
+
 describe('create', () => {
   describe('given an assignment that does not already exist', () => {
-    it.todo('assigns the user as admin of the group');
+    const command: AssignUserAsGroupAdminCommand = {
+      groupId: arbitraryGroupId(),
+      userId: arbitraryUserId(),
+    };
+    const result = pipe(
+      [],
+      create(command),
+      E.getOrElseW(shouldNotBeCalled),
+    );
+
+    it.failing('assigns the user as admin of the group', () => {
+      expect(result).toHaveLength(1);
+    });
   });
 
   describe('given an assignment that already exists', () => {
