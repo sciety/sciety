fee4bc531a4f5dd779a1ccd87518145de5b42733
LinaKind <l.kinduryte@elifesciences.org>
2024-05-28 15:18:07 +0100

    Stop relying on the hardcoded data in the tests #3269

--- test/read-side/html-pages/group-page/group-add-a-featured-list-form-page/construct-view-model.test.ts
+++ test/read-side/html-pages/group-page/group-add-a-featured-list-form-page/construct-view-model.test.ts
@@ -1,13 +1,10 @@
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import { constructViewModel } from '../../../../../src/read-side/html-pages/group-page/group-add-a-featured-list-form-page/construct-view-model';
-import { GroupId } from '../../../../../src/types/group-id';
-import { UserId } from '../../../../../src/types/user-id';
 import { TestFramework, createTestFramework } from '../../../../framework';
 import { arbitraryWord } from '../../../../helpers';
 import { arbitraryUserId } from '../../../../types/user-id.helper';
 import { arbitraryAddGroupCommand } from '../../../../write-side/commands/add-group-command.helper';
-import { arbitraryAssignUserAsGroupAdminCommand } from '../../../../write-side/commands/assign-user-as-group-admin-command.helper';
 
 describe('construct-view-model', () => {
   let framework: TestFramework;
@@ -26,30 +23,23 @@ describe('construct-view-model', () => {
     });
 
     describe('and the user is an admin of this group', () => {
-      const hardcodedUserId = 'auth0|650a91161c07d3acf5ff7da5' as UserId;
-      const addHardcodedGroupCommand = {
-        ...arbitraryAddGroupCommand(),
-        groupId: 'd6e1a913-76f8-40dc-9074-8eac033e1bc8' as GroupId,
-      };
-      const hardcodedGroupSlug = addHardcodedGroupCommand.slug;
+      const userId = arbitraryUserId();
 
       beforeEach(async () => {
-        await framework.commandHelpers.addGroup(addHardcodedGroupCommand);
         const assignUserAsGroupAdminCommand = {
-          ...arbitraryAssignUserAsGroupAdminCommand(),
-          userId: hardcodedUserId,
+          userId,
           groupId: addGroupCommand.groupId,
         };
         await framework.commandHelpers.assignUserAsGroupAdmin(assignUserAsGroupAdminCommand);
         result = pipe(
           {
-            slug: hardcodedGroupSlug,
+            slug: addGroupCommand.slug,
           },
-          constructViewModel(framework.dependenciesForViews, hardcodedUserId),
+          constructViewModel(framework.dependenciesForViews, userId),
         );
       });
 
-      it('returns on the right', () => {
+      it('returns on the right', async () => {
         expect(E.isRight(result)).toBe(true);
       });
     });
--- test/write-side/commands/assign-user-as-group-admin-command.helper.ts
+++
@@ -1,8 +0,0 @@
-import { AssignUserAsGroupAdminCommand } from '../../../src/write-side/commands';
-import { arbitraryGroupId } from '../../types/group-id.helper';
-import { arbitraryUserId } from '../../types/user-id.helper';
-
-export const arbitraryAssignUserAsGroupAdminCommand = (): AssignUserAsGroupAdminCommand => ({
-  userId: arbitraryUserId(),
-  groupId: arbitraryGroupId(),
-});
