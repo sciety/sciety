af76fa7d762d4c2c4ae2416509b7a5784c62dc3b
LinaKind <l.kinduryte@elifesciences.org>
2024-05-29 09:45:37 +0100

    Document a mikado step needed for a deprecation removal #3278

--- src/http/require-logged-in-user.ts
+++ src/http/require-logged-in-user.ts
@@ -4,8 +4,8 @@ import { Middleware } from 'koa';
 import { getLoggedInScietyUser, Dependencies as GetLoggedInScietyUserDependencies } from './authentication-and-logging-in-of-sciety-users';
 
 /**
- * @deprecated replace with pageHandlerWithLoggedInUser for GET requests
- * (and remember to change to handler type to ConstructLoggedInPage)
+ * @deprecated replace with pageHandlerWithLoggedInUser for GET requests & change handler type to ConstructLoggedInPage.
+ * As a mikado step, the codec of the page needs to be hidden inside the page
  */
 export const requireLoggedInUser = (
   dependencies: GetLoggedInScietyUserDependencies,
