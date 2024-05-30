f3526f13817607eca61025e52a3a3474d01ebf31
David Moulton <d.moulton@elifesciences.org>
2024-05-30 14:38:45 +0100

    Duplicate and adapt preexisting tests to work for the alternative groups page #3286

---
+++ test/read-side/html-pages/groups-page-alt/construct-view-model/construct-view-model.test.ts
@@ -0,0 +1,57 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { constructViewModel } from '../../../../../src/read-side/html-pages/groups-page-alt/construct-view-model/construct-view-model';
+import { GroupCardViewModel } from '../../../../../src/read-side/html-pages/shared-components/group-card/view-model';
+import { TestFramework, createTestFramework } from '../../../../framework';
+import { shouldNotBeCalled } from '../../../../should-not-be-called';
+import { arbitraryAddGroupCommand } from '../../../../write-side/commands/add-group-command.helper';
+import { arbitraryRecordEvaluationPublicationCommand } from '../../../../write-side/commands/record-evaluation-publication-command.helper';
+
+type ViewModel = ReadonlyArray<GroupCardViewModel>;
+
+describe('construct-view-model', () => {
+  let framework: TestFramework;
+  let groupNames: ReadonlyArray<ViewModel[number]['name']>;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
+  describe('when there is more than one group', () => {
+    const addMostRecentlyActiveGroup = arbitraryAddGroupCommand();
+    const addLeastRecentlyActiveGroup = arbitraryAddGroupCommand();
+
+    beforeEach(async () => {
+      await framework.commandHelpers.addGroup(addMostRecentlyActiveGroup);
+      await framework.commandHelpers.recordEvaluationPublication({
+        ...arbitraryRecordEvaluationPublicationCommand(),
+        groupId: addMostRecentlyActiveGroup.groupId,
+        publishedAt: new Date('2020'),
+      });
+      await framework.commandHelpers.addGroup(addLeastRecentlyActiveGroup);
+      await framework.commandHelpers.recordEvaluationPublication({
+        ...arbitraryRecordEvaluationPublicationCommand(),
+        groupId: addLeastRecentlyActiveGroup.groupId,
+        publishedAt: new Date('1970'),
+      });
+      groupNames = await pipe(
+        framework.queries,
+        constructViewModel,
+        TE.getOrElse(shouldNotBeCalled),
+        T.map((viewModel) => pipe(
+          viewModel.groupCards,
+          RA.map((groupCard) => groupCard.name),
+        )),
+      )();
+    });
+
+    it('the group cards are listed in descending order of latest activity', () => {
+      expect(groupNames).toStrictEqual([
+        addMostRecentlyActiveGroup.name,
+        addLeastRecentlyActiveGroup.name,
+      ]);
+    });
+  });
+});
