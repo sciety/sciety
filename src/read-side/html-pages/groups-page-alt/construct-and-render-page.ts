261d0d8dc199ddce0f0b47a5aea3128ff62d89b4
David Moulton <d.moulton@elifesciences.org>
2024-05-30 13:30:44 +0100

    Explicitly couple the page heading and the HTML title element #3286

--- src/read-side/html-pages/groups-page-alt/construct-and-render-page.ts
+++ src/read-side/html-pages/groups-page-alt/construct-and-render-page.ts
@@ -3,7 +3,6 @@ import { pipe } from 'fp-ts/function';
 import { constructViewModel } from './construct-view-model/construct-view-model';
 import { Dependencies } from './construct-view-model/dependencies';
 import { renderAsHtml } from './render-as-html';
-import { renderPage } from './render-page';
 import { ErrorPageBodyViewModel } from '../../../types/error-page-body-view-model';
 import { toUnavailable } from '../create-page-from-params';
 import { HtmlPage } from '../html-page';
@@ -12,6 +11,5 @@ type GroupsPage = TE.TaskEither<ErrorPageBodyViewModel, HtmlPage>;
 
 export const constructAndRenderPage = (dependencies: Dependencies): GroupsPage => pipe(
   constructViewModel(dependencies),
-  TE.map(renderPage),
   TE.bimap(toUnavailable, renderAsHtml),
 );
--- src/read-side/html-pages/groups-page-alt/render-as-html.ts
+++ src/read-side/html-pages/groups-page-alt/render-as-html.ts
@@ -1,9 +1,10 @@
-import { HtmlFragment } from '../../../types/html-fragment';
+import { ViewModel } from './construct-view-model/construct-view-model';
+import { renderPage } from './render-page';
 import { HtmlPage, NotHtml, toHtmlPage } from '../html-page';
 
-export const renderAsHtml = (content: HtmlFragment): HtmlPage => toHtmlPage({
-  title: 'Groups',
-  content,
+export const renderAsHtml = (viewModel: ViewModel): HtmlPage => toHtmlPage({
+  title: viewModel.title,
+  content: renderPage(viewModel),
   openGraph: {
     title: 'Sciety Groups',
     description: 'Content creators helping you decide which preprints to read and trust.' as NotHtml,
