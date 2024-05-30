f207299de43d5730dfdc94caea3ac0694dc55dd7
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-30 10:27:08 +0100

    Prepare to provide a third type of DataError #3283

--- src/types/data-error.ts
+++ src/types/data-error.ts
@@ -2,12 +2,15 @@ import { Lazy } from 'fp-ts/function';
 
 export const notFound = 'not-found' as const;
 export const unavailable = 'unavailable' as const;
+export const notAuthorised = 'not-authorised' as const;
 
-export type DataError = typeof notFound | typeof unavailable;
+export type DataError = typeof notFound | typeof unavailable | typeof notAuthorised;
 
 export const isNotFound = (de: DataError): boolean => de === notFound;
 
 export const isUnavailable = (de: DataError): boolean => de === unavailable;
+
+export const isNotAuthorised = (de: DataError): boolean => de === notAuthorised;
 
 export const match = <B>(opts: Record<'notFound' | 'unavailable', Lazy<B>>) => (e: DataError): B => (
   isNotFound(e) ? opts.notFound() : opts.unavailable()
