aa4f46810f74d0317e6816a3eeaeabde4d1d5ead
LinaKind <l.kinduryte@elifesciences.org>
2024-05-31 11:36:18 +0100

    Write a test list for the new resource action #3288

---
+++ test/write-side/resources/list-promotion/remove.test.ts
@@ -0,0 +1,9 @@
+describe('remove', () => {
+  describe('when the list is promoted', () => {
+    it.todo('removes the promotion');
+  });
+
+  describe('when the list is not promoted', () => {
+    it.todo('succeeds, doing nothing');
+  });
+});
