05c3f02457f262a57ab2910c3c8556d7a3392cb4
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-05-31 14:32:32 +0100

    Extract in preparation for re-use #3288

--- src/write-side/resources/list-promotion/create.ts
+++ src/write-side/resources/list-promotion/create.ts
@@ -2,24 +2,34 @@ import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import { constructEvent, isEventOfType, filterByName } from '../../../domain-events';
+import {
+  constructEvent, isEventOfType, filterByName, DomainEvent,
+} from '../../../domain-events';
 import { PromoteListCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
 
-export const create: ResourceAction<PromoteListCommand> = (command) => (events) => pipe(
+const promoteList = (command: PromoteListCommand) => [constructEvent('ListPromotionCreated')({
+  byGroup: command.forGroup,
+  listId: command.listId,
+})];
+
+const getLastEventRelevantToThisListPromotion = (
+  command: PromoteListCommand,
+) => (
+  events: ReadonlyArray<DomainEvent>,
+) => pipe(
   events,
   filterByName(['ListPromotionRemoved', 'ListPromotionCreated']),
   RA.filter((event) => event.byGroup === command.forGroup && event.listId === command.listId),
   RA.last,
+);
+
+export const create: ResourceAction<PromoteListCommand> = (command) => (events) => pipe(
+  events,
+  getLastEventRelevantToThisListPromotion(command),
   O.match(
-    () => [constructEvent('ListPromotionCreated')({
-      byGroup: command.forGroup,
-      listId: command.listId,
-    })],
-    (event) => (isEventOfType('ListPromotionRemoved')(event) ? [constructEvent('ListPromotionCreated')({
-      byGroup: command.forGroup,
-      listId: command.listId,
-    })] : []),
+    () => promoteList(command),
+    (event) => (isEventOfType('ListPromotionRemoved')(event) ? promoteList(command) : []),
   ),
   E.right,
 );
