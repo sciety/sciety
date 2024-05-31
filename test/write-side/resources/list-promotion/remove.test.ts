b7f4de9a8143b2deca0c28fd9922862a12598567
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-05-31 13:22:46 +0100

    Provide more test cases to provoke better implementation #3288

--- test/write-side/resources/list-promotion/remove.test.ts
+++ test/write-side/resources/list-promotion/remove.test.ts
@@ -2,19 +2,23 @@ import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import { DomainEvent, constructEvent } from '../../../../src/domain-events';
 import { remove } from '../../../../src/write-side/resources/list-promotion/remove';
+import { arbitraryListPromotionRemovedEvent } from '../../../domain-events/list-promotion-resource-events.helper';
 import { shouldNotBeCalled } from '../../../should-not-be-called';
 import { arbitraryRemoveListPromotionCommand } from '../../commands/remove-list-promotion-command.helper';
 
 describe('remove', () => {
   const command = arbitraryRemoveListPromotionCommand();
   let result: ReadonlyArray<DomainEvent>;
+  const listPromotionCreated = constructEvent('ListPromotionCreated')({ byGroup: command.forGroup, listId: command.listId });
+  const listPromotionRemoved = constructEvent('ListPromotionRemoved')({ byGroup: command.forGroup, listId: command.listId });
 
-  describe('when the list is promoted', () => {
+  describe.each([
+    [[listPromotionCreated]],
+    // [[listPromotionCreated, arbitraryListPromotionRemovedEvent()]],
+  ])('when the list is promoted', (events) => {
     beforeEach(() => {
       result = pipe(
-        [
-          constructEvent('ListPromotionCreated')({ byGroup: command.forGroup, listId: command.listId }),
-        ],
+        events,
         remove(command),
         E.getOrElseW(shouldNotBeCalled),
       );
@@ -29,13 +33,15 @@ describe('remove', () => {
     });
   });
 
-  describe('when the list is not promoted', () => {
+  describe.each([
+    // [[]],
+    [[listPromotionCreated, listPromotionRemoved]],
+    // [[arbitraryListPromotionCreatedEvent()]],
+    [[listPromotionCreated, listPromotionRemoved, arbitraryListPromotionRemovedEvent()]],
+  ])('when the list is not promoted', (events: ReadonlyArray<DomainEvent>) => {
     beforeEach(() => {
       result = pipe(
-        [
-          constructEvent('ListPromotionCreated')({ byGroup: command.forGroup, listId: command.listId }),
-          constructEvent('ListPromotionRemoved')({ byGroup: command.forGroup, listId: command.listId }),
-        ],
+        events,
         remove(command),
         E.getOrElseW(shouldNotBeCalled),
       );
