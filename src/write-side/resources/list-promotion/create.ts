d22eab1e555f04e6484f63b84bf327cb64896eb7
LinaKind <l.kinduryte@elifesciences.org>
2024-05-31 15:45:12 +0100

    Re-use shared behaviour #3288

--- src/write-side/resources/list-promotion/create.ts
+++ src/write-side/resources/list-promotion/create.ts
@@ -1,9 +1,9 @@
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
-import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
+import { getLastEventRelevantToThisListPromotion } from './get-last-event-relevant-to-this-list-promotion';
 import {
-  constructEvent, isEventOfType, filterByName, DomainEvent,
+  constructEvent, isEventOfType,
 } from '../../../domain-events';
 import { PromoteListCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
@@ -12,17 +12,6 @@ const promoteList = (command: PromoteListCommand) => [constructEvent('ListPromot
   byGroup: command.forGroup,
   listId: command.listId,
 })];
-
-const getLastEventRelevantToThisListPromotion = (
-  command: PromoteListCommand,
-) => (
-  events: ReadonlyArray<DomainEvent>,
-) => pipe(
-  events,
-  filterByName(['ListPromotionRemoved', 'ListPromotionCreated']),
-  RA.filter((event) => event.byGroup === command.forGroup && event.listId === command.listId),
-  RA.last,
-);
 
 export const create: ResourceAction<PromoteListCommand> = (command) => (events) => pipe(
   events,
--- src/write-side/resources/list-promotion/remove.ts
+++ src/write-side/resources/list-promotion/remove.ts
@@ -1,16 +1,14 @@
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
-import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import { constructEvent, filterByName, isEventOfType } from '../../../domain-events';
+import { getLastEventRelevantToThisListPromotion } from './get-last-event-relevant-to-this-list-promotion';
+import { constructEvent, isEventOfType } from '../../../domain-events';
 import { RemoveListPromotionCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
 
 export const remove: ResourceAction<RemoveListPromotionCommand> = (command) => (events) => pipe(
   events,
-  filterByName(['ListPromotionRemoved', 'ListPromotionCreated']),
-  RA.filter((event) => event.byGroup === command.forGroup && event.listId === command.listId),
-  RA.last,
+  getLastEventRelevantToThisListPromotion(command),
   O.match(
     () => [],
     (event) => (isEventOfType('ListPromotionCreated')(event) ? [constructEvent('ListPromotionRemoved')({
