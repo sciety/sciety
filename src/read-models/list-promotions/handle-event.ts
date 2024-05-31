cb9ed485b418d583cf2800353b455616a994596c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-05-31 10:55:44 +0100

    Revert "Provide a type for the read model #3288"
    This reverts commit 7a9322d94b3963ef639c05d550df0120a27a02fc.

--- src/read-models/list-promotions/handle-event.ts
+++ src/read-models/list-promotions/handle-event.ts
@@ -1,11 +1,10 @@
 /* eslint-disable no-param-reassign */
 import { DomainEvent } from '../../domain-events';
-import { GroupId } from '../../types/group-id';
-import { ListId } from '../../types/list-id';
 
-export type ReadModel = Map<GroupId, Array<ListId>>;
+export type ReadModel = object;
 
-export const initialState = (): ReadModel => new Map<GroupId, Array<ListId>>();
+export const initialState = (): ReadModel => ({
+});
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel => readmodel;
