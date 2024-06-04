72e49d10b4f0f6c6bf829dcbc952088f280aabe2
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-04 13:01:16 +0100

    Use a conventional approach for checking the logged-in user #3297

--- src/http/form-submission-handlers/create-list-handler.ts
+++ src/http/form-submission-handlers/create-list-handler.ts
@@ -1,33 +1,33 @@
-import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { Middleware } from 'koa';
+import { ensureUserIsLoggedIn, Dependencies as EnsureUserIsLoggedInDependencies } from './ensure-user-is-logged-in';
 import { CreateList, Logger } from '../../shared-ports';
 import * as LID from '../../types/list-id';
 import * as LOID from '../../types/list-owner-id';
 import { CreateListCommand } from '../../write-side/commands';
-import { getAuthenticatedUserIdFromContext, Dependencies as GetLoggedInScietyUserDependencies } from '../authentication-and-logging-in-of-sciety-users';
+import { Dependencies as GetLoggedInScietyUserDependencies } from '../authentication-and-logging-in-of-sciety-users';
 
-type Dependencies = GetLoggedInScietyUserDependencies & {
+type Dependencies = GetLoggedInScietyUserDependencies & EnsureUserIsLoggedInDependencies & {
   logger: Logger,
   createList: CreateList,
 };
 
 export const createListHandler = (dependencies: Dependencies): Middleware => async (context) => {
+  const loggedInUserId = ensureUserIsLoggedIn(dependencies, context, 'You must be logged in to feature a list.');
+  if (O.isNone(loggedInUserId)) {
+    return;
+  }
+
   await pipe(
-    getAuthenticatedUserIdFromContext(context),
-    E.fromOption(() => ({
-      message: 'No authenticated user',
-      payload: { formBody: context.request.body },
-    })),
-    TE.fromEither,
-    TE.map((userId): CreateListCommand => ({
+    {
       listId: LID.generate(),
-      ownerId: LOID.fromUserId(userId),
+      ownerId: LOID.fromUserId(loggedInUserId.value),
       name: 'Untitled',
       description: '',
-    })),
-    TE.chainW((command) => pipe(
+    } satisfies CreateListCommand,
+    (command) => pipe(
       command,
       dependencies.createList,
       TE.bimap(
@@ -39,7 +39,7 @@ export const createListHandler = (dependencies: Dependencies): Middleware => asy
         }),
         () => command.listId,
       ),
-    )),
+    ),
     TE.match(
       (error) => {
         dependencies.logger('error', error.message, error.payload);
