453afcec8a2b790a2e6fab9e2c22e46baa306243
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-04 11:46:19 +0100

    Validate the command before attempting to execute the resource action #3297

--- src/http/form-submission-handlers/follow-handler.ts
+++ src/http/form-submission-handlers/follow-handler.ts
@@ -1,7 +1,5 @@
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
-import * as T from 'fp-ts/Task';
-import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { StatusCodes } from 'http-status-codes';
 import * as t from 'io-ts';
@@ -9,10 +7,9 @@ import { Middleware } from 'koa';
 import { decodeFormSubmission } from './decode-form-submission';
 import { Queries } from '../../read-models';
 import { Logger } from '../../shared-ports';
-import * as DE from '../../types/data-error';
-import * as GroupId from '../../types/group-id';
 import { GroupIdFromStringCodec } from '../../types/group-id';
 import { follow } from '../../write-side/command-handlers/follow-command-handler';
+import { FollowCommand } from '../../write-side/commands';
 import { DependenciesForCommands } from '../../write-side/dependencies-for-commands';
 import { executeResourceAction } from '../../write-side/resources/execute-resource-action';
 import { getAuthenticatedUserIdFromContext } from '../authentication-and-logging-in-of-sciety-users';
@@ -25,12 +22,8 @@ type Dependencies = DependenciesForCommands & SendErrorHtmlResponseDependencies 
   getGroup: Queries['getGroup'],
 };
 
-const validate = (dependencies: Dependencies) => (groupId: GroupId.GroupId) => pipe(
-  dependencies.getGroup(groupId),
-  E.fromOption(() => DE.notFound),
-  E.map((group) => ({
-    groupId: group.id,
-  })),
+const isValid = (dependencies: Dependencies, command: FollowCommand) => (
+  O.isSome(dependencies.getGroup(command.groupId))
 );
 
 const formBodyCodec = t.type({
@@ -53,26 +46,20 @@ export const followHandler = (dependencies: Dependencies): Middleware => async (
     return;
   }
 
+  const command = {
+    userId: loggedInUserId.value,
+    groupId: formBody.right[groupProperty],
+  };
+
+  if (!isValid(dependencies, command)) {
+    dependencies.logger('error', 'Problem with /follow', { error: StatusCodes.BAD_REQUEST });
+    sendDefaultErrorHtmlResponse(dependencies, context, StatusCodes.INTERNAL_SERVER_ERROR, 'Something went wrong; we\'re looking into it.');
+    return;
+  }
+
   await pipe(
-    formBody.right[groupProperty],
-    validate(dependencies),
-    TE.fromEither,
-    TE.fold(
-      () => {
-        dependencies.logger('error', 'Problem with /follow', { error: StatusCodes.BAD_REQUEST });
-        sendDefaultErrorHtmlResponse(dependencies, context, StatusCodes.INTERNAL_SERVER_ERROR, 'Something went wrong; we\'re looking into it.');
-        return T.of(undefined);
-      },
-      (params) => {
-        context.redirect('back');
-        return pipe(
-          {
-            userId: loggedInUserId.value,
-            groupId: params.groupId,
-          },
-          executeResourceAction(dependencies, follow),
-        );
-      },
-    ),
+    command,
+    executeResourceAction(dependencies, follow),
   )();
+  context.redirect('back');
 };
