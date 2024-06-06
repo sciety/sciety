6d8319e8a8585e8cb95a396a1f74bc327e48742e
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-06 11:29:24 +0100

    Remove dead code that is already monkey-patched in update-all #3281

--- src/ingest/fetch-data.ts
+++ src/ingest/fetch-data.ts
@@ -1,17 +1,8 @@
 import { performance } from 'perf_hooks';
 import axios from 'axios';
-import axiosRetry from 'axios-retry';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { Configuration } from './generate-configuration-from-environment';
-
-axiosRetry(axios, {
-  retries: 3,
-  onRetry: (retryCount: number, error, requestConfig) => {
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    process.stdout.write(`Retrying retryCount: ${retryCount}, error: ${error}, requestConfig: ${requestConfig}\n`);
-  },
-});
 
 const axiosGet = async <D>(ingestDebug: Configuration['ingestDebug'], url: string, additionalHeaders: Record<string, string>) => {
   const startTime = performance.now();
