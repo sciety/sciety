027dfd43a5cd4897987191737df3548514505fbe
Giorgio Sironi <g.sironi@elifesciences.org>
2024-06-07 08:59:57 +0100

    Run tests successfully #3308

---
+++ ingest/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  cacheDirectory: './.jest',
+  collectCoverageFrom: [
+    '**/*.ts',
+  ],
+  coverageDirectory: './coverage',
+  coverageReporters: ['json', 'lcov', 'html', 'text'],
+  reporters: ['jest-wip-reporter'],
+  roots: ['./src/', './test/'],
+  setupFilesAfterEnv: ['./test/jest.setup.ts'],
+  testEnvironment: 'node',
+  testRunner: 'jest-circus/runner',
+  transform: {
+    '^.+\\.ts$': 'esbuild-jest',
+  },
+  transformIgnorePatterns: ['\\.pnp\\.[^\\\/]+$'],
+  verbose: true,
+};
