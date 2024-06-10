ff50422bd44408f016af894abc7f6e63c8e788f0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-06-10 11:53:20 +0100

    Expand the renderer to be able to work on new functionality without breaking the existing one #3314

--- src/read-side/html-pages/group-page/group-management-page/render-feature-a-list.ts
+++ src/read-side/html-pages/group-page/group-management-page/render-feature-a-list.ts
@@ -8,22 +8,31 @@ import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 export const renderFeatureAList = (viewModel: ViewModel): HtmlFragment => pipe(
   [],
   RA.match(
+    () => '<p>No lists available for featuring.</p>',
     () => toHtmlFragment(`
       <form action="${pathToSubmitAddAFeaturedList()}" method="post" class="standard-form">
         <section>
             <input type="hidden" name="${inputFieldNames.forGroup}" value="${viewModel.groupId}">
             <input type="hidden" name="${inputFieldNames.successRedirectPath}" value="${viewModel.successRedirectPath}">
-            <label for="listId" class="standard-form__sub_heading">List Id</label>
-            <input type="text" id="listId" name="${inputFieldNames.listId}" class="standard-form__full_width_text_input" value="">
+            <p class="standard-form__sub_heading">List Name</p>
+            <input type="hidden" id="listId" name="${inputFieldNames.listId}" class="standard-form__full_width_text_input" value="">
           </section>
           <button type="submit">Save</button><a href="#" class="standard-form__cancel">Cancel</a>
       </form>
     `),
-    () => toHtmlFragment(''),
   ),
   (forms) => toHtmlFragment(`
     <h2>Feature a list</h2>
     <p class="standard-form__sub_heading_secondary_text">Choose a list to feature on your group page.</p>
     ${forms}
+    <form action="${pathToSubmitAddAFeaturedList()}" method="post" class="standard-form">
+      <section>
+          <input type="hidden" name="${inputFieldNames.forGroup}" value="${viewModel.groupId}">
+          <input type="hidden" name="${inputFieldNames.successRedirectPath}" value="${viewModel.successRedirectPath}">
+          <label for="listId" class="standard-form__sub_heading">List Id</label>
+          <input type="text" id="listId" name="${inputFieldNames.listId}" class="standard-form__full_width_text_input" value="">
+        </section>
+        <button type="submit">Save</button><a href="#" class="standard-form__cancel">Cancel</a>
+    </form>
   `),
 );
