f08b054cadc0b74dcb6077d2ffc486080ea0abaa
Giorgio Sironi <g.sironi@elifesciences.org>
2024-06-10 08:47:47 +0100

    Remove one dependency from ingest/test to domain-events #3308

--- ingest/test/jest.setup.ts
+++ ingest/test/jest.setup.ts
@@ -1,27 +1,2 @@
-import { DomainEvent } from '../src/domain-events';
-
 expect.extend({
-  toBeDomainEvent(actual: DomainEvent, expectedType: DomainEvent['type'], expectedProperties?: object) {
-    if (actual.type !== expectedType) {
-      return {
-        pass: false,
-        message: () => `Expected event to have type ${expectedType} but was ${actual.type}`,
-      };
-    }
-    if (expectedProperties) {
-      if (this.isNot) {
-        expect(actual).not.toStrictEqual(expect.objectContaining(expectedProperties));
-      } else {
-        expect(actual).toStrictEqual(expect.objectContaining(expectedProperties));
-      }
-      return {
-        pass: !this.isNot,
-        message: () => 'Expected event to have the relevant properties',
-      };
-    }
-    return {
-      pass: true,
-      message: () => 'Expected event not to have the relevant properties',
-    };
-  },
 });
