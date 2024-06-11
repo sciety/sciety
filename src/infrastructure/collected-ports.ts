fe8a39d8a29b1e362d1e0517ded8d0b778860f55
LinaKind <l.kinduryte@elifesciences.org>
2024-06-11 14:36:59 +0100

    Start removing the concept of shared ports #3320

--- src/infrastructure/collected-ports.ts
+++ src/infrastructure/collected-ports.ts
@@ -1,4 +1,17 @@
+import { Logger } from '../logger';
 import { Queries } from '../read-models';
-import { SharedPorts } from '../shared-ports';
+import { AddArticleToList } from '../shared-ports/add-article-to-list';
+import { CommitEvents } from '../shared-ports/commit-events';
+import { CreateList } from '../shared-ports/create-list';
+import { GetAllEvents } from '../shared-ports/get-all-events';
+import { RecordSubjectArea } from '../shared-ports/record-subject-area';
+import { ExternalQueries } from '../third-parties';
 
-export type CollectedPorts = SharedPorts & Queries;
+export type CollectedPorts = Queries & ExternalQueries & {
+  addArticleToList: AddArticleToList,
+  commitEvents: CommitEvents,
+  createList: CreateList,
+  getAllEvents: GetAllEvents,
+  logger: Logger,
+  recordSubjectArea: RecordSubjectArea,
+};
--- src/shared-ports/index.ts
+++ src/shared-ports/index.ts
@@ -1,20 +1,3 @@
-import { AddArticleToList } from './add-article-to-list';
-import { CommitEvents } from './commit-events';
-import { CreateList } from './create-list';
-import { GetAllEvents } from './get-all-events';
-import { RecordSubjectArea } from './record-subject-area';
-import { Logger } from '../logger';
-import { ExternalQueries } from '../third-parties';
-
 export { AddArticleToList } from './add-article-to-list';
 export { CreateList } from './create-list';
 export { RecordSubjectArea } from './record-subject-area';
-
-export type SharedPorts = ExternalQueries & {
-  addArticleToList: AddArticleToList,
-  commitEvents: CommitEvents,
-  createList: CreateList,
-  getAllEvents: GetAllEvents,
-  logger: Logger,
-  recordSubjectArea: RecordSubjectArea,
-};
