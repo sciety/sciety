8144d055496037fe119d3a01b15b11b07cd4136c
LinaKind <l.kinduryte@elifesciences.org>
2024-06-12 10:37:43 +0100

    Express collected ports with minimal duplication #3320

--- src/infrastructure/collected-ports.ts
+++ src/infrastructure/collected-ports.ts
@@ -1,8 +1,6 @@
-import { EventStore } from '../event-store';
-import { Logger } from '../logger';
-import { Queries } from '../read-models';
-import { ExternalQueries } from '../third-parties';
+import { DependenciesForViews } from '../read-side/dependencies-for-views';
 import { CommandHandler } from '../types/command-handler';
+import { DependenciesForCommands } from '../write-side';
 import { AddArticleToListCommand, CreateListCommand, RecordSubjectAreaCommand } from '../write-side/commands';
 
 /**
@@ -14,10 +12,5 @@ type CommandHandlersForSagas = {
   recordSubjectArea: CommandHandler<RecordSubjectAreaCommand>,
 };
 
-export type CollectedPorts = Queries
-& ExternalQueries
-& EventStore
-& CommandHandlersForSagas
-& {
-  logger: Logger,
-};
+export type CollectedPorts = DependenciesForViews & DependenciesForCommands
+& CommandHandlersForSagas;
