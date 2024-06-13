dabbccc8cfa5c696a8301e6141daf293c51c9dec
David Moulton <d.moulton@elifesciences.org>
2024-06-13 11:33:03 +0100

    Create a seam to implement additional logic #3314

--- src/read-side/html-pages/group-page/group-management-page/construct-view-model/construct-lists-that-can-be-featured.ts
+++ src/read-side/html-pages/group-page/group-management-page/construct-view-model/construct-lists-that-can-be-featured.ts
@@ -1,11 +1,14 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { Dependencies } from './dependencies';
+import { List } from '../../../../../read-models/lists';
 import { Group } from '../../../../../types/group';
 import * as LOID from '../../../../../types/list-owner-id';
 import { UserId } from '../../../../../types/user-id';
 import { constructGroupPagePath } from '../../../../paths';
 import { ListsThatCanBeFeatured } from '../view-model';
+
+const excludeListsAlreadyFeaturedByTheGroup = (lists: ReadonlyArray<List>) => lists;
 
 export const constructListsThatCanBeFeatured = (
   dependencies: Dependencies,
@@ -15,6 +18,7 @@ export const constructListsThatCanBeFeatured = (
   userId,
   LOID.fromUserId,
   dependencies.selectAllListsOwnedBy,
+  excludeListsAlreadyFeaturedByTheGroup,
   RA.map((list) => ({
     listName: list.name,
     listId: list.id,
