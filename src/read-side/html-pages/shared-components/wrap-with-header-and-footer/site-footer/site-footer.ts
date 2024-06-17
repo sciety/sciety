352d8a79dc5579bc712b18e7438be90c84ca2e09
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-06-17 13:30:06 +0100

    Encapsulate site footer within another shared component #3325

--- src/read-side/html-pages/shared-components/wrap-with-header-and-footer/site-footer/site-footer.ts
+++ src/read-side/html-pages/shared-components/wrap-with-header-and-footer/site-footer/site-footer.ts
@@ -1,4 +1,4 @@
-import { toHtmlFragment } from '../../../../types/html-fragment';
+import { toHtmlFragment } from '../../../../../types/html-fragment';
 
 export const siteFooter = toHtmlFragment(`
   <footer>
--- src/read-side/html-pages/shared-components/wrap-with-header-and-footer/wrap-with-header-and-footer.ts
+++ src/read-side/html-pages/shared-components/wrap-with-header-and-footer/wrap-with-header-and-footer.ts
@@ -1,10 +1,10 @@
 import * as O from 'fp-ts/Option';
 import { mobileMenu } from './mobile-menu';
+import { siteFooter } from './site-footer';
 import { siteHeader } from './site-header';
 import { ColourSchemes } from './site-header/colour-schemes';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 import { UserDetails } from '../../../../types/user-details';
-import { siteFooter } from '../site-footer';
 
 export const wrapWithHeaderAndFooter = (pageContainerClass: string, user: O.Option<UserDetails>, scheme: ColourSchemes = 'light') => (main: HtmlFragment): HtmlFragment => toHtmlFragment(`
   <div class="${pageContainerClass}">
