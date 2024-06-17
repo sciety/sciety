f5da6da1503e9ed0c97fc179be51adf13dfe8ef8
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-06-17 09:41:19 +0100

    Align an error page to other pages in Sciety #3325

--- src/read-side/html-pages/render-oops-message.ts
+++ src/read-side/html-pages/render-oops-message.ts
@@ -1,10 +1,10 @@
 import { HtmlFragment, toHtmlFragment } from '../../types/html-fragment';
 
 export const renderOopsMessage = (message: HtmlFragment): HtmlFragment => toHtmlFragment(`
-  <div class="error-page">
+  <header class="page-header">
     <h1>Oops!</h1>
-    <p>
-      ${message}
-    </p>   
-  </div>
+  </header>
+  <p>
+    ${message}
+  </p>
 `);
