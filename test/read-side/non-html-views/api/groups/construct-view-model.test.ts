d2490fffecdf676014153f6236daee2200579b39
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-18 12:01:26 +0100

    Add an incomplete test #3334

--- test/read-side/non-html-views/api/groups/construct-view-model.test.ts
+++ test/read-side/non-html-views/api/groups/construct-view-model.test.ts
@@ -1,9 +1,27 @@
+import { constructViewModel } from '../../../../../src/read-side/non-html-views/api/groups/construct-view-model';
+import { createTestFramework, TestFramework } from '../../../../framework';
+
 describe('construct-view-model', () => {
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
   describe('when the group has admins', () => {
     it.todo('lists all of the admins');
   });
 
-  describe('when the group has no admins', () => {
-    it.todo('lists no admins');
+  describe.skip('when the group has no admins', () => {
+    let admins: ReadonlyArray<unknown>;
+
+    beforeEach(async () => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const viewModel = constructViewModel(framework.dependenciesForViews);
+    });
+
+    it('lists no admins', () => {
+      expect(admins).toHaveLength(0);
+    });
   });
 });
