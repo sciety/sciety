5f6d4e924845cd512fe4f3834cbb719756ceb14d
Giorgio Sironi <g.sironi@elifesciences.org>
2024-06-18 11:55:21 +0100

    Sketch a test list for extending /api/groups to show group admins #3334

---
+++ test/read-side/non-html-views/api/groups/construct-view-model.test.ts
@@ -0,0 +1,9 @@
+describe('construct-view-model', () => {
+  describe('when the group has admins', () => {
+    it.todo('lists all of the admins');
+  });
+
+  describe('when the group has no admins', () => {
+    it.todo('lists no admins');
+  });
+});
