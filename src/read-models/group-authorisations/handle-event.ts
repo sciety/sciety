50605f56e9bbac8154315caf60365c16592c3b96
Giorgio Sironi <g.sironi@elifesciences.org>
2024-06-19 13:43:15 +0100

    Expand the read model to be able to add more than one index #3334

--- src/read-models/group-authorisations/handle-event.ts
+++ src/read-models/group-authorisations/handle-event.ts
@@ -3,15 +3,19 @@ import { DomainEvent, isEventOfType } from '../../domain-events';
 import { GroupId } from '../../types/group-id';
 import { UserId } from '../../types/user-id';
 
-export type ReadModel = Record<UserId, Array<GroupId>>;
+export type ReadModel = {
+  byUserId: Record<UserId, Array<GroupId>>,
+};
 
-export const initialState = (): ReadModel => ({});
+export const initialState = (): ReadModel => ({
+  byUserId: {},
+});
 
 export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel => {
   if (isEventOfType('UserAssignedAsAdminOfGroup')(event)) {
-    const current = readmodel[event.userId] ?? [];
+    const current = readmodel.byUserId[event.userId] ?? [];
     current.push(event.groupId);
-    readmodel[event.userId] = current;
+    readmodel.byUserId[event.userId] = current;
   }
   return readmodel;
 };
