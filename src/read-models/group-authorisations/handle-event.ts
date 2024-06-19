457e5f52a20f79188b600a3bcbd6654fd105fb2c
Giorgio Sironi <g.sironi@elifesciences.org>
2024-06-19 13:59:12 +0100

    Allow ReadModel to be indexed by group that is being administered #3334

--- src/read-models/group-authorisations/handle-event.ts
+++ src/read-models/group-authorisations/handle-event.ts
@@ -5,17 +5,23 @@ import { UserId } from '../../types/user-id';
 
 export type ReadModel = {
   byUserId: Map<UserId, Array<GroupId>>,
+  byGroupId: Map<GroupId, Array<UserId>>,
 };
 
 export const initialState = (): ReadModel => ({
   byUserId: new Map(),
+  byGroupId: new Map(),
 });
 
 export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel => {
   if (isEventOfType('UserAssignedAsAdminOfGroup')(event)) {
-    const current = readmodel.byUserId.get(event.userId) ?? [];
-    current.push(event.groupId);
-    readmodel.byUserId.set(event.userId, current);
+    const groupsThatAreAdministeredByThisUser = readmodel.byUserId.get(event.userId) ?? [];
+    groupsThatAreAdministeredByThisUser.push(event.groupId);
+    readmodel.byUserId.set(event.userId, groupsThatAreAdministeredByThisUser);
+
+    const userIdsThatAreAdminsOfTheGroup = readmodel.byGroupId.get(event.groupId) ?? [];
+    userIdsThatAreAdminsOfTheGroup.push(event.userId);
+    readmodel.byGroupId.set(event.groupId, userIdsThatAreAdminsOfTheGroup);
   }
   return readmodel;
 };
