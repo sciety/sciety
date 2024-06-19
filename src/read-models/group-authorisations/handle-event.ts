529e88242a2fc8beaf896f6af0e597335420fe38
LinaKind <l.kinduryte@elifesciences.org>
2024-06-19 13:50:31 +0100

    Use a map instead of a record to avoid performance risks, like in other read models #3334

--- src/read-models/group-authorisations/handle-event.ts
+++ src/read-models/group-authorisations/handle-event.ts
@@ -4,18 +4,18 @@ import { GroupId } from '../../types/group-id';
 import { UserId } from '../../types/user-id';
 
 export type ReadModel = {
-  byUserId: Record<UserId, Array<GroupId>>,
+  byUserId: Map<UserId, Array<GroupId>>,
 };
 
 export const initialState = (): ReadModel => ({
-  byUserId: {},
+  byUserId: new Map(),
 });
 
 export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel => {
   if (isEventOfType('UserAssignedAsAdminOfGroup')(event)) {
-    const current = readmodel.byUserId[event.userId] ?? [];
+    const current = readmodel.byUserId.get(event.userId) ?? [];
     current.push(event.groupId);
-    readmodel.byUserId[event.userId] = current;
+    readmodel.byUserId.set(event.userId, current);
   }
   return readmodel;
 };
--- src/read-models/group-authorisations/is-user-admin-of-group.ts
+++ src/read-models/group-authorisations/is-user-admin-of-group.ts
@@ -2,6 +2,7 @@ import { ReadModel } from './handle-event';
 import { GroupId } from '../../types/group-id';
 import { UserId } from '../../types/user-id';
 
-export const isUserAdminOfGroup = (readModel: ReadModel) => (userId: UserId, groupId: GroupId): boolean => (
-  readModel.byUserId[userId] !== undefined && readModel.byUserId[userId].includes(groupId)
-);
+export const isUserAdminOfGroup = (readModel: ReadModel) => (userId: UserId, groupId: GroupId): boolean => {
+  const groupsUserIsAdminOf = readModel.byUserId.get(userId);
+  return groupsUserIsAdminOf !== undefined && groupsUserIsAdminOf.includes(groupId);
+};
