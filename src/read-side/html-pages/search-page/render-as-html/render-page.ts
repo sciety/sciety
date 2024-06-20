13ac4ffc0455341ce726cfb947324e1b5cf6f28a
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-20 13:55:54 +0100

    Add a hardcoded category, to create a pull for building a viewmodel #3342

--- src/read-side/html-pages/search-page/render-as-html/render-page.ts
+++ src/read-side/html-pages/search-page/render-as-html/render-page.ts
@@ -2,12 +2,19 @@ import { pipe } from 'fp-ts/function';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 import { renderSearchForm } from '../../shared-components/search-form';
 
+const renderSearchCategories = () => `
+  <section>
+    <a href="https://labs.sciety.org/categories/articles?category=Infectious%20Diseases%20(except%20HIV/AIDS)">Infectious Diseases (except HIV/AIDS)</a>
+  </section>
+`;
+
 export const renderPage = (): HtmlFragment => pipe(
   `
     <header class="page-header page-header--search-results">
       <h1>Search Sciety</h1>
     </header>
     ${renderSearchForm('', true)}
+    ${process.env.EXPERIMENT_ENABLED === 'true' ? renderSearchCategories() : ''}
   `,
   toHtmlFragment,
 );
