98a4923a409b85c0dbae38bf5715d1e223d5f976
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-20 13:34:50 +0100

    Clarify the page main pipe #3342

--- src/read-side/html-pages/search-page/search-page.ts
+++ src/read-side/html-pages/search-page/search-page.ts
@@ -1,15 +1,19 @@
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import { toHtmlFragment } from '../../../types/html-fragment';
 import { ErrorPageViewModel } from '../construct-error-page-view-model';
 import { HtmlPage, toHtmlPage } from '../html-page';
 import { renderSearchForm } from '../shared-components/search-form';
 
-export const searchPage: TE.TaskEither<ErrorPageViewModel, HtmlPage> = TE.right(toHtmlPage({
-  title: 'Search',
-  content: toHtmlFragment(`
+export const searchPage: TE.TaskEither<ErrorPageViewModel, HtmlPage> = pipe(
+  toHtmlPage({
+    title: 'Search',
+    content: toHtmlFragment(`
     <header class="page-header page-header--search-results">
       <h1>Search Sciety</h1>
     </header>
     ${renderSearchForm('', true)}
   `),
-}));
+  }),
+  TE.right,
+);
