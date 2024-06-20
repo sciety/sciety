a7c6d546680c3df9ed010ecc72526765c9ff93e5
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-20 14:24:28 +0100

    Provide a viewmodel constructor for the search page #3342

---
+++ src/read-side/html-pages/search-page/construct-view-model/construct-view-model.ts
@@ -0,0 +1,12 @@
+import { ViewModel } from '../view-model';
+
+export const constructViewModel = (): ViewModel => [
+  {
+    title: 'Infectious Diseases (except HIV/AIDS)',
+    href: 'https://labs.sciety.org/categories/articles?category=Infectious%20Diseases%20(except%20HIV/AIDS)',
+  },
+  {
+    title: 'Epidemiology',
+    href: 'https://labs.sciety.org/categories/articles?category=Epidemiology',
+  },
+];
--- src/read-side/html-pages/search-page/search-page.ts
+++ src/read-side/html-pages/search-page/search-page.ts
@@ -1,23 +1,12 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { constructViewModel } from './construct-view-model/construct-view-model';
 import { renderAsHtml } from './render-as-html/render-as-html';
-import { ViewModel } from './view-model';
 import { ErrorPageViewModel } from '../construct-error-page-view-model';
 import { HtmlPage } from '../html-page';
 
-const viewModel: ViewModel = [
-  {
-    title: 'Infectious Diseases (except HIV/AIDS)',
-    href: 'https://labs.sciety.org/categories/articles?category=Infectious%20Diseases%20(except%20HIV/AIDS)',
-  },
-  {
-    title: 'Epidemiology',
-    href: 'https://labs.sciety.org/categories/articles?category=Epidemiology',
-  },
-];
-
 export const searchPage: TE.TaskEither<ErrorPageViewModel, HtmlPage> = pipe(
-  viewModel,
+  constructViewModel(),
   renderAsHtml,
   TE.right,
 );
