9e1b3f8ece1634eebdf7f727dcff9de7ec008127
Giorgio Sironi <g.sironi@elifesciences.org>
2024-06-21 11:31:36 +0100

    Organize the search page into sections #3342

--- src/read-side/html-pages/shared-components/search-form/render-search-form.ts
+++ src/read-side/html-pages/shared-components/search-form/render-search-form.ts
@@ -2,21 +2,23 @@ import { htmlEscape } from 'escape-goat';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 
 export const renderSearchForm = (query: string, evaluatedOnly: boolean): HtmlFragment => toHtmlFragment(`
-  <form action="/search" method="get" class="search-form">
-    <label for="searchText" class="standard-form__sub_heading">Find preprints</label>
-    <div class="standard-form__helper_text">
-      Search articles by DOI, author or keyword.
-    </div>
-    <div class="search-form__positioning_context">
-      ${htmlEscape`<input value="${query}" id="searchText" name="query" class="standard-form__full_width_text_input">`}
-      <section>
-        <input type="checkbox" name="evaluatedOnly" value="true" id="searchEvaluatedOnlyFilter"${evaluatedOnly ? ' checked' : ''}>
-        <label for="searchEvaluatedOnlyFilter" class="search-form__checkbox_label">Search only evaluated articles</label>
-      </section>
-      <button type="submit" class="search-form__submit" aria-label="Run the search">Search</button>
-      <button type="reset" id="clearSearchText" class="search-form__clear visually-hidden">
-        <img src="/static/images/clear-search-text-icon.svg" class="search-form__clear_icon" alt="">
-      </button>
-    </div>
-  </form>
+  <section>
+    <h2>Find preprints</h2>
+    <form action="/search" method="get" class="search-form">
+      <label for="searchText" class="search-form__label">
+        Search articles by DOI, author or keyword.
+      </label>
+      <div class="search-form__positioning_context">
+        ${htmlEscape`<input value="${query}" id="searchText" name="query" class="standard-form__full_width_text_input">`}
+        <section>
+          <input type="checkbox" name="evaluatedOnly" value="true" id="searchEvaluatedOnlyFilter"${evaluatedOnly ? ' checked' : ''}>
+          <label for="searchEvaluatedOnlyFilter" class="search-form__checkbox_label">Search only evaluated articles</label>
+        </section>
+        <button type="submit" class="search-form__submit" aria-label="Run the search">Search</button>
+        <button type="reset" id="clearSearchText" class="search-form__clear visually-hidden">
+          <img src="/static/images/clear-search-text-icon.svg" class="search-form__clear_icon" alt="">
+        </button>
+      </div>
+    </form>
+  </section>
 `);
