29a0cc91a5afd292dbcab4f1fbd9662b885065ef
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-06-24 10:36:07 +0100

    Improve backstop coverage for browse section #3342

--- src/infrastructure/stub-adapters/fetch-search-categories.ts
+++ src/infrastructure/stub-adapters/fetch-search-categories.ts
@@ -1,4 +1,11 @@
 import * as TE from 'fp-ts/TaskEither';
 import { ExternalQueries } from '../../third-parties';
 
-export const fetchSearchCategories: ExternalQueries['fetchSearchCategories'] = () => TE.right(['Epidemiology']);
+export const fetchSearchCategories: ExternalQueries['fetchSearchCategories'] = () => TE.right([
+  'Epidemiology',
+  'Infectious Diseases (except HIV/AIDS)',
+  'Microbiology',
+  'Public and Global Health',
+  'Neuroscience',
+  'Immunology',
+]);
