cfad95d8007a0deff9cc57333b8359ceb52e3778
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-25 14:03:53 +0100

    Replace more uses of arbitraryArticleId #3356

--- test/third-parties/external-queries.helper.ts
+++ test/third-parties/external-queries.helper.ts
@@ -1,11 +1,12 @@
 import * as O from 'fp-ts/Option';
 import { ArticleDetails } from '../../src/types/article-details';
+import { ArticleId } from '../../src/types/article-id';
 import { arbitrarySanitisedHtmlFragment, arbitraryString } from '../helpers';
-import { arbitraryArticleId } from '../types/article-id.helper';
+import { arbitraryExpressionDoi } from '../types/expression-doi.helper';
 
 export const arbitraryArticleDetails = (): ArticleDetails => ({
   abstract: O.some(arbitrarySanitisedHtmlFragment()),
   authors: O.some([arbitraryString()]),
-  doi: arbitraryArticleId(),
+  doi: new ArticleId(arbitraryExpressionDoi()),
   title: arbitrarySanitisedHtmlFragment(),
 });
--- test/types/recorded-evaluation.helper.ts
+++ test/types/recorded-evaluation.helper.ts
@@ -1,22 +1,18 @@
 import * as O from 'fp-ts/Option';
-import { arbitraryArticleId } from './article-id.helper';
 import { arbitraryEvaluationLocator } from './evaluation-locator.helper';
 import { arbitraryEvaluationType } from './evaluation-type.helper';
+import { arbitraryExpressionDoi } from './expression-doi.helper';
 import { arbitraryGroupId } from './group-id.helper';
-import { toExpressionDoi } from '../../src/types/article-id';
 import { RecordedEvaluation } from '../../src/types/recorded-evaluation';
 import { arbitraryDate, arbitraryString } from '../helpers';
 
-export const arbitraryRecordedEvaluation = (): RecordedEvaluation => {
-  const articleId = arbitraryArticleId();
-  return {
-    expressionDoi: toExpressionDoi(articleId),
-    groupId: arbitraryGroupId(),
-    evaluationLocator: arbitraryEvaluationLocator(),
-    publishedAt: arbitraryDate(),
-    authors: [arbitraryString()],
-    recordedAt: arbitraryDate(),
-    updatedAt: arbitraryDate(),
-    type: O.some(arbitraryEvaluationType()),
-  };
-};
+export const arbitraryRecordedEvaluation = (): RecordedEvaluation => ({
+  expressionDoi: arbitraryExpressionDoi(),
+  groupId: arbitraryGroupId(),
+  evaluationLocator: arbitraryEvaluationLocator(),
+  publishedAt: arbitraryDate(),
+  authors: [arbitraryString()],
+  recordedAt: arbitraryDate(),
+  updatedAt: arbitraryDate(),
+  type: O.some(arbitraryEvaluationType()),
+});
