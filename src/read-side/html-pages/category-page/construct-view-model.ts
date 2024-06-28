32f1a9f0237470ff7bbe5a7909bcb98341594765
LinaKind <l.kinduryte@elifesciences.org>
2024-06-28 15:26:31 +0100

    Remove unnecessary linting comment #3358

--- src/read-side/html-pages/category-page/construct-view-model.ts
+++ src/read-side/html-pages/category-page/construct-view-model.ts
@@ -9,7 +9,6 @@ import * as EDOI from '../../../types/expression-doi';
 import { constructArticleCardStackWithSilentFailures } from '../shared-components/article-list';
 
 export const constructViewModel = (
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
   dependencies: Dependencies,
 ) => (params: Params): TE.TaskEither<DE.DataError, ViewModel> => pipe(
   [EDOI.fromValidatedString('10.1101/2024.01.16.575490')],
