1eaf0aa89d3928ab16a031e4f66915fe578becfe
LinaKind <l.kinduryte@elifesciences.org>
2024-06-28 14:55:07 +0100

    Remove the possibility of rendering error cards when they can never occur #3358

--- src/read-side/html-pages/category-page/render-articles-list.ts
+++ src/read-side/html-pages/category-page/render-articles-list.ts
@@ -1,30 +1,15 @@
-import * as E from 'fp-ts/Either';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import { HtmlFragment, toHtmlFragment } from '../../../types/html-fragment';
+import { HtmlFragment } from '../../../types/html-fragment';
 import {
-  ArticleCardViewModel,
-  ArticleErrorCardViewModel, renderArticleCard, renderArticleErrorCard,
+  ArticleCardViewModel, renderArticleCard,
 } from '../shared-components/article-card';
 import { renderArticleList } from '../shared-components/article-list';
 import { renderListItems } from '../shared-components/list-items';
 
-type ArticlesListViewModel = ReadonlyArray<E.Either<
-ArticleErrorCardViewModel,
-ArticleCardViewModel
->>;
-
-type RenderArticlesList = (
-  articleViewModels: ArticlesListViewModel,
-) => HtmlFragment;
-
-export const renderArticlesList: RenderArticlesList = (articles) => pipe(
-  articles,
-  RA.map(E.fold(
-    renderArticleErrorCard,
-    (articleCard) => renderArticleCard(articleCard),
-  )),
+export const renderArticlesList = (articleCardViewModels: ReadonlyArray<ArticleCardViewModel>): HtmlFragment => pipe(
+  articleCardViewModels,
+  RA.map(renderArticleCard),
   (listItems) => renderListItems(listItems),
   (list) => renderArticleList(list),
-  toHtmlFragment,
 );
