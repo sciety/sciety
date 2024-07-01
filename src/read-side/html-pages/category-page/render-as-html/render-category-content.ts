f8a9f543ba1aa08b792662d2cb8208aa956526d1
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-07-01 11:30:03 +0100

    Provide user with a more informative message if the query returns no results #3358

--- src/read-side/html-pages/category-page/render-as-html/render-category-content.ts
+++ src/read-side/html-pages/category-page/render-as-html/render-category-content.ts
@@ -1,9 +1,13 @@
+import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { renderArticlesList } from './render-articles-list';
-import { HtmlFragment } from '../../../../types/html-fragment';
+import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 import { ViewModel } from '../view-model';
 
 export const renderCategoryContent = (viewModel: ViewModel['categoryContent']): HtmlFragment => pipe(
   viewModel,
-  renderArticlesList,
+  RA.match(
+    () => toHtmlFragment('<p>No evaluated articles were found for this category.</p>'),
+    renderArticlesList,
+  ),
 );
