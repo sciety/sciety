ad201142e6658ce9e02f29471a49873907f8fed1
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-07-01 10:03:59 +0100

    Display one hardcoded category result #3358

--- src/third-parties/search-categories/fetch-by-category.ts
+++ src/third-parties/search-categories/fetch-by-category.ts
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as E from 'fp-ts/Either';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
 import { flow, pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
@@ -14,7 +14,13 @@ const categoryName = 'Epidemiology';
 
 const url = `https://labs.sciety.org/api/papers/v1/preprints?filter%5Bcategory%5D=${categoryName}&filter%5Bevaluated_only%5D=true&page%5Bsize%5D=10&page%5Bnumber%5D=1&fields%5Bpaper%5D=doi`;
 
-const scietyLabsByCategoryResponseCodec = t.type({});
+const scietyLabsByCategoryResponseCodec = t.type({
+  data: t.readonlyArray(t.strict({
+    attributes: t.strict({
+      doi: t.string,
+    }),
+  })),
+});
 
 export const fetchByCategory = (
   queryExternalService: QueryExternalService,
@@ -26,7 +32,7 @@ export const fetchByCategory = (
     decodeAndLogFailures(logger, scietyLabsByCategoryResponseCodec, { url }),
     E.mapLeft(() => DE.unavailable),
   )),
-  TE.map(() => [
-    EDOI.fromValidatedString('10.1101/2024.01.16.575490'),
-  ]),
+  TE.map((result) => result.data),
+  TE.map(RA.map((item) => item.attributes.doi)),
+  TE.map(RA.map(EDOI.fromValidatedString)),
 );
