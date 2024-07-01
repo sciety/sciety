67c139616e9de40a0aca9ecdebe6cdb388b5fd71
David Moulton <d.moulton@elifesciences.org>
2024-07-01 13:55:52 +0100

    Keep rolling out logging of page param decoding failures #3358

--- src/read-side/html-pages/save-article-form-page/save-article-form-page.ts
+++ src/read-side/html-pages/save-article-form-page/save-article-form-page.ts
@@ -1,24 +1,18 @@
-import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import { formatValidationErrors } from 'io-ts-reporters';
 import { constructViewModel } from './construct-view-model';
 import { Dependencies } from './dependencies';
 import { paramsCodec } from './params';
 import { renderAsHtml } from './render-as-html';
-import * as DE from '../../../types/data-error';
 import { constructErrorPageViewModel } from '../construct-error-page-view-model';
 import { ConstructLoggedInPage } from '../construct-page';
+import { decodePageParams } from '../decode-page-params';
 
 export const saveArticleFormPage = (
   dependencies: Dependencies,
 ): ConstructLoggedInPage => (userId, input) => pipe(
   input,
-  paramsCodec.decode,
-  E.mapLeft((errors) => {
-    dependencies.logger('warn', 'saveArticleFormPage params codec failed', { errors: formatValidationErrors(errors) });
-    return DE.notFound;
-  }),
+  decodePageParams(dependencies.logger, paramsCodec),
   TE.fromEither,
   TE.chainW(constructViewModel(dependencies, userId)),
   TE.bimap(constructErrorPageViewModel, renderAsHtml),
