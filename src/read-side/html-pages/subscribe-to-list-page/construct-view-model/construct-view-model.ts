1159b6709a71114a12d83a5f0f443395c23994e0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-07-01 14:01:29 +0100

    Roll out common page params decoding to another place #3358

--- src/read-side/html-pages/subscribe-to-list-page/construct-view-model/construct-view-model.ts
+++ src/read-side/html-pages/subscribe-to-list-page/construct-view-model/construct-view-model.ts
@@ -1,24 +1,25 @@
-import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
-import { Queries } from '../../../../read-models';
 import * as DE from '../../../../types/data-error';
 import { listIdCodec } from '../../../../types/list-id';
+import { DependenciesForViews } from '../../../dependencies-for-views';
+import { decodePageParams } from '../../decode-page-params';
 import { ViewModel } from '../view-model';
 
 const codec = t.strict({
   listId: listIdCodec,
 });
 
-type ConstructViewModel = (dependencies: Queries) => (params: unknown) => TE.TaskEither<DE.DataError, ViewModel>;
+type ConstructViewModel = (dependencies: DependenciesForViews)
+=> (params: Record<string, unknown>)
+=> TE.TaskEither<DE.DataError, ViewModel>;
 
 export const constructViewModel: ConstructViewModel = (dependencies) => (params) => pipe(
   params,
-  codec.decode,
-  E.mapLeft(() => DE.notFound),
+  decodePageParams(dependencies.logger, codec),
   TE.fromEither,
-  TE.chain(({ listId }) => pipe(
+  TE.chainW(({ listId }) => pipe(
     listId,
     dependencies.lookupList,
     TE.fromOption(() => DE.notFound),
--- src/read-side/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
+++ src/read-side/html-pages/subscribe-to-list-page/subscribe-to-list-page.ts
@@ -2,11 +2,11 @@ import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { constructViewModel } from './construct-view-model';
 import { renderAsHtml } from './render-as-html';
-import { Queries } from '../../../read-models';
+import { DependenciesForViews } from '../../dependencies-for-views';
 import { constructErrorPageViewModel } from '../construct-error-page-view-model';
 import { ConstructPage } from '../construct-page';
 
-export const subscribeToListPage = (dependencies: Queries): ConstructPage => (params) => pipe(
+export const subscribeToListPage = (dependencies: DependenciesForViews): ConstructPage => (params) => pipe(
   params,
   constructViewModel(dependencies),
   TE.bimap(constructErrorPageViewModel, renderAsHtml),
