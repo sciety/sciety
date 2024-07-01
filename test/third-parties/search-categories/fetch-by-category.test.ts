bfea110cbf6a38ae5a6969c96ca2c980be1ec8b3
David Moulton <d.moulton@elifesciences.org>
2024-07-01 10:38:10 +0100

    Write a passing happy path test #3358

--- test/third-parties/search-categories/fetch-by-category.test.ts
+++ test/third-parties/search-categories/fetch-by-category.test.ts
@@ -1,6 +1,31 @@
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { fetchByCategory } from '../../../src/third-parties/search-categories';
+import { dummyLogger } from '../../dummy-logger';
+import { arbitraryString } from '../../helpers';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+import { arbitraryExpressionDoi } from '../../types/expression-doi.helper';
+
+const responseWithValidDoi = { data: [{ attributes: { doi: arbitraryExpressionDoi() } }] };
+
 describe('fetch-by-category', () => {
   describe('when an item with a valid doi syntax is returned', () => {
-    it.todo('is included');
+    const queryExternalService = () => () => TE.right(responseWithValidDoi);
+    const invokeFetchByCategory = async (category: string) => pipe(
+      category,
+      fetchByCategory(queryExternalService, dummyLogger),
+      TE.getOrElse(shouldNotBeCalled),
+    )();
+
+    let result: Awaited<ReturnType<typeof invokeFetchByCategory>>;
+
+    beforeEach(async () => {
+      result = await invokeFetchByCategory(arbitraryString());
+    });
+
+    it('is included', () => {
+      expect(result).toHaveLength(1);
+    });
   });
 
   describe('when an item with an invalid doi syntax is returned', () => {
