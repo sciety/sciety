5b4cc7cc994657bdca86a6e37eeca9bfe585c48e
David Moulton <d.moulton@elifesciences.org>
2024-07-01 09:55:45 +0100

    Add boilerplate for querying external service #3358

--- src/third-parties/search-categories/fetch-by-category.ts
+++ src/third-parties/search-categories/fetch-by-category.ts
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
 import { Logger } from '../../logger';
+import * as DE from '../../types/data-error';
 import * as EDOI from '../../types/expression-doi';
+import { decodeAndLogFailures } from '../decode-and-log-failures';
 import { ExternalQueries } from '../external-queries';
 import { QueryExternalService } from '../query-external-service';
 
@@ -10,12 +14,19 @@ const categoryName = 'Epidemiology';
 
 const url = `https://labs.sciety.org/api/papers/v1/preprints?filter%5Bcategory%5D=${categoryName}&filter%5Bevaluated_only%5D=true&page%5Bsize%5D=10&page%5Bnumber%5D=1&fields%5Bpaper%5D=doi`;
 
+const scietyLabsByCategoryResponseCodec = t.type({});
+
 export const fetchByCategory = (
   queryExternalService: QueryExternalService,
   logger: Logger,
 ): ExternalQueries['fetchByCategory'] => () => pipe(
-  [
+  url,
+  queryExternalService(),
+  TE.chainEitherKW(flow(
+    decodeAndLogFailures(logger, scietyLabsByCategoryResponseCodec, { url }),
+    E.mapLeft(() => DE.unavailable),
+  )),
+  TE.map(() => [
     EDOI.fromValidatedString('10.1101/2024.01.16.575490'),
-  ],
-  TE.right,
+  ]),
 );
