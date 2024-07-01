62db6a29072208b2fc197ee06f908555b27e7a85
LinaKind <l.kinduryte@elifesciences.org>
2024-07-01 10:20:34 +0100

    Define desired behaviour with a test list #3358

---
+++ test/third-parties/search-categories/fetch-by-category.test.ts
@@ -0,0 +1,9 @@
+describe('fetch-by-category', () => {
+  describe('when an item with a valid doi syntax is returned', () => {
+    it.todo('is included');
+  });
+
+  describe('when an item with an invalid doi syntax is returned', () => {
+    it.todo('is not included');
+  });
+});
