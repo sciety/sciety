2b84b099b5c832d32a1667af91df97aaae906cc1
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-02 09:24:39 +0100

    Keep rolling out the generic ConstructViewModel type #3358

--- src/read-side/html-pages/category-page/construct-view-model.ts
+++ src/read-side/html-pages/category-page/construct-view-model.ts
@@ -1,14 +1,11 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import { Dependencies } from './dependencies';
 import { Params } from './params';
 import { ViewModel } from './view-model';
-import * as DE from '../../../types/data-error';
+import { ConstructViewModel } from '../construct-view-model';
 import { constructArticleCardStackWithSilentFailures } from '../shared-components/article-list';
 
-export const constructViewModel = (
-  dependencies: Dependencies,
-) => (params: Params): TE.TaskEither<DE.DataError, ViewModel> => pipe(
+export const constructViewModel: ConstructViewModel<Params, ViewModel> = (dependencies) => (params) => pipe(
   dependencies.fetchByCategory(params.categoryName),
   TE.chainTaskK(constructArticleCardStackWithSilentFailures(dependencies)),
   TE.map((articleCardViewModel) => ({
