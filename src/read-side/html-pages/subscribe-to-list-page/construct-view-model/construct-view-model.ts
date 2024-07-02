456711aeebb0666eb81eb2e9979ce828dfd0ca55
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-02 09:51:50 +0100

    Continue rolling out the ConstructViewModel type #3358

--- src/read-side/html-pages/subscribe-to-list-page/construct-view-model/construct-view-model.ts
+++ src/read-side/html-pages/subscribe-to-list-page/construct-view-model/construct-view-model.ts
@@ -1,15 +1,11 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as DE from '../../../../types/data-error';
-import { DependenciesForViews } from '../../../dependencies-for-views';
+import { ConstructViewModel } from '../../construct-view-model';
 import { Params } from '../params';
 import { ViewModel } from '../view-model';
 
-type ConstructViewModel = (dependencies: DependenciesForViews)
-=> (params: Params)
-=> TE.TaskEither<DE.DataError, ViewModel>;
-
-export const constructViewModel: ConstructViewModel = (dependencies) => (params) => pipe(
+export const constructViewModel: ConstructViewModel<Params, ViewModel> = (dependencies) => (params) => pipe(
   params.listId,
   dependencies.lookupList,
   TE.fromOption(() => DE.notFound),
