a2f3c2d9cd957583e29e521234bea06b3b18fe2b
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-07-02 09:27:18 +0100

    Remove an unnecessary local Dependencies type #3358

--- src/read-side/html-pages/category-page/category-page.ts
+++ src/read-side/html-pages/category-page/category-page.ts
@@ -1,14 +1,14 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { constructViewModel } from './construct-view-model';
-import { Dependencies } from './dependencies';
 import { paramsCodec } from './params';
 import { renderAsHtml } from './render-as-html/render-as-html';
+import { DependenciesForViews } from '../../dependencies-for-views';
 import { constructErrorPageViewModel } from '../construct-error-page-view-model';
 import { ConstructPage } from '../construct-page';
 import { decodePageParams } from '../decode-page-params';
 
-export const categoryPage = (dependencies: Dependencies): ConstructPage => (input) => pipe(
+export const categoryPage = (dependencies: DependenciesForViews): ConstructPage => (input) => pipe(
   input,
   decodePageParams(dependencies.logger, paramsCodec),
   TE.fromEither,
