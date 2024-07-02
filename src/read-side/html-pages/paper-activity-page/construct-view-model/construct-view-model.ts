43debe5bd1da5618d6f235afbd8edcacb930d1c3
David Moulton <d.moulton@elifesciences.org>
2024-07-02 09:48:23 +0100

    Continue rolling out the ConstructViewModel type #3358

--- src/read-side/html-pages/paper-activity-page/construct-view-model/construct-view-model.ts
+++ src/read-side/html-pages/paper-activity-page/construct-view-model/construct-view-model.ts
@@ -6,10 +6,8 @@ import { pipe } from 'fp-ts/function';
 import { constructContainingList } from './construct-containing-list';
 import { constructRelatedArticles } from './construct-related-articles';
 import { constructUserListManagement } from './construct-user-list-management';
-import { Dependencies } from './dependencies';
 import { feedSummary } from './feed-summary';
 import { getFeedItemsByDateDescending } from './get-feed-items-by-date-descending';
-import * as DE from '../../../../types/data-error';
 import { CanonicalExpressionDoi, ExpressionDoi } from '../../../../types/expression-doi';
 import { ExpressionFrontMatter } from '../../../../types/expression-front-matter';
 import { toHtmlFragment } from '../../../../types/html-fragment';
@@ -18,6 +16,7 @@ import { constructFrontMatter } from '../../../construct-front-matter';
 import { constructCurationStatements } from '../../../curation-statements';
 import { findAllListsContainingPaper } from '../../../find-all-lists-containing-paper';
 import { constructReviewingGroups } from '../../../reviewing-groups';
+import { ConstructViewModel } from '../../construct-view-model';
 import { detectLanguage } from '../../shared-components/lang-attribute';
 import { ViewModel } from '../view-model';
 
@@ -42,13 +41,7 @@ type Params = {
   user: O.Option<{ id: UserId }>,
 };
 
-type ConstructViewModel = (
-  dependencies: Dependencies
-) => (
-  params: Params
-) => TE.TaskEither<DE.DataError, ViewModel>;
-
-export const constructViewModel: ConstructViewModel = (dependencies) => (params) => pipe(
+export const constructViewModel: ConstructViewModel<Params, ViewModel> = (dependencies) => (params) => pipe(
   params.latestExpressionDoi,
   dependencies.fetchPublishingHistory,
   TE.chain((publishingHistory) => pipe(
