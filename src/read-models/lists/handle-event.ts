801de38b48f96841904533b093cec8c08a133b09
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-07-09 11:58:38 +0100

    Ensure read model state is left unchanged when given a non-sensical event #3375

--- src/read-models/lists/handle-event.ts
+++ src/read-models/lists/handle-event.ts
@@ -17,8 +17,10 @@ List,
 };
 
 const registerUpdateToList = (readModel: ReadModel, listId: ListId, date: Date) => {
-  readModel.byListId[listId].version += 1;
-  readModel.byListId[listId].updatedAt = date;
+  if (readModel.byListId[listId] !== undefined) {
+    readModel.byListId[listId].version += 1;
+    readModel.byListId[listId].updatedAt = date;
+  }
 };
 
 export type ReadModel = {
@@ -67,9 +69,11 @@ export const handleEvent = (readmodel: ReadModel, event: DomainEvent): ReadModel
     });
   } else if (isEventOfType('ArticleRemovedFromList')(event)) {
     registerUpdateToList(readmodel, event.listId, event.date);
-    readmodel.byListId[event.listId].entries = readmodel.byListId[event.listId].entries.filter(
-      (entry) => entry.expressionDoi !== toExpressionDoi(event.articleId),
-    );
+    if (readmodel.byListId[event.listId] !== undefined) {
+      readmodel.byListId[event.listId].entries = readmodel.byListId[event.listId].entries.filter(
+        (entry) => entry.expressionDoi !== toExpressionDoi(event.articleId),
+      );
+    }
   } else if (isEventOfType('ListNameEdited')(event)) {
     registerUpdateToList(readmodel, event.listId, event.date);
     readmodel.byListId[event.listId].name = event.name;
--- test/read-models/lists/handle-event.test.ts
+++ test/read-models/lists/handle-event.test.ts
@@ -1,10 +1,30 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { handleEvent, initialState } from '../../../src/read-models/lists/handle-event';
+import { arbitraryArticleRemovedFromListEvent, arbitraryListCreatedEvent } from '../../domain-events/list-resource-events.helper';
+
 describe('handle-event', () => {
   describe('given an ArticleRemovedFromList event', () => {
+    const listCreated = arbitraryListCreatedEvent();
+    const articleRemovedFromListEvent = {
+      ...arbitraryArticleRemovedFromListEvent(),
+      listId: listCreated.listId,
+    };
+
     describe.each([
-      ['when the list does not exist'],
-      ['when the article is not in the list'],
-    ])('%s', () => {
-      it.todo('does not change the read model state');
+      ['when the list does not exist', []],
+      ['when the article is not in the list', [listCreated]],
+    ])('%s', (_, events) => {
+      const startingReadModelState = pipe(
+        events,
+        RA.reduce(initialState(), handleEvent),
+      );
+
+      const readModel = handleEvent(startingReadModelState, articleRemovedFromListEvent);
+
+      it('does not change the read model state', () => {
+        expect(readModel).toStrictEqual(startingReadModelState);
+      });
     });
   });
 });
