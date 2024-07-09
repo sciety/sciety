5e5c0dd76d8d7b5bb55e1ef24f99e6f34c5bc82c
LinaKind <l.kinduryte@elifesciences.org>
2024-07-09 09:43:07 +0100

    Write a failing test to force read side to handle reuse of list ids #3375

--- test/read-models/lists/lookup-list.test.ts
+++ test/read-models/lists/lookup-list.test.ts
@@ -233,23 +233,28 @@ describe('lookup-list', () => {
     });
   });
 
-  describe('when the list has been deleted', () => {
-    const readModel = pipe(
-      [
-        {
-          ...arbitraryListCreatedEvent(),
-          listId,
-        },
-        {
-          ...arbitraryListDeletedEvent(),
-          listId,
-        },
-      ],
-      RA.reduce(initialState(), handleEvent),
-    );
+  describe('when the list has been', () => {
+    const created = {
+      ...arbitraryListCreatedEvent(),
+      listId,
+    };
+    const deleted = {
+      ...arbitraryListDeletedEvent(),
+      listId,
+    };
 
-    it('returns not found', () => {
-      expect(lookupList(readModel)(listId)).toStrictEqual(O.none);
+    describe.each([
+      ['created, deleted', [created, deleted]],
+      // ['created, deleted, created', [created, deleted, created]],
+    ])('%s', (_, events) => {
+      const readModel = pipe(
+        events,
+        RA.reduce(initialState(), handleEvent),
+      );
+
+      it('returns not found', () => {
+        expect(lookupList(readModel)(listId)).toStrictEqual(O.none);
+      });
     });
   });
 });
