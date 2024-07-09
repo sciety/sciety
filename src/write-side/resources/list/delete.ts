75ee429641db11d172aad4bf68fc96cdc4d993d4
David Moulton <d.moulton@elifesciences.org>
2024-07-09 10:57:31 +0100

    Continue to remove duplicated read-side behaviour from one more resource action #3375

--- src/write-side/resources/list/delete.ts
+++ src/write-side/resources/list/delete.ts
@@ -1,20 +1,10 @@
 import * as E from 'fp-ts/Either';
-import * as O from 'fp-ts/Option';
-import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import { constructEvent, filterByName, isEventOfType } from '../../../domain-events';
-import { toErrorMessage } from '../../../types/error-message';
+import { constructEvent } from '../../../domain-events';
 import { DeleteListCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
 
-export const deleteList: ResourceAction<DeleteListCommand> = (command) => (events) => pipe(
-  events,
-  filterByName(['ListCreated', 'ListDeleted']),
-  RA.filter((event) => event.listId === command.listId),
-  RA.last,
-  O.filter(isEventOfType('ListCreated')),
-  O.match(
-    () => E.left(toErrorMessage('not-found')),
-    () => E.right([constructEvent('ListDeleted')(command)]),
-  ),
+export const deleteList: ResourceAction<DeleteListCommand> = (command) => () => pipe(
+  [constructEvent('ListDeleted')(command)],
+  E.right,
 );
--- test/write-side/resources/list/delete.test.ts
+++ test/write-side/resources/list/delete.test.ts
@@ -1,13 +1,11 @@
 import * as E from 'fp-ts/Either';
-import { identity, pipe } from 'fp-ts/function';
-import { constructEvent } from '../../../../src/domain-events';
+import { pipe } from 'fp-ts/function';
 import { deleteList } from '../../../../src/write-side/resources/list';
 import { arbitraryListCreatedEvent } from '../../../domain-events/list-resource-events.helper';
 import { shouldNotBeCalled } from '../../../should-not-be-called';
-import { arbitraryListId } from '../../../types/list-id.helper';
 
 describe('delete', () => {
-  describe('when the list identified in the command exists', () => {
+  describe('given a command', () => {
     const listCreatedEvent = arbitraryListCreatedEvent();
     const result = pipe(
       [
@@ -25,41 +23,6 @@ describe('delete', () => {
       expect(result[0]).toBeDomainEvent('ListDeleted', {
         listId: listCreatedEvent.listId,
       });
-    });
-  });
-
-  describe('no list with the given id ever existed', () => {
-    const result = pipe(
-      [],
-      deleteList({ listId: arbitraryListId() }),
-      E.matchW(
-        identity,
-        shouldNotBeCalled,
-      ),
-    );
-
-    it('fails with not-found', () => {
-      expect(result).toBe('not-found');
-    });
-  });
-
-  describe('when the list identified in the command existed but has been deleted', () => {
-    const listCreatedEvent = arbitraryListCreatedEvent();
-    const listDeleteEvent = constructEvent('ListDeleted')({ listId: listCreatedEvent.listId });
-    const result = pipe(
-      [
-        listCreatedEvent,
-        listDeleteEvent,
-      ],
-      deleteList({ listId: listCreatedEvent.listId }),
-      E.matchW(
-        identity,
-        shouldNotBeCalled,
-      ),
-    );
-
-    it('fails with not-found', () => {
-      expect(result).toBe('not-found');
     });
   });
 });
