e52125168e5af18eba6d45c79d484b9670867b7c
David Moulton <d.moulton@elifesciences.org>
2024-07-09 13:08:39 +0100

    Continue making read model robust against nonsensical event sequences #3375

--- test/read-models/lists/handle-event.test.ts
+++ test/read-models/lists/handle-event.test.ts
@@ -1,27 +1,36 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { handleEvent, initialState } from '../../../src/read-models/lists/handle-event';
-import { arbitraryArticleRemovedFromListEvent, arbitraryListCreatedEvent } from '../../domain-events/list-resource-events.helper';
+import {
+  arbitraryArticleAddedToListEvent,
+  arbitraryArticleRemovedFromListEvent,
+  arbitraryListCreatedEvent,
+} from '../../domain-events/list-resource-events.helper';
 
 describe('handle-event', () => {
   describe('given an ArticleRemovedFromList event', () => {
     const listCreated = arbitraryListCreatedEvent();
-    const articleRemovedFromListEvent = {
+    const articleAdded = {
+      ...arbitraryArticleAddedToListEvent(),
+      listId: listCreated.listId,
+    };
+    const articleRemoved = {
       ...arbitraryArticleRemovedFromListEvent(),
       listId: listCreated.listId,
+      articleId: articleAdded.articleId,
     };
 
     describe.each([
       ['when the list does not exist', []],
       ['when the article has never been in the list', [listCreated]],
-      // ['when the article was added and then removed from the list', [listCreated, articleAdded, articleRemoved]],
+      ['when the article was added and then removed from the list', [listCreated, articleAdded, articleRemoved]],
     ])('%s', (_, events) => {
       const startingReadModelState = pipe(
         events,
         RA.reduce(initialState(), handleEvent),
       );
 
-      const readModel = handleEvent(startingReadModelState, articleRemovedFromListEvent);
+      const readModel = handleEvent(startingReadModelState, articleRemoved);
 
       it('does not change the read model state', () => {
         expect(readModel).toStrictEqual(startingReadModelState);
