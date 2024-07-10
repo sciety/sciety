72281584bb5d9d7dcb116023ef5272bf5e7aeebd
David Moulton <d.moulton@elifesciences.org>
2024-07-10 10:55:28 +0100

    Make list.remove resource action always raise an event #3375

--- test/write-side/resources/list/remove-article.test.ts
+++ test/write-side/resources/list/remove-article.test.ts
@@ -1,26 +1,18 @@
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
-import { constructEvent } from '../../../../src/domain-events';
 import { ArticleId } from '../../../../src/types/article-id';
 import { removeArticle } from '../../../../src/write-side/resources/list/remove-article';
-import { arbitraryString } from '../../../helpers';
 import { shouldNotBeCalled } from '../../../should-not-be-called';
 import { arbitraryExpressionDoi } from '../../../types/expression-doi.helper';
 import { arbitraryListId } from '../../../types/list-id.helper';
-import { arbitraryListOwnerId } from '../../../types/list-owner-id.helper';
 
 describe('remove-article', () => {
   const listId = arbitraryListId();
   const expressionDoi = arbitraryExpressionDoi();
 
-  describe('the article is in the list', () => {
+  describe('given a command', () => {
     const result = pipe(
-      [
-        constructEvent('ListCreated')({
-          listId, name: arbitraryString(), description: arbitraryString(), ownerId: arbitraryListOwnerId(),
-        }),
-        constructEvent('ArticleAddedToList')({ articleId: new ArticleId(expressionDoi), listId }),
-      ],
+      [],
       removeArticle({
         listId,
         articleId: expressionDoi,
@@ -32,30 +24,11 @@ describe('remove-article', () => {
       expect(result).toHaveLength(1);
     });
 
-    it('succeeds and raises an event', () => {
+    it('returns an ArticleRemovedFromList event', () => {
       expect(result[0]).toBeDomainEvent('ArticleRemovedFromList', {
         articleId: new ArticleId(expressionDoi),
         listId,
       });
-    });
-  });
-
-  describe('the article is not in the list', () => {
-    const result = pipe(
-      [
-        constructEvent('ListCreated')({
-          listId, name: arbitraryString(), description: arbitraryString(), ownerId: arbitraryListOwnerId(),
-        }),
-      ],
-      removeArticle({
-        listId,
-        articleId: expressionDoi,
-      }),
-      E.getOrElseW(shouldNotBeCalled),
-    );
-
-    it('succeeds and raises no events', () => {
-      expect(result).toHaveLength(0);
     });
   });
 });
