02688aa93bf5db30cf68f406d01171017c0412b1
LinaKind <l.kinduryte@elifesciences.org>
2024-07-10 09:55:37 +0100

    Express intent with failing test #3375

--- test/read-models/lists/handle-event.test.ts
+++ test/read-models/lists/handle-event.test.ts
@@ -59,6 +59,34 @@ describe('handle-event', () => {
     });
   });
 
+  describe.skip('given a ListNameEdited event', () => {
+    const listCreated = arbitraryListCreatedEvent();
+    const listNameEditedToTheSameValue = constructEvent('ListNameEdited')({
+      listId: listCreated.listId,
+      name: listCreated.name,
+    });
+
+    describe.each([
+      ['when the name in the event matches the name the list has been created with', [listCreated]],
+      ['when the name in the event matches the name the list already has been update to', [listCreated, listNameEditedToTheSameValue]],
+    ])('%s', (_, events) => {
+      const readModel = pipe(
+        events,
+        RA.reduce(initialState(), handleEvent),
+      );
+
+      const snapshot = structuredClone(readModel);
+
+      beforeEach(() => {
+        handleEvent(readModel, listNameEditedToTheSameValue);
+      });
+
+      it('does not change the read model state', () => {
+        expect(JSON.stringify(readModel)).toStrictEqual(JSON.stringify(snapshot));
+      });
+    });
+  });
+
   describe('given an ArticleAddedToList event', () => {
     const listCreated = arbitraryListCreatedEvent();
     const articleAdded = {
