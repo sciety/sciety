974a978422d69ba01a9f18fe78167c76a623b1b2
LinaKind <l.kinduryte@elifesciences.org>
2024-07-15 11:30:01 +0100

    Write a test list for a type intended for commands properties #3375

---
+++ test/write-side/commands/unsafe-annotation-content.test.ts
@@ -0,0 +1,13 @@
+describe('unsafe-annotation-content', () => {
+  describe('when decoding an empty string', () => {
+    it.todo('fails');
+  });
+
+  describe('when decoding a string that is too long', () => {
+    it.todo('fails');
+  });
+
+  describe('when decoding a valid annotation', () => {
+    it.todo('succeeds');
+  });
+});
