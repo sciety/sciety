92452d128b38a7b794cac1c371365c96ecb0ae71
David Moulton <d.moulton@elifesciences.org>
2024-07-17 14:20:12 +0100

    Revert "Make list.remove resource action always raise an event #3375"
    This reverts commit 72281584bb5d9d7dcb116023ef5272bf5e7aeebd.

--- src/write-side/resources/list/remove-article.ts
+++ src/write-side/resources/list/remove-article.ts
@@ -1,14 +1,28 @@
 import * as E from 'fp-ts/Either';
+import * as RA from 'fp-ts/ReadonlyArray';
+import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
+import { getListWriteModel } from './get-list-write-model';
+import { ListWriteModel } from './list-write-model';
 import { constructEvent } from '../../../domain-events';
 import { ArticleId } from '../../../types/article-id';
 import { RemoveArticleFromListCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
 
-export const removeArticle: ResourceAction<RemoveArticleFromListCommand> = (command) => () => pipe(
-  [constructEvent('ArticleRemovedFromList')(({
-    articleId: new ArticleId(command.articleId),
-    listId: command.listId,
-  }))],
-  E.right,
+const createAppropriateEvents = (command: RemoveArticleFromListCommand) => (listResource: ListWriteModel) => pipe(
+  listResource.articles,
+  RA.some((article) => article.articleId.value === command.articleId),
+  B.fold(
+    () => [],
+    () => [constructEvent('ArticleRemovedFromList')({
+      articleId: new ArticleId(command.articleId),
+      listId: command.listId,
+    })],
+  ),
+);
+
+export const removeArticle: ResourceAction<RemoveArticleFromListCommand> = (command) => (events) => pipe(
+  events,
+  getListWriteModel(command.listId),
+  E.map(createAppropriateEvents(command)),
 );
--- test/write-side/resources/list/remove-article.test.ts
+++ test/write-side/resources/list/remove-article.test.ts
@@ -1,18 +1,26 @@
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
+import { constructEvent } from '../../../../src/domain-events';
 import { ArticleId } from '../../../../src/types/article-id';
 import { removeArticle } from '../../../../src/write-side/resources/list/remove-article';
+import { arbitraryString } from '../../../helpers';
 import { shouldNotBeCalled } from '../../../should-not-be-called';
 import { arbitraryExpressionDoi } from '../../../types/expression-doi.helper';
 import { arbitraryListId } from '../../../types/list-id.helper';
+import { arbitraryListOwnerId } from '../../../types/list-owner-id.helper';
 
 describe('remove-article', () => {
   const listId = arbitraryListId();
   const expressionDoi = arbitraryExpressionDoi();
 
-  describe('given a command', () => {
+  describe('the article is in the list', () => {
     const result = pipe(
-      [],
+      [
+        constructEvent('ListCreated')({
+          listId, name: arbitraryString(), description: arbitraryString(), ownerId: arbitraryListOwnerId(),
+        }),
+        constructEvent('ArticleAddedToList')({ articleId: new ArticleId(expressionDoi), listId }),
+      ],
       removeArticle({
         listId,
         articleId: expressionDoi,
@@ -24,11 +32,30 @@ describe('remove-article', () => {
       expect(result).toHaveLength(1);
     });
 
-    it('returns an ArticleRemovedFromList event', () => {
+    it('succeeds and raises an event', () => {
       expect(result[0]).toBeDomainEvent('ArticleRemovedFromList', {
         articleId: new ArticleId(expressionDoi),
         listId,
       });
+    });
+  });
+
+  describe('the article is not in the list', () => {
+    const result = pipe(
+      [
+        constructEvent('ListCreated')({
+          listId, name: arbitraryString(), description: arbitraryString(), ownerId: arbitraryListOwnerId(),
+        }),
+      ],
+      removeArticle({
+        listId,
+        articleId: expressionDoi,
+      }),
+      E.getOrElseW(shouldNotBeCalled),
+    );
+
+    it('succeeds and raises no events', () => {
+      expect(result).toHaveLength(0);
     });
   });
 });
