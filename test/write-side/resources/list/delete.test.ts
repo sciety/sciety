23e067e0f2b4449189358072b0952c2303cc25fe
David Moulton <d.moulton@elifesciences.org>
2024-07-17 15:09:55 +0100

    Revert "Continue to remove duplicated read-side behaviour from one more resource action #3375"
    This reverts commit 75ee429641db11d172aad4bf68fc96cdc4d993d4.

--- test/write-side/resources/list/delete.test.ts
+++ test/write-side/resources/list/delete.test.ts
@@ -1,11 +1,13 @@
 import * as E from 'fp-ts/Either';
-import { pipe } from 'fp-ts/function';
+import { identity, pipe } from 'fp-ts/function';
+import { constructEvent } from '../../../../src/domain-events';
 import { deleteList } from '../../../../src/write-side/resources/list';
 import { arbitraryListCreatedEvent } from '../../../domain-events/list-resource-events.helper';
 import { shouldNotBeCalled } from '../../../should-not-be-called';
+import { arbitraryListId } from '../../../types/list-id.helper';
 
 describe('delete', () => {
-  describe('given a command', () => {
+  describe('when the list identified in the command exists', () => {
     const listCreatedEvent = arbitraryListCreatedEvent();
     const result = pipe(
       [
@@ -23,6 +25,41 @@ describe('delete', () => {
       expect(result[0]).toBeDomainEvent('ListDeleted', {
         listId: listCreatedEvent.listId,
       });
+    });
+  });
+
+  describe('no list with the given id ever existed', () => {
+    const result = pipe(
+      [],
+      deleteList({ listId: arbitraryListId() }),
+      E.matchW(
+        identity,
+        shouldNotBeCalled,
+      ),
+    );
+
+    it('fails with not-found', () => {
+      expect(result).toBe('not-found');
+    });
+  });
+
+  describe('when the list identified in the command existed but has been deleted', () => {
+    const listCreatedEvent = arbitraryListCreatedEvent();
+    const listDeleteEvent = constructEvent('ListDeleted')({ listId: listCreatedEvent.listId });
+    const result = pipe(
+      [
+        listCreatedEvent,
+        listDeleteEvent,
+      ],
+      deleteList({ listId: listCreatedEvent.listId }),
+      E.matchW(
+        identity,
+        shouldNotBeCalled,
+      ),
+    );
+
+    it('fails with not-found', () => {
+      expect(result).toBe('not-found');
     });
   });
 });
