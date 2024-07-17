0bfbd53c97ac0a1d824b781e99c43d60ecc68a90
Giorgio Sironi <g.sironi@elifesciences.org>
2024-07-17 14:57:26 +0100

    Revert "Continue making read model robust against nonsensical event sequences #3375"
    This reverts commit e52125168e5af18eba6d45c79d484b9670867b7c.

--- test/read-models/lists/handle-event.test.ts
+++ test/read-models/lists/handle-event.test.ts
@@ -1,36 +1,27 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { handleEvent, initialState } from '../../../src/read-models/lists/handle-event';
-import {
-  arbitraryArticleAddedToListEvent,
-  arbitraryArticleRemovedFromListEvent,
-  arbitraryListCreatedEvent,
-} from '../../domain-events/list-resource-events.helper';
+import { arbitraryArticleRemovedFromListEvent, arbitraryListCreatedEvent } from '../../domain-events/list-resource-events.helper';
 
 describe('handle-event', () => {
   describe('given an ArticleRemovedFromList event', () => {
     const listCreated = arbitraryListCreatedEvent();
-    const articleAdded = {
-      ...arbitraryArticleAddedToListEvent(),
-      listId: listCreated.listId,
-    };
-    const articleRemoved = {
+    const articleRemovedFromListEvent = {
       ...arbitraryArticleRemovedFromListEvent(),
       listId: listCreated.listId,
-      articleId: articleAdded.articleId,
     };
 
     describe.each([
       ['when the list does not exist', []],
       ['when the article has never been in the list', [listCreated]],
-      ['when the article was added and then removed from the list', [listCreated, articleAdded, articleRemoved]],
+      // ['when the article was added and then removed from the list', [listCreated, articleAdded, articleRemoved]],
     ])('%s', (_, events) => {
       const startingReadModelState = pipe(
         events,
         RA.reduce(initialState(), handleEvent),
       );
 
-      const readModel = handleEvent(startingReadModelState, articleRemoved);
+      const readModel = handleEvent(startingReadModelState, articleRemovedFromListEvent);
 
       it('does not change the read model state', () => {
         expect(readModel).toStrictEqual(startingReadModelState);
