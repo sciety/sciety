5ba8d70bf393912d808b05752985b7e9386cf145
David Moulton <d.moulton@elifesciences.org>
2024-07-17 14:47:50 +0100

    Revert "Reorder tests for readability #3375"
    This reverts commit c97b1c88bee64135090906edeb46fbf1b9947831.

--- test/read-models/lists/handle-event.test.ts
+++ test/read-models/lists/handle-event.test.ts
@@ -9,6 +9,68 @@ import {
 } from '../../domain-events/list-resource-events.helper';
 
 describe('handle-event', () => {
+  describe('given an ArticleRemovedFromList event', () => {
+    const listCreated = arbitraryListCreatedEvent();
+    const articleAdded = {
+      ...arbitraryArticleAddedToListEvent(),
+      listId: listCreated.listId,
+    };
+    const articleRemoved = {
+      ...arbitraryArticleRemovedFromListEvent(),
+      listId: listCreated.listId,
+      articleId: articleAdded.articleId,
+    };
+
+    describe.each([
+      ['when the list does not exist', []],
+      ['when the article has never been in the list', [listCreated]],
+      ['when the article was added and then removed from the list', [listCreated, articleAdded, articleRemoved]],
+    ])('%s', (_, events) => {
+      const readModel = pipe(
+        events,
+        RA.reduce(initialState(), handleEvent),
+      );
+
+      const snapshot = structuredClone(readModel);
+
+      beforeEach(() => {
+        handleEvent(readModel, articleRemoved);
+      });
+
+      it('does not change the read model state', () => {
+        expect(JSON.stringify(readModel)).toStrictEqual(JSON.stringify(snapshot));
+      });
+    });
+  });
+
+  describe('given an ArticleAddedToList event', () => {
+    const listCreated = arbitraryListCreatedEvent();
+    const articleAdded = {
+      ...arbitraryArticleAddedToListEvent(),
+      listId: listCreated.listId,
+    };
+
+    describe.each([
+      ['when the list does not exist', []],
+      ['when the article is already in the list', [listCreated, articleAdded]],
+    ])('%s', (_, events) => {
+      const readModel = pipe(
+        events,
+        RA.reduce(initialState(), handleEvent),
+      );
+
+      const snapshot = structuredClone(readModel);
+
+      beforeEach(() => {
+        handleEvent(readModel, articleAdded);
+      });
+
+      it('does not change the read model state', () => {
+        expect(JSON.stringify(readModel)).toStrictEqual(JSON.stringify(snapshot));
+      });
+    });
+  });
+
   describe('given a ListCreated event', () => {
     const listCreated = arbitraryListCreatedEvent();
     const listDeleted = constructEvent('ListDeleted')({ listId: listCreated.listId });
@@ -51,68 +113,6 @@ describe('handle-event', () => {
 
       beforeEach(() => {
         handleEvent(readModel, listDeleted);
-      });
-
-      it('does not change the read model state', () => {
-        expect(JSON.stringify(readModel)).toStrictEqual(JSON.stringify(snapshot));
-      });
-    });
-  });
-
-  describe('given an ArticleAddedToList event', () => {
-    const listCreated = arbitraryListCreatedEvent();
-    const articleAdded = {
-      ...arbitraryArticleAddedToListEvent(),
-      listId: listCreated.listId,
-    };
-
-    describe.each([
-      ['when the list does not exist', []],
-      ['when the article is already in the list', [listCreated, articleAdded]],
-    ])('%s', (_, events) => {
-      const readModel = pipe(
-        events,
-        RA.reduce(initialState(), handleEvent),
-      );
-
-      const snapshot = structuredClone(readModel);
-
-      beforeEach(() => {
-        handleEvent(readModel, articleAdded);
-      });
-
-      it('does not change the read model state', () => {
-        expect(JSON.stringify(readModel)).toStrictEqual(JSON.stringify(snapshot));
-      });
-    });
-  });
-
-  describe('given an ArticleRemovedFromList event', () => {
-    const listCreated = arbitraryListCreatedEvent();
-    const articleAdded = {
-      ...arbitraryArticleAddedToListEvent(),
-      listId: listCreated.listId,
-    };
-    const articleRemoved = {
-      ...arbitraryArticleRemovedFromListEvent(),
-      listId: listCreated.listId,
-      articleId: articleAdded.articleId,
-    };
-
-    describe.each([
-      ['when the list does not exist', []],
-      ['when the article has never been in the list', [listCreated]],
-      ['when the article was added and then removed from the list', [listCreated, articleAdded, articleRemoved]],
-    ])('%s', (_, events) => {
-      const readModel = pipe(
-        events,
-        RA.reduce(initialState(), handleEvent),
-      );
-
-      const snapshot = structuredClone(readModel);
-
-      beforeEach(() => {
-        handleEvent(readModel, articleRemoved);
       });
 
       it('does not change the read model state', () => {
