0bcfd3970d2e6b3d8de138a3b5ce2d65309dbb1e
David Moulton <d.moulton@elifesciences.org>
2024-07-17 14:12:34 +0100

    Revert "Do not allow empty annotations #3375"
    This reverts commit 1aece008e3235fc0c16fcafbc5a25294b738ce19.

--- src/write-side/commands/unsafe-annotation-content.ts
+++ src/write-side/commands/unsafe-annotation-content.ts
@@ -1,11 +1,3 @@
-import * as t from 'io-ts';
+import { unsafeUserInputCodec } from '../../types/unsafe-user-input';
 
-type UnsafeAnnotationContentBrand = {
-  readonly UnsafeAnnotationContent: unique symbol,
-};
-
-export const unsafeAnnotationContentCodec = t.brand(
-  t.string,
-  (input): input is t.Branded<string, UnsafeAnnotationContentBrand> => input.length > 0,
-  'UnsafeAnnotationContent',
-);
+export const unsafeAnnotationContentCodec = unsafeUserInputCodec;
