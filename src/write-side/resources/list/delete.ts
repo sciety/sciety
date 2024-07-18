449775100efc63a27d958347d177dccd71621f12
Giorgio Sironi <g.sironi@elifesciences.org>
2024-07-18 09:38:18 +0100

    Extract a named function to better express intent #3375

--- src/write-side/resources/list/delete.ts
+++ src/write-side/resources/list/delete.ts
@@ -1,19 +1,32 @@
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
-import { constructEvent, filterByName, isEventOfType } from '../../../domain-events';
+import {
+  DomainEvent, constructEvent, filterByName, isEventOfType,
+} from '../../../domain-events';
 import { toErrorMessage } from '../../../types/error-message';
+import { ListId } from '../../../types/list-id';
 import { DeleteListCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
 
-export const deleteList: ResourceAction<DeleteListCommand> = (command) => (events) => pipe(
+const doesListExist = (listId: ListId) => (events: ReadonlyArray<DomainEvent>) => pipe(
   events,
   filterByName(['ListCreated', 'ListDeleted']),
-  RA.filter((event) => event.listId === command.listId),
+  RA.filter((event) => event.listId === listId),
   RA.last,
   O.filter(isEventOfType('ListCreated')),
   O.match(
+    () => false,
+    () => true,
+  ),
+);
+
+export const deleteList: ResourceAction<DeleteListCommand> = (command) => (events) => pipe(
+  events,
+  doesListExist(command.listId),
+  B.fold(
     () => E.left(toErrorMessage('not-found')),
     () => E.right([constructEvent('ListDeleted')(command)]),
   ),
