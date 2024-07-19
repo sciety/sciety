023bf2e35cf9badb478c638d5c9027141d4f6664
David Moulton <d.moulton@elifesciences.org>
2024-07-19 11:35:52 +0100

    Rename articleId to expressionDoi in the remove article from list command #3395

--- src/write-side/commands/remove-article-from-list.ts
+++ src/write-side/commands/remove-article-from-list.ts
@@ -3,7 +3,7 @@ import { canonicalExpressionDoiCodec } from '../../types/expression-doi';
 import { listIdCodec } from '../../types/list-id';
 
 export const removeArticleFromListCommandCodec = t.strict({
-  articleId: canonicalExpressionDoiCodec,
+  expressionDoi: canonicalExpressionDoiCodec,
   listId: listIdCodec,
 });
 
