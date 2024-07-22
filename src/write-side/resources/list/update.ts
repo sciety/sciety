166312ac8cadc35daeb9be1215ea5146bca75629
David Moulton <d.moulton@elifesciences.org>
2024-07-22 10:53:33 +0100

    Remove superfluous code from a resource action #3375

--- src/write-side/resources/list/update.ts
+++ src/write-side/resources/list/update.ts
@@ -1,67 +1,32 @@
-import * as A from 'fp-ts/Array';
 import * as E from 'fp-ts/Either';
-import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { doesListExist } from './does-list-exist';
-import { ListWriteModel } from './list-write-model';
 import {
   constructEvent,
   isEventOfType,
   DomainEvent,
   filterByName,
 } from '../../../domain-events';
-import { eqArticleId } from '../../../types/article-id';
 import { ErrorMessage, toErrorMessage } from '../../../types/error-message';
 import { ListId } from '../../../types/list-id';
 import { EditListDetailsCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
 
-const filterToRelevantEventTypes = filterByName(['ListCreated', 'ArticleAddedToList', 'ArticleRemovedFromList', 'ListNameEdited', 'ListDescriptionEdited', 'ArticleInListAnnotated']);
+const filterToRelevantEventTypes = filterByName(['ListCreated', 'ListNameEdited', 'ListDescriptionEdited']);
 
 type RelevantEvent = ReturnType<typeof filterToRelevantEventTypes>[number];
+
+type ListWriteModel = {
+  name: string,
+  description: string,
+};
 
 const isAnEventOfThisList = (listId: ListId) => (event: RelevantEvent) => event.listId === listId;
 
 const updateListWriteModel = (resource: E.Either<ErrorMessage, ListWriteModel>, event: DomainEvent) => {
   if (isEventOfType('ListCreated')(event)) {
-    return E.right({ articles: [], name: event.name, description: event.description } satisfies ListWriteModel);
-  }
-  if (isEventOfType('ArticleAddedToList')(event)) {
-    pipe(
-      resource,
-      E.map((listResource) => {
-        listResource.articles.push({ articleId: event.articleId, annotated: false } satisfies ListWriteModel['articles'][number]);
-        return undefined;
-      }),
-    );
-  }
-  if (isEventOfType('ArticleInListAnnotated')(event)) {
-    pipe(
-      resource,
-      E.map((listResource) => {
-        pipe(
-          listResource.articles,
-          A.findFirst((article) => eqArticleId.equals(article.articleId, event.articleId)),
-          O.map((article) => {
-            // eslint-disable-next-line no-param-reassign
-            article.annotated = true;
-            return undefined;
-          }),
-        );
-        return undefined;
-      }),
-    );
-  }
-  if (isEventOfType('ArticleRemovedFromList')(event)) {
-    return pipe(
-      resource,
-      E.map((listResource) => pipe(
-        listResource.articles,
-        A.filter((article) => !eqArticleId.equals(article.articleId, event.articleId)),
-        (ids) => ({ ...listResource, articles: ids } satisfies ListWriteModel),
-      )),
-    );
+    return E.right({ name: event.name, description: event.description } satisfies ListWriteModel);
   }
   if (isEventOfType('ListNameEdited')(event)) {
     return pipe(
