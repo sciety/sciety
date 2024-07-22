c43e8779ed877f88af8d45647d7f40c7d92c2ef0
Giorgio Sironi <g.sironi@elifesciences.org>
2024-07-22 15:41:20 +0100

    Write a characterization test for an API query #3375

--- test/read-models/annotations/annotations-status.test.ts
+++ test/read-models/annotations/annotations-status.test.ts
@@ -1,5 +1,39 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { DomainEvent } from '../../../src/domain-events';
+import { annotationsStatus } from '../../../src/read-models/annotations/annotations-status';
+import { handleEvent, initialState } from '../../../src/read-models/annotations/handle-event';
+import { arbitraryArticleAddedToListEvent, arbitraryArticleInListAnnotatedEvent, arbitraryListCreatedEvent } from '../../domain-events/list-resource-events.helper';
+
+const runQuery = (events: ReadonlyArray<DomainEvent>) => {
+  const readmodel = pipe(
+    events,
+    RA.reduce(initialState(), handleEvent),
+  );
+  const status = annotationsStatus(readmodel)();
+  return status.total;
+};
+
 describe('annotations-status', () => {
   describe('when one article has been annotated on one list', () => {
-    it.todo('returns 1 as the total number of annotations');
+    const listCreated = arbitraryListCreatedEvent();
+    const articleAdded = {
+      ...arbitraryArticleAddedToListEvent(),
+      listId: listCreated.listId,
+    };
+    const articleAnnotated = {
+      ...arbitraryArticleInListAnnotatedEvent(),
+      listId: listCreated.listId,
+      articleId: articleAdded.articleId,
+    };
+    const events = [
+      listCreated,
+      articleAdded,
+      articleAnnotated,
+    ];
+
+    it('returns 1 as the total number of annotations', () => {
+      expect(runQuery(events)).toBe(1);
+    });
   });
 });
