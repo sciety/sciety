c3879ab7c033956aa4c7fcdaa67a40157434dad0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-07-22 10:35:38 +0100

    Inline deprecated functionality #3375

--- src/write-side/resources/list/update.ts
+++ src/write-side/resources/list/update.ts
@@ -1,11 +1,81 @@
+import * as A from 'fp-ts/Array';
 import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import { getListWriteModel } from './get-list-write-model';
 import { ListWriteModel } from './list-write-model';
-import { constructEvent } from '../../../domain-events';
+import {
+  constructEvent,
+  isEventOfType,
+  DomainEvent,
+  filterByName,
+} from '../../../domain-events';
+import { eqArticleId } from '../../../types/article-id';
+import { ErrorMessage, toErrorMessage } from '../../../types/error-message';
+import { ListId } from '../../../types/list-id';
 import { EditListDetailsCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
 
+const filterToEventsRelevantToWriteModel = filterByName(['ListCreated', 'ArticleAddedToList', 'ArticleRemovedFromList', 'ListNameEdited', 'ListDescriptionEdited', 'ArticleInListAnnotated']);
+
+type RelevantEvent = ReturnType<typeof filterToEventsRelevantToWriteModel>[number];
+
+const isAnEventOfThisList = (listId: ListId) => (event: RelevantEvent) => event.listId === listId;
+
+const updateListWriteModel = (resource: E.Either<ErrorMessage, ListWriteModel>, event: DomainEvent) => {
+  if (isEventOfType('ListCreated')(event)) {
+    return E.right({ articles: [], name: event.name, description: event.description } satisfies ListWriteModel);
+  }
+  if (isEventOfType('ArticleAddedToList')(event)) {
+    pipe(
+      resource,
+      E.map((listResource) => {
+        listResource.articles.push({ articleId: event.articleId, annotated: false } satisfies ListWriteModel['articles'][number]);
+        return undefined;
+      }),
+    );
+  }
+  if (isEventOfType('ArticleInListAnnotated')(event)) {
+    pipe(
+      resource,
+      E.map((listResource) => {
+        pipe(
+          listResource.articles,
+          A.findFirst((article) => eqArticleId.equals(article.articleId, event.articleId)),
+          O.map((article) => {
+            // eslint-disable-next-line no-param-reassign
+            article.annotated = true;
+            return undefined;
+          }),
+        );
+        return undefined;
+      }),
+    );
+  }
+  if (isEventOfType('ArticleRemovedFromList')(event)) {
+    return pipe(
+      resource,
+      E.map((listResource) => pipe(
+        listResource.articles,
+        A.filter((article) => !eqArticleId.equals(article.articleId, event.articleId)),
+        (ids) => ({ ...listResource, articles: ids } satisfies ListWriteModel),
+      )),
+    );
+  }
+  if (isEventOfType('ListNameEdited')(event)) {
+    return pipe(
+      resource,
+      E.map((listResource) => ({ ...listResource, name: event.name } satisfies ListWriteModel)),
+    );
+  }
+  if (isEventOfType('ListDescriptionEdited')(event)) {
+    return pipe(
+      resource,
+      E.map((listResource) => ({ ...listResource, description: event.description } satisfies ListWriteModel)),
+    );
+  }
+  return resource;
+};
 const handleEditingOfName = (listResource: ListWriteModel, command: EditListDetailsCommand) => (
   (listResource.name === command.name)
     ? []
@@ -20,7 +90,9 @@ const handleEditingOfDescription = (listResource: ListWriteModel, command: EditL
 
 export const update: ResourceAction<EditListDetailsCommand> = (command) => (events) => pipe(
   events,
-  getListWriteModel(command.listId),
+  filterToEventsRelevantToWriteModel,
+  RA.filter(isAnEventOfThisList(command.listId)),
+  RA.reduce(E.left(toErrorMessage('list-not-found')), updateListWriteModel),
   E.map((listResource) => [
     ...handleEditingOfName(listResource, command),
     ...handleEditingOfDescription(listResource, command),
