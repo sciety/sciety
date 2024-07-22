721dbc292cae90dfd5d7575d4a9caffd6c59047c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-07-22 15:27:16 +0100

    Narrow the return type in preparation for covering a query with tests #3375

--- src/read-models/annotations/annotations-status.ts
+++ src/read-models/annotations/annotations-status.ts
@@ -1,9 +1,8 @@
-import { Json } from 'fp-ts/Json';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { ReadModel } from './handle-event';
 
-export const annotationsStatus = (readModel: ReadModel) => (): Json => pipe(
+export const annotationsStatus = (readModel: ReadModel) => (): { total: number } => pipe(
   Object.values(readModel),
   RA.chain((listState) => Object.values(listState)),
   (annotations) => ({
