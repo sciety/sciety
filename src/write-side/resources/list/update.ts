3f564a8c2cb70580c61eecb9488e7d2ea4374821
David Moulton <d.moulton@elifesciences.org>
2024-07-22 09:52:23 +0100

    Follow conventions for resource action encapsulation #3375

--- src/write-side/resources/list/update.ts
+++ src/write-side/resources/list/update.ts
@@ -1,12 +1,28 @@
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
-import { executeCommand } from './execute-command';
 import { getListWriteModel } from './get-list-write-model';
+import { ListWriteModel } from './list-write-model';
+import { constructEvent } from '../../../domain-events';
 import { EditListDetailsCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
+
+const handleEditingOfName = (listResource: ListWriteModel, command: EditListDetailsCommand) => (
+  (listResource.name === command.name)
+    ? []
+    : [constructEvent('ListNameEdited')({ listId: command.listId, name: command.name })]
+);
+
+const handleEditingOfDescription = (listResource: ListWriteModel, command: EditListDetailsCommand) => (
+  (listResource.description === command.description)
+    ? []
+    : [constructEvent('ListDescriptionEdited')({ listId: command.listId, description: command.description })]
+);
 
 export const update: ResourceAction<EditListDetailsCommand> = (command) => (events) => pipe(
   events,
   getListWriteModel(command.listId),
-  E.map(executeCommand(command)),
+  E.map((listResource) => [
+    ...handleEditingOfName(listResource, command),
+    ...handleEditingOfDescription(listResource, command),
+  ]),
 );
