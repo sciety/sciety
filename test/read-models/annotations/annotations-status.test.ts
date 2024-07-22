4f45c7a76e99b6aa24f9d22b5f304d2cd1f05765
LinaKind <l.kinduryte@elifesciences.org>
2024-07-22 15:32:15 +0100

    Start minimal test coverage for a status query #3375

---
+++ test/read-models/annotations/annotations-status.test.ts
@@ -0,0 +1,5 @@
+describe('annotations-status', () => {
+  describe('when one article has been annotated on one list', () => {
+    it.todo('returns 1 as the total number of annotations');
+  });
+});
