e13ab60cd252d409d53c5be5260fd7d92970fa52
LinaKind <l.kinduryte@elifesciences.org>
2024-07-23 14:50:25 +0100

    Prefer sentence case #3402

--- src/read-side/html-pages/home-page/render-as-html/render-hero/render-homepage-hero-ctas.ts
+++ src/read-side/html-pages/home-page/render-as-html/render-hero/render-homepage-hero-ctas.ts
@@ -1,9 +1,9 @@
 import { HtmlFragment, toHtmlFragment } from '../../../../../types/html-fragment';
 import { explorePageHref } from '../../../../paths';
 
-const renderExploreReviewedPreprintsLink = () => `<a class="home-page-hero-explore-reviewed-preprints-link" href="${explorePageHref}">Explore Reviewed Preprints</a>`;
+const renderExploreReviewedPreprintsLink = () => `<a class="home-page-hero-explore-reviewed-preprints-link" href="${explorePageHref}">Explore reviewed preprints</a>`;
 
-const renderBrowseReviewingGroupsLink = () => '<a class="home-page-hero-browse-reviewing-groups-link" href="/groups">Browse Reviewing Groups</a>';
+const renderBrowseReviewingGroupsLink = () => '<a class="home-page-hero-browse-reviewing-groups-link" href="/groups">Browse reviewing groups</a>';
 
 export const renderHomepageHeroCtas = (): HtmlFragment => toHtmlFragment(`
   <div class="home-page-hero__ctas">
