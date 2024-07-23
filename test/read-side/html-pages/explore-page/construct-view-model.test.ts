d770d8e0f4effca611ed13c8823669b911d4f62d
LinaKind <l.kinduryte@elifesciences.org>
2024-07-23 15:25:23 +0100

    Write a characterisation test #3401

--- test/read-side/html-pages/explore-page/construct-view-model.test.ts
+++ test/read-side/html-pages/explore-page/construct-view-model.test.ts
@@ -1,9 +1,39 @@
+import * as O from 'fp-ts/Option';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { constructViewModel } from '../../../../src/read-side/html-pages/explore-page/construct-view-model/construct-view-model';
+import { ViewModel } from '../../../../src/read-side/html-pages/explore-page/view-model';
+import { TestFramework, createTestFramework } from '../../../framework';
+import { shouldNotBeCalled } from '../../../should-not-be-called';
+
 describe('construct-view-model', () => {
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
   describe('when there are categories', () => {
     it.todo('returns them in alphabetical order');
   });
 
   describe('when there are no categories available', () => {
-    it.todo('returns an empty array');
+    let categories: ViewModel['browseByCategory'];
+
+    beforeEach(async () => {
+      const dependencies = {
+        ...framework.dependenciesForViews,
+        fetchSearchCategories: () => TE.right([]),
+      };
+      categories = await pipe(
+        constructViewModel(dependencies),
+        TE.map((viewModel) => viewModel.browseByCategory),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+    });
+
+    it('returns an empty array', () => {
+      expect(categories).toStrictEqual(O.some([]));
+    });
   });
 });
