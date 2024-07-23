8bad12ace7a2c4f87613df0e313ef834cc2825d5
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-07-23 14:57:45 +0100

    Write a set of todo tests to cover view model construction #3401

---
+++ test/read-side/html-pages/explore-page/construct-view-model.test.ts
@@ -0,0 +1,9 @@
+describe('construct-view-model', () => {
+  describe('when there are categories', () => {
+    it.todo('returns them in alphabetical order');
+  });
+
+  describe('when there are no categories available', () => {
+    it.todo('returns an empty array');
+  });
+});
