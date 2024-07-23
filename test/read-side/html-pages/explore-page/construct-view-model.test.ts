c7b26078864dd4e99b9442496c721ae0e819c771
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-07-23 15:34:18 +0100

    Write another characterisation test #3401

--- test/read-side/html-pages/explore-page/construct-view-model.test.ts
+++ test/read-side/html-pages/explore-page/construct-view-model.test.ts
@@ -1,8 +1,9 @@
 import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { DependenciesForViews } from '../../../../src/read-side/dependencies-for-views';
 import { constructViewModel } from '../../../../src/read-side/html-pages/explore-page/construct-view-model/construct-view-model';
-import { ViewModel } from '../../../../src/read-side/html-pages/explore-page/view-model';
 import { TestFramework, createTestFramework } from '../../../framework';
 import { shouldNotBeCalled } from '../../../should-not-be-called';
 
@@ -13,23 +14,36 @@ describe('construct-view-model', () => {
     framework = createTestFramework();
   });
 
+  const getCategories = async (dependencies: DependenciesForViews) => pipe(
+    constructViewModel(dependencies),
+    TE.map((viewModel) => viewModel.browseByCategory),
+    TE.map(O.map(RA.map((category) => category.title))),
+    TE.getOrElse(shouldNotBeCalled),
+  )();
+
+  let categories: Awaited<ReturnType<typeof getCategories>>;
+
   describe('when there are categories', () => {
-    it.todo('returns them in alphabetical order');
+    beforeEach(async () => {
+      const dependencies = {
+        ...framework.dependenciesForViews,
+        fetchSearchCategories: () => TE.right(['B', 'C', 'A']),
+      };
+      categories = await getCategories(dependencies);
+    });
+
+    it('returns them in alphabetical order', () => {
+      expect(categories).toStrictEqual(O.some(['A', 'B', 'C']));
+    });
   });
 
   describe('when there are no categories available', () => {
-    let categories: ViewModel['browseByCategory'];
-
     beforeEach(async () => {
       const dependencies = {
         ...framework.dependenciesForViews,
         fetchSearchCategories: () => TE.right([]),
       };
-      categories = await pipe(
-        constructViewModel(dependencies),
-        TE.map((viewModel) => viewModel.browseByCategory),
-        TE.getOrElse(shouldNotBeCalled),
-      )();
+      categories = await getCategories(dependencies);
     });
 
     it('returns an empty array', () => {
