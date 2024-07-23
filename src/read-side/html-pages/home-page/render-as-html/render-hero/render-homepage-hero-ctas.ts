42ebcabb57315ae986a95917787002727d13821a
LinaKind <l.kinduryte@elifesciences.org>
2024-07-23 11:22:25 +0100

    Give exploring groups and reviewed preprints similar weight on home page #3402

--- src/read-side/html-pages/home-page/render-as-html/render-hero/render-homepage-hero-ctas.ts
+++ src/read-side/html-pages/home-page/render-as-html/render-hero/render-homepage-hero-ctas.ts
@@ -1,23 +1,12 @@
 import { HtmlFragment, toHtmlFragment } from '../../../../../types/html-fragment';
 
-const renderSearchForm = () => `
-  <form class="home-page-hero-search-form" action="/search" method="get">
-    <label for="searchText" class="home-page-hero-search-form__label">Start your search using keywords, author names or DOIs</label>
-    <div class="home-page-hero-search-form__visible_interactive_elements">
-      <input id="searchText" name="query" class="home-page-hero-search-form__text">
-      <button type="submit" class="home-page-hero-search-form__button">Search</button>
-    </div>
-    <button type="reset" class="visually-hidden">Reset</button>
-    <input type="hidden" name="evaluatedOnly" value="true">
-  </form>
-`;
+const renderExploreReviewedPreprintsLink = () => '<a class="home-page-hero-explore-reviewed-preprints-link" href="/search">Explore Reviewed Preprints</a>';
 
-const renderBrowseByCategoryLink = () => '<a class="home-page-hero-browse-by-category-link" href="/search">Browse by category</a>';
+const renderBrowseReviewingGroupsLink = () => '<a class="home-page-hero-browse-reviewing-groups-link" href="/groups">Browse Reviewing Groups</a>';
 
 export const renderHomepageHeroCtas = (): HtmlFragment => toHtmlFragment(`
   <div class="home-page-hero__ctas">
-    ${renderSearchForm()}
-    <span>or</span>
-    ${renderBrowseByCategoryLink()}
+    ${renderExploreReviewedPreprintsLink()}
+    ${renderBrowseReviewingGroupsLink()}
   </div>
 `);
