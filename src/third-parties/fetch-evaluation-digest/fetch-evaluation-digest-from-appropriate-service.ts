5686d271dd294aa7eeede05bab6bcba053c90c92
Giorgio Sironi <g.sironi@elifesciences.org>
2024-07-24 14:36:18 +0100

    Narrow down the digest host type #3408

--- src/third-parties/fetch-evaluation-digest/fetch-evaluation-digest-from-appropriate-service.ts
+++ src/third-parties/fetch-evaluation-digest/fetch-evaluation-digest-from-appropriate-service.ts
@@ -7,14 +7,16 @@ import * as EL from '../../types/evaluation-locator';
 import { EvaluationLocator } from '../../types/evaluation-locator';
 import { ExternalQueries } from '../external-queries';
 
+type DigestHost = 'zenodo' | 'access-microbiology' | 'hypothesis' | 'ncrc' | 'prelights' | 'rapidreviews';
+
 type DigestHostAndKey = {
-  host: string,
+  host: DigestHost,
   key: string,
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getEvaluationMachineReadableDigestHostAndKey = (evaluationLocator: EvaluationLocator): DigestHostAndKey => ({
-  host: 'foo',
+  host: 'access-microbiology',
   key: 'bar',
 });
 
