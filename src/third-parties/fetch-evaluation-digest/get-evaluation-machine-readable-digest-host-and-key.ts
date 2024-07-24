5b387fe6c6ccf30bba9b3ff390881b2951edc768
Giorgio Sironi <g.sironi@elifesciences.org>
2024-07-24 15:32:19 +0100

    Make one more test case pass by translating between service and expected digest host #3408

--- src/third-parties/fetch-evaluation-digest/get-evaluation-machine-readable-digest-host-and-key.ts
+++ src/third-parties/fetch-evaluation-digest/get-evaluation-machine-readable-digest-host-and-key.ts
@@ -2,18 +2,20 @@ import * as O from 'fp-ts/Option';
 import { EvaluationLocator } from '../../types/evaluation-locator';
 import * as EL from '../../types/evaluation-locator';
 
-type DigestHost = 'zenodo' | 'access-microbiology' | 'hypothesis' | 'ncrc' | 'prelights' | 'rapidreviews';
+type DigestHost = 'zenodo' | 'access-microbiology' | 'hypothesis' | 'ncrc' | 'prelights' | 'rapid-reviews';
 
 export type DigestHostAndKey = {
   host: DigestHost,
   key: string,
 };
 
-const extractHost = (evaluationLocator: EvaluationLocator) => {
+const extractHost = (evaluationLocator: EvaluationLocator): DigestHost => {
   const service = EL.service(evaluationLocator);
   switch (service) {
     case 'doi':
       return 'zenodo';
+    case 'rapidreviews':
+      return 'rapid-reviews';
     default:
       return service;
   }
