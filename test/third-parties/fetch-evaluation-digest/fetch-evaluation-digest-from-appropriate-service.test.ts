3d8dfb415d3f1c41b4fd03af5782dc807358586e
Giorgio Sironi <g.sironi@elifesciences.org>
2024-07-25 11:49:39 +0100

    Write a characterization test #3408

--- test/third-parties/fetch-evaluation-digest/fetch-evaluation-digest-from-appropriate-service.test.ts
+++ test/third-parties/fetch-evaluation-digest/fetch-evaluation-digest-from-appropriate-service.test.ts
@@ -1,22 +1,19 @@
 import * as E from 'fp-ts/Either';
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
-import { flow, identity, pipe } from 'fp-ts/function';
 import { fetchEvaluationDigestFromAppropriateService } from '../../../src/third-parties/fetch-evaluation-digest/fetch-evaluation-digest-from-appropriate-service';
 import * as DE from '../../../src/types/data-error';
 import { toEvaluationLocator } from '../../../src/types/evaluation-locator';
 import { arbitrarySanitisedHtmlFragment } from '../../helpers';
-import { shouldNotBeCalled } from '../../should-not-be-called';
-import { arbitraryEvaluationLocator } from '../../types/evaluation-locator.helper';
 
 describe('fetch-evaluation-digest-from-appropriate-service', () => {
+  let result: E.Either<unknown, unknown>;
+
   describe('when an evaluation digest fetcher for the inferred host is found', () => {
     const evaluationLocator = toEvaluationLocator('hypothesis:123');
     const digest = arbitrarySanitisedHtmlFragment();
     const fetchersByHost = {
       hypothesis: () => TE.right(digest),
     };
-    let result: E.Either<unknown, unknown>;
 
     beforeEach(async () => {
       result = await fetchEvaluationDigestFromAppropriateService(fetchersByHost)({})(evaluationLocator)();
@@ -27,24 +24,16 @@ describe('fetch-evaluation-digest-from-appropriate-service', () => {
     });
   });
 
-  describe('when the service is not configured', () => {
+  describe('when an evaluation digest fetcher for the inferred host cannot be found', () => {
+    // SMELL: we should be able to use `${arbitraryString()}:123` instead
+    const evaluationLocator = toEvaluationLocator('hypothesis:123');
+
+    beforeEach(async () => {
+      result = await fetchEvaluationDigestFromAppropriateService({})({})(evaluationLocator)();
+    });
+
     it('returns not found', async () => {
-      const fetchers = {};
-
-      const id = arbitraryEvaluationLocator();
-      const result = await pipe(
-        id,
-        fetchEvaluationDigestFromAppropriateService({})(fetchers),
-        T.map(flow(
-          E.matchW(
-            identity,
-            shouldNotBeCalled,
-          ),
-          DE.isNotFound,
-        )),
-      )();
-
-      expect(result).toBe(true);
+      expect(result).toStrictEqual(E.left(DE.notFound));
     });
   });
 });
