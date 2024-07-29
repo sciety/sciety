204b2bbfff1cdcc96d6b4870bc806ed49434a5ca
Giorgio Sironi <g.sironi@elifesciences.org>
2024-07-29 15:39:14 +0100

    Improve legibility of an implementation #3410

--- src/read-side/construct-evaluation-content-url.ts
+++ src/read-side/construct-evaluation-content-url.ts
@@ -1,9 +1,8 @@
 import { URL } from 'url';
 import * as EL from '../types/evaluation-locator';
 
-const webContentBase = new URL(process.env.APP_ORIGIN ?? 'https://sciety.org');
+const appOrigin = process.env.APP_ORIGIN ?? 'https://sciety.org';
 
 export const constructEvaluationContentUrl = (evaluationLocator: EL.EvaluationLocator): URL => new URL(
-  `./evaluations/${EL.serialize(evaluationLocator)}/content`,
-  webContentBase,
+  `${appOrigin}/evaluations/${EL.serialize(evaluationLocator)}/content`,
 );
