a2e0d8a21d87de0b39af9abbe62c79745dc77005
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-07-29 11:00:14 +0100

    Describe current behaviour with todo test list #3410

---
+++ test/read-side/non-html-views/docmaps/docmap/construct-view-model/construct-action.test.ts
@@ -0,0 +1,11 @@
+describe('construct-action', () => {
+  describe('when the human readable original url can be fetched', () => {
+    it.todo('the provided url is used as the sourceUrl');
+
+    it.todo('constructs webContentUrl by hardcoding sciety.org');
+  });
+
+  describe('when the human readable original url can not be fetched', () => {
+    it.todo('returns on the left');
+  });
+});
