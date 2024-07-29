d0a7eff61c428cdb43cb3d0b36935198ffd4ab3b
LinaKind <l.kinduryte@elifesciences.org>
2024-07-29 15:34:15 +0100

    Keep the evaluation web content url within the same environment that produces the docmap #3410

--- src/read-side/construct-evaluation-content-url.ts
+++ src/read-side/construct-evaluation-content-url.ts
@@ -1,7 +1,7 @@
 import { URL } from 'url';
 import * as EL from '../types/evaluation-locator';
 
-const webContentBase = new URL('https://sciety.org/');
+const webContentBase = new URL(process.env.APP_ORIGIN ?? 'https://sciety.org');
 
 export const constructEvaluationContentUrl = (evaluationLocator: EL.EvaluationLocator): URL => new URL(
   `./evaluations/${EL.serialize(evaluationLocator)}/content`,
