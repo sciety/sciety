2b9cd14d5c4a98400ac30667a9b8d6e393be6fcf
David Moulton <d.moulton@elifesciences.org>
2024-07-30 14:59:53 +0100

    Write a characterisation test #3410

--- feature-test/api/load-evaluation-content-non-html-view.test.ts
+++ feature-test/api/load-evaluation-content-non-html-view.test.ts
@@ -1,5 +1,16 @@
+import { goto, openBrowser } from 'taiko';
+import { screenshotTeardown } from '../utilities';
+
 describe('load-evaluation-content-non-html-view', () => {
-  describe('when an evaluation has been recorded', () => {
-    it.todo('its digest can be successfully loaded as an HTML fragment');
+  beforeEach(async () => {
+    await openBrowser();
+  });
+
+  afterEach(screenshotTeardown);
+
+  it('evaluation paths containing evaluation locators resolve correctly', async () => {
+    const response = await goto('http://localhost:8080/evaluations/doi:10.1101/123/content');
+
+    expect(response.status.code).toBe(200);
   });
 });
