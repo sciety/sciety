f5e0dd329407b48e285acde7d9f03c74e0c928f5
Giorgio Sironi <g.sironi@elifesciences.org>
2024-07-30 13:59:44 +0100

    Set an intention with a todo test #3410

---
+++ feature-test/api/load-evaluation-content-non-html-view.test.ts
@@ -0,0 +1,5 @@
+describe('load-evaluation-content-non-html-view', () => {
+  describe('when an evaluation has been recorded', () => {
+    it.todo('its digest can be successfully loaded as an HTML fragment');
+  });
+});
