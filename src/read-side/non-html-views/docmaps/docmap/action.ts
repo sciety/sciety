0c6db4c4b9c5e61ec550b442a28aacabe042b1e4
LinaKind <l.kinduryte@elifesciences.org>
2024-07-30 13:41:52 +0100

    Consistently deprecate properties that can be replaced by moving logic out of renderers #3410

--- src/read-side/non-html-views/docmaps/docmap/action.ts
+++ src/read-side/non-html-views/docmaps/docmap/action.ts
@@ -3,6 +3,7 @@ import * as EL from '../../../../types/evaluation-locator';
 
 export type Action = {
   webPageOriginalUrl: URL,
+  // @deprecated not part of an action property of a docmap, replace with a `webPageScietyUrl` on this type
   evaluationLocator: EL.EvaluationLocator,
   // @deprecated not part of an action property of a docmap, introduce a `created` property on the DocmapViewModel
   recordedAt: Date,
