6b95cd668ef6dcd28806849658dd24fe3a303e65
Giorgio Sironi <g.sironi@elifesciences.org>
2024-07-30 15:38:09 +0100

    Remove unused browser setup for one Taiko test #3410

--- feature-test/api/load-evaluation-content-non-html-view.test.ts
+++ feature-test/api/load-evaluation-content-non-html-view.test.ts
@@ -1,14 +1,6 @@
 import axios from 'axios';
-import { openBrowser } from 'taiko';
-import { screenshotTeardown } from '../utilities';
 
 describe('load-evaluation-content-non-html-view', () => {
-  beforeEach(async () => {
-    await openBrowser();
-  });
-
-  afterEach(screenshotTeardown);
-
   it('evaluation paths containing evaluation locators resolve correctly', async () => {
     const response = await axios.get('http://localhost:8080/evaluations/doi:10.1101/123/content');
 
