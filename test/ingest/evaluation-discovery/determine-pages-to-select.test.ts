96d1c5c52aeca8985b36067e22ae77e345844835
Giorgio Sironi <g.sironi@elifesciences.org>
2024-08-01 13:31:52 +0100

    Express intent with a failing test #3430

--- test/ingest/evaluation-discovery/determine-pages-to-select.test.ts
+++ test/ingest/evaluation-discovery/determine-pages-to-select.test.ts
@@ -1,6 +1,15 @@
+import * as TE from 'fp-ts/TaskEither';
+import { determinePagesToSelect } from '../../../src/ingest/evaluation-discovery/determine-pages-to-select';
+
+const fetchData = (stubbedResponse: unknown) => <D>() => TE.right(stubbedResponse as unknown as D);
+
 describe('determine-pages-to-select', () => {
   describe('when the total number of items is 0', () => {
-    it.todo('selects no pages');
+    const result = determinePagesToSelect({ fetchData: fetchData({ message: { 'total-results': 0 } }) });
+
+    it.failing('selects no pages', () => {
+      expect(result).toHaveLength(0);
+    });
   });
 
   describe('when the total number of items is less than the page size', () => {
