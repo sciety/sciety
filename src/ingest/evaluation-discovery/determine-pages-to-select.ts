f35415afb7416ba9557e82202aeb48009e7aea33
David Moulton <d.moulton@elifesciences.org>
2024-08-01 14:19:37 +0100

    Decode Crossref response to be able to use it #3430

--- src/ingest/evaluation-discovery/determine-pages-to-select.ts
+++ src/ingest/evaluation-discovery/determine-pages-to-select.ts
@@ -1,6 +1,23 @@
+import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
+import { formatValidationErrors } from 'io-ts-reporters';
 import { Dependencies } from '../discover-published-evaluations';
+
+const determinePagesToSelectCodec = t.strict({
+  message: t.strict({
+    'total-results': t.number,
+  }),
+});
+
+const toHumanFriendlyErrorMessage = (
+  errors: t.Errors,
+) => pipe(
+  errors,
+  formatValidationErrors,
+  (formattedErrors) => `acmi: could not decode crossref response to determine pages to select ${formattedErrors.join(', ')}`,
+);
 
 export type SelectedPage = { rows: number, offset: number };
 
@@ -9,6 +26,10 @@ export const determinePagesToSelect = (
 ): TE.TaskEither<string, ReadonlyArray<SelectedPage>> => pipe(
   'https://api.crossref.org/works?filter=prefix:10.1099,type:peer-review,relation.type:is-review-of',
   dependencies.fetchData,
+  TE.chainEitherK(flow(
+    determinePagesToSelectCodec.decode,
+    E.mapLeft(toHumanFriendlyErrorMessage),
+  )),
   TE.map(() => [
     { rows: 1000, offset: 0 },
     { rows: 1000, offset: 1000 },
