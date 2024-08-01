a3762e87607f082663ca019e2515373fcbaaaacd
LinaKind <l.kinduryte@elifesciences.org>
2024-08-01 15:32:12 +0100

    Increase confidence in handling of a failure #3430

--- test/ingest/evaluation-discovery/determine-pages-to-select.test.ts
+++ test/ingest/evaluation-discovery/determine-pages-to-select.test.ts
@@ -1,16 +1,18 @@
+import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { SelectedPage, determinePagesToSelect } from '../../../src/ingest/evaluation-discovery/determine-pages-to-select';
+import { arbitraryString } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 
 const fetchData = (stubbedResponse: unknown) => <D>() => TE.right(stubbedResponse as unknown as D);
 
 describe('determine-pages-to-select', () => {
-  let result: ReadonlyArray<SelectedPage>;
+  let selectedPages: ReadonlyArray<SelectedPage>;
 
   describe('when the total number of items is 0', () => {
     beforeEach(async () => {
-      result = await pipe(
+      selectedPages = await pipe(
         { fetchData: fetchData({ message: { 'total-results': 0 } }) },
         determinePagesToSelect(10),
         TE.getOrElse(shouldNotBeCalled),
@@ -18,13 +20,13 @@ describe('determine-pages-to-select', () => {
     });
 
     it('selects no pages', () => {
-      expect(result).toHaveLength(0);
+      expect(selectedPages).toHaveLength(0);
     });
   });
 
   describe('when the total number of items is less than the page size', () => {
     beforeEach(async () => {
-      result = await pipe(
+      selectedPages = await pipe(
         { fetchData: fetchData({ message: { 'total-results': 800 } }) },
         determinePagesToSelect(1000),
         TE.getOrElse(shouldNotBeCalled),
@@ -32,13 +34,13 @@ describe('determine-pages-to-select', () => {
     });
 
     it('selects one page of offset 0', () => {
-      expect(result).toStrictEqual([expect.objectContaining({ offset: 0 })]);
+      expect(selectedPages).toStrictEqual([expect.objectContaining({ offset: 0 })]);
     });
   });
 
   describe('when the total number of items is greater than the page size, but less than twice the page size', () => {
     beforeEach(async () => {
-      result = await pipe(
+      selectedPages = await pipe(
         { fetchData: fetchData({ message: { 'total-results': 1800 } }) },
         determinePagesToSelect(1000),
         TE.getOrElse(shouldNotBeCalled),
@@ -46,7 +48,7 @@ describe('determine-pages-to-select', () => {
     });
 
     it('selects two pages of offset 0 and 1000', () => {
-      expect(result).toStrictEqual([
+      expect(selectedPages).toStrictEqual([
         expect.objectContaining({ offset: 0 }),
         expect.objectContaining({ offset: 1000 }),
       ]);
@@ -54,6 +56,22 @@ describe('determine-pages-to-select', () => {
   });
 
   describe('when the API call fails', () => {
+    const failureMessage = arbitraryString();
+    let result: E.Either<unknown, unknown>;
+
+    beforeEach(async () => {
+      result = await pipe(
+        { fetchData: () => TE.left(failureMessage) },
+        determinePagesToSelect(1000),
+      )();
+    });
+
+    it('returns on the left', () => {
+      expect(result).toStrictEqual(E.left(failureMessage));
+    });
+  });
+
+  describe('when the API response cannot be understood', () => {
     it.todo('returns on the left');
   });
 });
