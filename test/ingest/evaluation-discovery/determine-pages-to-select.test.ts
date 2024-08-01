7208f531232b5b158f9b7ba9aee2b6de464a135b
LinaKind <l.kinduryte@elifesciences.org>
2024-08-01 13:51:59 +0100

    Make a test valid with respect to TaskEither usage #3430

--- test/ingest/evaluation-discovery/determine-pages-to-select.test.ts
+++ test/ingest/evaluation-discovery/determine-pages-to-select.test.ts
@@ -1,13 +1,22 @@
 import * as TE from 'fp-ts/TaskEither';
-import { determinePagesToSelect } from '../../../src/ingest/evaluation-discovery/determine-pages-to-select';
+import { pipe } from 'fp-ts/function';
+import { SelectedPage, determinePagesToSelect } from '../../../src/ingest/evaluation-discovery/determine-pages-to-select';
+import { shouldNotBeCalled } from '../../should-not-be-called';
 
 const fetchData = (stubbedResponse: unknown) => <D>() => TE.right(stubbedResponse as unknown as D);
 
 describe('determine-pages-to-select', () => {
-  describe('when the total number of items is 0', () => {
-    const result = determinePagesToSelect({ fetchData: fetchData({ message: { 'total-results': 0 } }) });
+  let result: ReadonlyArray<SelectedPage>;
 
-    it.skip('selects no pages', () => {
+  describe('when the total number of items is 0', () => {
+    beforeEach(async () => {
+      result = await pipe(
+        determinePagesToSelect({ fetchData: fetchData({ message: { 'total-results': 0 } }) }),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+    });
+
+    it.failing('selects no pages', () => {
       expect(result).toHaveLength(0);
     });
   });
@@ -18,5 +27,9 @@ describe('determine-pages-to-select', () => {
 
   describe('when the total number of items is greater than the page size, but less than twice the page size', () => {
     it.todo('selects two pages of offset 0 and 1000');
+  });
+
+  describe('when the API call fails', () => {
+    it.todo('returns on the left');
   });
 });
