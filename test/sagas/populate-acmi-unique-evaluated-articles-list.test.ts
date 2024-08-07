7af3a65d446e5d96fab046af88012ed449dd809c
David Moulton <d.moulton@elifesciences.org>
2024-08-07 14:27:37 +0100

    Descrbe intent with a test list #3443

---
+++ test/sagas/populate-acmi-unique-evaluated-articles-list.test.ts
@@ -0,0 +1,15 @@
+describe('populate-acmi-unique-evaluated-articles-list.test', () => {
+  describe('given an unlisted evaluated acmi article', () => {
+    describe('when there is no version of the article already in the list', () => {
+      it.todo('adds the article to the list');
+    });
+
+    describe('when there is already a version of the article in the list', () => {
+      it.todo('does nothing');
+    });
+  });
+
+  describe('given an unlisted evaluated article for any other group', () => {
+    it.todo('does nothing');
+  });
+});
