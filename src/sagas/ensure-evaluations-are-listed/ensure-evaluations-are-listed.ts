e4b7f5225edadd7276ed20f6fcbc87f69f0f2116
David Moulton <d.moulton@elifesciences.org>
2024-08-07 15:22:21 +0100

    Better describe the intended functionality by renaming and moving a file #3443

--- src/sagas/ensure-evaluations-are-listed/ensure-evaluations-are-listed.ts
+++ src/sagas/ensure-evaluations-are-listed/ensure-evaluations-are-listed.ts
@@ -2,6 +2,7 @@ import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { listAcmiArticle } from './list-acmi-article';
 import { Logger } from '../../logger';
 import { Queries } from '../../read-models';
 import { MissingArticle } from '../../read-models/evaluated-articles-lists/get-unlisted-evaluated-articles';
@@ -9,7 +10,6 @@ import * as EDOI from '../../types/expression-doi';
 import { DependenciesForCommands } from '../../write-side';
 import { executeResourceAction } from '../../write-side/resources/execute-resource-action';
 import * as list from '../../write-side/resources/list';
-import { listAcmiPapers } from '../list-acmi-papers';
 
 type Dependencies = Queries & DependenciesForCommands & {
   logger: Logger,
@@ -19,7 +19,7 @@ const listPapers = (dependencies: Dependencies) => (
   missingArticle: MissingArticle,
 ) => {
   if (missingArticle.listId === '53fd6f10-af16-4bf4-8473-707ca8daee97') {
-    return listAcmiPapers();
+    return listAcmiArticle();
   }
   return executeResourceAction(dependencies, list.addArticle)({
     listId: missingArticle.listId,
--- src/sagas/list-acmi-papers.ts
+++
@@ -1,5 +0,0 @@
-import * as TE from 'fp-ts/TaskEither';
-import { CommandResult } from '../types/command-result';
-import { ErrorMessage } from '../types/error-message';
-
-export const listAcmiPapers = (): TE.TaskEither<ErrorMessage, CommandResult> => TE.right('no-events-created');
