9c9ed853370ecf6a29a7c5a7089ee8d6cd077521
Giorgio Sironi <g.sironi@elifesciences.org>
2024-08-08 16:45:54 +0200

    Test assumptions by building state through events #3443

--- test/read-models/papers-evaluated-by-group/get-queue-of-evaluated-expressions.test.ts
+++ test/read-models/papers-evaluated-by-group/get-queue-of-evaluated-expressions.test.ts
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
-import { DomainEvent } from '../../../src/domain-events';
+import { DomainEvent, EventOfType } from '../../../src/domain-events';
+import { ArticleId } from '../../../src/types/article-id';
 import { arbitraryEvaluationPublicationRecordedEvent } from '../../domain-events/evaluation-resource-events.helper';
+import { arbitraryArticleAddedToListEvent } from '../../domain-events/list-resource-events.helper';
+import { arbitraryExpressionDoi } from '../../types/expression-doi.helper';
 
 type ReadModel = unknown;
 
@@ -30,6 +33,26 @@ describe('get-queue-of-evaluated-expressions', () => {
   });
 
   describe('when an evaluation publication has been recorded and the corresponding expression has been added to the list', () => {
-    it.todo('returns no elements in the queue');
+    const expressionDoi = arbitraryExpressionDoi();
+    const readModel = pipe(
+      [
+        {
+          ...arbitraryEvaluationPublicationRecordedEvent(),
+          articleId: expressionDoi,
+        } satisfies EventOfType<'EvaluationPublicationRecorded'>,
+        {
+          ...arbitraryArticleAddedToListEvent(),
+          articleId: new ArticleId(expressionDoi),
+        } satisfies EventOfType<'ArticleAddedToList'>,
+      ],
+      RA.reduce(initialState(), handleEvent),
+    );
+    const result = pipe(
+      getQueueOfEvaluatedExpressions(readModel)(),
+    );
+
+    it('returns no elements in the queue', () => {
+      expect(result).toHaveLength(0);
+    });
   });
 });
