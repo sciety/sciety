961035a8484168dad46d5436d17149cc3e48ca71
LinaKind <l.kinduryte@elifesciences.org>
2024-08-08 14:41:26 +0100

    Describe intent with a test list #3443

---
+++ test/read-models/papers-evaluated-by-group/lookup-papers-evaluated-by-group-list.test.ts
@@ -0,0 +1,9 @@
+describe('lookup-papers-evaluated-by-group-list', () => {
+  describe('when a group has evaluated multiple expressions of the same paper', () => {
+    it.todo('returns only one expression doi');
+  });
+
+  describe('when a group has evaluated different papers', () => {
+    it.todo('returns one expression per paper');
+  });
+});
