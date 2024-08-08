2a9d9a8a0b8bbccf5387b862c6d599c36faa0161
Giorgio Sironi <g.sironi@elifesciences.org>
2024-08-08 10:58:24 +0100

    Begin to write test setup #3443

--- test/sagas/ensure-evaluations-are-listed/list-acmi-article.test.ts
+++ test/sagas/ensure-evaluations-are-listed/list-acmi-article.test.ts
@@ -1,7 +1,34 @@
+import { listAcmiArticle } from '../../../src/sagas/ensure-evaluations-are-listed/list-acmi-article';
+import { TestFramework, createTestFramework } from '../../framework';
+import { arbitraryAddGroupCommand } from '../../write-side/commands/add-group-command.helper';
+import { arbitraryRecordEvaluationPublicationCommand } from '../../write-side/commands/record-evaluation-publication-command.helper';
+
 describe('list-acmi-article', () => {
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
+  const addGroupCommand = arbitraryAddGroupCommand();
+  const recordEvaluationPublicationCommand = {
+    ...arbitraryRecordEvaluationPublicationCommand(),
+    groupId: addGroupCommand.groupId,
+  };
+
   describe('given an unlisted evaluated acmi article', () => {
+    let result: unknown;
+
+    beforeEach(async () => {
+      await framework.commandHelpers.addGroup(addGroupCommand);
+      await framework.commandHelpers.recordEvaluationPublication(recordEvaluationPublicationCommand);
+      result = await listAcmiArticle()();
+    });
+
     describe('when there is no version of the article already in the list', () => {
-      it.todo('adds the article to the list');
+      it.failing('adds the article to the list', () => {
+        expect(result).toContain(recordEvaluationPublicationCommand.expressionDoi);
+      });
     });
 
     describe('when there is already a version of the article in the list', () => {
