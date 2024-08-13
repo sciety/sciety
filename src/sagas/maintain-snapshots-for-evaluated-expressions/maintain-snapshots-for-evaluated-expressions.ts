47931c0ddeda3432752276b9ade873d72defe0f3
LinaKind <l.kinduryte@elifesciences.org>
2024-08-13 14:56:07 +0100

    Implement a saga with a stubbed resource action #3455

--- src/sagas/maintain-snapshots-for-evaluated-expressions/maintain-snapshots-for-evaluated-expressions.ts
+++ src/sagas/maintain-snapshots-for-evaluated-expressions/maintain-snapshots-for-evaluated-expressions.ts
@@ -1,9 +1,25 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import { DependenciesForViews } from '../../read-side/dependencies-for-views';
+import * as PH from '../../types/publishing-history';
 import { DependenciesForCommands } from '../../write-side';
+import { executeResourceAction } from '../../write-side/resources/execute-resource-action';
+import * as paperSnapshot from '../../write-side/resources/paper-snapshot';
 
 type Dependencies = DependenciesForViews & DependenciesForCommands;
 
 export const maintainSnapshotsForEvaluatedExpressions = async (
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   dependencies: Dependencies,
-): Promise<void> => undefined;
+): Promise<void> => {
+  await pipe(
+    dependencies.getExpressionsWithNoAssociatedSnapshot(),
+    RA.head,
+    TE.fromOption(() => 'no evaluated expressions missing from snapshots'),
+    TE.chainW(dependencies.fetchPublishingHistory),
+    TE.map(PH.getAllExpressionDois),
+    TE.map((expressions) => executeResourceAction(dependencies, paperSnapshot.record)({
+      expressionDois: expressions,
+    })),
+  )();
+};
