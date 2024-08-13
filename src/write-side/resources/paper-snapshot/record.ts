c9d5f402847c5c9d3c0d3f55458cb431ced2f66e
David Moulton <d.moulton@elifesciences.org>
2024-08-13 15:04:47 +0100

    Naively implement record paper-snapshot resource action #3455

--- src/write-side/resources/paper-snapshot/record.ts
+++ src/write-side/resources/paper-snapshot/record.ts
@@ -1,7 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import { constructEvent } from '../../../domain-events';
 import { RecordPaperSnapshotCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
 
 // ts-unused-exports:disable-next-line
-export const record: ResourceAction<RecordPaperSnapshotCommand> = (command) => (events) => E.right([]);
+export const record: ResourceAction<RecordPaperSnapshotCommand> = (command) => (events) => pipe(
+  command,
+  constructEvent('PaperSnapshotRecorded'),
+  (event) => E.right([event]),
+);
