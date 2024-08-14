b9a76ec749e6091db27f7bd7e5a459a7f3b4e1c4
David Moulton <d.moulton@elifesciences.org>
2024-08-14 14:54:40 +0100

    Enable paper snapshots for Access Microbiology evaluated expressions on staging #3458

--- src/sagas/start-sagas.ts
+++ src/sagas/start-sagas.ts
@@ -1,8 +1,14 @@
 import { ensureEvaluationsAreListed } from './ensure-evaluations-are-listed';
+import {
+  maintainSnapshotsForEvaluatedExpressions,
+} from './maintain-snapshots-for-evaluated-expressions/maintain-snapshots-for-evaluated-expressions';
 import { CollectedPorts } from '../infrastructure';
 
 export const startSagas = (ports: CollectedPorts) => async (): Promise<void> => {
   ports.logger('info', 'Starting sagas');
   setInterval(async () => ensureEvaluationsAreListed(ports), 317 * 1000);
+  if (process.env.EXPERIMENT_ENABLED === 'true') {
+    setInterval(async () => maintainSnapshotsForEvaluatedExpressions(ports), 5 * 1000);
+  }
   ports.logger('info', 'Sagas started');
 };
