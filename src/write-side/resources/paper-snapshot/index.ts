5ef8fca7630bf19e2378de824f4e65980307cd97
David Moulton <d.moulton@elifesciences.org>
2024-08-16 11:41:12 +0100

    Remove superfluous lint disables #3458

--- src/write-side/resources/paper-snapshot/index.ts
+++ src/write-side/resources/paper-snapshot/index.ts
@@ -1,2 +1,1 @@
-// ts-unused-exports:disable-next-line
 export { record } from './record';
--- src/write-side/resources/paper-snapshot/record.ts
+++ src/write-side/resources/paper-snapshot/record.ts
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import { constructEvent } from '../../../domain-events';
 import { RecordPaperSnapshotCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
 
-// ts-unused-exports:disable-next-line
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const record: ResourceAction<RecordPaperSnapshotCommand> = (command) => (events) => pipe(
   command,
   constructEvent('PaperSnapshotRecorded'),
