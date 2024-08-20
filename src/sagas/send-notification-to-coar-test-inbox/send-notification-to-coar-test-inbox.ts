66a8962cf39ae38a08a4b343649724ac95e2ca18
Giorgio Sironi <g.sironi@elifesciences.org>
2024-08-20 15:10:30 +0100

    Avoid hardcoding notification ids #3472

--- src/sagas/send-notification-to-coar-test-inbox/send-notification-to-coar-test-inbox.ts
+++ src/sagas/send-notification-to-coar-test-inbox/send-notification-to-coar-test-inbox.ts
@@ -2,7 +2,7 @@ import { URL } from 'url';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import { v4 as uuidV4 } from 'uuid';
+import { v4 as uuidV4, v4 } from 'uuid';
 import * as paths from '../../read-side/paths';
 import { CoarNotificationModel } from '../../types/coar-notification-model';
 import { EvaluationLocator, toEvaluationLocator } from '../../types/evaluation-locator';
@@ -11,24 +11,20 @@ import * as EDOI from '../../types/expression-doi';
 import { DependenciesForSagas } from '../dependencies-for-sagas';
 
 type RecordedEvaluation = {
-  id: string,
   expressionDoi: ExpressionDoi,
   evaluationLocator: EvaluationLocator,
 };
 
 const hardcodedRecordedEvaluations = [
   {
-    id: 'urn:uuid:94ecae35-dcfd-4182-8550-22c7164fe23f',
     expressionDoi: EDOI.fromValidatedString('10.1101/2024.04.03.24305276'),
     evaluationLocator: toEvaluationLocator('doi:10.5281/zenodo.13274625'),
   },
   {
-    id: 'urn:uuid:bcebd78d-a869-4d4c-aaa5-eef703e9e583',
     expressionDoi: EDOI.fromValidatedString('10.1101/2024.04.03.24305276'),
     evaluationLocator: toEvaluationLocator('doi:10.5281/zenodo.12958884'),
   },
   {
-    id: 'urn:uuid:13ecd429-3611-4842-ad44-140195444152',
     expressionDoi: EDOI.fromValidatedString('10.1101/2024.05.07.592993'),
     evaluationLocator: toEvaluationLocator('doi:10.5281/zenodo.11644732'),
   },
@@ -37,7 +33,7 @@ const hardcodedRecordedEvaluations = [
 const constructCoarNotificationModel = (
   recordedEvaluation: RecordedEvaluation,
 ): CoarNotificationModel => ({
-  ...recordedEvaluation,
+  id: `urn:uuid:${v4()}`,
   objectId: new URL(`https://sciety.org${paths.constructPaperActivityPageFocusedOnEvaluationHref(recordedEvaluation.expressionDoi, recordedEvaluation.evaluationLocator)}`),
   contextId: new URL(`https://sciety.org${paths.constructPaperActivityPageHref(recordedEvaluation.expressionDoi)}`),
   contextCiteAs: new URL(`https://doi.org/${recordedEvaluation.expressionDoi}`),
