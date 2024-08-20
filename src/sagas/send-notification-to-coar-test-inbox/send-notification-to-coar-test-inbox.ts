32837d32b7753801561c7092f26680dbad8b7d4f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-08-20 14:03:42 +0100

    Increase amount of hardcoded data to let us differentiate between static and dynamic values #3472

--- src/sagas/send-notification-to-coar-test-inbox/send-notification-to-coar-test-inbox.ts
+++ src/sagas/send-notification-to-coar-test-inbox/send-notification-to-coar-test-inbox.ts
@@ -15,9 +15,17 @@ const hardcodedCoarNotificationModels = [
   },
   {
     id: 'urn:uuid:bcebd78d-a869-4d4c-aaa5-eef703e9e583',
-    objectId: new URL('https://sciety.org/articles/activity/10.1101/2024.04.03.24305276#doi:10.5281/zenodo.13274625'),
+    objectId: new URL('https://sciety.org/articles/activity/10.1101/2024.04.03.24305276#doi:10.5281/zenodo.12958884'),
     contextId: new URL('https://sciety.org/articles/activity/10.1101/2024.04.03.24305276'),
     contextCiteAs: new URL('https://doi.org/10.1101/2024.04.03.24305276'),
+    targetId: new URL('https://coar-notify-inbox.fly.dev'),
+    targetInbox: new URL('https://coar-notify-inbox.fly.dev/inbox'),
+  },
+  {
+    id: 'urn:uuid:13ecd429-3611-4842-ad44-140195444152',
+    objectId: new URL('https://sciety.org/articles/activity/10.1101/2024.05.07.592993#doi:10.5281/zenodo.11644732'),
+    contextId: new URL('https://sciety.org/articles/activity/10.1101/2024.05.07.592993'),
+    contextCiteAs: new URL('https://doi.org/10.1101/2024.05.07.592993'),
     targetId: new URL('https://coar-notify-inbox.fly.dev'),
     targetInbox: new URL('https://coar-notify-inbox.fly.dev/inbox'),
   },
