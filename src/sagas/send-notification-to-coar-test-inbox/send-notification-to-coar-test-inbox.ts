6e9483e57bc885313d767cf49dc774d7f0cf1da7
Giorgio Sironi <g.sironi@elifesciences.org>
2024-08-20 14:41:57 +0100

    Start deduplicating construction of URLs #3469

--- src/sagas/send-notification-to-coar-test-inbox/send-notification-to-coar-test-inbox.ts
+++ src/sagas/send-notification-to-coar-test-inbox/send-notification-to-coar-test-inbox.ts
@@ -4,13 +4,15 @@ import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { v4 as uuidV4 } from 'uuid';
 import { CoarNotificationModel } from '../../types/coar-notification-model';
+import { ExpressionDoi } from '../../types/expression-doi';
+import * as EDOI from '../../types/expression-doi';
 import { DependenciesForSagas } from '../dependencies-for-sagas';
 
 type RecordedEvaluation = {
   id: string,
   objectId: URL,
   contextId: URL,
-  contextCiteAs: URL,
+  expressionDoi: ExpressionDoi,
 };
 
 const hardcodedRecordedEvaluations = [
@@ -18,19 +20,19 @@ const hardcodedRecordedEvaluations = [
     id: 'urn:uuid:94ecae35-dcfd-4182-8550-22c7164fe23f',
     objectId: new URL('https://sciety.org/articles/activity/10.1101/2024.04.03.24305276#doi:10.5281/zenodo.13274625'),
     contextId: new URL('https://sciety.org/articles/activity/10.1101/2024.04.03.24305276'),
-    contextCiteAs: new URL('https://doi.org/10.1101/2024.04.03.24305276'),
+    expressionDoi: EDOI.fromValidatedString('10.1101/2024.04.03.24305276'),
   },
   {
     id: 'urn:uuid:bcebd78d-a869-4d4c-aaa5-eef703e9e583',
     objectId: new URL('https://sciety.org/articles/activity/10.1101/2024.04.03.24305276#doi:10.5281/zenodo.12958884'),
     contextId: new URL('https://sciety.org/articles/activity/10.1101/2024.04.03.24305276'),
-    contextCiteAs: new URL('https://doi.org/10.1101/2024.04.03.24305276'),
+    expressionDoi: EDOI.fromValidatedString('10.1101/2024.04.03.24305276'),
   },
   {
     id: 'urn:uuid:13ecd429-3611-4842-ad44-140195444152',
     objectId: new URL('https://sciety.org/articles/activity/10.1101/2024.05.07.592993#doi:10.5281/zenodo.11644732'),
     contextId: new URL('https://sciety.org/articles/activity/10.1101/2024.05.07.592993'),
-    contextCiteAs: new URL('https://doi.org/10.1101/2024.05.07.592993'),
+    expressionDoi: EDOI.fromValidatedString('10.1101/2024.05.07.592993'),
   },
 ] satisfies ReadonlyArray<RecordedEvaluation>;
 
@@ -38,6 +40,7 @@ const constructCoarNotificationModel = (
   recordedEvaluation: RecordedEvaluation,
 ): CoarNotificationModel => ({
   ...recordedEvaluation,
+  contextCiteAs: new URL(`https://doi.org/${recordedEvaluation.expressionDoi}`),
   targetId: new URL('https://coar-notify-inbox.fly.dev'),
   targetInbox: new URL('https://coar-notify-inbox.fly.dev/inbox'),
 });
