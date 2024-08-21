90d27fa786f27d1ac13dfb7a46ac2d93f5c3d5eb
LinaKind <l.kinduryte@elifesciences.org>
2024-08-21 15:27:01 +0100

    Add the first failing test for a saga's query #3475

--- test/read-models/evaluations-for-notifications/get-pending-evaluations.test.ts
+++ test/read-models/evaluations-for-notifications/get-pending-evaluations.test.ts
@@ -1,7 +1,38 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { DomainEvent } from '../../../src/domain-events';
+import { getPendingEvaluations, handleEvent, initialState } from '../../../src/read-models/evaluations-for-notifications/get-pending-evaluations';
+import { arbitraryEvaluationPublicationRecordedEvent } from '../../domain-events/evaluation-resource-events.helper';
+import { arbitraryGroupId } from '../../types/group-id.helper';
+
+const groupId = arbitraryGroupId();
+const consideredGroupIds = [groupId];
+
+const runQuery = (events: ReadonlyArray<DomainEvent>) => {
+  const readModel = pipe(
+    events,
+    RA.reduce(initialState(), handleEvent(consideredGroupIds)),
+  );
+  return getPendingEvaluations(readModel)();
+};
+
 describe('get-pending-evaluations', () => {
   describe('given activity by considered groups', () => {
     describe('when an evaluation publication has been recorded', () => {
-      it.todo('returns the evaluation');
+      const evaluationPublicationRecorded = {
+        ...arbitraryEvaluationPublicationRecordedEvent(),
+        groupId,
+      };
+      const events = [
+        evaluationPublicationRecorded,
+      ] satisfies ReadonlyArray<DomainEvent>;
+      const result = runQuery(events);
+
+      it.failing('returns the evaluation', () => {
+        expect(result).toHaveLength(1);
+        expect(result[0].evaluationLocator).toStrictEqual(evaluationPublicationRecorded.evaluationLocator);
+        expect(result[0].expressionDoi).toStrictEqual(evaluationPublicationRecorded.articleId);
+      });
     });
 
     describe('when two evaluation publications have been recorded', () => {
