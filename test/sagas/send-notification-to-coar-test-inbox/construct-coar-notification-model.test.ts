cbae68e848c46d32930a2395c275fc135b4bc0b1
LinaKind <l.kinduryte@elifesciences.org>
2024-08-21 09:59:49 +0100

    Add a characterisation test #3472

--- test/sagas/send-notification-to-coar-test-inbox/construct-coar-notification-model.test.ts
+++ test/sagas/send-notification-to-coar-test-inbox/construct-coar-notification-model.test.ts
@@ -1,7 +1,20 @@
+import { constructCoarNotificationModel } from '../../../src/sagas/send-notification-to-coar-test-inbox/construct-coar-notification-model';
+import { arbitraryEvaluationLocator } from '../../types/evaluation-locator.helper';
+import { arbitraryExpressionDoi } from '../../types/expression-doi.helper';
+
 describe('construct-coar-notification-model', () => {
   describe('given a pending evaluation', () => {
+    const evaluationLocator = arbitraryEvaluationLocator();
+    const expressionDoi = arbitraryExpressionDoi();
+    const coarNotification = constructCoarNotificationModel({
+      evaluationLocator,
+      expressionDoi,
+    });
+
     describe('constructs an objectId', () => {
-      it.todo('that contains the evaluation locator');
+      it('that is specific to the evaluation locator', () => {
+        expect(coarNotification.objectId.hash).toContain(evaluationLocator);
+      });
 
       it.todo('that contains the expression DOI');
 
