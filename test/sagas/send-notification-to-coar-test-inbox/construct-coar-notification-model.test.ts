58a9396f7bc0fa7f5602bbd86a753c55c945cd96
Giorgio Sironi <g.sironi@elifesciences.org>
2024-08-21 09:53:26 +0100

    Document the objectId of a COAR notification with a test list #3472

---
+++ test/sagas/send-notification-to-coar-test-inbox/construct-coar-notification-model.test.ts
@@ -0,0 +1,11 @@
+describe('construct-coar-notification-model', () => {
+  describe('given a pending evaluation', () => {
+    describe('constructs an objectId', () => {
+      it.todo('that contains the evaluation locator');
+
+      it.todo('that contains the expression DOI');
+
+      it.todo('that is a URL on https://sciety.org');
+    });
+  });
+});
