26f7c8bf487cb0601332e5983bb9fa855bc49dc9
Giorgio Sironi <g.sironi@elifesciences.org>
2024-08-21 15:15:45 +0100

    Describe intended query behavior with a test list #3475

---
+++ test/read-models/evaluations-for-notifications/get-pending-evaluations.test.ts
@@ -0,0 +1,19 @@
+describe('get-pending-evaluations', () => {
+  describe('given activity by considered groups', () => {
+    describe('when an evaluation publication has been recorded', () => {
+      it.todo('returns the evaluation');
+    });
+
+    describe('when two evaluation publications have been recorded', () => {
+      it.todo('returns the evaluations');
+    });
+
+    describe('when no evaluation publications have been recorded', () => {
+      it.todo('returns no evaluations');
+    });
+  });
+
+  describe('given activity by a group that is not considered', () => {
+    it.todo('returns no evaluations');
+  });
+});
