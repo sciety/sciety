4cad941a2bf81f0b3a5752fef6053a171c9490a5
Giorgio Sironi <g.sironi@elifesciences.org>
2024-08-21 15:36:42 +0100

    Extend ReadModel type to allow it to store information from EvaluationPublicationRecorded events #3475

--- src/read-models/evaluations-for-notifications/get-pending-evaluations.ts
+++ src/read-models/evaluations-for-notifications/get-pending-evaluations.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { DomainEvent } from '../../domain-events';
+import { DomainEvent, isEventOfType } from '../../domain-events';
 import { EvaluationLocator, toEvaluationLocator } from '../../types/evaluation-locator';
 import { ExpressionDoi } from '../../types/expression-doi';
 import * as EDOI from '../../types/expression-doi';
@@ -10,13 +10,21 @@ export type PendingEvaluation = {
   evaluationLocator: EvaluationLocator,
 };
 
-type ReadModel = unknown;
+type ReadModel = Array<PendingEvaluation>;
 
-export const initialState = (): ReadModel => ({});
+export const initialState = (): ReadModel => [];
 
 export const handleEvent = (
   consideredGroupIds: ReadonlyArray<GroupId>,
-) => (readModel: ReadModel, event: DomainEvent): ReadModel => readModel;
+) => (readModel: ReadModel, event: DomainEvent): ReadModel => {
+  if (isEventOfType('EvaluationPublicationRecorded')(event)) {
+    readModel.push({
+      expressionDoi: event.articleId,
+      evaluationLocator: event.evaluationLocator,
+    });
+  }
+  return readModel;
+};
 
 export const getPendingEvaluations = (readModel: ReadModel) => (): ReadonlyArray<PendingEvaluation> => [
   {
