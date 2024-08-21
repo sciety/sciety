d1448dd69676341a4e07f0efe98e7a1d030ed426
Giorgio Sironi <g.sironi@elifesciences.org>
2024-08-21 12:01:11 +0100

    Document in code a safer way of concatenating segments of a URL #3472

--- src/sagas/send-notification-to-coar-test-inbox/construct-coar-notification-model.ts
+++ src/sagas/send-notification-to-coar-test-inbox/construct-coar-notification-model.ts
@@ -5,11 +5,13 @@ import * as paths from '../../standards/paths';
 import { CoarNotificationModel } from '../../types/coar-notification-model';
 import { toDoiUrl } from '../../types/expression-doi';
 
+const scietyUiOrigin = new URL('https://sciety.org');
+
 export const constructCoarNotificationModel = (
   pendingEvaluation: PendingEvaluation,
 ): CoarNotificationModel => ({
   id: `urn:uuid:${v4()}`,
-  objectId: new URL(`https://sciety.org${paths.constructPaperActivityPageFocusedOnEvaluationHref(pendingEvaluation.expressionDoi, pendingEvaluation.evaluationLocator)}`),
+  objectId: new URL(`${scietyUiOrigin.origin}${paths.constructPaperActivityPageFocusedOnEvaluationHref(pendingEvaluation.expressionDoi, pendingEvaluation.evaluationLocator)}`),
   contextId: new URL(`https://sciety.org${paths.constructPaperActivityPageHref(pendingEvaluation.expressionDoi)}`),
   contextCiteAs: new URL(toDoiUrl(pendingEvaluation.expressionDoi)),
   targetId: new URL('https://coar-notify-inbox.fly.dev'),
