137b7d627b53cbc7da365c31b04cb87fcf38ec84
LinaKind <l.kinduryte@elifesciences.org>
2024-08-22 11:41:52 +0100

    Expose a bug #3475

--- test/read-models/evaluations-for-notifications/get-pending-evaluations.test.ts
+++ test/read-models/evaluations-for-notifications/get-pending-evaluations.test.ts
@@ -6,7 +6,8 @@ import { arbitraryEvaluationPublicationRecordedEvent } from '../../domain-events
 import { arbitraryGroupId } from '../../types/group-id.helper';
 
 const groupId = arbitraryGroupId();
-const consideredGroupIds = [groupId];
+const anotherGroupId = arbitraryGroupId();
+const consideredGroupIds = [groupId, anotherGroupId];
 
 const runQuery = (events: ReadonlyArray<DomainEvent>) => {
   const readModel = pipe(
@@ -44,7 +45,7 @@ describe('get-pending-evaluations', () => {
       });
     });
 
-    describe('when two evaluation publications have been recorded', () => {
+    describe('when two evaluation publications by the same group have been recorded', () => {
       const evaluationPublicationRecorded1 = {
         ...arbitraryEvaluationPublicationRecordedEvent(),
         groupId,
@@ -60,6 +61,30 @@ describe('get-pending-evaluations', () => {
       const result = runQuery(events);
 
       it('returns the evaluations', () => {
+        expect(result).toHaveLength(2);
+        expect(result[0].evaluationLocator).toStrictEqual(evaluationPublicationRecorded1.evaluationLocator);
+        expect(result[0].expressionDoi).toStrictEqual(evaluationPublicationRecorded1.articleId);
+        expect(result[1].evaluationLocator).toStrictEqual(evaluationPublicationRecorded2.evaluationLocator);
+        expect(result[1].expressionDoi).toStrictEqual(evaluationPublicationRecorded2.articleId);
+      });
+    });
+
+    describe('when two evaluation publications by two different groups have been recorded', () => {
+      const evaluationPublicationRecorded1 = {
+        ...arbitraryEvaluationPublicationRecordedEvent(),
+        groupId,
+      };
+      const evaluationPublicationRecorded2 = {
+        ...arbitraryEvaluationPublicationRecordedEvent(),
+        anotherGroupId,
+      };
+      const events = [
+        evaluationPublicationRecorded1,
+        evaluationPublicationRecorded2,
+      ] satisfies ReadonlyArray<DomainEvent>;
+      const result = runQuery(events);
+
+      it.failing('returns the evaluations', () => {
         expect(result).toHaveLength(2);
         expect(result[0].evaluationLocator).toStrictEqual(evaluationPublicationRecorded1.evaluationLocator);
         expect(result[0].expressionDoi).toStrictEqual(evaluationPublicationRecorded1.articleId);
