009aae7e1403017cba47607ccf14039be162f289
LinaKind <l.kinduryte@elifesciences.org>
2024-08-22 15:18:32 +0100

    Remove obsolete linting directive #3475

--- src/read-models/evaluations-for-notifications/handle-event.ts
+++ src/read-models/evaluations-for-notifications/handle-event.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DomainEvent, isEventOfType } from '../../domain-events';
 import { EvaluationLocator } from '../../types/evaluation-locator';
 import { ExpressionDoi } from '../../types/expression-doi';
