345e4e3cc9145e51bfc986dd0efae740bf95af88
LinaKind <l.kinduryte@elifesciences.org>
2024-08-26 13:59:50 +0100

    Prefer expressionDoiCodec to cannonicalExpressionDoiCodec #3481

--- src/write-side/commands/annotate-article-in-list.ts
+++ src/write-side/commands/annotate-article-in-list.ts
@@ -1,12 +1,12 @@
 import * as t from 'io-ts';
 import { inputFieldNames } from '../../standards';
-import { canonicalExpressionDoiCodec } from '../../types/expression-doi';
+import { expressionDoiCodec } from '../../types/expression-doi';
 import { listIdCodec } from '../../types/list-id';
 import { unsafeUserInputCodec } from '../../types/unsafe-user-input';
 
 export const annotateArticleInListCommandCodec = t.type({
   [inputFieldNames.annotationContent]: unsafeUserInputCodec,
-  [inputFieldNames.expressionDoi]: canonicalExpressionDoiCodec,
+  [inputFieldNames.expressionDoi]: expressionDoiCodec,
   [inputFieldNames.listId]: listIdCodec,
 }, 'annotateArticleInListCommandCodec');
 
