972ac622839d649f6cf508c823ff89a1784cbe22
LinaKind <l.kinduryte@elifesciences.org>
2024-08-28 15:40:00 +0100

    Introduce a test list for a new codec #3491

---
+++ test/types/url.test.ts
@@ -0,0 +1,13 @@
+describe('urlCodec', () => {
+  describe('given an URL object', () => {
+    it.todo('encodes to a string');
+  });
+
+  describe('given a string containing a valid url', () => {
+    it.todo('decodes to an URL object');
+  });
+
+  describe('given a string containing an invalid url', () => {
+    it.todo('fails to decode');
+  });
+});
