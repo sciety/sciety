cfbedbc5cdde64ef601fcf650006815aefea3cf9
LinaKind <l.kinduryte@elifesciences.org>
2024-08-29 13:37:21 +0100

    Revert premature change  "Express that target ids are not URLs as URL construction might modify the id #3491"
    This reverts commit b508b30967a9a2a73d3c1a509a2e339003ec9f96.

--- src/domain-events/coar-notification-delivered-event.ts
+++ src/domain-events/coar-notification-delivered-event.ts
@@ -2,13 +2,12 @@ import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
 import { EventIdFromString } from '../types/codecs/EventIdFromString';
 import { evaluationLocatorCodec } from '../types/evaluation-locator';
-
-const targetIdCodec = t.string;
+import { urlCodec } from '../types/url';
 
 export const coarNotificationDeliveredEventCodec = t.type({
   id: EventIdFromString,
   type: t.literal('CoarNotificationDelivered'),
   date: tt.DateFromISOString,
   evaluationLocator: evaluationLocatorCodec,
-  targetId: targetIdCodec,
+  targetId: urlCodec,
 });
