b508b30967a9a2a73d3c1a509a2e339003ec9f96
LinaKind <l.kinduryte@elifesciences.org>
2024-08-29 13:11:35 +0100

    Express that target ids are not URLs as URL construction might modify the id #3491

--- src/domain-events/coar-notification-delivered-event.ts
+++ src/domain-events/coar-notification-delivered-event.ts
@@ -2,12 +2,13 @@ import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
 import { EventIdFromString } from '../types/codecs/EventIdFromString';
 import { evaluationLocatorCodec } from '../types/evaluation-locator';
-import { urlCodec } from '../types/url';
+
+const targetIdCodec = t.string;
 
 export const coarNotificationDeliveredEventCodec = t.type({
   id: EventIdFromString,
   type: t.literal('CoarNotificationDelivered'),
   date: tt.DateFromISOString,
   evaluationLocator: evaluationLocatorCodec,
-  targetId: urlCodec,
+  targetId: targetIdCodec,
 });
