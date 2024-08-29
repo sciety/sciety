749522ab3f1e8fe870bd7dce66c7d06692a616a6
LinaKind <l.kinduryte@elifesciences.org>
2024-08-29 14:48:07 +0100

    Align event helper with the event type #3491

--- test/domain-events/arbitrary-coar-notification-delivered-event.helper.ts
+++ test/domain-events/arbitrary-coar-notification-delivered-event.helper.ts
@@ -1,8 +1,8 @@
 import { constructEvent, EventOfType } from '../../src/domain-events';
-import { arbitraryUrl } from '../helpers';
+import { arbitraryString } from '../helpers';
 import { arbitraryEvaluationLocator } from '../types/evaluation-locator.helper';
 
 export const arbitraryCoarNotificationDeliveredEvent = (): EventOfType<'CoarNotificationDelivered'> => constructEvent('CoarNotificationDelivered')({
   evaluationLocator: arbitraryEvaluationLocator(),
-  targetId: arbitraryUrl().href,
+  targetId: arbitraryString(),
 });
