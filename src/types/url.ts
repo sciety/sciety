ad9d6aba0a243450d5bd667370cebe588efe755b
LinaKind <l.kinduryte@elifesciences.org>
2024-08-29 11:30:36 +0100

    Ensure new codec is compatible with the existing event codecs #3491

--- src/types/url.ts
+++ src/types/url.ts
@@ -1,9 +1,14 @@
 import { URL } from 'url';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 
-export const urlCodec = new t.Type<URL, string, string>(
+export const urlCodec = new t.Type<URL, string, unknown>(
   'URLFromString',
   (value): value is URL => false,
-  (u) => t.success(new URL(u)),
+  (u, c) => pipe(
+    t.string.validate(u, c),
+    E.map((input) => new URL(input)),
+  ),
   (value) => value.href,
 );
