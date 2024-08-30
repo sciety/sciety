28587851f03d729b29ef25593f3007f998a8e0fd
Giorgio Sironi <g.sironi@elifesciences.org>
2024-08-30 11:41:22 +0100

    Describe intended behavior with a todo test list #3491

---
+++ test/write-side/resources/coar-notification/record-delivery.test.ts
@@ -0,0 +1,9 @@
+describe('record-delivery', () => {
+  describe('given a delivery that has not been recorded yet', () => {
+    it.todo('causes a state change in which the delivery has been recorded');
+  });
+
+  describe('given a delivery that has already been recorded', () => {
+    it.todo('accepts the command and causes no state change');
+  });
+});
