deff0e519ab61c681c444ea3ce711d631aedf46b
Giorgio Sironi <g.sironi@elifesciences.org>
2024-08-30 10:26:57 +0100

    Introduce a seam to be able to issue a command #3491

--- src/sagas/send-notifications-to-coar-inboxes/send-notifications-to-coar-inboxes.ts
+++ src/sagas/send-notifications-to-coar-inboxes/send-notifications-to-coar-inboxes.ts
@@ -1,9 +1,10 @@
 import { URL } from 'url';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe, flow } from 'fp-ts/function';
+import { pipe } from 'fp-ts/function';
 import { v4 as uuidV4 } from 'uuid';
 import { constructCoarNotificationModel } from './construct-coar-notification-model';
+import { CoarNotificationModel } from '../../types/coar-notification-model';
 import { DependenciesForSagas } from '../dependencies-for-sagas';
 
 type Dependencies = DependenciesForSagas;
@@ -15,13 +16,17 @@ export const sendNotificationsToCoarInboxes = async (
   const iterationId = uuidV4();
 
   dependencies.logger('debug', 'sendNotificationsToCoarInboxes starting', { iterationId });
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const issueCommand = (coarNotificationModel: CoarNotificationModel) => 'not-implemented-yet';
   await pipe(
     dependencies.getPendingNotifications(),
     RA.head,
     TE.fromOption(() => 'no pending notifications'),
     TE.map(constructCoarNotificationModel(scietyUiOrigin)),
-    TE.chainW(flow(
+    TE.chainW((coarNotificationModel) => pipe(
+      coarNotificationModel,
       dependencies.sendCoarNotification,
+      TE.map(() => issueCommand(coarNotificationModel)),
       TE.tapError(() => TE.right(dependencies.logger('error', 'sendNotificationsToCoarInboxes failed', { iterationId }))),
     )),
   )();
