0a823b696a9404c22e5ec44c867fbadbcaa0c3e4
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-02 10:04:30 +0100

    Write a failing test #3491

--- test/write-side/resources/coar-notification/record-delivery.test.ts
+++ test/write-side/resources/coar-notification/record-delivery.test.ts
@@ -1,6 +1,39 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import { DomainEvent } from '../../../../src/domain-events';
+import { RecordCoarNotificationDeliveryCommand } from '../../../../src/write-side/commands';
+import { recordDelivery } from '../../../../src/write-side/resources/coar-notification/record-delivery';
+import { arbitraryString } from '../../../helpers';
+import { shouldNotBeCalled } from '../../../should-not-be-called';
+import { arbitraryEvaluationLocator } from '../../../types/evaluation-locator.helper';
+
+const arbitraryRecordCoarNotificationDeliveryCommand = (): RecordCoarNotificationDeliveryCommand => ({
+  evaluationLocator: arbitraryEvaluationLocator(),
+  targetId: arbitraryString(),
+});
+
 describe('record-delivery', () => {
-  describe('given a delivery that has not been recorded yet', () => {
-    it.todo('causes a state change in which the delivery has been recorded');
+  const command = arbitraryRecordCoarNotificationDeliveryCommand();
+
+  describe.skip('given a delivery that has not been recorded yet', () => {
+    const events: ReadonlyArray<DomainEvent> = [];
+    let result: ReadonlyArray<DomainEvent>;
+
+    beforeEach(() => {
+      result = pipe(
+        events,
+        recordDelivery(command),
+        E.getOrElseW(shouldNotBeCalled),
+      );
+    });
+
+    it('causes a state change in which the delivery has been recorded', () => {
+      expect(result).toHaveLength(1);
+      expect(result[0]).toBeDomainEvent('CoarNotificationDelivered', {
+        evaluationLocator: command.evaluationLocator,
+        targetId: command.targetId,
+      });
+    });
   });
 
   describe('given a delivery that has already been recorded', () => {
