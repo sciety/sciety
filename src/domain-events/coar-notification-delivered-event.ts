615aa5ef3190be9ca6233506475199f6b2301239
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-02 14:44:03 +0100

    Prefer t.strict to avoid spurious fields being written to database #3491

--- src/domain-events/coar-notification-delivered-event.ts
+++ src/domain-events/coar-notification-delivered-event.ts
@@ -3,7 +3,7 @@ import * as tt from 'io-ts-types';
 import { EventIdFromString } from '../types/codecs/EventIdFromString';
 import { evaluationLocatorCodec } from '../types/evaluation-locator';
 
-export const coarNotificationDeliveredEventCodec = t.type({
+export const coarNotificationDeliveredEventCodec = t.strict({
   id: EventIdFromString,
   type: t.literal('CoarNotificationDelivered'),
   date: tt.DateFromISOString,
