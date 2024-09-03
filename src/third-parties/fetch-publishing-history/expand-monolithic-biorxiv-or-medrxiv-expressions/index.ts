cca24e59589ad8294c7159c864d52df176593bd9
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-03 15:20:32 +0100

    Improve legibility with a local type #3505

--- src/third-parties/fetch-publishing-history/expand-monolithic-biorxiv-or-medrxiv-expressions/index.ts
+++ src/third-parties/fetch-publishing-history/expand-monolithic-biorxiv-or-medrxiv-expressions/index.ts
@@ -1,11 +1,16 @@
+import * as TE from 'fp-ts/TaskEither';
 import { expandPaperExpressions } from './expand-paper-expressions';
 import { getPaperExpressionsFromBiorxiv } from './get-paper-expressions-from-biorxiv';
 import { Logger } from '../../../logger';
+import * as DE from '../../../types/data-error';
+import { PaperExpression } from '../../../types/paper-expression';
 import { QueryExternalService } from '../../query-external-service';
 
-export const expandMonolithicBiorxivOrMedrxivExpressions = (
-  queryExternalService: QueryExternalService,
-  logger: Logger,
-): ReturnType<typeof expandPaperExpressions> => expandPaperExpressions(
-  getPaperExpressionsFromBiorxiv({ queryExternalService, logger }),
-);
+type ExpandMonolithicBiorxivOrMedrvixExpressions = (queryExternalService: QueryExternalService, logger: Logger)
+=> (expressionsFromCrossref: ReadonlyArray<PaperExpression>)
+=> TE.TaskEither<DE.DataError, ReadonlyArray<PaperExpression>>;
+
+export const expandMonolithicBiorxivOrMedrxivExpressions: ExpandMonolithicBiorxivOrMedrvixExpressions = (
+  queryExternalService,
+  logger,
+) => expandPaperExpressions(getPaperExpressionsFromBiorxiv({ queryExternalService, logger }));
