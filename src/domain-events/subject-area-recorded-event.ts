fc0567f1d101e78bd02c7f2cf1387214471f1352
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-09-03 14:32:14 +0100

    Reduce the number of places in our code that connect to a legacy event #3505

--- src/domain-events/subject-area-recorded-event.ts
+++ src/domain-events/subject-area-recorded-event.ts
@@ -1,13 +1,16 @@
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
 import { articleIdCodec } from '../types/article-id';
+import { articleServerCodec } from '../types/article-server';
 import { EventIdFromString } from '../types/codecs/EventIdFromString';
-import { subjectAreaCodec } from '../types/subject-area';
 
 export const subjectAreaRecordedEventCodec = t.strict({
   id: EventIdFromString,
   type: t.literal('SubjectAreaRecorded'),
   date: tt.DateFromISOString,
   articleId: articleIdCodec,
-  subjectArea: subjectAreaCodec,
+  subjectArea: t.type({
+    value: t.string,
+    server: articleServerCodec,
+  }),
 });
--- src/types/subject-area.ts
+++
@@ -1,7 +0,0 @@
-import * as t from 'io-ts';
-import { articleServerCodec } from './article-server';
-
-export const subjectAreaCodec = t.type({
-  value: t.string,
-  server: articleServerCodec,
-});
