b6efe26b2b6c8271a2ed7acc3904ed1eeed15267
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-05 10:42:07 +0100

    Begin to define desired behavior with a test list #3514

---
+++ test/write-side/resources/paper-snapshot/record.test.ts
@@ -0,0 +1,11 @@
+describe('record', () => {
+  describe('when there are no paper snapshots', () => {
+    describe('when no expression dois are provided in the command', () => {
+      it.todo('rejects the command');
+    });
+
+    describe('when a set of expression dois is provided in the command', () => {
+      it.todo('causes a state change in which a paper snapshot is recorded');
+    });
+  });
+});
