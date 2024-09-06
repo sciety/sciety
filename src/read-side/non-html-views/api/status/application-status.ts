4f8911b31234ac8613e218b7963616cad21d20e3
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-06 08:53:50 +0100

    Render Sets by as an array of their values #3516
    Instead of a default object `{}` we can see the contents of a Set:
    ```
    $ curl -v "http://localhost:8080/api/status" | jq .readModels.papersEvaluatedByGroup.evaluatedExpressionsWithoutPaperSnapshot
    > GET /api/status HTTP/1.1
    ...
    < HTTP/1.1 200 OK
    < Content-Type: application/json; charset=utf-8
    < ...
    <
    {
      "4d6a8908-22a9-45c8-bd56-3c7140647709": []
    }
    ```

--- src/read-side/non-html-views/api/status/application-status.ts
+++ src/read-side/non-html-views/api/status/application-status.ts
@@ -5,9 +5,16 @@ import { DependenciesForViews } from '../../../dependencies-for-views';
 import { NonHtmlView } from '../../non-html-view';
 import { NonHtmlViewRepresentation, toNonHtmlViewRepresentation } from '../../non-html-view-representation';
 
+export const replacer = (_key: string, value: unknown): unknown => {
+  if (value instanceof Set) {
+    return Array.from(value.values());
+  }
+  return value;
+};
+
 const renderAsJson = (viewModel: unknown): NonHtmlViewRepresentation => pipe(
   viewModel,
-  JSON.stringify,
+  (object) => JSON.stringify(object, replacer),
   (representation) => toNonHtmlViewRepresentation(representation, 'application/json'),
 );
 
