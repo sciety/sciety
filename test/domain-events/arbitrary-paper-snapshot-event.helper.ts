eff7386cf8ee11e8bf2009c6437589468a96611c
David Moulton <d.moulton@elifesciences.org>
2024-09-06 15:35:06 +0100

    Randomise the number of expressions in PaperSnapshotRecorded events in tests #3514

--- test/domain-events/arbitrary-paper-snapshot-event.helper.ts
+++ test/domain-events/arbitrary-paper-snapshot-event.helper.ts
@@ -1,10 +1,13 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
 import { constructEvent, EventOfType } from '../../src/domain-events';
+import { arbitraryNumber } from '../helpers';
 import { arbitraryExpressionDoi } from '../types/expression-doi.helper';
 
-export const arbitraryPaperSnapshotRecordedEvent = (): EventOfType<'PaperSnapshotRecorded'> => constructEvent('PaperSnapshotRecorded')({
-  expressionDois: new Set([
-    arbitraryExpressionDoi(),
-    arbitraryExpressionDoi(),
-    arbitraryExpressionDoi(),
-  ]),
-});
+export const arbitraryPaperSnapshotRecordedEvent = (): EventOfType<'PaperSnapshotRecorded'> => pipe(
+  RA.replicate(arbitraryNumber(1, 10), undefined),
+  RA.map(() => arbitraryExpressionDoi()),
+  (expressionDois) => constructEvent('PaperSnapshotRecorded')({
+    expressionDois: new Set(expressionDois),
+  }),
+);
