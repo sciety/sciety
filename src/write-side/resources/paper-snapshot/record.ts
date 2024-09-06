678e35b36b1d9771954840e8e38f4ba66fe881b3
David Moulton <d.moulton@elifesciences.org>
2024-09-06 14:33:46 +0100

    Isolate a write side decision #3514

--- src/write-side/resources/paper-snapshot/record.ts
+++ src/write-side/resources/paper-snapshot/record.ts
@@ -1,24 +1,30 @@
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
-import { constructEvent } from '../../../domain-events';
+import { constructEvent, DomainEvent } from '../../../domain-events';
 import { toErrorMessage } from '../../../types/error-message';
 import { RecordPaperSnapshotCommand } from '../../commands';
 import { ResourceAction } from '../resource-action';
 
 const validateCommand = (command: RecordPaperSnapshotCommand) => command.expressionDois.size > 0;
 
-const causeStateChange = (command: RecordPaperSnapshotCommand) => [
+const changeState = (command: RecordPaperSnapshotCommand) => [
   constructEvent('PaperSnapshotRecorded')({ expressionDois: command.expressionDois }),
 ];
+
+const decideWhetherToChangeState = (
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+  events: ReadonlyArray<DomainEvent>,
+) => (
+  command: RecordPaperSnapshotCommand,
+) => changeState(command);
 
 export const record: ResourceAction<RecordPaperSnapshotCommand> = (
   command,
 ) => (
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
   events,
 ) => pipe(
   command,
   E.right,
   E.filterOrElse(validateCommand, () => toErrorMessage('')),
-  E.map(causeStateChange),
+  E.map(decideWhetherToChangeState(events)),
 );
