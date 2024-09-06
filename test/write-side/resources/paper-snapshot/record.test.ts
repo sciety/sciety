fec5fb1f4082534419dd2a0f007deefbce04e20f
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-06 14:13:30 +0100

    Write a failing test #3514

--- test/write-side/resources/paper-snapshot/record.test.ts
+++ test/write-side/resources/paper-snapshot/record.test.ts
@@ -3,6 +3,7 @@ import { pipe } from 'fp-ts/function';
 import { DomainEvent } from '../../../../src/domain-events';
 import { RecordPaperSnapshotCommand } from '../../../../src/write-side/commands';
 import { record } from '../../../../src/write-side/resources/paper-snapshot/record';
+import { arbitraryPaperSnapshotRecordedEvent } from '../../../domain-events/arbitrary-paper-snapshot-event.helper';
 import { shouldNotBeCalled } from '../../../should-not-be-called';
 import { arbitraryRecordPaperSnapshotCommand } from '../../commands/record-paper-snapshot-command.helper';
 
@@ -50,8 +51,28 @@ describe('record', () => {
   });
 
   describe('when there is a paper snapshot', () => {
+    const previousPaperSnapshotRecordedEvent = arbitraryPaperSnapshotRecordedEvent();
+    const events = [
+      previousPaperSnapshotRecordedEvent,
+    ];
+
     describe('when the same set of expression dois is provided in the command', () => {
-      it.todo('accepts the command and causes no state change');
+      const command: RecordPaperSnapshotCommand = {
+        expressionDois: previousPaperSnapshotRecordedEvent.expressionDois,
+      };
+      let result: ReadonlyArray<DomainEvent>;
+
+      beforeEach(() => {
+        result = pipe(
+          events,
+          record(command),
+          E.getOrElseW(shouldNotBeCalled),
+        );
+      });
+
+      it.failing('accepts the command and causes no state change', () => {
+        expect(result).toHaveLength(0);
+      });
     });
   });
 });
