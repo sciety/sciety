76baab7ad499d5399df25328bcbaec866bc5739e
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-06 13:51:59 +0100

    Make an arbitrary test helper more arbitrary #3514

--- test/write-side/commands/record-paper-snapshot-command.helper.ts
+++ test/write-side/commands/record-paper-snapshot-command.helper.ts
@@ -1,6 +1,13 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
 import { RecordPaperSnapshotCommand } from '../../../src/write-side/commands';
+import { arbitraryNumber } from '../../helpers';
 import { arbitraryExpressionDoi } from '../../types/expression-doi.helper';
 
-export const arbitraryRecordPaperSnapshotCommand = (): RecordPaperSnapshotCommand => ({
-  expressionDois: new Set([arbitraryExpressionDoi()]),
-});
+export const arbitraryRecordPaperSnapshotCommand = (): RecordPaperSnapshotCommand => pipe(
+  RA.replicate(arbitraryNumber(1, 10), undefined),
+  RA.map(() => arbitraryExpressionDoi()),
+  (expressionDois) => ({
+    expressionDois: new Set(expressionDois),
+  }),
+);
