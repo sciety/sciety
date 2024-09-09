3b7a17755726f914e469ff34bf926e2e6f33b95c
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-09 14:50:33 +0100

    Expose evaluated paper count via API #3514

--- src/read-models/papers-evaluated-by-group/papers-evaluated-by-group-status.ts
+++ src/read-models/papers-evaluated-by-group/papers-evaluated-by-group-status.ts
@@ -1,9 +1,13 @@
 import { Json } from 'fp-ts/Json';
+import * as R from 'fp-ts/Record';
 import { pipe } from 'fp-ts/function';
+import * as S from 'fp-ts/string';
 import { ReadModel } from './handle-event';
 
 export const papersEvaluatedByGroupStatus = (readmodel: ReadModel) => (): Json => pipe(
-  readmodel,
-  JSON.stringify,
-  JSON.parse,
-) as Json;
+  readmodel.paperSnapshotRepresentatives,
+  R.collect(S.Ord)((groupId, set) => ({
+    groupId,
+    total: set.size,
+  })),
+);
