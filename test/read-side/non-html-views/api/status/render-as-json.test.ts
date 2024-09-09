8bc434de06c06ecb35cb481f460daffb848a4e8b
LinaKind <l.kinduryte@elifesciences.org>
2024-09-09 15:44:44 +0100

    Add a test list #3514

---
+++ test/read-side/non-html-views/api/status/render-as-json.test.ts
@@ -0,0 +1,9 @@
+describe('render-as-json', () => {
+  describe('given an article id', () => {
+    it.todo('renders a string');
+  });
+
+  describe('given an URL', () => {
+    it.todo('renders a string');
+  });
+});
