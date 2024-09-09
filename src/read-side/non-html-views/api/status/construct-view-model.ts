cbc146079d270820c0d1e83c307f909bb9d988b0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-09-09 15:41:13 +0100

    Move the responsibility of extracting value from ArticleId to the renderer #3514

--- src/read-side/non-html-views/api/status/construct-view-model.ts
+++ src/read-side/non-html-views/api/status/construct-view-model.ts
@@ -1,9 +1,9 @@
-import { Json } from 'fp-ts/Json';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
+import { StatusData } from './render-as-json';
 import { DependenciesForViews } from '../../../dependencies-for-views';
 
-export const constructViewModel = (dependencies: DependenciesForViews): Json => ({
+export const constructViewModel = (dependencies: DependenciesForViews): StatusData => ({
   readModels: {
     annotations: dependencies.annotationsStatus(),
     evaluations: dependencies.evaluationsStatus(),
@@ -16,13 +16,7 @@ export const constructViewModel = (dependencies: DependenciesForViews): Json => 
     users: dependencies.usersStatus(),
   },
   sagaWorkQueues: {
-    ensureEvaluationsAreListed: pipe(
-      dependencies.getUnlistedEvaluatedArticles(),
-      RA.map((missingArticle) => ({
-        expressionDoi: missingArticle.expressionDoi.value,
-        listId: missingArticle.listId,
-      })),
-    ),
+    ensureEvaluationsAreListed: dependencies.getUnlistedEvaluatedArticles(),
     maintainSnapshotsForEvaluatedExpressions: dependencies.getExpressionsWithNoAssociatedSnapshot(),
     sendNotificationToCoarTestInbox: pipe(
       dependencies.getPendingNotifications(),
--- src/read-side/non-html-views/api/status/render-as-json.ts
+++ src/read-side/non-html-views/api/status/render-as-json.ts
@@ -1,4 +1,5 @@
 import { pipe } from 'fp-ts/function';
+import { ArticleId } from '../../../../types/article-id';
 import { NonHtmlViewRepresentation, toNonHtmlViewRepresentation } from '../../non-html-view-representation';
 
 type JsonRecord = {
@@ -8,10 +9,17 @@ type JsonRecord = {
 // eslint-disable-next-line @typescript-eslint/ban-types
 type JsonArray = {} & ReadonlyArray<StatusData>;
 
-type StatusData = boolean | number | string | null | JsonArray | JsonRecord;
+export type StatusData = boolean | number | string | null | JsonArray | JsonRecord | ArticleId;
+
+const replacer = (_key: string, value: unknown): unknown => {
+  if (value instanceof ArticleId) {
+    return value.value;
+  }
+  return value;
+};
 
 export const renderAsJson = (viewModel: StatusData): NonHtmlViewRepresentation => pipe(
   viewModel,
-  JSON.stringify,
+  (object) => JSON.stringify(object, replacer),
   (state) => toNonHtmlViewRepresentation(state, 'application/json'),
 );
