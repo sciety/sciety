7ff82cd26759a7ba6526f46a4f77a8e804fad040
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-10 09:29:28 +0100

    Add a regression test to cover deprecated ArticleId value #3514

--- test/read-side/non-html-views/api/status/render-as-json.test.ts
+++ test/read-side/non-html-views/api/status/render-as-json.test.ts
@@ -1,6 +1,14 @@
+import { renderAsJson } from '../../../../../src/read-side/non-html-views/api/status/render-as-json';
+import { ArticleId } from '../../../../../src/types/article-id';
+import { arbitraryExpressionDoi } from '../../../../types/expression-doi.helper';
+
 describe('render-as-json', () => {
   describe('given an article id', () => {
-    it.todo('renders a string');
+    const result = renderAsJson(new ArticleId(arbitraryExpressionDoi()));
+
+    it('renders a string', () => {
+      expect(typeof result.state).toBe('string');
+    });
   });
 
   describe('given an URL', () => {
