a75b6068566bb283562017b1767ca80d42fb51c0
LinaKind <l.kinduryte@elifesciences.org>
2024-09-10 11:45:49 +0100

    Add a basic test list #3521

---
+++ test/read-side/html-pages/group-page/group-home-page/construct-view-model/construct-sorted-feed.test.ts
@@ -0,0 +1,15 @@
+describe('construct-sorted-feed', () => {
+  describe('when the group has not evaluated any papers', () => {
+    it.todo('returns an empty array');
+  });
+
+  describe('when the group has evaluated one paper', () => {
+    it.todo('returns one expression doi');
+  });
+
+  describe('when the group has evaluated two papers', () => {
+    it.todo('returns the most recently evaluated first');
+
+    it.todo('returns the least recently evaluated second');
+  });
+});
