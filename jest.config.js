6fae1a6894ebab077a649f84d5a18c70a1112bbb
LinaKind <l.kinduryte@elifesciences.org>
2024-09-10 09:23:30 +0100

    Show correct line numbers in test failures #3514

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,12 @@ module.exports = {
   testEnvironment: 'node',
   testRunner: 'jest-circus/runner',
   transform: {
-    '^.+\\.ts$': 'esbuild-jest',
+    '^.+\\.ts$': [
+      'esbuild-jest',
+      {
+        sourcemap: true,
+      },
+    ],
   },
   transformIgnorePatterns: ['\\.pnp\\.[^\\\/]+$'],
   verbose: true,
