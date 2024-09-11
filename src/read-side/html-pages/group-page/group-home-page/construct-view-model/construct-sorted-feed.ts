d51ec9a1cec1c35bd3409dcf0938735004446e53
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-11 13:16:34 +0100

    Sort during construction of the view model #3521

--- src/read-side/html-pages/group-page/group-home-page/construct-view-model/construct-sorted-feed.ts
+++ src/read-side/html-pages/group-page/group-home-page/construct-view-model/construct-sorted-feed.ts
@@ -1,21 +1,29 @@
+import * as D from 'fp-ts/Date';
+import * as Ord from 'fp-ts/Ord';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { Dependencies } from './dependencies';
 import { ExpressionDoi } from '../../../../../types/expression-doi';
 import { GroupId } from '../../../../../types/group-id';
 
-type AugmentedWithSortProperty = ReadonlySet<{
+type EvaluatedPaper = {
   representative: ExpressionDoi,
   lastEvaluationByThisGroupPublishedAt: Date,
-}>;
+};
 
-const augmentWithSortProperty = (input: ReadonlySet<ExpressionDoi>): AugmentedWithSortProperty => pipe(
+const augmentWithSortProperty = (input: ReadonlySet<ExpressionDoi>): ReadonlySet<EvaluatedPaper> => pipe(
   Array.from(input),
   RA.map((expressionDoi) => ({
     representative: expressionDoi,
     lastEvaluationByThisGroupPublishedAt: new Date(),
   })),
   (arrayAugmentedWithSortProperty) => new Set(arrayAugmentedWithSortProperty),
+);
+
+const byLastEvaluationByThisGroupPublishedAt: Ord.Ord<EvaluatedPaper> = pipe(
+  D.Ord,
+  Ord.reverse,
+  Ord.contramap((entry) => entry.lastEvaluationByThisGroupPublishedAt),
 );
 
 export const constructSortedFeed = (
@@ -25,5 +33,6 @@ export const constructSortedFeed = (
   dependencies.getPapersEvaluatedByGroup(groupId),
   augmentWithSortProperty,
   (representatives) => Array.from(representatives),
+  RA.sort(byLastEvaluationByThisGroupPublishedAt),
   RA.map((representativeAugmentedWithSortProperty) => representativeAugmentedWithSortProperty.representative),
 );
