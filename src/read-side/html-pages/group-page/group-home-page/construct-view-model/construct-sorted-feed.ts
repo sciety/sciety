7a64ac34c822e6e307fe345fd806eac648b1484b
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-11 11:44:15 +0100

    Introduce a seam to be able to sort from the outside in #3521

--- src/read-side/html-pages/group-page/group-home-page/construct-view-model/construct-sorted-feed.ts
+++ src/read-side/html-pages/group-page/group-home-page/construct-view-model/construct-sorted-feed.ts
@@ -3,10 +3,13 @@ import { Dependencies } from './dependencies';
 import { ExpressionDoi } from '../../../../../types/expression-doi';
 import { GroupId } from '../../../../../types/group-id';
 
+const augmentWithSortProperty = (input: ReadonlySet<ExpressionDoi>) => input;
+
 export const constructSortedFeed = (
   dependencies: Dependencies,
   groupId: GroupId,
 ): ReadonlyArray<ExpressionDoi> => pipe(
   dependencies.getPapersEvaluatedByGroup(groupId),
+  augmentWithSortProperty,
   (representatives) => Array.from(representatives),
 );
