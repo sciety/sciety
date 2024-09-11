2b92b98abb4a80d1e3a85590631b867d1be8fa95
David Moulton <d.moulton@elifesciences.org>
2024-09-11 12:02:12 +0100

    Augment a query with hardcoded values to be able to sort within viewmodel construction #3521

--- src/read-side/html-pages/group-page/group-home-page/construct-view-model/construct-sorted-feed.ts
+++ src/read-side/html-pages/group-page/group-home-page/construct-view-model/construct-sorted-feed.ts
@@ -1,9 +1,22 @@
+import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { Dependencies } from './dependencies';
 import { ExpressionDoi } from '../../../../../types/expression-doi';
 import { GroupId } from '../../../../../types/group-id';
 
-const augmentWithSortProperty = (input: ReadonlySet<ExpressionDoi>) => input;
+type AugmentedWithSortProperty = ReadonlySet<{
+  representative: ExpressionDoi,
+  lastEvaluationByThisGroupPublishedAt: Date,
+}>;
+
+const augmentWithSortProperty = (input: ReadonlySet<ExpressionDoi>): AugmentedWithSortProperty => pipe(
+  Array.from(input),
+  RA.map((expressionDoi) => ({
+    representative: expressionDoi,
+    lastEvaluationByThisGroupPublishedAt: new Date(),
+  })),
+  (arrayAugmentedWithSortProperty) => new Set(arrayAugmentedWithSortProperty),
+);
 
 export const constructSortedFeed = (
   dependencies: Dependencies,
@@ -12,4 +25,5 @@ export const constructSortedFeed = (
   dependencies.getPapersEvaluatedByGroup(groupId),
   augmentWithSortProperty,
   (representatives) => Array.from(representatives),
+  RA.map((representativeAugmentedWithSortProperty) => representativeAugmentedWithSortProperty.representative),
 );
