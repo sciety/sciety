581683b1a60a8b406ecb58f31d36c18545ffb2b3
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-13 08:57:46 +0100

    Add tests for Option values #3531

--- test/read-side/non-html-views/api/status/render-as-json.test.ts
+++ test/read-side/non-html-views/api/status/render-as-json.test.ts
@@ -1,3 +1,4 @@
+import * as O from 'fp-ts/Option';
 import { renderAsJson } from '../../../../../src/read-side/non-html-views/api/status/render-as-json';
 import { ArticleId } from '../../../../../src/types/article-id';
 import { arbitraryUrl } from '../../../../helpers';
@@ -25,6 +26,30 @@ describe('render-as-json', () => {
 
     it('renders a string', () => {
       expect(typeof result.state).toBe('string');
+    });
+  });
+
+  describe('given an Option of a string which is a Some', () => {
+    const result = renderAsJson(O.some('foo'));
+
+    it('renders the string\'s value', () => {
+      expect(result.state).toBe('"foo"');
+    });
+  });
+
+  describe('given an Option of a Record which is a Some', () => {
+    const result = renderAsJson(O.some({ example: 42 }));
+
+    it('renders the Option\'s value', () => {
+      expect(result.state).toBe('{"example":42}');
+    });
+  });
+
+  describe('given an Option which is a None', () => {
+    const result = renderAsJson(O.none);
+
+    it('renders null', () => {
+      expect(result.state).toBe('null');
     });
   });
 });
