82b14a4c4782a96f14ddb714fb7dbf38d420b707
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-13 08:35:48 +0100

    Attempt reuse naively #3531
    ```
    src/read-side/non-html-views/api/groups/groups.ts:9:3 - error TS2345: Argument of type 'ViewModel' is not assignable to parameter of type 'StatusData'.
      Type 'readonly GroupStatus[]' is not assignable to type 'readonly StatusData[]'.
        Type 'GroupStatus' is not assignable to type 'StatusData'.
          Type 'GroupStatus' is not assignable to type 'StatusDataRecord'.
            Property 'largeLogoPath' is incompatible with index signature.
              Type 'Option<string>' is not assignable to type 'StatusData'.
                Type 'None' is not assignable to type 'StatusData'.
                  Type 'None' is not assignable to type 'StatusDataRecord'.
                    Index signature for type 'string' is missing in type 'None'.
    ```

--- src/read-side/non-html-views/api/groups/groups.ts
+++ src/read-side/non-html-views/api/groups/groups.ts
@@ -1,9 +1,9 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { constructViewModel } from './construct-view-model';
-import { renderAsJson } from './render-as-json';
 import { DependenciesForViews } from '../../../dependencies-for-views';
 import { NonHtmlView } from '../../non-html-view';
+import { renderAsJson } from '../status/render-as-json';
 
 export const groups = (dependencies: DependenciesForViews): NonHtmlView => () => pipe(
   constructViewModel(dependencies),
