a94c067d6f57b3b087608954772eb85b8386eadf
David Moulton <d.moulton@elifesciences.org>
2024-09-18 14:24:07 +0100

    Reuse the generic renderer #3521

--- src/read-side/non-html-views/api/evaluated-papers/evaluated-papers.ts
+++ src/read-side/non-html-views/api/evaluated-papers/evaluated-papers.ts
@@ -1,9 +1,7 @@
 import * as TE from 'fp-ts/TaskEither';
 import { DependenciesForViews } from '../../../dependencies-for-views';
 import { NonHtmlView } from '../../non-html-view';
+import { renderAsJson } from '../render-as-json';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const evaluatedPapers = (dependencies: DependenciesForViews): NonHtmlView => () => TE.right({
-  state: '',
-  mediaType: '',
-});
+export const evaluatedPapers = (dependencies: DependenciesForViews): NonHtmlView => () => TE.right(renderAsJson({ fakeField: 'fake value' }));
