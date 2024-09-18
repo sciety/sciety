147b0bb53653a79ab0cc6700d0a5362db5d880be
David Moulton <d.moulton@elifesciences.org>
2024-09-18 14:42:29 +0100

    Expose representatives of evaluated papers for observability #3521

--- src/read-side/non-html-views/api/evaluated-papers/construct-view-model.ts
+++ src/read-side/non-html-views/api/evaluated-papers/construct-view-model.ts
@@ -1,8 +1,15 @@
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { byLastEvaluatedAt, byRepresentative } from '../../../../read-models/papers-evaluated-by-group';
 import { GroupId } from '../../../../types/group-id';
 import { DependenciesForViews } from '../../../dependencies-for-views';
 import { ApiData } from '../render-as-json';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const constructViewModel = (dependencies: DependenciesForViews) => (groupId: GroupId): ApiData => ({
+export const constructViewModel = (dependencies: DependenciesForViews) => (groupId: GroupId): ApiData => pipe(
   groupId,
-});
+  dependencies.getPapersEvaluatedByGroup,
+  (evaluatedPapers) => Array.from(evaluatedPapers),
+  RA.sortBy([byLastEvaluatedAt, byRepresentative]),
+  RA.map((evaluatedPaper) => evaluatedPaper.representative),
+);
