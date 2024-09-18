6e4015c4d31c5a7756ddbb667defe18ad1eaebf9
LinaKind <l.kinduryte@elifesciences.org>
2024-09-18 14:34:39 +0100

    Provide a hardcoded group id to the new API #3521

--- src/read-side/non-html-views/api/evaluated-papers/construct-view-model.ts
+++ src/read-side/non-html-views/api/evaluated-papers/construct-view-model.ts
@@ -1,5 +1,8 @@
+import { GroupId } from '../../../../types/group-id';
 import { DependenciesForViews } from '../../../dependencies-for-views';
 import { ApiData } from '../render-as-json';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const constructViewModel = (dependencies: DependenciesForViews): ApiData => ({ fakeField: 'fake value' });
+export const constructViewModel = (dependencies: DependenciesForViews) => (groupId: GroupId): ApiData => ({
+  groupId,
+});
--- src/read-side/non-html-views/api/evaluated-papers/evaluated-papers.ts
+++ src/read-side/non-html-views/api/evaluated-papers/evaluated-papers.ts
@@ -1,11 +1,13 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { constructViewModel } from './construct-view-model';
+import * as GID from '../../../../types/group-id';
 import { DependenciesForViews } from '../../../dependencies-for-views';
 import { NonHtmlView } from '../../non-html-view';
 import { renderAsJson } from '../render-as-json';
 
 export const evaluatedPapers = (dependencies: DependenciesForViews): NonHtmlView => () => pipe(
+  GID.fromValidatedString('4d6a8908-22a9-45c8-bd56-3c7140647709'),
   constructViewModel(dependencies),
   renderAsJson,
   TE.right,
