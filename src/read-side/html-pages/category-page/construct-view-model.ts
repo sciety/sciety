246204327e8501c350c09cbf0ff56f1387ac50ac
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-09-30 11:46:19 +0100

    Display a genuine page count #3563

--- src/read-side/html-pages/category-page/construct-view-model.ts
+++ src/read-side/html-pages/category-page/construct-view-model.ts
@@ -6,28 +6,27 @@ import { ViewModel } from './view-model';
 import { ConstructViewModel } from '../construct-view-model';
 import { constructArticleCardStackWithSilentFailures } from '../shared-components/article-list';
 
-const constructPaginationControls = () => ({
+const constructPaginationControls = (totalItems: number): ViewModel['paginationControls'] => ({
   backwardPageHref: O.some('/backward-page-href'),
   forwardPageHref: O.some('/forward-page-href'),
   page: 1,
-  pageCount: 100,
+  pageCount: Math.ceil(totalItems / 10),
 });
 
 export const constructViewModel: ConstructViewModel<Params, ViewModel> = (dependencies) => (params) => pipe(
   dependencies.fetchByCategory(params.categoryName),
   TE.map((expressionDois) => ({
     expressionDois,
-    totalItems: 1000,
+    totalItems: 1234,
   })),
   TE.bindW('articleCardViewModels', ({ expressionDois }) => pipe(
     expressionDois,
     constructArticleCardStackWithSilentFailures(dependencies),
     TE.rightTask,
   )),
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   TE.map(({ articleCardViewModels, totalItems }) => ({
     pageHeading: `${params.categoryName}`,
     categoryContent: articleCardViewModels,
-    paginationControls: constructPaginationControls(),
+    paginationControls: constructPaginationControls(totalItems),
   })),
 );
