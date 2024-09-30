d8c52fd956ca02f89da209126c74e7d418b2c2ff
LinaKind <l.kinduryte@elifesciences.org>
2024-09-30 11:11:16 +0100

    Define a possible interface to expand the third party #3563

--- src/read-side/html-pages/category-page/construct-view-model.ts
+++ src/read-side/html-pages/category-page/construct-view-model.ts
@@ -1,4 +1,5 @@
 import * as O from 'fp-ts/Option';
+import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { Params } from './params';
@@ -15,10 +16,22 @@ const constructPaginationControls = () => ({
 
 export const constructViewModel: ConstructViewModel<Params, ViewModel> = (dependencies) => (params) => pipe(
   dependencies.fetchByCategory(params.categoryName),
-  TE.chainTaskK(constructArticleCardStackWithSilentFailures(dependencies)),
-  TE.map((articleCardViewModel) => ({
+  TE.map((expressionDois) => ({
+    expressionDois,
+    totalItems: 1000,
+  })),
+  TE.chainTaskK(({ expressionDois, totalItems }) => pipe(
+    expressionDois,
+    constructArticleCardStackWithSilentFailures(dependencies),
+    T.map((articleCardViewModels) => ({
+      articleCardViewModels,
+      totalItems,
+    })),
+  )),
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  TE.map(({ articleCardViewModels, totalItems }) => ({
     pageHeading: `${params.categoryName}`,
-    categoryContent: articleCardViewModel,
+    categoryContent: articleCardViewModels,
     paginationControls: constructPaginationControls(),
   })),
 );
