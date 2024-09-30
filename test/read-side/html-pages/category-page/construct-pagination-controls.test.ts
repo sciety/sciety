337c599cf7790477ba63525dafa06e590f31f01f
LinaKind <l.kinduryte@elifesciences.org>
2024-09-30 15:28:08 +0100

    Write a partial test list for custom pagination #3563

--- test/read-side/html-pages/category-page/construct-pagination-controls.test.ts
+++ test/read-side/html-pages/category-page/construct-pagination-controls.test.ts
@@ -1,3 +1,13 @@
 describe('construct-pagination-controls', () => {
-  it.todo('tbd');
+  describe('when two items are available', () => {
+    describe('and the page one is selected', () => {
+      it.todo('returns backwardPageHref as none');
+
+      it.todo('returns forwardPageHref as none');
+
+      it.todo('returns page as 1');
+
+      it.todo('returns pageCount as 1');
+    });
+  });
 });
