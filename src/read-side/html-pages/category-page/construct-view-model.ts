7e6c20d5bed6fd1f8b7e3104fd7b6bd7a69f6f04
LinaKind <l.kinduryte@elifesciences.org>
2024-09-30 14:54:07 +0100

    Implement a scalable query string construction #3563

--- src/read-side/html-pages/category-page/construct-view-model.ts
+++ src/read-side/html-pages/category-page/construct-view-model.ts
@@ -3,12 +3,13 @@ import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { Params } from './params';
 import { ViewModel } from './view-model';
+import { constructCategoryPageHref } from '../../../standards/paths';
 import { ConstructViewModel } from '../construct-view-model';
 import { constructArticleCardStackWithSilentFailures } from '../shared-components/article-list';
 
-const constructPaginationControls = (pageSize: number, totalItems: number): ViewModel['paginationControls'] => ({
+const constructPaginationControls = (pageSize: number, totalItems: number, categoryName: string): ViewModel['paginationControls'] => ({
   backwardPageHref: O.some('/backward-page-href'),
-  forwardPageHref: O.some('/forward-page-href'),
+  forwardPageHref: O.some(constructCategoryPageHref(categoryName, 2)),
   page: 1,
   pageCount: Math.ceil(totalItems / pageSize),
 });
@@ -23,6 +24,6 @@ export const constructViewModel: ConstructViewModel<Params, ViewModel> = (depend
   TE.map(({ articleCardViewModels, totalItems }) => ({
     pageHeading: `${params.categoryName}`,
     categoryContent: articleCardViewModels,
-    paginationControls: constructPaginationControls(10, totalItems),
+    paginationControls: constructPaginationControls(10, totalItems, params.categoryName),
   })),
 );
--- src/standards/paths/construct-category-page-href.ts
+++ src/standards/paths/construct-category-page-href.ts
@@ -1,5 +1,14 @@
+import { URLSearchParams } from 'url';
 import { queryStringParameters } from '..';
 
 export const categoryPagePathSpecification = '/category';
 
-export const constructCategoryPageHref = (categoryName: string, pageNumber?: number): string => `${categoryPagePathSpecification}?${queryStringParameters.categoryName}=${encodeURIComponent(categoryName)}${pageNumber !== undefined ? `&${queryStringParameters.page}=${pageNumber}` : ''}`;
+export const constructCategoryPageHref = (categoryName: string, pageNumber?: number): string => {
+  const queryString = new URLSearchParams({
+    [queryStringParameters.categoryName]: categoryName,
+  });
+  if (pageNumber !== undefined) {
+    queryString.append(queryStringParameters.page, pageNumber.toString());
+  }
+  return `${categoryPagePathSpecification}?${queryString.toString()}`;
+};
