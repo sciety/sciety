0512f90091789995b4a0d901cbc8293d28bd77e5
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-30 10:30:31 +0100

    Create a pull for pagination values #3563

--- src/read-side/html-pages/category-page/render-as-html/render-as-html.ts
+++ src/read-side/html-pages/category-page/render-as-html/render-as-html.ts
@@ -1,8 +1,17 @@
 import { htmlEscape } from 'escape-goat';
+import * as O from 'fp-ts/Option';
 import { renderCategoryContent } from './render-category-content';
 import { toHtmlFragment } from '../../../../types/html-fragment';
 import { HtmlPage, toHtmlPage } from '../../html-page';
+import { renderPaginationControls } from '../../shared-components/pagination';
 import { ViewModel } from '../view-model';
+
+const paginationControlsViewModel = {
+  backwardPageHref: O.some('/backward-page-href'),
+  forwardPageHref: O.some('/forward-page-href'),
+  page: 1,
+  pageCount: 100,
+};
 
 export const renderAsHtml = (viewModel: ViewModel): HtmlPage => toHtmlPage({
   title: viewModel.pageHeading,
@@ -13,6 +22,7 @@ export const renderAsHtml = (viewModel: ViewModel): HtmlPage => toHtmlPage({
     </header>
     <section>
       ${renderCategoryContent(viewModel.categoryContent)}
+      ${process.env.EXPERIMENT_ENABLED === 'true' ? renderPaginationControls(paginationControlsViewModel) : ''}
     </section>
   `,
   ),
