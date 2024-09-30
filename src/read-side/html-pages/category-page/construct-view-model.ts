98119600194994e17076e605ea880477f0f0e5a1
David Moulton <d.moulton@elifesciences.org>
2024-09-30 10:55:01 +0100

    Extract a local function to be able to provide custom pagination construction #3563

--- src/read-side/html-pages/category-page/construct-view-model.ts
+++ src/read-side/html-pages/category-page/construct-view-model.ts
@@ -6,17 +6,19 @@ import { ViewModel } from './view-model';
 import { ConstructViewModel } from '../construct-view-model';
 import { constructArticleCardStackWithSilentFailures } from '../shared-components/article-list';
 
+const constructPaginationControls = () => ({
+  backwardPageHref: O.some('/backward-page-href'),
+  forwardPageHref: O.some('/forward-page-href'),
+  page: 1,
+  pageCount: 100,
+});
+
 export const constructViewModel: ConstructViewModel<Params, ViewModel> = (dependencies) => (params) => pipe(
   dependencies.fetchByCategory(params.categoryName),
   TE.chainTaskK(constructArticleCardStackWithSilentFailures(dependencies)),
   TE.map((articleCardViewModel) => ({
     pageHeading: `${params.categoryName}`,
     categoryContent: articleCardViewModel,
-    paginationControls: {
-      backwardPageHref: O.some('/backward-page-href'),
-      forwardPageHref: O.some('/forward-page-href'),
-      page: 1,
-      pageCount: 100,
-    },
+    paginationControls: constructPaginationControls(),
   })),
 );
