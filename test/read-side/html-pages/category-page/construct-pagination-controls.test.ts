51ff5bf252a78e6c1d3eaffd939578bfd3b1898d
Giorgio Sironi <g.sironi@elifesciences.org>
2024-09-30 15:32:08 +0100

    Write a regression test #3563

--- test/read-side/html-pages/category-page/construct-pagination-controls.test.ts
+++ test/read-side/html-pages/category-page/construct-pagination-controls.test.ts
@@ -1,11 +1,23 @@
+import * as tt from 'io-ts-types';
+import { constructPaginationControls } from '../../../../src/read-side/html-pages/category-page/construct-pagination-controls';
+import { arbitraryString } from '../../../helpers';
+
 describe('construct-pagination-controls', () => {
   describe('when two items are available', () => {
     describe('and the page one is selected', () => {
+      const result = constructPaginationControls(
+        10,
+        { categoryName: arbitraryString() as tt.NonEmptyString, page: 1 },
+        2,
+      );
+
       it.todo('returns backwardPageHref as none');
 
       it.todo('returns forwardPageHref as none');
 
-      it.todo('returns page as 1');
+      it('returns page as 1', () => {
+        expect(result.page).toBe(1);
+      });
 
       it.todo('returns pageCount as 1');
     });
