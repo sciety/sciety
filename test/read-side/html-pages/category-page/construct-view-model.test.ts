fad2a405d984b2cc3ccb1d163091a76beca0cbdb
LinaKind <l.kinduryte@elifesciences.org>
2024-10-01 15:24:55 +0100

    Hide test set up details not specific to the various tests #3563

--- test/read-side/html-pages/category-page/construct-view-model.test.ts
+++ test/read-side/html-pages/category-page/construct-view-model.test.ts
@@ -1,16 +1,24 @@
 import * as E from 'fp-ts/Either';
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as tt from 'io-ts-types';
+import { DependenciesForViews } from '../../../../src/read-side/dependencies-for-views';
 import { constructViewModel } from '../../../../src/read-side/html-pages/category-page/construct-view-model';
 import { PaginatedCards, ViewModel } from '../../../../src/read-side/html-pages/category-page/view-model';
 import { abortTest } from '../../../abort-test';
 import { createTestFramework, TestFramework } from '../../../framework';
 import { arbitraryNumber, arbitraryString } from '../../../helpers';
-import { shouldNotBeCalled } from '../../../should-not-be-called';
 import { arbitraryDataError } from '../../../types/data-error.helper';
 import { arbitraryExpressionDoi } from '../../../types/expression-doi.helper';
+
+const performConstruction = async (dependencies: DependenciesForViews) => pipe(
+  {
+    categoryName: arbitraryString() as tt.NonEmptyString,
+    page: arbitraryNumber(1, 1000),
+  },
+  constructViewModel(dependencies),
+  TE.getOrElse(abortTest('expected view model construction to succeed')),
+)();
 
 describe('construct-view-model', () => {
   let framework: TestFramework;
@@ -27,14 +35,7 @@ describe('construct-view-model', () => {
         ...framework.dependenciesForViews,
         fetchByCategory: () => TE.right({ expressionDois: [], totalItems: 0 }),
       };
-      result = await pipe(
-        {
-          categoryName: arbitraryString() as tt.NonEmptyString,
-          page: arbitraryNumber(1, 1000),
-        },
-        constructViewModel(dependencies),
-        TE.getOrElse(shouldNotBeCalled),
-      )();
+      result = await performConstruction(dependencies);
     });
 
     it('displays an informational message', () => {
@@ -51,16 +52,11 @@ describe('construct-view-model', () => {
           ...framework.dependenciesForViews,
           fetchByCategory: () => TE.right({ expressionDois: [arbitraryExpressionDoi()], totalItems: 1 }),
         };
-        paginatedCards = await pipe(
-          {
-            categoryName: arbitraryString() as tt.NonEmptyString,
-            page: arbitraryNumber(1, 1000),
-          },
-          constructViewModel(dependencies),
-          TE.getOrElse(abortTest('expected view model construction to succeed')),
-          T.map((viewModel) => viewModel.content),
-          TE.getOrElse(abortTest('expected paginated cards')),
-        )();
+        paginatedCards = pipe(
+          await performConstruction(dependencies),
+          (viewModel) => viewModel.content,
+          E.getOrElseW(abortTest('expected paginated cards')),
+        );
       });
 
       it('displays 1 article card', () => {
@@ -78,16 +74,11 @@ describe('construct-view-model', () => {
           fetchByCategory: () => TE.right({ expressionDois: [arbitraryExpressionDoi()], totalItems: 1 }),
           fetchPublishingHistory: () => TE.left(arbitraryDataError()),
         };
-        paginatedCards = await pipe(
-          {
-            categoryName: arbitraryString() as tt.NonEmptyString,
-            page: arbitraryNumber(1, 1000),
-          },
-          constructViewModel(dependencies),
-          TE.getOrElse(abortTest('expected view model construction to succeed')),
-          T.map((viewModel) => viewModel.content),
-          TE.getOrElse(abortTest('expected paginated cards')),
-        )();
+        paginatedCards = pipe(
+          await performConstruction(dependencies),
+          (viewModel) => viewModel.content,
+          E.getOrElseW(abortTest('expected paginated cards')),
+        );
       });
 
       it.failing('displays an article error card', () => {
