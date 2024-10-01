69e6ab2049e72a5b2a4b597e51c98ef769ecd352
LinaKind <l.kinduryte@elifesciences.org>
2024-10-01 10:42:49 +0100

    Create a space to capture product decisions about this page #3563

---
+++ test/read-side/html-pages/category-page/construct-view-model.test.ts
@@ -0,0 +1,29 @@
+describe('construct-view-model', () => {
+  describe('when there are no evaluated expressions in the selected category', () => {
+    it.todo('displays an informational message');
+  });
+
+  describe('when there is 1 evaluated expression in the selected category', () => {
+    describe('and an article card can be displayed', () => {
+      it.todo('displays 1 article card');
+    });
+
+    describe('and an article card cannot be displayed', () => {
+      it.todo('?');
+    });
+  });
+
+  describe('when there are 2 evaluated expressions in the selected category', () => {
+    describe('and all article cards can be displayed', () => {
+      it.todo('displays 2 article cards');
+    });
+
+    describe('and 1 article card cannot be displayed', () => {
+      it.todo('?');
+    });
+
+    describe('and no article cards can be displayed', () => {
+      it.todo('?');
+    });
+  });
+});
