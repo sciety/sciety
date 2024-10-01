06a457ad93ab9241ced8430cdecda6565599ff0f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-10-01 11:47:06 +0100

    Write a passing test for current implementation #3563

--- test/read-side/html-pages/category-page/construct-view-model.test.ts
+++ test/read-side/html-pages/category-page/construct-view-model.test.ts
@@ -1,6 +1,41 @@
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import * as tt from 'io-ts-types';
+import { constructViewModel } from '../../../../src/read-side/html-pages/category-page/construct-view-model';
+import { ViewModel } from '../../../../src/read-side/html-pages/category-page/view-model';
+import { createTestFramework, TestFramework } from '../../../framework';
+import { arbitraryNumber, arbitraryString } from '../../../helpers';
+import { shouldNotBeCalled } from '../../../should-not-be-called';
+
 describe('construct-view-model', () => {
+  let framework: TestFramework;
+
+  beforeEach(() => {
+    framework = createTestFramework();
+  });
+
   describe('when there are no evaluated expressions in the selected category', () => {
-    it.todo('displays an informational message');
+    let result: ViewModel;
+
+    beforeEach(async () => {
+      const dependencies = {
+        ...framework.dependenciesForViews,
+        fetchByCategory: () => TE.right({ expressionDois: [], totalItems: 0 }),
+      };
+      result = await pipe(
+        {
+          categoryName: arbitraryString() as tt.NonEmptyString,
+          page: arbitraryNumber(1, 1000),
+        },
+        constructViewModel(dependencies),
+        TE.getOrElse(shouldNotBeCalled),
+      )();
+    });
+
+    it('displays an informational message', () => {
+      expect(result.content).toStrictEqual(E.left(expect.anything()));
+    });
   });
 
   describe('when there is 1 evaluated expression in the selected category', () => {
