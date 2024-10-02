e900a3e0bc6542f9fdfd28d42c04aaec12c9198a
LinaKind <l.kinduryte@elifesciences.org>
2024-10-02 15:34:08 +0100

    Create a seam to calculate a page's href #3563

--- src/read-side/html-pages/category-page/construct-pagination-controls.ts
+++ src/read-side/html-pages/category-page/construct-pagination-controls.ts
@@ -3,9 +3,11 @@ import { Params } from './params';
 import { PaginatedCards } from './view-model';
 import { constructCategoryPageHref } from '../../../standards/paths';
 
+const calculateForwardPageHref = (params: Params) => O.some(constructCategoryPageHref(params.categoryName, 2));
+
 export const constructPaginationControls = (pageSize: number, params: Params, totalItems: number): PaginatedCards['paginationControls'] => ({
   backwardPageHref: O.some('/backward-page-href'),
-  forwardPageHref: O.some(constructCategoryPageHref(params.categoryName, 2)),
+  forwardPageHref: calculateForwardPageHref(params),
   page: params.page,
   pageCount: Math.ceil(totalItems / pageSize),
 });
