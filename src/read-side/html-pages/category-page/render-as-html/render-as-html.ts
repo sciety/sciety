f5da479d64c6defc4738ffa57e2455085e5b796c
Giorgio Sironi <g.sironi@elifesciences.org>
2024-10-02 13:22:06 +0100

    Replace a local renderer with a shared component's renderer to avoid silently hiding article error cards #3563

--- src/read-side/html-pages/category-page/render-as-html/render-as-html.ts
+++ src/read-side/html-pages/category-page/render-as-html/render-as-html.ts
@@ -1,16 +1,16 @@
 import { htmlEscape } from 'escape-goat';
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
-import { renderCategoryContent } from './render-category-content';
 import { toHtmlFragment } from '../../../../types/html-fragment';
 import { HtmlPage, toHtmlPage } from '../../html-page';
+import { renderArticleCardStack } from '../../shared-components/article-card-stack';
 import { renderPaginationControls } from '../../shared-components/pagination';
 import { PaginatedCards, ViewModel } from '../view-model';
 
 const renderInformationalMessage = (message: string) => toHtmlFragment(`<p>${message}</p>`);
 
 const renderPaginatedCards = (paginatedCards: PaginatedCards) => `
-      ${renderCategoryContent(paginatedCards.categoryContent)}
+      ${renderArticleCardStack(paginatedCards.categoryContent)}
       ${process.env.EXPERIMENT_ENABLED === 'true' ? renderPaginationControls(paginatedCards.paginationControls) : ''}
       `;
 
