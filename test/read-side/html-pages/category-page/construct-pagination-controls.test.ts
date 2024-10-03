3ba657b19169a971eaec2f4d610b26baa8785ec4
David Moulton <d.moulton@elifesciences.org>
2024-10-03 09:36:52 +0100

    Express intent with a todo test list #3563

--- test/read-side/html-pages/category-page/construct-pagination-controls.test.ts
+++ test/read-side/html-pages/category-page/construct-pagination-controls.test.ts
@@ -33,6 +33,16 @@ describe('construct-pagination-controls', () => {
     });
 
     describe('when 12 items are available', () => {
+      describe('and the page 1 is selected', () => {
+        it.todo('returns backwardPageHref as none');
+
+        it.todo('returns forwardPageHref as some');
+
+        it.todo('returns page as the selected page');
+
+        it.todo('returns page count as 2');
+      });
+
       describe('and the page 2 is selected', () => {
         const selectedPage = 2;
         const result = constructPaginationControls(
