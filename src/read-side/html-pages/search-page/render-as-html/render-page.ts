afbdb7fe2add6c03dc93b534095242920ea36f90
LinaKind <l.kinduryte@elifesciences.org>
2024-10-07 11:14:58 +0100

    Extract booleans to named consts for readability #3569

--- src/read-side/html-pages/search-page/render-as-html/render-page.ts
+++ src/read-side/html-pages/search-page/render-as-html/render-page.ts
@@ -3,13 +3,17 @@ import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 import { renderSearchForm } from '../../shared-components/search-form';
 import { ViewModel } from '../view-model';
 
+const includeUnevaluatedPreprints = false;
+
+const evaluatedOnly = !includeUnevaluatedPreprints;
+
 export const renderPage = (viewModel: ViewModel): HtmlFragment => pipe(
   `
     <header class="page-header">
       <h1>${viewModel.pageHeading}</h1>
     </header>
     <section>
-      ${renderSearchForm('', true, false)}
+      ${renderSearchForm('', evaluatedOnly, includeUnevaluatedPreprints)}
     </section>
   `,
   toHtmlFragment,
