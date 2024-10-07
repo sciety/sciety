7d579d54c728f94d67ee35a94488ff67b7546549
David Moulton <d.moulton@elifesciences.org>
2024-10-07 10:23:23 +0100

    Introduce a seam to work on the new form section #3569

--- src/read-side/html-pages/shared-components/search-form/render-search-form.ts
+++ src/read-side/html-pages/shared-components/search-form/render-search-form.ts
@@ -2,6 +2,27 @@ import { htmlEscape } from 'escape-goat';
 import { searchResultsPagePath } from '../../../../standards/paths';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 
+const renderEvaluatedOnlyCheckbox = (evaluatedOnly: boolean) => `
+  <section>
+    <input type="checkbox" name="evaluatedOnly" value="true" id="searchEvaluatedOnlyFilter"${evaluatedOnly ? ' checked' : ''}>
+    <label for="searchEvaluatedOnlyFilter" class="search-form__checkbox_label">Search only evaluated preprints</label>
+  </section>
+`;
+
+const renderUnevaluatedPreprintsCheckbox = (evaluatedOnly: boolean) => `
+  <section>
+    <input type="checkbox" name="evaluatedOnly" value="true" id="searchEvaluatedOnlyFilter"${evaluatedOnly ? ' checked' : ''}>
+    <label for="searchEvaluatedOnlyFilter" class="search-form__checkbox_label">Search only evaluated preprints</label>
+  </section>
+`;
+
+const renderCheckbox = (evaluatedOnly: boolean) => {
+  if (process.env.EXPERIMENT_ENABLED === 'true') {
+    return renderUnevaluatedPreprintsCheckbox(evaluatedOnly);
+  }
+  return renderEvaluatedOnlyCheckbox(evaluatedOnly);
+};
+
 export const renderSearchForm = (query: string, evaluatedOnly: boolean): HtmlFragment => toHtmlFragment(`
   <form action="${searchResultsPagePath}" method="get" class="search-form">
     <label for="searchText" class="search-form__label">
@@ -9,10 +30,7 @@ export const renderSearchForm = (query: string, evaluatedOnly: boolean): HtmlFra
     </label>
     <div class="search-form__positioning_context">
       ${htmlEscape`<input value="${query}" id="searchText" name="query" class="standard-form__full_width_text_input">`}
-      <section>
-        <input type="checkbox" name="evaluatedOnly" value="true" id="searchEvaluatedOnlyFilter"${evaluatedOnly ? ' checked' : ''}>
-        <label for="searchEvaluatedOnlyFilter" class="search-form__checkbox_label">Search only evaluated preprints</label>
-      </section>
+      ${renderCheckbox(evaluatedOnly)}
       <button type="submit" class="search-form__submit" aria-label="Run the search">Search</button>
       <button type="reset" id="clearSearchText" class="search-form__clear visually-hidden">
         <img src="/static/images/clear-search-text-icon.svg" class="search-form__clear_icon" alt="">
