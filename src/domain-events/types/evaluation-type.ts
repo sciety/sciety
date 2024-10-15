cd43dec481387ef702380a00755f25553ff87649
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-10-15 13:47:33 +0100

    Explicitely couple evaluation type codec to the map of evaluation types #3522

--- src/domain-events/types/evaluation-type.ts
+++ src/domain-events/types/evaluation-type.ts
@@ -1,18 +1,18 @@
 import * as t from 'io-ts';
 
-export const evaluationTypeCodec = t.union([
-  t.literal('review'),
-  t.literal('author-response'),
-  t.literal('curation-statement'),
-  t.literal('not-provided'),
-]);
-
 export const evaluationTypes = <const>{
   review: 'review',
   authorResponse: 'author-response',
   curationStatement: 'curation-statement',
   notProvided: 'not-provided',
 };
+
+export const evaluationTypeCodec = t.union([
+  t.literal(evaluationTypes.review),
+  t.literal(evaluationTypes.authorResponse),
+  t.literal(evaluationTypes.curationStatement),
+  t.literal(evaluationTypes.notProvided),
+]);
 
 type MapOfEvaluationTypes = typeof evaluationTypes;
 
