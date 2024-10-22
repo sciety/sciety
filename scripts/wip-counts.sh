3d9dcdb824520be6c36717da621b3b6e599a3167
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-10-22 08:47:23 +0100

    Extend to cover wip in feature tests #3595

--- scripts/wip-counts.sh
+++ scripts/wip-counts.sh
@@ -6,4 +6,9 @@ fast_test_count=$(grep --recursive '.skip\|.failing' test | wc -l)
 echo "> Fast test files with WIP"
 grep --recursive --files-with-matches '.skip\|.failing' test
 
+feature_test_count=$(grep --recursive '.skip\|.failing' feature-test | wc -l)
+printf "\n> Feature test files with WIP\n"
+grep --recursive --files-with-matches '.skip\|.failing' feature-test
+
 printf "\nskipped or failing fast tests: %s\n" $fast_test_count
+printf "skipped or failing feature tests: %s\n" $feature_test_count
