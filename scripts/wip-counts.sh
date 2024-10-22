b833c2a471029d46a832afaa3070eb60a1ce2d9c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-10-22 08:59:40 +0100

    Cover hardcoded UUIDs #3595

--- scripts/wip-counts.sh
+++ scripts/wip-counts.sh
@@ -14,6 +14,11 @@ feature_flags=$(grep --recursive 'EXPERIMENT_ENABLED ===' src | wc -l)
 printf "\n> Files with feature flags\n"
 grep --recursive --files-with-matches 'EXPERIMENT_ENABLED ===' src
 
+uuids=$(grep -rE "'[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}'" src | grep -v 'style-guide' | wc -l)
+printf "\n> Files with hardcoded UUIDs\n"
+grep -rEl "'[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}'" src | grep -v 'style-guide'
+
 printf "\nSkipped or failing fast tests: %s\n" $fast_test_count
 printf "Skipped or failing feature tests: %s\n" $feature_test_count
 printf "Feature flags: %s\n" $feature_flags
+printf "Hardcoded UUIDs: %s\n" $uuids
