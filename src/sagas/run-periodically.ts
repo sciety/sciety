02e720826778982bbb957caa408aafec0af253b5
David Moulton <d.moulton@elifesciences.org>
2024-10-22 11:33:32 +0100

    Use jest mock timers to implement a happy path test #3596

--- src/sagas/run-periodically.ts
+++ src/sagas/run-periodically.ts
@@ -1,4 +1,4 @@
-type Saga = () => Promise<void>;
+export type Saga = () => Promise<void>;
 
 export const runPeriodically = (saga: Saga, seconds: number): void => {
   setInterval(saga, seconds * 1000);
--- test/sagas/run-periodically.test.ts
+++ test/sagas/run-periodically.test.ts
@@ -1,6 +1,26 @@
+import { runPeriodically, Saga } from '../../src/sagas/run-periodically';
+
 describe('run-periodically', () => {
   describe('given a saga that executes faster than the period', () => {
-    it.todo('allows all iterations to run');
+    const periodMs = 1000;
+    const iterations = 2;
+    const fudgeFactorMs = 100;
+    let counter: number;
+
+    const saga: Saga = async () => {
+      counter += 1;
+    };
+
+    beforeEach(() => {
+      jest.useFakeTimers();
+      counter = 0;
+      runPeriodically(saga, periodMs / 1000);
+      jest.advanceTimersByTime(periodMs * iterations + fudgeFactorMs);
+    });
+
+    it('allows all iterations to run', () => {
+      expect(counter).toBe(iterations);
+    });
   });
 
   describe('given a saga that takes longer to run than the period', () => {
