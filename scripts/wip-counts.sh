13aa490997ea93f9d36a1aaf39623c80468dc8c6
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-10-22 08:53:44 +0100

    Cover feature flags #3595

--- scripts/wip-counts.sh
+++ scripts/wip-counts.sh
@@ -10,5 +10,10 @@ feature_test_count=$(grep --recursive '.skip\|.failing' feature-test | wc -l)
 printf "\n> Feature test files with WIP\n"
 grep --recursive --files-with-matches '.skip\|.failing' feature-test
 
-printf "\nskipped or failing fast tests: %s\n" $fast_test_count
-printf "skipped or failing feature tests: %s\n" $feature_test_count
+feature_flags=$(grep --recursive 'EXPERIMENT_ENABLED ===' src | wc -l)
+printf "\n> Files with feature flags\n"
+grep --recursive --files-with-matches 'EXPERIMENT_ENABLED ===' src
+
+printf "\nSkipped or failing fast tests: %s\n" $fast_test_count
+printf "Skipped or failing feature tests: %s\n" $feature_test_count
+printf "Feature flags: %s\n" $feature_flags
