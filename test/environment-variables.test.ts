ad4229095c44c04edf74380db94ba3c678cc88cd
Giorgio Sironi <g.sironi@elifesciences.org>
2024-10-28 15:43:00 +0000

    Describe current behavior with a todo test list #3607

---
+++ test/environment-variables.test.ts
@@ -0,0 +1,19 @@
+describe('environment-variables', () => {
+  describe('withDefaultIfEmpty', () => {
+    describe('given a non-empty string that passes the codec', () => {
+      it.todo('returns the decoded value');
+    });
+
+    describe('given a non-empty string that does not pass the codec', () => {
+      it.todo('returns an error');
+    });
+
+    describe('given undefined', () => {
+      it.todo('returns a default value');
+    });
+
+    describe('given an empty string', () => {
+      it.todo('returns a default value');
+    });
+  });
+});
