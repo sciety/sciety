900ad6a85749e62cf805ef1d3d9d4dfda4a52d86
LinaKind <l.kinduryte@elifesciences.org>
2024-10-28 15:02:39 +0000

    Expand environment variables codec in preparation #3607

--- src/environment-variables-codec.ts
+++ src/environment-variables-codec.ts
@@ -4,6 +4,7 @@ import * as tt from 'io-ts-types';
 export const environmentVariablesCodec = t.type({
   APP_ORIGIN: tt.NonEmptyString,
   SCIETY_TEAM_API_BEARER_TOKEN: tt.NonEmptyString,
+  COAR_NOTIFICATION_DELIVERY_ENABLED: tt.withFallback(tt.BooleanFromString, false),
 });
 
 export type EnvironmentVariables = t.TypeOf<typeof environmentVariablesCodec>;
