4bb08aefab3db4799224e658cd6f2ec80820a93c
LinaKind <l.kinduryte@elifesciences.org>
2024-10-29 13:19:27 +0000

    Send coar notifications for biophysics colab to kotahi staging #3611
    This will include already recorded evaluations

--- src/read-models/evaluations-for-notifications/index.ts
+++ src/read-models/evaluations-for-notifications/index.ts
@@ -4,10 +4,16 @@ import { handleEvent, initialState } from './handle-event';
 import * as GID from '../../types/group-id';
 
 export const preReviewGroupId = GID.fromValidatedString('10360d97-bf52-4aef-b2fa-2f60d319edd7');
+const biophysicsColabGroupId = GID.fromValidatedString('4bbf0c12-629b-4bb8-91d6-974f4df8efb2');
 
-const testTarget = {
+const coarSandboxTarget = {
   id: 'https://coar-notify-inbox.fly.dev',
   inbox: new URL('https://coar-notify-inbox.fly.dev/inbox/'),
+};
+
+const biophysicsColabKotahiStagingTarget = {
+  id: 'https://staging.kotahi.cloud/api/coar/inbox/kotahi',
+  inbox: new URL('https://staging.kotahi.cloud/api/coar/inbox/kotahi'),
 };
 
 export const evaluationsForNotifications = {
@@ -17,7 +23,8 @@ export const evaluationsForNotifications = {
   initialState,
   handleEvent: handleEvent(
     new Map([
-      [preReviewGroupId, [testTarget]],
+      [preReviewGroupId, [coarSandboxTarget]],
+      [biophysicsColabGroupId, [biophysicsColabKotahiStagingTarget]],
     ]),
   ),
 };
