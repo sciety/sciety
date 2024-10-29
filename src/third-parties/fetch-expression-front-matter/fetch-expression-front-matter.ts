eb87c2d47b85bb35e865e2d59d4b6f48a678434e
David Moulton <d.moulton@elifesciences.org>
2024-10-29 14:24:41 +0000

    Hide crossref XML parsing via XMLDom in one file #3612

--- src/third-parties/fetch-expression-front-matter/fetch-expression-front-matter.ts
+++ src/third-parties/fetch-expression-front-matter/fetch-expression-front-matter.ts
@@ -1,69 +1,15 @@
-import { DOMParser } from '@xmldom/xmldom';
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
 import { flow, pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
-import { detectUnrecoverableError } from './detect-unrecoverable-error';
-import {
-  getAbstract, getAuthors, getTitle,
-} from './parse-crossref-article';
+import { parseResponseAndConstructDomainObject } from './parse-crossref-article';
 import { Logger } from '../../logger';
-import { ArticleAuthors } from '../../types/article-authors';
 import * as DE from '../../types/data-error';
 import { ExpressionDoi } from '../../types/expression-doi';
-import { SanitisedHtmlFragment } from '../../types/sanitised-html-fragment';
 import { decodeAndLogFailures } from '../decode-and-log-failures';
 import { ExternalQueries } from '../external-queries';
 import { QueryExternalService } from '../query-external-service';
-
-const parseResponseAndConstructDomainObject = (document: string, logger: Logger, expressionDoi: ExpressionDoi) => {
-  if (document.length === 0) {
-    logger('error', 'crossref/fetch-expression-front-matter: Empty document', { doi: expressionDoi, document });
-    return E.left(DE.unavailable);
-  }
-  const parser = new DOMParser({
-    errorHandler: (_, msg) => {
-      throw msg;
-    },
-  });
-  let abstract: O.Option<SanitisedHtmlFragment>;
-  let authors: ArticleAuthors;
-  let title: O.Option<SanitisedHtmlFragment>;
-  try {
-    const parsedXml = parser.parseFromString(document, 'text/xml');
-
-    const unrecoverableError = detectUnrecoverableError(parsedXml);
-    if (O.isSome(unrecoverableError)) {
-      logger('error', 'crossref/fetch-expression-front-matter: Unrecoverable error', { expressionDoi, document, reason: unrecoverableError.value });
-      return E.left(DE.unavailable);
-    }
-
-    authors = getAuthors(parsedXml);
-    if (O.isNone(authors)) {
-      logger('warn', 'crossref/fetch-expression-front-matter: Unable to find authors', { expressionDoi, document });
-    }
-
-    abstract = getAbstract(parsedXml);
-    if (O.isNone(abstract)) {
-      logger('warn', 'crossref/fetch-expression-front-matter: Unable to find abstract', { expressionDoi, document });
-    }
-
-    title = getTitle(parsedXml);
-    if (O.isNone(title)) {
-      logger('error', 'crossref/fetch-expression-front-matter: Unable to find title', { expressionDoi, document });
-      return E.left(DE.unavailable);
-    }
-  } catch (error: unknown) {
-    logger('error', 'crossref/fetch-expression-front-matter: Unable to parse document', { expressionDoi, document, error });
-    return E.left(DE.unavailable);
-  }
-  return E.right({
-    abstract,
-    title: title.value,
-    authors,
-  });
-};
 
 const crossrefWorksTransformEndpoint = (expressionDoi: ExpressionDoi): string => `https://api.crossref.org/works/${expressionDoi}/transform`;
 
--- src/third-parties/fetch-expression-front-matter/parse-crossref-article.ts
+++ src/third-parties/fetch-expression-front-matter/parse-crossref-article.ts
@@ -1,8 +1,14 @@
-import { XMLSerializer } from '@xmldom/xmldom';
+import { DOMParser, XMLSerializer } from '@xmldom/xmldom';
+import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
+import { detectUnrecoverableError } from './detect-unrecoverable-error';
 import { getElement } from './get-element';
+import { Logger } from '../../logger';
 import { ArticleAuthors } from '../../types/article-authors';
+import * as DE from '../../types/data-error';
+import { ExpressionDoi } from '../../types/expression-doi';
+import { ExpressionFrontMatter } from '../../types/expression-front-matter';
 import { toHtmlFragment } from '../../types/html-fragment';
 import { sanitise, SanitisedHtmlFragment } from '../../types/sanitised-html-fragment';
 
@@ -118,4 +124,56 @@ export const getAuthors = (doc: Document): ArticleAuthors => {
     });
 
   return pipe(authors, O.sequenceArray);
+};
+
+export const parseResponseAndConstructDomainObject = (
+  document: string,
+  logger: Logger,
+  expressionDoi: ExpressionDoi,
+): E.Either<DE.DataError, ExpressionFrontMatter> => {
+  if (document.length === 0) {
+    logger('error', 'crossref/fetch-expression-front-matter: Empty document', { doi: expressionDoi, document });
+    return E.left(DE.unavailable);
+  }
+  const parser = new DOMParser({
+    errorHandler: (_, msg) => {
+      throw msg;
+    },
+  });
+  let abstract: O.Option<SanitisedHtmlFragment>;
+  let authors: ArticleAuthors;
+  let title: O.Option<SanitisedHtmlFragment>;
+  try {
+    const parsedXml = parser.parseFromString(document, 'text/xml');
+
+    const unrecoverableError = detectUnrecoverableError(parsedXml);
+    if (O.isSome(unrecoverableError)) {
+      logger('error', 'crossref/fetch-expression-front-matter: Unrecoverable error', { expressionDoi, document, reason: unrecoverableError.value });
+      return E.left(DE.unavailable);
+    }
+
+    authors = getAuthors(parsedXml);
+    if (O.isNone(authors)) {
+      logger('warn', 'crossref/fetch-expression-front-matter: Unable to find authors', { expressionDoi, document });
+    }
+
+    abstract = getAbstract(parsedXml);
+    if (O.isNone(abstract)) {
+      logger('warn', 'crossref/fetch-expression-front-matter: Unable to find abstract', { expressionDoi, document });
+    }
+
+    title = getTitle(parsedXml);
+    if (O.isNone(title)) {
+      logger('error', 'crossref/fetch-expression-front-matter: Unable to find title', { expressionDoi, document });
+      return E.left(DE.unavailable);
+    }
+  } catch (error: unknown) {
+    logger('error', 'crossref/fetch-expression-front-matter: Unable to parse document', { expressionDoi, document, error });
+    return E.left(DE.unavailable);
+  }
+  return E.right({
+    abstract,
+    title: title.value,
+    authors,
+  });
 };
