db27db51be4cd3a9b5c3d1124c3f77b86d6d4e05
Giorgio Sironi <g.sironi@elifesciences.org>
2024-11-05 08:51:36 +0000

    Replace last instance of constructHtmlResponse #3589

--- src/read-side/html-pages/to-default-error-html-document.ts
+++ src/read-side/html-pages/to-default-error-html-document.ts
@@ -3,7 +3,7 @@ import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { CompleteHtmlDocument } from './complete-html-document';
 import { toErrorPageViewModel } from './construct-error-page-view-model';
-import { constructHtmlResponse } from './construct-html-response';
+import { constructHtmlResponseWithDependencies } from './construct-html-response';
 import { ClientClassification } from './shared-components/head';
 import { renderStandardPageLayout } from './shared-components/standard-page-layout';
 import * as DE from '../../types/data-error';
@@ -16,15 +16,13 @@ export const toDefaultErrorHtmlDocument = (
   loggedInUserId: O.Option<UserId>,
   errorMessage: string,
   clientClassification: ClientClassification,
-): CompleteHtmlDocument => {
-  const userDetails = pipe(
-    loggedInUserId,
-    O.chain((id) => dependencies.lookupUser(id)),
-  );
-  return pipe(
-    constructHtmlResponse(userDetails, renderStandardPageLayout, clientClassification)(E.left(toErrorPageViewModel({
-      message: toHtmlFragment(errorMessage),
-      type: DE.unavailable,
-    }))).document,
-  );
-};
+): CompleteHtmlDocument => pipe(
+  {
+    message: toHtmlFragment(errorMessage),
+    type: DE.unavailable,
+  },
+  toErrorPageViewModel,
+  E.left,
+  constructHtmlResponseWithDependencies(dependencies, loggedInUserId, renderStandardPageLayout, clientClassification),
+  ({ document }) => document,
+);
