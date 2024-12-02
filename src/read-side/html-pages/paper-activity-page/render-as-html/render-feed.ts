773e0bb0d403185d1a1a5acba84ae68c43cfcb06
Giorgio Sironi <g.sironi@elifesciences.org>
2024-12-02 15:37:06 +0000

    Provide initial HTML structure for a video feed item #3661

--- src/read-side/html-pages/paper-activity-page/render-as-html/render-feed.ts
+++ src/read-side/html-pages/paper-activity-page/render-as-html/render-feed.ts
@@ -3,11 +3,27 @@ import { pipe } from 'fp-ts/function';
 import { renderEvaluationPublishedFeedItem } from './render-evaluation-published-feed-item';
 import { renderExpressionPublishedFeedItem } from './render-expression-published-feed-item';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
+import { templateDate } from '../../shared-components/date';
 import { renderListItems } from '../../shared-components/list-items';
 import { CassyniSeminarPublishedFeedItem, FeedItem } from '../view-model';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const renderCassyniSeminarPublishedFeedItem = (feedItem: CassyniSeminarPublishedFeedItem) => toHtmlFragment('');
+const renderCassyniSeminarPublishedFeedItem = (feedItem: CassyniSeminarPublishedFeedItem) => toHtmlFragment(`
+  <div class="activity-feed__item__contents">
+    <header class="activity-feed__item__header">
+      
+      <div class="activity-feed__item__meta">
+        <div class="activity-feed__item__title">
+          <a href="#">
+            Version published to 
+          </a>
+        </div>
+        ${templateDate(feedItem.publishedAt, 'activity-feed__item__date')}
+      </div>
+    </header>
+    <a href="https://doi.org/10.52843/cassyni.y1p61f">A link</a>
+  </div>
+`);
 
 const renderFeedItem = (feedItem: FeedItem) => {
   switch (feedItem.type) {
