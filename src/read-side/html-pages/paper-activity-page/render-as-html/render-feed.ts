113a06c09046017465f16bee5e15ea8ed83ce717
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-12-03 10:49:59 +0000

    Visually indicate that the image is a video link #3661

--- src/read-side/html-pages/paper-activity-page/render-as-html/render-feed.ts
+++ src/read-side/html-pages/paper-activity-page/render-as-html/render-feed.ts
@@ -20,9 +20,9 @@ const renderCassyniSeminarPublishedFeedItem = (feedItem: CassyniSeminarPublished
       </div>
     </header>
     <div class="activity-feed__item__body">
-      <a href="https://doi.org/10.52843/cassyni.y1p61f">
+      <a class="activity-feed__item__video_link" href="https://doi.org/10.52843/cassyni.y1p61f">
         <img src="https://api.cassyni.com/api/videos/MH5YFnwYt2VpYChF8Jj9EE/poster?embedded=false" alt="Thumbnail for Cassyni video seminar">
-        <svg>
+        <svg class="activity-feed__item__play_icon">
           <circle cx="50" cy="50" r="50" fill="#00000030"/>
           <polygon points="30 20, 80 50, 30 80" fill="#ffffff"></polygon>
         </svg>
