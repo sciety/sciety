f84ac5bf91ee25cd0968722a1af8ca9e67c52559
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-12-03 11:48:49 +0000

    Improve the description of the seminar item #3661

--- src/read-side/html-pages/paper-activity-page/render-as-html/render-feed.ts
+++ src/read-side/html-pages/paper-activity-page/render-as-html/render-feed.ts
@@ -19,8 +19,10 @@ const renderCassyniSeminarPublishedFeedItem = (feedItem: CassyniSeminarPublished
       </div>
     </header>
     <div class="activity-feed__item__body">
+      <p>Seminar title: New bioinformatics resources from The International Cannabis Genome Research Consortium</p>
       <a class="activity-feed__item__video_link" href="https://doi.org/10.52843/cassyni.y1p61f">
-        <img src="https://api.cassyni.com/api/videos/MH5YFnwYt2VpYChF8Jj9EE/poster?embedded=false" alt="Thumbnail for Cassyni video seminar">
+        <span class="visually-hidden">Go to the seminar on Cassyni</span>
+        <img src="https://api.cassyni.com/api/videos/MH5YFnwYt2VpYChF8Jj9EE/poster?embedded=false" alt="">
         <svg class="activity-feed__item__play_icon">
           <style>
             .icon:hover circle {
@@ -33,7 +35,6 @@ const renderCassyniSeminarPublishedFeedItem = (feedItem: CassyniSeminarPublished
           </g>
         </svg>
       </a>
-      <p>New bioinformatics resources from The International Cannabis Genome Research Consortium</p>
     </div>
   </div>
 `);
