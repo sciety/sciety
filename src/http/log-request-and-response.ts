87e86f457dbf54a97db8b58bfd7fa0ea97e15458
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-12-24 17:00:51 +0000

    Log matched route to avoid having to use regex to build label

--- src/http/log-request-and-response.ts
+++ src/http/log-request-and-response.ts
@@ -1,31 +1,32 @@
 import { Middleware } from 'koa';
 import { Logger } from '../logger';
 
-export const logRequestAndResponse = (logger: Logger): Middleware => async ({ request, res }, next) => {
+export const logRequestAndResponse = (logger: Logger): Middleware => async (ctx, next) => {
   const startTime = new Date();
-  const logLevel = request.url.startsWith('/static') ? 'debug' : 'info';
+  const logLevel = ctx.request.url.startsWith('/static') ? 'debug' : 'info';
   logger(logLevel, 'Received HTTP request', {
-    method: request.method,
-    url: request.url,
-    referer: request.headers.referer,
+    method: ctx.request.method,
+    url: ctx.request.url,
+    referer: ctx.request.headers.referer,
   });
 
-  res.once('finish', () => {
+  ctx.res.once('finish', () => {
     const durationInMs = new Date().getTime() - startTime.getTime();
     logger(logLevel, 'Sent HTTP response', {
-      url: request.url,
-      status: res.statusCode,
+      url: ctx.request.url,
+      status: ctx.res.statusCode,
       durationInMs,
+      route: ctx._matchedRoute,
     });
   });
 
-  res.once('close', () => {
-    if (res.writableFinished) {
+  ctx.res.once('close', () => {
+    if (ctx.res.writableFinished) {
       return;
     }
 
     logger('warn', 'HTTP response may not have been completely sent', {
-      status: res.statusCode,
+      status: ctx.res.statusCode,
     });
   });
 
