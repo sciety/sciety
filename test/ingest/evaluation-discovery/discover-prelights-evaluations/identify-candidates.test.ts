036031e9d57bfc3bcc673402aaaf3e962a6bb39d
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-12-24 10:49:02 +0000

    Align test case with language and setup of other cases #3692

--- test/ingest/evaluation-discovery/discover-prelights-evaluations/identify-candidates.test.ts
+++ test/ingest/evaluation-discovery/discover-prelights-evaluations/identify-candidates.test.ts
@@ -3,6 +3,7 @@ import { pipe } from 'fp-ts/function';
 import { identifyCandidates } from '../../../../src/ingest/evaluation-discovery/discover-prelights-evaluations/identify-candidates';
 import { abortTest } from '../../../abort-test';
 import { arbitraryDate, arbitraryString, arbitraryUri } from '../../../helpers';
+import { arbitraryExpressionDoi } from '../../../types/expression-doi.helper';
 
 describe('identify-candidates', () => {
   describe('when the feed contains an item ...', () => {
@@ -103,39 +104,39 @@ describe('identify-candidates', () => {
     });
   });
 
-  describe('when highlights are not linked to a preprint', () => {
-    it('decodes three candidates', () => {
+  describe('when feed containes items that are not linked to a preprint', () => {
+    it('decodes them as candidates', () => {
       const inputCausingCodecFailure = `
         <rss version="2.0">
           <channel>
             <title>preLights</title>
             <item>
               <category><![CDATA[<a name = "highlight">highlight</a>]]></category>
-              <title>Restoring mechanophenotype reverts malignant properties of ECM-enriched vocal fold cancer</title>
-              <guid isPermaLink="false">https://prelights.biologists.com/?post_type=highlight&#038;p=39236</guid>
-              <author>Teodora Piskova</author>
-              <pubDate>Thu, 19 Dec 2024 11:33:37 +0000</pubDate>
+              <title>${arbitraryString()}</title>
+              <guid isPermaLink="false">${arbitraryUri()}</guid>
+              <author>${arbitraryString()}</author>
+              <pubDate>${arbitraryDate().toISOString()}</pubDate>
               <preprints>
                   <preprint>
-                    <preprintdoi>10.1101/2024.08.22.609159</preprintdoi>
+                    <preprintdoi>${arbitraryExpressionDoi()}</preprintdoi>
                   </preprint>
               </preprints>
             </item>
             <item>
               <category><![CDATA[<a name = "highlight">highlight</a>]]></category>
-              <title>Phosphorylation of huntingtin at S421 supercharges cellular transport by turbocharging kinesin and dynein motors on endosomes and lysosomes</title>
-              <guid isPermaLink="false">https://prelights.biologists.com/?post_type=highlight&#038;p=39265</guid>
-              <author>Abhishek Poddar</author>
-              <pubDate>Wed, 21 Aug 2024 06:25:53 +0000</pubDate>
-              <doi>https://doi.org/10.1242/prelights.39265</doi>
+              <title>${arbitraryString()}</title>
+              <guid isPermaLink="false">${arbitraryUri()}</guid>
+              <author>${arbitraryString()}</author>
+              <pubDate>${arbitraryDate().toISOString()}</pubDate>
+              <doi>${arbitraryExpressionDoi()}</doi>
             </item>
             <item>
               <category><![CDATA[<a name = "highlight">highlight</a>]]></category>
-              <title>Necrosulfonamide causes oxidation of PCM1 and impairs ciliogenesis and autophagy</title>
-              <guid isPermaLink="false">https://prelights.biologists.com/?post_type=highlight&#038;p=39257</guid>
-              <author>Abhishek Poddar</author>
-              <pubDate>Tue, 19 Mar 2024 07:00:46 +0000</pubDate>
-              <doi>https://doi.org/10.1242/prelights.39257</doi>
+              <title>${arbitraryString()}</title>
+              <guid isPermaLink="false">${arbitraryUri()}</guid>
+              <author>${arbitraryString()}</author>
+              <pubDate>${arbitraryDate().toISOString()}</pubDate>
+              <doi>${arbitraryExpressionDoi()}</doi>
             </item>
           </channel>
         </rss>
