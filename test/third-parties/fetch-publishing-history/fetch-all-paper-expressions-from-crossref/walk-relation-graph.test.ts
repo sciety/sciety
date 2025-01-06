4eafa67450c1133828e655361c1f4e5f2674dfec
Giorgio Sironi <g.sironi@elifesciences.org>
2025-01-06 11:32:48 +0000

    Implement another characterization test #3686

--- test/third-parties/fetch-publishing-history/fetch-all-paper-expressions-from-crossref/walk-relation-graph.test.ts
+++ test/third-parties/fetch-publishing-history/fetch-all-paper-expressions-from-crossref/walk-relation-graph.test.ts
@@ -1,12 +1,14 @@
 import * as E from 'fp-ts/Either';
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import { CrossrefWork } from '../../../../src/third-parties/fetch-publishing-history/fetch-all-paper-expressions-from-crossref/crossref-work';
 import { QueryCrossrefService } from '../../../../src/third-parties/fetch-publishing-history/fetch-all-paper-expressions-from-crossref/query-crossref-service';
 import { State } from '../../../../src/third-parties/fetch-publishing-history/fetch-all-paper-expressions-from-crossref/state';
 import { walkRelationGraph } from '../../../../src/third-parties/fetch-publishing-history/fetch-all-paper-expressions-from-crossref/walk-relation-graph';
 import * as DE from '../../../../src/types/data-error';
 import { dummyLogger } from '../../../dummy-logger';
-import { arbitraryUri } from '../../../helpers';
+import { arbitraryString, arbitraryUri } from '../../../helpers';
 import { arbitraryExpressionDoi } from '../../../types/expression-doi.helper';
 
 const arbitraryCrossrefWork = (): CrossrefWork => ({
@@ -50,7 +52,31 @@ describe('walk-relation-graph', () => {
 
   describe('if the queue is not empty', () => {
     describe('if there are currently more than 20 collected works', () => {
-      it.todo('returns the current collected works on the right');
+      const crossrefWorks = Array.from({ length: 21 }, arbitraryCrossrefWork);
+      let queryCrossrefService: QueryCrossrefService;
+
+      const state: State = {
+        queue: [arbitraryString()],
+        collectedWorks: pipe(
+          crossrefWorks,
+          RA.map((work) => [work.DOI, work] as const),
+          (entries) => new Map(entries),
+        ),
+      };
+      let result: E.Either<DE.DataError, ReadonlyArray<CrossrefWork>>;
+
+      beforeEach(async () => {
+        queryCrossrefService = jest.fn(() => TE.right(''));
+        result = await walkRelationGraph(
+          queryCrossrefService,
+          dummyLogger,
+          arbitraryExpressionDoi(),
+        )(state)();
+      });
+
+      it('returns the current collected works on the right', () => {
+        expect(result).toStrictEqual(E.right(crossrefWorks));
+      });
 
       it.todo('does not call queryCrossrefService');
     });
