1b341c911bc60bd688b2b4bbaac855566d12216a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-01-06 10:35:28 +0000

    Begin to describe current behaviour with a test list #3686

---
+++ test/third-parties/fetch-publishing-history/fetch-all-paper-expressions-from-crossref/walk-relation-graph.test.ts
@@ -0,0 +1,23 @@
+describe('walk-relation-graph', () => {
+  describe('if the queue is empty', () => {
+    it.todo('returns on the right');
+
+    it.todo('returns the current collected works');
+
+    it.todo('does not call queryCrossrefService');
+  });
+
+  describe('if the queue is not empty', () => {
+    describe('if there are currently more than 20 collected works', () => {
+      it.todo('returns on the right');
+
+      it.todo('returns the current collected works');
+
+      it.todo('does not call queryCrossrefService');
+    });
+
+    describe('if there are 20 or fewer collected works', () => {
+      it.todo('tbd');
+    });
+  });
+});
