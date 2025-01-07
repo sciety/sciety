9b4213e2648da7b3f7051840eb7bdc607320077e
LinaKind <l.kinduryte@elifesciences.org>
2025-01-07 14:40:46 +0000

    Complete encapsulation of state creation in the test file #3686

--- test/third-parties/fetch-publishing-history/fetch-all-paper-expressions-from-crossref/enqueue-all-related-dois-not-yet-collected.test.ts
+++ test/third-parties/fetch-publishing-history/fetch-all-paper-expressions-from-crossref/enqueue-all-related-dois-not-yet-collected.test.ts
@@ -9,16 +9,17 @@ import { arbitraryString } from '../../../helpers';
 import { arbitraryExpressionDoi } from '../../../types/expression-doi.helper';
 
 describe('enqueue-all-related-dois-not-yet-collected', () => {
-  describe('when there no records', () => {
-    const initialRecords = new Map();
+  describe('when there no works', () => {
     const state = pipe(
       [],
       collectWorksIntoStateAndEmptyQueue(initialState(arbitraryExpressionDoi())),
     );
     const result = enqueueAllRelatedDoisNotYetCollected(state);
 
-    it('the collected records are unchanged', () => {
-      expect(result.collectedWorks).toStrictEqual(initialRecords);
+    it('the collected works are unchanged', () => {
+      const alreadyCollectedWorks = new Map();
+
+      expect(result.collectedWorks).toStrictEqual(alreadyCollectedWorks);
     });
 
     it('the queue is empty', () => {
@@ -26,7 +27,7 @@ describe('enqueue-all-related-dois-not-yet-collected', () => {
     });
   });
 
-  describe('when there is one record with no relations', () => {
+  describe('when there is one work with no relations', () => {
     const collectedWork: CrossrefWork = {
       type: 'posted-content',
       DOI: EDOI.fromValidatedString('10.21203/rs.3.rs-1828415/v2'),
@@ -40,17 +41,18 @@ describe('enqueue-all-related-dois-not-yet-collected', () => {
       },
       relation: {},
     };
-    const initialRecords: State['collectedWorks'] = new Map([
-      [collectedWork.DOI, collectedWork],
-    ]);
     const state = pipe(
       [collectedWork],
       collectWorksIntoStateAndEmptyQueue(initialState(arbitraryExpressionDoi())),
     );
     const result = enqueueAllRelatedDoisNotYetCollected(state);
 
-    it('the collected records are unchanged', () => {
-      expect(result.collectedWorks).toStrictEqual(initialRecords);
+    it('the collected works are unchanged', () => {
+      const alreadyCollectedWorks: State['collectedWorks'] = new Map([
+        [collectedWork.DOI, collectedWork],
+      ]);
+
+      expect(result.collectedWorks).toStrictEqual(alreadyCollectedWorks);
     });
 
     it('the queue is empty', () => {
@@ -58,49 +60,57 @@ describe('enqueue-all-related-dois-not-yet-collected', () => {
     });
   });
 
-  describe('when there are two records that are related to each other', () => {
-    const initialRecords: State['collectedWorks'] = new Map([
-      [EDOI.fromValidatedString('10.21203/rs.3.rs-1828415/v2'), {
-        type: 'posted-content',
-        DOI: EDOI.fromValidatedString('10.21203/rs.3.rs-1828415/v2'),
-        posted: {
-          'date-parts': [[2023, 12, 7]],
+  describe('when there are two works that are related to each other', () => {
+    const collectedWork1: CrossrefWork = {
+      type: 'posted-content',
+      DOI: EDOI.fromValidatedString('10.21203/rs.3.rs-1828415/v2'),
+      posted: {
+        'date-parts': [[2023, 12, 7]],
+      },
+      resource: {
+        primary: {
+          URL: arbitraryString(),
         },
-        resource: {
-          primary: {
-            URL: arbitraryString(),
-          },
+      },
+      relation: {
+        'is-version-of': [{
+          'id-type': 'doi' as const,
+          id: EDOI.fromValidatedString('10.21203/rs.3.rs-1828415/v1'),
+        }],
+      },
+    };
+    const collectedWork2: CrossrefWork = {
+      type: 'posted-content',
+      DOI: EDOI.fromValidatedString('10.21203/rs.3.rs-1828415/v1'),
+      posted: {
+        'date-parts': [[2023, 12, 7]],
+      },
+      resource: {
+        primary: {
+          URL: arbitraryString(),
         },
-        relation: {
-          'is-version-of': [{
-            'id-type': 'doi' as const,
-            id: EDOI.fromValidatedString('10.21203/rs.3.rs-1828415/v1'),
-          }],
-        },
-      }],
-      [EDOI.fromValidatedString('10.21203/rs.3.rs-1828415/v1'), {
-        type: 'posted-content',
-        DOI: EDOI.fromValidatedString('10.21203/rs.3.rs-1828415/v1'),
-        posted: {
-          'date-parts': [[2023, 12, 7]],
-        },
-        resource: {
-          primary: {
-            URL: arbitraryString(),
-          },
-        },
-        relation: {
-          'has-version': [{
-            'id-type': 'doi' as const,
-            id: EDOI.fromValidatedString('10.21203/rs.3.rs-1828415/v2'),
-          }],
-        },
-      }],
-    ]);
-    const result = enqueueAllRelatedDoisNotYetCollected({ collectedWorks: initialRecords, queue: [] });
+      },
+      relation: {
+        'has-version': [{
+          'id-type': 'doi' as const,
+          id: EDOI.fromValidatedString('10.21203/rs.3.rs-1828415/v2'),
+        }],
+      },
+    };
+    const state = pipe(
+      [collectedWork1, collectedWork2],
+      collectWorksIntoStateAndEmptyQueue(initialState(arbitraryExpressionDoi())),
+    );
 
-    it('the collected records are unchanged', () => {
-      expect(result.collectedWorks).toStrictEqual(initialRecords);
+    const result = enqueueAllRelatedDoisNotYetCollected(state);
+
+    it('the collected works are unchanged', () => {
+      const alreadyCollectedWorks: State['collectedWorks'] = new Map([
+        [collectedWork1.DOI, collectedWork1],
+        [collectedWork2.DOI, collectedWork2],
+      ]);
+
+      expect(result.collectedWorks).toStrictEqual(alreadyCollectedWorks);
     });
 
     it('the queue is empty', () => {
