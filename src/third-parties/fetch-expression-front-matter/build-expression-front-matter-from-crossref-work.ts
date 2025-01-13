55a36042521c15fb3a31ed5f0f23b53b09f904ff
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-01-13 15:32:47 +0000

    Flatten behaviour into a pipe for better storytelling #3710

--- src/third-parties/fetch-expression-front-matter/build-expression-front-matter-from-crossref-work.ts
+++ src/third-parties/fetch-expression-front-matter/build-expression-front-matter-from-crossref-work.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, unused-imports/no-unused-imports-ts
-import { DOMParser } from '@xmldom/xmldom';
+import { auth } from '@googleapis/sheets';
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
@@ -20,43 +20,40 @@ const extractCommonFrontmatter = (journalOrPostedContent: JournalOrPostedContent
   return journalOrPostedContent.posted_content;
 };
 
+const warnAboutMissingOptionalFrontmatterParts = (
+  logger: Logger, context: Record<string, unknown>,
+) => (res: ExpressionFrontMatter) => {
+  if (O.isNone(res.abstract)) {
+    logger('warn', 'build-expression-front-matter-from-crossref-work: Unable to find abstract', context);
+  }
+  if (O.isNone(res.authors)) {
+    logger('warn', 'build-expression-front-matter-from-crossref-work: Unable to find authors', context);
+  }
+  return E.right(undefined);
+};
+
 export const buildExpressionFrontMatterFromCrossrefWork = (
   crossrefWorkXml: string,
   logger: Logger,
   expressionDoi: ExpressionDoi,
-): E.Either<DE.DataError, ExpressionFrontMatter> => {
-  const commonFrontmatter = pipe(
-    crossrefWorkXml,
-    parseXmlDocument,
-    E.chainW(decodeAndLogFailures(
-      logger,
-      frontMatterCrossrefXmlResponseCodec,
-      { expressionDoi, crossrefWorkXml },
-    )),
-    E.map((decodedWork) => decodedWork.doi_records.doi_record.crossref),
-    E.map(extractCommonFrontmatter),
-  );
-
-  if (E.isLeft(commonFrontmatter)) {
+): E.Either<DE.DataError, ExpressionFrontMatter> => pipe(
+  crossrefWorkXml,
+  parseXmlDocument,
+  E.chainW(decodeAndLogFailures(
+    logger,
+    frontMatterCrossrefXmlResponseCodec,
+    { expressionDoi, crossrefWorkXml },
+  )),
+  E.map((decodedWork) => decodedWork.doi_records.doi_record.crossref),
+  E.map(extractCommonFrontmatter),
+  E.map((commonFrontmatter) => ({
+    title: getTitle(commonFrontmatter),
+    abstract: getAbstract(commonFrontmatter),
+    authors: getAuthors(commonFrontmatter),
+  })),
+  E.mapLeft(() => {
     logger('error', 'crossref/fetch-expression-front-matter: Failed to parse XML', { doi: expressionDoi, crossrefWorkXml });
-    return E.left(DE.unavailable);
-  }
-
-  const title = getTitle(commonFrontmatter.right);
-
-  const abstract = getAbstract(commonFrontmatter.right);
-  if (O.isNone(abstract)) {
-    logger('warn', 'build-expression-front-matter-from-crossref-work: Unable to find abstract', { expressionDoi, crossrefWorkXml });
-  }
-
-  const authors = getAuthors(commonFrontmatter.right);
-  if (O.isNone(authors)) {
-    logger('warn', 'build-expression-front-matter-from-crossref-work: Unable to find authors', { expressionDoi, crossrefWorkXml });
-  }
-
-  return E.right({
-    abstract,
-    title,
-    authors,
-  });
-};
+    return DE.unavailable;
+  }),
+  E.tap(warnAboutMissingOptionalFrontmatterParts(logger, { expressionDoi, crossrefWorkXml })),
+);
