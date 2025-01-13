d34ce27006b13e4a7df1dc5ecbc29bfbf295e85f
Giorgio Sironi <g.sironi@elifesciences.org>
2025-01-13 11:56:25 +0000

    Provide context via extracting a variable #3686

--- src/third-parties/fetch-publishing-history/fetch-all-paper-expressions-from-crossref/fetch-works-that-point-to-individual-works.ts
+++ src/third-parties/fetch-publishing-history/fetch-all-paper-expressions-from-crossref/fetch-works-that-point-to-individual-works.ts
@@ -9,6 +9,8 @@ import { Logger } from '../../../logger';
 import * as DE from '../../../types/data-error';
 import { decodeAndLogFailures } from '../../decode-and-log-failures';
 
+const numberOfRequestedWorks = 1000;
+
 const crossrefMultipleWorksResponseCodec = t.strict({
   message: t.strict({
     items: t.readonlyArray(crossrefWorkCodec),
@@ -21,7 +23,7 @@ type CrossrefMultipleWorksResponse = t.TypeOf<typeof crossrefMultipleWorksRespon
 const constructUrl = (doi: string) => {
   const workTypes = 'type:posted-content,type:journal-article';
   const relationships = 'relation.type:has-version,relation.type:is-version-of,relation.type:has-preprint,relation.type:is-preprint-of';
-  return `https://api.crossref.org/works?filter=relation.object:${doi},${workTypes},${relationships}&rows=1000`;
+  return `https://api.crossref.org/works?filter=relation.object:${doi},${workTypes},${relationships}&rows=${numberOfRequestedWorks}`;
 };
 
 const detectInsufficientNumberOfPagesBeingRequested = (
@@ -29,7 +31,7 @@ const detectInsufficientNumberOfPagesBeingRequested = (
   response: CrossrefMultipleWorksResponse,
   doi: string,
 ) => {
-  if (response.message['total-results'] > 1000) {
+  if (response.message['total-results'] > numberOfRequestedWorks) {
     logger('error', 'fetchWorksThatPointToIndividualWorks: total number of related works is greater than the single page 1 we are fetching', { doi, url: constructUrl(doi) });
   }
 };
