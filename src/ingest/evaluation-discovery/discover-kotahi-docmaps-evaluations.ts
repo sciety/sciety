1dad2fd6c6770f7813df49397f7dfe07bb4dfbaf
David Moulton <d.moulton@elifesciences.org>
2025-04-11 11:15:46 +0100

    Retrieve paperExpressionDoi from the provided end point #3874

--- src/ingest/evaluation-discovery/discover-kotahi-docmaps-evaluations.ts
+++ src/ingest/evaluation-discovery/discover-kotahi-docmaps-evaluations.ts
@@ -30,14 +30,14 @@ export const discoverKotahiDocmapsEvaluations: DiscoverPublishedEvaluations = (
     understood: [
       {
         publishedOn: new Date('2025-04-10T09:39:50.072Z'),
-        paperExpressionDoi: '10.1101/2025.03.27.645397',
+        paperExpressionDoi: retrievedResponse.steps['_:b0'].inputs[0].doi,
         evaluationLocator: 'hypothesis:zcPWGBC-EfCXaG9pRxlnhA',
         authors: [],
         evaluationType: 'review',
       },
       {
         publishedOn: new Date('2025-04-10T09:39:50.106Z'),
-        paperExpressionDoi: '10.1101/2025.03.27.645397',
+        paperExpressionDoi: retrievedResponse.steps['_:b0'].inputs[0].doi,
         evaluationLocator: 'hypothesis:zYPccBC-EfCbwOfTQ6xWFQ',
         authors: [],
         evaluationType: 'curation-statement',
