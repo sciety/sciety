8632cabf4b5a6674f0033acdbe1c0219d4b38b34
LinaKind <l.kinduryte@elifesciences.org>
2025-04-14 13:24:50 +0300

    Reduce hardcoding when ingesting kotahi docmaps #3874

--- src/ingest/evaluation-discovery/discover-kotahi-docmaps-evaluations.ts
+++ src/ingest/evaluation-discovery/discover-kotahi-docmaps-evaluations.ts
@@ -51,14 +51,14 @@ export const discoverKotahiDocmapsEvaluations: DiscoverPublishedEvaluations = (
   TE.map((relevantStep) => ({
     understood: [
       {
-        publishedOn: new Date('2025-04-10T09:39:50.072Z'),
+        publishedOn: new Date(relevantStep.actions[0].outputs[0].published),
         paperExpressionDoi: constructExpressionDoi(relevantStep),
         evaluationLocator: 'hypothesis:zcPWGBC-EfCXaG9pRxlnhA',
         authors: [],
         evaluationType: 'review',
       },
       {
-        publishedOn: new Date('2025-04-10T09:39:50.106Z'),
+        publishedOn: new Date(relevantStep.actions[0].outputs[1].published),
         paperExpressionDoi: constructExpressionDoi(relevantStep),
         evaluationLocator: 'hypothesis:zYPccBC-EfCbwOfTQ6xWFQ',
         authors: [],
