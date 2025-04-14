661411556674fe7caccc46ba212d40762fed5c42
David Moulton <d.moulton@elifesciences.org>
2025-04-14 10:50:45 +0100

    Extract a codec to derive a type enabling removal of duplication #3874

--- src/ingest/evaluation-discovery/discover-kotahi-docmaps-evaluations.ts
+++ src/ingest/evaluation-discovery/discover-kotahi-docmaps-evaluations.ts
@@ -7,32 +7,38 @@ import { DiscoverPublishedEvaluations } from '../discover-published-evaluations'
 
 const url = 'https://kotahi-server-test.fly.dev/api/docmap/group_id/bcdf0f70-1b32-4aa8-9a9e-84265b23a30d/doi/10.1101/2025.03.27.645397';
 
-const kotahiResponseCodec = t.strict({
-  steps: t.strict({
-    '_:b0': t.strict({
-      inputs: t.readonlyArray(
+const stepCodec = t.strict({
+  inputs: t.readonlyArray(
+    t.strict({
+      doi: t.string,
+    }),
+  ),
+  actions: t.readonlyArray(
+    t.strict({
+      outputs: t.readonlyArray(
         t.strict({
-          doi: t.string,
-        }),
-      ),
-      actions: t.readonlyArray(
-        t.strict({
-          outputs: t.readonlyArray(
+          type: t.string,
+          content: t.readonlyArray(
             t.strict({
-              type: t.string,
-              content: t.readonlyArray(
-                t.strict({
-                  url: t.string,
-                }),
-              ),
-              published: t.string,
+              url: t.string,
             }),
           ),
+          published: t.string,
         }),
       ),
     }),
+  ),
+});
+
+type Step = t.TypeOf<typeof stepCodec>;
+
+const kotahiResponseCodec = t.strict({
+  steps: t.strict({
+    '_:b0': stepCodec,
   }),
 }, 'kotahiResponseCodec');
+
+const constructExpressionDoi = (step: Step) => step.inputs[0].doi;
 
 export const discoverKotahiDocmapsEvaluations: DiscoverPublishedEvaluations = (
   ingestDays,
@@ -46,14 +52,14 @@ export const discoverKotahiDocmapsEvaluations: DiscoverPublishedEvaluations = (
     understood: [
       {
         publishedOn: new Date('2025-04-10T09:39:50.072Z'),
-        paperExpressionDoi: relevantStep.inputs[0].doi,
+        paperExpressionDoi: constructExpressionDoi(relevantStep),
         evaluationLocator: 'hypothesis:zcPWGBC-EfCXaG9pRxlnhA',
         authors: [],
         evaluationType: 'review',
       },
       {
         publishedOn: new Date('2025-04-10T09:39:50.106Z'),
-        paperExpressionDoi: relevantStep.inputs[0].doi,
+        paperExpressionDoi: constructExpressionDoi(relevantStep),
         evaluationLocator: 'hypothesis:zYPccBC-EfCbwOfTQ6xWFQ',
         authors: [],
         evaluationType: 'curation-statement',
