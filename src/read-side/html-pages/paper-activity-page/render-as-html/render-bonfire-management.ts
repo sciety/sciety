2ebf27d22e555df1308657982255bdb632e418df
LinaKind <l.kinduryte@elifesciences.org>
2025-10-03 13:03:08 +0300

    Provide the users with more context for the Sciety discussions #4059

--- src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
+++ src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
@@ -5,6 +5,7 @@ export const renderBonfireManagement = (bonfireSocialLinkHref: string): HtmlFrag
     <section class="bonfire-management">
         <h2 class="article-actions-heading">Discuss this preprint</h2>
             <a href=${bonfireSocialLinkHref} class="bonfire-management-button">Start a discussion</a>
+            <a href="https://blog.sciety.org/sciety-secures-funding-from-nlnet-foundation-to-help-build-discourse-around-preprints/">What are Sciety discussions?</a>
     </section>
     `,
 );
