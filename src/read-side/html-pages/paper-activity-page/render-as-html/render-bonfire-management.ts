57edbd62d5c7794199652afc5547dced10f42dfa
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-03 15:49:06 +0100

    Create a seam for an additional link
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1289

--- src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
+++ src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
@@ -1,10 +1,13 @@
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 
+const renderJoinTheDiscussionButton = (): HtmlFragment => toHtmlFragment('');
+
 export const renderBonfireManagement = (bonfireSocialLinkHref: string): HtmlFragment => toHtmlFragment(
   `
     <section class="bonfire-management">
         <h2 class="article-actions-heading">Discuss this preprint</h2>
             <a href=${bonfireSocialLinkHref} class="bonfire-management-button">Start a discussion</a>
+            ${renderJoinTheDiscussionButton()}
             <a href="https://blog.sciety.org/sciety-secures-funding-from-nlnet-foundation-to-help-build-discourse-around-preprints/">What are Sciety discussions?</a>
     </section>
     `,
