05d90d7b5b90b38bc5ae73ec8906c8c56245c385
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-06 09:59:36 +0100

    Introduce a stricter equality check and refactor to a pipe
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1289

--- src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
+++ src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
@@ -1,7 +1,21 @@
-import { ExpressionDoi } from '../../../../types/expression-doi';
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { eqExpressionDoi, ExpressionDoi, fromValidatedString } from '../../../../types/expression-doi';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 
-const renderJoinTheDiscussionButton = (expressionDoi: ExpressionDoi): HtmlFragment => (expressionDoi === '10.7554/elife.95814.3' ? toHtmlFragment('') : toHtmlFragment(''));
+const isAppropriateDoi = (
+  expressionDoi: ExpressionDoi,
+) => (doiToBeChecked: ExpressionDoi): boolean => eqExpressionDoi.equals(doiToBeChecked, expressionDoi);
+
+const renderJoinTheDiscussionButton = (expressionDoi: ExpressionDoi) => pipe(
+  expressionDoi,
+  O.fromPredicate(isAppropriateDoi(fromValidatedString('10.7554/elife.95814.3'))),
+  O.match(
+    () => '',
+    () => '',
+  ),
+  toHtmlFragment,
+);
 
 export const renderBonfireManagement = (
   bonfireSocialLinkHref: string,
