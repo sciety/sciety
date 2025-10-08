51473c38d06a46013cd309615e8ec52052ea6922
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-08 10:57:32 +0100

    Begin to push hardcoding up the stack
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1290

--- src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
+++ src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
@@ -7,12 +7,12 @@ const isAppropriateDoi = (
   expressionDoi: ExpressionDoi,
 ) => (doiToBeChecked: ExpressionDoi): boolean => eqExpressionDoi.equals(doiToBeChecked, expressionDoi);
 
-const renderJoinTheDiscussionButton = (expressionDoi: ExpressionDoi) => pipe(
+const renderJoinTheDiscussionButton = (expressionDoi: ExpressionDoi, discussionLinkHref: string) => pipe(
   expressionDoi,
   O.fromPredicate(isAppropriateDoi(fromValidatedString('10.7554/elife.95814.3'))),
   O.match(
     () => '',
-    () => '<a href="https://discussions.sciety.org/post/01K6MQC5NZFYEHXYQ23VCK047B" class="bonfire-management-button">Join the discussion</a>',
+    () => `<a href="${discussionLinkHref}" class="bonfire-management-button">Join the discussion</a>`,
   ),
   toHtmlFragment,
 );
@@ -25,7 +25,7 @@ export const renderBonfireManagement = (
     <section class="bonfire-management">
         <h2 class="article-actions-heading">Discuss this preprint</h2>
             <a href=${bonfireSocialLinkHref} class="bonfire-management-button">Start a discussion</a>
-            ${renderJoinTheDiscussionButton(expressionDoi)}
+            ${renderJoinTheDiscussionButton(expressionDoi, 'https://discussions.sciety.org/post/01K6MQC5NZFYEHXYQ23VCK047B')}
             <a href="https://blog.sciety.org/sciety-secures-funding-from-nlnet-foundation-to-help-build-discourse-around-preprints/">What are Sciety discussions?</a>
     </section>
     `,
