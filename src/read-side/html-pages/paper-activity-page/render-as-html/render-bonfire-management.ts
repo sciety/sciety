879eab3aa27ee270f65c914bf8dd83a235c439b1
David Moulton <d.moulton@elifesciences.org>
2025-10-08 13:32:18 +0100

    Push hardcoding further up the stack
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1290

--- src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
+++ src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
@@ -20,12 +20,13 @@ const renderJoinTheDiscussionButton = (expressionDoi: ExpressionDoi, discussionL
 export const renderBonfireManagement = (
   bonfireSocialLinkHref: string,
   expressionDoi: ExpressionDoi,
+  discussionLinkHref: string,
 ): HtmlFragment => toHtmlFragment(
   `
     <section class="bonfire-management">
         <h2 class="article-actions-heading">Discuss this preprint</h2>
             <a href=${bonfireSocialLinkHref} class="bonfire-management-button">Start a discussion</a>
-            ${renderJoinTheDiscussionButton(expressionDoi, 'https://discussions.sciety.org/post/01K6MQC5NZFYEHXYQ23VCK047B')}
+            ${renderJoinTheDiscussionButton(expressionDoi, discussionLinkHref)}
             <a href="https://blog.sciety.org/sciety-secures-funding-from-nlnet-foundation-to-help-build-discourse-around-preprints/">What are Sciety discussions?</a>
     </section>
     `,
