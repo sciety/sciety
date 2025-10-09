e912e8c22e4f25ba13b159cd6da8f5ef9237655e
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-09 13:39:41 +0100

    Remove construction logic from the renderer
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1290

--- src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
+++ src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
@@ -1,19 +1,13 @@
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
-import { eqExpressionDoi, ExpressionDoi, fromValidatedString } from '../../../../types/expression-doi';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 import { ViewModel } from '../view-model';
 
-const isAppropriateDoi = (
-  expressionDoi: ExpressionDoi,
-) => (doiToBeChecked: ExpressionDoi): boolean => eqExpressionDoi.equals(doiToBeChecked, expressionDoi);
-
-const renderJoinTheDiscussionButton = (expressionDoi: ExpressionDoi, discussionLinkHref: string) => pipe(
-  expressionDoi,
-  O.fromPredicate(isAppropriateDoi(fromValidatedString('10.7554/elife.95814.3'))),
+const renderJoinTheDiscussionButton = (discussionLinkHref: ViewModel['bonfireManagement']['optionalJoinDiscussionLinkHref']) => pipe(
+  discussionLinkHref,
   O.match(
     () => '',
-    () => `<a href="${discussionLinkHref}" class="bonfire-management-button">Join the discussion</a>`,
+    (href) => `<a href="${href}" class="bonfire-management-button">Join the discussion</a>`,
   ),
   toHtmlFragment,
 );
@@ -25,7 +19,7 @@ export const renderBonfireManagement = (
     <section class="bonfire-management">
         <h2 class="article-actions-heading">Discuss this preprint</h2>
             <a href="${bonfireManagement.startDiscussionLinkHref}" class="bonfire-management-button">Start a discussion</a>
-            ${renderJoinTheDiscussionButton(bonfireManagement.expressionDoi, bonfireManagement.joinDiscussionLinkHref)}
+            ${renderJoinTheDiscussionButton(bonfireManagement.optionalJoinDiscussionLinkHref)}
             <a href="https://blog.sciety.org/sciety-secures-funding-from-nlnet-foundation-to-help-build-discourse-around-preprints/">What are Sciety discussions?</a>
     </section>
     `,
