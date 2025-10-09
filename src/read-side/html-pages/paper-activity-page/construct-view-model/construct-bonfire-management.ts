b3f498ab090119ec787268d2be6c3e549d13f43c
David Moulton <d.moulton@elifesciences.org>
2025-10-09 13:26:38 +0100

    Refactor a function to use a pipe
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1290

--- src/read-side/html-pages/paper-activity-page/construct-view-model/construct-bonfire-management.ts
+++ src/read-side/html-pages/paper-activity-page/construct-view-model/construct-bonfire-management.ts
@@ -1,4 +1,5 @@
 import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
 import {
   CanonicalExpressionDoi,
   eqExpressionDoi,
@@ -10,12 +11,11 @@ const isAppropriateDoi = (
   expressionDoi: ExpressionDoi,
 ) => (doiToBeChecked: ExpressionDoi): boolean => eqExpressionDoi.equals(doiToBeChecked, expressionDoi);
 
-const constructJoinDiscussionLinkHref = (expressionDoi: CanonicalExpressionDoi) => {
-  if (isAppropriateDoi(expressionDoi)(fromValidatedString('10.7554/elife.95814.3'))) {
-    return O.some('https://discussions.sciety.org/post/01K6MQC5NZFYEHXYQ23VCK047B');
-  }
-  return O.none;
-};
+const constructJoinDiscussionLinkHref = (expressionDoi: CanonicalExpressionDoi) => pipe(
+  expressionDoi,
+  O.fromPredicate(isAppropriateDoi(fromValidatedString('10.7554/elife.95814.3'))),
+  O.map(() => 'https://discussions.sciety.org/post/01K6MQC5NZFYEHXYQ23VCK047B'),
+);
 
 export type BonfireManagement = {
   startDiscussionLinkHref: string,
