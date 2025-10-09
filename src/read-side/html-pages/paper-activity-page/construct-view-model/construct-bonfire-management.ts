ea2d4ae13c134f5846c82504ea98a5486569ff84
LinaKind <l.kinduryte@elifesciences.org>
2025-10-09 15:48:34 +0300

    Remove unused view model property
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1290

--- src/read-side/html-pages/paper-activity-page/construct-view-model/construct-bonfire-management.ts
+++ src/read-side/html-pages/paper-activity-page/construct-view-model/construct-bonfire-management.ts
@@ -19,14 +19,12 @@ const constructJoinDiscussionLinkHref = (expressionDoi: CanonicalExpressionDoi) 
 
 export type BonfireManagement = {
   startDiscussionLinkHref: string,
-  joinDiscussionLinkHref: string,
   optionalJoinDiscussionLinkHref: O.Option<string>,
   expressionDoi: CanonicalExpressionDoi,
 };
 
 export const constructBonfireManagement = (latestExpressionDoi: CanonicalExpressionDoi): BonfireManagement => ({
   startDiscussionLinkHref: 'https://discussions.sciety.org/signup',
-  joinDiscussionLinkHref: 'https://discussions.sciety.org/post/01K6MQC5NZFYEHXYQ23VCK047B',
   optionalJoinDiscussionLinkHref: constructJoinDiscussionLinkHref(latestExpressionDoi),
   expressionDoi: latestExpressionDoi,
 });
