9a45296a233ae58c03178c42bc7858adc3c2a7ee
LinaKind <l.kinduryte@elifesciences.org>
2025-10-10 12:57:30 +0300

    Introduce expressionDoi to retrievePostId to allow us to build a query #4069

--- src/read-side/html-pages/paper-activity-page/construct-view-model/construct-bonfire-management.ts
+++ src/read-side/html-pages/paper-activity-page/construct-view-model/construct-bonfire-management.ts
@@ -11,12 +11,13 @@ const isAppropriateDoi = (
   expressionDoi: ExpressionDoi,
 ) => (doiToBeChecked: ExpressionDoi): boolean => eqExpressionDoi.equals(doiToBeChecked, expressionDoi);
 
-const retrievePostId = () => '01K6MQC5NZFYEHXYQ23VCK047B';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const retrievePostId = (expressionDoi: CanonicalExpressionDoi) => '01K6MQC5NZFYEHXYQ23VCK047B';
 
 const constructJoinDiscussionLinkHref = (expressionDoi: CanonicalExpressionDoi) => pipe(
   expressionDoi,
   O.fromPredicate(isAppropriateDoi(fromValidatedString('10.7554/elife.95814.3'))),
-  O.map(retrievePostId),
+  O.map(() => retrievePostId(expressionDoi)),
   O.map((postId) => `https://discussions.sciety.org/post/${postId}`),
 );
 
