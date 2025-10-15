fe1f276a0826c84ca5f8a1e87e76d0c73623a172
LinaKind <l.kinduryte@elifesciences.org>
2025-10-15 17:42:04 +0300

    Reintroduce visual regression coverage for Jointhediscussion link
    #4069

--- src/infrastructure/stub-adapters/fetch-bonfire-discussion-id.ts
+++ src/infrastructure/stub-adapters/fetch-bonfire-discussion-id.ts
@@ -1,5 +1,12 @@
 import * as TE from 'fp-ts/TaskEither';
 import { ExternalQueries } from '../../third-parties';
 import * as DE from '../../types/data-error';
+import {
+  CanonicalExpressionDoi, eqExpressionDoi, ExpressionDoi, fromValidatedString,
+} from '../../types/expression-doi';
 
-export const fetchBonfireDiscussionId: ExternalQueries['fetchBonfireDiscussionId'] = () => TE.left(DE.unavailable);
+const isAppropriateDoi = (
+  expressionDoi: ExpressionDoi,
+) => (doiToBeChecked: ExpressionDoi): boolean => eqExpressionDoi.equals(doiToBeChecked, expressionDoi);
+
+export const fetchBonfireDiscussionId: ExternalQueries['fetchBonfireDiscussionId'] = (expressionDoi: CanonicalExpressionDoi) => (isAppropriateDoi(expressionDoi)(fromValidatedString('10.7554/elife.95814.3')) ? TE.right('1234') : TE.left(DE.unavailable));
