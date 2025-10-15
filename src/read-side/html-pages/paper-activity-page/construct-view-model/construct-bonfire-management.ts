621c748d072fd545df0b68c0910dcd92a0f058d9
LinaKind <l.kinduryte@elifesciences.org>
2025-10-15 15:28:51 +0300

    Only isAppropriateDoi once #4069

--- src/read-side/html-pages/paper-activity-page/construct-view-model/construct-bonfire-management.ts
+++ src/read-side/html-pages/paper-activity-page/construct-view-model/construct-bonfire-management.ts
@@ -5,20 +5,7 @@ import { pipe } from 'fp-ts/function';
 import { Dependencies } from './dependencies';
 import {
   CanonicalExpressionDoi,
-  eqExpressionDoi,
-  ExpressionDoi,
-  fromValidatedString,
 } from '../../../../types/expression-doi';
-
-const isAppropriateDoi = (
-  expressionDoi: ExpressionDoi,
-) => (doiToBeChecked: ExpressionDoi): boolean => eqExpressionDoi.equals(doiToBeChecked, expressionDoi);
-
-const constructJoinDiscussionLinkHref = (bonfireDiscussionId: string, expressionDoi: CanonicalExpressionDoi) => pipe(
-  expressionDoi,
-  O.fromPredicate(isAppropriateDoi(fromValidatedString('10.7554/elife.95814.3'))),
-  O.map(() => `https://discussions.sciety.org/post/${bonfireDiscussionId}`),
-);
 
 export type BonfireManagement = {
   startDiscussionLinkHref: string,
@@ -34,7 +21,7 @@ export const constructBonfireManagement = (
   dependencies.fetchBonfireDiscussionId,
   TE.match(
     () => O.none,
-    (bonfireDiscussionId) => constructJoinDiscussionLinkHref(bonfireDiscussionId, latestExpressionDoi),
+    (bonfireDiscussionId) => O.some(`https://discussions.sciety.org/post/${bonfireDiscussionId}`),
   ),
   T.map((optionalJoinDiscussionLinkHref) => ({
     startDiscussionLinkHref: 'https://discussions.sciety.org/signup',
