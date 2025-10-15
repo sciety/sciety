69d3a98daa67f22e47f90cd817caf4f95d80c30f
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-15 11:10:24 +0100

    Begin to use codec on the response from Bonfire API items #4069

--- src/third-parties/fetch-bonfire-discussion-id/fetch-bonfire-discussion-id.ts
+++ src/third-parties/fetch-bonfire-discussion-id/fetch-bonfire-discussion-id.ts
@@ -1,9 +1,21 @@
+import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
 import { postDataBonfire } from './post-data-bonfire';
 import { Logger } from '../../logger';
+import * as DE from '../../types/data-error';
 import { CanonicalExpressionDoi } from '../../types/expression-doi';
+import { decodeAndLogFailures } from '../decode-and-log-failures';
 import { ExternalQueries } from '../external-queries';
+
+const bonfireDiscussionIdResponseCodec = t.type({
+  data: t.strict({
+    post: t.strict({
+      id: t.string,
+    }),
+  }),
+});
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const fetchBonfireDiscussionId = (logger: Logger): ExternalQueries['fetchBonfireDiscussionId'] => (expressionDoi: CanonicalExpressionDoi) => pipe(
@@ -12,5 +24,10 @@ export const fetchBonfireDiscussionId = (logger: Logger): ExternalQueries['fetch
   },
   JSON.stringify,
   postDataBonfire(logger, 'https://discussions.sciety.org/api/graphql'),
+  TE.map((response) => response.data as string),
+  TE.chainEitherKW(flow(
+    decodeAndLogFailures(logger, bonfireDiscussionIdResponseCodec),
+    E.mapLeft(() => DE.unavailable),
+  )),
   TE.map(() => '01K6MQC5NZFYEHXYQ23VCK047B'),
 );
