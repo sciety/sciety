ae4ecaeed84960b4878f4c6d331514fea4b2da08
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-15 13:24:56 +0100

    Stop making redundant calls to Bonfire API #4069

--- src/third-parties/fetch-bonfire-discussion-id/fetch-bonfire-discussion-id.ts
+++ src/third-parties/fetch-bonfire-discussion-id/fetch-bonfire-discussion-id.ts
@@ -5,7 +5,9 @@ import * as t from 'io-ts';
 import { postDataBonfire } from './post-data-bonfire';
 import { Logger } from '../../logger';
 import * as DE from '../../types/data-error';
-import { CanonicalExpressionDoi } from '../../types/expression-doi';
+import {
+  CanonicalExpressionDoi, eqExpressionDoi, ExpressionDoi, fromValidatedString,
+} from '../../types/expression-doi';
 import { decodeAndLogFailures } from '../decode-and-log-failures';
 import { ExternalQueries } from '../external-queries';
 
@@ -17,15 +19,25 @@ const bonfireDiscussionIdResponseCodec = t.type({
   }),
 });
 
-export const fetchBonfireDiscussionId = (logger: Logger): ExternalQueries['fetchBonfireDiscussionId'] => (expressionDoi: CanonicalExpressionDoi) => pipe(
-  {
-    query: '{ post(filter: {id: "01K6MQC5NZFYEHXYQ23VCK047B"}) { id postContent { htmlBody } } }',
-  },
-  JSON.stringify,
-  postDataBonfire(logger, 'https://discussions.sciety.org/api/graphql'),
-  TE.chainEitherKW(flow(
-    decodeAndLogFailures(logger, bonfireDiscussionIdResponseCodec, { expressionDoi }),
-    E.mapLeft(() => DE.unavailable),
-  )),
-  TE.map((decodedResponse) => decodedResponse.data.post.id),
-);
+const isAppropriateDoi = (
+  expressionDoi: ExpressionDoi,
+) => (doiToBeChecked: ExpressionDoi): boolean => eqExpressionDoi.equals(doiToBeChecked, expressionDoi);
+
+export const fetchBonfireDiscussionId = (logger: Logger): ExternalQueries['fetchBonfireDiscussionId'] => (expressionDoi: CanonicalExpressionDoi) => {
+  if (isAppropriateDoi(expressionDoi)(fromValidatedString('10.7554/elife.95814.3'))) {
+    return pipe(
+      {
+        query: '{ post(filter: {id: "01K6MQC5NZFYEHXYQ23VCK047B"}) { id postContent { htmlBody } } }',
+      },
+      JSON.stringify,
+      postDataBonfire(logger, 'https://discussions.sciety.org/api/graphql'),
+      TE.chainEitherKW(flow(
+        decodeAndLogFailures(logger, bonfireDiscussionIdResponseCodec, { expressionDoi }),
+        E.mapLeft(() => DE.unavailable),
+      )),
+      TE.map((decodedResponse) => decodedResponse.data.post.id),
+    );
+  }
+
+  return TE.left(DE.unavailable);
+};
