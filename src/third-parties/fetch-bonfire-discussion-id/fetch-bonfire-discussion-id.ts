31f9f2c033f8f9a8d2192d8c32b3f78f1e245385
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-15 10:51:04 +0100

    Adjust the query to return only necessary items #4069

--- src/third-parties/fetch-bonfire-discussion-id/fetch-bonfire-discussion-id.ts
+++ src/third-parties/fetch-bonfire-discussion-id/fetch-bonfire-discussion-id.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-useless-escape */
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { postDataBonfire } from './post-data-bonfire';
@@ -9,7 +8,7 @@ import { ExternalQueries } from '../external-queries';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const fetchBonfireDiscussionId = (logger: Logger): ExternalQueries['fetchBonfireDiscussionId'] => (expressionDoi: CanonicalExpressionDoi) => pipe(
   {
-    query: '{ post(filter: {id: "01K6MQC5NZFYEHXYQ23VCK047B"}) { postContent { htmlBody } activity { replied { threadId } } }}',
+    query: '{ post(filter: {id: "01K6MQC5NZFYEHXYQ23VCK047B"}) { id postContent { htmlBody } } }',
   },
   JSON.stringify,
   postDataBonfire(logger, 'https://discussions.sciety.org/api/graphql'),
