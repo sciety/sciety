40602dcc75aba8d348192bc9dcc3c8847ed3e251
LinaKind <l.kinduryte@elifesciences.org>
2025-10-16 11:30:34 +0300

    Extract hardcoded id and doi from the fetchBonfireDiscussionId #4069

--- src/third-parties/fetch-bonfire-discussion-id/fetch-bonfire-discussion-id.ts
+++ src/third-parties/fetch-bonfire-discussion-id/fetch-bonfire-discussion-id.ts
@@ -1,16 +1,20 @@
 import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
-import * as B from 'fp-ts/boolean';
 import { flow, pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 import { postDataBonfire } from './post-data-bonfire';
 import { Logger } from '../../logger';
 import * as DE from '../../types/data-error';
 import {
-  CanonicalExpressionDoi, fromValidatedString, isAppropriateDoi,
+  CanonicalExpressionDoi, ExpressionDoi, fromValidatedString,
 } from '../../types/expression-doi';
 import { decodeAndLogFailures } from '../decode-and-log-failures';
 import { ExternalQueries } from '../external-queries';
+
+const bonfireDiscussionIds = new Map<ExpressionDoi, string>(Object.entries({
+  '10.7554/elife.95814.3': '01K6MQC5NZFYEHXYQ23VCK047B',
+}).map(([doi, id]) => [fromValidatedString(doi), id]));
 
 const bonfireDiscussionIdResponseCodec = t.type({
   data: t.strict({
@@ -21,13 +25,14 @@ const bonfireDiscussionIdResponseCodec = t.type({
 });
 
 export const fetchBonfireDiscussionId = (logger: Logger): ExternalQueries['fetchBonfireDiscussionId'] => (expressionDoi: CanonicalExpressionDoi) => pipe(
-  expressionDoi,
-  isAppropriateDoi(fromValidatedString('10.7554/elife.95814.3')),
-  B.fold(
+  bonfireDiscussionIds.get(expressionDoi),
+  O.fromNullable,
+  (foo) => foo,
+  O.match(
     () => TE.left(DE.unavailable),
-    () => pipe(
+    (bonfireDiscussionId) => pipe(
       {
-        query: '{ post(filter: {id: "01K6MQC5NZFYEHXYQ23VCK047B"}) { id postContent { htmlBody } } }',
+        query: `{ post(filter: {id: "${bonfireDiscussionId}"}) { id postContent { htmlBody } } }`,
       },
       JSON.stringify,
       postDataBonfire(logger, 'https://discussions.sciety.org/api/graphql'),
