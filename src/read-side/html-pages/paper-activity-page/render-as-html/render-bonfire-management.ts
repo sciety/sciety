5a657e9839f150aba4e1f32efe4a673318436262
David Moulton <d.moulton@elifesciences.org>
2025-10-17 15:56:55 +0100

    Introduce a no-op form in preparation #4077

--- src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
+++ src/read-side/html-pages/paper-activity-page/render-as-html/render-bonfire-management.ts
@@ -1,5 +1,7 @@
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
+import { inputFieldNames } from '../../../../standards';
+import { ExpressionDoi } from '../../../../types/expression-doi';
 import { HtmlFragment, toHtmlFragment } from '../../../../types/html-fragment';
 import { ViewModel } from '../view-model';
 
@@ -12,7 +14,18 @@ const renderJoinTheDiscussionButton = (discussionLinkHref: ViewModel['bonfireMan
   toHtmlFragment,
 );
 
-const renderStartTheDiscussionButton = (startDiscussionLinkHref: string) => `<a href="${startDiscussionLinkHref}" class="bonfire-management-button">Start a discussion</a>`;
+const renderStartTheDiscussionButton = (startDiscussionLinkHref: string, expressionDoi: ExpressionDoi) => {
+  if (process.env.EXPERIMENT_ENABLED === 'true') {
+    return `<form method="post" action="#">
+    <input type="hidden" name="${inputFieldNames.expressionDoi}" value="${expressionDoi}">
+    <button aria-label="Start a discussion about this paper on the Sciety Bonfire at discussions.sciety.org" class="bonfire-management-button">
+    Start a discussion
+    </button>
+  </form>
+`;
+  }
+  return `<a href="${startDiscussionLinkHref}" class="bonfire-management-button">Start a discussion</a>`;
+};
 
 export const renderBonfireManagement = (
   bonfireManagement: ViewModel['bonfireManagement'],
@@ -20,7 +33,7 @@ export const renderBonfireManagement = (
   `
     <section class="bonfire-management">
         <h2 class="article-actions-heading">Discuss this preprint</h2>
-            ${renderStartTheDiscussionButton(bonfireManagement.startDiscussionLinkHref)}
+            ${renderStartTheDiscussionButton(bonfireManagement.startDiscussionLinkHref, bonfireManagement.expressionDoi)}
             ${renderJoinTheDiscussionButton(bonfireManagement.optionalJoinDiscussionLinkHref)}
             <a href="https://blog.sciety.org/sciety-secures-funding-from-nlnet-foundation-to-help-build-discourse-around-preprints/">What are Sciety discussions?</a>
     </section>
