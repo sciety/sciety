bef5bcd8abf5f3eb0d4785a33696ffa11733274e
David Moulton <d.moulton@elifesciences.org>
2025-10-20 14:15:26 +0100

    Begin handling start bonfire discussion form submission #4077

--- src/http/form-submission-handlers/start-bonfire-discussion-handler.ts
+++ src/http/form-submission-handlers/start-bonfire-discussion-handler.ts
@@ -1,9 +1,30 @@
+import * as E from 'fp-ts/Either';
+import * as t from 'io-ts';
 import { Middleware } from 'koa';
 import { Logger } from '../../logger';
+import { inputFieldNames } from '../../standards';
+import { ExpressionDoi, expressionDoiCodec } from '../../types/expression-doi';
 
 type Dependencies = {
   logger: Logger,
 };
 
+const requestCodec = t.type({
+  body: t.type({
+    [inputFieldNames.expressionDoi]: expressionDoiCodec,
+  }),
+});
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const startBonfireDiscussionHandler = (dependencies: Dependencies): Middleware => async (context) => {};
+const submitStartBonfireDiscussion = (expressionDoi: ExpressionDoi) => undefined;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const startBonfireDiscussionHandler = (dependencies: Dependencies): Middleware => async (context) => {
+  const formRequest = requestCodec.decode(context.request);
+  if (E.isLeft(formRequest)) {
+    context.redirect('/action-failed');
+    return;
+  }
+
+  submitStartBonfireDiscussion(formRequest.right.body[inputFieldNames.expressionDoi]);
+};
