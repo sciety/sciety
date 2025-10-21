0bf4cf8fa2303245c4e946cb8feaa90ec0472efd
David Moulton <d.moulton@elifesciences.org>
2025-10-21 11:09:27 +0100

    Begin to implement new Bonfire third-party query #4077

--- src/third-parties/bonfire/create-bonfire-discussion-and-retrieve-discussion-id.ts
+++ src/third-parties/bonfire/create-bonfire-discussion-and-retrieve-discussion-id.ts
@@ -1,7 +1,16 @@
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { postDataBonfire } from './post-data-bonfire';
 import { Logger } from '../../logger';
 import { ExpressionDoi } from '../../types/expression-doi';
 import { ExternalQueries } from '../external-queries';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const createBonfireDiscussionAndRetrieveDiscussionId = (logger: Logger): ExternalQueries['createBonfireDiscussionAndRetrieveDiscussionId'] => (expressionDoi: ExpressionDoi) => TE.right('');
+export const createBonfireDiscussionAndRetrieveDiscussionId = (logger: Logger): ExternalQueries['createBonfireDiscussionAndRetrieveDiscussionId'] => (expressionDoi: ExpressionDoi) => pipe(
+  {
+    query: '{}',
+  },
+  JSON.stringify,
+  postDataBonfire(logger, 'https://discussions.sciety.org/api/graphql'),
+  TE.map(() => ''),
+);
