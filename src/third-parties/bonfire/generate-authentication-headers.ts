4b087d4919342a27a13bd4cbfd3489ce8e5aeecf
David Moulton <d.moulton@elifesciences.org>
2025-10-22 15:16:29 +0100

    Allow a local user to start a discussion by clicking the button #4077

--- src/third-parties/bonfire/generate-authentication-headers.ts
+++ src/third-parties/bonfire/generate-authentication-headers.ts
@@ -1,8 +1,19 @@
+import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
 import { postDataBonfire } from './post-data-bonfire';
 import { Logger } from '../../logger';
 import * as DE from '../../types/data-error';
+import { decodeAndLogFailures } from '../decode-and-log-failures';
+
+const bonfireAuthenticationTokenCodec = t.type({
+  data: t.strict({
+    login: t.strict({
+      token: t.string,
+    }),
+  }),
+});
 
 const getUserToken = (logger: Logger) => pipe(
   {
@@ -10,7 +21,11 @@ const getUserToken = (logger: Logger) => pipe(
   },
   JSON.stringify,
   postDataBonfire(logger, 'https://discussions.sciety.org/api/graphql'),
-  TE.map(() => ''),
+  TE.chainEitherKW(flow(
+    decodeAndLogFailures(logger, bonfireAuthenticationTokenCodec),
+    E.mapLeft(() => DE.unavailable),
+  )),
+  TE.map((decodedResponse) => decodedResponse.data.login.token),
 );
 
 export const generateAuthenticationHeaders = (
