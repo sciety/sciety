ac89897b88d01cee33c5f5bded188be51db46aac
LinaKind <l.kinduryte@elifesciences.org>
2025-10-23 16:31:43 +0300

    Exit middleware if we receive an error from the third party query #4077

--- src/http/form-submission-handlers/start-bonfire-discussion-handler.ts
+++ src/http/form-submission-handlers/start-bonfire-discussion-handler.ts
@@ -31,6 +31,7 @@ export const startBonfireDiscussionHandler = (dependencies: Dependencies): Middl
 
   if (E.isLeft(result)) {
     context.redirect('/action-failed');
+    return;
   }
 
   if (E.isRight(result)) {
