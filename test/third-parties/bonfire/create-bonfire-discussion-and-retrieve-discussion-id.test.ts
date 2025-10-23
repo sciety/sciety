8edb5e31e0a6365e4326ee1bcff11bdfc8b9ca95
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-23 15:11:35 +0100

    Begin to the write a test for the new query to avoid regression #4077

---
+++ test/third-parties/bonfire/create-bonfire-discussion-and-retrieve-discussion-id.test.ts
@@ -0,0 +1,9 @@
+describe('create-bonfire-discussion-and-retrieve-discussion-id', () => {
+  describe('when an id with a correct type is returned', () => {
+    it.todo('includes the id in the query response');
+  });
+
+  describe('when an id is not returned', () => {
+    it.todo('returns an error');
+  });
+});
