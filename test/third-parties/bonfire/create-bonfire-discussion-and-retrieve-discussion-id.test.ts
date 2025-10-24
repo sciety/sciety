6d8ceb522b63f0471f3a2a0252a7d1e85ad7a186
David Moulton <d.moulton@elifesciences.org>
2025-10-24 13:41:22 +0100

    Introduce new jest tests mocking the bonfire dependencies #4077

--- test/third-parties/bonfire/create-bonfire-discussion-and-retrieve-discussion-id.test.ts
+++ test/third-parties/bonfire/create-bonfire-discussion-and-retrieve-discussion-id.test.ts
@@ -1,6 +1,52 @@
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { createBonfireDiscussionAndRetrieveDiscussionId } from '../../../src/third-parties/bonfire';
+import { generateAuthenticationHeaders } from '../../../src/third-parties/bonfire/generate-authentication-headers';
+import { ExpressionDoi } from '../../../src/types/expression-doi';
+import { dummyLogger } from '../../dummy-logger';
+import { arbitraryString } from '../../helpers';
+import { shouldNotBeCalled } from '../../should-not-be-called';
+import { arbitraryExpressionDoi } from '../../types/expression-doi.helper';
+import {postDataBonfire} from '../../../src/third-parties/bonfire/post-data-bonfire';
+
+jest.mock('../../../src/third-parties/bonfire/generate-authentication-headers');
+jest.mock('../../../src/third-parties/bonfire/post-data-bonfire');
+
+const invokeCreateBonfireDiscussionIdAndRetrieveDiscussionId = async (expressionDoi: ExpressionDoi) => pipe(
+  expressionDoi,
+  createBonfireDiscussionAndRetrieveDiscussionId(dummyLogger),
+  TE.getOrElse(shouldNotBeCalled),
+)();
+
 describe('create-bonfire-discussion-and-retrieve-discussion-id', () => {
+  beforeEach(() => {
+    (jest.mocked(generateAuthenticationHeaders)).mockReturnValueOnce(TE.right({}));
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   describe('when an id with a correct type is returned', () => {
-    it.todo('includes the id in the query response');
+    const bonfireDiscussionId = arbitraryString();
+    const doiOfDiscussedArticle = arbitraryExpressionDoi();
+    let result: Awaited<ReturnType<typeof invokeCreateBonfireDiscussionIdAndRetrieveDiscussionId>>;
+
+    (jest.mocked(postDataBonfire)).mockReturnValueOnce(() => TE.right({
+      data: {
+        addMediaByUri: {
+          id: bonfireDiscussionId,
+        },
+      },
+    }));
+
+    beforeEach(async () => {
+      result = await invokeCreateBonfireDiscussionIdAndRetrieveDiscussionId(doiOfDiscussedArticle);
+    });
+
+    it('includes the id in the query response', () => {
+      expect(result).toStrictEqual(bonfireDiscussionId);
+    });
   });
 
   describe('when an id is not returned', () => {
