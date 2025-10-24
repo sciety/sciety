e996a290faa3a9f617a85fb65f71b96ec42bafb5
LinaKind <l.kinduryte@elifesciences.org>
2025-10-24 15:53:09 +0300

    Attempt to remove jest error, does not recognise the ExpressionDoi type

--- test/third-parties/bonfire/create-bonfire-discussion-and-retrieve-discussion-id.test.ts
+++ test/third-parties/bonfire/create-bonfire-discussion-and-retrieve-discussion-id.test.ts
@@ -2,18 +2,19 @@ import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { createBonfireDiscussionAndRetrieveDiscussionId } from '../../../src/third-parties/bonfire';
 import { generateAuthenticationHeaders } from '../../../src/third-parties/bonfire/generate-authentication-headers';
+import { postDataBonfire } from '../../../src/third-parties/bonfire/post-data-bonfire';
 import { ExpressionDoi } from '../../../src/types/expression-doi';
 import { dummyLogger } from '../../dummy-logger';
 import { arbitraryString } from '../../helpers';
 import { shouldNotBeCalled } from '../../should-not-be-called';
 import { arbitraryExpressionDoi } from '../../types/expression-doi.helper';
-import {postDataBonfire} from '../../../src/third-parties/bonfire/post-data-bonfire';
 
 jest.mock('../../../src/third-parties/bonfire/generate-authentication-headers');
 jest.mock('../../../src/third-parties/bonfire/post-data-bonfire');
 
-const invokeCreateBonfireDiscussionIdAndRetrieveDiscussionId = async (expressionDoi: ExpressionDoi) => pipe(
-  expressionDoi,
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const invokeCreateBonfireDiscussionIdAndRetrieveDiscussionId = async (expressionDoi: any) => pipe(
+  expressionDoi as ExpressionDoi,
   createBonfireDiscussionAndRetrieveDiscussionId(dummyLogger),
   TE.getOrElse(shouldNotBeCalled),
 )();
@@ -32,16 +33,22 @@ describe('create-bonfire-discussion-and-retrieve-discussion-id', () => {
     const doiOfDiscussedArticle = arbitraryExpressionDoi();
     let result: Awaited<ReturnType<typeof invokeCreateBonfireDiscussionIdAndRetrieveDiscussionId>>;
 
-    (jest.mocked(postDataBonfire)).mockReturnValueOnce(() => TE.right({
-      data: {
-        addMediaByUri: {
-          id: bonfireDiscussionId,
-        },
-      },
-    }));
-
     beforeEach(async () => {
-      result = await invokeCreateBonfireDiscussionIdAndRetrieveDiscussionId(doiOfDiscussedArticle);
+      type ExpressionDoi = string & { readonly ExpressionDoi: unique symbol };
+      (jest.mocked(postDataBonfire)).mockReturnValueOnce(() => TE.right({
+        data: {
+          addMediaByUri: {
+            id: bonfireDiscussionId,
+          },
+        },
+      }));
+      result = await invokeCreateBonfireDiscussionIdAndRetrieveDiscussionId(
+        doiOfDiscussedArticle as unknown as ExpressionDoi,
+      );
+    });
+
+    afterEach(() => {
+      jest.resetAllMocks();
     });
 
     it('includes the id in the query response', () => {
