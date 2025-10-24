ca85a0241d2b97a53f6cd0da73b67dd6ea297ed4
David Moulton <d.moulton@elifesciences.org>
2025-10-24 13:57:51 +0100

    Introduce some test coverage for the new feature to prevent regression #4077

--- test/third-parties/bonfire/create-bonfire-discussion-and-retrieve-discussion-id.test.ts
+++ test/third-parties/bonfire/create-bonfire-discussion-and-retrieve-discussion-id.test.ts
@@ -1,9 +1,61 @@
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import { createBonfireDiscussionAndRetrieveDiscussionId } from '../../../src/third-parties/bonfire';
+import { generateAuthenticationHeaders } from '../../../src/third-parties/bonfire/generate-authentication-headers';
+import { postDataBonfire } from '../../../src/third-parties/bonfire/post-data-bonfire';
+import * as DE from '../../../src/types/data-error';
+import { dummyLogger } from '../../dummy-logger';
+import { arbitraryExpressionDoi } from '../../types/expression-doi.helper';
+
+jest.mock('../../../src/third-parties/bonfire/generate-authentication-headers');
+jest.mock('../../../src/third-parties/bonfire/post-data-bonfire');
+
 describe('create-bonfire-discussion-and-retrieve-discussion-id', () => {
+  beforeEach(() => {
+    (jest.mocked(generateAuthenticationHeaders)).mockReturnValueOnce(TE.right({}));
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   describe('when an id with a correct type is returned', () => {
-    it.todo('includes the id in the query response');
+    it('includes the id in the query response', async () => {
+      (jest.mocked(postDataBonfire)).mockReturnValueOnce(() => TE.right({
+        data: {
+          addMediaByUri: {
+            id: '1234',
+          },
+        },
+      }));
+
+      await expect(
+        createBonfireDiscussionAndRetrieveDiscussionId(dummyLogger)(arbitraryExpressionDoi())(),
+      ).resolves.toStrictEqual(E.right('1234'));
+    });
   });
 
   describe('when an id is not returned', () => {
-    it.todo('returns an error');
+    it('returns an error', async () => {
+      (jest.mocked(postDataBonfire)).mockReturnValueOnce(() => TE.left(DE.notFound));
+
+      await expect(
+        createBonfireDiscussionAndRetrieveDiscussionId(dummyLogger)(arbitraryExpressionDoi())(),
+      ).resolves.toStrictEqual(E.left('not-found'));
+    });
+
+    it('returns an error, if the response does not match codec', async () => {
+      (jest.mocked(postDataBonfire)).mockReturnValueOnce(() => TE.right({
+        data: {
+          addMediaByUri: {
+            id: null,
+          },
+        },
+      }));
+
+      await expect(
+        createBonfireDiscussionAndRetrieveDiscussionId(dummyLogger)(arbitraryExpressionDoi())(),
+      ).resolves.toStrictEqual(E.left('unavailable'));
+    });
   });
 });
