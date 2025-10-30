0397bacaac9c23713ad0de0383544d9835adca7b
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-30 15:41:52 +0000

    Describe intent with a todo test list #4093

---
+++ test/ingest/evaluation-discovery/coar/transform-coar-notification-uri-to-announcement-action-uri.test.ts
@@ -0,0 +1,13 @@
+describe('transform-coar-notification-uri-to-announcement-action-uri', () => {
+  describe('when the coar notification uri returns a 4xx or 5xx status code', () => {
+    it.todo('returns on the left');
+  });
+
+  describe('when the coar notification uri returns an announcement', () => {
+    it.todo('returns an announcement action uri');
+  });
+
+  describe('when the coar notification uri returns data that is not an announcement', () => {
+    it.todo('returns on the left');
+  });
+});
