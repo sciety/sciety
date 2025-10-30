5f9b21868c5a1273ea1de30069f389a0f6fb07a1
LinaKind <l.kinduryte@elifesciences.org>
2025-10-30 18:00:27 +0200

    Begin to describe a desired failure mode with a partial skipped test #4093

--- test/ingest/evaluation-discovery/coar/transform-coar-notification-uri-to-announcement-action-uri.test.ts
+++ test/ingest/evaluation-discovery/coar/transform-coar-notification-uri-to-announcement-action-uri.test.ts
@@ -1,6 +1,23 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import { transformCoarNotificationUriToAnnouncementActionUri } from '../../../../src/ingest/evaluation-discovery/coar/transform-coar-notification-uri-to-announcement-action-uri';
+import { arbitraryUri } from '../../../helpers';
+
 describe('transform-coar-notification-uri-to-announcement-action-uri', () => {
   describe('when the coar notification uri returns a 4xx or 5xx status code', () => {
-    it.todo('returns on the left');
+    const coarNotificationUri = arbitraryUri();
+    let result: E.Either<string, string>;
+
+    beforeEach(async () => {
+      result = await pipe(
+        coarNotificationUri,
+        transformCoarNotificationUriToAnnouncementActionUri,
+      )();
+    });
+
+    it.skip('returns on the left', () => {
+      expect(E.isLeft(result)).toBe(true);
+    });
   });
 
   describe('when the coar notification uri returns an announcement', () => {
