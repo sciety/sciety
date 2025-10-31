6f843ecac21ec3ef1abb2213eab3cc8aa076bf2e
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-31 15:47:49 +0000

    Expand the interface of the new query in preparation to move it to it's own file #4093

--- src/ingest/evaluation-discovery/coar/discover-pci-evaluations.ts
+++ src/ingest/evaluation-discovery/coar/discover-pci-evaluations.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import {
@@ -6,14 +5,15 @@ import {
 } from './transform-coar-notification-uri-to-announcement-action-uri';
 import { DiscoverPublishedEvaluations } from '../../discover-published-evaluations';
 
-const transformAnnouncementActionUriToSignpostingDocmapUri = () => TE.right('');
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const transformAnnouncementActionUriToSignpostingDocmapUri = (announcementActionUri: string): TE.TaskEither<string, string> => TE.right('');
 
 export const discoverPciEvaluations: DiscoverPublishedEvaluations = () => (
   dependencies,
 ) => pipe(
   'https://inbox-sciety-prod.elifesciences.org/inbox/urn:uuid:bf3513ee-1fef-4f30-a61b-20721b505f11',
   transformCoarNotificationUriToAnnouncementActionUri(dependencies),
-  transformAnnouncementActionUriToSignpostingDocmapUri,
+  TE.chain(transformAnnouncementActionUriToSignpostingDocmapUri),
   TE.map(() => ({
     understood: [],
     skipped: [],
