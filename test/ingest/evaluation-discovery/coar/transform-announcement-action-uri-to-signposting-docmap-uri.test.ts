8ef1fb52badbe21c8a0a1159f5983e6e72ebe72f
Nathan Lisgo <nathan@lisgo.co.uk>
2025-11-03 13:20:55 +0000

    Begin to write an unhappy path test for transformAnnouncementActionUriToSignpostingDocmapUri #4093

--- test/ingest/evaluation-discovery/coar/transform-announcement-action-uri-to-signposting-docmap-uri.test.ts
+++ test/ingest/evaluation-discovery/coar/transform-announcement-action-uri-to-signposting-docmap-uri.test.ts
@@ -1,10 +1,29 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import {
+  transformAnnouncementActionUriToSignpostingDocmapUri,
+} from '../../../../src/ingest/evaluation-discovery/coar/transform-announcement-action-uri-to-signposting-docmap-uri';
+import { arbitraryUri } from '../../../helpers';
+
 describe('transform-announcement-action-uri-to-signposting-docmap-uri', () => {
   describe('when the announcement action uri HEAD request returns a link header with a Signposting DocMap URI', () => {
     it.todo('returns a Signposting DocMap URI');
   });
 
   describe('when the announcement action uri HEAD request fails', () => {
-    it.todo('returns on the left');
+    const announcementActionUri = arbitraryUri();
+    let result: E.Either<string, string>;
+
+    beforeEach(async () => {
+      result = await pipe(
+        announcementActionUri,
+        transformAnnouncementActionUriToSignpostingDocmapUri,
+      )();
+    });
+
+    it.skip('returns on the left', () => {
+      expect(E.isLeft(result)).toBe(true);
+    });
   });
 
   describe('when the announcement action uri HEAD request does not return a link header', () => {
