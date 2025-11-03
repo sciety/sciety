1bdfd80554a661e87419435744959f2e0f56f6fa
David Moulton <d.moulton@elifesciences.org>
2025-11-03 11:57:51 +0000

    Introduce a test todos list to declare intention #4093

---
+++ test/ingest/evaluation-discovery/coar/transform-announcement-action-uri-to-signposting-docmap-uri.test.ts
@@ -0,0 +1,17 @@
+describe('transform-announcement-action-uri-to-signposting-docmap-uri', () => {
+  describe('when the announcement action uri HEAD request returns a link header with a Signposting DocMap URI', () => {
+    it.todo('returns a Signposting DocMap URI');
+  });
+
+  describe('when the announcement action uri HEAD request fails', () => {
+    it.todo('returns on the left');
+  });
+
+  describe('when the announcement action uri HEAD request does not return a link header', () => {
+    it.todo('returns on the left');
+  });
+
+  describe('when the announcement action uri HEAD request returns a link header without a Signposting DocMap URI', () => {
+    it.todo('returns on the left');
+  });
+});
