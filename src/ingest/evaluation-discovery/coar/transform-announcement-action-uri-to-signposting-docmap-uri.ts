c2f6de3eb7840201b3b6b91053b199b63df340b1
David Moulton <d.moulton@elifesciences.org>
2025-11-04 14:27:10 +0000

    Begin parsing of link header to detect signposting docmap uri #4093

--- src/ingest/evaluation-discovery/coar/transform-announcement-action-uri-to-signposting-docmap-uri.ts
+++ src/ingest/evaluation-discovery/coar/transform-announcement-action-uri-to-signposting-docmap-uri.ts
@@ -1,8 +1,18 @@
+import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as t from 'io-ts';
 import { Dependencies } from '../../discover-published-evaluations';
 import { decodeAndReportFailures } from '../decode-and-report-failures';
+
+// <https://neuro.peercommunityin.org/metadata/docmaps?article_id=217>; rel="describedby" type="application/ld+json" profile="https://w3id.org/docmaps/context.jsonld", <https://neuro.peercommunityin.org/metadata/crossref?article_id=217>; rel="describedby" type="application/xml" profile="http://www.crossref.org/schema/4.3.7"
+
+// <https://neuro.peercommunityin.org/metadata/docmaps?article_id=217>; rel="describedby" type="application/ld+json" profile="https://w3id.org/docmaps/context.jsonld"
+// <https://neuro.peercommunityin.org/metadata/crossref?article_id=217>; rel="describedby" type="application/xml" profile="http://www.crossref.org/schema/4.3.7"
+
+// <https://neuro.peercommunityin.org/metadata/docmaps?article_id=217>; rel="describedby" type="application/ld+json" profile="https://w3id.org/docmaps/context.jsonld"
+
+// https://neuro.peercommunityin.org/metadata/docmaps?article_id=217
 
 const headCodec = t.strict({
   link: t.string,
@@ -16,5 +26,9 @@ export const transformAnnouncementActionUriToSignpostingDocmapUri = (
   announcementActionUri,
   dependencies.fetchHead,
   TE.chainEitherK(decodeAndReportFailures(headCodec)),
+  TE.map((head) => head.link.split(/,\s*/)),
+  TE.map(RA.filter((link) => link.match(/<http[^>]+>/) !== null)),
+  TE.map(RA.filter((link) => link.match(/(^|\s)rel="describedby"/) !== null)),
+  TE.map(RA.filter((link) => link.match(/(^|\s)profile="https:\/\/w3id\.org\/docmaps\/context\.jsonld"/) !== null)),
   TE.map(() => ''),
 );
