8cc0184940a0665d6ded00a04cc52e8acf7a1b58
nlisgo <n.lisgo@elifesciences.org>
2025-11-08 01:29:32 +0000

    Refactor DocMap URI extraction with functional programming

--- src/ingest/evaluation-discovery/coar/transform-announcement-action-uri-to-signposting-docmap-uri.ts
+++ src/ingest/evaluation-discovery/coar/transform-announcement-action-uri-to-signposting-docmap-uri.ts
@@ -1,3 +1,5 @@
+import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
@@ -5,18 +7,42 @@ import * as t from 'io-ts';
 import { Dependencies } from '../../discover-published-evaluations';
 import { decodeAndReportFailures } from '../decode-and-report-failures';
 
-// <https://neuro.peercommunityin.org/metadata/docmaps?article_id=217>; rel="describedby" type="application/ld+json" profile="https://w3id.org/docmaps/context.jsonld", <https://neuro.peercommunityin.org/metadata/crossref?article_id=217>; rel="describedby" type="application/xml" profile="http://www.crossref.org/schema/4.3.7"
-
-// <https://neuro.peercommunityin.org/metadata/docmaps?article_id=217>; rel="describedby" type="application/ld+json" profile="https://w3id.org/docmaps/context.jsonld"
-// <https://neuro.peercommunityin.org/metadata/crossref?article_id=217>; rel="describedby" type="application/xml" profile="http://www.crossref.org/schema/4.3.7"
-
-// <https://neuro.peercommunityin.org/metadata/docmaps?article_id=217>; rel="describedby" type="application/ld+json" profile="https://w3id.org/docmaps/context.jsonld"
-
-// https://neuro.peercommunityin.org/metadata/docmaps?article_id=217
-
 const headCodec = t.strict({
   link: t.string,
 });
+
+const testLinkWithRegexp = (
+  regexp: RegExp,
+) => (
+  link: string,
+) => regexp.test(link);
+
+const escapeForRegexp = (value: string) => value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+const linkHasAttribute = (
+  attribute: 'rel' | 'profile',
+  value: string,
+) => testLinkWithRegexp(new RegExp(`(^|\\s)${attribute}="${escapeForRegexp(value)}"`));
+
+const linkHasUri = testLinkWithRegexp(/<http[^>]+>/);
+
+const linkHasDocmapUri = (link: string) => pipe(
+  O.some(link),
+  O.filter(linkHasUri),
+  O.filter(linkHasAttribute('rel', 'describedby')),
+  O.filter(linkHasAttribute('profile', 'https://w3id.org/docmaps/context.jsonld')),
+  O.map(
+    (linkWithDocmapUri) => linkWithDocmapUri.replace(/^.*<(http[^>]+)>.*$/, '$1'),
+  ),
+);
+
+const docmapUri = (
+  headerLink: string,
+) => pipe(
+  headerLink.split(/,\s*/),
+  RA.findFirstMap(linkHasDocmapUri),
+  E.fromOption(() => 'No DocMap uri found'),
+);
 
 export const transformAnnouncementActionUriToSignpostingDocmapUri = (
   dependencies: Dependencies,
@@ -25,11 +51,7 @@ export const transformAnnouncementActionUriToSignpostingDocmapUri = (
 ): TE.TaskEither<string, string> => pipe(
   announcementActionUri,
   dependencies.fetchHead,
-  TE.chainEitherK(decodeAndReportFailures(headCodec)),
-  TE.map((head) => head.link.split(/,\s*/)),
-  TE.map(RA.filter((link) => link.match(/<http[^>]+>/) !== null)),
-  TE.map(RA.filter((link) => link.match(/(^|\s)rel="describedby"/) !== null)),
-  TE.map(RA.filter((link) => link.match(/(^|\s)profile="https:\/\/w3id\.org\/docmaps\/context\.jsonld"/) !== null)),
-  TE.map(RA.map((link) => link.replace(/^.*<(http[^>]+)>.*$/, '$1'))),
-  TE.flatMap((links) => (links.length > 0 ? TE.right(links[0]) : TE.left(''))),
+  TE.flatMapEither(decodeAndReportFailures(headCodec)),
+  TE.map(({ link }) => link),
+  TE.flatMapEither(docmapUri),
 );
