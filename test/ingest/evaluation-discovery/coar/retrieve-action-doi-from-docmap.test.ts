1a7afa605f642790f953aa488ce323abde2f1482
nlisgo <n.lisgo@elifesciences.org>
2025-11-12 13:50:10 +0000

    Write a failing test #4093

--- test/ingest/evaluation-discovery/coar/retrieve-action-doi-from-docmap.test.ts
+++ test/ingest/evaluation-discovery/coar/retrieve-action-doi-from-docmap.test.ts
@@ -1,6 +1,30 @@
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import {
+  retrieveActionDoiFromDocmap,
+} from '../../../../src/ingest/evaluation-discovery/coar/retrieve-action-doi-from-docmap';
+import { arbitraryUri } from '../../../helpers';
+import { shouldNotBeCalled } from '../../../should-not-be-called';
+
 describe('retrieve-action-doi-from-docmap', () => {
   describe('when the request to the docmap uri fails', () => {
-    it.todo('returns on the left');
+    const docmapUri = arbitraryUri();
+    let result: E.Either<string, string>;
+
+    beforeEach(async () => {
+      result = await pipe(
+        docmapUri,
+        retrieveActionDoiFromDocmap({
+          fetchData: () => TE.left('fetch data fails for any reason'),
+          fetchHead: () => TE.right(shouldNotBeCalled()),
+        }),
+      )();
+    });
+
+    it.failing('returns on the left', () => {
+      expect(E.isLeft(result)).toBe(true);
+    });
   });
 
   describe('when the request to the docmap uri returns a docmap with an action doi', () => {
