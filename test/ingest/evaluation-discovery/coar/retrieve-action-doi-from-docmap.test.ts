ccf614a9f5e6b5ef93e7cf713ad02a83de94b67e
LinaKind <l.kinduryte@elifesciences.org>
2025-11-12 15:42:39 +0200

    Express intended behaviour in todo tests #4093

---
+++ test/ingest/evaluation-discovery/coar/retrieve-action-doi-from-docmap.test.ts
@@ -0,0 +1,13 @@
+describe('retrieve-action-doi-from-docmap', () => {
+  describe('when the request to the docmap uri fails', () => {
+    it.todo('returns on the left');
+  });
+
+  describe('when the request to the docmap uri returns a docmap with an action doi', () => {
+    it.todo('returns an action doi');
+  });
+
+  describe('when the request to the docmap uri returns a docmap without an action doi', () => {
+    it.todo('returns on the left');
+  });
+});
