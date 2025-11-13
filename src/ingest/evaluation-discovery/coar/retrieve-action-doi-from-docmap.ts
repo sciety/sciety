df837e47023272fa9c2a3e98496bdc1239e21227
David Moulton <d.moulton@elifesciences.org>
2025-11-13 10:36:46 +0000

    Verify that the docmap request returns an array #4093

--- src/ingest/evaluation-discovery/coar/retrieve-action-doi-from-docmap.ts
+++ src/ingest/evaluation-discovery/coar/retrieve-action-doi-from-docmap.ts
@@ -1,6 +1,8 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
 import { Dependencies } from '../../discover-published-evaluations';
+import { decodeAndReportFailures } from '../decode-and-report-failures';
 
 export const retrieveActionDoiFromDocmap = (
   dependencies: Dependencies,
@@ -9,4 +11,5 @@ export const retrieveActionDoiFromDocmap = (
 ): TE.TaskEither<string, unknown> => pipe(
   docmapUri,
   dependencies.fetchData<JSON>,
+  TE.flatMapEither(decodeAndReportFailures(t.readonlyArray(t.unknown))),
 );
