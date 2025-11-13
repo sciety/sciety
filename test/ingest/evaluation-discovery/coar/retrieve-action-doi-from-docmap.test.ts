bf715898def9e6e702845b8ddea025a4f20c5580
David Moulton <d.moulton@elifesciences.org>
2025-11-13 10:56:55 +0000

    Write a failing test #4093

--- test/ingest/evaluation-discovery/coar/retrieve-action-doi-from-docmap.test.ts
+++ test/ingest/evaluation-discovery/coar/retrieve-action-doi-from-docmap.test.ts
@@ -68,6 +68,25 @@ describe('retrieve-action-doi-from-docmap', () => {
     });
   });
 
+  describe('when the request to the docmap uri returns an empty array', () => {
+    const docmapUri = arbitraryUri();
+    let result: E.Either<string, unknown>;
+
+    beforeEach(async () => {
+      result = await pipe(
+        docmapUri,
+        retrieveActionDoiFromDocmap({
+          fetchData: <D>() => TE.right([] as D),
+          fetchHead: () => TE.right(shouldNotBeCalled()),
+        }),
+      )();
+    });
+
+    it.failing('returns on the left', () => {
+      expect(E.isLeft(result)).toBe(true);
+    });
+  });
+
   describe('when the request to the docmap uri returns a docmap without an action doi', () => {
     it.todo('returns on the left');
   });
