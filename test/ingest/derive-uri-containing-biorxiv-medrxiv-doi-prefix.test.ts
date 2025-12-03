5e099cbdff007a33c641233a2e418d5ebddd932c
nlisgo <n.lisgo@elifesciences.org>
2025-12-03 13:28:22 +0000

    Begin to implement an unhappy path test #4116

--- test/ingest/derive-uri-containing-biorxiv-medrxiv-doi-prefix.test.ts
+++ test/ingest/derive-uri-containing-biorxiv-medrxiv-doi-prefix.test.ts
@@ -1,9 +1,19 @@
+import * as E from 'fp-ts/Either';
+import {
+  deriveUriContainingBiorxivMedrxivDoiPrefix,
+} from '../../src/ingest/derive-uri-containing-biorxiv-medrxiv-doi-prefix';
+import { arbitraryUri } from '../helpers';
+
 describe('derive-uri-containing-biorxiv-medrxiv-doi-prefix', () => {
   describe('when a URI containing a biorxiv or medrxiv DOI prefix is fetched', () => {
     it.todo('returns URI on the right');
   });
 
   describe('when a URI that does not contain a biorxiv or medrxiv DOI prefix is fetched', () => {
-    it.todo('returns on the left');
+    const result = deriveUriContainingBiorxivMedrxivDoiPrefix(arbitraryUri());
+
+    it.skip('returns on the left', () => {
+      expect(E.isLeft(result)).toBe(true);
+    });
   });
 });
