3d5eaaa9afc135a69ae3fe8d49ba0503bfd4032f
nlisgo <n.lisgo@elifesciences.org>
2025-12-04 14:34:40 +0000

    Wire up in preparation to introduce additional logic for BioRxiv MedRxiv uris #4116

--- src/ingest/evaluation-discovery/discover-evaluations-from-hypothesis-group.ts
+++ src/ingest/evaluation-discovery/discover-evaluations-from-hypothesis-group.ts
@@ -4,6 +4,7 @@ import { pipe } from 'fp-ts/function';
 import * as Hyp from './hypothesis';
 import { convertHypothesisAnnotationToEvaluation } from './hypothesis/convert-hypothesis-annotation-to-evaluation';
 import { ingestionWindowStartDate } from './ingestion-window-start-date';
+import { deriveUriContainingBiorxivMedrxivDoiPrefix } from '../derive-uri-containing-biorxiv-medrxiv-doi-prefix';
 import { DiscoverPublishedEvaluations } from '../discover-published-evaluations';
 import { tagToEvaluationTypeMap } from '../tag-to-evaluation-type-map';
 
@@ -16,6 +17,7 @@ export const discoverEvaluationsFromHypothesisGroup = (
     ingestionWindowStartDate(ingestDays, avoidWhenPublishedBefore),
     dependencies.fetchData,
   ),
+  TE.flatMap(RA.traverse(TE.ApplicativePar)(deriveUriContainingBiorxivMedrxivDoiPrefix(dependencies))),
   TE.map(RA.map(convertHypothesisAnnotationToEvaluation(tagToEvaluationTypeMap))),
   TE.map((parts) => ({
     understood: RA.rights(parts),
