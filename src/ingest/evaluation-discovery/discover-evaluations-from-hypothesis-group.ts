6dd7aa0db4857b76f6031f1c0fecefc619d647ea
LinaKind <l.kinduryte@elifesciences.org>
2025-12-05 16:38:05 +0200

    Further extract function to its own module #4116

--- src/ingest/evaluation-discovery/discover-evaluations-from-hypothesis-group.ts
+++ src/ingest/evaluation-discovery/discover-evaluations-from-hypothesis-group.ts
@@ -1,29 +1,12 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
-import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
 import * as Hyp from './hypothesis';
-import { Annotation } from './hypothesis/annotation';
 import { convertHypothesisAnnotationToEvaluation } from './hypothesis/convert-hypothesis-annotation-to-evaluation';
 import { ingestionWindowStartDate } from './ingestion-window-start-date';
-import { isUriFromBiorxivMedrxiv } from './is-uri-from-biorxiv-medrxiv';
-import { uriIsMissingBiorxivMedrxivDoiPrefix } from './uri-is-missing-biorxiv-medrxiv-doi-prefix';
-import { deriveUriContainingBiorxivMedrxivDoiPrefix } from '../derive-uri-containing-biorxiv-medrxiv-doi-prefix';
-import { Dependencies, DiscoverPublishedEvaluations } from '../discover-published-evaluations';
+import { refineIfNecessaryAnnotationUriForBiorxivMedrxiv } from './refine-if-necessary-annotation-uri-for-biorxiv-medrxiv';
+import { DiscoverPublishedEvaluations } from '../discover-published-evaluations';
 import { tagToEvaluationTypeMap } from '../tag-to-evaluation-type-map';
-
-const refineIfNecessaryAnnotationUriForBiorxivMedrxiv = (
-  dependencies: Dependencies,
-) => (
-  annotation: Annotation,
-) => pipe(
-  annotation.uri,
-  (uri) => isUriFromBiorxivMedrxiv(uri) && uriIsMissingBiorxivMedrxivDoiPrefix(uri),
-  B.match(
-    () => TE.right(annotation),
-    () => (process.env.EXPERIMENT_ENABLED === 'true' ? deriveUriContainingBiorxivMedrxivDoiPrefix(dependencies)(annotation) : TE.right(annotation)),
-  ),
-);
 
 export const discoverEvaluationsFromHypothesisGroup = (
   publisherGroupId: string,
---
+++ src/ingest/evaluation-discovery/refine-if-necessary-annotation-uri-for-biorxiv-medrxiv.ts
@@ -0,0 +1,21 @@
+import * as TE from 'fp-ts/TaskEither';
+import * as B from 'fp-ts/boolean';
+import { pipe } from 'fp-ts/function';
+import { Annotation } from './hypothesis/annotation';
+import { isUriFromBiorxivMedrxiv } from './is-uri-from-biorxiv-medrxiv';
+import { uriIsMissingBiorxivMedrxivDoiPrefix } from './uri-is-missing-biorxiv-medrxiv-doi-prefix';
+import { deriveUriContainingBiorxivMedrxivDoiPrefix } from '../derive-uri-containing-biorxiv-medrxiv-doi-prefix';
+import { Dependencies } from '../discover-published-evaluations';
+
+export const refineIfNecessaryAnnotationUriForBiorxivMedrxiv = (
+  dependencies: Dependencies,
+) => (
+  annotation: Annotation,
+): TE.TaskEither<string, Annotation> => pipe(
+  annotation.uri,
+  (uri) => isUriFromBiorxivMedrxiv(uri) && uriIsMissingBiorxivMedrxivDoiPrefix(uri),
+  B.match(
+    () => TE.right(annotation),
+    () => (process.env.EXPERIMENT_ENABLED === 'true' ? deriveUriContainingBiorxivMedrxivDoiPrefix(dependencies)(annotation) : TE.right(annotation)),
+  ),
+);
