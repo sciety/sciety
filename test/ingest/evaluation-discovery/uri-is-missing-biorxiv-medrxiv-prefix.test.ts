e70b1c9864c6f7bfa9d60dd99ac550e8a557e87a
LinaKind <l.kinduryte@elifesciences.org>
2025-12-05 13:54:21 +0200

    Express intent with todo tests #4116

---
+++ test/ingest/evaluation-discovery/uri-is-missing-biorxiv-medrxiv-prefix.test.ts
@@ -0,0 +1,13 @@
+describe('uri-is-missing-biorxiv-medrxiv-prefix', () => {
+  describe('when the uri contains the openrxiv DOI prefix (10.64898)', () => {
+    it.todo('returns false');
+  });
+
+  describe('when the uri contains the Cold Spring Harbor Press DOI prefix (10.1101)', () => {
+    it.todo('returns false');
+  });
+
+  describe('when the uri contains neither the DOI prefixes for openrxiv nor Cold Spring Harbor Press', () => {
+    it.todo('returns true');
+  });
+});
