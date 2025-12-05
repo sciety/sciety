b2e2850665c25d645e2672a200e5711009944030
LinaKind <l.kinduryte@elifesciences.org>
2025-12-05 15:15:31 +0200

    Describe behaviour with a naively passing test #4116

--- test/ingest/evaluation-discovery/uri-is-missing-biorxiv-medrxiv-prefix.test.ts
+++ test/ingest/evaluation-discovery/uri-is-missing-biorxiv-medrxiv-prefix.test.ts
@@ -5,7 +5,11 @@ import { arbitraryUri } from '../../helpers';
 
 describe('uri-is-missing-biorxiv-medrxiv-prefix', () => {
   describe('when the uri contains the openrxiv DOI prefix (10.64898)', () => {
-    it.todo('returns false');
+    const result = uriIsMissingBiorxivMedrxivDoiPrefix('https://www.medrxiv.org/content/10.64898/2021.06.18.21258689v1');
+
+    it('returns false', () => {
+      expect(result).toBe(false);
+    });
   });
 
   describe('when the uri contains the Cold Spring Harbor Press DOI prefix (10.1101)', () => {
