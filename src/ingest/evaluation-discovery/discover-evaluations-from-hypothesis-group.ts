ca2ba7ce0113e86868d2a0330fc06044db50d09f
David Moulton <d.moulton@elifesciences.org>
2025-12-05 14:31:38 +0000

    Extract to tell a better story #4116

--- src/ingest/evaluation-discovery/discover-evaluations-from-hypothesis-group.ts
+++ src/ingest/evaluation-discovery/discover-evaluations-from-hypothesis-group.ts
@@ -3,13 +3,27 @@ import * as TE from 'fp-ts/TaskEither';
 import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
 import * as Hyp from './hypothesis';
+import { Annotation } from './hypothesis/annotation';
 import { convertHypothesisAnnotationToEvaluation } from './hypothesis/convert-hypothesis-annotation-to-evaluation';
 import { ingestionWindowStartDate } from './ingestion-window-start-date';
 import { isUriFromBiorxivMedrxiv } from './is-uri-from-biorxiv-medrxiv';
 import { uriIsMissingBiorxivMedrxivDoiPrefix } from './uri-is-missing-biorxiv-medrxiv-doi-prefix';
 import { deriveUriContainingBiorxivMedrxivDoiPrefix } from '../derive-uri-containing-biorxiv-medrxiv-doi-prefix';
-import { DiscoverPublishedEvaluations } from '../discover-published-evaluations';
+import { Dependencies, DiscoverPublishedEvaluations } from '../discover-published-evaluations';
 import { tagToEvaluationTypeMap } from '../tag-to-evaluation-type-map';
+
+const refineIfNecessaryAnnotationUriForBiorxivMedrxiv = (
+  dependencies: Dependencies,
+) => (
+  annotation: Annotation,
+) => pipe(
+  annotation.uri,
+  (uri) => isUriFromBiorxivMedrxiv(uri) && uriIsMissingBiorxivMedrxivDoiPrefix(uri),
+  B.match(
+    () => TE.right(annotation),
+    () => (process.env.EXPERIMENT_ENABLED === 'true' ? deriveUriContainingBiorxivMedrxivDoiPrefix(dependencies)(annotation) : TE.right(annotation)),
+  ),
+);
 
 export const discoverEvaluationsFromHypothesisGroup = (
   publisherGroupId: string,
@@ -20,14 +34,7 @@ export const discoverEvaluationsFromHypothesisGroup = (
     ingestionWindowStartDate(ingestDays, avoidWhenPublishedBefore),
     dependencies.fetchData,
   ),
-  TE.flatMap(TE.traverseArray((annotation) => pipe(
-    annotation.uri,
-    (uri) => isUriFromBiorxivMedrxiv(uri) && uriIsMissingBiorxivMedrxivDoiPrefix(uri),
-    B.match(
-      () => TE.right(annotation),
-      () => (process.env.EXPERIMENT_ENABLED === 'true' ? deriveUriContainingBiorxivMedrxivDoiPrefix(dependencies)(annotation) : TE.right(annotation)),
-    ),
-  ))),
+  TE.flatMap(TE.traverseArray(refineIfNecessaryAnnotationUriForBiorxivMedrxiv(dependencies))),
   TE.map(RA.map(convertHypothesisAnnotationToEvaluation(tagToEvaluationTypeMap))),
   TE.map((parts) => ({
     understood: RA.rights(parts),
