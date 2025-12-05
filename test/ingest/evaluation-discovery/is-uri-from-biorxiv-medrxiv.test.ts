1510ecd2f66dc97c0563321d014d3154df9fa855
nlisgo <n.lisgo@elifesciences.org>
2025-12-05 10:58:40 +0000

    Begin to write test list to cover existing functionality #4116

---
+++ test/ingest/evaluation-discovery/is-uri-from-biorxiv-medrxiv.test.ts
@@ -0,0 +1,19 @@
+describe('is-uri-from-biorxiv-medrxiv', () => {
+  describe('given input can be parsed as a url', () => {
+    describe('and contains biorxiv hostname', () => {
+      it.todo('returns true');
+    });
+
+    describe('and contains medrxiv hostname', () => {
+      it.todo('returns true');
+    });
+
+    describe('and does not contain biorxiv nor medrxiv hostname', () => {
+      it.todo('returns false');
+    });
+  });
+
+  describe('given input can not be parsed as a url', () => {
+    it.todo('returns false');
+  });
+});
