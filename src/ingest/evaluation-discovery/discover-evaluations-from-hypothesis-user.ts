b8be786b9f623d6d356bd7bb0e74b89b281a2166
LinaKind <l.kinduryte@elifesciences.org>
2025-12-08 13:27:56 +0200

    Extend support of biorxiv/medrxiv short URIs to hypothesis users #4116

--- src/ingest/evaluation-discovery/discover-evaluations-from-hypothesis-user.ts
+++ src/ingest/evaluation-discovery/discover-evaluations-from-hypothesis-user.ts
@@ -4,6 +4,7 @@ import { pipe } from 'fp-ts/function';
 import * as Hyp from './hypothesis';
 import { convertHypothesisAnnotationToEvaluation } from './hypothesis/convert-hypothesis-annotation-to-evaluation';
 import { ingestionWindowStartDate } from './ingestion-window-start-date';
+import { refineIfNecessaryAnnotationUriForBiorxivMedrxiv } from './refine-if-necessary-annotation-uri-for-biorxiv-medrxiv';
 import { DiscoverPublishedEvaluations } from '../discover-published-evaluations';
 import { tagToEvaluationTypeMap } from '../tag-to-evaluation-type-map';
 
@@ -12,6 +13,7 @@ export const discoverEvaluationsFromHypothesisUser = (
 ): DiscoverPublishedEvaluations => (ingestDays) => (dependencies) => pipe(
   publisherUserId,
   Hyp.fetchEvaluationsByUserSince(ingestionWindowStartDate(ingestDays), dependencies.fetchData),
+  TE.flatMap(TE.traverseArray(refineIfNecessaryAnnotationUriForBiorxivMedrxiv(dependencies))),
   TE.map(RA.map(convertHypothesisAnnotationToEvaluation(tagToEvaluationTypeMap))),
   TE.map((parts) => ({
     understood: RA.rights(parts),
