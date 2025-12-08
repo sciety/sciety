3b6604738574ee1ffaeecf4e20bd2c28acf95179
David Moulton <d.moulton@elifesciences.org>
2025-12-08 13:43:53 +0000

    Add another regression test at a higher level in the preparation of refactoring #4116

--- test/ingest/evaluation-discovery/refine-if-necessary-annotation-uri-for-biorxiv-medrxiv.test.ts
+++ test/ingest/evaluation-discovery/refine-if-necessary-annotation-uri-for-biorxiv-medrxiv.test.ts
@@ -13,7 +13,7 @@ const dependencies = {
 describe('refine-if-necessary-annotation-uri-for-biorxiv-medrxiv', () => {
   describe('given the annotation`s uri can be parsed as a url', () => {
     describe('when the uri contains the biorxiv hostname', () => {
-      describe('and the uri contains the Cold Spring Harbor Press DOI prefix', () => {
+      describe('and the uri contains the Cold Spring Harbor Press DOI prefix (10.1101)', () => {
         const annotation = {
           ...arbitraryAnnotation(),
           uri: 'https://biorxiv.org/content/10.1101/2021.11.04.467308',
@@ -33,8 +33,24 @@ describe('refine-if-necessary-annotation-uri-for-biorxiv-medrxiv', () => {
         });
       });
 
-      describe('and the uri contains the openrxiv DOI prefix', () => {
-        it.todo('does not refine the uri of the annotation');
+      describe('and the uri contains the openrxiv DOI prefix (10.64898)', () => {
+        const annotation = {
+          ...arbitraryAnnotation(),
+          uri: 'https://biorxiv.org/content/10.64898/2021.11.04.467308',
+        };
+        let result: Annotation;
+
+        beforeEach(async () => {
+          result = await pipe(
+            annotation,
+            refineIfNecessaryAnnotationUriForBiorxivMedrxiv(dependencies),
+            TE.getOrElse(shouldNotBeCalled),
+          )();
+        });
+
+        it('does not refine the uri of the annotation', () => {
+          expect(result).toStrictEqual(annotation);
+        });
       });
 
       describe('and the uri contains neither the DOI prefixes for openrxiv nor Cold Spring Harbor Press', () => {
