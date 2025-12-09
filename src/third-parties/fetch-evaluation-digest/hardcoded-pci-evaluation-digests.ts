66af03c70ad16198e4f6696e40e8bbd5d12ac4f7
David Moulton <d.moulton@elifesciences.org>
2025-12-09 15:43:41 +0000

    Begin to build the structure of hard coded responses #4131

---
+++ src/third-parties/fetch-evaluation-digest/hardcoded-pci-evaluation-digests.ts
@@ -0,0 +1,31 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const hardcodedPciEvaluationDigests = {
+  'doi:10.24072/pci.neuro.100217.d1': `<div class="pci2-recomm-review-container" id="d-320">
+      <h4 class="pci2-recomm-text-label">Decision by <span><span><a class="cyp-user-profile-link" href="https://neuro.peercommunityin.org/public/user_public_page?userId=631"><b>Amanda Almacellas Barbanoj</b></a><a href="https://orcid.org/0000-0001-8816-2642" rel="noreferrer noopener" style="margin-right: 3px; position: relative; bottom: 12px; left: 2px" target="_blank" title="0000-0001-8816-2642"><img alt="ORCID_LOGO" heigth="12px" src="/PCINeuro/static/_25.10.1/images/ORCID_ID.svg" width="12px"></a></span> and <span><a class="cyp-user-profile-link" href="https://neuro.peercommunityin.org/public/user_public_page?userId=9"><b>Mahesh Karnani</b></a><a href="https://orcid.org/0000-0002-4810-4146" rel="noreferrer noopener" style="margin-right: 3px; position: relative; bottom: 12px; left: 2px" target="_blank" title="0000-0002-4810-4146"><img alt="ORCID_LOGO" heigth="12px" src="/PCINeuro/static/_25.10.1/images/ORCID_ID.svg" width="12px"></a></span></span>, <span><i>posted 15 Oct 2024</i></span><span><i>, validated 15 Oct 2024</i></span></h4>
+      <div class="pci2-recomm-text">
+        <p>The authors present an open source statistical power calculator in binary choice tasks often used in behavioural assessment, and demonstrate its utility with an example behavioural experiment.&nbsp;</p>
+
+<p>This preprint has been reviewed by four reviewers, who generally saw merit in the work, although some major concerns were raised by Dr Lakens and anonymous reviewer 1, that we think should be addressed in detail. Particularly, the nested structure of the data. Addressing this would be a useful service to the field, as results are sometimes tested with the assumption that repeated measurements from the same animal are independent. In addition to Dr Lakens’ references, another useful reference is <a href="https://doi.org/10.1038/nn.3648">https://doi.org/10.1038/nn.3648</a> . We feel that ideally the authors would modify their simulation code to take this into account, and perhaps make it available as an option in the GUI, such that future users can see for themselves the difference. Additionally, a figure comparing results with and without this option might be useful.&nbsp;</p>
+
+<p><br>In addition to the reviewer’s comments, the recommenders noted additional minor points:</p>
+
+<p>In order to understand how different the 2-arm and 4-arm situations were, the behavioural experiment would benefit from a more detailed description:</p>
+
+<ul>
+<li>Were groups 1 and 2 the same 6 mice? If not, please clarify in text under ‘Animals’ because it implies that this was the case. If yes, which was conducted first?&nbsp;</li>
+<li>Please clarify definitions of session and block - if they are the same, please only use one word.</li>
+<li>Was limonene the only odour?</li>
+<li>What is classified as an animal visiting an arm? (reaching the reward hole, X% of the body entering the arm, or other criterion?)</li>
+<li>How deep are the reward holes?</li>
+<li>How big is the reward?</li>
+<li>How were animals returned to the start box?</li>
+<li>How were the reward holes reset? Were they cleaned? How was randomization of the correct arm sequence determined by the investigator?</li>
+<li>Authors mention the NC3Rs, which also offer power analysis tools for optimising experimental design e.g., at https://eda.nc3rs.org.uk/. It may be worth noting how the authors’ tool compares to this.</li>
+<li>Figures need attention:<br>Fig2, chance ½ and ¼ wrong way around, panel labels in very small font<br>Fig3, x-ticks not aligned with labels or data, spelling of ‘success’, panel labels very small.<br>Fig4, too many font sizes, clarify ‘8 trials’ refers only to block 16 e.g. with an arrow.&nbsp;</li>
+</ul>
+
+
+      
+      </div>
+    </div>`,
+};
