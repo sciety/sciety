84e22d70491defc2e2bd07bca265447dfa2d4a38
LinaKind <l.kinduryte@elifesciences.org>
2025-12-12 10:31:40 +0200

    Simplify pipe logic #4132

--- src/ingest/evaluation-discovery/coar/discover-pci-evaluations.ts
+++ src/ingest/evaluation-discovery/coar/discover-pci-evaluations.ts
@@ -1,6 +1,6 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
-import { identity, pipe } from 'fp-ts/function';
+import { pipe } from 'fp-ts/function';
 import { retrieveReviewActionsFromDocmap } from './retrieve-review-actions-from-docmap';
 import { transformAnnouncementActionUriToSignpostingDocmapUri } from './transform-announcement-action-uri-to-signposting-docmap-uri';
 import {
@@ -23,8 +23,7 @@ export const discoverPciEvaluations: DiscoverPublishedEvaluations = () => (
     'urn:uuid:6d59e586-5c75-417c-ae15-6abdd8030539',
     'urn:uuid:bf3513ee-1fef-4f30-a61b-20721b505f11',
   ],
-  RA.map(transformNotificationToReviewActions(dependencies)),
-  TE.traverseArray(identity),
+  TE.traverseArray(transformNotificationToReviewActions(dependencies)),
   TE.map(RA.flatten),
   TE.map(RA.map((reviewAction) => constructPublishedEvaluation({
     publishedOn: new Date(reviewAction.actionOutputDate),
