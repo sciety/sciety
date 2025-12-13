21ba5436302f40d561bedda9d267aede82929291
nlisgo <n.lisgo@elifesciences.org>
2025-12-13 01:21:54 +0000

    fix lint error

--- src/ingest/evaluation-discovery/coar/discover-pci-evaluations.ts
+++ src/ingest/evaluation-discovery/coar/discover-pci-evaluations.ts
@@ -1,6 +1,5 @@
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
-import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as Str from 'fp-ts/string';
