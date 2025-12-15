6525ff45d971f2a05865b93c45ed7538bc85c812
LinaKind <l.kinduryte@elifesciences.org>
2025-12-15 16:17:17 +0200

    Update function return type to be able to make external calls #4136

--- src/ingest/evaluation-discovery/coar/discover-pci-evaluations.ts
+++ src/ingest/evaluation-discovery/coar/discover-pci-evaluations.ts
@@ -18,20 +18,20 @@ const transformNotificationToReviewActions = (dependencies: Dependencies) => (no
 
 const retrieveCoarNotificationsByGroup = (
   groupIdentification: string,
-): ReadonlyArray<string> => {
+): TE.TaskEither<string, ReadonlyArray<string>> => {
   if (groupIdentification === 'https://evolbiol.peercommunityin.org/coar_notify/') {
-    return [
+    return TE.right([
       'urn:uuid:0964db9c-c988-4185-891e-0c8a5c79adb9',
       'urn:uuid:13add01f-61b3-4df5-bc7f-ad4ad9fe64f8',
-    ];
+    ]);
   }
   if (groupIdentification === 'https://neuro.peercommunityin.org/coar_notify/') {
-    return [
+    return TE.right([
       'urn:uuid:6d59e586-5c75-417c-ae15-6abdd8030539',
       'urn:uuid:bf3513ee-1fef-4f30-a61b-20721b505f11',
-    ];
+    ]);
   }
-  return [];
+  return TE.right([]);
 };
 
 export const discoverPciEvaluations = (groupIdentification: string): DiscoverPublishedEvaluations => () => (
@@ -39,7 +39,7 @@ export const discoverPciEvaluations = (groupIdentification: string): DiscoverPub
 ) => pipe(
   groupIdentification,
   retrieveCoarNotificationsByGroup,
-  TE.traverseArray(transformNotificationToReviewActions(dependencies)),
+  TE.flatMap(TE.traverseArray(transformNotificationToReviewActions(dependencies))),
   TE.map(RA.flatten),
   TE.map(RA.map((reviewAction) => constructPublishedEvaluation({
     publishedOn: new Date(reviewAction.actionOutputDate),
