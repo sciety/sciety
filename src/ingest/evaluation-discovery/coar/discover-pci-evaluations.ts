f162d11f4f62fdc1cbee20fa3699611167ac9f49
David Moulton <d.moulton@elifesciences.org>
2025-12-15 14:20:33 +0000

    Extract a function to its own file to separate responsibilities #4136

--- src/ingest/evaluation-discovery/coar/discover-pci-evaluations.ts
+++ src/ingest/evaluation-discovery/coar/discover-pci-evaluations.ts
@@ -1,6 +1,7 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { retrieveCoarNotificationsByGroup } from './retrieve-coar-notifications-by-group';
 import { retrieveReviewActionsFromDocmap } from './retrieve-review-actions-from-docmap';
 import { transformAnnouncementActionUriToSignpostingDocmapUri } from './transform-announcement-action-uri-to-signposting-docmap-uri';
 import {
@@ -15,24 +16,6 @@ const transformNotificationToReviewActions = (dependencies: Dependencies) => (no
   TE.flatMap(transformAnnouncementActionUriToSignpostingDocmapUri(dependencies)),
   TE.flatMap(retrieveReviewActionsFromDocmap(dependencies)),
 );
-
-const retrieveCoarNotificationsByGroup = (
-  groupIdentification: string,
-): TE.TaskEither<string, ReadonlyArray<string>> => {
-  if (groupIdentification === 'https://evolbiol.peercommunityin.org/coar_notify/') {
-    return TE.right([
-      'urn:uuid:0964db9c-c988-4185-891e-0c8a5c79adb9',
-      'urn:uuid:13add01f-61b3-4df5-bc7f-ad4ad9fe64f8',
-    ]);
-  }
-  if (groupIdentification === 'https://neuro.peercommunityin.org/coar_notify/') {
-    return TE.right([
-      'urn:uuid:6d59e586-5c75-417c-ae15-6abdd8030539',
-      'urn:uuid:bf3513ee-1fef-4f30-a61b-20721b505f11',
-    ]);
-  }
-  return TE.right([]);
-};
 
 export const discoverPciEvaluations = (groupIdentification: string): DiscoverPublishedEvaluations => () => (
   dependencies,
