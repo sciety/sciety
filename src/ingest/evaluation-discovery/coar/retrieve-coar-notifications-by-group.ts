9583b6cdb3038d8f0dd024e87b6e4b62a45f85b9
LinaKind <l.kinduryte@elifesciences.org>
2026-01-05 17:32:20 +0200

    Remove hardcoded coar notifications for PCI #4136

--- src/ingest/evaluation-discovery/coar/retrieve-coar-notifications-by-group.ts
+++ src/ingest/evaluation-discovery/coar/retrieve-coar-notifications-by-group.ts
@@ -35,56 +35,20 @@ const isRelevantGroup = (
 
 export const retrieveCoarNotificationsByGroup = (dependencies: Dependencies) => (
   groupIdentification: string,
-): TE.TaskEither<string, ReadonlyArray<NotificationDetails>> => {
-  pipe(
-    'https://inbox-sciety-prod.elifesciences.org/inbox/',
+): TE.TaskEither<string, ReadonlyArray<NotificationDetails>> => pipe(
+  'https://inbox-sciety-prod.elifesciences.org/inbox/',
+  dependencies.fetchData,
+  TE.chainEitherK(decodeAndReportFailures(coarInboxResponseCodec)),
+  TE.map((response) => response.contains),
+  TE.map(RA.map((knownBrokenUrl) => knownBrokenUrl.replace('inboxurn', 'inbox/urn'))),
+  TE.flatMap(TE.traverseArray((notificationUrl) => pipe(
+    notificationUrl,
     dependencies.fetchData,
-    TE.chainEitherK(decodeAndReportFailures(coarInboxResponseCodec)),
-    TE.map((response) => response.contains),
-    TE.map(RA.map((knownBrokenUrl) => knownBrokenUrl.replace('inboxurn', 'inbox/urn'))),
-    TE.flatMap(TE.traverseArray((notificationUrl) => pipe(
-      notificationUrl,
-      dependencies.fetchData,
-      TE.chainEitherK(decodeAndReportFailures(notificationCodec)),
-    ))),
-    TE.map(RA.filter(isRelevantGroup(groupIdentification))),
-    TE.map(RA.map((relevantNotification) => ({
-      notificationId: relevantNotification.id,
-      announcementActionUri: relevantNotification.object.id,
-    }))),
-  );
-
-  if (groupIdentification === 'https://evolbiol.peercommunityin.org/coar_notify/') {
-    return TE.right([
-      {
-        notificationId: 'urn:uuid:0964db9c-c988-4185-891e-0c8a5c79adb9',
-        notificationType: 'coar-notify:ReviewAction',
-        announcementActionUri: 'https://evolbiol.peercommunityin.org/articles/rec?id=874#review-3751',
-        originId: 'https://evolbiol.peercommunityin.org/coar_notify/',
-      },
-      {
-        notificationId: 'urn:uuid:13add01f-61b3-4df5-bc7f-ad4ad9fe64f8',
-        notificationType: 'coar-notify:ReviewAction',
-        announcementActionUri: 'https://evolbiol.peercommunityin.org/articles/rec?id=874#review-3633',
-        originId: 'https://evolbiol.peercommunityin.org/coar_notify/',
-      },
-    ]);
-  }
-  if (groupIdentification === 'https://neuro.peercommunityin.org/coar_notify/') {
-    return TE.right([
-      {
-        notificationId: 'urn:uuid:6d59e586-5c75-417c-ae15-6abdd8030539',
-        notificationType: 'coar-notify:ReviewAction',
-        announcementActionUri: 'https://neuro.peercommunityin.org/articles/rec?id=227',
-        originId: 'https://neuro.peercommunityin.org/coar_notify/',
-      },
-      {
-        notificationId: 'urn:uuid:bf3513ee-1fef-4f30-a61b-20721b505f11',
-        notificationType: 'coar-notify:ReviewAction',
-        announcementActionUri: 'https://neuro.peercommunityin.org/articles/rec?id=217#review-549',
-        originId: 'https://neuro.peercommunityin.org/coar_notify/',
-      },
-    ]);
-  }
-  return TE.right([]);
-};
+    TE.chainEitherK(decodeAndReportFailures(notificationCodec)),
+  ))),
+  TE.map(RA.filter(isRelevantGroup(groupIdentification))),
+  TE.map(RA.map((relevantNotification) => ({
+    notificationId: relevantNotification.id,
+    announcementActionUri: relevantNotification.object.id,
+  }))),
+);
