da7dadb8260ff3d3016b41c5c846319d38ba361f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-01-19 15:23:21 +0000

    Include call to one of the COAR notification supported PCI groups #4143

--- src/ingest/ingest-based-on-coar-notifications.ts
+++ src/ingest/ingest-based-on-coar-notifications.ts
@@ -1,15 +1,20 @@
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { Dependencies } from './discover-published-evaluations';
+import { discoverPciEvaluations as discoverPciEvaluationsViaCoar } from './evaluation-discovery/coar/discover-pci-evaluations';
+import { fetchData } from './fetch-data';
+import { fetchHead } from './fetch-head';
 
-const processNotifications = () => {
-  // eslint-disable-next-line no-console
-  console.log('To be implemented');
-  return TE.right(undefined);
+const processNotifications = () => discoverPciEvaluationsViaCoar('https://zool.peercommunityin.org/coar_notify/');
+
+const dependencies: Dependencies = {
+  fetchData: fetchData(true),
+  fetchHead: fetchHead(true),
 };
 
 void (async (): Promise<unknown> => pipe(
-  processNotifications(),
+  processNotifications()(1)(dependencies),
   TE.match(
     () => 1,
     () => 0,
