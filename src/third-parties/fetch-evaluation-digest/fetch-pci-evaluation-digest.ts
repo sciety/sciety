b0e2391064bb4e345264163a2d74b2f70886df24
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-01-26 15:12:50 +0000

    Remove hardcoded content leaving the seam in place #4143

--- src/third-parties/fetch-evaluation-digest/fetch-pci-evaluation-digest.ts
+++ src/third-parties/fetch-evaluation-digest/fetch-pci-evaluation-digest.ts
@@ -4,18 +4,19 @@ import * as R from 'fp-ts/Record';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { EvaluationDigestFetcher } from './evaluation-digest-fetcher';
-import { hardcodedPciEvaluationDigests } from './hardcoded-pci-evaluation-digests';
 import { Logger } from '../../logger';
 import * as DE from '../../types/data-error';
 import { toHtmlFragment } from '../../types/html-fragment';
 import { sanitise } from '../../types/sanitised-html-fragment';
 import { QueryExternalService } from '../query-external-service';
 
+const notImplementedWaitingForChangesOnPciSide: Record<string, string> = {};
+
 export const fetchPciEvaluationDigest = (
   queryExternalService: QueryExternalService,
   logger: Logger,
 ): EvaluationDigestFetcher => (key: string) => pipe(
-  hardcodedPciEvaluationDigests,
+  notImplementedWaitingForChangesOnPciSide,
   R.lookup(key),
   O.match(
     () => TE.left(DE.unavailable),
