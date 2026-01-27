14f7a792de7e13d0cde89f5a908222ab6fb0b1be
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-01-27 15:15:08 +0000

    Finish collecting legacy ingestion related code #4143

--- src/ingest/legacy/evaluation-discovery/discover-evaluations-from-hypothesis-group.ts
+++ src/ingest/legacy/evaluation-discovery/discover-evaluations-from-hypothesis-group.ts
@@ -3,9 +3,9 @@ import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as Hyp from './hypothesis';
 import { convertHypothesisAnnotationToEvaluation } from './hypothesis/convert-hypothesis-annotation-to-evaluation';
+import { tagToEvaluationTypeMap } from './hypothesis-tag-to-evaluation-type-map';
 import { ingestionWindowStartDate } from './ingestion-window-start-date';
 import { refineIfNecessaryAnnotationUriForBiorxivMedrxiv } from './refine-if-necessary-annotation-uri-for-biorxiv-medrxiv';
-import { tagToEvaluationTypeMap } from '../../tag-to-evaluation-type-map';
 import { DiscoverPublishedEvaluations } from '../discover-published-evaluations';
 
 export const discoverEvaluationsFromHypothesisGroup = (
