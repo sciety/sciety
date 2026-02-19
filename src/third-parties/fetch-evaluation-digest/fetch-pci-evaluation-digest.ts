4ccb8aaeac7a327438e133bf2dc57d512d0db81c
LinaKind <l.kinduryte@elifesciences.org>
2026-02-19 16:32:36 +0200

    Sketch a pipe #4173

--- src/third-parties/fetch-evaluation-digest/fetch-pci-evaluation-digest.ts
+++ src/third-parties/fetch-evaluation-digest/fetch-pci-evaluation-digest.ts
@@ -1,25 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import * as O from 'fp-ts/Option';
-import * as R from 'fp-ts/Record';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
+import { identity, pipe } from 'fp-ts/function';
 import { EvaluationDigestFetcher } from './evaluation-digest-fetcher';
 import { Logger } from '../../logger';
-import * as DE from '../../types/data-error';
 import { toHtmlFragment } from '../../types/html-fragment';
 import { sanitise } from '../../types/sanitised-html-fragment';
 import { QueryExternalService } from '../query-external-service';
 
-const notImplementedWaitingForChangesOnPciSide: Record<string, string> = {};
+const extractPciGroupAbbreviation = identity;
+
+const constructPciWebContentUrl = (key: string) => identity;
 
 export const fetchPciEvaluationDigest = (
   queryExternalService: QueryExternalService,
   logger: Logger,
 ): EvaluationDigestFetcher => (key: string) => pipe(
-  notImplementedWaitingForChangesOnPciSide,
-  R.lookup(key),
-  O.match(
-    () => TE.left(DE.unavailable),
-    (html) => TE.right(sanitise(toHtmlFragment(html))),
-  ),
+  key,
+  extractPciGroupAbbreviation,
+  constructPciWebContentUrl(key),
+  queryExternalService('error'),
+  TE.map((html) => sanitise(toHtmlFragment(html as string))),
 );
