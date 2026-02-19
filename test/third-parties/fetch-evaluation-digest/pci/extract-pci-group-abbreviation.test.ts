3e9bc17d162b9de91bfb6dec0044d030a5660530
LinaKind <l.kinduryte@elifesciences.org>
2026-02-19 18:01:43 +0200

    Add regression tests #4173

---
+++ test/third-parties/fetch-evaluation-digest/pci/extract-pci-group-abbreviation.test.ts
@@ -0,0 +1,17 @@
+import * as E from 'fp-ts/Either';
+import { extractPciGroupAbbreviation } from '../../../../src/third-parties/fetch-evaluation-digest/pci/extract-pci-group-abbreviation';
+
+describe('extract-pci-group-abbreviation', () => {
+  describe('given supported input', () => {
+    describe.each([
+      ['10.24072/pci.neuro.100228.rev21', 'neuro'],
+      ['10.24072/pci.evolbiol.100128', 'evolbiol'],
+    ])('%s', (input, expectedAbbreviation) => {
+      it('extracts the abbreviation', () => {
+        const result = extractPciGroupAbbreviation(input);
+
+        expect(result).toStrictEqual(E.right(expectedAbbreviation));
+      });
+    });
+  });
+});
