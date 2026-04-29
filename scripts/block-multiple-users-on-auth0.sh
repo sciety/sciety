457c674df72d5381dbffdcda14e14b4785420337
LinaKind <l.kinduryte@elifesciences.org>
2026-04-29 15:51:01 +0300

    Account for rate limiting in an Auth0 script
    https://github.com/elifesciences/issues/issues/9528

--- scripts/block-multiple-users-on-auth0.sh
+++ scripts/block-multiple-users-on-auth0.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-cat "$1" | xargs -I{} curl -L -g -X PATCH 'https://sciety.eu.auth0.com/api/v2/users/{}' \
--H "Authorization: Bearer $secret" \
--H 'Content-Type: application/json' \
--H 'Accept: application/json' \
---data-raw '{"blocked":true}'
+while IFS= read -r user_id; do
+  curl -L -g -X PATCH "https://sciety.eu.auth0.com/api/v2/users/${user_id}" \
+  -H "Authorization: Bearer secret" \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  --data-raw '{"blocked":true}'
+  sleep 0.5
+done < "$1"
