6066626b819d079c5d5d60c530adc845d9f88ac4
LinaKind <l.kinduryte@elifesciences.org>
2026-04-30 17:35:54 +0300

    Ensure the overview displays most recently updated lists first
    part of agreed architectural decision: renderers are responsible for sorting
    
    https://github.com/elifesciences/issues/issues/9530

--- src/read-models/lists/get-all-user-lists.ts
+++ src/read-models/lists/get-all-user-lists.ts
@@ -1,3 +1,5 @@
+import * as D from 'fp-ts/Date';
+import * as Ord from 'fp-ts/Ord';
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
 import { ReadModel } from './handle-event';
@@ -5,6 +7,12 @@ import { List } from './list';
 import { isGroupId } from '../../types/list-owner-id';
 
 type GetAllUserLists = () => ReadonlyArray<List>;
+
+export const byMostRecentlyUpdated: Ord.Ord<List> = pipe(
+  D.Ord,
+  Ord.reverse,
+  Ord.contramap((entry) => entry.updatedAt),
+);
 
 export const getAllUserLists = (
   readModel: ReadModel,
--- src/read-side/non-html-views/api/lists/construct-view-model.ts
+++ src/read-side/non-html-views/api/lists/construct-view-model.ts
@@ -1,10 +1,12 @@
 import * as RA from 'fp-ts/ReadonlyArray';
 import { pipe } from 'fp-ts/function';
+import { byMostRecentlyUpdated } from '../../../../read-models/lists/get-all-user-lists';
 import { DependenciesForViews } from '../../../dependencies-for-views';
 import { ApiData } from '../render-as-json';
 
 export const constructViewModel = (dependencies: DependenciesForViews): ApiData => pipe(
   dependencies.getAllUserLists(),
+  RA.sort(byMostRecentlyUpdated),
   RA.map((list) => ({
     listId: list.id,
     listName: list.name,
